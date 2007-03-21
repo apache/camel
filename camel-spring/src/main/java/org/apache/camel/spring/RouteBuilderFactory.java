aa24907ba5aa1594c74a3208e511fb75160be9d1
Hiram R. Chirino <chirino@apache.org>
2007-03-21 22:53:32 +0000

    renamed camel-xbean to camel-spring since this is turning out to be more of a spring integration.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@521035 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-spring/src/main/java/org/apache/camel/spring/RouteBuilderFactory.java
@@ -0,0 +1,68 @@
+package org.apache.camel.spring;
+
+import java.util.ArrayList;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.FactoryBean;
+
+public class RouteBuilderFactory implements FactoryBean, BeanFactoryAware {
+	private ArrayList<RouteBuilderStatement> routes;
+	private boolean singleton;
+	private BeanFactory beanFactory;
+
+	class SpringRouteBuilder extends RouteBuilder {
+		private ArrayList<RouteBuilderStatement> routes;
+		private BeanFactory beanFactory;
+
+		@Override
+		public void configure() {
+			for (RouteBuilderStatement routeFactory : routes) {
+				routeFactory.create(beanFactory, this);
+			}
+		}
+
+		public ArrayList<RouteBuilderStatement> getRoutes() {
+			return routes;
+		}
+		public void setRoutes(ArrayList<RouteBuilderStatement> routes) {
+			this.routes = routes;
+		}
+
+		public void setBeanFactory(BeanFactory beanFactory) {
+			this.beanFactory = beanFactory;
+		}
+	}
+	
+	public Object getObject() throws Exception {
+		SpringRouteBuilder builder = new SpringRouteBuilder();
+		builder.setBeanFactory(beanFactory);
+		builder.setRoutes(routes);
+		return builder;
+	}
+
+	public Class getObjectType() {
+		return SpringRouteBuilder.class;
+	}
+
+	public boolean isSingleton() {
+		return singleton;
+	}
+	public void setSingleton(boolean singleton) {
+		this.singleton = singleton;
+	}
+
+	public ArrayList<RouteBuilderStatement> getRoutes() {
+		return routes;
+	}
+	public void setRoutes(ArrayList<RouteBuilderStatement> routes) {
+		this.routes = routes;
+	}
+
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
+
+}
