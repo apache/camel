92a0ec859d4174dfe4d9f4a1a774aa6911dd33c4
Hiram R. Chirino <chirino@apache.org>
2007-03-21 15:30:58 +0000

    Started working on a CamelNamespaceHandler for spring.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@520915 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-xbean/src/main/java/org/apache/camel/spring/CamelNamespaceHandler.java
@@ -0,0 +1,11 @@
+package org.apache.camel.spring;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class CamelNamespaceHandler extends NamespaceHandlerSupport {
+
+	public void init() {
+		 registerBeanDefinitionParser("routeBuilder", new CamelBeanDefinitionParser());    
+	}
+
+}
---
+++ camel-xbean/src/main/java/org/apache/camel/spring/RouteBuilderStatement.java
@@ -0,0 +1,11 @@
+package org.apache.camel.spring;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class RouteBuilderStatement {
+
+	public void create(RouteBuilder builder) {
+		// TODO Auto-generated method stub
+		
+	}
+}
