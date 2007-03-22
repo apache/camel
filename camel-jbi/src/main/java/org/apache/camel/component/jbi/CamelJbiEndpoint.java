ce56e9f95d10340c86aed5a07fc3b1e7476d9622
Robert Davies <rajdavies@apache.org>
2007-03-22 18:02:53 +0000

    Use DefaultComponent
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@521386 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-jbi/src/main/java/org/apache/camel/component/jbi/CamelJbiEndpoint.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.apache.camel.component.jbi;
+
+import javax.jbi.messaging.MessageExchange;
+import javax.jbi.messaging.NormalizedMessage;
+import javax.xml.namespace.QName;
+import org.apache.servicemix.common.endpoints.ProviderEndpoint;
+
+/**
+ * The endpoint in the service engine
+ * 
+ * @version $Revision: 426415 $
+ */
+public class CamelJbiEndpoint extends ProviderEndpoint{
+
+    private static final QName SERVICE_NAME=new QName("http://camel.servicemix.org","CamelEndpointComponent");
+    private JbiEndpoint jbiEndpoint;
+
+    public CamelJbiEndpoint(JbiEndpoint jbiEndpoint){
+        this.jbiEndpoint=jbiEndpoint;
+        this.service=SERVICE_NAME;
+        this.endpoint=jbiEndpoint.getEndpointUri();
+    }
+
+    protected void processInOnly(MessageExchange exchange,NormalizedMessage in) throws Exception{
+        /*
+         * ToDo
+         */
+    }
+
+    protected void processInOut(MessageExchange exchange,NormalizedMessage in,NormalizedMessage out) throws Exception{
+        /*
+         * ToDo
+         */
+    }
+}
