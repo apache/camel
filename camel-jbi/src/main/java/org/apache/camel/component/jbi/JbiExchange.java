b65fe1d5e6dd35d1ef5cbb43d3e643ad6c6e514b
James Strachan <jstrachan@apache.org>
2007-03-22 13:59:34 +0000

    more tidying up of the Exchange/Message APIs and an initial JBI implementation
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@521274 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-jbi/src/main/java/org/apache/camel/component/jbi/JbiExchange.java
@@ -0,0 +1,93 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jbi;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultExchange;
+
+import javax.jbi.messaging.MessageExchange;
+
+/**
+ * An {@link Exchange} working with JBI which exposes the underlying JBI features such as the 
+ * JBI {@link #getMessageExchange()}
+ *
+ * @version $Revision$
+ */
+public class JbiExchange extends DefaultExchange {
+    private final JbiBinding binding;
+    private MessageExchange messageExchange;
+
+    public JbiExchange(CamelContext context, JbiBinding binding) {
+        super(context);
+        this.binding = binding;
+    }
+
+    public JbiExchange(CamelContext context, JbiBinding binding, MessageExchange messageExchange) {
+        super(context);
+        this.binding = binding;
+        this.messageExchange = messageExchange;
+
+        // TODO we could maybe use the typesafe APIs of different derived APIs from JBI 
+        setIn(new JbiMessage(messageExchange.getMessage("in")));
+        setOut(new JbiMessage(messageExchange.getMessage("out")));
+        setFault(new JbiMessage(messageExchange.getMessage("fault")));
+    }
+
+    /**
+     * Returns the underlying JBI message exchange for an inbound exchange
+     * or null for outbound messages
+     *
+     * @return the inbound message exchange
+     */
+    public MessageExchange getMessageExchange() {
+        return messageExchange;
+    }
+
+    @Override
+    public JbiMessage getIn() {
+        return (JbiMessage) super.getIn();
+    }
+
+    @Override
+    public JbiMessage getOut() {
+        return (JbiMessage) super.getOut();
+    }
+
+    @Override
+    public JbiMessage getFault() {
+        return (JbiMessage) super.getFault();
+    }
+
+    /**
+     * @return the Camel <-> JBI binding
+     */
+    public JbiBinding getBinding() {
+        return binding;
+    }
+
+    @Override
+    protected JbiMessage createInMessage() {
+        return new JbiMessage();
+    }
+
+    @Override
+    protected JbiMessage createOutMessage() {
+        return new JbiMessage();
+    }
+}
