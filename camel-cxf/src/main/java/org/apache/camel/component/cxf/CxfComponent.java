a58f02a2b088767414a4745a666561f50ce0b579
James Strachan <jstrachan@apache.org>
2007-03-23 14:50:19 +0000

    added initial shell of a CXF componet
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@521746 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfComponent.java
@@ -0,0 +1,57 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.cxf;
+
+import org.apache.camel.impl.DefaultComponent;
+import org.apache.camel.CamelContext;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URI;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
+
+/**
+ * @version $Revision$
+ */
+public class CxfComponent extends DefaultComponent<CxfExchange> {
+    private Map<String, CxfEndpoint> map = new HashMap<String, CxfEndpoint>();
+
+    public CxfComponent() {
+    }
+
+    public CxfComponent(CamelContext context) {
+        super(context);
+    }
+
+    public synchronized CxfEndpoint createEndpoint(String uri, String[] urlParts) throws IOException, URISyntaxException {
+        CxfEndpoint endpoint = map.get(uri);
+        if (endpoint == null) {
+            String remainingUrl = uri.substring("mina:".length());
+            URI u = new URI(remainingUrl);
+
+            String protocol = u.getScheme();
+
+            map.put(uri, endpoint);
+        }
+        return endpoint;
+    }
+
+}
---
+++ camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfEndpoint.java
@@ -0,0 +1,66 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.cxf;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultEndpoint;
+
+/**
+ * The endpoint in the service engine
+ *
+ * @version $Revision$
+ */
+public class CxfEndpoint extends DefaultEndpoint<CxfExchange> {
+    private CxfBinding binding;
+
+    protected CxfEndpoint(String uri, CamelContext camelContext) {
+        super(uri, camelContext);
+    }
+
+    public void onExchange(CxfExchange cxfExchange) {
+        // TODO send into CXF
+    }
+
+    public CxfExchange createExchange() {
+        return new CxfExchange(getContext(), getBinding());
+    }
+
+    public CxfBinding getBinding() {
+        if (binding == null) {
+            binding = new CxfBinding();
+        }
+        return binding;
+    }
+
+    public void setBinding(CxfBinding binding) {
+        this.binding = binding;
+    }
+
+    @Override
+    protected void doActivate() throws Exception {
+        super.doActivate();
+
+        // TODO process any inbound messages from CXF
+
+        Processor<CxfExchange> processor = getInboundProcessor();
+        if (processor != null) {
+
+        }
+    }
+}
