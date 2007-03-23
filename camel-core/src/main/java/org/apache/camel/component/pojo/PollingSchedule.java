aff2217ca9f9bd66c1245d67b220b03607f91dca
Robert Davies <rajdavies@apache.org>
2007-03-23 16:12:16 +0000

    Add a polling component
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@521788 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/component/pojo/PollingSchedule.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.pojo;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * Schedule used for polling an endpoint
+ *
+ */
+class PollingSchedule{
+
+    private String name;
+    private Timer timer;
+    private Date time;
+    private long period=-1;
+    private long delay=-1;
+    private boolean fixedRate;
+    private boolean daemon=true;
+
+    PollingSchedule(String name,Date time){
+        this(name,time,-1,-1,false);
+    }
+
+    PollingSchedule(String name,Date time,long period){
+        this(name,time,-1,period,false);
+    }
+
+    PollingSchedule(String name,long delay){
+        this(name,null,delay,-1,false);
+    }
+    
+    PollingSchedule(String name,long delay,long period){
+        this(name,null,delay,period,false);
+    }
+
+    PollingSchedule(String name,Date time,long delay,long period,boolean fixedRate){
+        this.name=name;
+        this.time=time;
+        this.delay=delay;
+        this.period=period;
+        this.fixedRate=fixedRate;
+    }
+
+    void activate(Runnable run){
+        timer=createTimerAndTask(name,run);
+    }
+
+    void deactivate(){
+        if(timer!=null){
+            timer.cancel();
+        }
+    }
+
+    private Timer createTimerAndTask(String name,final Runnable run){
+        TimerTask task=new TimerTask(){
+
+            @Override public void run(){
+                run.run();
+            }
+        };
+        Timer result=new Timer(name,daemon);
+        if(fixedRate){
+            if(time!=null){
+                result.scheduleAtFixedRate(task,time,period);
+            }else{
+                result.scheduleAtFixedRate(task,delay,period);
+            }
+        }else{
+            if(time!=null){
+                if(period>=0){
+                    result.schedule(task,time,period);
+                }else{
+                    result.schedule(task,time);
+                }
+            }else{
+                if(period>=0){
+                    result.schedule(task,delay,period);
+                }else{
+                    result.schedule(task,delay);
+                }
+            }
+        }
+        return result;
+    }
+}
