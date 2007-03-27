2835f7715458e89a55c4d24a6968bc0f44a0a343
James Strachan <jstrachan@apache.org>
2007-03-27 13:41:09 +0000

    a rough initial spike attempting to port the JMS transport for CXF to Camel
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@522906 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-cxf/src/main/java/org/apache/camel/component/cxf/transport/CamelTransportFactory.java
@@ -0,0 +1,88 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.cxf.transport;
+
+import org.apache.camel.CamelContext;
+import org.apache.cxf.Bus;
+import org.apache.cxf.configuration.Configurer;
+import org.apache.cxf.service.model.EndpointInfo;
+import org.apache.cxf.transport.AbstractTransportFactory;
+import org.apache.cxf.transport.Conduit;
+import org.apache.cxf.transport.ConduitInitiator;
+import org.apache.cxf.transport.Destination;
+import org.apache.cxf.transport.DestinationFactory;
+import org.apache.cxf.ws.addressing.EndpointReferenceType;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @version $Revision$
+ */
+public class CamelTransportFactory extends AbstractTransportFactory implements ConduitInitiator, DestinationFactory {
+    private static final Set<String> URI_PREFIXES = new HashSet<String>();
+
+    static {
+        URI_PREFIXES.add("camel://");
+    }
+
+    private Bus bus;
+    private CamelContext camelContext;
+
+    @Resource
+    public void setBus(Bus b) {
+        bus = b;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public CamelContext getCamelContext() {
+        return camelContext;
+    }
+
+    @Resource
+    public void setCamelContext(CamelContext camelContext) {
+        this.camelContext = camelContext;
+    }
+
+    public Conduit getConduit(EndpointInfo targetInfo) throws IOException {
+        return getConduit(targetInfo, null);
+    }
+
+    public Conduit getConduit(EndpointInfo endpointInfo, EndpointReferenceType target) throws IOException {
+        return new CamelConduit(camelContext, bus, endpointInfo, target);
+    }
+
+    public Destination getDestination(EndpointInfo endpointInfo) throws IOException {
+        CamelDestination destination = new CamelDestination(camelContext, bus, this, endpointInfo);
+        Configurer configurer = bus.getExtension(Configurer.class);
+        if (null != configurer) {
+            configurer.configureBean(destination);
+        }
+        return destination;
+    }
+
+    public Set<String> getUriPrefixes() {
+        return URI_PREFIXES;
+    }
+}
+
