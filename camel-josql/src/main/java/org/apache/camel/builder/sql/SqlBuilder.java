b9aa5e27c972fd55c2c69adbd95cf7cab79c5b7c
James Strachan <jstrachan@apache.org>
2007-04-16 08:37:25 +0000

    added support for SQL expressions and predicates on POJOs using JoSQL
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@529167 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-josql/src/main/java/org/apache/camel/builder/sql/SqlBuilder.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.builder.sql;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.Message;
+import org.apache.camel.Predicate;
+import org.apache.camel.RuntimeExpressionException;
+import org.josql.Query;
+import org.josql.QueryExecutionException;
+import org.josql.QueryParseException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+
+/**
+ * A builder of SQL {@link org.apache.camel.Expression} and {@link org.apache.camel.Predicate} implementations
+ *
+ * @version $Revision: $
+ */
+public class SqlBuilder<E extends Exchange> implements Expression<E>, Predicate<E> {
+
+    private Query query;
+    private Map<String,Object> variables = new HashMap<String, Object>();
+
+    public SqlBuilder(Query query) {
+        this.query = query;
+    }
+
+    public Object evaluate(E exchange) {
+        return evaluateQuery(exchange);
+    }
+
+    public boolean matches(E exchange) {
+        List list = evaluateQuery(exchange);
+        if (!list.isEmpty()) {
+            Object value = list.get(0);
+            if (value instanceof Boolean) {
+                Boolean flag = (Boolean) value;
+                return flag.booleanValue();
+            }
+            else {
+                // lets assume non-empty results are true
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Builder API
+    //-----------------------------------------------------------------------
+
+    /**
+     * Creates a new builder for the given SQL query string
+     *
+     * @param sql the SQL query to perform
+     * @return a new builder
+     * @throws QueryParseException if there is an issue with the SQL
+     */
+    public static <E extends Exchange> SqlBuilder<E> sql(String sql) throws QueryParseException {
+        Query q = new Query();
+        q.parse(sql);
+        return new SqlBuilder(q);
+    }
+
+    /**
+     * Adds the variable value to be used by the SQL query
+     */
+    public SqlBuilder<E> variable(String name, Object value) {
+        getVariables().put(name, value);
+        return this;
+    }
+
+
+    // Properties
+    //-----------------------------------------------------------------------
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> properties) {
+        this.variables = properties;
+    }
+
+
+    // Implementation methods
+    //-----------------------------------------------------------------------
+    protected List evaluateQuery(E exchange) {
+        configureQuery(exchange);
+        Message in = exchange.getIn();
+        List list = in.getBody(List.class);
+        if (list == null) {
+            list = Collections.singletonList(in.getBody());
+        }
+        try {
+            return query.execute(list).getResults();
+        }
+        catch (QueryExecutionException e) {
+            throw new RuntimeExpressionException(e);
+        }
+    }
+
+    protected void configureQuery(E exchange) {
+        // lets pass in the headers as variables that the SQL can use
+        addVariables(exchange.getProperties());
+        addVariables(exchange.getIn().getHeaders());
+        addVariables(getVariables());
+
+        query.setVariable("exchange", exchange);
+        query.setVariable("in", exchange.getIn());
+        query.setVariable("out", exchange.getOut());
+    }
+
+    protected void addVariables(Map <String, Object> map) {
+        Set<Map.Entry<String, Object>> propertyEntries = map.entrySet();
+        for (Map.Entry<String, Object> entry : propertyEntries) {
+            query.setVariable(entry.getKey(), entry.getValue());
+        }
+    }
+}
---
+++ camel-josql/src/test/java/org/apache/camel/builder/sql/SqlTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.builder.sql;
+
+import static org.apache.camel.builder.sql.SqlBuilder.sql;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.Message;
+import org.apache.camel.TestSupport;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+
+import java.util.List;
+
+/**
+ * @version $Revision: $
+ */
+public class SqlTest extends TestSupport {
+
+    protected CamelContext context = new DefaultCamelContext();
+    protected Exchange exchange;
+
+    public void testExpression() throws Exception {
+        Expression expression = sql("SELECT * FROM org.apache.camel.builder.sql.Person where city = 'London'");
+        Object value = expression.evaluate(exchange);
+        assertIsInstanceOf(List.class, value);
+
+        List list = (List) value;
+        assertEquals("List size", 2, list.size());
+
+        for (Object person : list) {
+            log.info("Found: " + person);
+        }
+    }
+
+    public void testExpressionWithHeaderVariable() throws Exception {
+        Expression expression = sql("SELECT * FROM org.apache.camel.builder.sql.Person where name = :fooHeader");
+        Object value = expression.evaluate(exchange);
+        assertIsInstanceOf(List.class, value);
+
+        List<Person> list = (List<Person>) value;
+        assertEquals("List size", 1, list.size());
+
+        for (Person person : list) {
+            log.info("Found: " + person);
+
+            assertEquals("name", "James", person.getName());
+        }
+    }
+
+    public void testPredicates() throws Exception {
+        assertPredicate(sql("SELECT * FROM org.apache.camel.builder.sql.Person where city = 'London'"), exchange, true);
+        assertPredicate(sql("SELECT * FROM org.apache.camel.builder.sql.Person where city = 'Manchester'"), exchange, false);
+    }
+
+    public void testPredicateWithHeaderVariable() throws Exception {
+        assertPredicate(sql("SELECT * FROM org.apache.camel.builder.sql.Person where name = :fooHeader"), exchange, true);
+    }
+
+    protected void setUp() throws Exception {
+        exchange = createExchange();
+    }
+
+    protected Exchange createExchange() {
+        Exchange exchange = new DefaultExchange(context);
+        Message message = exchange.getIn();
+        message.setHeader("fooHeader", "James");
+
+        Person[] people = {new Person("James", "London"),
+                new Person("Guillaume", "Normandy"),
+                new Person("Rob", "London"),
+                new Person("Hiram", "Tampa")};
+
+        message.setBody(people);
+        return exchange;
+    }
+}
