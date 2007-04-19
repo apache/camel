09924af51d845e04d9b660750020650fb11b497d
Hiram R. Chirino <chirino@apache.org>
2007-04-19 14:44:00 +0000

    Added a direct endpoint component
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@530433 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/component/direct/DirectEndpoint.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.direct;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.apache.camel.Consumer;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.impl.DefaultConsumer;
+import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultProducer;
+
+/**
+ * Represents a direct endpoint that synchronously invokes the consumers of the endpoint when a producer 
+ * sends a message to it.
+ *
+ * @org.apache.xbean.XBean
+ * @version $Revision: 519973 $
+ */
+public class DirectEndpoint<E extends Exchange> extends DefaultEndpoint<E> {
+
+	private final CopyOnWriteArrayList<DefaultConsumer<E>> consumers = new CopyOnWriteArrayList<DefaultConsumer<E>>();
+	
+	boolean allowMutlipleConsumers=true;	
+	
+    public DirectEndpoint(String uri, DirectComponent<E> component) {
+        super(uri, component);
+    }
+
+    public Producer<E> createProducer() throws Exception {
+        return startService(new DefaultProducer<E>(this) {
+            public void process(E exchange) {
+            	DirectEndpoint.this.process(exchange);
+            }
+        });    	
+    }
+
+    protected void process(E exchange) {
+    	for (DefaultConsumer<E> consumer : consumers) {
+			consumer.getProcessor().process(exchange);
+		}
+	}
+
+	public Consumer<E> createConsumer(Processor<E> processor) throws Exception {
+		DefaultConsumer<E> consumer = new DefaultConsumer<E>(this, processor) {
+			@Override
+			public void start() throws Exception {
+				if( !allowMutlipleConsumers && !consumers.isEmpty() )
+					throw new IllegalStateException("Endpoint "+getEndpointUri()+" only allows 1 active consumer but you attempted to start a 2nd consumer.");
+				
+				consumers.add(this);
+				super.start();
+			}
+			
+			@Override
+			public void stop() throws Exception {
+				super.stop();
+				consumers.remove(this);
+			}
+		};
+		
+		return consumer;
+    }
+
+    public E createExchange() {
+    	// How can we create a specific Exchange if we are generic??
+    	// perhaps it would be better if we did not implement this. 
+        return (E) new DefaultExchange(getContext());
+    }
+
+	public boolean isAllowMutlipleConsumers() {
+		return allowMutlipleConsumers;
+	}
+	public void setAllowMutlipleConsumers(boolean allowMutlipleConsumers) {
+		this.allowMutlipleConsumers = allowMutlipleConsumers;
+	}
+
+
+}
