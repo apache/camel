35c685c13f736cc5582d77968b7c56d2d244c854
James Strachan <jstrachan@apache.org>
2007-04-19 13:50:08 +0000

    added a mock endpoint which can be used for easier testing in a JMock sense; where expectations can be put on endpoints, then a route run and the assertions then applied
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@530414 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/processor/FilterTest.java
@@ -0,0 +1,99 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.processor;
+
+import junit.framework.TestCase;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.Message;
+import org.apache.camel.Endpoint;
+import org.apache.camel.TestSupport;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.util.ProducerCache;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class FilterTest extends TestSupport {
+    protected CamelContext context = new DefaultCamelContext();
+    protected Endpoint<Exchange> startEndpoint;
+    protected MockEndpoint resultEndpoint;
+    protected ProducerCache<Exchange> client = new ProducerCache<Exchange>();
+
+    public void testSendMatchingMessage() throws Exception {
+        resultEndpoint.expectedMessageCount(1);
+
+        sendMessage("bar", "matched");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    public void testSendNotMatchingMessage() throws Exception {
+        resultEndpoint.expectedMessageCount(0);
+
+        sendMessage("notMatchedHeaderValue", "notMatched");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        context.addRoutes(createRouteBuilder());
+
+        startEndpoint = resolveMandatoryEndpoint(context, "queue:a");
+        resultEndpoint = (MockEndpoint)resolveMandatoryEndpoint(context, "mock:result");
+
+        context.start();
+    }
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder<Exchange>() {
+            public void configure() {
+                from("queue:a").filter(header("foo").isEqualTo("bar")).to("mock:result");
+            }
+        };
+    }
+
+    protected void sendMessage(final Object headerValue, final Object body) throws Exception {
+        client.send(startEndpoint, new Processor<Exchange>() {
+            public void process(Exchange exchange) {
+                // now lets fire in a message
+                Message in = exchange.getIn();
+                in.setBody(body);
+                in.setHeader("foo", headerValue);
+            }
+        });
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        client.stop();
+        context.stop();
+    }
+}
