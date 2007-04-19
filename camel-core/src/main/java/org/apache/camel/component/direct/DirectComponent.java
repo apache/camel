eea364a6fe490cfa94d7e8c0871bef5369c7ddb8
Hiram R. Chirino <chirino@apache.org>
2007-04-19 14:44:00 +0000

    Added a direct endpoint component
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@530433 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/component/direct/DirectComponent.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.direct;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+
+/**
+ * Represents the component that manages {@link DirectEndpoint}.  It holds the 
+ * list of named direct endpoints.
+ *
+ * @org.apache.xbean.XBean
+ * @version $Revision: 519973 $
+ */
+public class DirectComponent<E extends Exchange> implements Component<E> {
+
+	private CamelContext context;
+
+	public CamelContext getCamelContext() {
+		return context;
+	}
+
+	public ScheduledExecutorService getExecutorService() {
+		return null;
+	}
+
+	public Endpoint<E> resolveEndpoint(String uri) throws Exception {
+		return new DirectEndpoint<E>(uri,this);
+	}
+
+	public void setCamelContext(CamelContext context) {
+		this.context = context;
+	}	
+
+}
