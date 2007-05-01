a6aa7819dd46bb4d04bb16f99887d521812424aa
Hiram R. Chirino <chirino@apache.org>
2007-05-01 13:57:33 +0000

    Added a camel-rmi component that can bind PojoExchanges to RMI/JRMP
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@534095 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-rmi/src/main/java/org/apache/camel/component/rmi/RmiProducer.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.rmi;
+
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+
+import org.apache.camel.component.pojo.PojoEndpoint;
+import org.apache.camel.component.pojo.PojoExchange;
+import org.apache.camel.impl.DefaultProducer;
+
+/**
+ * @version $Revision: 533076 $
+ */
+public class RmiProducer extends DefaultProducer<PojoExchange> {
+
+	private final RmiEndpoint endpoint;
+	private Remote remote;
+
+	public RmiProducer(RmiEndpoint endpoint) throws AccessException, RemoteException, NotBoundException {
+		super(endpoint);
+		this.endpoint = endpoint;
+	}
+
+	public void process(PojoExchange exchange) throws AccessException, RemoteException, NotBoundException {
+		
+		PojoEndpoint.invoke(getRemote(), exchange);
+	}
+
+	public Remote getRemote() throws AccessException, RemoteException, NotBoundException {
+		if( remote == null ) {
+			Registry registry = endpoint.getRegistry();				
+			remote = registry.lookup(endpoint.getName());			
+		}
+		return remote;
+	}
+	
+}
---
+++ camel-rmi/src/test/java/org/apache/camel/component/rmi/RmiRouteTest.java
@@ -0,0 +1,98 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.rmi;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.rmi.registry.LocateRegistry;
+
+import junit.framework.TestCase;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.pojo.PojoComponent;
+import org.apache.camel.component.pojo.PojoConsumer;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * @version $Revision: 520220 $
+ */
+public class RmiRouteTest extends TestCase {
+	
+    public void testPojoRoutes() throws Exception {    	
+    	if( classPathHasSpaces() ) {
+    		return;
+    	}
+    	
+    	// Boot up a local RMI registry
+    	LocateRegistry.createRegistry(1099);    	
+    	
+        CamelContext camelContext = new DefaultCamelContext();
+        
+        // START SNIPPET: register
+        PojoComponent component = new PojoComponent();
+        camelContext.addComponent("pojo", component);
+        component.addService("bye", new SayService("Good Bye!"));
+        // END SNIPPET: register
+        
+        // START SNIPPET: route
+        // lets add simple route
+        camelContext.addRoutes(new RouteBuilder() {
+            public void configure() {
+            	from("pojo:hello").to("rmi://localhost:1099/bye");                
+
+            	// When exposing an RMI endpoint, the interfaces it exposes must be configured.
+                RmiEndpoint bye = (RmiEndpoint) endpoint("rmi://localhost:1099/bye");
+            	bye.setRemoteInterfaces(ISay.class);
+                from(bye).to("pojo:bye");
+            }
+        });
+        // END SNIPPET: route
+
+        camelContext.start();
+        
+        // START SNIPPET: invoke
+        PojoConsumer consumer = component.getConsumer("hello");        
+        ISay proxy = consumer.createProxy(ISay.class);
+        String rc = proxy.say();
+        assertEquals("Good Bye!", rc);
+        // END SNIPPET: invoke
+        
+        camelContext.stop();
+    }
+
+	private boolean classPathHasSpaces() {		
+		ClassLoader cl = getClass().getClassLoader();
+		if( cl instanceof URLClassLoader ) {
+			URLClassLoader ucl = (URLClassLoader) cl;
+			URL[] urls = ucl.getURLs();
+			for (int i = 0; i < urls.length; i++) {
+				if( urls[i].getPath().contains(" ") ) {
+					System.err.println("=======================================================================");
+					System.err.println(" TEST Skipped: "+getName());
+					System.err.println("   Your probably on windows.  We detected that the classpath");
+					System.err.println("   has a space in it.  Try running maven with the following option: ");
+					System.err.println("   -Dmaven.repo.local=C:\\DOCUME~1\\userid\\.m2\\repository");
+					System.err.println("=======================================================================");
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
