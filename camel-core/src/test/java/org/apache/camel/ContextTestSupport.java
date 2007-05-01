f6a983a32391cef12bcc47d3392641524d90129a
James Strachan <jstrachan@apache.org>
2007-05-01 13:42:05 +0000

    added XSLT test case
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@534084 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/ContextTestSupport.java
+++ camel-core/src/test/java/org/apache/camel/ContextTestSupport.java
@@ -51,7 +51,7 @@ public abstract class ContextTestSupport extends TestSupport {
         return new DefaultCamelContext();
     }
 
-    protected RouteBuilder createRouteBuilder() {
+    protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             public void configure() {
                 // no routes added by default
---
+++ camel-core/src/test/resources/org/apache/camel/builder/xml/example.xsl
@@ -0,0 +1,10 @@
+<?xml version = "1.0"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+  <xsl:template match="/">
+    <goodbye>
+      <xsl:value-of select="/hello"/>
+    </goodbye>
+  </xsl:template>
+
+</xsl:stylesheet>
