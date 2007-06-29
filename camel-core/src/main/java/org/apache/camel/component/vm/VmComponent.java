2bb028af8c240de0093dc5313505659d1fd98d77
James Strachan <jstrachan@apache.org>
2007-06-29 17:42:37 +0000

    added a VM component for communicating across web applications providing the camel-core is on the classpath - for CAMEL-59
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@551974 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/component/vm/VmComponent.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.vm;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Endpoint;
+import org.apache.camel.component.queue.QueueComponent;
+import org.apache.camel.component.queue.QueueEndpoint;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * An implementation of the <a href="http://activemq.apache.org/camel/vm.html">VM components</a>
+ * for asynchronous SEDA exchanges on a {@link BlockingQueue} within the classloader tree containing
+ * the camel-core.jar. i.e. to handle communicating across CamelContext instances and possibly across
+ * web application contexts, providing that camel-core.jar is on the system classpath.
+ *
+ * @version $Revision: 1.1 $
+ */
+public class VmComponent<E extends Exchange> extends QueueComponent<E> {
+    protected static Map<String, BlockingQueue<Exchange>> queues = new HashMap<String, BlockingQueue<Exchange>>();
+
+    @Override
+    protected Endpoint<E> createEndpoint(String uri, String remaining, Map parameters) throws Exception {
+        BlockingQueue<E> blockingQueue = (BlockingQueue<E>) getBlockingQueue(uri);
+        return new QueueEndpoint<E>(uri, this, blockingQueue);
+    }
+
+    protected BlockingQueue<Exchange> getBlockingQueue(String uri) {
+        synchronized (queues) {
+            BlockingQueue<Exchange> answer = queues.get(uri);
+            if (answer == null) {
+                answer = (BlockingQueue<Exchange>) createQueue();
+                queues.put(uri, answer);
+            }
+            return answer;
+        }
+    }
+}
