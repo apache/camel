bc19e38f03a47beb960a19ea92b74105bf3842a9
James Strachan <jstrachan@apache.org>
2007-07-11 09:19:53 +0000

    refactor to move the JAXB2 POJOs into camel-core so that they can be merged with the DSL camel.builder package; as they are pretty much the same kinds of objects. For CAMEL-71
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@555221 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/model/language/ExpressionType.java
@@ -0,0 +1,94 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.model.language;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
+import org.apache.camel.CamelContext;
+import org.apache.camel.spi.Language;
+import org.apache.camel.model.RouteType;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+/**
+ * A useful base class for an expression
+ *
+ * @version $Revision: 1.1 $
+ */
+@XmlType(name = "expressionType")
+public abstract class ExpressionType {
+    private String expression;
+    private String id;
+
+    public abstract String getLanguage();
+
+    protected ExpressionType() {
+    }
+
+    protected ExpressionType(String expression) {
+        this.expression = expression;
+    }
+
+    @Override
+    public String toString() {
+        return getLanguage() + "Expression[" + getExpression() + "]";
+    }
+
+    public Predicate<Exchange> createPredicate(RouteType route) {
+        CamelContext camelContext = route.getCamelContext();
+        Language language = camelContext.getLanguageResolver().resolveLanguage(getLanguage(), camelContext);
+        return language.createPredicate(getExpression());
+    }
+
+    @XmlValue
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    /**
+     * Gets the value of the id property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    @XmlAttribute
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+    @XmlID
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setId(String value) {
+        this.id = value;
+    }
+}
