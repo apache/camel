2994d72010a83398ffd3b50d8086ef426dfc46d2
James Strachan <jstrachan@apache.org>
2007-07-27 08:37:46 +0000

    added an ETL example for CAMEL-79
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@560144 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-etl/src/main/java/org/apache/camel/example/etl/CustomerTransformer.java
@@ -0,0 +1,74 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example.etl;
+
+import org.apache.camel.Converter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.orm.jpa.JpaTemplate;
+
+import java.util.List;
+
+/**
+ * A Message Transformer of an XML document to a Customer entity bean
+ *
+ * @version $Revision: 1.1 $
+ */
+@Converter
+public class CustomerTransformer {
+    private static final transient Log log = LogFactory.getLog(CustomerTransformer.class);
+    private JpaTemplate template;
+
+    public CustomerTransformer(JpaTemplate template) {
+        this.template = template;
+    }
+
+    /**
+     * A transformation method to convert a person document into a customer entity
+     */
+    @Converter
+    public CustomerEntity toCustomer(PersonDocument doc) {
+        String user = doc.getUser();
+        CustomerEntity customer = findCustomerByName(user);
+
+        // lets convert information from the document into the entity bean
+
+        customer.setFirstName(doc.getFirstName());
+        customer.setSurname(doc.getLastName());
+        customer.setCity(doc.getCity());
+
+        log.debug("Created customer: " + customer);
+        return customer;
+    }
+
+    /**
+     * Finds a customer for the given username, or creates and inserts a new one
+     */
+    protected CustomerEntity findCustomerByName(String user) {
+        List<CustomerEntity> list = template.find("select x from " + CustomerEntity.class.getName() + " x where x.userName = ?1", user);
+        if (list.isEmpty()) {
+            CustomerEntity answer = new CustomerEntity();
+            answer.setUserName(user);
+            template.persist(answer);
+            return answer;
+        }
+        else {
+            return list.get(0);
+        }
+    }
+}
