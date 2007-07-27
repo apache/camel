da6bf987c8cfe51a9896868ae4093178cb394ad1
James Strachan <jstrachan@apache.org>
2007-07-27 16:26:50 +0000

    added spring transaction interceptor and better typesafe exceptions
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@560303 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/main/java/org/apache/camel/spring/spi/SpringTransactionPolicy.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/spi/SpringTransactionPolicy.java
@@ -17,16 +17,10 @@
  */
 package org.apache.camel.spring.spi;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.RuntimeCamelException;
-import org.apache.camel.processor.DelegateProcessor;
 import org.apache.camel.spi.Policy;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 /**
@@ -37,9 +31,6 @@ import org.springframework.transaction.support.TransactionTemplate;
 public class SpringTransactionPolicy<E> implements Policy<E> {
     private static final transient Log log = LogFactory.getLog(SpringTransactionPolicy.class);
     private TransactionTemplate template;
-
-    public SpringTransactionPolicy() {
-    }
 
     public SpringTransactionPolicy(TransactionTemplate template) {
         this.template = template;
@@ -52,45 +43,9 @@ public class SpringTransactionPolicy<E> implements Policy<E> {
             return processor;
         }
 
-        return new DelegateProcessor(processor) {
-            public void process(final Exchange exchange) {
-                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-                    protected void doInTransactionWithoutResult(TransactionStatus status) {
-                        try {
-                            processNext(exchange);
-                        }
-                        catch (Exception e) {
-                            throw new RuntimeCamelException(e);
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public String toString() {
-                return "SpringTransactionPolicy:" + propagationBehaviorToString(transactionTemplate.getPropagationBehavior()) + "[" + getProcessor() + "]";
-            }
-
-            private String propagationBehaviorToString(int propagationBehavior) {
-                switch (propagationBehavior) {
-                    case TransactionDefinition.PROPAGATION_MANDATORY:
-                        return "PROPAGATION_MANDATORY";
-                    case TransactionDefinition.PROPAGATION_NESTED:
-                        return "PROPAGATION_NESTED";
-                    case TransactionDefinition.PROPAGATION_NEVER:
-                        return "PROPAGATION_NEVER";
-                    case TransactionDefinition.PROPAGATION_NOT_SUPPORTED:
-                        return "PROPAGATION_NOT_SUPPORTED";
-                    case TransactionDefinition.PROPAGATION_REQUIRED:
-                        return "PROPAGATION_REQUIRED";
-                    case TransactionDefinition.PROPAGATION_REQUIRES_NEW:
-                        return "PROPAGATION_REQUIRES_NEW";
-                    case TransactionDefinition.PROPAGATION_SUPPORTS:
-                        return "PROPAGATION_SUPPORTS";
-                }
-                return "UNKOWN";
-            }
-        };
+        TransactionInterceptor answer = new TransactionInterceptor(transactionTemplate);
+        answer.setProcessor(processor);
+        return answer;
     }
 
     public TransactionTemplate getTemplate() {
