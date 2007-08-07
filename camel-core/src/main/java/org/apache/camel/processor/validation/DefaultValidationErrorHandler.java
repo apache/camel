9a7ce48890356b81b7a55ce35291ddadffc148e4
James Strachan <jstrachan@apache.org>
2007-08-07 18:46:58 +0000

    added some extra converters
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@563618 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/validation/DefaultValidationErrorHandler.java
+++ camel-core/src/main/java/org/apache/camel/processor/validation/DefaultValidationErrorHandler.java
@@ -22,6 +22,8 @@ import java.util.List;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.validation.Schema;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
@@ -29,23 +31,34 @@ import org.apache.camel.Exchange;
 import org.apache.camel.ValidationException;
 
 /**
+ * A default error handler which just stores all the errors so they can be reported or transformed.
+ * 
  * @version $Revision: $
  */
 public class DefaultValidationErrorHandler implements ValidatorErrorHandler {
+    private static final transient Log log = LogFactory.getLog(DefaultValidationErrorHandler.class);
     private List<SAXParseException> warnings = new ArrayList<SAXParseException>();
     private List<SAXParseException> errors = new ArrayList<SAXParseException>();
     private List<SAXParseException> fatalErrors = new ArrayList<SAXParseException>();
 
-
     public void warning(SAXParseException e) throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug("warning: " + e, e);
+        }
         warnings.add(e);
     }
 
     public void error(SAXParseException e) throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug("error: " + e, e);
+        }
         errors.add(e);
     }
 
     public void fatalError(SAXParseException e) throws SAXException {
+        if (log.isDebugEnabled()) {
+            log.debug("fatalError: " + e, e);
+        }
         fatalErrors.add(e);
     }
 
