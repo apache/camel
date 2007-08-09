b0ac731294b9cda85590d729f9a5c6f35e09844f
James Strachan <jstrachan@apache.org>
2007-08-09 13:11:02 +0000

    fixed the ETL and BAM examples so that they can be run via 'mvn camel:run' or 'mvn compile exec:java'. The killer thing that was really confusing the hell out of me as to why they wouldn't run was the dreaded <scope>test</scope> in commons-collections in the root pom - from now on, never put a test scope in root pom <dependencyManagement> sections - its so confusing as it breaks all kinds of plugins! :)
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@564193 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-example-bam/src/main/java/org/apache/camel/example/bam/Main.java
+++ examples/camel-example-bam/src/main/java/org/apache/camel/example/bam/Main.java
@@ -1,4 +1,5 @@
 /**
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -6,7 +7,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,16 +15,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.example.etl;
-
-import junit.framework.TestCase;
+package org.apache.camel.example.bam;
 
 /**
  * @version $Revision: 1.1 $
  */
-public class RunTest extends TestCase {
-    public void testRun() throws Exception {
-        String[] args = {};
-        Main.main(args);
+public class Main {
+    
+    public static void main(String[] args) {
+        org.apache.camel.spring.Main.main(args);
     }
 }
