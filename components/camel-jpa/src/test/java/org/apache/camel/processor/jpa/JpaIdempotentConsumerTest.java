0e7b0dd6fd5419a755b32c6db75c81282af3264d
James Strachan <jstrachan@apache.org>
2007-08-10 07:55:57 +0000

    patch applied for CAMEL-80 - thanks Hadrian!
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@564500 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jpa/src/test/java/org/apache/camel/processor/jpa/JpaIdempotentConsumerTest.java
+++ components/camel-jpa/src/test/java/org/apache/camel/processor/jpa/JpaIdempotentConsumerTest.java
@@ -16,15 +16,23 @@
  */
 package org.apache.camel.processor.jpa;
 
+import java.util.List;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.processor.IdempotentConsumerTest;
+import org.apache.camel.processor.idempotent.jpa.MessageProcessed;
 import org.apache.camel.spring.SpringCamelContext;
 import org.apache.camel.spring.SpringRouteBuilder;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.jpa.JpaTemplate;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import static org.apache.camel.processor.idempotent.jpa.JpaMessageIdRepository.jpaMessageIdRepository;
 
@@ -32,11 +40,16 @@ import static org.apache.camel.processor.idempotent.jpa.JpaMessageIdRepository.j
  * @version $Revision: 1.1 $
  */
 public class JpaIdempotentConsumerTest extends IdempotentConsumerTest {
+    protected static final String SELECT_ALL_STRING = "select x from " + MessageProcessed.class.getName() + " x where x.processorName = ?1";
+    protected static final String PROCESSOR_NAME = "myProcessorName";
+
     protected ApplicationContext applicationContext;
+    protected JpaTemplate jpaTemplate;
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
         applicationContext = new ClassPathXmlApplicationContext("org/apache/camel/processor/jpa/spring.xml");
+        //cleanupRepository();
         return SpringCamelContext.springCamelContext(applicationContext);
     }
 
@@ -47,10 +60,29 @@ public class JpaIdempotentConsumerTest extends IdempotentConsumerTest {
             public void configure() {
                 from("direct:start").idempotentConsumer(
                         header("messageId"),
-                        jpaMessageIdRepository(bean(JpaTemplate.class), "myProcessorName")
+                        jpaMessageIdRepository(bean(JpaTemplate.class), PROCESSOR_NAME)
                 ).to("mock:result");
             }
         };
         // END SNIPPET: idempotent
+    }
+    
+    protected void cleanupRepository() {
+        jpaTemplate = (JpaTemplate)applicationContext.getBean("jpaTemplate", JpaTemplate.class);
+
+        TransactionTemplate transactionTemplate = new TransactionTemplate();
+        transactionTemplate.setTransactionManager(new JpaTransactionManager(jpaTemplate.getEntityManagerFactory()));
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        transactionTemplate.execute(new TransactionCallback() {
+            public Object doInTransaction(TransactionStatus arg0) {
+                List list = jpaTemplate.find(SELECT_ALL_STRING, PROCESSOR_NAME);
+                for (Object item: list) {
+                	jpaTemplate.remove(item);
+                }
+                jpaTemplate.flush();
+                return Boolean.TRUE;
+            }
+        });
     }
 }
