81981b4f313efe2accc92f3b451ee4ee3f89103d
James Strachan <jstrachan@apache.org>
2007-08-17 06:46:42 +0000

    provided a bean() method on the DSL in addition to the beanRef() method so folks can easily bind to a bean in the DSL. Also made the camel-example-spring a little more interesting, sending messages by default
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@566936 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-example-spring/src/main/java/org/apache/camel/example/spring/MyRouteBuilder.java
+++ examples/camel-example-spring/src/main/java/org/apache/camel/example/spring/MyRouteBuilder.java
@@ -18,6 +18,7 @@ package org.apache.camel.example.spring;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spring.Main;
 
@@ -27,9 +28,9 @@ import org.apache.camel.spring.Main;
  * @version $Revision: 1.1 $
  */
 public class MyRouteBuilder extends RouteBuilder {
-
     /**
      * Allow this route to be run as an application
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -37,13 +38,22 @@ public class MyRouteBuilder extends RouteBuilder {
     }
 
     public void configure() {
-        from("activemq:test.MyQueue").to("file://test");
+        // lets populate the message queue with some messages
+        from("file:src/data?noop=true").
+                to("activemq:test.MyQueue");
+
+        from("activemq:test.MyQueue").
+                to("file://target/test?noop=true");
 
         // set up a listener on the file component
-        from("file://test").process(new Processor() {
-            public void process(Exchange e) {
-                System.out.println("Received exchange: " + e.getIn());
-            }
-        });
+        from("file://target/test?noop=true").
+                bean(new SomeBean());
+    }
+
+    public static class SomeBean {
+
+        public void someMethod(String body) {
+            System.out.println("Received: " + body);
+        }
     }
 }
