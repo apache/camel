7306e12ec0045a2d27b040f1d8b98f9e9c59aea0
James Strachan <jstrachan@apache.org>
2007-08-19 06:25:21 +0000

    added some extra collection converter helper methods
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@567356 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/converter/CollectionConverter.java
+++ camel-core/src/main/java/org/apache/camel/converter/CollectionConverter.java
@@ -16,14 +16,9 @@
  */
 package org.apache.camel.converter;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.camel.Converter;
+
+import java.util.*;
 
 /**
  * Some core java.util Collection based
@@ -33,11 +28,10 @@ import org.apache.camel.Converter;
  */
 @Converter
 public class CollectionConverter {
-
     /**
      * Utility classes should not have a public constructor.
      */
-    private CollectionConverter() {        
+    private CollectionConverter() {
     }
 
     /**
@@ -59,6 +53,14 @@ public class CollectionConverter {
         return Arrays.asList(array);
     }
 
+    /**
+     * Converts a collection to a List if it is not already
+     */
+    @Converter
+    public static List toList(Collection collection) {
+        return new ArrayList(collection);
+    }
+
     @Converter
     public static Set toSet(Object[] array) {
         Set answer = new HashSet();
@@ -76,5 +78,22 @@ public class CollectionConverter {
     @Converter
     public static Set toSet(Map map) {
         return map.entrySet();
+    }
+
+    @Converter
+    public static Properties toProperties(Map map) {
+        Properties answer = new Properties();
+        answer.putAll(map);
+        return answer;
+    }
+
+    @Converter
+    public static Hashtable toHashtable(Map map) {
+        return new Hashtable(map);
+    }
+
+    @Converter
+    public static HashMap toHashMap(Map map) {
+        return new HashMap(map);
     }
 }
