5131189a1d21210414378a9e40195b018924c5d3
James Strachan <jstrachan@apache.org>
2007-08-19 06:38:38 +0000

    test cases showing method disambiguation using annotations
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@567359 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/component/bean/BodyAnnotationToDisambiguateMethodsTest.java
@@ -0,0 +1,75 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.bean;
+
+import org.apache.camel.Body;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.BeanRouteTest;
+import org.apache.camel.util.jndi.JndiContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.naming.Context;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class BodyAnnotationToDisambiguateMethodsTest extends ContextTestSupport {
+    private static final transient Log LOG = LogFactory.getLog(BeanRouteTest.class);
+    protected MyBean myBean = new MyBean();
+
+    public void testSendMessage() throws Exception {
+        String expectedBody = "Wobble";
+
+        template.sendBodyAndHeader("direct:in", expectedBody, "foo", "bar");
+
+        assertEquals("bean body: " + myBean, expectedBody, myBean.body);
+    }
+
+    @Override
+    protected Context createJndiContext() throws Exception {
+        JndiContext answer = new JndiContext();
+        answer.bind("myBean", myBean);
+        return answer;
+    }
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:in").beanRef("myBean");
+            }
+        };
+    }
+
+    public static class MyBean {
+        public String body;
+
+        public void bar(String body) {
+            fail("bar() called with: " + body);
+        }
+
+        public void foo(@Body String body) {
+            this.body = body;
+            LOG.info("foo() method called on " + this);
+        }
+
+        public void wrongMethod(String body) {
+            fail("wrongMethod() called with: " + body);
+        }
+    }
+}
