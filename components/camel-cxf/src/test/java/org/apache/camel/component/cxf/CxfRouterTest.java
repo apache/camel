7dca1c8a42454b792ae18c0ff3c2f36c4041db84
James Strachan <jstrachan@apache.org>
2007-08-28 16:22:22 +0000

    applied patch for CAMEL-118 with thanks Willem!
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@570474 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-cxf/src/test/java/org/apache/camel/component/cxf/CxfRouterTest.java
@@ -0,0 +1,105 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.cxf;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Endpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.util.ObjectHelper;
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.bus.CXFBusFactory;
+import org.apache.cxf.endpoint.ServerImpl;
+import org.apache.cxf.frontend.ClientFactoryBean;
+import org.apache.cxf.frontend.ClientProxyFactoryBean;
+import org.apache.cxf.frontend.ServerFactoryBean;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+
+import junit.framework.TestCase;
+
+public class CxfRouterTest extends ContextTestSupport {
+    private static final String ROUTER_ADDRESS = "http://localhost:9000/router";
+    private static final String SERVICE_ADDRESS = "http://localhost:9002/helloworld";
+    private static final String SERVICE_CLASS = "serviceClass=org.apache.camel.component.cxf.HelloService";
+    
+    private String routerEndpointURI = "cxf://" + ROUTER_ADDRESS + "?" + SERVICE_CLASS + "&invoker=true";
+    private String serviceEndpointURI = "cxf://" + SERVICE_ADDRESS + "?" + SERVICE_CLASS + "&invoker=true";
+    //private Endpoint routerEndpoint;
+    //private Endpoint resultEndpoint;
+    private ServerImpl server;
+    
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();        
+        //routerEndpoint = resolveMandatoryEndpoint(routerEndpointURI);        
+        //resultEndpoint = resolveMandatoryEndpoint(serviceEndpointURI);
+        
+        startService();
+    }
+    
+    private void startService() {
+        //start a service
+        ServerFactoryBean svrBean = new ServerFactoryBean();
+
+        svrBean.setAddress(SERVICE_ADDRESS);
+        svrBean.setServiceClass(HelloService.class);
+        svrBean.setServiceBean(new HelloServiceImpl());
+        svrBean.setBus(CXFBusFactory.getDefaultBus());
+
+        server = (ServerImpl)svrBean.create();
+        server.start();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        if (server != null) {
+            server.stop();
+        }
+    }
+  
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from(routerEndpointURI).to(serviceEndpointURI);              
+            }
+        };
+    }
+    
+    protected CamelContext createCamelContext() throws Exception {
+        return new DefaultCamelContext();
+    }
+
+    
+    public void testInvokingServerFromCXFClient() throws Exception {  
+        Bus bus = BusFactory.getDefaultBus();
+        
+        ClientProxyFactoryBean proxyFactory = new ClientProxyFactoryBean();
+        ClientFactoryBean clientBean = proxyFactory.getClientFactoryBean();
+        clientBean.setAddress(ROUTER_ADDRESS);        
+        clientBean.setServiceClass(HelloService.class);
+        clientBean.setBus(bus);        
+        
+        HelloService client = (HelloService) proxyFactory.create();
+        String result = client.echo("hello world");
+        assertEquals("we should get the right answer from router", "hello world", result);
+        
+    }
+}
