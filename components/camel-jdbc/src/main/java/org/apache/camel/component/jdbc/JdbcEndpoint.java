d28048b92c36b80f6ae9f4b7a3cffe59e3dde457
James Strachan <jstrachan@apache.org>
2007-08-29 08:45:47 +0000

    added excellent patch from Nicky Sandhu for CAMEL-128 for a new JDBC component. Still working on the test case to get it passing...
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@570700 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jdbc/src/main/java/org/apache/camel/component/jdbc/JdbcEndpoint.java
@@ -0,0 +1,76 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing pejdbcssions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jdbc;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.RuntimeCamelException;
+import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.impl.DefaultExchange;
+
+/**
+ * @version $Revision:520964 $
+ */
+public class JdbcEndpoint extends DefaultEndpoint<DefaultExchange> {
+
+    private URI uri;
+    private String remaining;
+    private int readSize;
+
+    protected JdbcEndpoint(String endpointUri, String remaining, JdbcComponent component) throws URISyntaxException {
+        super(endpointUri, component);
+        this.uri = new URI(endpointUri);
+        this.remaining = remaining;
+    }
+
+    public boolean isSingleton() {
+        return false;
+    }
+
+    public DefaultExchange createExchange() {
+        return new DefaultExchange(getContext());
+    }
+
+    public Consumer<DefaultExchange> createConsumer(Processor processor) throws Exception {
+        throw new RuntimeCamelException("A JDBC Consumer would be the server side of database! No such support here");
+    }
+
+    public Producer<DefaultExchange> createProducer() throws Exception {
+        return new JdbcProducer(this, remaining, readSize);
+    }
+
+    public String getName() {
+        String path = uri.getPath();
+        if (path == null) {
+            path = uri.getSchemeSpecificPart();
+        }
+        return path;
+    }
+
+    public int getReadSize() {
+        return this.readSize;
+    }
+
+    public void setReadSize(int readSize) {
+        this.readSize = readSize;
+    }
+
+}
