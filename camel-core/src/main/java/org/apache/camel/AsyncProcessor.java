82c5f29efaa6029611e752d2f11f5a460b812f12
James Strachan <jstrachan@apache.org>
2007-09-14 09:58:50 +0000

    minor javadoc fix
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@575602 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/AsyncProcessor.java
+++ camel-core/src/main/java/org/apache/camel/AsyncProcessor.java
@@ -31,8 +31,9 @@ public interface AsyncProcessor extends Processor {
     /**
      * Processes the message exchange.  Similar to {@see Processor.process}, but
      * the caller supports having the exchange asynchronously processed.
-     * 
-     * @param  The @{see AsyncCallback} will be invoked when the processing 
+     *
+     * @param exchange the exchange to process
+     * @param  callback The @{see AsyncCallback} will be invoked when the processing
      *         of the exchange is completed. If the exchange is completed synchronously, then the 
      *         callback is also invoked synchronously.  The callback should therefore be careful of
      *         starting recursive loop.
