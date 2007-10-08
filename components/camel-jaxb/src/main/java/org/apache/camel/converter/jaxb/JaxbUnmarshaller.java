85057ce35a23ddd13048728a79f3142ea3791fa6
James Strachan <jstrachan@apache.org>
2007-10-08 15:11:12 +0000

    added an initial cut of a marshaller implementation using serialization and JAXB2 for CAMEL-165
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@582840 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jaxb/src/main/java/org/apache/camel/converter/jaxb/JaxbUnmarshaller.java
+++ components/camel-jaxb/src/main/java/org/apache/camel/converter/jaxb/JaxbUnmarshaller.java
@@ -1,4 +1,5 @@
 /**
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -6,7 +7,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,19 +15,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.spi;
+package org.apache.camel.converter.jaxb;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.bind.JAXBException;
+
+import org.apache.camel.spi.Unmarshaller;
+import org.apache.camel.util.IOHelper;
+
 /**
- * @version $Revision: 520124 $
+ * An {@link Unmarshaller} which uses JAXB2
+ * 
+ * @version $Revision: 1.1 $
  */
-public interface Unmarshaller {
+public class JaxbUnmarshaller implements Unmarshaller {
+    javax.xml.bind.Unmarshaller unmarshaller;
 
-    /**
-     * Unmarshals the given stream into an object.
-     */
-    Object unmarshal(InputStream stream) throws IOException, ClassNotFoundException;
+    public JaxbUnmarshaller(javax.xml.bind.Unmarshaller unmarshaller) {
+        this.unmarshaller = unmarshaller;
+    }
 
+
+    public Object unmarshal(InputStream stream) throws IOException, ClassNotFoundException {
+        try {
+            return unmarshaller.unmarshal(stream);
+        }
+        catch (JAXBException e) {
+            throw IOHelper.createIOException(e);
+        }
+    }
 }
