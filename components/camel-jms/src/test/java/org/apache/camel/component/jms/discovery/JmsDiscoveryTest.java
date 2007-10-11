de3e741a9147283238a54e636093b231cb2faa1e
James Strachan <jstrachan@apache.org>
2007-10-11 12:03:15 +0000

    added a little discovery example
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@583787 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/discovery/JmsDiscoveryTest.java
@@ -0,0 +1,81 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jms.discovery;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jms.ConnectionFactory;
+import javax.naming.Context;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import static org.apache.camel.component.jms.JmsComponent.jmsComponentClientAcknowledge;
+
+/**
+ * @version $Revision: 574458 $
+ */
+public class JmsDiscoveryTest extends ContextTestSupport {
+
+    protected MyRegistry registry = new MyRegistry();
+
+    public void testDiscovery() throws Exception {
+        // lets wait to see if we get 3 services
+
+        Thread.sleep(3000);
+
+        Map<String, Map> map = new HashMap<String, Map>(registry.getServices());
+        System.out.println("Received map: " + map);
+
+        assertEquals("Size of map: " + map, 3, map.size());
+    }
+
+
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext camelContext = super.createCamelContext();
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        camelContext.addComponent("activemq", jmsComponentClientAcknowledge(connectionFactory));
+
+        return camelContext;
+    }
+
+    @Override
+    protected Context createJndiContext() throws Exception {
+        Context context = super.createJndiContext();
+        context.bind("service1", new MyService("service1"));
+        context.bind("service2", new MyService("service2"));
+        context.bind("service3", new MyService("service3"));
+        context.bind("registry", registry);
+        return context;
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                // lets setup the heartbeats
+                from("bean:service1?method=status").to("activemq:topic:registry.heartbeats");
+                from("bean:service2?method=status").to("activemq:topic:registry.heartbeats");
+                from("bean:service3?method=status").to("activemq:topic:registry.heartbeats");
+
+                from("activemq:topic:registry.heartbeats").to("bean:registry?method=onEvent");
+            }
+        };
+    }
+}
