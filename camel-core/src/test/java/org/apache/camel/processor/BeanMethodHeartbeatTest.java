9531efa12a802d2dd2a378b0c51d7135f8fcfa3a
James Strachan <jstrachan@apache.org>
2007-10-11 11:17:05 +0000

    fixed bug in DefaultScheduledPollConsumer; when using a PollingConsumer we should take the OUT and use that as the IN for invoking the next Processor in the pipeline; also added a little test case demonstrating how to create heartbeat messages using the bean component
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@583773 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/processor/BeanMethodHeartbeatTest.java
@@ -0,0 +1,85 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.processor;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+import javax.naming.Context;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * This test shows we can poll a bean for a method and send the POJO over some transport
+ *
+ * @version $Revision: 1.1 $
+ */
+public class BeanMethodHeartbeatTest extends ContextTestSupport {
+
+    protected MyService bean = new MyService("Beer");
+    
+    public void testHeartbeatsArrive() throws Exception {
+        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
+        resultEndpoint.expectedMinimumMessageCount(1);
+
+        resultEndpoint.assertIsSatisfied();
+
+        List<Exchange> list = resultEndpoint.getReceivedExchanges();
+        log.debug("Received: " + list);
+        Exchange exchange = list.get(0);
+        log.debug("In: " + exchange.getIn());
+        log.debug("Out: " + exchange.getOut());
+        Map map = assertIsInstanceOf(Map.class, exchange.getIn().getBody());
+        log.debug("Received: " + map);
+    }
+
+    @Override
+    protected Context createJndiContext() throws Exception {
+        Context context = super.createJndiContext();
+        context.bind("myService", bean);
+        return context;
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("bean:myService?methodName=status").to("mock:result");
+            }
+        };
+    }
+
+    public static class MyService {
+        private String name;
+
+        public MyService(String name) {
+            this.name = name;
+        }
+
+        public Map status() {
+            Map answer = new HashMap();
+            answer.put("name", name);
+            answer.put("time", new Date());
+            return answer;
+        }
+    }
+}
