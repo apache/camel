5e217445dfc0519761b376892e0cedc334d1ada6
James Strachan <jstrachan@apache.org>
2007-10-26 11:54:37 +0000

    applied patch by Nicky Sandhau for CAMEL-191 with thanks
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@588614 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/processor/ValidationWithHandlePipelineTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.apache.camel.processor;
+
+import org.apache.camel.ValidationException;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * The handle catch clause has a pipeline processing the exception.
+ * 
+ * @author <a href="mailto:nsandhu">nsandhu</a>
+ * 
+ */
+public class ValidationWithHandlePipelineTest extends ValidationTest {
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:start").tryBlock().process(validator).to("mock:valid").handle(
+                        ValidationException.class).pipeline("direct:a", "mock:invalid");
+            }
+        };
+    }
+
+}
