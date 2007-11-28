0308628e025d6bb52a807650f654fbe66f0965f6
James Strachan <jstrachan@apache.org>
2007-11-28 11:36:44 +0000

    added a simple example for testing out the deployment of Camel routes inside the ServiceMix 4 runtime (http://cwiki.apache.org/SMX4/runtime.html)
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@598967 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-osgi/src/main/java/org/apache/camel/example/osgi/MyTransform.java
@@ -0,0 +1,57 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example.osgi;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class MyTransform  {
+    private static final transient Log LOG = LogFactory.getLog(MyTransform.class);
+    private boolean verbose = true;
+    private String prefix = "MyTransform";
+
+    public Object transform(Object body) {
+        String answer = prefix + " body at:  " + new Date() + " body: " + body;
+        if (verbose) {
+            System.out.println(">>>> " + answer);
+        }
+        LOG.info(">>>> " + answer);
+        return answer;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+}
