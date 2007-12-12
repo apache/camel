00806cadf216355d8a7141f543dad283df4f41d9
Hadrian Zbarcea <hadrian@apache.org>
2007-12-12 13:36:00 +0000

    New files missed from patch to CAMEL-258.
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@603611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/transport/spring/CamelConduitDefinitionParser.java
@@ -0,0 +1,43 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.camel.component.cxf.transport.spring;
+
+import org.w3c.dom.Element;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.apache.camel.component.cxf.transport.CamelConduit;
+import org.apache.camel.util.ObjectHelper;
+import org.apache.cxf.configuration.spring.AbstractBeanDefinitionParser;
+
+public class CamelConduitDefinitionParser extends AbstractCamelContextBeanDefinitionParser {
+        
+    public CamelConduitDefinitionParser() {
+        super();
+        setBeanClass(CamelConduit.class);
+    }
+       
+}
