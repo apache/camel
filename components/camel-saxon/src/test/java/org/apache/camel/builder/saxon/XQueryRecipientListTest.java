6e143143cde5754274d264d86d471afe28ab3270
James Strachan <jstrachan@apache.org>
2007-12-31 16:25:59 +0000

    added a test case showing how to use xquery with a Dynamic Recipient List
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@607750 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-saxon/src/test/java/org/apache/camel/builder/saxon/XQueryRecipientListTest.java
@@ -0,0 +1,69 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.builder.saxon;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class XQueryRecipientListTest extends ContextTestSupport {
+
+    protected MockEndpoint londonEndpoint;
+    protected MockEndpoint tampaEndpoint;
+
+    public void testSendLondonMessage() throws Exception {
+        londonEndpoint.expectedMessageCount(1);
+        tampaEndpoint.expectedMessageCount(0);
+
+        template.sendBody("direct:start", "<person name='James' city='London'/>");
+
+        assertMockEndpointsSatisifed();
+    }
+
+    public void testSendTampaMessage() throws Exception {
+        londonEndpoint.expectedMessageCount(0);
+        tampaEndpoint.expectedMessageCount(1);
+
+        template.sendBody("direct:start", "<person name='Hiram' city='Tampa'/>");
+
+        assertMockEndpointsSatisifed();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        londonEndpoint = getMockEndpoint("mock:foo.London");
+        tampaEndpoint = getMockEndpoint("mock:foo.Tampa");
+    }
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                // TODO is there a nicer way to do this with XQuery?
+                
+                // START SNIPPET: example
+                from("direct:start").
+                        recipientList().xquery("concat('mock:foo.', /person/@city)", String.class);
+                // END SNIPPET: example
+            }
+        };
+    }
+}
