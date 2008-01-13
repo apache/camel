0c5bdbf68d3339bfb8501eb8e15e0035d22bf049
Willem Ning Jiang <ningjiang@apache.org>
2008-01-13 09:08:11 +0000

    Updated the svn:ignore property
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@611552 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jhc/src/main/java/org/apache/camel/component/jhc/JhcProducer.java
+++ components/camel-jhc/src/main/java/org/apache/camel/component/jhc/JhcProducer.java
@@ -60,7 +60,7 @@ public class JhcProducer extends DefaultProducer<JhcExchange> implements AsyncPr
 
     private static Log LOG = LogFactory.getLog(JhcProducer.class);
 
-    private int nbThreads = 2;
+    private int nbThreads = 50;
     private ConnectingIOReactor ioReactor;
     private ThreadFactory threadFactory;
     private Thread runner;
@@ -78,6 +78,7 @@ public class JhcProducer extends DefaultProducer<JhcExchange> implements AsyncPr
     protected void doStart() throws Exception {
         super.doStart();
         HttpParams params = getEndpoint().getParams();
+        nbThreads = getEndpoint().getWorkers();
         ioReactor = new DefaultConnectingIOReactor(nbThreads, threadFactory, params);
         BasicHttpProcessor httpproc = new BasicHttpProcessor();
         httpproc.addInterceptor(new RequestContent());
@@ -238,7 +239,7 @@ public class JhcProducer extends DefaultProducer<JhcExchange> implements AsyncPr
 
         public void finalizeContext(HttpContext arg0) {
             // TODO Auto-generated method stub
-            
+
         }
     }
 
