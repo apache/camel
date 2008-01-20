23f13652728a148f53ff2dda02e6ddcac88377f7
Willem Ning Jiang <ningjiang@apache.org>
2008-01-20 05:39:38 +0000

    CAMEL-279 support override the bean invocation method name with the DSL
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@613509 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/component/bean/BeanProcessor.java
+++ camel-core/src/main/java/org/apache/camel/component/bean/BeanProcessor.java
@@ -34,7 +34,7 @@ import org.apache.commons.logging.LogFactory;
 /**
  * A {@link Processor} which converts the inbound exchange to a method
  * invocation on a POJO
- * 
+ *
  * @version $Revision: $
  */
 public class BeanProcessor extends ServiceSupport implements Processor {
@@ -94,21 +94,11 @@ public class BeanProcessor extends ServiceSupport implements Processor {
         if (methodObject != null) {
             invocation = beanInfo.createInvocation(methodObject, bean, exchange);
         } else {
-            // lets pass in the method name to use if its specified
-            boolean hasToClearMethod = false;
-            try {
-                if (ObjectHelper.isNotNullAndNonEmpty(method)) {
-                    if (isNullOrBlank(in.getHeader(METHOD_NAME, String.class))) {
-                        in.setHeader(METHOD_NAME, method);
-                        hasToClearMethod = true;
-                    }
-                }
-                invocation = beanInfo.createInvocation(bean, exchange);
-            } finally {
-                if (hasToClearMethod) {
-                    in.removeHeader(METHOD_NAME);
-                }
+            // we just override the bean's invocation method name here
+            if (ObjectHelper.isNotNullAndNonEmpty(method)) {
+                in.setHeader(METHOD_NAME, method);
             }
+            invocation = beanInfo.createInvocation(bean, exchange);
         }
         if (invocation == null) {
             throw new IllegalStateException("No method invocation could be created, no maching method could be found on: " + bean);
