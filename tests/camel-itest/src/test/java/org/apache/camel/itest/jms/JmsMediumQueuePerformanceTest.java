8d2b29db30bafa130f414fcefec1f50fc0b1cd62
James Strachan <jstrachan@apache.org>
2008-02-05 18:06:05 +0000

    Really fixing the broken test case :)
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@618725 13f79535-47bb-0310-9956-ffa450edef68

--- tests/camel-itest/src/test/java/org/apache/camel/itest/jms/JmsMediumQueuePerformanceTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/jms/JmsMediumQueuePerformanceTest.java
@@ -26,13 +26,13 @@ public class JmsMediumQueuePerformanceTest extends JmsPerformanceTest {
     @Override
     public void testSendingAndReceivingMessages() throws Exception {
         int expected = mediumQueueCount + messageCount;
-        setExpectedMessageCount(messageCount);
+        setExpectedMessageCount(expected);
 
         System.out.println("Sending " + mediumQueueCount + " messages first");
         sendLoop(0, mediumQueueCount);
         System.out.println("Sent!");
 
-        Thread.sleep(20000);
+        Thread.sleep(2000);
 
         // TODO now start the route!
         
