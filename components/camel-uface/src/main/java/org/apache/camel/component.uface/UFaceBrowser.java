02e0b8bae46e02d0fdceec370704be2b2d6fc36c
James Strachan <jstrachan@apache.org>
2008-02-12 16:06:52 +0000

    added a simple UFace component for browsing endpoints in Swing / JFace / Eclipse
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@620839 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-uface/src/main/java/org/apache/camel/component.uface/UFaceBrowser.java
@@ -0,0 +1,151 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.uface;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DelegateLifecycleStrategy;
+import org.apache.camel.spi.BrowsableEndpoint;
+import org.apache.camel.util.CamelContextHelper;
+import org.apache.camel.util.ObjectHelper;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.core.databinding.observable.list.WritableList;
+import org.eclipse.core.databinding.observable.value.WritableValue;
+import org.ufacekit.model.ModelHelper;
+import org.ufacekit.ui.AttributeDescriptor;
+import org.ufacekit.ui.UIComposite;
+import org.ufacekit.ui.UIFactory;
+import org.ufacekit.ui.beanform.BeanForm;
+import org.ufacekit.ui.controls.CellLabelProvider;
+import org.ufacekit.ui.controls.UITable;
+import org.ufacekit.ui.controls.UITableColumn;
+import org.ufacekit.ui.layouts.GridLayoutData;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class UFaceBrowser {
+    private final DefaultCamelContext camelContext;
+    private IObservableList endpoints;
+    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public UFaceBrowser(DefaultCamelContext camelContext) {
+        ObjectHelper.notNull(camelContext, "camelContext");
+
+        this.camelContext = camelContext;
+        this.endpoints = new WritableList(Realm.getDefault());
+
+        // lets add any endpoints already added
+        List<BrowsableEndpoint> list = CamelContextHelper.getSingletonEndpoints(camelContext, BrowsableEndpoint.class);
+        for (BrowsableEndpoint endpoint : list) {
+            if (!endpoints.contains(endpoint)) {
+                endpoints.add(endpoint);
+            }
+        }
+
+        camelContext.setLifecycleStrategy(new DelegateLifecycleStrategy(camelContext.getLifecycleStrategy()) {
+            @Override
+            public void onEndpointAdd(Endpoint<? extends Exchange> endpoint) {
+                super.onEndpointAdd(endpoint);
+
+                if (endpoint instanceof BrowsableEndpoint) {
+                    onBrowsableEndpoint((BrowsableEndpoint) endpoint);
+                }
+            }
+        });
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
+    public IObservableList getEndpoints() {
+        return endpoints;
+    }
+
+    public DefaultCamelContext getCamelContext() {
+        return camelContext;
+    }
+
+    public void createBrowserUI(UIComposite root) {
+        UIFactory ui = root.getFactory();
+        UIComposite splitter = ui.newHorizontalSplitPanel(root, new GridLayoutData(GridLayoutData.ALIGN_CENTER, GridLayoutData.ALIGN_CENTER));
+
+        UITable tree = createEndpointTableUI(splitter);
+
+        BeanForm selectionForm = new BeanForm();
+
+/*
+        UIComposite detailComposite = ui.newComposite(splitter, null, ui.newGridLayout(1));
+
+        ui.newLabel(detailComposite, new UILabel.LabelUIInfo(new GridLayoutData(GridLayoutData.ALIGN_END, GridLayoutData.ALIGN_CENTER))).setText("Exchanges");
+*/
+
+        UITable table = ui.newTable(splitter, new UITable.TableUIInfo(new GridLayoutData(GridLayoutData.ALIGN_FILL, GridLayoutData.ALIGN_FILL, true, true)),
+                new UITable.TableBindingInfo(selectionForm,
+                        selectionForm.detailList("exchanges", Collection.class)));
+        ui.newTableColumn(table, new UITableColumn.TableColumnUIInfo(null, new CellLabelProvider() {
+            public String getLabel(Object object) {
+                Exchange exchange = (Exchange) object;
+                return exchange.getIn().getBody(String.class);
+            }
+        }));
+        ui.newTableColumn(table, new UITableColumn.TableColumnUIInfo(null, new CellLabelProvider() {
+            public String getLabel(Object object) {
+                Exchange exchange = (Exchange) object;
+                return exchange.getIn().getHeaders().toString();
+            }
+        }));
+
+        selectionForm.bind(tree.getSelectionObservable());
+    }
+
+    protected UITable createEndpointTableUI(UIComposite root) {
+        BeanForm form = new BeanForm();
+
+        UIFactory ui = root.getFactory();
+        AttributeDescriptor bindingData = form.detailList("endpoints", Collection.class);
+
+        UITable table = ui.newTable(root, new UITable.TableUIInfo(null), new UITable.TableBindingInfo(form, bindingData));
+        ui.newTableColumn(table, new UITableColumn.TableColumnUIInfo(null, new CellLabelProvider() {
+            public String getLabel(Object object) {
+                BrowsableEndpoint endpoint = (BrowsableEndpoint) object;
+                return endpoint.getEndpointUri();
+            }
+        }));
+
+        WritableValue value = ModelHelper.createWritableValue(this);
+        form.bind(value);
+        return table;
+    }
+
+    protected void onBrowsableEndpoint(BrowsableEndpoint endpoint) {
+        endpoints.add(endpoint);
+    }
+}
