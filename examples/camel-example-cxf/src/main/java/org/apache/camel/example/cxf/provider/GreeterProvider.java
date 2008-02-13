29144359573b669df3e80d095aa08adb80781dbf
Willem Ning Jiang <ningjiang@apache.org>
2008-02-13 09:17:07 +0000

    CAMEL-332 added an example to show how to use the JaxWs WebServiceProvider service endpoint
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@627321 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-cxf/src/main/java/org/apache/camel/example/cxf/provider/GreeterProvider.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example.cxf.provider;
+
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.Provider;
+import javax.xml.ws.ServiceMode;
+import javax.xml.ws.WebServiceProvider;
+import javax.xml.ws.Service.Mode;
+
+@WebServiceProvider()
+@ServiceMode(Mode.MESSAGE)
+
+/**
+ * This class is used by Camel just for getting the endpoint configuration parameters. All the requests 
+ * aimed at this class would be intercepted routed to the camel route specified. The route has to set the 
+ * appropriate response message for the service to work.
+ */
+public class GreeterProvider implements Provider<SOAPMessage> {
+
+	public SOAPMessage invoke(SOAPMessage arg0) {
+		//request should not come here as camel route would intercept the call before this is invoked.
+		throw new UnsupportedOperationException("Placeholder method");
+	}
+    
+}
