78780b216459a51cc57755ada4b9b720311504ac
James Strachan <jstrachan@apache.org>
2008-02-14 17:15:20 +0000

    added an experimental cut of a simple TestNG based system for integration testing using Camel, Spring and TestNG using properties on a per test instance to overload stuff in the spring.xml to avoid XML overload
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@627805 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-testng/src/main/java/org/apache/camel/testng/SpringRunner.java
@@ -0,0 +1,85 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.testng;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.apache.camel.spring.SpringCamelContext;
+
+/**
+ * A helper base class for running Camel based test cases using TestNG which makes it easy to overload
+ * system properties before the spring application context is initialised; to allow a single spring XML to be reused
+ * with some properties being overloaded.
+ *
+ * @version $Revision: 1.1 $
+ */
+public class SpringRunner {
+    private Properties oldSystemProperties;
+    private AbstractXmlApplicationContext applicationContext;
+    private SpringCamelContext camelContext;
+
+    protected void assertApplicationContextStarts(String applicationContextLocations, Properties properties) throws Exception {
+        // lets overload the system properties
+        Set<Map.Entry<Object, Object>> entries = properties.entrySet();
+        for (Map.Entry<Object, Object> entry : entries) {
+            System.setProperty(entry.getKey().toString(), entry.getValue().toString());
+        }
+
+        // now lets load the context
+        applicationContext = new ClassPathXmlApplicationContext(applicationContextLocations);
+        applicationContext.start();
+
+        camelContext = SpringCamelContext.springCamelContext(applicationContext);
+    }
+
+    protected SpringCamelContext getCamelContext() {
+        return camelContext;
+    }
+
+    /**
+     * Creates a properties object with the given key and value 
+     */
+    protected Properties createProperties(String name, String value) {
+        Properties properties = new Properties();
+        properties.setProperty(name, value);
+        return properties;
+    }
+
+    @BeforeTest
+    protected void setUp() {
+        oldSystemProperties = new Properties(System.getProperties());
+        System.out.println("Setup!");
+    }
+
+    @AfterTest
+    protected void tearDown() {
+        if (applicationContext != null) {
+            applicationContext.close();
+        }
+        System.setProperties(oldSystemProperties);
+        System.out.println("Tearning down!");
+    }
+}
---
+++ components/camel-testng/src/test/java/org/apache/camel/testng/example/MyTest.java
@@ -0,0 +1,49 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.testng.example;
+
+import java.util.Properties;
+
+import org.apache.camel.testng.SpringRunner;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.testng.annotations.Test;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+@Test(groups = {"routing"})
+public class MyTest extends SpringRunner {
+    private String appContextLocation = " org/apache/camel/testng/example/spring.xml";
+
+    public void useCaseFoo() throws Exception {
+        assertApplicationContextStarts(appContextLocation, createProperties("useCase", "foo/input1"));
+        assertExpectedCount(2);
+    }
+
+    public void useCaseBar() throws Exception {
+        assertApplicationContextStarts(appContextLocation, createProperties("useCase", "bar/input1"));
+        assertExpectedCount(1);
+    }
+
+    protected void assertExpectedCount(int expectedCount) throws InterruptedException {
+        MockEndpoint endpoint = getCamelContext().getEndpoint("mock:results", MockEndpoint.class);
+        endpoint.expectedMessageCount(expectedCount);
+        endpoint.assertIsSatisfied();
+    }
+
+}
