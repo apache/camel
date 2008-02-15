9b92a2fae8a6947ec6e90015bce57b01da063ef7
James Strachan <jstrachan@apache.org>
2008-02-15 12:17:16 +0000

    added support for the new test endpoint for http://issues.apache.org/activemq/browse/CAMEL-327
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@628037 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/util/EndpointHelper.java
@@ -0,0 +1,79 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.util;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.PollingConsumer;
+import org.apache.camel.Processor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Some helper methods for working with {@link Endpoint} instances
+ *
+ * @version $Revision: 1.1 $
+ */
+public class EndpointHelper {
+    private static final transient Log LOG = LogFactory.getLog(EndpointHelper.class);
+
+    /**
+     * Creates a {@link PollingConsumer} and polls all pending messages on the endpoint
+     * and invokes the given {@link Processor} to process each {@link Exchange} and then closes
+     * down the consumer and throws any exceptions thrown.
+     *
+     * @param endpoint
+     * @param processor
+     */
+    public static void pollEndpoint(Endpoint endpoint, Processor processor, long timeout) throws Exception {
+        PollingConsumer consumer = endpoint.createPollingConsumer();
+        try {
+            consumer.start();
+
+            while (true) {
+                Exchange exchange = consumer.receive(timeout);
+                if (exchange == null) {
+                    break;
+                }
+                else {
+                    processor.process(exchange);
+                }
+            }
+        }
+        finally {
+            try {
+                consumer.stop();
+            }
+            catch (Exception e) {
+                LOG.warn("Failed to stop PollingConsumer: " + e, e);
+            }
+        }
+    }
+
+    /**
+     * Creates a {@link PollingConsumer} and polls all pending messages on the endpoint
+     * and invokes the given {@link Processor} to process each {@link Exchange} and then closes
+     * down the consumer and throws any exceptions thrown.
+     *
+     * @param endpoint
+     * @param processor
+     */
+    public static void pollEndpoint(Endpoint endpoint, Processor processor) throws Exception {
+        pollEndpoint(endpoint, processor, 1000L);
+    }
+}
