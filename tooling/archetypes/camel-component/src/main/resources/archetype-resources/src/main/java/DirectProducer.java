acb2e2944b4d394ac85849069d5711f78c61cabd
Willem Ning Jiang <ningjiang@apache.org>
2008-03-10 14:50:58 +0000

    CAMEL-48 Changed files to use the packageName and Updated the ReadMe.txt
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@635572 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/archetypes/camel-component/src/main/resources/archetype-resources/src/main/java/DirectProducer.java
+++ tooling/archetypes/camel-component/src/main/resources/archetype-resources/src/main/java/DirectProducer.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ${groupId};
+package ${packageName};
 
 import org.apache.camel.AsyncCallback;
 import org.apache.camel.AsyncProcessor;
