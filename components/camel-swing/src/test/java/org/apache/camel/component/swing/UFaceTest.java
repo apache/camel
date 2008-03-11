c502bd29adf5a342ff41a8ce14492dc19a487f18
James Strachan <jstrachan@apache.org>
2008-03-11 14:54:46 +0000

    upgraded to the latest milestone build of uface
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@635958 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-swing/src/test/java/org/apache/camel/component/swing/UFaceTest.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.swing;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision$
+ */
+public class UFaceTest extends ContextTestSupport {
+    public void testUFaceEndpoints() throws Exception {
+        MockEndpoint endpoint = getMockEndpoint("mock:results");
+        endpoint.expectedMessageCount(1);
+
+        template.sendBody("swing:a", "<hello>world!</hello>");
+
+        assertMockEndpointsSatisifed();
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                from("swing:a").to("swing:b").to("mock:results");
+            }
+        };
+    }
+}
