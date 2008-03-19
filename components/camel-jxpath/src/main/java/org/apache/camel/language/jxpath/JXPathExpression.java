3011078985e0f2f936523b270530eafedb5ed1c5
James Strachan <jstrachan@apache.org>
2008-03-19 13:51:19 +0000

    applied patch from Gert for JXPath support with thanks : https://issues.apache.org/activemq/browse/CAMEL-241 I also added the new language into the XSD and DSL
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@638833 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jxpath/src/main/java/org/apache/camel/language/jxpath/JXPathExpression.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.language.jxpath;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.impl.ExpressionSupport;
+import org.apache.camel.language.ExpressionEvaluationException;
+import org.apache.commons.jxpath.CompiledExpression;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.JXPathException;
+
+/**
+ * <a href="http://commons.apache.org/jxpath/">JXPath</a> {@link Expression} support 
+ */
+public class JXPathExpression extends ExpressionSupport<Exchange> {
+
+	private String expression;
+	private CompiledExpression compiledExpression;
+	private final Class<?> type;
+
+	/**
+	 * Creates a new JXPathExpression instance
+	 * 
+	 * @param expression the JXPath expression to be evaluated
+	 * @param type the expected result type
+	 */
+	public JXPathExpression(String expression, Class<?> type) {
+		super();
+		this.expression = expression;
+		this.type = type;
+	}
+
+    
+    public Object evaluate(Exchange exchange) {
+		try {
+			Object result = getJXPathExpression().getValue(JXPathContext.newContext(exchange));
+			assertResultType(exchange, result);
+			return result;
+		} catch (JXPathException e) {
+			throw new ExpressionEvaluationException(this, exchange, e);
+		}
+	}
+
+	/*
+	 * Check if the result is of the specified type
+	 */
+	private void assertResultType(Exchange exchange, Object result) {
+		if (!type.isAssignableFrom(result.getClass())) {
+			throw new JXPathException("JXPath result type is " + result.getClass() + " instead of required type " + type);
+		}
+	}
+
+	@Override
+	protected String assertionFailureMessage(Exchange exchange) {
+		return expression.toString();
+	}
+	
+	/*
+	 * Get a compiled expression instance for better performance
+	 */
+	private synchronized CompiledExpression getJXPathExpression() {
+		if (compiledExpression == null) {
+			compiledExpression = JXPathContext.compile(expression);
+		}
+		return compiledExpression;
+	}
+}
---
+++ components/camel-jxpath/src/test/java/org/apache/camel/language/jxpath/JXPathTest.java
@@ -0,0 +1,77 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.language.jxpath;
+
+import org.apache.camel.LanguageTestSupport;
+import org.apache.camel.language.ExpressionEvaluationException;
+
+/**
+ * Test for {@link JXPathExpression} and {@link JXPathLanguage}
+ */
+public class JXPathTest extends LanguageTestSupport {
+	
+	/**
+	 * Test JXPath expressions
+	 */
+	public void testJXPathExpressions() throws Exception {
+		assertExpression(".", exchange);
+		assertExpression("./in/body", "<hello id='m123'>world!</hello>");
+		assertExpression("in/body", "<hello id='m123'>world!</hello>");
+		assertExpression("in/headers", exchange.getIn().getHeaders());
+		assertExpression("in/headers/@foo", "abc");
+	}
+	
+	/**
+	 * Test JXPath predicates
+	 */
+	public void testJXPathPredicates() throws Exception {
+		assertPredicate("in/headers/@foo = 'abc'");
+	}
+	
+	/**
+	 * Test exceptions being thrown appropriately
+	 */
+	public void testExceptions() throws Exception {
+		assertInvalidExpression(".@.");
+		assertInvalidExpression("ins/body");
+		assertInvalidPredicate("in/body");
+	}
+
+	private void assertInvalidExpression(String expression) {
+		try {
+			assertExpression(expression, null);
+			fail("Expected an ExpressionEvaluationException");
+		} catch (ExpressionEvaluationException e) {
+			//nothing to do -- test success
+		}
+	}
+	
+	private void assertInvalidPredicate(String predicate) {
+		try {
+			assertPredicate(predicate);
+			fail("Expected an ExpressionEvaluationException");
+		} catch (ExpressionEvaluationException e) {
+			//nothing to do -- test success
+		}		
+	}
+
+	@Override
+	protected String getLanguageName() {
+		return "jxpath";
+	}
+
+}
