07e103c8fdc1cd1cc6e7bf1b9b3c16dac2dddf76
James Strachan <jstrachan@apache.org>
2008-03-19 12:11:42 +0000

    added test case and fix for https://issues.apache.org/activemq/browse/CAMEL-391
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@638810 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/main/java/org/apache/camel/spring/handler/BeanDefinitionParser.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/handler/BeanDefinitionParser.java
@@ -16,16 +16,24 @@
  */
 package org.apache.camel.spring.handler;
 
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.core.Conventions;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
-
-import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
+import org.w3c.dom.NamedNodeMap;
 
 /**
  * A base class for a parser for a bean.
  *
  * @version $Revision$
  */
-public class BeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
+// TODO cannot use AbstractSimpleBeanDefinitionParser
+// as doParse() is final and isEligableAttribute does not allow us to filter out attributes
+// with the name "xmlns:"
+public class BeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
     private Class type;
 
     protected BeanDefinitionParser() {
@@ -46,8 +54,76 @@ public class BeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
         throw new IllegalArgumentException("No type specified!");
     }
 
-    @Override
     protected boolean isEligibleAttribute(String attributeName) {
-        return attributeName != null && super.isEligibleAttribute(attributeName) && !attributeName.equals("xmlns");
+        return attributeName != null && !ID_ATTRIBUTE.equals(attributeName)
+                && !attributeName.equals("xmlns") && !attributeName.startsWith("xmlns:");
     }
+
+    // TODO the following code is copied from AbstractSimpleBeanDefinitionParser
+    // it can be removed if ever the doParse() method is not final!
+    // or the Spring bug http://jira.springframework.org/browse/SPR-4599 is resolved
+
+    /**
+     * Parse the supplied {@link Element} and populate the supplied
+     * {@link BeanDefinitionBuilder} as required.
+     * <p>This implementation maps any attributes present on the
+     * supplied element to {@link org.springframework.beans.PropertyValue}
+     * instances, and
+     * {@link BeanDefinitionBuilder#addPropertyValue(String, Object) adds them}
+     * to the
+     * {@link org.springframework.beans.factory.config.BeanDefinition builder}.
+     * <p>The {@link #extractPropertyName(String)} method is used to
+     * reconcile the name of an attribute with the name of a JavaBean
+     * property.
+     *
+     * @param element the XML element being parsed
+     * @param builder used to define the <code>BeanDefinition</code>
+     * @see #extractPropertyName(String)
+     */
+    protected final void doParse(Element element, BeanDefinitionBuilder builder) {
+        NamedNodeMap attributes = element.getAttributes();
+        for (int x = 0; x < attributes.getLength(); x++) {
+            Attr attribute = (Attr) attributes.item(x);
+            String name = attribute.getLocalName();
+            String fullName = attribute.getName();
+            if (!fullName.startsWith("xmlns:") && !fullName.equals("xmlns") && isEligibleAttribute(name)) {
+                String propertyName = extractPropertyName(name);
+                Assert.state(StringUtils.hasText(propertyName),
+                        "Illegal property name returned from 'extractPropertyName(String)': cannot be null or empty.");
+                builder.addPropertyValue(propertyName, attribute.getValue());
+            }
+        }
+        postProcess(builder, element);
+    }
+
+
+    /**
+     * Extract a JavaBean property name from the supplied attribute name.
+     * <p>The default implementation uses the
+     * {@link Conventions#attributeNameToPropertyName(String)}
+     * method to perform the extraction.
+     * <p>The name returned must obey the standard JavaBean property name
+     * conventions. For example for a class with a setter method
+     * '<code>setBingoHallFavourite(String)</code>', the name returned had
+     * better be '<code>bingoHallFavourite</code>' (with that exact casing).
+     *
+     * @param attributeName the attribute name taken straight from the
+     *                      XML element being parsed (never <code>null</code>)
+     * @return the extracted JavaBean property name (must never be <code>null</code>)
+     */
+    protected String extractPropertyName(String attributeName) {
+        return Conventions.attributeNameToPropertyName(attributeName);
+    }
+
+    /**
+     * Hook method that derived classes can implement to inspect/change a
+     * bean definition after parsing is complete.
+     * <p>The default implementation does nothing.
+     *
+     * @param beanDefinition the parsed (and probably totally defined) bean definition being built
+     * @param element        the XML element that was the source of the bean definition's metadata
+     */
+    protected void postProcess(BeanDefinitionBuilder beanDefinition, Element element) {
+	}
+
 }
