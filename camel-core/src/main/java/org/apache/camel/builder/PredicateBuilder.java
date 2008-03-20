4b8198e307009144bd7542aed796a96e7de2e913
Roman Kalukiewicz <romkal@apache.org>
2008-03-20 21:28:44 +0000

    Two small typos
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@639467 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/builder/PredicateBuilder.java
+++ camel-core/src/main/java/org/apache/camel/builder/PredicateBuilder.java
@@ -147,7 +147,7 @@ public class PredicateBuilder {
             }
 
             protected String getOperationText() {
-                return "==";
+                return "!=";
             }
         };
     }
