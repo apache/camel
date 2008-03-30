a7b76f381239066d48524c92fba9c5826b5a3c4a
Claus Ibsen <davsclaus@apache.org>
2008-03-30 12:01:12 +0000

    Added unit test to see if there is a problem in Camel according to the topic: http://www.nabble.com/FTP-to-FILE-td16354813s22882.html
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@642719 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FromFtpToAsciiFileTest.java
@@ -0,0 +1,54 @@
+package org.apache.camel.component.file.remote;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileComponent;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Producer;
+import org.apache.camel.Exchange;
+
+/**
+ * Unit test to verify that we can pool an ASCII file from the FTP Server and store it on a local file path
+ */
+public class FromFtpToAsciiFileTest extends FtpRouteTest {
+
+    public void testFtpRoute() throws Exception {
+        resultEndpoint.expectedMinimumMessageCount(1);
+        resultEndpoint.expectedBodiesReceived("Hello World from FTPServer");
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    protected String createFtpUrl() {
+        return "ftp://admin@localhost:" + port + "/tmp3/camel?password=admin&binary=false";
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        prepareFtpServer();
+    }
+
+    private void prepareFtpServer() throws Exception {
+        // prepares the FTP Server by creating a file on the server that we want to unit
+        // test that we can pool and store as a local file
+        Endpoint endpoint = context.getEndpoint(ftpUrl);
+        Exchange exchange = endpoint.createExchange();
+        exchange.getIn().setBody("Hello World from FTPServer");
+        exchange.getIn().setHeader(FileComponent.HEADER_FILE_NAME, "hello.txt");
+        Producer producer = endpoint.createProducer();
+        producer.start();
+        producer.process(exchange);
+        producer.stop();
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                String fileUrl = "file:target/ftptest/?append=false&noop=true";
+                from(ftpUrl).setHeader(FileComponent.HEADER_FILE_NAME, constant("deleteme.txt")).
+                        convertBodyTo(String.class).to(fileUrl).to("mock:result");
+            }
+        };
+    }
+
+}
+
