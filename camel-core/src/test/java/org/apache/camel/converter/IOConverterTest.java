3c857b49d856463c3fa0ec6c54e36b57d5951cc1
Willem Ning Jiang <ningjiang@apache.org>
2008-03-31 03:36:51 +0000

    Fixed the unit test of IOConverterTest failed on Windows Box
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@642864 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/converter/IOConverterTest.java
+++ camel-core/src/test/java/org/apache/camel/converter/IOConverterTest.java
@@ -28,26 +28,32 @@ import junit.framework.TestCase;
  * Test case for {@link IOConverter}
  */
 public class IOConverterTest extends TestCase {
-    
+
     private static final byte[] TESTDATA = "My test data".getBytes();
-    
+
     public void testToBytes() throws FileNotFoundException, IOException {
+        int fileSize = 962;
         byte[] data = IOConverter.toBytes(new FileInputStream("src/test/resources/org/apache/camel/converter/dummy.txt"));
-        assertEquals(962, data.length);
+        // since we set the dummy.txt svn property svn:eol-style to be native,
+        // the file size could be different when you run the test in Windows box
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            fileSize = 979;
+        }
+        assertEquals("get the wrong byte size", fileSize, data.length);
         assertEquals('#', (char) data[0]);
         assertEquals('!', (char) data[data.length - 1]);
     }
-    
+
     public void testCopy() throws IOException {
         ByteArrayInputStream bis = new ByteArrayInputStream(TESTDATA);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         IOConverter.copy(bis, bos);
         assertEquals(TESTDATA, bos.toByteArray());
     }
-    
+
     private void assertEquals(byte[] data1, byte[] data2) {
         assertEquals(data1.length, data2.length);
-        for (int i = 0 ; i < data1.length ; i++) {
+        for (int i = 0; i < data1.length; i++) {
             assertEquals(data1[i], data2[i]);
         }
     }
