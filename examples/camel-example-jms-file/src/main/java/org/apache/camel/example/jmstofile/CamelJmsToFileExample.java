0a88ac7fc2b1955f0dab8a363a9f25e72017d147
Claus Ibsen <davsclaus@apache.org>
2008-04-03 05:16:00 +0000

    CAMEL-431 Applied patch with thanks to Glen Mazza
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@644171 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-example-jms-file/src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
+++ examples/camel-example-jms-file/src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
@@ -28,9 +28,9 @@ import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
 /**
- * An example class for demonstrating some of the basics behind camel This
- * example will send some text messages on to a JMS Queue, consume them and
- * persist them to disk
+ * An example class for demonstrating some of the basics behind Camel. This
+ * example sends some text messages on to a JMS Queue, consumes them and
+ * persists them to disk
  * 
  * @version $Revision$
  */
@@ -46,7 +46,7 @@ public final class CamelJmsToFileExample {
         // Set up the ActiveMQ JMS Components
         // START SNIPPET: e2
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-        // note we can explicity name the component
+        // Note we can explicity name the component
         context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
         // END SNIPPET: e2
         // Add some configuration by hand ...
@@ -71,16 +71,15 @@ public final class CamelJmsToFileExample {
         // END SNIPPET: e4
         // Now everything is set up - lets start the context
         context.start();
-        // now send some test text to a component - for this case a JMS Queue
+        // Now send some test text to a component - for this case a JMS Queue
         // The text get converted to JMS messages - and sent to the Queue
         // test.queue
         // The file component is listening for messages from the Queue
         // test.queue, consumes
         // them and stores them to disk. The content of each file will be the
-        // test test we sent here.
-        // The listener on the file component gets notfied when new files are
-        // found ...
-        // that's it!
+        // test we sent here.
+        // The listener on the file component gets notified when new files are
+        // found ... that's it!
         // START SNIPPET: e5
         for (int i = 0; i < 10; i++) {
             template.sendBody("test-jms:queue:test.queue", "Test Message: " + i);
--- examples/camel-example-spring/README.txt
+++ examples/camel-example-spring/README.txt
@@ -10,12 +10,12 @@ To run the example type
   mvn camel:run
 
 You can see the routing rules by looking at the java code in the src/main/java directory
-and the Spring XML configuration lives in src/main/resources/META-INF/spring
+and the Spring XML configuration file in src/main/resources/META-INF/spring
 
 For the latest & greatest documentation on how to use this example please see
   http://activemq.apache.org/camel/spring-example.html
 
-If you hit any problems please talk to us on the Camel Forums
+If you hit any problems please let us know on the Camel Forums
   http://activemq.apache.org/camel/discussion-forums.html
 
 Please help us make Apache Camel better - we appreciate any feedback you may have.
@@ -23,4 +23,5 @@ Enjoy!
 
 ------------------------
 The Camel riders!
+
 
