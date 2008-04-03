635aaf3aa6f0d73312254e698cbaf1f5142a9884
James Strachan <jstrachan@apache.org>
2008-04-03 15:58:58 +0000

    added some type converters for HTTP messages - also switched the default binding to use the InputStream
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@644384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-http/src/main/java/org/apache/camel/component/http/HttpConverter.java
@@ -0,0 +1,67 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.camel.Converter;
+
+/**
+ * Some converter methods making it easy to convert the body of a message to servlet types or to switch between
+ * the underlying {@link ServletInputStream} or {@link BufferedReader} payloads etc.
+ *
+ * @version $Revision: 1.1 $
+ */
+@Converter
+public class HttpConverter {
+    @Converter
+    public HttpServletRequest toServletRequest(HttpMessage message) {
+        if (message == null) {
+            return null;
+        }
+        return message.getRequest();
+    }
+
+    @Converter
+    public ServletInputStream toServletInputStream(HttpMessage message) throws IOException {
+        System.out.println("Converting to an InputStream using the HttpConverter!");
+        HttpServletRequest request = toServletRequest(message);
+        if (request != null) {
+            return request.getInputStream();
+        }
+        return null;
+    }
+
+    @Converter
+    public ServletInputStream toInputStream(HttpMessage message) throws IOException {
+        return toServletInputStream(message);
+    }
+
+    @Converter
+    public BufferedReader toReader(HttpMessage message) throws IOException {
+        HttpServletRequest request = toServletRequest(message);
+        if (request != null) {
+            return request.getReader();
+        }
+        return null;
+    }
+}
