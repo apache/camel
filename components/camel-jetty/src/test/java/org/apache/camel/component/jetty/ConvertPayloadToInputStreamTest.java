a9f354257564b4cef9ab0468980fefa940445e5f
James Strachan <jstrachan@apache.org>
2008-04-03 15:58:58 +0000

    added some type converters for HTTP messages - also switched the default binding to use the InputStream
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@644384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jetty/src/test/java/org/apache/camel/component/jetty/ConvertPayloadToInputStreamTest.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jetty;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision$
+ */
+public class ConvertPayloadToInputStreamTest extends ContextTestSupport {
+    protected String expectedBody = "<hello>world!</hello>";
+
+    public void testConvertPayloadToInputStream() throws Exception {
+        MockEndpoint mockEndpoint = getMockEndpoint("mock:result");
+        mockEndpoint.expectedMessageCount(1);
+
+        template.sendBodyAndHeader("http://localhost:8080/test", expectedBody, "Content-Type", "application/xml");
+
+        mockEndpoint.assertIsSatisfied();
+        List<Exchange> list = mockEndpoint.getReceivedExchanges();
+        Exchange exchange = list.get(0);
+        assertNotNull("exchange", exchange);
+
+        Message in = exchange.getIn();
+        assertNotNull("in", in);
+
+        Object actual = in.getBody();
+        InputStream value = assertIsInstanceOf(InputStream.class, actual);
+        System.out.println("Found: " + value);
+    }
+
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() {
+                from("jetty:http://localhost:8080/test").
+                        process(new Processor() {
+                            public void process(Exchange exchange) throws Exception {
+                                Object value = exchange.getIn().getBody();
+                                System.out.println("Body is: " + value);
+
+                                if (value instanceof Reader) {
+                                    System.out.println("Is a Reader!");
+                                }
+                                if (value instanceof InputStream) {
+                                    System.out.println("Is an InputStream!");
+                                }
+                            }
+                        }).
+                        convertBodyTo(InputStream.class).
+                        to("mock:result");
+            }
+        };
+    }
+}
