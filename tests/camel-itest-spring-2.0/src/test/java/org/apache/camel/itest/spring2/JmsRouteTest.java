5bf5bb9b3c866de6b9e7a704e075d84b39ae4df3
James Strachan <jstrachan@apache.org>
2008-04-03 15:11:35 +0000

    added an integration test to show https://issues.apache.org/activemq/browse/CAMEL-435 is a problem, then patched the camel-jms code to not depend directly on BrowserCallback from Spring 2.5.x
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@644350 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest-spring-2.0/src/test/java/org/apache/camel/itest/spring2/JmsRouteTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.itest.spring2;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import static org.apache.camel.component.jms.JmsComponent.jmsComponentClientAcknowledge;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision$
+ */
+public class JmsRouteTest extends ContextTestSupport {
+    public void testSendAndReceiveMessage() throws Exception {
+        assertSendAndReceiveBody("Hello there!");
+    }
+
+    protected void assertSendAndReceiveBody(Object expectedBody) throws InterruptedException {
+        MockEndpoint resultEndpoint = getMockEndpoint("mock:result");
+
+        resultEndpoint.expectedBodiesReceived(expectedBody);
+        resultEndpoint.message(0).header("cheese").isEqualTo(123);
+
+        template.sendBodyAndHeader("activemq:queue:test.a", expectedBody, "cheese", 123);
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext camelContext = super.createCamelContext();
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        camelContext.addComponent("activemq", jmsComponentClientAcknowledge(connectionFactory));
+
+        return camelContext;
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                from("activemq:queue:test.a").to("activemq:queue:test.b");
+                from("activemq:queue:test.b").to("mock:result");
+            }
+        };
+    }
+}
