b4174d1a05987312bd82e27c99839a840b316c9a
James Strachan <jstrachan@apache.org>
2008-04-04 12:36:27 +0000

    by default lets use a reporter to dump the progress of sending messages
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@644696 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/component/dataset/DataSetConsumer.java
+++ camel-core/src/main/java/org/apache/camel/component/dataset/DataSetConsumer.java
@@ -18,6 +18,7 @@ package org.apache.camel.component.dataset;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.processor.ThroughputLogger;
 import org.apache.camel.impl.DefaultConsumer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -28,6 +29,7 @@ import org.apache.commons.logging.LogFactory;
 public class DataSetConsumer extends DefaultConsumer<Exchange> {
     private static final transient Log LOG = LogFactory.getLog(DataSetConsumer.class);
     private DataSetEndpoint endpoint;
+    private Processor reporter;
 
     public DataSetConsumer(DataSetEndpoint endpoint, Processor processor) {
         super(endpoint, processor);
@@ -38,17 +40,24 @@ public class DataSetConsumer extends DefaultConsumer<Exchange> {
     protected void doStart() throws Exception {
         super.doStart();
 
+        if (reporter == null) {
+            reporter = createReporter();
+        }
+        final DataSet dataSet = endpoint.getDataSet();
+        final long preloadSize = endpoint.getPreloadSize();
+
+        sendMessages(0, preloadSize);
+
         endpoint.getExecutorService().execute(new Runnable() {
             public void run() {
-                sendMessages();
+                sendMessages(preloadSize, dataSet.getSize());
             }
         });
     }
 
-    protected void sendMessages() {
+    protected void sendMessages(long startIndex, long endIndex) {
         try {
-            DataSet dataSet = endpoint.getDataSet();
-            for (long i = 0; i < dataSet.getSize(); i++) {
+            for (long i = startIndex; i < endIndex; i++) {
                 Exchange exchange = endpoint.createExchange(i);
                 getProcessor().process(exchange);
 
@@ -60,9 +69,18 @@ public class DataSetConsumer extends DefaultConsumer<Exchange> {
                         LOG.debug(e);
                     }
                 }
+                if (reporter != null) {
+                    reporter.process(exchange);
+                }
             }
         } catch (Exception e) {
             LOG.error(e);
         }
+    }
+
+    protected ThroughputLogger createReporter() {
+        ThroughputLogger answer = new ThroughputLogger(endpoint.getEndpointUri(), (int) endpoint.getDataSet().getReportCount());
+        answer.setAction("Sent");
+        return answer;
     }
 }
