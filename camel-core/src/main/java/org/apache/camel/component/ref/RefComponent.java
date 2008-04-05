1832a2b99b441a7a68822a391fc1c54748756142
Claus Ibsen <davsclaus@apache.org>
2008-04-05 15:28:49 +0000

    javadoc polishing (adding missing package.html)
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@645123 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/component/ref/RefComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/ref/RefComponent.java
@@ -23,7 +23,9 @@ import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultComponent;
 
 /**
- *
+ * Component for lookup of existing endpoints bound in the {@link org.apache.camel.spi.Registry}.
+ * <p/>
+ * This component uses the <tt>ref:</tt> notation instead of the mostly common <tt>uri:</tt> notation. 
  */
 public class RefComponent extends DefaultComponent<Exchange> {
 
