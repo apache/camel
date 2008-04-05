3c4fb7fd4b15d99b59b3f379b2fbd2fc0141ea9c
Willem Ning Jiang <ningjiang@apache.org>
2008-04-05 07:35:47 +0000

    Added the Apache license header to the files in the camel-scala component
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@645039 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-scala/src/test/scala/org/apache/camel/scala/dsl/FilterRouteBuilderTest.scala
+++ components/camel-scala/src/test/scala/org/apache/camel/scala/dsl/FilterRouteBuilderTest.scala
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.scala.dsl;
 
 class FilterRouteBuilderTest extends ScalaTestSupport {
@@ -7,7 +23,7 @@ class FilterRouteBuilderTest extends ScalaTestSupport {
     "direct:a" ! ("<hello/>", "<hellos/>")
     "mock:a" assert
   }
-  
+
   def testFilterWithAlternatives() = {
     "mock:b" expect {_.expectedMessageCount(1)}
     "mock:b" expect {_.expectedMessageCount(1)}
@@ -21,11 +37,11 @@ class FilterRouteBuilderTest extends ScalaTestSupport {
     "mock:e" assert()
     "mock:f" assert()
   }
-  
-  override protected def createRouteBuilder() = 
+
+  override protected def createRouteBuilder() =
     new RouteBuilder {
        "direct:a" when(_.in == "<hello/>") to "mock:a"
-         
+
        "direct:b" ==> {
          when(_.in == "<hello/>") then {
            to ("mock:b")
@@ -36,7 +52,7 @@ class FilterRouteBuilderTest extends ScalaTestSupport {
          } otherwise {
            to ("mock:f")
          }
-         to ("mock:d")  
+         to ("mock:d")
        }
     }.print
 
