9fde8dfd955d3a45c403aed5ce09c479b0e0f937
Claus Ibsen <davsclaus@apache.org>
2008-04-06 09:04:17 +0000

    CAMEL-420 - Added missing type converters to support consuming binary files - Refactored and added unit tests
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@645216 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpServerTestSupport.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.file.remote;
+
+import java.util.Properties;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.ConfigurableFtpServerContext;
+import org.apache.ftpserver.interfaces.FtpServerContext;
+import org.apache.ftpserver.config.PropertiesConfiguration;
+import org.apache.ftpserver.ftplet.Configuration;
+
+/**
+ * Base class for unit testing using a FTPServer
+ */
+public abstract class FtpServerTestSupport extends ContextTestSupport {
+    protected FtpServer ftpServer;
+
+    public abstract String getPort();
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        initFtpServer();
+        ftpServer.start();
+    }
+
+    protected void tearDown() throws Exception {
+        ftpServer.stop();
+        super.tearDown();
+    }
+
+    protected void initFtpServer() throws Exception {
+        // get the configuration object
+        Properties properties = createFtpServerProperties();
+        Configuration config = new PropertiesConfiguration(properties);
+
+        // create service context
+        FtpServerContext ftpConfig = new ConfigurableFtpServerContext(config);
+
+        // create the server object and start it
+        ftpServer = new FtpServer(ftpConfig);
+    }
+
+    protected Properties createFtpServerProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("config.listeners.default.port", getPort());
+        properties.setProperty("config.create-default-user", "true");
+        return properties;
+    }
+}
