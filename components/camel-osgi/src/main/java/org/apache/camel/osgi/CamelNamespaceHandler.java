d1e58df8ae82695e9e74ff0527f3514176ba4a8c
Claus Ibsen <davsclaus@apache.org>
2008-04-14 05:15:38 +0000

    javadoc polishing
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@647665 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-osgi/src/main/java/org/apache/camel/osgi/CamelNamespaceHandler.java
+++ components/camel-osgi/src/main/java/org/apache/camel/osgi/CamelNamespaceHandler.java
@@ -19,13 +19,6 @@ package org.apache.camel.osgi;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
-/**
- * Created by IntelliJ IDEA.
- * User: gnodet
- * Date: Sep 20, 2007
- * Time: 11:24:23 AM
- * To change this template use File | Settings | File Templates.
- */
 public class CamelNamespaceHandler extends org.apache.camel.spring.handler.CamelNamespaceHandler {
 
     public void init() {
