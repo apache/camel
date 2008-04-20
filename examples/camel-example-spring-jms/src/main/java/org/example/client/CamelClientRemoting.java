86af6f42059cdabd543d3c05d3252bcdfcd0ab00
Willem Ning Jiang <ningjiang@apache.org>
2008-04-20 12:47:22 +0000

    CAMEL-471 added the tutorial example into the example directory
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@649916 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-spring-jms/src/main/java/org/example/client/CamelClientRemoting.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example.client;
+
+import org.example.server.Multiplier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Requires that the JMS broker is running, as well as CamelServer
+ *
+ * @author martin.gilday
+ */
+public final class CamelClientRemoting {
+
+    private CamelClientRemoting() {
+        // the main class
+    }
+
+    public static void main(final String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("camel-client-remoting.xml");
+        Multiplier multiplier = (Multiplier)context.getBean("multiplierProxy");
+        int response = multiplier.multiply(22);
+        System.out.println("Invoking the multiply with 22, the result is " + response);
+        System.exit(0);
+
+    }
+
+}
