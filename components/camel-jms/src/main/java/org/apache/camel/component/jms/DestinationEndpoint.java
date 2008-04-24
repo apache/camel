36f45ec75db2b22d4ea9f53528e0e8d3e44f5c44
Willem Ning Jiang <ningjiang@apache.org>
2008-04-24 08:39:41 +0000

    Fixed the CS errors in camel-jms component
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@651197 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/DestinationEndpoint.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/DestinationEndpoint.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,12 +17,12 @@
 package org.apache.camel.component.jms;
 
 import javax.jms.Destination;
-import javax.jms.Session;
 import javax.jms.JMSException;
+import javax.jms.Session;
 
 /**
- * An optional interface that a {@link JmsEndpoint} may implement to return the underlying
- * {@link Destination} object
+ * An optional interface that a {@link JmsEndpoint} may implement to return the
+ * underlying {@link Destination} object
  *
  * @version $Revision: 1.1 $
  */
--- components/camel-jms/src/test/java/org/apache/camel/component/jms/temp/JmsProviderTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/temp/JmsProviderTest.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,13 +20,13 @@ import javax.jms.TemporaryQueue;
 import javax.jms.TemporaryTopic;
 import javax.naming.Context;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.activemq.command.ActiveMQTempQueue;
+import org.apache.activemq.command.ActiveMQTempTopic;
 import org.apache.camel.ContextTestSupport;
 import org.apache.camel.component.jms.JmsConfiguration;
 import org.apache.camel.component.jms.JmsEndpoint;
 import org.apache.camel.component.jms.JmsProviderMetadata;
-import org.apache.activemq.camel.component.ActiveMQComponent;
-import org.apache.activemq.command.ActiveMQTempQueue;
-import org.apache.activemq.command.ActiveMQTempTopic;
 
 /**
  * @version $Revision: 1.1 $
@@ -43,13 +42,13 @@ public class JmsProviderTest extends ContextTestSupport {
         Class<? extends TemporaryTopic> topicType = endpoint.getTemporaryTopicType();
 
         log.info("Found queue type: " + queueType);
-                log.info("Found topic type: " + topicType);
+        log.info("Found topic type: " + topicType);
 
-                assertNotNull("queueType", queueType);
+        assertNotNull("queueType", queueType);
         assertNotNull("topicType", topicType);
 
         assertEquals("queueType", ActiveMQTempQueue.class, queueType);
-        assertEquals("topicType", ActiveMQTempTopic.class, topicType);        
+        assertEquals("topicType", ActiveMQTempTopic.class, topicType);
     }
 
     @Override
