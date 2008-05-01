3bd0ceeb5de619de3a2f73e82053e7eec8dd19be
James Strachan <jstrachan@apache.org>
2008-05-01 11:42:44 +0000

    added a little library of hamcrest assertions folks can use in their unit testing of camel routes
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@652479 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-hamcrest/src/main/java/org/apache/camel/hamcrest/Assertions.java
@@ -0,0 +1,56 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.hamcrest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+
+/**
+ * A set of useful assertions you can use when testing
+ * 
+ * @version $Revision: 1.1 $
+ */
+public class Assertions {
+
+    /**
+     * Performs the assertion that the given value is an instance of the specified type
+     *
+     * @param value the value to be compared
+     * @param type  the type to assert
+     * @return the value cast as the type
+     * @throws AssertionError if the instance is not of the correct type
+     */
+    public static <T> T assertInstanceOf(Object value, Class<T> type) {
+        assertThat(value, instanceOf(type));
+        return type.cast(value);
+    }
+
+    /**
+     * Performs the assertion that the given value is an instance of the specified type
+     *
+     * @param message the description of the value
+     * @param value   the value to be compared
+     * @param type    the type to assert
+     * @return the value cast as the type
+     * @throws AssertionError if the instance is not of the correct type
+     */
+    public static <T> T assertInstanceOf(String message, Object value, Class<T> type) {
+        assertThat(message, value, instanceOf(type));
+        return type.cast(value);
+    }
+}
