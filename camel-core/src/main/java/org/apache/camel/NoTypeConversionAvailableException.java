6fbb8529deab8321a7d4153eb33cda31ad18126f
Claus Ibsen <davsclaus@apache.org>
2008-05-06 05:14:33 +0000

    CAMEL-84: improved error message for exception
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@653679 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/NoTypeConversionAvailableException.java
+++ camel-core/src/main/java/org/apache/camel/NoTypeConversionAvailableException.java
@@ -26,27 +26,36 @@ public class NoTypeConversionAvailableException extends RuntimeCamelException {
     private final Class type;
 
     public NoTypeConversionAvailableException(Object value, Class type) {
-        super("No converter available to convert value: " + value + " to the required type: "
-              + type.getName());
+        super("No type converter available to convert from type: " + (value != null ? value.getClass() : null) +
+            " to the required type " + type.getName() + " with value " + value);
         this.value = value;
         this.type = type;
     }
 
     /**
      * Returns the value which could not be converted
-     * 
-     * @return the value that could not be converted
      */
     public Object getValue() {
         return value;
     }
 
     /**
-     * Returns the required type
-     * 
-     * @return the required type
+     * Returns the required <tt>to</tt> type
      */
     public Class getType() {
         return type;
     }
+
+    /**
+     * Returns the required <tt>from</tt> type.
+     * Returns <tt>null</tt> if the provided value was null.
+     */
+    public Class getFromType() {
+        if (value != null) {
+            return value.getClass();
+        } else {
+            return null;
+        }
+    }
+
 }
