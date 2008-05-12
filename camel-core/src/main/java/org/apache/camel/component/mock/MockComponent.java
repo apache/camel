694eef10537f1e413d4e5f54270bcd707fec9a9b
Claus Ibsen <davsclaus@apache.org>
2008-05-12 08:42:34 +0000

    CAMEL-433: Added getAndRemoveParameter helper methods to follow Camel components programming idiom where used parameters must be removed from the parameters map
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@655440 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/component/mock/MockComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/mock/MockComponent.java
@@ -21,7 +21,6 @@ import java.util.Map;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.converter.ObjectConverter;
 import org.apache.camel.impl.DefaultComponent;
 import org.apache.camel.processor.ThroughputLogger;
 
@@ -35,7 +34,7 @@ public class MockComponent extends DefaultComponent<Exchange> {
     @Override
     protected Endpoint<Exchange> createEndpoint(String uri, String remaining, Map parameters) throws Exception {
         MockEndpoint endpoint = new MockEndpoint(uri, this);
-        Integer value = ObjectConverter.toInteger(parameters.remove("reportGroup"));
+        Integer value = getAndRemoveParameter(parameters, "reportGroup", Integer.class);
         if (value != null) {
             Processor reporter = new ThroughputLogger("org.apache.camel.mock:" + remaining, value);
             endpoint.setReporter(reporter);
