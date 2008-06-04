ed9d3b1e6dbe8cc87539ec043313391b2dd16b0e
Willem Ning Jiang <ningjiang@apache.org>
2008-06-04 08:07:26 +0000

    CAMEL-575 patch applied with thanks to William
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@663026 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring/src/test/java/org/apache/camel/spring/DummyLifecycleStrategy.java
@@ -0,0 +1,62 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring;
+
+import java.util.Collection;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Route;
+import org.apache.camel.Service;
+import org.apache.camel.spi.LifecycleStrategy;
+import org.apache.camel.spi.RouteContext;
+
+/**
+ * Dummy LifecycleStrategy for LifecycleStrategy injection test.
+ *
+ * @version $Revision$
+ *
+ */
+public class DummyLifecycleStrategy implements LifecycleStrategy {
+
+    public void onContextCreate(CamelContext context) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void onEndpointAdd(Endpoint<? extends Exchange> endpoint) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void onRouteContextCreate(RouteContext routeContext) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void onRoutesAdd(Collection<Route> routes) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void onServiceAdd(CamelContext context, Service service) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
