6f8f1caa8c544041327192aae956d2ec436547a8
James Strachan <jstrachan@apache.org>
2008-06-09 16:28:43 +0000

    made StringSource externalizable along with a test case
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@665748 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/converter/jaxp/StringSource.java
+++ camel-core/src/main/java/org/apache/camel/converter/jaxp/StringSource.java
@@ -16,24 +16,21 @@
  */
 package org.apache.camel.converter.jaxp;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.Reader;
-import java.io.Serializable;
-import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
-
 import javax.xml.transform.stream.StreamSource;
+import java.io.*;
 
 /**
  * A helper class which provides a JAXP {@link javax.xml.transform.Source Source} from a String which can
  * be read as many times as required.
- * 
+ *
  * @version $Revision$
  */
-public class StringSource extends StreamSource implements Serializable {
-    private final String text;
+public class StringSource extends StreamSource implements Externalizable {
+    private String text;
     private String encoding = "UTF-8";
+
+    public StringSource() {
+    }
 
     public StringSource(String text) {
         if (text == null) {
@@ -73,4 +70,29 @@ public class StringSource extends StreamSource implements Serializable {
         return text;
     }
 
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(text);
+        out.writeUTF(encoding);
+        out.writeUTF(getPublicId());
+        out.writeUTF(getSystemId());
+    }
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        text = in.readUTF();
+        encoding = in.readUTF();
+        setPublicId(in.readUTF());
+        setSystemId(in.readUTF());
+    }
 }
