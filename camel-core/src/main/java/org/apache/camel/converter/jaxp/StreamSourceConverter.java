8a1011eaf73fdbd9edc811df6f8b609ab54aec8d
James Strachan <jstrachan@apache.org>
2008-06-20 11:45:17 +0000

    fix for https://issues.apache.org/activemq/browse/CAMEL-624
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@669864 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/converter/jaxp/StreamSourceConverter.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.converter.jaxp;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.camel.Converter;
+
+/**
+ * A converter from {@link StreamSource} objects
+ *
+ * @version $Revision$
+ */
+@Converter
+public class StreamSourceConverter {
+
+    @Converter
+    public InputStream toInputStream(StreamSource source) {
+        return source.getInputStream();
+    }
+
+    @Converter
+    public Reader toReader(StreamSource source) {
+        return source.getReader();
+    }
+}
