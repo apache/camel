60b8af6dd6e87f72ffd6cabb731f8b25663b31eb
Claus Ibsen <davsclaus@apache.org>
2008-07-03 05:20:31 +0000

    Polished the JDBC component to use spring-jdbc for unit testing instead of our own datasource code etc. And log.warn when error closing JDBC resources as the norm is.
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@673587 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jdbc/src/test/java/org/apache/camel/component/jdbc/JdbcRouteTest.java
+++ components/camel-jdbc/src/test/java/org/apache/camel/component/jdbc/JdbcRouteTest.java
@@ -16,85 +16,94 @@
  */
 package org.apache.camel.component.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import javax.sql.DataSource;
 
-import junit.framework.TestCase;
-
-import org.apache.camel.CamelContext;
+import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.util.jndi.JndiContext;
+import org.apache.camel.impl.JndiRegistry;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 /**
  * @version $Revision$
  */
-public class JdbcRouteTest extends TestCase {
-    protected String driverClass = "org.hsqldb.jdbcDriver";
-    protected String url = "jdbc:hsqldb:mem:camel_jdbc";
-    protected String user = "sa";
-    protected String password = "";
-    protected Connection connection;
+public class JdbcRouteTest extends ContextTestSupport {
+    private String driverClass = "org.hsqldb.jdbcDriver";
+    private String url = "jdbc:hsqldb:mem:camel_jdbc";
+    private String user = "sa";
+    private String password = "";
+    private DataSource ds;
+    private JdbcTemplate jdbc;
 
     public void testPojoRoutes() throws Exception {
-        // START SNIPPET: register
-        DataSource ds = new TestDataSource(url, user, password);
-        JndiContext context = new JndiContext();
-        context.bind("testdb", ds);
-
-        CamelContext camelContext = new DefaultCamelContext(context);
-        // END SNIPPET: register
-
-        // START SNIPPET: route
-        // lets add simple route
-        camelContext.addRoutes(new RouteBuilder() {
-            public void configure() {
-                from("direct:hello").to("jdbc:testdb?readSize=100");
-            }
-        });
-        // END SNIPPET: route
-
-        camelContext.start();
-
         // START SNIPPET: invoke
-        Endpoint<Exchange> endpoint = camelContext.getEndpoint("direct:hello");
-        ProducerTemplate<Exchange> template = camelContext.createProducerTemplate();
+        // first we create our exchange using the endpoint
+        Endpoint endpoint = context.getEndpoint("direct:hello");
         Exchange exchange = endpoint.createExchange();
-        exchange.getIn().setBody("select * from customer");
+        // then we set the SQL on the in body
+        exchange.getIn().setBody("select * from customer order by ID");
+
+        // now we send the exchange to the endpoint, and receives the response from Camel
         Exchange out = template.send(endpoint, exchange);
+
+        // assertions of the response
         assertNotNull(out);
         assertNotNull(out.getOut());
         ArrayList<HashMap<String, Object>> data = out.getOut().getBody(ArrayList.class);
-        assertNotNull("out body could not be converted to an ArrayList - was: " + out.getOut().getBody(), data);
+        assertNotNull("out body could not be converted to an ArrayList - was: "
+            + out.getOut().getBody(), data);
         assertEquals(2, data.size());
+        HashMap<String, Object> row = data.get(0);
+        assertEquals("cust1", row.get("ID"));
+        assertEquals("jstrachan", row.get("NAME"));
+        row = data.get(1);
+        assertEquals("cust2", row.get("ID"));
+        assertEquals("nsandhu", row.get("NAME"));
         // END SNIPPET: invoke
-
-        camelContext.stop();
     }
 
-    /**
-     *
-     */
+
+    protected JndiRegistry createRegistry() throws Exception {
+        // START SNIPPET: register
+        JndiRegistry reg = super.createRegistry();
+        reg.bind("testdb", ds);
+        return reg;
+        // END SNIPPET: register
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            // START SNIPPET: route
+            // lets add simple route
+            public void configure() throws Exception {
+                from("direct:hello").to("jdbc:testdb?readSize=100");
+            }
+            // END SNIPPET: route
+        };
+    }
+
     protected void setUp() throws Exception {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(url, user, password);
+        dataSource.setDriverClassName(driverClass);
+        ds = dataSource;
+
+        JdbcTemplate jdbc = new JdbcTemplate(ds);
+        // START SNIPPET: setup
+        jdbc.execute("create table customer (id varchar(15), name varchar(10))");
+        jdbc.execute("insert into customer values('cust1','jstrachan')");
+        jdbc.execute("insert into customer values('cust2','nsandhu')");
+        // END SNIPPET: setup
         super.setUp();
-        Class.forName(driverClass);
-        connection = DriverManager.getConnection(url, user, password);
-        connection.createStatement().execute("create table customer (id varchar(15), name varchar(10))");
-        connection.createStatement().executeUpdate("insert into customer values('cust1','jstrachan')");
-        connection.createStatement().executeUpdate("insert into customer values('cust2','nsandhu')");
     }
 
     protected void tearDown() throws Exception {
-        if (connection != null) {
-            connection.createStatement().execute("drop table customer");
-            connection.close();
-        }
+        super.tearDown();
+        JdbcTemplate jdbc = new JdbcTemplate(ds);
+        jdbc.execute("drop table customer");
     }
+
 }
