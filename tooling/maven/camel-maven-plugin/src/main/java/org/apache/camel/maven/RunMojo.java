191e45e55ba1c7822d469a5b84584f7d2efcde9b
Willem Ning Jiang <ningjiang@apache.org>
2008-07-07 08:00:37 +0000

    CAMEL-681 Enable the duration option for camel:run plugin
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@674415 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/maven/camel-maven-plugin/src/main/java/org/apache/camel/maven/RunMojo.java
+++ tooling/maven/camel-maven-plugin/src/main/java/org/apache/camel/maven/RunMojo.java
@@ -85,12 +85,12 @@ public class RunMojo extends AbstractExecMojo {
      * Adding a s indicates seconds - eg "5s" means 5 seconds.
      *
      * @parameter expression="-1"
-     * @readonly
+     *
      */
     protected String duration;
 
     /**
-     * The DOT outputd irectory name used to generate the DOT diagram of the
+     * The DOT output directory name used to generate the DOT diagram of the
      * route definitions
      *
      * @parameter expression="${project.build.directory}/site/cameldoc"
