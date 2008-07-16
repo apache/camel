ff4efc71396ce5e9c3e85a2d8966ca377ba4e96d
Willem Ning Jiang <ningjiang@apache.org>
2008-07-16 07:42:40 +0000

    CAMEL-729 supports to set the consumer.delay etc in camel-spring-integration
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@677185 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring-integration/src/main/java/org/apache/camel/component/spring/integration/SpringIntegrationEndpoint.java
+++ components/camel-spring-integration/src/main/java/org/apache/camel/component/spring/integration/SpringIntegrationEndpoint.java
@@ -22,6 +22,7 @@ import org.apache.camel.ExchangePattern;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.impl.ScheduledPollEndpoint;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.integration.channel.MessageChannel;
@@ -31,7 +32,7 @@ import org.springframework.integration.channel.MessageChannel;
  *
  * @version $Revision$
  */
-public class SpringIntegrationEndpoint extends DefaultEndpoint<SpringIntegrationExchange> {
+public class SpringIntegrationEndpoint extends ScheduledPollEndpoint<SpringIntegrationExchange> {
     private static final Log LOG = LogFactory.getLog(SpringIntegrationEndpoint.class);
     private String inputChannel;
     private String outputChannel;
