d0d59955fd24603928e7fdcd218589362d06632c
Willem Ning Jiang <ningjiang@apache.org>
2008-07-22 12:19:04 +0000

    CAMEL-741 CAMEL-726 added test to verify the WS-Addressing with Camel transport
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@678734 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/customerrelations/CustomerServicesWsAddressingTest.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.itest.customerrelations;
+
+import java.lang.reflect.Proxy;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.apache.cxf.helpers.CastUtils;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.jaxws.JaxWsClientProxy;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CustomerServicesWsAddressingTest extends TestCase {
+
+    public void testCustomerService() throws Exception {
+        ClassPathXmlApplicationContext serverContext = null;
+        ClassPathXmlApplicationContext clientContext = null;
+        try {
+            serverContext = new ClassPathXmlApplicationContext(
+                new String[] {"spring-config/server-WsAddressingContext.xml"});
+            Object server = serverContext.getBean("org.apache.camel.itest.customerrelations.CustomerServiceV1");
+            assertNotNull("We should get server here", server);
+
+            clientContext =  new ClassPathXmlApplicationContext(
+                new String[] {"spring-config/client-WsAddressingContext.xml"});
+            CustomerServiceV1 customerService = (CustomerServiceV1) clientContext.getBean("org.apache.camel.itest.customerrelations.CustomerServiceV1");
+
+            Customer customer = customerService.getCustomer("12345");
+            assertNotNull("We should get Customer here", customer);
+        } finally {
+            if (clientContext != null) {
+                clientContext.destroy();
+            }
+            if (serverContext != null) {
+                serverContext.destroy();
+            }
+        }
+    }
+
+}
