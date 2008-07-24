8b24d3f3571956202226c21765724311397491e9
Willem Ning Jiang <ningjiang@apache.org>
2008-07-24 12:00:56 +0000

    CAMEL-751 Added a unit test of ErrorHandler in spring config
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@679378 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring/src/test/java/org/apache/camel/spring/config/ErrorHandlerTest.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.config;
+
+import java.util.List;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Processor;
+import org.apache.camel.Route;
+import org.apache.camel.impl.EventDrivenConsumerRoute;
+import org.apache.camel.processor.DeadLetterChannel;
+import org.apache.camel.processor.RedeliveryPolicy;
+import org.apache.camel.spring.SpringTestSupport;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ErrorHandlerTest extends SpringTestSupport {
+    protected ClassPathXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("org/apache/camel/spring/errorHandler.xml");
+    }
+
+    public void testEndpointConfiguration() throws Exception {
+        CamelContext context = getMandatoryBean(CamelContext.class, "camel");
+        List<Route> list = context.getRoutes();
+        assertEquals("Number routes created" + list, 1, list.size());
+        for (Route route : list) {
+            Endpoint key = route.getEndpoint();
+            assertEquals("From endpoint", "seda:a", key.getEndpointUri());
+
+            EventDrivenConsumerRoute consumerRoute = assertIsInstanceOf(EventDrivenConsumerRoute.class, route);
+            Processor processor = consumerRoute.getProcessor();
+            processor = unwrap(processor);
+
+            DeadLetterChannel deadLetterChannel = assertIsInstanceOf(DeadLetterChannel.class, processor);
+
+            RedeliveryPolicy redeliveryPolicy = deadLetterChannel.getRedeliveryPolicy();
+
+            assertEquals("getMaximumRedeliveries()", 1, redeliveryPolicy.getMaximumRedeliveries());
+            assertEquals("isUseExponentialBackOff()", true, redeliveryPolicy.isUseExponentialBackOff());
+        }
+    }
+
+}
