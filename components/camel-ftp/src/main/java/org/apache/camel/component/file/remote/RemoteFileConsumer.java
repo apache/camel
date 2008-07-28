ac536ad3439cc10471352c9a13fdfbfd169897e5
Claus Ibsen <davsclaus@apache.org>
2008-07-28 05:06:06 +0000

    Refactored camel-ftp so the producer and consumer share more code between the FTP and SFTP parts in their common super class.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@680254 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/RemoteFileConsumer.java
+++ components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/RemoteFileConsumer.java
@@ -20,9 +20,19 @@ import java.util.concurrent.ScheduledExecutorService;
 
 import org.apache.camel.Processor;
 import org.apache.camel.impl.ScheduledPollConsumer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public abstract class RemoteFileConsumer<T extends RemoteFileExchange> extends ScheduledPollConsumer<T> {
-    RemoteFileEndpoint<T> endpoint;
+    protected final transient Log LOG = LogFactory.getLog(getClass());
+    protected RemoteFileEndpoint<T> endpoint;
+    protected long lastPollTime;
+
+    protected boolean recursive = true;
+    protected String regexPattern;
+    protected boolean setNames = true;
+    protected boolean exclusiveRead = true;
+    protected boolean deleteFile;
 
     public RemoteFileConsumer(RemoteFileEndpoint<T> endpoint, Processor processor) {
         super(endpoint, processor);
@@ -32,4 +42,83 @@ public abstract class RemoteFileConsumer<T extends RemoteFileExchange> extends S
     public RemoteFileConsumer(RemoteFileEndpoint<T> endpoint, Processor processor, ScheduledExecutorService executor) {
         super(endpoint, processor, executor);
     }
+
+    /**
+     * Gets the filename.
+     *
+     * @param file the file object for the given consumer implementation.
+     * @return the filename as String.
+     */
+    protected abstract String getFileName(Object file);
+
+    /**
+     * Is the given file matched to be consumed (will consider regexp if provided as an option).
+     * <p/>
+     * Note: Returns true if no reg exp is used.
+     */
+    protected boolean isMatched(Object file) {
+        String fileName = getFileName(file);
+
+        boolean result = true;
+        if (regexPattern != null && regexPattern.length() > 0) {
+            result = fileName.matches(regexPattern);
+        }
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Matching file: " + fileName + " is " + result);
+        }
+        return result;
+    }
+
+    protected String remoteServer() {
+        return endpoint.getConfiguration().remoteServerInformation();
+    }
+
+    public boolean isRecursive() {
+        return recursive;
+    }
+
+    public void setRecursive(boolean recursive) {
+        this.recursive = recursive;
+    }
+
+    public long getLastPollTime() {
+        return lastPollTime;
+    }
+
+    public void setLastPollTime(long lastPollTime) {
+        this.lastPollTime = lastPollTime;
+    }
+
+    public String getRegexPattern() {
+        return regexPattern;
+    }
+
+    public void setRegexPattern(String regexPattern) {
+        this.regexPattern = regexPattern;
+    }
+
+    public boolean isSetNames() {
+        return setNames;
+    }
+
+    public void setSetNames(boolean setNames) {
+        this.setNames = setNames;
+    }
+
+    public boolean isExclusiveRead() {
+        return exclusiveRead;
+    }
+
+    public void setExclusiveRead(boolean exclusiveRead) {
+        this.exclusiveRead = exclusiveRead;
+    }
+
+    public boolean isDeleteFile() {
+        return deleteFile;
+    }
+
+    public void setDeleteFile(boolean deleteFile) {
+        this.deleteFile = deleteFile;
+    }
+
 }
--- components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/RemoteFileProducer.java
+++ components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/RemoteFileProducer.java
@@ -19,11 +19,16 @@ package org.apache.camel.component.file.remote;
 import org.apache.camel.Message;
 import org.apache.camel.component.file.FileComponent;
 import org.apache.camel.impl.DefaultProducer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public abstract class RemoteFileProducer<T extends RemoteFileExchange> extends DefaultProducer<T> {
+    protected final transient Log LOG = LogFactory.getLog(getClass());
+    protected RemoteFileEndpoint<T> endpoint;
 
     protected RemoteFileProducer(RemoteFileEndpoint<T> endpoint) {
         super(endpoint);
+        this.endpoint = endpoint;
     }
 
     protected String createFileName(Message message, RemoteFileConfiguration fileConfig) {
@@ -42,6 +47,32 @@ public abstract class RemoteFileProducer<T extends RemoteFileExchange> extends D
             answer = endpointFileName;
         }
         return answer;
+    }
+
+    protected String remoteServer() {
+        return endpoint.getConfiguration().remoteServerInformation();
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        LOG.info("Starting");
+        // do not connect when componet starts, just wait until we process as we will
+        // connect at that time if needed
+        super.doStart();
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        LOG.info("Stopping");
+        // disconnect when stopping
+        try {
+            disconnect();
+        } catch (Exception e) {
+            // ignore just log a warning
+            LOG.warn("Exception occured during disconecting from " + remoteServer() + ". "
+                     + e.getClass().getCanonicalName() + " message: " + e.getMessage());
+        }
+        super.doStop();
     }
 
     protected abstract void connectIfNecessary() throws Exception;
