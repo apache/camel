c397752238be481ad0744d02a8766443a138fca6
Claus Ibsen <davsclaus@apache.org>
2008-07-30 15:51:43 +0000

    CAMEL-771: A little better feedback on route mis configuration
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@681093 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/model/ProcessorTypeConfigurationTest.java
@@ -0,0 +1,41 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.model;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.RuntimeCamelException;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Unit test to verify end-user exceptions for miss configuration
+ */
+public class ProcessorTypeConfigurationTest extends ContextTestSupport {
+
+    public void testProcessorRefMissConfigured() throws Exception {
+        try {
+            context.addRoutes(new RouteBuilder() {
+                public void configure() throws Exception {
+                    from("direct:in").processRef("hello");
+                }
+            });
+            fail("Should have thrown RuntimeCamelException");
+        } catch (IllegalArgumentException e) {
+            assertEquals("target not provided on node: Processor[ref:  hello]", e.getMessage());
+        }
+    }
+
+}
