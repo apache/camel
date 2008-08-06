b1f234b493513651e263e016277e45162d0b5d96
Claus Ibsen <davsclaus@apache.org>
2008-08-06 04:25:01 +0000

    CAMEL-777: Added wiki example for camel-mail
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@683103 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-mail/src/test/java/org/apache/camel/component/mail/MailUsingHeadersTest.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.mail;
+
+import java.util.Map;
+import java.util.HashMap;
+import javax.mail.Message;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.jvnet.mock_javamail.Mailbox;
+
+/**
+ * Unit test for Mail using camel headers to set recipeient subject.
+ */
+public class MailUsingHeadersTest extends ContextTestSupport {
+
+    public void testMailUsingHeaders() throws Exception {
+        Mailbox.clearAll();
+
+        // START SNIPPET: e1
+        Map map = new HashMap();
+        map.put("To", "davsclaus@apache.org");
+        map.put("From", "jstrachan@apache.org");
+        map.put("Subject", "Camel rocks");
+
+        String body = "Hello Claus.\nYes it does.\n\nRegards James.";
+        template.sendBodyAndHeaders("smtp://james@localhost", body, map);
+        // END SNIPPET: e1
+
+        Mailbox box = Mailbox.get("davsclaus@apache.org");
+        Message msg = box.get(0);
+        assertEquals("davsclaus@apache.org", msg.getRecipients(Message.RecipientType.TO)[0].toString());
+        assertEquals("jstrachan@apache.org", msg.getFrom()[0].toString());
+        assertEquals("Camel rocks", msg.getSubject());
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                // no routes
+            }
+        };
+    }
+}
