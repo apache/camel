378e4a325103accc774f12f9b67d2d19e4bbda96
Claus Ibsen <davsclaus@apache.org>
2008-08-10 11:01:36 +0000

    CAMEL-805: hl7 MSH fields is added as camel message headers for much easier routing etc. Added route unit test with wiki sample.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@684455 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-hl7/src/test/java/org/apache/camel/component/hl7/HL7RouteTest.java
@@ -0,0 +1,196 @@
+package org.apache.camel.component.hl7;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.dataformat.hl7.HL7DataFormat;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.spi.DataFormat;
+
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.v24.message.ADR_A19;
+import ca.uhn.hl7v2.model.v24.message.ADT_A01;
+import ca.uhn.hl7v2.model.v24.message.QRY_A19;
+import ca.uhn.hl7v2.model.v24.segment.MSA;
+import ca.uhn.hl7v2.model.v24.segment.MSH;
+import ca.uhn.hl7v2.model.v24.segment.PID;
+import ca.uhn.hl7v2.model.v24.segment.QRD;
+
+/**
+ * Unit test for HL7 routing.
+ */
+public class HL7RouteTest extends ContextTestSupport {
+
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry jndi = super.createRegistry();
+
+        HL7MLLPCodec codec = new HL7MLLPCodec();
+        codec.setCharset("iso-8859-1");
+
+        jndi.bind("hl7codec", codec);
+
+        MyHL7BusinessLogic logic = new MyHL7BusinessLogic();
+        jndi.bind("hl7service", logic);
+
+        return jndi;
+    }
+
+    public void testSendA19() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:a19");
+        mock.expectedMessageCount(1);
+        mock.message(0).body().isInstanceOf(Message.class);
+
+        String line1 = "MSH|^~\\&|MYSENDER|MYSENDERAPP|MYCLIENT|MYCLIENTAPP|200612211200||QRY^A19|1234|P|2.4";
+        String line2 = "QRD|200612211200|R|I|GetPatient|||1^RD|0101701234|DEM||";
+
+        StringBuffer in = new StringBuffer();
+        in.append(line1);
+        in.append("\n");
+        in.append(line2);
+
+        String out = (String) template.requestBody("mina:tcp://localhost:8888?sync=true&codec=hl7codec", in.toString());
+
+        String[] lines = out.split("\r");
+        assertEquals("MSH|^~\\&|MYSENDER||||200701011539||ADR^A19||||123", lines[0]);
+        assertEquals("MSA|AA|123", lines[1]);
+
+        assertMockEndpointsSatisifed();
+    }
+
+    public void testSendA01() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:a01");
+        mock.expectedMessageCount(1);
+        mock.message(0).body().isInstanceOf(Message.class);
+
+        String line1 = "MSH|^~\\&|MYSENDER|MYSENDERAPP|MYCLIENT|MYCLIENTAPP|200612211200||ADT^A01|1234|P|2.4";
+        String line2 = "PID|||123456||Doe^John";
+
+        StringBuffer in = new StringBuffer();
+        in.append(line1);
+        in.append("\n");
+        in.append(line2);
+
+        String out = (String) template.requestBody("mina:tcp://localhost:8888?sync=true&codec=hl7codec", in.toString());
+        String[] lines = out.split("\r");
+        assertEquals("MSH|^~\\&|MYSENDER||||200701011539||ADT^A01||||123", lines[0]);
+        assertEquals("PID|||123456||Doe^John", lines[1]);
+
+        assertMockEndpointsSatisifed();
+    }
+
+    public void testSendUnknown() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:unknown");
+        mock.expectedMessageCount(1);
+        mock.message(0).body().isInstanceOf(Message.class);
+
+        String line1 = "MSH|^~\\&|MYSENDER|MYSENDERAPP|MYCLIENT|MYCLIENTAPP|200612211200||ADT^A02|1234|P|2.4";
+        String line2 = "PID|||123456||Doe^John";
+
+        StringBuffer in = new StringBuffer();
+        in.append(line1);
+        in.append("\n");
+        in.append(line2);
+
+        template.requestBody("mina:tcp://localhost:8888?sync=true&codec=hl7codec", in.toString());
+
+        assertMockEndpointsSatisifed();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                // START SNIPPET: e1
+                DataFormat hl7 = new HL7DataFormat();
+                // we setup or HL7 listener on port 8888 (using the hl7codec) and in sync mode so we can return a response
+                from("mina:tcp://localhost:8888?sync=true&codec=hl7codec")
+                    // we use the HL7 data format to unmarshal from HL7 stream to the HAPI Message model
+                    // this ensures that the camel message has been enriched with hl7 specific headers to
+                    // make the routing much easier (see below)
+                    .unmarshal(hl7)
+                    // using choice as the content base router
+                    .choice()
+                        // where we choose that A19 queries so go to mock:a19
+                        .when(header("hl7.msh.triggerEvent").isEqualTo("A19"))
+                            .beanRef("hl7service", "handleA19")
+                            .to("mock:a19")
+                        // and A01 should go to mock:a01
+                        .when(header("hl7.msh.triggerEvent").isEqualTo("A01")).to("mock:a01")
+                            .beanRef("hl7service", "handleA01")
+                            .to("mock:a19")
+                        // other types should go to unknown
+                        .otherwise()
+                            .to("mock:unknown")
+                    // end choice block
+                    .end()
+                    // marhsal response back
+                    .marshal(hl7);
+                // END SNIPPET: e1
+            }
+        };
+    }
+
+    public static class MyHL7BusinessLogic {
+
+        public Message handleA19(Message msg) throws Exception {
+            // here you can have your business logic for A19 messages
+            assertTrue(msg instanceof QRY_A19);
+            // just return the same dummy response
+            return createADR19Message();
+        }
+
+        public Message handleA01(Message msg) throws Exception {
+            // here you can have your business logic for A01 messages
+            assertTrue(msg instanceof ADT_A01);
+            // just return the same dummy response
+            return createADT01Message();
+        }
+    }
+
+    private static Message createADR19Message() throws Exception {
+        ADR_A19 adr = new ADR_A19();
+
+        // Populate the MSH Segment
+        MSH mshSegment = adr.getMSH();
+        mshSegment.getFieldSeparator().setValue("|");
+        mshSegment.getEncodingCharacters().setValue("^~\\&");
+        mshSegment.getDateTimeOfMessage().getTimeOfAnEvent().setValue("200701011539");
+        mshSegment.getSendingApplication().getNamespaceID().setValue("MYSENDER");
+        mshSegment.getSequenceNumber().setValue("123");
+        mshSegment.getMessageType().getMessageType().setValue("ADR");
+        mshSegment.getMessageType().getTriggerEvent().setValue("A19");
+
+        // Populate the PID Segment
+        MSA msa = adr.getMSA();
+        msa.getAcknowledgementCode().setValue("AA");
+        msa.getMessageControlID().setValue("123");
+
+        QRD qrd = adr.getQRD();
+        qrd.getQueryDateTime().getTimeOfAnEvent().setValue("20080805120000");
+
+        return adr.getMessage();
+    }
+
+    private static Message createADT01Message() throws Exception {
+        ADT_A01 adt = new ADT_A01();
+
+        // Populate the MSH Segment
+        MSH mshSegment = adt.getMSH();
+        mshSegment.getFieldSeparator().setValue("|");
+        mshSegment.getEncodingCharacters().setValue("^~\\&");
+        mshSegment.getDateTimeOfMessage().getTimeOfAnEvent().setValue("200701011539");
+        mshSegment.getSendingApplication().getNamespaceID().setValue("MYSENDER");
+        mshSegment.getSequenceNumber().setValue("123");
+        mshSegment.getMessageType().getMessageType().setValue("ADT");
+        mshSegment.getMessageType().getTriggerEvent().setValue("A01");
+
+        // Populate the PID Segment
+        PID pid = adt.getPID();
+        pid.getPatientName(0).getFamilyName().getSurname().setValue("Doe");
+        pid.getPatientName(0).getGivenName().setValue("John");
+        pid.getPatientIdentifierList(0).getID().setValue("123456");
+
+        return adt.getMessage();
+    }
+
+}
