2e33cb4cd07be99e03fdfd88555f09521b628f50
James Strachan <jstrachan@apache.org>
2008-08-12 17:20:09 +0000

    added an initial implementation of CAMEL-816 to support @Produce and @Consume
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@685233 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring/src/test/java/org/apache/camel/spring/produce/ProduceTest.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.produce;
+
+import java.util.List;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Produce;
+import org.apache.camel.spring.remoting.IAsyncService;
+import org.apache.camel.spring.remoting.SpringRemotingWithOneWayTest;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit38.AbstractJUnit38SpringContextTests;
+
+/**
+ * @version $Revision: 685051 $
+ */
+@ContextConfiguration
+public class ProduceTest extends AbstractJUnit38SpringContextTests {
+    private static final Log LOG = LogFactory.getLog(ProduceTest.class);
+
+    @Produce(uri = "direct:myService")
+    protected MyListener producer;
+
+    public void testInvokeService() throws Exception {
+        // lets send a message
+        String actual = producer.sayHello("James");
+        assertEquals("response", "Hello James", actual);
+
+    }
+}
