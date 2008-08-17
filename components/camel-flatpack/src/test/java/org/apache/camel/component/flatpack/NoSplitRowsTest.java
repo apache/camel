a250644ea94413efffd2134d04e3a0b662a5d3c7
Claus Ibsen <davsclaus@apache.org>
2008-08-17 15:11:16 +0000

    CAMEL-830: Added option splitRows to camel-flatpack
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@686616 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-flatpack/src/test/java/org/apache/camel/component/flatpack/NoSplitRowsTest.java
+++ components/camel-flatpack/src/test/java/org/apache/camel/component/flatpack/NoSplitRowsTest.java
@@ -20,43 +20,53 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.util.ObjectHelper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit38.AbstractJUnit38SpringContextTests;
 
 /**
+ * Unit test to verify that splitRows=false option.
+ *
  * @version $Revision$
  */
 @ContextConfiguration
-public class FixedLengthTest extends AbstractJUnit38SpringContextTests {
-    private static final transient Log LOG = LogFactory.getLog(FixedLengthTest.class);
+public class NoSplitRowsTest extends AbstractJUnit38SpringContextTests {
+    
+    private static final transient Log LOG = LogFactory.getLog(NoSplitRowsTest.class);
 
     @EndpointInject(uri = "mock:results")
     protected MockEndpoint results;
 
     protected String[] expectedFirstName = {"JOHN", "JIMMY", "JANE", "FRED"};
 
-    public void testCamel() throws Exception {
-        results.expectedMessageCount(4);
+    public void testHeaderAndTrailer() throws Exception {
+        results.expectedMessageCount(1);
+        results.message(0).body().isInstanceOf(List.class);
+        results.message(0).header("camelFlatpackCounter").isEqualTo(6);
+
         results.assertIsSatisfied();
 
+        List<Map> data = (List) results.getExchanges().get(0).getIn().getBody();
+
+        // assert header
+        Map header = data.get(0);
+        assertEquals("HBT", header.get("INDICATOR"));
+        assertEquals("20080817", header.get("DATE"));
+
+        // assert body
         int counter = 0;
-        List<Exchange> list = results.getReceivedExchanges();
-        for (Exchange exchange : list) {
-            Message in = exchange.getIn();
-            assertEquals("counter", in.getHeader("camelFlatpackCounter"), counter);
-            Map body = in.getBody(Map.class);
-            assertNotNull("Should have found body as a Map but was: " + ObjectHelper.className(in.getBody()), body);
-            assertEquals("FIRSTNAME", expectedFirstName[counter], body.get("FIRSTNAME"));
-            LOG.info("Result: " + counter + " = " + body);
+        for (Map row : data.subList(1, 5)) {
+            assertEquals("FIRSTNAME", expectedFirstName[counter], row.get("FIRSTNAME"));
+            LOG.info("Result: " + counter + " = " + row);
             counter++;
         }
 
+        // assert trailer
+        Map trailer = data.get(5);
+        assertEquals("FBT", trailer.get("INDICATOR"));
+        assertEquals("SUCCESS", trailer.get("STATUS"));
     }
 
 }
