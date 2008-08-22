2a59458d3d4e7279e81db7b40c6ca85cb8a55ccf
Claus Ibsen <davsclaus@apache.org>
2008-08-22 06:16:41 +0000

    CAMEL-621: Added support for using Date objects from either in or out message.
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@687962 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/language/simple/SimpleLanguage.java
+++ camel-core/src/main/java/org/apache/camel/language/simple/SimpleLanguage.java
@@ -28,10 +28,10 @@ import org.apache.camel.util.ObjectHelper;
  * Examples of supported expressions are:
  * <ul>
  * <li>id to access the inbound message Id</li>
- * <li>in.header.foo or header.foo to access an inbound header called 'foo'</li>
  * <li>in.body or body to access the inbound body</li>
- * <li>out.header.foo to access an outbound header called 'foo'</li>
  * <li>out.body to access the inbound body</li>
+ * <li>in.header.foo or header.foo to access an inbound header called 'foo'</li>
+ * <li>out.header.foo to access an outbound header called 'foo'</li>
  * <li>property.foo to access the exchange property called 'foo'</li>
  * <li>sys.foo to access the system property called 'foo'</li>
  * </ul>
@@ -87,7 +87,7 @@ public class SimpleLanguage extends AbstractSimpleLanguage {
         // system property
         remainder = ifStartsWithReturnRemainder("sys.", expression);
         if (remainder != null) {
-            return ExpressionBuilder.propertyExpression(remainder);
+            return ExpressionBuilder.systemProperty(remainder);
         }
 
         throw new IllegalSyntaxException(this, expression);
