8bbac291c9924c20e4f06ba43d96d446e78c0bbf
Hadrian Zbarcea <hadrian@apache.org>
2008-08-27 00:38:59 +0000

    CAMEL-853. Fix package name.
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@689303 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-ldap/src/test/java/org/apache/camel/component/ldap/LdapTestSupport.java
+++ components/camel-ldap/src/test/java/org/apache/camel/component/ldap/LdapTestSupport.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.ldap;
+package org.apache.camel.component.ldap;
 
 import java.util.Collection;
 import java.util.HashSet;
