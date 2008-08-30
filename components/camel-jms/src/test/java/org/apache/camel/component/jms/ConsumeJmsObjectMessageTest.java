51df0481a902efef687df94adf18e433654e7ac2
Claus Ibsen <davsclaus@apache.org>
2008-08-30 13:38:00 +0000

    Polished javadoc. Added unit test for sending object with jms
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@690502 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/ConsumeJmsObjectMessageTest.java
@@ -0,0 +1,115 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jms;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import static org.apache.camel.component.jms.JmsComponent.jmsComponentClientAcknowledge;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.*;
+import java.io.Serializable;
+
+/**
+ * @version $Revision$
+ */
+public class ConsumeJmsObjectMessageTest extends ContextTestSupport {
+    protected JmsTemplate jmsTemplate;
+    private MockEndpoint endpoint;
+
+    public void testConsumeObjectMessage() throws Exception {
+        endpoint.expectedMessageCount(1);
+
+        jmsTemplate.setPubSubDomain(false);
+        jmsTemplate.send("test.object", new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                ObjectMessage msg = session.createObjectMessage();
+
+                MyUser user = new MyUser();
+                user.setName("Claus");
+                msg.setObject(user);
+
+                return msg;
+            }
+        });
+
+        endpoint.assertIsSatisfied();
+        assertCorrectObjectReceived();
+    }
+
+    public void testSendBytesMessage() throws Exception {
+        endpoint.expectedMessageCount(1);
+
+        MyUser user = new MyUser();
+        user.setName("Claus");
+        template.sendBody("direct:test", user);
+
+        endpoint.assertIsSatisfied();
+        assertCorrectObjectReceived();
+    }
+
+    protected void assertCorrectObjectReceived() {
+        Exchange exchange = endpoint.getReceivedExchanges().get(0);
+        JmsExchange jmsExchange = assertIsInstanceOf(JmsExchange.class, exchange);
+        assertIsInstanceOf(ObjectMessage.class, jmsExchange.getInMessage());
+
+        MyUser user = exchange.getIn().getBody(MyUser.class);
+        assertEquals("Claus", user.getName());
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        endpoint = getMockEndpoint("mock:result");
+    }
+
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext camelContext = super.createCamelContext();
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        jmsTemplate = new JmsTemplate(connectionFactory);
+        camelContext.addComponent("activemq", jmsComponentClientAcknowledge(connectionFactory));
+
+        return camelContext;
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                from("activemq:test.object").to("mock:result");
+                from("direct:test").to("activemq:test.object");
+            }
+        };
+    }
+
+    public static class MyUser implements Serializable {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
