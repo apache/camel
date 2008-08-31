921c64b79ea2eccaa96dc05e3caf0d6f59ad30b1
Claus Ibsen <davsclaus@apache.org>
2008-08-31 13:02:22 +0000

    CAMEL-831, CAMEL-616: Added flatpack writer to camel-flatpack for the dataformat. Improved the dataformat with the marshal operation as well.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@690690 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-flatpack/src/test/java/org/apache/camel/component/flatpack/FlatpackFixedLengthDataFormatTest.java
@@ -0,0 +1,91 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.flatpack;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.converter.IOConverter;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Unit test for fixed length DataFormat.
+ */
+public class FlatpackFixedLengthDataFormatTest extends ContextTestSupport {
+
+    public void testUnmarshal() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:unmarshal");
+        // by default we get on big message
+        mock.expectedMessageCount(1);
+        mock.message(0).body().isInstanceOf(DataSetList.class);
+
+        String data = IOConverter.toString(new File("src/test/data/fixed/PEOPLE-FixedLength.txt").getAbsoluteFile());
+
+        template.sendBody("direct:unmarshal", data);
+        assertMockEndpointsSatisifed();
+
+        DataSetList list = mock.getExchanges().get(0).getIn().getBody(DataSetList.class);
+        assertEquals(4, list.size());
+        Map row = (Map) list.get(0);
+        assertEquals("JOHN", row.get("FIRSTNAME"));
+    }
+
+    public void testMarshalWithDefinition() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:marshal");
+        // by default we get on big message
+        mock.expectedMessageCount(1);
+
+        List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+        Map<String, Object> row = new LinkedHashMap<String, Object>();
+        row.put("FIRSTNAME", "JOHN");
+        row.put("LASTNAME", "DOE");
+        row.put("ADDRESS", "1234 CIRCLE CT");
+        row.put("CITY", "ELYRIA");
+        row.put("STATE", "OH");
+        row.put("ZIP", "44035");
+        data.add(row);
+
+        template.sendBody("direct:marshal", data);
+        assertMockEndpointsSatisifed();
+
+        String s = mock.getExchanges().get(0).getIn().getBody(String.class);
+        assertTrue(s.startsWith("JOHN                               DOE"));
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                FlatpackDataFormat df = new FlatpackDataFormat();
+                df.setDefinition(new ClassPathResource("PEOPLE-FixedLength.pzmap.xml"));
+                df.setFixed(true);
+                df.setIgnoreFirstRecord(false);
+
+                from("direct:unmarshal").unmarshal(df).to("mock:unmarshal");
+
+                // with the definition
+                from("direct:marshal").marshal(df).convertBodyTo(String.class).to("mock:marshal");
+            }
+        };
+    }
+}
