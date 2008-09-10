6b37dc3575789f44fc25c6997d0b19c95601c144
James Strachan <jstrachan@apache.org>
2008-09-10 14:22:09 +0000

    added a first spike of camel-rest to make it easy to expose a CamelContext as a number of RESTful resources
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@693850 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-rest/src/main/java/org/apache/camel/rest/model/Endpoints.java
@@ -0,0 +1,65 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.rest.model;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Endpoints {
+    @XmlElement(name="endpoint")
+    private List<EndpointLink> endpoints = new ArrayList<EndpointLink>();
+
+    public List<EndpointLink> getEndpoints() {
+        return endpoints;
+    }
+
+    public void setEndpoints(List<EndpointLink> endpoints) {
+        this.endpoints = endpoints;
+    }
+
+    public void load(CamelContext camelContext) {
+        Collection<Endpoint> endpoints = camelContext.getSingletonEndpoints();
+        for (Endpoint endpoint : endpoints) {
+            addEndpoint(createEndpointLink(endpoint));
+        }
+
+    }
+
+    protected EndpointLink createEndpointLink(Endpoint endpoint) {
+        EndpointLink answer = new EndpointLink();
+        answer.load(endpoint);
+        return answer;
+    }
+
+    public void addEndpoint(EndpointLink endpointLink) {
+        getEndpoints().add(endpointLink);
+    }
+}
