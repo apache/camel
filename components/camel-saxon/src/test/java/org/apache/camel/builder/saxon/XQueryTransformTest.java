d18f0db1a6b1b8339dec5e94ea8d5806cf4cad09
Jonathan Anstey <janstey@apache.org>
2008-09-11 17:31:00 +0000

    Added test for extracting a piece of an XML message and setting it as the out message.
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@694371 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-saxon/src/test/java/org/apache/camel/builder/saxon/XQueryTransformTest.java
@@ -0,0 +1,62 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.builder.saxon;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision: 641680 $
+ */
+public class XQueryTransformTest extends ContextTestSupport {
+
+    protected Endpoint<Exchange> startEndpoint;
+    protected MockEndpoint resultEndpoint;
+
+    public void testSendMatchingMessage() throws Exception {
+        // saxon converts all quotes to "
+        resultEndpoint.expectedBodiesReceived("<person name=\"Jonathan\"/>");
+
+        template.sendBody("direct:start", "<people><person name='Jonathan'/></people>");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        startEndpoint = resolveMandatoryEndpoint("direct:start");
+        resultEndpoint = getMockEndpoint("mock:result");
+    }
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                // START SNIPPET: example
+                from("direct:start").
+                  transform().xquery("/people/person[@name='Jonathan']").
+                  to("mock:result");
+                // END SNIPPET: example
+            }
+        };
+    }
+}
