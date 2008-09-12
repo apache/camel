51a7b39b6ed228d39383c3421ac2f4ff9aac0d6a
Willem Ning Jiang <ningjiang@apache.org>
2008-09-12 11:10:15 +0000

    Fixed the CS errors
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@694680 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-ldap/src/main/java/org/apache/camel/component/ldap/LdapProducer.java
+++ components/camel-ldap/src/main/java/org/apache/camel/component/ldap/LdapProducer.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -42,32 +41,32 @@ public class LdapProducer<E extends Exchange> extends DefaultProducer<DefaultExc
 
     public LdapProducer(LdapEndpoint endpoint, String remaining, String base, int scope) throws Exception {
         super(endpoint);
-        
+
         ldapContext = (DirContext)getEndpoint().getCamelContext().getRegistry().lookup(remaining);
         searchBase = base;
         controls = new SearchControls();
         controls.setSearchScope(scope);
     }
 
-	public void process(Exchange exchange) throws Exception {
+    public void process(Exchange exchange) throws Exception {
         String filter = exchange.getIn().getBody(String.class);
 
         // could throw NamingException
         List<SearchResult> data = new ArrayList<SearchResult>();
-        NamingEnumeration<SearchResult> namingEnumeration = 
+        NamingEnumeration<SearchResult> namingEnumeration =
             ldapContext.search(searchBase, filter, getControls());
-        
-        while(namingEnumeration.hasMore()) {
+
+        while (namingEnumeration.hasMore()) {
             data.add(namingEnumeration.next());
         }
         exchange.getOut().setBody(data);
-	}
+    }
 
     public DirContext getDirContext() {
-    	return ldapContext;
+        return ldapContext;
     }
-    
+
     protected SearchControls getControls() {
-    	return controls;
+        return controls;
     }
 }
