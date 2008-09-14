b1d696b2faf554d2ef75a1a149ae64e6a5b9515b
Willem Ning Jiang <ningjiang@apache.org>
2008-09-14 03:34:08 +0000

    Fixed the CS errors of camel-jetty
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@695106 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jetty/src/test/java/org/apache/camel/component/jetty/JettyHttpGetWithParamAsExchangeHeaderTest.java
+++ components/camel-jetty/src/test/java/org/apache/camel/component/jetty/JettyHttpGetWithParamAsExchangeHeaderTest.java
@@ -38,7 +38,7 @@ public class JettyHttpGetWithParamAsExchangeHeaderTest extends ContextTestSuppor
 
         template.sendBody(serverUri + "?one=einz&two=twei", "Hello World");
 
-        assertMockEndpointsSatisifed();
+        assertMockEndpointsSatisfied();
     }
 
     public void testHttpGetWithParamsViaHeader() throws Exception {
@@ -50,7 +50,7 @@ public class JettyHttpGetWithParamAsExchangeHeaderTest extends ContextTestSuppor
 
         template.sendBodyAndHeader(serverUri, "Hello World", HttpProducer.QUERY, "one=uno&two=dos");
 
-        assertMockEndpointsSatisifed();
+        assertMockEndpointsSatisfied();
     }
 
     protected RouteBuilder createRouteBuilder() throws Exception {
