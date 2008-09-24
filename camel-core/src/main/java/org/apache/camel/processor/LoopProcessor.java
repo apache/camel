1919118993f9af91dcc4c50c98c2194023307e67
Hadrian Zbarcea <hadrian@apache.org>
2008-09-24 18:28:04 +0000

    CAMEL-325.  Add test for invalid expression and loop count/index properties.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@698682 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/LoopProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/LoopProcessor.java
@@ -19,6 +19,7 @@ package org.apache.camel.processor;
 import org.apache.camel.Exchange;
 import org.apache.camel.Expression;
 import org.apache.camel.Processor;
+import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.util.ExchangeHelper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,6 +30,9 @@ import org.apache.commons.logging.LogFactory;
  * @version $Revision: $
  */
 public class LoopProcessor extends DelegateProcessor {
+    public static final String PROP_ITER_COUNT = "CamelIterationCount";
+    public static final String PROP_ITER_INDEX = "CamelIterationIndex";
+    
     private static final Log LOG = LogFactory.getLog(LoopProcessor.class);
     
     private Expression<Exchange> expression;
@@ -44,9 +48,15 @@ public class LoopProcessor extends DelegateProcessor {
         // but evaluation result is a textual representation of a numeric value.
         String text = ExchangeHelper.convertToType(exchange, String.class, expression.evaluate(exchange));
         Integer value = ExchangeHelper.convertToType(exchange, Integer.class, text);
-        int count = value != null ? value.intValue() : 0;
+        if (value == null) {
+        	// TODO: we should probably catch evaluate/convert exception an set is as fault (after fix for CAMEL-316)
+        	throw new RuntimeCamelException("Expression \"" + expression + "\" does not evaluate to an int.");
+        }
+        int count = value.intValue();
+        exchange.setProperty(PROP_ITER_COUNT, count);
         for (int i = 0; i < count; i++) {
             LOG.debug("LoopProcessor: iteration #" + i);
+            exchange.setProperty(PROP_ITER_INDEX, i);
             super.process(exchange);
         }
     }
