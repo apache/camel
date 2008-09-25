5c8c8c0f59f118c924dbb32f4ba92ac91f3c7e84
Willem Ning Jiang <ningjiang@apache.org>
2008-09-25 09:36:20 +0000

    CAMEL-807 use fault message to store the response when the response code != 200
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@698897 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-http/src/test/java/org/apache/camel/component/http/HttpPostWithBodyTest.java
+++ components/camel-http/src/test/java/org/apache/camel/component/http/HttpPostWithBodyTest.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 
@@ -32,35 +33,42 @@ import static org.apache.camel.component.http.HttpMethods.POST;
 public class HttpPostWithBodyTest extends ContextTestSupport {
     protected String expectedText = "<html";
 
-    public void testHttpGet() throws Exception {
-        MockEndpoint mockEndpoint = resolveMandatoryEndpoint("mock:results", MockEndpoint.class);
-        mockEndpoint.expectedMinimumMessageCount(1);
-        sendBody("direct:start", "q=test1234");
-        mockEndpoint.assertIsSatisfied();
-        List<Exchange> list = mockEndpoint.getReceivedExchanges();
-        Exchange exchange = list.get(0);
+    public void testHttpPostWithError() throws Exception {
+
+        Exchange exchange = template.send("direct:start", new Processor() {
+
+            public void process(Exchange exchange) throws Exception {
+                exchange.getIn().setBody("q=test1234");
+            }
+
+        });
+
         assertNotNull("exchange", exchange);
+        assertTrue("The exchange should be failed", exchange.isFailed());
 
-        Message in = exchange.getIn();
-        assertNotNull("in", in);
+        // get the fault message
+        Message fault = exchange.getFault();
+        assertNotNull("fault", fault);
 
-        Map<String, Object> headers = in.getHeaders();
-
+        Map<String, Object> headers = fault.getHeaders();
         log.debug("Headers: " + headers);
         assertTrue("Should be more than one header but was: " + headers, headers.size() > 0);
 
-        String body = in.getBody(String.class);
+        int responseCode = fault.getHeader(HttpProducer.HTTP_RESPONSE_CODE, Integer.class);
+        assertTrue("The response code should not be 200", responseCode != 200);
+
+        String body = fault.getBody(String.class);
         log.debug("Body: " + body);
         assertNotNull("Should have a body!", body);
         assertTrue("body should contain: " + expectedText, body.contains(expectedText));
+
     }
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             public void configure() {
-                from("direct:start").setHeader(HTTP_METHOD, POST).to("http://www.google.com")
-                    .to("mock:results");
+                from("direct:start").setHeader(HTTP_METHOD, POST).to("http://www.google.com");
             }
         };
     }
