a20b902d3a08ec5effd56ee1d7c63973b5fd9415
James Strachan <jstrachan@apache.org>
2008-10-21 15:11:30 +0000

    added better tostring...
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@706657 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-example-guice-jms/src/main/java/org/apache/camel/example/guice/jms/SomeBean.java
+++ examples/camel-example-guice-jms/src/main/java/org/apache/camel/example/guice/jms/SomeBean.java
@@ -25,4 +25,9 @@ public class SomeBean {
     public void someMethod(String body) {
         System.out.println("Received: " + body);
     }
+
+    @Override
+    public String toString() {
+        return "SomeBean";
+    }
 }
