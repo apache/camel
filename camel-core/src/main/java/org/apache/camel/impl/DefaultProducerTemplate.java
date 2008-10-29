808d6d6a3b44505cacad999f868df0605c6781e1
James Strachan <jstrachan@apache.org>
2008-10-29 11:23:27 +0000

    added a fix for CAMEL-1033 making it easy to create a JMS MessageListener for processing by Camel - along with helper methods to make a Processor from a ProducerTemplate
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@708879 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/impl/DefaultProducerTemplate.java
+++ camel-core/src/main/java/org/apache/camel/impl/DefaultProducerTemplate.java
@@ -30,6 +30,7 @@ import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.util.ObjectHelper;
+import org.apache.camel.util.CamelContextHelper;
 import static org.apache.camel.util.ObjectHelper.wrapRuntimeCamelException;
 
 /**
@@ -45,6 +46,11 @@ public class DefaultProducerTemplate<E extends Exchange> extends ServiceSupport 
     private boolean useEndpointCache = true;
     private final Map<String, Endpoint<E>> endpointCache = new HashMap<String, Endpoint<E>>();
     private Endpoint<E> defaultEndpoint;
+
+    public static DefaultProducerTemplate newInstance(CamelContext camelContext, String defaultEndpointUri) {
+        Endpoint endpoint = CamelContextHelper.getMandatoryEndpoint(camelContext, defaultEndpointUri);
+        return new DefaultProducerTemplate(camelContext, endpoint);
+    }
 
     public DefaultProducerTemplate(CamelContext context) {
         this.context = context;
