3842f553fa20ab556c0f5c61bf76511d3c896c1c
Willem Ning Jiang <ningjiang@apache.org>
2008-11-04 11:36:43 +0000

    Merged revisions 711206 via svnmerge from https://svn.apache.org/repos/asf/activemq/camel/trunk
    ........
      r711206 | ningjiang | 2008-11-04 16:09:44 +0800 (Tue, 04 Nov 2008) | 1 line
      
      CAMEL-1051 Get HandleFaultProcessor to work with AsyncProcessor
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/branches/camel-1.x@711229 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/HandleFaultProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/HandleFaultProcessor.java
@@ -16,15 +16,60 @@
  */
 package org.apache.camel.processor;
 
+import org.apache.camel.AsyncCallback;
+import org.apache.camel.AsyncProcessor;
 import org.apache.camel.CamelException;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.util.AsyncProcessorHelper;
 
-public class HandleFaultProcessor extends DelegateProcessor {
+public class HandleFaultProcessor extends DelegateProcessor implements AsyncProcessor {
+    
+    @Override
+    public String toString() {
+        return "HandleFaultProcessor(" + processor + ")";
+    }
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        super.process(exchange);
+        AsyncProcessorHelper.process(this, exchange);
+    }
+
+    public boolean process(final Exchange exchange, final AsyncCallback callback) {
+        if (processor == null) {
+            // no processor so nothing to process, so return
+            callback.done(true);
+            return true;
+        }      
+
+        if (processor instanceof AsyncProcessor) {
+            return ((AsyncProcessor)processor).process(exchange, new AsyncCallback() {
+                
+                public void done(boolean doneSynchronously) {
+                    callback.done(doneSynchronously);
+                    Message faultMessage = exchange.getFault(false);
+                    if (faultMessage != null) {
+                        final Object faultBody = faultMessage.getBody();
+                        if (faultBody != null) {
+                            faultMessage.setBody(null); // Reset it since we are handling it.
+                            if (faultBody instanceof Throwable) {
+                                exchange.setException((Throwable)faultBody);
+                            } else {
+                                exchange.setException(new CamelException("Message contains fault of type "
+                                    + faultBody.getClass().getName() + ":\n" + faultBody));
+                            }
+                        }
+                    }                    
+                }                
+            });
+        }
+        
+        try {
+            processor.process(exchange);
+        } catch (Throwable e) {
+            exchange.setException(e);
+        }
+        
         final Message faultMessage = exchange.getFault(false);
         if (faultMessage != null) {
             final Object faultBody = faultMessage.getBody();
@@ -38,5 +83,7 @@ public class HandleFaultProcessor extends DelegateProcessor {
                 }
             }
         }
+        callback.done(true);
+        return true;
     }
 }
