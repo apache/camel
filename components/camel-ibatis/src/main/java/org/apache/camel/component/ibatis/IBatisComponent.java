bd7297ff3ad5277eceb89776650c36782567a80b
Hadrian Zbarcea <hadrian@apache.org>
2008-11-06 01:21:28 +0000

    CAMEL-630.  Patch applied with many thanks to Clayton.
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@711750 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-ibatis/src/main/java/org/apache/camel/component/ibatis/IBatisComponent.java
+++ components/camel-ibatis/src/main/java/org/apache/camel/component/ibatis/IBatisComponent.java
@@ -18,11 +18,13 @@ package org.apache.camel.component.ibatis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Map;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import org.apache.camel.Endpoint;
+import org.apache.camel.component.ResourceBasedComponent;
 import org.apache.camel.impl.DefaultComponent;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -34,55 +36,99 @@ import org.springframework.core.io.Resource;
  * for performing SQL operations using an XML mapping file to abstract away the SQL
  *
  * @version $Revision$
+ * 
+ * <pre>
+ * Ibatis Component used to read/write to a database.
+ *
+ * <u>Requires one of the following:</u>
+ *
+ * 1. A Sql Map config file either on the root of
+ * the classpath or explicitly set.
+ *
+ * <b>OR</b>
+ *
+ * 2. A SqlMapClient explicityly set.
+ *
+ * Using Ibatis as a source of data (&lt;from&gt;) you can use this component
+ * to treat a database table as a logical queue.
+ * Details are available in the {@link IBatisPollingConsumer}
+ *
+ * Using Ibatis as a destination for data (&lt;to&gt;) you can use this
+ * component to run an insert statement either on a single message or if the
+ * delivered content contains a collection of messages it can iterate through
+ * the collection and run the insert on each element.
+ * Details are available in the {@link IBatisProducer}
+ * </pre>
+ *
+ * @see IBatisProducer
+ * @see IBatisPollingConsumer
  */
-public class IBatisComponent extends DefaultComponent {
-    public static final String DEFAULT_CONFIG_URI = "SqlMapConfig.xml";
+public class IBatisComponent extends ResourceBasedComponent {
     private static final transient Log LOG = LogFactory.getLog(IBatisComponent.class);
-
-
+    private static final String DEFAULT_CONFIG_URI = "classpath:SqlMapConfig.xml";
     private SqlMapClient sqlMapClient;
-    private Resource sqlMapResource;
+    private String sqlMapConfig = DEFAULT_CONFIG_URI;
+    private boolean useTransactions = true;
 
-    public IBatisComponent() {
+    public IBatisComponent(){
     }
 
-    public IBatisComponent(SqlMapClient sqlMapClient) {
+    public IBatisComponent(SqlMapClient sqlMapClient){
         this.sqlMapClient = sqlMapClient;
     }
 
     // Properties
     //-------------------------------------------------------------------------
+
+    /**
+     * Returns the configured SqlMapClient.
+     *
+     * @return com.ibatis.sqlmap.client.SqlMapClient
+     * @throws IOException If configured with a SqlMapConfig and there
+     * is a problem reading the resource.
+     */
     public SqlMapClient getSqlMapClient() throws IOException {
         if (sqlMapClient == null) {
             sqlMapClient = createSqlMapClient();
         }
         return sqlMapClient;
     }
-
+    
+    /**
+     * Sets the SqlMapClient
+     * @param sqlMapClient The client
+     */
     public void setSqlMapClient(SqlMapClient sqlMapClient) {
         this.sqlMapClient = sqlMapClient;
     }
 
-    public Resource getSqlMapResource() {
-        if (sqlMapResource == null) {
-            sqlMapResource = new ClassPathResource(DEFAULT_CONFIG_URI);
-            LOG.debug("Defaulting to use the iBatis configuration from: " + sqlMapResource);
-        }
-        return sqlMapResource;
+    /**
+     * The Spring uri of the SqlMapConfig
+     * @return java.lang.String
+     */
+    public String getSqlMapConfig() {
+        return sqlMapConfig;
     }
 
-    public void setSqlMapResource(Resource sqlMapResource) {
-        this.sqlMapResource = sqlMapResource;
+    /**
+     * Creates an IbatisEndpoint for use by an IbatisConsumer or IbatisProducer.
+     */
+    @Override
+    protected IBatisEndpoint createEndpoint(String uri, String remaining, Map params) throws Exception {
+        return new IBatisEndpoint(uri, this, remaining, params);
     }
-
-    // Implementation methods
-    //-------------------------------------------------------------------------
-    protected Endpoint createEndpoint(String uri, String remaining, Map parameters) throws Exception {
-        return new IBatisEndpoint(uri, this, remaining);
+    
+    private SqlMapClient createSqlMapClient() throws IOException {
+        Resource resource = resolveMandatoryResource(sqlMapConfig);
+        InputStream is = resource.getInputStream();
+        return SqlMapClientBuilder.buildSqlMapClient(new InputStreamReader(is));
     }
-
-    protected SqlMapClient createSqlMapClient() throws IOException {
-        InputStream in = getSqlMapResource().getInputStream();
-        return SqlMapClientBuilder.buildSqlMapClient(in);
+    
+    public boolean isUseTransactions() {
+        return useTransactions;
+    }
+    
+    public void setUseTransactions(boolean useTransactions) {
+        this.useTransactions = useTransactions;
     }
 }
--- components/camel-ibatis/src/main/java/org/apache/camel/component/ibatis/IBatisPollingConsumer.java
+++ components/camel-ibatis/src/main/java/org/apache/camel/component/ibatis/IBatisPollingConsumer.java
@@ -16,44 +16,182 @@
  */
 package org.apache.camel.component.ibatis;
 
+import java.util.List;
+
+import org.apache.camel.AsyncCallback;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.Message;
-import org.apache.camel.RuntimeCamelException;
-import org.apache.camel.impl.PollingConsumerSupport;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.ScheduledPollConsumer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @version $Revision$
+ * <pre>
+ *  Ibatis Camel Component used to read data from a database.
+ * 
+ *  Example Configuration :
+ *  &lt;route&gt;
+ *   &lt;from uri=&quot;ibatis:selectRecords&quot; /&gt;
+ *   &lt;to uri=&quot;jms:destinationQueue&quot; /&gt;
+ *  &lt;/route&gt;
+ * 
+ * 
+ *  This also can be configured to treat a table as a logical queue by defining
+ *  an &quot;onConsume&quot; statement.
+ * 
+ *  Example Configuration :
+ *  &lt;route&gt;
+ *   &lt;from uri=&quot;ibatis:selectRecords?consumer.onConsume=updateRecord&quot; /&gt;
+ *   &lt;to uri=&quot;jms:destinationQueue&quot; /&gt;
+ *  &lt;/route&gt;
+ * 
+ *  By default, if the select statement contains multiple rows, it will
+ *  iterate over the set and deliver each row to the route.  If this is not the
+ *  desired behavior then set &quot;useIterator=false&quot;.  This will deliver the entire
+ *  set to the route as a list.
+ * </pre>
+ *
+ * <b>URI Options</b>
+ * <table border="1">
+ * <thead>
+ * <th>Name</th>
+ * <th>Default Value</th>
+ * <th>description</th>
+ * </thead>
+ * <tbody>
+ * <tr>
+ * <td>initialDelay</td>
+ * <td>1000 ms</td>
+ * <td>time before polling starts</td>
+ * </tr>
+ * <tr>
+ * <td>delay</td>
+ * <td>500 ms</td>
+ * <td>time before the next poll</td>
+ * </tr>
+ * <tr>
+ * <td>timeUnit</td>
+ * <td>MILLISECONDS</td>
+ * <td>Time unit to use for delay properties (NANOSECONDS, MICROSECONDS,
+ * MILLISECONDS, SECONDS)</td>
+ * </tr>
+ * <tr>
+ * <td>useIterator</td>
+ * <td>true</td>
+ * <td>If true, processes one exchange per row. If false processes one exchange
+ * for all rows</td>
+ * </tr>
+ * <tr>
+ * <td>onConsume</td>
+ * <td>null</td>
+ * <td>statement to run after data has been processed</td>
+ * </tr>
+ * <tbody> </table>
+ *
+ * @see strategy.IBatisProcessingStrategy
  */
-public class IBatisPollingConsumer extends PollingConsumerSupport {
-    private final IBatisEndpoint endpoint;
+public class IBatisPollingConsumer extends ScheduledPollConsumer<Exchange> {
+    private static Log logger = LogFactory.getLog(IBatisPollingConsumer.class);
+    /**
+     * Statement to run after data has been processed in the route
+     */
+    private String onConsume;
+    /**
+     * Process resultset individually or as a list
+     */
+    private boolean useIterator = true;
 
-    public IBatisPollingConsumer(IBatisEndpoint endpoint) {
-        super(endpoint);
-        this.endpoint = endpoint;
+    public IBatisPollingConsumer(IBatisEndpoint endpoint, Processor processor) throws Exception {
+        super(endpoint, processor);
     }
 
-    public Exchange receive(long timeout) {
-        return receiveNoWait();
+    public IBatisEndpoint getEndpoint() {
+        return (IBatisEndpoint) super.getEndpoint();
     }
 
-    public Exchange receive() {
-        return receiveNoWait();
-    }
-
-    public Exchange receiveNoWait() {
-        try {
-            Exchange exchange = endpoint.createExchange();
-            Message in = exchange.getIn();
-            endpoint.query(in);
-            return exchange;
-        } catch (Exception e) {
-            throw new RuntimeCamelException("Failed to poll: " + endpoint + ". Reason: " + e, e);
+    /**
+     * Polls the database
+     */
+    @Override
+    protected void poll() throws Exception {
+        IBatisEndpoint endpoint = getEndpoint();
+        List data = endpoint.getProcessingStrategy().poll(this, getEndpoint());
+        if (useIterator) {
+            for (Object object : data) {
+                if (!super.isStopped()) {
+                    process(object);
+                }
+            }
+        } else {
+            process(data);
         }
     }
 
-    protected void doStart() throws Exception {
+    /**
+     * delivers the content
+     *
+     * @param data
+     *            a single row object if useIterator=true otherwise the entire
+     *            result set
+     */
+    protected void process(final Object data) throws Exception {
+        final IBatisEndpoint endpoint = getEndpoint();
+        final Exchange exchange = endpoint.createExchange(ExchangePattern.InOnly);
+
+        Message msg = exchange.getIn();
+        msg.setBody(data);
+        msg.setHeader("org.apache.camel.ibatis.queryName", endpoint.getStatement());
+    
+        logger.debug("Setting message");
+
+        getAsyncProcessor().process(exchange, new AsyncCallback() {
+            public void done(boolean sync) {
+                try {
+                    if (onConsume != null) {
+                        endpoint.getProcessingStrategy().commit(endpoint, exchange, data, onConsume);
+                    }
+                } catch (Exception e) {
+                    handleException(e);
+                }
+            }
+        });
+    }
+    
+    /**
+     * Gets the statement to run after successful processing
+     * @return Name of the statement
+     */
+    public String getOnConsume() {
+        return onConsume;
     }
 
-    protected void doStop() throws Exception {
+    /**
+     * Sets the statement to run after successful processing
+     * @param onConsume The name of the statement
+     */
+    public void setOnConsume(String onConsume) {
+        this.onConsume = onConsume;
+    }
+
+
+    /**
+     * Indicates how resultset should be delivered to the route
+     * @return boolean 
+     */
+    public boolean isUseIterator() {
+        return useIterator;
+    }
+
+    /**
+     * Sets how resultset should be delivered to route.
+     * Indicates delivery as either a list or individual object.
+     * defaults to true.
+     * @param useIterator
+     */
+    public void setUseIterator(boolean useIterator) {
+        this.useIterator = useIterator;
     }
 }
