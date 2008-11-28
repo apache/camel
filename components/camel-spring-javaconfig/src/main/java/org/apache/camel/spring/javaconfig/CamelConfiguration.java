d690a8e64b426fa02f2b7a181d82bc6b9fc06de6
James Strachan <jstrachan@apache.org>
2008-11-28 15:41:11 +0000

    added a first spike for CAMEL-1128
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@721522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring-javaconfig/src/main/java/org/apache/camel/spring/javaconfig/CamelConfiguration.java
@@ -0,0 +1,82 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.javaconfig;
+
+import org.apache.camel.Routes;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.CamelBeanPostProcessor;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.config.java.annotation.Bean;
+import org.springframework.config.java.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * A useful base class for writing
+ * <a href="http://www.springsource.org/javaconfig">Spring JavaConfig</a>
+ * configurations for working with Camel
+ *
+ * @version $Revision: 1.1 $
+ */
+@Configuration
+public abstract class CamelConfiguration    {
+
+    @Bean
+    public CamelBeanPostProcessor camelBeanPostProcessor() throws Exception {
+        return new CamelBeanPostProcessor(){
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                try {
+                    SpringCamelContext context = getCamelContext();
+                    if (context == null) {
+                        // we have not yet injected the context so lets set it
+                        setCamelContext(camelContext());
+                    }
+                    return super.postProcessAfterInitialization(bean, beanName);
+                } catch (BeansException e) {
+                    throw e;
+                } catch (Exception e) {
+                    throw new BeanInitializationException(e.getMessage(), e);
+                }
+            }
+        };
+    }
+
+    /**
+     * Returns the CamelContext
+     */
+    @Bean
+    public SpringCamelContext camelContext() throws Exception {
+        SpringCamelContext camelContext = new SpringCamelContext();
+        List<RouteBuilder> routes = routes();
+        for (Routes route : routes) {
+            camelContext.addRoutes(route);
+        }
+        return camelContext;
+    }
+
+
+    /**
+     * Returns the list of routes to use in this configuration
+     */
+    @Bean
+    public abstract List<RouteBuilder> routes();
+
+}
---
+++ components/camel-spring-javaconfig/src/main/java/org/apache/camel/spring/javaconfig/SingleRouteCamelConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.javaconfig;
+
+import org.apache.camel.Routes;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.config.java.annotation.Bean;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * A useful base class for writing
+ * <a href="http://www.springsource.org/javaconfig">Spring JavaConfig</a>
+ * configurations to configure a CamelContext with a single {@link RouteBuilder} instance
+
+ * @version $Revision: 1.1 $
+ */
+public abstract class SingleRouteCamelConfiguration extends CamelConfiguration {
+    //@Bean
+    public List<RouteBuilder> routes() {
+        return Collections.singletonList(route());
+    }
+
+    /**
+     * Creates the single {@link RouteBuilder} to use in this confguration
+     * @return
+     */
+    //@Bean
+    public abstract RouteBuilder route();
+}
