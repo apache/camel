92fc134b41f5f4eb7a3d48f86dd6acbc8cf070d8
James Strachan <jstrachan@apache.org>
2008-12-01 17:00:56 +0000

    Added initial spike for CAMEL-1136 to add a reusable base class, CamelTestSupport for easy unit testing
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@722127 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-test/src/main/java/org/apache/camel/test/CamelTestSupport.java
@@ -0,0 +1,339 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.test;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.Message;
+import org.apache.camel.Predicate;
+import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.Service;
+import org.apache.camel.spring.CamelBeanPostProcessor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.management.JmxSystemPropertyKeys;
+import org.apache.camel.spi.Language;
+import org.apache.camel.util.CamelContextHelper;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * A useful base class which creates a {@link org.apache.camel.CamelContext} with some routes
+ * along with a {@link org.apache.camel.ProducerTemplate} for use in the test case
+ *
+ * @version $Revision: 712844 $
+ */
+public abstract class CamelTestSupport extends TestSupport {
+    protected CamelContext context;
+    protected ProducerTemplate template;
+    private boolean useRouteBuilder = true;
+    private Service camelContextService;
+
+    public boolean isUseRouteBuilder() {
+        return useRouteBuilder;
+    }
+
+    public void setUseRouteBuilder(boolean useRouteBuilder) {
+        this.useRouteBuilder = useRouteBuilder;
+    }
+
+    public Service getCamelContextService() {
+        return camelContextService;
+    }
+
+    /**
+     * Allows a service to be registered a separate lifecycle service to start
+     * and stop the context; such as for Spring when the ApplicationContext is
+     * started and stopped, rather than directly stopping the CamelContext
+     */
+    public void setCamelContextService(Service camelContextService) {
+        this.camelContextService = camelContextService;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        context = createCamelContext();
+        assertValidContext(context);
+
+        template = context.createProducerTemplate();
+
+        postProcessTest();
+        
+        if (isUseRouteBuilder()) {
+            RouteBuilder[] builders = createRouteBuilders();
+            for (RouteBuilder builder : builders) {
+                log.debug("Using created route builder: " + builder);
+                context.addRoutes(builder);
+            }
+            startCamelContext();
+            log.debug("Routing Rules are: " + context.getRoutes());
+        } else {
+            log.debug("Using route builder from the created context: " + context);
+        }
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        log.debug("tearDown test: " + getName());
+        template.stop();
+        stopCamelContext();
+    }
+    
+    /**
+     * Lets post process this test instance to process any Camel annotations.
+     * Note that using Spring Test or Guice is a more powerful approach.
+     */
+    protected void postProcessTest() throws Exception {
+        CamelBeanPostProcessor processor = new CamelBeanPostProcessor();
+        processor.setCamelContext(context);
+        processor.postProcessBeforeInitialization(this, "this");
+    }
+
+    protected void stopCamelContext() throws Exception {
+        if (camelContextService != null) {
+            camelContextService.stop();
+        } else {
+            context.stop();
+        }
+    }
+
+    protected void startCamelContext() throws Exception {
+        if (camelContextService != null) {
+            camelContextService.start();
+        } else {
+            if (context instanceof DefaultCamelContext) {
+                DefaultCamelContext defaultCamelContext = (DefaultCamelContext) context;
+                if (!defaultCamelContext.isStarted()) {
+                    defaultCamelContext.start();
+                }
+            } else {
+                context.start();
+            }
+        }
+    }
+
+    protected CamelContext createCamelContext() throws Exception {
+        return new DefaultCamelContext(createRegistry());
+    }
+
+    protected JndiRegistry createRegistry() throws Exception {
+        return new JndiRegistry(createJndiContext());
+    }
+
+    protected Context createJndiContext() throws Exception {
+        InputStream in = getClass().getClassLoader().getResourceAsStream("jndi.properties");
+        assertNotNull("Cannot find jndi.properties on the classpath!", in);
+        Properties properties = new Properties();
+        properties.load(in);
+        return new InitialContext(new Hashtable(properties));
+    }
+
+    /**
+     * Factory method which derived classes can use to create a {@link org.apache.camel.builder.RouteBuilder}
+     * to define the routes for testing
+     */
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() {
+                // no routes added by default
+            }
+        };
+    }
+
+    /**
+     * Factory method which derived classes can use to create an array of
+     * {@link org.apache.camel.builder.RouteBuilder}s to define the routes for testing
+     *
+     * @see #createRouteBuilder()
+     */
+    protected RouteBuilder[] createRouteBuilders() throws Exception {
+        return new RouteBuilder[]{createRouteBuilder()};
+    }
+
+    /**
+     * Resolves a mandatory endpoint for the given URI or an exception is thrown
+     *
+     * @param uri the Camel <a href="">URI</a> to use to create or resolve an endpoint
+     * @return the endpoint
+     */
+    protected Endpoint resolveMandatoryEndpoint(String uri) {
+        return resolveMandatoryEndpoint(context, uri);
+    }
+
+    /**
+     * Resolves a mandatory endpoint for the given URI and expected type or an exception is thrown
+     *
+     * @param uri the Camel <a href="">URI</a> to use to create or resolve an endpoint
+     * @return the endpoint
+     */
+    protected <T extends Endpoint> T resolveMandatoryEndpoint(String uri, Class<T> endpointType) {
+        return resolveMandatoryEndpoint(context, uri, endpointType);
+    }
+
+    /**
+     * Resolves the mandatory Mock endpoint using a URI of the form <code>mock:someName</code>
+     *
+     * @param uri the URI which typically starts with "mock:" and has some name
+     * @return the mandatory mock endpoint or an exception is thrown if it could not be resolved
+     */
+    protected MockEndpoint getMockEndpoint(String uri) {
+        return resolveMandatoryEndpoint(uri, MockEndpoint.class);
+    }
+
+    /**
+     * Sends a message to the given endpoint URI with the body value
+     *
+     * @param endpointUri the URI of the endpoint to send to
+     * @param body        the body for the message
+     */
+    protected void sendBody(String endpointUri, final Object body) {
+        template.send(endpointUri, new Processor() {
+            public void process(Exchange exchange) {
+                Message in = exchange.getIn();
+                in.setBody(body);
+                in.setHeader("testCase", getName());
+            }
+        });
+    }
+
+    /**
+     * Sends a message to the given endpoint URI with the body value and specified headers
+     *
+     * @param endpointUri the URI of the endpoint to send to
+     * @param body        the body for the message
+     * @param headers     any headers to set on the message
+     */
+    protected void sendBody(String endpointUri, final Object body, final Map<String, Object> headers) {
+        template.send(endpointUri, new Processor() {
+            public void process(Exchange exchange) {
+                Message in = exchange.getIn();
+                in.setBody(body);
+                in.setHeader("testCase", getName());
+                for (Map.Entry<String, Object> entry : headers.entrySet()) {
+                    in.setHeader(entry.getKey(), entry.getValue());
+                }
+            }
+        });
+    }
+
+    /**
+     * Sends messages to the given endpoint for each of the specified bodies
+     *
+     * @param endpointUri the endpoint URI to send to
+     * @param bodies      the bodies to send, one per message
+     */
+    protected void sendBodies(String endpointUri, Object... bodies) {
+        for (Object body : bodies) {
+            sendBody(endpointUri, body);
+        }
+    }
+
+    /**
+     * Creates an exchange with the given body
+     */
+    protected Exchange createExchangeWithBody(Object body) {
+        return createExchangeWithBody(context, body);
+    }
+
+    /**
+     * Asserts that the given language name and expression evaluates to the
+     * given value on a specific exchange
+     */
+    protected void assertExpression(Exchange exchange, String languageName, String expressionText, Object expectedValue) {
+        Language language = assertResolveLanguage(languageName);
+
+        Expression expression = language.createExpression(expressionText);
+        assertNotNull("No Expression could be created for text: " + expressionText + " language: " + language, expression);
+
+        assertExpression(expression, exchange, expectedValue);
+    }
+
+    /**
+     * Asserts that the given language name and predicate expression evaluates
+     * to the expected value on the message exchange
+     */
+    protected void assertPredicate(String languageName, String expressionText, Exchange exchange, boolean expected) {
+        Language language = assertResolveLanguage(languageName);
+
+        Predicate predicate = language.createPredicate(expressionText);
+        assertNotNull("No Predicate could be created for text: " + expressionText + " language: " + language, predicate);
+
+        assertPredicate(predicate, exchange, expected);
+    }
+
+    /**
+     * Asserts that the language name can be resolved
+     */
+    protected Language assertResolveLanguage(String languageName) {
+        Language language = context.resolveLanguage(languageName);
+        assertNotNull("No language found for name: " + languageName, language);
+        return language;
+    }
+
+    /**
+     * Asserts that all the expectations of the Mock endpoints are valid
+     */
+    protected void assertMockEndpointsSatisfied() throws InterruptedException {
+        MockEndpoint.assertIsSatisfied(context);
+    }
+
+    protected void assertValidContext(CamelContext context) {
+        assertNotNull("No context found!", context);
+    }
+
+    protected <T> List<T> getSingletonEndpoints(Class<T> type) {
+        return CamelContextHelper.getSingletonEndpoints(context, type);
+    }
+
+    protected <T extends Endpoint> T getMandatoryEndpoint(String uri, Class<T> type) {
+        T endpoint = context.getEndpoint(uri, type);
+        assertNotNull("No endpoint found for uri: " + uri, endpoint);
+        return endpoint;
+    }
+
+    protected Endpoint getMandatoryEndpoint(String uri) {
+        Endpoint endpoint = context.getEndpoint(uri);
+        assertNotNull("No endpoint found for uri: " + uri, endpoint);
+        return endpoint;
+    }
+
+    /**
+     * Disables the JMX agent. Must be called before the {@link #setUp()} method.
+     */
+    protected void disableJMX() {
+        System.setProperty(JmxSystemPropertyKeys.DISABLED, "true");
+    }
+
+    /**
+     * Enables the JMX agent. Must be called before the {@link #setUp()} method.
+     */
+    protected void enableJMX() {
+        System.setProperty(JmxSystemPropertyKeys.DISABLED, "false");
+    }
+
+}
---
+++ components/camel-test/src/main/java/org/apache/camel/test/TestSupport.java
@@ -0,0 +1,373 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.test;
+
+import java.io.File;
+import java.util.List;
+
+import junit.framework.TestCase;
+import org.apache.camel.builder.Builder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.ValueBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.processor.DelegateAsyncProcessor;
+import org.apache.camel.processor.DelegateProcessor;
+import org.apache.camel.util.ExchangeHelper;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.InvalidPayloadException;
+import org.apache.camel.Message;
+import org.apache.camel.Expression;
+import org.apache.camel.Predicate;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Route;
+import org.apache.camel.Processor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A bunch of useful testing methods
+ *
+ * @version $Revision: 712937 $
+ */
+public abstract class TestSupport extends TestCase {
+
+    protected transient Log log = LogFactory.getLog(getClass());
+
+    // Builder methods for expressions used when testing
+    // -------------------------------------------------------------------------
+
+    /**
+     * Returns a value builder for the given header
+     */
+    public ValueBuilder header(String name) {
+        return Builder.header(name);
+    }
+
+    /**
+     * Returns a predicate and value builder for the inbound body on an exchange
+     */
+    public ValueBuilder body() {
+        return Builder.body();
+    }
+
+    /**
+     * Returns a predicate and value builder for the inbound message body as a
+     * specific type
+     */
+    public <T> ValueBuilder bodyAs(Class<T> type) {
+        return Builder.bodyAs(type);
+    }
+
+    /**
+     * Returns a predicate and value builder for the outbound body on an
+     * exchange
+     */
+    public ValueBuilder outBody() {
+        return Builder.outBody();
+    }
+
+    /**
+     * Returns a predicate and value builder for the outbound message body as a
+     * specific type
+     */
+    public <T> ValueBuilder outBodyAs(Class<T> type) {
+        return Builder.outBodyAs(type);
+    }
+
+    /**
+     * Returns a predicate and value builder for the fault body on an
+     * exchange
+     */
+    public ValueBuilder faultBody() {
+        return Builder.faultBody();
+    }
+
+    /**
+     * Returns a predicate and value builder for the fault message body as a
+     * specific type
+     */
+    public <T> ValueBuilder faultBodyAs(Class<T> type) {
+        return Builder.faultBodyAs(type);
+    }
+
+    /**
+     * Returns a value builder for the given system property
+     */
+    public ValueBuilder systemProperty(String name) {
+        return Builder.systemProperty(name);
+    }
+
+    /**
+     * Returns a value builder for the given system property
+     */
+    public ValueBuilder systemProperty(String name, String defaultValue) {
+        return Builder.systemProperty(name, defaultValue);
+    }
+
+    // Assertions
+    // -----------------------------------------------------------------------
+
+    protected <T> T assertIsInstanceOf(Class<T> expectedType, Object value) {
+        assertNotNull("Expected an instance of type: " + expectedType.getName() + " but was null", value);
+        assertTrue("object should be a " + expectedType.getName() + " but was: " + value + " with type: "
+                   + value.getClass().getName(), expectedType.isInstance(value));
+        return expectedType.cast(value);
+    }
+
+    protected void assertEndpointUri(Endpoint endpoint, String uri) {
+        assertNotNull("Endpoint is null when expecting endpoint for: " + uri, endpoint);
+        assertEquals("Endoint uri for: " + endpoint, uri, endpoint.getEndpointUri());
+    }
+
+    /**
+     * Asserts the In message on the exchange contains the expected value
+     */
+    protected Object assertInMessageHeader(Exchange exchange, String name, Object expected) {
+        return assertMessageHeader(exchange.getIn(), name, expected);
+    }
+
+    /**
+     * Asserts the Out message on the exchange contains the expected value
+     */
+    protected Object assertOutMessageHeader(Exchange exchange, String name, Object expected) {
+        return assertMessageHeader(exchange.getOut(), name, expected);
+    }
+
+    /**
+     * Asserts that the given exchange has an OUT message of the given body value
+     *
+     * @param exchange the exchange which should have an OUT message
+     * @param expected the expected value of the OUT message
+     * @throws org.apache.camel.InvalidPayloadException is thrown if the payload is not the expected class type
+     */
+    protected void assertInMessageBodyEquals(Exchange exchange, Object expected) throws InvalidPayloadException {
+        assertNotNull("Should have a response exchange!", exchange);
+
+        Object actual;
+        if (expected == null) {
+            actual = ExchangeHelper.getMandatoryInBody(exchange);
+            assertEquals("in body of: " + exchange, expected, actual);
+        } else {
+            actual = ExchangeHelper.getMandatoryInBody(exchange, expected.getClass());
+        }
+        assertEquals("in body of: " + exchange, expected, actual);
+
+        log.debug("Received response: " + exchange + " with in: " + exchange.getIn());
+    }
+
+    /**
+     * Asserts that the given exchange has an OUT message of the given body value
+     *
+     * @param exchange the exchange which should have an OUT message
+     * @param expected the expected value of the OUT message
+     * @throws org.apache.camel.InvalidPayloadException is thrown if the payload is not the expected class type
+     */
+    protected void assertOutMessageBodyEquals(Exchange exchange, Object expected) throws InvalidPayloadException {
+        assertNotNull("Should have a response exchange!", exchange);
+
+        Object actual;
+        if (expected == null) {
+            actual = ExchangeHelper.getMandatoryOutBody(exchange);
+            assertEquals("output body of: " + exchange, expected, actual);
+        } else {
+            actual = ExchangeHelper.getMandatoryOutBody(exchange, expected.getClass());
+        }
+        assertEquals("output body of: " + exchange, expected, actual);
+
+        log.debug("Received response: " + exchange + " with out: " + exchange.getOut());
+    }
+
+    protected Object assertMessageHeader(Message message, String name, Object expected) {
+        Object value = message.getHeader(name);
+        assertEquals("Header: " + name + " on Message: " + message, expected, value);
+        return value;
+    }
+
+    /**
+     * Asserts that the given expression when evaluated returns the given answer
+     */
+    protected Object assertExpression(Expression expression, Exchange exchange, Object expected) {
+        Object value = expression.evaluate(exchange);
+
+        // lets try convert to the type of the expected
+        if (expected != null) {
+            value = ExchangeHelper.convertToType(exchange, expected.getClass(), value);
+        }
+
+        log.debug("Evaluated expression: " + expression + " on exchange: " + exchange + " result: " + value);
+
+        assertEquals("Expression: " + expression + " on Exchange: " + exchange, expected, value);
+        return value;
+    }
+
+    /**
+     * Asserts that the predicate returns the expected value on the exchange
+     */
+    protected void assertPredicateMatches(Predicate predicate, Exchange exchange) {
+        assertPredicate(predicate, exchange, true);
+    }
+
+    /**
+     * Asserts that the predicate returns the expected value on the exchange
+     */
+    protected void assertPredicateDoesNotMatch(Predicate predicate, Exchange exchange) {
+        try {
+            predicate.assertMatches("Predicate should match", exchange);
+        } catch (AssertionError e) {
+            log.debug("Caught expected assertion error: " + e);
+        }
+        assertPredicate(predicate, exchange, false);
+    }
+
+    /**
+     * Asserts that the predicate returns the expected value on the exchange
+     */
+    protected boolean assertPredicate(Predicate predicate, Exchange exchange, boolean expected) {
+        if (expected) {
+            predicate.assertMatches("Predicate failed", exchange);
+        }
+        boolean value = predicate.matches(exchange);
+
+        log.debug("Evaluated predicate: " + predicate + " on exchange: " + exchange + " result: " + value);
+
+        assertEquals("Predicate: " + predicate + " on Exchange: " + exchange, expected, value);
+        return value;
+    }
+
+    /**
+     * Resolves an endpoint and asserts that it is found
+     */
+    protected Endpoint resolveMandatoryEndpoint(CamelContext context, String uri) {
+        Endpoint endpoint = context.getEndpoint(uri);
+
+        assertNotNull("No endpoint found for URI: " + uri, endpoint);
+
+        return endpoint;
+    }
+
+    /**
+     * Resolves an endpoint and asserts that it is found
+     */
+    protected <T extends Endpoint> T resolveMandatoryEndpoint(CamelContext context, String uri,
+                                                              Class<T> endpointType) {
+        T endpoint = context.getEndpoint(uri, endpointType);
+
+        assertNotNull("No endpoint found for URI: " + uri, endpoint);
+
+        return endpoint;
+    }
+
+    /**
+     * Creates an exchange with the given body
+     */
+    protected Exchange createExchangeWithBody(CamelContext camelContext, Object body) {
+        Exchange exchange = new DefaultExchange(camelContext);
+        Message message = exchange.getIn();
+        message.setHeader("testName", getName());
+        message.setHeader("testClass", getClass().getName());
+        message.setBody(body);
+        return exchange;
+    }
+
+    protected <T> T assertOneElement(List<T> list) {
+        assertEquals("Size of list should be 1: " + list, 1, list.size());
+        return list.get(0);
+    }
+
+    /**
+     * Asserts that a list is of the given size
+     */
+    protected <T> List<T> assertListSize(List<T> list, int size) {
+        assertEquals("List should be of size: " + size + " but is: " + list, size, list.size());
+        return list;
+    }
+
+    /**
+     * A helper method to create a list of Route objects for a given route builder
+     */
+    protected List<Route> getRouteList(RouteBuilder builder) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(builder);
+        context.start();
+        List<Route> answer = context.getRoutes();
+        context.stop();
+        return answer;
+    }
+
+    /**
+     * Asserts that the text contains the given string
+     *
+     * @param text the text to compare
+     * @param containedText the text which must be contained inside the other text parameter
+     */
+    protected void assertStringContains(String text, String containedText) {
+        assertNotNull("Text should not be null!", text);
+        assertTrue("Text: " + text + " does not contain: " + containedText, text.contains(containedText));
+    }
+
+    /**
+     * If a processor is wrapped with a bunch of DelegateProcessor or DelegateAsyncProcessor objects
+     * this call will drill through them and return the wrapped Processor.
+     */
+    protected Processor unwrap(Processor processor) {
+        while (true) {
+            if (processor instanceof DelegateAsyncProcessor) {
+                processor = ((DelegateAsyncProcessor)processor).getProcessor();
+            } else if (processor instanceof DelegateProcessor) {
+                processor = ((DelegateProcessor)processor).getProcessor();
+            } else {
+                return processor;
+            }
+        }
+    }
+
+    /**
+     * Recursively delete a directory, useful to zapping test data
+     *
+     * @param file the directory to be deleted
+     */
+    public static void deleteDirectory(String file) {
+        deleteDirectory(new File(file));
+    }
+
+    /**
+     * Recursively delete a directory, useful to zapping test data
+     *
+     * @param file the directory to be deleted
+     */
+    public static void deleteDirectory(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                deleteDirectory(files[i]);
+            }
+        }
+        file.delete();
+    }
+
+    /**
+     * create the directory
+     *
+     * @param file the directory to be created
+     */
+    public static void createDirectory(String file) {
+        File dir = new File(file);
+        dir.mkdirs();
+    }
+}
