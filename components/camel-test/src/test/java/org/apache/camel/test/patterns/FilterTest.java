4f026357406cf801af7875b603d28c7e993bf1cc
James Strachan <jstrachan@apache.org>
2008-12-01 17:00:56 +0000

    Added initial spike for CAMEL-1136 to add a reusable base class, CamelTestSupport for easy unit testing
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@722127 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-test/src/test/java/org/apache/camel/test/patterns/FilterTest.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.test.patterns;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.test.CamelTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision: 630568 $
+ */
+public class FilterTest extends CamelTestSupport {
+
+    @EndpointInject(uri="mock:result")
+    protected MockEndpoint resultEndpoint;
+
+    public void testSendMatchingMessage() throws Exception {
+        resultEndpoint.expectedMessageCount(1);
+
+        template.sendBodyAndHeader("direct:start", "<matched/>", "foo", "bar");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    public void testSendNotMatchingMessage() throws Exception {
+        resultEndpoint.expectedMessageCount(0);
+
+        template.sendBodyAndHeader("direct:start", "<notMatched/>", "foo", "notMatchedHeaderValue");
+
+        resultEndpoint.assertIsSatisfied();
+    }
+
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:start").filter(header("foo").isEqualTo("bar")).to("mock:result");
+            }
+        };
+    }
+}
