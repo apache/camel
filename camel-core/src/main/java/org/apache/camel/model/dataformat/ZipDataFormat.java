30918b0493a0f010a015b23a3b35180b8a30e2b4
Willem Ning Jiang <ningjiang@apache.org>
2008-12-01 09:14:28 +0000

    Added the files which I missed in the last commit
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/branches/camel-1.x@722008 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/main/java/org/apache/camel/model/dataformat/ZipDataFormat.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.model.dataformat;
+
+
+import java.util.zip.Deflater;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.camel.spi.DataFormat;
+import org.apache.camel.spi.RouteContext;
+
+@XmlRootElement(name = "zip")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ZipDataFormat extends DataFormatType {
+
+    @XmlAttribute(required = false)
+    private int compressionLevel = Deflater.BEST_SPEED;
+    
+    public ZipDataFormat() {
+        
+    }
+
+    public ZipDataFormat(int compressionLevel) {
+        this.compressionLevel = compressionLevel;
+    }
+
+    @Override
+    protected DataFormat createDataFormat(RouteContext routeContext) {
+        return new org.apache.camel.impl.ZipDataFormat(compressionLevel);
+    }
+
+    public int getCompressionLevel() {
+        return compressionLevel;
+    }
+
+    public void setCompressionLevel(int compressionLevel) {
+        this.compressionLevel = compressionLevel;
+    }
+}
