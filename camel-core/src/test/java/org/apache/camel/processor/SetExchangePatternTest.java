38cd99ea6d25741eb9c588658c5f7e656363163c
Willem Ning Jiang <ningjiang@apache.org>
2008-12-11 13:21:40 +0000

    Merged revisions 725652,725660 via svnmerge from https://svn.apache.org/repos/asf/activemq/camel/trunk
    ........
      r725652 | jstrachan | 2008-12-11 18:48:32 +0800 (Thu, 11 Dec 2008) | 1 line
      
      added a few more tests and examples to CAMEL-1171
    ........
      r725660 | jstrachan | 2008-12-11 19:07:48 +0800 (Thu, 11 Dec 2008) | 1 line
      
      added a few more tests and examples to CAMEL-1171 and reformatted the test a little to act as slightly more clear snippets on the different permutations available
    ........
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/branches/camel-1.x@725680 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/SetExchangePatternTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/SetExchangePatternTest.java
@@ -17,69 +17,91 @@
 package org.apache.camel.processor;
 
 import org.apache.camel.ContextTestSupport;
-import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 
 public class SetExchangePatternTest extends ContextTestSupport {
-    
-    private void sendMessage(String uri, final ExchangePattern ep, final String message) {
-        template.send(uri, new Processor() {
-            public void process(Exchange exchange) throws Exception {
-                exchange.setPattern(ep); 
-                exchange.getIn().setBody(message);
-            }
-            
-        });
+
+    public void testInOut() throws Exception {
+        assertMessageReceivedWithPattern("direct:testInOut", ExchangePattern.InOut);
     }
-   
-    public void testSetInOut() throws Exception {
-        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
-        resultEndpoint.expectedBodiesReceived("InOnlyMessage");        
-        sendMessage("direct:inOnly", ExchangePattern.InOnly, "InOnlyMessage");
+
+    public void testInOnly() throws Exception {
+        assertMessageReceivedWithPattern("direct:testInOnly", ExchangePattern.InOnly);
+    }
+
+    public void testSetToInOnlyThenTo() throws Exception {
+        assertMessageReceivedWithPattern("direct:testSetToInOnlyThenTo", ExchangePattern.InOnly);
+    }
+
+    public void testSetToInOutThenTo() throws Exception {
+        assertMessageReceivedWithPattern("direct:testSetToInOutThenTo", ExchangePattern.InOut);
+    }
+
+    public void testToWithInOnlyParam() throws Exception {
+        assertMessageReceivedWithPattern("direct:testToWithInOnlyParam", ExchangePattern.InOnly);
+    }
+
+    public void testToWithInOutParam() throws Exception {
+        assertMessageReceivedWithPattern("direct:testToWithInOutParam", ExchangePattern.InOut);
+    }
+
+    public void testToWithRobustInOnlyParam() throws Exception {
+        assertMessageReceivedWithPattern("direct:testToWithRobustInOnlyParam", ExchangePattern.RobustInOnly);
+    }
+
+    public void testSetExchangePatternInOnly() throws Exception {
+        assertMessageReceivedWithPattern("direct:testSetExchangePatternInOnly", ExchangePattern.InOnly);
+    }
+
+
+    protected void assertMessageReceivedWithPattern(String sendUri, ExchangePattern expectedPattern) throws InterruptedException {
+        ExchangePattern sendPattern;
+        switch (expectedPattern) {
+            case InOut:
+                sendPattern = ExchangePattern.InOnly;
+                break;
+            case InOnly:
+            case RobustInOnly:
+                sendPattern = ExchangePattern.InOut;
+                break;
+            default:
+                sendPattern = ExchangePattern.InOnly;
+        }
+
+        MockEndpoint resultEndpoint = getMockEndpoint("mock:result");
+        String expectedBody = "InOnlyMessage";
+        resultEndpoint.expectedBodiesReceived(expectedBody);
+        template.sendBody(sendUri, sendPattern, expectedBody);
         resultEndpoint.assertIsSatisfied();
-        assertEquals("The exchange pattern should be InOut", resultEndpoint.getExchanges().get(0).getPattern(), ExchangePattern.InOut);
+        ExchangePattern actualPattern = resultEndpoint.getExchanges().get(0).getPattern();
+        assertEquals("received exchange pattern", actualPattern, expectedPattern);
     }
-    
-    public void testSetInOnly() throws Exception {
-        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
-        resultEndpoint.expectedBodiesReceived("InOutMessage");        
-        sendMessage("direct:inOut", ExchangePattern.InOut, "InOutMessage");
-        resultEndpoint.assertIsSatisfied();
-        assertEquals("The exchange pattern should be InOnly", resultEndpoint.getExchanges().get(0).getPattern(), ExchangePattern.InOnly);
-    }
-    
-    public void testSetRobustInOnly() throws Exception {
-        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
-        resultEndpoint.expectedBodiesReceived("InOutMessage");        
-        sendMessage("direct:inOut1", ExchangePattern.InOut, "InOutMessage");
-        resultEndpoint.assertIsSatisfied();
-        assertEquals("The exchange pattern should be InOnly", resultEndpoint.getExchanges().get(0).getPattern(), ExchangePattern.RobustInOnly);
-    }
-    
-    public void testSetInOnly2() throws Exception {
-        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
-        resultEndpoint.expectedBodiesReceived("InOutMessage");        
-        sendMessage("direct:inOut2", ExchangePattern.InOut, "InOutMessage");
-        resultEndpoint.assertIsSatisfied();
-        assertEquals("The exchange pattern should be InOnly", resultEndpoint.getExchanges().get(0).getPattern(), ExchangePattern.InOnly);
-    }
-    
+
+
     protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             public void configure() {
              // START SNIPPET: example
-                //Set the exchange pattern to InOut, then send it from direct:inOnly to mock:result endpoint
-                from("direct:inOnly").inOut().to("mock:result");
-                //Set the exchange pattern to InOut, then send it from direct:inOut to mock:result endpoint               
-                from("direct:inOut").setExchangePattern(ExchangePattern.InOnly).to("mock:result");
-                //Send the exchange from direct:inOut1 to mock:result with setting the exchange pattern to be RobustInOnly
-                from("direct:inOut1").to(ExchangePattern.RobustInOnly, "mock:result");
-                //Send the exchange from direct:inOut2 to mock:result with setting the exchange pattern to be InOnly
-                from("direct:inOut2").inOnly("mock:result");
-             // END SNIPPET: example   
+                // Send to an endpoint using InOut
+                from("direct:testInOut").inOut("mock:result");
+
+                // Send to an endpoint using InOut
+                from("direct:testInOnly").inOnly("mock:result");
+
+                // Set the exchange pattern to InOut, then send it from direct:inOnly to mock:result endpoint
+                from("direct:testSetToInOnlyThenTo").inOnly().to("mock:result");
+                from("direct:testSetToInOutThenTo").inOut().to("mock:result");
+
+                // Or we can pass the pattern as a parameter to the to() method
+                from("direct:testToWithInOnlyParam").to(ExchangePattern.InOnly, "mock:result");
+                from("direct:testToWithInOutParam").to(ExchangePattern.InOut, "mock:result");
+                from("direct:testToWithRobustInOnlyParam").to(ExchangePattern.RobustInOnly, "mock:result");
+
+                // Set the exchange pattern to InOut, then send it on
+                from("direct:testSetExchangePatternInOnly").setExchangePattern(ExchangePattern.InOnly).to("mock:result");
+             // END SNIPPET: example
             }
         };
     }
