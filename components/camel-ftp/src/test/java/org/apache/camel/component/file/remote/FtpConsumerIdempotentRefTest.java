a6c92c1bbe995cfe80138628355d41200031e80d
Claus Ibsen <davsclaus@apache.org>
2008-12-28 10:06:49 +0000

    CAMEL-1154: Added idempotent to camel-ftp
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@729714 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpConsumerIdempotentRefTest.java
@@ -0,0 +1,98 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.file.remote;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileComponent;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.spi.IdempotentRepository;
+
+/**
+ * Unit test for the idempotentRepository # option.
+ */
+public class FtpConsumerIdempotentRefTest extends FtpServerTestSupport {
+
+    private static boolean invoked;
+
+    private int port = 20078;
+    private String ftpUrl = "ftp://admin@localhost:" + port
+            + "/idempotent?password=admin&binary=false&idempotent=true&idempotentRepository=#myRepo&delete=true";
+
+    public int getPort() {
+        return port;
+    }
+
+    @Override
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry jndi = super.createRegistry();
+        jndi.bind("myRepo", new MyIdempotentRepository());
+        return jndi;
+    }
+
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                from(ftpUrl).to("mock:result");
+            }
+        };
+    }
+
+    public void testIdempotent() throws Exception {
+        // consume the file the first time
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedBodiesReceived("Hello World");
+        mock.expectedMessageCount(1);
+
+        template.sendBodyAndHeader(ftpUrl, "Hello World", FileComponent.HEADER_FILE_NAME, "report.txt");
+
+        assertMockEndpointsSatisfied();
+
+        Thread.sleep(100);
+
+        // reset mock and set new expectations
+        mock.reset();
+        mock.expectedMessageCount(0);
+
+        // move file back
+        template.sendBodyAndHeader(ftpUrl, "Hello World", FileComponent.HEADER_FILE_NAME, "report.txt");
+
+        // should NOT consume the file again, let 2 secs pass to let the consumer try to consume it but it should not
+        Thread.sleep(2000);
+        assertMockEndpointsSatisfied();
+
+        assertTrue("MyIdempotentRepository should have been invoked", invoked);
+    }
+
+    public class MyIdempotentRepository implements IdempotentRepository<String> {
+
+        public boolean add(String messageId) {
+            // will return true 1st time, and false 2nd time
+            boolean result = invoked;
+            invoked = true;
+            assertEquals("report.txt", messageId);
+            return !result;
+        }
+
+        public boolean contains(String key) {
+            return false;
+        }
+    }
+
+}
