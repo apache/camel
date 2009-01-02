8d8ab73dab1c4fe818377976dab4c1e5f127f803
Jonathan Anstey <janstey@apache.org>
2009-01-02 13:58:44 +0000

    CAMEL-1031 - Intial go at the mvel component. Thanks to Przemyslaw for contributing this!
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@730720 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-mvel/src/main/java/org/apache/camel/language/mvel/MvelExpression.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.language.mvel;
+
+import java.io.Serializable;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.ExpressionSupport;
+import org.apache.camel.language.ExpressionEvaluationException;
+import org.apache.camel.language.IllegalSyntaxException;
+
+public class MvelExpression extends ExpressionSupport {
+
+    private final String expressionString;
+    private final Class<?> type;
+    private Serializable compiled;
+
+    public MvelExpression(MvelLanguage language, String expressionString, Class<?> type) {
+        this.expressionString = expressionString;
+        this.type = type;
+        try {
+            this.compiled = org.mvel.MVEL.compileExpression(expressionString);
+        } catch (Exception e) {
+            throw new IllegalSyntaxException(language, expressionString, e);
+        }
+    }
+
+    public static MvelExpression mvel(String expression) {
+        return new MvelExpression(new MvelLanguage(), expression, Object.class);
+    }
+
+    public Object evaluate(Exchange exchange) {
+        try {
+            return org.mvel.MVEL.executeExpression(compiled, new RootObject(exchange));
+        } catch (Exception e) {
+            throw new ExpressionEvaluationException(this, exchange, e);
+        }
+    }
+
+    protected String assertionFailureMessage(Exchange exchange) {
+        return expressionString;
+    }
+}
