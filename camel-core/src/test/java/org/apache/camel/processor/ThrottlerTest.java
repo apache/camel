80c6e483f3b93510524c0cd9ee541702f13eacfd
Gert Vanthienen <gertv@apache.org>
2009-01-05 08:47:47 +0000

    Merged revisions 730132,730157,731488 via svnmerge from https://svn.eu.apache.org/repos/asf/activemq/camel/trunk
    ........
      r730132 | gertv | 2008-12-30 14:59:16 +0100 (Tue, 30 Dec 2008) | 1 line
      
      CAMEL-1199: Throttler appears to throttle per thread instead of over all threads
    ........
      r730157 | gertv | 2008-12-30 16:51:25 +0100 (Tue, 30 Dec 2008) | 1 line
      
      CAMEL-1199: Fixing bug I just introduced
    ........
      r731488 | gertv | 2009-01-05 09:14:27 +0100 (Mon, 05 Jan 2009) | 1 line
      
      CAMEL-1199: Throttler appears to throttle per thread
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/branches/camel-1.x@731493 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/ThrottlerTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/ThrottlerTest.java
@@ -16,14 +16,19 @@
  */
 package org.apache.camel.processor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.apache.camel.ContextTestSupport;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.processor.Throttler.TimeSlot;
 
 /**
  * @version $Revision$
  */
 public class ThrottlerTest extends ContextTestSupport {
+    private static final int INTERVAL = 500;
     protected int messageCount = 6;
 
     public void testSendLotsOfMessagesButOnly3GetThrough() throws Exception {
@@ -39,13 +44,52 @@ public class ThrottlerTest extends ContextTestSupport {
         // to check that the throttle really does kick in
         resultEndpoint.assertIsSatisfied();
     }
+    
+    public void testSendLotsOfMessagesSimultaneouslyButOnly3GetThrough() throws Exception {
+        long start = System.currentTimeMillis();
+        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
+        resultEndpoint.expectedMessageCount(messageCount);
+
+        ExecutorService executor = Executors.newFixedThreadPool(messageCount);
+        for (int i = 0; i < messageCount; i++) {
+            executor.execute(new Runnable() {
+                public void run() {
+                    template.sendBody("direct:a", "<message>payload</message>");
+                }                
+            });
+        }
+        
+        // let's wait for the exchanges to arrive
+        resultEndpoint.assertIsSatisfied();
+        
+        // now assert that they have actually been throttled
+        long minimumTime = (messageCount - 1) * INTERVAL;
+        assertTrue("Should take at least " + minimumTime + "ms", System.currentTimeMillis() - start >= minimumTime);
+    }
+    
+    public void testTimeSlotCalculus() throws Exception {
+        Throttler throttler = new Throttler(null, 2, 1000);
+        TimeSlot slot = throttler.nextSlot();
+        // start a new time slot
+        assertNotNull(slot);
+        // make sure the same slot is used (2 exchanges per slot)
+        assertSame(slot, throttler.nextSlot());
+        assertTrue(slot.isFull());
+        
+        TimeSlot next = throttler.nextSlot();
+        // now we should have a new slot that starts somewhere in the future
+        assertNotSame(slot, next);
+        assertFalse(next.isActive());
+    }
 
     protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             public void configure() {
                 // START SNIPPET: ex
-                from("seda:a").throttler(3).timePeriodMillis(30000).to("mock:result");
+                from("seda:a").throttler(3).timePeriodMillis(10000).to("mock:result");
                 // END SNIPPET: ex
+                
+                from("direct:a").throttler(1).timePeriodMillis(INTERVAL).to("mock:result");
             }
         };
     }
