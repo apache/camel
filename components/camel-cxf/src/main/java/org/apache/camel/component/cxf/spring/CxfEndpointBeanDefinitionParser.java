ed299bb898949f4c783c9a179cae78b3a883a695
Willem Ning Jiang <ningjiang@apache.org>
2009-01-09 05:49:54 +0000

    CAMEL-1238 added the comments of CxfSpringEndpointBean as William suggested
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/camel/trunk@732933 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/spring/CxfEndpointBeanDefinitionParser.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/spring/CxfEndpointBeanDefinitionParser.java
@@ -96,6 +96,8 @@ public class CxfEndpointBeanDefinitionParser extends AbstractBeanDefinitionParse
         return true;
     }
     
+    // To make the CxfEndpointBean clear without touching any Spring relates class 
+    // , we implements the ApplicationContextAware here
     public static class CxfSpringEndpointBean extends CxfEndpointBean implements ApplicationContextAware {
         public CxfSpringEndpointBean() {
             super();
