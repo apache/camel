ce1b0b7064eb1be05fe926e3812ce7082aa179e0
Willem Ning Jiang <ningjiang@apache.org>
2009-01-16 04:00:01 +0000

    Fixed the CS errors in camel-bam
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@734904 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-bam/src/main/java/org/apache/camel/bam/model/ProcessInstance.java
+++ components/camel-bam/src/main/java/org/apache/camel/bam/model/ProcessInstance.java
@@ -26,10 +26,10 @@ import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
-import javax.persistence.Table;
 
 import org.apache.camel.bam.rules.ActivityRules;
 import org.apache.commons.logging.Log;
