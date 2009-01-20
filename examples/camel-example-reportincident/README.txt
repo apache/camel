be2de8fb6f03f5103ac4aff965412d55d168160d
Claus Ibsen <davsclaus@apache.org>
2009-01-20 10:46:22 +0000

    CAMEL-1273: added report incident as example
    git-svn-id: https://svn.eu.apache.org/repos/asf/camel/trunk@735990 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-reportincident/README.txt
@@ -0,0 +1,70 @@
+Report Incident Example
+=======================
+
+An example based on real life use case for reporting incidents using webservice
+that are transformed and send as emails to a backing system
+
+It can be run using
+Maven or Ant.
+
+You will need to compile this example first:
+  mvn compile
+
+To run the example type
+  mvn jetty:run
+
+The webservice is exposed at:
+  http://localhost:8080/reportincident/webservice/incident
+
+
+To run the example with Ant
+  a. You need to have Apache CXF, Spring and Jetty installed. They can be
+  downloaded from the following locations
+    Apache CXF
+    http://cxf.apache.org/
+    Spring 2.5
+    http://www.springframework.org/download
+    Jetty6
+    http://dist.codehaus.org/jetty/
+
+
+  b. Export / Set home directories for the above as follows
+    UNIX
+    export CXF_HOME=<path to CXF install directory>
+    export SPRING_HOME=<path to Spring install directory>
+    export JETTY_HOME=<path to Jetty install directory>
+    Windows
+    set CXF_HOME=<path to CXF install directory>
+    set SPRING_HOME=<path to Spring install directory>
+    set JETTY_HOME=<path to Jetty install directory>
+
+  c. To Run using Ant, type
+    ant run
+
+The webservice is exposed at:
+  http://localhost:8080/reportincident/webservice/incident
+
+
+To stop the example hit ctrl + c
+
+To use log4j as the logging framework add this to the pom.xml:
+    <dependency>
+      <groupId>log4j</groupId>
+      <artifactId>log4j</artifactId>
+    </dependency>
+and log4j.properties is located in src/main/resources
+
+For the latest & greatest documentation on how to use this example please see
+  http://camel.apache.org/tutorial-example-reportincident.html
+
+If you hit any problems please let us know on the Camel Forums
+  http://activemq.apache.org/camel/discussion-forums.html
+
+Please help us make Apache Camel better - we appreciate any feedback you may
+have.  Enjoy!
+
+------------------------
+The Camel riders!
+
+
+
---
+++ examples/camel-example-reportincident/src/test/java/org/apache/camel/example/reportincident/ReportIncidentRoutesTest.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example.reportincident;
+
+import junit.framework.TestCase;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.jvnet.mock_javamail.Mailbox;
+
+/**
+ * Unit test of our routes
+ */
+public class ReportIncidentRoutesTest extends TestCase {
+
+    // should be the same address as we have in our route
+    private static final String URL = "http://localhost:8080/reportincident/webservices/incident";
+
+    private CamelContext camel;
+
+    protected void startCamel() throws Exception {
+        camel = new DefaultCamelContext();
+        camel.addRoutes(new ReportIncidentRoutes());
+        camel.start();
+    }
+
+    protected static ReportIncidentEndpoint createCXFClient() {
+        // we use CXF to create a client for us as its easier than JAXWS and works
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setServiceClass(ReportIncidentEndpoint.class);
+        factory.setAddress(URL);
+        return (ReportIncidentEndpoint) factory.create();
+    }
+
+    public void testRendportIncident() throws Exception {
+        // start camel
+        startCamel();
+
+        // assert mailbox is empty before starting
+        Mailbox inbox = Mailbox.get("incident@mycompany.com");
+        assertEquals("Should not have mails", 0, inbox.size());
+
+        // create input parameter
+        InputReportIncident input = new InputReportIncident();
+        input.setIncidentId("123");
+        input.setIncidentDate("2008-08-18");
+        input.setGivenName("Claus");
+        input.setFamilyName("Ibsen");
+        input.setSummary("Bla");
+        input.setDetails("Bla bla");
+        input.setEmail("davsclaus@apache.org");
+        input.setPhone("0045 2962 7576");
+
+        // create the webservice client and send the request
+        ReportIncidentEndpoint client = createCXFClient();
+        OutputReportIncident out = client.reportIncident(input);
+
+        // assert we got a OK back
+        assertEquals("0", out.getCode());
+
+        // let some time pass to allow Camel to pickup the file and send it as an email
+        Thread.sleep(3000);
+
+        // assert mail box
+        assertEquals("Should have got 1 mail", 1, inbox.size());
+
+        // stop camel
+        camel.stop();
+    }
+}
