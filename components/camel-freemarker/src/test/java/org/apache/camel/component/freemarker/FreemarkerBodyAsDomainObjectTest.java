4acd538c04d0d64faeeb742ed15ca0291f987dd0
Claus Ibsen <davsclaus@apache.org>
2009-01-22 12:17:44 +0000

    CAMEL-1287: Added freemarker component.
    git-svn-id: https://svn.eu.apache.org/repos/asf/camel/trunk@736617 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-freemarker/src/test/java/org/apache/camel/component/freemarker/FreemarkerBodyAsDomainObjectTest.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.freemarker;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * Unit test with the body as a Domain object.
+ *
+ * @version $Revision$
+ */
+public class FreemarkerBodyAsDomainObjectTest extends ContextTestSupport {
+
+    public void testWithObject() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived("Hi Claus how are you? Its a nice day.\nGive my regards to the family Ibsen.");
+
+        MyPerson person = new MyPerson();
+        person.setFamilyName("Ibsen");
+        person.setGivenName("Claus");
+
+        template.requestBody("direct:in", person);
+
+        mock.assertIsSatisfied();
+    }
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:in")
+                    .to("freemarker:org/apache/camel/component/freemarker/BodyAsDomainObject.ftl")
+                    .to("mock:result");
+            }
+        };
+    }
+
+    public static class MyPerson {
+        private String givenName;
+        private String familyName;
+
+        public String getGivenName() {
+            return givenName;
+        }
+
+        public void setGivenName(String givenName) {
+            this.givenName = givenName;
+        }
+
+        public String getFamilyName() {
+            return familyName;
+        }
+
+        public void setFamilyName(String familyName) {
+            this.familyName = familyName;
+        }
+
+        public String toString() {
+            return "MyPerson{"
+                + "givenName='"
+                + givenName + '\''
+                + ", familyName='"
+                + familyName + '\''
+                + '}';
+        }
+    }
+
+}
