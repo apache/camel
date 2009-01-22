6cafcda367b9b55edf1c496a5b1f68d48168a9ee
William Tam <wtam@apache.org>
2009-01-22 04:58:43 +0000

    [CAMEL-1284] Restlet binding does not properly insert/extract Restlet message.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@736562 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletPostContentTest.java
@@ -0,0 +1,67 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.restlet;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.EntityEnclosingMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+
+/**
+ *
+ * @version $Revision$
+ */
+public class RestletPostContentTest extends ContextTestSupport {
+
+    private static final String MSG_BODY = "Hello World!";
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("restlet:http://localhost:8080/users/{username}?restletMethod=POST")
+                    .process(new SetUserProcessor());
+                
+            }
+            
+        };
+    }
+    
+    class SetUserProcessor implements Processor {
+        public void process(Exchange exchange) throws Exception {   
+            assertEquals(MSG_BODY, exchange.getIn().getBody(String.class));
+        }
+        
+    }
+    
+    public void testPostBody() throws Exception {
+        HttpMethod method = new PostMethod("http://localhost:8080/users/homer");
+        RequestEntity requestEntity = new StringRequestEntity(MSG_BODY, null, null);
+        ((EntityEnclosingMethod)method).setRequestEntity(requestEntity);
+        HttpClient client = new HttpClient();
+        assertEquals(200, client.executeMethod(method));
+
+    }
+}
