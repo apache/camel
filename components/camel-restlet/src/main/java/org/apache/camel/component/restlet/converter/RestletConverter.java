ce99cd75b018cabaab26787a63b90cebb51bcb85
William Tam <wtam@apache.org>
2009-02-04 04:16:47 +0000

    Merged revisions 740596 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r740596 | wtam | 2009-02-03 23:06:28 -0500 (Tue, 03 Feb 2009) | 1 line
      
      [CAMEL-1312]  Restlet component should allow response content type and return code to be set
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-1.x@740599 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-restlet/src/main/java/org/apache/camel/component/restlet/converter/RestletConverter.java
+++ components/camel-restlet/src/main/java/org/apache/camel/component/restlet/converter/RestletConverter.java
@@ -17,6 +17,7 @@
 package org.apache.camel.component.restlet.converter;
 
 import org.apache.camel.Converter;
+import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 
 /**
@@ -29,6 +30,11 @@ public class RestletConverter {
     @Converter
     public Method toMethod(String name) {
         return Method.valueOf(name.toUpperCase());
+    }
+    
+    @Converter
+    public MediaType toMediaType(String name) {
+        return MediaType.valueOf(name);
     }
 
 }
