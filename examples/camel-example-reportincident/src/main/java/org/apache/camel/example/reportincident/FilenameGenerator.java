d7bbe7c54d4298930317b7429060aadc023a4023
Willem Ning Jiang <ningjiang@apache.org>
2009-02-05 08:29:59 +0000

    CAMEL-1314 fixed the tomcat issue of camel-example-reportincident
    git-svn-id: https://svn.eu.apache.org/repos/asf/camel/trunk@741044 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/FilenameGenerator.java
+++ examples/camel-example-reportincident/src/main/java/org/apache/camel/example/reportincident/FilenameGenerator.java
@@ -21,7 +21,7 @@ package org.apache.camel.example.reportincident;
  */
 public class FilenameGenerator {
 
-    public String generateFilename(InputReportIncident input) {
+    public String generateFilename(InputReportIncident input) {        
         // compute the filename
         return "incident-" + input.getIncidentId() + ".txt";
     }
