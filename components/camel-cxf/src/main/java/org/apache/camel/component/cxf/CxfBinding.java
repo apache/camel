5bbba4209928018959bb89d3d0a1450f31563bb7
Willem Ning Jiang <ningjiang@apache.org>
2009-02-11 06:13:10 +0000

    CAMEL-1330 added a unit test for testing the response code, also fixed this issue in Camel 1.x branch
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-1.x@743243 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfBinding.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfBinding.java
@@ -165,7 +165,11 @@ public final class CxfBinding {
         if (context != null) {
             MessageContext messageContext = new WrappedMessageContext(context, null, Scope.HANDLER);
             response.put(Client.RESPONSE_CONTEXT, messageContext);
-
+            //put the ResponseCode in the response context
+            Object value = context.get(Message.RESPONSE_CODE);
+            if (value != null) {
+                response.put(Message.RESPONSE_CODE, value);
+            }
         }
     }
 
