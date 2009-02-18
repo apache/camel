d6e0109cf8c5c7b5c457fb4935496709b61663d5
Jonathan Anstey <janstey@apache.org>
2009-02-18 18:32:19 +0000

    Pesky CS...
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@745595 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/model/Constants.java
+++ camel-core/src/main/java/org/apache/camel/model/Constants.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,6 +19,10 @@ package org.apache.camel.model;
 /**
  * @version $Revision: 1.1 $
  */
-public class Constants {
-    public static final String JAXB_CONTEXT_PACKAGES = "org.apache.camel:org.apache.camel.model:org.apache.camel.model.config:org.apache.camel.model.dataformat:org.apache.camel.model.language:org.apache.camel.model.loadbalancer";
+public final class Constants {
+    public static final String JAXB_CONTEXT_PACKAGES = "org.apache.camel:org.apache.camel.model:org.apache.camel.model.config:org.apache.camel.model.dataformat" 
+                                                       + ":org.apache.camel.model.language:org.apache.camel.model.loadbalancer";
+
+    private Constants() {
+    }
 }
