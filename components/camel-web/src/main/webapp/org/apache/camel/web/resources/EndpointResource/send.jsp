120cff4bccd727eddfa17c41438951afb8bc9240
James Strachan <jstrachan@apache.org>
2009-02-24 17:52:03 +0000

    CAMEL-1355 added some nice literate HTML based test cases (which can be used to power an embedded browser too, like Selenium!) to test the functionality of the web application (as opposed to the REST API)  using WebDriver
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@747460 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-web/src/main/webapp/org/apache/camel/web/resources/EndpointResource/send.jsp
+++ components/camel-web/src/main/webapp/org/apache/camel/web/resources/EndpointResource/send.jsp
@@ -7,7 +7,7 @@
 
 <h1>Endpoint: ${it.uri}</h1>
 
-<form action='<c:url value="${it.href}"/>' method="post">
+<form action='<c:url value="${it.href}"/>' method="post" name="sendMessage">
   <input type="submit" value="Send"> <input type="reset"> <br>
   <textarea name="body" rows="30" cols="80"></textarea>
 </form>
---
+++ components/camel-web/src/test/java/org/apache/camel/web/htmlunit/CreateEndpointTest.java
@@ -0,0 +1,89 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.web.htmlunit;
+
+import org.apache.camel.web.htmlunit.pages.EndpointsPage;
+import org.apache.camel.web.htmlunit.pages.SendMessagePage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import static org.openqa.selenium.lift.Finders.link;
+import static org.openqa.selenium.lift.Finders.links;
+import static org.openqa.selenium.lift.Finders.title;
+import static org.openqa.selenium.lift.Matchers.atLeast;
+import static org.openqa.selenium.lift.Matchers.text;
+import org.openqa.selenium.WebElement;
+import static org.openqa.selenium.By.xpath;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.List;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class CreateEndpointTest extends HtmlTestSupport {
+    private static final transient Log LOG = LogFactory.getLog(CreateEndpointTest.class);
+
+    protected String newEndpointUri = "mock:myNewlyCreatedEndpoint";
+    protected String messageBody = "hello world!";
+
+    public void testCreateEndpoint() throws Exception {
+
+        goToRootPage();
+
+        assertPresenceOf(atLeast(4), links().with(text(not(equalTo("Images")))));
+
+        // TODO one day we should do this
+        //clickOn(xpath("//a[text() = 'Endpoints']"));
+        WebElement element = findElement(xpath("//a[text() = 'Endpoints']"));
+        element.click();
+
+        assertThat(getTitle(), containsString("Endpoints"));
+
+        List<WebElement> endpointLinks = findElements(xpath("//a[@class='endpoint']"));
+        assertTrue("Should be several endpoint links!", endpointLinks.size() > 0);
+
+        LOG.info("Found " + endpointLinks.size() + " endpoints links!");
+
+
+        // lets create a new endpoint
+        new EndpointsPage(getWebDriver()).createEndpoint(newEndpointUri);
+
+        assertThat(getTitle(), containsString(newEndpointUri));
+
+
+        // now lets send a message to it
+        findElement(xpath("//a[@class='send']")).click();
+
+        new SendMessagePage(getWebDriver()).sendMessage(messageBody);
+
+        // now lets view the message
+        // TODO can we force the last link to be clicked via xpath?
+        findElement(xpath("//a[@class='message']")).click();
+
+        String actualMessage = findElement(xpath("//div[@class='message']")).getText();
+        LOG.info("Found message body: " +actualMessage);
+
+        assertThat(actualMessage, containsString(messageBody));
+
+        LOG.debug("Source: " + getPageSource());
+    }
+
+}
