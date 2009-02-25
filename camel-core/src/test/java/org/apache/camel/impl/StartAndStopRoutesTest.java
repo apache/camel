2f501ff39717fa699e2c4fee26769fb9ba97f757
James Strachan <jstrachan@apache.org>
2009-02-25 18:20:06 +0000

    CAMEL-1004 - added a test case showing how to stop, mutate and restart a route
    git-svn-id: https://svn.eu.apache.org/repos/asf/camel/trunk@747883 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/impl/StartAndStopRoutesTest.java
@@ -0,0 +1,98 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.impl;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.seda.SedaEndpoint;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.RouteType;
+import org.apache.camel.model.FromType;
+
+import java.util.List;
+
+/**
+ * This test stops a route, mutates it then restarts it
+ *
+ * @version $Revision: 1.1 $
+ */
+public class StartAndStopRoutesTest extends ContextTestSupport {
+    protected SedaEndpoint endpointA;
+    protected SedaEndpoint endpointB;
+    protected SedaEndpoint endpointC;
+    protected Object expectedBody = "<hello>world!</hello>";
+
+    public void testStartRouteThenStopMutateAndStartRouteAgain() throws Exception {
+        List<RouteType> routes = context.getRouteDefinitions();
+        assertCollectionSize("Route", routes, 1);
+        RouteType route = routes.get(0);
+
+        endpointA = getMandatoryEndpoint("seda:test.a", SedaEndpoint.class);
+        endpointB = getMandatoryEndpoint("seda:test.b", SedaEndpoint.class);
+        endpointC = getMandatoryEndpoint("seda:test.C", SedaEndpoint.class);
+
+        assertProduceConsumers("endpointA", endpointA, 0, 1);
+        assertProduceConsumers("endpointB", endpointB, 1, 0);
+        assertProduceConsumers("endpointC", endpointC, 0, 0);
+
+        context.stopRoute(route);
+
+        assertProduceConsumers("endpointA", endpointA, 0, 0);
+        assertProduceConsumers("endpointB", endpointB, 0, 0);
+        assertProduceConsumers("endpointC", endpointC, 0, 0);
+
+
+        // lets mutate the route...
+        FromType fromType = assertOneElement(route.getInputs());
+        fromType.setUri("seda:test.C");
+        context.startRoute(route);
+
+        assertProduceConsumers("endpointA", endpointA, 0, 0);
+        assertProduceConsumers("endpointB", endpointB, 1, 0);
+        assertProduceConsumers("endpointC", endpointC, 0, 1);
+
+
+        // now lets check it works
+        MockEndpoint results = getMockEndpoint("mock:results");
+        results.expectedBodiesReceived(expectedBody);
+
+        template.sendBody(endpointC, expectedBody);
+
+        assertMockEndpointsSatisfied();
+    }
+
+    protected void assertProduceConsumers(String name, SedaEndpoint endpoint, int producerCount, int consumerCount) {
+        assertCollectionSize("Producers for " + name, endpoint.getProducers(), producerCount);
+        assertCollectionSize("Consumers for " + name, endpoint.getConsumers(), consumerCount);
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("seda:test.a").
+                        to("seda:test.b").
+                        to("mock:results");
+            }
+        };
+
+    }
+
+
+}
