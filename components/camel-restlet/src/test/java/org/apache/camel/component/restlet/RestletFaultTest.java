51fa03cb27b9ea612e8cc4bd9677e0c6ddc03465
William Tam <wtam@apache.org>
2009-02-27 04:59:45 +0000

    Merged revisions 748392 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r748392 | wtam | 2009-02-26 23:55:02 -0500 (Thu, 26 Feb 2009) | 1 line
      
      [CAMEL-1400] Restlet default binding does not acknowledge Fault message in the exchange when creating a response
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-1.x@748393 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletFaultTest.java
@@ -0,0 +1,63 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.restlet;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/**
+ *
+ * @version $Revision$
+ */
+public class RestletFaultTest extends ContextTestSupport {
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("restlet:http://localhost:9080/users/{username}?restletMethod=POST").process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        exchange.getFault().setHeader(RestletConstants.RESPONSE_CODE, "404");
+                        exchange.getFault().setHeader(RestletConstants.MEDIA_TYPE, "text/plain");
+                        exchange.getFault().setBody("Application fault");
+                        
+                    }        
+                });
+            }
+        };
+    }
+    
+    public void testFaultResponse() throws Exception {
+        HttpMethod method = new PostMethod("http://localhost:9080/users/homer");
+        try {
+            HttpClient client = new HttpClient();
+            assertEquals(404, client.executeMethod(method));
+            assertTrue(method.getResponseHeader("Content-Type").getValue()
+                    .startsWith("text/plain"));
+        } finally {
+            method.releaseConnection();
+        }
+
+    }
+}
