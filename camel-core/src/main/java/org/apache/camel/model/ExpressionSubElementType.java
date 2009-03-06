aaa1849372b91d9a1e2cd265b2fa8eef433aebc8
Claus Ibsen <davsclaus@apache.org>
2009-03-06 13:06:21 +0000

    Fixed hadrians big commit before
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@750883 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/model/ExpressionSubElementType.java
+++ camel-core/src/main/java/org/apache/camel/model/ExpressionSubElementType.java
@@ -24,7 +24,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 import org.apache.camel.Expression;
 import org.apache.camel.Predicate;
-import org.apache.camel.model.language.ExpressionType;
+import org.apache.camel.model.language.ExpressionDefinition;
 import org.apache.camel.spi.RouteContext;
 
 /**
@@ -34,7 +34,7 @@ import org.apache.camel.spi.RouteContext;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ExpressionSubElementType {
     @XmlElementRef
-    private ExpressionType expressionType;
+    private ExpressionDefinition expressionType;
     @XmlTransient
     private Expression expression;
     @XmlTransient
@@ -51,11 +51,11 @@ public class ExpressionSubElementType {
         this.predicate = predicate;
     }   
     
-    public ExpressionType getExpressionType() {
+    public ExpressionDefinition getExpressionType() {
         return expressionType;
     }
 
-    public void setExpressionType(ExpressionType expressionType) {
+    public void setExpressionType(ExpressionDefinition expressionType) {
         this.expressionType = expressionType;
     }
 
@@ -76,7 +76,7 @@ public class ExpressionSubElementType {
     }    
     
     public Expression createExpression(RouteContext routeContext) {
-        ExpressionType expressionType = getExpressionType();
+        ExpressionDefinition expressionType = getExpressionType();
         if (expressionType != null && expression == null) {
             expression = expressionType.createExpression(routeContext);
         }
@@ -84,7 +84,7 @@ public class ExpressionSubElementType {
     }
     
     public Predicate createPredicate(RouteContext routeContext) {
-        ExpressionType expressionType = getExpressionType();
+        ExpressionDefinition expressionType = getExpressionType();
         if (expressionType != null && getPredicate() == null) {
             setPredicate(expressionType.createPredicate(routeContext));
         }
