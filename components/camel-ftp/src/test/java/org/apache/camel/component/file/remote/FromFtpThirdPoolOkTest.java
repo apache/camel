dd899eb88d9c1ffa47a183a2563f9b792d93e7c5
Claus Ibsen <davsclaus@apache.org>
2009-03-13 07:52:18 +0000

    added unit test for Camel VFS consumer doing rollback and try again at next pool. Polished javadoc. Fixed failing test on hudson.
    git-svn-id: https://svn.eu.apache.org/repos/asf/camel/trunk@753153 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FromFtpThirdPoolOkTest.java
@@ -0,0 +1,86 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.file.remote;
+
+import java.io.File;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * @version $Revision$
+ */
+public class FromFtpThirdPoolOkTest extends FtpServerTestSupport {
+
+    private String body = "Hello World this file will be deleted";
+    private static int counter;
+
+    private String getFtpUrl() {
+        return "ftp://admin@localhost:" + getPort() + "/thirdpool?password=admin&delete=true";
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        deleteDirectory("res/home/thirdpool");
+        deleteDirectory("target/thridpool");
+        super.setUp();
+    }
+
+    public void testPollFileAndShouldBeDeletedAtThirdPoll() throws Exception {
+        template.sendBodyAndHeader(getFtpUrl(), body, Exchange.FILE_NAME, "hello.txt");
+
+        getMockEndpoint("mock:result").expectedBodiesReceived(body);
+        // 2 first attempt should fail
+        getMockEndpoint("mock:error").expectedMessageCount(2);
+
+        assertMockEndpointsSatisfied();
+
+        // give time to delete file
+        Thread.sleep(200);
+
+        assertEquals(3, counter);
+
+        // assert the file is deleted
+        File file = new File("./res/home/thirdpool/hello.txt");
+        file = file.getAbsoluteFile();
+        assertFalse("The file should have been deleted", file.exists());
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                // no redeliveries as we want the ftp consumer to try again
+                errorHandler(deadLetterChannel("mock:error").maximumRedeliveries(0).logStackTrace(false));
+
+                from(getFtpUrl()).process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        counter++;
+                        if (counter < 3) {
+                            // file should exists
+                            File file = new File("./res/home/thirdpool/hello.txt");
+                            file = file.getAbsoluteFile();
+                            assertTrue("The file should NOT have been deleted", file.exists());
+                            throw new IllegalArgumentException("Forced by unittest");
+                        }
+                    }
+                }).to("mock:result");
+            }
+        };
+    }
+
+}
