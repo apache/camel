1e31fa5f695e2f3f99fec2488197e99985f36376
Claus Ibsen <davsclaus@apache.org>
2009-03-24 09:36:29 +0000

    CAMEL-1461: JMSProducer only sets JMSReplyTo if exchange is out capable. Fixed a NPE.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@757693 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsRouteWithInOnlyTest.java
@@ -0,0 +1,95 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jms;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.JndiRegistry;
+import static org.apache.camel.component.jms.JmsComponent.jmsComponentClientAcknowledge;
+
+/**
+ * Unit test inspired by user forum
+ *
+ * @version $Revision$
+ */
+public class JmsRouteWithInOnlyTest extends ContextTestSupport {
+
+    protected String componentName = "activemq";
+
+    public void testSendOrder() throws Exception {
+        MockEndpoint inbox = getMockEndpoint("mock:inbox");
+        inbox.expectedBodiesReceived("Camel in Action");
+
+        MockEndpoint order = getMockEndpoint("mock:topic");
+        order.expectedBodiesReceived("Camel in Action");
+
+        Object out = template.requestBody("activemq:queue:inbox", "Camel in Action");
+        assertEquals("OK: Camel in Action", out);
+
+        assertMockEndpointsSatisfied();
+
+        // assert MEP
+        assertEquals(ExchangePattern.InOut, inbox.getReceivedExchanges().get(0).getPattern());
+        assertEquals(ExchangePattern.InOnly, order.getReceivedExchanges().get(0).getPattern());
+    }
+
+    @Override
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry jndi = super.createRegistry();
+        jndi.bind("orderService", new MyOrderServiceBean());
+        return jndi;
+    }
+
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext camelContext = super.createCamelContext();
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        camelContext.addComponent(componentName, jmsComponentClientAcknowledge(connectionFactory));
+
+        return camelContext;
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("activemq:queue:inbox")
+                    .to("mock:inbox")
+                    .inOnly("activemq:topic:order")
+                    .beanRef("orderService", "handleOrder");
+
+                from("activemq:topic:order")
+                    .to("mock:topic");
+            }
+        };
+    }
+
+    public static class MyOrderServiceBean {
+
+        public String handleOrder(String body) {
+            return "OK: " + body;
+        }
+
+    }
+}
