3fa10d24262e6f9073d7bb45bd8e61f7ad14b994
Claus Ibsen <davsclaus@apache.org>
2009-04-01 13:50:30 +0000

    Merged revisions 760890 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r760890 | davsclaus | 2009-04-01 15:46:01 +0200 (Wed, 01 Apr 2009) | 1 line
      
      Mina - use a timeout for writing to mina in case mina is hanging, otherwise we will hang forever. The client will get an exception anyway that the write failed.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-1.x@760891 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-mina/src/main/java/org/apache/camel/component/mina/MinaHelper.java
+++ components/camel-mina/src/main/java/org/apache/camel/component/mina/MinaHelper.java
@@ -42,7 +42,8 @@ public final class MinaHelper {
     public static void writeBody(IoSession session, Object body, Exchange exchange) throws CamelExchangeException {
         // the write operation is asynchronous. Use WriteFuture to wait until the session has been written
         WriteFuture future = session.write(body);
-        future.join();
+        // must use a timeout (we use 10s) as in some very high performance scenarious a write can cause thred hanging forever
+        future.join(10 * 1000L);
         if (!future.isWritten()) {
             throw new CamelExchangeException("Could not write body", exchange);
         }
