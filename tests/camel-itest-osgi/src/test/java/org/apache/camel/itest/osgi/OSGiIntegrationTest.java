4672f457a77b21bfc997e1b68f291f44a16a87e9
James Strachan <jstrachan@apache.org>
2009-04-16 15:37:26 +0000

    CAMEL-1536: added an OSGi integration test for Camel using Pax Exam to test Camel in Felix and Equinox
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@765670 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/OSGiIntegrationTest.java
@@ -0,0 +1,89 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.itest.osgi;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+/** @version $Revision: 1.1 $ */
+@RunWith(JUnit4TestRunner.class)
+public class OSGiIntegrationTest {
+  private static final transient Log LOG = LogFactory.getLog(OSGiIntegrationTest.class);
+
+  @Inject
+  BundleContext bundleContext;
+
+  @Test
+  public void listBundles() throws Exception {
+    System.out.println("************ Hello from OSGi ************");
+
+    for (Bundle b : bundleContext.getBundles()) {
+      System.out.println("Bundle " + b.getBundleId() + " : " + b.getSymbolicName());
+    }
+
+    // TODO we should be using Camel OSGi really to deal with class loader issues
+    CamelContext camelContext = new DefaultCamelContext();
+    
+    camelContext.addRoutes(new RouteBuilder() {
+      public void configure() throws Exception {
+        from("seda:foo").to("seda:bar");
+      }
+    });
+
+    camelContext.start();
+
+    Thread.sleep(2000);
+    camelContext.stop();
+
+  }
+
+  @Configuration
+  public static Option[] configure() {
+    return options(
+        // install log service using pax runners profile abstraction (there are more profiles, like DS)
+        //logProfile(),
+        
+        // this is how you set the default log level when using pax logging (logProfile)
+        //systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
+
+        // TODO why can't we find these from the maven pom.xml with transitive dependency?
+        mavenBundle().groupId("org.apache.camel").artifactId("camel-core").version(
+            "2.0-SNAPSHOT"),
+        wrappedBundle(
+            mavenBundle().groupId("commons-logging").artifactId("commons-logging").version("1.1.1")
+        ),
+
+
+        felix(), equinox()
+    );
+  }
+}
