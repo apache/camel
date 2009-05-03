a73059b4a01526d2de686e837156b0950656be1c
Willem Ning Jiang <ningjiang@apache.org>
2009-05-03 05:38:35 +0000

    Fixed the IOConverterTest error on Windows box
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@771029 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/converter/IOConverterTest.java
+++ camel-core/src/test/java/org/apache/camel/converter/IOConverterTest.java
@@ -36,7 +36,12 @@ public class IOConverterTest extends TestCase {
         byte[] data = IOConverter.toBytes(new FileInputStream(file));
         assertEquals("get the wrong byte size", file.length(), data.length);
         assertEquals('#', (char) data[0]);
-        assertEquals('!', (char) data[data.length - 2]);
+        char testChar = (char) data[data.length - 2];
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            // to skip the "0D" character
+            testChar = (char) data[data.length - 3];
+        }
+        assertEquals('!', testChar);
         // should end with a new line
         assertEquals('\n', (char) data[data.length - 1]);
     }
