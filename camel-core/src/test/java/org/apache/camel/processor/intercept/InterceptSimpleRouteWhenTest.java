3d38b6581845d8305380247a4c470cbbac70cc33
Claus Ibsen <davsclaus@apache.org>
2009-05-03 09:00:46 +0000

    CAMEL-1569: Added when() to intercept(). Its nice when the code is nice and clean now, and easy to add when predicate. (unlike the old code in InterceptFromDefinition, that got to complex)
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@771042 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/intercept/InterceptSimpleRouteWhenTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/intercept/InterceptSimpleRouteWhenTest.java
@@ -42,10 +42,12 @@ public class InterceptSimpleRouteWhenTest extends ContextTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
+                // START SNIPPET: e1
                 intercept().when(body().contains("Hello")).to("mock:intercepted");
 
                 from("direct:start")
                     .to("mock:foo", "mock:bar", "mock:result");
+                // END SNIPPET: e1
             }
         };
     }
