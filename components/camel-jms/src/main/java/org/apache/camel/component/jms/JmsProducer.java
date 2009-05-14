ed99a77bfd062649111179bccc36261e5d2c9bbc
Claus Ibsen <davsclaus@apache.org>
2009-05-14 08:20:58 +0000

    CAMEL-1614:Fixed bug if JMSReplyTo was in headers but null. To avoid WARN logs for InOnly messages with no JMSReplyTo anyway.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@774678 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsProducer.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsProducer.java
@@ -273,15 +273,17 @@ public class JmsProducer extends DefaultProducer {
             }
         } else {
             // we must honor these special flags to preverse QoS 
-            if (!endpoint.isPreserveMessageQos() && !endpoint.isExplicitQosEnabled()
-                    && exchange.getIn().getHeaders().containsKey("JMSReplyTo")) {
-                // we are routing an existing JmsMessage, origin from another JMS endpoint
-                // then we need to remove the existing JMSReplyTo, JMSCorrelationID.
-                // as we are not out capable and thus do not expect a reply, and therefore
-                // the consumer of this message we send should not return a reply
-                String to = destinationName != null ? destinationName : "" + destination;
-                LOG.warn("Disabling JMSReplyTo as this Exchange is not OUT capable: " + exchange + " with destination: " + to);
-                exchange.getIn().setHeader("JMSReplyTo", null);
+            if (!endpoint.isPreserveMessageQos() && !endpoint.isExplicitQosEnabled()) {
+                Object replyTo = exchange.getIn().getHeader("JMSReplyTo");
+                if (replyTo != null) {
+                    // we are routing an existing JmsMessage, origin from another JMS endpoint
+                    // then we need to remove the existing JMSReplyTo
+                    // as we are not out capable and thus do not expect a reply, and therefore
+                    // the consumer of this message we send should not return a reply
+                    String to = destinationName != null ? destinationName : "" + destination;
+                    LOG.warn("Disabling JMSReplyTo as this Exchange is not OUT capable with JMSReplyTo: " + replyTo + " to destination: " + to + " for Exchange: " + exchange);
+                    exchange.getIn().setHeader("JMSReplyTo", null);
+                }
             }
 
             MessageCreator messageCreator = new MessageCreator() {
