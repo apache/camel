cf731032c78f58f1f440b5403209eb3c4a28f1a5
Claus Ibsen <davsclaus@apache.org>
2009-05-25 12:39:55 +0000

    Added concurrent producer unit test.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@778409 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-mail/src/test/java/org/apache/camel/component/mail/MailProducerConcurrentTest.java
@@ -0,0 +1,86 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.mail;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.RouteBuilder;
+import org.jvnet.mock_javamail.Mailbox;
+
+/**
+ * Mail producer concurrent test.
+ *
+ * @version $Revision$
+ */
+public class MailProducerConcurrentTest extends ContextTestSupport {
+
+    public void testNoConcurrentProducers() throws Exception {
+        doSendMessages(1, 1);
+    }
+
+    public void testConcurrentProducers() throws Exception {
+        doSendMessages(10, 5);
+    }
+
+    private void doSendMessages(int files, int poolSize) throws Exception {
+        Mailbox.clearAll();
+
+        getMockEndpoint("mock:result").expectedMessageCount(files);
+        getMockEndpoint("mock:result").expectsNoDuplicates(body());
+
+        ExecutorService executor = Executors.newFixedThreadPool(poolSize);
+        for (int i = 0; i < files; i++) {
+            final int index = i;
+            executor.submit(new Callable<Object>() {
+                public Object call() throws Exception {
+                    template.sendBodyAndHeader("direct:start", "Message " + index, "To", "someone@localhost");
+                    return null;
+                }
+            });
+        }
+
+        assertMockEndpointsSatisfied();
+
+        Mailbox box = Mailbox.get("someone@localhost");
+        assertEquals(files, box.size());
+
+        // as we use concurrent producers the mails can arrive out of order
+        Set bodies = new HashSet();
+        for (int i = 0; i < files; i++) {
+            bodies.add(box.get(i).getContent());
+        }
+
+        assertEquals("There should be " + files + " unique mails", files, bodies.size());
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start").to("smtp://camel@localhost", "mock:result");
+            }
+        };
+    }
+
+
+}
