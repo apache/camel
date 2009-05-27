5a99d749f09b22da392ec162367bd9ca47b5a03d
Willem Ning Jiang <ningjiang@apache.org>
2009-05-27 02:34:06 +0000

    Added a unit test which is based on the user mailing list
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@778974 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/language/XPathFunctionTest.java
+++ camel-core/src/test/java/org/apache/camel/language/XPathFunctionTest.java
@@ -16,8 +16,12 @@
  */
 package org.apache.camel.language;
 
+import org.w3c.dom.NodeList;
+
 import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
 import org.apache.camel.component.mock.MockEndpoint;
 import static org.apache.camel.component.mock.MockEndpoint.expectsMessageCount;
 
@@ -29,6 +33,7 @@ public class XPathFunctionTest extends ContextTestSupport {
     protected MockEndpoint x;
     protected MockEndpoint y;
     protected MockEndpoint z;
+    protected MockEndpoint end;
 
     public void testCheckHeader() throws Exception {
         String body = "<one/>";
@@ -51,6 +56,21 @@ public class XPathFunctionTest extends ContextTestSupport {
 
         assertMockEndpointsSatisfied();
     }
+    
+    public void testSetXpathProperty() throws Exception {
+        String body = "<soapenv:Body xmlns:ns=\"http://myNamesapce\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+            + "<ns:Addresses> <Address>address1</Address>"
+            + " <Address>address2</Address> <Address>address3</Address>"
+            + " <Address>address4</Address> </ns:Addresses> </soapenv:Body>";
+        end.reset();
+        end.expectedMessageCount(1);
+        template.sendBody("direct:setProperty", body);
+        assertMockEndpointsSatisfied();
+        Exchange exchange = end.getExchanges().get(0);
+        NodeList nodeList = exchange.getProperty("Addresses", NodeList.class);
+        assertNotNull("The node list should not be null", nodeList);
+        
+    }
 
     protected void sendMessage(final Object headerValue, final Object body) throws Exception {
         template.sendBodyAndHeader("direct:start", body, "foo", headerValue);
@@ -63,17 +83,23 @@ public class XPathFunctionTest extends ContextTestSupport {
         x = getMockEndpoint("mock:x");
         y = getMockEndpoint("mock:y");
         z = getMockEndpoint("mock:z");
+        end = getMockEndpoint("mock:end");
     }    
     
     protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             public void configure() {
+                
                 // START SNIPPET: ex                
                 from("direct:start").choice()
                   .when().xpath("in:header('foo') = 'bar'").to("mock:x")
                   .when().xpath("in:body() = '<two/>'").to("mock:y")
                   .otherwise().to("mock:z");
                 // END SNIPPET: ex
+               
+                from("direct:setProperty")
+                    .setProperty("Addresses").xpath("//Address", NodeList.class)
+                    .to("mock:end");
             }
         };
     }
