8b59afeaaa4458b6d21d88b2edc0d22ed04d9798
Claus Ibsen <davsclaus@apache.org>
2009-05-30 06:57:54 +0000

    Fixed unit test problem on Windows reported by TC
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@780189 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/IdempotentConsumerAsyncTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/IdempotentConsumerAsyncTest.java
@@ -48,7 +48,7 @@ public class IdempotentConsumerAsyncTest extends ContextTestSupport {
         });
         context.start();
 
-        resultEndpoint.expectedBodiesReceived("one", "two", "three");
+        resultEndpoint.expectedBodiesReceivedInAnyOrder("one", "two", "three");
 
         sendMessage("1", "one");
         sendMessage("2", "two");
@@ -72,7 +72,7 @@ public class IdempotentConsumerAsyncTest extends ContextTestSupport {
                     public void process(Exchange exchange) throws Exception {
                         String id = exchange.getIn().getHeader("messageId", String.class);
                         if (id.equals("2")) {
-                            throw new IllegalArgumentException("Damm I cannot handle id 2");
+                            throw new IllegalArgumentException("Damn I cannot handle id 2");
                         }
                     }
                 }).to("mock:result");
@@ -82,7 +82,7 @@ public class IdempotentConsumerAsyncTest extends ContextTestSupport {
 
         // we send in 2 messages with id 2 that fails
         getMockEndpoint("mock:error").expectedMessageCount(2);
-        resultEndpoint.expectedBodiesReceived("one", "three");
+        resultEndpoint.expectedBodiesReceivedInAnyOrder("one", "three");
 
         sendMessage("1", "one");
         sendMessage("2", "two");
