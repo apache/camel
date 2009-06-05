cb26116c1aca652d688739107089e085753f0ac7
Claus Ibsen <davsclaus@apache.org>
2009-06-05 11:34:42 +0000

    Merged revisions 781974 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r781974 | davsclaus | 2009-06-05 12:17:52 +0200 (Fri, 05 Jun 2009) | 1 line
      
      CAMEL-1663: Added unit test with a POJO using mixed annotations from Spring and Camel. And based on idea from CAMEL-1601.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-1.x@781988 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring/src/test/java/org/apache/camel/spring/interceptor/AnnotatedBookServiceImpl.java
@@ -0,0 +1,59 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.interceptor;
+
+import javax.sql.DataSource;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Used for unit testing that we can use spring and camel annotations mixed together
+ */
+@Transactional
+@Service
+public class AnnotatedBookServiceImpl implements AnnotatedBookStore {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @EndpointInject(uri = "mock:book")
+    private ProducerTemplate template;
+
+    public void orderBook(String title) throws Exception {
+        Transactional tx = this.getClass().getAnnotation(Transactional.class);
+        if (tx == null) {
+            throw new IllegalArgumentException("Spring annotation-driven should have instrumented this class as @Transactional");
+        }
+        if (!"REQUIRED".equals(tx.propagation().name())) {
+            throw new IllegalArgumentException("Should be REQUIRED propagation");
+        }
+
+        if (title.startsWith("Donkey")) {
+            throw new IllegalArgumentException("We don't have Donkeys, only Camels");
+        }
+
+        // create new local datasource to store in DB
+        new SimpleJdbcTemplate(dataSource).update("insert into books (title) values (?)", title);
+
+        template.sendBody(title);
+    }
+}
---
+++ components/camel-spring/src/test/java/org/apache/camel/spring/interceptor/AnnotatedRoute.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.interceptor;
+
+import org.apache.camel.spring.SpringRouteBuilder;
+import org.apache.camel.spring.spi.SpringTransactionPolicy;
+
+/**
+ * @version $Revision$
+ */
+public class AnnotatedRoute extends SpringRouteBuilder {
+
+    public void configure() throws Exception {
+        SpringTransactionPolicy required = getContext().getRegistry()
+            .lookup("PROPAGATION_REQUIRED", SpringTransactionPolicy.class);
+
+        from("direct:okay").
+                policy(required).
+                setBody(constant("Tiger in Action")).beanRef("bookService").
+                setBody(constant("Elephant in Action")).beanRef("bookService");
+
+        from("direct:fail").
+                policy(required).
+                setBody(constant("Tiger in Action")).beanRef("bookService").
+                setBody(constant("Donkey in Action")).beanRef("bookService");
+    }
+
+}
