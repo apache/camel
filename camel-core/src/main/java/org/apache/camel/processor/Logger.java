98f78977be57918187686a2e3d7f33b01fe89650
Claus Ibsen <davsclaus@apache.org>
2009-06-25 12:53:24 +0000

    Added missing OFF logging level
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@788345 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/Logger.java
+++ camel-core/src/main/java/org/apache/camel/processor/Logger.java
@@ -137,6 +137,8 @@ public class Logger implements Processor {
                 log.warn(logMessage(exchange), exception);
             }
             break;
+        case OFF:
+            break;
         default:
             log.error("Unknown level: " + level + " when trying to log exchange: " + logMessage(exchange));
         }
