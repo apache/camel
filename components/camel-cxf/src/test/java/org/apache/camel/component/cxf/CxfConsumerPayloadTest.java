a269ae4e21ae4c284b4a1a470e46301800f4895f
Willem Ning Jiang <ningjiang@apache.org>
2009-06-26 13:55:44 +0000

    CAMEL-1761 added an unit test to show how to set the response for the camel-cxf PAYLOAD consumer
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@788702 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-cxf/src/test/java/org/apache/camel/component/cxf/CxfConsumerPayloadTest.java
@@ -0,0 +1,73 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.cxf;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import org.xml.sax.SAXException;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxp.XmlConverter;
+import org.apache.cxf.binding.soap.SoapHeader;
+import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.headers.Header;
+import org.apache.cxf.helpers.CastUtils;
+
+public class CxfConsumerPayloadTest extends CxfConsumerTest {
+        
+    private static final String ECHO_RESPONSE = "<ns1:echoResponse xmlns:ns1=\"http://cxf.component.camel.apache.org/\">"
+            + "<return xmlns=\"http://cxf.component.camel.apache.org/\">echo Hello World!</return>"
+            + "</ns1:echoResponse>";
+    private static final String ECHO_BOOLEAN_RESPONSE = "<ns1:echoBooleanResponse xmlns:ns1=\"http://cxf.component.camel.apache.org/\">"
+            + "<return xmlns=\"http://cxf.component.camel.apache.org/\">true</return>"
+            + "</ns1:echoBooleanResponse>";
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from(SIMPLE_ENDPOINT_URI + "&dataFormat=PAYLOAD").to("log:info").process(new Processor() {
+                    public void process(final Exchange exchange) throws Exception {                        
+                        CxfPayload<SoapHeader> requestPayload = exchange.getIn().getBody(CxfPayload.class);
+                        List<Element> inElements = requestPayload.getBody();
+                        List<Element> outElements = new ArrayList<Element>();
+                        XmlConverter converter = new XmlConverter();
+                        String documentString = ECHO_RESPONSE;
+                        if (inElements.get(0).getLocalName().equals("echoBoolean")) {
+                            documentString = ECHO_BOOLEAN_RESPONSE;
+                        }
+                        Document outDocument = converter.toDOMDocument(documentString);
+                        outElements.add(outDocument.getDocumentElement());
+                        // set the payload header with null
+                        CxfPayload<SoapHeader> responsePayload = new CxfPayload<SoapHeader>(null, outElements);
+                        exchange.getOut().setBody(responsePayload); 
+                    }
+                });
+            }
+        };
+    }
+
+}
