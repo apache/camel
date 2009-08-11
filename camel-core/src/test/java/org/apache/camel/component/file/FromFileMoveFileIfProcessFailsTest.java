2ae973cd85d3e00c755988e1585d39675d7e85b4
Moulliard Charles <cmoulliard@apache.org>
2009-08-11 08:24:16 +0000

    Add new unit test : FromFileMoveFileIfProcessFailsTest.java to test uri option : moveFailed
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@803025 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/component/file/FromFileMoveFileIfProcessFailsTest.java
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.file;
+
+import java.io.File;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+
+/**
+ * @version $Revision: 784513 $
+ */
+public class FromFileMoveFileIfProcessFailsTest extends ContextTestSupport {
+
+    private String body = "Hello World this file will NOT be moved";
+
+    @Override
+    protected void setUp() throws Exception {
+        deleteDirectory("./target/movefile");
+        super.setUp();
+    }
+
+    public void testPollFileAndShouldNotBeMoved() throws Exception {
+        template.sendBodyAndHeader("file://target/movefile", body, Exchange.FILE_NAME, "hello.txt");
+
+        MockEndpoint mock = getMockEndpoint("mock:error");
+        mock.expectedBodiesReceived(body);
+
+        mock.assertIsSatisfied();
+
+        // give time to NOT delete file
+        Thread.sleep(200);
+
+        // assert the file is moved
+        File file = new File("./target/movefile/error/hello.txt");
+        file = file.getAbsoluteFile();
+        assertTrue("The file should have been moved", file.exists());
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                errorHandler(deadLetterChannel("mock:error").maximumRedeliveries(2).redeliverDelay(0).logStackTrace(false).handled(false));
+
+                from("file://target/movefile?moveFailed=error").process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        throw new IllegalArgumentException("Forced by unittest");
+                    }
+                });
+            }
+        };
+    }
+
+
+}
