a115820be9614ac00631d84db93a1002492f4c40
Willem Ning Jiang <ningjiang@apache.org>
2009-08-27 01:32:36 +0000

    CAMEL-1934 Fixed the assert parameter issue as Christian suggested
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@808247 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jetty/src/test/java/org/apache/camel/component/jetty/HttpStreamCacheFileTest.java
+++ components/camel-jetty/src/test/java/org/apache/camel/component/jetty/HttpStreamCacheFileTest.java
@@ -51,7 +51,7 @@ public class HttpStreamCacheFileTest extends CamelTestSupport {
         // the temporary files should have been deleted
         File file = new File("./target/cachedir");
         String[] files = file.list();
-        assertEquals("There should be no files", files.length, 0);
+        assertEquals("There should be no files", 0, files.length);
     }
 
     @Test
@@ -68,7 +68,7 @@ public class HttpStreamCacheFileTest extends CamelTestSupport {
         // the temporary files should have been deleted
         File file = new File("./target/cachedir");
         String[] files = file.list();
-        assertEquals("There should be no files", files.length, 0);
+        assertEquals("There should be no files", 0, files.length);
     }
 
     @Override
