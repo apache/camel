6a9e8a7860257545e4381d1e18df58234ee6f7c6
Claus Ibsen <davsclaus@apache.org>
2009-09-18 12:21:55 +0000

    Fixing uni test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@816599 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/test/java/org/apache/camel/spring/interceptor/TransactionalClientDataSourceTransactedWithFileTest.java
+++ components/camel-spring/src/test/java/org/apache/camel/spring/interceptor/TransactionalClientDataSourceTransactedWithFileTest.java
@@ -35,7 +35,7 @@ public class TransactionalClientDataSourceTransactedWithFileTest extends Transac
         template.sendBodyAndHeader("file://target/transacted/okay", "Hello World", Exchange.FILE_NAME, "okay.txt");
 
         // wait for route to complete
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         int count = jdbc.queryForInt("select count(*) from books");
         assertEquals("Number of books", 3, count);
@@ -45,7 +45,7 @@ public class TransactionalClientDataSourceTransactedWithFileTest extends Transac
         template.sendBodyAndHeader("file://target/transacted/fail", "Hello World", Exchange.FILE_NAME, "fail.txt");
 
         // wait for route to complete
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         // should not be able to process the file so we still got 1 book as we did from the start
         int count = jdbc.queryForInt("select count(*) from books");
