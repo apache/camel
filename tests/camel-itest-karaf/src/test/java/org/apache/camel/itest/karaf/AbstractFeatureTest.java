73957372c56d60be91af39f3fd4cab9881d590e3
Guillaume Nodet <gnodet@apache.org>
2009-09-28 09:21:36 +0000

    CAMEL-2063: Add integration tests for karaf features
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@819482 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
@@ -0,0 +1,161 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.itest.karaf;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.osgi.CamelContextFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.cleanCaches;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.profile;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import org.osgi.framework.BundleContext;
+
+public abstract class AbstractFeatureTest {
+
+    protected final transient Log LOG = LogFactory.getLog(getClass());
+
+    @Inject
+    protected BundleContext bundleContext;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    protected void testComponent(String component) throws Exception {
+        long max = System.currentTimeMillis() + 10000;
+        while (true) {
+            try {
+                assertNotNull(createCamelContext().getComponent(component));
+                return;
+            } catch (Exception t) {
+                if (System.currentTimeMillis() < max) {
+                    Thread.sleep(1000);
+                } else {
+                    throw t;
+                }
+            }
+        }
+    }
+
+    protected void testDataFormat(String format) throws Exception {
+        // TODO: how to test data formats ?
+//        long max = System.currentTimeMillis() + 10000;
+//        while (true) {
+//            try {
+//                assertNotNull(createCamelContext().getDataFormats().get(format));
+//                return;
+//            } catch (Exception t) {
+//                if (System.currentTimeMillis() < max) {
+//                    Thread.sleep(1000);
+//                    continue;
+//                } else {
+//                    throw t;
+//                }
+//            }
+//        }
+    }
+
+    protected void testLanguage(String lang) throws Exception {
+        long max = System.currentTimeMillis() + 10000;
+        while (true) {
+            try {
+                assertNotNull(createCamelContext().resolveLanguage(lang));
+                return;
+            } catch (Exception t) {
+                if (System.currentTimeMillis() < max) {
+                    Thread.sleep(1000);
+                    continue;
+                } else {
+                    throw t;
+                }
+            }
+        }
+    }
+
+    protected void testLanguageResolver(String lang) throws Exception {
+        // TODO: how to test language resolver ?
+//        long max = System.currentTimeMillis() + 10000;
+//        while (true) {
+//            try {
+//                assertNotNull(createCamelContext().resolveLanguage(lang));
+//                return;
+//            } catch (Exception t) {
+//                if (System.currentTimeMillis() < max) {
+//                    Thread.sleep(1000);
+//                    continue;
+//                } else {
+//                    throw t;
+//                }
+//            }
+//        }
+    }
+
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContextFactory factory = new CamelContextFactory();
+        factory.setBundleContext(bundleContext);
+        LOG.info("Get the bundleContext is " + bundleContext);
+        return factory.createContext();
+    }
+
+    public static String extractName(Class clazz) {
+        String name = clazz.getName();
+        int id0 = name.indexOf("Camel") + "Camel".length();
+        int id1 = name.indexOf("Test");
+        StringBuilder sb = new StringBuilder();
+        for (int i = id0; i < id1; i++) {
+            char c = name.charAt(i);
+            if (Character.isUpperCase(c) && sb.length() > 0) {
+                sb.append("-");
+            }
+            sb.append(Character.toLowerCase(c));
+        }
+        return sb.toString();
+    }
+
+    public static Option[] configure(String feature) {
+        Option[] options = options(
+            profile("log").version("1.4"),
+            // this is how you set the default log level when using pax logging (logProfile)
+            org.ops4j.pax.exam.CoreOptions.systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
+
+            scanFeatures(mavenBundle().groupId("org.apache.camel.karaf").
+                         artifactId("features").versionAsInProject().type("xml/features"),
+                          "camel-osgi", "camel-" + feature),
+
+            cleanCaches(),
+
+            felix(), 
+            equinox());
+
+        return options;
+    }
+}
