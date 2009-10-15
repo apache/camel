cffe6334d78d78c6038cc703d182683b4cf1a500
Jonathan Anstey <janstey@apache.org>
2009-10-15 13:57:10 +0000

    include tracer example so it gets released properly next time
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@825497 13f79535-47bb-0310-9956-ffa450edef68

--- examples/camel-example-tracer/src/main/java/org/apache/camel/example/tracer/Main.java
+++ examples/camel-example-tracer/src/main/java/org/apache/camel/example/tracer/Main.java
@@ -25,7 +25,7 @@ public final class Main {
         // do nothing here
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         org.apache.camel.spring.Main.main(args);
     }
 
