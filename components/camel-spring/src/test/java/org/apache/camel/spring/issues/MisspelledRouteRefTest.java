c3f43d025274b6b6410b418ffdfdb19a7ef14cdb
Claus Ibsen <davsclaus@apache.org>
2009-10-16 04:13:05 +0000

    CAMEL-1517: Applied patch with thanks to Stan Lewis. Added unit test.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@825758 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring/src/test/java/org/apache/camel/spring/issues/MisspelledRouteRefTest.java
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.spring.issues;
+
+import org.apache.camel.spring.Main;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * @version $Revision$
+ */
+public class MisspelledRouteRefTest extends TestCase {
+
+    private static final transient Log LOG = LogFactory.getLog(MisspelledRouteRefTest.class);
+
+    @Test    
+    public void testApplicationContextFailed() {
+        try {
+            Main main = new Main();
+            main.setApplicationContextUri("org/apache/camel/spring/issues/MisspelledRouteRefTest.xml");
+            main.start();
+            fail("Should have thrown an exception");
+        } catch (Exception e) {
+            //expected but want to see what it looks like...
+            LOG.debug("Exception message : " + e.getMessage());
+            assertEquals("Error creating bean with name 'camel': Invocation of init method failed; nested exception is org.apache.camel.CamelException: Cannot find any routes with this RouteBuilder reference: RouteBuilderRef[xxxroute]", e.getMessage());
+        }
+    }
+}
+
