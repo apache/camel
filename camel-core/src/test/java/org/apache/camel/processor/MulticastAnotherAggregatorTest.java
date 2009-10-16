9e13f54d041dd8b252c324a831a40d155df24726
Hadrian Zbarcea <hadrian@apache.org>
2009-10-16 01:22:53 +0000

    CAMEL-2069.  Patch applied with thanks to Dave.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@825728 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/MulticastAnotherAggregatorTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/MulticastAnotherAggregatorTest.java
@@ -17,8 +17,13 @@
 package org.apache.camel.processor;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Exchange;
@@ -26,8 +31,12 @@ import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class MulticastAnotherAggregatorTest extends ContextTestSupport {
+
+    private static final Log LOG = LogFactory.getLog(MulticastAnotherAggregatorTest.class);
 
     public void testMulticastReceivesItsOwnExchangeParallelly() throws Exception {
         sendingAMessageUsingMulticastReceivesItsOwnExchange(true);
@@ -63,11 +72,77 @@ public class MulticastAnotherAggregatorTest extends ContextTestSupport {
         assertMockEndpointsSatisfied();
     }
 
+    public void testMulticastLoadParallelly() throws Exception {
+        sendLoad(true);
+    }
+
+    public void testMulticastLoadSequentially() throws Exception {
+        sendLoad(false);
+    }
+
+    public void sendLoad(boolean isParallel) throws Exception {
+        final int numMsgs = 10;
+        final int numThreads = 10;
+        final AtomicLong total = new AtomicLong(0);
+        final String url = isParallel ? "direct:parallel" : "direct:sequential";
+        
+        MockEndpoint result = getMockEndpoint("mock:result");
+        result.expectedMessageCount(numThreads * numMsgs);
+    
+        Runnable runner = new Runnable() {
+            public void run() {
+                try {
+                    Processor processor = new Processor() {
+                        public void process(Exchange exchange) {
+                            Message in = exchange.getIn();
+                            in.setBody("input");
+                            in.setHeader("foo", "bar");
+                        }
+                    };
+
+                    for (int count = 0; count < numMsgs; count++) {
+                        template.request(url, processor);
+                    }
+
+                    LOG.debug("Runner completed: " + total.incrementAndGet());
+                } catch (Exception e) {
+                    fail(e.getMessage());
+                }
+            }
+        };
+        ExecutorService executor = Executors.newCachedThreadPool();
+        
+        for (int count = 0; count < numThreads; count++) {
+            executor.execute(runner);
+        }
+        
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        }
+
+        assertMockEndpointsSatisfied();
+    }
+    
+    private class WorkQueuePolicy implements RejectedExecutionHandler {
+        public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
+            try {
+                executor.getQueue().put(runnable);
+            } catch (InterruptedException e) {
+                // should not happen
+                throw new RejectedExecutionException(e);
+            }
+        }
+    }
+    
     protected RouteBuilder createRouteBuilder() {
 
         return new RouteBuilder() {
             public void configure() {
-                ThreadPoolExecutor tpExecutor = new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(10));
+                ThreadPoolExecutor tpExecutor = new ThreadPoolExecutor(1, 10, 0, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(10));
+                tpExecutor.setRejectedExecutionHandler(new WorkQueuePolicy());
+                
+                
                 // START SNIPPET: example
                 // The message will be sent parallelly to the endpoints
                 from("direct:parallel")
@@ -85,7 +160,5 @@ public class MulticastAnotherAggregatorTest extends ContextTestSupport {
                 // END SNIPPET: example
             }
         };
-
     }
-
 }
