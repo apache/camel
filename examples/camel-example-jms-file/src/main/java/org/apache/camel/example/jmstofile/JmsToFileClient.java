f76d10c092827e0dcbb5c1f4e7e2167d56c7862c
Willem Ning Jiang <ningjiang@apache.org>
2009-11-02 10:19:41 +0000

    CAMEL-2125 added camel-context.xml to start the example from SericeMix
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@831843 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-jms-file/src/main/java/org/apache/camel/example/jmstofile/JmsToFileClient.java
@@ -0,0 +1,51 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.example.jmstofile;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public final class JmsToFileClient {
+    
+    private JmsToFileClient() {
+        //Helper class
+    }
+    
+    public static void mail(String[] args) throws Exception {
+        
+        CamelContext context = new DefaultCamelContext();
+       
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:51616");
+        
+        context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        
+        ProducerTemplate template = context.createProducerTemplate();
+        
+        context.start();
+        
+        for (int i = 0; i < 10; i++) {
+            template.sendBody("test-jms:queue:test.queue", "Test Message: " + i);
+        }
+    }
+
+}
