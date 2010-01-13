298964dc444ecebcd230db6f6547bf62c9019a95
Claus Ibsen <davsclaus@apache.org>
2010-01-13 06:15:06 +0000

    Fixed unit test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@898655 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/management/PublishEventNotifierTest.java
+++ camel-core/src/test/java/org/apache/camel/management/PublishEventNotifierTest.java
@@ -41,7 +41,7 @@ public class PublishEventNotifierTest extends ContextTestSupport {
 
     public void testExchangeDone() throws Exception {
         getMockEndpoint("mock:result").expectedMessageCount(1);
-        getMockEndpoint("mock:event").expectedMessageCount(5);
+        getMockEndpoint("mock:event").expectedMessageCount(6);
 
         template.sendBody("direct:start", "Hello World");
 
@@ -49,7 +49,7 @@ public class PublishEventNotifierTest extends ContextTestSupport {
     }
 
     public void testExchangeFailed() throws Exception {
-        getMockEndpoint("mock:event").expectedMessageCount(5);
+        getMockEndpoint("mock:event").expectedMessageCount(6);
 
         try {
             template.sendBody("direct:fail", "Hello World");
