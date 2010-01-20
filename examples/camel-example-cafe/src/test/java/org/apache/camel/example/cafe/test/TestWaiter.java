9d8cfe4fa78c8ad2da773dc19a3d29a840280fbf
Willem Ning Jiang <ningjiang@apache.org>
2010-01-20 13:30:47 +0000

    CAMEL-2383 Added Cafe Demo example
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@901190 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-cafe/src/test/java/org/apache/camel/example/cafe/test/TestWaiter.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.example.cafe.test;
+
+import java.util.List;
+
+import org.apache.camel.example.cafe.Delivery;
+import org.apache.camel.example.cafe.Drink;
+import org.apache.camel.example.cafe.stuff.Waiter;
+
+public class TestWaiter extends Waiter {
+    protected List<Drink> expectDrinks;
+    protected List<Drink> deliveredDrinks;
+    
+    public void setVerfiyDrinks(List<Drink> drinks) {
+        this.expectDrinks = drinks;
+    }
+    
+    public void deliverCafes(Delivery delivery) {
+        super.deliverCafes(delivery);
+        deliveredDrinks = delivery.getDeliveredDrinks();
+    }
+   
+    public void verifyDrinks() {
+        if (expectDrinks.size() != deliveredDrinks.size()) {
+            throw new AssertionError("Can't get expect size of drinks");
+        }
+        for (Drink drink : expectDrinks) {
+            if (!deliveredDrinks.contains(drink)) {
+                throw new AssertionError("Can't find drink " + drink + " in the deliveredDrinks");
+            }
+        }
+    }
+
+}
