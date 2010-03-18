755e4647c3bdc08cf7f1d6d411cf69c2839b953f
Stanley J. Lewis <slewis@apache.org>
2010-03-18 15:08:15 +0000

    CAMEL-2559 - HttpComponent only checks the registry for an HttpClientConfigurer, setHttpClientConfigurer no longer works.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@924849 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-http/src/test/java/org/apache/camel/component/http/HttpClientConfigurerTest.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.http;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @version $Revision: 905992 $
+ */
+public class HttpClientConfigurerTest extends CamelTestSupport {
+    protected String expectedText = "activemq";
+    
+    @Test
+    public void testHttpClientConfigurer() throws Exception {
+        HttpClientConfigurer configurer = getMandatoryEndpoint("http://www.google.com/search", HttpEndpoint.class).getHttpClientConfigurer();
+
+        assertNotNull("Client configurer should not be null!", configurer);
+
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() {
+
+                ProxyHttpClientConfigurer configurer = new ProxyHttpClientConfigurer(
+                        "proxyhost",
+                        80,
+                        "user",
+                        "password",
+                        null,
+                        null);
+
+                getContext().getComponent("http", HttpComponent.class).setHttpClientConfigurer(configurer);
+
+                from("direct:start")
+                    .to("http://www.google.com/search");
+            }
+        };
+    }
+}
