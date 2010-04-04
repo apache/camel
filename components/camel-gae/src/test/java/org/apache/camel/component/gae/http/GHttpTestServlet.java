082a298a183d0b9d7d2def9cb08e85156973bf77
Martin Krasser <krasserm@apache.org>
2010-04-04 07:29:56 +0000

    fix broken camel-gae build. I owe Claus a beer.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@930633 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-gae/src/test/java/org/apache/camel/component/gae/http/GHttpTestServlet.java
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.gae.http;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+public class GHttpTestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Set default response code
+        resp.setStatus(200);
+        // Copy headers from request to response
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            Enumeration<String> headers = req.getHeaders(headerName);
+            while (headers.hasMoreElements()) {
+                String headerValue = headers.nextElement();
+                resp.addHeader(headerName, headerValue);
+                // Set custom response code
+                // if requested by client
+                if (headerName.equals("code")) {
+                    resp.setStatus(Integer.parseInt(headerValue));
+                }
+            }
+        }
+
+        // add some special response headers
+        resp.addHeader("testUrl", req.getRequestURL().toString());
+        resp.addHeader("testMethod", req.getMethod().toString());
+        if (req.getQueryString() != null) {
+            resp.addHeader("testQuery", req.getQueryString());            
+        }
+
+        // Copy body from request to response
+        resp.getWriter().println(IOUtils.toString(req.getInputStream()));
+        resp.getWriter().flush();
+    }
+}
