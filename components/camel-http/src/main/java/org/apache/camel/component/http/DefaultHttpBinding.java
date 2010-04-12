f04442383f8c63598f88382a7d0d276e4714b360
Willem Ning Jiang <ningjiang@apache.org>
2010-04-12 08:48:40 +0000

    CAMEL-2634 merged reversion 926047 into camel-http
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@933148 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-http/src/main/java/org/apache/camel/component/http/DefaultHttpBinding.java
+++ components/camel-http/src/main/java/org/apache/camel/component/http/DefaultHttpBinding.java
@@ -139,6 +139,9 @@ public class DefaultHttpBinding implements HttpBinding {
             String contentEncoding = request.getHeader(Exchange.CONTENT_ENCODING, String.class);
             response.setHeader(Exchange.CONTENT_ENCODING, contentEncoding);
         }        
+        if (checkChunked(response, response.getExchange())) {
+            response.setHeader(Exchange.TRANSFER_ENCODING, "chunked");
+        }
     }
 
     public void doWriteExceptionResponse(Throwable exception, HttpServletResponse response) throws IOException {
@@ -189,7 +192,7 @@ public class DefaultHttpBinding implements HttpBinding {
 
     protected void doWriteDirectResponse(Message message, HttpServletResponse response, Exchange exchange) throws IOException {
         InputStream is = null;
-        if (checkChucked(message, exchange)) {
+        if (checkChunked(message, exchange)) {
             is = message.getBody(InputStream.class);
         }
         if (is != null) {
@@ -221,7 +224,7 @@ public class DefaultHttpBinding implements HttpBinding {
         }
     }
 
-    protected boolean checkChucked(Message message, Exchange exchange) {
+    protected boolean checkChunked(Message message, Exchange exchange) {
         boolean answer = true;
         if (message.getHeader(Exchange.HTTP_CHUNKED) == null) {
             // check the endpoint option
