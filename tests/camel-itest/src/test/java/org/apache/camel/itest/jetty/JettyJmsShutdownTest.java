891688d8c0302d0447578d03ad1e9462eef5f06a
Claus Ibsen <davsclaus@apache.org>
2010-04-19 18:06:29 +0000

    Added unit test based on user forum issue.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@935691 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/jetty/JettyJmsShutdownTest.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.itest.jetty;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.Assert;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration
+public class JettyJmsShutdownTest extends AbstractJUnit4SpringContextTests {
+
+    @Autowired
+    protected CamelContext camelContext;
+
+    @Autowired
+    protected ProducerTemplate template;
+
+    @Test
+    public void testShutdown() throws Exception {
+        Future<String> reply1 = template.asyncRequestBody("jetty:http://localhost:9000/test", "World", String.class);
+        Future<String> reply2 = template.asyncRequestBody("jetty:http://localhost:9000/test", "Camel", String.class);
+        Future<String> reply3 = template.asyncRequestBody("jetty:http://localhost:9000/test", "Tiger", String.class);
+
+        Thread.sleep(1000);
+
+        // shutdown while in progress
+        camelContext.stop();
+
+        Assert.assertEquals("Bye World", reply1.get(5, TimeUnit.SECONDS));
+        Assert.assertEquals("Bye Camel", reply2.get(5, TimeUnit.SECONDS));
+        Assert.assertEquals("Bye Tiger", reply3.get(5, TimeUnit.SECONDS));
+    }
+
+}
