ac38daa8ece6fd685f206c1d9df577b80afe4b5d
Willem Ning Jiang <ningjiang@apache.org>
2010-04-29 08:25:58 +0000

    Fixed the an unit test error in camel-mina
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@939227 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-mina/src/test/java/org/apache/camel/component/mina/MinaVMFileTcpTest.java
+++ components/camel-mina/src/test/java/org/apache/camel/component/mina/MinaVMFileTcpTest.java
@@ -17,6 +17,8 @@
 package org.apache.camel.component.mina;
 
 import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 
@@ -27,7 +29,7 @@ public class MinaVMFileTcpTest extends ContextTestSupport {
 
     public void testMinaRoute() throws Exception {
         MockEndpoint endpoint = getMockEndpoint("mock:results");
-        endpoint.expectedMessageCount(1);
+        endpoint.expectedMessageCount(2);
         endpoint.message(0).body().startsWith("Hello World");
 
         assertMockEndpointsSatisfied();
@@ -39,8 +41,9 @@ public class MinaVMFileTcpTest extends ContextTestSupport {
                 // lets setup a server
                 from("mina:vm://localhost:9123?sync=false&textline=true").to("mock:results");
 
-                from("file:src/test/data?noop=true").
-                        to("mina:vm://localhost:9123?sync=false&textline=true");
+                from("file:src/test/data?noop=true&delay=2000")
+                    .to("mina:vm://localhost:9123?sync=false&textline=true");
+                    
             }
         };
     }
