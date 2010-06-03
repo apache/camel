21d599c47f168ac3b0b6fdc0c3d9c3d8b9aaf51a
Claus Ibsen <davsclaus@apache.org>
2010-06-03 09:45:42 +0000

    CAMEL-2787: Using <package> scan now detcts @Component annotated RouteBuilder and includes them as well.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@950925 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/main/java/org/apache/camel/spring/RouteBuilderFinder.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/RouteBuilderFinder.java
@@ -22,9 +22,14 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spi.PackageScanClassResolver;
+import org.apache.camel.util.ObjectHelper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 /**
  * A helper class which will find all {@link org.apache.camel.builder.RouteBuilder} instances on the classpath
@@ -32,6 +37,7 @@ import org.springframework.context.ApplicationContext;
  * @version $Revision$
  */
 public class RouteBuilderFinder {
+    private static final transient Log LOG = LogFactory.getLog(RouteBuilderFinder.class);
     private final SpringCamelContext camelContext;
     private final String[] packages;
     private PackageScanClassResolver resolver;
@@ -55,22 +61,87 @@ public class RouteBuilderFinder {
     public void appendBuilders(List<RoutesBuilder> list) throws IllegalAccessException, InstantiationException {
         Set<Class<?>> classes = resolver.findImplementations(RoutesBuilder.class, packages);
         for (Class aClass : classes) {
-            if (shouldIgnoreBean(aClass)) {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("Found RouteBuilder class: " + aClass);
+            }
+
+            // check whether the class has already been instantiate by Spring as it was @Component annotated
+            // and its already enlisted in the Spring registry
+            RouteBuilder existing = isRouteBuilderAlreadyRegisteredByComponentAnnotation(aClass);
+            if (existing != null) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Adding existing @Component annotated RouteBuilder: " + aClass);
+                }
+                list.add(existing);
                 continue;
             }
-            if (isValidClass(aClass)) {
-                RoutesBuilder builder = instantiateBuilder(aClass);
-                if (beanPostProcessor != null) {
-                    // Inject the annotated resource
-                    beanPostProcessor.postProcessBeforeInitialization(builder, builder.toString());
+            // certain beans should be ignored
+            if (shouldIgnoreBean(aClass)) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Ignoring RouteBuilder class: " + aClass);
                 }
-                list.add(builder);
+                continue;
             }
+
+            if (!isValidClass(aClass)) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Ignoring invalid RouteBuilder class: " + aClass);
+                }
+                continue;
+            }
+
+            // type is valid so create and instantiate the builder
+            RoutesBuilder builder = instantiateBuilder(aClass);
+            if (beanPostProcessor != null) {
+                // Inject the annotated resource
+                beanPostProcessor.postProcessBeforeInitialization(builder, builder.toString());
+            }
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Adding instantiated RouteBuilder: " + builder);
+            }
+            list.add(builder);
         }
     }
 
     /**
-     * Lets ignore beans that are not explicitly configured in the spring.xml
+     * Lookup if the given class has already been enlisted in Spring registry since the class
+     * has been @Component annotated.
+     * 
+     * @param type  the class type
+     * @return the existing route builder instance, or <tt>null</tt> if none existed
+     */
+    protected RouteBuilder isRouteBuilderAlreadyRegisteredByComponentAnnotation(Class<?> type) {
+        Component ann = type.getAnnotation(Component.class);
+        if (ann != null) {
+            String id = ann.value();
+            if (ObjectHelper.isEmpty(id)) {
+                // no explicit id set, so Spring auto assigns the id, so lets try to find that id then
+                String[] names = applicationContext.getBeanNamesForType(type, true, true);
+                if (names != null && names.length == 1) {
+                    id = names[0];
+                }
+            }
+            if (ObjectHelper.isNotEmpty(id)) {
+                boolean match = applicationContext.isTypeMatch(id, type);
+                if (LOG.isTraceEnabled()) {
+                    LOG.trace("Is there already a RouteBuilder registering in Spring with id " + id + ": " + match);
+                }
+                if (match) {
+                    Object existing = applicationContext.getBean(id, type);
+                    if (existing instanceof RouteBuilder) {
+                        if (LOG.isDebugEnabled()) {
+                            LOG.debug("Found existing @Component annotated RouteBuilder with id " + id);
+                        }
+                        return RouteBuilder.class.cast(existing);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Lets ignore beans that are explicitly configured in the Spring XML files
      */
     protected boolean shouldIgnoreBean(Class<?> type) {
         Map beans = applicationContext.getBeansOfType(type, true, true);
