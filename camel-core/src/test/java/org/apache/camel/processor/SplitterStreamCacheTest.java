62dae92608f62248e7f0334d228e2032fc5d0f74
Claus Ibsen <davsclaus@apache.org>
2010-06-20 07:31:23 +0000

    Fixed test on other platforms.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@956322 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/SplitterStreamCacheTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/SplitterStreamCacheTest.java
@@ -31,7 +31,7 @@ import org.apache.camel.converter.stream.CachedOutputStream;
 public class SplitterStreamCacheTest extends ContextTestSupport {
 
     private static final String TEST_FILE = "org/apache/camel/converter/stream/test.xml";
-    protected int numMessages = 1000;
+    protected int numMessages = 200;
     
     public void testSendStreamSource() throws Exception {
         MockEndpoint resultEndpoint = getMockEndpoint("mock:result");
@@ -50,7 +50,10 @@ public class SplitterStreamCacheTest extends ContextTestSupport {
                 //ensure stream is spooled to disk
                 getContext().getProperties().put(CachedOutputStream.TEMP_DIR, "target/tmp");
                 getContext().getProperties().put(CachedOutputStream.THRESHOLD, "1");
-                from("seda:parallel?size=1000&concurrentConsumers=5").streamCaching().split(XPathBuilder.xpath("//person/city")).to("mock:result");
+
+                from("seda:parallel?concurrentConsumers=5").streamCaching()
+                    .split(XPathBuilder.xpath("//person/city"))
+                        .to("mock:result");
             }
         };
     }
