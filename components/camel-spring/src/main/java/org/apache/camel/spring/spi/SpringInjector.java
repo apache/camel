28f66690ee476b7b5c84e4fec96ba52c91fe0d2a
Claus Ibsen <davsclaus@apache.org>
2010-08-12 12:50:04 +0000

    Added test based on spring 3 dependency injection issue
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@984748 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/main/java/org/apache/camel/spring/spi/SpringInjector.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/spi/SpringInjector.java
@@ -66,7 +66,6 @@ public class SpringInjector implements Injector {
         this.dependencyCheck = dependencyCheck;
     }
 
-
     public ConfigurableApplicationContext getApplicationContext() {
         return applicationContext;
     }
