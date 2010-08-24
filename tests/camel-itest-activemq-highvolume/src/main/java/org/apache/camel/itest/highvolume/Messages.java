29752839cbd3ce1f29a72d1dc0986981a528fd08
Moulliard Charles <cmoulliard@apache.org>
2010-08-24 12:17:33 +0000

    Add a new project to simulate high-volume (production & consumption) using camel routes + activemq.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@988501 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest-activemq-highvolume/src/main/java/org/apache/camel/itest/highvolume/Messages.java
@@ -0,0 +1,34 @@
+package org.apache.camel.itest.highvolume;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Handler;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Messages {
+	
+	@EndpointInject(ref="Direct")
+    ProducerTemplate template;
+	
+	@EndpointInject(ref="JmsQueueProducer")
+    ProducerTemplate templateActiveMq;
+	
+	public void split(Exchange exchange) {
+		String messages = (String) exchange.getIn().getBody();
+		
+		String[] result = messages.split(",");
+		for (int x=0; x<result.length; x++) {
+			templateActiveMq.sendBody(result[x]);
+		}
+	}
+    
+    public void generate(Exchange exchange) {
+     	StringBuilder messages = new StringBuilder();
+        for (int i = 1; i < 1000; i++) {
+        	messages.append("Test Message: " + i + ",");
+        }
+        template.sendBody(messages.toString());
+    }
+
+}
