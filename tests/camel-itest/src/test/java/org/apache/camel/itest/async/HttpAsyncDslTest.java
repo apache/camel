e8b25311b3f726d57eb357ecdbb8bff257f3ef9e
Claus Ibsen <davsclaus@apache.org>
2010-09-02 15:34:00 +0000

    Fixed test on other boxes.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@991984 13f79535-47bb-0310-9956-ffa450edef68

--- tests/camel-itest/src/test/java/org/apache/camel/itest/async/HttpAsyncDslTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/async/HttpAsyncDslTest.java
@@ -111,8 +111,8 @@ public class HttpAsyncDslTest extends CamelTestSupport {
                     // if the JMS ReplyTo was set then we expect a reply, otherwise not
                     // use a pool of 20 threads for the point forward
                     .threads(20)
-                    // do some CPU heavy processing of the message (we simulate and delay just 100 ms)
-                    .unmarshal(mySecureDataFormat).delay(100).to("bean:handleOrder").to("mock:order");
+                    // do some CPU heavy processing of the message (we simulate and delay just 500 ms)
+                    .unmarshal(mySecureDataFormat).delay(500).to("bean:handleOrder").to("mock:order");
                 // END SNIPPET: e1
             }
         };
