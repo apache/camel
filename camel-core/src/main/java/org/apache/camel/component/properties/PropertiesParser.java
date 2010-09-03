61679b3a275a90be79d8e8994b7602bbc5428ecd
Ashwin J. Karpe <akarpe@apache.org>
2010-09-03 14:32:31 +0000

    CAMEL-2791 Removed unnecessary import of DefaultCamelContext in PropertiesParser
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@992310 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/component/properties/PropertiesParser.java
+++ camel-core/src/main/java/org/apache/camel/component/properties/PropertiesParser.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
