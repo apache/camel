14d741f40999617f02a2cf1bcb9bbd5daeeefd10
Claus Ibsen <davsclaus@apache.org>
2010-09-14 11:59:53 +0000

    CAMEL-3121: Added unit test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@996857 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/processor/SplitterStreamingUoWIssueTest.java
@@ -0,0 +1,67 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.processor;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * @version $Revision$
+ */
+public class SplitterStreamingUoWIssueTest extends ContextTestSupport {
+
+    @Override
+    protected void setUp() throws Exception {
+        deleteDirectory("target/splitter");
+        super.setUp();
+    }
+
+    public void testSplitterStreamingUoWIssue() throws Exception {
+        getMockEndpoint("mock:foo").expectedBodiesReceived("A", "B", "C", "D", "E");
+        getMockEndpoint("mock:result").expectedBodiesReceived("A,B,C,D,E");
+
+        template.sendBodyAndHeader("file:target/splitter", "A,B,C,D,E", Exchange.FILE_NAME, "splitme.txt");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:target/splitter?delete=true")
+                    .split(body().tokenize(","))
+                        .to("seda:queue")
+                        .process(new Processor() {
+                            public void process(Exchange exchange) throws Exception {
+                                Thread.sleep(500);
+                            }
+                        })
+                    .end()
+                    .log("End of file ${file:name}")
+                    .to("mock:result");
+
+                from("seda:queue")
+                    .log("Token: ${body}")
+                    .to("mock:foo");
+            }
+        };
+    }
+}
