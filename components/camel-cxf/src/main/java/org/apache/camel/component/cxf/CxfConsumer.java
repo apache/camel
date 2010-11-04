94fab9de2494ed9c421d1ff20e56ac8a9cc0394f
Willem Ning Jiang <ningjiang@apache.org>
2010-11-04 07:52:16 +0000

    CAMEL-3308 upgrade the cxf version to 2.3.0
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1030848 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfConsumer.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfConsumer.java
@@ -75,9 +75,16 @@ public class CxfConsumer extends DefaultConsumer {
                 synchronized (continuation) {
                     if (continuation.isNew()) {
                         final org.apache.camel.Exchange camelExchange = perpareCamelExchange(cxfExchange);
+                        
+                        // Now we don't set up the timeout value
+                        if (LOG.isTraceEnabled()) {
+                            LOG.trace("Suspending continuation of exchangeId: " + camelExchange.getExchangeId());
+                        }
+                        // The continuation could be called before the suspend is called
+                        continuation.suspend(0);
 
                         // use the asynchronous API to process the exchange
-                        boolean sync = getAsyncProcessor().process(camelExchange, new AsyncCallback() {
+                        getAsyncProcessor().process(camelExchange, new AsyncCallback() {
                             public void done(boolean doneSync) {
                                 // make sure the continuation resume will not be called before the suspend method in other thread
                                 synchronized (continuation) {
@@ -91,28 +98,7 @@ public class CxfConsumer extends DefaultConsumer {
                                 }
                             }
                         });
-                        // just need to avoid the continuation.resume is called
-                        // before the continuation.suspend is called
-                        if (continuation.getObject() != camelExchange && !sync) {
-                            // Now we don't set up the timeout value
-                            if (LOG.isTraceEnabled()) {
-                                LOG.trace("Suspending continuation of exchangeId: "
-                                          + camelExchange.getExchangeId());
-                            }
-                            // The continuation could be called before the
-                            // suspend
-                            // is called
-                            continuation.suspend(0);
-                        } else {
-                            // just set the response back, as the invoking
-                            // thread is
-                            // not changed
-                            if (LOG.isTraceEnabled()) {
-                                LOG.trace("Processed the Exchange : " + camelExchange.getExchangeId());
-                            }
-                            setResponseBack(cxfExchange, camelExchange);
-                        }
-
+                        
                     }
                     if (continuation.isResumed()) {
                         org.apache.camel.Exchange camelExchange = (org.apache.camel.Exchange)continuation
