67a73ab99b82d6468a6738a8c7356d37d6b68dc5
Hadrian Zbarcea <hadrian@apache.org>
2010-11-05 03:09:40 +0000

    fix logic issue that confuses checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1031412 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-core-osgi/src/test/java/org/apache/camel/core/osgi/CamelMockBundle.java
+++ components/camel-core-osgi/src/test/java/org/apache/camel/core/osgi/CamelMockBundle.java
@@ -110,11 +110,6 @@ public class CamelMockBundle extends MockBundle {
     }
 
     protected boolean isLoadableClass(String name) {
-        if (name.startsWith("org.apache.camel.core.osgi.other")) {
-            return false;
-        } else {
-            return true;
-        }
+        return !name.startsWith("org.apache.camel.core.osgi.other");
     }
-
 }
