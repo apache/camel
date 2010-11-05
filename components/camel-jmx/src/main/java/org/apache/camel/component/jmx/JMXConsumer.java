2409243b78edebbdc1bbdeec9fa2f66ab9e2c75b
Claus Ibsen <davsclaus@apache.org>
2010-11-05 07:40:01 +0000

    CAMEL-3236: New camel-jmx component. Thanks to Mark Ford for donating to Apache.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1031461 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-jmx/src/main/java/org/apache/camel/component/jmx/JMXConsumer.java
@@ -0,0 +1,136 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jmx;
+
+import java.lang.management.ManagementFactory;
+import java.util.Collections;
+import java.util.Map;
+import javax.management.MBeanServerConnection;
+import javax.management.Notification;
+import javax.management.NotificationFilter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultConsumer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Consumer will add itself as a NotificationListener on the object
+ * specified by the objectName param.
+ */
+public class JMXConsumer extends DefaultConsumer implements NotificationListener {
+    private static final Log LOG = LogFactory.getLog(JMXConsumer.class);
+
+    /**
+     * connection to the mbean server (local or remote)
+     */
+    private MBeanServerConnection mServerConnection;
+
+    /**
+     * used to format Notification objects as xml
+     */
+    private NotificationXmlFormatter mFormatter;
+
+    public JMXConsumer(JMXEndpoint aEndpoint, Processor aProcessor) {
+        super(aEndpoint, aProcessor);
+        mFormatter = new NotificationXmlFormatter();
+    }
+
+    /**
+     * Initializes the mbean server connection and starts listening for
+     * Notification events from the object.
+     */
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+
+        JMXEndpoint ep = (JMXEndpoint) getEndpoint();
+
+        // connect to the mbean server
+        if (ep.isPlatformServer()) {
+            setServerConnection(ManagementFactory.getPlatformMBeanServer());
+        } else {
+            JMXServiceURL url = new JMXServiceURL(ep.getServerURL());
+            String[] creds = {ep.getUser(), ep.getPassword()};
+            Map map = Collections.singletonMap(JMXConnector.CREDENTIALS, creds);
+            JMXConnector connector = JMXConnectorFactory.connect(url, map);
+            setServerConnection(connector.getMBeanServerConnection());
+        }
+        // subscribe
+        NotificationFilter nf = ep.getNotificationFilter();
+
+        ObjectName objectName = ep.getJMXObjectName();
+
+        getServerConnection().addNotificationListener(objectName, this, nf, ep.getHandback());
+    }
+
+    /**
+     * Removes the notification listener
+     */
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+        JMXEndpoint ep = (JMXEndpoint) getEndpoint();
+        getServerConnection().removeNotificationListener(ep.getJMXObjectName(), this);
+    }
+
+    protected MBeanServerConnection getServerConnection() {
+        return mServerConnection;
+    }
+
+    protected void setServerConnection(MBeanServerConnection aServerConnection) {
+        mServerConnection = aServerConnection;
+    }
+
+    /**
+     * Processes the Notification received. The handback will be set as
+     * the header "jmx.handback" while the Notification will be set as
+     * the body.
+     * <p/>
+     * If the format is set to "xml" then the Notification will be converted
+     * to XML first using {@link NotificationXmlFormatter}
+     *
+     * @see javax.management.NotificationListener#handleNotification(javax.management.Notification, java.lang.Object)
+     */
+    public void handleNotification(Notification aNotification, Object aHandback) {
+        JMXEndpoint ep = (JMXEndpoint) getEndpoint();
+        Exchange exchange = getEndpoint().createExchange(ExchangePattern.InOnly);
+        Message message = exchange.getIn();
+        message.setHeader("jmx.handback", aHandback);
+        try {
+            if (ep.isXML()) {
+                message.setBody(mFormatter.format(aNotification));
+            } else {
+                message.setBody(aNotification);
+            }
+            getProcessor().process(exchange);
+        } catch (NotificationFormatException e) {
+            getExceptionHandler().handleException("Failed to marshal notification", e);
+        } catch (Exception e) {
+            getExceptionHandler().handleException("Failed to process notification", e);
+        }
+    }
+
+}
---
+++ components/camel-jmx/src/test/java/org/apache/camel/component/jmx/JMXNotificationFilterTest.java
@@ -0,0 +1,90 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jmx;
+
+import java.util.LinkedHashSet;
+import javax.management.Notification;
+import javax.management.NotificationFilter;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.component.jmx.beans.ISimpleMXBean;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests that the NotificationFilter is applied if configured
+ */
+public class JMXNotificationFilterTest extends SimpleBeanFixture {
+
+    /**
+     * we'll track the rejected messages so we know what got filtered
+     */
+    private LinkedHashSet<Notification> mRejected = new LinkedHashSet<Notification>();
+
+    @Test
+    public void testNotificationFilter() throws Exception {
+        ISimpleMXBean bean = getSimpleMXBean();
+
+        assertEquals("no notifications should have been filtered at this point", 0, mRejected.size());
+
+        // we should only get 5 messages, which is 1/2 the number of times we touched the object.
+        // The 1/2 is due to the behavior of the test NotificationFilter implemented below 
+        getMockEndpoint().setExpectedMessageCount(5);
+        for (int i = 0; i < 10; i++) {
+            bean.touch();
+        }
+
+        waitForMessages();
+        assertEquals("5 notifications should have been filtered", 5, mRejected.size());
+
+        // assert that all of the rejected ones are odd and accepted ones even
+        for (Notification rejected : mRejected) {
+            assertEquals(1, rejected.getSequenceNumber() % 2);
+        }
+
+        for (Exchange received : getMockEndpoint().getReceivedExchanges()) {
+            Notification n = (Notification) received.getIn().getBody();
+            assertEquals(0, n.getSequenceNumber() % 2);
+        }
+    }
+
+    @Override
+    protected JMXUriBuilder buildFromURI() {
+        // use the raw format so we can we can get the Notification and assert on the sequence
+        return super.buildFromURI().withNotificationFilter("#myFilter").withFormat("raw");
+    }
+
+    @SuppressWarnings("serial")
+    @Override
+    protected void initRegistry() {
+        super.initRegistry();
+
+        // initialize the registry with our filter
+        getRegistry().put("myFilter", new NotificationFilter() {
+            public boolean isNotificationEnabled(Notification aNotification) {
+                // only accept even notifications
+                boolean enabled = aNotification.getSequenceNumber() % 2 == 0;
+                if (!enabled) {
+                    mRejected.add(aNotification);
+                }
+                return enabled;
+            }
+        });
+    }
+
+}
