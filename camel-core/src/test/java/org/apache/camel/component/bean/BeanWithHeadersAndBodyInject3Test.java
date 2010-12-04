52091c543b3facdf93bf7daafdfb7ea16974c8ad
Aaron Mulder <ammulder@apache.org>
2010-12-04 22:30:19 +0000

    Add a test that shows a weird bean annotation failure mode
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1042252 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/component/bean/BeanWithHeadersAndBodyInject3Test.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.bean;
+
+import java.util.Map;
+import javax.naming.Context;
+import org.apache.camel.Body;
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Headers;
+import org.apache.camel.OutHeaders;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.util.jndi.JndiContext;
+
+/**
+ * @version $Revision$
+ */
+public class BeanWithHeadersAndBodyInject3Test extends ContextTestSupport {
+    private MyBean myBean = new MyBean();
+
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:start").to("bean:myBean?method=doSomething").to("mock:finish");
+            }
+        };
+    }
+
+    public void testAnnotations() throws Exception {
+        MockEndpoint end = getMockEndpoint("mock:finish");
+        end.setExpectedMessageCount(1);
+        sendBody("direct:start", "Test Input");
+        end.assertIsSatisfied();
+        assertNotNull(end.getExchanges().get(0).getIn().getBody());
+        assertEquals("Hello!", end.getExchanges().get(0).getIn().getBody());
+    }
+
+    @Override
+    protected Context createJndiContext() throws Exception {
+        JndiContext answer = new JndiContext();
+        answer.bind("myBean", myBean);
+        return answer;
+    }
+
+    public static class MyBean {
+        public String doSomething(@Body String body, @Headers Map headers, @OutHeaders Map outHeaders) {
+            return "Hello!";
+        }
+    }
+}
