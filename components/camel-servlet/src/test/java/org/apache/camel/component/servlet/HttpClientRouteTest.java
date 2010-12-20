09732deb3c73ccf1f6c09d8b585055de8c6f8d67
Willem Ning Jiang <ningjiang@apache.org>
2010-12-20 04:56:28 +0000

    CAMEL-3443 Added test on camel-servlet
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1050990 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-servlet/src/test/java/org/apache/camel/component/servlet/HttpClientRouteTest.java
+++ components/camel-servlet/src/test/java/org/apache/camel/component/servlet/HttpClientRouteTest.java
@@ -78,8 +78,11 @@ public class HttpClientRouteTest extends ServletCamelRouterTestSupport {
                     String contentType = exchange.getIn().getHeader(Exchange.CONTENT_TYPE, String.class);
                     String path = exchange.getIn().getHeader(Exchange.HTTP_PATH, String.class);
                     assertEquals("Get a wrong content type", CONTENT_TYPE, contentType);
+                    // assert camel http header
                     String charsetEncoding = exchange.getIn().getHeader(Exchange.HTTP_CHARACTER_ENCODING, String.class);
-                    assertEquals("Get a wrong charset name", "UTF-8", charsetEncoding);
+                    assertEquals("Get a wrong charset name from the message heaer", "UTF-8", charsetEncoding);
+                    // assert exchange charset
+                    assertEquals("Get a wrong charset naem from the exchange property", "UTF-8", exchange.getProperty(Exchange.CHARSET_NAME));
                     exchange.getOut().setHeader(Exchange.CONTENT_TYPE, contentType + "; charset=UTF-8");                        
                     exchange.getOut().setHeader("PATH", path);
                     exchange.getOut().setBody("<b>Hello World</b>");
