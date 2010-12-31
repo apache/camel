3360b43d1513e0ee013010f3046af4f67c8abf7f
Claus Ibsen <davsclaus@apache.org>
2010-12-31 10:00:22 +0000

    CAMEL-1397: Added some basic route statistics to web console. Thanks to Ben for patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1054080 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-web/src/main/java/org/apache/camel/web/model/Route.java
+++ components/camel-web/src/main/java/org/apache/camel/web/model/Route.java
@@ -20,6 +20,8 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.ServiceStatus;
 import org.apache.camel.model.DescriptionDefinition;
 import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.web.util.JMXRouteStatistics;
+import org.apache.camel.web.util.RouteStatistics;
 
 /**
  * Represents a route.
@@ -29,6 +31,7 @@ import org.apache.camel.model.RouteDefinition;
 public class Route {
     private CamelContext camelContext;
     private RouteDefinition route;
+    private final RouteStatistics statistics = new JMXRouteStatistics();
 
     public Route() {
     }
@@ -65,5 +68,14 @@ public class Route {
 
     public boolean isStoppable() {
         return route.isStoppable(camelContext);
+    }
+    
+    public Object getStatistic(String attribute) {
+        Object answer = statistics.getRouteStatistic(camelContext, route.getId(), attribute);
+        if (answer == null) {
+            // we don't want null on web pages so we return an empty string
+            return "";
+        }
+        return answer;
     }
 }
