deae2573fe118424deeb3f0092bbbe2c8ea9e02f
Claus Ibsen <davsclaus@apache.org>
2011-02-09 14:11:26 +0000

    CAMEL-3646: Added rmi example.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1068899 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/camel-example-osgi-rmi/src/test/java/org/apache/camel/example/osgi/RmiTest.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example.osgi;
+
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Unit test the example.
+ *
+ * @version $Revision$
+ */
+public class RmiTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/camelContext.xml");
+    }
+
+    @Test
+    public void testRmi() throws Exception {
+        String out = template.requestBody("rmi://localhost:37541/helloServiceBean", "Camel", String.class);
+        assertEquals("Hello Camel", out);
+    }
+
+}
