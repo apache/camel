304e352d0d25959c98ac28a21db4a421475c210d
Claus Ibsen <davsclaus@apache.org>
2011-02-24 05:24:12 +0000

    CAMEL-3712: Route preparation logic is now fully reused for all DSLs (Java, XML etc.)
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1074047 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/model/RoutesDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/RoutesDefinition.java
@@ -198,50 +198,12 @@ public class RoutesDefinition extends OptionalIdentifiedDefinition<RoutesDefinit
      * @return the builder
      */
     public RouteDefinition route(RouteDefinition route) {
-
-        // configure intercept
-        for (InterceptDefinition intercept : getIntercepts()) {
-            // add as first output so intercept is handled before the actual route and that gives
-            // us the needed head start to init and be able to intercept all the remaining processing steps
-            route.getOutputs().add(0, intercept);
-        }
-
-        // configure intercept from
-        for (InterceptFromDefinition intercept : getInterceptFroms()) {
-
-            // should we only apply interceptor for a given endpoint uri
-            boolean match = true;
-            if (intercept.getUri() != null) {
-                match = false;
-                for (FromDefinition input : route.getInputs()) {
-                    if (EndpointHelper.matchEndpoint(input.getUri(), intercept.getUri())) {
-                        match = true;
-                        break;
-                    }
-                }
-            }
-
-            if (match) {
-                // add as first output so intercept is handled before the acutal route and that gives
-                // us the needed head start to init and be able to intercept all the remaining processing steps
-                route.getOutputs().add(0, intercept);
-            }
-        }
-
-        // configure intercept send to endpoint
-        for (InterceptSendToEndpointDefinition sendTo : getInterceptSendTos()) {
-            // add as first output so intercept is handled before the actual route and that gives
-            // us the needed head start to init and be able to intercept all the remaining processing steps
-            route.getOutputs().add(0, sendTo);
-        }
-
-        // add on completions after the interceptors
-        route.getOutputs().addAll(getOnCompletions());
-
-        // add on exceptions at top since we need to inject this by the error handlers
-        route.getOutputs().addAll(0, getOnExceptions());
-
+        // must prepare the route before we can add it to the routes list
+        RouteDefinitionHelper.prepareRoute(route, getOnExceptions(), getIntercepts(), getInterceptFroms(),
+                getInterceptSendTos(), getOnCompletions());
         getRoutes().add(route);
+        // mark this route as prepared
+        route.markPrepared();
         return route;
     }
 
