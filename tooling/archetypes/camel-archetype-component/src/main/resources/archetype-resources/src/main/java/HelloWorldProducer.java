3c638034a33e811bbe141d3bef5ffac5f8da3cd2
Claus Ibsen <davsclaus@apache.org>
2011-03-03 14:59:43 +0000

    CAMEL-3658: Migrated the component archetype to the new format. And fixed so it works.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1076644 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/archetypes/camel-archetype-component/src/main/resources/archetype-resources/src/main/java/HelloWorldProducer.java
+++ tooling/archetypes/camel-archetype-component/src/main/resources/archetype-resources/src/main/java/HelloWorldProducer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ${packageName};
+package ${package};
 
 import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultConsumer;
