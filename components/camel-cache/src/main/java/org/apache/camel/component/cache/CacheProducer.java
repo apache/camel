55fb8b3008f3a0514ad28982f884d5d20fba9fe2
Claus Ibsen <davsclaus@apache.org>
2011-04-16 12:24:23 +0000

    CAMEL-3862: Fixed camel-cache to replicate cache across nodes using listeners. Thanks to Piotr for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1093978 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-cache/src/main/java/org/apache/camel/component/cache/CacheProducer.java
+++ components/camel-cache/src/main/java/org/apache/camel/component/cache/CacheProducer.java
@@ -102,13 +102,13 @@ public class CacheProducer extends DefaultProducer {
                 LOG.debug("Adding an element with key " + key + " into the Cache");
             }
             element = createElementFromBody(exchange, CacheConstants.CACHE_OPERATION_ADD);
-            cache.put(new Element(key, element), true);
+            cache.put(new Element(key, element));
         } else if (operation.equalsIgnoreCase(CacheConstants.CACHE_OPERATION_UPDATE)) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Updating an element with key " + key + " into the Cache");
             }
             element = createElementFromBody(exchange, CacheConstants.CACHE_OPERATION_UPDATE);
-            cache.put(new Element(key, element), true);
+            cache.put(new Element(key, element));
         } else if (operation.equalsIgnoreCase(CacheConstants.CACHE_OPERATION_DELETEALL)) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Deleting All elements from the Cache");
@@ -118,7 +118,7 @@ public class CacheProducer extends DefaultProducer {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Deleting an element with key " + key + " into the Cache");
             }
-            cache.remove(key, true);
+            cache.remove(key);
         } else if (operation.equalsIgnoreCase(CacheConstants.CACHE_OPERATION_GET)) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Quering an element with key " + key + " from the Cache");
