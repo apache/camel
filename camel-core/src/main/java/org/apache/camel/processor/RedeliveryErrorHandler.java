b9094cb596d03ffbb6f875cee6a00c276b6f82fc
Claus Ibsen <davsclaus@apache.org>
2011-04-17 14:27:09 +0000

    CAMEL-3878: Stopping a route doesnt stop error handlers as they could potentialy be shared by other routes.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1094147 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/RedeliveryErrorHandler.java
+++ camel-core/src/main/java/org/apache/camel/processor/RedeliveryErrorHandler.java
@@ -840,7 +840,13 @@ public abstract class RedeliveryErrorHandler extends ErrorHandlerSupport impleme
 
     @Override
     protected void doStop() throws Exception {
-        ServiceHelper.stopServices(deadLetter, output, outputAsync);
+        // noop, do not stop any services which we only do when shutting down
+        // as the error handler can be context scoped, and should not stop in case
+        // a route stops
     }
 
+    @Override
+    protected void doShutdown() throws Exception {
+        ServiceHelper.stopServices(deadLetter, output, outputAsync);
+    }
 }
