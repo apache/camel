ad08e033be267c58f6b604a6ef7c5ec0b8787405
Claus Ibsen <davsclaus@apache.org>
2011-05-07 10:34:13 +0000

    CAMEL-3032: Do not use test-jar in pom.xml.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1100505 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jing/src/test/java/org/apache/camel/component/validator/jing/RNGRouteTest.java
+++ components/camel-jing/src/test/java/org/apache/camel/component/validator/jing/RNGRouteTest.java
@@ -16,15 +16,41 @@
  */
 package org.apache.camel.component.validator.jing;
 
-import org.apache.camel.component.validator.ValidatorRouteTest;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @version 
  */
-public class RNGRouteTest extends ValidatorRouteTest {
+public class RNGRouteTest extends CamelSpringTestSupport {
 
     protected ClassPathXmlApplicationContext createApplicationContext() {
         return new ClassPathXmlApplicationContext("org/apache/camel/component/validator/jing/rng-context.xml");
     }
+
+    @Test
+    public void testValidMessage() throws Exception {
+        getMockEndpoint("mock:valid").expectedMessageCount(1);
+        getMockEndpoint("mock:invalid").expectedMessageCount(0);
+        getMockEndpoint("mock:finally").expectedMessageCount(1);
+
+        template.sendBody("direct:start",
+                "<mail xmlns='http://foo.com/bar'><subject>Hey</subject><body>Hello world!</body></mail>");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void testInvalidMessage() throws Exception {
+        getMockEndpoint("mock:valid").expectedMessageCount(0);
+        getMockEndpoint("mock:invalid").expectedMessageCount(1);
+        getMockEndpoint("mock:finally").expectedMessageCount(1);
+
+        template.sendBody("direct:start",
+                "<mail xmlns='http://foo.com/bar'><body>Hello world!</body></mail>");
+
+        assertMockEndpointsSatisfied();
+    }
+
 }
