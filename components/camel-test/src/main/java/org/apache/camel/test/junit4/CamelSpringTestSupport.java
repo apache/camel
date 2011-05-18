b8548e707fb47f77a154a77a55b035d6c4441690
Claus Ibsen <davsclaus@apache.org>
2011-05-18 09:35:08 +0000

    CAMEL-3960: Added stuff in spring test support I forgot.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1124138 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-test/src/main/java/org/apache/camel/test/junit4/CamelSpringTestSupport.java
+++ components/camel-test/src/main/java/org/apache/camel/test/junit4/CamelSpringTestSupport.java
@@ -32,6 +32,7 @@ import org.apache.camel.util.CastUtils;
 import org.apache.camel.util.ObjectHelper;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -43,6 +44,22 @@ import org.springframework.context.support.GenericApplicationContext;
 public abstract class CamelSpringTestSupport extends CamelTestSupport {
     protected static AbstractApplicationContext applicationContext;
     protected abstract AbstractApplicationContext createApplicationContext();
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        if (!"true".equalsIgnoreCase(System.getProperty("skipStartingCamelContext"))) {
+            // tell camel-spring it should not trigger starting CamelContext, since we do that later
+            // after we are finished setting up the unit test
+            System.setProperty("maybeStartCamelContext", "false");
+            applicationContext = createApplicationContext();
+            assertNotNull("Should have created a valid spring context", applicationContext);
+            super.setUp();
+            System.clearProperty("maybeStartCamelContext");
+        } else {
+            log.info("Skipping starting CamelContext as system property skipStartingCamelContext is set to be true.");
+        }
+    }
 
     @Override
     public void doSetUp() throws Exception {
