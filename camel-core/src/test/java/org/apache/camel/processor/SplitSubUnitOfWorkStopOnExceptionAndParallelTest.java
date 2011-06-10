e0c6e82e0ca28c45cb2eae9dd11d9b1fab0d02e3
Claus Ibsen <davsclaus@apache.org>
2011-06-10 07:56:46 +0000

    CAMEL-4078: Fixed test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1134220 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/SplitSubUnitOfWorkStopOnExceptionAndParallelTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/SplitSubUnitOfWorkStopOnExceptionAndParallelTest.java
@@ -50,8 +50,8 @@ public class SplitSubUnitOfWorkStopOnExceptionAndParallelTest extends ContextTes
         // b should get between 1 or 3 depending when we stop (we run parallel)
         getMockEndpoint("mock:b").expectedMinimumMessageCount(1);
         getMockEndpoint("mock:result").expectedMessageCount(0);
-        // line should get between 1 or 2 depending when we stop (we run parallel)
-        getMockEndpoint("mock:line").expectedMinimumMessageCount(1);
+        // line should get between 0 or 2 depending when we stop (we run parallel)
+        getMockEndpoint("mock:line").expectedMinimumMessageCount(0);
 
         template.sendBody("direct:start", "Tiger,Donkey,Camel");
 
