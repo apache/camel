883f7505590e568a73e0c36d7edeb48454c6eda9
Claus Ibsen <davsclaus@apache.org>
2011-06-11 16:11:49 +0000

    CAMEL-4057: Keep test as it was so the wiki example is the same and simple. Added 2nd test to test the issue with 2x continued. Added better validation that it was actually the 2nd exception that was caught at the end of the test.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1134681 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/onexception/OnExceptionContinueTwoTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/onexception/OnExceptionContinueTwoTest.java
@@ -24,10 +24,11 @@ import org.apache.camel.component.mock.MockEndpoint;
 /**
  * @version 
  */
-public class OnExceptionContinueTest extends ContextTestSupport {
+public class OnExceptionContinueTwoTest extends ContextTestSupport {
 
-    public void testContinued() throws Exception {
+    public void testContinuedTwo() throws Exception {
         getMockEndpoint("mock:start").expectedMessageCount(1);
+        getMockEndpoint("mock:middle").expectedMessageCount(1);
 
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedBodiesReceived("Hello World");
@@ -37,13 +38,15 @@ public class OnExceptionContinueTest extends ContextTestSupport {
         template.sendBody("direct:start", "Hello World");
 
         assertMockEndpointsSatisfied();
+
+        Exception cause = mock.getReceivedExchanges().get(0).getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+        assertEquals("Forced Again", cause.getMessage());
     }
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
         return new RouteBuilder() {
             @Override
-            // START SNIPPET: e1
             public void configure() throws Exception {
                 // tell Camel to handle and continue when this exception is thrown
                 onException(IllegalArgumentException.class).continued(true);
@@ -51,9 +54,11 @@ public class OnExceptionContinueTest extends ContextTestSupport {
                 from("direct:start")
                     .to("mock:start")
                     .throwException(new IllegalArgumentException("Forced"))
+                    .to("mock:middle")
+                    //throw a second time to validate that the exchange is reset appropriately
+                    .throwException(new IllegalArgumentException("Forced Again"))
                     .to("mock:result");
             }
-            // END SNIPPET: e1
         };
     }
 }
