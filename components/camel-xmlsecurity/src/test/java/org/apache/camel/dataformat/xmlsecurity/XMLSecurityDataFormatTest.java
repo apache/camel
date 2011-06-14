eea03a0a66a8b6593e02fa681d2daea91a2ffaeb
Hadrian Zbarcea <hadrian@apache.org>
2011-06-14 02:21:03 +0000

    CAMEL-4061. Support for asymmetric encription. Patch applied with thanks to Rich Newcomb
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1135364 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-xmlsecurity/src/test/java/org/apache/camel/dataformat/xmlsecurity/XMLSecurityDataFormatTest.java
+++ components/camel-xmlsecurity/src/test/java/org/apache/camel/dataformat/xmlsecurity/XMLSecurityDataFormatTest.java
@@ -18,6 +18,11 @@ package org.apache.camel.dataformat.xmlsecurity;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+import java.util.Map;
+
+import javax.xml.transform.OutputKeys;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -26,35 +31,42 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.converter.jaxp.XmlConverter;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.xml.security.encryption.XMLCipher;
+import org.junit.Ignore;
 import org.junit.Test;
-
 
 /**
  * Unit test of the encryptXML data format.
  */
 public class XMLSecurityDataFormatTest extends CamelTestSupport {
-    private static final String XML_FRAGMENT = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
-            + "<cheesesites>"
-            + "<netherlands>"
-            + "<source>cow</source>"
-            + "<cheese>gouda</cheese>"
-            + "</netherlands>"
-            + "<italy>"
-            + "<source>cow</source>"
-            + "<cheese>gorgonzola</cheese>"
-            + "</italy>"
-            + "<france>"
-            + "<source>goat</source>"
-            + "<cheese>brie</cheese>"
-            + "</france>"
-            + "</cheesesites>";
+    private static final String XML_FRAGMENT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+        + "<cheesesites>"
+        + "<netherlands>"
+        + "<source>cow</source>"
+        + "<cheese>gouda</cheese>"
+        + "</netherlands>"
+        + "<italy>"
+        + "<source>cow</source>"
+        + "<cheese>gorgonzola</cheese>"
+        + "</italy>"
+        + "<france>"
+        + "<source>goat</source>"
+        + "<cheese>brie</cheese>"
+        + "</france>"
+        + "</cheesesites>";
 
     @Override
     public boolean isUseRouteBuilder() {
         return false;
+    }
+    
+    @Override 
+    public void setUp() throws Exception {
+        super.setUp();
+        context.getProperties().put(XmlConverter.OUTPUT_PROPERTIES_PREFIX + OutputKeys.ENCODING, "UTF-8");
     }
 
     private void sendText() throws Exception {
@@ -66,59 +78,114 @@ public class XMLSecurityDataFormatTest extends CamelTestSupport {
                 in.setBody(XML_FRAGMENT);
                 log.info("xmlFragment: " + XML_FRAGMENT);
             }
-
         });
+    }
+    
+    private void testEncryption() throws Exception {
+        MockEndpoint resultEndpoint = context.getEndpoint("mock:encrypted", MockEndpoint.class);
+        resultEndpoint.setExpectedMessageCount(1);
+        context.start();
+        sendText();
+        resultEndpoint.assertIsSatisfied(100);
+        Exchange exchange = resultEndpoint.getExchanges().get(0);
+        Document inDoc = getDocumentForInMessage(exchange);
+        if (log.isDebugEnabled()) {
+            logMessage(exchange, inDoc);
+        }
+        assertTrue("The XML message has no encrypted data.", hasEncryptedData(inDoc));
+    }
+    
+    private void testDecryption() throws Exception {
+        MockEndpoint resultEndpoint = context.getEndpoint("mock:decrypted", MockEndpoint.class);
+        resultEndpoint.setExpectedMessageCount(1);
+        // verify that the message was encrypted before checking that it is decrypted
+        testEncryption();
+
+        resultEndpoint.assertIsSatisfied(100);
+        Exchange exchange = resultEndpoint.getExchanges().get(0);
+        Document inDoc = getDocumentForInMessage(exchange);
+        if (log.isDebugEnabled()) {
+            logMessage(exchange, inDoc);
+        }
+        assertFalse("The XML message has encrypted data.", hasEncryptedData(inDoc));
+        
+        // verify that the decrypted message matches what was sent
+        XmlConverter converter = new XmlConverter();
+        String xmlStr = converter.toString(inDoc, exchange);
+        assertTrue(xmlStr.equals(XML_FRAGMENT));
+    }
+    
+    private boolean hasEncryptedData(Document doc) throws Exception {
+        NodeList nodeList = doc.getElementsByTagNameNS("http://www.w3.org/2001/04/xmlenc#", "EncryptedData");
+        return nodeList.getLength() > 0;
+    }
+    
+    private void logMessage(Exchange exchange, Document inDoc) throws Exception {
+        XmlConverter converter = new XmlConverter();
+        String xmlStr = converter.toString(inDoc, exchange);
+        log.debug(xmlStr);   
+    }
+    
+    private Document getDocumentForInMessage(Exchange exchange) {
+        byte[] body = exchange.getIn().getBody(byte[].class);
+        Document d = createDocumentfromInputStream(new ByteArrayInputStream(body));
+        return d;
+    }
+    
+    private Document createDocumentfromInputStream(InputStream is) {
+        return context.getTypeConverter().convertTo(Document.class, is);
     }
 
     /*
      * Encryption Tests
      */
+    
     @Test
     public void testFullPayloadXMLEncryption() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML().process(new EncryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML()
+                    .to("mock:encrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testEncryption();
     }
 
     @Test
-    public void testPartialPayloadXMLContentEncryption() throws Exception {
+    public void testPartialPayloadXMLContentEncryption() throws Exception {       
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/italy/cheese", true).process(new EncryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/italy/cheese", true)
+                    .to("mock:encrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testEncryption();
     }
 
     @Test
     public void testPartialPayloadMultiNodeXMLContentEncryption() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/*/cheese", true).process(new EncryptedXMLMessageProcessor());
+                from("direct:start")
+                     .marshal().secureXML("//cheesesites/*/cheese", true)
+                     .to("mock:encrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testEncryption();
     }
 
     @Test
     public void testPartialPayloadXMLElementEncryptionWithKey() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/france/cheese", false, "Just another 24 Byte key").process(new EncryptedXMLMessageProcessor());
-            }
+                from("direct:start")
+                     .marshal().secureXML("//cheesesites/france/cheese", false, "Just another 24 Byte key")
+                     .to("mock:encrypted");
+            }    
         });
-        context.start();
-
-        sendText();
+        testEncryption();
     }
 
     @Test
@@ -132,14 +199,81 @@ public class XMLSecurityDataFormatTest extends CamelTestSupport {
         final String passCode = new String(bits128);
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/netherlands", false, passCode, XMLCipher.AES_128).process(new EncryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/netherlands", false, passCode, XMLCipher.AES_128)
+                    .to("mock:encrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testEncryption();
     }
 
+    @Ignore
+    @Test
+    public void testFullPayloadAsymmetricKeyEncryption() throws Exception {
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        URL trustStoreUrl = getClass().getClassLoader().getResource("sender.ts");
+        trustStore.load(trustStoreUrl.openStream(), "password".toCharArray());
+
+        final XMLSecurityDataFormat xmlEncDataFormat = new XMLSecurityDataFormat();
+        xmlEncDataFormat.setKeyCipherAlgorithm(XMLCipher.RSA_v1dot5);
+        xmlEncDataFormat.setXmlCipherAlgorithm(XMLCipher.AES_256);
+        xmlEncDataFormat.setTrustStore(trustStore);
+        xmlEncDataFormat.setTrustStorePassword("password");
+        xmlEncDataFormat.setRecipientKeyAlias("recipient");
+
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("direct:start")
+                    .marshal(xmlEncDataFormat).to("mock:encrypted");
+            }
+        });
+        testEncryption();
+    }
+
+    @Ignore
+    @Test
+    public void testPartialPayloadAsymmetricKeyEncryptionWithContextTruststoreProperties() throws Exception {
+        Map<String, String> contextProps = context.getProperties();
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_URL, 
+            getClass().getClassLoader().getResource("sender.ts").toString());
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_PASSWORD, "password");
+ 
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/italy/cheese", true, "recipient", XMLCipher.AES_256, XMLCipher.RSA_v1dot5)
+                    .to("mock:encrypted");
+            }
+        });
+        testEncryption();
+    }
+ 
+    @Ignore
+    @Test
+    public void testPartialPayloadAsymmetricKeyEncryptionWithExchangeRecipientAlias() throws Exception {
+        MockEndpoint resultEndpoint = context.getEndpoint("mock:foo", MockEndpoint.class);
+        resultEndpoint.setExpectedMessageCount(1);
+ 
+        Map<String, String> contextProps = context.getProperties();
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_URL,
+            getClass().getClassLoader().getResource("sender.ts").toString());
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_PASSWORD, "password");
+ 
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("direct:start")
+                    .process(new Processor() {
+                        public void process(Exchange exchange) throws Exception {
+                            exchange.getIn().setHeader(XMLSecurityDataFormat.XML_ENC_RECIPIENT_ALIAS, "recipient");
+                        }
+                    })
+                    .marshal().secureXML("//cheesesites/italy/cheese", true, null, XMLCipher.AES_256, XMLCipher.RSA_v1dot5)
+                    .to("mock:encrypted");
+            }
+        });
+        testEncryption();
+    }
+ 
     /*
     * Decryption Tests
     */
@@ -147,51 +281,48 @@ public class XMLSecurityDataFormatTest extends CamelTestSupport {
     public void testFullPayloadXMLDecryption() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML().unmarshal().secureXML().process(new DecryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML().to("mock:encrypted")
+                    .unmarshal().secureXML().to("mock:decrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testDecryption();
     }
     
     @Test
     public void testPartialPayloadXMLContentDecryption() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/italy/cheese", true).
-                        unmarshal().secureXML("//cheesesites/italy/cheese", true).process(new DecryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/italy/cheese", true).to("mock:encrypted")
+                    .unmarshal().secureXML("//cheesesites/italy/cheese", true).to("mock:decrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testDecryption();
     }
     
     @Test
     public void testPartialPayloadMultiNodeXMLContentDecryption() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/*/cheese", true).
-                        unmarshal().secureXML("//cheesesites/*/cheese", true).process(new DecryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/*/cheese", true).to("mock:encrypted")
+                    .unmarshal().secureXML("//cheesesites/*/cheese", true).to("mock:decrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testDecryption();
     }
 
     @Test
     public void testPartialPayloadXMLElementDecryptionWithKey() throws Exception {
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("//cheesesites/france/cheese", false, "Just another 24 Byte key").
-                        unmarshal().secureXML("//cheesesites/france", false, "Just another 24 Byte key").process(new DecryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/france/cheese", false, "Just another 24 Byte key").to("mock:encrypted")
+                    .unmarshal().secureXML("//cheesesites/france", false, "Just another 24 Byte key").to("mock:decrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testDecryption();
     }
 
     @Test
@@ -204,50 +335,62 @@ public class XMLSecurityDataFormatTest extends CamelTestSupport {
         final String passCode = new String(bits128);
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("direct:start").marshal().secureXML("cheese", true, passCode, XMLCipher.AES_128).
-                        unmarshal().secureXML("cheese", true, passCode, XMLCipher.AES_128).process(new DecryptedXMLMessageProcessor());
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/italy", true, passCode, XMLCipher.AES_128).to("mock:encrypted")
+                    .unmarshal().secureXML("//cheesesites/italy", true, passCode, XMLCipher.AES_128).to("mock:decrypted");
             }
         });
-        context.start();
-
-        sendText();
+        testDecryption();
     }
 
+    @Ignore
+    @Test
+    public void testFullPayloadAsymmetricKeyDecryption() throws Exception {
 
-    private class EncryptedXMLMessageProcessor implements Processor {
+        Map<String, String> contextProps = context.getProperties();
+        // context properties for encryption
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_URL, getClass().getClassLoader().getResource("sender.ts").toString());
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_PASSWORD, "password");
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_RECIPIENT_ALIAS, "recipient");
 
-        public void process(Exchange exchange) throws Exception {
-            byte[] body = exchange.getIn().getBody(byte[].class);
+        // context properties for decryption
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_KEY_STORE_URL, getClass().getClassLoader().getResource("recipient.ks").toString());
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_KEY_STORE_PASSWORD, "password");
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_KEY_STORE_ALIAS, "recipient");
 
-            Document d = createDocumentfromInputStream(new ByteArrayInputStream(body));
-
-            NodeList nodeList = d.getElementsByTagNameNS("http://www.w3.org/2001/04/xmlenc#", "EncryptedData");
-            if (nodeList.getLength() >= 0) {
-                assertTrue(true);
-            } else {
-                assertTrue(false);
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("direct:start")
+                    .marshal().secureXML("", true, null, XMLCipher.AES_256, XMLCipher.RSA_v1dot5).to("mock:encrypted")
+                    .unmarshal().secureXML("", true, null, XMLCipher.AES_256, XMLCipher.RSA_v1dot5).to("mock:decrypted");
             }
-        }
+        });
+        testDecryption();
     }
 
-    private class DecryptedXMLMessageProcessor implements Processor {
+    @Ignore
+    @Test
+    public void testPartialPayloadAsymmetricKeyDecryption() throws Exception {
 
-        public void process(Exchange exchange) throws Exception {
-            byte[] body = exchange.getIn().getBody(byte[].class);
+        Map<String, String> contextProps = context.getProperties();
 
-            Document d = createDocumentfromInputStream(new ByteArrayInputStream(body));
+        // context properties for encryption
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_URL, getClass().getClassLoader().getResource("sender.ts").toString());
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_TRUST_STORE_PASSWORD, "password");
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_RECIPIENT_ALIAS, "recipient");
 
-            NodeList nodeList = d.getElementsByTagNameNS("http://www.w3.org/2001/04/xmlenc#", "EncryptedData");
-            if (nodeList.getLength() == 0) {
-                assertTrue(true);
-            } else {
-                assertTrue(false);
+        // context properties for decryption
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_KEY_STORE_URL, getClass().getClassLoader().getResource("recipient.ks").toString());
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_KEY_STORE_PASSWORD, "password");
+        contextProps.put(XMLSecurityDataFormat.XML_ENC_KEY_STORE_ALIAS, "recipient");
+
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("direct:start")
+                    .marshal().secureXML("//cheesesites/italy", true, null, XMLCipher.AES_256, XMLCipher.RSA_v1dot5).to("mock:encrypted")
+                    .unmarshal().secureXML("//cheesesites/italy", true, null, XMLCipher.AES_256, XMLCipher.RSA_v1dot5).to("mock:decrypted");
             }
-        }
-    }
-
-
-    private Document createDocumentfromInputStream(InputStream is) {
-        return context.getTypeConverter().convertTo(Document.class, is);
+        });
+        testDecryption();
     }
 }
