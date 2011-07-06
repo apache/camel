5752a232160242e72e5fe6181b33b286e50eb513
Claus Ibsen <davsclaus@apache.org>
2011-07-06 10:40:33 +0000

    CAMEL-4170: Fixed broken unit tests.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1143332 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-smpp/src/test/java/org/apache/camel/component/smpp/SmppProducerLazySessionCreationTest.java
+++ components/camel-smpp/src/test/java/org/apache/camel/component/smpp/SmppProducerLazySessionCreationTest.java
@@ -25,6 +25,7 @@ import org.jsmpp.bean.ESMClass;
 import org.jsmpp.bean.GeneralDataCoding;
 import org.jsmpp.bean.MessageClass;
 import org.jsmpp.bean.NumberingPlanIndicator;
+import org.jsmpp.bean.OptionalParameter;
 import org.jsmpp.bean.RegisteredDelivery;
 import org.jsmpp.bean.SMSCDeliveryReceipt;
 import org.jsmpp.bean.SubmitSm;
@@ -86,6 +87,7 @@ public class SmppProducerLazySessionCreationTest {
         expect(submitSm.getReplaceIfPresent()).andReturn((byte) 0);
         expect(submitSm.getDataCoding()).andReturn((byte) 0);
         expect(submitSm.getShortMessage()).andReturn("Hello SMPP world!".getBytes("ISO-8859-1"));
+        expect(submitSm.getOptionalParametes()).andReturn(new OptionalParameter[]{});
     }
 
     @Test
