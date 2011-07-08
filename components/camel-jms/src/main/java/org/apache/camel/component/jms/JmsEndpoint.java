2d98b394cd84ca723a0e174312520752382e088a
Claus Ibsen <davsclaus@apache.org>
2011-07-08 10:52:02 +0000

    CAMEL-4194: jmx topic endpoints should be non-singleton.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1144248 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsEndpoint.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsEndpoint.java
@@ -323,6 +323,12 @@ public class JmsEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
 
     @ManagedAttribute
     public boolean isSingleton() {
+        if (isPubSubDomain()) {
+            // topic cannot be singleton, as there may be multiple consumers
+            // on the same topic
+            return false;
+        }
+        // but queues can be singleton
         return true;
     }
 
