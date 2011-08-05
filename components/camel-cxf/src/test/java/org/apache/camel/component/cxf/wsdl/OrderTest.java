84154d0694bc0f1aa2a6ada5bcc40784acb3433d
Willem Ning Jiang <ningjiang@apache.org>
2011-08-05 03:55:43 +0000

    Merged revisions 1153323 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r1153323 | ningjiang | 2011-08-03 10:59:30 +0800 (Wed, 03 Aug 2011) | 1 line
      
      CAMEL-4294 Fixed the WSDL generation issue of Camel 2.8.0
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.8.x@1154087 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-cxf/src/test/java/org/apache/camel/component/cxf/wsdl/OrderTest.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.cxf.wsdl;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class OrderTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("org/apache/camel/component/cxf/wsdl/camel-route.xml");
+    }
+
+    @Test
+    public void testCamelWsdl() throws Exception {
+        Object body = template.sendBody("seda:camelWsdl", ExchangePattern.InOut, null);
+        checkWsdl(InputStream.class.cast(body));
+    }
+
+    @Test
+    public void testCxfWsdl() throws Exception {
+        Object body = template.sendBody("seda:cxfWsdl", ExchangePattern.InOut, null);
+        checkWsdl(InputStream.class.cast(body));
+    }
+
+    public void checkWsdl(InputStream in) throws Exception {
+
+        boolean containsOrderComplexType = false;
+        LineNumberReader reader = new LineNumberReader(new InputStreamReader(in));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.contains("complexType name=\"order\"")) {
+                containsOrderComplexType = true;
+                // break;
+            }
+
+        }
+
+        if (!containsOrderComplexType) {
+            throw new RuntimeException("WSDL does not contain complex type defintion for class Order");
+        }
+
+    }
+}
