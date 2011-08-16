cce3b9d7b982b65ce0352d6af537291a0e37fb06
Claus Ibsen <davsclaus@apache.org>
2011-08-16 08:05:45 +0000

    CAMEL-4337: Bindy in CSV format now uses trim, clip, and length attributes on @DataField when marshalling. Thanks to Reinhard Luft for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1158143 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/model/simple/oneclassandtrimandclip/Customer.java
@@ -0,0 +1,69 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.dataformat.bindy.model.simple.oneclassandtrimandclip;
+
+import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
+import org.apache.camel.dataformat.bindy.annotation.DataField;
+
+@CsvRecord(separator = ",")
+public class Customer {
+
+    @DataField(pos = 1, trim = true, length = 2, clip = true)
+    private String salutation;
+
+    @DataField(pos = 2, trim = true, length = 10, clip = true)
+    private String firstName;
+
+    @DataField(pos = 3, trim = true, length = 10, clip = true)
+    private String lastName;
+
+    @DataField(pos = 4, trim = true, length = 10, clip = true)
+    private String city;
+
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
