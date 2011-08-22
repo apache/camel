64d1cf21c7e7194708de510e65f218d4d5d4dd98
Christian Schneider <cschneider@apache.org>
2011-08-22 12:36:51 +0000

    CAMEL-4357 Move Main class to package .main as it is not part of the camel API
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1160238 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/main/MainExample.java
+++ camel-core/src/test/java/org/apache/camel/main/MainExample.java
@@ -14,10 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel;
+package org.apache.camel.main;
 
 import java.util.Date;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
--- camel-core/src/test/java/org/apache/camel/main/MainTest.java
+++ camel-core/src/test/java/org/apache/camel/main/MainTest.java
@@ -14,12 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel;
+package org.apache.camel.main;
 
 import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 
