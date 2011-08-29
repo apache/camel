3246cc5880b9e171dbd58e4d9561ff62823f7645
Willem Ning Jiang <ningjiang@apache.org>
2011-08-29 02:19:46 +0000

    CAMEL-4356 faster way of testing for file existence
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1162635 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/FtpOperations.java
+++ components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/FtpOperations.java
@@ -507,29 +507,12 @@ public class FtpOperations implements RemoteFileOperations<FTPFile> {
 
     public boolean existsFile(String name) throws GenericFileOperationFailedException {
         log.trace("existsFile({})", name);
-
-        // check whether a file already exists
-        String directory = FileUtil.onlyPath(name);
-        String onlyName = FileUtil.stripPath(name);
         try {
-            String[] names;
-            if (directory != null) {
-                names = client.listNames(directory);
-            } else {
-                names = client.listNames();
-            }
-            // can return either null or an empty list depending on FTP servers
+            String[] names = client.listNames(name);
             if (names == null) {
                 return false;
             }
-            for (String existing : names) {
-                log.trace("Existing file: {}, target file: {}", existing, name);
-                existing = FileUtil.stripPath(existing);
-                if (existing != null && existing.equals(onlyName)) {
-                    return true;
-                }
-            }
-            return false;
+            return (names.length >= 1);
         } catch (IOException e) {
             throw new GenericFileOperationFailedException(client.getReplyCode(), client.getReplyString(), e.getMessage(), e);
         }
