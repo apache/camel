26d97d9b4d5428b695de89ad729b8ef20e93fab1
Christian Schneider <cschneider@apache.org>
2011-09-15 11:50:40 +0000

    Fix for compile error
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1171063 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/main/java/org/apache/camel/spring/spi/SpringTransactionPolicy.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/spi/SpringTransactionPolicy.java
@@ -67,7 +67,7 @@ public class SpringTransactionPolicy implements TransactedPolicy {
         // if we should not support this we do not need to wrap the processor as we only need one transacted error handler
 
         // find the existing error handler builder
-        ErrorHandlerFactory builder = routeContext.getRoute().getErrorHandlerBuilder();
+        ErrorHandlerBuilder builder = (ErrorHandlerBuilder)routeContext.getRoute().getErrorHandlerBuilder();
 
         // check if its a ref if so then do a lookup
         if (builder instanceof ErrorHandlerBuilderRef) {
@@ -79,7 +79,7 @@ public class SpringTransactionPolicy implements TransactedPolicy {
             // and if so then we can safely replace that with our transacted error handler
             if (ErrorHandlerBuilderRef.isErrorHandlerBuilderConfigured(ref)) {
                 LOG.debug("Looking up ErrorHandlerBuilder with ref: {}", ref);
-                builder = ErrorHandlerBuilderRef.lookupErrorHandlerBuilder(routeContext, ref);
+                builder = (ErrorHandlerBuilder)ErrorHandlerBuilderRef.lookupErrorHandlerBuilder(routeContext, ref);
             }
         }
 
