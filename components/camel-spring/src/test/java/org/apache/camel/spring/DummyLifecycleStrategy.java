f0d7df739cd5bce5d3a02061e5c5476881bf72b0
Christian Schneider <cschneider@apache.org>
2011-09-15 11:55:04 +0000

    CAMEL-4453 Fix compile error
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1171065 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-spring/src/test/java/org/apache/camel/spring/DummyLifecycleStrategy.java
+++ components/camel-spring/src/test/java/org/apache/camel/spring/DummyLifecycleStrategy.java
@@ -22,6 +22,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Component;
 import org.apache.camel.Endpoint;
+import org.apache.camel.ErrorHandlerFactory;
 import org.apache.camel.Processor;
 import org.apache.camel.Route;
 import org.apache.camel.Service;
@@ -63,7 +64,7 @@ public class DummyLifecycleStrategy implements LifecycleStrategy {
     public void onRouteContextCreate(RouteContext routeContext) {
     }
 
-    public void onErrorHandlerAdd(RouteContext routeContext, Processor errorHandler, ErrorHandlerBuilder errorHandlerBuilder) {
+    public void onErrorHandlerAdd(RouteContext routeContext, Processor errorHandler, ErrorHandlerFactory errorHandlerBuilder) {
     }
 
     public void onRoutesRemove(Collection<Route> routes) {
