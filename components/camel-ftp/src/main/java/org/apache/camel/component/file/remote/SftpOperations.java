284306a3c608db8e4435f1a0a8dd9e9378046e7f
J. Daniel Kulp <dkulp@apache.org>
2011-09-19 22:58:29 +0000

    Merged revisions 1162635 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r1162635 | ningjiang | 2011-08-28 22:19:46 -0400 (Sun, 28 Aug 2011) | 1 line
      
      CAMEL-4356 faster way of testing for file existence
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.8.x@1172883 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/SftpOperations.java
+++ components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/SftpOperations.java
@@ -678,31 +678,12 @@ public class SftpOperations implements RemoteFileOperations<ChannelSftp.LsEntry>
 
     public boolean existsFile(String name) throws GenericFileOperationFailedException {
         LOG.trace("existsFile({})", name);
-
-        // check whether a file already exists
-        String directory = FileUtil.onlyPath(name);
-        if (directory == null) {
-            // assume current dir if no path could be extracted
-            directory = ".";
-        }
-        String onlyName = FileUtil.stripPath(name);
-
         try {
-            Vector files = channel.ls(directory);
-            // can return either null or an empty list depending on FTP servers
+            Vector files = channel.ls(name);
             if (files == null) {
                 return false;
             }
-            for (Object file : files) {
-                ChannelSftp.LsEntry entry = (ChannelSftp.LsEntry) file;
-                String existing = entry.getFilename();
-                LOG.trace("Existing file: {}, target file: {}", existing, name);
-                existing = FileUtil.stripPath(existing);
-                if (existing != null && existing.equals(onlyName)) {
-                    return true;
-                }
-            }
-            return false;
+            return (files.size() >= 1);
         } catch (SftpException e) {
             // or an exception can be thrown with id 2 which means file does not exists
             if (ChannelSftp.SSH_FX_NO_SUCH_FILE == e.id) {
@@ -711,6 +692,7 @@ public class SftpOperations implements RemoteFileOperations<ChannelSftp.LsEntry>
             // otherwise its a more serious error so rethrow
             throw new GenericFileOperationFailedException(e.getMessage(), e);
         }
+
     }
 
     public boolean sendNoop() throws GenericFileOperationFailedException {
