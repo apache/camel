f98ac676b5f604403414a728c4afce39c3d79e22
Claus Ibsen <davsclaus@apache.org>
2011-09-25 17:34:19 +0000

    CAMEL-4486: Fixed issue with routing slip EIP not propagating failed to resolve endpoint exception properly.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1175438 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/RoutingSlip.java
+++ camel-core/src/main/java/org/apache/camel/processor/RoutingSlip.java
@@ -170,7 +170,7 @@ public class RoutingSlip extends ServiceSupport implements AsyncProcessor, Trace
         };
     }
 
-    private boolean doRoutingSlip(Exchange exchange, AsyncCallback callback) {
+    private boolean doRoutingSlip(final Exchange exchange, final AsyncCallback callback) {
         Exchange current = exchange;
         RoutingSlipIterator iter;
         try {
@@ -196,8 +196,8 @@ public class RoutingSlip extends ServiceSupport implements AsyncProcessor, Trace
                 }
             } catch (Exception e) {
                 // error resolving endpoint so we should break out
-                exchange.setException(e);
-                return true;
+                current.setException(e);
+                break;
             }
 
             // prepare and process the routing slip
