605d8746763697ed6e9fb00043b21d6f7f756771
Claus Ibsen <davsclaus@apache.org>
2011-10-01 08:47:08 +0000

    Merged revisions 1177948 via svnmerge from https://svn.apache.org/repos/asf/camel/trunk
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.8.x@1177949 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-printer/src/main/java/org/apache/camel/component/printer/PrinterProducer.java
+++ components/camel-printer/src/main/java/org/apache/camel/component/printer/PrinterProducer.java
@@ -87,7 +87,15 @@ public class PrinterProducer extends DefaultProducer {
             printService = PrintServiceLookup.lookupDefaultPrintService();            
         } else {
             PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
-            setPrinter("\\\\" + config.getHostname() + "\\" + config.getPrintername());
+            String name;
+            if (config.getHostname().equalsIgnoreCase("localhost")) {
+                // no hostname for localhost printers
+                name = config.getPrintername();
+            } else {
+                name = "\\\\" + config.getHostname() + "\\" + config.getPrintername();
+            }
+            log.debug("Using printer name: {}", name);
+            setPrinter(name);
             int position = findPrinter(services, printer);
             if (position < 0) {
                 throw new PrintException("No printer found with name: " + printer + ". Please verify that the host and printer are registered and reachable from this machine.");
