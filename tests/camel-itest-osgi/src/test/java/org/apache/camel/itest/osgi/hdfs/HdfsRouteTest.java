88a538eae1a91978093a61c2ff34ee3455687ba1
Willem Ning Jiang <ningjiang@apache.org>
2011-10-21 04:05:31 +0000

    CAMEL-4562 Add OSGi integration test for Hdfs component with thanks to Ioannis
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1187176 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/hdfs/HdfsRouteTest.java
@@ -0,0 +1,114 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.itest.osgi.hdfs;
+
+import java.io.File;
+import java.io.InputStream;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.itest.osgi.OSGiIntegrationTestSupport;
+import org.apache.camel.itest.osgi.jpa.SendEmail;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.karaf.testing.Helper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Customizer;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Constants;
+
+
+import static org.apache.hadoop.io.SequenceFile.createWriter;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.workingDirectory;
+import static org.ops4j.pax.swissbox.tinybundles.core.TinyBundles.modifyBundle;
+
+@RunWith(JUnit4TestRunner.class)
+public class HdfsRouteTest extends OSGiIntegrationTestSupport {
+    //Hadoop doesn't run on IBM JDK
+    private static final boolean SKIP = System.getProperty("java.vendor").contains("IBM");
+
+    @Test
+    public void testReadString() throws Exception {
+        if (SKIP) {
+            return;
+        }
+
+        final Path file = new Path(new File("target/test/test-camel-string").getAbsolutePath());
+        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+        FileSystem fs1 = FileSystem.get(file.toUri(), conf);
+        SequenceFile.Writer writer = createWriter(fs1, conf, file, NullWritable.class, Text.class);
+        NullWritable keyWritable = NullWritable.get();
+        Text valueWritable = new Text();
+        String value = "CIAO!";
+        valueWritable.set(value);
+        writer.append(keyWritable, valueWritable);
+        writer.sync();
+        writer.close();
+
+        context.addRoutes(new RouteBuilder() {
+            public void configure() {
+                from("hdfs:///" + file.toUri() + "?fileSystemType=LOCAL&fileType=SEQUENCE_FILE&initialDelay=0").to("mock:result");
+            }
+        });
+        context.start();
+
+        MockEndpoint resultEndpoint = (MockEndpoint) context.getEndpoint("mock:result");
+        resultEndpoint.expectedMessageCount(1);
+        resultEndpoint.assertIsSatisfied();
+    }
+
+    @Configuration
+    public static Option[] configure() throws Exception {
+        Option[] options = combine(
+                // Default karaf environment
+                Helper.getDefaultOptions(
+                        // this is how you set the default log level when using pax logging (logProfile)
+                        Helper.setLogLevel("WARN")),
+                new Customizer() {
+                    @Override
+                    public InputStream customizeTestProbe(InputStream testProbe) {
+                        return modifyBundle(testProbe)
+                                .add(SendEmail.class)
+                                .add("core-default.xml", HdfsRouteTest.class.getResource("core-default.xml"))
+                                        //.add("hdfs-default.xml", HdfsRouteTest.class.getResource("hdfs-default.xml"))
+                                .set(Constants.BUNDLE_SYMBOLICNAME, "CamelHdfsTestBundle")
+                                .set(Constants.DYNAMICIMPORT_PACKAGE, "*")
+                                .build();
+                    }
+                },
+
+                // install the spring.
+                scanFeatures(getKarafFeatureUrl(), "spring"),
+                // using the features to install the camel components
+                scanFeatures(getCamelKarafFeatureUrl(),
+                        "camel-core", "camel-spring", "camel-test", "camel-hdfs"),
+                workingDirectory("target/paxrunner/"),
+                //vmOption("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
+                felix(), equinox());
+
+        return options;
+    }
+}
