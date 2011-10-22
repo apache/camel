69c74ce2706b11eb0c47b42a645e5f1ac45a423f
Claus Ibsen <davsclaus@apache.org>
2011-10-22 08:00:43 +0000

    CAMEL-4571: Fixed compiler error due backport, and more code needed in camel-core.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.7.x@1187668 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/util/StringHelper.java
+++ camel-core/src/main/java/org/apache/camel/util/StringHelper.java
@@ -76,4 +76,18 @@ public final class StringHelper {
         return s;
     }
     
+    public static String removeLeadingAndEndingQuotes(String s) {
+        if (ObjectHelper.isEmpty(s)) {
+            return s;
+        }
+
+        if (s.startsWith("'") && s.endsWith("'")) {
+            return s.substring(1, s.length() - 1);
+        }
+        if (s.startsWith("\"") && s.endsWith("\"")) {
+            return s.substring(1, s.length() - 1);
+        }
+        return s;
+    }
+
 }
