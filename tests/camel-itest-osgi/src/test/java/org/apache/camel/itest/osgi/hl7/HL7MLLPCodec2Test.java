e8994782bbde0188514a425f05d6258582409a3b
Claus Ibsen <davsclaus@apache.org>
2011-11-15 07:52:47 +0000

    Fixed compilation of unit test.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.7.x@1202083 13f79535-47bb-0310-9956-ffa450edef68

--- tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/hl7/HL7MLLPCodec2Test.java
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/hl7/HL7MLLPCodec2Test.java
@@ -28,8 +28,13 @@ import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.springframework.osgi.context.support.OsgiBundleXmlApplicationContext;
 
-import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.profile;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.workingDirectory;
 
 @RunWith(JUnit4TestRunner.class)
 public class HL7MLLPCodec2Test extends OSGiIntegrationSpringTestSupport implements Processor {
@@ -67,12 +72,23 @@ public class HL7MLLPCodec2Test extends OSGiIntegrationSpringTestSupport implemen
 
     @Configuration
     public static Option[] configure() {
-        Option[] options = combine(
-            getDefaultCamelKarafOptions(),
-             // using the features to install the other camel components
-            scanFeatures(getCamelKarafFeatureUrl(), "camel-hl7", "camel-mina"));
+        Option[] options = options(
+            // install the spring dm profile
+            profile("spring.dm").version("1.2.0"),
+            // this is how you set the default log level when using pax logging (logProfile)
+            org.ops4j.pax.exam.CoreOptions.systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
+
+            // using the features to install the camel components
+            scanFeatures(getCamelKarafFeatureUrl(),
+                          "camel-core", "camel-spring", "camel-test", "camel-mina", "camel-hl7"),
+
+            // add hl7 osgi bundle
+            mavenBundle().groupId("http://hl7api.sourceforge.net/m2/!ca.uhn.hapi").artifactId("hapi-osgi-base").version("1.0.1"),
+
+            workingDirectory("target/paxrunner/"),
+
+            felix(), equinox());
 
         return options;
     }
-
 }
