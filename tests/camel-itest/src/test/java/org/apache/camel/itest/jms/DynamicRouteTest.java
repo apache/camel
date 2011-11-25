0f323bd5b47eae23c460b1fd90690abba632c188
Willem Ning Jiang <ningjiang@apache.org>
2011-11-25 04:13:30 +0000

    Added a dynamic route test based on the user forum.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1206055 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/jms/DynamicRouteTest.java
@@ -0,0 +1,104 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.itest.jms;
+
+import javax.naming.Context;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.Exchange;
+import org.apache.camel.Header;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.AvailablePortFinder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.util.jndi.JndiContext;
+import org.junit.Test;
+
+public class DynamicRouteTest extends CamelTestSupport {
+    
+    @Test
+    public void testDynamicRouteWithJms() throws Exception {
+        String response = template.requestBody("jms:queue:request?replyTo=bar", "foo", String.class);
+        assertEquals("response is foo", response);
+        response = template.requestBody("jms:queue:request", "bar", String.class);
+        assertEquals("response is bar", response);
+        
+      
+    }
+    
+    @Test
+    public void testDynamicRouteWithDirect() throws Exception {
+        String response = template.requestBody("direct:start", "foo", String.class);
+        assertEquals("response is foo", response);
+        response = template.requestBody("direct:start", "bar", String.class);
+        assertEquals("response is bar", response);
+        
+      
+    }
+    
+    
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+       
+        return new RouteBuilder() {
+            public void configure() {
+                from("jms:queue:request") 
+                    .dynamicRouter().method(MyDynamicRouter.class, "route");
+                from("direct:start")
+                    .dynamicRouter(bean(new MyDynamicRouter()));
+            }
+
+        };
+    }
+
+    @Override
+    protected Context createJndiContext() throws Exception {
+        JndiContext answer = new JndiContext();
+
+        // add ActiveMQ with embedded broker
+        ActiveMQComponent amq = ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false");
+        amq.setCamelContext(context);
+        answer.bind("jms", amq);
+        answer.bind("myBean", new MyBean());
+        return answer;
+    }
+    
+    public static class MyBean {
+
+        public String foo() {
+            return "response is foo";
+        }
+
+        public String bar() {
+            return "response is bar";
+        }
+    }
+    
+    public static class MyDynamicRouter {
+        public String route(String methodName, @Header(Exchange.SLIP_ENDPOINT) String previous) {
+            System.out.println("method name is  " + methodName + " previous " +  previous);
+            if (previous != null && previous.startsWith("bean://myBean?method")) {
+                // we get the result here and stop routing
+                return null;
+            } else {
+                return "bean:myBean?method=" + methodName;
+            }
+        }
+    }
+
+}
+
