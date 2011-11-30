59b343a2fa6b443726e7ab760d60a2a57e614cfc
Freeman Yue Fang <ffang@apache.org>
2011-11-30 03:18:23 +0000

    [CAMEL-4727]keep consistent for CamelContext:  output
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1208231 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
+++ camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
@@ -1508,7 +1508,7 @@ public class DefaultCamelContext extends ServiceSupport implements ModelCamelCon
 
     protected synchronized void doStop() throws Exception {
         stopWatch.restart();
-        log.info("Apache Camel " + getVersion() + " (CamelContext:" + getName() + ") is shutting down");
+        log.info("Apache Camel " + getVersion() + " (CamelContext: " + getName() + ") is shutting down");
         EventHelper.notifyCamelContextStopping(this);
 
         // stop route inputs in the same order as they was started so we stop the very first inputs first
