f4b563250b290bad4bfc26a2ab037d96966437ce
Ioannis Canellos <iocanel@apache.org>
2011-12-02 17:17:15 +0000

    [CAMEL-4736] Added camel-xstream osgi itest that uses the blueprint.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1209592 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/xstream/XstreamBlueprintRouteTest.java
@@ -0,0 +1,90 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.itest.osgi.xstream;
+
+import java.io.InputStream;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.itest.osgi.blueprint.OSGiBlueprintTestSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Customizer;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Constants;
+import org.osgi.service.blueprint.container.BlueprintContainer;
+
+
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import static org.ops4j.pax.swissbox.tinybundles.core.TinyBundles.modifyBundle;
+
+@RunWith(JUnit4TestRunner.class)
+public class XstreamBlueprintRouteTest extends OSGiBlueprintTestSupport {
+
+    @Test
+    public void testUnmarshal() throws Exception {
+        getInstalledBundle("CamelBlueprintXstreamTestBundle").start();
+        BlueprintContainer ctn = getOsgiService(BlueprintContainer.class, "(osgi.blueprint.container.symbolicname=CamelBlueprintXstreamTestBundle)", 10000);
+        CamelContext ctx = getOsgiService(CamelContext.class, "(camel.context.symbolicname=CamelBlueprintXstreamTestBundle)", 10000);
+
+        MockEndpoint mock = (MockEndpoint) ctx.getEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+
+        String body = "<org.apache.camel.itest.osgi.xstream.SampleObject>\n"
+                + "    <id>1</id>\n"
+                + "    <value>test</value>\n"
+                + "</org.apache.camel.itest.osgi.xstream.SampleObject>";
+
+        ProducerTemplate template = ctx.createProducerTemplate();
+        template.sendBody("direct:start", body);
+        mock.assertIsSatisfied();
+        Object result = mock.getReceivedExchanges().get(0).getIn().getBody();
+        assertEquals(new SampleObject("1"), result);
+    }
+
+    @Configuration
+    public static Option[] configure() throws Exception {
+
+        Option[] options = combine(
+                getDefaultCamelKarafOptions(),
+                new Customizer() {
+                    @Override
+                    public InputStream customizeTestProbe(InputStream testProbe) {
+                        return modifyBundle(testProbe)
+                                .add(SampleObject.class)
+                                .add(XstreamRouteBuilder.class)
+                                .add("OSGI-INF/blueprint/test.xml", XstreamBlueprintRouteTest.class.getResource("blueprintCamelContext.xml"))
+                                .set(Constants.BUNDLE_SYMBOLICNAME, "CamelBlueprintXstreamTestBundle")
+                                .set(Constants.DYNAMICIMPORT_PACKAGE, "*")
+                                .build();
+                    }
+                },
+                // using the features to install the camel components
+                scanFeatures(getCamelKarafFeatureUrl(), "xml-specs-api", "camel-blueprint", "camel-xstream"),
+                //vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
+                felix(), equinox());
+
+        return options;
+    }
+
+}
