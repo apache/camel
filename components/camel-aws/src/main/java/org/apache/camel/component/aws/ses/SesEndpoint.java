78e7407286583b62636c221e6f1ebd364b9225ed
Christian Mueller <cmueller@apache.org>
2011-12-18 11:59:10 +0000

    CAMEL-3591: Add Simple Email Service SES to camel-aws Thanks Bilgin for the patch
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1220370 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/ses/SesEndpoint.java
@@ -0,0 +1,70 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.ses;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.impl.DefaultEndpoint;
+
+/**
+ * Defines the <a href="http://camel.apache.org/aws.html">AWS SES Endpoint</a>.  
+ *
+ */
+public class SesEndpoint extends DefaultEndpoint {
+    
+    private SesConfiguration configuration;
+
+    public SesEndpoint(String uri, CamelContext context, SesConfiguration configuration) {
+        super(uri, context);
+        this.configuration = configuration;
+    }
+
+    public Consumer createConsumer(Processor processor) throws Exception {
+        throw new UnsupportedOperationException("You cannot receive messages from this endpoint");
+    }
+
+    public Producer createProducer() throws Exception {
+        return new SesProducer(this);
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public SesConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public AmazonSimpleEmailService getSESClient() {
+        return configuration.getAmazonSESClient() != null
+                ? configuration.getAmazonSESClient()
+                : createSESClient();
+    }
+
+    private AmazonSimpleEmailService createSESClient() {
+        AWSCredentials credentials = new BasicAWSCredentials(configuration.getAccessKey(), configuration.getSecretKey());
+        AmazonSimpleEmailService client = new AmazonSimpleEmailServiceClient(credentials);
+        configuration.setAmazonSESClient(client);
+        return client;
+    }
+}
