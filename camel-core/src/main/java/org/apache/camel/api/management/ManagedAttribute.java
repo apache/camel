e385e4465239a42f6fc8bb070af98d538b261caf
Claus Ibsen <davsclaus@apache.org>
2011-12-19 08:20:27 +0000

    CAMEL-4792: Using @Inherited on JMX annotations ensures mbean assembler can detect the details from the xxxMBean interfaces, and mixin with custom mbeans.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1220644 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/api/management/ManagedAttribute.java
+++ camel-core/src/main/java/org/apache/camel/api/management/ManagedAttribute.java
@@ -18,6 +18,7 @@ package org.apache.camel.api.management;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -27,6 +28,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Documented
 public @interface ManagedAttribute {
 
