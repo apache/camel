16e1a9ff359b0d9e5f81df4acd33cf364274276e
Willem Ning Jiang <ningjiang@apache.org>
2012-01-05 10:34:25 +0000

    CAMEL-4017 Added camel-twitter component into trunk
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1227546 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-twitter/src/main/java/org/apache/camel/component/twitter/consumer/timeline/RetweetsConsumer.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.component.twitter.consumer.timeline;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.camel.component.twitter.TwitterEndpoint;
+import org.apache.camel.component.twitter.consumer.Twitter4JConsumer;
+import org.apache.camel.component.twitter.data.Status;
+import org.apache.camel.component.twitter.util.TwitterConverter;
+
+import twitter4j.Paging;
+import twitter4j.TwitterException;
+
+/**
+ * Consumes a user's tweets that have been retweeted
+ * 
+ */
+public class RetweetsConsumer implements Twitter4JConsumer {
+
+    TwitterEndpoint te;
+
+    public RetweetsConsumer(TwitterEndpoint te) {
+        this.te = te;
+    }
+
+    public Iterator<Status> requestPollingStatus(long lastStatusUpdateId) throws TwitterException {
+        List<twitter4j.Status> statusList = te.getTwitter().getRetweetsOfMe(new Paging(lastStatusUpdateId));
+        return TwitterConverter.convertStatuses(statusList).iterator();
+    }
+
+    public Iterator<Status> requestDirectStatus() throws TwitterException {
+        List<twitter4j.Status> statusList = te.getTwitter().getRetweetsOfMe();
+        return TwitterConverter.convertStatuses(statusList).iterator();
+    }
+}
---
+++ components/camel-twitter/src/main/java/org/apache/camel/component/twitter/producer/UserProducer.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.component.twitter.producer;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.component.twitter.TwitterEndpoint;
+import org.apache.camel.impl.DefaultProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Produces text as a status update.
+ * 
+ */
+public class UserProducer extends DefaultProducer implements Processor {
+
+    private static final transient Logger LOG = LoggerFactory.getLogger(UserProducer.class);
+
+    private TwitterEndpoint te;
+
+    public UserProducer(TwitterEndpoint te) {
+        super(te);
+        this.te = te;
+    }
+
+    public void process(Exchange exchange) throws Exception {
+        // update user's status
+        String status = exchange.getIn().getBody(String.class);
+
+        if (status.length() > 160) {
+            LOG.warn("Message is longer than 160 characteres and it will be truncated!");
+            status.substring(0, 160);
+        }
+
+        te.getTwitter().updateStatus(status);
+        LOG.debug("Successfully updated status with '{0}'", status);
+    }
+}
