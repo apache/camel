463afef9a6700958700a85c4aace574dba6b46f2
Christian Mueller <cmueller@apache.org>
2012-01-06 20:53:50 +0000

    CAMEL-4734: Consolidate the database vendors in our unit tests
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.9.x@1228396 13f79535-47bb-0310-9956-ffa450edef68

--- tests/camel-itest/src/test/resources/sql/init.sql
+++ tests/camel-itest/src/test/resources/sql/init.sql
@@ -15,13 +15,4 @@
 -- limitations under the License.
 -- ------------------------------------------------------------------------
 
-CREATE TABLE aggregationRepo2 (
-    id varchar(255) NOT NULL,
-    exchange blob NOT NULL,
-    constraint aggregationRepo2_pk PRIMARY KEY (id)
-);
-CREATE TABLE aggregationRepo2_completed (
-    id varchar(255) NOT NULL,
-    exchange blob NOT NULL,
-    constraint aggregationRepo2_completed_pk PRIMARY KEY (id)
-);
+create table ProcessedPayments (paymentIdentifier varchar(24));
