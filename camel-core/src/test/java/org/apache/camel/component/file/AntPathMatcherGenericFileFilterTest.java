98a8875db1735c4a60bf1989fd727217a739baf0
Hadrian Zbarcea <hadrian@apache.org>
2012-01-12 23:25:41 +0000

    Fix failing test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1230837 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/component/file/AntPathMatcherGenericFileFilterTest.java
+++ camel-core/src/test/java/org/apache/camel/component/file/AntPathMatcherGenericFileFilterTest.java
@@ -47,7 +47,7 @@ public class AntPathMatcherGenericFileFilterTest extends ContextTestSupport {
         template.sendBodyAndHeader("file://target/files/ant-path-1/x/y/z", "Hello World", Exchange.FILE_NAME, "report.txt");
 
         MockEndpoint mock = getMockEndpoint("mock:result1");
-        mock.expectedBodiesReceived("Hello World");
+        mock.expectedBodiesReceivedInAnyOrder("Hello World");
 
         assertMockEndpointsSatisfied();
         oneExchangeDone.matchesMockWaitTime();
@@ -59,7 +59,7 @@ public class AntPathMatcherGenericFileFilterTest extends ContextTestSupport {
         template.sendBodyAndHeader("file://target/files/ant-path-2/x/y/z", "Hello World 2", Exchange.FILE_NAME, "report.txt");
 
         MockEndpoint mock = getMockEndpoint("mock:result2");
-        mock.expectedBodiesReceived("Hello World 2");
+        mock.expectedBodiesReceivedInAnyOrder("Hello World 2");
 
         assertMockEndpointsSatisfied();
         oneExchangeDone.matchesMockWaitTime();
@@ -75,7 +75,7 @@ public class AntPathMatcherGenericFileFilterTest extends ContextTestSupport {
         template.sendBodyAndHeader("file://target/files/ant-path-3/x/y/z", "Hello World 6", Exchange.FILE_NAME, "m.bak");
 
         MockEndpoint mock = getMockEndpoint("mock:result3");
-        mock.expectedBodiesReceived("Hello World 2", "Hello World 4");
+        mock.expectedBodiesReceivedInAnyOrder("Hello World 2", "Hello World 4");
 
         assertMockEndpointsSatisfied();
         oneExchangeDone.matchesMockWaitTime();
@@ -88,7 +88,7 @@ public class AntPathMatcherGenericFileFilterTest extends ContextTestSupport {
         template.sendBodyAndHeader("file://target/files/ant-path-4/x/y/z", "Hello World 3", Exchange.FILE_NAME, "c.txt");
 
         MockEndpoint mock = getMockEndpoint("mock:result4");
-        mock.expectedBodiesReceived("Hello World 3");
+        mock.expectedBodiesReceivedInAnyOrder("Hello World 3");
 
         assertMockEndpointsSatisfied();
         oneExchangeDone.matchesMockWaitTime();
