2ef04edfa4f2a9ab64582ae101ba38c13b26befa
Claus Ibsen <davsclaus@apache.org>
2012-01-18 09:02:47 +0000

    Polished
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1232792 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/CamelContext.java
+++ camel-core/src/main/java/org/apache/camel/CamelContext.java
@@ -359,7 +359,11 @@ public interface CamelContext extends SuspendableService, RuntimeConfiguration {
 
     /**
      * Adds a collection of routes to this context using the given builder
-     * to build them
+     * to build them.
+     * <p/>
+     * <b>Important:</b> The added routes will <b>only</b> be started, if {@link CamelContext}
+     * is already started. You may want to check the state of {@link CamelContext} before
+     * adding the routes, using the {@link org.apache.camel.CamelContext#getStatus()} method.
      *
      * @param builder the builder which will create the routes and add them to this context
      * @throws Exception if the routes could not be created for whatever reason
