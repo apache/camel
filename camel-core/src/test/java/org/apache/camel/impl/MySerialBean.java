09e435b5ed9a22a772827f161e38b47a0147f188
Babak Vahdat <bvahdat@apache.org>
2012-01-23 15:14:39 +0000

    Avoid compiler warnings
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1234844 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/impl/MySerialBean.java
+++ camel-core/src/test/java/org/apache/camel/impl/MySerialBean.java
@@ -23,6 +23,7 @@ import java.io.Serializable;
  */
 public class MySerialBean implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private int id;
     private String name;
 
