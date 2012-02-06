ccac51c5746375fef81611586d6ccd406e8d91c7
Hadrian Zbarcea <hadrian@apache.org>
2012-02-06 19:56:01 +0000

    Remove warning. Added serialVersionUID
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1241132 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-apns/src/main/java/org/apache/camel/component/apns/model/InactiveDevice.java
+++ components/camel-apns/src/main/java/org/apache/camel/component/apns/model/InactiveDevice.java
@@ -20,6 +20,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class InactiveDevice implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private String deviceToken;
     private Date date;
