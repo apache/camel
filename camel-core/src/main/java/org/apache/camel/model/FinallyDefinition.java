ad5d97cd7759efd5b0579668f4c0b73e45d2369e
Babak Vahdat <bvahdat@apache.org>
2012-02-15 22:14:48 +0000

    Removed the obsolete "implements Block" by FinallyDefinition as it's already done by ProcessorDefinition.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1244758 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/model/FinallyDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/FinallyDefinition.java
@@ -30,7 +30,7 @@ import org.apache.camel.spi.RouteContext;
  */
 @XmlRootElement(name = "doFinally")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class FinallyDefinition extends OutputDefinition<FinallyDefinition> implements Block {
+public class FinallyDefinition extends OutputDefinition<FinallyDefinition> {
 
     @Override
     public String toString() {
