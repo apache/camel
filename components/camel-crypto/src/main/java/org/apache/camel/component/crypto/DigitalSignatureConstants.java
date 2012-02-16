f321ab89b7fab5fefa3e644644da4d89e1494fd3
Willem Ning Jiang <ningjiang@apache.org>
2012-02-16 07:36:39 +0000

    Fixed the CS error of camel-crypto
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1244869 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-crypto/src/main/java/org/apache/camel/component/crypto/DigitalSignatureConstants.java
+++ components/camel-crypto/src/main/java/org/apache/camel/component/crypto/DigitalSignatureConstants.java
@@ -21,14 +21,13 @@ package org.apache.camel.component.crypto;
  * header keys.
  */
 public final class DigitalSignatureConstants {
-
     public static final String SIGNATURE_PRIVATE_KEY = "CamelSignaturePrivateKey";
     public static final String SIGNATURE_PUBLIC_KEY_OR_CERT = "CamelSignaturePublicKeyOrCert";
     public static final String SIGNATURE = "CamelDigitalSignature";
     public static final String KEYSTORE_ALIAS = "CamelSignatureKeyStoreAlias";
     public static final String KEYSTORE_PASSWORD = "CamelSignatureKeyStorePassword";
-
+    
     private DigitalSignatureConstants() {
-        // Utility class
+        // Helper Class
     }
 }
