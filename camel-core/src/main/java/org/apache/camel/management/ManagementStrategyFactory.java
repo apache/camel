d13af0e3717e7070b523a42ef437064372707f33
Claus Ibsen <davsclaus@apache.org>
2012-02-17 05:02:22 +0000

    Fixed last commit
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1245318 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/management/ManagementStrategyFactory.java
+++ camel-core/src/main/java/org/apache/camel/management/ManagementStrategyFactory.java
@@ -35,9 +35,9 @@ public class ManagementStrategyFactory {
             log.info("JMX is disabled.");
         } else {
             try {
-                ManagedManagementStrategy jmx = new ManagedManagementStrategy(new DefaultManagementAgent(context));
+                answer = new ManagedManagementStrategy(new DefaultManagementAgent(context));
                 // must start it to ensure JMX works and can load needed Spring JARs
-                ServiceHelper.startService(jmx);
+                ServiceHelper.startService(answer);
                 // prefer to have it at first strategy
                 context.getLifecycleStrategies().add(0, new DefaultManagementLifecycleStrategy(context));
                 log.info("JMX enabled.");
