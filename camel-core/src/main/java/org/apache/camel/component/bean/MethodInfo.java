864f8112e9fbe1839d8e35b760d7c9dd47dedd9f
Claus Ibsen <davsclaus@apache.org>
2012-03-07 22:30:44 +0000

    CAMEL-4230: Improved exception message if illegal access invoking a bean. Thanks to Michael Warecki for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1298172 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/component/bean/MethodInfo.java
+++ camel-core/src/main/java/org/apache/camel/component/bean/MethodInfo.java
@@ -336,9 +336,11 @@ public class MethodInfo {
         return Modifier.isStatic(method.getModifiers());
     }
 
-    protected Object invoke(Method mth, Object pojo, Object[] arguments, Exchange exchange) throws IllegalAccessException, InvocationTargetException {
+    protected Object invoke(Method mth, Object pojo, Object[] arguments, Exchange exchange) throws InvocationTargetException {
         try {
             return mth.invoke(pojo, arguments);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeExchangeException("IllegalAccessException occurred invoking method: " + mth + " using arguments: " + Arrays.asList(arguments), exchange, e);
         } catch (IllegalArgumentException e) {
             throw new RuntimeExchangeException("IllegalArgumentException occurred invoking method: " + mth + " using arguments: " + Arrays.asList(arguments), exchange, e);
         }
