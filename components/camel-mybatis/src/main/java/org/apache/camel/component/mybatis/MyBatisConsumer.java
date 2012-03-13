6235e122eaa077d16f3401c339a7eceebb3c4d7f
Christian Mueller <cmueller@apache.org>
2012-03-13 21:27:41 +0000

    CAMEL-5081: org.apache.camel.component.mybatis.MyBatisBatchConsumerTest.testBatchConsumer() fails unpredictable
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1300359 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-mybatis/src/main/java/org/apache/camel/component/mybatis/MyBatisConsumer.java
+++ components/camel-mybatis/src/main/java/org/apache/camel/component/mybatis/MyBatisConsumer.java
@@ -140,7 +140,7 @@ public class MyBatisConsumer extends ScheduledBatchPollingConsumer {
             pendingExchanges = total - index - 1;
 
             // process the current exchange
-            LOG.debug("Processing exchange: {}", exchange);
+            LOG.debug("Processing exchange: {} with properties: {}", exchange, exchange.getProperties());
             getProcessor().process(exchange);
 
             try {
