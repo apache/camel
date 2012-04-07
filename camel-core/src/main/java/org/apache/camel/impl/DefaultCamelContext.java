6f0f01057dbc610ad4b3cdd42aa4a3197cf87bbf
Claus Ibsen <davsclaus@apache.org>
2012-04-07 10:36:28 +0000

    CAMEL-5149: The default injector should perform bean post processing to setup @Produce and the likes on the bean
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1310724 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
+++ camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
@@ -120,7 +120,6 @@ import org.apache.camel.util.CastUtils;
 import org.apache.camel.util.EndpointHelper;
 import org.apache.camel.util.EventHelper;
 import org.apache.camel.util.ObjectHelper;
-import org.apache.camel.util.ReflectionInjector;
 import org.apache.camel.util.ServiceHelper;
 import org.apache.camel.util.StopWatch;
 import org.apache.camel.util.TimeUtils;
@@ -2098,8 +2097,8 @@ public class DefaultCamelContext extends ServiceSupport implements ModelCamelCon
         try {
             return (Injector) finder.newInstance("Injector");
         } catch (NoFactoryAvailableException e) {
-            // lets use the default
-            return new ReflectionInjector();
+            // lets use the default injector
+            return new DefaultInjector(this);
         }
     }
 
