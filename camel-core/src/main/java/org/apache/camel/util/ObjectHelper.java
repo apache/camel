da5e1827cf1c86cb1bddf3886a83736990a36430
Claus Ibsen <davsclaus@apache.org>
2012-04-16 07:53:08 +0000

    CAMEL-5172: Fixed and improved TypeConverter to fail faster by throwing TypeConversionException to the caller. Use the new tryConvertTo API for the old behavior. This would also be more in line what people would expect that exceptions to be propated back as is to the caller.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1326525 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/util/ObjectHelper.java
+++ camel-core/src/main/java/org/apache/camel/util/ObjectHelper.java
@@ -152,7 +152,7 @@ public final class ObjectHelper {
 
         // prefer to coerce to the right hand side at first
         if (rightValue instanceof Comparable) {
-            Object value = converter.convertTo(rightValue.getClass(), leftValue);
+            Object value = converter.tryConvertTo(rightValue.getClass(), leftValue);
             if (value != null) {
                 return ((Comparable) rightValue).compareTo(value) * -1;
             }
@@ -160,7 +160,7 @@ public final class ObjectHelper {
 
         // then fallback to the left hand side
         if (leftValue instanceof Comparable) {
-            Object value = converter.convertTo(leftValue.getClass(), rightValue);
+            Object value = converter.tryConvertTo(leftValue.getClass(), rightValue);
             if (value != null) {
                 return ((Comparable) leftValue).compareTo(value);
             }
