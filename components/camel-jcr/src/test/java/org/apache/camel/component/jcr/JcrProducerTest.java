10f0c88500dcdb4224366f2081f0d32562380a78
Bilgin Ismet Ibryam <bibryam@apache.org>
2012-04-24 22:56:11 +0000

    CAMEL-5155: Support JCR Component as Consumer. Thanks to Woonsan Ko for the contribution
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1330053 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jcr/src/test/java/org/apache/camel/component/jcr/JcrProducerTest.java
+++ components/camel-jcr/src/test/java/org/apache/camel/component/jcr/JcrProducerTest.java
@@ -20,18 +20,13 @@ import javax.jcr.Node;
 import javax.jcr.Repository;
 import javax.jcr.Session;
 import javax.jcr.SimpleCredentials;
-import javax.naming.Context;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.jackrabbit.core.TransientRepository;
 import org.junit.Before;
 import org.junit.Test;
 
-public class JcrRouteTest extends CamelTestSupport {
-
-    private Repository repository;
+public class JcrProducerTest extends JcrRouteTestSupport {
 
     @Override
     @Before
@@ -41,12 +36,12 @@ public class JcrRouteTest extends CamelTestSupport {
     }
 
     @Test
-    public void testJcrRoute() throws Exception {
+    public void testJcrProducer() throws Exception {
         Exchange exchange = createExchangeWithBody("<hello>world!</hello>");
         Exchange out = template.send("direct:a", exchange);
         assertNotNull(out);
         String uuid = out.getOut().getBody(String.class);
-        Session session = repository.login(new SimpleCredentials("user", "pass".toCharArray()));
+        Session session = getRepository().login(new SimpleCredentials("user", "pass".toCharArray()));
         try {
             Node node = session.getNodeByIdentifier(uuid);
             assertNotNull(node);
@@ -66,17 +61,11 @@ public class JcrRouteTest extends CamelTestSupport {
             public void configure() throws Exception {
                 // START SNIPPET: jcr
                 from("direct:a").setProperty(JcrConstants.JCR_NODE_NAME, constant("node"))
-                    .setProperty("my.contents.property", body()).to("jcr://user:pass@repository/home/test");
+                        .setProperty("my.contents.property", body())
+                        .to("jcr://user:pass@repository/home/test");
                 // END SNIPPET: jcr
             }
         };
     }
 
-    @Override
-    protected Context createJndiContext() throws Exception {
-        Context context = super.createJndiContext();
-        repository = new TransientRepository("target/repository.xml", "target/repository");
-        context.bind("repository", repository);
-        return context;
-    }
 }
---
+++ components/camel-jcr/src/test/java/org/apache/camel/component/jcr/JcrRouteTestSupport.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.jcr;
+
+import javax.jcr.Repository;
+import javax.naming.Context;
+
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.jackrabbit.core.TransientRepository;
+import org.junit.Before;
+
+/**
+ * JcrRouteTestSupport
+ * 
+ * @version $Id$
+ */
+public abstract class JcrRouteTestSupport extends CamelTestSupport {
+
+    private Repository repository;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        deleteDirectory("target/repository");
+        super.setUp();
+    }
+
+    protected Repository getRepository() {
+        return repository;
+    }
+
+    @Override
+    protected Context createJndiContext() throws Exception {
+        Context context = super.createJndiContext();
+        repository = new TransientRepository("target/repository.xml", "target/repository");
+        context.bind("repository", repository);
+        return context;
+    }
+}
