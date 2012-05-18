92aa235f44ee0e1fa32bc545a4609d9b7e4f1721
Hadrian Zbarcea <hadrian@apache.org>
2012-05-18 00:28:33 +0000

    CAMEL-5284. Do not close stream after each write
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1339933 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-stream/src/main/java/org/apache/camel/component/stream/StreamEndpoint.java
+++ components/camel-stream/src/main/java/org/apache/camel/component/stream/StreamEndpoint.java
@@ -29,17 +29,20 @@ import org.slf4j.LoggerFactory;
 public class StreamEndpoint extends DefaultEndpoint {
     private static final transient Logger LOG = LoggerFactory.getLogger(StreamEndpoint.class);
 
+    private String url;
+
     private String fileName;
     private boolean scanStream;
     private boolean retry;
     private long scanStreamDelay;
-    private String url;
     private long delay;
     private String encoding;
     private String promptMessage;
     private long promptDelay;
     private long initialPromptDelay = 2000;
     private int groupLines;
+    private int autoCloseCount = 0;
+    
     private Charset charset;
     private GroupStrategy groupStrategy = new DefaultGroupStrategy();
 
@@ -163,6 +166,14 @@ public class StreamEndpoint extends DefaultEndpoint {
         this.groupLines = groupLines;
     }
     
+    public int getAutoCloseCount() {
+        return autoCloseCount;
+    }
+
+    public void setAutoCloseCount(int autoCloseCount) {
+        this.autoCloseCount = autoCloseCount;
+    }
+
     public Charset getCharset() {
         return charset;
     }
@@ -185,5 +196,4 @@ public class StreamEndpoint extends DefaultEndpoint {
 
         return Charset.forName(encoding);
     }
-
 }
