ed67a827cb18cf93047bac111b4f22aa0b307a9b
Claus Ibsen <davsclaus@apache.org>
2012-05-23 17:12:50 +0000

    Fixed test on a slower box
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1341953 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-sql/src/test/java/org/apache/camel/processor/aggregate/jdbc/JdbcAggregateDiscardOnTimeoutTest.java
+++ components/camel-sql/src/test/java/org/apache/camel/processor/aggregate/jdbc/JdbcAggregateDiscardOnTimeoutTest.java
@@ -38,8 +38,8 @@ public class JdbcAggregateDiscardOnTimeoutTest extends AbstractJdbcAggregationTe
         template.sendBodyAndHeader("direct:start", "A", "id", 123);
         template.sendBodyAndHeader("direct:start", "B", "id", 123);
 
-        // wait 3 seconds
-        Thread.sleep(3000);
+        // wait 4 seconds
+        Thread.sleep(4000);
 
         mock.assertIsSatisfied();
 
@@ -63,9 +63,9 @@ public class JdbcAggregateDiscardOnTimeoutTest extends AbstractJdbcAggregationTe
                 from("direct:start")
                         .aggregate(header("id"), new MyAggregationStrategy())
                         .completionSize(3).aggregationRepository(repo)
-                        // use a 3 second timeout
+                        // use a 2 second timeout
                         .completionTimeout(2000)
-                                // and if timeout occurred then just discard the aggregated message
+                        // and if timeout occurred then just discard the aggregated message
                         .discardOnCompletionTimeout()
                         .to("mock:aggregated");
             }
