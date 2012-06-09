cb2ea5638d499fa89cd963e872473abd065e90b6
Willem Ning Jiang <ningjiang@apache.org>
2012-06-09 14:44:59 +0000

    Merged revisions 1348346 via svnmerge from https://svn.apache.org/repos/asf/camel/branches/camel-2.9.x
    ................
      r1348346 | ningjiang | 2012-06-09 18:09:01 +0800 (Sat, 09 Jun 2012) | 9 lines
      
      Merged revisions 1348341 via svnmerge from 
      https://svn.apache.org/repos/asf/camel/trunk
      
      ........
        r1348341 | ningjiang | 2012-06-09 17:24:15 +0800 (Sat, 09 Jun 2012) | 1 line
        
        CAMEL-5348 fix the issue of Uptime
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.8.x@1348419 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/util/TimeUtils.java
+++ camel-core/src/main/java/org/apache/camel/util/TimeUtils.java
@@ -39,7 +39,7 @@ public final class TimeUtils {
      */
     public static String printDuration(double uptime) {
         // Code taken from Karaf
-        // https://svn.apache.org/repos/asf/felix/trunk/karaf/shell/commands/src/main/java/org/apache/felix/karaf/shell/commands/InfoAction.java
+        // https://svn.apache.org/repos/asf/karaf/trunk/shell/commands/src/main/java/org/apache/karaf/shell/commands/impl/InfoAction.java
 
         NumberFormat fmtI = new DecimalFormat("###,###", new DecimalFormatSymbols(Locale.ENGLISH));
         NumberFormat fmtD = new DecimalFormat("###,##0.000", new DecimalFormatSymbols(Locale.ENGLISH));
@@ -66,7 +66,7 @@ public final class TimeUtils {
         }
         uptime /= 24;
         long days = (long) uptime;
-        long hours = (long) ((uptime - days) * 60);
+        long hours = (long) ((uptime - days) * 24);
         String s = fmtI.format(days) + (days > 1 ? " days" : " day");
         if (hours != 0) {
             s += " " + fmtI.format(hours) + (hours > 1 ? " hours" : " hour");
