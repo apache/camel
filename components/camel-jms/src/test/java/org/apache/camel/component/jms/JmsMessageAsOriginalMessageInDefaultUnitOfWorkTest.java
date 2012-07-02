27ddae7ca5778b5f71ee3e2a144ba5d25bdf3e57
Christian Mueller <cmueller@apache.org>
2012-07-02 19:39:18 +0000

    fixed CS failure
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.8.x@1356432 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsMessageAsOriginalMessageInDefaultUnitOfWorkTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsMessageAsOriginalMessageInDefaultUnitOfWorkTest.java
@@ -16,8 +16,6 @@
  */
 package org.apache.camel.component.jms;
 
-import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknowledge;
-
 import javax.jms.ConnectionFactory;
 
 import org.apache.camel.CamelContext;
@@ -26,6 +24,8 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
+
+import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknowledge;
 
 public class JmsMessageAsOriginalMessageInDefaultUnitOfWorkTest extends CamelTestSupport {
 
