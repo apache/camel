8faf0179a8a652dd32bab48ce53d2cc72810139e
Claus Ibsen <davsclaus@apache.org>
2012-07-04 09:16:53 +0000

    Added test based on user forum issue
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1357188 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/csv2/BindyUnmarshalCommaIssueTest.java
+++ components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/csv2/BindyUnmarshalCommaIssueTest.java
@@ -26,7 +26,7 @@ import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 /**
- *                                cd wo
+ *
  */
 public class BindyUnmarshalCommaIssueTest extends CamelTestSupport {
 
