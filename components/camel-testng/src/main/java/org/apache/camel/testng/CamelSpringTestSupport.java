29eccb8283d081bea27a586d371d0121180f2004
Claus Ibsen <davsclaus@apache.org>
2012-08-13 09:05:12 +0000

    CAMEL-5500: Spring test support in camel-testng should use @AfterMethod like its base class does in teardown.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1372330 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-testng/src/main/java/org/apache/camel/testng/CamelSpringTestSupport.java
+++ components/camel-testng/src/main/java/org/apache/camel/testng/CamelSpringTestSupport.java
@@ -30,6 +30,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
 public abstract class CamelSpringTestSupport extends CamelTestSupport {
@@ -80,7 +81,7 @@ public abstract class CamelSpringTestSupport extends CamelTestSupport {
 
 
     @Override
-    @AfterTest
+    @AfterMethod
     public void tearDown() throws Exception {
         super.tearDown();
 
