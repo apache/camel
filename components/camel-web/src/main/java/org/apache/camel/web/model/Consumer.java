31cad4c981ed07b65148422682815a052de7677d
michalwarecki <michal.warecki@gmail.com>
2012-08-14 16:03:45 +0200

    Extended consumer resource

--- components/camel-web/src/main/java/org/apache/camel/web/model/Consumer.java
+++ components/camel-web/src/main/java/org/apache/camel/web/model/Consumer.java
@@ -15,17 +15,73 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Consumer {
 
     @XmlAttribute
-    private String consumerName;
+    private String name;
+
+    private String description;
+
+    private String status;
+
+    private String endpointUri;
+
+    private String routeId;
 
     public void load(CamelManagedBean bean) {
-        consumerName = bean.getName();
+        name = bean.getName();
+        description = bean.getDescription();
+        status = (String) bean.getProperty("State");
+        endpointUri = (String) bean.getProperty("EndpointUri");
+        routeId = (String) bean.getProperty("RouteId");
     }
 
-    public String getConsumerName() {
-        return consumerName;
+    public String getName() {
+        return name;
     }
 
-    public void setConsumerName(String consumerName) {
-        this.consumerName = consumerName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEndpointUri() {
+        return endpointUri;
+    }
+
+    public void setEndpointUri(String endpointUri) {
+        this.endpointUri = endpointUri;
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public boolean isStartable() {
+        if(!status.equals("Started"))
+            return true;
+        return false;
+    }
+
+    public boolean isStoppable() {
+        if(!status.equals("Stopped"))
+            return true;
+        return false;
     }
 }
--- components/camel-web/src/main/java/org/apache/camel/web/resources/ConsumersResource.java
+++ components/camel-web/src/main/java/org/apache/camel/web/resources/ConsumersResource.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -45,6 +47,17 @@ public class ConsumersResource {
 
     public List<Consumer> getConsumers() {
         return getDTO().getConsumers();
+    }
+
+    @Path("{name}/status")
+    public ConsumerResource getConsumerStatus(@PathParam("name") String name) {
+        for(Consumer consumer : getConsumers()) {
+            if(consumer.getName().equals(name)) {
+                return new ConsumerResource(consumer, camelConnection);
+            }
+        }
+
+        return null;
     }
 
 }
