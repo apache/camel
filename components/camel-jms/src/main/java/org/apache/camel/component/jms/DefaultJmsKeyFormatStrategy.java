d453d2d4ac8401e6b0981e4770898faf952f16e6
Claus Ibsen <davsclaus@apache.org>
2012-08-28 13:13:37 +0000

    CAMEL-5396: Improved performance of jms key format strategy. Thanks to Henryk Konsek for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1378124 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/DefaultJmsKeyFormatStrategy.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/DefaultJmsKeyFormatStrategy.java
@@ -16,24 +16,39 @@
  */
 package org.apache.camel.component.jms;
 
+import java.util.regex.Pattern;
+
 /**
  * Default strategy that handles dots and hyphens.
  * <p/>
- * This can be used for sending keys containg package names that is common by Java frameworks.
+ * This can be used for sending keys containing package names that is common by Java frameworks.
  *
  * @version 
  */
 public class DefaultJmsKeyFormatStrategy implements JmsKeyFormatStrategy {
 
+    // use pre compiled patterns as they are faster
+    private static final String DOT = ".";
+    private static final Pattern DOT_PATTERN = Pattern.compile(DOT, Pattern.LITERAL);
+
+    private static final String DOT_REPLACEMENT = "_DOT_";
+    private static final Pattern DOT_REPLACEMENT_PATTERN = Pattern.compile(DOT_REPLACEMENT, Pattern.LITERAL);
+
+    private static final String HYPHEN = "-";
+    private static final Pattern HYPHEN_PATTERN = Pattern.compile(HYPHEN, Pattern.LITERAL);
+
+    private static final String HYPHEN_REPLACEMENT = "_HYPHEN_";
+    private static final Pattern HYPHEN_REPLACEMENT_PATTERN = Pattern.compile(HYPHEN_REPLACEMENT, Pattern.LITERAL);
+
     public String encodeKey(String key) {
-        String answer = key.replace(".", "_DOT_");
-        answer = answer.replaceAll("-", "_HYPHEN_");
+        String answer = DOT_PATTERN.matcher(key).replaceAll(DOT_REPLACEMENT);
+        answer = HYPHEN_PATTERN.matcher(answer).replaceAll(HYPHEN_REPLACEMENT);
         return answer;
     }
 
     public String decodeKey(String key) {
-        String answer = key.replaceAll("_HYPHEN_", "-");
-        answer = answer.replace("_DOT_", ".");
+        String answer = HYPHEN_REPLACEMENT_PATTERN.matcher(key).replaceAll(HYPHEN);
+        answer = DOT_REPLACEMENT_PATTERN.matcher(answer).replaceAll(DOT);
         return answer;
     }
 
