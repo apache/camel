31e7e93ff49ce1da3a53d14bcc149c5486c7cf0f
Claus Ibsen <davsclaus@apache.org>
2012-09-05 13:50:09 +0000

    CAMEL-5570: Fixed issue with onException and using -1 for maximumRedelivieries may cause side effect on other onException. Thanks to Øyvind Ingebrigtsen Øvergaard for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1381183 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/model/OnExceptionDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/OnExceptionDefinition.java
@@ -144,7 +144,7 @@ public class OnExceptionDefinition extends ProcessorDefinition<OnExceptionDefini
             return CamelContextHelper.mandatoryLookup(context, redeliveryPolicyRef, RedeliveryPolicy.class);
         } else if (redeliveryPolicy != null) {
             return redeliveryPolicy.createRedeliveryPolicy(context, parentPolicy);
-        } else if (!outputs.isEmpty() && parentPolicy.getMaximumRedeliveries() > 0) {
+        } else if (!outputs.isEmpty() && parentPolicy.getMaximumRedeliveries() != 0) {
             // if we have outputs, then do not inherit parent maximumRedeliveries
             // as you would have to explicit configure maximumRedeliveries on this onException to use it
             // this is the behavior Camel has always had
