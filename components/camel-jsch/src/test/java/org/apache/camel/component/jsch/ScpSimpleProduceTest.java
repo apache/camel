75ae003647d2ebcf6edd3e51160da32750c43b09
Christian Mueller <cmueller@apache.org>
2012-09-08 21:32:48 +0000

    fixed test which randomly fail on slow CI server
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1382369 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jsch/src/test/java/org/apache/camel/component/jsch/ScpSimpleProduceTest.java
+++ components/camel-jsch/src/test/java/org/apache/camel/component/jsch/ScpSimpleProduceTest.java
@@ -22,10 +22,8 @@ import org.apache.camel.Exchange;
 import org.junit.Assume;
 import org.junit.Test;
 
-/**
- * @version 
- */
 public class ScpSimpleProduceTest extends ScpServerTestSupport {
+    
     @Override
     public boolean isUseRouteBuilder() {
         return false;
@@ -37,9 +35,10 @@ public class ScpSimpleProduceTest extends ScpServerTestSupport {
 
         String uri = getScpUri() + "?username=admin&password=admin&knownHostsFile=" + getKnownHostsFile();
         template.sendBodyAndHeader(uri, "Hello World", Exchange.FILE_NAME, "hello.txt");
+        Thread.sleep(100);
 
-        File file = new File(getScpPath() + "/hello.txt").getAbsoluteFile();
-        assertTrue("File should exist: " + file, file.exists());
+        File file = new File(getScpPath() + "/hello.txt");
+        assertFileExists(file.getAbsolutePath());
         assertEquals("Hello World", context.getTypeConverter().convertTo(String.class, file));
     }
 
@@ -49,9 +48,10 @@ public class ScpSimpleProduceTest extends ScpServerTestSupport {
 
         String uri = getScpUri() + "?username=admin&password=admin&knownHostsFile=" + getKnownHostsFile();
         template.sendBodyAndHeader(uri, "Bye World", Exchange.FILE_NAME, "mysub/bye.txt");
+        Thread.sleep(100);
 
-        File file = new File(getScpPath() + "/mysub/bye.txt").getAbsoluteFile();
-        assertTrue("File should exist: " + file, file.exists());
+        File file = new File(getScpPath() + "/mysub/bye.txt");
+        assertFileExists(file.getAbsolutePath());
         assertEquals("Bye World", context.getTypeConverter().convertTo(String.class, file));
     }
 
@@ -61,9 +61,10 @@ public class ScpSimpleProduceTest extends ScpServerTestSupport {
 
         String uri = getScpUri() + "?username=admin&password=admin&knownHostsFile=" + getKnownHostsFile();
         template.sendBodyAndHeader(uri, "Farewell World", Exchange.FILE_NAME, "mysub/mysubsub/farewell.txt");
+        Thread.sleep(100);
 
-        File file = new File(getScpPath() + "/mysub/mysubsub/farewell.txt").getAbsoluteFile();
-        assertTrue("File should exist: " + file, file.exists());
+        File file = new File(getScpPath() + "/mysub/mysubsub/farewell.txt");
+        assertFileExists(file.getAbsolutePath());
         assertEquals("Farewell World", context.getTypeConverter().convertTo(String.class, file));
     }
 
@@ -73,9 +74,10 @@ public class ScpSimpleProduceTest extends ScpServerTestSupport {
 
         String uri = getScpUri() + "?username=admin&password=admin&chmod=640&knownHostsFile=" + getKnownHostsFile();
         template.sendBodyAndHeader(uri, "Bonjour Monde", Exchange.FILE_NAME, "monde.txt");
+        Thread.sleep(100);
 
-        File file = new File(getScpPath() + "/monde.txt").getAbsoluteFile();
-        assertTrue("File should exist: " + file, file.exists());
+        File file = new File(getScpPath() + "/monde.txt");
+        assertFileExists(file.getAbsolutePath());
         // Mina sshd we use for testing ignores file perms;
         // assertFalse("File should not have execute rights: " + file, file.canExecute());
         assertEquals("Bonjour Monde", context.getTypeConverter().convertTo(String.class, file));
@@ -87,9 +89,10 @@ public class ScpSimpleProduceTest extends ScpServerTestSupport {
 
         String uri = getScpUri() + "?username=admin&privateKeyFile=src/test/resources/camel-key.priv&privateKeyFilePassphrase=password&knownHostsFile=" + getKnownHostsFile();
         template.sendBodyAndHeader(uri, "Hallo Welt", Exchange.FILE_NAME, "welt.txt");
+        Thread.sleep(100);
 
-        File file = new File(getScpPath() + "/welt.txt").getAbsoluteFile();
-        assertTrue("File should exist: " + file, file.exists());
+        File file = new File(getScpPath() + "/welt.txt");
+        assertFileExists(file.getAbsolutePath());
         // Mina sshd we use for testing ignores file perms;
         // assertFalse("File should not have execute rights: " + file, file.canExecute());
         assertEquals("Hallo Welt", context.getTypeConverter().convertTo(String.class, file));
