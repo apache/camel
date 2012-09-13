c41ec3a94d45163e2f6f59dab4b3add95b4473e1
Claus Ibsen <davsclaus@apache.org>
2012-09-13 08:22:45 +0000

    Added test based on user forum issue
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1384228 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FromFilePercentSignInPasswordTest.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.file.remote;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Test;
+
+/**
+ * @version 
+ */
+public class FromFilePercentSignInPasswordTest extends FtpServerTestSupport {
+
+    protected String getFtpUrl() {
+        return "ftp://localhost:" + getPort() + "/tmp3/camel?username=us@r&password=t%25st&consumer.initialDelay=3000";
+    }
+
+    @Test
+    public void testFromFileToFtp() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(2);
+
+        assertMockEndpointsSatisfied();
+    }
+
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                from(getFtpUrl()).to("mock:result");
+
+                from("file:src/main/data?noop=true&consumer.delay=3000").to(getFtpUrl());
+            }
+        };
+    }
+}
