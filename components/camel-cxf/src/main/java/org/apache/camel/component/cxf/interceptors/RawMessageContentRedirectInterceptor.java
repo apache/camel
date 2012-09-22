9b8825b5c06cbf4f37b8a4ff6e37060f0d532c37
Daniel Kulp <dkulp@apache.org>
2012-09-22 12:40:55 +0000

    Merged revisions 1388790 via  git cherry-pick from https://svn.apache.org/repos/asf/camel/trunk
    ........
      r1388790 | dkulp | 2012-09-22 08:39:58 -0400 (Sat, 22 Sep 2012) | 2 lines
    
      Some optimizations for writing the stream caches out to another stream to avoid some buffer copies
    
    ........
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1388791 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/interceptors/RawMessageContentRedirectInterceptor.java
@@ -20,6 +20,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
+import org.apache.camel.StreamCache;
 import org.apache.camel.util.IOHelper;
 import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.interceptor.Fault;
@@ -48,9 +49,12 @@ public class RawMessageContentRedirectInterceptor extends AbstractPhaseIntercept
         if (null != params) {
             InputStream is = (InputStream)params.get(0);
             OutputStream os = message.getContent(OutputStream.class);
-
             try {
-                IOUtils.copy(is, os);
+                if (is instanceof StreamCache) {
+                    ((StreamCache)is).writeTo(os);
+                } else {
+                    IOUtils.copy(is, os);
+                }
             } catch (Exception e) {
                 throw new Fault(e);
             } finally {
