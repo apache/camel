bbb28b514aadd4fc649dd0b7a47c6ece4d629fa7
Scott England-Sullivan <sully6768@apache.org>
2012-09-24 22:05:46 +0000

    Document clean up and test fix for BatchTransactions
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1389621 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-sjms/src/test/java/org/apache/camel/component/sjms/tx/BatchTransactedTopicConsumerTest.java
+++ components/camel-sjms/src/test/java/org/apache/camel/component/sjms/tx/BatchTransactedTopicConsumerTest.java
@@ -46,7 +46,7 @@ public class BatchTransactedTopicConsumerTest extends CamelTestSupport {
         // We should get two sets of 10 messages.  10 before the rollback and 10 after the rollback.
         getMockEndpoint("mock:test.before.1").expectedMessageCount(10);
         getMockEndpoint("mock:test.before.2").expectedMessageCount(10);
-        getMockEndpoint("mock:test.after.1").expectedMinimumMessageCount(10);
+        getMockEndpoint("mock:test.after.1").expectedMessageCount(10);
         getMockEndpoint("mock:test.after.2").expectedMessageCount(10);
 
         // Send only 10 messages
@@ -101,7 +101,7 @@ public class BatchTransactedTopicConsumerTest extends CamelTestSupport {
                                         // Try failing in two places to
                                         // ensure we still get the number of messages that
                                         // we expect across the topics
-                                        if (body.endsWith("6")) {
+                                        if (body.endsWith("6") || body.endsWith("10")) {
                                             log.info("5th message received.  Rolling back.");
                                             exchange.getOut().setFault(true);
                                             exchange.getOut().setBody("5th message received.  Rolling back.");
@@ -129,7 +129,7 @@ public class BatchTransactedTopicConsumerTest extends CamelTestSupport {
                                         // Try failing in two places to
                                         // ensure we still get the number of messages that
                                         // we expect across the topics
-                                        if (body.endsWith("3") || body.endsWith("7")) {
+                                        if (body.endsWith("3") || body.endsWith("10")) {
                                             log.info("5th message received.  Rolling back.");
                                             exchange.getOut().setFault(true);
                                             exchange.getOut().setBody("5th message received.  Rolling back.");
