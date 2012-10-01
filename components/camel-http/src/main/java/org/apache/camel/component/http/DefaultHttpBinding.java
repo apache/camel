c1a63825b0f7c20ca254a5e585d37b0cd0bdedf5
Willem Ning Jiang <ningjiang@apache.org>
2012-10-01 03:32:59 +0000

    CAMEL-5668 fix the soap fault response code issue with thanks to James
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1392188 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-http/src/main/java/org/apache/camel/component/http/DefaultHttpBinding.java
+++ components/camel-http/src/main/java/org/apache/camel/component/http/DefaultHttpBinding.java
@@ -254,6 +254,7 @@ public class DefaultHttpBinding implements HttpBinding {
     }
 
     public void doWriteFaultResponse(Message message, HttpServletResponse response, Exchange exchange) throws IOException {
+        message.setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
         doWriteResponse(message, response, exchange);
     }
 
