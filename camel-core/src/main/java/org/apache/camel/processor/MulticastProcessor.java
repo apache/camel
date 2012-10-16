91e273ba9914467b9e217bd497b4d974993f093d
Willem Ning Jiang <ningjiang@apache.org>
2012-10-16 14:25:59 +0000

    CAMEL-5704 Fixed the Split inside Split - Parallel processing issue
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1398814 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/MulticastProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/MulticastProcessor.java
@@ -1002,6 +1002,10 @@ public class MulticastProcessor extends ServiceSupport implements AsyncProcessor
         Map<Object, AggregationStrategy> map = CastUtils.cast(property);
         if (map == null) {
             map = new HashMap<Object, AggregationStrategy>();
+        } else {
+            // it is not safe to use the map directly as the exchange doesn't have the deep copy of it's properties
+            // we just create a new copy if we need to change the map
+            map = new HashMap<Object, AggregationStrategy>(map);
         }
         // store the strategy using this processor as the key
         // (so we can store multiple strategies on the same exchange)
