6a89e03ca6455ac7bf12f9503b3ed99fc12653e8
Claus Ibsen <davsclaus@apache.org>
2012-10-17 16:43:15 +0000

    CAMEL-5608: Ensure validator component closes input stream after usage. Align code with the xlst/xpath is doing. Added option to fail if no body or not.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1399335 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/validation/ValidatingProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/validation/ValidatingProcessor.java
@@ -18,27 +18,39 @@ package org.apache.camel.processor.validation;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.Collections;
-
 import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stax.StAXSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.w3c.dom.Node;
 import org.w3c.dom.ls.LSResourceResolver;
-
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.ExpectedBodyTypeException;
 import org.apache.camel.Processor;
+import org.apache.camel.RuntimeTransformException;
+import org.apache.camel.TypeConverter;
+import org.apache.camel.converter.jaxp.XmlConverter;
+import org.apache.camel.util.IOHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A processor which validates the XML version of the inbound message body
@@ -47,7 +59,8 @@ import org.apache.camel.Processor;
  * @version
  */
 public class ValidatingProcessor implements Processor {
-    // for lazy creation of the Schema
+    private static final Logger LOG = LoggerFactory.getLogger(ValidatingProcessor.class);
+    private XmlConverter converter = new XmlConverter();
     private String schemaLanguage = XMLConstants.W3C_XML_SCHEMA_NS_URI;
     private Schema schema;
     private Source schemaSource;
@@ -58,6 +71,7 @@ public class ValidatingProcessor implements Processor {
     private boolean useDom;
     private boolean useSharedSchema = true;
     private LSResourceResolver resourceResolver;
+    private boolean failOnNullBody = true;
 
     public void process(Exchange exchange) throws Exception {
         Schema schema;
@@ -69,39 +83,59 @@ public class ValidatingProcessor implements Processor {
 
         Validator validator = schema.newValidator();
 
-        Source source;
-        Result result;
+        // the underlying input stream, which we need to close to avoid locking files or other resources
+        Source source = null;
+        InputStream is = null;
         try {
-            if (useDom) {
-                source = exchange.getIn().getBody(DOMSource.class);
-                result = new DOMResult();
+            Result result = null;
+            // only convert to input stream if really needed
+            if (isInputStreamNeeded(exchange)) {
+                is = exchange.getIn().getBody(InputStream.class);
+                if (is != null) {
+                    source = getSource(exchange, is);
+                }
             } else {
-                source = exchange.getIn().getBody(SAXSource.class);
-                result = new SAXResult();
+                Object body = exchange.getIn().getBody();
+                if (body != null) {
+                    source = getSource(exchange, body);
+                }
             }
-        } catch (Exception e) {
-            throw new NoXmlBodyValidationException(exchange, e);
-        }
-        if (source == null) {
-            throw new NoXmlBodyValidationException(exchange);
-        }
 
-        // create a new errorHandler and set it on the validator
-        // must be a local instance to avoid problems with concurrency (to be
-        // thread safe)
-        ValidatorErrorHandler handler = errorHandler.getClass().newInstance();
-        validator.setErrorHandler(handler);
+            if (source == null && isFailOnNullBody()) {
+                throw new NoXmlBodyValidationException(exchange);
+            }
 
-        try {
-            validator.validate(source, result);
-        } catch (SAXParseException e) {
-            // can be thrown for non well formed XML
-            throw new SchemaValidationException(exchange, schema, Collections.singletonList(e),
-                    Collections.<SAXParseException> emptyList(),
-                    Collections.<SAXParseException> emptyList());
+            if (source instanceof DOMSource) {
+                result = new DOMResult();
+            } else if (source instanceof StreamSource) {
+                result = new StreamResult(new StringWriter());
+            } else if (source instanceof SAXSource) {
+                result = new SAXResult();
+            } else if (source instanceof StAXSource) {
+                result = null;
+            }
+
+            if (source != null) {
+                // create a new errorHandler and set it on the validator
+                // must be a local instance to avoid problems with concurrency (to be
+                // thread safe)
+                ValidatorErrorHandler handler = errorHandler.getClass().newInstance();
+                validator.setErrorHandler(handler);
+
+                try {
+                    LOG.trace("Validating {}", source);
+                    validator.validate(source, result);
+                    handler.handleErrors(exchange, schema, result);
+                } catch (SAXParseException e) {
+                    // can be thrown for non well formed XML
+                    throw new SchemaValidationException(exchange, schema, Collections.singletonList(e),
+                            Collections.<SAXParseException> emptyList(),
+                            Collections.<SAXParseException> emptyList());
+                }
+            }
+        } finally {
+            IOHelper.close(is);
         }
-
-        handler.handleErrors(exchange, schema, result);
     }
 
     public void loadSchema() throws Exception {
@@ -177,17 +211,17 @@ public class ValidatingProcessor implements Processor {
         this.errorHandler = errorHandler;
     }
 
+    @Deprecated
     public boolean isUseDom() {
         return useDom;
     }
 
     /**
-     * Sets whether DOMSource and DOMResult should be used, or SaxSource and
-     * SaxResult.
-     * 
-     * @param useDom
-     *            true to use DOM otherwise Sax is used
+     * Sets whether DOMSource and DOMResult should be used.
+     *
+     * @param useDom true to use DOM otherwise
      */
+    @Deprecated
     public void setUseDom(boolean useDom) {
         this.useDom = useDom;
     }
@@ -206,6 +240,14 @@ public class ValidatingProcessor implements Processor {
 
     public void setResourceResolver(LSResourceResolver resourceResolver) {
         this.resourceResolver = resourceResolver;
+    }
+
+    public boolean isFailOnNullBody() {
+        return failOnNullBody;
+    }
+
+    public void setFailOnNullBody(boolean failOnNullBody) {
+        this.failOnNullBody = failOnNullBody;
     }
 
     // Implementation methods
@@ -236,4 +278,96 @@ public class ValidatingProcessor implements Processor {
         }
         return factory.newSchema(getSchemaSource());
     }
+
+    /**
+     * Checks whether we need an {@link InputStream} to access the message body.
+     * <p/>
+     * Depending on the content in the message body, we may not need to convert
+     * to {@link InputStream}.
+     *
+     * @param exchange the current exchange
+     * @return <tt>true</tt> to convert to {@link InputStream} beforehand converting to {@link Source} afterwards.
+     */
+    protected boolean isInputStreamNeeded(Exchange exchange) {
+        Object body = exchange.getIn().getBody();
+        if (body == null) {
+            return false;
+        }
+
+        if (body instanceof InputStream) {
+            return true;
+        } else if (body instanceof Source) {
+            return false;
+        } else if (body instanceof String) {
+            return false;
+        } else if (body instanceof byte[]) {
+            return false;
+        } else if (body instanceof Node) {
+            return false;
+        } else if (exchange.getContext().getTypeConverterRegistry().lookup(Source.class, body.getClass()) != null) {
+            //there is a direct and hopefully optimized converter to Source
+            return false;
+        }
+        // yes an input stream is needed
+        return true;
+    }
+
+    /**
+     * Converts the inbound body to a {@link Source}, if the body is <b>not</b> already a {@link Source}.
+     * <p/>
+     * This implementation will prefer to source in the following order:
+     * <ul>
+     *   <li>DOM - DOM if explicit configured to use DOM</li>
+     *   <li>SAX - SAX as 2nd choice</li>
+     *   <li>Stream - Stream as 3rd choice</li>
+     *   <li>DOM - DOM as 4th choice</li>
+     * </ul>
+     */
+    protected Source getSource(Exchange exchange, Object body) {
+        if (isUseDom()) {
+            // force DOM
+            return exchange.getContext().getTypeConverter().tryConvertTo(DOMSource.class, exchange, body);
+        }
+
+        // body may already be a source
+        if (body instanceof Source) {
+            return (Source) body;
+        }
+        Source source = null;
+        if (body instanceof InputStream) {
+            return new StreamSource((InputStream)body);
+        }
+        if (body != null) {
+            TypeConverter tc = exchange.getContext().getTypeConverterRegistry().lookup(Source.class, body.getClass());
+            if (tc != null) {
+                source = tc.convertTo(Source.class, body);
+            }
+        }
+
+        if (source == null) {
+            // then try SAX
+            source = exchange.getContext().getTypeConverter().tryConvertTo(SAXSource.class, exchange, body);
+        }
+        if (source == null) {
+            // then try stream
+            source = exchange.getContext().getTypeConverter().tryConvertTo(StreamSource.class, exchange, body);
+        }
+        if (source == null) {
+            // and fallback to DOM
+            source = exchange.getContext().getTypeConverter().tryConvertTo(DOMSource.class, exchange, body);
+        }
+        if (source == null) {
+            if (isFailOnNullBody()) {
+                throw new ExpectedBodyTypeException(exchange, Source.class);
+            } else {
+                try {
+                    source = converter.toDOMSource(converter.createDocument());
+                } catch (ParserConfigurationException e) {
+                    throw new RuntimeTransformException(e);
+                }
+            }
+        }
+        return source;
+    }
+
 }
