73cb873cc82536ba4503d8b11618747cf4573597
Claus Ibsen <davsclaus@apache.org>
2012-10-17 16:08:23 +0000

    CAMEL-5707: NotifyBuilder is now thread safe.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.9.x@1399325 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/builder/NotifyBuilder.java
+++ camel-core/src/main/java/org/apache/camel/builder/NotifyBuilder.java
@@ -444,7 +444,7 @@ public class NotifyBuilder {
             @Override
             public boolean onExchangeCompleted(Exchange exchange) {
                 if (exchange.getExchangeId().equals(id)) {
-                    done.set(false);
+                    done.set(true);
                 }
                 return true;
             }
