0462e8a901500cc064ecf0bd73e549ed786dd80d
Claus Ibsen <davsclaus@apache.org>
2012-10-31 09:47:07 +0000

    CAMEL-5750: Fixed test on slow CI servers
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1404061 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/component/file/FileConcurrentWriteAppendSameFileTest.java
+++ camel-core/src/test/java/org/apache/camel/component/file/FileConcurrentWriteAppendSameFileTest.java
@@ -28,7 +28,7 @@ import org.apache.camel.component.mock.MockEndpoint;
 
 public class FileConcurrentWriteAppendSameFileTest extends ContextTestSupport {
 
-    private final int size = 500;
+    private final int size = 100;
 
     @Override
     protected void setUp() throws Exception {
@@ -52,12 +52,12 @@ public class FileConcurrentWriteAppendSameFileTest extends ContextTestSupport {
         mock.setResultWaitTime(30000);
 
         // we need to wait a bit for our slow CI server to make sure the entire file is written on disc
-        Thread.sleep(100);
+        Thread.sleep(1000);
         context.startRoute("foo");
 
         assertMockEndpointsSatisfied();
 
-        // check the file has 500 lines
+        // check the file has correct number of lines
         String txt = context.getTypeConverter().convertTo(String.class, new File("target/concurrent/outbox/result.txt"));
         assertNotNull(txt);
 
