5da9743d060c5e4b3cc435c73d54beb099f9a03b
Claus Ibsen <davsclaus@apache.org>
2012-11-10 11:37:31 +0000

    Fixed test
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1407769 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/component/file/FileConsumerDeleteAndFailureTest.java
+++ camel-core/src/test/java/org/apache/camel/component/file/FileConsumerDeleteAndFailureTest.java
@@ -37,7 +37,7 @@ public class FileConsumerDeleteAndFailureTest extends ContextTestSupport {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedBodiesReceived("Hello World IS processed!");
         
-        mock.expectedFileExists("target/failed/error/bye.txt", "Kabom");
+        mock.expectedFileExists("target/failed/error/bye.txt");
 
         template.sendBodyAndHeader("file://target/failed", "Hello World", Exchange.FILE_NAME, "hello.txt");
         template.sendBodyAndHeader("file://target/failed", "Kabom", Exchange.FILE_NAME, "bye.txt");
