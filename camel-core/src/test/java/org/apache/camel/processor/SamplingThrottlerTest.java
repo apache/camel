9771ba62390a56363b20e52f12a8b9b34c51da62
Claus Ibsen <davsclaus@apache.org>
2012-11-11 13:59:09 +0000

    Fixed test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1407987 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/processor/SamplingThrottlerTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/SamplingThrottlerTest.java
@@ -37,7 +37,7 @@ public class SamplingThrottlerTest extends ContextTestSupport {
 
     public void testSamplingFromExchangeStream() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
-        mock.expectedMessageCount(2);
+        mock.expectedMinimumMessageCount(2);
         mock.setResultWaitTime(3000);
 
         List<Exchange> sentExchanges = new ArrayList<Exchange>();
@@ -49,7 +49,7 @@ public class SamplingThrottlerTest extends ContextTestSupport {
 
     public void testBurstySampling() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
-        mock.expectedMessageCount(2);
+        mock.expectedMinimumMessageCount(2);
         mock.setResultWaitTime(3000);
 
         List<Exchange> sentExchanges = new ArrayList<Exchange>();
@@ -67,7 +67,7 @@ public class SamplingThrottlerTest extends ContextTestSupport {
 
     public void testSendLotsOfMessagesSimultaneouslyButOnly3GetThrough() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
-        mock.expectedMessageCount(3);
+        mock.expectedMinimumMessageCount(3);
         mock.setResultWaitTime(4000);
 
         final List<Exchange> sentExchanges = Collections.synchronizedList(new ArrayList<Exchange>());
@@ -91,7 +91,7 @@ public class SamplingThrottlerTest extends ContextTestSupport {
     public void testSamplingUsingmessageFrequency() throws Exception {
         long totalMessages = 100;
         MockEndpoint mock = getMockEndpoint("mock:result");
-        mock.expectedMessageCount(10);
+        mock.expectedMinimumMessageCount(10);
         mock.setResultWaitTime(100);
 
         for (int i = 0; i < totalMessages; i++) {
@@ -104,7 +104,7 @@ public class SamplingThrottlerTest extends ContextTestSupport {
     public void testSamplingUsingmessageFrequencyViaDSL() throws Exception {
         long totalMessages = 50;
         MockEndpoint mock = getMockEndpoint("mock:result");
-        mock.expectedMessageCount(10);
+        mock.expectedMinimumMessageCount(10);
         mock.setResultWaitTime(100);
 
         for (int i = 0; i < totalMessages; i++) {
