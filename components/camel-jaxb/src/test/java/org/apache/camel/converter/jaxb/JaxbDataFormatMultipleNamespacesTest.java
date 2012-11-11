c071cf1699ed9ccb0c4520acad9f6c775016ab56
Claus Ibsen <davsclaus@apache.org>
2012-11-11 13:48:35 +0000

    Fixed test
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1407986 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jaxb/src/test/java/org/apache/camel/converter/jaxb/JaxbDataFormatMultipleNamespacesTest.java
+++ components/camel-jaxb/src/test/java/org/apache/camel/converter/jaxb/JaxbDataFormatMultipleNamespacesTest.java
@@ -51,8 +51,12 @@ public class JaxbDataFormatMultipleNamespacesTest extends CamelTestSupport {
         assertMockEndpointsSatisfied();
 
         String payload = mockMarshall.getExchanges().get(0).getIn().getBody(String.class);
+        log.info(payload);
+
         assertTrue(payload.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"));
-        assertTrue(payload.contains("<order:order xmlns:address=\"http://www.camel.apache.org/jaxb/example/address/1\" xmlns:order=\"http://www.camel.apache.org/jaxb/example/order/1\">"));
+        assertTrue(payload.contains("<order:order"));
+        assertTrue(payload.contains("xmlns:address=\"http://www.camel.apache.org/jaxb/example/address/1\""));
+        assertTrue(payload.contains("xmlns:order=\"http://www.camel.apache.org/jaxb/example/order/1\">"));
         assertTrue(payload.contains("<order:id>1</order:id>"));
         assertTrue(payload.contains("<address:address>"));
         assertTrue(payload.contains("<address:street>Main Street</address:street>"));
