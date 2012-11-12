083c893f8e6cbcd57b55d178e4c4473e7bbfa3ce
Claus Ibsen <davsclaus@apache.org>
2012-11-12 09:14:44 +0000

    Fixed test
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1408195 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-jaxb/src/test/java/org/apache/camel/converter/jaxb/JaxbDataFormatPartClassTest.java
+++ components/camel-jaxb/src/test/java/org/apache/camel/converter/jaxb/JaxbDataFormatPartClassTest.java
@@ -45,15 +45,18 @@ public class JaxbDataFormatPartClassTest extends CamelTestSupport {
 
         String payload = mockMarshall.getExchanges().get(0).getIn().getBody(String.class);
         log.info(payload);
+
         assertTrue(payload.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"));
         assertTrue(payload.contains("<address:address"));
-        assertTrue(payload.contains("xmlns:address=\"http://www.camel.apache.org/jaxb/example/address/1\""));
-        assertTrue(payload.contains("xmlns:order=\"http://www.camel.apache.org/jaxb/example/order/1\">"));
         assertTrue(payload.contains("<address:street>Main Street</address:street>"));
         assertTrue(payload.contains("<address:streetNumber>3a</address:streetNumber>"));
         assertTrue(payload.contains("<address:zip>65843</address:zip>"));
         assertTrue(payload.contains("<address:city>Sulzbach</address:city>"));
         assertTrue(payload.contains("</address:address>"));
+
+        // the namespaces
+        assertTrue(payload.contains("http://www.camel.apache.org/jaxb/example/address/1"));
+        assertTrue(payload.contains("http://www.camel.apache.org/jaxb/example/order/1"));
     }
 
     @Override
