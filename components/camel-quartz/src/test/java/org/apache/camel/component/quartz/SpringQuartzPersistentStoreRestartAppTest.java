519d4536929c24fca55ce4267261b24bd2cf6424
Claus Ibsen <davsclaus@apache.org>
2012-11-12 14:38:29 +0000

    Fixed test
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x@1408308 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-quartz/src/test/java/org/apache/camel/component/quartz/SpringQuartzPersistentStoreRestartAppTest.java
+++ components/camel-quartz/src/test/java/org/apache/camel/component/quartz/SpringQuartzPersistentStoreRestartAppTest.java
@@ -39,7 +39,7 @@ public class SpringQuartzPersistentStoreRestartAppTest extends TestSupport {
         assertNotNull(camel);
 
         MockEndpoint mock = camel.getEndpoint("mock:result", MockEndpoint.class);
-        mock.expectedMinimumMessageCount(3);
+        mock.expectedMinimumMessageCount(2);
 
         mock.assertIsSatisfied();
 
@@ -65,7 +65,7 @@ public class SpringQuartzPersistentStoreRestartAppTest extends TestSupport {
         assertNotNull(camel2);
 
         MockEndpoint mock2 = camel2.getEndpoint("mock:result", MockEndpoint.class);
-        mock2.expectedMinimumMessageCount(3);
+        mock2.expectedMinimumMessageCount(2);
 
         mock2.assertIsSatisfied();
 
