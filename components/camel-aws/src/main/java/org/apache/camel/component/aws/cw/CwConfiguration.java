6636bfabe39ab322e6c3dab48e2b819fa2eeb5cd
Bilgin Ismet Ibryam <bibryam@apache.org>
2012-11-15 11:47:47 +0000

    Added Amazon CloudWatch component with a producer
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1409743 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/cw/CwConfiguration.java
@@ -0,0 +1,121 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.cw;
+
+/**
+ * The AWS CW component configuration properties
+ * 
+ */
+import java.util.Date;
+
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+
+public class CwConfiguration implements Cloneable {
+
+    private AmazonCloudWatchClient amazonCwClient;
+    private String amazonCwEndpoint;
+    private String accessKey;
+    private String secretKey;
+    private String name;
+    private Double value;
+    private String unit;
+    private String namespace;
+    private Date timestamp;
+
+    public void setAmazonCwEndpoint(String amazonCwEndpoint) {
+        this.amazonCwEndpoint = amazonCwEndpoint;
+    }
+
+    public String getAmazonCwEndpoint() {
+        return amazonCwEndpoint;
+    }
+
+    public String getAccessKey() {
+        return accessKey;
+    }
+
+    public void setAccessKey(String accessKey) {
+        this.accessKey = accessKey;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "CwConfiguration[name=" + name
+                + ", amazonCwClient=" + amazonCwClient
+                + ", accessKey=" + accessKey
+                + ", secretKey=xxxxxxxxxxxxxxx"
+                + ", value=" + value
+                + ", unit=" + unit
+                + "]";
+    }
+
+    public AmazonCloudWatchClient getAmazonCwClient() {
+        return amazonCwClient;
+    }
+
+    public void setAmazonCwClient(AmazonCloudWatchClient amazonCwClient) {
+        this.amazonCwClient = amazonCwClient;
+    }
+}
---
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/cw/CwEndpoint.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.cw;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.impl.DefaultEndpoint;
+
+/**
+ * Defines the <a href="http://aws.amazon.com/cloudwatch/">AWS CloudWatch Endpoint</a>
+ */
+public class CwEndpoint extends DefaultEndpoint {
+
+    private CwConfiguration configuration;
+    private AmazonCloudWatchClient cloudWatchClient;
+
+    @Deprecated
+    public CwEndpoint(String uri, CamelContext context, CwConfiguration configuration) {
+        super(uri, context);
+        this.configuration = configuration;
+    }
+
+    public CwEndpoint(String uri, Component component, CwConfiguration configuration) {
+        super(uri, component);
+        this.configuration = configuration;
+    }
+
+    public Consumer createConsumer(Processor processor) throws Exception {
+        throw new UnsupportedOperationException("You cannot receive messages from this endpoint");
+    }
+
+    public Producer createProducer() throws Exception {
+        return new CwProducer(this);
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+
+    @Override
+    public void doStart() throws Exception {
+        super.doStart();
+    }
+
+    public CwConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(CwConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    public void setCloudWatchClient(AmazonCloudWatchClient cloudWatchClient) {
+        this.cloudWatchClient = cloudWatchClient;
+    }
+
+    public AmazonCloudWatchClient getCloudWatchClient() {
+        if (cloudWatchClient == null) {
+            cloudWatchClient = configuration.getAmazonCwClient() != null
+                    ? configuration.getAmazonCwClient() : createCloudWatchClient();
+        }
+        return cloudWatchClient;
+    }
+
+    AmazonCloudWatchClient createCloudWatchClient() {
+        AWSCredentials credentials = new BasicAWSCredentials(configuration.getAccessKey(), configuration.getSecretKey());
+        AmazonCloudWatchClient client = new AmazonCloudWatchClient(credentials);
+        if (configuration.getAmazonCwEndpoint() != null) {
+            client.setEndpoint(configuration.getAmazonCwEndpoint());
+        }
+        return client;
+    }
+}
