dcc862482294ef9e80766e7c96ed7ca5cc423860
Babak Vahdat <bvahdat@apache.org>
2012-11-18 19:01:56 +0000

    Merged revisions 1410960 via svnmerge from https://svn.apache.org/repos/asf/camel/branches/camel-2.10.x
    ................
      r1410960 | bvahdat | 2012-11-18 19:59:23 +0100 (So, 18 Nov 2012) | 9 lines
      
      Merged revisions 1410959 via svnmerge from 
      https://svn.apache.org/repos/asf/camel/trunk
      
      ........
        r1410959 | bvahdat | 2012-11-18 19:57:22 +0100 (So, 18 Nov 2012) | 1 line
        
        CAMEL-5796: Move TransactedPropertyPlaceholderIssueTest to the correct package.
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.9.x@1410961 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/test/java/org/apache/camel/issues/TransactedPropertyPlaceholderIssueTest.java
+++ camel-core/src/test/java/org/apache/camel/issues/TransactedPropertyPlaceholderIssueTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.processor.intercept;
+package org.apache.camel.issues;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ContextTestSupport;
