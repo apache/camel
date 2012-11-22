7ab7c33ef8e19c61354ea8b89a605b3ad49b486c
Willem Ning Jiang <ningjiang@apache.org>
2012-11-22 06:30:21 +0000

    CAMEL-5813 Added support to convert from a Flatpack Dataset to an XML Document with thanks to Chris
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1412434 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-flatpack/src/main/java/org/apache/camel/component/flatpack/FlatpackConverter.java
+++ components/camel-flatpack/src/main/java/org/apache/camel/component/flatpack/FlatpackConverter.java
@@ -21,6 +21,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import net.sf.flatpack.DataSet;
 import org.apache.camel.Converter;
 
@@ -55,26 +61,71 @@ public final class FlatpackConverter {
         return answer;
     }
 
+    @Converter
+    public static Document toDocument(DataSet dataSet) throws ParserConfigurationException {
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+
+        if (dataSet.getIndex() == -1) {
+            Element list = doc.createElement("Dataset");
+
+            dataSet.goTop();
+            while (dataSet.next()) {
+                list.appendChild(createDatasetRecord(dataSet, doc));
+            }
+
+            doc.appendChild(list);
+        } else {
+            doc.appendChild(createDatasetRecord(dataSet, doc));
+        }
+
+        return doc;
+    }
+
     /**
      * Puts the values of the dataset into the map
      */
     private static void putValues(Map<String, Object> map, DataSet dataSet) {
-        boolean header = dataSet.isRecordID(FlatpackComponent.HEADER_ID);
-        boolean trailer = dataSet.isRecordID(FlatpackComponent.TRAILER_ID);
-
-        // the columns can vary depending on header, body or trailer
-        String[] columns;
-        if (header) {
-            columns = dataSet.getColumns(FlatpackComponent.HEADER_ID);
-        } else if (trailer) {
-            columns = dataSet.getColumns(FlatpackComponent.TRAILER_ID);
-        } else {
-            columns = dataSet.getColumns();
-        }
+        String[] columns = getColumns(dataSet);
 
         for (String column : columns) {
             String value = dataSet.getString(column);
             map.put(column, value);
+        }
+    }
+
+    private static Element createDatasetRecord(DataSet dataSet, Document doc) {
+        Element record;
+        if (dataSet.isRecordID(FlatpackComponent.HEADER_ID)) {
+            record = doc.createElement("DatasetHeader");
+        } else if (dataSet.isRecordID(FlatpackComponent.TRAILER_ID)) {
+            record = doc.createElement("DatasetTrailer");
+        } else {
+            record = doc.createElement("DatasetRecord");
+        }
+
+        String[] columns = getColumns(dataSet);
+
+        for (String column : columns) {
+            String value = dataSet.getString(column);
+
+            Element columnElement = doc.createElement("Column");
+            columnElement.setAttribute("name", column);
+            columnElement.setTextContent(value);
+
+            record.appendChild(columnElement);
+        }
+
+        return record;
+    }
+
+    private static String[] getColumns(DataSet dataSet) {
+        // the columns can vary depending on header, body or trailer
+        if (dataSet.isRecordID(FlatpackComponent.HEADER_ID)) {
+            return dataSet.getColumns(FlatpackComponent.HEADER_ID);
+        } else if (dataSet.isRecordID(FlatpackComponent.TRAILER_ID)) {
+            return dataSet.getColumns(FlatpackComponent.TRAILER_ID);
+        } else {
+            return dataSet.getColumns();
         }
     }
 }
---
+++ components/camel-flatpack/src/test/java/org/apache/camel/component/flatpack/XMLNoSplitRowsTest.java
@@ -0,0 +1,108 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.flatpack;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Unit test to verify that splitRows=false option works with XML Conversion.
+ *
+ * @version
+ */
+@ContextConfiguration
+public class XMLNoSplitRowsTest extends AbstractJUnit4SpringContextTests {
+
+    private static final transient Logger LOG = LoggerFactory.getLogger(XMLNoSplitRowsTest.class);
+
+    @EndpointInject(uri = "mock:results")
+    protected MockEndpoint results;
+
+    protected String[] expectedFirstName = {"JOHN", "JIMMY", "JANE", "FRED"};
+
+    @Test
+    public void testHeaderAndTrailer() throws Exception {
+        results.expectedMessageCount(1);
+        results.message(0).body().isInstanceOf(Document.class);
+        results.message(0).header("camelFlatpackCounter").isEqualTo(6);
+
+        results.assertIsSatisfied();
+
+        Document data = results.getExchanges().get(0).getIn().getBody(Document.class);
+        Element docElement = data.getDocumentElement();
+        assertEquals("Dataset", docElement.getTagName());
+
+        // assert header
+        Element header = (Element)docElement.getElementsByTagName("DatasetHeader").item(0);
+        NodeList headerNodes = header.getElementsByTagName("Column");
+        for (int i = 0; i < headerNodes.getLength(); i++) {
+            Element column = (Element)headerNodes.item(i);
+            if (column.getAttribute("name").equals("INDICATOR")) {
+                assertEquals("HBT", column.getTextContent());
+            } else if (column.getAttribute("name").equals("DATE")) {
+                assertEquals("20080817", column.getTextContent());
+            } else {
+                fail("Invalid Header Field");
+            }
+        }
+
+        // assert body
+        NodeList list = docElement.getElementsByTagName("DatasetRecord");
+        for (int counter = 0; counter < list.getLength(); counter++) {
+            Element record = (Element)list.item(counter);
+            NodeList columnNodes = record.getElementsByTagName("Column");
+            boolean firstNameFound = false;
+            for (int i = 0; i < columnNodes.getLength(); i++) {
+                Element column = (Element)columnNodes.item(i);
+                if (column.getAttribute("name").equals("FIRSTNAME")) {
+                    assertEquals(expectedFirstName[counter], column.getTextContent());
+                    firstNameFound = true;
+                }
+            }
+            assertTrue(firstNameFound);
+            LOG.info("Result: " + counter + " = " + record);
+        }
+
+        // assert trailer
+        Element trailer = (Element)docElement.getElementsByTagName("DatasetTrailer").item(0);
+        NodeList trailerNodes = trailer.getElementsByTagName("Column");
+        for (int i = 0; i < trailerNodes.getLength(); i++) {
+            Element column = (Element)trailerNodes.item(i);
+            if (column.getAttribute("name").equals("INDICATOR")) {
+                assertEquals("FBT", column.getTextContent());
+            } else if (column.getAttribute("name").equals("STATUS")) {
+                assertEquals("SUCCESS", column.getTextContent());
+            } else {
+                fail("Invalid Trailer Field");
+            }
+        }
+    }
+
+}
