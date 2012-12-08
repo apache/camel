8e8a1ded1f537a69368127ca4c565aeae61c7480
Willem Ning Jiang <ningjiang@apache.org>
2012-12-08 02:43:01 +0000

    CAMEL-4963 revert the change which should not be committed
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1418598 13f79535-47bb-0310-9956-ffa450edef68

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfEndpoint.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfEndpoint.java
@@ -156,7 +156,6 @@ public class CxfEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
     private BindingConfiguration bindingConfig;
     private DataBinding dataBinding;
     private ReflectionServiceFactoryBean serviceFactoryBean;
-    private CxfEndpointConfigurer configurer;
     
 
     public CxfEndpoint(String remaining, CxfComponent cxfComponent) {
@@ -324,9 +323,6 @@ public class CxfEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
 
         sfb.setBus(getBus());
         sfb.setStart(false);
-        if (getCxfEndpointConfigurer() != null) {
-            getCxfEndpointConfigurer().configure(sfb);
-        }
     }
 
     /**
@@ -498,9 +494,6 @@ public class CxfEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
         }
 
         factoryBean.setBus(getBus());
-        if (getCxfEndpointConfigurer() != null) {
-            getCxfEndpointConfigurer().configure(factoryBean);
-        }
     }
 
     // Package private methods
@@ -1081,14 +1074,6 @@ public class CxfEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
 
     public void setServiceFactoryBean(ReflectionServiceFactoryBean serviceFactoryBean) {
         this.serviceFactoryBean = serviceFactoryBean;
-    }
-
-    public CxfEndpointConfigurer getCxfEndpointConfigurer() {
-        return configurer;
-    }
-
-    public void setCxfEndpointConfigurer(CxfEndpointConfigurer configurer) {
-        this.configurer = configurer;
     }
 
     
