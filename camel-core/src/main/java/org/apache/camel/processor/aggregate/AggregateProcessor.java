c648674c94e0e0d15387af35ef082234bec94f41
Hadrian Zbarcea <hadrian@apache.org>
2013-03-05 02:26:20 +0000

    Checkstyle fix in preparation of release
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.9.x@1452638 13f79535-47bb-0310-9956-ffa450edef68

--- camel-core/src/main/java/org/apache/camel/processor/aggregate/AggregateProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/aggregate/AggregateProcessor.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
