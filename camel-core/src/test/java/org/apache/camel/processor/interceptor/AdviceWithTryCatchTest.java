ae190eed0e487cc48d568da9f0c3d27d6b7937f8
Claus Ibsen <davsclaus@apache.org>
2013-04-22 13:46:19 +0000

    CAMEL-6300: Fixed advice with not working with doTry .. doCatch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1470508 13f79535-47bb-0310-9956-ffa450edef68

---
+++ camel-core/src/test/java/org/apache/camel/processor/interceptor/AdviceWithTryCatchTest.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.processor.interceptor;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Advice with try catch
+ */
+public class AdviceWithTryCatchTest extends ContextTestSupport {
+
+    public void testTryCatch() throws Exception {
+        context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                weaveById("foo").replace().process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        throw new IllegalArgumentException("Kaboom");
+                    }
+                });
+            }
+        });
+
+        getMockEndpoint("mock:before").expectedMessageCount(1);
+        getMockEndpoint("mock:after").expectedMessageCount(1);
+        getMockEndpoint("mock:error").expectedMessageCount(1);
+
+        template.sendBody("direct:start", "Hello World");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    .to("mock:before")
+                    .doTry()
+                        .to("mock:foo").id("foo")
+                    .doCatch(Exception.class)
+                        .to("mock:error")
+                    .end()
+                    .to("mock:after");
+            }
+        };
+    }
+}
