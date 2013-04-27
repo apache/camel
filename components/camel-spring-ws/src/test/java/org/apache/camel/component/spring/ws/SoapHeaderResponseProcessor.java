2b6eae23d55c48fc1e9136a87a173dd5c0173cd8
Claus Ibsen <davsclaus@apache.org>
2013-04-27 06:37:33 +0000

    CAMEL-4515: spring-ws now supports SOAP header to/from Camel Message so end users can set/access it. Thanks to Damian for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/branches/camel-2.11.x@1476503 13f79535-47bb-0310-9956-ffa450edef68

---
+++ components/camel-spring-ws/src/test/java/org/apache/camel/component/spring/ws/SoapHeaderResponseProcessor.java
@@ -0,0 +1,32 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.spring.ws;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+/**
+ * Returns the request as the response so it can be analysed (eg. for presence
+ * of SOAP Headers). The SOAP Header will be copied from in to out.
+ */
+public class SoapHeaderResponseProcessor implements Processor {
+
+    public void process(Exchange exchange) throws Exception {
+        exchange.setOut(exchange.getIn());
+    }
+
+}
