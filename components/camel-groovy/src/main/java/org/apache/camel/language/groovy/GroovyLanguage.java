178294d85fbbe61a8a9c9921251035b234347f55
Claus Ibsen <davsclaus@apache.org>
2013-05-08 10:52:57 +0200

    CAMEL-6340: Added cache for compiled groovy scripts to speedy performance. Thanks to Antoine DESSAIGNE for the patch.

--- components/camel-groovy/src/main/java/org/apache/camel/language/groovy/GroovyLanguage.java
+++ components/camel-groovy/src/main/java/org/apache/camel/language/groovy/GroovyLanguage.java
@@ -16,12 +16,17 @@
  */
 package org.apache.camel.language.groovy;
 
+import groovy.lang.Script;
 import org.apache.camel.support.LanguageSupport;
+import org.apache.camel.util.LRUSoftCache;
 
 /**
- * @version 
+ * @version
  */
 public class GroovyLanguage extends LanguageSupport {
+
+    // Cache used to stores the compiled scripts (aka their classes)
+    private final LRUSoftCache<String, Class<Script>> scriptCache = new LRUSoftCache<String, Class<Script>>(1000);
 
     public static GroovyExpression groovy(String expression) {
         return new GroovyLanguage().createExpression(expression);
@@ -34,6 +39,14 @@ public class GroovyLanguage extends LanguageSupport {
     public GroovyExpression createExpression(String expression) {
         expression = loadResource(expression);
         return new GroovyExpression(expression);
+    }
+
+    Class<Script> getScriptFromCache(String script) {
+        return scriptCache.get(script);
+    }
+
+    void addScriptToCache(String script, Class<Script> scriptClass) {
+        scriptCache.put(script, scriptClass);
     }
 
 }
