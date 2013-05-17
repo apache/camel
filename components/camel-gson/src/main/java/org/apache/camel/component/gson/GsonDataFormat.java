52b597f997606c239e2f6b9fe9ce55d1bdef8d4e
Claus Ibsen <davsclaus@apache.org>
2013-05-17 08:49:28 +0200

    CAMEL-6339: Fixed gson data format to use explicit configured gson if provided.

--- components/camel-gson/src/main/java/org/apache/camel/component/gson/GsonDataFormat.java
+++ components/camel-gson/src/main/java/org/apache/camel/component/gson/GsonDataFormat.java
@@ -109,30 +109,32 @@ public class GsonDataFormat extends ServiceSupport implements DataFormat {
 
     @Override
     protected void doStart() throws Exception {
-        GsonBuilder builder = new GsonBuilder();
-        if (exclusionStrategies != null && !exclusionStrategies.isEmpty()) {
-            ExclusionStrategy[] strategies = exclusionStrategies.toArray(new ExclusionStrategy[exclusionStrategies.size()]);
-            builder.setExclusionStrategies(strategies);
+        if (gson == null) {
+            GsonBuilder builder = new GsonBuilder();
+            if (exclusionStrategies != null && !exclusionStrategies.isEmpty()) {
+                ExclusionStrategy[] strategies = exclusionStrategies.toArray(new ExclusionStrategy[exclusionStrategies.size()]);
+                builder.setExclusionStrategies(strategies);
+            }
+            if (longSerializationPolicy != null) {
+                builder.setLongSerializationPolicy(longSerializationPolicy);
+            }
+            if (fieldNamingPolicy != null) {
+                builder.setFieldNamingPolicy(fieldNamingPolicy);
+            }
+            if (fieldNamingStrategy != null) {
+                builder.setFieldNamingStrategy(fieldNamingStrategy);
+            }
+            if (serializeNulls != null && serializeNulls) {
+                builder.serializeNulls();
+            }
+            if (prettyPrinting != null && prettyPrinting) {
+                builder.setPrettyPrinting();
+            }
+            if (dateFormatPattern != null) {
+                builder.setDateFormat(dateFormatPattern);
+            }
+            gson = builder.create();
         }
-        if (longSerializationPolicy != null) {
-            builder.setLongSerializationPolicy(longSerializationPolicy);
-        }
-        if (fieldNamingPolicy != null) {
-            builder.setFieldNamingPolicy(fieldNamingPolicy);
-        }
-        if (fieldNamingStrategy != null) {
-            builder.setFieldNamingStrategy(fieldNamingStrategy);
-        }
-        if (serializeNulls != null && serializeNulls) {
-            builder.serializeNulls();
-        }
-        if (prettyPrinting != null && prettyPrinting) {
-            builder.setPrettyPrinting();
-        }
-        if (dateFormatPattern != null) {
-            builder.setDateFormat(dateFormatPattern);
-        }
-        gson = builder.create();
     }
 
     @Override
