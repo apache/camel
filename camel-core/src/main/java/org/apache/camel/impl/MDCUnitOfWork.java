4408623727f089637754ac8d36b2907991a620ba
Claus Ibsen <davsclaus@apache.org>
2013-05-21 07:37:29 +0200

    CAMEL-6377: Optimized routing engine to reduce stack frames in use during routing. Work in progress.

--- camel-core/src/main/java/org/apache/camel/impl/MDCUnitOfWork.java
+++ camel-core/src/main/java/org/apache/camel/impl/MDCUnitOfWork.java
@@ -184,46 +184,17 @@ public class MDCUnitOfWork extends DefaultUnitOfWork {
     private static final class MDCCallback implements AsyncCallback {
 
         private final AsyncCallback delegate;
-        private final String breadcrumbId;
-        private final String exchangeId;
-        private final String messageId;
-        private final String correlationId;
         private final String routeId;
-        private final String camelContextId;
 
         private MDCCallback(AsyncCallback delegate) {
             this.delegate = delegate;
-            this.exchangeId = MDC.get(MDC_EXCHANGE_ID);
-            this.messageId = MDC.get(MDC_MESSAGE_ID);
-            this.breadcrumbId = MDC.get(MDC_BREADCRUMB_ID);
-            this.correlationId = MDC.get(MDC_CORRELATION_ID);
-            this.camelContextId = MDC.get(MDC_CAMEL_CONTEXT_ID);
+            // we only need to keep track of route id as we may change routes during routing
             this.routeId = MDC.get(MDC_ROUTE_ID);
         }
 
         public void done(boolean doneSync) {
             try {
-                if (!doneSync) {
-                    // when done asynchronously then restore information from previous thread
-                    if (breadcrumbId != null) {
-                        MDC.put(MDC_BREADCRUMB_ID, breadcrumbId);
-                    }
-                    if (exchangeId != null) {
-                        MDC.put(MDC_EXCHANGE_ID, exchangeId);
-                    }
-                    if (messageId != null) {
-                        MDC.put(MDC_MESSAGE_ID, messageId);
-                    }
-                    if (correlationId != null) {
-                        MDC.put(MDC_CORRELATION_ID, correlationId);
-                    }
-                    if (routeId != null) {
-                        MDC.put(MDC_ROUTE_ID, routeId);
-                    }
-                    if (camelContextId != null) {
-                        MDC.put(MDC_CAMEL_CONTEXT_ID, camelContextId);
-                    }
-                }
+                MDC.put(MDC_ROUTE_ID, routeId);
             } finally {
                 // muse ensure delegate is invoked
                 delegate.done(doneSync);
