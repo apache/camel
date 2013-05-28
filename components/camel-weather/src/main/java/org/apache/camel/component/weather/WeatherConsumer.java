b5902106e9b6f85c216e9e788845b2ab4941d5ee
Babak Vahdat <bvahdat@apache.org>
2013-05-28 07:55:12 +0200

    Polished the code base of the new Camel Weather component.

--- components/camel-weather/src/main/java/org/apache/camel/component/weather/WeatherConsumer.java
+++ components/camel-weather/src/main/java/org/apache/camel/component/weather/WeatherConsumer.java
@@ -38,14 +38,14 @@ public class WeatherConsumer extends ScheduledPollConsumer {
     @Override
     protected int poll() throws Exception {
         LOG.debug("Going to execute the query '{}'", query);
-        String answer = getEndpoint().getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, new URL(query));
-        LOG.debug("Got back the answer '{}'", answer);
-        if (ObjectHelper.isEmpty(answer)) {
-            throw new IllegalStateException("Got the empty string '" + answer + "' as the result of the query '" + query + "'");
+        String weather = getEndpoint().getCamelContext().getTypeConverter().mandatoryConvertTo(String.class, new URL(query));
+        LOG.debug("Got back the answer '{}'", weather);
+        if (ObjectHelper.isEmpty(weather)) {
+            throw new IllegalStateException("Got the empty string '" + weather + "' as the result of the query '" + query + "'");
         }
 
         Exchange exchange = getEndpoint().createExchange();
-        exchange.getIn().setBody(answer);
+        exchange.getIn().setBody(weather);
         exchange.getIn().setHeader(WeatherConstants.WEATHER_QUERY, query);
         getProcessor().process(exchange);
 
