353495222dafacbe999679a8f7d16313638d3665
Claus Ibsen <davsclaus@apache.org>
2013-06-03 10:59:34 +0200

    CAMEL-6417: camel-http4 can only support one instance of SSLContextParameters per component. Added validation to fail if trying to use a 2nd instance.

--- components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpComponent.java
+++ components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpComponent.java
@@ -27,6 +27,7 @@ import org.apache.camel.component.http4.helper.HttpHelper;
 import org.apache.camel.impl.HeaderFilterStrategyComponent;
 import org.apache.camel.spi.HeaderFilterStrategy;
 import org.apache.camel.util.IntrospectionSupport;
+import org.apache.camel.util.ObjectHelper;
 import org.apache.camel.util.URISupport;
 import org.apache.camel.util.jsse.SSLContextParameters;
 import org.apache.http.auth.params.AuthParamBean;
@@ -70,6 +71,8 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
     protected int connectionsPerRoute = 20;
     // It's MILLISECONDS, the default value is always keep alive
     protected long connectionTimeToLive = -1;
+
+    private volatile SSLContextParameters usedSslContextParams;
 
     /**
      * Connects the URL specified on the endpoint to the specified processor.
@@ -302,6 +305,19 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
     
     @SuppressWarnings("deprecation")
     protected void registerPort(boolean secure, X509HostnameVerifier x509HostnameVerifier, int port, SSLContextParameters sslContextParams) throws Exception {
+        if (usedSslContextParams == null) {
+            usedSslContextParams = sslContextParams;
+        }
+
+        // we must use same SSLContextParameters for this component.
+        if (usedSslContextParams != sslContextParams) {
+            // use identity hashcode in exception message
+            Object previous = ObjectHelper.getIdentityHashCode(usedSslContextParams);
+            Object next = ObjectHelper.getIdentityHashCode(sslContextParams);
+            throw new IllegalArgumentException("Only same instance of SSLContextParameters is supported. Cannot use a different instance."
+                    + " Previous instance hashcode: " + previous + ", New instance hashcode: " + next);
+        }
+
         SchemeRegistry registry = clientConnectionManager.getSchemeRegistry();
         if (secure) {
             SSLSocketFactory socketFactory;
@@ -310,7 +326,7 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
             } else {
                 socketFactory = new SSLSocketFactory(sslContextParams.createSSLContext());
             }
-            
+
             socketFactory.setHostnameVerifier(x509HostnameVerifier);
             // must register both https and https4
             registry.register(new Scheme("https", port, socketFactory));
@@ -460,6 +476,7 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
             clientConnectionManager.shutdown();
             clientConnectionManager = null;
         }
+        usedSslContextParams = null;
         super.doStop();
     }
 }
