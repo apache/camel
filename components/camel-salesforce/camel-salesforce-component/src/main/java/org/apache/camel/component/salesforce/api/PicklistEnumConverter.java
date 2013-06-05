5463fb1d2d2ae067920d1132a15557f72599e2fd
Babak Vahdat <bvahdat@apache.org>
2013-06-05 22:14:33 +0200

    CAMEL-6428: The camel-salesforce code base should support Java 6.

--- components/camel-salesforce/camel-salesforce-component/src/main/java/org/apache/camel/component/salesforce/api/PicklistEnumConverter.java
+++ components/camel-salesforce/camel-salesforce-component/src/main/java/org/apache/camel/component/salesforce/api/PicklistEnumConverter.java
@@ -37,7 +37,7 @@ public class PicklistEnumConverter implements Converter {
         try {
             Method getterMethod = aClass.getMethod("value");
             writer.setValue((String) getterMethod.invoke(o));
-        } catch (ReflectiveOperationException e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException(
                     String.format("Exception writing pick list value %s of type %s: %s",
                             o, o.getClass().getName(), e.getMessage()),
@@ -53,14 +53,14 @@ public class PicklistEnumConverter implements Converter {
             Method factoryMethod = requiredType.getMethod(FACTORY_METHOD, String.class);
             // use factory method to create object
             return factoryMethod.invoke(null, value);
-        } catch (ReflectiveOperationException e) {
-            throw new IllegalArgumentException(
-                    String.format("Exception reading pick list value %s of type %s: %s",
-                            value, context.getRequiredType().getName(), e.getMessage()),
-                    e);
         } catch (SecurityException e) {
             throw new IllegalArgumentException(
                     String.format("Security Exception reading pick list value %s of type %s: %s",
+                            value, context.getRequiredType().getName(), e.getMessage()),
+                    e);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(
+                    String.format("Exception reading pick list value %s of type %s: %s",
                             value, context.getRequiredType().getName(), e.getMessage()),
                     e);
         }
