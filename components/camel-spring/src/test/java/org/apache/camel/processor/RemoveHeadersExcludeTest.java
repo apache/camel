0abd8fb34b3f77ecdb9fb2701381dae033217c69
cmueller <cmueller@apache.org>
2013-06-11 17:00:39 +0200

    added unit test for an issue on the user mailing list

---
+++ components/camel-spring/src/test/java/org/apache/camel/processor/RemoveHeadersExcludeTest.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.processor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.spring.SpringTestSupport;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class RemoveHeadersExcludeTest extends SpringTestSupport {
+
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("org/apache/camel/processor/camel-context.xml");
+    }
+
+    public void testRemoveHeadersWithExcludePatterns() throws Exception {
+        MockEndpoint mockEndpoint = getMockEndpoint("mock:result");
+        mockEndpoint.expectedMessageCount(1);
+        mockEndpoint.expectedHeaderReceived("header1", "value1");
+        mockEndpoint.expectedHeaderReceived("header3", "value3");
+
+        Map<String, Object> headers = new HashMap<String, Object>();
+        headers.put("header1", "value1");
+        headers.put("header2", "value2");
+        headers.put("header3", "value3");
+
+        template.sendBodyAndHeaders("direct:start", null, headers);
+
+        assertMockEndpointsSatisfied();
+    }
+}
