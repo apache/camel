6c0c2c1709e3fec18d61e1f84caecf2910814580
Jonathan Anstey <janstey@gmail.com>
2013-06-28 16:42:15 -0230

    Fix checkstyle errors

--- components/camel-yammer/src/test/java/org/apache/camel/component/yammer/YammerConsumerMessageLimitTest.java
+++ components/camel-yammer/src/test/java/org/apache/camel/component/yammer/YammerConsumerMessageLimitTest.java
@@ -1,4 +1,3 @@
-package org.apache.camel.component.yammer;
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,7 @@ package org.apache.camel.component.yammer;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.camel.component.yammer;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
