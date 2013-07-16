20e5435e35dfc13b42b4f82e94c37758bda40cde
Willem Jiang <ningjiang@apache.org>
2013-07-16 10:36:46 +0800

    Fixed CS errors of CsvTest

--- components/camel-beanio/src/test/java/org/apache/camel/dataformat/beanio/csv/CsvTest.java
+++ components/camel-beanio/src/test/java/org/apache/camel/dataformat/beanio/csv/CsvTest.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +16,10 @@
  */
 package org.apache.camel.dataformat.beanio.csv;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
@@ -25,18 +28,13 @@ import org.apache.camel.spi.DataFormat;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
  */
 public class CsvTest extends CamelTestSupport {
     private static final String FIXED_DATA =
-            "James,Strachan,22" + LS +
-                    "Claus,Ibsen,21" + LS;
+            "James,Strachan,22" + LS + "Claus,Ibsen,21" + LS;
 
-    private boolean verbose = false;
+    private boolean verbose;
 
 /*
     @Test
