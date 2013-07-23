2d6da2693b4d723efb47e7ca12d236e066db0bde
Akitoshi Yoshida <ay@apache.org>
2013-07-23 13:12:22 +0200

    CAMEL-6309: Sftp upload to subdirectory does not work if home directory is root

--- camel-core/src/main/java/org/apache/camel/util/FileUtil.java
+++ camel-core/src/main/java/org/apache/camel/util/FileUtil.java
@@ -273,7 +273,7 @@ public final class FileUtil {
             }
         }
 
-        if (endsWithSlash) {
+        if (endsWithSlash && stack.size() > 0) {
             sb.append(separator);
         }
 
