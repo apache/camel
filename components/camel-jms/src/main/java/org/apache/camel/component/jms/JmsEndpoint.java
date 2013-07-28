51369fbc5bdb5d4787666cfb3c6f735e0753d20f
Claus Ibsen <davsclaus@apache.org>
2013-07-28 14:05:52 +0200

    CAMEL-6583: Added option includeAllJMSXProperties to camel-jms.

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsEndpoint.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsEndpoint.java
@@ -305,7 +305,7 @@ public class JmsEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
 
     public HeaderFilterStrategy getHeaderFilterStrategy() {
         if (headerFilterStrategy == null) {
-            headerFilterStrategy = new JmsHeaderFilterStrategy();
+            headerFilterStrategy = new JmsHeaderFilterStrategy(isIncludeAllJMSXProperties());
         }
         return headerFilterStrategy;
     }
@@ -1072,6 +1072,16 @@ public class JmsEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
     @ManagedAttribute
     public void setIncludeSentJMSMessageID(boolean includeSentJMSMessageID) {
         configuration.setIncludeSentJMSMessageID(includeSentJMSMessageID);
+    }
+
+    @ManagedAttribute
+    public boolean isIncludeAllJMSXProperties() {
+        return configuration.isIncludeAllJMSXProperties();
+    }
+
+    @ManagedAttribute
+    public void setIncludeAllJMSXProperties(boolean includeAllJMSXProperties) {
+        configuration.setIncludeAllJMSXProperties(includeAllJMSXProperties);
     }
 
     @ManagedAttribute
