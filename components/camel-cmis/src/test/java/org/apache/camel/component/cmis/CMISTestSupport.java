c90c9dc99a6db71107cc1e3d52f2237f4417c975
Willem Jiang <ningjiang@apache.org>
2013-08-01 11:43:51 +0800

    CAMEL-6595 fixed the test packages name issue of camel-cmis with thanks to Maurizio

--- components/camel-cmis/src/test/java/org/apache/camel/component/cmis/CMISTestSupport.java
+++ components/camel-cmis/src/test/java/org/apache/camel/component/cmis/CMISTestSupport.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.componenet.cmis;
+package org.apache.camel.component.cmis;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
