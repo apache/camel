55751402055e01a774be3c9e3c1d440f6cfb5f1b
Claus Ibsen <davsclaus@apache.org>
2013-08-03 09:37:07 +0200

    CAMEL-6604: Fixed routing slip and dynamic router EIP to work with stream caching properly.

--- camel-core/src/main/java/org/apache/camel/processor/RoutingSlip.java
+++ camel-core/src/main/java/org/apache/camel/processor/RoutingSlip.java
@@ -36,6 +36,7 @@ import org.apache.camel.impl.ProducerCache;
 import org.apache.camel.support.ServiceSupport;
 import org.apache.camel.util.AsyncProcessorHelper;
 import org.apache.camel.util.ExchangeHelper;
+import org.apache.camel.util.MessageHelper;
 import org.apache.camel.util.ObjectHelper;
 import org.apache.camel.util.ServiceHelper;
 import org.slf4j.Logger;
@@ -268,6 +269,10 @@ public class RoutingSlip extends ServiceSupport implements AsyncProcessor, Trace
         // exchange being routed.
         copy.setExchangeId(current.getExchangeId());
         copyOutToIn(copy, current);
+
+        // ensure stream caching is reset
+        MessageHelper.resetStreamCache(copy.getIn());
+
         return copy;
     }
 
