b867d32c799201e75770aaa0008f85694c3653f8
Willem Jiang <ningjiang@apache.org>
2013-08-06 07:36:17 +0800

    CAMEL-6602 fix the ClassCastException when using cxf FailoverFeature

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfEndpoint.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfEndpoint.java
@@ -923,7 +923,9 @@ public class CxfEndpoint extends DefaultEndpoint implements HeaderFilterStrategy
                 message.remove(CxfConstants.CAMEL_CXF_ATTACHMENTS);
             }
 
-            if (DataFormat.PAYLOAD == message.get(DataFormat.class)) {
+            // Don't try to reset the parameters if the parameter is not CxfPayload instance
+            // as the setParameter will be called more than once when using the fail over feature
+            if (DataFormat.PAYLOAD == message.get(DataFormat.class) && params[0] instanceof CxfPayload) {
 
                 CxfPayload<?> payload = (CxfPayload<?>) params[0];
                 List<Source> elements = payload.getBodySources();
