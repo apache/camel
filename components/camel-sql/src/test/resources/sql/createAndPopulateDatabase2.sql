7b45b8466ddfbac72358c28a5e0c1be44eef8c2c
Claus Ibsen <davsclaus@apache.org>
2013-08-07 12:04:36 +0200

    CAMEL-6561: Fixed sql producer to support json like structure for # parameters when only 1 parameter in use.

---
+++ components/camel-sql/src/test/resources/sql/createAndPopulateDatabase2.sql
@@ -0,0 +1,23 @@
+-- ------------------------------------------------------------------------
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- ------------------------------------------------------------------------
+
+-- START SNIPPET: e1
+create table projects (id integer primary key, project varchar(10), license varchar(5), description varchar(1000) default null);
+insert into projects values (1, 'Camel', 'ASF', '');
+insert into projects values (2, 'AMQ', 'ASF', '');
+insert into projects values (3, 'Linux', 'XXX', '');
+-- END SNIPPET: e1
