7c031be099fb4790f64c607273f12d42c0e0c6aa
Willem Jiang <ningjiang@apache.org>
2013-08-20 18:21:16 +0800

    CAMEL-6653  Support to set preferredAuthentications on SFTP

--- components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/SftpConfiguration.java
+++ components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/SftpConfiguration.java
@@ -39,6 +39,7 @@ public class SftpConfiguration extends RemoteFileConfiguration {
     // null means default jsch list will be used
     private String ciphers;
     private int compression;
+    private String preferredAuthentications;
 
     public SftpConfiguration() {
         setProtocol("sftp");
@@ -165,5 +166,13 @@ public class SftpConfiguration extends RemoteFileConfiguration {
 
     public void setCompression(int compression) {
         this.compression = compression;
+    }
+    
+    public void setPreferredAuthentications(String pAuthentications) {
+        this.preferredAuthentications = pAuthentications;
+    }
+    
+    public String getPreferredAuthentications() {
+        return preferredAuthentications;
     }
 }
