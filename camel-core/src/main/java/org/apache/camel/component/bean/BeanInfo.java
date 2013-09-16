aebe527746df8babc943a33102d01ac71e6240ce
Willem Jiang <ningjiang@apache.org>
2013-09-16 11:41:24 +0800

    CAMEL-6752 Fixed the Bean binding to covariant methods issue with thanks to Guy

--- camel-core/src/main/java/org/apache/camel/component/bean/BeanInfo.java
+++ camel-core/src/main/java/org/apache/camel/component/bean/BeanInfo.java
@@ -535,7 +535,11 @@ public class BeanInfo {
                     answer = chooseMethodWithMatchingParameters(exchange, parameters, possibleOperations);
                 }
             }
-
+            
+            if (answer == null && possibleOperations.size() > 1) {
+                answer = getSingleCovariantMethod(possibleOperations);
+            }
+            
             if (answer == null) {
                 throw new AmbiguousMethodCallException(exchange, possibleOperations);
             } else {
@@ -597,9 +601,24 @@ public class BeanInfo {
         }
 
         if (candidates.size() > 1) {
-            throw new AmbiguousMethodCallException(exchange, candidates);
+            MethodInfo answer = getSingleCovariantMethod(candidates);
+            if (answer == null) {
+                throw new AmbiguousMethodCallException(exchange, candidates);
+            }
+            return answer;
         }
         return candidates.size() == 1 ? candidates.get(0) : null;
+    }
+
+    private MethodInfo getSingleCovariantMethod(Collection<MethodInfo> candidates) {
+        // if all the candidates are actually covariant, it doesn't matter which one we call
+        MethodInfo firstCandidate = candidates.iterator().next();
+        for (MethodInfo candidate: candidates) {
+            if (!firstCandidate.isCovariantWith(candidate)) {
+                return null;
+            }
+        }
+        return firstCandidate;
     }
 
     private MethodInfo chooseMethodWithMatchingBody(Exchange exchange, Collection<MethodInfo> operationList,
@@ -697,8 +716,17 @@ public class BeanInfo {
                 LOG.trace("There are only one method with annotations so we choose it: {}", answer);
                 return answer;
             }
-            // phew try to choose among multiple methods with annotations
-            return chooseMethodWithCustomAnnotations(exchange, possibles);
+            // try to choose among multiple methods with annotations
+            MethodInfo chosen = chooseMethodWithCustomAnnotations(exchange, possibles);
+            if (chosen != null) {
+                return chosen;
+            }
+            // just make sure the methods aren't all actually the same
+            chosen = getSingleCovariantMethod(possibles);
+            if (chosen != null) {
+                return chosen;
+            }
+            throw new AmbiguousMethodCallException(exchange, possibles);
         }
 
         // cannot find a good method to use
@@ -769,10 +797,7 @@ public class BeanInfo {
                 }
             }
         }
-        if (chosen != null) {
-            return chosen;
-        }
-        throw new AmbiguousMethodCallException(exchange, possibles);
+        return chosen;
     }
 
     /**
