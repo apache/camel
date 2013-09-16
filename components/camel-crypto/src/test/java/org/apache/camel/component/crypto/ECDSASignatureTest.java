89c61caccc6f2138c1b20e4ff3d92daa8acee613
Claus Ibsen <davsclaus@apache.org>
2013-09-16 11:39:41 +0200

    Skip test if cannot run on platform due security provider not supporting ECDSA.

--- components/camel-crypto/src/test/java/org/apache/camel/component/crypto/ECDSASignatureTest.java
+++ components/camel-crypto/src/test/java/org/apache/camel/component/crypto/ECDSASignatureTest.java
@@ -40,12 +40,15 @@ public class ECDSASignatureTest extends CamelTestSupport {
 
     private String payload = "Dear Alice, Rest assured it's me, signed Bob";
     private boolean ibmJDK;
+    private PrivateKey privateKey;
+    private X509Certificate x509;
+    private boolean canRun = true;
 
     public ECDSASignatureTest() throws Exception {
         // BouncyCastle is required for ECDSA support for JDK 1.6
         if (isJava16()
             && Security.getProvider("BC") == null) {
-            Constructor<?> cons = null;
+            Constructor<?> cons;
             Class<?> c = Class.forName("org.bouncycastle.jce.provider.BouncyCastleProvider");
             cons = c.getConstructor(new Class[] {});
             
@@ -57,28 +60,40 @@ public class ECDSASignatureTest extends CamelTestSupport {
         if (isJavaVendor("IBM")) {
             ibmJDK = true;
         }
+
+        // see if we can load the keystore et all
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            InputStream in = ECDSASignatureTest.class.getResourceAsStream("/org/apache/camel/component/crypto/ecdsa.jks");
+            keyStore.load(in, "security".toCharArray());
+            privateKey = (PrivateKey) keyStore.getKey("ECDSA", "security".toCharArray());
+            x509 = (X509Certificate)keyStore.getCertificate("ECDSA");
+        } catch (Throwable e) {
+            log.warn("Cannot setup keystore for running this test due " + e.getMessage() + ". This test is skipped.", e);
+            canRun = false;
+        }
     }
     
     @Override
     protected RouteBuilder[] createRouteBuilders() throws Exception {
-        if (ibmJDK) {
+        if (ibmJDK || !canRun) {
             return new RouteBuilder[] {};
         }
 
         return new RouteBuilder[]{new RouteBuilder() {
             public void configure() throws Exception {
                 // START SNIPPET: ecdsa-sha1
-                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                InputStream in = ECDSASignatureTest.class.getResourceAsStream("/org/apache/camel/component/crypto/ecdsa.jks");
-                keyStore.load(in, "security".toCharArray());
-                PrivateKey privateKey = 
-                    (PrivateKey)keyStore.getKey("ECDSA", "security".toCharArray());
-                X509Certificate x509 = (X509Certificate)keyStore.getCertificate("ECDSA");
 
                 // we can set the keys explicitly on the endpoint instances.
-                context.getEndpoint("crypto:sign://ecdsa-sha1?algorithm=SHA1withECDSA", DigitalSignatureEndpoint.class).setPrivateKey(privateKey);
-                context.getEndpoint("crypto:verify://ecdsa-sha1?algorithm=SHA1withECDSA", DigitalSignatureEndpoint.class).setPublicKey(x509.getPublicKey());
-                from("direct:ecdsa-sha1").to("crypto:sign://ecdsa-sha1?algorithm=SHA1withECDSA", "crypto:verify://ecdsa-sha1?algorithm=SHA1withECDSA", "mock:result");
+                context.getEndpoint("crypto:sign://ecdsa-sha1?algorithm=SHA1withECDSA", DigitalSignatureEndpoint.class)
+                        .setPrivateKey(privateKey);
+                context.getEndpoint("crypto:verify://ecdsa-sha1?algorithm=SHA1withECDSA", DigitalSignatureEndpoint.class)
+                        .setPublicKey(x509.getPublicKey());
+
+                from("direct:ecdsa-sha1")
+                    .to("crypto:sign://ecdsa-sha1?algorithm=SHA1withECDSA")
+                    .to("crypto:verify://ecdsa-sha1?algorithm=SHA1withECDSA")
+                    .to("mock:result");
                 // END SNIPPET: ecdsa-sha1
             }
         }};
@@ -86,9 +101,10 @@ public class ECDSASignatureTest extends CamelTestSupport {
 
     @Test
     public void testECDSASHA1() throws Exception {
-        if (ibmJDK) {
+        if (ibmJDK || !canRun) {
             return;
         }
+
         setupMock();
         sendBody("direct:ecdsa-sha1", payload);
         assertMockEndpointsSatisfied();
