c59c9c7557e89445a982211a4d3f9edf44130f2f
Claus Ibsen <davsclaus@apache.org>
2013-09-29 12:17:25 +0200

    CAMEL-6771: Fixed potential concurrency exception in splitter. Thanks to James Carman for patch.

--- camel-core/src/main/java/org/apache/camel/processor/MulticastProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/MulticastProcessor.java
@@ -1021,11 +1021,11 @@ public class MulticastProcessor extends ServiceSupport implements AsyncProcessor
         Map<?, ?> property = exchange.getProperty(Exchange.AGGREGATION_STRATEGY, Map.class);
         Map<Object, AggregationStrategy> map = CastUtils.cast(property);
         if (map == null) {
-            map = new HashMap<Object, AggregationStrategy>();
+            map = new ConcurrentHashMap<Object, AggregationStrategy>();
         } else {
             // it is not safe to use the map directly as the exchange doesn't have the deep copy of it's properties
             // we just create a new copy if we need to change the map
-            map = new HashMap<Object, AggregationStrategy>(map);
+            map = new ConcurrentHashMap<Object, AggregationStrategy>(map);
         }
         // store the strategy using this processor as the key
         // (so we can store multiple strategies on the same exchange)
