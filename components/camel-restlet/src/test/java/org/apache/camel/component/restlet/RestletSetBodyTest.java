2ebe220c306c3cc7fc10b80a839cbb873632f098
Willem Jiang <ningjiang@apache.org>
2013-10-08 13:15:46 +0800

    CAMEL-6834 camel-restlet supports to send the Representation object back

--- components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletSetBodyTest.java
+++ components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletSetBodyTest.java
@@ -16,8 +16,17 @@
  */
 package org.apache.camel.component.restlet;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Test;
+import org.restlet.data.MediaType;
+import org.restlet.representation.InputRepresentation;
 
 /**
  * @version 
@@ -29,6 +38,31 @@ public class RestletSetBodyTest extends RestletTestSupport {
         String response = template.requestBody("restlet:http://0.0.0.0:" + portNum + "/stock/ORCL?restletMethod=get", null, String.class);
         assertEquals("110", response);
     }
+    
+    @Test
+    public void testSetBodyRepresentation() throws Exception {
+        HttpGet get = new HttpGet("http://0.0.0.0:" + portNum + "/images/123");
+        HttpClient httpclient = new DefaultHttpClient();
+        InputStream is = null;
+        try {
+            HttpResponse response = httpclient.execute(get);
+            assertEquals(200, response.getStatusLine().getStatusCode());
+            assertEquals("image/png", response.getEntity().getContentType().getValue());
+            is = response.getEntity().getContent();
+            assertEquals("Get wrong available size", 10, is.available());
+            byte[] buffer = new byte[10];
+            is.read(buffer);
+            for (int i = 0; i < 10; i++) {
+                assertEquals(i + 1, buffer[i]);
+            }
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
@@ -37,6 +71,14 @@ public class RestletSetBodyTest extends RestletTestSupport {
             public void configure() throws Exception {
                 from("restlet:http://0.0.0.0:" + portNum + "/stock/{symbol}?restletMethods=get")
                     .setBody().constant("110");
+                // create ByteArrayRepresentation for response
+                byte[] image = new byte[10];
+                for (int i = 0; i < 10; i++) {
+                    image[i] = (byte)(i + 1);
+                }
+                ByteArrayInputStream inputStream = new ByteArrayInputStream(image);
+                from("restlet:http://0.0.0.0:" + portNum + "/images/{symbol}?restletMethods=get")
+                    .setBody().constant(new InputRepresentation(inputStream, MediaType.IMAGE_PNG, 10));
             }
         };
     }
