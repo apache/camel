82235ada3ea4252846c2f54074dfcb7fbbe3230d
Claus Ibsen <davsclaus@apache.org>
2013-10-10 08:04:37 +0200

    CAMEL-6845: Using recipient list to a route that has no error handler should allow caller route to trigger its error handler

--- camel-core/src/main/java/org/apache/camel/builder/NoErrorHandlerBuilder.java
+++ camel-core/src/main/java/org/apache/camel/builder/NoErrorHandlerBuilder.java
@@ -16,7 +16,10 @@
  */
 package org.apache.camel.builder;
 
+import org.apache.camel.AsyncCallback;
+import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.processor.DelegateAsyncProcessor;
 import org.apache.camel.spi.RouteContext;
 
 /**
@@ -31,7 +34,27 @@ import org.apache.camel.spi.RouteContext;
 public class NoErrorHandlerBuilder extends ErrorHandlerBuilderSupport {
 
     public Processor createErrorHandler(RouteContext routeContext, Processor processor) {
-        return processor;
+        return new DelegateAsyncProcessor(processor) {
+            @Override
+            public boolean process(final Exchange exchange, final AsyncCallback callback) {
+                return super.process(exchange, new AsyncCallback() {
+                    @Override
+                    public void done(boolean doneSync) {
+                        exchange.removeProperty(Exchange.REDELIVERY_EXHAUSTED);
+                        callback.done(doneSync);
+                    }
+                });
+            }
+
+            @Override
+            public String toString() {
+                if (processor == null) {
+                    // if no output then dont do any description
+                    return "";
+                }
+                return "NoErrorHandler[" + processor + "]";
+            }
+        };
     }
 
     public boolean supportTransacted() {
