66648ee0478a6834d9191b38caa0c290ab253f12
Claus Ibsen <davsclaus@apache.org>
2013-10-21 10:00:30 +0200

    CAMEL-6879: camel-restlet consumer should send back HTTP headers, such as Location etc.

--- components/camel-restlet/src/main/java/org/apache/camel/component/restlet/RestletHeaderFilterStrategy.java
+++ components/camel-restlet/src/main/java/org/apache/camel/component/restlet/RestletHeaderFilterStrategy.java
@@ -38,8 +38,9 @@ public class RestletHeaderFilterStrategy extends DefaultHeaderFilterStrategy {
         // The "CamelAcceptContentType" header is not added to the outgoing HTTP 
         // headers but it will be going out as "Accept.
         getOutFilter().add(Exchange.ACCEPT_CONTENT_TYPE);
-        
-        // Remove the restlet headers from the out message.
-        getOutFilter().add(HeaderConstants.ATTRIBUTE_HEADERS);
+
+        // Remove Camel and org.restlet internal headers
+//        setOutFilterPattern("(Camel.*)|(org\\.restlet\\..*)");
+//        setOutFilterPattern("org\\.restlet\\..*");
     }
 }
