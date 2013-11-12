43704eb715ff72b4341527d455738ca1da991e22
Claus Ibsen <davsclaus@apache.org>
2013-11-12 11:26:47 +0100

    CAMEL-6679: Added unit test

--- components/camel-zipfile/src/test/java/org/apache/camel/dataformat/zipfile/ZipSplitterRouteTest.java
+++ components/camel-zipfile/src/test/java/org/apache/camel/dataformat/zipfile/ZipSplitterRouteTest.java
@@ -16,14 +16,12 @@
  */
 package org.apache.camel.dataformat.zipfile;
 
-
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 public class ZipSplitterRouteTest extends CamelTestSupport {
-    
 
     @Test
     public void testSplitter() throws InterruptedException {
@@ -39,9 +37,12 @@ public class ZipSplitterRouteTest extends CamelTestSupport {
             public void configure() throws Exception {
                 // Unzip file and Split it according to FileEntry
                 from("file:src/test/resources/org/apache/camel/dataformat/zipfile?consumer.delay=1000&noop=true")
-                        .log("Start processing big file: ${header.CamelFileName}").split(new ZipSplitter())
-                        .streaming().convertBodyTo(String.class).to("mock:processZipEntry")
-                        .end().log("Done processing big file: ${header.CamelFileName}");
+                    .log("Start processing big file: ${header.CamelFileName}")
+                    .split(new ZipSplitter()).streaming()
+                        .to("log:entry")
+                        .convertBodyTo(String.class).to("mock:processZipEntry")
+                    .end()
+                    .log("Done processing big file: ${header.CamelFileName}");
             }
         };
 
