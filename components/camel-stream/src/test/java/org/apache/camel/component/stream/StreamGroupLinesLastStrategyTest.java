3f07f04fa8da745686fedc3757521e223dd87dc1
Claus Ibsen <davsclaus@apache.org>
2013-11-12 10:37:07 +0100

    CAMEL-6899: stream consumer with groupLines should send last message when EOL but the groupLines limit was not hit, otherwise the last group is never sent out.

--- components/camel-stream/src/test/java/org/apache/camel/component/stream/StreamGroupLinesLastStrategyTest.java
+++ components/camel-stream/src/test/java/org/apache/camel/component/stream/StreamGroupLinesLastStrategyTest.java
@@ -16,33 +16,11 @@
  */
 package org.apache.camel.component.stream;
 
-import java.util.List;
-
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.junit.Test;
 
-public class StreamGroupLinesStrategyTest extends StreamGroupLinesTest {
-    
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry jndi = super.createRegistry();
-        jndi.bind("myGroupStrategy", new MyGroupStrategy());
-        return jndi;
-    }
-    
-    class MyGroupStrategy implements GroupStrategy {
-
-        @Override
-        public Object groupLines(List<String> lines) {
-            StringBuilder buffer = new StringBuilder();
-            for (String line : lines) {
-                buffer.append(line);
-                buffer.append("\n");
-            }
-            return buffer.toString();
-        }
-    }
+public class StreamGroupLinesLastStrategyTest extends StreamGroupLinesStrategyTest {
     
     @Test
     public void testGroupLines() throws Exception {
@@ -53,10 +31,11 @@ public class StreamGroupLinesStrategyTest extends StreamGroupLinesTest {
         assertMockEndpointsSatisfied();
 
         Object result = mock.getExchanges().get(0).getIn().getBody();
-        assertEquals("Get a wrong result.", "A\nB\nC\n", result);
+        assertEquals("Get a wrong result.", "A\nB\nC\nD\n", result);
 
+        // we did not have 4 lines but since its the last it was triggered anyway
         Object result2 = mock.getExchanges().get(1).getIn().getBody();
-        assertEquals("Get a wrong result.", "D\nE\nF\n", result2);
+        assertEquals("Get a wrong result.", "E\nF\n", result2);
     }
 
     @Override
@@ -64,7 +43,7 @@ public class StreamGroupLinesStrategyTest extends StreamGroupLinesTest {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("stream:file?fileName=target/stream/streamfile.txt&groupLines=3&groupStrategy=#myGroupStrategy").to("mock:result");
+                from("stream:file?fileName=target/stream/streamfile.txt&groupLines=4&groupStrategy=#myGroupStrategy").to("mock:result");
             }
         };
     }
