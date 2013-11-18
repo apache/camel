8d7aa6200aa88477074ce93dcc9a810941035a5e
Claus Ibsen <davsclaus@apache.org>
2013-11-18 21:55:49 +0100

    CAMEL-6972: Improved Camel in OSGi MBean naming to use symbolicName and only use counter when there is a clash. This avoids ugly looking mbean names out of the box. See ticket for before examples. Also the examples should not have long symbolicNames in their bundles, so we skip having the org.apache.camel groupId as prefix.

--- components/camel-core-osgi/src/main/java/org/apache/camel/core/osgi/OsgiCamelContextNameStrategy.java
+++ components/camel-core-osgi/src/main/java/org/apache/camel/core/osgi/OsgiCamelContextNameStrategy.java
@@ -16,15 +16,78 @@
  */
 package org.apache.camel.core.osgi;
 
-import org.apache.camel.impl.DefaultCamelContextNameStrategy;
-import org.osgi.framework.BundleContext;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class OsgiCamelContextNameStrategy extends DefaultCamelContextNameStrategy {
+import org.apache.camel.CamelContext;
+import org.apache.camel.spi.CamelContextNameStrategy;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.camel.core.osgi.OsgiCamelContextPublisher.CONTEXT_NAME_PROPERTY;
+
+/**
+ * In OSGi we want to use a {@link CamelContextNameStrategy} that finds a free name in the
+ * OSGi Service Registry to be used for auto assigned names.
+ * <p/>
+ * If there is a name clash in the OSGi registry, then a new candidate name is used by appending
+ * a unique counter.
+ */
+public class OsgiCamelContextNameStrategy implements CamelContextNameStrategy {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OsgiCamelContextNameStrategy.class);
+    private static final AtomicInteger CONTEXT_COUNTER = new AtomicInteger(0);
+    private final BundleContext context;
+    private final String prefix = "camel";
+    private volatile String name;
 
     public OsgiCamelContextNameStrategy(BundleContext context) {
-        // use bundle id in auto assigned names to make it unique and have the bundle id as prefix
-        // which makes the ordering of the camel apps in JMX nicely sorted
-        super(context.getBundle().getBundleId() + "-camel");
+        this.context = context;
+    }
+
+    @Override
+    public String getName() {
+        if (name == null) {
+            name = getNextName();
+        }
+        return name;
+    }
+
+    @Override
+    public synchronized String getNextName() {
+        // generate new candidate
+        String candidate = prefix + "-" + getNextCounter();
+
+        boolean clash = false;
+        do {
+            try {
+                LOG.trace("Checking OSGi Service Registry for existence of existing CamelContext with name: {}", candidate);
+                ServiceReference[] refs = context.getServiceReferences(CamelContext.class.getName(), "(" + CONTEXT_NAME_PROPERTY + "=" + candidate + ")");
+                clash = refs != null && refs.length > 0;
+                if (clash) {
+                    // generate new candidate
+                    candidate = prefix + "-" + getNextCounter();
+                }
+            } catch (InvalidSyntaxException e) {
+                LOG.debug("Error finding free Camel name in OSGi Service Registry due " + e.getMessage() + ". This exception is ignored.", e);
+                break;
+            }
+        } while (clash);
+
+        LOG.debug("Generated CamelContext name for bundle id: {}, clash: {} -> {}", new Object[]{context.getBundle().getBundleId(), clash, name});
+        return candidate;
+    }
+
+    @Override
+    public boolean isFixedName() {
+        return false;
+    }
+
+    public static int getNextCounter() {
+        // we want to start counting from 1, so increment first
+        return CONTEXT_COUNTER.incrementAndGet();
     }
 
 }
