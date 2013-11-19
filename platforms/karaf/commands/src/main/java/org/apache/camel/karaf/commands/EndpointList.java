f5302d3ef9d2b69480f0a1877b4acb932e662c10
Claus Ibsen <davsclaus@apache.org>
2013-11-19 12:16:38 +0100

    CAMEL-9680: Camel Karaf commands now output commands sorted and auto width calculated columns so it looks nicer.

--- platforms/karaf/commands/src/main/java/org/apache/camel/karaf/commands/EndpointList.java
+++ platforms/karaf/commands/src/main/java/org/apache/camel/karaf/commands/EndpointList.java
@@ -16,13 +16,19 @@
  */
 package org.apache.camel.karaf.commands;
 
+import java.io.PrintStream;
+import java.net.URLDecoder;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.ServiceStatus;
 import org.apache.camel.StatefulService;
+import org.apache.camel.util.URISupport;
+import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
+import org.apache.felix.gogo.commands.Option;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 
 /**
@@ -31,8 +37,29 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 @Command(scope = "camel", name = "endpoint-list", description = "Lists all Camel endpoints available in CamelContexts.")
 public class EndpointList extends OsgiCommandSupport {
 
-    protected static final String HEADER_FORMAT = "%-20s %-20s %-20s";
-    protected static final String OUTPUT_FORMAT = "[%-18s] [%-18s] [%-18s]";
+    private static final String CONTEXT_COLUMN_LABEL = "Context";
+    private static final String URI_COLUMN_LABEL = "Uri";
+    private static final String STATUS_COLUMN_LABEL = "Status";
+
+    private static final int DEFAULT_COLUMN_WIDTH_INCREMENT = 0;
+    private static final String DEFAULT_FIELD_PREAMBLE = "[ ";
+    private static final String DEFAULT_FIELD_POSTAMBLE = " ]";
+    private static final String DEFAULT_HEADER_PREAMBLE = "  ";
+    private static final String DEFAULT_HEADER_POSTAMBLE = "  ";
+    private static final int DEFAULT_FORMAT_BUFFER_LENGTH = 24;
+    // endpoint uris can be very long so clip by default after 120 chars
+    private static final int MAX_COLUMN_WIDTH = 120;
+
+    @Argument(index = 0, name = "name", description = "The Camel context name where to look for the endpoints", required = false, multiValued = false)
+    String name;
+
+    @Option(name = "--decode", aliases = "-d", description = "Whether to decode the endpoint uri so its human readable",
+            required = false, multiValued = false, valueToShowInHelp = "true")
+    Boolean decode;
+
+    @Option(name = "--verbose", aliases = "-v", description = "Verbose output which does not limit the length of the uri shown",
+            required = false, multiValued = false, valueToShowInHelp = "false")
+    Boolean verbose;
 
     private CamelController camelController;
 
@@ -41,20 +68,102 @@ public class EndpointList extends OsgiCommandSupport {
     }
 
     protected Object doExecute() throws Exception {
-        System.out.println(String.format(HEADER_FORMAT, "camel-id", "uri", "Status"));
+        List<Endpoint> endpoints = camelController.getEndpoints(name);
 
-        List<CamelContext> camelContexts = camelController.getCamelContexts();
-        for (CamelContext camelContext : camelContexts) {
-            List<Endpoint> endpoints = (List<Endpoint>) camelContext.getEndpoints();
-            for (Endpoint endpoint : endpoints) {
-                System.out.println(String.format(OUTPUT_FORMAT, camelContext.getName(), endpoint.getEndpointUri(), getState(endpoint)));
+        final Map<String, Integer> columnWidths = computeColumnWidths(endpoints);
+        final String headerFormat = buildFormatString(columnWidths, true);
+        final String rowFormat = buildFormatString(columnWidths, false);
+        final PrintStream out = System.out;
+
+        if (endpoints.size() > 0) {
+            out.println(String.format(headerFormat, CONTEXT_COLUMN_LABEL, URI_COLUMN_LABEL, STATUS_COLUMN_LABEL));
+            for (final Endpoint endpoint : endpoints) {
+                String contextId = endpoint.getCamelContext().getName();
+                String uri = endpoint.getEndpointUri();
+                if (decode == null || decode) {
+                    // decode uri so its more human readable
+                    uri = URLDecoder.decode(uri, "UTF-8");
+                }
+                // sanitize and mask uri so we dont see passwords
+                uri = URISupport.sanitizeUri(uri);
+                String state = getEndpointState(endpoint);
+                out.println(String.format(rowFormat, contextId, uri, state));
             }
         }
 
         return null;
     }
 
-    protected String getState(Endpoint endpoint) {
+    private Map<String, Integer> computeColumnWidths(final Iterable<Endpoint> endpoints) throws Exception {
+        if (endpoints == null) {
+            throw new IllegalArgumentException("Unable to determine column widths from null Iterable<Endpoint>");
+        } else {
+            int maxContextLen = 0;
+            int maxUriLen = 0;
+            int maxStatusLen = 0;
+
+            for (final Endpoint endpoint : endpoints) {
+                final String name = endpoint.getCamelContext().getName();
+                maxContextLen = java.lang.Math.max(maxContextLen, name == null ? 0 : name.length());
+
+                String uri = endpoint.getEndpointUri();
+                if (decode == null || decode) {
+                    // decode uri so its more human readable
+                    uri = URLDecoder.decode(uri, "UTF-8");
+                }
+                // sanitize and mask uri so we dont see passwords
+                uri = URISupport.sanitizeUri(uri);
+
+                maxUriLen = java.lang.Math.max(maxUriLen, uri == null ? 0 : uri.length());
+
+                final String status = getEndpointState(endpoint);
+                maxStatusLen = java.lang.Math.max(maxStatusLen, status == null ? 0 : status.length());
+            }
+
+            final Map<String, Integer> retval = new Hashtable<String, Integer>(3);
+            retval.put(CONTEXT_COLUMN_LABEL, maxContextLen);
+            retval.put(URI_COLUMN_LABEL, maxUriLen);
+            retval.put(STATUS_COLUMN_LABEL, maxStatusLen);
+
+            return retval;
+        }
+    }
+
+    private String buildFormatString(final Map<String, Integer> columnWidths, final boolean isHeader) {
+        final String fieldPreamble;
+        final String fieldPostamble;
+        final int columnWidthIncrement;
+
+        if (isHeader) {
+            fieldPreamble = DEFAULT_HEADER_PREAMBLE;
+            fieldPostamble = DEFAULT_HEADER_POSTAMBLE;
+        } else {
+            fieldPreamble = DEFAULT_FIELD_PREAMBLE;
+            fieldPostamble = DEFAULT_FIELD_POSTAMBLE;
+        }
+        columnWidthIncrement = DEFAULT_COLUMN_WIDTH_INCREMENT;
+
+        final int contextLen = java.lang.Math.min(columnWidths.get(CONTEXT_COLUMN_LABEL) + columnWidthIncrement, getMaxColumnWidth());
+        final int uriLen = java.lang.Math.min(columnWidths.get(URI_COLUMN_LABEL) + columnWidthIncrement, getMaxColumnWidth());
+        final int statusLen = java.lang.Math.min(columnWidths.get(STATUS_COLUMN_LABEL) + columnWidthIncrement, getMaxColumnWidth());
+
+        final StringBuilder retval = new StringBuilder(DEFAULT_FORMAT_BUFFER_LENGTH);
+        retval.append(fieldPreamble).append("%-").append(contextLen).append('.').append(contextLen).append('s').append(fieldPostamble).append(' ');
+        retval.append(fieldPreamble).append("%-").append(uriLen).append('.').append(uriLen).append('s').append(fieldPostamble).append(' ');
+        retval.append(fieldPreamble).append("%-").append(statusLen).append('.').append(statusLen).append('s').append(fieldPostamble).append(' ');
+
+        return retval.toString();
+    }
+
+    private int getMaxColumnWidth() {
+        if (verbose != null && verbose) {
+            return Integer.MAX_VALUE;
+        } else {
+            return MAX_COLUMN_WIDTH;
+        }
+    }
+
+    private static String getEndpointState(Endpoint endpoint) {
         // must use String type to be sure remote JMX can read the attribute without requiring Camel classes.
         if (endpoint instanceof StatefulService) {
             ServiceStatus status = ((StatefulService) endpoint).getStatus();
--- platforms/karaf/commands/src/main/java/org/apache/camel/karaf/commands/RouteList.java
+++ platforms/karaf/commands/src/main/java/org/apache/camel/karaf/commands/RouteList.java
@@ -16,12 +16,13 @@
  */
 package org.apache.camel.karaf.commands;
 
-import java.util.LinkedList;
+import java.io.PrintStream;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
-import org.apache.camel.CamelContext;
+import org.apache.camel.Route;
 import org.apache.camel.ServiceStatus;
-import org.apache.camel.model.RouteDefinition;
 import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
@@ -32,12 +33,17 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 @Command(scope = "camel", name = "route-list", description = "List Camel routes.")
 public class RouteList extends OsgiCommandSupport {
 
-    protected static final String HEADER_FORMAT = "%-20s %-20s %-20s";
-    protected static final String OUTPUT_FORMAT = "[%-18s] [%-18s] [%-18s]";
-    protected static final String UNKNOWN = "Unknown";
-    protected static final String ROUTE_ID = "Route Id";
-    protected static final String CONTEXT_ID = "Context Name";
-    protected static final String STATUS = "Status";
+    private static final String CONTEXT_COLUMN_LABEL = "Context";
+    private static final String ROUTE_COLUMN_LABEL = "Route";
+    private static final String STATUS_COLUMN_LABEL = "Status";
+
+    private static final int DEFAULT_COLUMN_WIDTH_INCREMENT = 0;
+    private static final String DEFAULT_FIELD_PREAMBLE = "[ ";
+    private static final String DEFAULT_FIELD_POSTAMBLE = " ]";
+    private static final String DEFAULT_HEADER_PREAMBLE = "  ";
+    private static final String DEFAULT_HEADER_POSTAMBLE = "  ";
+    private static final int DEFAULT_FORMAT_BUFFER_LENGTH = 24;
+    private static final int MAX_COLUMN_WIDTH = Integer.MAX_VALUE;
 
     @Argument(index = 0, name = "name", description = "The Camel context name where to look for the route", required = false, multiValued = false)
     String name;
@@ -49,27 +55,78 @@ public class RouteList extends OsgiCommandSupport {
     }
 
     protected Object doExecute() throws Exception {
-        System.out.println(String.format(HEADER_FORMAT, ROUTE_ID, CONTEXT_ID, STATUS));
+        List<Route> routes = camelController.getRoutes(name);
 
-        List<CamelContext> camelContexts = new LinkedList<CamelContext>();
-        if (name != null && camelController.getCamelContext(name) != null) {
-            camelContexts.add(camelController.getCamelContext(name));
-        } else {
-            camelContexts = camelController.getCamelContexts();
-        }
+        final Map<String, Integer> columnWidths = computeColumnWidths(routes);
+        final String headerFormat = buildFormatString(columnWidths, true);
+        final String rowFormat = buildFormatString(columnWidths, false);
+        final PrintStream out = System.out;
 
-        for (CamelContext camelContext : camelContexts) {
-            List<RouteDefinition> routeDefinitions = camelController.getRouteDefinitions(camelContext.getName());
-            if (routeDefinitions != null && !routeDefinitions.isEmpty()) {
-                for (RouteDefinition routeDefinition : routeDefinitions) {
-                    String contextName = camelContext.getName();
-                    ServiceStatus status = camelContext.getRouteStatus(routeDefinition.getId());
-                    System.out.println(String.format(OUTPUT_FORMAT, routeDefinition.getId(), contextName, status != null ? status.name() : UNKNOWN));
-                }
+        if (routes.size() > 0) {
+            out.println(String.format(headerFormat, CONTEXT_COLUMN_LABEL, ROUTE_COLUMN_LABEL, STATUS_COLUMN_LABEL));
+            for (Route route : routes) {
+                String contextId = route.getRouteContext().getCamelContext().getName();
+                String routeId = route.getId();
+                ServiceStatus status = route.getRouteContext().getCamelContext().getRouteStatus(routeId);
+                out.println(String.format(rowFormat, contextId, routeId, status));
             }
         }
 
         return null;
+    }
+
+    private static Map<String, Integer> computeColumnWidths(final Iterable<Route> routes) throws Exception {
+        if (routes == null) {
+            throw new IllegalArgumentException("Unable to determine column widths from null Iterable<Route>");
+        } else {
+            int maxContextLen = 0;
+            int maxRouteLen = 0;
+            int maxStatusLen = 0;
+
+            for (final Route route : routes) {
+                final String contextId = route.getRouteContext().getCamelContext().getName();
+                maxContextLen = java.lang.Math.max(maxContextLen, contextId == null ? 0 : contextId.length());
+
+                final String routeId = route.getId();
+                maxRouteLen = java.lang.Math.max(maxRouteLen, routeId == null ? 0 : routeId.length());
+
+                final String status = route.getRouteContext().getCamelContext().getRouteStatus(routeId).name();
+                maxStatusLen = java.lang.Math.max(maxStatusLen, status == null ? 0 : status.length());
+            }
+
+            final Map<String, Integer> retval = new Hashtable<String, Integer>(3);
+            retval.put(CONTEXT_COLUMN_LABEL, maxContextLen);
+            retval.put(ROUTE_COLUMN_LABEL, maxRouteLen);
+            retval.put(STATUS_COLUMN_LABEL, maxStatusLen);
+
+            return retval;
+        }
+    }
+
+    private static String buildFormatString(final Map<String, Integer> columnWidths, final boolean isHeader) {
+        final String fieldPreamble;
+        final String fieldPostamble;
+        final int columnWidthIncrement;
+
+        if (isHeader) {
+            fieldPreamble = DEFAULT_HEADER_PREAMBLE;
+            fieldPostamble = DEFAULT_HEADER_POSTAMBLE;
+        } else {
+            fieldPreamble = DEFAULT_FIELD_PREAMBLE;
+            fieldPostamble = DEFAULT_FIELD_POSTAMBLE;
+        }
+        columnWidthIncrement = DEFAULT_COLUMN_WIDTH_INCREMENT;
+
+        final int contextLen = java.lang.Math.min(columnWidths.get(CONTEXT_COLUMN_LABEL) + columnWidthIncrement, MAX_COLUMN_WIDTH);
+        final int routeLen = java.lang.Math.min(columnWidths.get(ROUTE_COLUMN_LABEL) + columnWidthIncrement, MAX_COLUMN_WIDTH);
+        final int statusLen = java.lang.Math.min(columnWidths.get(STATUS_COLUMN_LABEL) + columnWidthIncrement, MAX_COLUMN_WIDTH);
+
+        final StringBuilder retval = new StringBuilder(DEFAULT_FORMAT_BUFFER_LENGTH);
+        retval.append(fieldPreamble).append("%-").append(contextLen).append('.').append(contextLen).append('s').append(fieldPostamble).append(' ');
+        retval.append(fieldPreamble).append("%-").append(routeLen).append('.').append(routeLen).append('s').append(fieldPostamble).append(' ');
+        retval.append(fieldPreamble).append("%-").append(statusLen).append('.').append(statusLen).append('s').append(fieldPostamble).append(' ');
+
+        return retval.toString();
     }
 
 }
