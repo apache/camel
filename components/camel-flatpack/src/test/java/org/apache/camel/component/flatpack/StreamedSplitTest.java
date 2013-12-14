96a991760b74d1cc31b09f98c4bc09ab16770bb5
Claus Ibsen <davsclaus@apache.org>
2013-12-14 10:05:11 +0100

    CAMEL-7068: Fixed camel-flatpack in splitter streaming mode. Thanks to Xavier Fournet for the patch.

--- components/camel-flatpack/src/test/java/org/apache/camel/component/flatpack/StreamedSplitTest.java
+++ components/camel-flatpack/src/test/java/org/apache/camel/component/flatpack/StreamedSplitTest.java
@@ -37,7 +37,7 @@ import static org.junit.Assert.assertNotNull;
  * @version
  */
 @ContextConfiguration
-public class DelimitedWithNoDescriptorTest extends AbstractJUnit4SpringContextTests {
+public class StreamedSplitTest extends AbstractJUnit4SpringContextTests {
     private static final Logger LOG = LoggerFactory.getLogger(FixedLengthTest.class);
 
     @EndpointInject(uri = "mock:results")
