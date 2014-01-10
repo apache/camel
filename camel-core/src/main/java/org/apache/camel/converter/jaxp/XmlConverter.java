483b445dc77487e2d0f3d8c8bf1a7bbab04464c9
Willem Jiang <willem.jiang@gmail.com>
2014-01-10 17:17:30 +0800

    CAMEL-7123 Enable the xml transformer security processing feature by default

--- camel-core/src/main/java/org/apache/camel/converter/jaxp/XmlConverter.java
+++ camel-core/src/main/java/org/apache/camel/converter/jaxp/XmlConverter.java
@@ -974,6 +974,12 @@ public class XmlConverter {
 
     public TransformerFactory createTransformerFactory() {
         TransformerFactory factory = TransformerFactory.newInstance();
+        // Enable the Security feature by default
+        try {
+            factory.setFeature(javax.xml.XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        } catch (TransformerConfigurationException e) {
+            LOG.warn("TransformerFactory doesn't support the feature {} with value {}, due to {}.", new Object[]{javax.xml.XMLConstants.FEATURE_SECURE_PROCESSING, "true", e});
+        }
         factory.setErrorListener(new XmlErrorListener());
         return factory;
     }
--- camel-core/src/test/java/org/apache/camel/component/xslt/XsltRouteTest.java
+++ camel-core/src/test/java/org/apache/camel/component/xslt/XsltRouteTest.java
@@ -22,15 +22,35 @@ import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.converter.jaxp.XmlConverter;
 import org.apache.camel.impl.JndiRegistry;
 
 public class XsltRouteTest extends ContextTestSupport {
+    
     public void testSendStringMessage() throws Exception {
         sendMessageAndHaveItTransformed("<mail><subject>Hey</subject><body>Hello world!</body></mail>");
     }
     
     public void testSendBytesMessage() throws Exception {
         sendMessageAndHaveItTransformed("<mail><subject>Hey</subject><body>Hello world!</body></mail>".getBytes());
+    }
+    
+    public void testSendEntityMessage() throws Exception {
+        
+        MockEndpoint endpoint = getMockEndpoint("mock:result");
+        endpoint.expectedMessageCount(1);
+        //String message = "<!DOCTYPE foo [<!ENTITY xxe SYSTEM \"file:///Users//jiangning//.CFUserTextEncoding\">]><task><name>&xxe;</name></task>";
+
+        String message = "<hello/>";
+        template.sendBody("direct:start2", message);
+
+        assertMockEndpointsSatisfied();
+        
+        List<Exchange> list = endpoint.getReceivedExchanges();
+        Exchange exchange = list.get(0);
+        String xml = exchange.getIn().getBody(String.class);
+        
+        System.out.println(xml);
     }
 
     private void sendMessageAndHaveItTransformed(Object body) throws Exception {
@@ -44,7 +64,8 @@ public class XsltRouteTest extends ContextTestSupport {
         List<Exchange> list = endpoint.getReceivedExchanges();
         Exchange exchange = list.get(0);
         String xml = exchange.getIn().getBody(String.class);
-
+        System.out.println(xml);
+        
         assertNotNull("The transformed XML should not be null", xml);
         assertTrue(xml.indexOf("transformed") > -1);
         // the cheese tag is in the transform.xsl
@@ -62,11 +83,16 @@ public class XsltRouteTest extends ContextTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
+                
                 from("direct:start")
                     .to("xslt:org/apache/camel/component/xslt/transform.xsl")
                     .multicast()
                         .beanRef("testBean")
                         .to("mock:result");
+                
+                from("direct:start2")
+                    .to("xslt:org/apache/camel/component/xslt/transform_text_imported.xsl")
+                    .to("mock:result");
             }
         };
     }
