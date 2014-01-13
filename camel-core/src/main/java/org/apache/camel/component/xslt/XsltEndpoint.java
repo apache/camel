341d4e6cca71c53c90962d1c3d45fc9e05cc50c6
Willem Jiang <willem.jiang@gmail.com>
2014-01-13 11:02:48 +0800

    CAMEL-7129: Remove the CamelXsltResourceUri header from xslt component

--- camel-core/src/main/java/org/apache/camel/component/xslt/XsltEndpoint.java
+++ camel-core/src/main/java/org/apache/camel/component/xslt/XsltEndpoint.java
@@ -81,19 +81,12 @@ public class XsltEndpoint extends ProcessorEndpoint {
 
     @Override
     protected void onExchange(Exchange exchange) throws Exception {
-        String newResourceUri = exchange.getIn().getHeader(XsltConstants.XSLT_RESOURCE_URI, String.class);
-        if (newResourceUri != null) {
-            exchange.getIn().removeHeader(XsltConstants.XSLT_RESOURCE_URI);
 
-            LOG.trace("{} set to {} creating new endpoint to handle exchange", XsltConstants.XSLT_RESOURCE_URI, newResourceUri);
-            XsltEndpoint newEndpoint = findOrCreateEndpoint(getEndpointUri(), newResourceUri);
-            newEndpoint.onExchange(exchange);
-        } else {
-            if (!cacheStylesheet || cacheCleared) {
-                loadResource(resourceUri);
-            }
-            super.onExchange(exchange);
+        if (!cacheStylesheet || cacheCleared) {
+            loadResource(resourceUri);
         }
+        super.onExchange(exchange);
+
     }
 
     /**
