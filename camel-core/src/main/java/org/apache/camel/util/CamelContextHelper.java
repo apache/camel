0147b7a5a8771a145906f7951e60722bf2f80ba4
Claus Ibsen <davsclaus@apache.org>
2014-02-13 11:35:20 +0100

    CAMEL-7188: Fixed findComponents to work in OSGi, so we can discover all the Camel components in the container.

--- camel-core/src/main/java/org/apache/camel/util/CamelContextHelper.java
+++ camel-core/src/main/java/org/apache/camel/util/CamelContextHelper.java
@@ -365,7 +365,7 @@ public final class CamelContextHelper {
      */
     public static SortedMap<String, Properties> findComponents(CamelContext camelContext) throws LoadPropertiesException {
         ClassResolver resolver = camelContext.getClassResolver();
-        LOG.info("Finding all components using class resolver: {} -> {}", new Object[]{resolver});
+        LOG.debug("Finding all components using class resolver: {} -> {}", new Object[]{resolver});
         Enumeration<URL> iter = resolver.loadAllResourcesAsURL(COMPONENT_DESCRIPTOR);
         return findComponents(camelContext, iter);
     }
@@ -376,7 +376,7 @@ public final class CamelContextHelper {
         SortedMap<String, Properties> map = new TreeMap<String, Properties>();
         while (componentDescriptionIter != null && componentDescriptionIter.hasMoreElements()) {
             URL url = componentDescriptionIter.nextElement();
-            LOG.info("Finding components in url: {}", url);
+            LOG.trace("Finding components in url: {}", url);
             try {
                 Properties properties = new Properties();
                 properties.load(url.openStream());
