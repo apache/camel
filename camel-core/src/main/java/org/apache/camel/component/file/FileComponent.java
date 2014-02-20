ab0f8522391e75954c822455f259b3b3a9289a72
Willem Jiang <willem.jiang@gmail.com>
2014-02-20 16:42:04 +0800

    Fixed the CS error of FileComponent

--- camel-core/src/main/java/org/apache/camel/component/file/FileComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/file/FileComponent.java
@@ -33,6 +33,16 @@ import org.apache.camel.util.StringHelper;
  */
 public class FileComponent extends GenericFileComponent<File> implements EndpointCompleter {
 
+    /**
+     * GenericFile property on Camel Exchanges.
+     */
+    public static final String FILE_EXCHANGE_FILE = "CamelFileExchangeFile";
+
+    /**
+     * Default camel lock filename postfix
+     */
+    public static final String DEFAULT_LOCK_FILE_POSTFIX = ".camelLock";
+
     public FileComponent() {
         super();
         setEndpointClass(FileEndpoint.class);
@@ -42,16 +52,6 @@ public class FileComponent extends GenericFileComponent<File> implements Endpoin
         super(context);
         setEndpointClass(FileEndpoint.class);
     }
-
-    /**
-     * GenericFile property on Camel Exchanges.
-     */
-    public static final String FILE_EXCHANGE_FILE = "CamelFileExchangeFile";
-    
-    /**
-     * Default camel lock filename postfix
-     */
-    public static final String DEFAULT_LOCK_FILE_POSTFIX = ".camelLock";
 
     protected GenericFileEndpoint<File> buildFileEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
         // the starting directory must be a static (not containing dynamic expressions)
