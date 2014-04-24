9105dd7f2285b53e53e3c1458974f43c0dd02c2e
Claus Ibsen <davsclaus@apache.org>
2014-04-24 16:08:58 +0200

    CAMEL-7391: camel-netty - NettyProducer need to use its own timer, due netty issue https://github.com/netty/netty/issues/2424

--- components/camel-netty/src/main/java/org/apache/camel/component/netty/NettyProducer.java
+++ components/camel-netty/src/main/java/org/apache/camel/component/netty/NettyProducer.java
@@ -51,6 +51,8 @@ import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
 import org.jboss.netty.channel.socket.nio.NioDatagramWorkerPool;
 import org.jboss.netty.channel.socket.nio.WorkerPool;
+import org.jboss.netty.util.HashedWheelTimer;
+import org.jboss.netty.util.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -66,6 +68,7 @@ public class NettyProducer extends DefaultAsyncProducer {
     private BossPool bossPool;
     private WorkerPool workerPool;
     private ObjectPool<Channel> pool;
+    private Timer timer;
 
     public NettyProducer(NettyEndpoint nettyEndpoint, NettyConfiguration configuration) {
         super(nettyEndpoint);
@@ -123,6 +126,8 @@ public class NettyProducer extends DefaultAsyncProducer {
             }
         }
 
+        timer = new HashedWheelTimer();
+
         // setup pipeline factory
         ClientPipelineFactory factory = configuration.getClientPipelineFactory();
         if (factory != null) {
@@ -173,6 +178,11 @@ public class NettyProducer extends DefaultAsyncProducer {
             }
             pool.close();
             pool = null;
+        }
+
+        if (timer != null) {
+            timer.stop();
+            timer = null;
         }
 
         super.doStop();
@@ -327,7 +337,7 @@ public class NettyProducer extends DefaultAsyncProducer {
             if (bp == null) {
                 // create new pool which we should shutdown when stopping as its not shared
                 bossPool = new NettyClientBossPoolBuilder()
-                        .withTimer(getEndpoint().getTimer())
+                        .withTimer(timer)
                         .withBossCount(configuration.getBossCount())
                         .withName("NettyClientTCPBoss")
                         .build();
