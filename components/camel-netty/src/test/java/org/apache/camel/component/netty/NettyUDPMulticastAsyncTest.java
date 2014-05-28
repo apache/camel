b8999654a1faa5173a9f561ebf94a7e537dffe11
Babak Vahdat <bvahdat@apache.org>
2014-05-28 22:29:46 +0200

    CAMEL-7472: SingleUDPNettyServerBootstrapFactory should wait synchronously for the outcome of joining a UDP multicast group. Added a manual test for this UDP multicast scenario verification. Also polished some glitches by NettyUDPAsyncTest and tightened up it's asserts a bit.

--- components/camel-netty/src/test/java/org/apache/camel/component/netty/NettyUDPMulticastAsyncTest.java
+++ components/camel-netty/src/test/java/org/apache/camel/component/netty/NettyUDPMulticastAsyncTest.java
@@ -23,9 +23,38 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-public class NettyUDPAsyncTest extends BaseNettyTest {
+/**
+ * To run this test manually through Maven first remove the {@link Ignore}
+ * annotation below, then make sure you've got a Network interface with the name
+ * <code>en0</code> as given by the route below. If this is not the case run
+ * your OS specific command to find out which Network interfaces you've got
+ * supporting IPv4. For example on OS-X you can use the following command for
+ * this:
+ * 
+ * <pre>
+ *   <code>$> ifconfig -a</code>
+ * </pre>
+ * 
+ * Then replace the <code>en0</code> Network interface name below with your own
+ * one. Now running the test manually should succeed (<b>only</b> when using
+ * Java7+):
+ * 
+ * <pre>
+ *   <code>mvn test -Djava.net.preferIPv4Stack=true -Dtest=NettyUDPMulticastAsyncTest</code>
+ * </pre>
+ * 
+ * Note that the usage of JUnit {@link BeforeClass} annotation to achieve the
+ * same effect would not work in this case as at that stage it would be too late
+ * to use {@link System#setProperty(String, String) the Java API} to reach the
+ * same effect. Also setting such a system property through the surefire-plugin
+ * would cause side effect by the other tests of this component.
+ */
+@Ignore("See the Javadoc")
+public class NettyUDPMulticastAsyncTest extends BaseNettyTest {
 
     private void sendFile(String uri) throws Exception {
         template.send(uri, new Processor() {
@@ -43,7 +72,7 @@ public class NettyUDPAsyncTest extends BaseNettyTest {
         mock.expectedMessageCount(1);
         mock.message(0).body().startsWith("Song Of A Dream".getBytes());
 
-        sendFile("netty:udp://localhost:{{port}}?sync=false");
+        sendFile("netty:udp://224.1.2.3:{{port}}?sync=false");
 
         mock.assertIsSatisfied();
     }
@@ -53,7 +82,7 @@ public class NettyUDPAsyncTest extends BaseNettyTest {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("netty:udp://localhost:{{port}}?sync=false")
+                from("netty:udp://224.1.2.3:{{port}}?sync=false&networkInterface=en0")
                     .to("mock:result")
                     .to("log:Message"); 
             }
