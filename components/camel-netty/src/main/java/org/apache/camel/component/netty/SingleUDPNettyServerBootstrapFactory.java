b94664af145731aa1325433ee41e237417a59757
Babak Vahdat <bvahdat@apache.org>
2014-05-28 22:29:46 +0200

    CAMEL-7472: SingleUDPNettyServerBootstrapFactory should wait synchronously for the outcome of joining a UDP multicast group. Added a manual test for this UDP multicast scenario verification. Also polished some glitches by NettyUDPAsyncTest and tightened up it's asserts a bit.

--- components/camel-netty/src/main/java/org/apache/camel/component/netty/SingleUDPNettyServerBootstrapFactory.java
+++ components/camel-netty/src/main/java/org/apache/camel/component/netty/SingleUDPNettyServerBootstrapFactory.java
@@ -18,8 +18,6 @@ package org.apache.camel.component.netty;
 
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
@@ -118,7 +116,7 @@ public class SingleUDPNettyServerBootstrapFactory extends ServiceSupport impleme
         // noop
     }
 
-    protected void startServerBootstrap() throws UnknownHostException, SocketException {
+    protected void startServerBootstrap() throws Exception {
         // create non-shared worker pool
         int count = configuration.getWorkerCount() > 0 ? configuration.getWorkerCount() : NettyHelper.DEFAULT_IO_THREADS;
         workerPool = new NioDatagramWorkerPool(Executors.newCachedThreadPool(), count);
@@ -164,7 +162,7 @@ public class SingleUDPNettyServerBootstrapFactory extends ServiceSupport impleme
             multicastNetworkInterface = NetworkInterface.getByName(networkInterface);
             ObjectHelper.notNull(multicastNetworkInterface, "No network interface found for '" + networkInterface + "'.");
             LOG.info("ConnectionlessBootstrap joining {}:{} using network interface: {}", new Object[]{configuration.getHost(), configuration.getPort(), multicastNetworkInterface.getName()});
-            datagramChannel.joinGroup(hostAddress, multicastNetworkInterface);
+            datagramChannel.joinGroup(hostAddress, multicastNetworkInterface).syncUninterruptibly();
             allChannels.add(datagramChannel);
         } else {
             LOG.info("ConnectionlessBootstrap binding to {}:{}", configuration.getHost(), configuration.getPort());
