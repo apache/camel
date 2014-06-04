911e541323c16386371faedeb0a55f35b7ed3e9b
Dhiraj Bokde <dhirajsb@yahoo.com>
2014-06-04 13:55:36 -0700

    Updated archetype files to first real implementation

--- tooling/archetypes/camel-archetype-api-component/src/main/resources/archetype-resources/__artifactId__-component/src/main/java/__name__Component.java
+++ tooling/archetypes/camel-archetype-api-component/src/main/resources/archetype-resources/__artifactId__-component/src/main/java/__name__Component.java
@@ -16,19 +16,89 @@
 ## ------------------------------------------------------------------------
 package ${package};
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.CamelException;
 import org.apache.camel.Endpoint;
-import org.apache.camel.impl.DefaultComponent;
+import org.apache.camel.impl.UriEndpointComponent;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriParam;
+import org.apache.camel.util.IntrospectionSupport;
+
+import ${package}.internal.${name}ApiCollection;
+import ${package}.internal.${name}ApiName;
 
 /**
  * Represents the component that manages {@link ${name}Endpoint}.
  */
-public class ${name}Component extends DefaultComponent {
+@UriEndpoint(scheme = "${scheme}", consumerClass = ${name}Consumer.class, consumerPrefix = "consumer")
+public class ${name}Component extends UriEndpointComponent {
+
+    @UriParam
+    private ${name}Configuration configuration;
+
+    private final ${name}ApiCollection collection = ${name}ApiCollection.getCollection();
+
+    public ${name}Component(Class<? extends Endpoint> endpointClass) {
+        super(endpointClass);
+    }
+
+    public ${name}Component(CamelContext context, Class<? extends Endpoint> endpointClass) {
+        super(context, endpointClass);
+    }
 
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-        Endpoint endpoint = new ${name}Endpoint(uri, this);
+        // split remaining path to get API name and method
+        final String[] pathElements = remaining.split("/");
+        String apiNameStr;
+        String methodName;
+        switch (pathElements.length) {
+            case 1:
+                apiNameStr = "";
+                methodName = pathElements[0];
+                break;
+            case 2:
+                apiNameStr = pathElements[0];
+                methodName = pathElements[1];
+                break;
+            default:
+                throw new CamelException("Invalid URI path [" + remaining +
+                "], must be of the format " + collection.getApiNames() + "/<operation-apiName>");
+        }
+        // get API enum from apiName string
+        final ${name}ApiName apiName;
+        try {
+            apiName = ${name}ApiName.fromValue(apiNameStr);
+        } catch (IllegalArgumentException e) {
+            throw new CamelException("Invalid URI path [" + apiNameStr +
+                "], must be one of " + collection.getApiNames());
+        }
+
+        final ${name}Configuration endpointConfiguration = createEndpointConfiguration(apiName);
+        final Endpoint endpoint = new ${name}Endpoint(uri, this, apiName, methodName, endpointConfiguration);
+
+        // set endpoint property inBody
         setProperties(endpoint, parameters);
         return endpoint;
+    }
+
+    private ${name}Configuration createEndpointConfiguration(${name}ApiName name) throws Exception {
+        final Map<String, Object> componentProperties = new HashMap<String, Object>();
+        IntrospectionSupport.getProperties(configuration, componentProperties, null, false);
+
+        // create endpoint configuration with component properties
+        final ${name}Configuration endpointConfiguration = collection.getEndpointConfiguration(name);
+        IntrospectionSupport.setProperties(endpointConfiguration, componentProperties);
+        return endpointConfiguration;
+    }
+
+    public ${name}Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(${name}Configuration configuration) {
+        this.configuration = configuration;
     }
 }
--- tooling/archetypes/camel-archetype-api-component/src/main/resources/archetype-resources/__artifactId__-component/src/main/java/__name__Endpoint.java
+++ tooling/archetypes/camel-archetype-api-component/src/main/resources/archetype-resources/__artifactId__-component/src/main/java/__name__Endpoint.java
@@ -16,25 +16,73 @@
 ## ------------------------------------------------------------------------
 package ${package};
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriParam;
+import org.apache.camel.util.EndpointHelper;
+import org.apache.camel.util.ObjectHelper;
+import org.apache.camel.util.component.ApiMethod;
+import org.apache.camel.util.component.ApiMethodHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ${package}.internal.${name}ApiCollection;
+import ${package}.internal.${name}ApiName;
+import ${package}.internal.${name}PropertiesHelper;
 
 /**
  * Represents a ${name} endpoint.
  */
+@UriEndpoint(scheme = "${scheme}", consumerClass = ${name}Consumer.class, consumerPrefix = "consumer")
 public class ${name}Endpoint extends DefaultEndpoint {
 
-    public ${name}Endpoint() {
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(${name}Endpoint.class);
 
-    public ${name}Endpoint(String uri, ${name}Component component) {
+    @UriParam
+    private final ${name}Configuration configuration;
+
+    // property name for Exchange 'In' message body
+    @UriParam
+    private String inBody;
+
+    // API name
+    private final ${name}ApiName apiName;
+
+    // API method name
+    private final String methodName;
+
+    // API method helper
+    private final ApiMethodHelper methodHelper;
+
+    // candidate methods based on method name and endpoint configuration
+    private List<Enum<? extends ApiMethod>> candidates;
+
+    // TODO create and manage API proxies
+    private final Map<${name}ApiName, Object> proxyCache;
+
+    public ${name}Endpoint(String uri, ${name}Component component, ${name}ApiName apiName, String methodName, ${name}Configuration endpointConfiguration) {
         super(uri, component);
-    }
+        this.apiName = apiName;
+        this.methodName = methodName;
+        this.configuration = endpointConfiguration;
 
-    public ${name}Endpoint(String endpointUri) {
-        super(endpointUri);
+        methodHelper = ${name}ApiCollection.getCollection().getHelper(apiName);
+
+        // TODO manage API proxies
+        proxyCache = new HashMap<${name}ApiName, Object>();
+        proxyCache.put(${name}ApiName.HELLO1, new ${name}FileSigApi());
+        proxyCache.put(${name}ApiName.HELLO2, new ${name}JavadocSigApi());
     }
 
     public Producer createProducer() throws Exception {
@@ -42,10 +90,108 @@ public class ${name}Endpoint extends DefaultEndpoint {
     }
 
     public Consumer createConsumer(Processor processor) throws Exception {
-        return new ${name}Consumer(this, processor);
+        // make sure inBody is not set for consumers
+        if (inBody != null) {
+            throw new IllegalArgumentException("Option inBody is not supported for consumer endpoint");
+        }
+        final ${name}Consumer consumer = new ${name}Consumer(this, processor);
+        // also set consumer.* properties
+        configureConsumer(consumer);
+        return consumer;
     }
 
     public boolean isSingleton() {
         return true;
+    }
+
+    @Override
+    public void configureProperties(Map<String, Object> options) {
+        super.configureProperties(options);
+
+        // set configuration properties first
+        try {
+            EndpointHelper.setReferenceProperties(getCamelContext(), configuration, options);
+            EndpointHelper.setProperties(getCamelContext(), configuration, options);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+
+        // validate and initialize state
+        initState();
+    }
+
+    private void initState() {
+
+        // get endpoint property names
+        final Set<String> arguments = new HashSet<String>();
+        arguments.addAll(${name}PropertiesHelper.getHelper().getEndpointPropertyNames(configuration));
+
+        // add inBody argument for producers
+        if (inBody != null) {
+            arguments.add(inBody);
+        }
+        final String[] argNames = arguments.toArray(new String[arguments.size()]);
+
+        // create a list of candidate methods
+        candidates = new ArrayList<Enum<? extends ApiMethod>>();
+        candidates.addAll(methodHelper.getCandidateMethods(methodName, argNames));
+
+        // error if there are no candidates
+        if (candidates.isEmpty()) {
+            throw new IllegalArgumentException(
+                    String.format("No matching method for %s/%s, with arguments %s",
+                            apiName.getName(), methodName, arguments));
+        }
+
+        // log missing/extra properties for debugging
+        if (LOG.isDebugEnabled()) {
+            final Set<String> missing = methodHelper.getMissingProperties(methodName, arguments);
+            if (!missing.isEmpty()) {
+                LOG.debug("Method {} could use one or more properties from {}", methodName, missing);
+            }
+        }
+    }
+
+    @Override
+    public ${name}Component getComponent() {
+        return (${name}Component) super.getComponent();
+    }
+
+    public ${name}Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public ${name}ApiName getApiName() {
+        return apiName;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public ApiMethodHelper getMethodHelper() {
+        return methodHelper;
+    }
+
+    public List<Enum<? extends ApiMethod>> getCandidates() {
+        return Collections.unmodifiableList(candidates);
+    }
+
+    public String getInBody() {
+        return inBody;
+    }
+
+    public void setInBody(String inBody) {
+        // validate property name
+        ObjectHelper.notNull(inBody, "inBody");
+        if (!${name}PropertiesHelper.getHelper().getValidEndpointProperties(configuration).contains(inBody)) {
+            throw new IllegalArgumentException("Unknown property " + inBody);
+        }
+        this.inBody = inBody;
+    }
+
+    public Object getApiProxy() {
+        // TODO manage API proxies
+        return proxyCache.get(apiName);
     }
 }
