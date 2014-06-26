4cc8db61b199fc6a2f2bad46974d5f345ebe6cd8
Willem Jiang <willem.jiang@gmail.com>
2014-06-26 11:17:23 +0800

    CAMEL-7541 fixed the issue of RedisIdempotentRepository doesn't work out of box

--- components/camel-spring-redis/src/main/java/org/apache/camel/component/redis/processor/idempotent/RedisIdempotentRepository.java
+++ components/camel-spring-redis/src/main/java/org/apache/camel/component/redis/processor/idempotent/RedisIdempotentRepository.java
@@ -54,7 +54,11 @@ public class RedisIdempotentRepository extends ServiceSupport implements Idempot
 
     @ManagedOperation(description = "Adds the key to the store")
     public boolean add(String key) {
-        return setOperations.add(processorName, key) != null;
+        if (!contains(key)) { 
+            return setOperations.add(processorName, key) != null;
+        } else {
+            return false;
+        }
     }
 
     @ManagedOperation(description = "Does the store contain the given key")
@@ -64,6 +68,7 @@ public class RedisIdempotentRepository extends ServiceSupport implements Idempot
 
     @ManagedOperation(description = "Remove the key from the store")
     public boolean remove(String key) {
+        System.out.println("Remove the key" + key);
         return setOperations.remove(processorName, key) != null;
     }
 
