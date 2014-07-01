90c4fecddab3fb2ba0caca32d3244829f036a4ed
Alexander Lomov <alexandr.lomov@gmail.com>
2014-07-01 21:14:10 +0400

    Optimize imports.

--- components/camel-hazelcast/src/test/java/org/apache/camel/component/hazelcast/HazelcastComponentInstanceReferenceSpringTest.java
+++ components/camel-hazelcast/src/test/java/org/apache/camel/component/hazelcast/HazelcastComponentInstanceReferenceSpringTest.java
@@ -1,14 +1,10 @@
 package org.apache.camel.component.hazelcast;
 
 import org.junit.Test;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Created by Alexander Lomov on 01/07/14.
- */
+
 public class HazelcastComponentInstanceReferenceSpringTest extends HazelcastCamelSpringTestSupport {
 
     private static final String TEST_VALUE = "TestValue";
