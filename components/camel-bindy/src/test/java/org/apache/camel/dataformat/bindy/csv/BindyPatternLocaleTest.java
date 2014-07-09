3905d4fc384962bb4901a9e63fbc5749e8729316
ancosen <ancosen@gmail.com>
2014-07-09 09:01:56 +0200

    Test Camel-7578 updated

--- components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/csv/BindyPatternLocaleTest.java
+++ components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/csv/BindyPatternLocaleTest.java
@@ -1,102 +1,104 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.apache.camel.dataformat.bindy.csv;
 
-import java.text.DateFormat;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.Locale;
 
-import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.dataformat.bindy.model.padding.Unity;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 /**
  * @version
  */
-@RunWith(Parameterized.class)
 public class BindyPatternLocaleTest extends CamelTestSupport {
+    private Locale origLocale;
 
-	private String locale;
+    @Before
+    public void setLanguage() {
+        origLocale = Locale.getDefault();
+        Locale.setDefault(Locale.US);
+    }
 
-	public BindyPatternLocaleTest(String locale) {
-		this.locale = locale;
-	}
+    @After
+    public void restoreLanguage() {
+        Locale.setDefault(origLocale);
+    }
 
-	@Parameters
-	public static Collection<String[]> localeList() {
-		Locale list[] = DateFormat.getAvailableLocales();
-		Collection<String[]> collection = new HashSet<String[]>();
-		for (Locale aLocale : list) {
-			String[] localeLanguage = { aLocale.getLanguage() };
-			collection.add(localeLanguage);
-		}
-		return collection;
-	}
+    @Test
+    public void testMarshalling1() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:marshal");
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived("050,010\r\n");
 
-	@Test
-	public void testMarshalling() throws Exception {
-		context.getRouteDefinitions().get(0)
-				.adviceWith(context, new AdviceWithRouteBuilder() {
-					@Override
-					public void configure() throws Exception {
-						BindyCsvDataFormat bindy = new BindyCsvDataFormat(
-								Unity.class);
+        Unity unity = new Unity();
+        unity.setMandant(50f);
+        unity.setReceiver(10f);
+        template.sendBody("direct:marshal", unity);
 
-						// Injection of one of the available locale
-						bindy.setLocale(locale);
+        assertMockEndpointsSatisfied();
+    }
+    
+    @Test
+    public void testMarshalling2() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:marshal");
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived("080,001\r\n");
 
-						// weave the node in the route which has id = marshaller
-						// and replace it with the following route path
-						weaveById("marshaller").replace().marshal(bindy);
-					}
-				});
-		MockEndpoint mock = getMockEndpoint("mock:marshal");
-		mock.expectedMessageCount(1);
-		mock.expectedBodiesReceived("050,010\r\n");
+        Unity unity = new Unity();
+        unity.setMandant(80f);
+        unity.setReceiver(1f);
+        template.sendBody("direct:marshal", unity);
 
-		Unity unity = new Unity();
-		unity.setMandant(50f);
-		unity.setReceiver(10f);
-		template.sendBody("direct:marshal", unity);
+        assertMockEndpointsSatisfied();
+    }
+    
+    @Test
+    public void testMarshalling3() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:marshal");
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived("000,000\r\n");
 
-		assertMockEndpointsSatisfied();
-	}
+        Unity unity = new Unity();
+        unity.setMandant(0f);
+        unity.setReceiver(0f);
+        template.sendBody("direct:marshal", unity);
 
-	@Override
-	protected RouteBuilder createRouteBuilder() throws Exception {
-		return new RouteBuilder() {
-			@Override
-			public void configure() throws Exception {
-				BindyCsvDataFormat bindy = new BindyCsvDataFormat(Unity.class);
+        assertMockEndpointsSatisfied();
+    }
 
-				// As recommended, when we use @Datafield Pattern we must
-				// specify the default locale
-				bindy.setLocale("default");
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                BindyCsvDataFormat bindy = new BindyCsvDataFormat(Unity.class);
 
-				from("direct:marshal").marshal(bindy).id("marshaller")
-						.to("mock:marshal");
-			}
-		};
-	}
+                // As recommended, when we use @Datafield Pattern we must
+                // specify the default locale
+                bindy.setLocale("default");
+
+                from("direct:marshal").marshal(bindy).
+                to("mock:marshal");
+            }
+        };
+    }
 }
