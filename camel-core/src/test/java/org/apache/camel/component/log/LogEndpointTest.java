2bb29b7a8ffabcbbaf3116a423fe03b04088867c
Willem Jiang <willem.jiang@gmail.com>
2014-07-15 15:56:00 +0800

    CAMEL-7599 support to configure the LoggerEndpoint without using URI

--- camel-core/src/test/java/org/apache/camel/component/log/LogEndpointTest.java
+++ camel-core/src/test/java/org/apache/camel/component/log/LogEndpointTest.java
@@ -47,11 +47,22 @@ public class LogEndpointTest extends ContextTestSupport {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMessageCount(1);
 
-        template.sendBody("direct:start", "Hello World");
+        template.sendBody("direct:start1", "Hello World");
 
         assertMockEndpointsSatisfied();
 
         assertNotNull(logged);
+    }
+    
+    
+    public void testLogEndpointGroupSize() throws InterruptedException {
+        MockEndpoint out = getMockEndpoint("mock:result");
+        int expectedCount = 50;
+        out.expectedMessageCount(expectedCount);
+        for (int i = 0; i < expectedCount; i++) {
+            template.sendBody("direct:start2", "blub");
+        }
+        out.assertIsSatisfied();
     }
 
     @Override
@@ -62,10 +73,18 @@ public class LogEndpointTest extends ContextTestSupport {
                 LogEndpoint end = new LogEndpoint();
                 end.setCamelContext(context);
                 end.setLogger(new MyLogger());
+                
+                LogEndpoint endpoint = new LogEndpoint();
+                endpoint.setLoggerName("loggerSetter");
+                endpoint.setGroupSize(10);
+                endpoint.setCamelContext(context);
+                endpoint.start();
 
                 assertEquals("log:myLogger", end.getEndpointUri());
 
-                from("direct:start").to(end).to("mock:result");
+                from("direct:start1").to(end).to("mock:result");
+                
+                from("direct:start2").to(endpoint).to("mock:result");
             }
         };
     }
