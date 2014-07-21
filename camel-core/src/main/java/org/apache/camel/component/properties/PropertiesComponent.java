3039e87fe12e38f4b437273fe7c7047b3505ccc8
Grzegorz Grzybek <gr.grzybek@gmail.com>
2014-07-21 16:07:14 +0200

    [CAMEL-7456] Preserve get/set contract
    (cherry picked from commit e8ce1da8ec65b68d9609efc4b0b2581034ec4fe1)

--- camel-core/src/main/java/org/apache/camel/component/properties/PropertiesComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/properties/PropertiesComponent.java
@@ -76,7 +76,9 @@ public class PropertiesComponent extends DefaultComponent {
     private boolean ignoreMissingLocation;
     private boolean cache = true;
     private String propertyPrefix;
+    private String propertyPrefixResolved;
     private String propertySuffix;
+    private String propertySuffixResolved;
     private boolean fallbackToUnaugmentedProperty = true;
     private String prefixToken = DEFAULT_PREFIX_TOKEN;
     private String suffixToken = DEFAULT_SUFFIX_TOKEN;
@@ -156,7 +158,7 @@ public class PropertiesComponent extends DefaultComponent {
         
         if (propertiesParser instanceof AugmentedPropertyNameAwarePropertiesParser) {
             return ((AugmentedPropertyNameAwarePropertiesParser) propertiesParser).parseUri(uri, prop, prefixToken, suffixToken,
-                                                                                            propertyPrefix, propertySuffix, fallbackToUnaugmentedProperty);
+                                                                                            propertyPrefixResolved, propertySuffixResolved, fallbackToUnaugmentedProperty);
         } else {
             return propertiesParser.parseUri(uri, prop, prefixToken, suffixToken);
         }
@@ -204,8 +206,9 @@ public class PropertiesComponent extends DefaultComponent {
 
     public void setPropertyPrefix(String propertyPrefix) {
         this.propertyPrefix = propertyPrefix;
+        this.propertyPrefixResolved = propertyPrefix;
         if (ObjectHelper.isNotEmpty(this.propertyPrefix)) {
-            this.propertyPrefix = FilePathResolver.resolvePath(this.propertyPrefix);
+            this.propertyPrefixResolved = FilePathResolver.resolvePath(this.propertyPrefix);
         }
     }
 
@@ -215,8 +218,9 @@ public class PropertiesComponent extends DefaultComponent {
 
     public void setPropertySuffix(String propertySuffix) {
         this.propertySuffix = propertySuffix;
+        this.propertySuffixResolved = propertySuffix;
         if (ObjectHelper.isNotEmpty(this.propertySuffix)) {
-            this.propertySuffix = FilePathResolver.resolvePath(this.propertySuffix);
+            this.propertySuffixResolved = FilePathResolver.resolvePath(this.propertySuffix);
         }
     }
 
