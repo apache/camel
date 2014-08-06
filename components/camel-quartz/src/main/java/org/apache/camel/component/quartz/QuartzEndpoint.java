329a40c00efdb2eea8c002974083e51ae1ec3d5b
Kevin Earls <kevin@kevinearls.com>
2014-08-06 10:19:11 +0200

    Fix for CAMEL-7510 Expose the component options for Camel Quartz

--- components/camel-quartz/src/main/java/org/apache/camel/component/quartz/QuartzEndpoint.java
+++ components/camel-quartz/src/main/java/org/apache/camel/component/quartz/QuartzEndpoint.java
@@ -26,6 +26,8 @@ import org.apache.camel.ShutdownableService;
 import org.apache.camel.impl.DefaultEndpoint;
 import org.apache.camel.processor.loadbalancer.LoadBalancer;
 import org.apache.camel.processor.loadbalancer.RoundRobinLoadBalancer;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriParam;
 import org.apache.camel.support.ServiceSupport;
 import org.apache.camel.util.ObjectHelper;
 import org.apache.camel.util.ServiceHelper;
@@ -42,6 +44,7 @@ import org.slf4j.LoggerFactory;
  *
  * @version 
  */
+@UriEndpoint(scheme = "quartz", consumerClass = QuartzConsumer.class)
 public class QuartzEndpoint extends DefaultEndpoint implements ShutdownableService {
     private static final Logger LOG = LoggerFactory.getLogger(QuartzEndpoint.class);
 
@@ -49,8 +52,11 @@ public class QuartzEndpoint extends DefaultEndpoint implements ShutdownableServi
     private Trigger trigger;
     private JobDetail jobDetail = new JobDetail();
     private volatile boolean started;
+    @UriParam
     private volatile boolean stateful;
+    @UriParam
     private boolean deleteJob = true;
+    @UriParam
     private boolean pauseJob;
 
     public QuartzEndpoint(final String endpointUri, final QuartzComponent component) {
