c8c7474291fa6265f804c37ed21e20ca64d19198
Kevin Earls <kevin@kevinearls.com>
2014-08-13 19:42:00 +0200

    Fix for CAMEL-7694 Eliminated race condition which caused intermittent test failures

--- tests/camel-itest/src/test/java/org/apache/camel/itest/restlet/example/RestletGroovyIssueTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/restlet/example/RestletGroovyIssueTest.java
@@ -16,39 +16,48 @@
  */
 package org.apache.camel.itest.restlet.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.AvailablePortFinder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @version 
  */
 public class RestletGroovyIssueTest extends CamelTestSupport {
-
+    protected static final Logger LOG = LoggerFactory.getLogger(RestletGroovyIssueTest.class);
     private long port = AvailablePortFinder.getNextAvailable(16000);
     private ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     @Test
     public void testRestletGroovy() throws Exception {
-        getMockEndpoint("mock:input").expectedMessageCount(10);
-        getMockEndpoint("mock:output").expectedBodiesReceivedInAnyOrder("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-        
-        for (int i = 0; i < 10; i++) {
-            final Integer num = i;
+        String[] bodies = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        List<String> expectedBodies = Arrays.asList(bodies);
+        final CountDownLatch responsesToReceive = new CountDownLatch(expectedBodies.size());
+        getMockEndpoint("mock:input").expectedMessageCount(expectedBodies.size());
+        getMockEndpoint("mock:output").expectedBodiesReceivedInAnyOrder(expectedBodies);
+
+        for (final String s : expectedBodies) {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
-                    String s = "" + num;
                     Object response = template.requestBody("restlet:http://localhost:" + port + "/foo/" + s + "?restletMethod=GET", "");
                     assertEquals(s, response);
+                    responsesToReceive.countDown();
                 };
             });
         }
 
+        responsesToReceive.await(5, TimeUnit.SECONDS);
         assertMockEndpointsSatisfied();
 
         executorService.shutdownNow();
