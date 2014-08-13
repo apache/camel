0007e18611664fdbf9995f21e142f032388617ae
Claus Ibsen <davsclaus@apache.org>
2014-08-13 12:00:33 +0200

    CAMEL-7686: camel-jdbc - Fix this issue report about retriving generated keys

--- components/camel-jdbc/src/main/java/org/apache/camel/component/jdbc/JdbcProducer.java
+++ components/camel-jdbc/src/main/java/org/apache/camel/component/jdbc/JdbcProducer.java
@@ -189,6 +189,9 @@ public class JdbcProducer extends DefaultProducer {
                 setResultSet(exchange, rs);
             } else {
                 int updateCount = stmt.getUpdateCount();
+                // preserve headers
+                exchange.getOut().getHeaders().putAll(exchange.getIn().getHeaders());
+                // and then set the new header
                 exchange.getOut().setHeader(JdbcConstants.JDBC_UPDATE_COUNT, updateCount);
             }
 
