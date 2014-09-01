7f9959b65bab10821ea2b5ad5dedb2f23412c624
Willem Jiang <willem.jiang@gmail.com>
2014-09-01 13:45:35 +0800

    CAMEL-7755 Fixed the karaf feature test

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
@@ -227,6 +227,8 @@ public abstract class AbstractFeatureTest {
             //KarafDistributionOption.keepRuntimeFolder(),
             // override the config.properties (to fix pax-exam bug)
             //KarafDistributionOption.replaceConfigurationFile("etc/config.properties", new File("src/test/resources/org/apache/camel/itest/karaf/config.properties")),
+            // Update the jre.properties to export the sun.misc package
+            KarafDistributionOption.replaceConfigurationFile("etc/jre.properties", new File("src/test/resources/org/apache/camel/itest/karaf/jre.properties")),
             KarafDistributionOption.replaceConfigurationFile("etc/custom.properties", new File("src/test/resources/org/apache/camel/itest/karaf/custom.properties")),
             KarafDistributionOption.replaceConfigurationFile("etc/org.ops4j.pax.url.mvn.cfg", new File("src/test/resources/org/apache/camel/itest/karaf/org.ops4j.pax.url.mvn.cfg")),
             
