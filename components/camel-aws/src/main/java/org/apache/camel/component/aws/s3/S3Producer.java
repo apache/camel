aa73efb1b88ec216399c8064dd7f6be4c80e6744
Willem Jiang <willem.jiang@gmail.com>
2014-11-03 11:29:21 +0800

    CAMEL-7991 Added MultiPartUpload functionality to S3Producer with thanks to Andreas

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Producer.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Producer.java
@@ -18,19 +18,31 @@ package org.apache.camel.component.aws.s3;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
+import com.amazonaws.services.cloudfront.model.InvalidArgumentException;
+import com.amazonaws.services.s3.model.AbortMultipartUploadRequest;
 import com.amazonaws.services.s3.model.AccessControlList;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadResult;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PartETag;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.StorageClass;
+import com.amazonaws.services.s3.model.UploadPartRequest;
 
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.impl.DefaultProducer;
+import org.apache.camel.util.CastUtils;
 import org.apache.camel.util.FileUtil;
 import org.apache.camel.util.IOHelper;
 import org.apache.camel.util.URISupport;
@@ -49,55 +61,114 @@ public class S3Producer extends DefaultProducer {
         super(endpoint);
     }
 
+
     @Override
     public void process(final Exchange exchange) throws Exception {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-
-        Long contentLength = exchange.getIn().getHeader(S3Constants.CONTENT_LENGTH, Long.class);
-        if (contentLength != null) {
-            objectMetadata.setContentLength(contentLength);
+        if (getConfiguration().isMultiPartUpload()) {
+            processMultiPart(exchange);
+        } else {
+            processSingleOp(exchange);
         }
 
-        String contentType = exchange.getIn().getHeader(S3Constants.CONTENT_TYPE, String.class);
-        if (contentType != null) {
-            objectMetadata.setContentType(contentType);
+
+    }
+
+    public void processMultiPart(final Exchange exchange) throws Exception {
+        File filePayload = null;
+        final Object obj = exchange.getIn().getMandatoryBody();
+        if (obj instanceof File) {
+            filePayload = (File) obj;
+        } else {
+            throw new InvalidArgumentException("aws-s3: MultiPart upload requires a File input.");
         }
 
-        String cacheControl = exchange.getIn().getHeader(S3Constants.CACHE_CONTROL, String.class);
-        if (cacheControl != null) {
-            objectMetadata.setCacheControl(cacheControl);
+        ObjectMetadata objectMetadata = determineMetadata(exchange);
+        if (objectMetadata.getContentLength() == 0) {
+            objectMetadata.setContentLength(filePayload.length());
         }
 
-        String contentDisposition = exchange.getIn().getHeader(S3Constants.CONTENT_DISPOSITION, String.class);
-        if (contentDisposition != null) {
-            objectMetadata.setContentDisposition(contentDisposition);
+        final String keyName = determineKey(exchange);
+        final InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(getConfiguration().getBucketName(),
+                keyName, objectMetadata);
+
+        String storageClass = determineStorageClass(exchange);
+        if (storageClass != null) {
+            initRequest.setStorageClass(StorageClass.fromValue(storageClass));
         }
 
-        String contentEncoding = exchange.getIn().getHeader(S3Constants.CONTENT_ENCODING, String.class);
-        if (contentEncoding != null) {
-            objectMetadata.setContentEncoding(contentEncoding);
+        String cannedAcl = exchange.getIn().getHeader(S3Constants.CANNED_ACL, String.class);
+        if (cannedAcl != null) {
+            CannedAccessControlList objectAcl = CannedAccessControlList.valueOf(cannedAcl);
+            initRequest.setCannedACL(objectAcl);
         }
 
-        String contentMD5 = exchange.getIn().getHeader(S3Constants.CONTENT_MD5, String.class);
-        if (contentMD5 != null) {
-            objectMetadata.setContentMD5(contentMD5);
+        AccessControlList acl = exchange.getIn().getHeader(S3Constants.ACL, AccessControlList.class);
+        if (acl != null) {
+            // note: if cannedacl and acl are both specified the last one will be used. refer to
+            // PutObjectRequest#setAccessControlList for more details
+            initRequest.setAccessControlList(acl);
         }
 
-        Date lastModified = exchange.getIn().getHeader(S3Constants.LAST_MODIFIED, Date.class);
-        if (lastModified != null) {
-            objectMetadata.setLastModified(lastModified);
+        LOG.trace("Initiating multipart upload [{}] from exchange [{}]...", initRequest, exchange);
+
+        final InitiateMultipartUploadResult initResponse = getEndpoint().getS3Client().initiateMultipartUpload(initRequest);
+        final long contentLength = objectMetadata.getContentLength();
+        final List<PartETag> partETags = new ArrayList<PartETag>();
+        long partSize = getConfiguration().getPartSize();
+        CompleteMultipartUploadResult uploadResult = null;
+
+        long filePosition = 0;
+
+
+        try {
+            for (int part = 1; filePosition < contentLength; part++) {
+                partSize = Math.min(partSize, contentLength - filePosition);
+
+                UploadPartRequest uploadRequest = new UploadPartRequest()
+                        .withBucketName(getConfiguration().getBucketName()).withKey(keyName)
+                        .withUploadId(initResponse.getUploadId()).withPartNumber(part)
+                        .withFileOffset(filePosition)
+                        .withFile(filePayload)
+                        .withPartSize(partSize);
+
+                LOG.trace("Uploading part [{}] for {}", part, keyName);
+                partETags.add(getEndpoint().getS3Client().uploadPart(uploadRequest).getPartETag());
+
+                filePosition += partSize;
+            }
+            CompleteMultipartUploadRequest compRequest = new
+                    CompleteMultipartUploadRequest(getConfiguration().getBucketName(),
+                    keyName,
+                    initResponse.getUploadId(),
+                    partETags);
+
+            uploadResult = getEndpoint().getS3Client().completeMultipartUpload(compRequest);
+
+        } catch (Exception e) {
+            getEndpoint().getS3Client().abortMultipartUpload(new AbortMultipartUploadRequest(
+                    getConfiguration().getBucketName(), keyName, initResponse.getUploadId()));
+            throw e;
         }
 
-        Map<String, String> userMetadata = exchange.getIn().getHeader(S3Constants.USER_METADATA, Map.class);
-        if (userMetadata != null) {
-            objectMetadata.setUserMetadata(userMetadata);
+        Message message = getMessageForResponse(exchange);
+        message.setHeader(S3Constants.E_TAG, uploadResult.getETag());
+        if (uploadResult.getVersionId() != null) {
+            message.setHeader(S3Constants.VERSION_ID, uploadResult.getVersionId());
         }
+
+        if (getConfiguration().isDeleteAfterWrite() && filePayload != null) {
+            FileUtil.deleteFile(filePayload);
+        }
+    }
+
+    public void processSingleOp(final Exchange exchange) throws Exception {
+
+        ObjectMetadata objectMetadata = determineMetadata(exchange);
 
         File filePayload = null;
-
         Object obj = exchange.getIn().getMandatoryBody();
         if (obj instanceof File) {
-            filePayload = (File)obj;
+            filePayload = (File) obj;
         }
         PutObjectRequest putObjectRequest = new PutObjectRequest(getConfiguration().getBucketName(),
                 determineKey(exchange), exchange.getIn().getMandatoryBody(InputStream.class), objectMetadata);
@@ -135,6 +206,52 @@ public class S3Producer extends DefaultProducer {
             IOHelper.close(putObjectRequest.getInputStream());
             FileUtil.deleteFile(filePayload);
         }
+    }
+
+    private ObjectMetadata determineMetadata(final Exchange exchange) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+
+        Long contentLength = exchange.getIn().getHeader(S3Constants.CONTENT_LENGTH, Long.class);
+        if (contentLength != null) {
+            objectMetadata.setContentLength(contentLength);
+        }
+
+        String contentType = exchange.getIn().getHeader(S3Constants.CONTENT_TYPE, String.class);
+        if (contentType != null) {
+            objectMetadata.setContentType(contentType);
+        }
+
+        String cacheControl = exchange.getIn().getHeader(S3Constants.CACHE_CONTROL, String.class);
+        if (cacheControl != null) {
+            objectMetadata.setCacheControl(cacheControl);
+        }
+
+        String contentDisposition = exchange.getIn().getHeader(S3Constants.CONTENT_DISPOSITION, String.class);
+        if (contentDisposition != null) {
+            objectMetadata.setContentDisposition(contentDisposition);
+        }
+
+        String contentEncoding = exchange.getIn().getHeader(S3Constants.CONTENT_ENCODING, String.class);
+        if (contentEncoding != null) {
+            objectMetadata.setContentEncoding(contentEncoding);
+        }
+
+        String contentMD5 = exchange.getIn().getHeader(S3Constants.CONTENT_MD5, String.class);
+        if (contentMD5 != null) {
+            objectMetadata.setContentMD5(contentMD5);
+        }
+
+        Date lastModified = exchange.getIn().getHeader(S3Constants.LAST_MODIFIED, Date.class);
+        if (lastModified != null) {
+            objectMetadata.setLastModified(lastModified);
+        }
+
+        Map<String, String> userMetadata = CastUtils.cast(exchange.getIn().getHeader(S3Constants.USER_METADATA, Map.class));
+        if (userMetadata != null) {
+            objectMetadata.setUserMetadata(userMetadata);
+        }
+
+        return objectMetadata;
     }
 
     private String determineKey(final Exchange exchange) {
@@ -177,4 +294,5 @@ public class S3Producer extends DefaultProducer {
     public S3Endpoint getEndpoint() {
         return (S3Endpoint) super.getEndpoint();
     }
+
 }
