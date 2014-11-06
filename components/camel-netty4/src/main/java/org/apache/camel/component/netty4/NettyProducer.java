af8640ca8d91c4d429dfadbfc2a4dceb795e51f1
tterm <tterm@strace.org>
2014-11-06 20:52:53 +0100

    CAMEL-7998 Support connection less udp sending

--- components/camel-netty4/src/main/java/org/apache/camel/component/netty4/NettyProducer.java
+++ components/camel-netty4/src/main/java/org/apache/camel/component/netty4/NettyProducer.java
@@ -381,11 +381,17 @@ public class NettyProducer extends DefaultAsyncProducer {
             // set the pipeline factory, which creates the pipeline for each newly created channels
             connectionlessClientBootstrap.handler(pipelineFactory);
             // bind and store channel so we can close it when stopping
-            ChannelFuture channelFuture = connectionlessClientBootstrap.bind(new InetSocketAddress(0));
-            channelFuture.awaitUninterruptibly();
-            Channel channel = channelFuture.channel();
+            answer = connectionlessClientBootstrap.bind(new InetSocketAddress(0));
+            answer.awaitUninterruptibly();
+            Channel channel = answer.channel();
             allChannels.add(channel);
-            answer = connectionlessClientBootstrap.connect(new InetSocketAddress(configuration.getHost(), configuration.getPort()));
+            // if connectionless send is true we don't do a connect.
+            // we just send on the channel created with bind which means
+            // really fire and forget. You wont get an PortUnreachableException
+            // if no one is listen on the port
+            if (!configuration.isUdpConnectionlessSend()) {
+                answer = connectionlessClientBootstrap.connect(new InetSocketAddress(configuration.getHost(), configuration.getPort()));
+            }
 
             if (LOG.isDebugEnabled()) {
                 LOG.debug("Created new UDP client bootstrap connecting to {}:{} with options: {}",
