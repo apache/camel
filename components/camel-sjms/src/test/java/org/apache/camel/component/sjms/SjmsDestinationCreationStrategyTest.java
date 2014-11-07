aa4bc98bae40250667386068338ccea9155b2dc2
Willem Jiang <willem.jiang@gmail.com>
2014-11-07 10:37:21 +0800

    CAMEL-7908 Fixed some CS errors of camel-sjms

--- components/camel-sjms/src/test/java/org/apache/camel/component/sjms/SjmsDestinationCreationStrategyTest.java
+++ components/camel-sjms/src/test/java/org/apache/camel/component/sjms/SjmsDestinationCreationStrategyTest.java
@@ -1,4 +1,24 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.sjms;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
@@ -11,17 +31,10 @@ import org.apache.camel.component.sjms.support.JmsTestSupport;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.Test;
 
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Session;
-
-/**
- * @author
- */
 public class SjmsDestinationCreationStrategyTest extends JmsTestSupport {
 
-    private boolean createDestinationCalled = false;
-    private boolean createTemporaryDestination = false;
+    private boolean createDestinationCalled;
+    private boolean createTemporaryDestination;
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
@@ -56,7 +69,7 @@ public class SjmsDestinationCreationStrategyTest extends JmsTestSupport {
         assertTrue(createDestinationCalled);
 
         assertFalse(createTemporaryDestination);
-        String response = (String)template.sendBody("sjms:queue:inout?prefillPool=false&exchangePattern=InOut", ExchangePattern.InOut, "hello world 2" );
+        String response = (String)template.sendBody("sjms:queue:inout?prefillPool=false&exchangePattern=InOut", ExchangePattern.InOut, "hello world 2");
         assertTrue(createTemporaryDestination);
         assertEquals("response", response);
     }
