eede5a6c586a06e9a690a6a3be538c91b89d708f
Deven Phillips <deven.phillips@gmail.com>
2014-11-09 21:02:12 -0500

    Fixed regexes causing wrong channel names
    The regular expressions used to match the URIs were incorrect and this producing the 
    wrong channel names for NOTIFY/LISTEN.

--- components/camel-pgevent/src/main/java/org/apache/camel/component/pgevent/PgEventConsumer.java
+++ components/camel-pgevent/src/main/java/org/apache/camel/component/pgevent/PgEventConsumer.java
@@ -1,5 +1,6 @@
 package org.apache.camel.component.pgevent;
 
+import com.impossibl.postgres.api.jdbc.PGConnection;
 import com.impossibl.postgres.api.jdbc.PGNotificationListener;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
@@ -14,10 +15,20 @@ import org.slf4j.LoggerFactory;
 public class PgEventConsumer extends DefaultConsumer implements PGNotificationListener {
     private static final Logger LOG = LoggerFactory.getLogger(PgEventConsumer.class);
     private final PgEventEndpoint endpoint;
+    private PGConnection dbConnection;
 
     public PgEventConsumer(PgEventEndpoint endpoint, Processor processor) {
         super(endpoint, processor);
         this.endpoint = endpoint;
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+        
+        dbConnection = endpoint.initJdbc();
+        dbConnection.createStatement().execute("LISTEN "+endpoint.getChannel());
+        dbConnection.addNotificationListener(endpoint.getChannel(), endpoint.getChannel(), this);
     }
 
     @Override
@@ -32,5 +43,12 @@ public class PgEventConsumer extends DefaultConsumer implements PGNotificationLi
         } catch (Exception ex) {
             LOG.error("Unable to process incoming notification from PostgreSQL: processId='"+processId+"', channel='"+channel+"', payload='"+payload+"'", ex);
         }
+    }
+
+    @Override
+    protected void doShutdown() throws Exception {
+        super.doShutdown();
+        dbConnection.removeNotificationListener(endpoint.getChannel());
+        dbConnection.close();
     }
 }
