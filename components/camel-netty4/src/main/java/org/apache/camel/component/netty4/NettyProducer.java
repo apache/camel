9fd24db7e226974a57795dbc45065aa88c568374
tterm <tterm@strace.org>
2014-11-10 13:50:25 +0100

    CAMEL-7998 use bind connectionless and connect for connected udp

--- components/camel-netty4/src/main/java/org/apache/camel/component/netty4/NettyProducer.java
+++ components/camel-netty4/src/main/java/org/apache/camel/component/netty4/NettyProducer.java
@@ -381,15 +381,17 @@ public class NettyProducer extends DefaultAsyncProducer {
             // set the pipeline factory, which creates the pipeline for each newly created channels
             connectionlessClientBootstrap.handler(pipelineFactory);
             // bind and store channel so we can close it when stopping
-            answer = connectionlessClientBootstrap.bind(new InetSocketAddress(0));
-            answer.awaitUninterruptibly();
-            Channel channel = answer.channel();
-            allChannels.add(channel);
-            // if connectionless send is true we don't do a connect.
-            // we just send on the channel created with bind which means
-            // really fire and forget. You wont get an PortUnreachableException
-            // if no one is listen on the port
-            if (!configuration.isUdpConnectionlessSend()) {
+            if (configuration.isUdpConnectionlessSend()) {
+                // if connectionless send is true we don't do a connect.
+                // we just send on the channel created with bind which means
+                // really fire and forget. You wont get an PortUnreachableException
+                // if no one is listen on the port
+                answer = connectionlessClientBootstrap.bind(new InetSocketAddress(0));
+                answer.awaitUninterruptibly();
+                Channel channel = answer.channel();
+                allChannels.add(channel);
+            } else {
+
                 answer = connectionlessClientBootstrap.connect(new InetSocketAddress(configuration.getHost(), configuration.getPort()));
             }
 
