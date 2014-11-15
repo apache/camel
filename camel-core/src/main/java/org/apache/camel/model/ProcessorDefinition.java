149df99b672a4bee744a2cfb12f96f93021607ce
ancosen <ancosen@gmail.com>
2014-11-15 17:21:45 +0100

    Camel-8052 fixed checkstyle

--- camel-core/src/main/java/org/apache/camel/model/ProcessorDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/ProcessorDefinition.java
@@ -2882,7 +2882,7 @@ public abstract class ProcessorDefinition<Type extends ProcessorDefinition<Type>
      */
     @SuppressWarnings("unchecked")
     public Type removeProperties(String pattern) {
-        RemovePropertiesDefinition answer = new RemovePropertiesDefinition(pattern);
+    	RemovePropertiesDefinition answer = new RemovePropertiesDefinition(pattern);
         addOutput(answer);
         return (Type) this;
     }
