4883e1f5d51fad8a7bf0dc4348e95d19c7706d24
Claus Ibsen <davsclaus@apache.org>
2014-11-26 11:28:03 +0100

    CAMEL-8038: Fixed @UriPath

--- camel-core/src/main/java/org/apache/camel/component/binding/BindingNameComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/binding/BindingNameComponent.java
@@ -18,13 +18,8 @@ package org.apache.camel.component.binding;
 
 import java.util.Map;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.impl.UriEndpointComponent;
-import org.apache.camel.spi.Binding;
-import org.apache.camel.util.CamelContextHelper;
-
-import static org.apache.camel.util.CamelContextHelper.getMandatoryEndpoint;
 
 /**
  * To compose a Camel component with a Camel data-format as a single binding unit.
@@ -43,7 +38,6 @@ public class BindingNameComponent extends UriEndpointComponent {
 
     @Override
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-        CamelContext camelContext = getCamelContext();
         int idx = remaining.indexOf(":");
         if (idx <= 0) {
             throw new IllegalArgumentException(BAD_FORMAT_MESSAGE);
@@ -53,9 +47,7 @@ public class BindingNameComponent extends UriEndpointComponent {
         if (delegateURI.isEmpty()) {
             throw new IllegalArgumentException(BAD_FORMAT_MESSAGE);
         }
-        Binding binding = CamelContextHelper.mandatoryLookup(camelContext, bindingName, Binding.class);
-        Endpoint delegate = getMandatoryEndpoint(camelContext, delegateURI);
-        return new BindingEndpoint(uri, this, binding,  delegate);
+        return new BindingEndpoint(uri, this, bindingName, delegateURI);
     }
 
 }
