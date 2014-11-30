6674e4ef44c6f03cf94b129d41497690f06cf08a
Claus Ibsen <davsclaus@apache.org>
2014-11-30 17:28:11 +0100

    Fixed CS

--- camel-core/src/main/java/org/apache/camel/support/ServiceSupport.java
+++ camel-core/src/main/java/org/apache/camel/support/ServiceSupport.java
@@ -235,8 +235,9 @@ public abstract class ServiceSupport implements StatefulService {
     @Override
     public boolean isRunAllowed() {
         // if we have not yet initialized, then all options is false
-        if (!started.get() && !starting.get() && !stopping.get() && !stopped.get()
-            && !suspending.get() && !suspended.get() && !shutdown.get() && !shuttingdown.get()) {
+        boolean unused1 = !started.get() && !starting.get() && !stopping.get() && !stopped.get();
+        boolean unused2 = !suspending.get() && !suspended.get() && !shutdown.get() && !shuttingdown.get();
+        if (unused1 && unused2) {
             return false;
         }
         return !isStoppingOrStopped();
