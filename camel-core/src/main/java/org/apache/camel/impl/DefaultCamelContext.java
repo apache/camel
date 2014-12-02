e3821ca28605d856b6bbc3b4f95c3a7b04430876
Jochen Cordes <jcordes@redhat.com>
2014-12-02 08:47:14 +0100

    Fix for https://issues.apache.org/jira/browse/CAMEL-8103.

--- camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
+++ camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
@@ -2369,6 +2369,11 @@ public class DefaultCamelContext extends ServiceSupport implements ModelCamelCon
             addStartupListener(listener);
         }
 
+        if (service instanceof CamelContextAware) {
+            CamelContextAware aware = (CamelContextAware) service;
+            aware.setCamelContext(this);
+        }
+
         service.start();
     }
     
