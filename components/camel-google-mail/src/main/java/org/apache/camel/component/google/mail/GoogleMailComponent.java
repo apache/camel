3c6769dbd287d4ec919104649947b84eafc3050c
Jonathan Anstey <janstey@gmail.com>
2014-12-02 17:13:30 -0330

    CAMEL-8110 - google mail component

---
+++ components/camel-google-mail/src/main/java/org/apache/camel/component/google/mail/GoogleMailComponent.java
@@ -0,0 +1,75 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.google.mail;
+
+
+import com.google.api.services.gmail.Gmail;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.util.component.AbstractApiComponent;
+import org.apache.camel.component.google.mail.internal.GoogleMailApiCollection;
+import org.apache.camel.component.google.mail.internal.GoogleMailApiName;
+
+/**
+ * Represents the component that manages {@link GoogleMailEndpoint}.
+ */
+@UriEndpoint(scheme = "google-mail", consumerClass = GoogleMailConsumer.class, consumerPrefix = "consumer")
+public class GoogleMailComponent extends AbstractApiComponent<GoogleMailApiName, GoogleMailConfiguration, GoogleMailApiCollection> {
+
+    private Gmail client;
+    private GoogleMailClientFactory clientFactory;
+    
+    public GoogleMailComponent() {
+        super(GoogleMailEndpoint.class, GoogleMailApiName.class, GoogleMailApiCollection.getCollection());
+    }
+
+    public GoogleMailComponent(CamelContext context) {
+        super(context, GoogleMailEndpoint.class, GoogleMailApiName.class, GoogleMailApiCollection.getCollection());
+    }
+
+    @Override
+    protected GoogleMailApiName getApiName(String apiNameStr) throws IllegalArgumentException {
+        return GoogleMailApiName.fromValue(apiNameStr);
+    }
+
+    public Gmail getClient() {
+        if (client == null) {
+            client = getClientFactory().makeClient(configuration.getClientId(), configuration.getClientSecret(), configuration.getScopes(), 
+                configuration.getApplicationName(), configuration.getRefreshToken(), configuration.getAccessToken());
+        }
+        return client;
+    }
+    
+    public GoogleMailClientFactory getClientFactory() {
+        if (clientFactory == null) {
+            clientFactory = new BatchGoogleMailClientFactory();
+        }
+        return clientFactory;
+    }
+    
+    public void setClientFactory(GoogleMailClientFactory clientFactory) {
+        this.clientFactory = clientFactory;
+    }
+    
+    @Override
+    protected Endpoint createEndpoint(String uri, String methodName, GoogleMailApiName apiName,
+                                      GoogleMailConfiguration endpointConfiguration) {
+        return new GoogleMailEndpoint(uri, this, apiName, methodName, endpointConfiguration);
+    }
+}
---
+++ components/camel-google-mail/src/main/java/org/apache/camel/component/google/mail/GoogleMailConsumer.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.google.mail;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+
+import org.apache.camel.Processor;
+import org.apache.camel.RuntimeCamelException;
+import org.apache.camel.util.IntrospectionSupport;
+import org.apache.camel.util.component.AbstractApiConsumer;
+import org.apache.camel.component.google.mail.internal.GoogleMailApiName;
+
+/**
+ * The GoogleMail consumer.
+ */
+public class GoogleMailConsumer extends AbstractApiConsumer<GoogleMailApiName, GoogleMailConfiguration> {
+
+    public GoogleMailConsumer(GoogleMailEndpoint endpoint, Processor processor) {
+        super(endpoint, processor);
+    }
+    
+    @Override
+    protected Object doInvokeMethod(Map<String, Object> properties) throws RuntimeCamelException {
+        AbstractGoogleClientRequest request = (AbstractGoogleClientRequest) super.doInvokeMethod(properties);
+        try {
+            setProperty(properties, request, "q");
+            setProperty(properties, request, "maxResults");
+            setProperty(properties, request, "pageToken");
+            setProperty(properties, request, "format");            
+            return request.execute();
+        } catch (Exception e) {
+            throw new RuntimeCamelException(e);
+        }
+    }
+    
+    private void setProperty(Map<String, Object> properties, AbstractGoogleClientRequest request, String key) throws Exception {
+        if (properties.containsKey(key)) {
+            IntrospectionSupport.setProperty(getEndpoint().getCamelContext().getTypeConverter(), request, key, properties.get(key));
+        }
+    }
+}
