9a42f37e69ba37e0c60a0c4ec4bb83094b173685
Christian Schneider <chris@die-schneider.net>
2014-12-11 11:33:14 +0100

    Adding better assertion for exceptions

--- components/camel-test/src/main/java/org/apache/camel/test/junit4/TestSupport.java
+++ components/camel-test/src/main/java/org/apache/camel/test/junit4/TestSupport.java
@@ -147,6 +147,14 @@ public abstract class TestSupport extends Assert {
                    + value.getClass().getName(), expectedType.isInstance(value));
         return expectedType.cast(value);
     }
+    
+    public static <T extends Throwable> T assertThrowable(Class<T> expectedType, Throwable t) {
+    	assertNotNull("Expected an exinstance of type: " + expectedType.getName() + " but was null", t);
+    	if (!expectedType.isInstance(t)) {
+    		throw new AssertionError("Unexpected throwable", t);
+    	}
+    	return expectedType.cast(t);
+    }
 
     public static void assertEndpointUri(Endpoint endpoint, String uri) {
         assertNotNull("Endpoint is null when expecting endpoint for: " + uri, endpoint);
