52b7b9458d1db313a095edd28e51406b125b8c69
Jonathan Anstey <janstey@gmail.com>
2014-12-16 16:34:27 -0330

    CAMEL-8158 - Provide way to specify fields to patch in google drive endpoints

--- components/camel-google-drive/src/main/java/org/apache/camel/component/google/drive/GoogleDriveConsumer.java
+++ components/camel-google-drive/src/main/java/org/apache/camel/component/google/drive/GoogleDriveConsumer.java
@@ -46,6 +46,7 @@ public class GoogleDriveConsumer extends AbstractApiConsumer<GoogleDriveApiName,
             setProperty(properties, request, "q");
             setProperty(properties, request, "maxResults");
             setProperty(properties, request, "pageToken");
+            setProperty(properties, request, "fields");
             return request.execute();
         } catch (Exception e) {
             throw new RuntimeCamelException(e);
