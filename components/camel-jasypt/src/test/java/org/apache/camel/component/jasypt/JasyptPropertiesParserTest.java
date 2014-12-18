03b7e6cb12bce77267b20e4fe33e1dc49f8fa1db
Marius Thøring <marius.thoring@avinor.no>
2014-12-18 13:32:14 +0100

    -Added support for decrypting parts of property values -- Example property: some.endpoint=http://somehost?authUsername=username&authPassword=ENC(BjAoLxfpb1h)&token=ENC(ZWbB211g) -- Decrypted result: some.endpoint=http://somehost?authUsername=username&authPassword=password&token=sometoken -Added possibility to provide a custom encryptor (setEncryptor). -- Useful when the encryptor is provided through an osgi service.
    Note: The password and algorithm property is ignored when the encryptor property is set.

--- components/camel-jasypt/src/test/java/org/apache/camel/component/jasypt/JasyptPropertiesParserTest.java
+++ components/camel-jasypt/src/test/java/org/apache/camel/component/jasypt/JasyptPropertiesParserTest.java
@@ -16,31 +16,73 @@
  */
 package org.apache.camel.component.jasypt;
 
-import junit.framework.TestCase;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * @version 
- */
-public class JasyptPropertiesParserTest extends TestCase {
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
-    public void testJasyptPropertiesParser() throws Exception {
-        JasyptPropertiesParser parser = new JasyptPropertiesParser();
-        parser.setPassword("secret");
+public class JasyptPropertiesParserTest {
 
-        assertEquals("foo", parser.parseProperty(null, "foo", null));
-        assertEquals("tiger", parser.parseProperty(null, "ENC(bsW9uV37gQ0QHFu7KO03Ww==)", null));
+    private static final String KEY = "somekey";
+
+    private static final String ENCRYPTED_VALUE = "ENC(bsW9uV37gQ0QHFu7KO03Ww==)";
+    private static final String DECRYPTED_VALUE = "tiger";
+
+    private JasyptPropertiesParser jasyptPropertiesParser = new JasyptPropertiesParser();
+
+    @Before
+    public void before() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("secret");
+        jasyptPropertiesParser.setEncryptor(encryptor);
     }
 
-    public void testJasyptPropertiesParserSys() throws Exception {
+    @Test
+    public void testPlainPropertyIsUntouched() {
+        String result = jasyptPropertiesParser.parseProperty(KEY, "abc?1=decrypted1&2=decrypted2&cde=()", null);
+        assertThat(result, is("abc?1=decrypted1&2=decrypted2&cde=()"));
+    }
+
+    @Test
+    public void testDecryptsEncryptedProperty() {
+        String result = jasyptPropertiesParser.parseProperty(KEY, ENCRYPTED_VALUE, null);
+        assertThat(result, is(DECRYPTED_VALUE));
+    }
+
+    @Test
+    public void testDecryptsSinglePartEncryptedProperty() {
+        String result = jasyptPropertiesParser.parseProperty(KEY, "abc?1=" + ENCRYPTED_VALUE + "&cde=()", null);
+        assertThat(result, is("abc?1=" + DECRYPTED_VALUE + "&cde=()"));
+    }
+
+    @Test
+    public void testDecryptsMultiPartEncryptedProperty() {
+        String result = jasyptPropertiesParser.parseProperty(KEY, "abc?1=" + ENCRYPTED_VALUE + "&2=" + ENCRYPTED_VALUE + "&cde=()", null);
+        assertThat(result, is("abc?1=" + DECRYPTED_VALUE + "&2=" + DECRYPTED_VALUE + "&cde=()"));
+    }
+
+    @Test
+    public void testUsesProvidedPasswordIfEncryptorIsNotSet() throws Exception {
+        jasyptPropertiesParser.setEncryptor(null);
+        jasyptPropertiesParser.setPassword("secret");
+
+        assertEquals("foo", jasyptPropertiesParser.parseProperty(KEY, "foo", null));
+        assertEquals(DECRYPTED_VALUE, jasyptPropertiesParser.parseProperty(KEY, ENCRYPTED_VALUE, null));
+    }
+
+    @Test
+    public void testUsesProvidedPasswordFromSystemPropertyIfEncryptorIsNotSet() throws Exception {
         System.setProperty("myfoo", "secret");
 
-        JasyptPropertiesParser parser = new JasyptPropertiesParser();
-        parser.setPassword("sys:myfoo");
+        jasyptPropertiesParser.setEncryptor(null);
+        jasyptPropertiesParser.setPassword("sys:myfoo");
 
-        assertEquals("foo", parser.parseProperty(null, "foo", null));
-        assertEquals("tiger", parser.parseProperty(null, "ENC(bsW9uV37gQ0QHFu7KO03Ww==)", null));
+        assertEquals("foo", jasyptPropertiesParser.parseProperty(KEY, "foo", null));
+        assertEquals(DECRYPTED_VALUE, jasyptPropertiesParser.parseProperty(KEY, ENCRYPTED_VALUE, null));
 
         System.clearProperty("myfoo");
     }
-
 }
