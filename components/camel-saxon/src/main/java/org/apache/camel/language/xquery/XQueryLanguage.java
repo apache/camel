33b731f0b4496114e0cce42a8cf1ccf02ce627a1
Claus Ibsen <davsclaus@apache.org>
2015-01-14 13:35:56 +0100

    CAMEL-8242: Avoid clash of component and language in the same package for camel-saxon using same name = xquery.

--- components/camel-saxon/src/main/java/org/apache/camel/language/xquery/XQueryLanguage.java
+++ components/camel-saxon/src/main/java/org/apache/camel/language/xquery/XQueryLanguage.java
@@ -14,10 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.xquery;
+package org.apache.camel.language.xquery;
 
 import org.apache.camel.Expression;
 import org.apache.camel.Predicate;
+import org.apache.camel.component.xquery.XQueryBuilder;
 import org.apache.camel.support.LanguageSupport;
 
 /**
