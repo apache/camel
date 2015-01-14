893ba714bfa3d7c4dc70f97b9ff2b94a7bf4f0a8
Claus Ibsen <davsclaus@apache.org>
2015-01-14 13:38:56 +0100

    CAMEL-8242: Camel Toolbox - Include documentation about languages

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
