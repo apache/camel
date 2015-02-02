a97c6421ba4dc12a03fa84f66094e92f68182148
Willem Jiang <willem.jiang@gmail.com>
2015-02-02 12:42:27 +0800

    Fixed CS errors of camel-jetty9

--- components/camel-jetty9/src/main/java/org/apache/camel/component/jetty9/AttachmentHttpBinding.java
+++ components/camel-jetty9/src/main/java/org/apache/camel/component/jetty9/AttachmentHttpBinding.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.jetty9;
 
 import java.io.IOException;
@@ -24,24 +40,23 @@ final class AttachmentHttpBinding extends DefaultHttpBinding {
     protected void populateAttachments(HttpServletRequest request, HttpMessage message) {
         Object object = request.getAttribute("org.eclipse.jetty.servlet.MultiPartFile.multiPartInputStream");
         if (object instanceof MultiPartInputStreamParser) {
-                MultiPartInputStreamParser parser = (MultiPartInputStreamParser)object;
-                Collection<Part> parts;
-                try {
-                    parts = parser.getParts();
-                    for (Part part : parts) {
-                        String contentType = part.getContentType();
-                        if (!contentType.startsWith("application/octet-stream")) {
-                            continue;
-                        }
-                       
-                        DataSource ds = new PartDataSource(part);
-                        message.addAttachment(part.getName(), new DataHandler(ds));
+            MultiPartInputStreamParser parser = (MultiPartInputStreamParser)object;
+            Collection<Part> parts;
+            try {
+                parts = parser.getParts();
+                for (Part part : parts) {
+                    String contentType = part.getContentType();
+                    if (!contentType.startsWith("application/octet-stream")) {
+                        continue;
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
+
+                    DataSource ds = new PartDataSource(part);
+                    message.addAttachment(part.getName(), new DataHandler(ds));
                 }
-                
-                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
     
