5279296d5b082cae02c84826bd145d449b7fe6a2
nkukhar <kukhar.n@gmail.com>
2015-02-08 23:19:06 -0800

    CAMEL-8197 Added maven plagin to inject EIP documentation into the spring and blueprint XML DSL.

---
+++ tooling/maven/camel-eip-documentation-enricher-maven-plugin/src/main/java/org/apache/camel/maven/DomParser.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.maven;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+
+public class DomParser {
+  private final Logger logger = LoggerFactory.getLogger(DomParser.class);
+
+
+  public NodeList findElementsAndTypes(Document document, XPath xPath) throws XPathExpressionException {
+    return  (NodeList) xPath.compile("/xs:schema/xs:element")
+        .evaluate(document, XPathConstants.NODESET);
+  }
+
+  public NodeList findAttributesElements(Document document, XPath xPath, String name) throws XPathExpressionException {
+    return (NodeList) xPath.compile(
+        "/xs:schema/xs:complexType[@name='" + name + "']//xs:attribute")
+        .evaluate(document, XPathConstants.NODESET);
+  }
+}
