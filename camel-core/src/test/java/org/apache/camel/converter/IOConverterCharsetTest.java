f07a0c0291c92567bb26ad1088d90a917060b692
Willem Jiang <willem.jiang@gmail.com>
2015-02-25 12:57:37 +0800

    CAMEL-8356 Clean up the code

--- camel-core/src/test/java/org/apache/camel/converter/IOConverterCharsetTest.java
+++ camel-core/src/test/java/org/apache/camel/converter/IOConverterCharsetTest.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.converter;
 
 import java.io.BufferedReader;
@@ -10,68 +26,73 @@ import java.util.Arrays;
 import org.apache.camel.ContextTestSupport;
 
 public class IOConverterCharsetTest extends ContextTestSupport {
+    private static final String CONTENT = "G\u00f6tzend\u00e4mmerung,Joseph und seine Br\u00fcder";
 
-	public void testToInputStreamFileWithCharsetUTF8() throws Exception {
-		File file = new File("src/test/resources/org/apache/camel/converter/german.utf-8.txt");
-		InputStream in = IOConverter.toInputStream(file, "UTF-8");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
-		String line = reader.readLine();
-		String naiveLine = naiveReader.readLine();
-		assertEquals(naiveLine, line);
-		assertEquals("Götzendämmerung,Joseph und seine Brüder", line);
-		reader.close();
-		naiveReader.close();
-	}
+    public void testToInputStreamFileWithCharsetUTF8() throws Exception {
+        File file = new File("src/test/resources/org/apache/camel/converter/german.utf-8.txt");
+        InputStream in = IOConverter.toInputStream(file, "UTF-8");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+                                                                              "UTF-8"));
+        String line = reader.readLine();
+        String naiveLine = naiveReader.readLine();
+        assertEquals(naiveLine, line);
+        assertEquals(CONTENT, line);
+        reader.close();
+        naiveReader.close();
+    }
 
-	public void testToInputStreamFileWithCharsetLatin1() throws Exception {
-		File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
-		InputStream in = IOConverter.toInputStream(file, "ISO-8859-1");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "ISO-8859-1"));
-		String line = reader.readLine();
-		String naiveLine = naiveReader.readLine();
-		assertEquals(naiveLine, line);
-		assertEquals("Götzendämmerung,Joseph und seine Brüder", line);
-		reader.close();
-		naiveReader.close();
-	}
-	
-	public void testToInputStreamFileDirectByteDumpWithCharsetLatin1() throws Exception {
-		File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
-		InputStream in = IOConverter.toInputStream(file, "ISO-8859-1");
-		InputStream naiveIn = new FileInputStream(file);
-		byte[] bytes = new byte[8192];
-		in.read(bytes);
-		byte[] naiveBytes = new byte[8192];
-		naiveIn.read(naiveBytes);
-		assertFalse("both input streams deliver the same byte sequence", Arrays.equals(naiveBytes, bytes));
-		in.close();
-		naiveIn.close();
-	}
-	
-	public void testToReaderFileWithCharsetUTF8() throws Exception {
-		File file = new File("src/test/resources/org/apache/camel/converter/german.utf-8.txt");
-		BufferedReader reader = IOConverter.toReader(file, "UTF-8");
-		BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
-		String line = reader.readLine();
-		String naiveLine = naiveReader.readLine();
-		assertEquals(naiveLine, line);
-		assertEquals("Götzendämmerung,Joseph und seine Brüder", line);
-		reader.close();
-		naiveReader.close();
-	}
+    public void testToInputStreamFileWithCharsetLatin1() throws Exception {
+        File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
+        InputStream in = IOConverter.toInputStream(file, "ISO-8859-1");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+                                                                              "ISO-8859-1"));
+        String line = reader.readLine();
+        String naiveLine = naiveReader.readLine();
+        assertEquals(naiveLine, line);
+        assertEquals(CONTENT, line);
+        reader.close();
+        naiveReader.close();
+    }
 
-	public void testToReaderFileWithCharsetLatin1() throws Exception {
-		File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
-		BufferedReader reader = IOConverter.toReader(file, "ISO-8859-1");
-		BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "ISO-8859-1"));
-		String line = reader.readLine();
-		String naiveLine = naiveReader.readLine();
-		assertEquals(naiveLine, line);
-		assertEquals("Götzendämmerung,Joseph und seine Brüder", line);
-		reader.close();
-		naiveReader.close();
-	}
-	
+    public void testToInputStreamFileDirectByteDumpWithCharsetLatin1() throws Exception {
+        File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
+        InputStream in = IOConverter.toInputStream(file, "ISO-8859-1");
+        InputStream naiveIn = new FileInputStream(file);
+        byte[] bytes = new byte[8192];
+        in.read(bytes);
+        byte[] naiveBytes = new byte[8192];
+        naiveIn.read(naiveBytes);
+        assertFalse("both input streams deliver the same byte sequence", Arrays.equals(naiveBytes, bytes));
+        in.close();
+        naiveIn.close();
+    }
+
+    public void testToReaderFileWithCharsetUTF8() throws Exception {
+        File file = new File("src/test/resources/org/apache/camel/converter/german.utf-8.txt");
+        BufferedReader reader = IOConverter.toReader(file, "UTF-8");
+        BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+                                                                              "UTF-8"));
+        String line = reader.readLine();
+        String naiveLine = naiveReader.readLine();
+        assertEquals(naiveLine, line);
+        assertEquals(CONTENT, line);
+        reader.close();
+        naiveReader.close();
+    }
+
+    public void testToReaderFileWithCharsetLatin1() throws Exception {
+        File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
+        BufferedReader reader = IOConverter.toReader(file, "ISO-8859-1");
+        BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+                                                                              "ISO-8859-1"));
+        String line = reader.readLine();
+        String naiveLine = naiveReader.readLine();
+        assertEquals(naiveLine, line);
+        assertEquals(CONTENT, line);
+        reader.close();
+        naiveReader.close();
+    }
+
 }
