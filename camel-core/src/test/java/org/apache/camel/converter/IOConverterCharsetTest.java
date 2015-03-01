7f6538cb1b30a80eef3f743d0f9e98b3e7b8f1bc
Stefan Mandel <mandelst@gmail.com>
2015-03-01 12:26:15 +0100

    CAMEL-8356 corrections to default charset

--- camel-core/src/test/java/org/apache/camel/converter/IOConverterCharsetTest.java
+++ camel-core/src/test/java/org/apache/camel/converter/IOConverterCharsetTest.java
@@ -21,6 +21,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import org.apache.camel.ContextTestSupport;
@@ -29,10 +31,30 @@ public class IOConverterCharsetTest extends ContextTestSupport {
     private static final String CONTENT = "G\u00f6tzend\u00e4mmerung,Joseph und seine Br\u00fcder";
 
     public void testToInputStreamFileWithCharsetUTF8() throws Exception {
+    	switchToDefaultCharset("UTF-8");
         File file = new File("src/test/resources/org/apache/camel/converter/german.utf-8.txt");
         InputStream in = IOConverter.toInputStream(file, "UTF-8");
-        // need to specify the encoding of the input stream bytes
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        // do read with default charset!
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+        try {   
+            String line = reader.readLine();
+            String naiveLine = naiveReader.readLine();
+            assertEquals(naiveLine, line);
+            assertEquals(CONTENT, line);
+        } finally {
+            reader.close();
+            naiveReader.close();
+        }
+        
+    }
+
+    public void testToInputStreamFileWithCharsetUTF8withOtherDefaultEncoding() throws Exception {
+    	switchToDefaultCharset("ISO-8859-1");
+        File file = new File("src/test/resources/org/apache/camel/converter/german.utf-8.txt");
+        InputStream in = IOConverter.toInputStream(file, "UTF-8");
+        // do read with default charset!
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
         BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
         try {   
             String line = reader.readLine();
@@ -47,10 +69,11 @@ public class IOConverterCharsetTest extends ContextTestSupport {
     }
 
     public void testToInputStreamFileWithCharsetLatin1() throws Exception {
+    	switchToDefaultCharset("UTF-8");
         File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
         InputStream in = IOConverter.toInputStream(file, "ISO-8859-1");
-        // need to specify the encoding of the input stream bytes
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        // do read with default charset!
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
         BufferedReader naiveReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "ISO-8859-1"));
         try {
             String line = reader.readLine();
@@ -64,6 +87,7 @@ public class IOConverterCharsetTest extends ContextTestSupport {
     }
 
     public void testToInputStreamFileDirectByteDumpWithCharsetLatin1() throws Exception {
+    	switchToDefaultCharset("UTF-8");
         File file = new File("src/test/resources/org/apache/camel/converter/german.iso-8859-1.txt");
         InputStream in = IOConverter.toInputStream(file, "ISO-8859-1");
         InputStream naiveIn = new FileInputStream(file);
@@ -109,4 +133,13 @@ public class IOConverterCharsetTest extends ContextTestSupport {
         }
     }
 
+
+	private void switchToDefaultCharset(String charset) {
+		try {
+			Field defaultCharset = Charset.class.getDeclaredField("defaultCharset");
+			defaultCharset.setAccessible(true);
+			defaultCharset.set(null, Charset.forName(charset));
+		} catch (Exception e) {
+		}
+	}
 }
