601ddda449ae9a350597ac73f88f470a6f6549be
Claus Ibsen <davsclaus@apache.org>
2015-03-01 11:51:49 +0100

    CAMEL-8312: XML External Entity (XXE) injection in XPath. Thanks to Stephan Siano for the patch.
    Conflicts:
    	camel-core/src/main/java/org/apache/camel/builder/xml/XPathBuilder.java

--- camel-core/src/main/java/org/apache/camel/builder/xml/XPathBuilder.java
+++ camel-core/src/main/java/org/apache/camel/builder/xml/XPathBuilder.java
@@ -52,7 +52,6 @@ import org.apache.camel.NoTypeConversionAvailableException;
 import org.apache.camel.Predicate;
 import org.apache.camel.RuntimeExpressionException;
 import org.apache.camel.WrappedFile;
-import org.apache.camel.component.bean.BeanInvocation;
 import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.spi.Language;
 import org.apache.camel.spi.NamespaceAware;
@@ -1114,25 +1113,6 @@ public class XPathBuilder extends ServiceSupport implements Expression, Predicat
             } catch (Exception e) {
                 // we want to store the caused exception, if we could not convert
                 cause = e;
-            }
-        }
-
-        // okay we can try to remedy the failed conversion by some special types
-        if (answer == null) {
-            // let's try coercing some common types into something JAXP work with the best for special types
-            if (body instanceof WrappedFile) {
-                // special for files so we can work with them out of the box
-                InputStream is = exchange.getContext().getTypeConverter().convertTo(InputStream.class, exchange, body);
-                answer = new InputSource(is);
-            } else if (body instanceof BeanInvocation) {
-                // if its a null bean invocation then handle that specially
-                BeanInvocation bi = exchange.getContext().getTypeConverter().convertTo(BeanInvocation.class, exchange, body);
-                if (bi.getArgs() != null && bi.getArgs().length == 1 && bi.getArgs()[0] == null) {
-                    // its a null argument from the bean invocation so use null as answer
-                    answer = null;
-                }
-            } else if (body instanceof String) {
-                answer = new InputSource(new StringReader((String) body));
             }
         }
 
