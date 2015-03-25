e2183378b12c13707600816339442737778b364e
Claus Ibsen <davsclaus@apache.org>
2015-03-25 10:24:08 +0100

    CAMEL-5452: DoCatch now also emits the failure handled event

--- camel-core/src/main/java/org/apache/camel/processor/CatchProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/CatchProcessor.java
@@ -24,6 +24,7 @@ import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
 import org.apache.camel.Traceable;
 import org.apache.camel.spi.IdAware;
+import org.apache.camel.util.EventHelper;
 import org.apache.camel.util.ExchangeHelper;
 import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
@@ -100,7 +101,11 @@ public class CatchProcessor extends DelegateAsyncProcessor implements Traceable,
 
         boolean sync = processor.process(exchange, new AsyncCallback() {
             public void done(boolean doneSync) {
-                if (!handled) {
+                if (handled) {
+                    ExchangeHelper.setFailureHandled(exchange);
+                    // emit event that the failure was handled
+                    EventHelper.notifyExchangeFailureHandled(exchange.getContext(), exchange, processor, false, null);
+                } else {
                     if (exchange.getException() == null) {
                         exchange.setException(exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class));
                     }
