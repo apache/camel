b18adddfdbf08a22f5a3b18da4447ed4998e0038
Claus Ibsen <davsclaus@apache.org>
2015-03-27 17:56:50 +0100

    CAMEL-8562: Removing a route - Should check if the route input endpoint is static and used by other routes

--- camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
+++ camel-core/src/main/java/org/apache/camel/impl/DefaultCamelContext.java
@@ -1002,7 +1002,8 @@ public class DefaultCamelContext extends ServiceSupport implements ModelCamelCon
                     }
                 }
                 for (Endpoint endpoint : toRemove) {
-                    log.debug("Removing: {} which was only in use by route: {}", endpoint, routeId);
+                    log.debug("Stopping and removing: {} which was only in use by route: {}", endpoint, routeId);
+                    ServiceHelper.stopAndShutdownService(endpoint);
                     removeEndpoint(endpoint);
                 }
                 return true;
