b3a915c026734a8635d1d21308c70938ca7c8ea5
ancosen <ancosen@gmail.com>
2015-03-29 14:24:57 +0200

    Fix Camel-8540 S3Consumer uses maxMessagesPerPoll incorrectly

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Consumer.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Consumer.java
@@ -75,7 +75,9 @@ public class S3Consumer extends ScheduledBatchPollingConsumer {
             ListObjectsRequest listObjectsRequest = new ListObjectsRequest();
             listObjectsRequest.setBucketName(bucketName);
             listObjectsRequest.setPrefix(getConfiguration().getPrefix());
-            listObjectsRequest.setMaxKeys(maxMessagesPerPoll);
+            if (maxMessagesPerPoll > 0) {
+                listObjectsRequest.setMaxKeys(maxMessagesPerPoll);
+            }
             if (marker != null && !getConfiguration().isDeleteAfterRead()) {
                 listObjectsRequest.setMarker(marker);
             }
