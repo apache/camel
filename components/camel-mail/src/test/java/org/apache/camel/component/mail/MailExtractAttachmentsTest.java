4b1e749b9ec58c473efe56f88eaca8560d5efe84
Simon van der Sluis <simon.vandersluis@edmi.co.nz>
2015-03-30 17:05:58 +1300

    Correct copyright in MailExtractAttachmentsTest

--- components/camel-mail/src/test/java/org/apache/camel/component/mail/MailExtractAttachmentsTest.java
+++ components/camel-mail/src/test/java/org/apache/camel/component/mail/MailExtractAttachmentsTest.java
@@ -1,12 +1,18 @@
-/*
- * Copyright (c) 2015 EDMI NZ
- * All rights reserved.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * This software is the confidential and proprietary information of EDMI. 
- * ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with EDMI.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.camel.component.mail;
 
@@ -25,7 +31,7 @@ import org.junit.Test;
 import org.jvnet.mock_javamail.Mailbox;
 
 /**
- * Tests
+ * Tests for extracting email attachments.
  *
  * @author simonvandersluis
  */
