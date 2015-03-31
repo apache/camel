97f4a7cfab29056c5054fc46d6e7fca288e803c1
Pierre-Alban DEWITTE <padewitte@gmail.com>
2015-03-31 21:25:24 +0200

    CAMEL-7378 Adding outputType to allow user to choose between a List or a DBCursor while performing a findAll operation.

--- components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/MongoDbProducer.java
+++ components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/MongoDbProducer.java
@@ -331,9 +331,13 @@ public class MongoDbProducer extends DefaultProducer {
             }
 
             Message resultMessage = prepareResponseMessage(exchange, MongoDbOperation.findAll);
-            resultMessage.setBody(ret.toArray());
-            resultMessage.setHeader(MongoDbConstants.RESULT_TOTAL_SIZE, ret.count());
-            resultMessage.setHeader(MongoDbConstants.RESULT_PAGE_SIZE, ret.size());
+            if(MongoDbOutputType.DBCursor.equals(endpoint.getOutputType())){
+                resultMessage.setBody(ret.iterator());
+            }else{
+                resultMessage.setBody(ret.toArray());
+                resultMessage.setHeader(MongoDbConstants.RESULT_TOTAL_SIZE, ret.count());
+                resultMessage.setHeader(MongoDbConstants.RESULT_PAGE_SIZE, ret.size());
+            }
         } finally {
             // make sure the cursor is closed
             if (ret != null) {
