5384bc26859c1af73610ce3d74a3f014620a19b7
Jyrki Ruuskanen <yuruki@kotikone.fi>
2015-04-01 18:34:47 +0300

    Added CamelOperatorTest

---
+++ components/camel-rx/src/test/java/org/apache/camel/rx/CamelOperatorTest.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.rx;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.Message;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.Observable;
+
+/**
+ */
+public class CamelOperatorTest extends RxTestSupport {
+    private static final Logger LOG = LoggerFactory.getLogger(CamelOperatorTest.class);
+
+    @Test
+    public void testCamelOperator() throws Exception {
+        final MockEndpoint mockEndpoint1 = camelContext.getEndpoint("mock:results1", MockEndpoint.class);
+        final MockEndpoint mockEndpoint2 = camelContext.getEndpoint("mock:results2", MockEndpoint.class);
+        final MockEndpoint mockEndpoint3 = camelContext.getEndpoint("mock:results3", MockEndpoint.class);
+        mockEndpoint1.expectedMessageCount(2);
+        mockEndpoint2.expectedMessageCount(1);
+        mockEndpoint3.expectedMessageCount(1);
+
+        Observable<Message> result = reactiveCamel.toObservable("direct:start")
+            .lift(new CamelOperator(camelContext, "mock:results1"))
+            .lift(new CamelOperator(camelContext, "log:foo"))
+            .debounce(1, TimeUnit.SECONDS)
+            .lift(new CamelOperator(mockEndpoint2));
+        reactiveCamel.sendTo(result, "mock:results3");
+
+        // Send two test messages
+        producerTemplate.sendBody("direct:start", "<test/>");
+        producerTemplate.sendBody("direct:start", "<test/>");
+
+        mockEndpoint1.assertIsSatisfied();
+        mockEndpoint2.assertIsSatisfied();
+        mockEndpoint3.assertIsSatisfied();
+    }
+}
