33ebe714a9b304632eaeed2bdd0859d583b41563
Jyrki Ruuskanen <yuruki@kotikone.fi>
2015-04-05 12:59:12 +0300

    CAMEL-7833 InOnly and InOut routes as Observable<Exchange> sequences

--- components/camel-rx/src/test/java/org/apache/camel/rx/CamelOperatorTest.java
+++ components/camel-rx/src/test/java/org/apache/camel/rx/CamelOperatorTest.java
@@ -18,14 +18,17 @@ package org.apache.camel.rx;
 
 import java.util.concurrent.TimeUnit;
 
-import org.apache.camel.Message;
+import org.apache.camel.Exchange;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import rx.Observable;
 import rx.Subscription;
+import rx.functions.Func1;
 import rx.observables.ConnectableObservable;
+
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 /**
  */
@@ -37,30 +40,64 @@ public class CamelOperatorTest extends RxTestSupport {
         final MockEndpoint mockEndpoint1 = camelContext.getEndpoint("mock:results1", MockEndpoint.class);
         final MockEndpoint mockEndpoint2 = camelContext.getEndpoint("mock:results2", MockEndpoint.class);
         final MockEndpoint mockEndpoint3 = camelContext.getEndpoint("mock:results3", MockEndpoint.class);
+        final MockEndpoint mockEndpoint4 = camelContext.getEndpoint("mock:results4", MockEndpoint.class);
         mockEndpoint1.expectedMessageCount(2);
         mockEndpoint2.expectedMessageCount(1);
         mockEndpoint3.expectedMessageCount(1);
+        mockEndpoint4.expectedMessageCount(2);
 
-        ConnectableObservable<Message> route = reactiveCamel.toObservable("direct:start")
+        // Define an InOnly route
+        ConnectableObservable<Exchange> inOnly = reactiveCamel.from("direct:start")
             .lift(new CamelOperator(mockEndpoint1))
-            .lift(new CamelOperator(camelContext, "log:foo"))
+            .lift(new CamelOperator(camelContext, "log:inOnly"))
             .debounce(1, TimeUnit.SECONDS)
             .lift(reactiveCamel.to(mockEndpoint2))
             .lift(reactiveCamel.to("mock:results3"))
             .publish();
 
         // Start the route
-        Subscription routeSubscription = route.connect();
+        Subscription inSubscription = inOnly.connect();
 
         // Send two test messages
-        producerTemplate.sendBody("direct:start", "<test/>");
-        producerTemplate.sendBody("direct:start", "<test/>");
+        producerTemplate.sendBody("direct:start", "<test1/>");
+        producerTemplate.sendBody("direct:start", "<test2/>");
+
+        // Define an InOut route
+        ConnectableObservable<Exchange> inOut = reactiveCamel.from("restlet:http://localhost:9080/test?restletMethod=POST")
+            .map(new Func1<Exchange, Exchange>() { // Convert body to String
+                @Override
+                public Exchange call(Exchange exchange) {
+                    exchange.getIn().setBody(exchange.getIn().getBody(String.class));
+                    return exchange;
+                }
+            })
+            .lift(reactiveCamel.to("log:inOut"))
+            .map(new Func1<Exchange, Exchange>() { // Change body for response
+                @Override
+                public Exchange call(Exchange exchange) {
+                    exchange.getIn().setBody(exchange.getIn().getBody(String.class) + " back");
+                    return exchange;
+                }
+            })
+            .lift(reactiveCamel.to(mockEndpoint4))
+            .publish();
+
+        // Start the route
+        Subscription inoutSubscription = inOut.connect();
+
+        // Send two messages and check the responses
+        given().body("hello").when().post("http://localhost:9080/test").then().assertThat().body(containsString("hello back"));
+        given().body("holla").when().post("http://localhost:9080/test").then().assertThat().body(containsString("holla back"));
 
         mockEndpoint1.assertIsSatisfied();
         mockEndpoint2.assertIsSatisfied();
         mockEndpoint3.assertIsSatisfied();
+        mockEndpoint4.assertIsSatisfied();
 
         // Stop the route
-        routeSubscription.unsubscribe();
+        inSubscription.unsubscribe();
+
+        // Stop the route
+        inoutSubscription.unsubscribe();
     }
 }
