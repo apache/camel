ae6599c6ec02c7973209010991a23fde5db8217f
Claus Ibsen <davsclaus@apache.org>
2015-04-05 10:16:31 +0200

    CAMEL-4074: Fixed CS

--- camel-core/src/test/java/org/apache/camel/impl/converter/MyConverters.java
+++ camel-core/src/test/java/org/apache/camel/impl/converter/MyConverters.java
@@ -22,7 +22,7 @@ import org.apache.camel.TypeConverters;
 public class MyConverters implements TypeConverters {
 
     @Converter
-    public static Country toCountry(String iso) {
+    public Country toCountry(String iso) {
         Country answer = new Country();
         answer.setIso("en");
         answer.setName("England");
@@ -30,7 +30,7 @@ public class MyConverters implements TypeConverters {
     }
 
     @Converter
-    public static String toIso(Country country) {
+    public String toIso(Country country) {
         return country.getIso();
     }
 }
