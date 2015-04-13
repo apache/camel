597883fa57fbd1b4dcc282e9493b5ebfade4b5a7
Claus Ibsen <davsclaus@apache.org>
2015-04-13 10:40:48 +0200

    CAMEL-8624: Bean component - Potential NPE in BeanInfo

--- camel-core/src/main/java/org/apache/camel/component/bean/BeanInfo.java
+++ camel-core/src/main/java/org/apache/camel/component/bean/BeanInfo.java
@@ -759,20 +759,22 @@ public class BeanInfo {
             MethodInfo matched = null;
             int matchCounter = 0;
             for (MethodInfo methodInfo : operationList) {
-                if (methodInfo.getBodyParameterType().isInstance(body)) {
-                    return methodInfo;
-                }
-
-                // we should only try to convert, as we are looking for best match
-                Object value = exchange.getContext().getTypeConverter().tryConvertTo(methodInfo.getBodyParameterType(), exchange, body);
-                if (value != null) {
-                    if (LOG.isTraceEnabled()) {
-                        LOG.trace("Converted body from: {} to: {}",
-                                body.getClass().getCanonicalName(), methodInfo.getBodyParameterType().getCanonicalName());
+                if (methodInfo.getBodyParameterType() != null) {
+                    if (methodInfo.getBodyParameterType().isInstance(body)) {
+                        return methodInfo;
                     }
-                    matchCounter++;
-                    newBody = value;
-                    matched = methodInfo;
+
+                    // we should only try to convert, as we are looking for best match
+                    Object value = exchange.getContext().getTypeConverter().tryConvertTo(methodInfo.getBodyParameterType(), exchange, body);
+                    if (value != null) {
+                        if (LOG.isTraceEnabled()) {
+                            LOG.trace("Converted body from: {} to: {}",
+                                    body.getClass().getCanonicalName(), methodInfo.getBodyParameterType().getCanonicalName());
+                        }
+                        matchCounter++;
+                        newBody = value;
+                        matched = methodInfo;
+                    }
                 }
             }
             if (matchCounter > 1) {
