ef1df28fec1c9c1322c52b3040b7a6411410c535
Willem Jiang <willem.jiang@gmail.com>
2015-05-08 19:29:59 +0800

    Fixed the CS error of camel-ftp

--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpBadLoginInProducerConnectionLeakTest.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpBadLoginInProducerConnectionLeakTest.java
@@ -36,8 +36,8 @@ public class FtpBadLoginInProducerConnectionLeakTest extends FtpServerTestSuppor
     private Map<Integer, boolean[]> socketAudits = new HashMap<>();
 
     private String getFtpUrl() {
-        return "ftp://dummy@localhost:" + getPort() + "/badlogin?password=cantremeber&maximumReconnectAttempts=3" +
-                "&throwExceptionOnConnectFailed=false&ftpClient.socketFactory=#sf";
+        return "ftp://dummy@localhost:" + getPort() + "/badlogin?password=cantremeber&maximumReconnectAttempts=3"
+            + "&throwExceptionOnConnectFailed=false&ftpClient.socketFactory=#sf";
     }
 
     @Override
@@ -51,7 +51,7 @@ public class FtpBadLoginInProducerConnectionLeakTest extends FtpServerTestSuppor
 
     @Test
     public void testConnectionLeak() throws Exception {
-        for (String filename : new String[] { "claus.txt", "grzegorz.txt" }) {
+        for (String filename : new String[] {"claus.txt", "grzegorz.txt"}) {
             try {
                 sendFile(getFtpUrl(), "Hello World", filename);
             } catch (Exception ignored) {
@@ -93,7 +93,7 @@ public class FtpBadLoginInProducerConnectionLeakTest extends FtpServerTestSuppor
         @Override
         public Socket createSocket() throws IOException {
             AuditingSocket socket = new AuditingSocket();
-            socketAudits.put(System.identityHashCode(socket), new boolean[] { false, false });
+            socketAudits.put(System.identityHashCode(socket), new boolean[] {false, false});
             return socket;
         }
 
@@ -111,13 +111,15 @@ public class FtpBadLoginInProducerConnectionLeakTest extends FtpServerTestSuppor
         @Override
         public void connect(SocketAddress endpoint, int timeout) throws IOException {
             super.connect(endpoint, timeout);
-            socketAudits.get(System.identityHashCode(this))[0] = true;
+            boolean[] value = socketAudits.get(System.identityHashCode(this));
+            value[0] = true;
         }
 
         @Override
         public synchronized void close() throws IOException {
             super.close();
-            socketAudits.get(System.identityHashCode(this))[1] = true;
+            boolean[] value = socketAudits.get(System.identityHashCode(this));
+            value[1] = true;
         }
     }
 
