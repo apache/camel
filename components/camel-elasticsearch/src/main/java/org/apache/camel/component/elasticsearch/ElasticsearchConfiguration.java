746e4263438f9094add5c4328960074dd773ed4f
Andrea Cosentino <ancosen@gmail.com>
2015-05-12 23:14:11 +0200

    Elasticsearch component docs

--- components/camel-elasticsearch/src/main/java/org/apache/camel/component/elasticsearch/ElasticsearchConfiguration.java
+++ components/camel-elasticsearch/src/main/java/org/apache/camel/component/elasticsearch/ElasticsearchConfiguration.java
@@ -61,7 +61,118 @@ public class ElasticsearchConfiguration {
     private String transportAddresses;
     @UriParam(defaultValue = "9300")
     private int port = ElasticsearchConstants.DEFAULT_PORT;
+
+    /**
+     * Name of cluster or use local for local mode
+     */
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+
+    /**
+     * What operation to perform
+     */
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    /**
+     * The name of the index to act against
+     */
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public void setIndexName(String indexName) {
+        this.indexName = indexName;
+    }
+
+    /**
+     * The type of the index to act against
+     */
+    public String getIndexType() {
+        return indexType;
+    }
+
+    public void setIndexType(String indexType) {
+        this.indexType = indexType;
+    }
+
+    /**
+     * The write consistency level to use with INDEX and BULK operations (can be any of ONE, QUORUM, ALL or DEFAULT)
+     */
+    public WriteConsistencyLevel getConsistencyLevel() {
+        return consistencyLevel;
+    }
+
+    public void setConsistencyLevel(WriteConsistencyLevel consistencyLevel) {
+        this.consistencyLevel = consistencyLevel;
+    }
+
+    /**
+     * The replication type to use with INDEX and BULK operations (can be any of SYNC, ASYNC or DEFAULT)
+     */
+    public ReplicationType getReplicationType() {
+        return replicationType;
+    }
+
+    public void setReplicationType(ReplicationType replicationType) {
+        this.replicationType = replicationType;
+    }
     
+    /**
+     * Is the node going to be allowed to allocate data (shards) to it or not. This setting map to the <tt>node.data</tt> setting.
+     */
+    public Boolean getData() {
+        return data;
+    }
+
+    public void setData(Boolean data) {
+        this.data = data;
+    }
+
+    /**
+     * The TransportClient remote host ip to use
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    /**
+     * Comma separated list with ip:port formatted remote transport addresses to use.
+     * The ip and port options must be left blank for transportAddresses to be considered instead.
+     */
+    public String getTransportAddresses() {
+        return transportAddresses;
+    }
+
+    public void setTransportAddresses(String transportAddresses) {
+        this.transportAddresses = transportAddresses;
+    }
+
+    /**
+     * The TransportClient remote port to use (defaults to 9300)
+     */
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+       
     public boolean isLocal() {
         return local;
     }
@@ -76,85 +187,5 @@ public class ElasticsearchConfiguration {
 
     public void setTransportAddressesList(List<InetSocketTransportAddress> transportAddressesList) {
         this.transportAddressesList = transportAddressesList;
-    }
-
-    public String getClusterName() {
-        return clusterName;
-    }
-
-    public void setClusterName(String clusterName) {
-        this.clusterName = clusterName;
-    }
-
-    public String getOperation() {
-        return operation;
-    }
-
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
-
-    public String getIndexName() {
-        return indexName;
-    }
-
-    public void setIndexName(String indexName) {
-        this.indexName = indexName;
-    }
-
-    public String getIndexType() {
-        return indexType;
-    }
-
-    public void setIndexType(String indexType) {
-        this.indexType = indexType;
-    }
-
-    public WriteConsistencyLevel getConsistencyLevel() {
-        return consistencyLevel;
-    }
-
-    public void setConsistencyLevel(WriteConsistencyLevel consistencyLevel) {
-        this.consistencyLevel = consistencyLevel;
-    }
-
-    public ReplicationType getReplicationType() {
-        return replicationType;
-    }
-
-    public void setReplicationType(ReplicationType replicationType) {
-        this.replicationType = replicationType;
-    }
-
-    public Boolean getData() {
-        return data;
-    }
-
-    public void setData(Boolean data) {
-        this.data = data;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public String getTransportAddresses() {
-        return transportAddresses;
-    }
-
-    public void setTransportAddresses(String transportAddresses) {
-        this.transportAddresses = transportAddresses;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
     }
 }
