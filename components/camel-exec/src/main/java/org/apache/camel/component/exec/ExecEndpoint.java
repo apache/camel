a1de79893053ca27a53048e6110bbfb7048bad78
Willem Jiang <willem.jiang@gmail.com>
2015-05-18 14:19:35 +0800

    Fixed the eclipse warnings of camel-exec

--- components/camel-exec/src/main/java/org/apache/camel/component/exec/ExecEndpoint.java
+++ components/camel-exec/src/main/java/org/apache/camel/component/exec/ExecEndpoint.java
@@ -20,7 +20,6 @@ import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.component.exec.impl.DefaultExecBinding;
-import org.apache.camel.component.exec.impl.ExecParseUtils;
 import org.apache.camel.impl.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
