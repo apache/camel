35c14d2fc1ed19ed53b3819bd8d6b1a1f131bda7
Victor Noël <victor.noel@linagora.com>
2015-05-18 12:43:04 +0200

    Maven plugins: m2e compatibility
     - Uses BuildContext to support m2e and improve user experience in Eclipse
      - Only trigger execution when input has changed
      - Notify eclipse of change of output
     - Only support the following goals in camel-package-maven-plugin (the others are not really meant to be used by non-Camel developers)
      - generate-components-list
      - generate-dataformats-list
      - generate-languages-list
      - prepare-components

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareComponentMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareComponentMojo.java
@@ -16,6 +16,10 @@
  */
 package org.apache.camel.maven.packaging;
 
+import static org.apache.camel.maven.packaging.PackageComponentMojo.prepareComponent;
+import static org.apache.camel.maven.packaging.PackageDataFormatMojo.prepareDataFormat;
+import static org.apache.camel.maven.packaging.PackageLanguageMojo.prepareLanguage;
+
 import java.io.File;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -23,10 +27,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
-
-import static org.apache.camel.maven.packaging.PackageComponentMojo.prepareComponent;
-import static org.apache.camel.maven.packaging.PackageDataFormatMojo.prepareDataFormat;
-import static org.apache.camel.maven.packaging.PackageLanguageMojo.prepareLanguage;
+import org.sonatype.plexus.build.incremental.BuildContext;
 
 /**
  * Prepares a Camel component analyzing if the maven module contains Camel
@@ -87,6 +88,15 @@ public class PrepareComponentMojo extends AbstractMojo {
     private MavenProjectHelper projectHelper;
 
     /**
+     * build context to check changed files and mark them for refresh
+     * (used for m2e compatibility)
+     * 
+     * @component
+     * @readonly
+     */
+    private BuildContext buildContext;
+    
+    /**
      * Execute goal.
      *
      * @throws org.apache.maven.plugin.MojoExecutionException execution of the main class or one of the
@@ -94,9 +104,9 @@ public class PrepareComponentMojo extends AbstractMojo {
      * @throws org.apache.maven.plugin.MojoFailureException   something bad happened...
      */
     public void execute() throws MojoExecutionException, MojoFailureException {
-        prepareComponent(getLog(), project, projectHelper, componentOutDir);
-        prepareDataFormat(getLog(), project, projectHelper, dataFormatOutDir, schemaOutDir);
-        prepareLanguage(getLog(), project, projectHelper, languageOutDir, schemaOutDir);
+        prepareComponent(getLog(), project, projectHelper, componentOutDir, buildContext);
+        prepareDataFormat(getLog(), project, projectHelper, dataFormatOutDir, schemaOutDir, buildContext);
+        prepareLanguage(getLog(), project, projectHelper, languageOutDir, schemaOutDir, buildContext);
     }
 
 }
