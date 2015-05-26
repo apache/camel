3c33808a022efa907da9ed7cc46674442aae40d8
Candle <candle@candle.me.uk>
2015-05-26 13:31:37 +0100

    Fix the logging event to use placeholders correctly.

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/sqs/SqsProducer.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/sqs/SqsProducer.java
@@ -125,7 +125,7 @@ public class SqsProducer extends DefaultProducer {
                     result.put(entry.getKey(), mav);
                 } else {
                     // cannot translate the message header to message attribute value
-                    LOG.warn("Cannot put the message header key={0}, value={1} into Sqs MessageAttribute", entry.getKey(), entry.getValue());
+                    LOG.warn("Cannot put the message header key={}, value={} into Sqs MessageAttribute", entry.getKey(), entry.getValue());
                 }
             }
         }
