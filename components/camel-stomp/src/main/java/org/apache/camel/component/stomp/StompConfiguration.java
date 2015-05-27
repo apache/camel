373e1e130d6a931fcf96447e12a30dda841a3d40
David Vilaverde <dvilaverde@gmail.com>
2015-05-27 19:15:55 -0400

    added stomp support for virtual host in Apache Apollo by adding a host option

--- components/camel-stomp/src/main/java/org/apache/camel/component/stomp/StompConfiguration.java
+++ components/camel-stomp/src/main/java/org/apache/camel/component/stomp/StompConfiguration.java
@@ -30,6 +30,8 @@ public class StompConfiguration implements Cloneable {
     private String login;
     @UriParam
     private String passcode;
+    @UriParam
+    private String host;
 
     /**
      * Returns a copy of this configuration
@@ -45,6 +47,10 @@ public class StompConfiguration implements Cloneable {
 
     public String getBrokerURL() {
         return brokerURL;
+    }
+    
+    public String getHost() {
+        return host;
     }
 
     /**
