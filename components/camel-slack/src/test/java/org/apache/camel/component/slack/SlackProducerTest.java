dc8e761e475b3b1d7e7c9e21ef1539b4b9b0eca5
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 23:46:49 -0700

    Refactored test to use blueprint

--- components/camel-slack/src/test/java/org/apache/camel/component/slack/SlackProducerTest.java
+++ components/camel-slack/src/test/java/org/apache/camel/component/slack/SlackProducerTest.java
@@ -1,7 +1,26 @@
 package org.apache.camel.component.slack;
 
-/**
- * Created by mikek on 5/30/15.
- */
-public class SlackProducerTest {
+import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.junit.Test;
+
+public class SlackProducerTest extends CamelBlueprintTestSupport {
+
+    @Override
+    protected String getBlueprintDescriptor() {
+        return "OSGI-INF/blueprint/blueprint.xml";
+    }
+
+    @Test
+    public void testSlackMessage() throws Exception {
+        getMockEndpoint(("mock:errors")).expectedMessageCount(0);
+        template.sendBody("direct:test", "Hello from Camel!");
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    public void testSlackError() throws Exception {
+        getMockEndpoint(("mock:errors")).expectedMessageCount(1);
+        template.sendBody("direct:error", "Error from Camel!");
+        assertMockEndpointsSatisfied();
+    }
 }
