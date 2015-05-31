b1f0a09081401fec75a167bfbbec66691729a79f
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 23:28:50 -0700

    Added dependencies on httpclient and json-simple

--- components/camel-slack/src/main/java/org/apache/camel/component/slack/SlackProducer.java
+++ components/camel-slack/src/main/java/org/apache/camel/component/slack/SlackProducer.java
@@ -1,4 +1,59 @@
 package org.apache.camel.component.slack;
 
-public class SlackProducer {
+import org.apache.camel.CamelException;
+import org.apache.camel.Exchange;
+import org.apache.camel.component.slack.helper.SlackMessage;
+import org.apache.camel.impl.DefaultProducer;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlackProducer extends DefaultProducer {
+
+    private static final transient Logger LOG = LoggerFactory.getLogger(SlackProducer.class);
+
+    private SlackEndpoint slackEndpoint;
+
+    /**
+     * Constuctor
+     *
+     * @param endpoint a SlackEndpoint
+     */
+    public SlackProducer(SlackEndpoint endpoint) {
+        super(endpoint);
+        this.slackEndpoint = endpoint;
+    }
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+        // Create an HttpClient and Post object
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost(slackEndpoint.getWebhookUrl());
+
+        // Build Helper object
+        SlackMessage slackMessage = new SlackMessage();
+        slackMessage.setText(exchange.getIn().getBody(String.class));
+        slackMessage.setChannel(slackEndpoint.getChannel());
+        slackMessage.setUsername(slackEndpoint.getUsername());
+        slackMessage.setIconUrl(slackEndpoint.getIconUrl());
+        slackMessage.setIconEmoji(slackEndpoint.getIconEmoji());
+
+        // Set the post body
+        StringEntity body = new StringEntity(slackMessage.toString());
+
+        // Do the post
+        httpPost.setEntity(body);
+
+        HttpResponse response = client.execute(httpPost);
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            LOG.error("Error POSTing to Slack API: " + response.toString());
+            throw new CamelException("Error POSTing to Slack API: " + response.toString());
+        }
+    }
 }
--- components/camel-slack/src/main/java/org/apache/camel/component/slack/helper/SlackMessage.java
+++ components/camel-slack/src/main/java/org/apache/camel/component/slack/helper/SlackMessage.java
@@ -1,7 +1,80 @@
 package org.apache.camel.component.slack.helper;
 
-/**
- * Created by mikek on 5/30/15.
- */
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SlackMessage {
+
+    private String text;
+    private String channel;
+    private String username;
+    private String iconUrl;
+    private String iconEmoji;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public String getIconEmoji() {
+        return iconEmoji;
+    }
+
+    public void setIconEmoji(String iconEmoji) {
+        this.iconEmoji = iconEmoji;
+    }
+
+    /**
+     * Returns a JSON string to be posted to the Slack API
+     *
+     * @return JSON string
+     */
+    public String toString() {
+
+        Map<String, String> jsonMap = new HashMap<String, String>();
+
+        // Put the values in a map
+        jsonMap.put("text", this.text);
+        jsonMap.put("channel", this.channel);
+        jsonMap.put("username", this.username);
+        jsonMap.put("icon_url", this.iconUrl);
+        jsonMap.put("icon_emoji", this.iconEmoji);
+
+        // Generate a JSONObject
+        JSONObject jsonObject = new JSONObject(jsonMap);
+
+        // Return the string based on the JSON Object
+        return JSONObject.toJSONString(jsonMap);
+
+    }
 }
+
