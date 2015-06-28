a5dd9a053c13c547fbf45b79bf70de946379eb9b
Claus Ibsen <davsclaus@apache.org>
2015-06-28 20:52:03 +0200

    CAMEL-8175: camel-swagger - Support for multiple CamelContexts.

--- components/camel-swagger/src/main/scala/org/apache/camel/component/swagger/DefaultCamelSwaggerServlet.scala
+++ components/camel-swagger/src/main/scala/org/apache/camel/component/swagger/DefaultCamelSwaggerServlet.scala
@@ -69,4 +69,27 @@ class DefaultCamelSwaggerServlet extends RestSwaggerApiDeclarationServlet {
     null
   }
 
+  override def findCamelContexts(): List[String] = {
+    var answer = mutable.MutableList[String]()
+
+    val server: MBeanServer = ManagementFactory.getPlatformMBeanServer
+    val names = server.queryNames(new ObjectName("*:type=context,*"), null)
+    for (name <- names.asScala) {
+      val on = name.asInstanceOf[ObjectName]
+      var id: String = on.getKeyProperty("name")
+      if (id.startsWith("\"") && id.endsWith("\"")) {
+        id = id.substring(1, id.length() - 1)
+      }
+
+      if (id != null) {
+        // filter out older Camel versions as this requires Camel 2.15 or better
+        val version = server.getAttribute(on, "CamelVersion").asInstanceOf[String]
+        if (CamelVersionHelper.isGE("2.15.0", version)) {
+          answer += id
+        }
+      }
+    }
+    return answer.toList
+  }
+
 }
