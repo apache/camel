29a746103958564aaeb6c1954d07f80f41ddc197
Claus Ibsen <davsclaus@apache.org>
2015-07-01 20:34:01 +0200

    CAMEL-8832: Added equalis ignore case operation to value builder and simple language

--- camel-core/src/main/java/org/apache/camel/builder/PredicateBuilder.java
+++ camel-core/src/main/java/org/apache/camel/builder/PredicateBuilder.java
@@ -201,7 +201,7 @@ public final class PredicateBuilder {
             }
 
             protected String getOperationText() {
-                return "~=";
+                return "=~";
             }
         };
     }
