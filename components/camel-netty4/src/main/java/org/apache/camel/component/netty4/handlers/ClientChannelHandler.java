6473653e65dc73a0bf88804211689898cc2614e2
Claus Ibsen <davsclaus@apache.org>
2015-07-17 09:42:16 +0200

    CAMEL-8946: camel-netty may hide exception. Thanks to Joe Qiang Luo for unit test

--- components/camel-netty4/src/main/java/org/apache/camel/component/netty4/handlers/ClientChannelHandler.java
+++ components/camel-netty4/src/main/java/org/apache/camel/component/netty4/handlers/ClientChannelHandler.java
@@ -77,8 +77,13 @@ public class ClientChannelHandler extends SimpleChannelInboundHandler<Object> {
 
         // the state may not be set
         if (exchange != null && callback != null) {
-            // set the cause on the exchange
-            exchange.setException(cause);
+            Throwable initialCause = exchange.getException();
+            if (initialCause != null && initialCause.getCause() == null) {
+                initialCause.initCause(cause);
+            } else {
+                // set the cause on the exchange
+                exchange.setException(cause);
+            }
 
             // close channel in case an exception was thrown
             NettyHelper.close(ctx.channel());
