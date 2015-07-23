9ce76f97e6ae6facf38244261b1d0c5b58d2cac9
Daniel Kulp <dkulp@apache.org>
2015-07-23 13:13:15 -0400

    Put license header in correct spot

--- components/camel-http-common/src/main/java/org/apache/camel/http/common/HttpMethods.java
+++ components/camel-http-common/src/main/java/org/apache/camel/http/common/HttpMethods.java
@@ -1,5 +1,3 @@
-package org.apache.camel.http.common;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -7,15 +5,17 @@ package org.apache.camel.http.common;
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.camel.http.common;
+
 public enum HttpMethods {
 
     GET, POST, PUT, DELETE, HEAD, OPTIONS, TRACE, PATCH
