b671ac5d79c7db7ebdf05886541825435369534a
Claus Ibsen <davsclaus@apache.org>
2015-07-29 09:22:52 +0200

    Fixed CS

--- components/camel-velocity/src/test/java/org/apache/camel/component/velocity/VelocitySupplementalContextTest.java
+++ components/camel-velocity/src/test/java/org/apache/camel/component/velocity/VelocitySupplementalContextTest.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.velocity;
 
 import java.util.HashMap;
@@ -28,8 +44,7 @@ public class VelocitySupplementalContextTest extends CamelTestSupport {
 
         Map<String, Object> headers = new HashMap<String, Object>();
         headers.put(VelocityConstants.VELOCITY_TEMPLATE,
-                "#set( $headers.body = ${body} )\n" +
-                "#set( $headers['in.body'] = $in.body )\n" + "bar");
+                "#set( $headers.body = ${body} )\n#set( $headers['in.body'] = $in.body )\n" + "bar");
         inputEndpoint.sendBodyAndHeaders("old_body", headers);
 
         assertMockEndpointsSatisfied();
@@ -45,9 +60,9 @@ public class VelocitySupplementalContextTest extends CamelTestSupport {
             @Override
             public void configure() throws Exception {
                 from("direct:input")
-                .setHeader(VelocityConstants.VELOCITY_SUPPLEMENTAL_CONTEXT).constant(supplementalContext)
-                .to("velocity:template-in-header")
-                .to("mock:results");
+                    .setHeader(VelocityConstants.VELOCITY_SUPPLEMENTAL_CONTEXT).constant(supplementalContext)
+                    .to("velocity:template-in-header")
+                    .to("mock:results");
             }
         };
     }
