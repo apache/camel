cfb6cd629c6e38c6e93e810ec9a38e32685705b2
Christian Schneider <chris@die-schneider.net>
2015-08-05 17:52:50 +0200

    [CAMEL-9048] Special handling for camel-core

--- camel-core/src/main/java/org/apache/camel/impl/osgi/Activator.java
+++ camel-core/src/main/java/org/apache/camel/impl/osgi/Activator.java
@@ -92,6 +92,7 @@ public class Activator implements BundleActivator, BundleTrackerCustomizer {
 
     public void start(BundleContext context) throws Exception {
         LOG.info("Camel activator starting");
+        bundleId = context.getBundle().getBundleId();
         tracker = new BundleTracker(context, Bundle.ACTIVE, this);
         cachePackageCapabilities(context);
         tracker.open();
@@ -210,6 +211,10 @@ public class Activator implements BundleActivator, BundleTrackerCustomizer {
      * Check if bundle can see the given class
      */
     protected boolean canSee(Bundle bundle, Class<?> clazz) {
+        if (bundle.getBundleId() == bundleId) {
+            // Need extra handling of camel core as it does not import the api
+            return true;
+        }
         BundleCapability packageCap = packageCapabilities.get(clazz.getPackage().getName());
         BundleWiring wiring = bundle.adapt(BundleWiring.class);
         List<BundleWire> imports = wiring.getRequiredWires(PACKAGE_NAMESPACE);
