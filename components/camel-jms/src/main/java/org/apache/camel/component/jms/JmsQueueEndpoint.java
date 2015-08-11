0a6be48e46a1aac779e4c40c7257a680986a2065
Claus Ibsen <davsclaus@apache.org>
2015-08-11 14:36:06 +0200

    CAMEL-8147: Camel JMX when using custom @ManagedResource should mixin the standard mbean operations/attributes so you get both worlds.

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsQueueEndpoint.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsQueueEndpoint.java
@@ -22,13 +22,9 @@ import javax.jms.JMSException;
 import javax.jms.Queue;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
 import org.apache.camel.api.management.ManagedAttribute;
-import org.apache.camel.api.management.ManagedOperation;
 import org.apache.camel.api.management.ManagedResource;
 import org.apache.camel.spi.BrowsableEndpoint;
-import org.apache.camel.util.EndpointHelper;
-import org.apache.camel.util.MessageHelper;
 import org.springframework.jms.core.JmsOperations;
 
 /**
@@ -101,74 +97,6 @@ public class JmsQueueEndpoint extends JmsEndpoint implements BrowsableEndpoint {
         String queue = getDestinationName();
         JmsOperations template = getConfiguration().createInOnlyTemplate(this, false, queue);
         return queueBrowseStrategy.browse(template, queue, this);
-    }
-
-    @ManagedOperation(description = "Current number of Exchanges in Queue")
-    public long queueSize() {
-        return getExchanges().size();
-    }
-
-    @ManagedOperation(description = "Get Exchange from queue by index")
-    public String browseExchange(Integer index) {
-        List<Exchange> exchanges = getExchanges();
-        if (index >= exchanges.size()) {
-            return null;
-        }
-        Exchange exchange = exchanges.get(index);
-        if (exchange == null) {
-            return null;
-        }
-        // must use java type with JMX such as java.lang.String
-        return exchange.toString();
-    }
-
-    @ManagedOperation(description = "Get message body from queue by index")
-    public String browseMessageBody(Integer index) {
-        List<Exchange> exchanges = getExchanges();
-        if (index >= exchanges.size()) {
-            return null;
-        }
-        Exchange exchange = exchanges.get(index);
-        if (exchange == null) {
-            return null;
-        }
-
-        // must use java type with JMX such as java.lang.String
-        String body;
-        if (exchange.hasOut()) {
-            body = exchange.getOut().getBody(String.class);
-        } else {
-            body = exchange.getIn().getBody(String.class);
-        }
-
-        return body;
-    }
-
-    @ManagedOperation(description = "Get message as XML from queue by index")
-    public String browseMessageAsXml(Integer index, Boolean includeBody) {
-        List<Exchange> exchanges = getExchanges();
-        if (index >= exchanges.size()) {
-            return null;
-        }
-        Exchange exchange = exchanges.get(index);
-        if (exchange == null) {
-            return null;
-        }
-
-        Message msg = exchange.hasOut() ? exchange.getOut() : exchange.getIn();
-        String xml = MessageHelper.dumpAsXml(msg, includeBody);
-
-        return xml;
-    }
-
-    @ManagedOperation(description = "Gets all the messages as XML from the queue")
-    public String browseAllMessagesAsXml(Boolean includeBody) {
-        return browseRangeMessagesAsXml(0, Integer.MAX_VALUE, includeBody);
-    }
-
-    @ManagedOperation(description = "Gets the range of messages as XML from the queue")
-    public String browseRangeMessagesAsXml(Integer fromIndex, Integer toIndex, Boolean includeBody) {
-        return EndpointHelper.browseRangeMessagesAsXml(this, fromIndex, toIndex, includeBody);
     }
 
     protected QueueBrowseStrategy createQueueBrowseStrategy() {
