8165cbbf6dd8413861cd9f185f52e9f7099edb2e
Andrew Block <andy.block@gmail.com>
2015-08-21 08:22:27 -0500

    CAMEL-8961: Upgrade Docker component to docker-java 1.4.0 and reenabled unit tests

--- components/camel-docker/src/test/java/org/apache/camel/component/docker/headers/TagImageCmdHeaderTest.java
+++ components/camel-docker/src/test/java/org/apache/camel/component/docker/headers/TagImageCmdHeaderTest.java
@@ -22,7 +22,6 @@ import com.github.dockerjava.api.command.TagImageCmd;
 
 import org.apache.camel.component.docker.DockerConstants;
 import org.apache.camel.component.docker.DockerOperation;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Matchers;
 import org.mockito.Mock;
@@ -36,7 +35,6 @@ public class TagImageCmdHeaderTest extends BaseDockerHeaderTest<TagImageCmd> {
     @Mock
     private TagImageCmd mockObject;
 
-    @Ignore
     @Test
     public void tagImageHeaderTest() {
 
