6d43390c00ef057ea33869288dc045ccd1c4d7aa
Claus Ibsen <davsclaus@apache.org>
2015-08-21 13:51:11 +0200

    CAMEL-9096: rest-dsl - Reuse logic for matching best rest path to use

---
+++ camel-core/src/main/java/org/apache/camel/support/RestConsumerContextPathMatcher.java
@@ -0,0 +1,197 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.support;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+public final class RestConsumerContextPathMatcher {
+
+    public interface ConsumerPath<T> {
+
+        String getRestrictMethod();
+
+        String getConsumerPath();
+
+        T getConsumer();
+
+    }
+
+    public static ConsumerPath matchBestPath(String requestMethod, String requestPath, List<ConsumerPath> consumerPaths) {
+        ConsumerPath answer = null;
+
+        List<ConsumerPath> candidates = new ArrayList<ConsumerPath>();
+
+        // first match by http method
+        for (ConsumerPath entry : consumerPaths) {
+            if (matchRestMethod(requestMethod, entry.getRestrictMethod())) {
+                candidates.add(entry);
+            }
+        }
+
+        // then see if we got a direct match
+        Iterator<ConsumerPath> it = candidates.iterator();
+        while (it.hasNext()) {
+            ConsumerPath consumer = it.next();
+            if (matchRestPath(requestPath, consumer.getConsumerPath(), false)) {
+                answer = consumer;
+                break;
+            }
+        }
+
+        // then match by wildcard path
+        if (answer == null) {
+            it = candidates.iterator();
+            while (it.hasNext()) {
+                ConsumerPath consumer = it.next();
+                // filter non matching paths
+                if (!matchRestPath(requestPath, consumer.getConsumerPath(), true)) {
+                    it.remove();
+                }
+            }
+
+            // if there is multiple candidates with wildcards then pick anyone with the least number of wildcards
+            int bestWildcard = Integer.MAX_VALUE;
+            ConsumerPath best = null;
+            if (candidates.size() > 1) {
+                it = candidates.iterator();
+                while (it.hasNext()) {
+                    ConsumerPath entry = it.next();
+                    int wildcards = countWildcards(entry.getConsumerPath());
+                    if (wildcards > 0) {
+                        if (best == null || wildcards < bestWildcard) {
+                            best = entry;
+                            bestWildcard = wildcards;
+                        }
+                    }
+                }
+
+                if (best != null) {
+                    // pick the best among the wildcards
+                    answer = best;
+                }
+            }
+
+            // if there is one left then its our answer
+            if (answer == null && candidates.size() == 1) {
+                answer = candidates.get(0);
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * Matches the given request HTTP method with the configured HTTP method of the consumer
+     *
+     * @param method    the request HTTP method
+     * @param restrict  the consumer configured HTTP restrict method
+     * @return <tt>true</tt> if matched, <tt>false</tt> otherwise
+     */
+    private static boolean matchRestMethod(String method, String restrict) {
+        if (restrict == null) {
+            return true;
+        }
+
+        // always match OPTIONS as some REST clients uses that prior to calling the service
+        if ("OPTIONS".equals(method)) {
+            return true;
+        }
+
+        return restrict.toLowerCase(Locale.US).contains(method.toLowerCase(Locale.US));
+    }
+
+    /**
+     * Matches the given request path with the configured consumer path
+     *
+     * @param requestPath   the request path
+     * @param consumerPath  the consumer path which may use { } tokens
+     * @return <tt>true</tt> if matched, <tt>false</tt> otherwise
+     */
+    private static boolean matchRestPath(String requestPath, String consumerPath, boolean wildcard) {
+        // remove starting/ending slashes
+        if (requestPath.startsWith("/")) {
+            requestPath = requestPath.substring(1);
+        }
+        if (requestPath.endsWith("/")) {
+            requestPath = requestPath.substring(0, requestPath.length() - 1);
+        }
+        // remove starting/ending slashes
+        if (consumerPath.startsWith("/")) {
+            consumerPath = consumerPath.substring(1);
+        }
+        if (consumerPath.endsWith("/")) {
+            consumerPath = consumerPath.substring(0, consumerPath.length() - 1);
+        }
+
+        // split using single char / is optimized in the jdk
+        String[] requestPaths = requestPath.split("/");
+        String[] consumerPaths = consumerPath.split("/");
+
+        // must be same number of path's
+        if (requestPaths.length != consumerPaths.length) {
+            return false;
+        }
+
+        for (int i = 0; i < requestPaths.length; i++) {
+            String p1 = requestPaths[i];
+            String p2 = consumerPaths[i];
+
+            if (wildcard && p2.startsWith("{") && p2.endsWith("}")) {
+                // always matches
+                continue;
+            }
+
+            if (!p1.equals(p2)) {
+                return false;
+            }
+        }
+
+        // assume matching
+        return true;
+    }
+
+    /**
+     * Counts the number of wildcards in the path
+     *
+     * @param consumerPath  the consumer path which may use { } tokens
+     * @return number of wildcards, or <tt>0</tt> if no wildcards
+     */
+    private static int countWildcards(String consumerPath) {
+        int wildcards = 0;
+
+        // remove starting/ending slashes
+        if (consumerPath.startsWith("/")) {
+            consumerPath = consumerPath.substring(1);
+        }
+        if (consumerPath.endsWith("/")) {
+            consumerPath = consumerPath.substring(0, consumerPath.length() - 1);
+        }
+
+        String[] consumerPaths = consumerPath.split("/");
+        for (String p2 : consumerPaths) {
+            if (p2.startsWith("{") && p2.endsWith("}")) {
+                wildcards++;
+            }
+        }
+
+        return wildcards;
+    }
+
+}
--- components/camel-jetty-common/src/main/java/org/apache/camel/component/jetty/JettyRestServletResolveConsumerStrategy.java
+++ components/camel-jetty-common/src/main/java/org/apache/camel/component/jetty/JettyRestServletResolveConsumerStrategy.java
@@ -17,14 +17,13 @@
 package org.apache.camel.component.jetty;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.camel.http.common.HttpConsumer;
 import org.apache.camel.http.common.HttpServletResolveConsumerStrategy;
+import org.apache.camel.support.RestConsumerContextPathMatcher;
 
 /**
  * A {@link org.apache.camel.http.common.HttpServletResolveConsumerStrategy} that supports the Rest DSL.
@@ -44,66 +43,29 @@ public class JettyRestServletResolveConsumerStrategy extends HttpServletResolveC
             return null;
         }
 
-        List<HttpConsumer> candidates = new ArrayList<HttpConsumer>();
+        List<RestConsumerContextPathMatcher.ConsumerPath> paths = new ArrayList<RestConsumerContextPathMatcher.ConsumerPath>();
+        for (final Map.Entry<String, HttpConsumer> entry : consumers.entrySet()) {
+            paths.add(new RestConsumerContextPathMatcher.ConsumerPath<HttpConsumer>() {
+                @Override
+                public String getRestrictMethod() {
+                    return entry.getValue().getEndpoint().getHttpMethodRestrict();
+                }
 
-        // first match by http method
-        for (Map.Entry<String, HttpConsumer> entry : consumers.entrySet()) {
-            String restrict = entry.getValue().getEndpoint().getHttpMethodRestrict();
-            if (matchRestMethod(method, restrict)) {
-                candidates.add(entry.getValue());
-            }
+                @Override
+                public String getConsumerPath() {
+                    return entry.getValue().getPath();
+                }
+
+                @Override
+                public HttpConsumer getConsumer() {
+                    return entry.getValue();
+                }
+            });
         }
 
-        // then see if we got a direct match
-        Iterator<HttpConsumer> it = candidates.iterator();
-        while (it.hasNext()) {
-            HttpConsumer consumer = it.next();
-            String consumerPath = consumer.getPath();
-            if (matchRestPath(path, consumerPath, false)) {
-                answer = consumer;
-                break;
-            }
-        }
-
-        // then match by wildcard path
-        if (answer == null) {
-            it = candidates.iterator();
-            while (it.hasNext()) {
-                HttpConsumer consumer = it.next();
-                String consumerPath = consumer.getPath();
-                // filter non matching paths
-                if (!matchRestPath(path, consumerPath, true)) {
-                    it.remove();
-                }
-            }
-
-            // if there is multiple candidates with wildcards then pick anyone with the least number of wildcards
-            int bestWildcard = Integer.MAX_VALUE;
-            HttpConsumer best = null;
-            if (candidates.size() > 1) {
-                it = candidates.iterator();
-                while (it.hasNext()) {
-                    HttpConsumer entry = it.next();
-                    String consumerPath = entry.getPath();
-                    int wildcards = countWildcards(consumerPath);
-                    if (wildcards > 0) {
-                        if (best == null || wildcards < bestWildcard) {
-                            best = entry;
-                            bestWildcard = wildcards;
-                        }
-                    }
-                }
-
-                if (best != null) {
-                    // pick the best among the wildcards
-                    answer = best;
-                }
-            }
-
-            // if there is one left then its our answer
-            if (answer == null && candidates.size() == 1) {
-                answer = candidates.get(0);
-            }
+        RestConsumerContextPathMatcher.ConsumerPath<HttpConsumer> best = RestConsumerContextPathMatcher.matchBestPath(method, path, paths);
+        if (best != null) {
+            answer = best.getConsumer();
         }
 
         if (answer == null) {
@@ -114,101 +76,5 @@ public class JettyRestServletResolveConsumerStrategy extends HttpServletResolveC
         return answer;
     }
 
-    /**
-     * Matches the given request path with the configured consumer path
-     *
-     * @param requestPath   the request path
-     * @param consumerPath  the consumer path which may use { } tokens
-     * @return <tt>true</tt> if matched, <tt>false</tt> otherwise
-     */
-    public boolean matchRestPath(String requestPath, String consumerPath, boolean wildcard) {
-        // remove starting/ending slashes
-        if (requestPath.startsWith("/")) {
-            requestPath = requestPath.substring(1);
-        }
-        if (requestPath.endsWith("/")) {
-            requestPath = requestPath.substring(0, requestPath.length() - 1);
-        }
-        // remove starting/ending slashes
-        if (consumerPath.startsWith("/")) {
-            consumerPath = consumerPath.substring(1);
-        }
-        if (consumerPath.endsWith("/")) {
-            consumerPath = consumerPath.substring(0, consumerPath.length() - 1);
-        }
-
-        // split using single char / is optimized in the jdk
-        String[] requestPaths = requestPath.split("/");
-        String[] consumerPaths = consumerPath.split("/");
-
-        // must be same number of path's
-        if (requestPaths.length != consumerPaths.length) {
-            return false;
-        }
-
-        for (int i = 0; i < requestPaths.length; i++) {
-            String p1 = requestPaths[i];
-            String p2 = consumerPaths[i];
-
-            if (wildcard && p2.startsWith("{") && p2.endsWith("}")) {
-                // always matches
-                continue;
-            }
-
-            if (!p1.equals(p2)) {
-                return false;
-            }
-        }
-
-        // assume matching
-        return true;
-    }
-
-    /**
-     * Counts the number of wildcards in the path
-     *
-     * @param consumerPath  the consumer path which may use { } tokens
-     * @return number of wildcards, or <tt>0</tt> if no wildcards
-     */
-    public int countWildcards(String consumerPath) {
-        int wildcards = 0;
-
-        // remove starting/ending slashes
-        if (consumerPath.startsWith("/")) {
-            consumerPath = consumerPath.substring(1);
-        }
-        if (consumerPath.endsWith("/")) {
-            consumerPath = consumerPath.substring(0, consumerPath.length() - 1);
-        }
-
-        String[] consumerPaths = consumerPath.split("/");
-        for (String p2 : consumerPaths) {
-            if (p2.startsWith("{") && p2.endsWith("}")) {
-                wildcards++;
-            }
-        }
-
-        return wildcards;
-    }
-
-    /**
-     * Matches the given request HTTP method with the configured HTTP method of the consumer
-     *
-     * @param method    the request HTTP method
-     * @param restrict  the consumer configured HTTP restrict method
-     * @return <tt>true</tt> if matched, <tt>false</tt> otherwise
-     */
-    public boolean matchRestMethod(String method, String restrict) {
-        if (restrict == null) {
-            return true;
-        }
-
-        // always match OPTIONS as some REST clients uses that prior to calling the service
-        if ("OPTIONS".equals(method)) {
-            return true;
-        }
-
-        return restrict.toLowerCase(Locale.US).contains(method.toLowerCase(Locale.US));
-    }
 
 }
