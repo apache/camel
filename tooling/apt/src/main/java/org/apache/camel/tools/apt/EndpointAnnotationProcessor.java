f72aa1d4198f55354d5dd3916c61819a40862c81
Claus Ibsen <davsclaus@apache.org>
2015-09-07 16:56:25 +0200

    CAMEL-8446: Component docs - Add support for @Metadata on component fields which is the common pattern.

--- tooling/apt/src/main/java/org/apache/camel/tools/apt/EndpointAnnotationProcessor.java
+++ tooling/apt/src/main/java/org/apache/camel/tools/apt/EndpointAnnotationProcessor.java
@@ -466,12 +466,11 @@ public class EndpointAnnotationProcessor extends AbstractAnnotationProcessor {
                 String fieldName = methodName.substring(3);
                 fieldName = fieldName.substring(0, 1).toLowerCase() + fieldName.substring(1);
 
-                ExecutableElement setter = method;
-                String name = fieldName;
-                name = prefix + name;
-                TypeMirror fieldType = setter.getParameters().get(0).asType();
-                String fieldTypeName = fieldType.toString();
-                TypeElement fieldTypeElement = findTypeElement(roundEnv, fieldTypeName);
+                // we usually favor putting the @Metadata annotation on the field instead of the setter, so try to use it if its there
+                VariableElement field = findFieldElement(classElement, fieldName);
+                if (field != null && metadata == null) {
+                    metadata = field.getAnnotation(Metadata.class);
+                }
 
                 String required = metadata != null ? metadata.required() : null;
                 String label = metadata != null ? metadata.label() : null;
@@ -480,6 +479,13 @@ public class EndpointAnnotationProcessor extends AbstractAnnotationProcessor {
                 // String defaultValueNote = param.defaultValueNote();
                 String defaultValue = metadata != null ? metadata.defaultValue() : null;
                 String defaultValueNote = null;
+
+                ExecutableElement setter = method;
+                String name = fieldName;
+                name = prefix + name;
+                TypeMirror fieldType = setter.getParameters().get(0).asType();
+                String fieldTypeName = fieldType.toString();
+                TypeElement fieldTypeElement = findTypeElement(roundEnv, fieldTypeName);
 
                 String docComment = findJavaDoc(elementUtils, method, fieldName, name, classElement, false);
                 if (isNullOrEmpty(docComment)) {
