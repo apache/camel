ea53dce2d072816bdf73607cb02184dad2d1fc6f
Andrea Cosentino <ancosen@gmail.com>
2015-09-15 21:04:46 +0200

    Fixed CS

--- components/camel-facebook/src/test/java/org/apache/camel/component/facebook/FacebookComponentPageIdProducerTest.java
+++ components/camel-facebook/src/test/java/org/apache/camel/component/facebook/FacebookComponentPageIdProducerTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,10 +27,10 @@ import org.junit.Test;
 public class FacebookComponentPageIdProducerTest extends CamelFacebookTestSupport {
     public static final String APACHE_FOUNDATION_PAGE_ID = "6538157161";
 
+    long lastTimestamp = -1;
+    
     public FacebookComponentPageIdProducerTest() throws Exception {
     }
-
-    long lastTimestamp = -1;
 
     @Test
     public void testProducers() throws Exception {
