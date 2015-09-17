e1105553d16abf916b6d632f2bb831d133f76dc8
Claus Ibsen <davsclaus@apache.org>
2015-09-17 11:18:01 +0200

    CAMEL-9143: Fixed JMX leak when using ServicePoolAware producers. Thanks to Bob Browning for the test case.
    Conflicts:
    	camel-core/src/main/java/org/apache/camel/impl/ProducerCache.java

--- camel-core/src/main/java/org/apache/camel/impl/ProducerCache.java
+++ camel-core/src/main/java/org/apache/camel/impl/ProducerCache.java
@@ -55,23 +55,31 @@ public class ProducerCache extends ServiceSupport {
     private final Map<String, Producer> producers;
     private final Object source;
     private boolean eventNotifierEnabled = true;
+    private boolean stopServicePool;
 
     public ProducerCache(Object source, CamelContext camelContext) {
         this(source, camelContext, CamelContextHelper.getMaximumCachePoolSize(camelContext));
     }
 
     public ProducerCache(Object source, CamelContext camelContext, int cacheSize) {
-        this(source, camelContext, camelContext.getProducerServicePool(), createLRUCache(cacheSize));
+        this(source, camelContext, null, createLRUCache(cacheSize));
     }
 
     public ProducerCache(Object source, CamelContext camelContext, Map<String, Producer> cache) {
-        this(source, camelContext, camelContext.getProducerServicePool(), cache);
+        this(source, camelContext, null, cache);
     }
 
     public ProducerCache(Object source, CamelContext camelContext, ServicePool<Endpoint, Producer> producerServicePool, Map<String, Producer> cache) {
         this.source = source;
         this.camelContext = camelContext;
-        this.pool = producerServicePool;
+        if (producerServicePool == null) {
+            // use shared producer pool which lifecycle is managed by CamelContext
+            this.pool = camelContext.getProducerServicePool();
+            this.stopServicePool = false;
+        } else {
+            this.pool = producerServicePool;
+            this.stopServicePool = true;
+        }
         this.producers = cache;
     }
 
@@ -427,7 +435,9 @@ public class ProducerCache extends ServiceSupport {
 
     protected void doStop() throws Exception {
         // when stopping we intend to shutdown
-        ServiceHelper.stopAndShutdownService(pool);
+        if (stopServicePool) {
+            ServiceHelper.stopAndShutdownService(pool);
+        }
         try {
             ServiceHelper.stopAndShutdownServices(producers.values());
         } finally {
