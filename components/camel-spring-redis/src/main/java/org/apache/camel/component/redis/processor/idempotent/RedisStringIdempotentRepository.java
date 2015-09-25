357653a6e246fe518723043b7819644b4a427932
Claus Ibsen <davsclaus@apache.org>
2015-09-25 11:21:37 +0200

    Fixed CS. fixes #619.

--- components/camel-spring-redis/src/main/java/org/apache/camel/component/redis/processor/idempotent/RedisStringIdempotentRepository.java
+++ components/camel-spring-redis/src/main/java/org/apache/camel/component/redis/processor/idempotent/RedisStringIdempotentRepository.java
@@ -1,5 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.redis.processor.idempotent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.api.management.ManagedOperation;
+import org.apache.camel.api.management.ManagedResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.Cursor;
@@ -8,84 +30,75 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.ValueOperations;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
+@ManagedResource(description = "Spring Redis based message id repository")
 public class RedisStringIdempotentRepository extends RedisIdempotentRepository {
 
-  private final ValueOperations<String, String> valueOperations;
+    private final ValueOperations<String, String> valueOperations;
 
-  /*
-  The expiry time frame for the item in seconds
-   */
-  private Long expiry;
+    private long expiry;
 
-  public RedisStringIdempotentRepository(
-      RedisTemplate<String, String> redisTemplate,
-      String processorName) {
-    super(redisTemplate, processorName);
-    this.valueOperations = redisTemplate.opsForValue();
-  }
-
-  @Override
-  public boolean contains(String key) {
-    String value = valueOperations.get(createRedisKey(key));
-    if (value != null) {
-      return true;
-    } else {
-      return false;
+    public RedisStringIdempotentRepository(RedisTemplate<String, String> redisTemplate, String processorName) {
+        super(redisTemplate, processorName);
+        this.valueOperations = redisTemplate.opsForValue();
     }
-  }
 
-  @Override
-  public boolean add(String key) {
-    boolean added = valueOperations.setIfAbsent(createRedisKey(key), key);
-    if (expiry != null) {
-      valueOperations.getOperations().expire(createRedisKey(key), expiry, TimeUnit.SECONDS);
+    @ManagedOperation(description = "Does the store contain the given key")
+    @Override
+    public boolean contains(String key) {
+        String value = valueOperations.get(createRedisKey(key));
+        return value != null;
     }
-    return added;
-  }
 
-  @Override
-  public boolean remove(String key) {
-    valueOperations.getOperations().delete(createRedisKey(key));
-    return true;
-  }
-
-  public void clear() {
-    valueOperations.getOperations().execute(new RedisCallback<List<byte[]>>() {
-      @Override
-      public List<byte[]> doInRedis(RedisConnection connection) throws DataAccessException {
-        List<byte[]> binaryKeys = new ArrayList<>();
-        Cursor<byte[]>
-            cursor =
-            connection.scan(ScanOptions.scanOptions().match("*" + createRedisKey("*")).build());
-
-        while (cursor.hasNext()) {
-          byte[] key = cursor.next();
-          binaryKeys.add(key);
+    @ManagedOperation(description = "Adds the key to the store")
+    @Override
+    public boolean add(String key) {
+        boolean added = valueOperations.setIfAbsent(createRedisKey(key), key);
+        if (expiry > 0) {
+            valueOperations.getOperations().expire(createRedisKey(key), expiry, TimeUnit.SECONDS);
         }
-        if (binaryKeys.size() > 0) {
-          connection.del(binaryKeys.toArray(new byte[][]{}));
-        }
-        return binaryKeys;
-      }
-    });
-  }
+        return added;
+    }
 
-  public String createRedisKey(String key) {
-    return new StringBuilder(getProcessorName()).append(":").append(key).toString();
-  }
+    @ManagedOperation(description = "Remove the key from the store")
+    @Override
+    public boolean remove(String key) {
+        valueOperations.getOperations().delete(createRedisKey(key));
+        return true;
+    }
 
-  public Long getExpiry() {
-    return expiry;
-  }
+    @ManagedOperation(description = "Clear the store")
+    @Override
+    public void clear() {
+        valueOperations.getOperations().execute(new RedisCallback<List<byte[]>>() {
+            @Override
+            public List<byte[]> doInRedis(RedisConnection connection) throws DataAccessException {
+                List<byte[]> binaryKeys = new ArrayList<>();
+                Cursor<byte[]> cursor = connection.scan(ScanOptions.scanOptions().match("*" + createRedisKey("*")).build());
 
-  /**
-   * Exire all newly added items after the given number of seconds
-   */
-  public void setExpiry(Long expiry) {
-    this.expiry = expiry;
-  }
+                while (cursor.hasNext()) {
+                    byte[] key = cursor.next();
+                    binaryKeys.add(key);
+                }
+                if (binaryKeys.size() > 0) {
+                    connection.del(binaryKeys.toArray(new byte[][]{}));
+                }
+                return binaryKeys;
+            }
+        });
+    }
+
+    protected String createRedisKey(String key) {
+        return getProcessorName() + ":" + key;
+    }
+
+    public long getExpiry() {
+        return expiry;
+    }
+
+    /**
+     * Expire all newly added items after the given number of seconds (0 means never expire)
+     */
+    public void setExpiry(long expiry) {
+        this.expiry = expiry;
+    }
 }
