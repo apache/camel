1f91ea7820f85cf968bde650bf5f9721e0fb7799
davidwilliams <david@getgathering.com>
2015-11-11 14:11:36 -0500

    CAMEL-8302: Added skipQueueDeclare flag to camel-rabbitmq

--- components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQEndpoint.java
+++ components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQEndpoint.java
@@ -95,6 +95,8 @@ public class RabbitMQEndpoint extends DefaultEndpoint {
     private String exchangeType = "direct";
     @UriParam
     private String routingKey;
+    @UriParam(defaultValue = "false")
+    private boolean skipQueueDeclare = false;
     @UriParam
     private Address[] addresses;
     @UriParam(defaultValue = "" + ConnectionFactory.DEFAULT_CONNECTION_TIMEOUT)
@@ -369,7 +371,7 @@ public class RabbitMQEndpoint extends DefaultEndpoint {
                 getExchangeType(),
                 isDurable(),
                 isAutoDelete(), exchangeArgs);
-        if (getQueue() != null) {
+        if (!isSkipQueueDeclare() && getQueue() != null) {
             // need to make sure the queueDeclare is same with the exchange declare
             channel.queueDeclare(getQueue(), isDurable(), false,
                     isAutoDelete(), queueArgs);
@@ -585,6 +587,18 @@ public class RabbitMQEndpoint extends DefaultEndpoint {
      */
     public void setRoutingKey(String routingKey) {
         this.routingKey = routingKey;
+    }
+
+    /**
+     * If true the producer will not declare and bind a queue.
+     * This can be used for directing messages via an existing routing key.
+     */
+    public void setSkipQueueDeclare(boolean skipQueueDeclare) {
+        this.skipQueueDeclare = skipQueueDeclare;
+    }
+
+    public boolean isSkipQueueDeclare() {
+        return skipQueueDeclare;
     }
 
     /**
