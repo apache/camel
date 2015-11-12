9cbd5867fe73ef07ecba6f16d64689632e3f2a16
Claus Ibsen <davsclaus@apache.org>
2015-11-12 19:02:38 +0100

    CAMEL-9309: Make it easier to turn on|off java transport over http

--- components/camel-http/src/main/java/org/apache/camel/component/http/HttpComponent.java
+++ components/camel-http/src/main/java/org/apache/camel/component/http/HttpComponent.java
@@ -349,23 +349,12 @@ public class HttpComponent extends HeaderFilterStrategyComponent {
         this.httpConfiguration = httpConfiguration;
     }
 
-    public boolean isAllowJavaSerializedObject() {
-        return allowJavaSerializedObject;
-    }
-
     public void setAllowJavaSerializedObject(boolean allowJavaSerializedObject) {
         this.allowJavaSerializedObject = allowJavaSerializedObject;
     }
 
-    /**
-     * Whether to allow java serialization when a request uses context-type=application/x-java-serialized-object
-     * <p/>
-     * This is by default turned off. If you enable this then be aware that Java will deserialize the incoming
-     * data from the request to Java and that can be a potential security risk.
-     */
-    @Override
-    public void setAllowJavaSerializedObject(boolean allowJavaSerializedObject) {
-        // need to override and call super for component docs
-        super.setAllowJavaSerializedObject(allowJavaSerializedObject);
+    public boolean isAllowJavaSerializedObject() {
+        return allowJavaSerializedObject;
     }
+
 }
