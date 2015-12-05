d04c83c867b0f38cb886fc567974a652b9b1e197
Claus Ibsen <davsclaus@apache.org>
2015-12-05 11:09:17 +0100

    Fixed CS

--- camel-core/src/main/java/org/apache/camel/CamelContext.java
+++ camel-core/src/main/java/org/apache/camel/CamelContext.java
@@ -1384,14 +1384,14 @@ public interface CamelContext extends SuspendableService, RuntimeConfiguration {
      *
      * @return the factory
      */
-    public MessageHistoryFactory getMessageHistoryFactory();
+    MessageHistoryFactory getMessageHistoryFactory();
 
     /**
      * Sets a custom {@link org.apache.camel.spi.MessageHistoryFactory}
      *
      * @param messageHistoryFactory the custom factory
      */
-    public void setMessageHistoryFactory(MessageHistoryFactory messageHistoryFactory);
+    void setMessageHistoryFactory(MessageHistoryFactory messageHistoryFactory);
 
     /**
      * Gets the current {@link Debugger}
