7d715c895d6b4ff82342b55af8f125085c47f3f2
Claus Ibsen <davsclaus@apache.org>
2015-12-10 09:10:31 +0100

    CAMEL-9338: Upgrade Spring Integration to version 4.2.2

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelSpringIntegrationTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelSpringIntegrationTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.camel.itest.karaf;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -23,6 +24,7 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 
 @RunWith(PaxExam.class)
+@Ignore("Spring Integration 4.x is not OSGi bundle")
 public class CamelSpringIntegrationTest extends AbstractFeatureTest {
 
     public static final String COMPONENT = extractName(CamelSpringIntegrationTest.class);
