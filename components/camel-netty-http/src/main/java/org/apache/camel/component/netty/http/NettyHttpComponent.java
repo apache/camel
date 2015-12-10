2a66cb22088979fc401d96dcc857a892c8ca0f58
Claus Ibsen <davsclaus@apache.org>
2015-12-10 16:22:49 +0100

    CAMEL-9382: netty http should be more friendly in the hostname and support default http as protocol. And whether people include double slashes or not.

--- components/camel-netty-http/src/main/java/org/apache/camel/component/netty/http/NettyHttpComponent.java
+++ components/camel-netty-http/src/main/java/org/apache/camel/component/netty/http/NettyHttpComponent.java
@@ -100,15 +100,31 @@ public class NettyHttpComponent extends NettyComponent implements HeaderFilterSt
             sharedPort = shared.getPort();
         }
 
-        // configure configuration
-        config = parseConfiguration(config, remaining, parameters);
+        // we must include the protocol in the remaining
+        boolean hasProtocol = remaining.startsWith("http://") || remaining.startsWith("http:")
+                || remaining.startsWith("https://") || remaining.startsWith("https:");
+        if (!hasProtocol) {
+            // http is the default protocol
+            remaining = "http://" + remaining;
+        }
+        boolean hasSlash = remaining.startsWith("http://") || remaining.startsWith("https://");
+        if (!hasSlash) {
+            // must have double slash after protocol
+            if (remaining.startsWith("http:")) {
+                remaining = "http://" + remaining.substring(5);
+            } else {
+                remaining = "https://" + remaining.substring(6);
+            }
+        }
+        LOG.debug("Netty http url: {}", remaining);
+
         // set port on configuration which is either shared or using default values
         if (sharedPort != -1) {
             config.setPort(sharedPort);
         } else if (config.getPort() == -1) {
-            if ("http".equals(config.getProtocol())) {
+            if (remaining.startsWith("http:")) {
                 config.setPort(80);
-            } else if ("https".equals(config.getProtocol())) {
+            } else if (remaining.startsWith("https:")) {
                 config.setPort(443);
             }
         }
@@ -116,6 +132,8 @@ public class NettyHttpComponent extends NettyComponent implements HeaderFilterSt
             throw new IllegalArgumentException("Port number must be configured");
         }
 
+        // configure configuration
+        config = parseConfiguration(config, remaining, parameters);
         setProperties(config, parameters);
 
         // validate config
