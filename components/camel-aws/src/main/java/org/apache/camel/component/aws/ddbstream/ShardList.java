51e251c700dfd1bdb0f443d9e42d1281890338dd
Candle <candle@candle.me.uk>
2015-12-15 18:22:20 +0000

    Fixed CS.

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/ddbstream/ShardList.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/ddbstream/ShardList.java
@@ -1,14 +1,31 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.aws.ddbstream;
 
-import com.amazonaws.services.dynamodbv2.model.Shard;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.model.Shard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class ShardList {
-    private final Logger LOG = LoggerFactory.getLogger(ShardList.class);
+    private final Logger log = LoggerFactory.getLogger(ShardList.class);
 
     private final Map<String, Shard> shards = new HashMap<>();
 
@@ -58,7 +75,7 @@ class ShardList {
                 current = s.getParentShardId();
             }
         }
-        LOG.trace("removed {} shards from the store, new size is {}", removedShards, shards.size());
+        log.trace("removed {} shards from the store, new size is {}", removedShards, shards.size());
     }
 
     @Override
