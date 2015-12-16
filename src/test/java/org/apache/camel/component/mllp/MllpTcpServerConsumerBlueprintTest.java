77b76062f75d3509db0166cab47e23ee39154881
Quinn Stevenson <quinn@pronoia-solutions.com>
2015-12-16 10:34:07 -0700

    Intial version of MllpTcpServerConsumerBlueprintTest - not yet running

--- src/test/java/org/apache/camel/component/mllp/MllpTcpServerConsumerBlueprintTest.java
+++ src/test/java/org/apache/camel/component/mllp/MllpTcpServerConsumerBlueprintTest.java
@@ -18,17 +18,67 @@ package org.apache.camel.component.mllp;
 
 import org.apache.camel.EndpointInject;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.AvailablePortFinder;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.apache.camel.test.junit.rule.mllp.MllpClientResource;
 import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Dictionary;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.camel.test.Hl7MessageGenerator.generateMessage;
 
 @Ignore( value = "Not Yet Implemented")
 // TODO: Implement this
 public class MllpTcpServerConsumerBlueprintTest extends CamelBlueprintTestSupport {
-    @EndpointInject( uri = "mock://target")
-    MockEndpoint target;
+    @Rule
+    public MllpClientResource mllpClient = new MllpClientResource();
+
+    @EndpointInject(uri = "mock://received")
+    MockEndpoint received;
+
+    @EndpointInject(uri = "mock://timeout-ex")
+    MockEndpoint timeout;
+
+    @EndpointInject(uri = "mock://frame-ex")
+    MockEndpoint frame;
+
 
     @Override
     protected String getBlueprintDescriptor() {
         return "OSGI-INF/blueprint/mllp-tcp-server-consumer.xml";
     }
+
+    @Override
+    protected void doPreSetup() throws Exception {
+        mllpClient.setMllpPort(AvailablePortFinder.getNextAvailable());
+
+        super.doPreSetup();
+    }
+
+    @Override
+    protected String useOverridePropertiesWithConfigAdmin(Dictionary props) throws Exception {
+
+        props.put("mllp.port", mllpClient.getMllpHost() );
+
+        return "MllpTcpServerConsumerBlueprintTest";
+    }
+
+    @Test
+    public void testReceiveMultipleMessages() throws Exception {
+        int sendMessageCount = 5;
+        received.expectedMinimumMessageCount(5);
+
+        mllpClient.connect();
+
+        for ( int i=1; i<=sendMessageCount; ++i ) {
+            mllpClient.sendMessageAndWaitForAcknowledgement(generateMessage(i));
+        }
+
+        assertMockEndpointsSatisfied(10, TimeUnit.SECONDS);
+    }
+
+
 }
