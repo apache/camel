fdea368f16906b3a3e9a83690dead2e4c1c4f74e
Claus Ibsen <davsclaus@apache.org>
2016-01-05 19:50:46 +0100

    Polished

--- components/camel-atmos/src/test/java/org/apache/camel/component/atmos/AtmosConsumerTest.java
+++ components/camel-atmos/src/test/java/org/apache/camel/component/atmos/AtmosConsumerTest.java
@@ -32,7 +32,7 @@ public class AtmosConsumerTest extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("atmos:foo/" + get + "?remotePath=/path").to("mock:test");
+                from("atmos:foo/get?remotePath=/path").to("mock:test");
             }
         };
     }
@@ -40,7 +40,7 @@ public class AtmosConsumerTest extends CamelTestSupport {
     @Test
     public void shouldCreateGetConsumer() throws Exception {
         // Given
-        AtmosEndpoint atmosEndpoint = context.getEndpoint("atmos:foo/" + get + "?remotePath=/path", AtmosEndpoint.class);
+        AtmosEndpoint atmosEndpoint = context.getEndpoint("atmos:foo/get?remotePath=/path", AtmosEndpoint.class);
 
         // When
         Consumer consumer = atmosEndpoint.createConsumer(null);
