dbaee38dc699e7b1a71d670826fc8e7cd719de93
Claus Ibsen <davsclaus@apache.org>
2016-01-10 15:09:47 +0100

    Component docs

--- components/camel-facebook/src/main/java/org/apache/camel/component/facebook/FacebookEndpoint.java
+++ components/camel-facebook/src/main/java/org/apache/camel/component/facebook/FacebookEndpoint.java
@@ -56,10 +56,10 @@ public class FacebookEndpoint extends DefaultEndpoint implements FacebookConstan
     private static final Logger LOG = LoggerFactory.getLogger(FacebookEndpoint.class);
 
     private FacebookNameStyle nameStyle;
-    private String method;
 
-    // Facebook4J method name
-    @UriPath(description = "What operation to perform") @Metadata(required = "true")
+    @UriPath(name = "methodName", description = "What operation to perform") @Metadata(required = "true")
+
+    private String method;
     private FacebookMethodsType methodName;
     @UriParam
     private FacebookEndpointConfiguration configuration;
@@ -202,8 +202,4 @@ public class FacebookEndpoint extends DefaultEndpoint implements FacebookConstan
         this.inBody = inBody;
     }
 
-    @Override
-    protected void doStart() throws Exception {
-        super.doStart();
-    }
 }
