93f8f6d6e930f09e8a3c6294f1fb027398962687
Andrea Cosentino <ancosen@gmail.com>
2016-01-11 11:34:56 +0100

    Upgrade Jackson to version 2.7.0

--- components/camel-jacksonxml/src/test/java/org/apache/camel/component/jacksonxml/SpringJacksonJsonDataFormatTest.java
+++ components/camel-jacksonxml/src/test/java/org/apache/camel/component/jacksonxml/SpringJacksonJsonDataFormatTest.java
@@ -61,7 +61,7 @@ public class SpringJacksonJsonDataFormatTest extends CamelSpringTestSupport {
 
         Object marshalled = template.requestBody("direct:inPretty", in);
         String marshalledAsString = context.getTypeConverter().convertTo(String.class, marshalled);
-        String expected = "<HashMap>\n  <name>Camel</name>\n</HashMap>";
+        String expected = "<HashMap>\n  <name>Camel</name>\n</HashMap>\n";
         assertEquals(expected, marshalledAsString);
 
         template.sendBody("direct:back", marshalled);
