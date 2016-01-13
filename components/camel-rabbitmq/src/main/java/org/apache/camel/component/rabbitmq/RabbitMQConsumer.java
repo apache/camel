f775fb334b39d48352e60a06f15388dfbd147019
Andrea Cosentino <ancosen@gmail.com>
2016-01-13 14:05:34 +0100

    CAMEL-9505: RabbitMQConsumer don't use Camel ExceptionHandler BEFORE requeing message

--- components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQConsumer.java
+++ components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQConsumer.java
@@ -219,6 +219,9 @@ public class RabbitMQConsumer extends DefaultConsumer {
                     channel.basicAck(deliveryTag, false);
                 }
             } else {
+                if (exchange.getException() != null) {
+                    getExceptionHandler().handleException("Error processing exchange", exchange, exchange.getException());
+                }
                 boolean isRequeueHeaderSet = msg.getHeader(RabbitMQConstants.REQUEUE, false, boolean.class);
                 // processing failed, then reject and handle the exception
                 if (deliveryTag != 0 && !consumer.endpoint.isAutoAck()) {
@@ -228,9 +231,6 @@ public class RabbitMQConsumer extends DefaultConsumer {
                     } else {
                         channel.basicReject(deliveryTag, false);
                     }
-                }
-                if (exchange.getException() != null) {
-                    getExceptionHandler().handleException("Error processing exchange", exchange, exchange.getException());
                 }
             }
         }
