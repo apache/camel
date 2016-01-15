501185d23b1c68676fc7e4709270208ab29ab2f3
Antonin Stefanutti <antonin@stefanutti.fr>
2016-01-15 15:14:50 +0100

    Avoid non-static nested class and fix CS error

--- camel-core/src/main/java/org/apache/camel/main/MainSupport.java
+++ camel-core/src/main/java/org/apache/camel/main/MainSupport.java
@@ -62,11 +62,11 @@ public abstract class MainSupport extends ServiceSupport {
     /**
      * A class for intercepting the hang up signal and do a graceful shutdown of the Camel.
      */
-    private final class HangupInterceptor extends Thread {
+    private static final class HangupInterceptor extends Thread {
         Logger log = LoggerFactory.getLogger(this.getClass());
         final MainSupport mainInstance;
 
-        public HangupInterceptor(MainSupport main) {
+        HangupInterceptor(MainSupport main) {
             mainInstance = main;
         }
 
