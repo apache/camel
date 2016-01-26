985331d9726c28c9ba9d53c5e6aed6e19fc4ba53
Claus Ibsen <davsclaus@apache.org>
2016-01-26 16:30:38 +0100

    Camel catalog - Add support for multi valued options

--- platforms/catalog/src/main/java/org/apache/camel/catalog/URISupport.java
+++ platforms/catalog/src/main/java/org/apache/camel/catalog/URISupport.java
@@ -22,6 +22,7 @@ import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,6 +50,23 @@ public final class URISupport {
      */
     public static URI normalizeUri(String uri) throws URISyntaxException {
         return new URI(UnsafeUriCharactersEncoder.encode(uri, true));
+    }
+
+    public static Map<String, Object> extractProperties(Map<String, Object> properties, String optionPrefix) {
+        Map<String, Object> rc = new LinkedHashMap<String, Object>(properties.size());
+
+        for (Iterator<Map.Entry<String, Object>> it = properties.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, Object> entry = it.next();
+            String name = entry.getKey();
+            if (name.startsWith(optionPrefix)) {
+                Object value = properties.get(name);
+                name = name.substring(optionPrefix.length());
+                rc.put(name, value);
+                it.remove();
+            }
+        }
+
+        return rc;
     }
 
     /**
