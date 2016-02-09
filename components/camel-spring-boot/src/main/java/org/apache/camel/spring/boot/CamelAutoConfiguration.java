4af2c3c34bfee00e58632a3b1dc6e299cfd43661
Claus Ibsen <davsclaus@apache.org>
2016-02-09 10:30:08 +0100

    CAMEL-9431: Fixed spring-boot to not handle lifecycle of beans that Camel is handling, this ensure a proper startup/shutdown sequence.

--- components/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/CamelAutoConfiguration.java
+++ components/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/CamelAutoConfiguration.java
@@ -75,7 +75,8 @@ public class CamelAutoConfiguration {
     /**
      * Default producer template for the bootstrapped Camel context.
      */
-    @Bean
+    @Bean(initMethod = "", destroyMethod = "")
+    // Camel handles the lifecycle of this bean
     @ConditionalOnMissingBean(ProducerTemplate.class)
     ProducerTemplate producerTemplate(CamelContext camelContext,
                                       CamelConfigurationProperties configurationProperties) {
@@ -85,7 +86,8 @@ public class CamelAutoConfiguration {
     /**
      * Default consumer template for the bootstrapped Camel context.
      */
-    @Bean
+    @Bean(initMethod = "", destroyMethod = "")
+    // Camel handles the lifecycle of this bean
     @ConditionalOnMissingBean(ConsumerTemplate.class)
     ConsumerTemplate consumerTemplate(CamelContext camelContext,
                                       CamelConfigurationProperties configurationProperties) {
@@ -99,11 +101,17 @@ public class CamelAutoConfiguration {
         return new SpringPropertiesParser();
     }
 
-    @Bean
-    PropertiesComponent properties() {
-        PropertiesComponent properties = new PropertiesComponent();
-        properties.setPropertiesParser(propertiesParser());
-        return properties;
+    @Bean(initMethod = "", destroyMethod = "")
+    // Camel handles the lifecycle of this bean
+    PropertiesComponent properties(CamelContext camelContext, PropertiesParser parser) {
+        if (camelContext.hasComponent("properties") != null) {
+            return camelContext.getComponent("properties", PropertiesComponent.class);
+        } else {
+            PropertiesComponent pc = new PropertiesComponent();
+            pc.setPropertiesParser(parser);
+            camelContext.addComponent("properties", pc);
+            return pc;
+        }
     }
 
     /**
