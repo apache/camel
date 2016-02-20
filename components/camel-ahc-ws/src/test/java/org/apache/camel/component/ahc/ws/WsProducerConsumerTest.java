bd3251cbc410d5130a696bc3a6a1464590c80b19
Claus Ibsen <davsclaus@apache.org>
2016-02-20 09:10:24 +0100

    CAMEL-9566: Added unit test

--- components/camel-ahc-ws/src/test/java/org/apache/camel/component/ahc/ws/WsProducerConsumerTest.java
+++ components/camel-ahc-ws/src/test/java/org/apache/camel/component/ahc/ws/WsProducerConsumerTest.java
@@ -87,19 +87,41 @@ public class WsProducerConsumerTest extends CamelTestSupport {
         mock.assertIsSatisfied();
     }
 
-    
+    @Test
+    public void testTwoRoutesRestart() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedBodiesReceived(TEST_MESSAGE);
+
+        template.sendBody("direct:input", TEST_MESSAGE);
+
+        mock.assertIsSatisfied();
+
+        resetMocks();
+
+        log.info("Restarting bar route");
+        context.stopRoute("bar");
+        Thread.sleep(500);
+        context.startRoute("bar");
+
+        mock.expectedBodiesReceived(TEST_MESSAGE);
+
+        template.sendBody("direct:input", TEST_MESSAGE);
+
+        mock.assertIsSatisfied();
+    }
+
     @Override
     protected RouteBuilder[] createRouteBuilders() throws Exception {
         RouteBuilder[] rbs = new RouteBuilder[2];
         rbs[0] = new RouteBuilder() {
             public void configure() {
-                from("direct:input")
+                from("direct:input").routeId("foo")
                     .to("ahc-ws://localhost:" + PORT);
             }
         };
         rbs[1] = new RouteBuilder() {
             public void configure() {
-                from("ahc-ws://localhost:" + PORT)
+                from("ahc-ws://localhost:" + PORT).routeId("bar")
                     .to("mock:result");
             }
         };
