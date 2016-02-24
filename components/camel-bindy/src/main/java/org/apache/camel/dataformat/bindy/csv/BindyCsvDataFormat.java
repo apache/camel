08d97d879adba8b9cec98cf9774b8704e5fd85b7
lburgazzoli <lburgazzoli@gmail.com>
2016-02-24 14:31:46 +0100

    CAMEL-9432 - Bindy CSV separator not treated as regex but fixed character in all cases

--- components/camel-bindy/src/main/java/org/apache/camel/dataformat/bindy/csv/BindyCsvDataFormat.java
+++ components/camel-bindy/src/main/java/org/apache/camel/dataformat/bindy/csv/BindyCsvDataFormat.java
@@ -165,7 +165,7 @@ public class BindyCsvDataFormat extends BindyAbstractDataFormat {
 
                 // Split the CSV record according to the separator defined in
                 // annotated class @CSVRecord
-                String[] tokens = line.split(separator, -1);
+                String[] tokens = line.split(separator, factory.getAutospanLine() ? factory.getMaxpos() : -1);
                 List<String> result = Arrays.asList(tokens);
                 // must unquote tokens before use
                 result = unquoteTokens(result, separator, quote);
@@ -175,10 +175,6 @@ public class BindyCsvDataFormat extends BindyAbstractDataFormat {
                 } else {
                     if (LOG.isDebugEnabled()) {
                         LOG.debug("Size of the record splitted : {}", result.size());
-                    }
-
-                    if (factory.getAutospanLine()) {
-                        result = autospanLine(result, factory.getMaxpos(), separator);
                     }
 
                     // Bind data from CSV record with model classes
@@ -207,38 +203,6 @@ public class BindyCsvDataFormat extends BindyAbstractDataFormat {
             IOHelper.close(in, "in", LOG);
         }
 
-    }
-
-    /**
-     * Concatenate "the rest of the line" as the last record. Works similar as if quoted
-     *
-     * @param result    input result set
-     * @param maxpos    position of maximum record
-     * @param separator csv separator char
-     * @return List<String> with concatenated last record
-     */
-    private static List<String> autospanLine(final List<String> result, final int maxpos, final String separator) {
-        if (result.size() <= maxpos) {
-            return result;
-        }
-
-        final List<String> answer = new ArrayList<String>();
-        final StringBuilder lastRecord = new StringBuilder();
-
-        final Iterator<String> it = result.iterator();
-        for (int counter = 0; counter < maxpos - 1; counter++) {
-            answer.add(it.next());
-        }
-
-        while (it.hasNext()) {
-            lastRecord.append(it.next());
-            if (it.hasNext()) {
-                lastRecord.append(separator);
-            }
-        }
-        answer.add(lastRecord.toString());
-
-        return answer;
     }
 
     /**
