d0b49ac707c797f9b4a89c39f4e88e04974c4796
lburgazzoli <lburgazzoli@gmail.com>
2016-02-25 10:14:51 +0100

    CAMEL-9285 - controlbus - Allow to grab current route id so you can control current route easier

--- camel-core/src/test/java/org/apache/camel/component/controlbus/ControlBusStatsTest.java
+++ camel-core/src/test/java/org/apache/camel/component/controlbus/ControlBusStatsTest.java
@@ -45,6 +45,22 @@ public class ControlBusStatsTest extends ContextTestSupport {
         assertTrue(xml.contains("exchangesCompleted=\"1\""));
     }
 
+    public void testControlBusCurrentRouteStat() throws Exception {
+        getMockEndpoint("mock:current").expectedBodiesReceived("Hello World");
+
+        template.sendBody("direct:current", "Hello World");
+
+        assertMockEndpointsSatisfied();
+
+        String xml = template.requestBody("controlbus:route?routeId=current&action=stats", null, String.class);
+        assertNotNull(xml);
+
+        assertTrue(xml.contains("routeStat"));
+        assertTrue(xml.contains("processorStat"));
+        assertTrue(xml.contains("id=\"current\""));
+        assertTrue(xml.contains("exchangesCompleted=\"1\""));
+    }
+
     public void testControlBusContextStat() throws Exception {
         getMockEndpoint("mock:bar").expectedBodiesReceived("Hello World");
 
@@ -69,9 +85,10 @@ public class ControlBusStatsTest extends ContextTestSupport {
             public void configure() throws Exception {
                 from("direct:foo").routeId("foo")
                     .to("mock:foo");
-
                 from("direct:bar").routeId("bar")
                     .to("mock:bar");
+                from("direct:current").routeId("current")
+                    .to("mock:current");
             }
         };
     }
