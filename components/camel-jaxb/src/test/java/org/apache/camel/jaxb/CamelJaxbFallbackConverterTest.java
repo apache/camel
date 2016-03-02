e413b9103973fbab68e25376e3f5107dca9097d2
Martin Basovnik <mbasovni@redhat.com>
2016-03-02 09:31:26 +0100

    CAMEL-9649: Do not require @XmlRootElement annotation in JAXB FallbackTypeConverter

--- components/camel-jaxb/src/test/java/org/apache/camel/jaxb/CamelJaxbFallbackConverterTest.java
+++ components/camel-jaxb/src/test/java/org/apache/camel/jaxb/CamelJaxbFallbackConverterTest.java
@@ -22,6 +22,7 @@ import java.io.InputStream;
 import org.apache.camel.Exchange;
 import org.apache.camel.TypeConversionException;
 import org.apache.camel.TypeConverter;
+import org.apache.camel.converter.jaxb.message.Message;
 import org.apache.camel.example.Bar;
 import org.apache.camel.example.Foo;
 import org.apache.camel.foo.bar.PersonType;
@@ -115,7 +116,15 @@ public class CamelJaxbFallbackConverterTest extends CamelTestSupport {
 
         value = converter.convertTo(String.class, exchange, person);
         assertTrue("Should not filter the non-xml chars", value.indexOf("<lastName>BAR\uD8FF</lastName>") > 0);
+    }
 
+    @Test
+    public void testNoXmlRootElementAnnotation() throws Exception {
+        Message in = new Message("Hello World");
+        TypeConverter converter = context.getTypeConverter();
+        String marshalled = converter.convertTo(String.class, in);
+        Message out = converter.convertTo(Message.class, marshalled);
+        assertEquals(in, out);
     }
 
 }
