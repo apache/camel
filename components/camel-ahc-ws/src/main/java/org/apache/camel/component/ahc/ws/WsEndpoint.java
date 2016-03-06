0dfbbb76ca9de5604362e9e80ac15d9aff109503
Claus Ibsen <davsclaus@apache.org>
2016-03-06 12:36:07 +0100

    CAMEL-9665: Fixed ahc-ws consumer to connect to the websocket when starting. Thanks to Thomas Gunter for the patch/suggestion.

--- components/camel-ahc-ws/src/main/java/org/apache/camel/component/ahc/ws/WsEndpoint.java
+++ components/camel-ahc-ws/src/main/java/org/apache/camel/component/ahc/ws/WsEndpoint.java
@@ -16,10 +16,8 @@
  */
 package org.apache.camel.component.ahc.ws;
 
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
@@ -43,7 +41,7 @@ public class WsEndpoint extends AhcEndpoint {
     private static final transient Logger LOG = LoggerFactory.getLogger(WsEndpoint.class);
 
     // for using websocket streaming/fragments
-    private static final boolean GRIZZLY_AVAILABLE = 
+    private static final boolean GRIZZLY_AVAILABLE =
         probeClass("com.ning.http.client.providers.grizzly.GrizzlyAsyncHttpProvider");
 
     private final Set<WsConsumer> consumers  = new HashSet<WsConsumer>();
@@ -51,7 +49,7 @@ public class WsEndpoint extends AhcEndpoint {
     private WebSocket websocket;
     @UriParam
     private boolean useStreaming;
-    
+
     public WsEndpoint(String endpointUri, WsComponent component) {
         super(endpointUri, component, null);
     }
@@ -64,7 +62,7 @@ public class WsEndpoint extends AhcEndpoint {
             return false;
         }
     }
-    
+
     @Override
     public WsComponent getComponent() {
         return (WsComponent) super.getComponent();
@@ -116,34 +114,44 @@ public class WsEndpoint extends AhcEndpoint {
         } else {
             client = new AsyncHttpClient(ahp, config);
         }
-        return client; 
+        return client;
     }
 
-    public void connect() throws InterruptedException, ExecutionException, IOException {
-        websocket = getClient().prepareGet(getHttpUri().toASCIIString()).execute(
+    public void connect() throws Exception {
+        String uri = getHttpUri().toASCIIString();
+
+        LOG.debug("Connecting to {}", uri);
+        websocket = getClient().prepareGet(uri).execute(
             new WebSocketUpgradeHandler.Builder()
                 .addWebSocketListener(new WsListener()).build()).get();
     }
-    
+
     @Override
     protected void doStop() throws Exception {
         if (websocket != null && websocket.isOpen()) {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Disconnecting from {}", getHttpUri().toASCIIString());
+            }
             websocket.close();
             websocket = null;
         }
         super.doStop();
     }
 
-    void connect(WsConsumer wsConsumer) {
+    void connect(WsConsumer wsConsumer) throws Exception {
         consumers.add(wsConsumer);
+
+        if (websocket == null || !websocket.isOpen()) {
+            connect();
+        }
     }
 
     void disconnect(WsConsumer wsConsumer) {
         consumers.remove(wsConsumer);
     }
-    
+
     class WsListener implements WebSocketTextListener, WebSocketByteListener {
-                
+
         @Override
         public void onOpen(WebSocket websocket) {
             LOG.debug("websocket opened");
@@ -161,7 +169,7 @@ public class WsEndpoint extends AhcEndpoint {
 
         @Override
         public void onMessage(byte[] message) {
-            LOG.debug("received message --> {}", message);
+            LOG.debug("Received message --> {}", message);
             for (WsConsumer consumer : consumers) {
                 consumer.sendMessage(message);
             }
@@ -169,14 +177,14 @@ public class WsEndpoint extends AhcEndpoint {
 
         @Override
         public void onMessage(String message) {
-            LOG.debug("received message --> {}", message);
+            LOG.debug("Received message --> {}", message);
             for (WsConsumer consumer : consumers) {
                 consumer.sendMessage(message);
             }
         }
 
     }
-    
+
     protected AsyncHttpProvider getAsyncHttpProvider(AsyncHttpClientConfig config) {
         if (GRIZZLY_AVAILABLE) {
             return new GrizzlyAsyncHttpProvider(config);
