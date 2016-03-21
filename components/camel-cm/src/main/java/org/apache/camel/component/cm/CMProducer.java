10ae69eeb7294fd95649ced32b0fa9a979acf545
oalles <oalles@neivi.es>
2016-03-21 13:11:09 +0100

    LOG instance removed. Parent Logger is going to be used.
    Logging debug level instead of info. 
    
    CMProducer is a Processor. process() method throws Exception. Let camel
    do the work and not catch exceptions. 
    
    In order to obtain the payload, Message.getMandatoryBody() is used
    instead of getBody() so there is no need to the check if the payload is
    null. Camel 's InvalidPayloadException is going to be thrown.

--- components/camel-cm/src/main/java/org/apache/camel/component/cm/CMProducer.java
+++ components/camel-cm/src/main/java/org/apache/camel/component/cm/CMProducer.java
@@ -28,15 +28,11 @@ import org.apache.camel.component.cm.exceptions.InvalidPayloadException;
 import org.apache.camel.impl.DefaultProducer;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * is the exchange processor. Sends a validated sms message to CM Endpoints.
  */
 public class CMProducer extends DefaultProducer {
-
-    private static final Logger LOG = LoggerFactory.getLogger(CMProducer.class);
 
     private Validator validator;
 
@@ -51,64 +47,60 @@ public class CMProducer extends DefaultProducer {
     }
 
     /**
-     * Producer is a exchange processor. This process is built in several steps. 1. Validate message receive from client 2. Send validated message to CM endpoints. 3. Process response from CM
-     * endpoints.
+     * Producer is a exchange processor. This process is built in several steps.
+     * 1. Validate message receive from client 2. Send validated message to CM
+     * endpoints. 3. Process response from CM endpoints.
      */
     @Override
-    public void process(final Exchange exchange) {
+    public void process(final Exchange exchange) throws Exception {
 
-        try {
+        // Immutable message receive from clients. Throws camel ' s
+        // InvalidPayloadException
+        final SMSMessage smsMessage = exchange.getIn()
+                .getMandatoryBody(SMSMessage.class);
 
-            // Immutable message receive from clients.
-            final SMSMessage smsMessage = exchange.getIn().getBody(SMSMessage.class);
-            if (smsMessage == null) {
-                throw new NullPointerException();
+        // Validates Payload - SMSMessage
+        log.trace("Validating SMSMessage instance provided: {}",
+                smsMessage.toString());
+        final Set<ConstraintViolation<SMSMessage>> constraintViolations = getValidator()
+                .validate(smsMessage);
+        if (constraintViolations.size() > 0) {
+            final StringBuffer msg = new StringBuffer();
+            for (final ConstraintViolation<SMSMessage> cv : constraintViolations) {
+                msg.append(String.format("- Invalid value for %s: %s",
+                        cv.getPropertyPath().toString(), cv.getMessage()));
             }
-
-            // Validates Payload - SMSMessage
-            LOG.trace("Validating SMSMessage instance provided: {}", smsMessage.toString());
-            final Set<ConstraintViolation<SMSMessage>> constraintViolations = getValidator().validate(smsMessage);
-            if (constraintViolations.size() > 0) {
-                final StringBuffer msg = new StringBuffer();
-                for (final ConstraintViolation<SMSMessage> cv : constraintViolations) {
-                    msg.append(String.format("- Invalid value for %s: %s", cv.getPropertyPath().toString(), cv.getMessage()));
-                }
-                throw new InvalidPayloadException(msg.toString());
-            }
-            LOG.trace("SMSMessage instance is valid: {}", smsMessage.toString());
-
-            // We have a valid (immutable) SMSMessage instance, lets extend to CMMessage
-            // This is the instance we will use to build the XML document to be
-            // sent to CM SMS GW.
-            final CMMessage cmMessage = new CMMessage(smsMessage.getPhoneNumber(), smsMessage.getMessage());
-            LOG.debug("CMMessage instance build from valid SMSMessage instance");
-
-            if (smsMessage.getFrom() == null || smsMessage.getFrom().isEmpty()) {
-                String df = getConfiguration().getDefaultFrom();
-                cmMessage.setSender(df);
-                LOG.debug("Dynamic sender is set to default dynamic sender: {}", df);
-            }
-
-            // Remember, this can be null.
-            cmMessage.setIdAsString(smsMessage.getId());
-
-            // Unicode and multipart
-            cmMessage.setUnicodeAndMultipart(getConfiguration().getDefaultMaxNumberOfParts());
-
-            // 2. Send a validated sms message to CM endpoints
-            // throws MessagingException for abnormal situations.
-            sender.send(cmMessage);
-
-            log.debug("Request accepted by CM Host: {}", cmMessage.toString());
-        } catch (final NullPointerException e) {
-            // Body hast to be an instance of SMSMessage
-            final String m = "Check Message body - Has to be an SMSMessage instance";
-            log.error(m, e);
-            exchange.setException(new InvalidPayloadException(m));
-        } catch (final RuntimeException e) {
-            log.error("Message cannot be sent ", e);
-            exchange.setException(e);
+            throw new InvalidPayloadException(msg.toString());
         }
+        log.trace("SMSMessage instance is valid: {}", smsMessage.toString());
+
+        // We have a valid (immutable) SMSMessage instance, lets extend to
+        // CMMessage
+        // This is the instance we will use to build the XML document to be
+        // sent to CM SMS GW.
+        final CMMessage cmMessage = new CMMessage(smsMessage.getPhoneNumber(),
+                smsMessage.getMessage());
+        log.debug("CMMessage instance build from valid SMSMessage instance");
+
+        if (smsMessage.getFrom() == null || smsMessage.getFrom().isEmpty()) {
+            String df = getConfiguration().getDefaultFrom();
+            cmMessage.setSender(df);
+            log.debug("Dynamic sender is set to default dynamic sender: {}",
+                    df);
+        }
+
+        // Remember, this can be null.
+        cmMessage.setIdAsString(smsMessage.getId());
+
+        // Unicode and multipart
+        cmMessage.setUnicodeAndMultipart(
+                getConfiguration().getDefaultMaxNumberOfParts());
+
+        // 2. Send a validated sms message to CM endpoints
+        // throws MessagingException for abnormal situations.
+        sender.send(cmMessage);
+
+        log.debug("Request accepted by CM Host: {}", cmMessage.toString());
     }
 
     @Override
@@ -124,10 +116,12 @@ public class CMProducer extends DefaultProducer {
         if (configuration.isTestConnectionOnStartup()) {
             try {
                 log.debug("Checking connection - {}", getEndpoint().getCMUrl());
-                HttpClientBuilder.create().build().execute(new HttpHead(getEndpoint().getCMUrl()));
-                log.info("Connection to {}: OK", getEndpoint().getCMUrl());
+                HttpClientBuilder.create().build()
+                        .execute(new HttpHead(getEndpoint().getCMUrl()));
+                log.debug("Connection to {}: OK", getEndpoint().getCMUrl());
             } catch (final Exception e) {
-                log.info("Connection to {}: NOT AVAILABLE", getEndpoint().getCMUrl());
+                log.debug("Connection to {}: NOT AVAILABLE",
+                        getEndpoint().getCMUrl());
                 throw new HostUnavailableException(e);
             }
         }
@@ -135,7 +129,7 @@ public class CMProducer extends DefaultProducer {
         // keep starting
         super.doStart();
 
-        log.info("CMProducer started");
+        log.debug("CMProducer started");
     }
 
     @Override
