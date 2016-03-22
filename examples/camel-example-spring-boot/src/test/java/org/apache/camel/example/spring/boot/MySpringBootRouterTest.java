0dc9ee6e0842017b6f5bee7b72dc086e00ad23f8
Claus Ibsen <davsclaus@apache.org>
2016-03-22 10:08:52 +0100

    CAMEL-9656: Bean component fixed to detect duplicate methods from generic bridge methods.

--- examples/camel-example-spring-boot/src/test/java/org/apache/camel/example/spring/boot/MySpringBootRouterTest.java
+++ examples/camel-example-spring-boot/src/test/java/org/apache/camel/example/spring/boot/MySpringBootRouterTest.java
@@ -38,9 +38,9 @@ public class MySpringBootRouterTest extends Assert {
 
     @Test
     public void shouldProduceMessages() throws InterruptedException {
-        // we expect that one or more messages is automatic done by the Camel
+        // we expect that a number of messages is automatic done by the Camel
         // route as it uses a timer to trigger
-        NotifyBuilder notify = new NotifyBuilder(camelContext).whenDone(1).create();
+        NotifyBuilder notify = new NotifyBuilder(camelContext).whenDone(4).create();
 
         assertTrue(notify.matches(10, TimeUnit.SECONDS));
     }
