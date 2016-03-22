5639b7860037f8ad387bfaa49a7a2b5e61d0d0e3
Claus Ibsen <davsclaus@apache.org>
2016-03-22 10:08:52 +0100

    CAMEL-9656: Bean component fixed to detect duplicate methods from generic bridge methods.

--- examples/camel-example-spring-boot/src/main/java/org/apache/camel/example/spring/boot/MySpringBootRouter.java
+++ examples/camel-example-spring-boot/src/main/java/org/apache/camel/example/spring/boot/MySpringBootRouter.java
@@ -17,17 +17,26 @@
 package org.apache.camel.example.spring.boot;
 
 import org.apache.camel.spring.boot.FatJarRouter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.HealthEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringBootRouter extends FatJarRouter {
 
+    @Autowired
+    private HealthEndpoint health;
+
     @Override
     public void configure() {
         from("timer:trigger")
                 .transform().simple("ref:myBean")
                 .to("log:out");
+
+        from("timer:status")
+            .bean(health, "invoke")
+            .log("Health is ${body}");
     }
 
     @Bean
