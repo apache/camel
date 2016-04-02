7f3cd61a40e4c3004243b7e65e7e3feaaa49940e
Andrea Cosentino <ancosen@gmail.com>
2016-04-02 11:11:19 +0200

    CAMEL-9797: camel-elastichsearch - Tests fails since recent upgrade

--- components/camel-lucene/src/main/java/org/apache/camel/component/lucene/LuceneConstants.java
+++ components/camel-lucene/src/main/java/org/apache/camel/component/lucene/LuceneConstants.java
@@ -19,5 +19,5 @@ package org.apache.camel.component.lucene;
 import org.apache.lucene.util.Version;
 
 public interface LuceneConstants {
-    Version LUCENE_VERSION = Version.LUCENE_5_5_0;
+    Version LUCENE_VERSION = Version.LUCENE_5_4_1;
 }
