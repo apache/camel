cdbe9fde5523faa218e24c8ed2781267078b9caf
Andrea Cosentino <ancosen@gmail.com>
2016-04-06 10:53:15 +0200

    Camel catalog - Include the adoc files. Fixed a little bug and excluded hidden directory

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
@@ -701,14 +701,14 @@ public class PrepareCatalogMojo extends AbstractMojo {
             File[] components = componentsDir.listFiles();
             if (components != null) {
                 for (File dir : components) {
-                    if (dir.isDirectory() && !"target".equals(dir.getName())) {
+                    if (dir.isDirectory() && !"target".equals(dir.getName()) && !dir.getName().startsWith(".")) {
                         File target = new File(dir, "src/main/docs");
 
                         int before = adocFiles.size();
                         findAsciiDocFilesRecursive(target, adocFiles, new CamelAsciiDocFileFilter());
                         int after = adocFiles.size();
 
-                        if (before != after) {
+                        if (before == after) {
                             missingAdocFiles.add(dir);
                         }
                     }
