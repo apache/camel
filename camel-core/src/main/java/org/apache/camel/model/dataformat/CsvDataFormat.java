b6d9db8114f18cee513861d9cfd62d22d1bbb6cf
Andrea Cosentino <ancosen@gmail.com>
2016-04-12 13:49:35 +0200

    CAMEL-9860: csv dataformat - Should have quoteMode option in model

--- camel-core/src/main/java/org/apache/camel/model/dataformat/CsvDataFormat.java
+++ camel-core/src/main/java/org/apache/camel/model/dataformat/CsvDataFormat.java
@@ -76,7 +76,8 @@ public class CsvDataFormat extends DataFormatDefinition {
     private String recordSeparator;
     @XmlAttribute
     private Boolean skipHeaderRecord;
-
+    @XmlAttribute
+    private String quoteMode;
     // Unmarshall options
     @XmlAttribute
     private Boolean lazyLoad;
@@ -159,7 +160,10 @@ public class CsvDataFormat extends DataFormatDefinition {
         if (skipHeaderRecord != null) {
             setProperty(camelContext, dataFormat, "skipHeaderRecord", skipHeaderRecord);
         }
-
+        if (quoteMode != null) {
+            setProperty(camelContext, dataFormat, "quoteMode", quoteMode);
+        }
+        
         // Unmarshall options
         if (lazyLoad != null) {
             setProperty(camelContext, dataFormat, "lazyLoad", lazyLoad);
@@ -391,7 +395,18 @@ public class CsvDataFormat extends DataFormatDefinition {
         this.skipHeaderRecord = skipHeaderRecord;
     }
 
-    public Boolean getLazyLoad() {
+    public String getQuoteMode() {
+		return quoteMode;
+	}
+
+    /**
+     * Sets the quote mode
+     */
+	public void setQuoteMode(String quoteMode) {
+		this.quoteMode = quoteMode;
+	}
+
+	public Boolean getLazyLoad() {
         return lazyLoad;
     }
 
