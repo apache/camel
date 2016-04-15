11cc56e0f4b9f1efa372e8f89325d70a4845c29e
Claus Ibsen <davsclaus@apache.org>
2016-04-15 14:45:45 +0200

    CAMEL-9332: Add support for spring-boot with the @RunWith so you can do that kind of junit testing with Camel and spring boot, and use the @MockEndpoint annotations and whatnot.

--- components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/mockendpoints/MyRoute.java
+++ components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/mockendpoints/MyRoute.java
@@ -5,27 +5,26 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.test.spring;
+package org.apache.camel.spring.boot.mockendpoints;
 
-import org.springframework.test.context.ContextLoader;
-import org.springframework.test.context.support.DefaultTestContextBootstrapper;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
-/**
- * To bootstrap Camel for testing with Spring 4.1 onwards.
- */
-public class CamelTestContextBootstrapper extends DefaultTestContextBootstrapper {
+@Component
+public class MyRoute extends RouteBuilder {
 
     @Override
-    protected Class<? extends ContextLoader> getDefaultContextLoaderClass(Class<?> testClass) {
-        return CamelSpringTestContextLoader.class;
+    public void configure() throws Exception {
+        from("direct:start")
+            .to("seda:foo");
     }
 }
