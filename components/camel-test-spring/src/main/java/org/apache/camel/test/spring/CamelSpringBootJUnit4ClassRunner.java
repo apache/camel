8662c12f5f12678187306c12038fb731eb4970c9
Claus Ibsen <davsclaus@apache.org>
2016-04-18 07:23:39 +0200

    Use a shorter name for the Camel @RunWith so its easier to remember

--- components/camel-test-spring/src/main/java/org/apache/camel/test/spring/CamelSpringBootJUnit4ClassRunner.java
+++ components/camel-test-spring/src/main/java/org/apache/camel/test/spring/CamelSpringBootJUnit4ClassRunner.java
@@ -24,7 +24,7 @@ import org.junit.runners.model.InitializationError;
  *
  * @deprecated use {@link CamelSpringBootRunner}
  */
-public class CamelSpringBootJUnit4ClassRunner extends CamelSpringRunner {
+public class CamelSpringBootJUnit4ClassRunner extends CamelSpringBootRunner {
 
     public CamelSpringBootJUnit4ClassRunner(Class<?> clazz) throws InitializationError {
         super(clazz);
