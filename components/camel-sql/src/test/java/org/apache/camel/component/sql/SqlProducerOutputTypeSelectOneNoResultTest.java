d4a724684b9ae3dcd62d87d9cf300bc73b3d5f2a
Andrea Cosentino <ancosen@gmail.com>
2016-04-28 09:54:50 +0200

    Fixed CS

--- components/camel-sql/src/test/java/org/apache/camel/component/sql/SqlProducerOutputTypeSelectOneNoResultTest.java
+++ components/camel-sql/src/test/java/org/apache/camel/component/sql/SqlProducerOutputTypeSelectOneNoResultTest.java
@@ -1,12 +1,25 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.sql;
 
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.camel.test.spring.CamelSpringDelegatingTestContextLoader;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +27,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-public class SqlProducerOutputTypeSelectOneNoResultTest extends CamelTestSupport{
+public class SqlProducerOutputTypeSelectOneNoResultTest extends CamelTestSupport {
 
     @EndpointInject(uri = "mock:result")
     MockEndpoint result;
@@ -35,15 +48,13 @@ public class SqlProducerOutputTypeSelectOneNoResultTest extends CamelTestSupport
         db.shutdown();
     }
     
-	@Test
-	public void testSqlEndpoint() throws Exception {
-
-		String expectedBody = "body";
-		result.expectedBodiesReceived(expectedBody);
-		template.sendBody("direct:start",expectedBody);
-		result.assertIsSatisfied();
-
-	}
+    @Test
+    public void testSqlEndpoint() throws Exception {
+        String expectedBody = "body";
+        result.expectedBodiesReceived(expectedBody);
+        template.sendBody("direct:start", expectedBody);
+        result.assertIsSatisfied();
+    }
     
     
     @Override
@@ -52,9 +63,9 @@ public class SqlProducerOutputTypeSelectOneNoResultTest extends CamelTestSupport
             public void configure() {
                 getContext().getComponent("sql", SqlComponent.class).setDataSource(db);
 
-				from("direct:start")
-				.to("sql:select id from mytable where 1 = 2?outputHeader=myHeader&outputType=SelectOne")
-				.log("${body}").to("mock:result");
+                from("direct:start")
+                    .to("sql:select id from mytable where 1 = 2?outputHeader=myHeader&outputType=SelectOne")
+                    .log("${body}").to("mock:result");
             }
         };
     }
