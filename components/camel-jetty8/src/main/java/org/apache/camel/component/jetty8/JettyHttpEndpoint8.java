9d7824bda32ff3be174600e344ad0f2a183d7a6d
Claus Ibsen <davsclaus@apache.org>
2016-04-29 11:51:51 +0200

    CAMEL-9874: Camel Jetty consumer endpoint incorrectly handles multipart/form-data

--- components/camel-jetty8/src/main/java/org/apache/camel/component/jetty8/JettyHttpEndpoint8.java
+++ components/camel-jetty8/src/main/java/org/apache/camel/component/jetty8/JettyHttpEndpoint8.java
@@ -22,6 +22,7 @@ import java.net.URISyntaxException;
 import org.apache.camel.component.jetty.JettyContentExchange;
 import org.apache.camel.component.jetty.JettyHttpComponent;
 import org.apache.camel.component.jetty.JettyHttpEndpoint;
+import org.apache.camel.http.common.HttpBinding;
 import org.apache.camel.http.common.HttpConsumer;
 import org.apache.camel.spi.UriEndpoint;
 
@@ -31,9 +32,30 @@ import org.apache.camel.spi.UriEndpoint;
 @UriEndpoint(scheme = "jetty", extendsScheme = "http", title = "Jetty",
         syntax = "jetty:httpUri", consumerClass = HttpConsumer.class, label = "http", lenientProperties = true)
 public class JettyHttpEndpoint8 extends JettyHttpEndpoint {
+    private HttpBinding binding;
 
     public JettyHttpEndpoint8(JettyHttpComponent component, String uri, URI httpURL) throws URISyntaxException {
         super(component, uri, httpURL);
+    }
+
+    @Override
+    public HttpBinding getHttpBinding() {
+        // make sure we include jetty9 variant of the http binding
+        if (this.binding == null) {
+            this.binding = new AttachmentHttpBinding();
+            this.binding.setTransferException(isTransferException());
+            if (getComponent() != null) {
+                this.binding.setAllowJavaSerializedObject(getComponent().isAllowJavaSerializedObject());
+            }
+            this.binding.setHeaderFilterStrategy(getHeaderFilterStrategy());
+        }
+        return this.binding;
+    }
+
+    @Override
+    public void setHttpBinding(HttpBinding binding) {
+        super.setHttpBinding(binding);
+        this.binding = binding;
     }
 
     @Override
