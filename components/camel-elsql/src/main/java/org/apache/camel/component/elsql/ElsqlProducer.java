893d256453d22bd60b2b0b5220608831d965d1c3
Claus Ibsen <davsclaus@apache.org>
2016-04-30 10:52:31 +0200

    CAMEL-9805: camel-sql - body not copied from in to out when useing outputHeader and outputType=SelectOne when sql doesn't return a result. Same fix for camel-elsql

--- components/camel-elsql/src/main/java/org/apache/camel/component/elsql/ElsqlProducer.java
+++ components/camel-elsql/src/main/java/org/apache/camel/component/elsql/ElsqlProducer.java
@@ -102,6 +102,13 @@ public class ElsqlProducer extends DefaultProducer {
                                     exchange.getOut().setBody(data);
                                 }
                                 exchange.getOut().setHeader(SqlConstants.SQL_ROW_COUNT, 1);
+                            } else {
+                                if (getEndpoint().isNoop()) {
+                                    exchange.getOut().setBody(exchange.getIn().getBody());
+                                } else if (getEndpoint().getOutputHeader() != null) {
+                                    exchange.getOut().setBody(exchange.getIn().getBody());
+                                }
+                                exchange.getOut().setHeader(SqlConstants.SQL_ROW_COUNT, 0);
                             }
                         } else {
                             throw new IllegalArgumentException("Invalid outputType=" + outputType);
