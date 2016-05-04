c52309c51e085418f1b882bb42aea966be5cb91f
Andrea Cosentino <ancosen@gmail.com>
2016-05-04 09:54:00 +0200

    Added Brave-zipkin bundle to camel-zipkin feature and remove @ignore annotation on CamelZipkinTest of Karaf Integration Tests

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelZipkinTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelZipkinTest.java
@@ -22,7 +22,6 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.PaxExam;
 
 @RunWith(PaxExam.class)
-@Ignore("Does not yet work in OSGi: https://issues.apache.org/jira/browse/SMX4-1983")
 public class CamelZipkinTest extends BaseKarafTest {
 
     public static final String COMPONENT = extractName(CamelZipkinTest.class);
