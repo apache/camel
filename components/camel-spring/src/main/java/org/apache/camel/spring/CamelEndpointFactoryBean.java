5d09896801d3ddb31c170819c2af832b861b0fb5
Claus Ibsen <davsclaus@apache.org>
2016-06-03 10:43:51 +0200

    CAMEL-10009: Fixed Spring XML DSL to not fail when using <to> with both id and ref attribute.

--- components/camel-spring/src/main/java/org/apache/camel/spring/CamelEndpointFactoryBean.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/CamelEndpointFactoryBean.java
@@ -40,6 +40,11 @@ import org.springframework.context.ApplicationContextAware;
 public class CamelEndpointFactoryBean extends AbstractCamelEndpointFactoryBean implements FactoryBean<Endpoint>, ApplicationContextAware {
     @XmlTransient
     private ApplicationContext applicationContext;
+    // ref is needed as transient as namespace parser registerEndpointsWithIdsDefinedInFromOrToTypes
+    // will discover <endpoint>, <to> etc and parse those eager and would attempt to call setRef on
+    // this factory bean for a <to id="foo" ref="bar"/> that is using both id and ref.
+    @XmlTransient
+    private String ref;
 
     @Override
     protected CamelContext getCamelContextWithId(String camelContextId) {
@@ -54,5 +59,13 @@ public class CamelEndpointFactoryBean extends AbstractCamelEndpointFactoryBean i
     public Endpoint getObject() throws Exception {
         Endpoint answer = super.getObject();
         return answer;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
     }
 }
