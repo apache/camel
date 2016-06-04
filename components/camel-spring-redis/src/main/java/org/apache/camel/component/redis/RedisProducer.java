add3d66f29ca8b24efae3399ea84dd74bcac6266
Arno Noordover <anoordover@users.noreply.github.com>
2016-06-04 16:42:10 +0200

    CS-fixes, but still an error on size of method and class

--- components/camel-spring-redis/src/main/java/org/apache/camel/component/redis/RedisProducer.java
+++ components/camel-spring-redis/src/main/java/org/apache/camel/component/redis/RedisProducer.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -30,7 +30,7 @@ import org.apache.camel.impl.HeaderSelectorProducer;
 final class RedisProducer extends HeaderSelectorProducer {
     private final Map<String, Processor> processors = new HashMap<>();
 
-    public RedisProducer(Endpoint endpoint,
+    RedisProducer(Endpoint endpoint,
                          RedisClient redisClient,
                          String header,
                          String defaultHeaderValue,
