28e24d3d45ba9ca82ab5820e6db1ea5583b987c8
Akitoshi Yoshida <ay@apache.org>
2016-06-18 19:35:54 +0200

    fix CS for CAMEL-10070

--- camel-core/src/main/java/org/apache/camel/converter/jaxp/XMLStreamReaderInputStream.java
+++ camel-core/src/main/java/org/apache/camel/converter/jaxp/XMLStreamReaderInputStream.java
@@ -39,7 +39,7 @@ class XMLStreamReaderInputStream extends InputStream {
     private String charset;
     private int bpos;
 
-    public XMLStreamReaderInputStream(XMLStreamReader reader, String charset, XMLOutputFactory outfactory) {
+    XMLStreamReaderInputStream(XMLStreamReader reader, String charset, XMLOutputFactory outfactory) {
         this.reader = reader;
         this.buffer = new byte[BUFFER_SIZE];
         this.chunk = new TrimmableByteArrayOutputStream();
@@ -51,7 +51,7 @@ class XMLStreamReaderInputStream extends InputStream {
         }
     }
 
-    public XMLStreamReaderInputStream(XMLStreamReader reader, XMLOutputFactory outfactory) {
+    XMLStreamReaderInputStream(XMLStreamReader reader, XMLOutputFactory outfactory) {
         this(reader, "utf-8", outfactory);
     }
 
