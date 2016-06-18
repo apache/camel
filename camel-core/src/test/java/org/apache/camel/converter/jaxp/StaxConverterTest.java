04c2bf318078d9da0b362d407ff87b225b44268d
Akitoshi Yoshida <ay@apache.org>
2016-06-18 20:09:33 +0200

    fix the previos style check

--- camel-core/src/test/java/org/apache/camel/converter/jaxp/StaxConverterTest.java
+++ camel-core/src/test/java/org/apache/camel/converter/jaxp/StaxConverterTest.java
@@ -18,6 +18,8 @@ package org.apache.camel.converter.jaxp;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.nio.charset.Charset;
