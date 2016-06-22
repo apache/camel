79658f8c75804851e2b4333248fefb521e6fc210
Andrea Cosentino <ancosen@gmail.com>
2016-06-22 11:22:55 +0200

    Upgrade commons-compress to version 1.12

--- components/camel-gson/src/main/java/org/apache/camel/component/gson/springboot/GsonDataFormatConfiguration.java
+++ components/camel-gson/src/main/java/org/apache/camel/component/gson/springboot/GsonDataFormatConfiguration.java
@@ -38,7 +38,7 @@ public class GsonDataFormatConfiguration {
      */
     private Boolean prettyPrint = false;
     /**
-     * Which json library to use such.
+     * Which json library to use.
      */
     private JsonLibrary library = JsonLibrary.Gson;
     /**
