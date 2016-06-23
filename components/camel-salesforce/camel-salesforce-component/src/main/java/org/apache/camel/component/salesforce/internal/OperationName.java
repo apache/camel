54eecb5b4de58883606a3ae15ef14cdbaed7b2dc
Sune Keller <absukl@almbrand.dk>
2016-06-23 16:58:37 +0200

    Support Salesforce queryAll
    Signed-off-by: Sune Keller <absukl@almbrand.dk>

--- components/camel-salesforce/camel-salesforce-component/src/main/java/org/apache/camel/component/salesforce/internal/OperationName.java
+++ components/camel-salesforce/camel-salesforce-component/src/main/java/org/apache/camel/component/salesforce/internal/OperationName.java
@@ -34,6 +34,7 @@ public enum OperationName {
     GET_BLOB_FIELD("getBlobField"),
     QUERY("query"),
     QUERY_MORE("queryMore"),
+    QUERY_ALL("queryAll"),
     SEARCH("search"),
     APEX_CALL("apexCall"),
 
