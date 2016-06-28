48e8283136af16ecadd2949ab3c6d15c06c2d980
Claus Ibsen <davsclaus@apache.org>
2016-06-28 09:34:13 +0200

    Fixed NPE

--- components/camel-core-xml/src/main/java/org/apache/camel/core/xml/util/jsse/SNIHostNamesDefinition.java
+++ components/camel-core-xml/src/main/java/org/apache/camel/core/xml/util/jsse/SNIHostNamesDefinition.java
@@ -16,7 +16,6 @@
  */
 package org.apache.camel.core.xml.util.jsse;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
