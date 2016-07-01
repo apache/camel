de9ea0afe3c8043cc662248acd82ad15edb0bce1
Arno Noordover <anoordover@users.noreply.github.com>
2016-07-02 01:01:16 +0200

    CAMEL-10103: More unittest

--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/OsgiParserFactoryTest.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/OsgiParserFactoryTest.java
@@ -19,7 +19,12 @@ package org.apache.camel.component.file.remote;
 import org.apache.commons.net.ftp.FTPClientConfig;
 import org.apache.commons.net.ftp.FTPFileEntryParser;
 import org.apache.commons.net.ftp.parser.CompositeFileEntryParser;
+import org.apache.commons.net.ftp.parser.MVSFTPEntryParser;
+import org.apache.commons.net.ftp.parser.MacOsPeterFTPEntryParser;
 import org.apache.commons.net.ftp.parser.NTFTPEntryParser;
+import org.apache.commons.net.ftp.parser.NetwareFTPEntryParser;
+import org.apache.commons.net.ftp.parser.OS2FTPEntryParser;
+import org.apache.commons.net.ftp.parser.OS400FTPEntryParser;
 import org.apache.commons.net.ftp.parser.UnixFTPEntryParser;
 import org.apache.commons.net.ftp.parser.VMSVersioningFTPEntryParser;
 import org.junit.Before;
@@ -96,9 +101,57 @@ public class OsgiParserFactoryTest {
     @Test
     public void createFileEntryParserWin32()
             throws Exception {
-        when(ftpClientConfig.getServerSystemKey()).thenReturn("WIN32");
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("bla WIN32 bla");
         FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
         assertThat(result, instanceOf(CompositeFileEntryParser.class));
+    }
+
+    @Test
+    public void createFileEntryParserOs2()
+            throws Exception {
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("bla OS/2 bla");
+        FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
+        assertThat(result, instanceOf(OS2FTPEntryParser.class));
+    }
+
+    @Test
+    public void createFileEntryParserPlainOs400()
+            throws Exception {
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("OS/400");
+        FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
+        assertThat(result, instanceOf(OS400FTPEntryParser.class));
+    }
+
+    @Test
+    public void createFileEntryParserNotPlainOs400()
+            throws Exception {
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("OS/400 bla");
+        FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
+        assertThat(result, instanceOf(CompositeFileEntryParser.class));
+    }
+
+    @Test
+    public void createFileEntryParserMvs()
+            throws Exception {
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("bla MvS bla");
+        FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
+        assertThat(result, instanceOf(MVSFTPEntryParser.class));
+    }
+
+    @Test
+    public void createFileEntryParserNetware()
+            throws Exception {
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("bla NeTwArE bla");
+        FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
+        assertThat(result, instanceOf(NetwareFTPEntryParser.class));
+    }
+
+    @Test
+    public void createFileEntryParserMacOsPeter()
+            throws Exception {
+        when(ftpClientConfig.getServerSystemKey()).thenReturn("bla MaCoS PeTER bla");
+        FTPFileEntryParser result = OSGI_PARSER_FACTORY.createFileEntryParser(ftpClientConfig);
+        assertThat(result, instanceOf(MacOsPeterFTPEntryParser.class));
     }
 
 }
