dabe1d5a82576c985dbbf5fbfa589304178ba265
Andrea Cosentino <ancosen@gmail.com>
2016-07-05 09:31:16 +0200

    Fixed CS

--- camel-core/src/main/java/org/apache/camel/util/UnsafeUriCharactersEncoder.java
+++ camel-core/src/main/java/org/apache/camel/util/UnsafeUriCharactersEncoder.java
@@ -32,7 +32,7 @@ public final class UnsafeUriCharactersEncoder {
     private static BitSet unsafeCharactersHttp;
     private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
                                               'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'};
-    private static Pattern rawPattern = Pattern.compile("RAW\\([^\\)]+\\)");
+    private static final Pattern RAW_PATTERN = Pattern.compile("RAW\\([^\\)]+\\)");
 
     static {
         unsafeCharactersRfc1738 = new BitSet(256);
@@ -95,7 +95,7 @@ public final class UnsafeUriCharactersEncoder {
     }
 
     private static List<Pair> checkRAW(String s) {
-        Matcher matcher = rawPattern.matcher(s);
+        Matcher matcher = RAW_PATTERN.matcher(s);
         List<Pair> answer = new ArrayList<Pair>();
         // Check all occurrences
         while (matcher.find()) {
