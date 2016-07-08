2e7756ce2b78ee1bcc5d82e4aeb22cc467521826
Antoine DESSAIGNE <antoine.dessaigne@gmail.com>
2016-07-08 09:37:53 +0200

    Add Lumberjack component

---
+++ components/camel-lumberjack/src/main/java/org/apache/camel/component/lumberjack/LumberjackEndpoint.java
@@ -0,0 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.lumberjack;
+
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.impl.DefaultEndpoint;
+import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriPath;
+
+@UriEndpoint(scheme = "lumberjack", title = "Lumberjack", syntax = "lumberjack:host:port", consumerClass = LumberjackConsumer.class, label = "log")
+class LumberjackEndpoint extends DefaultEndpoint {
+    @UriPath(description = "Network interface on which to listen for Lumberjack")
+    @Metadata(required = "true")
+    final String host;
+    @UriPath(description = "Network port on which to listen for Lumberjack", defaultValue = "" + LumberjackComponent.DEFAULT_PORT)
+    final int port;
+
+    LumberjackEndpoint(String endpointUri, LumberjackComponent component, String host, int port) {
+        super(endpointUri, component);
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public Producer createProducer() throws Exception {
+        throw new UnsupportedOperationException("The Lumberjack component cannot be used as a producer");
+    }
+
+    @Override
+    public LumberjackConsumer createConsumer(Processor processor) throws Exception {
+        return new LumberjackConsumer(this, processor, host, port, null);
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+}
---
+++ components/camel-lumberjack/src/main/java/org/apache/camel/component/lumberjack/io/LumberjackMessageProcessor.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.lumberjack.io;
+
+/**
+ * This functional interface defines a processor that will be called when a lumberjack message is received
+ */
+@FunctionalInterface
+public interface LumberjackMessageProcessor {
+    /**
+     * Called when a message is received. The {@code callback} must be called with the status of the processing
+     *
+     * @param payload Lumberjack message payload
+     * @param callback Callback to call when the processing is complete
+     */
+    void onMessageReceived(Object payload, Callback callback);
+
+    /**
+     * This functional interface defines the callback to call when the processing of a Lumberjack message is complete
+     */
+    @FunctionalInterface
+    interface Callback {
+        /**
+         * Called when the processing is complete.
+         *
+         * @param success {@code true} is the processing is a success, {@code false} otherwise
+         */
+        void onComplete(boolean success);
+    }
+}
