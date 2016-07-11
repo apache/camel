4eff18f9c2d848ae3d2165fe473d5487c1245944
Colm O hEigeartaigh <coheigea@apache.org>
2016-07-11 14:15:41 +0100

    Typo

--- camel-core/src/main/java/org/apache/camel/impl/DefaultUnitOfWork.java
+++ camel-core/src/main/java/org/apache/camel/impl/DefaultUnitOfWork.java
@@ -268,7 +268,7 @@ public class DefaultUnitOfWork implements UnitOfWork, Service {
     @Override
     public void afterRoute(Exchange exchange, Route route) {
         if (log.isTraceEnabled()) {
-            log.trace("UnitOfWork afterRouteL: {} for ExchangeId: {} with {}", new Object[]{route.getId(), exchange.getExchangeId(), exchange});
+            log.trace("UnitOfWork afterRoute: {} for ExchangeId: {} with {}", new Object[]{route.getId(), exchange.getExchangeId(), exchange});
         }
         UnitOfWorkHelper.afterRouteSynchronizations(route, exchange, synchronizations, log);
     }
