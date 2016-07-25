565348d3b106f6cddc47c39079e0b334c0c0451b
Claus Ibsen <davsclaus@apache.org>
2016-07-25 17:15:42 +0200

    Fixed CS. This closes #1080

--- components/camel-restlet/src/main/java/org/apache/camel/component/restlet/DefaultRestletBinding.java
+++ components/camel-restlet/src/main/java/org/apache/camel/component/restlet/DefaultRestletBinding.java
@@ -195,23 +195,22 @@ public class DefaultRestletBinding implements RestletBinding, HeaderFilterStrate
             }
         }
 
-        //Get outgoing custom http headers from the exchange if they exists
-        Series<Header> restletHeaders = exchange.getIn().getHeader(HeaderConstants.ATTRIBUTE_HEADERS,Series.class);
+        // get outgoing custom http headers from the exchange if they exists
+        Series<Header> restletHeaders = exchange.getIn().getHeader(HeaderConstants.ATTRIBUTE_HEADERS, Series.class);
         if (restletHeaders == null) {
-            restletHeaders = new Series<>(Header.class);
+            restletHeaders = new Series<Header>(Header.class);
             request.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS, restletHeaders);
         } else {
-            
-            //If the restlet headers already exists on the exchange, we need to filter them
-            for(String name : restletHeaders.getNames()){
-                if(headerFilterStrategy.applyFilterToCamelHeaders(name, restletHeaders.getValues(name), exchange)) {
+            // if the restlet headers already exists on the exchange, we need to filter them
+            for (String name : restletHeaders.getNames()) {
+                if (headerFilterStrategy.applyFilterToCamelHeaders(name, restletHeaders.getValues(name), exchange)) {
                     restletHeaders.removeAll(name);
                 }
             }
             request.getAttributes().put(HeaderConstants.ATTRIBUTE_HEADERS, restletHeaders);
-            
-            //Since the restlet headers already exists remove them from the exchange so they don't get added again below
-            //We will get a new set of restlet headers on the response
+
+            // since the restlet headers already exists remove them from the exchange so they don't get added again below
+            // we will get a new set of restlet headers on the response
             exchange.getIn().removeHeader(HeaderConstants.ATTRIBUTE_HEADERS);
         }
 
