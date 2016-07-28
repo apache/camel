287d08d566cf247cdf9d6d01a5f991b0f7a29786
Andrea Cosentino <ancosen@gmail.com>
2016-07-28 16:35:44 +0200

    Upgrade Spring-boot Configuration for camel-jgroups

--- components/camel-jgroups/src/main/java/org/apache/camel/component/jgroups/springboot/JGroupsComponentAutoConfiguration.java
+++ components/camel-jgroups/src/main/java/org/apache/camel/component/jgroups/springboot/JGroupsComponentAutoConfiguration.java
@@ -34,7 +34,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(JGroupsComponentConfiguration.class)
 public class JGroupsComponentAutoConfiguration {
 
-    @Bean(name = "jgroups-component")
+    @Bean
     @ConditionalOnClass(CamelContext.class)
     @ConditionalOnMissingBean(JGroupsComponent.class)
     public JGroupsComponent configureJGroupsComponent(
