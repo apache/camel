fba5c194d9e1b70214a21b432e1ab971e02429eb
Claus Ibsen <davsclaus@apache.org>
2016-08-01 13:49:32 +0200

    Fixed CS

--- camel-core/src/main/java/org/apache/camel/FluentProducerTemplate.java
+++ camel-core/src/main/java/org/apache/camel/FluentProducerTemplate.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@
 package org.apache.camel;
 
 import java.util.concurrent.Future;
-import java.util.function.*;
+import java.util.function.Supplier;
 
 /**
  * Template for working with Camel and sending {@link Message} instances in an
