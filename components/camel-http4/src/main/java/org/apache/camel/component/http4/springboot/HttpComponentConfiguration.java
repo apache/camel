d6e7ef46a397d3fc1dab274830bf206738dc7ef3
Andrea Cosentino <ancosen@gmail.com>
2016-08-02 14:52:57 +0200

    Revert "CAMEL-10216: Camel-Http4: Endpoint parameters proxyHost and proxyPort are ignored"
    This reverts commit 6329f57caf7ac75994c352c3abd32157847499b9.

--- components/camel-http4/src/main/java/org/apache/camel/component/http4/springboot/HttpComponentConfiguration.java
+++ components/camel-http4/src/main/java/org/apache/camel/component/http4/springboot/HttpComponentConfiguration.java
@@ -60,7 +60,7 @@ public class HttpComponentConfiguration {
      * the incoming data from the request to Java and that can be a potential
      * security risk.
      */
-    private Boolean allowJavaSerializedObject = false;
+    private Boolean allowJavaSerializedObject;
     /**
      * To use a custom org.apache.http.protocol.HttpContext when executing
      * requests.
