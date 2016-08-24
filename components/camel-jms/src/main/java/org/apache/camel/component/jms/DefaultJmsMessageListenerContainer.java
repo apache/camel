c329d35f1c2490402de86683775ab8d5f16539f2
Andrea Cosentino <ancosen@gmail.com>
2016-08-24 09:40:42 +0200

    Fix followup CAMEL-3759

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/DefaultJmsMessageListenerContainer.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/DefaultJmsMessageListenerContainer.java
@@ -73,7 +73,7 @@ public class DefaultJmsMessageListenerContainer extends DefaultMessageListenerCo
             // otherwise we only run if the endpoint is running
             boolean answer = endpoint.isRunning();
             // log at trace level as otherwise this can be noisy during normal operation
-            logger.trace("runningAllowed() -> {}", answer);
+            logger.trace("runningAllowed() -> " + answer);
             return answer;
         }
     }
