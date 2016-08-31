ebceb2ae1f2ab1423b38f9b9c8776271dddeea89
Hiram Chirino <hiram@hiramchirino.com>
2016-08-31 11:48:09 -0400

    Fix for CAMEL-10279 - Can't use @ImportResource and configure() in the same SB app.
    This assumes there is always a RouteBuilder in a SB app.

--- components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/fatjarroutertests/JUnitFatJarRouterTest.java
+++ components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/fatjarroutertests/JUnitFatJarRouterTest.java
@@ -17,7 +17,9 @@
 package org.apache.camel.spring.boot.fatjarroutertests;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.net.URL;
+import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
@@ -27,6 +29,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.SocketUtils;
+
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestFatJarRouter.class, properties = "spring.main.sources=org.apache.camel.spring.boot.fatjarroutertests")
@@ -40,10 +46,15 @@ public class JUnitFatJarRouterTest extends Assert {
     }
 
     @Test
-    public void shouldStartCamelRoute() throws InterruptedException, IOException {
+    public void shouldStartCamelRoute() throws InterruptedException, IOException, MalformedObjectNameException {
         String response = IOUtils.toString(new URL("http://localhost:" + port));
 
         assertEquals("stringBean", response);
+
+        // There should be 3 routes running..
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        Set<ObjectName> objectNames = mbs.queryNames(new ObjectName("org.apache.camel:type=routes,*"), null);
+        assertEquals(3, objectNames.size());
     }
 
 }
