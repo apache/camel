4943f46ecd57735e3ce711c948561f46d0cffd5d
lburgazzoli <lburgazzoli@gmail.com>
2016-09-02 15:37:38 +0200

    CAMEL-6399: hazelcast - route policy for having one route being master, and others as slaves

---
+++ components/camel-hazelcast/src/main/java/org/apache/camel/component/hazelcast/policy/HazelcastRoutePolicy.java
@@ -0,0 +1,304 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.hazelcast.policy;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import org.apache.camel.Exchange;
+import org.apache.camel.NonManagedService;
+import org.apache.camel.Route;
+import org.apache.camel.component.hazelcast.HazelcastUtil;
+import org.apache.camel.support.RoutePolicySupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HazelcastRoutePolicy extends RoutePolicySupport implements NonManagedService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HazelcastRoutePolicy.class);
+
+    private final Object lock;
+    private final boolean managedInstance;
+    private final AtomicBoolean leader;
+    private final Set<Route> suspendedRoutes;
+    private final ExecutorService executorService;
+
+    private long tryLockTimeout;
+    private TimeUnit tryLockTimeoutUnit;
+    private HazelcastInstance instance;
+    private String lockMapName;
+    private String lockKey;
+    private String lockValue;
+    private boolean shouldStopConsumer;
+    private IMap<String, String> locks;
+    private volatile Future<Void> future;
+
+    public HazelcastRoutePolicy() {
+        this(HazelcastUtil.newInstance(), true);
+    }
+
+    public HazelcastRoutePolicy(HazelcastInstance instance) {
+        this(instance, false);
+    }
+
+    public HazelcastRoutePolicy(HazelcastInstance instance, boolean managedInstance) {
+        this.instance = instance;
+        this.managedInstance = managedInstance;
+        this.suspendedRoutes =  new HashSet<>();
+        this.leader = new AtomicBoolean(false);
+        this.lock = new Object();
+        this.shouldStopConsumer = true;
+        this.lockMapName = null;
+        this.lockKey = null;
+        this.lockValue = null;
+        this.locks = null;
+        this.future = null;
+        this.tryLockTimeout = Long.MAX_VALUE;
+        this.tryLockTimeoutUnit = TimeUnit.MILLISECONDS;
+
+        this.executorService =  Executors.newSingleThreadExecutor(r -> {
+            Thread thread = new Thread(r, "Camel RoutePolicy");
+            thread.setDaemon(true);
+            return thread;
+        });
+    }
+
+    @Override
+    public void onExchangeBegin(Route route, Exchange exchange)  {
+        if (leader.get()) {
+            if (shouldStopConsumer) {
+                startConsumer(route);
+            }
+        } else {
+            if (shouldStopConsumer) {
+                stopConsumer(route);
+            }
+
+            exchange.setException(new IllegalStateException(
+                "Hazelcast based route policy prohibits processing exchanges, stopping route and failing the exchange")
+            );
+        }
+    }
+
+    @Override
+    public void onStop(Route route) {
+        synchronized (lock) {
+            suspendedRoutes.remove(route);
+        }
+    }
+
+    @Override
+    public synchronized void onSuspend(Route route) {
+        synchronized (lock) {
+            suspendedRoutes.remove(route);
+        }
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        locks = instance.getMap(lockMapName);
+        future = executorService.submit(this::acquireLeadership);
+
+        super.doStart();
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        if (future != null) {
+            future.cancel(true);
+            future = null;
+        }
+
+        if (managedInstance) {
+            instance.shutdown();
+        }
+
+        super.doStop();
+    }
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    protected void setLeader(boolean isLeader) {
+        if (isLeader && leader.compareAndSet(false, isLeader)) {
+            LOGGER.info("Leadership taken (map={}, key={}, val={})",
+                lockMapName,
+                lockKey,
+                lockValue);
+
+            startAllStoppedConsumers();
+        } else {
+            if (!leader.getAndSet(isLeader) && isLeader) {
+                LOGGER.info("Leadership lost (map={}, key={} val={})",
+                    lockMapName,
+                    lockKey,
+                    lockValue);
+            }
+        }
+    }
+
+    private void startConsumer(Route route) {
+        synchronized (lock) {
+            try {
+                if (suspendedRoutes.contains(route)) {
+                    startConsumer(route.getConsumer());
+                    suspendedRoutes.remove(route);
+                }
+            } catch (Exception e) {
+                handleException(e);
+            }
+        }
+    }
+
+    private void stopConsumer(Route route) {
+        synchronized (lock) {
+            try {
+                if (!suspendedRoutes.contains(route)) {
+                    LOGGER.debug("Stopping consumer for {} ({})", route.getId(), route.getConsumer());
+                    stopConsumer(route.getConsumer());
+                    suspendedRoutes.add(route);
+                }
+            } catch (Exception e) {
+                handleException(e);
+            }
+        }
+    }
+
+    private void startAllStoppedConsumers() {
+        synchronized (lock) {
+            try {
+                for (Route route : suspendedRoutes) {
+                    LOGGER.debug("Starting consumer for {} ({})", route.getId(), route.getConsumer());
+                    startConsumer(route.getConsumer());
+                }
+
+                suspendedRoutes.clear();
+            } catch (Exception e) {
+                handleException(e);
+            }
+        }
+    }
+
+    // *************************************************************************
+    // Getter/Setters
+    // *************************************************************************
+
+    public String getLockMapName() {
+        return lockMapName;
+    }
+
+    public void setLockMapName(String lockMapName) {
+        this.lockMapName = lockMapName;
+    }
+
+    public boolean isShouldStopConsumer() {
+        return shouldStopConsumer;
+    }
+
+    public void setShouldStopConsumer(boolean shouldStopConsumer) {
+        this.shouldStopConsumer = shouldStopConsumer;
+    }
+
+    public String getLockKey() {
+        return lockKey;
+    }
+
+    public void setLockKey(String lockKey) {
+        this.lockKey = lockKey;
+    }
+
+    public String getLockValue() {
+        return lockValue;
+    }
+
+    public void setLockValue(String lockValue) {
+        this.lockValue = lockValue;
+    }
+
+    public long getTryLockTimeout() {
+        return tryLockTimeout;
+    }
+
+    public void setTryLockTimeout(long tryLockTimeout) {
+        this.tryLockTimeout = tryLockTimeout;
+    }
+
+    public void setTryLockTimeout(long tryLockTimeout, TimeUnit tryLockTimeoutUnit) {
+        this.tryLockTimeout = tryLockTimeout;
+        this.tryLockTimeoutUnit = tryLockTimeoutUnit;
+    }
+
+    public TimeUnit getTryLockTimeoutUnit() {
+        return tryLockTimeoutUnit;
+    }
+
+    public void setTryLockTimeoutUnit(TimeUnit tryLockTimeoutUnit) {
+        this.tryLockTimeoutUnit = tryLockTimeoutUnit;
+    }
+
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    private Void acquireLeadership() throws Exception {
+        boolean locked = false;
+        while (isRunAllowed()) {
+            try {
+                locked = locks.tryLock(lockKey, tryLockTimeout, tryLockTimeoutUnit);
+                if (locked) {
+                    locks.put(lockKey, lockValue);
+                    setLeader(true);
+
+                    // Wait almost forever
+                    Thread.sleep(Long.MAX_VALUE);
+                } else {
+                    LOGGER.debug("Failed to acquire lock (map={}, key={}, val={}) after {} {}",
+                        lockMapName,
+                        lockKey,
+                        lockValue,
+                        tryLockTimeout,
+                        tryLockTimeoutUnit.name()
+                    );
+                }
+            } catch (InterruptedException e) {
+                if (isRunAllowed()) {
+                    LOGGER.warn("Interrupted Exception caught", e);
+                } else {
+                    LOGGER.debug("Interrupted Exception caught", e);
+                }
+            } catch (Exception e) {
+                LOGGER.warn("Exception caught", e);
+            } finally {
+                if (locked) {
+                    locks.remove(lockKey);
+                    locks.unlock(lockKey);
+                    locked = false;
+                }
+
+                setLeader(false);
+            }
+        }
+
+        return null;
+    }
+}
