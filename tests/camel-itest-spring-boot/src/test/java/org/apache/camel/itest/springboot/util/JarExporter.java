7f351389f96ecccc2d424964cd4506ff776b1f6e
Nicola Ferraro <ni.ferraro@gmail.com>
2016-09-02 13:43:38 +0200

    CAMEL-10222: Fixed resolution of wrong versions

--- tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/util/JarExporter.java
+++ tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/util/JarExporter.java
@@ -32,7 +32,7 @@ public class JarExporter {
     public void exportJar() throws Exception {
 
         Archive<?> archive = ArquillianPackager.springBootPackage(new ITestConfigBuilder()
-                .module("camel-jetty9")
+                .module("camel-hbase")
                 .build());
 
         new ZipExporterImpl(archive).exportTo(new File("target/export.zip"), true);
