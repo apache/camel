1cc13fc6eecf1b795ffb37493afaa36d0f874106
Maxim Pogorelov <de.pensiero@gmail.com>
2016-09-05 17:35:22 +0300

    Update MySpringBootRouterTest.java

--- examples/camel-example-spring-boot/src/test/java/org/apache/camel/example/spring/boot/MySpringBootRouterTest.java
+++ examples/camel-example-spring-boot/src/test/java/org/apache/camel/example/spring/boot/MySpringBootRouterTest.java
@@ -25,12 +25,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MySpringBootRouter.class)
-@WebIntegrationTest(randomPort = true)
+@SpringBootTest(classes = MySpringBootRouter.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MySpringBootRouterTest extends Assert {
 
     @Autowired
