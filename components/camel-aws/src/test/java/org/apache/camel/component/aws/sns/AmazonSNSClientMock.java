983e53b14f90ad3130f262789524fc4f1a75ac7c
Andrea Cosentino <ancosen@gmail.com>
2016-09-05 10:34:52 +0200

    CAMEL-10280 Don't require create privileges to publish to named SNS topic

--- components/camel-aws/src/test/java/org/apache/camel/component/aws/sns/AmazonSNSClientMock.java
+++ components/camel-aws/src/test/java/org/apache/camel/component/aws/sns/AmazonSNSClientMock.java
@@ -46,8 +46,12 @@ import com.amazonaws.services.sns.model.SetTopicAttributesRequest;
 import com.amazonaws.services.sns.model.SetTopicAttributesResult;
 import com.amazonaws.services.sns.model.SubscribeRequest;
 import com.amazonaws.services.sns.model.SubscribeResult;
+import com.amazonaws.services.sns.model.Topic;
 import com.amazonaws.services.sns.model.UnsubscribeRequest;
 import com.amazonaws.services.sns.model.UnsubscribeResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 
@@ -149,6 +153,17 @@ public class AmazonSNSClientMock extends AmazonSNSClient {
     @Override
     public ListTopicsResult listTopics() throws AmazonServiceException, AmazonClientException {
         throw new UnsupportedOperationException();
+    }
+    
+    @Override
+    public ListTopicsResult listTopics(String nextToken) {
+        ListTopicsResult res = new ListTopicsResult();
+        Topic topic = new Topic();
+        topic.setTopicArn(DEFAULT_TOPIC_ARN);
+        List<Topic> list = new ArrayList<Topic>();
+        list.add(topic);
+        res.setTopics(list);
+        return res;
     }
 
     @Override
