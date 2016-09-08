7d0db4d06d4eb1bdc465c760b23463ed9889a1e4
lburgazzoli <lburgazzoli@gmail.com>
2016-09-08 13:16:52 +0200

    CAMEL-10140: Java 8 type safe process DSL

--- examples/camel-example-dsl-java8/src/main/java/org/apache/camel/example/dsl/java8/MyApplication.java
+++ examples/camel-example-dsl-java8/src/main/java/org/apache/camel/example/dsl/java8/MyApplication.java
@@ -16,20 +16,65 @@
  */
 package org.apache.camel.example.dsl.java8;
 
-import org.apache.camel.spring.javaconfig.CamelConfiguration;
-import org.apache.camel.spring.javaconfig.Main;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import java.util.Date;
+import java.util.Objects;
 
-@Configuration
-@ComponentScan
-public class MyApplication extends CamelConfiguration {
-    /**
-     * Allow this route to be run as an application
-     */
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.main.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class MyApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyApplication.class);
+
+    private MyApplication() {
+    }
+
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-        main.setConfigClass(MyApplication.class);
-        main.run();
+        main.addRouteBuilder(new MyRouteBuilder());
+        main.run(args);
+    }
+
+    private static class MyRouteBuilder extends RouteBuilder {
+        @Override
+        public void configure() throws Exception {
+            from("timer:simple?period=503")
+                .id("simple-route")
+                .transform()
+                    .exchange(this::dateToTime)
+                .process()
+                    .message(this::log)
+                .process()
+                    .body(this::log)
+                .choice()
+                    .when()
+                        .body(Integer.class, b -> (b & 1) == 0)
+                        .log("Received even number")
+                    .when()
+                        .body(Integer.class, (b, h) -> h.containsKey("skip") ? false : (b & 1) == 0)
+                        .log("Received odd number")
+                    .when()
+                        .body(Objects::isNull)
+                        .log("Received null body")
+                    .when()
+                        .body(Integer.class, b -> (b & 1) != 0)
+                        .log("Received odd number")
+                .endChoice();
+        }
+
+        private Long dateToTime(Exchange e) {
+            return e.getProperty(Exchange.TIMER_FIRED_TIME, Date.class).getTime();
+        }
+
+        private void log(Object b) {
+            LOGGER.info("body is: {}", b);
+        }
+
+        private void log(Message m) {
+            LOGGER.info("message is: {}", m);
+        }
     }
 }
