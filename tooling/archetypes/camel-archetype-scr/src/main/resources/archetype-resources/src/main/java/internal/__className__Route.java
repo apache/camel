281bac41e9de4fdc8a7bbee9c6d9d5fe8bebbaba
Jyrki Ruuskanen <yuruki@kotikone.fi>
2016-09-27 12:32:23 +0300

    CAMEL-10347: camel-scr todos and polish

--- tooling/archetypes/camel-archetype-scr/src/main/resources/archetype-resources/src/main/java/internal/__className__Route.java
+++ tooling/archetypes/camel-archetype-scr/src/main/resources/archetype-resources/src/main/java/internal/__className__Route.java
@@ -20,6 +20,8 @@
 package ${package}.internal;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.SimpleRegistry;
+import org.apache.camel.scr.AbstractCamelRunner;
 
 public class ${className}Route extends RouteBuilder {
 
@@ -28,6 +30,9 @@ public class ${className}Route extends RouteBuilder {
 
     @Override
 	public void configure() throws Exception {
+        // Add a bean to Camel context registry
+        AbstractCamelRunner.getRegistry(getContext(), SimpleRegistry.class).put("testString", "this is a test");
+
         from("{{from}}").routeId(camelRouteId)
             .to("{{to}}");
 	}
