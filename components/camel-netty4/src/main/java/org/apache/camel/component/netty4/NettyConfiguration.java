eb1f6982c3e7acecbf087d956118e864c5b9db0a
Dhiraj Bokde <dhirajsb@yahoo.com>
2016-09-29 17:38:19 -0700

    Fixed CS errors

--- components/camel-netty4/src/main/java/org/apache/camel/component/netty4/NettyConfiguration.java
+++ components/camel-netty4/src/main/java/org/apache/camel/component/netty4/NettyConfiguration.java
@@ -429,7 +429,7 @@ public class NettyConfiguration extends NettyServerBootstrapConfiguration implem
     }
 
     public boolean isAllowSerializedHeaders() {
-    	return allowSerializedHeaders;
+        return allowSerializedHeaders;
     }
     
     /**
