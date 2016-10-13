9ae7ebea7d9f6c0723b9d44057b937bf83b65e16
dmitriy <xhub4u@gmail.com>
2016-10-13 09:44:17 -0400

    CAMEL-10385: fix checkstyle errors

--- components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/issues/SimpleOgnlTest.java
+++ components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/issues/SimpleOgnlTest.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.spring.boot.issues;
 
 import java.util.ArrayList;
@@ -18,36 +34,36 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration
-@SpringBootTest(classes = {SimpleOgnlTest.class})
+@SpringBootTest(classes = { SimpleOgnlTest.class })
 public class SimpleOgnlTest {
 	@EndpointInject(uri = "mock:result")
-    protected MockEndpoint resultEndpoint;
- 
-    @Produce(uri = "direct:start")
-    protected ProducerTemplate template;
-    
+	protected MockEndpoint resultEndpoint;
+
+	@Produce(uri = "direct:start")
+	protected ProducerTemplate template;
+
 	@Test
-    public void testSimpleOgnlListExpression() throws Exception {
+	public void testSimpleOgnlListExpression() throws Exception {
 		List<String> list = new ArrayList<String>();
 		list.add("one");
 		list.add("two");
- 
-        resultEndpoint.expectedBodiesReceived(list.get(0));
- 
-        template.sendBody(list);
- 
-        resultEndpoint.assertIsSatisfied();
-    }
-	
+
+		resultEndpoint.expectedBodiesReceived(list.get(0));
+
+		template.sendBody(list);
+
+		resultEndpoint.assertIsSatisfied();
+	}
+
 	@Configuration
-    public static class ContextConfig {
-        @Bean
-        public RouteBuilder route() {
-            return new RouteBuilder() {
-                public void configure() {
-                    from("direct:start").setBody(simple("${body[0]}")).to("mock:result");
-                }
-            };
-        }
-    }
+	public static class ContextConfig {
+		@Bean
+		public RouteBuilder route() {
+			return new RouteBuilder() {
+				public void configure() {
+					from("direct:start").setBody(simple("${body[0]}")).to("mock:result");
+				}
+			};
+		}
+	}
 }
