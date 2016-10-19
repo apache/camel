fd55146af4986fe77cebba753562dad40caf272a
James Netherton <jamesnetherton@gmail.com>
2016-10-19 09:34:20 +0100

    CAMEL-10400: Upgrade easymock to version 3.4

--- components/camel-smpp/src/test/java/org/apache/camel/component/smpp/SmppReplaceSmCommandTest.java
+++ components/camel-smpp/src/test/java/org/apache/camel/component/smpp/SmppReplaceSmCommandTest.java
@@ -37,9 +37,9 @@ import org.junit.Test;
 import static org.easymock.EasyMock.aryEq;
 import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.isNull;
-import static org.easymock.classextension.EasyMock.createMock;
-import static org.easymock.classextension.EasyMock.replay;
-import static org.easymock.classextension.EasyMock.verify;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
 
 public class SmppReplaceSmCommandTest {
