2d99f74d888cfaae8304dcb4f6d4bacc89765683
Andrea Cosentino <ancosen@gmail.com>
2016-10-22 12:27:42 +0200

    CAMEL-10408: Camel-JMS: Suspending/resuming won't work in case of timeout

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsConsumer.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsConsumer.java
@@ -185,6 +185,7 @@ public class JmsConsumer extends DefaultConsumer implements SuspendableService {
         // then we will use updated configuration from jms endpoint that may have been managed using JMX
         listenerContainer = null;
         messageListener = null;
+        initialized = false;
 
         // shutdown thread pool if listener container was using a private thread pool
         if (shutdownExecutorService && executorService != null) {
