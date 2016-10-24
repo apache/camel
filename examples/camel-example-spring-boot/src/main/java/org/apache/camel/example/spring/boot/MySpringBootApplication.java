c28ebae32d1961a491970d3624e782a48c8c49ea
Darius <dariuscooper@gmail.com>
2016-10-24 15:10:33 -0400

    Changed from FatJarRoute, to newer approach [CAMEL-10404]

--- examples/camel-example-spring-boot/src/main/java/org/apache/camel/example/spring/boot/MySpringBootApplication.java
+++ examples/camel-example-spring-boot/src/main/java/org/apache/camel/example/spring/boot/MySpringBootApplication.java
@@ -16,32 +16,17 @@
  */
 package org.apache.camel.example.spring.boot;
 
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.HealthEndpoint;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Component
-public class MySpringBootRouter extends RouteBuilder {
+@SpringBootApplication
+public class MySpringBootApplication {
 
-    @Autowired
-    private HealthEndpoint health;
-
-    @Override
-    public void configure() {
-        from("timer:trigger")
-                .transform().simple("ref:myBean")
-                .to("log:out");
-
-        from("timer:status")
-            .bean(health, "invoke")
-            .log("Health is ${body}");
-    }
-
-    @Bean
-    String myBean() {
-        return "I'm Spring bean!";
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
     }
 
 }
--- examples/camel-example-spring-boot/src/test/java/org/apache/camel/example/spring/boot/MySpringBootRouterTest.java
+++ examples/camel-example-spring-boot/src/test/java/org/apache/camel/example/spring/boot/MySpringBootRouterTest.java
@@ -24,11 +24,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MySpringBootRouter.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
 public class MySpringBootRouterTest extends Assert {
 
     @Autowired
