191f1b93f8851a4f97b9988e6f546a90d732a3d1
Nick Busy <nbusy@enfoll.com>
2016-11-05 16:30:22 +1100

    CAMEL-10444: return header CamelMongoDbRecordsAffected from update and remove operations

--- components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/MongoDbProducer.java
+++ components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/MongoDbProducer.java
@@ -409,6 +409,9 @@ public class MongoDbProducer extends DefaultProducer {
                 } else {
                     result = dbCol.updateMany(updateCriteria, objNew, options);
                 }
+                if (result.isModifiedCountAvailable()) {
+                    exchange1.getOut().setHeader(MongoDbConstants.RECORDS_AFFECTED, result.getModifiedCount());
+                }
                 return result;
             } catch (InvalidPayloadException e) {
                 throw new CamelMongoDbException("Invalid payload for update", e);
@@ -423,6 +426,9 @@ public class MongoDbProducer extends DefaultProducer {
                 BasicDBObject removeObj = exchange1.getIn().getMandatoryBody(BasicDBObject.class);
 
                 DeleteResult result = dbCol.deleteMany(removeObj);
+                if (result.wasAcknowledged()) {
+                    exchange1.getOut().setHeader(MongoDbConstants.RECORDS_AFFECTED, result.getDeletedCount());
+                }
                 return result;
             } catch (InvalidPayloadException e) {
                 throw new CamelMongoDbException("Invalid payload for remove", e);
