237101e76ad7750450e29fb408f35f962218c9c6
Vitalii Tymchyshyn <vit@tym.im>
2016-11-13 23:51:54 -0500

    CAMEL-10409 Double release of netty buffer

---
+++ components/camel-netty4-http/src/test/java/org/apache/camel/component/netty4/http/LogCaptureAppender.java
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------
+*
+* Copyright (c) 2003-2014, PostgreSQL Global Development Group
+* Copyright (c) 2004, Open Cloud Limited.
+*
+*
+*-------------------------------------------------------------------------
+*/
+package org.apache.camel.component.netty4.http;
+
+/**
+ * @author vit@tym.im
+ */
+public class LogCaptureAppender {
+}
