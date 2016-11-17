7bd0a7305fc0d0c70878e2dd464ec6898621780b
Edoardo Causarano <edoardo.causarano@gmail.com>
2016-11-17 14:31:01 +0100

    Various tweaks and corrections suggested by IntelliJ

--- components/camel-dropbox/src/main/java/org/apache/camel/component/dropbox/dto/DropboxSearchResult.java
+++ components/camel-dropbox/src/main/java/org/apache/camel/component/dropbox/dto/DropboxSearchResult.java
@@ -1,18 +1,18 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+  Licensed to the Apache Software Foundation (ASF) under one or more
+  contributor license agreements.  See the NOTICE file distributed with
+  this work for additional information regarding copyright ownership.
+  The ASF licenses this file to You under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with
+  the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 package org.apache.camel.component.dropbox.dto;
 
@@ -22,16 +22,13 @@ import java.util.List;
 
 import com.dropbox.core.DbxEntry;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.component.dropbox.util.DropboxResultHeader;
-
 
 public class DropboxSearchResult {
 
     private final List<DbxEntry> found;
 
     public DropboxSearchResult(List<DbxEntry> found) {
-        this.found = new ArrayList<DbxEntry>(found);
+        this.found = new ArrayList<>(found);
     }
 
     public List<DbxEntry> getFound() {
