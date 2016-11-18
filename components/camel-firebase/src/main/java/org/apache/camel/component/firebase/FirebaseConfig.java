ccf8721cf09ed9d9e99e1e6e2779a4b847846ae7
gilfernandes <gil.fernandes@gmail.com>
2016-11-18 17:59:58 +0000

    Fixed style and documentation issues. The documentation is not yet complete.

--- components/camel-firebase/src/main/java/org/apache/camel/component/firebase/FirebaseConfig.java
+++ components/camel-firebase/src/main/java/org/apache/camel/component/firebase/FirebaseConfig.java
@@ -1,7 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.firebase;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,10 +22,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
 /**
  * Contains the elements needed to connect to Firebase
  */
-public class FirebaseConfig {
+public final class FirebaseConfig {
 
     private final String databaseUrl;
 
@@ -35,7 +51,7 @@ public class FirebaseConfig {
     }
 
     public void init() throws IOException {
-        try(InputStream in = Files.newInputStream(Paths.get(serviceAccountFile))) {
+        try (InputStream in = Files.newInputStream(Paths.get(serviceAccountFile))) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setServiceAccount(in)
                     .setDatabaseUrl(this.databaseUrl)
