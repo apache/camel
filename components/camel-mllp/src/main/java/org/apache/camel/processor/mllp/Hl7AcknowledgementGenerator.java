8b399ce5f2096e081798466eb11152cd07d1786b
Quinn Stevenson <quinn@pronoia-solutions.com>
2016-11-22 12:04:04 -0700

    CAMEL-10510: Change HL7AcknowledgementGenerator to set CamelMllpAcknowledgement exchange property instead of message header

--- components/camel-mllp/src/main/java/org/apache/camel/processor/mllp/Hl7AcknowledgementGenerator.java
+++ components/camel-mllp/src/main/java/org/apache/camel/processor/mllp/Hl7AcknowledgementGenerator.java
@@ -56,13 +56,13 @@ public class Hl7AcknowledgementGenerator implements Processor {
         byte[] acknowledgementBytes = null;
         if (null == exchange.getException()) {
             acknowledgementBytes = generateApplicationAcceptAcknowledgementMessage(hl7Bytes);
-            message.setHeader(MLLP_ACKNOWLEDGEMENT_TYPE, "AA");
+            exchange.setProperty(MLLP_ACKNOWLEDGEMENT_TYPE, "AA");
         } else {
             acknowledgementBytes = generateApplicationErrorAcknowledgementMessage(hl7Bytes);
-            message.setHeader(MLLP_ACKNOWLEDGEMENT_TYPE, "AE");
+            exchange.setProperty(MLLP_ACKNOWLEDGEMENT_TYPE, "AE");
         }
 
-        message.setHeader(MLLP_ACKNOWLEDGEMENT, acknowledgementBytes);
+        exchange.setProperty(MLLP_ACKNOWLEDGEMENT, acknowledgementBytes);
     }
 
     public byte[] generateApplicationAcceptAcknowledgementMessage(byte[] hl7MessageBytes) throws Hl7AcknowledgementGenerationException {
@@ -109,7 +109,7 @@ public class Hl7AcknowledgementGenerator implements Processor {
 
         // Build the MSH Segment
         ByteArrayOutputStream acknowledgement = new ByteArrayOutputStream(1024);
-        acknowledgement.write(hl7MessageBytes, 0, 8); // through MSH-2 (without trailing field separator)
+        acknowledgement.write(hl7MessageBytes, 0, fieldSeparatorIndexes.get(1)); // through MSH-2 (without trailing field separator)
         acknowledgement.write(hl7MessageBytes, fieldSeparatorIndexes.get(3), fieldSeparatorIndexes.get(4) - fieldSeparatorIndexes.get(3)); // MSH-5
         acknowledgement.write(hl7MessageBytes, fieldSeparatorIndexes.get(4), fieldSeparatorIndexes.get(5) - fieldSeparatorIndexes.get(4)); // MSH-6
         acknowledgement.write(hl7MessageBytes, fieldSeparatorIndexes.get(1), fieldSeparatorIndexes.get(2) - fieldSeparatorIndexes.get(1)); // MSH-3
