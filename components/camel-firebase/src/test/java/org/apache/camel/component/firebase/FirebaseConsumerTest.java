25dbd0d8ce0ba5d57cb2fc350c8a0b125b7f0b72
Andrea Cosentino <ancosen@gmail.com>
2016-11-25 12:50:21 +0100

    Revert "Fixed style and documentation issues. The documentation is not yet complete."
    This reverts commit cec7b30f3395529dae6dc5b5d46ff0e64cb1d209.

--- components/camel-firebase/src/test/java/org/apache/camel/component/firebase/FirebaseConsumerTest.java
+++ components/camel-firebase/src/test/java/org/apache/camel/component/firebase/FirebaseConsumerTest.java
@@ -1,27 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.camel.component.firebase;
-
-
-import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 import com.google.firebase.database.FirebaseDatabase;
 import org.apache.camel.builder.RouteBuilder;
@@ -30,17 +7,19 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Writes a dummy message and then checks, if the consumer receives at least one message.
  */
 public class FirebaseConsumerTest extends CamelTestSupport {
 
-    private final ReentrantLock lock = new ReentrantLock();
-
-    private final Condition wake = lock.newCondition();
-
     @Test
-    public void whenFirebaseListenerShouldReceiveMessages() throws Exception {
+    public void whenFirebaseListener_ShouldReceiveMessages() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMinimumMessageCount(1);
         assertMockEndpointsSatisfied();
@@ -68,6 +47,10 @@ public class FirebaseConsumerTest extends CamelTestSupport {
         };
     }
 
+    private final ReentrantLock lock = new ReentrantLock();
+
+    private final Condition wake = lock.newCondition();
+
     private void insertDummyData(String databaseUrl, String originalRootReference, String serviceAccountFile) throws IOException, InterruptedException {
         FirebaseConfig config = new FirebaseConfig.Builder(databaseUrl, originalRootReference, URLDecoder.decode(serviceAccountFile, "UTF-8"))
                 .build();
@@ -78,14 +61,16 @@ public class FirebaseConsumerTest extends CamelTestSupport {
                     try {
                         lock.lock();
                         wake.signal();
-                    } finally {
+                    }
+                    finally {
                         lock.unlock();
                     }
                 });
         try {
             lock.lock();
             wake.await();
-        } finally {
+        }
+        finally {
             lock.unlock();
         }
     }
