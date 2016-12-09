e497455d5a6efb5f43b9bad9bf912ddc448d9c3e
Zoran Regvart <zoran@regvart.com>
2016-12-09 12:13:46 +0100

    CAMEL-10593 Support for Composite API batch
    This commit implements support for Salesforce Composite Batch API[1]
    that allows the user to combine up to 25 requests in a single batch and
    then send them in a single HTTP request saving on the request round trip
    time and bandwidth.
    
    One would use this operation like this:
    
    //Create the batch request:
    final SObjectBatch batch = new SObjectBatch("38.0");
    
    final Account updates = new Account();
    updates.set...
    
    //Use the builder methods to add up to 25 operations
    batch.addUpdate("Account", accountId, updates)
         .addGet("Account", "001D000000K0fXOIAZ")
         .add...
    
    final SObjectBatchResponse response =
    template.requestBody("salesforce:composite-batch?format=JSON", batch,
    SObjectBatchResponse.class);
    
    [1]
    https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_composite_batch.htm

---
+++ components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/api/utils/VersionTest.java
@@ -0,0 +1,76 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.salesforce.api.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class VersionTest {
+
+    private static final Version V34_0 = Version.create("34.0");
+
+    private static final Version V34_3 = Version.create("34.3");
+
+    private static final Version V35_0 = Version.create("35.0");
+
+    @Test
+    public void shouldCreate() {
+        final Version version = V34_3;
+
+        assertEquals(34, version.getMajor());
+        assertEquals(3, version.getMinor());
+    }
+
+    @Test
+    public void shouldObserveApiLimits() {
+        V34_0.requireAtLeast(34, 0);
+        V34_0.requireAtLeast(33, 9);
+        V35_0.requireAtLeast(34, 0);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldObserveApiLimitsOnMajorVersions() {
+        V35_0.requireAtLeast(36, 0);
+
+        fail("No UnsupportedOperationException thrown, but expected");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldObserveApiLimitsOnMinorVersions() {
+        V35_0.requireAtLeast(35, 1);
+
+        fail("No UnsupportedOperationException thrown, but expected");
+    }
+
+    @Test
+    public void testComparator() {
+        assertTrue(V34_0.compareTo(V34_3) < 0);
+        assertTrue(V34_0.compareTo(V35_0) < 0);
+        assertTrue(V34_3.compareTo(V35_0) < 0);
+
+        assertTrue(V34_3.compareTo(V34_0) > 0);
+        assertTrue(V35_0.compareTo(V34_0) > 0);
+        assertTrue(V35_0.compareTo(V34_3) > 0);
+
+        assertTrue(V34_0.compareTo(V34_0) == 0);
+        assertTrue(V34_3.compareTo(V34_3) == 0);
+        assertTrue(V35_0.compareTo(V35_0) == 0);
+    }
+}
