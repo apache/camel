5f2b57777f9ad829f1440dc3cf8757fb62981e80
Claus Ibsen <davsclaus@apache.org>
2016-12-21 22:03:34 +0100

    CAMEL-10636: Component options docs - Include more details like endpoint options.

--- camel-core/src/test/java/org/apache/camel/management/ManagedCamelContextTest.java
+++ camel-core/src/test/java/org/apache/camel/management/ManagedCamelContextTest.java
@@ -390,9 +390,10 @@ public class ManagedCamelContextTest extends ManagementTestSupport {
         assertNotNull(json);
 
         assertTrue(json.contains("\"label\": \"core,endpoint\""));
-        assertTrue(json.contains("\"defaultQueueFactory\": { \"kind\": \"property\", \"type\": \"object\", \"javaType\":"
-            + " \"org.apache.camel.component.seda.BlockingQueueFactory<org.apache.camel.Exchange>\","));
-        assertTrue(json.contains("\"queueSize\": { \"kind\": \"property\", \"type\": \"integer\", \"javaType\": \"int\", \"deprecated\": \"false\", \"secret\": \"false\", \"value\": \"0\""));
+        assertTrue(json.contains("\"defaultQueueFactory\": { \"kind\": \"property\", \"label\": \"advanced\", \"required\": \"false\","
+                        + " \"type\": \"object\", \"javaType\": \"org.apache.camel.component.seda.BlockingQueueFactory<org.apache.camel.Exchange>\","));
+        assertTrue(json.contains("\"queueSize\": { \"kind\": \"property\", \"label\": \"advanced\", \"required\": \"false\","
+                + " \"type\": \"integer\", \"javaType\": \"int\", \"deprecated\": \"false\", \"secret\": \"false\", \"value\": \"0\""));
     }
 
     @Override
