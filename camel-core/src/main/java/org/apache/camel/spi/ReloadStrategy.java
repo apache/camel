bf00386ecb1f4a84430745b90123789173254e62
Claus Ibsen <davsclaus@apache.org>
2016-12-22 09:50:40 +0100

    CAMEL-10599: Add ReloadStrategy to allow watching for file changes and reload routes on the fly.

--- camel-core/src/main/java/org/apache/camel/spi/ReloadStrategy.java
+++ camel-core/src/main/java/org/apache/camel/spi/ReloadStrategy.java
@@ -29,7 +29,7 @@ import org.apache.camel.StaticService;
 public interface ReloadStrategy extends Service, StaticService, CamelContextAware {
 
     /**
-     * A reload is triggered when a XML resource is changed which contains Camel routes or rest services
+     * A reload is triggered when a XML resource is changed which contains Camel routes.
      *
      * @param camelContext  the running CamelContext
      * @param name          name of resource such as a file name (can be null)
