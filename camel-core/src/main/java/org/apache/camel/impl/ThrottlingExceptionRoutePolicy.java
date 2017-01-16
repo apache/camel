72438fd8140b531fb633e9d22760fffd8bcb59bb
CodeSmell <mbarlotta@gmail.com>
2017-01-16 10:24:22 -0500

    Add Apache License to classes

--- camel-core/src/main/java/org/apache/camel/impl/ThrottlingExceptionRoutePolicy.java
+++ camel-core/src/main/java/org/apache/camel/impl/ThrottlingExceptionRoutePolicy.java
@@ -1,15 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.impl;
-
-import org.apache.camel.CamelContext;
-import org.apache.camel.CamelContextAware;
-import org.apache.camel.Exchange;
-import org.apache.camel.Route;
-import org.apache.camel.impl.ThrottlingInflightRoutePolicy;
-import org.apache.camel.processor.loadbalancer.CircuitBreakerLoadBalancer;
-import org.apache.camel.spi.RoutePolicy;
-import org.apache.camel.support.RoutePolicySupport;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Timer;
@@ -17,6 +22,16 @@ import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.CamelContextAware;
+import org.apache.camel.Exchange;
+import org.apache.camel.Route;
+import org.apache.camel.processor.loadbalancer.CircuitBreakerLoadBalancer;
+import org.apache.camel.spi.RoutePolicy;
+import org.apache.camel.support.RoutePolicySupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * modeled after the {@link CircuitBreakerLoadBalancer} and {@link ThrottlingInflightRoutePolicy}
@@ -53,7 +68,7 @@ public class ThrottlingExceptionRoutePolicy extends RoutePolicySupport implement
     // handler for half open circuit
     // can be used instead of resuming route
     // to check on resources
-    ThrottingExceptionHalfOpenHandler halfOpenHandler;
+    private ThrottingExceptionHalfOpenHandler halfOpenHandler;
 
     // stateful information
     private Timer halfOpenTimer;
@@ -266,7 +281,8 @@ public class ThrottlingExceptionRoutePolicy extends RoutePolicySupport implement
     
     class HalfOpenTask extends TimerTask {
         private final Route route;
-        public HalfOpenTask(Route route) {
+        
+        HalfOpenTask(Route route) {
             this.route = route;
         }
         
