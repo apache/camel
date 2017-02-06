d945b860ee151d1d5a123e4ce4326c6d96a6708d
Jaira abderrahim <abderrahim.jaira.externe@capgemini.com>
2017-02-06 15:35:36 +0100

    Authentication : provide a way to use refresh_token mode in addition to password method

--- components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/LoginConfigHelper.java
+++ components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/LoginConfigHelper.java
@@ -34,28 +34,44 @@ public class LoginConfigHelper extends Assert {
         Properties properties = new Properties();
         InputStream stream = null;
         try {
+            final SalesforceLoginConfig config;
             stream = new FileInputStream(TEST_LOGIN_PROPERTIES);
             properties.load(stream);
 
-            final SalesforceLoginConfig config = new SalesforceLoginConfig(
-                properties.getProperty("loginUrl", SalesforceLoginConfig.DEFAULT_LOGIN_URL),
-                properties.getProperty("clientId"),
-                properties.getProperty("clientSecret"),
-                properties.getProperty("userName"),
-                properties.getProperty("password"),
-                Boolean.parseBoolean(properties.getProperty("lazyLogin", "false")));
+            if (properties.getProperty("refreshToken")== null) {
+                config = new SalesforceLoginConfig(
+                        properties.getProperty("loginUrl", SalesforceLoginConfig.DEFAULT_LOGIN_URL),
+                        properties.getProperty("clientId"),
+                        properties.getProperty("clientSecret"),
+                        properties.getProperty("userName"),
+                        properties.getProperty("password"),
+                        Boolean.parseBoolean(properties.getProperty("lazyLogin", "false")));
+            } else {
+                config = new SalesforceLoginConfig(
+                        properties.getProperty("loginUrl", SalesforceLoginConfig.DEFAULT_LOGIN_URL),
+                        properties.getProperty("clientId"), //
+                        properties.getProperty("clientSecret"), //
+                        properties.getProperty("refreshToken"), //
+                        Boolean.parseBoolean(properties.getProperty("lazyLogin", "false")));
+            }
+
 
             assertNotNull("Null loginUrl", config.getLoginUrl());
             assertNotNull("Null clientId", config.getClientId());
             assertNotNull("Null clientSecret", config.getClientSecret());
-            assertNotNull("Null userName", config.getUserName());
-            assertNotNull("Null password", config.getPassword());
+            if (properties.getProperty("refreshToken")== null) {
+                assertNotNull("Null userName", config.getUserName());
+                assertNotNull("Null password", config.getPassword());
+            } else {
+                assertNotNull("Null refreshToken", config.getRefreshToken());
+            }
+
 
             return config;
 
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("Create a properties file named "
-                + TEST_LOGIN_PROPERTIES + " with clientId, clientSecret, userName, and password"
+                + TEST_LOGIN_PROPERTIES + " with clientId, clientSecret, userName, and password or with clientId, clientSecret and refreshToken"
                 + " for a Salesforce account with Merchandise and Invoice objects from Salesforce Guides.");
         } finally {
             if (stream != null) {
