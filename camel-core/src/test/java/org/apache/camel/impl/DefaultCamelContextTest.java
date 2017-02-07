14eaca7b809c5baa3137866f41cfe33242a1fbbe
Claus Ibsen <davsclaus@apache.org>
2017-02-07 13:27:05 +0100

    CAMEL-10797: Create endpoint from uri without context-path

--- camel-core/src/test/java/org/apache/camel/impl/DefaultCamelContextTest.java
+++ camel-core/src/test/java/org/apache/camel/impl/DefaultCamelContextTest.java
@@ -111,6 +111,13 @@ public class DefaultCamelContextTest extends TestSupport {
         }
     }
     
+    public void testGetEndpointNoScheme() throws Exception {
+        DefaultCamelContext ctx = new DefaultCamelContext();
+        ctx.disableJMX();
+        Endpoint endpoint = ctx.getEndpoint("log");
+        assertNotNull(endpoint);
+    }
+
     public void testGetEndPointByTypeUnknown() {
         DefaultCamelContext camelContext = new DefaultCamelContext();
         try {
@@ -159,11 +166,11 @@ public class DefaultCamelContextTest extends TestSupport {
         }
     }
 
-    public void testGetEndpointNoScheme() throws Exception {
+    public void testGetEndpointUnknownComponentNoScheme() throws Exception {
         DefaultCamelContext ctx = new DefaultCamelContext();
         ctx.disableJMX();
         try {
-            CamelContextHelper.getMandatoryEndpoint(ctx, "log.foo");
+            CamelContextHelper.getMandatoryEndpoint(ctx, "unknownname");
             fail("Should have thrown a NoSuchEndpointException");
         } catch (NoSuchEndpointException e) {
             // expected
