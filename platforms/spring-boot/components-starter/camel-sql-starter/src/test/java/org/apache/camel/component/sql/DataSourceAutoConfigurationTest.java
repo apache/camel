0548f87de23a38b31620b439ab2530a7bc2138f6
Nicola Ferraro <ni.ferraro@gmail.com>
2017-02-13 17:32:07 +0100

    CAMEL-10618: documentation

--- platforms/spring-boot/components-starter/camel-sql-starter/src/test/java/org/apache/camel/component/sql/DataSourceAutoConfigurationTest.java
+++ platforms/spring-boot/components-starter/camel-sql-starter/src/test/java/org/apache/camel/component/sql/DataSourceAutoConfigurationTest.java
@@ -1,8 +1,26 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.sql;
 
 import javax.sql.DataSource;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.component.sql.stored.SqlStoredComponent;
+import org.apache.camel.component.sql.stored.SqlStoredEndpoint;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,9 +57,16 @@ public class DataSourceAutoConfigurationTest {
     }
 
     @Test
-    public void testCamelUsesTheConfiguredDatasource() throws Exception {
+    public void testSqlComponentUsesTheConfiguredDatasource() throws Exception {
         SqlComponent component = (SqlComponent) context.getComponent("sql");
         SqlEndpoint endpoint = (SqlEndpoint) component.createEndpoint("sql:select * from table where id=#");
+        assertEquals(datasource, endpoint.getJdbcTemplate().getDataSource());
+    }
+
+    @Test
+    public void testSqlStoredComponentUsesTheConfiguredDatasource() throws Exception {
+        SqlStoredComponent component = (SqlStoredComponent) context.getComponent("sql-stored");
+        SqlStoredEndpoint endpoint = (SqlStoredEndpoint) component.createEndpoint("sql:select * from table where id=#");
         assertEquals(datasource, endpoint.getJdbcTemplate().getDataSource());
     }
 
