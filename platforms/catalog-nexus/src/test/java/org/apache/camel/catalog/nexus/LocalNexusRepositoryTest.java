fbbe4b4cc4cce218c86c9a2c5c067c3a6584cf70
Claus Ibsen <davsclaus@apache.org>
2017-02-14 15:44:35 +0100

    CAMEL-10828: camel-catalog-nexus - Initial work

---
+++ platforms/catalog-nexus/src/test/java/org/apache/camel/catalog/nexus/LocalNexusRepositoryTest.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.catalog.nexus;
+
+import junit.framework.TestCase;
+import org.apache.camel.catalog.CamelCatalog;
+import org.apache.camel.catalog.DefaultCamelCatalog;
+import org.junit.Test;
+
+public class LocalNexusRepositoryTest extends TestCase {
+
+    private LocalFileNexusRepository repo = new LocalFileNexusRepository();
+    private CamelCatalog catalog = new DefaultCamelCatalog();
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        repo.setCamelCatalog(catalog);
+        repo.setInitialDelay(1);
+        repo.setNexusUrl("dummy");
+    }
+
+    @Test
+    public void testLocalNexus() throws Exception {
+        int before = catalog.findComponentNames().size();
+
+        repo.start();
+
+        // TODO: wait 5 sec
+        Thread.sleep(5000);
+
+        repo.stop();
+
+        int after = catalog.findComponentNames().size();
+
+        assertTrue("There should be 1 component found", after - before == 1);
+    }
+}
