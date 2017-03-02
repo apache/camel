6db129a9447d7c00943bc62a3bebbd4119048cd4
Nicola Ferraro <ni.ferraro@gmail.com>
2017-03-02 13:51:00 +0100

    Moving test to a different package to use auto-configuration

--- platforms/spring-boot/components-starter/camel-servlet-starter/src/test/java/org/apache/camel/component/servlet/springboot/test/ServletMappingDisablingTest.java
+++ platforms/spring-boot/components-starter/camel-servlet-starter/src/test/java/org/apache/camel/component/servlet/springboot/test/ServletMappingDisablingTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.servlet.springboot;
+package org.apache.camel.component.servlet.springboot.test;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
