bb45ed120e5f9eb72265d1a1d2df1e0fcdea6a61
Jakub Korab <jakub.korab@gmail.com>
2017-03-03 11:50:22 +0000

    Fixed checkstyle errors. Added ability to set max cache size for the idempotent repository.

--- components/camel-kafka/src/test/java/org/apache/camel/processor/idempotent/kafka/KafkaIdempotentRepositoryLazyTest.java
+++ components/camel-kafka/src/test/java/org/apache/camel/processor/idempotent/kafka/KafkaIdempotentRepositoryLazyTest.java
@@ -1,13 +1,31 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.processor.idempotent.kafka;
 
-import org.apache.camel.*;
+import org.apache.camel.CamelContext;
+import org.apache.camel.CamelExecutionException;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.embedded.EmbeddedKafkaBroker;
 import org.apache.camel.component.kafka.embedded.EmbeddedZookeeper;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.SimpleRegistry;
-import org.apache.camel.spi.Synchronization;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,6 +42,12 @@ public class KafkaIdempotentRepositoryLazyTest extends CamelTestSupport {
     public EmbeddedKafkaBroker kafkaBroker = new EmbeddedKafkaBroker(0, zookeeper.getConnection());
 
     private KafkaIdempotentRepository kafkaIdempotentRepository;
+
+    @EndpointInject(uri = "mock:out")
+    private MockEndpoint mockOut;
+
+    @EndpointInject(uri = "mock:before")
+    private MockEndpoint mockBefore;
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
@@ -51,12 +75,6 @@ public class KafkaIdempotentRepositoryLazyTest extends CamelTestSupport {
         };
     }
 
-    @EndpointInject(uri = "mock:out")
-    MockEndpoint mockOut;
-
-    @EndpointInject(uri = "mock:before")
-    MockEndpoint mockBefore;
-
     @Test
     public void testRemovesDuplicates() throws InterruptedException {
         for (int i = 0; i < 10; i++) {
@@ -71,12 +89,12 @@ public class KafkaIdempotentRepositoryLazyTest extends CamelTestSupport {
 
     @Test
     public void testRollsBackOnException() throws InterruptedException {
-        mockOut.whenAnyExchangeReceived((exchange -> {
+        mockOut.whenAnyExchangeReceived(exchange -> {
             int id = exchange.getIn().getHeader("id", Integer.class);
             if (id == 0) {
                 throw new IllegalArgumentException("Boom!");
             }
-        }));
+        });
 
         for (int i = 0; i < 10; i++) {
             try {
