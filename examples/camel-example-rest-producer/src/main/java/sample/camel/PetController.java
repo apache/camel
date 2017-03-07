b877b45840b3166f0c9213337595fbc506075197
Claus Ibsen <davsclaus@apache.org>
2017-03-07 17:23:23 +0100

    Fixed CS

--- examples/camel-example-rest-producer/src/main/java/sample/camel/PetController.java
+++ examples/camel-example-rest-producer/src/main/java/sample/camel/PetController.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PetController {
 
-    private static String[] PETS = new String[]{"Snoopy", "Fido", "Tony the Tiger"};
+    private static final String[] PETS = new String[]{"Snoopy", "Fido", "Tony the Tiger"};
 
     @RequestMapping(value = "/petById/{id}", produces = "application/json")
     public String petById(@PathVariable("id") Integer id) {
