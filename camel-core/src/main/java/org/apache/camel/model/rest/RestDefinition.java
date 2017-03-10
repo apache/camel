4f9d81b7b3afa14b57c0893c81f954d0d100ee84
Scott Cranton <scott@cranton.com>
2017-03-10 08:17:00 -0500

    CAMEL-10931: deprecated typeList and outTypeList, and removed references from rest-api doc

--- camel-core/src/main/java/org/apache/camel/model/rest/RestDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/rest/RestDefinition.java
@@ -412,6 +412,13 @@ public class RestDefinition extends OptionalIdentifiedDefinition<RestDefinition>
         return this;
     }
 
+    /**
+     * @param classType the canonical class name for the array passed as input
+     *
+     * @deprecated as of 2.19.0. Replaced wtih {@link #type(Class)} with {@code []} appended to canonical class name
+     * , e.g. {@code type(MyClass[].class}
+     */
+    @Deprecated
     public RestDefinition typeList(Class<?> classType) {
         // add to last verb
         if (getVerbs().isEmpty()) {
@@ -435,6 +442,13 @@ public class RestDefinition extends OptionalIdentifiedDefinition<RestDefinition>
         return this;
     }
 
+    /**
+     * @param classType the canonical class name for the array passed as output
+     *
+     * @deprecated as of 2.19.0. Replaced wtih {@link #outType(Class)} with {@code []} appended to canonical class name
+     * , e.g. {@code outType(MyClass[].class}
+     */
+    @Deprecated
     public RestDefinition outTypeList(Class<?> classType) {
         // add to last verb
         if (getVerbs().isEmpty()) {
