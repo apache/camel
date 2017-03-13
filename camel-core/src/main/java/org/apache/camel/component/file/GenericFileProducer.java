c2b2ee2ed5f14e12f3f794c12b6f920c9fea6935
onders86 <ondersezgin@gmail.com>
2017-03-13 11:10:23 +0300

    CAMEL-10791-added option to handle empty directories while unzipping

--- camel-core/src/main/java/org/apache/camel/component/file/GenericFileProducer.java
+++ camel-core/src/main/java/org/apache/camel/component/file/GenericFileProducer.java
@@ -274,11 +274,7 @@ public class GenericFileProducer<T> extends DefaultProducer {
             log.trace("About to write [{}] to [{}] from exchange [{}]", new Object[]{fileName, getEndpoint(), exchange});
         }
 
-        // if filename indicates the current directory and the directory is created
-        // but no need to store a file under the directory like touch <dir>
-        // this is added due to considering to handle empty directories in zipfile 
-        boolean success = !fileName.substring(fileName.lastIndexOf(File.separator) + 1, fileName.length()).equals(".")
-                                            ? operations.storeFile(fileName, exchange) : true;
+        boolean success = operations.storeFile(fileName, exchange);
         if (!success) {
             throw new GenericFileOperationFailedException("Error writing file [" + fileName + "]");
         }
