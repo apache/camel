b537350cb941b664eff9a4ebd5315c622e9bef55
Andrea Cosentino <ancosen@gmail.com>
2017-03-14 14:50:10 +0100

    Camel-Hazelcast: Removed deprecated code

--- components/camel-hazelcast/src/main/java/org/apache/camel/processor/aggregate/hazelcast/HazelcastAggregationRepository.java
+++ components/camel-hazelcast/src/main/java/org/apache/camel/processor/aggregate/hazelcast/HazelcastAggregationRepository.java
@@ -38,6 +38,7 @@ import org.apache.camel.spi.OptimisticLockingAggregationRepository;
 import org.apache.camel.spi.RecoverableAggregationRepository;
 import org.apache.camel.support.ServiceSupport;
 import org.apache.camel.util.ObjectHelper;
+import org.apache.camel.util.StringHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -345,7 +346,7 @@ public class HazelcastAggregationRepository extends ServiceSupport
                 // if no commit occurs during the timeout. So we are still consistent whether local node crashes.
                 TransactionOptions tOpts = new TransactionOptions();
 
-                tOpts.setTransactionType(TransactionOptions.TransactionType.LOCAL);
+                tOpts.setTransactionType(TransactionOptions.TransactionType.ONE_PHASE);
                 TransactionContext tCtx = hzInstance.newTransactionContext(tOpts);
 
                 try {
@@ -405,7 +406,7 @@ public class HazelcastAggregationRepository extends ServiceSupport
         if (recoveryInterval < 0) {
             throw new IllegalArgumentException("Recovery interval must be zero or a positive integer.");
         }
-        ObjectHelper.notEmpty(mapName, "repositoryName");
+        StringHelper.notEmpty(mapName, "repositoryName");
         if (useLocalHzInstance)  {
             Config cfg = new XmlConfigBuilder().build();
             cfg.setProperty("hazelcast.version.check.enabled", "false");
