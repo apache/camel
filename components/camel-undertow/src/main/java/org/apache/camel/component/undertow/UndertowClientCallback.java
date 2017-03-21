0108bc18e0160ab1ff46e583b8790a486e032206
Zoran Regvart <zregvart@apache.org>
2017-03-21 14:59:47 +0100

    CAMEL-11034 Undertow producer does not support ...
    ...`https`
    
    This started as a simple addition of `XnioSSL` to
    `UndertowClient::sendRequest`, but it has evolved into a partial rewrite
    of `UndertowProducer`.
    This I feel needed to be done as the former `UndertowProducerCallback`
    did not take into account flushing/closing the request channel. Which if
    added to current body of the callback resulted in
    `TruncatedResponseException` due to asynchronous nature of Undertow's
    IO.
    Also handling of errors was a bit inconsistent with the endpoint
    property `throwExceptionOnFailure` which could be intentional, but I
    felt it was not.
    So, this adds UndertowClientCallback that ensures consistent error
    handling, resource management (assured close) and Camel async exchange
    assured end.

---
+++ components/camel-undertow/src/main/java/org/apache/camel/component/undertow/UndertowClientCallback.java
@@ -0,0 +1,249 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.undertow;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.function.Consumer;
+
+import io.undertow.client.ClientCallback;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientExchange;
+import io.undertow.client.ClientRequest;
+import io.undertow.util.HeaderMap;
+import io.undertow.util.HttpString;
+
+import org.apache.camel.AsyncCallback;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.util.ExchangeHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.ChannelListener;
+import org.xnio.ChannelListeners;
+import org.xnio.IoUtils;
+import org.xnio.channels.StreamSinkChannel;
+
+class UndertowClientCallback implements ClientCallback<ClientConnection> {
+
+    class ErrorHandlingClientCallback<T> implements ClientCallback<T> {
+
+        private final Consumer<T> consumer;
+
+        private ErrorHandlingClientCallback(final Consumer<T> consumer) {
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void completed(final T result) {
+            consumer.accept(result);
+        }
+
+        @Override
+        public void failed(final IOException e) {
+            hasFailedWith(e);
+        }
+
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(UndertowClientCallback.class);
+
+    private final ByteBuffer body;
+
+    private final AsyncCallback callback;
+
+    private final BlockingDeque<Closeable> closables = new LinkedBlockingDeque<>();
+
+    private final UndertowEndpoint endpoint;
+
+    private final Exchange exchange;
+
+    private final ClientRequest request;
+
+    private final Boolean throwExceptionOnFailure;
+
+    UndertowClientCallback(final Exchange exchange, final AsyncCallback callback, final UndertowEndpoint endpoint,
+        final ClientRequest request, final ByteBuffer body) {
+        this.exchange = exchange;
+        this.callback = callback;
+        this.endpoint = endpoint;
+        this.request = request;
+        this.body = body;
+        throwExceptionOnFailure = endpoint.getThrowExceptionOnFailure();
+    }
+
+    @Override
+    public void completed(final ClientConnection connection) {
+        // we have established connection, make sure we close it
+        deferClose(connection);
+
+        // now we can send the request and perform the exchange: writing the
+        // request and reading the response
+        connection.sendRequest(request, on(this::performClientExchange));
+    }
+
+    @Override
+    public void failed(final IOException e) {
+        hasFailedWith(e);
+    }
+
+    ChannelListener<StreamSinkChannel> asyncWriter(final ByteBuffer body) {
+        return channel -> {
+            try {
+                write(channel, body);
+
+                if (body.hasRemaining()) {
+                    channel.resumeWrites();
+                } else {
+                    flush(channel);
+                }
+            } catch (final IOException e) {
+                hasFailedWith(e);
+            }
+        };
+    }
+
+    void deferClose(final Closeable closeable) {
+        try {
+            closables.putFirst(closeable);
+        } catch (final InterruptedException e) {
+            hasFailedWith(e);
+        }
+    }
+
+    void finish(final Message result) {
+        for (final Closeable closeable : closables) {
+            IoUtils.safeClose(closeable);
+        }
+
+        if (result != null) {
+            if (ExchangeHelper.isOutCapable(exchange)) {
+                exchange.setOut(result);
+            } else {
+                exchange.setIn(result);
+            }
+        }
+
+        callback.done(false);
+    }
+
+    void hasFailedWith(final Exception e) {
+        LOG.trace("Exchange has failed with", e);
+        if (Boolean.TRUE.equals(throwExceptionOnFailure)) {
+            exchange.setException(e);
+        }
+
+        finish(null);
+    }
+
+    <T> ClientCallback<T> on(final Consumer<T> consumer) {
+        return new ErrorHandlingClientCallback<>(consumer);
+    }
+
+    void performClientExchange(final ClientExchange clientExchange) {
+        // add response listener to the exchange, we could receive the response
+        // at any time (async)
+        setupResponseListner(clientExchange);
+
+        // write the request
+        writeRequest(clientExchange, body);
+    }
+
+    void setupResponseListner(final ClientExchange clientExchange) {
+        clientExchange.setResponseListener(on(response -> {
+            LOG.trace("completed: {}", clientExchange);
+
+            try {
+                storeCookies(clientExchange);
+
+                final UndertowHttpBinding binding = endpoint.getUndertowHttpBinding();
+                final Message result = binding.toCamelMessage(clientExchange, exchange);
+
+                // we end Camel exchange here
+                finish(result);
+            } catch (final Exception e) {
+                hasFailedWith(e);
+            }
+        }));
+    }
+
+    void storeCookies(final ClientExchange clientExchange) throws IOException, URISyntaxException {
+        if (endpoint.getCookieHandler() != null) {
+            // creating the url to use takes 2-steps
+            final String url = UndertowHelper.createURL(exchange, endpoint);
+            final URI uri = UndertowHelper.createURI(exchange, url, endpoint);
+            final HeaderMap headerMap = clientExchange.getResponse().getResponseHeaders();
+            final Map<String, List<String>> m = new HashMap<>();
+            for (final HttpString headerName : headerMap.getHeaderNames()) {
+                final List<String> headerValue = new LinkedList<>();
+                for (int i = 0; i < headerMap.count(headerName); i++) {
+                    headerValue.add(headerMap.get(headerName, i));
+                }
+                m.put(headerName.toString(), headerValue);
+            }
+            endpoint.getCookieHandler().storeCookies(exchange, uri, m);
+        }
+    }
+
+    void writeRequest(final ClientExchange clientExchange, final ByteBuffer body) {
+        final StreamSinkChannel requestChannel = clientExchange.getRequestChannel();
+        if (body != null) {
+            try {
+                // try writing, we could be on IO thread and ready to write to
+                // the socket (or not)
+                write(requestChannel, body);
+
+                if (body.hasRemaining()) {
+                    // we did not write all of body (or at all) register a write
+                    // listener to write asynchronously
+                    requestChannel.getWriteSetter().set(asyncWriter(body));
+                    requestChannel.resumeWrites();
+                } else {
+                    // we are done, we need to flush the request
+                    flush(requestChannel);
+                }
+            } catch (final IOException e) {
+                hasFailedWith(e);
+            }
+        }
+    }
+
+    static void flush(final StreamSinkChannel channel) throws IOException {
+        channel.shutdownWrites();
+        if (!channel.flush()) {
+            channel.getWriteSetter().set(ChannelListeners.flushingChannelListener(IoUtils::safeClose,
+                ChannelListeners.closingChannelExceptionHandler()));
+            channel.resumeWrites();
+        }
+    }
+
+    static void write(final StreamSinkChannel channel, final ByteBuffer body) throws IOException {
+        int written = 1;
+        while (body.hasRemaining() && written > 0) {
+            written = channel.write(body);
+        }
+    }
+}
