26f888c298dea5a7263088d6d8d0659c1993927a
Thomas Lehoux <tlehoux@gmail.com>
2017-03-24 11:15:54 +0100

    initial commit

---
+++ components/camel-digitalocean/src/test/java/com/viwilo/camel/DigitalOceanClientMock.java
@@ -0,0 +1,26 @@
+package com.viwilo.camel;
+
+import com.myjeeva.digitalocean.exception.DigitalOceanException;
+import com.myjeeva.digitalocean.exception.RequestUnsuccessfulException;
+import com.myjeeva.digitalocean.impl.DigitalOceanClient;
+import com.myjeeva.digitalocean.pojo.Account;
+
+/**
+ * Created by thomas on 17/03/2017.
+ */
+public class DigitalOceanClientMock extends DigitalOceanClient {
+
+
+
+    public DigitalOceanClientMock() {
+        super("token");
+    }
+
+    @Override
+    public Account getAccountInfo() throws DigitalOceanException, RequestUnsuccessfulException {
+        Account account = new Account();
+        account.setEmail("camel@apache.org");
+
+        return account;
+    }
+}
