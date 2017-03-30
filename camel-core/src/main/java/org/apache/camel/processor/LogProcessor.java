6e9e0be8d08652a69c03da7bee4cca60d4fee216
Tomohisa Igarashi <tm.igarashi@gmail.com>
2017-03-30 22:56:21 +0900

    CAMEL-11054 Create SPI for Log EIP to enable other components to intercept/enrich logged messages

--- camel-core/src/main/java/org/apache/camel/processor/LogProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/LogProcessor.java
@@ -16,16 +16,23 @@
  */
 package org.apache.camel.processor;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.apache.camel.AsyncCallback;
 import org.apache.camel.AsyncProcessor;
 import org.apache.camel.Exchange;
 import org.apache.camel.Expression;
 import org.apache.camel.Traceable;
 import org.apache.camel.spi.IdAware;
+import org.apache.camel.spi.LogListener;
 import org.apache.camel.spi.MaskingFormatter;
 import org.apache.camel.support.ServiceSupport;
 import org.apache.camel.util.AsyncProcessorHelper;
 import org.apache.camel.util.CamelLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A processor which evaluates an {@link Expression} and logs it.
@@ -34,15 +41,18 @@ import org.apache.camel.util.CamelLogger;
  */
 public class LogProcessor extends ServiceSupport implements AsyncProcessor, Traceable, IdAware {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LogProcessor.class);
     private String id;
     private final Expression expression;
     private final CamelLogger logger;
     private final MaskingFormatter formatter;
+    private final Set<LogListener> listeners;
 
-    public LogProcessor(Expression expression, CamelLogger logger, MaskingFormatter formatter) {
+    public LogProcessor(Expression expression, CamelLogger logger, MaskingFormatter formatter, Set<LogListener> listeners) {
         this.expression = expression;
         this.logger = logger;
         this.formatter = formatter;
+        this.listeners = listeners;
     }
 
     public void process(Exchange exchange) throws Exception {
@@ -57,6 +67,7 @@ public class LogProcessor extends ServiceSupport implements AsyncProcessor, Trac
                 if (formatter != null) {
                     msg = formatter.format(msg);
                 }
+                msg = fireListeners(exchange, msg);
                 logger.doLog(msg);
             }
         } catch (Exception e) {
@@ -66,6 +77,27 @@ public class LogProcessor extends ServiceSupport implements AsyncProcessor, Trac
             callback.done(true);
         }
         return true;
+    }
+
+    private String fireListeners(Exchange exchange, String message) {
+        if (listeners == null) {
+            return message;
+        }
+        for (LogListener listener : listeners) {
+            if (listener == null) {
+                continue;
+            }
+            try {
+                String output = listener.onLog(exchange, logger, message);
+                message = output != null ? output : message;
+            } catch (Throwable t) {
+                LOG.warn("Ignoring an exception thrown by {}: {}", listener.getClass().getName(), t.getMessage());
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("", t);
+                }
+            }
+        }
+        return message;
     }
 
     @Override
