643c3e3e0059c80b067814dffb4df41d003906ce
Scott Cranton <scott@cranton.com>
2017-04-05 07:49:04 -0400

    CAMEL-11111: added unit test for throwExceptionOnFailure

---
+++ components/camel-undertow/src/test/java/org/apache/camel/component/undertow/UndertowProducerThrowExceptionOnFailureTest.java
@@ -0,0 +1,60 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.undertow;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class UndertowProducerThrowExceptionOnFailureTest extends BaseUndertowTest {
+
+    @Test
+    public void testFailWithoutException() throws Exception {
+        try {
+            String out = template().requestBody("undertow:http://localhost:{{port}}/fail?throwExceptionOnFailure=false", null, String.class);
+            assertEquals("Fail", out);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail("Should not throw an exception");
+        }
+    }
+
+    @Test
+    public void testFailWithException() throws Exception {
+        try {
+            String out = template().requestBody("undertow:http://localhost:{{port}}/fail?throwExceptionOnFailure=true", null, String.class);
+        } catch (Throwable t) {
+            //t.printStackTrace();
+            assertNotNull(t);
+        }
+        fail("Should throw an exception");
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("undertow:http://localhost:{{port}}/fail")
+                        .setHeader(Exchange.HTTP_RESPONSE_CODE).constant(404)
+                        .transform(constant("Fail"));
+            }
+        };
+    }
+}
