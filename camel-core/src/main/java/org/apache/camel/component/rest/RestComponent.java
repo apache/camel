6396867d1598ca4bfa52d63140d369deccdd214b
Zoran Regvart <zregvart@apache.org>
2017-04-05 11:37:16 +0200

    CAMEL-11110 REST component host parameter handling
    Changes the method of accessing the `host` parameter from
    `resolveAndRemoveReferenceParameter` to
    `getAndRemoveOrResolveReferenceParameter` that looks if the parameter is
    in the reference syntax (starts with `#`) and tries to resolve only
    those.

--- camel-core/src/main/java/org/apache/camel/component/rest/RestComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/rest/RestComponent.java
@@ -58,7 +58,7 @@ public class RestComponent extends DefaultComponent {
         mergeConfigurations(config, getCamelContext().getRestConfiguration(restConfigurationName, true));
 
         // if no explicit host was given, then fallback and use default configured host
-        String h = resolveAndRemoveReferenceParameter(parameters, "host", String.class, host);
+        String h = getAndRemoveOrResolveReferenceParameter(parameters, "host", String.class, host);
         if (h == null && config != null) {
             h = config.getHost();
             int port = config.getPort();
