f304831b74d07054732cc4dc57f2a4dfe063b4ce
Claus Ibsen <davsclaus@apache.org>
2017-04-11 14:49:11 +0200

    CAMEL-11130: Remove -kill option as CI server dont use it

--- tests/camel-itest-osgi/kill-karaf.sh
+++ tests/camel-itest-osgi/kill-karaf.sh
@@ -18,4 +18,4 @@
 ## ---------------------------------------------------------------------------
 
 echo "Killing all karaf processes"
-jps -l | grep karaf | cut -d ' ' -f 1 | xargs -n1 kill -kill
+jps -l | grep karaf | cut -d ' ' -f 1 | xargs -n1 kill
