d661a7a33b6acb35b24c8f80d21f9a9913b62515
Claus Ibsen <davsclaus@apache.org>
2017-04-13 13:26:42 +0200

    CAMEL-11145: Component docs - Fix broken links on github

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/model/ComponentModel.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/model/ComponentModel.java
@@ -218,6 +218,19 @@ public class ComponentModel {
     }
 
     public String getDocLink() {
+        // special for these components
+        if ("camel-box".equals(artifactId)) {
+            return "camel-box/camel-box-component/src/main/docs";
+        } else if ("camel-linkedin".equals(artifactId)) {
+            return "camel-linkedin/camel-linkedin-component/src/main/docs";
+        } else if ("camel-olingo2".equals(artifactId)) {
+            return "camel-olingo2/camel-olingo2-component/src/main/docs";
+        } else if ("camel-olingo4".equals(artifactId)) {
+            return "camel-olingo4/camel-olingo4-component/src/main/docs";
+        } else if ("camel-salesforce".equals(artifactId)) {
+            return "camel-salesforce/camel-salesforce-component/src/main/docs";
+        }
+
         if ("camel-core".equals(artifactId)) {
             return coreOnly ? "src/main/docs" : "../camel-core/src/main/docs";
         } else {
