5bc22d1f1c94f633ac9499ddbf7f20f546c73883
Claus Ibsen <davsclaus@apache.org>
2017-04-21 21:01:52 +0200

    Fixed CS

--- tests/camel-itest/src/test/java/org/apache/camel/itest/doc/FtpComponentConfigurationAndDocumentationTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/doc/FtpComponentConfigurationAndDocumentationTest.java
@@ -42,8 +42,10 @@ public class FtpComponentConfigurationAndDocumentationTest extends CamelTestSupp
         String json = compConf.createParameterJsonSchema();
         assertNotNull(json);
 
-        assertTrue(json.contains("\"minDepth\": { \"kind\": \"parameter\", \"displayName\": \"Min Depth\", \"group\": \"filter\", \"label\": \"consumer,filter\", \"type\": \"integer\", \"javaType\": \"int\""));
-        assertTrue(json.contains("\"username\": { \"kind\": \"parameter\", \"displayName\": \"Username\", \"group\": \"security\", \"label\": \"security\", \"type\": \"string\""));
+        assertTrue(json.contains("\"minDepth\": { \"kind\": \"parameter\", \"displayName\": \"Min Depth\", \"group\": \"filter\""
+            + ", \"label\": \"consumer,filter\", \"type\": \"integer\", \"javaType\": \"int\""));
+        assertTrue(json.contains("\"username\": { \"kind\": \"parameter\", \"displayName\": \"Username\", \"group\": \"security\""
+            + ", \"label\": \"security\", \"type\": \"string\""));
     }
 
 }
