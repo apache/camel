d1177cd926ee28cedd3c6462a8db758bc8cb1fb0
Zoran Regvart <zregvart@apache.org>
2017-04-26 13:38:42 +0200

    CAMEL-11203 Verifier should support exclusion o...
    ...f properties in option groups
    
    This adds an exclusion modifier to the parameter given in an option
    group. If the parameter name is preceded with `!` then it should not be
    present when evaluating if the group is valid.

--- camel-core/src/main/java/org/apache/camel/impl/verifier/ResultErrorHelper.java
+++ camel-core/src/main/java/org/apache/camel/impl/verifier/ResultErrorHelper.java
@@ -25,10 +25,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.apache.camel.ComponentVerifier.VerificationError;
 import org.apache.camel.util.ObjectHelper;
 
+/**
+ * Helper that validates component parameters.
+ */
 public final class ResultErrorHelper {
 
     private ResultErrorHelper() {
@@ -46,34 +51,92 @@ public final class ResultErrorHelper {
      */
     public static Optional<VerificationError> requiresOption(String parameterName, Map<String, Object> parameters) {
         if (ObjectHelper.isEmpty(parameters.get(parameterName))) {
-            return Optional.of(
-                ResultErrorBuilder.withMissingOption(parameterName).build()
-            );
+            return Optional.of(ResultErrorBuilder.withMissingOption(parameterName).build());
         }
 
         return Optional.empty();
     }
 
+    /**
+     * Validates that the given parameters satisfy any grouped options
+     * ({@link OptionsGroup}). A parameter set is valid if it is
+     * present and required by least one of the groups.
+     *
+     * <p>As an example consider that there are two option groups that
+     * can be specified:
+     * <ul>
+     * <li>optionA: requires param1 and param2
+     * <li>optionB: requires param1 and param3
+     * </ul>
+     *
+     * Valid parameters are those that include param1 and either param2
+     * and/or param3.
+     *
+     * <p>Note the special syntax of {@link OptionsGroup#getOptions()}
+     * that can require an property ({@code "propertyName"}) or can
+     * forbid the presence of a property ({@code "!propertyName"}).
+     *
+     * <p>With that if in the example above if param2 is specified
+     * specifying param3 is not allowed, and vice versa option groups
+     * should be defined with options:
+     * <ul>
+     * <li>optionA: ["param1", "param2", "!param3"]
+     * <li>optionB: ["param1", "!param2", "param3"]
+     * </ul>
+     *
+     * @param parameters given parameters of a component
+     * @param groups groups of options
+     * @see OptionsGroup
+     */
     public static List<VerificationError> requiresAny(Map<String, Object> parameters, OptionsGroup... groups) {
         return requiresAny(parameters, Arrays.asList(groups));
     }
 
+    /**
+     * Validates that the given parameters satisfy any grouped options
+     * ({@link OptionsGroup}). A parameter set is valid if it is
+     * present and required by least one of the groups.
+     *
+     * @param parameters given parameters of a component
+     * @param groups groups of options
+     * @see #requiresAny(Map, OptionsGroup...)
+     * @see OptionsGroup
+     */
     public static List<VerificationError> requiresAny(Map<String, Object> parameters, Collection<OptionsGroup> groups) {
         final List<VerificationError> verificationErrors = new ArrayList<>();
         final Set<String> keys = new HashSet<>(parameters.keySet());
 
         for (OptionsGroup group : groups) {
-            if (keys.containsAll(group.getOptions())) {
-                // All the options of this group are found so we are good
-                return Collections.emptyList();
-            } else {
-                ResultErrorBuilder builder = new ResultErrorBuilder()
-                    .code(VerificationError.StandardCode.INCOMPLETE_PARAMETER_GROUP)
-                    .detail(VerificationError.GroupAttribute.GROUP_NAME, group.getName())
-                    .detail(VerificationError.GroupAttribute.GROUP_OPTIONS, String.join(",", group.getOptions()));
+            final Set<String> required = required(group.getOptions());
+            final Set<String> excluded = excluded(group.getOptions());
 
-                for (String option : group.getOptions()) {
+            final ResultErrorBuilder builder = new ResultErrorBuilder()
+                .code(VerificationError.StandardCode.ILLEGAL_PARAMETER_GROUP_COMBINATION)
+                .detail(VerificationError.GroupAttribute.GROUP_NAME, group.getName())
+                .detail(VerificationError.GroupAttribute.GROUP_OPTIONS, String.join(",", parameters(group.getOptions())));
+
+            if (keys.containsAll(required)) {
+                // All the options of this group are found so we are good
+                final Set<String> shouldBeExcluded = new HashSet<>(keys);
+                shouldBeExcluded.retainAll(excluded);
+
+                if (shouldBeExcluded.isEmpty()) {
+                    // None of the excluded properties is present, also good
+                    return Collections.emptyList();
+                }
+
+                shouldBeExcluded.forEach(builder::parameterKey);
+                verificationErrors.add(builder.build());
+            } else {
+
+                for (String option : required) {
                     if (!parameters.containsKey(option)) {
+                        builder.parameterKey(option);
+                    }
+                }
+
+                for (String option : excluded) {
+                    if (parameters.containsKey(option)) {
                         builder.parameterKey(option);
                     }
                 }
@@ -83,5 +146,19 @@ public final class ResultErrorHelper {
         }
 
         return verificationErrors;
+    }
+
+    static Set<String> required(final Set<String> options) {
+        return options.stream().filter(o -> !o.startsWith("!")).collect(Collectors.toSet());
+    }
+
+    static Set<String> excluded(final Set<String> options) {
+        return options.stream().filter(o -> o.startsWith("!")).map(o -> o.substring(1)).collect(Collectors.toSet());
+    }
+
+    static Set<String> parameters(final Set<String> options) {
+        final Set<String> withoutExclusionMark = options.stream().map(o -> o.replaceFirst("!", "")).collect(Collectors.toSet());
+
+        return new TreeSet<String>(withoutExclusionMark);
     }
 }
