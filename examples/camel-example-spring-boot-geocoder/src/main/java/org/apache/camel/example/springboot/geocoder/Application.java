8b8b9febdcda4566a5b3a08eefd779ae037f0870
manos <manos@sobol>
2017-04-26 19:44:42 +0300

    replaced servlet component registration with starter

--- examples/camel-example-spring-boot-geocoder/src/main/java/org/apache/camel/example/springboot/geocoder/Application.java
+++ examples/camel-example-spring-boot-geocoder/src/main/java/org/apache/camel/example/springboot/geocoder/Application.java
@@ -16,27 +16,24 @@
  */
 package org.apache.camel.example.springboot.geocoder;
 
-import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+    /*
+     * For  PMD HideUtilityClassConstructorCheck
+     */
+    private void noop() {
+
+    }
 
     /**
      * Main method to start the application.
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean camelServletRegistrationBean() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/camel/*");
-        registration.setName("CamelServlet");
-        return registration;
     }
 
 }
