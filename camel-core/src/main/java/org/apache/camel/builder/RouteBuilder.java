6b497f5c2b45979660f0f9d60ace3d9dec9c9b32
Ramu <kkakarla@redhat.com>
2017-05-09 12:54:32 +0530

    Corrected check style Errors

--- camel-core/src/main/java/org/apache/camel/builder/RouteBuilder.java
+++ camel-core/src/main/java/org/apache/camel/builder/RouteBuilder.java
@@ -489,8 +489,8 @@ public abstract class RouteBuilder extends BuilderSupport implements RoutesBuild
             for (Map.Entry<String, RestConfigurationDefinition> entry : getRestConfigurations().entrySet()) {
                 RestConfiguration config = entry.getValue().asRestConfiguration(getContext());
                 if ("".equals(entry.getKey())) {
-                    if(entry.getValue().getHost()==null) {
-              	       config.setHost("");
+                    if (entry.getValue().getHost() == null) {
+                        config.setHost("");
                     }
                     camelContext.setRestConfiguration(config);
                 } else {
