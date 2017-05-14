eeb70213ce8aaffbf893d77968fdbc27a00f9c7e
Prasanth Pagolu <ppagolu@redhat.com>
2017-05-14 21:49:34 +0100

    CAMEL-11264: Fixed potential NPE in DefaultUndertowHttpBinding

--- components/camel-undertow/src/main/java/org/apache/camel/component/undertow/DefaultUndertowHttpBinding.java
+++ components/camel-undertow/src/main/java/org/apache/camel/component/undertow/DefaultUndertowHttpBinding.java
@@ -325,7 +325,7 @@ public class DefaultUndertowHttpBinding implements UndertowHttpBinding {
                 // we failed due an exception so print it as plain text
                 StringWriter sw = new StringWriter();
                 PrintWriter pw = new PrintWriter(sw);
-                ObjectHelper.applyIfNotEmpty(exception.getCause(), value -> value, () -> exception).printStackTrace(pw);
+                exception.printStackTrace(pw);
 
                 // the body should then be the stacktrace
                 body = ByteBuffer.wrap(sw.toString().getBytes());
