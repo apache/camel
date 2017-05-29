e3860198b80e1b21560118a2839ca051a3079947
Andrea Cosentino <ancosen@gmail.com>
2017-05-29 15:21:59 +0200

    CAMEL-11356: Camel-Kubernetes: refactoring tests by using Kubernetes-server-mock and Openshift-server-mock - Configmaps Tests

--- components/camel-kubernetes/src/test/java/org/apache/camel/component/kubernetes/producer/KubernetesBuildsProducerTest.java
+++ components/camel-kubernetes/src/test/java/org/apache/camel/component/kubernetes/producer/KubernetesBuildsProducerTest.java
@@ -83,8 +83,4 @@ public class KubernetesBuildsProducerTest extends KubernetesTestSupport {
             }
         };
     }
-
-    private static String toUrlEncoded(String str) {
-        return str.replaceAll("=", "%3D");
-    }
 }
