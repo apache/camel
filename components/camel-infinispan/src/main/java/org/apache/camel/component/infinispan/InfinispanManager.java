bd78bca13fdc162261c3fa45622e7a42c1784323
lburgazzoli <lburgazzoli@gmail.com>
2017-06-22 18:59:00 +0200

    CAMEL-11388: camel-infinispan - InfinispanRoutePolicy issue with locking from remote server

--- components/camel-infinispan/src/main/java/org/apache/camel/component/infinispan/InfinispanManager.java
+++ components/camel-infinispan/src/main/java/org/apache/camel/component/infinispan/InfinispanManager.java
@@ -165,6 +165,24 @@ public class InfinispanManager implements Service {
         return cache;
     }
 
+    public <K, V> BasicCache<K, V> getCache(String cacheName, boolean forceReturnValue) {
+        if (isCacheContainerRemote()) {
+            BasicCache<K, V> cache;
+            if (ObjectHelper.isEmpty(cacheName)) {
+                cache = InfinispanUtil.asRemote(cacheContainer).getCache(forceReturnValue);
+                cacheName = cache.getName();
+            } else {
+                cache = InfinispanUtil.asRemote(cacheContainer).getCache(cacheName, forceReturnValue);
+            }
+
+            LOGGER.trace("Cache[{}]", cacheName);
+
+            return cache;
+        } else {
+            return getCache(cacheName);
+        }
+    }
+
     public <K, V> BasicCache<K, V> getCache(Exchange exchange, String defaultCache) {
         return getCache(exchange.getIn(), defaultCache);
     }
