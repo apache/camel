0afb706286b335736f046998ef74a916fda8eebe
Andrea Cosentino <ancosen@gmail.com>
2017-06-30 10:52:21 +0200

    Fixed CS

--- components/camel-caffeine/src/main/java/org/apache/camel/component/caffeine/processor/idempotent/CaffeineIdempotentRepository.java
+++ components/camel-caffeine/src/main/java/org/apache/camel/component/caffeine/processor/idempotent/CaffeineIdempotentRepository.java
@@ -16,14 +16,14 @@
  */
 package org.apache.camel.component.caffeine.processor.idempotent;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 import org.apache.camel.api.management.ManagedAttribute;
 import org.apache.camel.api.management.ManagedOperation;
 import org.apache.camel.api.management.ManagedResource;
 import org.apache.camel.spi.IdempotentRepository;
 import org.apache.camel.support.ServiceSupport;
-
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 
 @ManagedResource(description = "Caffeine based message id repository")
 public class CaffeineIdempotentRepository extends ServiceSupport implements IdempotentRepository<String> {
