8bb1285b29b7eae2128caaf6e91112cd4ab5d2c8
Claus Ibsen <davsclaus@apache.org>
2017-07-08 14:00:41 +0200

    CAMEL-11446: Use awaitility in camel-core for testing where we otherwise use thred sleep which can be speeded up.

--- camel-core/src/test/java/org/apache/camel/component/file/FileLanguageCBRTest.java
+++ camel-core/src/test/java/org/apache/camel/component/file/FileLanguageCBRTest.java
@@ -66,7 +66,7 @@ public class FileLanguageCBRTest extends ContextTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("file://target/cbr?delete=true")
+                from("file://target/cbr?delete=true&initialDelay=0&delay=10")
                     .convertBodyTo(String.class)
                     .choice()
                         .when().simple("${file:ext} == 'txt'").to("mock:txt")
