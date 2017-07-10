3ec32869a388d0c845912a16e88877b821b9622d
Claus Ibsen <davsclaus@apache.org>
2017-07-10 22:18:48 +0200

    CAMEL-11529: Fixed wrong syntax in ignite component.

--- components/camel-ignite/src/main/java/org/apache/camel/component/ignite/messaging/IgniteMessagingEndpoint.java
+++ components/camel-ignite/src/main/java/org/apache/camel/component/ignite/messaging/IgniteMessagingEndpoint.java
@@ -37,7 +37,7 @@ import org.apache.ignite.IgniteMessaging;
  * consume messages from an <a href="https://apacheignite.readme.io/docs/messaging">Ignite topic</a>.
  * This endpoint supports producers (to send messages) and consumers (to receive messages).
  */
-@UriEndpoint(firstVersion = "2.17.0", scheme = "ignite-messaging", title = "Ignite Messaging", syntax = "ignite-messaging:[topic]", label = "nosql,cache,messaging", 
+@UriEndpoint(firstVersion = "2.17.0", scheme = "ignite-messaging", title = "Ignite Messaging", syntax = "ignite-messaging:topic", label = "nosql,cache,messaging",
     consumerClass = IgniteMessagingConsumer.class)
 public class IgniteMessagingEndpoint extends AbstractIgniteEndpoint {
 
