261ce8d29d410ca444ac3613720dc43f41312a7c
Claus Ibsen <davsclaus@apache.org>
2017-07-23 16:10:20 +0200

    CAMEL-11563: Add predicate option to @Consume so the bean is only called if its evaluated to true

--- examples/camel-example-spring-boot-pojo/src/main/java/sample/camel/NumberPojo.java
+++ examples/camel-example-spring-boot-pojo/src/main/java/sample/camel/NumberPojo.java
@@ -35,13 +35,13 @@ public class NumberPojo {
     private MagicNumber magic;
 
     // only consume when the predicate matches, eg when the message body is lower than 100
-    @Consume(uri = "seda:numbers", predicate = "${body} < 100")
+    @Consume(uri = "direct:numbers", predicate = "${body} < 100")
     public void lowNumber(int number) {
         magic.onMagicNumber("Got a low number " + number);
     }
 
     // only consume when the predicate matches, eg when the message body is higher or equal to 100
-    @Consume(uri = "seda:numbers", predicate = "${body} >= 100")
+    @Consume(uri = "direct:numbers", predicate = "${body} >= 100")
     public void highNumber(int number) {
         magic.onMagicNumber("Got a high number " + number);
     }
