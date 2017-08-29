d9a573862141be4f67c9c0ab02186e1118d595ce
Freeman Fang <freeman.fang@gmail.com>
2017-08-29 15:12:56 +0800

    [CAMEL-11719]add a string to ChildReference converter for camel-google-drive
    (cherry picked from commit 3044de6223fa030c70c146d08967ac1984d4978c)

--- components/camel-google-drive/src/test/java/org/apache/camel/component/google/drive/DriveChildrenIntegrationTest.java
+++ components/camel-google-drive/src/test/java/org/apache/camel/component/google/drive/DriveChildrenIntegrationTest.java
@@ -35,6 +35,7 @@ public class DriveChildrenIntegrationTest extends AbstractGoogleDriveTestSupport
 
     private static final Logger LOG = LoggerFactory.getLogger(DriveChildrenIntegrationTest.class);
     private static final String PATH_PREFIX = GoogleDriveApiCollection.getCollection().getApiName(DriveChildrenApiMethod.class).getName();
+    private static final String ROOT_FOLDER = "root";
 
     @Test
     public void testUploadFileToFolder() throws Exception {
@@ -55,7 +56,46 @@ public class DriveChildrenIntegrationTest extends AbstractGoogleDriveTestSupport
         final com.google.api.services.drive.model.ChildList result = requestBody("direct://LIST", folder.getId());
         assertNotNull("insert result", result);
         LOG.debug("insert: " + result);
+        headers.put("CamelGoogleDrive.childId", child.getId());
+        com.google.api.services.drive.model.ChildReference childReference = requestBodyAndHeaders("direct://GET", null, headers);
+        assertNotNull("inserted child", childReference);
+        requestBodyAndHeaders("direct://DELETE", null, headers);
+        try {
+            childReference = requestBodyAndHeaders("direct://GET", null, headers);
+            fail("can't fetch a child that already get deleted");
+        } catch (Exception ex) {
+            assertTrue(ex.getCause().getCause() instanceof com.google.api.client.googleapis.json.GoogleJsonResponseException);
+        }
+        
     }
+    
+    @Test
+    public void testUploadFileToRootFolder() throws Exception {
+        File file = uploadTestFile();
+        
+        final Map<String, Object> headers = new HashMap<String, Object>();
+        // parameter type is String
+        headers.put("CamelGoogleDrive.folderId", ROOT_FOLDER);
+        
+        com.google.api.services.drive.model.ChildReference child = new com.google.api.services.drive.model.ChildReference();
+        child.setId(file.getId());
+        // parameter type is com.google.api.services.drive.model.ChildReference
+        headers.put("CamelGoogleDrive.content", child);
+
+        requestBodyAndHeaders("direct://INSERT", null, headers);
+      
+        headers.put("CamelGoogleDrive.childId", child.getId());
+        com.google.api.services.drive.model.ChildReference childReference = requestBodyAndHeaders("direct://GET-BODY", child.getId(), headers);
+        assertNotNull("inserted child", childReference);
+        requestBodyAndHeaders("direct://DELETE", null, headers);
+        try {
+            childReference = requestBodyAndHeaders("direct://GET-BODY", child.getId(), headers, com.google.api.services.drive.model.ChildReference.class);
+            fail("can't fetch a child that already get deleted");
+        } catch (Exception ex) {
+            assertTrue(ex.getCause().getCause() instanceof com.google.api.client.googleapis.json.GoogleJsonResponseException);
+        }
+    }
+
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
@@ -68,6 +108,10 @@ public class DriveChildrenIntegrationTest extends AbstractGoogleDriveTestSupport
                 // test route for get
                 from("direct://GET")
                     .to("google-drive://" + PATH_PREFIX + "/get");
+                
+             // test route for get with body
+                from("direct://GET-BODY")
+                    .to("google-drive://" + PATH_PREFIX + "/get?inBody=childId");
 
                 // test route for insert
                 from("direct://INSERT")
