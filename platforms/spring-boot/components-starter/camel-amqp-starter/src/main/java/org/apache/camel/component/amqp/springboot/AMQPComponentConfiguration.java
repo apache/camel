aefa11f8ba1fcb871177fd95aafcdb03bac0aaef
Claus Ibsen <davsclaus@apache.org>
2017-09-04 08:52:36 +0200

    Regen

--- platforms/spring-boot/components-starter/camel-amqp-starter/src/main/java/org/apache/camel/component/amqp/springboot/AMQPComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-amqp-starter/src/main/java/org/apache/camel/component/amqp/springboot/AMQPComponentConfiguration.java
@@ -533,9 +533,10 @@ public class AMQPComponentConfiguration
     /**
      * This option is used to allow additional headers which may have values
      * that are invalid according to JMS specification. For example some message
-     * systems such as WMQ do this vith headers JMS_IBM_MQMD_ that contains byte
-     * or other invalid types. You can specify multiple header names separated
-     * by comma and use as suffix for wildcard matching.
+     * systems such as WMQ do this with header names using prefix JMS_IBM_MQMD_
+     * containing values with byte array or other invalid types. You can specify
+     * multiple header names separated by comma and use as suffix for wildcard
+     * matching.
      */
     private String allowAdditionalHeaders;
     /**
