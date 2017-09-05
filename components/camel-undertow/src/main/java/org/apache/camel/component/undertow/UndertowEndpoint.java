0e44b58795886084d8ab0f3a6c687164caa5d573
Andrea Cosentino <ancosen@gmail.com>
2017-09-05 19:50:28 +0200

    CAMEL-11748 - Camel-Undertow: transferException option doesn't work - improved docs

--- components/camel-undertow/src/main/java/org/apache/camel/component/undertow/UndertowEndpoint.java
+++ components/camel-undertow/src/main/java/org/apache/camel/component/undertow/UndertowEndpoint.java
@@ -223,8 +223,12 @@ public class UndertowEndpoint extends DefaultEndpoint implements AsyncEndpoint, 
     }
 
     /**
-     * Option to disable throwing the HttpOperationFailedException in case of failed responses from the remote server.
-     * This allows you to get all responses regardless of the HTTP status code.
+     * If enabled and an Exchange failed processing on the consumer side and if the caused Exception 
+     * was send back serialized in the response as a application/x-java-serialized-object content type. 
+     * On the producer side the exception will be deserialized and thrown as is instead of the HttpOperationFailedException. The caused exception is required to be serialized. 
+     * This is by default turned off. If you enable this 
+     * then be aware that Java will deserialize the incoming data from the request to Java and that can be a potential security risk.
+     * 
      */
     public void setTransferException(Boolean transferException) {
         this.transferException = transferException;
