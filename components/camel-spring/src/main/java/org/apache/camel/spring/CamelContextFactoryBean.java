b2782152346f878129d302b8d78dd636706907b3
Claus Ibsen <davsclaus@apache.org>
2017-09-06 20:26:14 +0200

    CAMEL-11752: Using XML routes with Spring Boot now supports spring auto configuration so you can configure in application.properties but still define Camel in XML file.

--- components/camel-spring/src/main/java/org/apache/camel/spring/CamelContextFactoryBean.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/CamelContextFactoryBean.java
@@ -77,7 +77,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
