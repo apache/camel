4a8acc246034cfb503f30d92c322209086f532cd
Claus Ibsen <davsclaus@apache.org>
2017-09-23 09:36:46 +0200

    Polished

--- components/camel-iec60870/src/main/java/org/apache/camel/component/iec60870/server/ServerEndpoint.java
+++ components/camel-iec60870/src/main/java/org/apache/camel/component/iec60870/server/ServerEndpoint.java
@@ -32,7 +32,7 @@ import org.apache.camel.spi.UriParam;
  * such as controlling electric power transmission grids and other geographically widespread control systems.
  */
 @UriEndpoint(firstVersion = "2.20.0", scheme = "iec60870-server", syntax = "iec60870-server:endpointUri",
-    title = "IEC 60870-5-104 Server", consumerClass = ServerConsumer.class, label = "iot")
+    title = "IEC 60870 Server", consumerClass = ServerConsumer.class, label = "iot")
 public class ServerEndpoint extends AbstractIecEndpoint<ServerConnectionMultiplexor> {
 
     /**
