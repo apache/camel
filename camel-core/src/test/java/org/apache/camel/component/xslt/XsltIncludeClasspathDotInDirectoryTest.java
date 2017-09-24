4e7da3f3353f0455a49df71070dc2f8d7dcca721
Claus Ibsen <davsclaus@apache.org>
2017-09-24 12:23:09 +0200

    Do not include test data in classes as it will be included in the final camel-core JAR

--- camel-core/src/test/java/org/apache/camel/component/xslt/XsltIncludeClasspathDotInDirectoryTest.java
+++ camel-core/src/test/java/org/apache/camel/component/xslt/XsltIncludeClasspathDotInDirectoryTest.java
@@ -43,6 +43,12 @@ public class XsltIncludeClasspathDotInDirectoryTest extends ContextTestSupport {
         super.setUp();
     }
 
+    @Override
+    protected void tearDown() throws Exception {
+        deleteDirectory("target/classes/com.mycompany");
+        super.tearDown();
+    }
+
     public void testXsltIncludeClasspath() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMessageCount(1);
