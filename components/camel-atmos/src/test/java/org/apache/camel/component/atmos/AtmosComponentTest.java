0203d0a2f27cdc87d37a3714c9a8bdbc44def591
aldettinger <aldettinger@gmail.com>
2017-10-29 22:10:39 +0100

    CAMEL-11869: Upgrade mockito-core to 2.11.0 for camel-pdf and camel-atmos

--- components/camel-atmos/src/test/java/org/apache/camel/component/atmos/AtmosComponentTest.java
+++ components/camel-atmos/src/test/java/org/apache/camel/component/atmos/AtmosComponentTest.java
@@ -24,7 +24,7 @@ import org.apache.camel.util.URISupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
