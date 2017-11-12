65b85e7facb6cb290129e40511e63b3571812faf
Pascal Schumacher <pascalschumacher@gmx.net>
2017-11-12 19:29:51 +0100

    camel-hazelcast: HazelcastAggregationRepositoryOperationsTest use different keys, to prevent OptimisticLockingExceptions when running all test methods

--- components/camel-hazelcast/src/test/java/org/apache/camel/processor/aggregate/hazelcast/HazelcastAggregationRepositoryOperationsTest.java
+++ components/camel-hazelcast/src/test/java/org/apache/camel/processor/aggregate/hazelcast/HazelcastAggregationRepositoryOperationsTest.java
@@ -96,7 +96,7 @@ public class HazelcastAggregationRepositoryOperationsTest extends HazelcastAggre
             repoTwo.start();
 
             final String testBody = "This is an optimistic test body. Sincerely yours, Captain Obvious.";
-            final String key = "optimisticKey";
+            final String key = "optimisticGetKey";
 
             Exchange ex = createExchangeWithBody(testBody);
             repoOne.add(context(), key, null, ex);
@@ -149,7 +149,7 @@ public class HazelcastAggregationRepositoryOperationsTest extends HazelcastAggre
             repoTwo.doStart();
 
             final String testBody = "This is an optimistic test body. Sincerely yours, Captain Obvious.";
-            final String key = "optimisticKey";
+            final String key = "optimisticPersistentRemoveKey";
 
             Exchange ex = createExchangeWithBody(testBody);
 
