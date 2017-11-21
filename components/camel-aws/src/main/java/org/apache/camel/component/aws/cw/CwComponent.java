e7fd657cbf67e75eaf441457507ef34cd8c4ae76
Andrea Cosentino <ancosen@gmail.com>
2017-11-21 11:19:13 +0100

    CAMEL-12012 - Camel-AWS: Add component verifiers like S3 for all the AWS components - CW Component

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/cw/CwComponent.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/cw/CwComponent.java
@@ -20,16 +20,18 @@ import java.util.Map;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
-import org.apache.camel.impl.UriEndpointComponent;
+import org.apache.camel.impl.DefaultComponent;
 
-public class CwComponent extends UriEndpointComponent {
+public class CwComponent extends DefaultComponent {
 
     public CwComponent() {
-        super(CwEndpoint.class);
+        this(null);
     }
 
     public CwComponent(CamelContext context) {
-        super(context, CwEndpoint.class);
+        super(context);
+        
+        registerExtension(new CwComponentVerifierExtension());
     }
 
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
