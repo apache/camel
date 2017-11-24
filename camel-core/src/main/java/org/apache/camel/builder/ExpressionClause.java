291c546db3fa46b8e8d1a060aed3f25666d84c03
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2017-11-24 14:39:05 +0100

    Revert "CAMEL-11532 Java DSL enhancement to accept supplier and function arguments (#2098)"
    This reverts commit 1a26e25343803e32aa88503831e843e6766ee8d9.

--- camel-core/src/main/java/org/apache/camel/builder/ExpressionClause.java
+++ camel-core/src/main/java/org/apache/camel/builder/ExpressionClause.java
@@ -20,7 +20,6 @@ import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import java.util.function.Supplier;
 import org.apache.camel.Exchange;
 import org.apache.camel.Expression;
 import org.apache.camel.Message;
@@ -147,17 +146,6 @@ public class ExpressionClause<T> extends ExpressionDefinition {
         return delegate.expression(new ExpressionAdapter() {
             public Object evaluate(Exchange exchange) {
                 return function.apply(exchange.getIn().getBody());
-            }
-        });
-    }
-
-    /**
-     * A functional expression of an inbound message body
-     */
-    public T body(final Supplier<Object> supplier) {
-        return delegate.expression(new ExpressionAdapter() {
-            public Object evaluate(Exchange exchange) {
-                return supplier.get();
             }
         });
     }
