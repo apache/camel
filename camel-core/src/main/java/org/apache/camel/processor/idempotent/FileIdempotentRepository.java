daf844454cde5bdeb3a138bfe536ac57d01288c6
Claus Ibsen <claus.ibsen@gmail.com>
2017-11-25 18:03:46 +0100

    CAMEL-12037: Fixed CS. This closes #2118

--- camel-core/src/main/java/org/apache/camel/processor/idempotent/FileIdempotentRepository.java
+++ camel-core/src/main/java/org/apache/camel/processor/idempotent/FileIdempotentRepository.java
@@ -51,7 +51,7 @@ public class FileIdempotentRepository extends ServiceSupport implements Idempote
     private Map<String, Object> cache;
     private File fileStore;
     private long maxFileStoreSize = 1024 * 1000L; // 1mb store file
-    private AtomicBoolean init = new AtomicBoolean();
+    private final AtomicBoolean init = new AtomicBoolean();
 
     public FileIdempotentRepository() {
     }
@@ -334,10 +334,9 @@ public class FileIdempotentRepository extends ServiceSupport implements Idempote
     protected void doStart() throws Exception {
         ObjectHelper.notNull(fileStore, "fileStore", this);
 
-        //CAMEL-12037
         if (this.cache == null) {
-        	// default use a 1st level cache
-        	this.cache = LRUCacheFactory.newLRUCache(1000);
+            // default use a 1st level cache
+            this.cache = LRUCacheFactory.newLRUCache(1000);
         }
 
         // init store if not loaded before
