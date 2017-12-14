ed9d4d6fe5f4ccc9975c62b89a145beebf079ef2
Dmitry Volodin <dmvolod@gmail.com>
2017-12-14 12:43:50 +0300

    CAMEL-12076: Specified topic is ignored when KafkaConfiguration is used

--- components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaEndpoint.java
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaEndpoint.java
@@ -164,6 +164,7 @@ public class KafkaEndpoint extends DefaultEndpoint implements MultipleConsumersS
         return getCamelContext().getExecutorServiceManager().newThreadPool(this, "KafkaProducer[" + configuration.getTopic() + "]", core, max);
     }
 
+    @SuppressWarnings("rawtypes")
     public Exchange createKafkaExchange(ConsumerRecord record) {
         Exchange exchange = super.createExchange();
 
