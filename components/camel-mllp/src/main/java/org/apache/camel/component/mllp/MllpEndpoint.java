ca91f06d4d371592d35ee48c83f9f6083b1469ee
Claus Ibsen <claus.ibsen@gmail.com>
2018-01-13 09:39:11 +0100

    Fix component description and regen docs.

--- components/camel-mllp/src/main/java/org/apache/camel/component/mllp/MllpEndpoint.java
+++ components/camel-mllp/src/main/java/org/apache/camel/component/mllp/MllpEndpoint.java
@@ -42,7 +42,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The MLLP component is designed to handle the MLLP protocol and provide the functionality required by Healthcare providers to communicate with other systems using the MLLP protocol.
+ * Provides functionality required by Healthcare providers to communicate with other systems using the MLLP protocol.
  *
  * <p/>
  * NOTE: MLLP payloads are not logged unless the logging level is set to DEBUG or TRACE to avoid introducing PHI into the log files.  Logging of PHI can be globally disabled by setting the
@@ -50,7 +50,7 @@ import org.slf4j.LoggerFactory;
  * <p/>
  */
 @ManagedResource(description = "MLLP Endpoint")
-@UriEndpoint(scheme = "mllp", title = "MLLP", syntax = "mllp:hostname:port", consumerClass = MllpTcpServerConsumer.class, label = "mllp")
+@UriEndpoint(scheme = "mllp", firstVersion = "2.17.0", title = "MLLP", syntax = "mllp:hostname:port", consumerClass = MllpTcpServerConsumer.class, label = "mllp")
 public class MllpEndpoint extends DefaultEndpoint {
     // Use constants from MllpProtocolConstants
     @Deprecated()
