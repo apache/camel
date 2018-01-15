e78764875440c8fdfd55c997d0843489bd0abaeb
Andrea Cosentino <ancosen@gmail.com>
2018-01-15 10:58:05 +0100

    Removed unused imports

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/sns/SnsComponentVerifierExtension.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/sns/SnsComponentVerifierExtension.java
@@ -23,8 +23,6 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 
