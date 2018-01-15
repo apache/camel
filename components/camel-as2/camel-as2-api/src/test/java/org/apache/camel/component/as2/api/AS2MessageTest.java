bbf32945758facec2762b38c71fe40829bd60f75
William Collins <punkhornsw@gmail.com>
2018-01-15 15:50:03 -0500

    [CAMEL-11257] Updated AS2MessageTest

--- components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/AS2MessageTest.java
+++ components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/AS2MessageTest.java
@@ -1,5 +1,10 @@
 package org.apache.camel.component.as2.api;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyStore;
@@ -8,24 +13,40 @@ import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.component.as2.api.entity.ApplicationEDIFACTEntity;
+import org.apache.camel.component.as2.api.entity.ApplicationPkcs7SignatureEntity;
 import org.apache.camel.component.as2.api.entity.MultipartSignedEntity;
 import org.apache.camel.component.as2.api.protocol.RequestAS2;
+import org.apache.http.ExceptionLogger;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.ParseException;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.impl.bootstrap.HttpServer;
+import org.apache.http.impl.bootstrap.ServerBootstrap;
 import org.apache.http.message.BasicHttpEntityEnclosingRequest;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.protocol.HttpCoreContext;
 import org.apache.http.protocol.HttpProcessor;
 import org.apache.http.protocol.HttpProcessorBuilder;
 import org.apache.http.protocol.HttpRequestExecutor;
+import org.apache.http.protocol.HttpRequestHandler;
 import org.apache.http.protocol.RequestConnControl;
 import org.apache.http.protocol.RequestContent;
 import org.apache.http.protocol.RequestDate;
 import org.apache.http.protocol.RequestExpectContinue;
 import org.apache.http.protocol.RequestTargetHost;
 import org.apache.http.protocol.RequestUserAgent;
+import org.apache.http.protocol.ResponseConnControl;
+import org.apache.http.protocol.ResponseContent;
+import org.apache.http.protocol.ResponseDate;
+import org.apache.http.protocol.ResponseServer;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.cms.AttributeTable;
 import org.bouncycastle.asn1.cms.IssuerAndSerialNumber;
@@ -37,12 +58,14 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.jcajce.JcaCertStore;
 import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoGeneratorBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AS2MessageTest {
 
+    private static final String METHOD = "POST";
     private static final String TARGET_HOST = "localhost";
     private static final int TARGET_PORT = 80;
     private static final String AS2_VERSION = "1.1";
@@ -81,7 +104,51 @@ public class AS2MessageTest {
             +"UNZ+1+00000000000778'\n";
 
     private AS2SignedDataGenerator gen;
+    
+    private static HttpServer testServer;
+    private String algorithmName;
+    private Certificate[] chain;
+    private PrivateKey privateKey;
+    
+    @BeforeClass
+    public static void setUpOne() throws Exception {
+        // Setup Test Server
+        HttpProcessor httpProcessor = HttpProcessorBuilder.create()
+                .add(new ResponseDate())
+                .add(new ResponseServer("MyServer-HTTP/1.1"))
+                .add(new ResponseContent())
+                .add(new ResponseConnControl())
+                .build();
+        SocketConfig socketConfig = SocketConfig.custom()
+                .setSoTimeout(150000)
+                .setTcpNoDelay(true)
+                .build();
+        testServer = ServerBootstrap.bootstrap()
+                .setListenerPort(8080)
+                .setHttpProcessor(httpProcessor)
+                .setSocketConfig(socketConfig)
+                .setExceptionLogger(new ExceptionLogger() {
+                    @Override
+                    public void log(Exception ex) {
+                    }
+                })
+                .registerHandler("*", new HttpRequestHandler() {
+                    @Override
+                    public void handle(HttpRequest request, HttpResponse response, HttpContext context)
+                            throws HttpException, IOException {
+                        Util.printRequest(System.out, request);
+                    }
+                })
+                .create();
+        testServer.start();
+      
+    }
 
+    @AfterClass
+    public static void tearDownOnce() throws Exception {
+        testServer.shutdown(5, TimeUnit.SECONDS);
+    }
+    
     @Before
     public void setUp() throws Exception {
         Security.addProvider(new BouncyCastleProvider());
@@ -90,11 +157,10 @@ public class AS2MessageTest {
         KeyStore keystore = KeyStore.getInstance("PKCS12");
         keystore.load(new FileInputStream("keystore.pfx"), "CamelsKool".toCharArray()); // TODO remove before checkin
         
-        // Get certificate chain, signing certificate, private key and algorithm name
-        Certificate[] chain = keystore.getCertificateChain("mailidentitykeys");
+        chain = keystore.getCertificateChain("mailidentitykeys");
         X509Certificate signingCert = (X509Certificate) chain[0];
-        PrivateKey privateKey = (PrivateKey)keystore.getKey("mailidentitykeys", "CamelsKool".toCharArray());
-        String algorithmName = "DSA".equals(privateKey.getAlgorithm()) ? "SHA1withDSA" : "MD5withRSA";
+        privateKey = (PrivateKey)keystore.getKey("mailidentitykeys", "CamelsKool".toCharArray());
+        algorithmName = "DSA".equals(privateKey.getAlgorithm()) ? "SHA1withDSA" : "MD5withRSA";
         
         // Create and populate certificate store.
         JcaCertStore certs = new JcaCertStore(Arrays.asList(chain));
@@ -113,49 +179,66 @@ public class AS2MessageTest {
         gen = new AS2SignedDataGenerator();
         gen.addSignerInfoGenerator(new JcaSimpleSignerInfoGeneratorBuilder().setProvider("BC").setSignedAttributeGenerator(new AttributeTable(attributes)).build(algorithmName, privateKey, signingCert));
         gen.addCertificates(certs);
-    }
-
-    @After
-    public void tearDown() throws Exception {
+        
     }
 
     @Test
-    public void createSignedMessageTest() throws InvalidAS2NameException, ParseException, IOException, HttpException {
-        BasicHttpEntityEnclosingRequest request = new BasicHttpEntityEnclosingRequest("POST", REQUEST_URI);
-        
-        // Build Context
-        HttpCoreContext httpContext = HttpCoreContext.create();
-        HttpHost targetHost = new HttpHost(TARGET_HOST, TARGET_PORT);
-        httpContext.setTargetHost(targetHost);
+    public void multipartSignedMessageTest() throws Exception {
+        AS2ClientConnection clientConnection = new AS2ClientConnection(AS2_VERSION, USER_AGENT, CLIENT_FQDN, TARGET_HOST, TARGET_PORT);
+        AS2ClientManager clientManager = new AS2ClientManager(clientConnection);
         
         // Add Context attributes
-        httpContext.setAttribute(RequestAS2.SUBJECT, SUBJECT);
-        httpContext.setAttribute(RequestAS2.FROM, FROM);
-        httpContext.setAttribute(RequestAS2.AS2_FROM, AS2_NAME);
-        httpContext.setAttribute(RequestAS2.AS2_TO, AS2_NAME);
+        HttpCoreContext httpContext = HttpCoreContext.create();
+        httpContext.setAttribute(AS2ClientManager.REQUEST_URI, REQUEST_URI);
+        httpContext.setAttribute(AS2ClientManager.SUBJECT, SUBJECT);
+        httpContext.setAttribute(AS2ClientManager.FROM, FROM);
+        httpContext.setAttribute(AS2ClientManager.AS2_FROM, AS2_NAME);
+        httpContext.setAttribute(AS2ClientManager.AS2_TO, AS2_NAME);
+        httpContext.setAttribute(AS2ClientManager.AS2_MESSAGE_STRUCTURE, AS2MessageStructure.SIGNED);
+        httpContext.setAttribute(AS2ClientManager.SIGNING_ALGORITHM_NAME, algorithmName);
+        httpContext.setAttribute(AS2ClientManager.SIGNING_CERTIFICATE_CHAIN, chain);
+        httpContext.setAttribute(AS2ClientManager.SIGNING_PRIVATE_KEY, privateKey);
         
-        // Build Processor
-        HttpProcessor httpProcessor = HttpProcessorBuilder.create()
-                .add(new RequestAS2(AS2_VERSION, CLIENT_FQDN))
-                .add(new RequestTargetHost())
-                .add(new RequestUserAgent(USER_AGENT))
-                .add(new RequestDate())
-                .add(new RequestContent())
-                .add(new RequestConnControl())
-                .add(new RequestExpectContinue(true)).build();
-
-
-        // Create Application EDIFACT Mime Part
-        ApplicationEDIFACTEntity applicationEDIFACTEntity = new ApplicationEDIFACTEntity(EDI_MESSAGE, AS2CharSet.US_ASCII, AS2TransferEncoding.BASE64, false);
+        clientManager.send(EDI_MESSAGE, httpContext);
         
-         // Create Multipart Signed Message Body
-        MultipartSignedEntity multipartSignedEntity = new MultipartSignedEntity(applicationEDIFACTEntity, gen, AS2CharSet.US_ASCII, AS2TransferEncoding.BASE64, false, null);
-        request.setEntity(multipartSignedEntity);
-        
-        HttpRequestExecutor httpexecutor = new HttpRequestExecutor();
-        httpexecutor.preProcess(request, httpProcessor, httpContext);
-        
+        HttpRequest request = httpContext.getRequest();
         Util.printRequest(System.out, request);
+        assertEquals("Unexpected method value", METHOD, request.getRequestLine().getMethod());
+        assertEquals("Unexpected request URI value", REQUEST_URI, request.getRequestLine().getUri());
+        assertEquals("Unexpected HTTP version value", HttpVersion.HTTP_1_1, request.getRequestLine().getProtocolVersion());
+        
+        assertEquals("Unexpected subject value", SUBJECT, request.getFirstHeader(AS2Header.SUBJECT).getValue());
+        assertEquals("Unexpected from value", FROM, request.getFirstHeader(AS2Header.FROM).getValue());
+        assertEquals("Unexpected AS2 version value", AS2_VERSION, request.getFirstHeader(AS2Header.AS2_VERSION).getValue());
+        assertEquals("Unexpected AS2 from value", AS2_NAME, request.getFirstHeader(AS2Header.AS2_FROM).getValue());
+        assertEquals("Unexpected AS2 to value", AS2_NAME, request.getFirstHeader(AS2Header.AS2_TO).getValue());
+        assertTrue("Unexpected message id value", request.getFirstHeader(AS2Header.MESSAGE_ID).getValue().endsWith(CLIENT_FQDN + ">"));
+        assertEquals("Unexpected target host value", TARGET_HOST + ":" + TARGET_PORT, request.getFirstHeader(AS2Header.TARGET_HOST).getValue());
+        assertEquals("Unexpected user agent value", USER_AGENT, request.getFirstHeader(AS2Header.USER_AGENT).getValue());
+        assertNotNull("Date value missing", request.getFirstHeader(AS2Header.DATE));
+        assertNotNull("Content length value missing", request.getFirstHeader(AS2Header.CONTENT_LENGTH));
+        assertTrue("Unexpected content type for message", request.getFirstHeader(AS2Header.CONTENT_TYPE).getValue().startsWith(AS2MediaType.MULTIPART_SIGNED));
+        
+        assertTrue("Request does not contain entity", request instanceof BasicHttpEntityEnclosingRequest);
+        HttpEntity entity = ((BasicHttpEntityEnclosingRequest)request).getEntity();
+        assertNotNull("Request does not contain entity", entity);
+        assertTrue("Unexpected request entity type", entity instanceof MultipartSignedEntity);
+        MultipartSignedEntity signedEntity = (MultipartSignedEntity)entity;
+        assertTrue("Entity not set as main body of request", signedEntity.isMainBody());
+        assertTrue("Request contains invalid number of mime parts", signedEntity.getPartCount() == 2);
+        
+        // Validated first mime part.
+        assertTrue("First mime part incorrect type ", signedEntity.getPart(0) instanceof ApplicationEDIFACTEntity);
+        ApplicationEDIFACTEntity ediEntity = (ApplicationEDIFACTEntity) signedEntity.getPart(0);
+        assertTrue("Unexpected content type for first mime part", ediEntity.getContentType().getValue().startsWith(AS2MediaType.APPLICATION_EDIFACT));
+        assertFalse("First mime type set as main body of request", ediEntity.isMainBody());
+        
+        // Validate second mime part.
+        assertTrue("Second mime part incorrect type ", signedEntity.getPart(1) instanceof ApplicationPkcs7SignatureEntity);
+        ApplicationPkcs7SignatureEntity signatureEntity = (ApplicationPkcs7SignatureEntity) signedEntity.getPart(1);
+        assertTrue("Unexpected content type for second mime part", signatureEntity.getContentType().getValue().startsWith(AS2MediaType.APPLICATION_PKCS7_SIGNATURE));
+        assertFalse("First mime type set as main body of request", signatureEntity.isMainBody());
+
     }
-    
+        
 }
