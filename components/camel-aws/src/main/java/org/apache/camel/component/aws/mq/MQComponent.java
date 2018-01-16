d05ee46910859037bc9c1c6aa3935a997909d9f9
Andrea Cosentino <ancosen@gmail.com>
2018-01-16 13:50:52 +0100

    CAMEL-12147 - Camel-AWS MQ: Add the ability to specify credentials and region at component level

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/mq/MQComponent.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/mq/MQComponent.java
@@ -21,12 +21,23 @@ import java.util.Map;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.impl.DefaultComponent;
+import org.apache.camel.spi.Metadata;
+import org.apache.camel.util.ObjectHelper;
 
 /**
  * For working with Amazon MQ.
  */
 public class MQComponent extends DefaultComponent {
 
+    @Metadata
+    private String accessKey;
+    @Metadata
+    private String secretKey;
+    @Metadata
+    private String region;
+    @Metadata(label = "advanced")    
+    private MQConfiguration configuration;
+    
     public MQComponent() {
         this(null);
     }
@@ -34,20 +45,74 @@ public class MQComponent extends DefaultComponent {
     public MQComponent(CamelContext context) {
         super(context);
         
+        this.configuration = new MQConfiguration();
         registerExtension(new MQComponentVerifierExtension());
     }
 
     @Override
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-        MQConfiguration configuration = new MQConfiguration();
+        MQConfiguration configuration = this.configuration.copy();
         setProperties(configuration, parameters);
 
+        if (ObjectHelper.isEmpty(configuration.getAccessKey())) {
+            setAccessKey(accessKey);
+        }
+        if (ObjectHelper.isEmpty(configuration.getSecretKey())) {
+            setSecretKey(secretKey);
+        }
+        if (ObjectHelper.isEmpty(configuration.getRegion())) {
+            setRegion(region);
+        }
         if (configuration.getAmazonMqClient() == null && (configuration.getAccessKey() == null || configuration.getSecretKey() == null)) {
             throw new IllegalArgumentException("amazonMQClient or accessKey and secretKey must be specified");
         }
         
         MQEndpoint endpoint = new MQEndpoint(uri, this, configuration);
         return endpoint;
+    }
+    
+    public MQConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    /**
+     * The AWS MQ default configuration
+     */
+    public void setConfiguration(MQConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    public String getAccessKey() {
+        return configuration.getAccessKey();
+    }
+
+    /**
+     * Amazon AWS Access Key
+     */
+    public void setAccessKey(String accessKey) {
+        configuration.setAccessKey(accessKey);
+    }
+
+    public String getSecretKey() {
+        return configuration.getSecretKey();
+    }
+
+    /**
+     * Amazon AWS Secret Key
+     */
+    public void setSecretKey(String secretKey) {
+        configuration.setSecretKey(secretKey);
+    }
+    
+    public String getRegion() {
+        return configuration.getRegion();
+    }
+
+    /**
+     * The region in which MQ client needs to work
+     */
+    public void setRegion(String region) {
+        configuration.setRegion(region);
     }
 
 }
