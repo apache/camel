47c7cd80ec6ff3bfdcb2afcc4f8dae79b3c935b5
Andrea Cosentino <ancosen@gmail.com>
2018-01-27 12:57:01 +0100

    Camel-AWS S3: Removed unused imports

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Endpoint.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Endpoint.java
@@ -26,7 +26,6 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.AmazonS3EncryptionClientBuilder;
-import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
