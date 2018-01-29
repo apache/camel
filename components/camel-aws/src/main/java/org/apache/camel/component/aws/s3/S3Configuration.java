2b2e96443cff58514592527edd89aca8045cd37f
Andrea Cosentino <ancosen@gmail.com>
2018-01-29 10:26:21 +0100

    CAMEL-12190 - Camel-AWS S3: Add a parameter to specify chunkedEncodingDisabled option - change name to the option, regen docs

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Configuration.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Configuration.java
@@ -73,7 +73,7 @@ public class S3Configuration implements Cloneable {
     @UriParam(label = "common,advanced", defaultValue = "false")
     private boolean useEncryption;
     @UriParam(label = "common, advanced", defaultValue = "false")
-    private boolean chunkedEncodingDisabled;
+    private boolean chunkedEncodingEnabled;
     @UriParam(label = "producer,advanced", defaultValue = "false")
     private boolean useAwsKMS;
     @UriParam(label = "producer,advanced")
@@ -384,17 +384,17 @@ public class S3Configuration implements Cloneable {
         this.awsKMSKeyId = awsKMSKeyId;
     }
 
-    public boolean isChunkedEncodingDisabled() {
-        return chunkedEncodingDisabled;
+    public boolean isChunkedEncodingEnabled() {
+        return chunkedEncodingEnabled;
     }
 
     /**
      * Define if disabled Chunked Encoding is true or false
      */
-    public void setChunkedEncodingDisabled(boolean chunkedEncodingDisabled) {
-        this.chunkedEncodingDisabled = chunkedEncodingDisabled;
+    public void setChunkedEncodingEnabled(boolean chunkedEncodingEnabled) {
+        this.chunkedEncodingEnabled = chunkedEncodingEnabled;
     }
-
+    
     boolean hasProxyConfiguration() {
         return ObjectHelper.isNotEmpty(getProxyHost()) && ObjectHelper.isNotEmpty(getProxyPort());
     }
