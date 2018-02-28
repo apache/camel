89c0d87607cc5bb4c5626b4dec682467179f7d6a
Farès Hassak <fares.hassak@gmail.com>
2018-02-28 22:23:53 +0100

    CAMEL-12302 : add bulk writes operation

--- components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/MongoDbComponent.java
+++ components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/MongoDbComponent.java
@@ -33,7 +33,8 @@ public class MongoDbComponent extends UriEndpointComponent {
     
     public static final Set<MongoDbOperation> WRITE_OPERATIONS = 
             new HashSet<MongoDbOperation>(Arrays.asList(MongoDbOperation.insert, MongoDbOperation.save, 
-                    MongoDbOperation.update, MongoDbOperation.remove));
+                    MongoDbOperation.update, MongoDbOperation.remove, MongoDbOperation.bulkWrite));
+    
     private static final Logger LOG = LoggerFactory.getLogger(MongoDbComponent.class);
 
     public MongoDbComponent() {
