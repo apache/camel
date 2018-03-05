ebbad8daac8028f868bb35cab3e3634874a78642
William Collins <punkhornsw@gmail.com>
2018-03-05 14:44:23 -0500

    [CAMEL-11257] Added and updated unit tests.

--- components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/AS2HeaderUtilsTest.java
+++ components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/AS2HeaderUtilsTest.java
@@ -26,15 +26,13 @@
  */
 package org.apache.camel.component.as2.api;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.apache.camel.component.as2.api.util.AS2HeaderUtils;
 import org.apache.camel.component.as2.api.util.AS2HeaderUtils.Parameter;
 import org.apache.http.message.ParserCursor;
 import org.apache.http.util.CharArrayBuffer;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class AS2HeaderUtilsTest {
@@ -47,31 +45,22 @@ public class AS2HeaderUtilsTest {
     private static String SIGNED_RECEIPT_MICALG_IMPORTANCE = "required";
     private static String[] SIGNED_RECEIPT_MICALG_VALUES = { "sha1" };
     
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
     public void parseNameValuePairTest() {
 
         final CharArrayBuffer buffer = new CharArrayBuffer(TEST_NAME_VALUES.length());
         buffer.append(TEST_NAME_VALUES);
         final ParserCursor cursor = new ParserCursor(0, TEST_NAME_VALUES.length());
-
         
         Parameter parameter = AS2HeaderUtils.parseNameValuePair(buffer, cursor);
         assertEquals("Unexpected value for parameter attribute", SIGNED_RECEIPT_PROTOCOL_ATTRIBUTE, parameter.getAttribute());
         assertEquals("Unexpected value for parameter importance", SIGNED_RECEIPT_PROTOCOL_IMPORTANCE, parameter.getImportance().getImportance());
-        assertArrayEquals("Unexpected value for parameter importance", SIGNED_RECEIPT_PROTOCOL_VALUES, parameter.getValues());
+        assertArrayEquals("Unexpected value for parameter values", SIGNED_RECEIPT_PROTOCOL_VALUES, parameter.getValues());
         
         parameter = AS2HeaderUtils.parseNameValuePair(buffer, cursor);
         assertEquals("Unexpected value for parameter attribute", SIGNED_RECEIPT_MICALG_ATTRIBUTE, parameter.getAttribute());
         assertEquals("Unexpected value for parameter importance", SIGNED_RECEIPT_MICALG_IMPORTANCE, parameter.getImportance().getImportance());
-        assertArrayEquals("Unexpected value for parameter importance", SIGNED_RECEIPT_MICALG_VALUES, parameter.getValues());
+        assertArrayEquals("Unexpected value for parameter values", SIGNED_RECEIPT_MICALG_VALUES, parameter.getValues());
     }
 
 }
---
+++ components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/DispositionNotificationOptionsParserTest.java
@@ -0,0 +1,49 @@
+package org.apache.camel.component.as2.api;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.camel.component.as2.api.entity.DispositionNotificationOptions;
+import org.apache.camel.component.as2.api.entity.DispositionNotificationOptionsParser;
+import org.apache.camel.component.as2.api.util.AS2HeaderUtils.Parameter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DispositionNotificationOptionsParserTest {
+
+    private static String TEST_NAME_VALUES = " signed-receipt-protocol   =   optional  , pkcs7-signature  ;    signed-receipt-micalg   =    required  ,  sha1  ";
+    private static String SIGNED_RECEIPT_PROTOCOL_ATTRIBUTE = "signed-receipt-protocol";
+    private static String SIGNED_RECEIPT_PROTOCOL_IMPORTANCE = "optional";
+    private static String[] SIGNED_RECEIPT_PROTOCOL_VALUES = { "pkcs7-signature" };
+    private static String SIGNED_RECEIPT_MICALG_ATTRIBUTE = "signed-receipt-micalg";
+    private static String SIGNED_RECEIPT_MICALG_IMPORTANCE = "required";
+    private static String[] SIGNED_RECEIPT_MICALG_VALUES = { "sha1" };
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void parseDispositionNotificationOptionsTest() {
+
+        DispositionNotificationOptions dispositionNotificationOptions =  DispositionNotificationOptionsParser.parseDispositionNotificationOptions(TEST_NAME_VALUES, null);
+        Parameter signedReceiptProtocol = dispositionNotificationOptions.getSignedReceiptProtocol();
+        assertNotNull("signed receipt protocol not parsed", signedReceiptProtocol);
+        assertEquals("Unexpected value for signed receipt protocol attribute", SIGNED_RECEIPT_PROTOCOL_ATTRIBUTE, signedReceiptProtocol.getAttribute());
+        assertEquals("Unexpected value for signed receipt protocol importance", SIGNED_RECEIPT_PROTOCOL_IMPORTANCE, signedReceiptProtocol.getImportance().getImportance());
+        assertArrayEquals("Unexpected value for parameter importance", SIGNED_RECEIPT_PROTOCOL_VALUES, signedReceiptProtocol.getValues());
+
+        Parameter signedReceiptMicalg = dispositionNotificationOptions.getSignedReceiptMicalg();
+        assertNotNull("signed receipt micalg not parsed", signedReceiptProtocol);
+        assertEquals("Unexpected value for signed receipt micalg attribute", SIGNED_RECEIPT_MICALG_ATTRIBUTE, signedReceiptMicalg.getAttribute());
+        assertEquals("Unexpected value for signed receipt micalg importance", SIGNED_RECEIPT_MICALG_IMPORTANCE, signedReceiptMicalg.getImportance().getImportance());
+        assertArrayEquals("Unexpected value for micalg importance", SIGNED_RECEIPT_MICALG_VALUES, signedReceiptMicalg.getValues());
+    }
+
+}
