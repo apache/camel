2821cb7eb27ba627351173446e6f5917176858ac
Andrea Cosentino <ancosen@gmail.com>
2018-03-21 10:51:40 +0100

    Polished

--- components/camel-elasticsearch-rest/src/main/java/org/apache/camel/component/elasticsearch/ElasticsearchProducer.java
+++ components/camel-elasticsearch-rest/src/main/java/org/apache/camel/component/elasticsearch/ElasticsearchProducer.java
@@ -21,7 +21,6 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.camel.Exchange;
