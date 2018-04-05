33517b03c2a09a356fe2ce2453239ebc1fade160
Andrea Cosentino <ancosen@gmail.com>
2018-04-05 08:31:28 +0200

    Upgrade Ignite to version 2.4.0

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelIgniteTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelIgniteTest.java
@@ -47,7 +47,7 @@ public class CamelIgniteTest extends BaseKarafTest {
     @Test
     public void test() throws Exception {
         // install ignite first
-        String version = "2.3.0";
+        String version = "2.4.0";
         LOG.info("Using Apache Ignite version: {}", version);
         URI url = new URI("mvn:org.apache.ignite/ignite-osgi-karaf/" + version + "/xml/features");
 
