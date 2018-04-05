959b6ec8fa5c6d6c42510bdca5411e0784b4eee2
Zoran Regvart <zregvart@apache.org>
2018-04-05 12:04:00 +0200

    Remove unused imports

--- camel-core/src/test/java/org/apache/camel/processor/ShutdownNotDeferTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/ShutdownNotDeferTest.java
@@ -26,7 +26,6 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.file.FileConsumer;
 import org.apache.camel.component.file.FileEndpoint;
 import org.apache.camel.component.file.GenericFileOperations;
-import org.apache.camel.component.file.strategy.GenericFileNoOpProcessStrategy;
 import org.apache.camel.component.mock.MockEndpoint;
 
 import static org.apache.camel.ShutdownRoute.Default;
