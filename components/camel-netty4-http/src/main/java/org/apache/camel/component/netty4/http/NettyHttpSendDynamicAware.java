094572e387e5f209955a43f756040b5f4b70bd38
Claus Ibsen <claus.ibsen@gmail.com>
2018-04-26 09:41:26 +0200

    CAMEL-12462: Better name thanks to Alex for suggestion.

--- components/camel-netty4-http/src/main/java/org/apache/camel/component/netty4/http/NettyHttpSendDynamicAware.java
+++ components/camel-netty4-http/src/main/java/org/apache/camel/component/netty4/http/NettyHttpSendDynamicAware.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,11 +29,11 @@ public class NettyHttpSendDynamicAware extends HttpSendDynamicAware {
         String port = entry.getProperties().get("port");
         String path = entry.getProperties().get("path");
 
-        String s = scheme + "://" + host;
+        String baseUrl = scheme + "://" + host;
         if (port != null) {
-            s += ":" + port;
+            baseUrl += ":" + port;
         }
-        return new String[]{s, path};
+        return new String[]{baseUrl, path};
     }
 
 }
