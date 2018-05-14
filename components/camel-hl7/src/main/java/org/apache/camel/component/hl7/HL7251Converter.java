a39051c542f640bd3b6829920899943d2ac78b66
Christian Ohr <christian.ohr@gmail.com>
2018-05-14 08:59:42 +0200

    CAMEL-12493:make type converters and its required dependencies optional

--- components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL7251Converter.java
+++ components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL7251Converter.java
@@ -238,7 +238,7 @@ import org.apache.camel.converter.IOConverter;
 /**
  * HL7 converters.
  */
-@Converter
+@Converter(ignoreOnLoadError = true)
 public final class HL7251Converter {
 
     private static final HapiContext DEFAULT_CONTEXT;
