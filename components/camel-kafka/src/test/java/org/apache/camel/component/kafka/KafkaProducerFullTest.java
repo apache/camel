340b13d888b3f139b19681916757b166d191cdaa
Claus Ibsen <claus.ibsen@gmail.com>
2018-05-15 13:50:10 +0200

    CAMEL-12503: Fixed test as breadcrumbId header is included by default.

--- components/camel-kafka/src/test/java/org/apache/camel/component/kafka/KafkaProducerFullTest.java
+++ components/camel-kafka/src/test/java/org/apache/camel/component/kafka/KafkaProducerFullTest.java
@@ -336,7 +336,8 @@ public class KafkaProducerFullTest extends BaseEmbeddedKafkaTest {
         ConsumerRecord<String, String> record = records.get(0);
         Headers headers = record.headers();
         assertNotNull("Kafka Headers should not be null.", headers);
-        assertEquals("One propagated header is expected.", 5, headers.toArray().length);
+        // we have 5 headers and 1 header with breadcrumbId
+        assertEquals("One propagated header is expected.", 6, headers.toArray().length);
         assertEquals("Propagated string value received", propagatedStringHeaderValue,
                 new String(getHeaderValue(propagatedStringHeaderKey, headers)));
         assertEquals("Propagated integer value received", propagatedIntegerHeaderValue,
