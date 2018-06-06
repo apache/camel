eba729afff34794872d3b3b51eb8e33c0f223804
Andrea Cosentino <ancosen@gmail.com>
2018-06-06 16:29:02 +0200

    CAMEL-12537 - Moved to compliant package

--- components/camel-google-mail/src/main/java/org/apache/camel/component/google/mail/stream/GoogleMailStreamConsumer.java
+++ components/camel-google-mail/src/main/java/org/apache/camel/component/google/mail/stream/GoogleMailStreamConsumer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.mail.stream;
+package org.apache.camel.component.google.mail.stream;
 
 import java.io.IOException;
 import java.util.ArrayList;
