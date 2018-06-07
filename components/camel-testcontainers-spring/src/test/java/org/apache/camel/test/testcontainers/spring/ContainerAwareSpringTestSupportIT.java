225f1a997d77e18b0a3535695b896ae125ca15c3
Zoran Regvart <zregvart@apache.org>
2018-06-07 15:20:08 +0200

    Move ContainerAwareSpringTestSupportIT to package
    (fix bad commit)

--- components/camel-testcontainers-spring/src/test/java/org/apache/camel/test/testcontainers/spring/ContainerAwareSpringTestSupportIT.java
+++ components/camel-testcontainers-spring/src/test/java/org/apache/camel/test/testcontainers/spring/ContainerAwareSpringTestSupportIT.java
@@ -1,3 +1,4 @@
+package org.apache.camel.test.testcontainers.spring;
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
