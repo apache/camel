c3bfdca7cf509b62aa6f89cb80112bd75aebc7ca
Claus Ibsen <claus.ibsen@gmail.com>
2018-06-09 15:42:45 +0200

    CAMEL-12567: camel-stream - Add support for configuring timeout for HTTP urls

--- components/camel-stream/src/main/java/org/apache/camel/component/stream/StreamConsumer.java
+++ components/camel-stream/src/main/java/org/apache/camel/component/stream/StreamConsumer.java
@@ -296,6 +296,12 @@ public class StreamConsumer extends DefaultConsumer implements Runnable {
 
         URL url = new URL(u);
         URLConnection c = url.openConnection();
+        if (endpoint.getConnectTimeout() > 0) {
+            c.setConnectTimeout(endpoint.getConnectTimeout());
+        }
+        if (endpoint.getReadTimeout() > 0) {
+            c.setReadTimeout(endpoint.getReadTimeout());
+        }
         if (endpoint.getHttpHeaders() != null) {
             endpoint.getHttpHeaders().forEach((k, v) -> c.addRequestProperty(k, v.toString()));
         }
