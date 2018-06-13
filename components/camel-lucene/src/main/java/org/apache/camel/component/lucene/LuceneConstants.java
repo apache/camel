f76d8bcbff81c7180003a68f32fa3dd880ec8f9b
Andrea Cosentino <ancosen@gmail.com>
2018-06-13 09:26:58 +0200

    CAMEL-12576 - Bump to Lucene and Solr 7.2.1

--- components/camel-lucene/src/main/java/org/apache/camel/component/lucene/LuceneConstants.java
+++ components/camel-lucene/src/main/java/org/apache/camel/component/lucene/LuceneConstants.java
@@ -19,5 +19,5 @@ package org.apache.camel.component.lucene;
 import org.apache.lucene.util.Version;
 
 public interface LuceneConstants {
-    Version LUCENE_VERSION = Version.LUCENE_7_0_0;
+    Version LUCENE_VERSION = Version.LUCENE_7_2_1;
 }
