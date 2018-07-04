5bf5975b9206ae4bd26b2c432aedb6485e62a32a
önder sezgin <onder.sezgin@nokia.com>
2018-07-04 09:38:38 +0300

    if channel.getExistStatus() is null, then calling
    this constructor may fail due to auto-unboxing with potential NullPointerException

--- components/camel-ssh/src/main/java/org/apache/camel/component/ssh/SshResult.java
+++ components/camel-ssh/src/main/java/org/apache/camel/component/ssh/SshResult.java
@@ -36,13 +36,13 @@ public class SshResult {
     
     private final String command;
     
-    private final int exitValue;
+    private final Integer exitValue;
 
     private final InputStream stdout;
 
     private final InputStream stderr;
     
-    public SshResult(String command, int exitValue, InputStream out, InputStream err) {
+    public SshResult(String command, Integer exitValue, InputStream out, InputStream err) {
         this.command = command;
         this.exitValue = exitValue;
         this.stdout = out;
@@ -53,7 +53,7 @@ public class SshResult {
         return command;
     }
 
-    public int getExitValue() {
+    public Integer getExitValue() {
         return exitValue;
     }
 
