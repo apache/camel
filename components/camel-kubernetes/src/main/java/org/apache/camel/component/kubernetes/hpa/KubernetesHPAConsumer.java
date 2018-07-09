2e2d356993e9ed2f7aa38bbbb9827fcb187e9531
Andrea Cosentino <ancosen@gmail.com>
2018-07-09 11:25:30 +0200

    CAMEL-12583 - Fixed CS

--- components/camel-kubernetes/src/main/java/org/apache/camel/component/kubernetes/hpa/KubernetesHPAConsumer.java
+++ components/camel-kubernetes/src/main/java/org/apache/camel/component/kubernetes/hpa/KubernetesHPAConsumer.java
@@ -18,6 +18,15 @@ package org.apache.camel.component.kubernetes.hpa;
 
 import java.util.concurrent.ExecutorService;
 
+import io.fabric8.kubernetes.api.model.DoneableHorizontalPodAutoscaler;
+import io.fabric8.kubernetes.api.model.HorizontalPodAutoscaler;
+import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerList;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.Watch;
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.component.kubernetes.AbstractKubernetesEndpoint;
@@ -28,116 +37,104 @@ import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.fabric8.kubernetes.api.model.DoneableHorizontalPodAutoscaler;
-import io.fabric8.kubernetes.api.model.HorizontalPodAutoscaler;
-import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerList;
-import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.Watch;
-import io.fabric8.kubernetes.client.Watcher;
-import io.fabric8.kubernetes.client.dsl.MixedOperation;
-import io.fabric8.kubernetes.client.dsl.Resource;
-
 public class KubernetesHPAConsumer extends DefaultConsumer {
 
-	private static final Logger LOG = LoggerFactory.getLogger(KubernetesHPAConsumer.class);
+    private static final Logger LOG = LoggerFactory.getLogger(KubernetesHPAConsumer.class);
 
-	private final Processor processor;
-	private ExecutorService executor;
-	private HpaConsumerTask hpasWatcher;
+    private final Processor processor;
+    private ExecutorService executor;
+    private HpaConsumerTask hpasWatcher;
 
-	public KubernetesHPAConsumer(AbstractKubernetesEndpoint endpoint, Processor processor) {
-		super(endpoint, processor);
-		this.processor = processor;
-	}
+    public KubernetesHPAConsumer(AbstractKubernetesEndpoint endpoint, Processor processor) {
+        super(endpoint, processor);
+        this.processor = processor;
+    }
 
-	@Override
-	public AbstractKubernetesEndpoint getEndpoint() {
-		return (AbstractKubernetesEndpoint) super.getEndpoint();
-	}
+    @Override
+    public AbstractKubernetesEndpoint getEndpoint() {
+        return (AbstractKubernetesEndpoint)super.getEndpoint();
+    }
 
-	@Override
-	protected void doStart() throws Exception {
-		super.doStart();
-		executor = getEndpoint().createExecutor();
+    @Override
+    protected void doStart() throws Exception {
+        super.doStart();
+        executor = getEndpoint().createExecutor();
 
-		hpasWatcher = new HpaConsumerTask();
-		executor.submit(hpasWatcher);
-	}
+        hpasWatcher = new HpaConsumerTask();
+        executor.submit(hpasWatcher);
+    }
 
-	@Override
-	protected void doStop() throws Exception {
-		super.doStop();
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
 
-		LOG.debug("Stopping Kubernetes HPA Consumer");
-		if (executor != null) {
-			if (getEndpoint() != null && getEndpoint().getCamelContext() != null) {
-				if (hpasWatcher != null) {
-					hpasWatcher.getWatch().close();
-				}
-				getEndpoint().getCamelContext().getExecutorServiceManager().shutdownNow(executor);
-			} else {
-				if (hpasWatcher != null) {
-					hpasWatcher.getWatch().close();
-				}
-				executor.shutdownNow();
-			}
-		}
-		executor = null;
-	}
+        LOG.debug("Stopping Kubernetes HPA Consumer");
+        if (executor != null) {
+            if (getEndpoint() != null && getEndpoint().getCamelContext() != null) {
+                if (hpasWatcher != null) {
+                    hpasWatcher.getWatch().close();
+                }
+                getEndpoint().getCamelContext().getExecutorServiceManager().shutdownNow(executor);
+            } else {
+                if (hpasWatcher != null) {
+                    hpasWatcher.getWatch().close();
+                }
+                executor.shutdownNow();
+            }
+        }
+        executor = null;
+    }
 
-	class HpaConsumerTask implements Runnable {
+    class HpaConsumerTask implements Runnable {
 
-		private Watch watch;
+        private Watch watch;
 
-		@Override
-		public void run() {
-			MixedOperation<HorizontalPodAutoscaler, HorizontalPodAutoscalerList, DoneableHorizontalPodAutoscaler, Resource<HorizontalPodAutoscaler, DoneableHorizontalPodAutoscaler>> w = getEndpoint()
-					.getKubernetesClient().autoscaling().horizontalPodAutoscalers();
-			if (ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getNamespace())) {
-				w.inNamespace(getEndpoint().getKubernetesConfiguration().getNamespace());
-			}
-			if (ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getLabelKey())
-					&& ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getLabelValue())) {
-				w.withLabel(getEndpoint().getKubernetesConfiguration().getLabelKey(),
-						getEndpoint().getKubernetesConfiguration().getLabelValue());
-			}
-			if (ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getResourceName())) {
-				w.withName(getEndpoint().getKubernetesConfiguration().getResourceName());
-			}
-			watch = w.watch(new Watcher<HorizontalPodAutoscaler>() {
+        @Override
+        public void run() {
+            MixedOperation<HorizontalPodAutoscaler, HorizontalPodAutoscalerList, DoneableHorizontalPodAutoscaler, Resource<HorizontalPodAutoscaler, DoneableHorizontalPodAutoscaler>> w = getEndpoint()
+                .getKubernetesClient().autoscaling().horizontalPodAutoscalers();
+            if (ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getNamespace())) {
+                w.inNamespace(getEndpoint().getKubernetesConfiguration().getNamespace());
+            }
+            if (ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getLabelKey())
+                && ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getLabelValue())) {
+                w.withLabel(getEndpoint().getKubernetesConfiguration().getLabelKey(), getEndpoint().getKubernetesConfiguration().getLabelValue());
+            }
+            if (ObjectHelper.isNotEmpty(getEndpoint().getKubernetesConfiguration().getResourceName())) {
+                w.withName(getEndpoint().getKubernetesConfiguration().getResourceName());
+            }
+            watch = w.watch(new Watcher<HorizontalPodAutoscaler>() {
 
-				@Override
-				public void eventReceived(io.fabric8.kubernetes.client.Watcher.Action action,
-						HorizontalPodAutoscaler resource) {
-					HPAEvent hpae = new HPAEvent(action, resource);
-					Exchange exchange = getEndpoint().createExchange();
-					exchange.getIn().setBody(hpae.getHpa());
-					exchange.getIn().setHeader(KubernetesConstants.KUBERNETES_EVENT_ACTION, hpae.getAction());
-					exchange.getIn().setHeader(KubernetesConstants.KUBERNETES_EVENT_TIMESTAMP,
-							System.currentTimeMillis());
-					try {
-						processor.process(exchange);
-					} catch (Exception e) {
-						getExceptionHandler().handleException("Error during processing", exchange, e);
-					}
-				}
+                @Override
+                public void eventReceived(io.fabric8.kubernetes.client.Watcher.Action action, HorizontalPodAutoscaler resource) {
+                    HPAEvent hpae = new HPAEvent(action, resource);
+                    Exchange exchange = getEndpoint().createExchange();
+                    exchange.getIn().setBody(hpae.getHpa());
+                    exchange.getIn().setHeader(KubernetesConstants.KUBERNETES_EVENT_ACTION, hpae.getAction());
+                    exchange.getIn().setHeader(KubernetesConstants.KUBERNETES_EVENT_TIMESTAMP, System.currentTimeMillis());
+                    try {
+                        processor.process(exchange);
+                    } catch (Exception e) {
+                        getExceptionHandler().handleException("Error during processing", exchange, e);
+                    }
+                }
 
-				@Override
-				public void onClose(KubernetesClientException cause) {
-					if (cause != null) {
-						LOG.error(cause.getMessage(), cause);
-					}
+                @Override
+                public void onClose(KubernetesClientException cause) {
+                    if (cause != null) {
+                        LOG.error(cause.getMessage(), cause);
+                    }
 
-				}
-			});
-		}
+                }
+            });
+        }
 
-		public Watch getWatch() {
-			return watch;
-		}
+        public Watch getWatch() {
+            return watch;
+        }
 
-		public void setWatch(Watch watch) {
-			this.watch = watch;
-		}
-	}
+        public void setWatch(Watch watch) {
+            this.watch = watch;
+        }
+    }
 }
--- components/camel-kubernetes/src/main/java/org/apache/camel/component/kubernetes/hpa/KubernetesHPAProducer.java
+++ components/camel-kubernetes/src/main/java/org/apache/camel/component/kubernetes/hpa/KubernetesHPAProducer.java
@@ -18,6 +18,14 @@ package org.apache.camel.component.kubernetes.hpa;
 
 import java.util.Map;
 
+import io.fabric8.kubernetes.api.model.HorizontalPodAutoscaler;
+import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerBuilder;
+import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerList;
+import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerSpec;
+import io.fabric8.kubernetes.client.Watch;
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.dsl.FilterWatchListMultiDeletable;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.component.kubernetes.AbstractKubernetesEndpoint;
 import org.apache.camel.component.kubernetes.KubernetesConstants;
@@ -28,18 +36,9 @@ import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.fabric8.kubernetes.api.model.HorizontalPodAutoscaler;
-import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerBuilder;
-import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerList;
-import io.fabric8.kubernetes.api.model.HorizontalPodAutoscalerSpec;
-import io.fabric8.kubernetes.client.Watch;
-import io.fabric8.kubernetes.client.Watcher;
-import io.fabric8.kubernetes.client.dsl.FilterWatchListMultiDeletable;
-
 public class KubernetesHPAProducer extends DefaultProducer {
 
-    private static final Logger LOG = LoggerFactory
-            .getLogger(KubernetesHPAProducer.class);
+    private static final Logger LOG = LoggerFactory.getLogger(KubernetesHPAProducer.class);
 
     public KubernetesHPAProducer(AbstractKubernetesEndpoint endpoint) {
         super(endpoint);
@@ -47,20 +46,17 @@ public class KubernetesHPAProducer extends DefaultProducer {
 
     @Override
     public AbstractKubernetesEndpoint getEndpoint() {
-        return (AbstractKubernetesEndpoint) super.getEndpoint();
+        return (AbstractKubernetesEndpoint)super.getEndpoint();
     }
 
     @Override
     public void process(Exchange exchange) throws Exception {
         String operation;
 
-        if (ObjectHelper.isEmpty(getEndpoint().getKubernetesConfiguration()
-                .getOperation())) {
-            operation = exchange.getIn().getHeader(
-                    KubernetesConstants.KUBERNETES_OPERATION, String.class);
+        if (ObjectHelper.isEmpty(getEndpoint().getKubernetesConfiguration().getOperation())) {
+            operation = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_OPERATION, String.class);
         } else {
-            operation = getEndpoint().getKubernetesConfiguration()
-                    .getOperation();
+            operation = getEndpoint().getKubernetesConfiguration().getOperation();
         }
 
         switch (operation) {
@@ -68,134 +64,109 @@ public class KubernetesHPAProducer extends DefaultProducer {
         case KubernetesOperations.LIST_HPA:
             doList(exchange, operation);
             break;
-            
+
         case KubernetesOperations.LIST_HPA_BY_LABELS_OPERATION:
-        	doListHPAByLabel(exchange, operation);
+            doListHPAByLabel(exchange, operation);
             break;
 
         case KubernetesOperations.GET_HPA_OPERATION:
-        	doGetHPA(exchange, operation);
+            doGetHPA(exchange, operation);
             break;
 
         case KubernetesOperations.CREATE_HPA_OPERATION:
-        	doCreateHPA(exchange, operation);
+            doCreateHPA(exchange, operation);
             break;
-            
+
         case KubernetesOperations.DELETE_HPA_OPERATION:
-        	doDeleteHPA(exchange, operation);
+            doDeleteHPA(exchange, operation);
             break;
-            
+
         default:
-            throw new IllegalArgumentException("Unsupported operation "
-                    + operation);
+            throw new IllegalArgumentException("Unsupported operation " + operation);
         }
     }
 
     protected void doList(Exchange exchange, String operation) throws Exception {
         HorizontalPodAutoscalerList hpaList = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers().list();
-        
+
         MessageHelper.copyHeaders(exchange.getIn(), exchange.getOut(), true);
         exchange.getOut().setBody(hpaList.getItems());
     }
 
     protected void doListHPAByLabel(Exchange exchange, String operation) {
-        Map<String, String> labels = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_PODS_LABELS, Map.class);
+        Map<String, String> labels = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_PODS_LABELS, Map.class);
         if (ObjectHelper.isEmpty(labels)) {
             LOG.error("Get HPA by labels require specify a labels set");
-            throw new IllegalArgumentException(
-                    "Get HPA by labels require specify a labels set");
+            throw new IllegalArgumentException("Get HPA by labels require specify a labels set");
         }
-        
-        FilterWatchListMultiDeletable<HorizontalPodAutoscaler, HorizontalPodAutoscalerList, Boolean, Watch, Watcher<HorizontalPodAutoscaler>> hpas = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers();
+
+        FilterWatchListMultiDeletable<HorizontalPodAutoscaler, HorizontalPodAutoscalerList, Boolean, Watch, Watcher<HorizontalPodAutoscaler>> hpas = getEndpoint()
+            .getKubernetesClient().autoscaling().horizontalPodAutoscalers();
         for (Map.Entry<String, String> entry : labels.entrySet()) {
-        	hpas.withLabel(entry.getKey(), entry.getValue());
+            hpas.withLabel(entry.getKey(), entry.getValue());
         }
         HorizontalPodAutoscalerList hpaList = hpas.list();
-        
+
         MessageHelper.copyHeaders(exchange.getIn(), exchange.getOut(), true);
         exchange.getOut().setBody(hpaList.getItems());
     }
-    
-    protected void doGetHPA(Exchange exchange, String operation)
-            throws Exception {
+
+    protected void doGetHPA(Exchange exchange, String operation) throws Exception {
         HorizontalPodAutoscaler hpa = null;
-        String podName = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_HPA_NAME, String.class);
-        String namespaceName = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_NAMESPACE_NAME, String.class);
+        String podName = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_HPA_NAME, String.class);
+        String namespaceName = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, String.class);
         if (ObjectHelper.isEmpty(podName)) {
             LOG.error("Get a specific hpa require specify an hpa name");
-            throw new IllegalArgumentException(
-                    "Get a specific hpa require specify an hpa name");
+            throw new IllegalArgumentException("Get a specific hpa require specify an hpa name");
         }
         if (ObjectHelper.isEmpty(namespaceName)) {
             LOG.error("Get a specific hpa require specify a namespace name");
-            throw new IllegalArgumentException(
-                    "Get a specific hpa require specify a namespace name");
+            throw new IllegalArgumentException("Get a specific hpa require specify a namespace name");
         }
-        hpa = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers()
-                .inNamespace(namespaceName).withName(podName).get();
-        
+        hpa = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers().inNamespace(namespaceName).withName(podName).get();
+
         MessageHelper.copyHeaders(exchange.getIn(), exchange.getOut(), true);
         exchange.getOut().setBody(hpa);
     }
-    
-    protected void doCreateHPA(Exchange exchange, String operation)
-            throws Exception {
+
+    protected void doCreateHPA(Exchange exchange, String operation) throws Exception {
         HorizontalPodAutoscaler hpa = null;
-        String hpaName = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_HPA_NAME, String.class);
-        String namespaceName = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_NAMESPACE_NAME, String.class);
-        HorizontalPodAutoscalerSpec hpaSpec = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_HPA_SPEC, HorizontalPodAutoscalerSpec.class);
+        String hpaName = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_HPA_NAME, String.class);
+        String namespaceName = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, String.class);
+        HorizontalPodAutoscalerSpec hpaSpec = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_HPA_SPEC, HorizontalPodAutoscalerSpec.class);
         if (ObjectHelper.isEmpty(hpaName)) {
             LOG.error("Create a specific hpa require specify a hpa name");
-            throw new IllegalArgumentException(
-                    "Create a specific hpa require specify a hpa name");
+            throw new IllegalArgumentException("Create a specific hpa require specify a hpa name");
         }
         if (ObjectHelper.isEmpty(namespaceName)) {
             LOG.error("Create a specific hpa require specify a namespace name");
-            throw new IllegalArgumentException(
-                    "Create a specific hpa require specify a namespace name");
+            throw new IllegalArgumentException("Create a specific hpa require specify a namespace name");
         }
         if (ObjectHelper.isEmpty(hpaSpec)) {
             LOG.error("Create a specific hpa require specify a hpa spec bean");
-            throw new IllegalArgumentException(
-                    "Create a specific hpa require specify a hpa spec bean");
+            throw new IllegalArgumentException("Create a specific hpa require specify a hpa spec bean");
         }
-        Map<String, String> labels = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_PODS_LABELS, Map.class);
-        HorizontalPodAutoscaler hpaCreating = new HorizontalPodAutoscalerBuilder().withNewMetadata()
-                .withName(hpaName).withLabels(labels).endMetadata()
-                .withSpec(hpaSpec).build();
-        hpa = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers()
-                .inNamespace(namespaceName).create(hpaCreating);
-        
+        Map<String, String> labels = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_PODS_LABELS, Map.class);
+        HorizontalPodAutoscaler hpaCreating = new HorizontalPodAutoscalerBuilder().withNewMetadata().withName(hpaName).withLabels(labels).endMetadata().withSpec(hpaSpec).build();
+        hpa = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers().inNamespace(namespaceName).create(hpaCreating);
+
         MessageHelper.copyHeaders(exchange.getIn(), exchange.getOut(), true);
         exchange.getOut().setBody(hpa);
     }
-    
-    protected void doDeleteHPA(Exchange exchange, String operation)
-            throws Exception {
-        String hpaName = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_HPA_NAME, String.class);
-        String namespaceName = exchange.getIn().getHeader(
-                KubernetesConstants.KUBERNETES_NAMESPACE_NAME, String.class);
+
+    protected void doDeleteHPA(Exchange exchange, String operation) throws Exception {
+        String hpaName = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_HPA_NAME, String.class);
+        String namespaceName = exchange.getIn().getHeader(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, String.class);
         if (ObjectHelper.isEmpty(hpaName)) {
             LOG.error("Delete a specific hpa require specify a hpa name");
-            throw new IllegalArgumentException(
-                    "Delete a specific hpa require specify a hpa name");
+            throw new IllegalArgumentException("Delete a specific hpa require specify a hpa name");
         }
         if (ObjectHelper.isEmpty(namespaceName)) {
             LOG.error("Delete a specific hpa require specify a namespace name");
-            throw new IllegalArgumentException(
-                    "Delete a specific hpa require specify a namespace name");
+            throw new IllegalArgumentException("Delete a specific hpa require specify a namespace name");
         }
-        boolean hpaDeleted = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers()
-                .inNamespace(namespaceName).withName(hpaName).delete();
-        
+        boolean hpaDeleted = getEndpoint().getKubernetesClient().autoscaling().horizontalPodAutoscalers().inNamespace(namespaceName).withName(hpaName).delete();
+
         MessageHelper.copyHeaders(exchange.getIn(), exchange.getOut(), true);
         exchange.getOut().setBody(hpaDeleted);
     }
