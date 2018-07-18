5fc165bc664d7e84d50d714c9718c2bfc5efe578
Andrea Cosentino <ancosen@gmail.com>
2018-07-18 12:24:19 +0200

    CAMEL-11158 - Camel-Kubernetes: Add support for Job resources

--- components/camel-kubernetes/src/main/java/org/apache/camel/component/kubernetes/KubernetesConstants.java
+++ components/camel-kubernetes/src/main/java/org/apache/camel/component/kubernetes/KubernetesConstants.java
@@ -60,6 +60,9 @@ public interface KubernetesConstants {
     String KUBERNETES_DEPLOYMENT_REPLICAS = "CamelKubernetesDeploymentReplicas";
     String KUBERNETES_HPA_NAME = "CamelKubernetesHPAName";
     String KUBERNETES_HPA_SPEC = "CamelKubernetesHPASpec";
+    String KUBERNETES_JOB_NAME = "CamelKubernetesJobName";
+    String KUBERNETES_JOB_SPEC = "CamelKubernetesJobSpec";
+    String KUBERNETES_JOB_LABELS = "CamelKubernetesJobLabels";
 
     // Consumer
     String KUBERNETES_EVENT_ACTION = "CamelKubernetesEventAction";
