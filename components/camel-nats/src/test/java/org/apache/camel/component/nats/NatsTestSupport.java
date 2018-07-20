98b25c89b920afc1e8c60729c78503f32ef4d708
Andrea Cosentino <ancosen@gmail.com>
2018-07-20 09:53:50 +0200

    Camel-Nats: Fixed CS

--- components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsTestSupport.java
+++ components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsTestSupport.java
@@ -1,10 +1,26 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.nats;
 
 import org.apache.camel.test.testcontainers.ContainerAwareTestSupport;
 import org.apache.camel.test.testcontainers.Wait;
 import org.testcontainers.containers.GenericContainer;
 
-public class NatsTestSupport extends ContainerAwareTestSupport{
+public class NatsTestSupport extends ContainerAwareTestSupport {
 
     public static final String CONTAINER_IMAGE = "nats:1.2.0";
     public static final String CONTAINER_NAME = "nats";
