64ea426cf3812e35ce433899219fde6892ed1dc4
Saravanakumar Selvaraj <saravanakumar.java@gmail.com>
2018-07-25 02:56:57 +0530

    CAMEL-12681 : BreadcrumbId not required for aws-sqs aws-sns endpoints

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/sns/SnsHeaderFilterStrategy.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/sns/SnsHeaderFilterStrategy.java
@@ -25,6 +25,6 @@ public class SnsHeaderFilterStrategy extends DefaultHeaderFilterStrategy {
 
     protected void initialize() {
         // filter headers begin with "Camel" or "org.apache.camel"
-        setOutFilterPattern("(Camel|org\\.apache\\.camel)[\\.|a-z|A-z|0-9]*"); 
+        setOutFilterPattern("(breadcrumbId|Camel|org\\.apache\\.camel)[\\.|a-z|A-z|0-9]*"); 
     }
 }
