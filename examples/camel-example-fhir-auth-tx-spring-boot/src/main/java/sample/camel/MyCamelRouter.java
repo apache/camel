f68af36c00a638fe73b429d62a6b78ac9872af49
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2018-08-20 19:27:44 +0200

    CAMEL-12736 Create FHIR authorization and transaction quickstart (#2484)

--- examples/camel-example-fhir-auth-tx-spring-boot/src/main/java/sample/camel/MyCamelRouter.java
+++ examples/camel-example-fhir-auth-tx-spring-boot/src/main/java/sample/camel/MyCamelRouter.java
@@ -16,19 +16,19 @@
  */
 package sample.camel;
 
-import ca.uhn.hl7v2.HL7Exception;
-import ca.uhn.hl7v2.model.v24.message.ORU_R01;
-import ca.uhn.hl7v2.model.v24.segment.PID;
+import org.apache.camel.builder.RouteBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.builder.RouteBuilder;
 import org.apache.http.ProtocolException;
-
 import org.hl7.fhir.dstu3.model.Patient;
 import org.springframework.stereotype.Component;
 
+
 /**
- * A simple Camel route that triggers from a file and posts to a FHIR server.
+ * A simple Camel route that triggers from a file and pushes to a FHIR server.
  * <p/>
  * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
  */
@@ -42,33 +42,25 @@ public class MyCamelRouter extends RouteBuilder {
                     .handled(true)
                     .log(LoggingLevel.ERROR, "Error connecting to FHIR server with URL:{{serverUrl}}, please check the application.properties file ${exception.message}")
                     .end()
-                .onException(HL7Exception.class)
-                    .handled(true)
-                    .log(LoggingLevel.ERROR, "Error unmarshalling ${file:name} ${exception.message}")
-                    .end()
                 .log("Converting ${file:name}")
-                // unmarshall file to hl7 message
-                .unmarshal().hl7()
-                // very simple mapping from a HLV2 patient to dstu3 patient
+                .unmarshal().csv()
                 .process(exchange -> {
-                    ORU_R01 msg = exchange.getIn().getBody(ORU_R01.class);
-                    final PID pid = msg.getPATIENT_RESULT().getPATIENT().getPID();
-                    String surname = pid.getPatientName()[0].getFamilyName().getFn1_Surname().getValue();
-                    String name = pid.getPatientName()[0].getGivenName().getValue();
-                    String patientId = msg.getPATIENT_RESULT().getPATIENT().getPID().getPatientID().getCx1_ID().getValue();
-                    Patient patient = new Patient();
-                    patient.addName().addGiven(name);
-                    patient.getNameFirstRep().setFamily(surname);
-                    patient.setId(patientId);
-                    exchange.getIn().setBody(patient);
+                	List<Patient> bundle = new ArrayList<>();
+                	@SuppressWarnings("unchecked")
+					List<List<String>> patients = (List<List<String>>) exchange.getIn().getBody();
+                    for (List<String> patient: patients) {
+                    	Patient fhirPatient = new Patient();
+                    	fhirPatient.setId(patient.get(0));
+                    	fhirPatient.addName().addGiven(patient.get(1));
+                    	fhirPatient.getNameFirstRep().setFamily(patient.get(2));
+                    	bundle.add(fhirPatient);
+                    }
+                    exchange.getIn().setBody(bundle);
                 })
-                // marshall to JSON for logging
-                .marshal().fhirJson("{{fhirVersion}}")
-                .log("Inserting Patient: ${body}")
                 // create Patient in our FHIR server
-                .to("fhir://create/resource?inBody=resourceAsString&serverUrl={{serverUrl}}&fhirVersion={{fhirVersion}}")
+                .to("fhir://transaction/withResources?inBody=resources&serverUrl={{serverUrl}}&username={{serverUser}}&password={{serverPassword}}&fhirVersion={{fhirVersion}}")
                 // log the outcome
-                .log("Patient created successfully: ${body}");
+                .log("Patients created successfully: ${body}");
     }
 
 }
