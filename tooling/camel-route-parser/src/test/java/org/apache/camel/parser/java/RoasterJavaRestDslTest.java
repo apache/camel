adcf45c5dea9c5ce25eb58debb3b67f29095d71b
Claus Ibsen <claus.ibsen@gmail.com>
2018-09-20 11:09:55 +0200

    CAMEL-12824: camel-route-parser - Add parser for rest-dsl

---
+++ tooling/camel-route-parser/src/test/java/org/apache/camel/parser/java/RoasterJavaRestDslTest.java
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.parser.java;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.camel.parser.RestDslParser;
+import org.apache.camel.parser.model.RestConfigurationDetails;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoasterJavaRestDslTest extends CamelTestSupport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RoasterJavaRestDslTest.class);
+
+    @Override
+    public boolean isDumpRouteCoverage() {
+        return false;
+    }
+
+    @Test
+    public void parseTree() throws Exception {
+        JavaClassSource clazz = (JavaClassSource) Roaster.parse(new File("src/test/java/org/apache/camel/parser/java/MyRestDslRouteBuilder.java"));
+
+        List<RestConfigurationDetails> list = RestDslParser.parseRestConfiguration(clazz, ".",
+            "src/test/java/org/apache/camel/parser/java/MyRestDslRouteBuilder.java", true);
+        assertEquals(1, list.size());
+        RestConfigurationDetails details = list.get(0);
+        assertEquals("src/test/java/org/apache/camel/parser/java/MyRestDslRouteBuilder.java", details.getFileName());
+        assertEquals("configure", details.getMethodName());
+        assertEquals("org.apache.camel.parser.java.MyRestDslRouteBuilder", details.getClassName());
+        assertEquals("1234", details.getPort());
+        assertEquals("myapi", details.getContextPath());
+    }
+
+}
