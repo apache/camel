2037087a438fa94fd5aa51e90a8a74a97351e533
jpoth <poth.john@gmail.com>
2018-09-21 12:27:06 +0200

    camel-fhir Id elements are optional when updating a resource

--- components/camel-fhir/camel-fhir-component/src/test/java/org/apache/camel/component/fhir/FhirUpdateIT.java
+++ components/camel-fhir/camel-fhir-component/src/test/java/org/apache/camel/component/fhir/FhirUpdateIT.java
@@ -61,6 +61,24 @@ public class FhirUpdateIT extends AbstractFhirTestSupport {
     }
 
     @Test
+    public void testResourceNoId() throws Exception {
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1998-04-29");
+        assertNotEquals(date, patient.getBirthDate());
+        this.patient.setBirthDate(date);
+        final Map<String, Object> headers = new HashMap<>();
+        // parameter type is org.hl7.fhir.instance.model.api.IBaseResource
+        headers.put("CamelFhir.resource", this.patient);
+        // parameter type is ca.uhn.fhir.rest.api.PreferReturnEnum
+        headers.put("CamelFhir.preferReturn", PreferReturnEnum.REPRESENTATION);
+
+        MethodOutcome result = requestBodyAndHeaders("direct://RESOURCE", null, headers);
+
+        assertNotNull("resource result", result);
+        LOG.debug("resource: " + result);
+        assertEquals("Birth date not updated!", date, ((Patient)result.getResource()).getBirthDate());
+    }
+
+    @Test
     public void testResourceStringId() throws Exception {
         Date date = new SimpleDateFormat("yyyy-MM-dd").parse("1998-04-29");
         assertNotEquals(date, patient.getBirthDate());
@@ -168,27 +186,27 @@ public class FhirUpdateIT extends AbstractFhirTestSupport {
             public void configure() {
                 // test route for resource
                 from("direct://RESOURCE")
-                    .to("fhir://" + PATH_PREFIX + "/resource");
+                        .to("fhir://" + PATH_PREFIX + "/resource");
 
                 // test route for resource
                 from("direct://RESOURCE_WITH_STRING_ID")
-                    .to("fhir://" + PATH_PREFIX + "/resource");
+                        .to("fhir://" + PATH_PREFIX + "/resource");
 
                 // test route for resource
                 from("direct://RESOURCE_AS_STRING")
-                    .to("fhir://" + PATH_PREFIX + "/resource");
+                        .to("fhir://" + PATH_PREFIX + "/resource");
 
                 // test route for resource
                 from("direct://RESOURCE_AS_STRING_WITH_STRING_ID")
-                    .to("fhir://" + PATH_PREFIX + "/resource");
+                        .to("fhir://" + PATH_PREFIX + "/resource");
 
                 // test route for resourceBySearchUrl
                 from("direct://RESOURCE_BY_SEARCH_URL")
-                    .to("fhir://" + PATH_PREFIX + "/resourceBySearchUrl");
+                        .to("fhir://" + PATH_PREFIX + "/resourceBySearchUrl");
 
                 // test route for resourceBySearchUrl
                 from("direct://RESOURCE_BY_SEARCH_URL_AND_RESOURCE_AS_STRING")
-                    .to("fhir://" + PATH_PREFIX + "/resourceBySearchUrl");
+                        .to("fhir://" + PATH_PREFIX + "/resourceBySearchUrl");
 
             }
         };
