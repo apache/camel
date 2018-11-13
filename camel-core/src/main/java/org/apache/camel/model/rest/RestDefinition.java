8f94ab7e23b8978a684e7b33bdf141421606c363
Dmitry Volodin <dmvolod@gmail.com>
2018-11-13 16:53:26 +0300

    CAMEL-12908: Fix CS

--- camel-core/src/main/java/org/apache/camel/model/rest/RestDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/rest/RestDefinition.java
@@ -902,27 +902,7 @@ public class RestDefinition extends OptionalIdentifiedDefinition<RestDefinition>
             if (outType != null) {
                 options.put("outType", outType);
             }
-            // if no route id has been set, then use the verb id as route id
-            /*
-            if (!route.hasCustomIdAssigned()) {
-                // use id of verb as route id
-                String id = verb.getId();
-                if (id != null) {
-                    route.setId(id);
-                }
-            }
-            */
 
-            /*
-            String routeId = verb.idOrCreate(camelContext.getNodeIdFactory());
-
-            if (!verb.getUsedForGeneratingNodeId()) {
-                routeId = route.idOrCreate(camelContext.getNodeIdFactory());
-            }
-
-            verb.setRouteId(routeId);
-            options.put("routeId", routeId);
-            */
             if (component != null && !component.isEmpty()) {
                 options.put("componentName", component);
             }
@@ -1016,7 +996,6 @@ public class RestDefinition extends OptionalIdentifiedDefinition<RestDefinition>
 
             // the route should be from this rest endpoint
             route.fromRest(from);
-            //route.routeId(routeId);
             route.setRestDefinition(this);
             answer.add(route);
         }
