65e2f2f7aa3d4b0782404abb7db67880f7025807
onders <onders@apache.org>
2018-12-04 01:03:28 +0300

    CAMEL-12944 - externalize configs as PR #2549 tried to avoid potentially conflicting jetty port during test execution

--- components/camel-ribbon/src/test/java/org/apache/camel/component/ribbon/cloud/SpringBeanServiceCallRouteTest.java
+++ components/camel-ribbon/src/test/java/org/apache/camel/component/ribbon/cloud/SpringBeanServiceCallRouteTest.java
@@ -42,13 +42,5 @@ public class SpringBeanServiceCallRouteTest extends SpringRibbonServiceCallRoute
         RibbonServiceLoadBalancer loadBalancer = (RibbonServiceLoadBalancer)processor.getLoadBalancer();
         Assert.assertTrue(loadBalancer.getServiceDiscovery() instanceof StaticServiceDiscovery);
     }
-    
-    protected String getFirstPort() {
-        return "9092";
-    }
-    
-    protected String getSecondPort() {
-        return "9093";
-    }
 }
 
