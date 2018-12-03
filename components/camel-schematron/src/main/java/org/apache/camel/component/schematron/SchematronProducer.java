18cd56d40a81ea6445d0bab7a1b1c69083e02fa3
Andrea Cosentino <ancosen@gmail.com>
2018-12-03 15:39:02 +0100

    Fix backport of Schematron fix to 3.x

--- components/camel-schematron/src/main/java/org/apache/camel/component/schematron/SchematronProducer.java
+++ components/camel-schematron/src/main/java/org/apache/camel/component/schematron/SchematronProducer.java
@@ -52,7 +52,7 @@ public class SchematronProducer extends DefaultProducer {
      * @throws Exception
      */
     public void process(Exchange exchange) throws Exception {
-        final SchematronProcessor schematronProcessor = SchematronProcessorFactory.newScehamtronEngine(endpoint.getRules());
+        final SchematronProcessor schematronProcessor = SchematronProcessorFactory.newSchematronEngine(endpoint.getRules());
         final Object payload = exchange.getIn().getBody();
         final String report;
 
