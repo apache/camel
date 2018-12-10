85d434dba98684a8b6f77ad71a838b94fea81243
Andrea Cosentino <ancosen@gmail.com>
2018-12-10 11:48:44 +0100

    CAMEL-11888 - Fixed backport from 2.x and regen

--- components/camel-jgroups-raft/src/main/java/org/apache/camel/component/jgroups/raft/JGroupsRaftConsumer.java
+++ components/camel-jgroups-raft/src/main/java/org/apache/camel/component/jgroups/raft/JGroupsRaftConsumer.java
@@ -17,7 +17,7 @@
 package org.apache.camel.component.jgroups.raft;
 
 import org.apache.camel.Processor;
-import org.apache.camel.impl.DefaultConsumer;
+import org.apache.camel.support.DefaultConsumer;
 import org.jgroups.raft.RaftHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
