6c946ac82ede7cfea31e0073641e93b18480998a
Andrea Cosentino <ancosen@gmail.com>
2018-12-19 08:29:22 +0100

    CAMEL-13014 - Fixed CS

--- components/camel-mqtt/src/main/java/org/apache/camel/component/mqtt/MQTTComponent.java
+++ components/camel-mqtt/src/main/java/org/apache/camel/component/mqtt/MQTTComponent.java
@@ -19,8 +19,8 @@ package org.apache.camel.component.mqtt;
 import java.util.Map;
 
 import org.apache.camel.Endpoint;
-import org.apache.camel.support.DefaultComponent;
 import org.apache.camel.spi.Metadata;
+import org.apache.camel.support.DefaultComponent;
 
 /**
  * MQTT Component
