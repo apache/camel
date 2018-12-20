f59ae7735816544477ce599529200ebe4aeba92e
Dmitry Volodin <dmvolod@gmail.com>
2018-12-20 16:40:20 +0300

    CAMEL-13008: Odata-connector assumes '/' at end of URI

--- components/camel-olingo4/camel-olingo4-api/src/main/java/org/apache/camel/component/olingo4/api/impl/Olingo4AppImpl.java
+++ components/camel-olingo4/camel-olingo4-api/src/main/java/org/apache/camel/component/olingo4/api/impl/Olingo4AppImpl.java
@@ -778,7 +778,7 @@ public final class Olingo4AppImpl implements Olingo4App {
 
         final StringBuilder absolutUri = new StringBuilder(resourceUri).append(SEPARATOR).append(resourcePath);
         if (queryOptions != null && !queryOptions.isEmpty()) {
-            absolutUri.append("/?" + queryOptions);
+            absolutUri.append("?" + queryOptions);
         }
         return absolutUri.toString();
 
