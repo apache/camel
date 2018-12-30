ab0e7f6c1f3fc95bb093b0f76cbf1a954b1efee9
fvaleri <fvaleri@localhost>
2018-12-30 16:54:34 +0100

    fix for after process work leak

--- camel-core/src/main/java/org/apache/camel/support/ReactiveHelper.java
+++ camel-core/src/main/java/org/apache/camel/support/ReactiveHelper.java
@@ -52,7 +52,7 @@ public final class ReactiveHelper {
     }
 
     public static void scheduleLast(Runnable runnable, String description) {
-        WORKERS.get().schedule(describe(runnable, description), false, false, false);
+        WORKERS.get().schedule(describe(runnable, description), false, true, false);
     }
 
     public static void scheduleSync(Runnable runnable, String description) {
