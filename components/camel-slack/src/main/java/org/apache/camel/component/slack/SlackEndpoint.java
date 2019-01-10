c2bf2977191227a5e3af36acf2ddedebb50390ff
Andrea Cosentino <ancosen@gmail.com>
2019-01-10 08:19:41 +0100

    CAMEL-13045 - Fixed CS

--- components/camel-slack/src/main/java/org/apache/camel/component/slack/SlackEndpoint.java
+++ components/camel-slack/src/main/java/org/apache/camel/component/slack/SlackEndpoint.java
@@ -24,11 +24,11 @@ import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.component.slack.helper.SlackMessage;
-import org.apache.camel.support.ScheduledPollEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
+import org.apache.camel.support.ScheduledPollEndpoint;
 import org.apache.camel.util.ObjectHelper;
 import org.json.simple.JSONObject;
 
