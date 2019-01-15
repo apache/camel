a4a70bc2f1bc5fe81761a906ee61f9b5e1b98cff
Claus Ibsen <claus.ibsen@gmail.com>
2019-01-15 11:14:51 +0100

    CAMEL-13055: generate legal can be done a bit later in the maven lifecycle.

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PackageLegalMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PackageLegalMojo.java
@@ -17,19 +17,14 @@
 package org.apache.camel.maven.packaging;
 
 import java.io.File;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.Objects;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -39,12 +34,11 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
-import org.sonatype.plexus.build.incremental.BuildContext;
 
 /**
  * Analyses the Camel plugins in a project and generates legal files.
  */
-@Mojo(name = "generate-legal", threadSafe = true, defaultPhase = LifecyclePhase.PROCESS_CLASSES)
+@Mojo(name = "generate-legal", threadSafe = true, defaultPhase = LifecyclePhase.PREPARE_PACKAGE)
 public class PackageLegalMojo extends AbstractMojo {
 
     /**
@@ -56,7 +50,7 @@ public class PackageLegalMojo extends AbstractMojo {
     /**
      * The output directory for generated components file
      */
-    @Parameter(defaultValue = "${project.build.directory}/generated/camel/legal")
+    @Parameter(defaultValue = "${project.build.directory}/classes")
     protected File legalOutDir;
 
     /**
@@ -64,13 +58,6 @@ public class PackageLegalMojo extends AbstractMojo {
      */
     @Component
     private MavenProjectHelper projectHelper;
-
-    /**
-     * build context to check changed files and mark them for refresh (used for
-     * m2e compatibility)
-     */
-    @Component
-    private BuildContext buildContext;
 
     /**
      * Execute goal.
@@ -81,8 +68,6 @@ public class PackageLegalMojo extends AbstractMojo {
      */
     public void execute() throws MojoExecutionException, MojoFailureException {
         processLegal(legalOutDir.toPath());
-
-        projectHelper.addResource(project, legalOutDir.getPath(), Collections.singletonList("**/*"), Collections.emptyList());
     }
 
     public void processLegal(Path legalOutDir) throws MojoExecutionException {
@@ -91,62 +76,34 @@ public class PackageLegalMojo extends AbstractMojo {
             return;
         }
 
-        boolean hasLicense = project.getResources().stream()
-                .map(Resource::getDirectory)
-                .map(Paths::get)
-                .map(p -> p.resolve("META-INF").resolve("LICENSE.txt"))
-                .anyMatch(Files::isRegularFile);
-        if (!hasLicense) {
-            try (InputStream isLicense = getClass().getResourceAsStream("/camel-LICENSE.txt")) {
-                String license = IOUtils.toString(isLicense, StandardCharsets.UTF_8);
-                updateResource(legalOutDir.resolve("META-INF").resolve("LICENSE.txt"), license);
-            } catch (IOException e) {
-                throw new MojoExecutionException("Failed to write legal files. Reason: " + e, e);
-            }
-        }
-        boolean hasNotice = project.getResources().stream()
-                .map(Resource::getDirectory)
-                .map(Paths::get)
-                .map(p -> p.resolve("META-INF").resolve("NOTICE.txt"))
-                .anyMatch(Files::isRegularFile);
-        if (!hasNotice) {
-            try (InputStream isNotice = getClass().getResourceAsStream("/camel-NOTICE.txt")) {
-                String notice = IOUtils.toString(isNotice, StandardCharsets.UTF_8);
-                updateResource(legalOutDir.resolve("META-INF").resolve("NOTICE.txt"), notice);
-            } catch (IOException e) {
-                throw new MojoExecutionException("Failed to write legal files. Reason: " + e, e);
-            }
-        }
-    }
-
-    protected void updateResource(Path out, String data) {
-        try {
-            if (data == null) {
-                if (Files.isRegularFile(out)) {
-                    Files.delete(out);
-                    refresh(out);
-                }
-            } else {
-                if (Files.isRegularFile(out) && Files.isReadable(out)) {
-                    String content = new String(Files.readAllBytes(out), StandardCharsets.UTF_8);
-                    if (Objects.equals(content, data)) {
-                        return;
-                    }
-                }
-                Files.createDirectories(out.getParent());
-                try (Writer w = Files.newBufferedWriter(out, StandardCharsets.UTF_8)) {
-                    w.append(data);
-                }
-                refresh(out);
-            }
+        try (InputStream isLicense = getClass().getResourceAsStream("/camel-LICENSE.txt")) {
+            String license = IOUtils.toString(isLicense, StandardCharsets.UTF_8);
+            updateResource(legalOutDir.resolve("META-INF").resolve("LICENSE.txt"), license);
         } catch (IOException e) {
-            throw new IOError(e);
+            throw new MojoExecutionException("Failed to write legal files. Reason: " + e, e);
+        }
+        try (InputStream isNotice = getClass().getResourceAsStream("/camel-NOTICE.txt")) {
+            String notice = IOUtils.toString(isNotice, StandardCharsets.UTF_8);
+            updateResource(legalOutDir.resolve("META-INF").resolve("NOTICE.txt"), notice);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to write legal files. Reason: " + e, e);
         }
     }
 
-    protected void refresh(Path file) {
-        if (buildContext != null) {
-            buildContext.refresh(file.toFile());
+    protected void updateResource(Path out, String data) throws IOException {
+        if (Files.isRegularFile(out)) {
+            // file already exists
+            return;
+        }
+        if (data == null) {
+            if (Files.isRegularFile(out)) {
+                Files.delete(out);
+            }
+        } else {
+            Files.createDirectories(out.getParent());
+            try (Writer w = Files.newBufferedWriter(out, StandardCharsets.UTF_8)) {
+                w.append(data);
+            }
         }
     }
 
