ca47dac5c74704f9fa2d8b794c9380df3ed72b1f
Andrea Cosentino <ancosen@gmail.com>
2019-01-15 16:22:34 +0100

    Fixed CS for Camel-AWS

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/ddbstream/DdbStreamComponent.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/ddbstream/DdbStreamComponent.java
@@ -20,8 +20,8 @@ import java.util.Map;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 
-import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 import org.apache.camel.util.ObjectHelper;
 
--- components/camel-aws/src/main/java/org/apache/camel/component/aws/sns/SnsComponent.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/sns/SnsComponent.java
@@ -22,8 +22,8 @@ import com.amazonaws.regions.Regions;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 
-import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 import org.apache.camel.util.ObjectHelper;
 
