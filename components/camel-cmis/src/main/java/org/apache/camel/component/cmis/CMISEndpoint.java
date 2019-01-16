3428410bf985e08c00346730790591c13f489264
Andrea Cosentino <ancosen@gmail.com>
2019-01-16 08:18:42 +0100

    Fixed CS for Camel-CMIS

--- components/camel-cmis/src/main/java/org/apache/camel/component/cmis/CMISEndpoint.java
+++ components/camel-cmis/src/main/java/org/apache/camel/component/cmis/CMISEndpoint.java
@@ -21,11 +21,11 @@ import java.util.Map;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
+import org.apache.camel.support.DefaultEndpoint;
 
 /**
  * The cmis component uses the Apache Chemistry client API and allows you to add/read nodes to/from a CMIS compliant content repositories.
