ae72539a07bd8f618762412e5a7dbfc3ebbaa1d7
Andrea Cosentino <ancosen@gmail.com>
2019-01-16 07:42:50 +0100

    Fixed CS for Camel-Atom

--- components/camel-atom/src/test/java/org/apache/camel/component/atom/AtomEntryPollingConsumerTest.java
+++ components/camel-atom/src/test/java/org/apache/camel/component/atom/AtomEntryPollingConsumerTest.java
@@ -22,8 +22,8 @@ import javax.naming.Context;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.support.jndi.JndiContext;
+import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 /**
