c3b9c521c294d021b58b34cc17c9170774f2f527
Andrea Cosentino <ancosen@gmail.com>
2019-01-17 10:27:49 +0100

    Fixed CS for Camel-Etcd

--- components/camel-etcd/src/main/java/org/apache/camel/component/etcd/EtcdComponent.java
+++ components/camel-etcd/src/main/java/org/apache/camel/component/etcd/EtcdComponent.java
@@ -22,11 +22,11 @@ import java.util.Optional;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.SSLContextParametersAware;
+import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
-import org.apache.camel.spi.Metadata;
-import org.apache.camel.util.StringHelper;
 import org.apache.camel.support.jsse.SSLContextParameters;
+import org.apache.camel.util.StringHelper;
 
 /**
  * Represents the component that manages {@link AbstractEtcdEndpoint}.
