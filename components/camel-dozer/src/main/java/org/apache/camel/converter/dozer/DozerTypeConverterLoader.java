52de230b8a3fb770440086968c3e94805a992643
Andrea Cosentino <ancosen@gmail.com>
2019-01-17 09:18:56 +0100

    Fixed CS for Camel-dozer

--- components/camel-dozer/src/main/java/org/apache/camel/converter/dozer/DozerTypeConverterLoader.java
+++ components/camel-dozer/src/main/java/org/apache/camel/converter/dozer/DozerTypeConverterLoader.java
@@ -32,8 +32,8 @@ import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.TypeConverter;
 import org.apache.camel.spi.ClassResolver;
 import org.apache.camel.spi.TypeConverterRegistry;
-import org.apache.camel.support.ServiceSupport;
 import org.apache.camel.support.ResourceHelper;
+import org.apache.camel.support.ServiceSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
