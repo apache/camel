d0ec866a6fb3cd58f6825e92a85bd8ba7d87be98
Andrea Cosentino <ancosen@gmail.com>
2019-01-17 15:34:47 +0100

    Fixed CS for Camel-Fop

--- components/camel-fop/src/main/java/org/apache/camel/component/fop/FopEndpoint.java
+++ components/camel-fop/src/main/java/org/apache/camel/component/fop/FopEndpoint.java
@@ -22,11 +22,11 @@ import java.net.URI;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
+import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.support.ResourceHelper;
 import org.apache.fop.apps.FopFactory;
 
