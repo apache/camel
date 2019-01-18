42bd6ea5a7f929076966660566115844e855aa7c
Andrea Cosentino <ancosen@gmail.com>
2019-01-18 11:42:17 +0100

    Fixed CS for Camel-hdfs2

--- components/camel-hdfs2/src/main/java/org/apache/camel/component/hdfs2/HdfsEndpoint.java
+++ components/camel-hdfs2/src/main/java/org/apache/camel/component/hdfs2/HdfsEndpoint.java
@@ -22,9 +22,9 @@ import java.net.URISyntaxException;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import org.apache.camel.support.ScheduledPollEndpoint;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
+import org.apache.camel.support.ScheduledPollEndpoint;
 
 /**
  * For reading/writing from/to an HDFS filesystem using Hadoop 2.x.
