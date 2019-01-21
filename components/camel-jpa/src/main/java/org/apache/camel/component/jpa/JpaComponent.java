2a67fc9baf7be7da095fb3cb55c9c06a36ce1fce
Andrea Cosentino <ancosen@gmail.com>
2019-01-21 15:23:13 +0100

    Fixed CS for Camel-JPA

--- components/camel-jpa/src/main/java/org/apache/camel/component/jpa/JpaComponent.java
+++ components/camel-jpa/src/main/java/org/apache/camel/component/jpa/JpaComponent.java
@@ -21,9 +21,9 @@ import java.util.concurrent.ExecutorService;
 import javax.persistence.EntityManagerFactory;
 
 import org.apache.camel.Endpoint;
+import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
-import org.apache.camel.spi.Metadata;
 import org.apache.camel.util.ObjectHelper;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
