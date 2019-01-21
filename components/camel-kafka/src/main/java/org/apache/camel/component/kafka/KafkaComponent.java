8c27044a7bee539fe11ba5ad8ef7fa0c44ebe12a
Andrea Cosentino <ancosen@gmail.com>
2019-01-21 15:48:37 +0100

    Fixed CS for Camel-Kafka

--- components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaComponent.java
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaComponent.java
@@ -22,9 +22,9 @@ import java.util.concurrent.ExecutorService;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.SSLContextParametersAware;
+import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
-import org.apache.camel.spi.Metadata;
 import org.apache.camel.util.ObjectHelper;
 
 @Component("kafka")
