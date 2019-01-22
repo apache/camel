e37209edb2d64742ec4482e04630e99712d960fd
Andrea Cosentino <ancosen@gmail.com>
2019-01-22 13:54:59 +0100

    Fixed CS for Camel-metrics

--- components/camel-metrics/src/main/java/org/apache/camel/component/metrics/MetricsEndpoint.java
+++ components/camel-metrics/src/main/java/org/apache/camel/component/metrics/MetricsEndpoint.java
@@ -22,11 +22,11 @@ import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.RuntimeCamelException;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
+import org.apache.camel.support.DefaultEndpoint;
 
 /**
  * To collect various metrics directly from Camel routes using the DropWizard metrics library.
