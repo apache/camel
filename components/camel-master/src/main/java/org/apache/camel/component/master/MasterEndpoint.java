e3867a1d25c6dfd6868b990d7d4ca3d3b79f3be2
Andrea Cosentino <ancosen@gmail.com>
2019-01-22 13:37:31 +0100

    Fixed CS for Camel-master

--- components/camel-master/src/main/java/org/apache/camel/component/master/MasterEndpoint.java
+++ components/camel-master/src/main/java/org/apache/camel/component/master/MasterEndpoint.java
@@ -25,10 +25,10 @@ import org.apache.camel.api.management.ManagedAttribute;
 import org.apache.camel.api.management.ManagedResource;
 import org.apache.camel.cluster.CamelClusterService;
 import org.apache.camel.cluster.CamelClusterView;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriPath;
+import org.apache.camel.support.DefaultEndpoint;
 
 /**
  * Represents an endpoint which only becomes active when the {@link CamelClusterView}
