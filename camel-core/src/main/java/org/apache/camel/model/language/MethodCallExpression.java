751690bf97212db00ed479a701d52fba8ead6112
Claus Ibsen <claus.ibsen@gmail.com>
2019-01-23 11:03:42 +0100

    Deprecate not used methods

--- camel-core/src/main/java/org/apache/camel/model/language/MethodCallExpression.java
+++ camel-core/src/main/java/org/apache/camel/model/language/MethodCallExpression.java
@@ -192,7 +192,7 @@ public class MethodCallExpression extends ExpressionDefinition {
 
     @Override
     public Predicate createPredicate(CamelContext camelContext) {
-        return (BeanExpression) createExpression(camelContext);
+        return (Predicate) createExpression(camelContext);
     }
 
     /**
