d5ec530eaa237ce558ee94eba39631447742933e
Claus Ibsen <claus.ibsen@gmail.com>
2019-01-23 18:32:47 +0100

    CAMEL-13117: Move properties component out of camel-core

--- camel-core/src/main/java/org/apache/camel/main/Main.java
+++ camel-core/src/main/java/org/apache/camel/main/Main.java
@@ -21,7 +21,6 @@ import java.util.Map;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.CompositeRegistry;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.SimpleRegistry;
@@ -113,16 +112,6 @@ public class Main extends MainSupport {
         } else {
             throw new IllegalStateException("Error creating CamelContext");
         }
-    }
-
-    /**
-     * A list of locations to load properties. You can use comma to separate multiple locations.
-     * This option will override any default locations and only use the locations from this option.
-     */
-    public void setPropertyPlaceholderLocations(String location) {
-        PropertiesComponent pc = new PropertiesComponent();
-        pc.setLocation(location);
-        bind("properties", pc);
     }
 
     // Implementation methods
