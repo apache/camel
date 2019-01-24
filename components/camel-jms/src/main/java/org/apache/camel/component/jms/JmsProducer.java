53f33be54f28cd3b8b76e687cf16ade0ede7decb
Andrea Cosentino <ancosen@gmail.com>
2019-01-24 08:24:19 +0100

    Fixed CS for Camel-JMS

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsProducer.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsProducer.java
@@ -21,6 +21,8 @@ import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import static java.util.Optional.ofNullable;
+
 import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.JMSException;
@@ -45,7 +47,6 @@ import org.springframework.jms.core.JmsOperations;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.jms.support.JmsUtils;
 
-import static java.util.Optional.ofNullable;
 import static org.apache.camel.component.jms.JmsMessageHelper.isQueuePrefix;
 import static org.apache.camel.component.jms.JmsMessageHelper.isTopicPrefix;
 import static org.apache.camel.component.jms.JmsMessageHelper.normalizeDestinationName;
