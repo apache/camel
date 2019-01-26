9431cf8345c9e58bea17a729ce04d68768c04562
Andrea Cosentino <ancosen@gmail.com>
2019-01-26 13:09:07 +0100

    Fixed CS for Camel-Spring-Integration

--- components/camel-spring-integration/src/main/java/org/apache/camel/component/spring/integration/SpringIntegrationEndpoint.java
+++ components/camel-spring-integration/src/main/java/org/apache/camel/component/spring/integration/SpringIntegrationEndpoint.java
@@ -19,12 +19,12 @@ package org.apache.camel.component.spring.integration;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
 import org.apache.camel.spring.SpringCamelContext;
+import org.apache.camel.support.DefaultEndpoint;
 import org.springframework.messaging.MessageChannel;
 
 /**
