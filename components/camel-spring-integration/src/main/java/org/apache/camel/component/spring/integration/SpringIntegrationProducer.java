55508f8994838b30e1b893f8f45c8658ff13594f
Andrea Cosentino <ancosen@gmail.com>
2019-01-26 13:09:07 +0100

    Fixed CS for Camel-Spring-Integration

--- components/camel-spring-integration/src/main/java/org/apache/camel/component/spring/integration/SpringIntegrationProducer.java
+++ components/camel-spring-integration/src/main/java/org/apache/camel/component/spring/integration/SpringIntegrationProducer.java
@@ -18,8 +18,8 @@ package org.apache.camel.component.spring.integration;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.support.DefaultProducer;
 import org.apache.camel.spring.SpringCamelContext;
+import org.apache.camel.support.DefaultProducer;
 import org.apache.camel.util.ObjectHelper;
 import org.apache.camel.util.StringHelper;
 import org.springframework.integration.channel.DirectChannel;
