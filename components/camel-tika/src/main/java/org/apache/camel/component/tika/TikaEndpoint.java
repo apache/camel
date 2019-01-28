dc408b87c62370fdc5100f873eb4fbfd98d8c3b5
Andrea Cosentino <ancosen@gmail.com>
2019-01-28 08:48:23 +0100

    Fixed CS for Camel-Tika

--- components/camel-tika/src/main/java/org/apache/camel/component/tika/TikaEndpoint.java
+++ components/camel-tika/src/main/java/org/apache/camel/component/tika/TikaEndpoint.java
@@ -20,9 +20,9 @@ import org.apache.camel.Component;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
+import org.apache.camel.support.DefaultEndpoint;
 
 @UriEndpoint(firstVersion = "2.19.0", scheme = "tika", title = "Tika", syntax = "tika:operation", producerOnly = true, label = "document,transformation")
 public class TikaEndpoint extends DefaultEndpoint {
