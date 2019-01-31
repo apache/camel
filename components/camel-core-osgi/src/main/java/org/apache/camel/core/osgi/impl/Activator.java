e65d06954105e0b74b49735dc892739e1b10cfb1
Guillaume Nodet <gnodet@gmail.com>
2019-01-31 10:12:38 +0100

    [CAMEL-13129] Move osgi out of camel-core

--- components/camel-core-osgi/src/main/java/org/apache/camel/core/osgi/impl/Activator.java
+++ components/camel-core-osgi/src/main/java/org/apache/camel/core/osgi/impl/Activator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.impl.osgi;
+package org.apache.camel.core.osgi.impl;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
