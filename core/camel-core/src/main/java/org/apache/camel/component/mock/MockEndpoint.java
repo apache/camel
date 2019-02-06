2371b6626e73e7d1884c97630ca4f0d208d75129
Claus Ibsen <claus.ibsen@gmail.com>
2019-02-06 07:06:51 +0100

    CAMEL-13096: Fixed issue with expected message size was already set then minimum size should not be set also.

--- core/camel-core/src/main/java/org/apache/camel/component/mock/MockEndpoint.java
+++ core/camel-core/src/main/java/org/apache/camel/component/mock/MockEndpoint.java
@@ -482,7 +482,7 @@ public class MockEndpoint extends DefaultEndpoint implements BrowsableEndpoint {
      * the assertion is satisfied when 5 or more message arrives. To ensure that
      * exactly 5 messages arrives, then you would need to wait a little period
      * to ensure no further message arrives. This is what you can use this
-     * {@link #setAssertPeriod(long)} method for.
+     * method for.
      * <p/>
      * By default this period is disabled.
      *
@@ -508,11 +508,9 @@ public class MockEndpoint extends DefaultEndpoint implements BrowsableEndpoint {
      * <p/>
      * You can set multiple expectations for different header names.
      * If you set a value of <tt>null</tt> that means we accept either the header is absent, or its value is <tt>null</tt>
-     * <p/>
-     * <b>Important:</b> This overrides any previous set value using {@link #expectedMessageCount(int)}
      */
     public void expectedHeaderReceived(final String name, final Object value) {
-        if (expectedMinimumCount == -1) {
+        if (expectedMinimumCount == -1 && expectedCount <= 0) {
             expectedMinimumMessageCount(1);
         }
         if (expectedHeaderValues == null) {
