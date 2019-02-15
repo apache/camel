3ee7a45d9e8fe7d50dc13f98eb0cb015377a3fd6
bszeti <bszeti@redhat.com>
2019-02-15 17:58:52 -0500

    add XML route tests and examples

--- components/camel-jcache/src/test/java/org/apache/camel/component/jcache/policy/SpringJCachePolicyTest.java
+++ components/camel-jcache/src/test/java/org/apache/camel/component/jcache/policy/SpringJCachePolicyTest.java
@@ -18,9 +18,13 @@ package org.apache.camel.component.jcache.policy;
 
 import javax.cache.Cache;
 
+import com.hazelcast.cache.HazelcastCachingProvider;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.language.SimpleExpression;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -34,6 +38,15 @@ public class SpringJCachePolicyTest extends CamelSpringTestSupport {
         return new ClassPathXmlApplicationContext("org/apache/camel/component/jcache/policy/SpringJCachePolicyTest.xml");
     }
 
+    @BeforeClass
+    public static void beforeAll() {
+        System.setProperty("hazelcast.config","classpath:org/apache/camel/component/jcache/policy/hazelcast-spring.xml");
+    }
+    @AfterClass
+    public static void afterAll() {
+        System.clearProperty("hazelcast.config");
+    }
+
     @Before
     public void before() {
         //reset mock
@@ -43,14 +56,14 @@ public class SpringJCachePolicyTest extends CamelSpringTestSupport {
             e.getMessage().setBody(generateValue(e.getMessage().getBody(String.class))));
     }
 
-    //Basic test to verify value gets cached and route is not executed for the second time
+    //Verify value gets cached and route is not executed for the second time
     @Test
     public void testXmlDslValueGetsCached() throws Exception {
         final String key = randomString();
         MockEndpoint mock = getMockEndpoint("mock:spring");
 
         //Send first, key is not in cache
-        Object responseBody = this.template().requestBody("direct:policy-spring", key);
+        Object responseBody = this.template().requestBody("direct:spring", key);
 
         //We got back the value, mock was called once, value got cached.
         Cache cache = lookupCache("spring");
@@ -59,7 +72,7 @@ public class SpringJCachePolicyTest extends CamelSpringTestSupport {
         assertEquals(1, mock.getExchanges().size());
 
         //Send again, key is already in cache
-        responseBody = this.template().requestBody("direct:policy-spring", key);
+        responseBody = this.template().requestBody("direct:spring", key);
 
         //We got back the stored value, but the mock was not called again
         assertEquals(generateValue(key), cache.get(key));
@@ -67,14 +80,14 @@ public class SpringJCachePolicyTest extends CamelSpringTestSupport {
         assertEquals(1, mock.getExchanges().size());
     }
 
-    //Basic test to verify if we call the route with different keys, both gets cached
+    //Verify if we call the route with different keys, both gets cached
     @Test
     public void testXmlDslDifferent() throws Exception {
         final String key1 = randomString();
         MockEndpoint mock = getMockEndpoint("mock:spring");
 
         //Send first, key is not in cache
-        Object responseBody = this.template().requestBody("direct:policy-spring", key1);
+        Object responseBody = this.template().requestBody("direct:spring", key1);
 
         //We got back the value, mock was called once, value got cached.
         Cache cache = lookupCache("spring");
@@ -84,7 +97,7 @@ public class SpringJCachePolicyTest extends CamelSpringTestSupport {
 
         //Send again, different key
         final String key2 = randomString();
-        responseBody = this.template().requestBody("direct:policy-spring", key2);
+        responseBody = this.template().requestBody("direct:spring", key2);
 
         //We got back the stored value, mock was called again, value got cached.
         assertEquals(generateValue(key2), cache.get(key2));
@@ -92,7 +105,58 @@ public class SpringJCachePolicyTest extends CamelSpringTestSupport {
         assertEquals(2, mock.getExchanges().size());
     }
 
+    //Verify policy applies only on the section of the route wrapped
+    @Test
+    public void testXmlDslPartial() throws Exception {
+        final String key = randomString();
+        MockEndpoint mock = getMockEndpoint("mock:spring");
+        MockEndpoint mockUnwrapped = getMockEndpoint("mock:unwrapped");
 
+        //Send first, key is not in cache
+        Object responseBody = this.template().requestBody("direct:spring-partial", key);
+
+        //We got back the value, mock was called once, value got cached.
+        Cache cache = lookupCache("spring");
+        assertEquals(generateValue(key), cache.get(key));
+        assertEquals(generateValue(key), responseBody);
+        assertEquals(1, mock.getExchanges().size());
+        assertEquals(1, mockUnwrapped.getExchanges().size());
+
+        //Send again, key is already in cache
+        responseBody = this.template().requestBody("direct:spring-partial", key);
+
+        //We got back the stored value, but the mock was not called again
+        assertEquals(generateValue(key), cache.get(key));
+        assertEquals(generateValue(key), responseBody);
+        assertEquals(1, mock.getExchanges().size());
+        assertEquals(2, mockUnwrapped.getExchanges().size());
+    }
+
+    //Use a key expression ${header.mykey}
+    @Test
+    public void testXmlDslKeyExpression() throws Exception {
+        final String key = randomString();
+        final String body = randomString();
+        MockEndpoint mock = getMockEndpoint("mock:spring");
+
+        //Send first, key is not in cache
+        Object responseBody = this.template().requestBodyAndHeader("direct:spring-byheader", body, "mykey", key);
+
+        //We got back the value, mock was called once, value got cached.
+        Cache cache = lookupCache("spring");
+        assertEquals(generateValue(body), cache.get(key));
+        assertEquals(generateValue(body), responseBody);
+        assertEquals(1, mock.getExchanges().size());
+
+        //Send again, use another body, but the same key
+        responseBody = this.template().requestBodyAndHeader("direct:spring-byheader", randomString(), "mykey", key);
+
+        //We got back the stored value, and the mock was not called again
+        assertEquals(generateValue(body), cache.get(key));
+        assertEquals(generateValue(body), responseBody);
+        assertEquals(1, mock.getExchanges().size());
+
+    }
 
 
 
