2a28bc5d129758432c2bd8dee9a8da7502e19c3f
Andrea Cosentino <ancosen@gmail.com>
2019-02-27 12:07:18 +0100

    CAMEL-13182 - split camel-aws component in individual components, moved aws-sqs

---
+++ components/camel-aws-sqs/src/test/java/org/apache/camel/component/aws/sqs/SqsBatchConsumerTest.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.sqs;
+
+import com.amazonaws.services.sqs.model.Message;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class SqsBatchConsumerTest extends CamelTestSupport {
+    
+    @EndpointInject(uri = "mock:result")
+    private MockEndpoint mock;
+        
+    @Test
+    public void receiveBatch() throws Exception {
+        mock.expectedMessageCount(5);
+        assertMockEndpointsSatisfied();
+        
+        mock.message(0).exchangeProperty(Exchange.BATCH_INDEX).isEqualTo(0);
+        mock.message(1).exchangeProperty(Exchange.BATCH_INDEX).isEqualTo(1);
+        mock.message(2).exchangeProperty(Exchange.BATCH_INDEX).isEqualTo(2);
+        mock.message(3).exchangeProperty(Exchange.BATCH_INDEX).isEqualTo(3);
+        mock.message(4).exchangeProperty(Exchange.BATCH_INDEX).isEqualTo(4);
+        mock.message(0).exchangeProperty(Exchange.BATCH_COMPLETE).isEqualTo(false);
+        mock.message(1).exchangeProperty(Exchange.BATCH_COMPLETE).isEqualTo(false);
+        mock.message(2).exchangeProperty(Exchange.BATCH_COMPLETE).isEqualTo(false);
+        mock.message(3).exchangeProperty(Exchange.BATCH_COMPLETE).isEqualTo(false);
+        mock.message(3).exchangeProperty(Exchange.BATCH_COMPLETE).isEqualTo(false);
+        mock.message(4).exchangeProperty(Exchange.BATCH_COMPLETE).isEqualTo(true);
+        mock.expectedPropertyReceived(Exchange.BATCH_SIZE, 5);
+    }
+    
+    @Override
+    protected JndiRegistry createRegistry() throws Exception {
+        JndiRegistry registry = super.createRegistry();
+        
+        AmazonSQSClientMock clientMock = new AmazonSQSClientMock();
+        // add 6 messages, one more we will poll
+        for (int counter = 0; counter < 6; counter++) {
+            Message message = new Message();
+            message.setBody("Message " + counter);
+            message.setMD5OfBody("6a1559560f67c5e7a7d5d838bf0272ee");
+            message.setMessageId("f6fb6f99-5eb2-4be4-9b15-144774141458");
+            message.setReceiptHandle("0NNAq8PwvXsyZkR6yu4nQ07FGxNmOBWi5");
+            
+            clientMock.messages.add(message);
+        }
+        
+        registry.bind("amazonSQSClient", clientMock);
+        
+        return registry;
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("aws-sqs://MyQueue?amazonSQSClient=#amazonSQSClient&delay=5000&maxMessagesPerPoll=5")
+                    .to("mock:result");
+            }
+        };
+    }
+}
---
+++ components/camel-aws-sqs/src/test/java/org/apache/camel/component/aws/sqs/SqsConfigurationTest.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.sqs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class SqsConfigurationTest {
+
+    @Test
+    public void itReturnsAnInformativeErrorForBadMessageGroupIdStrategy() throws Exception {
+        SqsConfiguration sqsConfiguration = new SqsConfiguration();
+        try {
+            sqsConfiguration.setMessageGroupIdStrategy("useUnknownStrategy");
+            fail("Should have thrown exception");
+        } catch (Exception e) {
+            assertTrue("Bad error message: " + e.getMessage(), e.getMessage().startsWith("Unrecognised MessageGroupIdStrategy"));
+        }
+    }
+
+    
+    @Test
+    public void itReturnsAnInformativeErrorForBadMessageDeduplicationIdStrategy() throws Exception {
+        SqsConfiguration sqsConfiguration = new SqsConfiguration();
+        try {
+            sqsConfiguration.setMessageDeduplicationIdStrategy("useUnknownStrategy");
+            fail("Should have thrown exception");
+        } catch (Exception e) {
+            assertTrue("Bad error message: " + e.getMessage(), e.getMessage().startsWith("Unrecognised MessageDeduplicationIdStrategy"));
+        }
+    }
+
+}
