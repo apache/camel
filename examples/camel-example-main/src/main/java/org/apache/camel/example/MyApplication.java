2073b04d79f85ae5ee15002bdda0ab19d5f6be94
Claus Ibsen <claus.ibsen@gmail.com>
2019-03-05 12:52:18 +0100

    CAMEL-13283: Add @BindRegistry annotation to allow to bind beans/classes to registry.

--- examples/camel-example-main/src/main/java/org/apache/camel/example/MyApplication.java
+++ examples/camel-example-main/src/main/java/org/apache/camel/example/MyApplication.java
@@ -29,10 +29,10 @@ public final class MyApplication {
     public static void main(String[] args) throws Exception {
         // use Camels Main class
         Main main = new Main();
-        // lets use a configuration class
-        // properties are automatic loaded from application.properties
-        main.setConfigurationClass(MyConfiguration.class);
-        // and add the routes
+        // lets use a configuration class (you can specify multiple classes)
+        // (properties are automatic loaded from application.properties)
+        main.addConfigurationClass(MyConfiguration.class);
+        // and add the routes (you can specify multiple classes)
         main.addRouteBuilder(MyRouteBuilder.class);
         // now keep the application running until the JVM is terminated (ctrl + c or sigterm)
         main.run(args);
