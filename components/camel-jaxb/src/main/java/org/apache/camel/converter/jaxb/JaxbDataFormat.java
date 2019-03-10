542211f08256a4567663a57b6a1ead655930ed93
Michael Lück <michael@lueckonline.net>
2019-03-10 21:34:01 +0100

    Fix for CAMEL-13282
    When a JAXB class that is created or generated has no @XmlRootElement
    annotation JaxbDataFormat would try to find a ObjectFactory method which
    can create a JAXBElement for the type of the object. It would then use
    the JAXBElement to marshal the object.
    
    While trying to find the objectFactory and to call the appropriate
    method it would catch ALL Exceptions that are thrown and just logs a
    message.
    
    While i don't see why an Exception should only be logged here this
    breaks when the exception is thrown due to a validation error. Because
    the calls to the marshal operation will throw an MarshalException in
    that case which would be caught by the catch block. In other cases where
    no object factory is used but JAXB classes with XmlRootElement
    Annotations or JAXBElements are used the MarshalExceptions will be
    thrown and the doMarshal method will be left.
    
    So i basically check the type of the exception that is thrown and
    rethrow it if it's a MarshalException. All other exceptions are handled
    in the same way as before. I kept the old exception handling in order to
    keep the code as much backwards compatible as possible. But it should be
    removed for 3.0.0 at the latest

--- components/camel-jaxb/src/main/java/org/apache/camel/converter/jaxb/JaxbDataFormat.java
+++ components/camel-jaxb/src/main/java/org/apache/camel/converter/jaxb/JaxbDataFormat.java
@@ -35,6 +35,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.JAXBIntrospector;
+import javax.xml.bind.MarshalException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.ValidationEvent;
@@ -61,7 +62,6 @@ import org.apache.camel.util.CamelContextHelper;
 import org.apache.camel.util.IOHelper;
 import org.apache.camel.util.ObjectHelper;
 import org.apache.camel.util.ResourceHelper;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -240,6 +240,15 @@ public class JaxbDataFormat extends ServiceSupport implements DataFormat, DataFo
                         return;
                     }
                 } catch (Exception e) {
+                    // if a schema is set then an MarshallException is thrown when the XML is not valid
+                    // and the method must throw this exception as it would when the object in the body is a root element
+                    // or a partial class (the other alternatives above)
+                    // 
+                    // it would be best to completely remove the exception handler here but it's left for backwards compatibility reasons.
+                    if (MarshalException.class.isAssignableFrom(e.getClass()) && schema != null) {
+                        throw e;
+                    }
+                    
                     LOG.debug("Unable to create JAXBElement object for type " + element.getClass() + " due to " + e.getMessage(), e);
                 }
             }
