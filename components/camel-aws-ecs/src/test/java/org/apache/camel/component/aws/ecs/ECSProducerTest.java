4d5c98c4b6771668105b5e9b8983db492fcb23e2
Andrea Cosentino <ancosen@gmail.com>
2019-03-12 11:05:35 +0100

    Camel-AWS ECS: Polished and fixed CS

--- components/camel-aws-ecs/src/test/java/org/apache/camel/component/aws/ecs/ECSProducerTest.java
+++ components/camel-aws-ecs/src/test/java/org/apache/camel/component/aws/ecs/ECSProducerTest.java
@@ -27,13 +27,12 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 public class ECSProducerTest extends CamelTestSupport {
-	
-	@BindToRegistry("amazonEcsClient")
+
+    @BindToRegistry("amazonEcsClient")
     AmazonECSClientMock clientMock = new AmazonECSClientMock();
     
     @EndpointInject(uri = "mock:result")
