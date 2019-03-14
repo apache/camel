6ec833b32e93395d3f82ae281377cf2a8f716246
Ramu <kkakarla@redhat.com>
2019-03-14 10:20:38 +0530

    CAMEL-13120:camel-spring-boot - implemented suggestions

--- components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/actuate/health/CamelHealthTest.java
+++ components/camel-spring-boot/src/test/java/org/apache/camel/spring/boot/actuate/health/CamelHealthTest.java
@@ -33,7 +33,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootApplication
 @SpringBootTest(
     classes = {CamelAutoConfiguration.class, CamelHealthAutoConfiguration.class, MyCamelRoute.class},
-    properties = {"management.endpoint.health.verbose = true"}
+    properties = {"management.info.camel.verbose = true"}
     )
 public class CamelHealthTest extends Assert {
 
@@ -53,6 +53,9 @@ public class CamelHealthTest extends Assert {
         
         String version = (String) health.getDetails().get("version");
         assertEquals(camelContext.getVersion(), version);
+        
+        String name = (String) health.getDetails().get("name");
+        assertEquals(camelContext.getName(), name);
     }
 
 }
