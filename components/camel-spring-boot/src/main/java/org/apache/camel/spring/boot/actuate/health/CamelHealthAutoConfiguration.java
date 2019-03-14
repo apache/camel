2c621d54ce15316835578155aeff3e5126944ad4
Ramu <kkakarla@redhat.com>
2019-03-14 08:37:14 +0530

    CAMEL-13120:camel-spring-boot - Add detailed option to turn on|off showing camel version etc

--- components/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/actuate/health/CamelHealthAutoConfiguration.java
+++ components/camel-spring-boot/src/main/java/org/apache/camel/spring/boot/actuate/health/CamelHealthAutoConfiguration.java
@@ -18,11 +18,14 @@ package org.apache.camel.spring.boot.actuate.health;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.spring.boot.CamelAutoConfiguration;
+import org.apache.camel.spring.boot.actuate.endpoint.CamelRoutesEndpointProperties;
+import org.apache.camel.spring.boot.health.HealthCheckVerboseConfiguration;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,15 +33,21 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass({HealthIndicator.class})
 @ConditionalOnBean(CamelAutoConfiguration.class)
 @AutoConfigureAfter(CamelAutoConfiguration.class)
+@EnableConfigurationProperties(HealthCheckVerboseConfiguration.class)
 public class CamelHealthAutoConfiguration {
+
+    private  HealthCheckVerboseConfiguration properties; 
+    public CamelHealthAutoConfiguration(HealthCheckVerboseConfiguration configuration) {
+        this.properties = configuration;
+    }
 
     @ConditionalOnClass({CamelContext.class})
     @ConditionalOnMissingBean(CamelHealthIndicator.class)
-    protected static class CamelHealthIndicatorInitializer {
+    protected  class CamelHealthIndicatorInitializer {
 
         @Bean
         public HealthIndicator camelHealthIndicator(CamelContext camelContext) {
-            return new CamelHealthIndicator(camelContext);
+            return new CamelHealthIndicator(camelContext, properties);
         }
 
     }
