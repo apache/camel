63f53628eb368b036f5539bc65e00df3c33950ef
Claus Ibsen <claus.ibsen@gmail.com>
2019-03-14 07:14:36 +0100

    CAMEL-13313: Add support for generating type converter loader source code to be able to load component type converters in a faster way.

--- components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL723Converter.java
+++ components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL723Converter.java
@@ -271,7 +271,7 @@ import org.apache.camel.converter.IOConverter;
 /**
  * HL7 converters.
  */
-@Converter(ignoreOnLoadError = true)
+@Converter(loader = true, ignoreOnLoadError = true)
 public final class HL723Converter {
 
     private static final HapiContext DEFAULT_CONTEXT;
--- components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL725Converter.java
+++ components/camel-hl7/src/main/java/org/apache/camel/component/hl7/HL725Converter.java
@@ -237,7 +237,7 @@ import org.apache.camel.converter.IOConverter;
 /**
  * HL7 converters.
  */
-@Converter(ignoreOnLoadError = true)
+@Converter(loader = true, ignoreOnLoadError = true)
 public final class HL725Converter {
 
     private static final HapiContext DEFAULT_CONTEXT;
