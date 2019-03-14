24ffc048604c0e290048872676c3b9b16c814b0b
Andrea Cosentino <ancosen@gmail.com>
2019-03-14 08:18:01 +0100

    Fixed Backport of camel-restlet tests

--- components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletProducerSSLTest.java
+++ components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletProducerSSLTest.java
@@ -22,10 +22,10 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.JndiRegistry;
-import org.apache.camel.util.jsse.KeyManagersParameters;
-import org.apache.camel.util.jsse.KeyStoreParameters;
-import org.apache.camel.util.jsse.SSLContextParameters;
-import org.apache.camel.util.jsse.TrustManagersParameters;
+import org.apache.camel.support.jsse.KeyManagersParameters;
+import org.apache.camel.support.jsse.KeyStoreParameters;
+import org.apache.camel.support.jsse.SSLContextParameters;
+import org.apache.camel.support.jsse.TrustManagersParameters;
 import org.junit.Test;
 
 public class RestletProducerSSLTest extends RestletTestSupport {
