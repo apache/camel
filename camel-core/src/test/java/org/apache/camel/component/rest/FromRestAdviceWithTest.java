8f75ceaa42aabf0dc48d7ea31b8a9beb794951df
Claus Ibsen <claus.ibsen@gmail.com>
2019-03-19 10:33:43 +0100

    (fix) CAMEL-13162: Fixed rest-dsl advice-with using inlined route with route id and replacing from should not append routeId as uri parameter.

---
+++ camel-core/src/test/java/org/apache/camel/component/rest/FromRestAdviceWithTest.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.rest;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.RouteDefinition;
+import org.junit.Test;
+
+public class FromRestAdviceWithTest extends ContextTestSupport {
+
+    @Override
+    public boolean isUseRouteBuilder() {
+        return false;
+    }
+
+    @Test
+    public void testAdviceWith() throws Exception {
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                rest("/say/hello")
+                    .get()
+                    .route().routeId("myRoute")
+                    .transform().constant("Bye World")
+                    .to("direct:hello");
+
+                from("direct:hello")
+                    .to("mock:hello");
+            }
+        });
+
+        RouteDefinition route = context.getRouteDefinition("myRoute");
+        route.adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                replaceFromWith("direct:foo");
+            }
+        });
+
+        context.start();
+
+        getMockEndpoint("mock:hello").expectedMessageCount(1);
+
+        String out = template.requestBody("direct:foo", "I was here", String.class);
+        assertEquals("Bye World", out);
+
+        assertMockEndpointsSatisfied();
+    }
+
+}
