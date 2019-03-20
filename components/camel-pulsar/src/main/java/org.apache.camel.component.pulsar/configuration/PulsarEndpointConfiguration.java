8aac728c56501f9dcb93a0f609ac52bc79ed3c95
Sherman Richard <shermanr@thehutgroup.com>
2019-03-20 16:27:15 +0000

    Add endpoint documentation

--- components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/configuration/PulsarEndpointConfiguration.java
+++ components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/configuration/PulsarEndpointConfiguration.java
@@ -11,33 +11,33 @@ import org.apache.pulsar.client.api.PulsarClient;
 @UriParams
 public class PulsarEndpointConfiguration {
 
-    @UriPath(label = "consumer, producer")
+    @UriPath(label = "consumer, producer", description = "Whether the topic is persistent or not", enums = "persistent, non-persistent")
     @Metadata(required = "true")
     private String topicType;
-    @UriPath(label = "consumer, producer")
+    @UriPath(label = "consumer, producer", description = "Pulsar tenant for the topic namespace to reside in")
     @Metadata(required = "true")
     private String tenant;
-    @UriPath(label = "consumer, producer")
+    @UriPath(label = "consumer, producer", description = "Pulsar namespace for the topic to reside in")
     @Metadata(required = "true")
     private String namespace;
-    @UriPath(label = "consumer, producer")
+    @UriPath(label = "consumer, producer", description = "Name of the Pulsar topic")
     @Metadata(required = "true")
     private String topic;
-    @UriParam(label = "consumer")
+    @UriParam(label = "consumer", description = "Name of the subscription to use")
     private String subscriptionName;
-    @UriParam(label = "consumer")
+    @UriParam(label = "consumer", description = "Type of the subscription", enums = "EXCLUSIVE, SHARED, FAILOVER")
     private SubscriptionType subscriptionType;
-    @UriParam(label = "consumer")
+    @UriParam(label = "consumer", description = "Number of consumers")
     private int numberOfConsumers;
-    @UriParam(label = "consumer")
+    @UriParam(label = "consumer", description = "Size of the consumer queue")
     private int consumerQueueSize;
-    @UriParam(label = "consumer")
+    @UriParam(label = "consumer", description = "Name of the consumer")
     private String consumerName;
-    @UriParam(label = "producer")
+    @UriParam(label = "producer", description = "Name of the producer")
     private String producerName;
-    @UriParam(label = "consumer")
+    @UriParam(label = "consumer", description = "Prefix to add to consumer names")
     private String consumerNamePrefix;
-    @UriParam(label = "consumer, producer")
+    @UriParam(label = "consumer, producer", description = "The pulsar client")
     private PulsarClient pulsarClient;
 
     public PulsarEndpointConfiguration(PulsarUri uri) {
