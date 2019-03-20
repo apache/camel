e579f8c3d71fec32faef44b6b56a1e9a5c2c9857
Abuah Chukwuedo <chukwuedo.abuah@thehutgroup.com>
2019-03-20 13:21:55 +0000

    Message headers passed unto camel

---
+++ components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/utils/PulsarMessageHeaders.java
@@ -0,0 +1,13 @@
+package org.apache.camel.component.pulsar.utils;
+
+public class PulsarMessageHeaders {
+    public static final String PRODUCER_NAME = "producer_name";
+    public static final String SEQUENCE_ID = "sequence_id";
+    public static final String PUBLISH_TIME = "publish_time";
+    public static final String PROPERTIES = "properties";
+    public static final String MESSAGE_ID = "message_id";
+    public static final String EVENT_TIME = "event_time";
+    public static final String KEY = "key";
+    public static final String KEY_BYTES = "key_bytes";
+    public static final String TOPIC_NAME = "topic_name";
+}
