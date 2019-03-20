efcea5d0eb179ea73713b556684572038d6bb278
Abuah Chukwuedo <chukwuedo.abuah@thehutgroup.com>
2019-03-20 14:18:35 +0000

    Make common builder strategy static

--- components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/utils/consumers/CommonCreationStrategy.java
+++ components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/utils/consumers/CommonCreationStrategy.java
@@ -6,9 +6,9 @@ import org.apache.camel.component.pulsar.PulsarMessageListener;
 import org.apache.camel.component.pulsar.configuration.PulsarEndpointConfiguration;
 import org.apache.pulsar.client.api.ConsumerBuilder;
 
-public class CommonCreationStrategy {
+public final class CommonCreationStrategy {
 
-    public ConsumerBuilder<byte[]> create(final String name, final PulsarEndpoint pulsarEndpoint, final PulsarConsumer pulsarConsumer) {
+    public static ConsumerBuilder<byte[]> create(final String name, final PulsarEndpoint pulsarEndpoint, final PulsarConsumer pulsarConsumer) {
         final PulsarEndpointConfiguration endpointConfiguration = pulsarEndpoint.getConfiguration();
 
         return pulsarEndpoint
