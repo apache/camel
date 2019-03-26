6cdedc93b9e0acda257e1932e0b836dc8097d0ed
Kevin Jackson <kevin.jackson@thehutgroup.com>
2019-03-26 05:52:13 +0000

    add license headers, tidy-up tests to remove unnecessary runner and reduce reliance on annaotations

--- components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/utils/AutoConfiguration.java
+++ components/camel-pulsar/src/main/java/org.apache.camel.component.pulsar/utils/AutoConfiguration.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.pulsar.utils;
 
 import org.apache.camel.component.pulsar.configuration.AdminConfiguration;
@@ -12,10 +28,13 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * What is the purpose of this? Needs documentation here
+ */
 public class AutoConfiguration {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AutoConfiguration.class);
-    private static final Pattern pattern = Pattern.compile("^(?<namespace>(?<tenant>.+)/.+)/.+$");
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutoConfiguration.class);
+    private static final Pattern URI_PATTERN = Pattern.compile("^(?<namespace>(?<tenant>.+)/.+)/.+$");
 
     private PulsarAdmin pulsarAdmin;
     private AdminConfiguration adminConfiguration;
@@ -24,11 +43,9 @@ public class AutoConfiguration {
         setPulsarAdmin(pulsarAdmin);
     }
 
-    public AutoConfiguration(){}
-
     public void ensureNameSpaceAndTenant(String path) {
         if(pulsarAdmin != null && adminConfiguration != null && adminConfiguration.isAutoCreateAllowed()) {
-            Matcher matcher = pattern.matcher(path);
+            Matcher matcher = URI_PATTERN.matcher(path);
             if (matcher.matches()) {
                 String tenant = matcher.group("tenant");
                 String namespace = matcher.group("namespace");
