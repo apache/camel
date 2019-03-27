aa782b0b5006d65a4f451edfc9c86900527cf10c
Mikael Grevsten <mikael.grevsten@tradechannel.se>
2019-03-27 16:49:43 +0100

    Fix using NIO utils instead

--- components/camel-leveldb/src/main/java/org/apache/camel/component/leveldb/LevelDBFile.java
+++ components/camel-leveldb/src/main/java/org/apache/camel/component/leveldb/LevelDBFile.java
@@ -1,23 +1,46 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright (c)  2000-2019, TradeChannel AB. All rights reserved.
+ * Any right to utilize the System under this Agreement shall be subject to the terms and condition of the
+ * License Agreement between Customer "X" and TradeChannel AB.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * TradeseC contains third party software which includes software owned or licensed by a third party and
+ * sub licensed to the Customer by TradeChannel AB in accordance with the License Agreement.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * TradeChannel AB owns the rights to the software product TradeseC.
+ *
+ * TradeChannel AB grants a right to the Customer and the Customer accepts a non-exclusive,
+ * non-transferrable license to use TradeseC and Third Party Software, in accordance with the conditions
+ * specified in this License Agreement.
+ *
+ * The Customer may not use TradeseC or the Third Party Software for time-sharing, rental,
+ * service bureau use, or similar use. The Customer is responsible for that all use of TradeseC
+ * and the Third Party Software is in accordance with the License Agreement.
+ *
+ * The Customer may not transfer, sell, sublicense, let, lend or in any other way permit any person or entity
+ * other than the Customer, avail himself, herself or itself of or otherwise any rights to TradeseC or the
+ * Third Party Software, either directly or indirectly.
+ *
+ * The Customer may not use, copy, modify or in any other way transfer or use TradeseC or the
+ * Third Party Software wholly or partially, nor allow another person or entity to do so, in any way other than
+ * what is expressly permitted according to the License Agreement. Nor, consequently, may the Customer,
+ * independently or through an agent, reverse engineer, decompile or disassemble TradeseC, the Third Party Software
+ * or any accessories that may be related to it.
+ *
+ * The Customer acknowledges TradeseC <i>(including but not limited to any copyrights, trademarks,
+ * documentation, enhancements or other intellectual property or proprietary rights relating to it)</i>
+ * and Third Party Software is the proprietary material of the Supplier and respectively Third Party.
+ *
+ * The Third Party Software are protected by copyright law.
+ *
+ * The Customer shall not remove, erase or hide from view any information about a patent, copyright,
+ * trademark, confidentiality notice, mark or legend appearing on any of TradeseC or Third Party Software,
+ * any medium by which they are made available or any form of output produced by them.
+ *
+ * The License Agreement will only grant the Customer the right to use TradeseC and Third Party Software
+ * under the terms of the License Agreement.
  */
 package org.apache.camel.component.leveldb;
-
-import java.io.File;
-import java.io.IOException;
 
 import org.apache.camel.Service;
 import org.apache.camel.util.IOHelper;
@@ -27,6 +50,12 @@ import org.iq80.leveldb.Options;
 import org.iq80.leveldb.WriteOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.fusesource.leveldbjni.JniDBFactory.factory;
 
@@ -56,27 +85,27 @@ public class LevelDBFile implements Service {
         return db;
     }
 
-    public void setFile(File file) throws IOException {
-        this.file = file;
-    }
-
     public File getFile() {
         return file;
     }
 
-    public void setFileName(String fileName) {
-        this.file = new File(fileName);
+    public void setFile(final File file) throws IOException {
+        this.file = file;
     }
 
     public String getFileName() throws IOException {
         return file.getCanonicalPath();
     }
 
+    public void setFileName(final String fileName) {
+        this.file = new File(fileName);
+    }
+
     public int getWriteBufferSize() {
         return writeBufferSize;
     }
 
-    public void setWriteBufferSize(int writeBufferSize) {
+    public void setWriteBufferSize(final int writeBufferSize) {
         this.writeBufferSize = writeBufferSize;
     }
 
@@ -84,7 +113,7 @@ public class LevelDBFile implements Service {
         return maxOpenFiles;
     }
 
-    public void setMaxOpenFiles(int maxOpenFiles) {
+    public void setMaxOpenFiles(final int maxOpenFiles) {
         this.maxOpenFiles = maxOpenFiles;
     }
 
@@ -92,7 +121,7 @@ public class LevelDBFile implements Service {
         return blockRestartInterval;
     }
 
-    public void setBlockRestartInterval(int blockRestartInterval) {
+    public void setBlockRestartInterval(final int blockRestartInterval) {
         this.blockRestartInterval = blockRestartInterval;
     }
 
@@ -100,7 +129,7 @@ public class LevelDBFile implements Service {
         return blockSize;
     }
 
-    public void setBlockSize(int blockSize) {
+    public void setBlockSize(final int blockSize) {
         this.blockSize = blockSize;
     }
 
@@ -108,7 +137,7 @@ public class LevelDBFile implements Service {
         return compressionType;
     }
 
-    public void setCompressionType(String compressionType) {
+    public void setCompressionType(final String compressionType) {
         this.compressionType = compressionType;
     }
 
@@ -116,7 +145,7 @@ public class LevelDBFile implements Service {
         return verifyChecksums;
     }
 
-    public void setVerifyChecksums(boolean verifyChecksums) {
+    public void setVerifyChecksums(final boolean verifyChecksums) {
         this.verifyChecksums = verifyChecksums;
     }
 
@@ -124,7 +153,7 @@ public class LevelDBFile implements Service {
         return paranoidChecks;
     }
 
-    public void setParanoidChecks(boolean paranoidChecks) {
+    public void setParanoidChecks(final boolean paranoidChecks) {
         this.paranoidChecks = paranoidChecks;
     }
 
@@ -132,7 +161,7 @@ public class LevelDBFile implements Service {
         return cacheSize;
     }
 
-    public void setCacheSize(long cacheSize) {
+    public void setCacheSize(final long cacheSize) {
         this.cacheSize = cacheSize;
     }
 
@@ -140,12 +169,12 @@ public class LevelDBFile implements Service {
         return sync;
     }
 
-    public void setSync(boolean sync) {
+    public void setSync(final boolean sync) {
         this.sync = sync;
     }
 
     public WriteOptions getWriteOptions() {
-        WriteOptions options = new WriteOptions();
+        final WriteOptions options = new WriteOptions();
         options.sync(sync);
         return options;
     }
@@ -159,7 +188,7 @@ public class LevelDBFile implements Service {
             LOG.debug("Starting LevelDB using file: {}", getFile());
         }
 
-        Options options = new Options().writeBufferSize(writeBufferSize).maxOpenFiles(maxOpenFiles)
+        final Options options = new Options().writeBufferSize(writeBufferSize).maxOpenFiles(maxOpenFiles)
                 .blockRestartInterval(blockRestartInterval).blockSize(blockSize).verifyChecksums(verifyChecksums)
                 .paranoidChecks(paranoidChecks).cacheSize(cacheSize);
 
@@ -171,15 +200,16 @@ public class LevelDBFile implements Service {
 
         options.createIfMissing(true);
         try {
-            getFile().getParentFile().mkdirs();
+            final Path dbFile = Paths.get(this.getFileName());
+            Files.createDirectories(dbFile.getParent());
             db = factory.open(getFile(), options);
-        } catch (IOException ioe) {
+        } catch (final IOException ioe) {
             throw new RuntimeException("Error opening LevelDB with file " + getFile(), ioe);
         }
     }
 
     public void stop() {
-        File file = getFile();
+        final File file = getFile();
 
         LOG.debug("Stopping LevelDB using file: {}", file);
         if (db != null) {
---
+++ components/camel-leveldb/src/test/java/org/apache/camel/component/leveldb/LevelDBSetupTest.java
@@ -0,0 +1,87 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * Copyright (c)  2000-2019, TradeChannel AB. All rights reserved.
+ * Any right to utilize the System under this Agreement shall be subject to the terms and condition of the
+ * License Agreement between Customer "X" and TradeChannel AB.
+ *
+ * TradeseC contains third party software which includes software owned or licensed by a third party and
+ * sub licensed to the Customer by TradeChannel AB in accordance with the License Agreement.
+ *
+ * TradeChannel AB owns the rights to the software product TradeseC.
+ *
+ * TradeChannel AB grants a right to the Customer and the Customer accepts a non-exclusive,
+ * non-transferrable license to use TradeseC and Third Party Software, in accordance with the conditions
+ * specified in this License Agreement.
+ *
+ * The Customer may not use TradeseC or the Third Party Software for time-sharing, rental,
+ * service bureau use, or similar use. The Customer is responsible for that all use of TradeseC
+ * and the Third Party Software is in accordance with the License Agreement.
+ *
+ * The Customer may not transfer, sell, sublicense, let, lend or in any other way permit any person or entity
+ * other than the Customer, avail himself, herself or itself of or otherwise any rights to TradeseC or the
+ * Third Party Software, either directly or indirectly.
+ *
+ * The Customer may not use, copy, modify or in any other way transfer or use TradeseC or the
+ * Third Party Software wholly or partially, nor allow another person or entity to do so, in any way other than
+ * what is expressly permitted according to the License Agreement. Nor, consequently, may the Customer,
+ * independently or through an agent, reverse engineer, decompile or disassemble TradeseC, the Third Party Software
+ * or any accessories that may be related to it.
+ *
+ * The Customer acknowledges TradeseC <i>(including but not limited to any copyrights, trademarks,
+ * documentation, enhancements or other intellectual property or proprietary rights relating to it)</i>
+ * and Third Party Software is the proprietary material of the Supplier and respectively Third Party.
+ *
+ * The Third Party Software are protected by copyright law.
+ *
+ * The Customer shall not remove, erase or hide from view any information about a patent, copyright,
+ * trademark, confidentiality notice, mark or legend appearing on any of TradeseC or Third Party Software,
+ * any medium by which they are made available or any form of output produced by them.
+ *
+ * The License Agreement will only grant the Customer the right to use TradeseC and Third Party Software
+ * under the terms of the License Agreement.
+ */
+
+package org.apache.camel.component.leveldb;
+
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * The class LevelDBSetupTest
+ *
+ * @author mgr,  2019-03-27
+ * @version 1.0
+ */
+public class LevelDBSetupTest extends CamelTestSupport {
+    private LevelDBFile levelDBFile;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testLevelDBStartWithNoPath() throws IOException {
+        deleteDirectory("leveldb.dat");
+        levelDBFile = new LevelDBFile();
+        levelDBFile.setFileName("leveldb.dat");
+        levelDBFile.start();
+        levelDBFile.stop();
+    }
+
+    @Test
+    public void testLevelDBStartWithPath() throws IOException {
+        deleteDirectory("target/data");
+        levelDBFile = new LevelDBFile();
+        levelDBFile.setFileName("target/data/leveldb.dat");
+        levelDBFile.start();
+        levelDBFile.stop();
+    }
+}
