47137ff416f2cac9525c9ddb921766eaf8ed946c
Andrea Cosentino <ancosen@gmail.com>
2019-03-27 16:16:48 +0100

    Camel-Digitalocean: Use BindToRegistry annotation where possible in tests

--- components/camel-digitalocean/src/test/java/org/apache/camel/component/digitalocean/DigitalOceanComponentTest.java
+++ components/camel-digitalocean/src/test/java/org/apache/camel/component/digitalocean/DigitalOceanComponentTest.java
@@ -18,18 +18,22 @@ package org.apache.camel.component.digitalocean;
 
 import com.myjeeva.digitalocean.impl.DigitalOceanClient;
 import com.myjeeva.digitalocean.pojo.Account;
+
+import org.apache.camel.BindToRegistry;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.digitalocean.constants.DigitalOceanHeaders;
 import org.apache.camel.component.digitalocean.constants.DigitalOceanOperations;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
 public class DigitalOceanComponentTest extends CamelTestSupport {
-
+	
+    @BindToRegistry("digitalOceanClient")
+    DigitalOceanClient digitalOceanClient = new DigitalOceanClientMock();
+    
     @EndpointInject("mock:result")
     protected MockEndpoint mockResultEndpoint;
 
@@ -53,14 +57,5 @@ public class DigitalOceanComponentTest extends CamelTestSupport {
         assertMockEndpointsSatisfied();
         assertIsInstanceOf(Account.class, exchange.getOut().getBody());
         assertEquals(exchange.getIn().getBody(Account.class).getEmail(), "camel@apache.org");
-    }
-
-
-    @Override
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry registry = super.createRegistry();
-        DigitalOceanClient digitalOceanClient = new DigitalOceanClientMock();
-        registry.bind("digitalOceanClient", digitalOceanClient);
-        return registry;
     }
 }
