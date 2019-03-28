b23e3a4c65e5d0a401bc46233c34933ef038d870
Mikael Grevsten <mikael.grevsten@tradechannel.se>
2019-03-28 12:21:05 +0100

    Fixed checkstyle issues
    Signed-off-by: Mikael Grevsten <mikael.grevsten@tradechannel.se>

--- components/camel-leveldb/src/main/java/org/apache/camel/component/leveldb/LevelDBFile.java
+++ components/camel-leveldb/src/main/java/org/apache/camel/component/leveldb/LevelDBFile.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,11 @@
  */
 package org.apache.camel.component.leveldb;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.apache.camel.Service;
 import org.apache.camel.util.IOHelper;
 import org.iq80.leveldb.CompressionType;
@@ -25,11 +30,6 @@ import org.iq80.leveldb.WriteOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static org.fusesource.leveldbjni.JniDBFactory.factory;
 
