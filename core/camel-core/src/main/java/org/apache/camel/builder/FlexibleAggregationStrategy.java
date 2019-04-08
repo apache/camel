b16adb4af6a5c5c1f74763939b876ee29af3da66
Gregor Zurowski <gregor@zurowski.org>
2019-04-08 08:35:01 +0200

    [Minor] Remove redundant declaration

--- core/camel-core/src/main/java/org/apache/camel/builder/FlexibleAggregationStrategy.java
+++ core/camel-core/src/main/java/org/apache/camel/builder/FlexibleAggregationStrategy.java
@@ -44,7 +44,7 @@ import org.slf4j.LoggerFactory;
  * <p/>
  * This Aggregation Strategy is suitable for usage in aggregate, split, multicast, enrich and recipient list EIPs.
  */
-public class FlexibleAggregationStrategy<E extends Object> implements AggregationStrategy {
+public class FlexibleAggregationStrategy<E> implements AggregationStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(FlexibleAggregationStrategy.class);
 
