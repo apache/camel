b1278645923e0410466cb3bd0f22618c8927f5bb
nferraro <ni.ferraro@gmail.com>
2019-04-09 15:53:56 +0200

    CAMEL-13401: create a webhook meta component

---
+++ components/camel-webhook/src/main/java/org/apache/camel/component/webhook/WebhookComponent.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.webhook;
+
+import java.util.Map;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.RestConfiguration;
+import org.apache.camel.spi.annotations.Component;
+import org.apache.camel.support.DefaultComponent;
+import org.apache.camel.util.ObjectHelper;
+
+/**
+ * A Camel meta-component for exposing other components through webhooks.
+ */
+@Component("webhook")
+public class WebhookComponent extends DefaultComponent {
+
+    @Metadata(label = "advanced")
+    private WebhookConfiguration configuration;
+
+    public WebhookComponent() {
+    }
+
+    @Override
+    protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
+        String delegateUri = remaining;
+        if (ObjectHelper.isEmpty(delegateUri)) {
+            throw new IllegalArgumentException("Wrong uri syntax : webhook:uri, got " + remaining);
+        }
+
+        WebhookConfiguration configuration = getConfiguration().copy();
+        setProperties(configuration, parameters);
+
+        RestConfiguration restConfig = getCamelContext().getRestConfiguration(configuration.getWebhookComponentName(), true);
+        configuration.setRestConfiguration(restConfig);
+
+        // we need to apply the params here
+        if (parameters != null && parameters.size() > 0) {
+            delegateUri = delegateUri + "?" + uri.substring(uri.indexOf('?') + 1);
+        }
+        configuration.setEndpointUri(delegateUri);
+
+        return new WebhookEndpoint(
+                uri,
+                this,
+                configuration,
+                delegateUri
+        );
+    }
+
+
+    public WebhookConfiguration getConfiguration() {
+        if (this.configuration == null) {
+            this.configuration = new WebhookConfiguration();
+        }
+        return this.configuration;
+    }
+
+    /**
+     * Set the default configuration for the webhook meta-component.
+     */
+    public void setConfiguration(WebhookConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+}
---
+++ components/camel-webhook/src/test/java/org/apache/camel/component/webhook/support/TestEndpoint.java
@@ -0,0 +1,137 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.webhook.support;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.apache.camel.Component;
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
+import org.apache.camel.component.webhook.WebhookCapableEndpoint;
+import org.apache.camel.component.webhook.WebhookConfiguration;
+import org.apache.camel.support.DefaultEndpoint;
+
+/**
+ * A test endpoint for testing webhook capabilities
+ */
+public class TestEndpoint extends DefaultEndpoint implements WebhookCapableEndpoint {
+
+    private static final List<String> DEFAULT_METHOD = Collections.unmodifiableList(Collections.singletonList("POST"));
+
+    private Function<Processor, Processor> webhookHandler;
+
+    private Runnable register;
+
+    private Runnable unregister;
+
+    private Supplier<List<String>> methods;
+
+    private Supplier<Producer> producer;
+
+    private Function<Processor, Consumer> consumer;
+
+    private WebhookConfiguration webhookConfiguration;
+
+    private boolean singleton;
+
+    public TestEndpoint(String endpointUri, Component component) {
+        super(endpointUri, component);
+    }
+
+    @Override
+    public Processor createWebhookHandler(Processor next) {
+        if (this.webhookHandler != null) {
+            return this.webhookHandler.apply(next);
+        }
+        return next;
+    }
+
+    @Override
+    public void registerWebhook() {
+        if (this.register != null) {
+            this.register.run();
+        }
+    }
+
+    @Override
+    public void unregisterWebhook() {
+        if (this.unregister != null) {
+            this.unregister.run();
+        }
+    }
+
+    @Override
+    public void setWebhookConfiguration(WebhookConfiguration webhookConfiguration) {
+        this.webhookConfiguration = webhookConfiguration;
+    }
+
+    public WebhookConfiguration getWebhookConfiguration() {
+        return webhookConfiguration;
+    }
+
+    @Override
+    public List<String> getWebhookMethods() {
+        return this.methods != null ? this.methods.get() : DEFAULT_METHOD;
+    }
+
+    @Override
+    public Producer createProducer() throws Exception {
+        return this.producer != null ? this.producer.get() : null;
+    }
+
+    @Override
+    public Consumer createConsumer(Processor processor) throws Exception {
+        return this.consumer != null ? this.consumer.apply(processor) : null;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return singleton;
+    }
+
+    public void setSingleton(boolean singleton) {
+        this.singleton = singleton;
+    }
+
+    public void setWebhookHandler(Function<Processor, Processor> webhookHandler) {
+        this.webhookHandler = webhookHandler;
+    }
+
+    public void setRegisterWebhook(Runnable register) {
+        this.register = register;
+    }
+
+    public void setUnregisterWebhook(Runnable unregister) {
+        this.unregister = unregister;
+    }
+
+    public void setWebhookMethods(Supplier<List<String>> methods) {
+        this.methods = methods;
+    }
+
+    public void setWebhookProducer(Supplier<Producer> producer) {
+        this.producer = producer;
+    }
+
+    public void setConsumer(Function<Processor, Consumer> consumer) {
+        this.consumer = consumer;
+    }
+}
