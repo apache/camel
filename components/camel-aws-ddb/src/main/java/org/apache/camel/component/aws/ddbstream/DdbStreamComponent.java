fbfa44e92b8e8234a632e8a567287c17d890e53c
Andrea Cosentino <ancosen@gmail.com>
2019-04-09 09:22:47 +0200

    CAMEL-13398 - Fixed CS for AWS-DDB and AWS-DDBStreams

--- components/camel-aws-ddb/src/main/java/org/apache/camel/component/aws/ddbstream/DdbStreamComponent.java
+++ components/camel-aws-ddb/src/main/java/org/apache/camel/component/aws/ddbstream/DdbStreamComponent.java
@@ -19,16 +19,14 @@ package org.apache.camel.component.aws.ddbstream;
 import java.util.Map;
 import java.util.Set;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBStreams;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
-import org.apache.camel.component.aws.ddb.DdbConfiguration;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 import org.apache.camel.util.ObjectHelper;
-
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBStreams;
 
 @Component("aws-ddbstream")
 public class DdbStreamComponent extends DefaultComponent {
