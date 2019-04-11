bc0cb5bb0571e0044c5958142c519e9d2b7b610f
Andrea Cosentino <ancosen@gmail.com>
2019-04-11 09:07:51 +0200

    CAMEL-13398 - Fixed CS for AWS-SES

--- components/camel-aws-ses/src/main/java/org/apache/camel/component/aws/ses/SesComponent.java
+++ components/camel-aws-ses/src/main/java/org/apache/camel/component/aws/ses/SesComponent.java
@@ -19,14 +19,14 @@ package org.apache.camel.component.aws.ses;
 import java.util.Map;
 import java.util.Set;
 
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
 import org.apache.camel.util.ObjectHelper;
-
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 
 @Component("aws-ses")
 public class SesComponent extends DefaultComponent {
