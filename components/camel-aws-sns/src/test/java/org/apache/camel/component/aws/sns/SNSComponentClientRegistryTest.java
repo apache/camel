7ad5963c9f0a702773fac67ac4f2164aa071e4e0
Andrea Cosentino <ancosen@gmail.com>
2019-04-11 09:59:47 +0200

    CAMEL-13398 - Camel-AWS-*: Lets adopt the same approach as CAMEL-12936 for all the components, camel-aws-sns

---
+++ components/camel-aws-sns/src/test/java/org/apache/camel/component/aws/sns/SNSComponentClientRegistryTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws.sns;
+
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class SNSComponentClientRegistryTest extends CamelTestSupport {
+
+    @Test
+    public void createEndpointWithMinimalSNSClientConfiguration() throws Exception {
+
+        AmazonSNSClientMock awsSNSClient = new AmazonSNSClientMock();
+        context.getRegistry().bind("awsSNSClient", awsSNSClient);
+        SnsComponent component = new SnsComponent(context);
+        SnsEndpoint endpoint = (SnsEndpoint) component.createEndpoint("aws-sns://MyTopic");
+
+        assertNotNull(endpoint.getConfiguration().getAmazonSNSClient());
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void createEndpointWithMinimalSNSClientMisconfiguration() throws Exception {
+
+        SnsComponent component = new SnsComponent(context);
+        SnsEndpoint endpoint = (SnsEndpoint) component.createEndpoint("aws-sns://MyTopic");
+    }
+}
