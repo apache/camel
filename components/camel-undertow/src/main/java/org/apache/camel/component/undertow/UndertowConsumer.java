7ba1a86d7abbc0b0ba379d44e5ea0e8495a7d2a7
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2019-04-18 11:39:04 +0900

    CAMEL-13428: camel-undertow - Response with large data gets truncated on cloud

--- components/camel-undertow/src/main/java/org/apache/camel/component/undertow/UndertowConsumer.java
+++ components/camel-undertow/src/main/java/org/apache/camel/component/undertow/UndertowConsumer.java
@@ -106,7 +106,7 @@ public class UndertowConsumer extends DefaultConsumer implements HttpHandler {
             httpExchange.getResponseHeaders().put(ExchangeHeaders.CONTENT_LENGTH, 0);
             // do not include content-type as that would indicate to the caller that we can only do text/plain
             httpExchange.getResponseHeaders().put(Headers.ALLOW, allowedMethods);
-            httpExchange.getResponseSender().close();
+            httpExchange.endExchange();
             return;
         }
 
@@ -141,7 +141,6 @@ public class UndertowConsumer extends DefaultConsumer implements HttpHandler {
             ByteBuffer bodyAsByteBuffer = tc.convertTo(ByteBuffer.class, body);
             httpExchange.getResponseSender().send(bodyAsByteBuffer);
         }
-        httpExchange.getResponseSender().close();
     }
 
     /**
