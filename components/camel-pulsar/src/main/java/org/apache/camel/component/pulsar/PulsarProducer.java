eadc9f06c34392f36f139d9444e5521669d325e4
Sherman Richard <shermanr@thehutgroup.com>
2019-04-25 14:47:29 +0100

    Fix new connection per message. Also use configured producername if set

--- components/camel-pulsar/src/main/java/org/apache/camel/component/pulsar/PulsarProducer.java
+++ components/camel-pulsar/src/main/java/org/apache/camel/component/pulsar/PulsarProducer.java
@@ -16,57 +16,72 @@
  */
 package org.apache.camel.component.pulsar;
 
-import java.util.Map;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.NoTypeConversionAvailableException;
 import org.apache.camel.TypeConversionException;
-import org.apache.camel.component.pulsar.utils.message.PulsarMessageHeaders;
 import org.apache.camel.component.pulsar.utils.message.PulsarMessageUtils;
-import org.apache.camel.support.DefaultProducer;
+import org.apache.camel.impl.DefaultProducer;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.ProducerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PulsarProducer extends DefaultProducer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PulsarProducer.class);
+
     private final PulsarEndpoint pulsarEndpoint;
+    private Producer<byte[]> producer;
 
     public PulsarProducer(PulsarEndpoint pulsarEndpoint) {
         super(pulsarEndpoint);
-
         this.pulsarEndpoint = pulsarEndpoint;
+    }
+
+    public static PulsarProducer create(final PulsarEndpoint pulsarEndpoint) {
+        return new PulsarProducer(pulsarEndpoint);
     }
 
     @Override
     public void process(final Exchange exchange) throws Exception {
         final Message message = exchange.getIn();
-        final String topic = pulsarEndpoint.getTopic();
-        final String producerName = topic + "-" + Thread.currentThread().getId();
-
-        final Map properties = message.getHeader(PulsarMessageHeaders.PROPERTIES, Map.class);
-
-        final ProducerBuilder<byte[]> producerBuilder = pulsarEndpoint
-            .getPulsarClient()
-            .newProducer()
-            .producerName(producerName)
-            .topic(topic);
-
-        if (properties != null && !properties.isEmpty()) {
-            producerBuilder.properties(properties);
+        if(producer == null) {
+            createProducer();
         }
-
-        final Producer<byte[]> producer = producerBuilder.create();
-
+        byte[] body;
         try {
-            byte[] body = exchange.getContext().getTypeConverter()
+            body = exchange.getContext().getTypeConverter()
                 .mandatoryConvertTo(byte[].class, exchange, message.getBody());
-            producer.send(body);
         } catch (NoTypeConversionAvailableException | TypeConversionException exception) {
-            // fallback to try serialize the data
-            byte[] body = PulsarMessageUtils.serialize(message.getBody());
-            producer.send(body);
-        } finally {
+            LOGGER.info("An error occurred while serializing to byte array, using fall back strategy", exception);
+            body = PulsarMessageUtils.serialize(message.getBody());
+        }
+        producer.send(body);
+    }
+
+    private synchronized void createProducer() throws org.apache.pulsar.client.api.PulsarClientException {
+        if (producer == null) {
+            final String topic = pulsarEndpoint.getTopic();
+            String producerName;
+            if (pulsarEndpoint.getConfiguration().getProducerName() == null) {
+                producerName = topic + "-" + Thread.currentThread().getId();
+            } else {
+                producerName = pulsarEndpoint.getConfiguration().getProducerName();
+            }
+            final ProducerBuilder<byte[]> producerBuilder = pulsarEndpoint
+                    .getPulsarClient()
+                    .newProducer()
+                    .producerName(producerName)
+                    .topic(topic);
+            producer = producerBuilder.create();
+        }
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        log.debug("Stopping producer: {}", this);
+        if (producer != null) {
             producer.close();
         }
     }
