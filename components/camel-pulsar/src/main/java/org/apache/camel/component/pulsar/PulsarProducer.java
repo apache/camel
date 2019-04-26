ef8e57ca2d75d4d7ad1fc82546b7ff869e1d3a5f
Sherman Richard <shermanr@thehutgroup.com>
2019-04-26 13:32:22 +0100

    Fix performance issues in sending messages to Pulsar

--- components/camel-pulsar/src/main/java/org/apache/camel/component/pulsar/PulsarProducer.java
+++ components/camel-pulsar/src/main/java/org/apache/camel/component/pulsar/PulsarProducer.java
@@ -22,7 +22,6 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.NoTypeConversionAvailableException;
 import org.apache.camel.TypeConversionException;
-import org.apache.camel.component.pulsar.utils.message.PulsarMessageHeaders;
 import org.apache.camel.component.pulsar.utils.message.PulsarMessageUtils;
 import org.apache.camel.impl.DefaultProducer;
 import org.apache.pulsar.client.api.Producer;
@@ -31,6 +30,7 @@ import org.apache.pulsar.client.api.ProducerBuilder;
 public class PulsarProducer extends DefaultProducer {
 
     private final PulsarEndpoint pulsarEndpoint;
+    private Producer<byte[]> producer;
 
     public PulsarProducer(PulsarEndpoint pulsarEndpoint) {
         super(pulsarEndpoint);
@@ -41,32 +41,45 @@ public class PulsarProducer extends DefaultProducer {
     @Override
     public void process(final Exchange exchange) throws Exception {
         final Message message = exchange.getIn();
-        final String topic = pulsarEndpoint.getTopic();
-        final String producerName = topic + "-" + Thread.currentThread().getId();
-
-        final Map properties = message.getHeader(PulsarMessageHeaders.PROPERTIES, Map.class);
-
-        final ProducerBuilder<byte[]> producerBuilder = pulsarEndpoint
-            .getPulsarClient()
-            .newProducer()
-            .producerName(producerName)
-            .topic(topic);
-
-        if (properties != null && !properties.isEmpty()) {
-            producerBuilder.properties(properties);
-        }
-
-        final Producer<byte[]> producer = producerBuilder.create();
-
+        byte[] body;
         try {
-            byte[] body = exchange.getContext().getTypeConverter()
-                .mandatoryConvertTo(byte[].class, exchange, message.getBody());
-            producer.send(body);
+            body = exchange.getContext().getTypeConverter()
+                    .mandatoryConvertTo(byte[].class, exchange, message.getBody());
         } catch (NoTypeConversionAvailableException | TypeConversionException exception) {
             // fallback to try serialize the data
-            byte[] body = PulsarMessageUtils.serialize(message.getBody());
-            producer.send(body);
-        } finally {
+            body = PulsarMessageUtils.serialize(message.getBody());
+        }
+        producer.send(body);
+    }
+
+    private synchronized void createProducer() throws org.apache.pulsar.client.api.PulsarClientException {
+        if (producer == null) {
+            final String topic = pulsarEndpoint.getTopic();
+            String producerName = pulsarEndpoint.getPulsarConfiguration().getProducerName();
+            if (producerName == null) {
+                producerName = topic + "-" + Thread.currentThread().getId();
+            }
+            final ProducerBuilder<byte[]> producerBuilder = pulsarEndpoint
+                    .getPulsarClient()
+                    .newProducer()
+                    .producerName(producerName)
+                    .topic(topic);
+            producer = producerBuilder.create();
+        }
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        log.debug("Starting producer: {}", this);
+        if (producer == null) {
+            createProducer();
+        }
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        log.debug("Stopping producer: {}", this);
+        if (producer != null) {
             producer.close();
         }
     }
