16884ce42e168f0f592cee95e34a27c48c3b074c
bd2019us <>
2019-05-10 18:44:16 -0500

    [CAMEL-13499] Use Random.nextDouble instead of Math.random for better performance

--- examples/camel-example-cdi-metrics/src/main/java/org/apache/camel/example/cdi/metrics/UnreliableService.java
+++ examples/camel-example-cdi-metrics/src/main/java/org/apache/camel/example/cdi/metrics/UnreliableService.java
@@ -21,13 +21,15 @@ import javax.enterprise.context.ApplicationScoped;
 import com.codahale.metrics.annotation.Metered;
 import org.apache.camel.Exchange;
 import org.apache.camel.RuntimeExchangeException;
+import java.util.Random;
 
 @ApplicationScoped
 public class UnreliableService {
 
     @Metered
     public void attempt(Exchange exchange) {
-        if (Math.random() < 0.5) {
+	Random rand = new Random();
+        if (rand.nextDouble() < 0.5) {
             throw new RuntimeExchangeException("Random failure", exchange);
         }
     }
