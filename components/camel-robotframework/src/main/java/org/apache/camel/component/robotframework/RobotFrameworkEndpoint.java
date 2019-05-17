009fc03126034d266d875335e24ad0599010e0a7
onders <onders@apache.org>
2019-05-17 16:12:13 +0300

    create camel-robotframework component camel-robotframework component: rebase and polish add karaf feature for robotframework component docs and cleanup Fix CS
    CR changes

---
+++ components/camel-robotframework/src/main/java/org/apache/camel/component/robotframework/RobotFrameworkEndpoint.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.robotframework;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.component.ResourceEndpoint;
+import org.apache.camel.spi.UriEndpoint;
+import org.apache.camel.spi.UriParam;
+import org.apache.camel.util.ObjectHelper;
+import org.robotframework.RobotFramework;
+
+/**
+ * Represents a RobotFramework endpoint.
+ */
+@UriEndpoint(firstVersion = "3.0.0", scheme = "robotframework", title = "Robot Framework", syntax = "robotframework:resourceUri", label = "testing")
+public class RobotFrameworkEndpoint extends ResourceEndpoint {
+
+    @UriParam
+    private RobotFrameworkCamelConfiguration configuration;
+
+    public RobotFrameworkEndpoint(String uri, RobotFrameworkComponent component, String resourceUri, RobotFrameworkCamelConfiguration configuration) {
+        super(uri, component, resourceUri);
+        this.configuration = configuration;
+    }
+
+    @Override
+    public ExchangePattern getExchangePattern() {
+        return ExchangePattern.InOut;
+    }
+
+    @Override
+    protected String createEndpointUri() {
+        return "robotframework:" + getResourceUri();
+    }
+
+    public RobotFrameworkCamelConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    @Override
+    protected void onExchange(Exchange exchange) throws Exception {
+        // create robot arguments to pass
+        RobotFrameworkArguments generatedArguments = new RobotFrameworkArguments();
+
+        generatedArguments.addFileToArguments(configuration.getOutputDirectory(), "-d");
+        generatedArguments.addFileToArguments(configuration.getOutput(), "-o");
+        generatedArguments.addFileToArguments(configuration.getLog(), "-l");
+        generatedArguments.addFileToArguments(configuration.getReport(), "-r");
+        generatedArguments.addFileToArguments(configuration.getDebugFile(), "-b");
+        generatedArguments.addFileToArguments(configuration.getArgumentFile(), "-A");
+        generatedArguments.addFileToArguments(configuration.getRunFailed(), "-R");
+
+        generatedArguments.addNonEmptyStringToArguments(configuration.getName(), "-N");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getDocument(), "-D");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getRunMode(), "--runmode");
+        generatedArguments.addFlagToArguments(configuration.isDryrun(), "--dryrun");
+        generatedArguments.addFlagToArguments(configuration.isExitOnFailure(), "--exitonfailure");
+        generatedArguments.addFlagToArguments(configuration.isSkipTeardownOnExit(), "--skipteardownonexit");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getRandomize(), "--randomize");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getSplitOutputs(), "--splitoutputs");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getLogTitle(), "--logtitle");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getReportTitle(), "--reporttitle");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getReportBackground(), "--reportbackground");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getSummaryTitle(), "--summarytitle");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getLogLevel(), "-L");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getSuiteStatLevel(), "--suitestatlevel");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getMonitorWidth(), "--monitorwidth");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getMonitorColors(), "--monitorcolors");
+        generatedArguments.addNonEmptyStringToArguments(configuration.getListener(), "--listener");
+
+        generatedArguments.addFlagToArguments(configuration.isRunEmptySuite(), "--runemptysuite");
+        generatedArguments.addFlagToArguments(configuration.isNoStatusReturnCode(), "--nostatusrc");
+        generatedArguments.addFlagToArguments(configuration.isTimestampOutputs(), "-T");
+        generatedArguments.addFlagToArguments(configuration.isWarnOnSkippedFiles(), "--warnonskippedfiles");
+
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getMetadata() != null ? configuration.getMetadata() : "").split(",")), "-M");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getTags() != null ? configuration.getTags() : "").split(",")), "-G");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getTests() != null ? configuration.getTests() : "").split(",")), "-t");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getSuites() != null ? configuration.getSuites() : "").split(",")), "-s");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getIncludes() != null ? configuration.getIncludes() : "").split(",")), "-i");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getExcludes() != null ? configuration.getExcludes() : "").split(",")), "-e");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getCriticalTags() != null ? configuration.getCriticalTags() : "").split(",")), "-c");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getNonCriticalTags() != null ? configuration.getNonCriticalTags() : "").split(",")), "-n");
+
+        // create variables from camel exchange to pass into robot
+        List<String> variables = RobotFrameworkCamelUtils.createRobotVariablesFromCamelExchange(exchange);
+        exchange.getIn().setHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_VARIABLES, variables);
+        generatedArguments.addListToArguments(variables, "-v");
+
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getVariableFiles() != null ? configuration.getVariableFiles() : "").split(",")), "-V");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getTagStatIncludes() != null ? configuration.getTagStatIncludes() : "").split(",")), "--tagstatinclude");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getTagStatExcludes() != null ? configuration.getTagStatExcludes() : "").split(",")), "--tagstatexclude");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getCombinedTagStats() != null ? configuration.getCombinedTagStats() : "").split(",")), "--tagstatcombine");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getTagDocs() != null ? configuration.getTagDocs() : "").split(",")), "--tagdoc");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getTagStatLinks() != null ? configuration.getTagStatLinks() : "").split(",")), "--tagstatlink");
+        generatedArguments.addListToArguments(Arrays.asList((configuration.getListeners() != null ? configuration.getListeners() : "").split(",")), "--listener");
+
+        // process path and set robot env by that to specify which test cases to
+        // run
+        // either from a directory or from a file
+        String path = getResourceUri();
+        ObjectHelper.notNull(path, "resourceUri");
+        log.debug("RobotFrameworkEndpoint resourceUri:{}", path);
+        
+        String newResourceUri = exchange.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RESOURCE_URI, String.class);
+        if (newResourceUri != null) {
+            exchange.getIn().removeHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RESOURCE_URI);
+            log.debug("{} set to {} setting resourceUri to pass robotframework", RobotFrameworkCamelConstants.CAMEL_ROBOT_RESOURCE_URI, newResourceUri);
+            path = newResourceUri;
+        }
+
+        if (configuration.getXunitFile() == null) {
+            configuration.setXunitFile(new File("TEST-" + path.replace(' ', '_') + ".xml"));
+        }
+        generatedArguments.addFileToArguments(configuration.getXunitFile(), "-x");
+        generatedArguments.addFlagToArguments(true, "--xunitskipnoncritical");
+
+        generatedArguments.add(path);
+
+        // run robot framework
+        int camelRobotReturnCode = RobotFramework.run(generatedArguments.toArray());
+        
+        exchange.getIn().setHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE, camelRobotReturnCode);
+    }
+}
---
+++ components/camel-robotframework/src/test/java/org/apache/camel/component/robotframework/RobotFrameworkComponentTest.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.robotframework;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.util.ObjectHelper;
+import org.junit.Test;
+
+public class RobotFrameworkComponentTest extends CamelTestSupport {
+
+    @Test
+    public void testRobotFrameworkCamelBodyAsString() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        MockEndpoint mockString = getMockEndpoint("mock:resultString");
+        mock.expectedMinimumMessageCount(1);
+        mockString.expectedMinimumMessageCount(1);
+
+        template.sendBody("direct:setVariableCamelBody", "Hello Robot");
+        template.sendBody("direct:assertRobotCamelInputAsString", "Hello Robot");
+
+        assertMockEndpointsSatisfied();
+        
+        Exchange exchange = mock.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchange.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+        Exchange exchangeString = mockString.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchangeString.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+    }
+
+    @Test
+    public void testRobotFrameworkCamelBodyAsNumeric() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        MockEndpoint mockNumeric = getMockEndpoint("mock:resultNumeric");
+        mock.expectedMinimumMessageCount(1);
+        mockNumeric.expectedMinimumMessageCount(1);
+
+        template.sendBody("direct:setVariableCamelBody", 1);
+        template.sendBody("direct:assertRobotCamelInputAsNumeric", 1);
+
+        assertMockEndpointsSatisfied();
+        
+        Exchange exchange = mock.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchange.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+        Exchange exchangeNumeric = mockNumeric.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchangeNumeric.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+    }
+    
+    @Test
+    public void testRobotFrameworkCamelBodyAndHeaderAsString() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:resultHeader");
+        mock.expectedMinimumMessageCount(1);
+
+        template.sendBodyAndHeader("direct:setVariableCamelBodyAndHeader", "Hello Robot", "stringKey", "headerValue");
+
+        assertMockEndpointsSatisfied();
+
+        Exchange exchange = mock.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchange.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+    }
+    
+    @Test
+    public void testRobotFrameworkCamelBodyAndPropertyAsString() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:resultProperty");
+        mock.expectedMinimumMessageCount(1);
+
+        template.sendBodyAndProperty("direct:setVariableCamelBodyAndProperty", "Hello Robot", "stringKey", "propertyValue");
+
+        assertMockEndpointsSatisfied();
+        
+        Exchange exchange = mock.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchange.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+    }
+    
+    @Test
+    public void testRobotFrameworkResourceUriHeader() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:resultResourceUri");
+        mock.expectedMinimumMessageCount(1);
+
+        template.sendBody("direct:setVariableCamelBodyResourceUri", "Hello Robot");
+
+        assertMockEndpointsSatisfied();
+        
+        Exchange exchange = mock.getExchanges().get(0);
+        assertTrue(ObjectHelper.cast(Integer.class,
+                exchange.getIn().getHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RETURN_CODE)) == 0);
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:setVariableCamelBody").to("robotframework:src/test/resources/org/apache/camel/component/robotframework/set_variable_camel_body.robot?xunitFile=target/out.xml")
+                    .to("mock:result");
+
+                from("direct:assertRobotCamelInputAsString")
+                    .to("robotframework:src/test/resources/org/apache/camel/component/robotframework/assert_string_robot_with_camel_exchange_value_as_string.robot?xunitFile=target/out.xml")
+                    .to("mock:resultString");
+
+                from("direct:assertRobotCamelInputAsNumeric")
+                    .to("robotframework:src/test/resources/org/apache/camel/component/robotframework/assert_string_robot_with_camel_exchange_value_as_numeric.robot?xunitFile=target/out.xml")
+                    .to("mock:resultNumeric");
+                
+                from("direct:setVariableCamelBodyAndHeader").to("robotframework:src/test/resources/org/apache/camel/component/robotframework/set_variable_camel_header.robot?xunitFile=target/out.xml")
+                    .to("mock:resultHeader");
+                
+                from("direct:setVariableCamelBodyAndProperty").to("robotframework:src/test/resources/org/apache/camel/component/robotframework/set_variable_camel_property.robot?xunitFile=target/out.xml")
+                    .to("mock:resultProperty");
+                
+                from("direct:setVariableCamelBodyResourceUri")
+                    .setHeader(RobotFrameworkCamelConstants.CAMEL_ROBOT_RESOURCE_URI).constant("src/test/resources/org/apache/camel/component/robotframework/set_variable_camel_body.robot")
+                    .to("robotframework:dummy?xunitFile=target/out.xml")
+                    .to("mock:resultResourceUri");
+            }
+        };
+    }
+}
