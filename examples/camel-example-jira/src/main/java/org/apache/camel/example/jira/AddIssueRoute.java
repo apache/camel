d7b83cb3596e4f9d41145bdb3e4832b15ed057b0
Andrea Cosentino <ancosen@gmail.com>
2019-05-17 14:31:53 +0200

    CAMEL-13390 - Fixed CS in the example

--- examples/camel-example-jira/src/main/java/org/apache/camel/example/jira/AddIssueRoute.java
+++ examples/camel-example-jira/src/main/java/org/apache/camel/example/jira/AddIssueRoute.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,12 +31,12 @@ import static org.apache.camel.component.jira.JiraConstants.ISSUE_TYPE_NAME;
 @Component
 public class AddIssueRoute extends RouteBuilder {
 
-    private static final Logger logger = LoggerFactory.getLogger(AddIssueRoute.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AddIssueRoute.class);
 
     @Override
     public void configure() {
 
-        logger.info(" >>>>>>>>>>>>>>>>>>>>> jira example - add new issue");
+        LOG.info(" >>>>>>>>>>>>>>>>>>>>> jira example - add new issue");
         // change the fields accordinly to your target jira server
         from("timer://foo?fixedRate=true&period=50000")
                 .setHeader(ISSUE_PROJECT_KEY, () -> "COM")
--- examples/camel-example-jira/src/main/java/org/apache/camel/example/jira/AttachFileRoute.java
+++ examples/camel-example-jira/src/main/java/org/apache/camel/example/jira/AttachFileRoute.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,11 +26,11 @@ import static org.apache.camel.component.jira.JiraConstants.ISSUE_KEY;
 @Component
 public class AttachFileRoute extends RouteBuilder {
 
-    private static final Logger _log = LoggerFactory.getLogger(AttachFileRoute.class);
+    private static final Logger LOG = LoggerFactory.getLogger(AttachFileRoute.class);
 
     @Override
     public void configure() {
-        _log.info(" >>>>>>>>>>>>>>>>>>>>> jira example - add attachment");
+        LOG.info(" >>>>>>>>>>>>>>>>>>>>> jira example - add attachment");
         // change the fields accordinly to your target jira server
         from("file:///A_valid_directory?fileName=my_file.png&noop=true&delay=50000")
                 .setHeader(ISSUE_KEY, () -> "MYP-13")
