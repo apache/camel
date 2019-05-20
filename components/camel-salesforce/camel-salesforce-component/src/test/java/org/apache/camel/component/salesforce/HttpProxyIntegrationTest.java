9caef912514221e43cf27b009416ec60b3da39e1
aldettinger <aldettinger@gmail.com>
2019-05-20 17:55:18 +0200

    CAMEL-13550: Fixed some integration tests in camel-salesforce

--- components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/HttpProxyIntegrationTest.java
+++ components/camel-salesforce/camel-salesforce-component/src/test/java/org/apache/camel/component/salesforce/HttpProxyIntegrationTest.java
@@ -61,7 +61,7 @@ public class HttpProxyIntegrationTest extends AbstractSalesforceTestBase {
     private static Server server;
     private static int httpProxyPort;
 
-    private static final AtomicBoolean WENT_TORUGH_PROXY = new AtomicBoolean();
+    private static final AtomicBoolean WENT_THROUGH_PROXY = new AtomicBoolean();
 
     @Parameter(0)
     public Consumer<SalesforceComponent> configurationMethod;
@@ -77,7 +77,7 @@ public class HttpProxyIntegrationTest extends AbstractSalesforceTestBase {
         doTestGetVersions("");
         doTestGetVersions("Xml");
 
-        assertTrue("Should have gone through the test proxy", WENT_TORUGH_PROXY.get());
+        assertTrue("Should have gone through the test proxy", WENT_THROUGH_PROXY.get());
     }
 
     @SuppressWarnings("unchecked")
@@ -120,7 +120,7 @@ public class HttpProxyIntegrationTest extends AbstractSalesforceTestBase {
                     return false;
                 }
                 LOG.info("Request contains required header " + PROXY_AUTHORIZATION);
-                WENT_TORUGH_PROXY.set(true);
+                WENT_THROUGH_PROXY.set(true);
                 return true;
             }
         };
