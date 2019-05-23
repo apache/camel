8262f0c3316e21a2ccc5e31d740cd727477c7714
Claus Ibsen <claus.ibsen@gmail.com>
2019-05-23 06:38:33 +0200

    CAMEL-13557: Add property binding support to make it convenient to configure components and whatnot.

--- core/camel-support/src/main/java/org/apache/camel/support/PropertyBindingSupport.java
+++ core/camel-support/src/main/java/org/apache/camel/support/PropertyBindingSupport.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.RuntimeCamelException;
 
 import static org.apache.camel.support.IntrospectionSupport.findSetterMethods;
 import static org.apache.camel.support.IntrospectionSupport.getOrElseProperty;
@@ -29,6 +30,7 @@ import static org.apache.camel.support.IntrospectionSupport.getOrElseProperty;
  * A convenient support class for binding String valued properties to an instance which
  * uses a set of conventions:
  * <ul>
+ *     <li>property placeholders - Keys and values using Camels property placeholder will be resolved</li>
  *     <li>nested - Properties can be nested using the dot syntax (OGNL and builder pattern using with as prefix), eg foo.bar=123</li>
  *     <li>reference by id - Values can refer to other beans in the registry by prefixing with # syntax, eg #myBean</li>
  * </ul>
@@ -41,34 +43,52 @@ public final class PropertyBindingSupport {
     private PropertyBindingSupport() {
     }
 
+    /**
+     * Binds the properties to the target object.
+     *
+     * @param camelContext  the camel context
+     * @param target        the target object
+     * @param properties    the properties
+     * @return              true if one or more properties was bound, false otherwise
+     */
     public static boolean bindProperties(CamelContext camelContext, Object target, Map<String, Object> properties) throws Exception {
-        boolean answer = true;
+        boolean answer = false;
         for (Map.Entry<String, Object> entry : properties.entrySet()) {
-            answer &= bindProperty(camelContext, target, entry.getKey(), entry.getValue());
+            answer |= bindProperty(camelContext, target, entry.getKey(), entry.getValue());
         }
         return answer;
     }
 
+    /**
+     * Binds the property to the target object.
+     *
+     * @param camelContext  the camel context
+     * @param target        the target object
+     * @param name          name of property
+     * @param value         value of property
+     * @return              true if property was bound, false otherwise
+     */
     public static boolean bindProperty(CamelContext camelContext, Object target, String name, Object value) throws Exception {
-        return setProperty(camelContext, target, name, value, null, true, true);
+        if (target != null && name != null) {
+            return setProperty(camelContext, target, name, value);
+        } else {
+            return false;
+        }
     }
 
-    /**
-     * This method supports two modes to set a property:
-     *
-     * 1. Setting a property that has already been resolved, this is the case when {@code context} and {@code refName} are
-     * NULL and {@code value} is non-NULL.
-     *
-     * 2. Setting a property that has not yet been resolved, the property will be resolved based on the suitable methods
-     * found matching the property name on the {@code target} bean. For this mode to be triggered the parameters
-     * {@code context} and {@code refName} must NOT be NULL, and {@code value} MUST be NULL.
-     */
-    private static boolean setProperty(CamelContext context, Object target, String name, Object value, String refName,
-                                      boolean allowBuilderPattern, boolean allowNestedProperties) throws Exception {
+    private static boolean setProperty(CamelContext context, Object target, String name, Object value) {
         Class<?> clazz = target.getClass();
+        String refName = null;
+
+        // resolve property placeholders
+        name = context.resolvePropertyPlaceholders(name);
+        if (value instanceof String) {
+            // resolve property placeholders
+            value = context.resolvePropertyPlaceholders(value.toString());
+        }
 
         // if name has dot then we need to OGNL walk it
-        if (allowNestedProperties && name.indexOf('.') > 0) {
+        if (name.indexOf('.') > 0) {
             String[] parts = name.split("\\.");
             Object newTarget = target;
             Class<?> newClass = clazz;
@@ -78,7 +98,7 @@ public final class PropertyBindingSupport {
                 Object prop = getOrElseProperty(newTarget, part, null);
                 if (prop == null) {
                     // okay is there a setter so we can create a new instance and set it automatic
-                    Set<Method> newSetters = findSetterMethods(newClass, part, allowBuilderPattern);
+                    Set<Method> newSetters = findSetterMethods(newClass, part, true);
                     if (newSetters.size() == 1) {
                         Method method = newSetters.iterator().next();
                         Class<?> parameterType = method.getParameterTypes()[0];
@@ -98,7 +118,6 @@ public final class PropertyBindingSupport {
             }
             // okay we found a nested property, then lets change to use that
             target = newTarget;
-            clazz = newTarget.getClass();
             name = parts[parts.length - 1];
             if (value instanceof String) {
                 if (EndpointHelper.isReferenceParameter(value.toString())) {
@@ -109,7 +128,11 @@ public final class PropertyBindingSupport {
             }
         }
 
-        return IntrospectionSupport.setProperty(context, context.getTypeConverter(), target, name, value, refName, allowBuilderPattern);
+        try {
+            return IntrospectionSupport.setProperty(context, context.getTypeConverter(), target, name, value, refName, true);
+        } catch (Exception e) {
+            throw RuntimeCamelException.wrapRuntimeException(e);
+        }
     }
 
 }
