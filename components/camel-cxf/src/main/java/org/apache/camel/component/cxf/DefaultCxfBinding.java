ec56002b1a9740cf0422231b21a685c7b12f88c9
Freeman Fang <freeman.fang@gmail.com>
2019-05-24 11:21:06 -0400

    [CAMEL-13576]avoid adding cxf message context map into camel exchange

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/DefaultCxfBinding.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/DefaultCxfBinding.java
@@ -526,7 +526,6 @@ public class DefaultCxfBinding implements CxfBinding, HeaderFilterStrategyAware 
             cxfContext.putAll(camelExchange.getProperties());
         }
         
-        camelExchange.setProperty(contextKey, cxfContext);
     }
 
     /**
