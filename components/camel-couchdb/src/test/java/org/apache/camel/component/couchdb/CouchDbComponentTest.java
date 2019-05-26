5da68724d140a25d078e061b6cbb583bd08eed0a
Claus Ibsen <claus.ibsen@gmail.com>
2019-05-26 10:54:58 +0200

    CAMEL-13557: Fixed test to use property binding support as the old has removed the legacy JDK PropertyEditor in CAMEL-13566

--- components/camel-couchdb/src/test/java/org/apache/camel/component/couchdb/CouchDbComponentTest.java
+++ components/camel-couchdb/src/test/java/org/apache/camel/component/couchdb/CouchDbComponentTest.java
@@ -30,6 +30,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CouchDbComponentTest {
@@ -50,6 +53,8 @@ public class CouchDbComponentTest {
 
     @Test
     public void testPropertiesSet() throws Exception {
+        when(context.resolvePropertyPlaceholders(anyString())).then(returnsFirstArg());
+
         Map<String, Object> params = new HashMap<>();
         params.put("createDatabase", true);
         params.put("username", "coldplay");
