9008caab173f51dbcfdbf3a10c1a4953d48180ef
Claus Ibsen <claus.ibsen@gmail.com>
2019-05-28 06:10:15 +0200

    Fixed CS

--- components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsXMLRouteTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsXMLRouteTest.java
@@ -25,10 +25,10 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.util.xml.StringSource;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.util.xml.StringSource;
 import org.junit.Test;
 
 import static org.apache.camel.component.jms.JmsComponent.jmsComponentAutoAcknowledge;
