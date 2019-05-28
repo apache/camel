02b2f265a57b757155757fc330c1d7108574bcfa
Claus Ibsen <claus.ibsen@gmail.com>
2019-05-28 14:03:48 +0200

    CAMEL-13587: Inflight repository browse should compute elapsed if the message is still inflight at a given node. Thanks to Barbara De Vido for reporting.

--- core/camel-api/src/main/java/org/apache/camel/MessageHistory.java
+++ core/camel-api/src/main/java/org/apache/camel/MessageHistory.java
@@ -38,6 +38,7 @@ public interface MessageHistory {
 
     /**
      * Gets the elapsed time in millis processing the node took
+     * (this is 0 until the node processing is done)
      */
     long getElapsed();
 
