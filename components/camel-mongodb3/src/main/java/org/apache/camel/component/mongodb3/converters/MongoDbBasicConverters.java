feb723a40888d18d2c63af770c3047c2d4ad0fd9
Denis Istomin <istomin.den@gmail.com>
2019-05-31 20:19:10 +0500

    Fix MongoDB InputStream conversion bug

--- components/camel-mongodb3/src/main/java/org/apache/camel/component/mongodb3/converters/MongoDbBasicConverters.java
+++ components/camel-mongodb3/src/main/java/org/apache/camel/component/mongodb3/converters/MongoDbBasicConverters.java
@@ -106,6 +106,8 @@ public final class MongoDbBasicConverters {
             } else if (!Character.isWhitespace(input[i])) {
                 return true;
             }
+
+            i++;
         }
         return true;
     }
