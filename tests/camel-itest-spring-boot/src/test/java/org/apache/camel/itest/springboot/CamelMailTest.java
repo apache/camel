b367016d57b4cafae7d386499c8ba8eb08bf9db7
Freeman Fang <freeman.fang@gmail.com>
2019-06-03 11:40:05 -0400

    [CAMEL-13593]ensure javax.mail dependency still in camel-mail.pom
    (cherry picked from commit f581b977a48f3d0f1f820276e3938563450fa273)
    (cherry picked from commit 26464c1ec03d7796e9ed4db5901f240bdea3c702)
    (cherry picked from commit dc402cabbdbd40132222e36b425a5f78f9acde2d)

--- tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelMailTest.java
+++ tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelMailTest.java
@@ -35,7 +35,6 @@ public class CamelMailTest extends AbstractSpringBootTestSupport {
     public static ITestConfig createTestConfig() {
         return new ITestConfigBuilder()
                 .module(inferModuleName(CamelMailTest.class))
-                .dependency("javax.mail:javax.mail-api")
                 .build();
         // sometimes the arquillian test fails, but the unit tests don't.
     }
