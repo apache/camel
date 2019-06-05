3e81b1f5d004349f5fe627b41ac6b1bea3665b1c
Andrea Cosentino <ancosen@gmail.com>
2019-06-05 15:03:27 +0200

    CAMEL-13614 - Fixed CS

--- components/camel-yammer/src/main/java/org/apache/camel/component/yammer/model/PhoneNumber.java
+++ components/camel-yammer/src/main/java/org/apache/camel/component/yammer/model/PhoneNumber.java
@@ -23,22 +23,26 @@ public class PhoneNumber {
 
     private String type;
     private String number;
-    
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
-	@Override
-	public String toString() {
-		return "PhoneNumber [type=" + type + ", number=" + number + "]";
-	}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneNumber [type=" + type + ", number=" + number + "]";
+    }
 
 }
