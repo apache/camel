c8eb104d5f810967be4914c37342e6549a77bee1
Claus Ibsen <claus.ibsen@gmail.com>
2019-06-05 05:23:22 +0200

    Fixed CS

--- core/camel-core/src/main/java/org/apache/camel/model/RouteFilters.java
+++ core/camel-core/src/main/java/org/apache/camel/model/RouteFilters.java
@@ -1,13 +1,13 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,6 +37,13 @@ public final class RouteFilters implements Function<RouteDefinition, Boolean> {
     private final String[] includes;
     private final String[] excludes;
 
+    private RouteFilters(String include, String exclude) {
+        this.includesText = include;
+        this.excludesText = exclude;
+        this.includes = include != null ? include.split(",") : null;
+        this.excludes = exclude != null ? exclude.split(",") : null;
+    }
+
     /**
      * Used for filtering routes routes matching the given pattern, which follows the following rules:
      *
@@ -57,13 +64,6 @@ public final class RouteFilters implements Function<RouteDefinition, Boolean> {
      */
     public static Function<RouteDefinition, Boolean> filterByPattern(String include, String exclude) {
         return new RouteFilters(include, exclude);
-    }
-
-    private RouteFilters(String include, String exclude) {
-        this.includesText = include;
-        this.excludesText = exclude;
-        this.includes = include != null ? include.split(",") : null;
-        this.excludes = exclude != null ? exclude.split(",") : null;
     }
 
     @Override
