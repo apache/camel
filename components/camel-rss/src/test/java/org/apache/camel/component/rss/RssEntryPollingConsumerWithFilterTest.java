0ebce9da4df5d698dc1ca6d7cbf8df750afe5309
Colm O hEigeartaigh <coheigea@apache.org>
2019-06-06 12:49:10 +0100

    Updating Rome dependency

--- components/camel-rss/src/test/java/org/apache/camel/component/rss/RssEntryPollingConsumerWithFilterTest.java
+++ components/camel-rss/src/test/java/org/apache/camel/component/rss/RssEntryPollingConsumerWithFilterTest.java
@@ -20,14 +20,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndFeed;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.spi.Registry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
 
 public class RssEntryPollingConsumerWithFilterTest extends CamelTestSupport {
 
@@ -68,7 +69,7 @@ public class RssEntryPollingConsumerWithFilterTest extends CamelTestSupport {
         public boolean isAfterDate(Exchange ex) {
             SyndFeed feed = ex.getIn().getBody(SyndFeed.class);
             assertTrue(feed.getEntries().size() == 1);
-            SyndEntry entry = (SyndEntry) feed.getEntries().get(0);
+            SyndEntry entry = feed.getEntries().get(0);
             return entry.getPublishedDate().after(time);
         }
     }
