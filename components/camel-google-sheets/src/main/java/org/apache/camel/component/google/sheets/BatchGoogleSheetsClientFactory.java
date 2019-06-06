99c54ce18a3b41aec80ca006ae90d790d62320c3
Christoph Deppisch <cdeppisch@redhat.com>
2019-06-06 10:26:45 +0200

    CAMEL-13617: Improve testability of google-sheets component and incorporate latest enhancements to 3.x

--- components/camel-google-sheets/src/main/java/org/apache/camel/component/google/sheets/BatchGoogleSheetsClientFactory.java
+++ components/camel-google-sheets/src/main/java/org/apache/camel/component/google/sheets/BatchGoogleSheetsClientFactory.java
@@ -18,49 +18,78 @@ package org.apache.camel.component.google.sheets;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import org.apache.camel.RuntimeCamelException;
+import org.apache.camel.util.ObjectHelper;
 
 public class BatchGoogleSheetsClientFactory implements GoogleSheetsClientFactory {
 
-    private final NetHttpTransport transport;
+    private final HttpTransport transport;
     private final JacksonFactory jsonFactory;
 
     public BatchGoogleSheetsClientFactory() {
-        this.transport = new NetHttpTransport();
-        this.jsonFactory = new JacksonFactory();
+        this(new NetHttpTransport(), new JacksonFactory());
+    }
+
+    public BatchGoogleSheetsClientFactory(HttpTransport httpTransport) {
+        this(httpTransport, new JacksonFactory());
+    }
+
+    public BatchGoogleSheetsClientFactory(HttpTransport httpTransport, JacksonFactory jacksonFactory) {
+        this.transport = httpTransport;
+        this.jsonFactory = jacksonFactory;
     }
 
     @Override
-    public Sheets makeClient(String clientId, String clientSecret, String applicationName, String refreshToken, String accessToken) {
+    public Sheets makeClient(String clientId,
+                             String clientSecret,
+                             String applicationName,
+                             String refreshToken,
+                             String accessToken) {
         if (clientId == null || clientSecret == null) {
             throw new IllegalArgumentException("clientId and clientSecret are required to create Google Sheets client.");
         }
-        try {
-            Credential credential = authorize(clientId, clientSecret);
 
-            if (refreshToken != null && !"".equals(refreshToken)) {
-                credential.setRefreshToken(refreshToken);
-            }
-            if (accessToken != null && !"".equals(accessToken)) {
-                credential.setAccessToken(accessToken);
-            }
-            return new Sheets.Builder(transport, jsonFactory, credential)
-                                .setApplicationName(applicationName)
-                                .build();
+        try {
+            Credential credential = authorize(clientId, clientSecret, refreshToken, accessToken);
+
+            Sheets.Builder clientBuilder = new Sheets.Builder(transport, jsonFactory, credential)
+                                                     .setApplicationName(applicationName);
+            configure(clientBuilder);
+            return clientBuilder.build();
         } catch (Exception e) {
             throw new RuntimeCamelException("Could not create Google Sheets client.", e);
         }
     }
 
+    /**
+     * Subclasses may add customized configuration to client builder.
+     * @param clientBuilder
+     */
+    protected void configure(Sheets.Builder clientBuilder) {
+        clientBuilder.setRootUrl(Sheets.DEFAULT_ROOT_URL);
+    }
+
     // Authorizes the installed application to access user's protected data.
-    private Credential authorize(String clientId, String clientSecret) {
+    private Credential authorize(String clientId, String clientSecret, String refreshToken, String accessToken) {
         // authorize
-        return new GoogleCredential.Builder()
+        Credential credential = new GoogleCredential.Builder()
                         .setJsonFactory(jsonFactory)
                         .setTransport(transport)
-                        .setClientSecrets(clientId, clientSecret).build();
+                        .setClientSecrets(clientId, clientSecret)
+                        .build();
+
+        if (ObjectHelper.isNotEmpty(refreshToken)) {
+            credential.setRefreshToken(refreshToken);
+        }
+
+        if (ObjectHelper.isNotEmpty(accessToken)) {
+            credential.setAccessToken(accessToken);
+        }
+
+        return credential;
     }
 }
