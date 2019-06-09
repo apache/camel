bb088383ed8115bb536803e4416c3a12127a48e1
Claus Ibsen <claus.ibsen@gmail.com>
2019-06-09 12:39:35 +0200

    CAMEL-13625: Fixed camel-quarz2 fireNow to let the quartz scheduler be starter after the consumer. Thanks to Deepak for the analsys.

--- components/camel-quartz2/src/main/java/org/apache/camel/component/quartz2/QuartzComponent.java
+++ components/camel-quartz2/src/main/java/org/apache/camel/component/quartz2/QuartzComponent.java
@@ -25,6 +25,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
+import org.apache.camel.ExtendedStartupListener;
 import org.apache.camel.StartupListener;
 import org.apache.camel.impl.UriEndpointComponent;
 import org.apache.camel.spi.Metadata;
@@ -50,7 +51,7 @@ import org.slf4j.LoggerFactory;
  * of the code, but mostly has been re-written in attempt to be more easier to maintain, and use Quartz more
  * fully.</p>
  */
-public class QuartzComponent extends UriEndpointComponent implements StartupListener {
+public class QuartzComponent extends UriEndpointComponent implements ExtendedStartupListener {
     private static final Logger LOG = LoggerFactory.getLogger(QuartzComponent.class);
     @Metadata(label = "advanced")
     private Scheduler scheduler;
@@ -457,16 +458,28 @@ public class QuartzComponent extends UriEndpointComponent implements StartupList
 
     @Override
     public void onCamelContextStarted(CamelContext context, boolean alreadyStarted) throws Exception {
+        if (alreadyStarted) {
+            // a route may have been added or starter after CamelContext is started so ensure we startup the scheduler
+            doStartScheduler();
+        }
+    }
+
+    @Override
+    public void onCamelContextFullyStarted(CamelContext context, boolean alreadyStarted) throws Exception {
+        doStartScheduler();
+    }
+
+    protected void doStartScheduler() throws Exception {
         // If Camel has already started and then user add a route dynamically, we need to ensure
         // to create and init the scheduler first.
         if (scheduler == null) {
             createAndInitScheduler();
         } else {
-            // in case custom scheduler was injected (i.e. created elsewhere), we may need to add 
+            // in case custom scheduler was injected (i.e. created elsewhere), we may need to add
             // current camel context to quartz context so jobs have access
             storeCamelContextInQuartzContext();
         }
-        
+
         // Now scheduler is ready, let see how we should start it.
         if (!autoStartScheduler) {
             LOG.info("Not starting scheduler because autoStartScheduler is set to false.");
