d303182f8d6bca686c3eacc34a0226301b159cae
Claus Ibsen <claus.ibsen@gmail.com>
2019-06-13 10:07:09 +0200

    CAMEL-13636: camel3 - SPI for ReactiveHelper so we can plugin different reactive engines

--- core/camel-reactive-executor-vertx/src/main/java/org/apache/camel/reactive/vertx/VertXReactiveExecutor.java
+++ core/camel-reactive-executor-vertx/src/main/java/org/apache/camel/reactive/vertx/VertXReactiveExecutor.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.reacitve;
+package org.apache.camel.reactive.vertx;
 
 import io.vertx.core.Vertx;
 import org.apache.camel.StaticService;
