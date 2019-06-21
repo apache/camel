97bd6290eafc2e131398573bfb7d0298fe030140
Andrea Cosentino <ancosen@gmail.com>
2019-06-21 16:06:05 +0200

    Camel-Nats: Use BindToRegistry annotation where possible in tests

--- components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsProducerTLSTest.java
+++ components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsProducerTLSTest.java
@@ -16,8 +16,8 @@
  */
 package org.apache.camel.component.nats;
 
+import org.apache.camel.BindToRegistry;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.support.jsse.KeyManagersParameters;
 import org.apache.camel.support.jsse.KeyStoreParameters;
 import org.apache.camel.support.jsse.SSLContextParameters;
@@ -31,18 +31,16 @@ import org.junit.Test;
  */
 @Ignore("Require a running Nats server")
 public class NatsProducerTLSTest extends CamelTestSupport {
-    
+   
+
+	@BindToRegistry("ssl")
+	SSLContextParameters ssl = createSSLContextParameters();
+
+	
     @Test
     public void sendTest() throws Exception {
         
         template.sendBody("direct:send", "pippo");
-    }
-    
-    @Override
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry registry = super.createRegistry();
-        registry.bind("ssl", createSSLContextParameters());
-        return registry;
     }
     
     private SSLContextParameters createSSLContextParameters() {
