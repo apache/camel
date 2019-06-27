9deb209a68302403f468ac935e47efb1cfdc9dfc
Freeman Fang <freeman.fang@gmail.com>
2019-06-27 11:29:23 -0400

    [CAMEL-13693]avoid useOverridePropertiesWithPropertiesComponent being invoked twice when running a unit-test with OSGI Blueprint XML.
    (cherry picked from commit 728bbcbe7cf0b58db367072b08d0ff1444e24b10)

--- components/camel-test/src/main/java/org/apache/camel/test/junit4/CamelTestSupport.java
+++ components/camel-test/src/main/java/org/apache/camel/test/junit4/CamelTestSupport.java
@@ -104,6 +104,7 @@ public abstract class CamelTestSupport extends TestSupport {
     private static ThreadLocal<FluentProducerTemplate> threadFluentTemplate = new ThreadLocal<>();
     private static ThreadLocal<ConsumerTemplate> threadConsumer = new ThreadLocal<>();
     private static ThreadLocal<Service> threadService = new ThreadLocal<>();
+    protected Properties extra;
     protected volatile ModelCamelContext context;
     protected volatile ProducerTemplate template;
     protected volatile FluentProducerTemplate fluentTemplate;
@@ -365,7 +366,9 @@ public abstract class CamelTestSupport extends TestSupport {
 
         // configure properties component (mandatory for testing)
         PropertiesComponent pc = context.getComponent("properties", PropertiesComponent.class);
-        Properties extra = useOverridePropertiesWithPropertiesComponent();
+        if (extra == null) {
+            extra = useOverridePropertiesWithPropertiesComponent();
+        }
         if (extra != null && !extra.isEmpty()) {
             pc.setOverrideProperties(extra);
         }
