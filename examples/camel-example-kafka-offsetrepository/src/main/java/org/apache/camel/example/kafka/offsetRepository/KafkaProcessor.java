be02d730decbb838cf0ccf711b4fc4c808ef10bb
Kodanda Ramu Kakarla <kkakarla@kkakarla.pnq.csb>
2019-07-02 20:44:11 +0530

    "CAMEL-13710:Camel kafka with Spring FileStateRepository example"

---
+++ examples/camel-example-kafka-offsetrepository/src/main/java/org/apache/camel/example/kafka/offsetRepository/KafkaProcessor.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.example.kafka.offsetRepository;
+
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaProcessor implements Processor {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaProcessor.class);
+    
+    @Override
+    public void process(Exchange exc) throws Exception {
+        @SuppressWarnings("unchecked")
+        List<RecordMetadata> recordMetaData1 = (List<RecordMetadata>) exc.getIn().getHeader(KafkaConstants.KAFKA_RECORDMETA);
+        for (RecordMetadata rd: recordMetaData1) {
+            LOG.info("producer partition is:"  + rd.partition());
+        }
+    }
+}
