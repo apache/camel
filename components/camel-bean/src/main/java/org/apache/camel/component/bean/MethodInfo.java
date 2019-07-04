0cd8110dcd5da1a77e81f6bf53de9ba78e109b7d
Federico Valeri <fvaleri@localhost>
2019-07-04 14:34:17 +0200

    Fix NPE at component.bean.MethodInfo.invoke

--- components/camel-bean/src/main/java/org/apache/camel/component/bean/MethodInfo.java
+++ components/camel-bean/src/main/java/org/apache/camel/component/bean/MethodInfo.java
@@ -56,6 +56,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.apache.camel.util.ObjectHelper.asString;
+import static org.apache.camel.util.ObjectHelper.asList;
 
 /**
  * Information about a method to be used for invocation.
@@ -438,9 +439,9 @@ public class MethodInfo {
         try {
             return ObjectHelper.invokeMethodSafe(mth, pojo, arguments);
         } catch (IllegalAccessException e) {
-            throw new RuntimeExchangeException("IllegalAccessException occurred invoking method: " + mth + " using arguments: " + Arrays.asList(arguments), exchange, e);
+            throw new RuntimeExchangeException("IllegalAccessException occurred invoking method: " + mth + " using arguments: " + asList(arguments), exchange, e);
         } catch (IllegalArgumentException e) {
-            throw new RuntimeExchangeException("IllegalArgumentException occurred invoking method: " + mth + " using arguments: " + Arrays.asList(arguments), exchange, e);
+            throw new RuntimeExchangeException("IllegalArgumentException occurred invoking method: " + mth + " using arguments: " + asList(arguments), exchange, e);
         }
     }
 
