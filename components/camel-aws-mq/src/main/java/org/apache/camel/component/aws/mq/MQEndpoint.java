97777e445181ef50f682520f41a395b721c8199c
Andrea Cosentino <ancosen@gmail.com>
2019-07-04 15:54:19 +0200

    CAMEL-13723 - Camel-AWS: Harmonize how to specify region in all the components, MQ

--- components/camel-aws-mq/src/main/java/org/apache/camel/component/aws/mq/MQEndpoint.java
+++ components/camel-aws-mq/src/main/java/org/apache/camel/component/aws/mq/MQEndpoint.java
@@ -21,6 +21,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.mq.AmazonMQ;
 import com.amazonaws.services.mq.AmazonMQClient;
 import com.amazonaws.services.mq.AmazonMQClientBuilder;
@@ -110,7 +111,7 @@ public class MQEndpoint extends ScheduledPollEndpoint {
             }
         }
         if (ObjectHelper.isNotEmpty(configuration.getRegion())) {
-            clientBuilder = clientBuilder.withRegion(configuration.getRegion());
+            clientBuilder = clientBuilder.withRegion(Regions.valueOf(configuration.getRegion()));
         }
         client = clientBuilder.build();
         return client;
