1c157c02355c78b5782defb31a5d99190ac3c757
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:28:40 +0200

    CAMEL-13723 - Better docs, MSK

--- components/camel-aws-msk/src/main/java/org/apache/camel/component/aws/msk/MSKConfiguration.java
+++ components/camel-aws-msk/src/main/java/org/apache/camel/component/aws/msk/MSKConfiguration.java
@@ -117,7 +117,8 @@ public class MSKConfiguration implements Cloneable {
     }
 
     /**
-     * The region in which MSK client needs to work
+     * The region in which MSK client needs to work. When using this parameter, the configuration will expect the capitalized name of the region (for example AP_EAST_1)
+     * You'll need to use the name Regions.EU_WEST_1.name()
      */
     public void setRegion(String region) {
         this.region = region;
