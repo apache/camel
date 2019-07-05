23c0c8d64e2d179e48ed2dcd417ebc39d847fa29
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:13:11 +0200

    CAMEL-13723 - Better docs, EC2

--- components/camel-aws-ec2/src/main/java/org/apache/camel/component/aws/ec2/EC2Configuration.java
+++ components/camel-aws-ec2/src/main/java/org/apache/camel/component/aws/ec2/EC2Configuration.java
@@ -119,7 +119,8 @@ public class EC2Configuration implements Cloneable {
     }
 
     /**
-     * The region in which EC2 client needs to work
+     * The region in which EC2 client needs to work. When using this parameter, the configuration will expect the capitalized name of the region (for example AP_EAST_1)
+     * You'll need to use the name Regions.EU_WEST_1.name()
      */
     public void setRegion(String region) {
         this.region = region;
