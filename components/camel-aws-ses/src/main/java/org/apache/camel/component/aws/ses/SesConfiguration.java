e6e1c5ffb79897961bb1bef515c091829f189197
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:33:41 +0200

    CAMEL-13723 - Better docs, SES

--- components/camel-aws-ses/src/main/java/org/apache/camel/component/aws/ses/SesConfiguration.java
+++ components/camel-aws-ses/src/main/java/org/apache/camel/component/aws/ses/SesConfiguration.java
@@ -179,7 +179,8 @@ public class SesConfiguration implements Cloneable {
     }
 
     /**
-     * The region in which SES client needs to work
+     * The region in which SES client needs to work. When using this parameter, the configuration will expect the capitalized name of the region (for example AP_EAST_1)
+     * You'll need to use the name Regions.EU_WEST_1.name()
      */
     public void setRegion(String region) {
         this.region = region;
--- core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/SesEndpointBuilderFactory.java
+++ core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/SesEndpointBuilderFactory.java
@@ -97,7 +97,10 @@ public interface SesEndpointBuilderFactory {
             return this;
         }
         /**
-         * The region in which SES client needs to work.
+         * The region in which SES client needs to work. When using this
+         * parameter, the configuration will expect the capitalized name of the
+         * region (for example AP_EAST_1) You'll need to use the name
+         * Regions.EU_WEST_1.name().
          * 
          * The option is a: <code>java.lang.String</code> type.
          * 
