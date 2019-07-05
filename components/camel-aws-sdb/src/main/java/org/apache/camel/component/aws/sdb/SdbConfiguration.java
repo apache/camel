e805d78da6885cb2be5cb98ea27387301f06c570
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:32:45 +0200

    CAMEL-13723 - Better docs, SDB

--- components/camel-aws-sdb/src/main/java/org/apache/camel/component/aws/sdb/SdbConfiguration.java
+++ components/camel-aws-sdb/src/main/java/org/apache/camel/component/aws/sdb/SdbConfiguration.java
@@ -151,7 +151,8 @@ public class SdbConfiguration {
     }
 
     /**
-     * The region in which SDB client needs to work
+     * The region in which SDB client needs to work. When using this parameter, the configuration will expect the capitalized name of the region (for example AP_EAST_1)
+     * You'll need to use the name Regions.EU_WEST_1.name()
      */
     public void setRegion(String region) {
         this.region = region;
