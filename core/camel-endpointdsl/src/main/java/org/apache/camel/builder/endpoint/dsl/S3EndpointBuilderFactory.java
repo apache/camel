39e5b404bb3268c63641fa8b419ccc01973d0025
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:31:39 +0200

    CAMEL-13723 - Better docs, S3

--- core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/S3EndpointBuilderFactory.java
+++ core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/S3EndpointBuilderFactory.java
@@ -166,7 +166,10 @@ public interface S3EndpointBuilderFactory {
             return this;
         }
         /**
-         * The region in which S3 client needs to work.
+         * The region in which S3 client needs to work. When using this
+         * parameter, the configuration will expect the capitalized name of the
+         * region (for example AP_EAST_1) You'll need to use the name
+         * Regions.EU_WEST_1.name().
          * 
          * The option is a: <code>java.lang.String</code> type.
          * 
@@ -1276,7 +1279,10 @@ public interface S3EndpointBuilderFactory {
             return this;
         }
         /**
-         * The region in which S3 client needs to work.
+         * The region in which S3 client needs to work. When using this
+         * parameter, the configuration will expect the capitalized name of the
+         * region (for example AP_EAST_1) You'll need to use the name
+         * Regions.EU_WEST_1.name().
          * 
          * The option is a: <code>java.lang.String</code> type.
          * 
@@ -1904,7 +1910,10 @@ public interface S3EndpointBuilderFactory {
             return this;
         }
         /**
-         * The region in which S3 client needs to work.
+         * The region in which S3 client needs to work. When using this
+         * parameter, the configuration will expect the capitalized name of the
+         * region (for example AP_EAST_1) You'll need to use the name
+         * Regions.EU_WEST_1.name().
          * 
          * The option is a: <code>java.lang.String</code> type.
          * 
