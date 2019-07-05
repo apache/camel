c1460c57deb89c769c3edfa2b8331a8a38aa99da
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:13:11 +0200

    CAMEL-13723 - Better docs, EC2

--- platforms/spring-boot/components-starter/camel-aws-ec2-starter/src/main/java/org/apache/camel/component/aws/ec2/springboot/EC2ComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-aws-ec2-starter/src/main/java/org/apache/camel/component/aws/ec2/springboot/EC2ComponentConfiguration.java
@@ -146,7 +146,10 @@ public class EC2ComponentConfiguration
          */
         private Integer proxyPort;
         /**
-         * The region in which EC2 client needs to work
+         * The region in which EC2 client needs to work. When using this
+         * parameter, the configuration will expect the capitalized name of the
+         * region (for example AP_EAST_1) You'll need to use the name
+         * Regions.EU_WEST_1.name()
          */
         private String region;
 
