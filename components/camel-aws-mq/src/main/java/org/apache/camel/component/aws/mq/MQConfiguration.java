74d5879c2e6add6457943cfd8fdb87b9d66b552d
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:27:25 +0200

    CAMEL-13723 - Better docs, MQ

--- components/camel-aws-mq/src/main/java/org/apache/camel/component/aws/mq/MQConfiguration.java
+++ components/camel-aws-mq/src/main/java/org/apache/camel/component/aws/mq/MQConfiguration.java
@@ -117,7 +117,8 @@ public class MQConfiguration implements Cloneable {
     }
 
     /**
-     * The region in which MQ client needs to work
+     * The region in which MQ client needs to work. When using this parameter, the configuration will expect the capitalized name of the region (for example AP_EAST_1)
+     * You'll need to use the name Regions.EU_WEST_1.name()
      */
     public void setRegion(String region) {
         this.region = region;
--- platforms/spring-boot/components-starter/camel-aws-mq-starter/src/main/java/org/apache/camel/component/aws/mq/springboot/MQComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-aws-mq-starter/src/main/java/org/apache/camel/component/aws/mq/springboot/MQComponentConfiguration.java
@@ -144,7 +144,10 @@ public class MQComponentConfiguration
          */
         private Integer proxyPort;
         /**
-         * The region in which MQ client needs to work
+         * The region in which MQ client needs to work. When using this
+         * parameter, the configuration will expect the capitalized name of the
+         * region (for example AP_EAST_1) You'll need to use the name
+         * Regions.EU_WEST_1.name()
          */
         private String region;
 
