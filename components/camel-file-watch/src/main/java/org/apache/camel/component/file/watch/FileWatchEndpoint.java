d423b86b51157156e7dd7823030acd1b1c051201
Jan Bednář <mail@janbednar.eu>
2019-07-08 20:21:41 +0200

    Revert "CAMEL-13628: Dropped directory-watcher dependency and related stuff"
    This reverts commit 15b5f846

--- components/camel-file-watch/src/main/java/org/apache/camel/component/file/watch/FileWatchEndpoint.java
+++ components/camel-file-watch/src/main/java/org/apache/camel/component/file/watch/FileWatchEndpoint.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import io.methvin.watcher.hashing.FileHasher;
 import org.apache.camel.Consumer;
 import org.apache.camel.MultipleConsumersSupport;
 import org.apache.camel.Processor;
@@ -49,10 +50,18 @@ public class FileWatchEndpoint extends DefaultEndpoint implements MultipleConsum
     @UriParam(label = "consumer", description = "Auto create directory if does not exists.", defaultValue = "true")
     private boolean autoCreate = true;
 
+    @UriParam(label = "consumer", description = "Watch recursive in current and child directories (including newly created directories).", defaultValue = "true")
+    private boolean recursive = true;
+
     @UriParam(label = "consumer",
     description = "The number of concurrent consumers. Increase this value, if your route is slow to prevent buffering in queue.",
     defaultValue = "1")
     private int concurrentConsumers = 1;
+
+    @UriParam(label = "consumer",
+    description = "The number of threads polling WatchService. Increase this value, if you see OVERFLOW messages in log.",
+    defaultValue = "1")
+    private int pollThreads = 1;
 
     @UriParam(label = "consumer",
     description = "ANT style pattern to match files. The file is matched against path relative to endpoint path. "
@@ -63,6 +72,21 @@ public class FileWatchEndpoint extends DefaultEndpoint implements MultipleConsum
     @UriParam(label = "consumer", description = "Maximum size of queue between WatchService and consumer. Unbounded by default.",
     defaultValue = "" + Integer.MAX_VALUE)
     private int queueSize = Integer.MAX_VALUE;
+
+    @UriParam(label = "consumer",
+    description = "Reference to io.methvin.watcher.hashing.FileHasher. "
+    + "This prevents emitting duplicate events on some platforms. "
+    + "For working with large files and if you dont need detect multiple modifications per second per file, "
+    + "use #lastModifiedTimeFileHasher. You can also provide custom implementation in registry.",
+    defaultValue = "#murmur3FFileHasher")
+    private FileHasher fileHasher = FileHasher.DEFAULT_FILE_HASHER;
+
+    @UriParam(label = "consumer",
+    description = "Enables or disables file hashing to detect duplicate events. "
+    + "If you disable this, you can get some events multiple times on some platforms and JDKs. "
+    + "Check java.nio.file.WatchService limitations for your target platform.",
+    defaultValue = "true")
+    private boolean useFileHashing = true;
 
     public FileWatchEndpoint() {
     }
@@ -119,12 +143,28 @@ public class FileWatchEndpoint extends DefaultEndpoint implements MultipleConsum
         this.autoCreate = autoCreate;
     }
 
+    public boolean isRecursive() {
+        return recursive;
+    }
+
+    public void setRecursive(boolean recursive) {
+        this.recursive = recursive;
+    }
+
     public int getConcurrentConsumers() {
         return concurrentConsumers;
     }
 
     public void setConcurrentConsumers(int concurrentConsumers) {
         this.concurrentConsumers = concurrentConsumers;
+    }
+
+    public int getPollThreads() {
+        return pollThreads;
+    }
+
+    public void setPollThreads(int pollThreads) {
+        this.pollThreads = pollThreads;
     }
 
     public int getQueueSize() {
@@ -141,6 +181,22 @@ public class FileWatchEndpoint extends DefaultEndpoint implements MultipleConsum
 
     public void setAntInclude(String antInclude) {
         this.antInclude = antInclude;
+    }
+
+    public FileHasher getFileHasher() {
+        return fileHasher;
+    }
+
+    public void setFileHasher(FileHasher fileHasher) {
+        this.fileHasher = fileHasher;
+    }
+
+    public boolean isUseFileHashing() {
+        return useFileHashing;
+    }
+
+    public void setUseFileHashing(boolean useFileHashing) {
+        this.useFileHashing = useFileHashing;
     }
 
     @Override
