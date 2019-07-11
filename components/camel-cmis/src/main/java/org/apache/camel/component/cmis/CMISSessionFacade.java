5b9d66e826b51f8c7e62df20711097bca3db51e1
Vladimir Cherepnalkovski <cherepnalkovski@hotmail.com>
2019-07-11 15:00:08 +0200

    Get object by Id, not by path

--- components/camel-cmis/src/main/java/org/apache/camel/component/cmis/CMISSessionFacade.java
+++ components/camel-cmis/src/main/java/org/apache/camel/component/cmis/CMISSessionFacade.java
@@ -18,11 +18,7 @@ package org.apache.camel.component.cmis;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriParams;
@@ -47,24 +43,31 @@ import org.slf4j.LoggerFactory;
 @UriParams
 public class CMISSessionFacade {
     private static final Logger LOG = LoggerFactory.getLogger(CMISSessionFacade.class);
-
     private transient Session session;
-
     private final String url;
-
-    @UriParam(defaultValue = "100")
+    @UriParam(
+            defaultValue = "100"
+    )
     private int pageSize = 100;
     @UriParam
     private int readCount;
     @UriParam
     private boolean readContent;
-    @UriParam(label = "security", secret = true)
+    @UriParam(
+            label = "security",
+            secret = true
+    )
     private String username;
-    @UriParam(label = "security", secret = true)
+    @UriParam(
+            label = "security",
+            secret = true
+    )
     private String password;
     @UriParam
     private String repositoryId;
-    @UriParam(label = "consumer")
+    @UriParam(
+            label = "consumer"
+    )
     private String query;
 
     public CMISSessionFacade(String url) {
@@ -72,30 +75,27 @@ public class CMISSessionFacade {
     }
 
     void initSession() {
-        Map<String, String> parameter = new HashMap<>();
-        parameter.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
-        parameter.put(SessionParameter.ATOMPUB_URL, this.url);
-        parameter.put(SessionParameter.USER, this.username);
-        parameter.put(SessionParameter.PASSWORD, this.password);
+        Map<String, String> parameter = new HashMap();
+        parameter.put("org.apache.chemistry.opencmis.binding.spi.type", BindingType.ATOMPUB.value());
+        parameter.put("org.apache.chemistry.opencmis.binding.atompub.url", this.url);
+        parameter.put("org.apache.chemistry.opencmis.user", this.username);
+        parameter.put("org.apache.chemistry.opencmis.password", this.password);
         if (this.repositoryId != null) {
-            parameter.put(SessionParameter.REPOSITORY_ID, this.repositoryId);
+            parameter.put("org.apache.chemistry.opencmis.session.repository.id", this.repositoryId);
             this.session = SessionFactoryLocator.getSessionFactory().createSession(parameter);
         } else {
-            this.session = SessionFactoryLocator.getSessionFactory().getRepositories(parameter).get(0).createSession();
+            this.session = ((Repository)SessionFactoryLocator.getSessionFactory().getRepositories(parameter).get(0)).createSession();
         }
+
     }
 
     public int poll(CMISConsumer cmisConsumer) throws Exception {
-        if (query != null) {
-            return pollWithQuery(cmisConsumer);
-        }
-        return pollTree(cmisConsumer);
+        return this.query != null ? this.pollWithQuery(cmisConsumer) : this.pollTree(cmisConsumer);
     }
 
     private int pollTree(CMISConsumer cmisConsumer) throws Exception {
-        Folder rootFolder = session.getRootFolder();
-        RecursiveTreeWalker treeWalker = new RecursiveTreeWalker(cmisConsumer, readContent, readCount,
-                pageSize);
+        Folder rootFolder = this.session.getRootFolder();
+        RecursiveTreeWalker treeWalker = new RecursiveTreeWalker(cmisConsumer, this.readContent, this.readCount, this.pageSize);
         return treeWalker.processFolderRecursively(rootFolder);
     }
 
@@ -103,181 +103,182 @@ public class CMISSessionFacade {
         int count = 0;
         int pageNumber = 0;
         boolean finished = false;
-        ItemIterable<QueryResult> itemIterable = executeQuery(query);
-        while (!finished) {
-            ItemIterable<QueryResult> currentPage = itemIterable.skipTo(count).getPage();
+        ItemIterable itemIterable = this.executeQuery(this.query);
+
+        while(!finished) {
+            ItemIterable<QueryResult> currentPage = itemIterable.skipTo((long)count).getPage();
             LOG.debug("Processing page {}", pageNumber);
-            for (QueryResult item : currentPage) {
+            Iterator var7 = currentPage.iterator();
+
+            while(var7.hasNext()) {
+                QueryResult item = (QueryResult)var7.next();
                 Map<String, Object> properties = CMISHelper.propertyDataToMap(item.getProperties());
-                Object objectTypeId = item.getPropertyValueById(PropertyIds.OBJECT_TYPE_ID);
+                Object objectTypeId = item.getPropertyValueById("cmis:objectTypeId");
                 InputStream inputStream = null;
-                if (readContent && CamelCMISConstants.CMIS_DOCUMENT.equals(objectTypeId)) {
-                    inputStream = getContentStreamFor(item);
+                if (this.readContent && "cmis:document".equals(objectTypeId)) {
+                    inputStream = this.getContentStreamFor(item);
                 }
 
                 cmisConsumer.sendExchangeWithPropsAndBody(properties, inputStream);
-                count++;
-                if (count == readCount) {
+                ++count;
+                if (count == this.readCount) {
                     finished = true;
                     break;
                 }
             }
-            pageNumber++;
+
+            ++pageNumber;
             if (!currentPage.getHasMoreItems()) {
                 finished = true;
             }
         }
+
         return count;
     }
 
-    //some duplication
-    public List<Map<String, Object>> retrieveResult(Boolean retrieveContent, Integer readSize,
-                                                    ItemIterable<QueryResult> itemIterable) {
-        List<Map<String, Object>> result = new ArrayList<>();
-        boolean queryForContent = retrieveContent != null ? retrieveContent : readContent;
-        int documentsToRead = readSize != null ? readSize : readCount;
+    public List<Map<String, Object>> retrieveResult(Boolean retrieveContent, Integer readSize, ItemIterable<QueryResult> itemIterable) {
+        List<Map<String, Object>> result = new ArrayList();
+        boolean queryForContent = retrieveContent != null ? retrieveContent : this.readContent;
+        int documentsToRead = readSize != null ? readSize : this.readCount;
         int count = 0;
         int pageNumber = 0;
         boolean finished = false;
-        while (!finished) {
-            ItemIterable<QueryResult> currentPage = itemIterable.skipTo(count).getPage();
+
+        while(!finished) {
+            ItemIterable<QueryResult> currentPage = itemIterable.skipTo((long)count).getPage();
             LOG.debug("Processing page {}", pageNumber);
-            for (QueryResult item : currentPage) {
+            Iterator var11 = currentPage.iterator();
+
+            while(var11.hasNext()) {
+                QueryResult item = (QueryResult)var11.next();
                 Map<String, Object> properties = CMISHelper.propertyDataToMap(item.getProperties());
                 if (queryForContent) {
-                    InputStream inputStream = getContentStreamFor(item);
-                    properties.put(CamelCMISConstants.CAMEL_CMIS_CONTENT_STREAM, inputStream);
+                    InputStream inputStream = this.getContentStreamFor(item);
+                    properties.put("CamelCMISContent", inputStream);
                 }
 
                 result.add(properties);
-                count++;
+                ++count;
                 if (count == documentsToRead) {
                     finished = true;
                     break;
                 }
             }
-            pageNumber++;
+
+            ++pageNumber;
             if (!currentPage.getHasMoreItems()) {
                 finished = true;
             }
         }
+
         return result;
     }
 
     public ItemIterable<QueryResult> executeQuery(String query) {
-        OperationContext operationContext = session.createOperationContext();
-        operationContext.setMaxItemsPerPage(pageSize);
-        return session.query(query, false, operationContext);
+        OperationContext operationContext = this.session.createOperationContext();
+        operationContext.setMaxItemsPerPage(this.pageSize);
+        return this.session.query(query, false, operationContext);
     }
 
     public Document getDocument(QueryResult queryResult) {
-        if (CamelCMISConstants.CMIS_DOCUMENT.equals(queryResult.getPropertyValueById(PropertyIds.OBJECT_TYPE_ID))
-                || CamelCMISConstants.CMIS_DOCUMENT.equals(queryResult.getPropertyValueById(PropertyIds.BASE_TYPE_ID))) {
-            String objectId = (String) queryResult.getPropertyById(PropertyIds.OBJECT_ID).getFirstValue();
-            return (org.apache.chemistry.opencmis.client.api.Document) session.getObject(objectId);
+        if (!"cmis:document".equals(queryResult.getPropertyValueById("cmis:objectTypeId")) && !"cmis:document".equals(queryResult.getPropertyValueById("cmis:baseTypeId"))) {
+            return null;
+        } else {
+            String objectId = (String)queryResult.getPropertyById("cmis:objectId").getFirstValue();
+            return (Document)this.session.getObject(objectId);
         }
-        return null;
     }
 
     public InputStream getContentStreamFor(QueryResult item) {
-        Document document = getDocument(item);
+        Document document = this.getDocument(item);
         if (document != null) {
             ContentStream contentStream = document.getContentStream();
             if (contentStream != null) {
                 return contentStream.getStream();
             }
         }
+
         return null;
     }
 
     public CmisObject getObjectByPath(String path) {
-        return session.getObjectByPath(path);
+        return this.session.getObjectByPath(path);
+    }
+
+    public CmisObject getObjectById(String id)
+    {
+        return this.session.getObject(id);
     }
 
     public boolean isObjectTypeVersionable(String objectType) {
-        if (CamelCMISConstants.CMIS_DOCUMENT.equals(getCMISTypeFor(objectType))) {
-            ObjectType typeDefinition = session.getTypeDefinition(objectType);
-            return ((DocumentType) typeDefinition).isVersionable();
+        if ("cmis:document".equals(this.getCMISTypeFor(objectType))) {
+            ObjectType typeDefinition = this.session.getTypeDefinition(objectType);
+            return ((DocumentType)typeDefinition).isVersionable();
+        } else {
+            return false;
         }
-        return false;
     }
 
     public boolean supportsSecondaries() {
-        if (session.getRepositoryInfo().getCmisVersion() == CmisVersion.CMIS_1_0) {
+        if (this.session.getRepositoryInfo().getCmisVersion() == CmisVersion.CMIS_1_0) {
             return false;
+        } else {
+            Iterator var1 = this.session.getTypeChildren((String)null, false).iterator();
+
+            ObjectType type;
+            do {
+                if (!var1.hasNext()) {
+                    return false;
+                }
+
+                type = (ObjectType)var1.next();
+            } while(!BaseTypeId.CMIS_SECONDARY.value().equals(type.getId()));
+
+            return true;
         }
-        for (ObjectType type : session.getTypeChildren(null, false)) {
-            if (BaseTypeId.CMIS_SECONDARY.value().equals(type.getId())) {
-                return true;
-            }
-        }
-        return false;
     }
 
     public ContentStream createContentStream(String fileName, byte[] buf, String mimeType) throws Exception {
-        return buf != null ? session.getObjectFactory()
-                .createContentStream(fileName, buf.length, mimeType, new ByteArrayInputStream(buf)) : null;
+        return buf != null ? this.session.getObjectFactory().createContentStream(fileName, (long)buf.length, mimeType, new ByteArrayInputStream(buf)) : null;
     }
 
     public String getCMISTypeFor(String customOrCMISType) {
-        ObjectType objectBaseType = session.getTypeDefinition(customOrCMISType).getBaseType();
+        ObjectType objectBaseType = this.session.getTypeDefinition(customOrCMISType).getBaseType();
         return objectBaseType == null ? customOrCMISType : objectBaseType.getId();
     }
 
     public Set<String> getPropertiesFor(String objectType) {
-        return session.getTypeDefinition(objectType).getPropertyDefinitions().keySet();
+        return this.session.getTypeDefinition(objectType).getPropertyDefinitions().keySet();
     }
 
     public OperationContext createOperationContext() {
-        return session.createOperationContext();
+        return this.session.createOperationContext();
     }
 
-    /**
-     * Username for the cmis repository
-     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-    /**
-     * Password for the cmis repository
-     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    /**
-     * The Id of the repository to use. If not specified the first available repository is used
-     */
     public void setRepositoryId(String repositoryId) {
         this.repositoryId = repositoryId;
     }
 
-    /**
-     * If set to true, the content of document node will be retrieved in addition to the properties
-     */
     public void setReadContent(boolean readContent) {
         this.readContent = readContent;
     }
 
-    /**
-     * Max number of nodes to read
-     */
     public void setReadCount(int readCount) {
         this.readCount = readCount;
     }
 
-    /**
-     * The cmis query to execute against the repository.
-     * If not specified, the consumer will retrieve every node from the content repository by iterating the content tree recursively
-     */
     public void setQuery(String query) {
         this.query = query;
     }
 
-    /**
-     * Number of nodes to retrieve per page
-     */
     public void setPageSize(int pageSize) {
         this.pageSize = pageSize;
     }
