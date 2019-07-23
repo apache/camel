7e7e4bc08508dd2b952b21b61f1524d99ba75de2
Andrea Cosentino <ancosen@gmail.com>
2019-07-23 18:05:53 +0200

    Removed unused imports

--- components/camel-exec/src/test/java/org/apache/camel/component/exec/ExecJavaProcessTest.java
+++ components/camel-exec/src/test/java/org/apache/camel/component/exec/ExecJavaProcessTest.java
@@ -31,7 +31,6 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.exec.impl.ProvokeExceptionExecCommandExecutor;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.converter.IOConverter;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.reifier.RouteReifier;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.commons.io.IOUtils;
