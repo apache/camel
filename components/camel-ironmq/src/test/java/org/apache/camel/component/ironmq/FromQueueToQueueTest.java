1e1cd2a982052a252b13d963c19d6e8fdc562114
Andrea Cosentino <ancosen@gmail.com>
2019-07-26 15:07:55 +0200

    Removed unused imports

--- components/camel-ironmq/src/test/java/org/apache/camel/component/ironmq/FromQueueToQueueTest.java
+++ components/camel-ironmq/src/test/java/org/apache/camel/component/ironmq/FromQueueToQueueTest.java
@@ -27,7 +27,6 @@ import org.apache.camel.ExchangePattern;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
