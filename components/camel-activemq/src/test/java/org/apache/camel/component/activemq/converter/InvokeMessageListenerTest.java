09c11af4d0e7ce3c1f67d5eecee1630d8cf3a570
Claus Ibsen <claus.ibsen@gmail.com>
2019-07-26 06:09:13 +0200

    CAMEL-13750: Omit JMSCorrelationID when sending with useMessageIDAsCorrelationID

--- components/camel-activemq/src/test/java/org/apache/camel/component/activemq/converter/InvokeMessageListenerTest.java
+++ components/camel-activemq/src/test/java/org/apache/camel/component/activemq/converter/InvokeMessageListenerTest.java
@@ -24,7 +24,6 @@ import javax.jms.TextMessage;
 import org.apache.activemq.spring.ConsumerBean;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
@@ -34,7 +33,6 @@ import static org.apache.camel.component.activemq.ActiveMQComponent.activeMQComp
  * 
  */
 public class InvokeMessageListenerTest extends CamelTestSupport {
-    protected MockEndpoint resultEndpoint;
     protected String startEndpointUri = "activemq:queue:test.a";
     protected ConsumerBean listener = new ConsumerBean();
 
@@ -44,7 +42,7 @@ public class InvokeMessageListenerTest extends CamelTestSupport {
 
         template.sendBodyAndHeader(startEndpointUri, expectedBody, "cheese", 123);
 
-        listener.assertMessagesArrived(1);
+        listener.assertMessagesArrived(1, 5000);
 
         List<Message> list = listener.flushMessages();
         assertTrue("Should have received some messages!", !list.isEmpty());
