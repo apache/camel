2931c66abe2405556a4ed54c560d035cf4192855
Claus Ibsen <claus.ibsen@gmail.com>
2019-07-29 07:50:36 +0200

    Polished

--- core/camel-core/src/test/java/org/apache/camel/issues/TwoTimerWithJMXIssue.java
+++ core/camel-core/src/test/java/org/apache/camel/issues/TwoTimerWithJMXIssue.java
@@ -24,6 +24,7 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.spi.InterceptStrategy;
+import org.junit.Test;
 
 /**
  * Trying to reproduce CAMEL-927.
@@ -38,6 +39,7 @@ public class TwoTimerWithJMXIssue extends ContextTestSupport {
         return true;
     }
 
+    @Test
     public void testFromWithNoOutputs() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMinimumMessageCount(2);
@@ -53,7 +55,8 @@ public class TwoTimerWithJMXIssue extends ContextTestSupport {
             public void configure() throws Exception {
                 context.adapt(ExtendedCamelContext.class).addInterceptStrategy(new MyTracer());
 
-                from("timer://kickoff_1?period=250").from("timer://kickoff_2?period=250&delay=10").to("mock:result");
+                from("timer://kickoff_1?period=250").to("mock:result");
+                from("timer://kickoff_2?period=250&delay=10").to("mock:result");
             }
         };
     }
