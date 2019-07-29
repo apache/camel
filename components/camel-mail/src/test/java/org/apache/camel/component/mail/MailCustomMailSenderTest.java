9fe1f9ec075e205b2d39e5d8e254ebf1194b34c8
Andrea Cosentino <ancosen@gmail.com>
2019-07-29 14:06:28 +0200

    Removed unused imports

--- components/camel-mail/src/test/java/org/apache/camel/component/mail/MailCustomMailSenderTest.java
+++ components/camel-mail/src/test/java/org/apache/camel/component/mail/MailCustomMailSenderTest.java
@@ -23,7 +23,6 @@ import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.camel.BindToRegistry;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
--- components/camel-mail/src/test/java/org/apache/camel/component/mail/MailSortTermTest.java
+++ components/camel-mail/src/test/java/org/apache/camel/component/mail/MailSortTermTest.java
@@ -29,7 +29,6 @@ import com.sun.mail.imap.SortTerm;
 import org.apache.camel.BindToRegistry;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Before;
 import org.junit.Test;
