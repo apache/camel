aa147e0343639e9b2f4454ac8ecb9fc0ebfa5d27
Andrea Cosentino <ancosen@gmail.com>
2019-07-29 11:36:26 +0200

    Removed unused imports

--- components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsRouteWithCustomListenerContainerTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsRouteWithCustomListenerContainerTest.java
@@ -23,7 +23,6 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 import org.springframework.jms.listener.AbstractMessageListenerContainer;
--- components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsRoutingSlipInOutTest.java
+++ components/camel-jms/src/test/java/org/apache/camel/component/jms/JmsRoutingSlipInOutTest.java
@@ -25,7 +25,6 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
