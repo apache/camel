ffc76c3a8836c4bcd31aab78bc2908e37c3a9038
Andrea Cosentino <ancosen@gmail.com>
2019-07-31 08:27:03 +0200

    Camel-Support: Fixed CS

--- core/camel-support/src/main/java/org/apache/camel/support/HeaderSelectorProducer.java
+++ core/camel-support/src/main/java/org/apache/camel/support/HeaderSelectorProducer.java
@@ -25,11 +25,11 @@ import java.util.function.Supplier;
 
 import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
-import org.apache.camel.spi.InvokeOnHeader;
-import org.apache.camel.spi.InvokeOnHeaders;
 import org.apache.camel.Message;
 import org.apache.camel.NoSuchHeaderException;
 import org.apache.camel.Processor;
+import org.apache.camel.spi.InvokeOnHeader;
+import org.apache.camel.spi.InvokeOnHeaders;
 import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
