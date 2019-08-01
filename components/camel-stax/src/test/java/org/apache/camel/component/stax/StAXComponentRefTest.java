365bbd439bcc93025c39e44113da6186f5702e3f
Andrea Cosentino <ancosen@gmail.com>
2019-08-01 16:00:14 +0200

    Removed unused imports

--- components/camel-stax/src/test/java/org/apache/camel/component/stax/StAXComponentRefTest.java
+++ components/camel-stax/src/test/java/org/apache/camel/component/stax/StAXComponentRefTest.java
@@ -23,7 +23,6 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.component.stax.model.RecordsUtil;
-import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.BeforeClass;
 import org.junit.Test;
