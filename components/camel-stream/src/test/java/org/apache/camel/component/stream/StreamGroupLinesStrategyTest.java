bc719943b85e3ef98d28df53cc2218fc5f41e4c8
Andrea Cosentino <ancosen@gmail.com>
2019-08-01 16:30:33 +0200

    Removed unused imports

--- components/camel-stream/src/test/java/org/apache/camel/component/stream/StreamGroupLinesStrategyTest.java
+++ components/camel-stream/src/test/java/org/apache/camel/component/stream/StreamGroupLinesStrategyTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 import org.apache.camel.BindToRegistry;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.impl.JndiRegistry;
 import org.junit.Test;
 
 public class StreamGroupLinesStrategyTest extends StreamGroupLinesTest {
