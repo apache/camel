96af619d202d68419edecd0c899f1dead00c5a6b
Andrea Cosentino <ancosen@gmail.com>
2019-08-01 10:43:21 +0200

    CAMEL-13801 - camel3 - Use @BindToRegistry wherever possible, Camel-Spring-WS

--- components/camel-spring-ws/src/test/java/org/apache/camel/component/spring/ws/ConsumerMarshallingRouteTest.java
+++ components/camel-spring-ws/src/test/java/org/apache/camel/component/spring/ws/ConsumerMarshallingRouteTest.java
@@ -22,6 +22,8 @@ import org.apache.camel.component.spring.ws.jaxb.QuoteRequest;
 import org.apache.camel.component.spring.ws.jaxb.QuoteResponse;
 import org.apache.camel.impl.JndiRegistry;
 import org.apache.camel.model.dataformat.JaxbDataFormat;
+import org.apache.camel.spi.Registry;
+import org.apache.camel.support.SimpleRegistry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Before;
 import org.junit.Test;
@@ -49,8 +51,8 @@ public class ConsumerMarshallingRouteTest extends CamelTestSupport {
     }
 
     @Override
-    protected JndiRegistry createRegistry() throws Exception {
-        JndiRegistry registry = super.createRegistry();
+    protected Registry createCamelRegistry() throws Exception {
+        Registry registry = new SimpleRegistry();
         registry.bind("endpointMapping", this.endpointMapping);
         registry.bind("webServiceTemplate", this.webServiceTemplate);
         return registry;
