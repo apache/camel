8dd651ab2fbffeabba9f1e0a54192235492b32fb
Claus Ibsen <claus.ibsen@gmail.com>
2019-08-02 07:45:43 +0200

    CAMEL-12003: Add failFast option to mock endpoint

--- components/camel-mock/src/main/java/org/apache/camel/component/mock/AssertionClauseTask.java
+++ components/camel-mock/src/main/java/org/apache/camel/component/mock/AssertionClauseTask.java
@@ -1,13 +1,13 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,16 +16,9 @@
  */
 package org.apache.camel.component.mock;
 
-/**
- * Assertion task that supports fail fast mode by running the assertion asap on the n'th received message.
- */
-public interface AssertionTask extends Runnable {
+public abstract class AssertionClauseTask extends AssertionClause implements AssertionTask {
 
-    /**
-     * Asserts on the n'th received message
-     *
-     * @param index the n'th received message
-     */
-    public void assertOnIndex(int index);
-
+    public AssertionClauseTask(MockEndpoint mock) {
+        super(mock);
+    }
 }
