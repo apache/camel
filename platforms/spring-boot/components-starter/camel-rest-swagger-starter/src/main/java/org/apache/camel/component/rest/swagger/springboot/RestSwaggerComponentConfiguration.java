13b85a1d302f93c40d68a4bbeea890b7f31f565e
Claus Ibsen <claus.ibsen@gmail.com>
2019-08-06 16:44:30 +0200

    Polished and fixed typo

--- platforms/spring-boot/components-starter/camel-rest-swagger-starter/src/main/java/org/apache/camel/component/rest/swagger/springboot/RestSwaggerComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-rest-swagger-starter/src/main/java/org/apache/camel/component/rest/swagger/springboot/RestSwaggerComponentConfiguration.java
@@ -61,7 +61,7 @@ public class RestSwaggerComponentConfiguration
     /**
      * Scheme hostname and port to direct the HTTP requests to in the form of
      * https://hostname:port. Can be configured at the endpoint, component or in
-     * the correspoding REST configuration in the Camel Context. If you give
+     * the corresponding REST configuration in the Camel Context. If you give
      * this component a name (e.g. petstore) that REST configuration is
      * consulted first, rest-swagger next, and global configuration last. If set
      * overrides any value found in the Swagger specification,
@@ -77,11 +77,11 @@ public class RestSwaggerComponentConfiguration
     private String produces;
     /**
      * Path to the Swagger specification file. The scheme, host base path are
-     * taken from this specification, but these can be overriden with properties
-     * on the component or endpoint level. If not given the component tries to
-     * load swagger.json resource. Note that the host defined on the component
-     * and endpoint of this Component should contain the scheme, hostname and
-     * optionally the port in the URI syntax (i.e.
+     * taken from this specification, but these can be overridden with
+     * properties on the component or endpoint level. If not given the component
+     * tries to load swagger.json resource. Note that the host defined on the
+     * component and endpoint of this Component should contain the scheme,
+     * hostname and optionally the port in the URI syntax (i.e.
      * https://api.example.com:8080). Can be overridden in endpoint
      * configuration.
      */
