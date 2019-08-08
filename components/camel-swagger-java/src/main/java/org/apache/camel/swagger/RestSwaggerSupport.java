59055931556acf79d97920f1ad57b58fedbe1056
Andrea Cosentino <ancosen@gmail.com>
2019-08-08 12:27:36 +0200

    CAMEL-13792 - Rename components to default names, Camel-netty4-http to camel-netty-http - Fixed refs in camel-swagger-java

--- components/camel-swagger-java/src/main/java/org/apache/camel/swagger/RestSwaggerSupport.java
+++ components/camel-swagger-java/src/main/java/org/apache/camel/swagger/RestSwaggerSupport.java
@@ -66,7 +66,7 @@ import static org.apache.camel.swagger.SwaggerHelper.clearVendorExtensions;
 /**
  * A support class for that allows SPI to plugin
  * and offer Swagger API service listings as part of the Camel component. This allows rest-dsl components
- * such as servlet/jetty/netty4-http to offer Swagger API listings with minimal effort.
+ * such as servlet/jetty/netty-http to offer Swagger API listings with minimal effort.
  */
 public class RestSwaggerSupport {
 
