c3014f334954b732b45b879770edd1e04791742f
Andrea Cosentino <ancosen@gmail.com>
2019-08-08 12:24:15 +0200

    CAMEL-13792 - Rename components to default names, Camel-netty4-http to camel-netty-http - Fixed refs and camel-route-parser tests

--- catalog/camel-route-parser/src/test/java/org/apache/camel/parser/java/RoasterEndpointInjectTest.java
+++ catalog/camel-route-parser/src/test/java/org/apache/camel/parser/java/RoasterEndpointInjectTest.java
@@ -57,7 +57,7 @@ public class RoasterEndpointInjectTest {
         Assert.assertEquals(1144, details.get(1).getAbsolutePosition());
         Assert.assertEquals(10, details.get(1).getLinePosition());
 
-        Assert.assertEquals("netty4-http:http:someserver:80/hello", details.get(2).getEndpointUri());
+        Assert.assertEquals("netty-http:http:someserver:80/hello", details.get(2).getEndpointUri());
         Assert.assertEquals("36", details.get(2).getLineNumber());
         Assert.assertEquals("36", details.get(2).getLineNumberEnd());
         Assert.assertEquals(1202, details.get(2).getAbsolutePosition());
@@ -67,7 +67,7 @@ public class RoasterEndpointInjectTest {
         Assert.assertEquals("mock:foo?retainFirst=1", details.get(6).getEndpointUri());
         Assert.assertEquals("45", details.get(6).getLineNumber());
         Assert.assertEquals("46", details.get(6).getLineNumberEnd());
-        Assert.assertEquals(1456, details.get(6).getAbsolutePosition());
+        Assert.assertEquals(1455, details.get(6).getAbsolutePosition());
         Assert.assertEquals(17, details.get(6).getLinePosition());
 
         List<ParserResult> list = CamelJavaParserHelper.parseCamelConsumerUris(method, true, true);
