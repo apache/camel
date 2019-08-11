d2753fd244c9662fa08fe68853ab095253cf5c4d
lburgazzoli <lburgazzoli@gmail.com>
2019-08-11 23:24:20 +0200

    CAMEL-13854: camel-microprofile-config: service file point to the wrong class

--- components/camel-properties/src/main/java/org/apache/camel/component/properties/PropertiesComponent.java
+++ components/camel-properties/src/main/java/org/apache/camel/component/properties/PropertiesComponent.java
@@ -461,7 +461,7 @@ public class PropertiesComponent extends DefaultComponent implements org.apache.
 
         // discover any 3rd party properties sources
         try {
-            FactoryFinder factoryFinder = getCamelContext().adapt(ExtendedCamelContext.class).getFactoryFinder("META-INF/services/org/apache/camel");
+            FactoryFinder factoryFinder = getCamelContext().adapt(ExtendedCamelContext.class).getFactoryFinder("META-INF/services/org/apache/camel/");
             Class<?> type = factoryFinder.findClass("properties-source-factory").orElse(null);
             if (type != null) {
                 Object obj = getCamelContext().getInjector().newInstance(type, false);
