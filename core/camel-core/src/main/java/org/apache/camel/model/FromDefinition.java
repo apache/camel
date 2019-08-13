bec1affdebd3b7ce143c2102fe44a9c00d93085a
Claus Ibsen <claus.ibsen@gmail.com>
2019-08-13 08:37:57 +0200

    CAMEL-13850: Polished

--- core/camel-core/src/main/java/org/apache/camel/model/FromDefinition.java
+++ core/camel-core/src/main/java/org/apache/camel/model/FromDefinition.java
@@ -16,17 +16,12 @@
  */
 package org.apache.camel.model;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.builder.EndpointConsumerBuilder;
 import org.apache.camel.spi.Metadata;
