463b757cf3d0a4946882a3e5714f03e84131043e
Babak Vahdat <bvahdat@apache.org>
2019-08-14 21:05:04 +0200

    cleanups for camel-mongodb3 to camel-mongodb component renaming

--- platforms/spring-boot/components-starter/camel-mongodb-starter/src/main/java/org/apache/camel/component/mongodb/springboot/MongoDbComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-mongodb-starter/src/main/java/org/apache/camel/component/mongodb/springboot/MongoDbComponentConfiguration.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.mongodb3.springboot;
+package org.apache.camel.component.mongodb.springboot;
 
 import javax.annotation.Generated;
 import org.apache.camel.spring.boot.ComponentConfigurationPropertiesCommon;
