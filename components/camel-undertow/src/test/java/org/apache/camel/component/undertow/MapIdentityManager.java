1f6dde3d6dfc7d7980bf774ac9d98e8db71a2d9b
Andrea Cosentino <ancosen@gmail.com>
2019-08-20 12:04:17 +0200

    Fixed CS for Camel-Undertow

--- components/camel-undertow/src/test/java/org/apache/camel/component/undertow/MapIdentityManager.java
+++ components/camel-undertow/src/test/java/org/apache/camel/component/undertow/MapIdentityManager.java
@@ -16,8 +16,6 @@
  */
 package org.apache.camel.component.undertow;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
@@ -25,6 +23,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import io.undertow.security.idm.Account;
 import io.undertow.security.idm.Credential;
