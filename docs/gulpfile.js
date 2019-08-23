80d2d2a55d2806f887a48dbf0a2ef49029b8f79d
Zoran Regvart <zregvart@apache.org>
2019-08-23 17:22:16 +0200

    CAMEL-13932: backport changes from master
    This backports the documentation build system needed for Antora to
    publish component reference only.

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -14,41 +14,137 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const gulp = require('gulp')
-const { dest, src, symlink } = require('gulp');
+const { dest, series, parallel, src, symlink } = require('gulp');
+const del = require('del');
+const inject = require('gulp-inject');
 const map = require('map-stream')
 const path = require('path');
-const inject = require('gulp-inject');
 const rename = require('gulp-rename');
+const sort = require('gulp-sort');
+const through2 = require('through2');
+const File = require('vinyl')
+const fs = require('fs');
 
-gulp.task('symlinks', () => {
-    return src('../components/*/src/main/docs/*.adoc')
+function deleteComponentSymlinks() {
+    return del(['components/modules/ROOT/pages/*', '!components/modules/ROOT/pages/index.adoc']);
+}
+
+function deleteComponentImageSymlinks() {
+    return del(['components/modules/ROOT/assets/images/*']);
+}
+
+function createComponentSymlinks() {
+    return src('../components/{*,*/*}/src/main/docs/*.adoc')
         .pipe(map((file, done) => {
             // this flattens the output to just .../pages/....adoc
             // instead of .../pages/camel-.../src/main/docs/....adoc
             file.base = path.dirname(file.path);
             done(null, file);
         }))
-        .pipe(symlink('components/modules/ROOT/pages/', {
-            relativeSymlinks: true
-        }));
-});
+        // Antora disabled symlinks, there is an issue open
+        // https://gitlab.com/antora/antora/issues/188
+        // to reinstate symlink support, until that's resolved
+        // we'll simply copy over instead of creating symlinks
+        // .pipe(symlink('components/modules/ROOT/pages/', {
+        //     relativeSymlinks: true
+        // }));
+        // uncomment above .pipe() and remove the .pipe() below
+        // when antora#188 is resolved
+        .pipe(dest('components/modules/ROOT/pages/'));
+}
 
-gulp.task('nav', () => {
-    return src('nav.adoc.template')
-        .pipe(inject(src('../components/*/src/main/docs/*.adoc'), {
+function createComponentImageSymlinks() {
+    return src('../components/{*,*/*}/src/main/docs/*.png')
+        .pipe(map((file, done) => {
+            // this flattens the output to just .../pages/....adoc
+            // instead of .../pages/camel-.../src/main/docs/....adoc
+            file.base = path.dirname(file.path);
+            done(null, file);
+        }))
+        // Antora disabled symlinks, there is an issue open
+        // https://gitlab.com/antora/antora/issues/188
+        // to reinstate symlink support, until that's resolved
+        // we'll simply copy over instead of creating symlinks
+        // .pipe(symlink('components/modules/ROOT/pages/', {
+        //     relativeSymlinks: true
+        // }));
+        // uncomment above .pipe() and remove the .pipe() below
+        // when antora#188 is resolved
+        .pipe(dest('components/modules/ROOT/assets/images/'));
+}
+
+function titleFrom(file) {
+    const maybeName = /(?:=|#) (.*)/.exec(file.contents.toString())
+    if (maybeName == null) {
+        throw new Error(`${file.path} doesn't contain Asciidoc heading ('= <Title>') or ('# <Title')`);
+    }
+
+    return maybeName[1];
+}
+
+function insertGeneratedNotice() {
+    return inject(src('./generated.txt'), {
+               name: 'generated',
+               removeTags: true,
+               transform: (filename, file) => {
+                   return file.contents.toString('utf8');
+               }
+           });
+}
+
+function createComponentNav() {
+    return src('component-nav.adoc.template')
+        .pipe(insertGeneratedNotice())
+        .pipe(inject(src('../components/{*,*/*}/src/main/docs/*.adoc').pipe(sort()), {
             removeTags: true,
             transform: (filename, file) => {
                 const filepath = path.basename(filename);
-                const maybeName = /(?:==|##) (.*)/.exec(file.contents.toString())
-                if (maybeName == null) {
-                    throw new Error(`${file.path} doesn't contain Asciidoc heading ('== <Title>') or ('## <Title')`);
-                }
-                return `* xref:${filepath}[${maybeName[1]}]`;
+                const title = titleFrom(file);
+                return `* xref:${filepath}[${title}]`;
             }
         }))
         .pipe(rename('nav.adoc'))
         .pipe(dest('components/modules/ROOT/'))
-});
+}
 
-gulp.task('default', gulp.series('symlinks', 'nav'));
+const extractExamples = function(file, enc, next) {
+    const asciidoc = file.contents.toString();
+    const includes = /(?:include::\{examplesdir\}\/)([^[]+)/g;
+    let example;
+    let exampleFiles = new Set()
+    while (example = includes.exec(asciidoc)) {
+        let examplePath = path.resolve(path.join('..', example[1]));
+        exampleFiles.add(examplePath);
+    }
+
+    exampleFiles.forEach(examplePath => this.push(new File({
+        base: path.resolve('..'),
+        path: examplePath,
+        contents: fs.createReadStream(examplePath)
+    })));
+
+    return next();
+}
+
+function deleteExamples(){
+    return del('components/modules/ROOT/examples/');
+}
+
+function createComponentExamples() {
+    return src('../components/{*,*/*}/src/main/docs/*.adoc')
+        .pipe(through2.obj(extractExamples))
+        .pipe(dest('components/modules/ROOT/examples/'));
+}
+
+const symlinks = parallel(
+  series(deleteComponentSymlinks, createComponentSymlinks),
+  series(deleteComponentImageSymlinks, createComponentImageSymlinks)
+);
+const nav = createComponentNav;
+const examples = series(deleteExamples, createComponentExamples);
+
+exports.symlinks = symlinks;
+exports.nav = nav;
+exports.examples = examples;
+exports.default = series(symlinks, nav, examples);
+
