9e6c89c3b7197aa5c50959ba8fd7f5b0f7f4abd7
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2019-09-12 18:19:06 +0900

    CAMEL-13886: camel-servlet + camel-http4 with null body causes "Stream closed" IOException
    Normally servlet request can be read only once, but when
    Exchange#getOut() is invoked HttpMessage may be copied for the out
    message with the original request that has been already read.
    This fix protects it from being read again.

--- components/camel-http-common/src/main/java/org/apache/camel/http/common/HttpMessage.java
+++ components/camel-http-common/src/main/java/org/apache/camel/http/common/HttpMessage.java
@@ -30,9 +30,11 @@ public class HttpMessage extends DefaultMessage {
     private final HttpServletRequest request;
     private final HttpServletResponse response;
     private final HttpCommonEndpoint endpoint;
+    private boolean requestRead;
 
     public HttpMessage(Exchange exchange, HttpCommonEndpoint endpoint, HttpServletRequest request, HttpServletResponse response) {
         super(exchange);
+        this.requestRead = false;
         this.endpoint = endpoint;
 
         this.request = request;
@@ -52,11 +54,13 @@ public class HttpMessage extends DefaultMessage {
         endpoint.getHttpBinding().readRequest(request, this);
     }
 
-    private HttpMessage(HttpServletRequest request, HttpServletResponse response, Exchange exchange, HttpCommonEndpoint endpoint) {
+    private HttpMessage(HttpServletRequest request, HttpServletResponse response, Exchange exchange, HttpCommonEndpoint endpoint,
+                        boolean requestRead) {
         super(exchange);
         this.request = request;
         this.response = response;
         this.endpoint = endpoint;
+        this.requestRead = requestRead;
     }
 
     public HttpServletRequest getRequest() {
@@ -69,16 +73,23 @@ public class HttpMessage extends DefaultMessage {
 
     @Override
     protected Object createBody() {
+        // HTTP request may be read only once
+        if (requestRead) {
+            return null;
+        }
+
         try {
             return endpoint.getHttpBinding().parseBody(this);
         } catch (IOException e) {
             throw new RuntimeCamelException(e);
+        } finally {
+            requestRead = true;
         }
     }
 
     @Override
     public HttpMessage newInstance() {
-        return new HttpMessage(request, response, getExchange(), endpoint);
+        return new HttpMessage(request, response, getExchange(), endpoint, requestRead);
     }
 
     @Override
