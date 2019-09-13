3ec2cc5d70243d2b569229f95f0f646697cf1eba
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2019-09-13 12:10:03 +0900

    CAMEL-13886: Add integration test to camel-servlet-starter

---
+++ platforms/spring-boot/components-starter/camel-servlet-starter/src/test/java/org/apache/camel/component/servlet/springboot/test/ServletHttpMessageTest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.servlet.springboot.test;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Testing HttpMessage used with Servlet component.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootApplication
+@DirtiesContext
+@ContextConfiguration(classes = ServletHttpMessageTest.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ServletHttpMessageTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private CamelContext context;
+
+    @Before
+    public void setup() throws Exception {
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+
+                rest().get("/outMessageNullBody")
+                        .produces("text/plain")
+                        .route()
+                        // read body at least once
+                        .log("${body}")
+                        // simulate endpoints that may put null to out message body
+                        .process(e -> e.getOut().setBody(null))
+                        // ensure reading body again does not cause an exception
+                        .log("${body}");
+            }
+        });
+    }
+
+    @Test
+    public void testOutMessageNullBody() {
+        Assert.assertNull(restTemplate.getForEntity("/camel/outMessageNullBody", String.class).getBody());
+    }
+
+}
+
