51df429f31e392faf1f6fdf51e4c3ec52e704b52
marius cornescu <marius_cornescu@yahoo.com>
2019-09-19 18:37:02 +0200

    CAMEL-13998 - update all to LF

--- components/camel-hdfs/src/test/java/org/apache/camel/component/hdfs/kerberos/HdfsKerberosConfigurationFactoryTest.java
+++ components/camel-hdfs/src/test/java/org/apache/camel/component/hdfs/kerberos/HdfsKerberosConfigurationFactoryTest.java
@@ -1,10 +1,8 @@
 package org.apache.camel.component.hdfs.kerberos;
 
-import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 
 public class HdfsKerberosConfigurationFactoryTest {
 
