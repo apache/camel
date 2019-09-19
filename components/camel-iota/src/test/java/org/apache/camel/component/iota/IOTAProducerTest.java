0c8b0c0c93afd7942b4b9881815a2a75e383a322
Andrea Cosentino <ancosen@gmail.com>
2019-09-19 08:47:05 +0200

    Ignore Iota sendTransferTest for the moment

--- components/camel-iota/src/test/java/org/apache/camel/component/iota/IOTAProducerTest.java
+++ components/camel-iota/src/test/java/org/apache/camel/component/iota/IOTAProducerTest.java
@@ -19,6 +19,7 @@ package org.apache.camel.component.iota;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class IOTAProducerTest extends CamelTestSupport {
@@ -28,6 +29,7 @@ public class IOTAProducerTest extends CamelTestSupport {
 
     private static final String IOTA_NODE_URL = "https://nodes.thetangle.org:443";
 
+    @Ignore
     @Test
     public void sendTransferTest() throws Exception {
         final String message = "ILOVEAPACHECAMEL";
