c5e7c7ef0572078fd825ad91c4054164ed38ebcf
Omar Al-Safi <omarsmak@gmail.com>
2019-10-15 13:37:24 +0200

    CAMEL-14009: Cleanup and fix dependencies

--- components/camel-debezium/camel-debezium-maven-plugin/src/main/java/org/apache/camel/maven/AbstractGenerateConnectorConfig.java
+++ components/camel-debezium/camel-debezium-maven-plugin/src/main/java/org/apache/camel/maven/AbstractGenerateConnectorConfig.java
@@ -1,5 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.maven;
-
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -16,7 +31,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 public abstract class AbstractGenerateConnectorConfig extends AbstractMojo {
     @Parameter(defaultValue = "${project.build.directory}/generated-sources/connector-configurations")
-    protected File generatedSrcDir;
+    private File generatedSrcDir;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -27,7 +42,7 @@ public abstract class AbstractGenerateConnectorConfig extends AbstractMojo {
         try {
             final File parentPath = new File(generatedSrcDir, connectorConfigGenerator.getPackageName().replace(".", "/"));
             final File connectorConfigClassFile = new File(parentPath, connectorConfigGenerator.getClassName() + ".java");
-            if(!connectorConfigClassFile.exists()){
+            if (!connectorConfigClassFile.exists()) {
                 connectorConfigClassFile.getParentFile().mkdirs();
                 connectorConfigClassFile.createNewFile();
             }
