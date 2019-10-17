b41a9e8680b4b0eb36936fc0240e91292e445003
Stig Rohde Døssing <stigdoessing@gmail.com>
2019-10-17 18:59:40 +0200

    CAMEL-13752: Make new BridgePropertyPlaceholderConfigurer independent of Spring framework changes

--- components/camel-spring/src/main/java/org/apache/camel/spring/spi/BridgePropertyPlaceholderConfigurer.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/spi/BridgePropertyPlaceholderConfigurer.java
@@ -16,14 +16,26 @@
  */
 package org.apache.camel.spring.spi;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 import org.apache.camel.component.properties.PropertiesLookup;
 import org.apache.camel.component.properties.PropertiesParser;
 import org.apache.camel.spi.PropertiesSource;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.ConfigurablePropertyResolver;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.PropertySources;
+import org.springframework.core.env.PropertySourcesPropertyResolver;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 /**
  * A {@link PropertySourcesPlaceholderConfigurer} that bridges Camel's <a href="http://camel.apache.org/using-propertyplaceholder.html">
@@ -37,10 +49,67 @@ public class BridgePropertyPlaceholderConfigurer extends PropertySourcesPlacehol
     private ConfigurablePropertyResolver resolver;
     private PropertiesParser parser;
 
+    private MutablePropertySources propertySources;
+    private PropertySources appliedPropertySources;
+    private Environment environment;
+
     @Override
     protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, ConfigurablePropertyResolver propertyResolver) throws BeansException {
         super.processProperties(beanFactoryToProcess, propertyResolver);
         resolver = propertyResolver;
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        if (this.propertySources == null) {
+            this.propertySources = new MutablePropertySources();
+            if (this.environment != null) {
+                this.propertySources.addLast(
+                    new PropertySource<Environment>(ENVIRONMENT_PROPERTIES_PROPERTY_SOURCE_NAME, this.environment) {
+                    @Override
+                    @Nullable
+                    public String getProperty(String key) {
+                        return this.source.getProperty(key);
+                    }
+                }
+                );
+            }
+            try {
+                PropertySource<?> localPropertySource =
+                    new PropertiesPropertySource(LOCAL_PROPERTIES_PROPERTY_SOURCE_NAME, mergeProperties());
+                if (this.localOverride) {
+                    this.propertySources.addFirst(localPropertySource);
+                } else {
+                    this.propertySources.addLast(localPropertySource);
+                }
+            } catch (IOException ex) {
+                throw new BeanInitializationException("Could not load properties", ex);
+            }
+        }
+
+        List<String> sourceNames = this.propertySources.stream()
+            .map(source -> source.getName())
+            .collect(Collectors.toList());
+        for (String sourceName : sourceNames) {
+            this.propertySources.replace(sourceName, new ConvertingPropertySourceDecorator<>(this.propertySources.get(sourceName)));
+        }
+        processProperties(beanFactory, new PropertySourcesPropertyResolver(this.propertySources));
+        this.appliedPropertySources = this.propertySources;
+    }
+
+    @Override
+    public void setPropertySources(PropertySources propertySources) {
+        this.propertySources = new MutablePropertySources(propertySources);
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    public PropertySources getAppliedPropertySources() throws IllegalStateException {
+        Assert.state(this.appliedPropertySources != null, "PropertySources have not yet been applied");
+        return this.appliedPropertySources;
     }
 
     @Override
@@ -49,7 +118,6 @@ public class BridgePropertyPlaceholderConfigurer extends PropertySourcesPlacehol
         super.processProperties(beanFactoryToProcess, props);
     }
 
-    @Override
     protected Object convertProperty(String propertyName, Object propertyValue) {
         if (parser != null && propertyValue instanceof String) {
             //Apply the parser to the placeholder value, to allow Camel placeholder processing to work outside the <CamelContext>, e.g. for decryption.
@@ -127,6 +195,25 @@ public class BridgePropertyPlaceholderConfigurer extends PropertySourcesPlacehol
                 value = answer;
             }
             return parser.parseProperty(key, value);
+        }
+    }
+
+    private class ConvertingPropertySourceDecorator<T> extends PropertySource<T> {
+
+        private final PropertySource<T> delegate;
+
+        public ConvertingPropertySourceDecorator(PropertySource<T> delegate) {
+            super(delegate.getName(), delegate.getSource());
+            this.delegate = delegate;
+        }
+
+        @Override
+        public Object getProperty(String name) {
+            Object value = this.delegate.getProperty(name);
+            if (value != null) {
+                return convertProperty(name, value);
+            }
+            return value;
         }
     }
 }
