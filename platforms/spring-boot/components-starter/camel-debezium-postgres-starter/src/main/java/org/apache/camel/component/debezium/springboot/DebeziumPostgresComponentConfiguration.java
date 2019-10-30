e754b9b78289ceb48691e983faf3b0a9898a68e3
Omar Al-Safi <omarsmak@gmail.com>
2019-10-30 17:02:40 +0100

    CAMEL-14091: Add tests
    Signed-off-by: Omar Al-Safi <omarsmak@gmail.com>

--- platforms/spring-boot/components-starter/camel-debezium-postgres-starter/src/main/java/org/apache/camel/component/debezium/springboot/DebeziumPostgresComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-debezium-postgres-starter/src/main/java/org/apache/camel/component/debezium/springboot/DebeziumPostgresComponentConfiguration.java
@@ -358,7 +358,7 @@ public class DebeziumPostgresComponentConfiguration
          * The name of the Java class of the commit policy. It defines when
          * offsets commit has to be triggered based on the number of events
          * processed and the time elapsed since the last commit. This class must
-         * implement the interface <…​>.OffsetCommitPolicy. The default is a
+         * implement the interface <������>.OffsetCommitPolicy. The default is a
          * periodic commit policy based upon time intervals.
          */
         private String offsetCommitPolicy = "io.debezium.embedded.spi.OffsetCommitPolicy.PeriodicCommitOffsetPolicy";
@@ -376,7 +376,7 @@ public class DebeziumPostgresComponentConfiguration
         /**
          * The number of partitions used when creating the offset storage topic.
          * Required when offset.storage is set to the
-         * <…​>.KafkaOffsetBackingStore.
+         * <������>.KafkaOffsetBackingStore.
          */
         private Integer offsetStoragePartitions;
         /**
