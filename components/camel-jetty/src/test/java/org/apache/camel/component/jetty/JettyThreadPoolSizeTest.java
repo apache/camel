3012796b81308f3e7bc82a2d6d534a2d7caf5d44
Luigi De Masi <ldemasi@redhat.com>
2019-11-04 23:30:23 +0100

    CAMEL-14137 Thread leak in camel-jetty component if maxThreads or minThreads property is set

--- components/camel-jetty/src/test/java/org/apache/camel/component/jetty/JettyThreadPoolSizeTest.java
+++ components/camel-jetty/src/test/java/org/apache/camel/component/jetty/JettyThreadPoolSizeTest.java
@@ -12,61 +12,57 @@ public class JettyThreadPoolSizeTest extends BaseJettyTest {
 
     private static final Logger log =  LoggerFactory.getLogger(JettyThreadPoolSizeTest.class);
 
-
-    private JettyHttpComponent jettyComponent;
-
-    private RouteBuilder builder;
-
     @Test
     public void threadPoolTest(){
 
-
-        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-        long initialJettyThreadNumber = threadSet.stream().filter(thread -> thread.getName().contains("CamelJettyServer")).count();
+        long initialJettyThreadNumber = countJettyThread();
 
         log.info("initial Jetty thread number (expected 5): "+ initialJettyThreadNumber);
 
         context.stop();
 
-        Set<Thread> threadSetAfterStop = Thread.getAllStackTraces().keySet();
-        long jettyThreadNumberAfterStop = threadSetAfterStop.stream().filter(thread -> thread.getName().contains("CamelJettyServer")).count();
+        long jettyThreadNumberAfterStop =  countJettyThread();
 
         log.info("Jetty thread number after stopping Camel Context: (expected 0): "+ jettyThreadNumberAfterStop);
 
-
-        jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
+        JettyHttpComponent jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
         jettyComponent.setMinThreads(5);
         jettyComponent.setMaxThreads(5);
 
         context.start();
-        Set<Thread> threadSetAfterRestart = Thread.getAllStackTraces().keySet();
-        long jettyThreadNumberAfterRestart = threadSetAfterRestart.stream().filter(thread -> thread.getName().contains("CamelJettyServer")).count();
+
+        long jettyThreadNumberAfterRestart = countJettyThread();
 
         log.info("Jetty thread number after starting Camel Context: (expected 5): "+ jettyThreadNumberAfterRestart);
 
+        assertEquals(5L,initialJettyThreadNumber);
 
-        assertEquals(5,initialJettyThreadNumber);
+        assertEquals(0L,jettyThreadNumberAfterStop);
 
-        assertEquals(0,jettyThreadNumberAfterStop);
-
-        assertEquals(5,jettyThreadNumberAfterRestart);
+        assertEquals(5L,jettyThreadNumberAfterRestart);
     }
 
     @Override
     protected RouteBuilder createRouteBuilder() throws Exception {
 
-        builder = new RouteBuilder() {
+        return new  RouteBuilder() {
             @Override
             public void configure() throws Exception {
                 // setup the jetty component with the custom minThreads
-                jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
+               JettyHttpComponent jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
                 jettyComponent.setMinThreads(5);
                 jettyComponent.setMaxThreads(5);
 
                 from("jetty://http://localhost:{{port}}/myserverWithCustomPoolSize").to("mock:result");
             }
         };
-        return builder;
+    }
+
+    private long countJettyThread(){
+
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        return threadSet.stream().filter(thread -> thread.getName().contains("CamelJettyServer")).count();
+
     }
 
 }
