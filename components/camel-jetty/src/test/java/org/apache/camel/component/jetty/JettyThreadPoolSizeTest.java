2f2d808354166e661998dc8bcc88eae548138c19
Luigi De Masi <>
2019-11-05 11:26:27 +0100

    CAMEL-14137: removed qtp variable according to pr comment.

--- components/camel-jetty/src/test/java/org/apache/camel/component/jetty/JettyThreadPoolSizeTest.java
+++ components/camel-jetty/src/test/java/org/apache/camel/component/jetty/JettyThreadPoolSizeTest.java
@@ -10,20 +10,20 @@ import java.util.Set;
 public class JettyThreadPoolSizeTest extends BaseJettyTest {
 
 
-    private static final Logger log =  LoggerFactory.getLogger(JettyThreadPoolSizeTest.class);
+    private static final Logger LOG =  LoggerFactory.getLogger(JettyThreadPoolSizeTest.class);
 
     @Test
-    public void threadPoolTest(){
+    public void threadPoolTest() {
 
         long initialJettyThreadNumber = countJettyThread();
 
-        log.info("initial Jetty thread number (expected 5): "+ initialJettyThreadNumber);
+        LOG.info("initial Jetty thread number (expected 5): " + initialJettyThreadNumber);
 
         context.stop();
 
         long jettyThreadNumberAfterStop =  countJettyThread();
 
-        log.info("Jetty thread number after stopping Camel Context: (expected 0): "+ jettyThreadNumberAfterStop);
+        LOG.info("Jetty thread number after stopping Camel Context: (expected 0): " + jettyThreadNumberAfterStop);
 
         JettyHttpComponent jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
         jettyComponent.setMinThreads(5);
@@ -33,13 +33,13 @@ public class JettyThreadPoolSizeTest extends BaseJettyTest {
 
         long jettyThreadNumberAfterRestart = countJettyThread();
 
-        log.info("Jetty thread number after starting Camel Context: (expected 5): "+ jettyThreadNumberAfterRestart);
+        LOG.info("Jetty thread number after starting Camel Context: (expected 5): "+ jettyThreadNumberAfterRestart);
 
-        assertEquals(5L,initialJettyThreadNumber);
+        assertEquals(5L, initialJettyThreadNumber);
 
-        assertEquals(0L,jettyThreadNumberAfterStop);
+        assertEquals(0L, jettyThreadNumberAfterStop);
 
-        assertEquals(5L,jettyThreadNumberAfterRestart);
+        assertEquals(5L, jettyThreadNumberAfterRestart);
     }
 
     @Override
@@ -49,7 +49,7 @@ public class JettyThreadPoolSizeTest extends BaseJettyTest {
             @Override
             public void configure() throws Exception {
                 // setup the jetty component with the custom minThreads
-               JettyHttpComponent jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
+                JettyHttpComponent jettyComponent = (JettyHttpComponent)context.getComponent("jetty");
                 jettyComponent.setMinThreads(5);
                 jettyComponent.setMaxThreads(5);
 
@@ -58,11 +58,10 @@ public class JettyThreadPoolSizeTest extends BaseJettyTest {
         };
     }
 
-    private long countJettyThread(){
+    private long countJettyThread() {
 
         Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
         return threadSet.stream().filter(thread -> thread.getName().contains("CamelJettyServer")).count();
-
     }
 
 }
