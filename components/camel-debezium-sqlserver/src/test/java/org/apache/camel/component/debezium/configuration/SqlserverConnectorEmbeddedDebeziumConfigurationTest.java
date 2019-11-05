7439b12624220a9d1368d714094cf87761cd87ef
Omar Al-Safi <omarsmak@gmail.com>
2019-11-05 16:11:45 +0100

    CAMEL-14093: Remove junit assert all import and replace it with specific imports

--- components/camel-debezium-sqlserver/src/test/java/org/apache/camel/component/debezium/configuration/SqlserverConnectorEmbeddedDebeziumConfigurationTest.java
+++ components/camel-debezium-sqlserver/src/test/java/org/apache/camel/component/debezium/configuration/SqlserverConnectorEmbeddedDebeziumConfigurationTest.java
@@ -23,7 +23,9 @@ import io.debezium.embedded.EmbeddedEngine;
 import org.apache.camel.component.debezium.DebeziumConstants;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SqlserverConnectorEmbeddedDebeziumConfigurationTest {
 
