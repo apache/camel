d82470f9cd8ebf796b0c4de73d3fd055c1b61c20
Luigi De Masi <ldemasi@redhat.com>
2019-11-06 09:03:43 +0100

    CAMEL-14137 Added licence header to unit test

--- components/camel-jetty9/src/test/java/org/apache/camel/component/jetty/JettyThreadPoolSizeTest.java
+++ components/camel-jetty9/src/test/java/org/apache/camel/component/jetty/JettyThreadPoolSizeTest.java
@@ -1,11 +1,28 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.jetty;
 
+import java.util.Set;
 import org.apache.camel.builder.RouteBuilder;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Set;
+
 
 public class JettyThreadPoolSizeTest extends BaseJettyTest {
 
@@ -33,7 +50,7 @@ public class JettyThreadPoolSizeTest extends BaseJettyTest {
 
         long jettyThreadNumberAfterRestart = countJettyThread();
 
-        LOG.info("Jetty thread number after starting Camel Context: (expected 5): "+ jettyThreadNumberAfterRestart);
+        LOG.info("Jetty thread number after starting Camel Context: (expected 5): " + jettyThreadNumberAfterRestart);
 
         assertEquals(5L, initialJettyThreadNumber);
 
