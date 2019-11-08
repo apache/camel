e5397a4b87f977a3d8c879718d530debde8881cf
Zoran Regvart <zregvart@apache.org>
2019-11-08 22:22:28 +0100

    CAMEL-14163: multiple request query parameters ...
    ...in Rest component
    
    Instead of forcing the parameter value to be converted to a String, the
    value is passed to URISupport that generates multiple request parameters
    for parameters whose value is instance of java.util.List.
    
    (cherry picked from commit bbc0b86e8ddfe84541df4ba0201b416323def5a6)

--- camel-core/src/test/java/org/apache/camel/component/rest/RestProducerTest.java
+++ camel-core/src/test/java/org/apache/camel/component/rest/RestProducerTest.java
@@ -18,6 +18,8 @@ package org.apache.camel.component.rest;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -30,6 +32,22 @@ import static org.junit.Assert.assertNull;
 public class RestProducerTest {
 
     private CamelContext camelContext = new DefaultCamelContext();
+
+    @Test
+    public void shouldCreateMultipleQueryParameters() throws UnsupportedEncodingException, URISyntaxException {
+        final DefaultMessage message = new DefaultMessage(camelContext);
+        message.setHeader("multiple", Arrays.asList("value1", "value2", "value3"));
+
+        assertEquals("param=value1&param=value2&param=value3", RestProducer.createQueryParameters("param={multiple}", message));
+    }
+
+    @Test
+    public void shouldCreateMultipleOptionalQueryParameters() throws UnsupportedEncodingException, URISyntaxException {
+        final DefaultMessage message = new DefaultMessage(camelContext);
+        message.setHeader("multiple", Collections.EMPTY_LIST);
+
+        assertEquals("", RestProducer.createQueryParameters("multiple={multiple?}", message));
+    }
 
     @Test
     public void shouldCreateDefinedQueryParameters() throws UnsupportedEncodingException, URISyntaxException {
@@ -71,9 +89,19 @@ public class RestProducerTest {
         final DefaultMessage message = new DefaultMessage(camelContext);
         message.setHeader("requiredParamPlaceholder", "header_required_value");
         message.setHeader("optionalPresentParamPlaceholder", "header_optional_present_value");
+        message.setHeader("multiple", Arrays.asList("value1", "value2", "value3"));
+        message.setHeader("multipleOptional", Collections.EMPTY_LIST);
 
-        assertEquals("given=value&required=header_required_value&optional_present=header_optional_present_value",
-            RestProducer.createQueryParameters(
-                "given=value&required={requiredParamPlaceholder}&optional={optionalParamPlaceholder?}&optional_present={optionalPresentParamPlaceholder?}", message));
+        assertEquals("given=value"
+            + "&required=header_required_value"
+            + "&optional_present=header_optional_present_value"
+            + "&multiple=value1&multiple=value2&multiple=value3",
+            RestProducer.createQueryParameters("given=value"
+                + "&required={requiredParamPlaceholder}"
+                + "&optional={optionalParamPlaceholder?}"
+                + "&optional_present={optionalPresentParamPlaceholder?}"
+                + "&multiple={multiple}"
+                + "&multipleOptional={multipleOptional?}",
+                message));
     }
 }
