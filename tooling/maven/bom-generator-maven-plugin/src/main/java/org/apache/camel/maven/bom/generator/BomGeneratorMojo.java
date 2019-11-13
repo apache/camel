9f0b8669224f4d98c8274d53c35b779615b02672
Zoran Regvart <zregvart@apache.org>
2019-11-13 11:48:49 +0100

    CAMEL-13643: Change Spring Boot starters groupId
    This changes the Spring Boot starters Maven groupId from
    `org.apache.camel` to `org.apache.camel.springboot`. Also updates the
    documentation, BOM generation, archetype and examples.

--- tooling/maven/bom-generator-maven-plugin/src/main/java/org/apache/camel/maven/bom/generator/BomGeneratorMojo.java
+++ tooling/maven/bom-generator-maven-plugin/src/main/java/org/apache/camel/maven/bom/generator/BomGeneratorMojo.java
@@ -157,7 +157,7 @@ public class BomGeneratorMojo extends AbstractMojo {
     private List<Dependency> enhance(List<Dependency> dependencyList) {
 
         for (Dependency dep : dependencyList) {
-            if (dep.getGroupId().startsWith(project.getGroupId()) && project.getVersion().equals(dep.getVersion())) {
+            if (dep.getGroupId().startsWith("org.apache.camel") && project.getVersion().equals(dep.getVersion())) {
                 dep.setVersion("${project.version}");
             }
         }
