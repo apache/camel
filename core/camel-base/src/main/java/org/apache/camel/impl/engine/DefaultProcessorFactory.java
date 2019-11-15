ecb1afc3d30570208665452d12d19a12b6f3d5d0
Claus Ibsen <claus.ibsen@gmail.com>
2019-11-15 12:05:11 +0100

    Polished

--- core/camel-base/src/main/java/org/apache/camel/impl/engine/DefaultProcessorFactory.java
+++ core/camel-base/src/main/java/org/apache/camel/impl/engine/DefaultProcessorFactory.java
@@ -82,23 +82,22 @@ public class DefaultProcessorFactory implements ProcessorFactory {
 
     @Override
     public Processor createProcessor(CamelContext camelContext, String definitionName, Map<String, Object> args) throws Exception {
-        // currently only SendDynamicProcessor is supported
-        SendDynamicProcessor answer = null;
         if ("SendDynamicProcessor".equals(definitionName)) {
             String uri = (String) args.get("uri");
             Expression expression = (Expression) args.get("expression");
             ExchangePattern exchangePattern = (ExchangePattern) args.get("exchangePattern");
-            answer = new SendDynamicProcessor(uri, expression);
-            answer.setCamelContext(camelContext);
+            SendDynamicProcessor processor = new SendDynamicProcessor(uri, expression);
+            processor.setCamelContext(camelContext);
             if (exchangePattern != null) {
-                answer.setPattern(exchangePattern);
+                processor.setPattern(exchangePattern);
             }
+            return processor;
         } else if ("UnitOfWorkProducer".equals(definitionName)) {
             Producer producer = (Producer) args.get("producer");
             return new UnitOfWorkProducer(producer);
         }
 
-        return answer;
+        return null;
     }
     
 }
