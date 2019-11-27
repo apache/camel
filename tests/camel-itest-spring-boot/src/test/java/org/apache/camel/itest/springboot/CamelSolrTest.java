d0956e773c22fc2cfba1e880b1c637ed14cc21ed
Andrea Cosentino <ancosen@gmail.com>
2019-11-27 14:53:23 +0100

    Ignore Camel-Solr SB Integration test, it's failing only on CI

--- tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelSolrTest.java
+++ tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelSolrTest.java
@@ -20,9 +20,11 @@ import org.apache.camel.itest.springboot.util.ArquillianPackager;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+@Ignore("Don't run this on CI")
 @RunWith(Arquillian.class)
 public class CamelSolrTest extends AbstractSpringBootTestSupport {
 
