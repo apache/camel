apache/camel

commit d5020bea38833547c7a2c6634cd7232bc333917e

Add an option to set the default application.properties location as weel as disabling it

core/camel-main/src/main/java/org/apache/camel/main/BaseMainSupport.java
@@ -88,6 +88,7 @@ public abstract class BaseMainSupport extends ServiceSupport {
     protected List<Object> configurations = new ArrayList<>();
     protected String configurationClasses;
     protected String propertyPlaceholderLocations;
+    protected String defaultPropertyPlaceholderLocation = "classpath:application.properties;optional=true";
     protected Properties initialProperties;
     protected Properties overrideProperties;
 
@@ -241,6 +242,20 @@ public abstract class BaseMainSupport extends ServiceSupport {
      */
     public void setPropertyPlaceholderLocations(String location) {
         this.propertyPlaceholderLocations = location;
+    }
+
+    public String getDefaultPropertyPlaceholderLocation() {
+        return defaultPropertyPlaceholderLocation;
+    }
+
+    /**
+     * Set the default location for application properties if no locations have been set.
+     * If the value is set to "false" or empty, the default location is not taken into account.
+     * <p/>
+     * Default value is "classpath:application.properties;optional=true".
+     */
+    public void setDefaultPropertyPlaceholderLocation(String defaultPropertyPlaceholderLocation) {
+        this.defaultPropertyPlaceholderLocation = defaultPropertyPlaceholderLocation;
     }
 
     @Deprecated
@@ -480,12 +495,12 @@ public abstract class BaseMainSupport extends ServiceSupport {
                 pc.setOverrideProperties(overrideProperties);
             }
             LOG.info("Using properties from: {}", propertyPlaceholderLocations);
-        } else {
-            // lets default to application.properties and ignore if its missing
-            // if there are no existing locations configured
+        } else if (ObjectHelper.isNotEmpty(defaultPropertyPlaceholderLocation) && !ObjectHelper.equal("false", defaultPropertyPlaceholderLocation)) {
+            // lets default to defaultPropertyPlaceholderLocation if
+            // there are no existing locations configured
             PropertiesComponent pc = camelContext.getPropertiesComponent();
             if (pc.getLocations().isEmpty()) {
-                pc.addLocation("classpath:application.properties;optional=true");
+                pc.addLocation(defaultPropertyPlaceholderLocation);
             }
             if (initialProperties != null) {
                 pc.setInitialProperties(initialProperties);
@@ -493,7 +508,7 @@ public abstract class BaseMainSupport extends ServiceSupport {
             if (overrideProperties != null) {
                 pc.setOverrideProperties(overrideProperties);
             }
-            LOG.info("Using properties from classpath:application.properties");
+            LOG.info("Using properties from {}", defaultPropertyPlaceholderLocation);
         }
 
         if (mainConfigurationProperties.getDurationMaxMessages() > 0 || mainConfigurationProperties.getDurationMaxIdleSeconds() > 0) {

core/camel-main/src/test/java/org/apache/camel/main/MainPropertyPlaceholderLocationsTest.java
@@ -1,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.main;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainPropertyPlaceholderLocationsTest extends Assert {
+    @Test(expected = IllegalArgumentException.class)
+    public void testDefaultPropertyPlaceholderLocationDisabled() {
+        Main main = new Main();
+        try {
+            main.setDefaultPropertyPlaceholderLocation("false");
+            main.start();
+            main.getCamelContext().resolvePropertyPlaceholders("{{hello}}");
+        } finally {
+            main.stop();
+        }
+    }
+
+    @Test
+    public void testDefaultPropertyPlaceholderLocationEnabled() {
+        Main main = new Main();
+        try {
+            main.start();
+            assertEquals("World", main.getCamelContext().resolvePropertyPlaceholders("{{hello}}"));
+        } finally {
+            main.stop();
+        }
+    }
+
+    @Test
+    public void testCustomPropertyPlaceholderLocationEnabled() {
+        Main main = new Main();
+        try {
+            main.setDefaultPropertyPlaceholderLocation("classpath:default.properties");
+            main.start();
+            assertEquals("default", main.getCamelContext().resolvePropertyPlaceholders("{{hello}}"));
+        } finally {
+            main.stop();
+        }
+    }
+}

core/camel-main/src/test/resources/default.properties
@@ -1,0 +1,17 @@
+## ---------------------------------------------------------------------------
+## Licensed to the Apache Software Foundation (ASF) under one or more
+## contributor license agreements.  See the NOTICE file distributed with
+## this work for additional information regarding copyright ownership.
+## The ASF licenses this file to You under the Apache License, Version 2.0
+## (the "License"); you may not use this file except in compliance with
+## the License.  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## ---------------------------------------------------------------------------
+hello=default

commit d6ea36c94caf1083f268249741bab73bb90c6849

Fix regression caused by d5020bea388

core/camel-main/src/main/java/org/apache/camel/main/BaseMainSupport.java
@@ -488,12 +488,6 @@ public abstract class BaseMainSupport extends ServiceSupport {
         if (propertyPlaceholderLocations != null) {
             PropertiesComponent pc = camelContext.getPropertiesComponent();
             pc.addLocation(propertyPlaceholderLocations);
-            if (initialProperties != null) {
-                pc.setInitialProperties(initialProperties);
-            }
-            if (overrideProperties != null) {
-                pc.setOverrideProperties(overrideProperties);
-            }
             LOG.info("Using properties from: {}", propertyPlaceholderLocations);
         } else if (ObjectHelper.isNotEmpty(defaultPropertyPlaceholderLocation) && !ObjectHelper.equal("false", defaultPropertyPlaceholderLocation)) {
             // lets default to defaultPropertyPlaceholderLocation if
@@ -502,13 +496,16 @@ public abstract class BaseMainSupport extends ServiceSupport {
             if (pc.getLocations().isEmpty()) {
                 pc.addLocation(defaultPropertyPlaceholderLocation);
             }
-            if (initialProperties != null) {
-                pc.setInitialProperties(initialProperties);
-            }
-            if (overrideProperties != null) {
-                pc.setOverrideProperties(overrideProperties);
-            }
+
             LOG.info("Using properties from {}", defaultPropertyPlaceholderLocation);
+        }
+
+        PropertiesComponent pc = camelContext.getPropertiesComponent();
+        if (initialProperties != null) {
+            pc.setInitialProperties(initialProperties);
+        }
+        if (overrideProperties != null) {
+            pc.setOverrideProperties(overrideProperties);
         }
 
         if (mainConfigurationProperties.getDurationMaxMessages() > 0 || mainConfigurationProperties.getDurationMaxIdleSeconds() > 0) {