577570105381a110982ed1715abb620556329e2e
Claus Ibsen <claus.ibsen@gmail.com>
2019-12-07 21:27:00 +0100

    CAMEL-14263: camel-crypto should use source code generated configurer to avoid reflection configuration.

--- components/camel-crypto/src/main/java/org/apache/camel/component/crypto/DigitalSignatureComponent.java
+++ components/camel-crypto/src/main/java/org/apache/camel/component/crypto/DigitalSignatureComponent.java
@@ -31,7 +31,7 @@ import org.apache.camel.util.ObjectHelper;
 public class DigitalSignatureComponent extends DefaultComponent {
 
     @Metadata(label = "advanced")
-    private DigitalSignatureConfiguration configuration;
+    private DigitalSignatureConfiguration configuration = new DigitalSignatureConfiguration();
 
     public DigitalSignatureComponent() {
     }
@@ -46,7 +46,6 @@ public class DigitalSignatureComponent extends DefaultComponent {
 
         DigitalSignatureConfiguration config = getConfiguration().copy();
 
-        setProperties(config, parameters);
         config.setCamelContext(getCamelContext());
         try {
             config.setCryptoOperation(new URI(remaining).getScheme());
@@ -54,13 +53,12 @@ public class DigitalSignatureComponent extends DefaultComponent {
             throw new MalformedURLException(String.format("An invalid crypto uri was provided '%s'."
                     + " Check the uri matches the format crypto:sign or crypto:verify", uri));
         }
-        return new DigitalSignatureEndpoint(uri, this, config);
+        Endpoint endpoint = new DigitalSignatureEndpoint(uri, this, config);
+        setProperties(endpoint, parameters);
+        return endpoint;
     }
 
     public DigitalSignatureConfiguration getConfiguration() {
-        if (configuration == null) {
-            configuration = new DigitalSignatureConfiguration();
-        }
         return configuration;
     }
 
