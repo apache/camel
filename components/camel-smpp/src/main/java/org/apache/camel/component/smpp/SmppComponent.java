1379c2ff685d9900b4f94d4909a2d71a9cbf711b
Claus Ibsen <claus.ibsen@gmail.com>
2019-12-07 21:33:05 +0100

    CAMEL-14263: camel-smpp should use source code generated configurer to avoid reflection configuration.

--- components/camel-smpp/src/main/java/org/apache/camel/component/smpp/SmppComponent.java
+++ components/camel-smpp/src/main/java/org/apache/camel/component/smpp/SmppComponent.java
@@ -52,9 +52,7 @@ public class SmppComponent extends DefaultComponent {
 
         // create a copy of the configuration as other endpoints can adjust their copy as well
         SmppConfiguration config = this.configuration.copy();
-
         config.configureFromURI(new URI(uri));
-        setProperties(config, parameters);
 
         SmppEndpoint answer = new SmppEndpoint(uri, this, config);
         setProperties(answer, parameters);
