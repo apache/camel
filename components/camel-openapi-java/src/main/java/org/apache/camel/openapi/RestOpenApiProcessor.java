793bce17d12aee030fc230103f6d3c984578850d
Freeman Fang <freeman.fang@gmail.com>
2019-12-13 15:12:12 -0500

    [CAMEL-12619]add camel-openapi-java component to support both OpenApi V2 and V3

---
+++ components/camel-openapi-java/src/main/java/org/apache/camel/openapi/RestOpenApiProcessor.java
@@ -0,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.openapi;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.spi.RestConfiguration;
+import org.apache.camel.support.PatternHelper;
+import org.apache.camel.util.ObjectHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class RestOpenApiProcessor implements Processor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestOpenApiProcessor.class);
+    private final BeanConfig openApiConfig;
+    private final RestOpenApiSupport support;
+    private final String contextIdPattern;
+    private final boolean contextIdListing;
+    private final RestConfiguration configuration;
+
+    @SuppressWarnings("unchecked")
+    public RestOpenApiProcessor(String contextIdPattern, boolean contextIdListing, Map<String, Object> parameters, RestConfiguration configuration) {
+        this.contextIdPattern = contextIdPattern;
+        this.contextIdListing = contextIdListing;
+        this.configuration = configuration;
+        this.support = new RestOpenApiSupport();
+        this.openApiConfig = new BeanConfig();
+
+        if (parameters == null) {
+            parameters = Collections.EMPTY_MAP;
+        }
+        support.initOpenApi(openApiConfig, parameters);
+    }
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+        String contextId = exchange.getContext().getName();
+        String route = exchange.getIn().getHeader(Exchange.HTTP_PATH, String.class);
+        String accept = exchange.getIn().getHeader("Accept", String.class);
+
+        RestApiResponseAdapter adapter = new ExchangeRestApiResponseAdapter(exchange);
+
+        // whether to use json or yaml
+        boolean json = false;
+        boolean yaml = false;
+        if (route != null && route.endsWith("/openapi.json")) {
+            json = true;
+            route = route.substring(0, route.length() - 13);
+        } else if (route != null && route.endsWith("/openapi.yaml")) {
+            yaml = true;
+            route = route.substring(0, route.length() - 13);
+        }
+        if (accept != null && !json && !yaml) {
+            json = accept.toLowerCase(Locale.US).contains("json");
+            yaml = accept.toLowerCase(Locale.US).contains("yaml");
+        }
+        if (!json && !yaml) {
+            // json is default
+            json = true;
+        }
+
+        try {
+            // render list of camel contexts as root
+            if (contextIdListing && (ObjectHelper.isEmpty(route) || route.equals("/"))) {
+                support.renderCamelContexts(adapter, contextId, contextIdPattern, json, yaml, configuration);
+            } else {
+                String name;
+                if (ObjectHelper.isNotEmpty(route)) {
+                    // first part is the camel context
+                    if (route.startsWith("/")) {
+                        route = route.substring(1);
+                    }
+                    // the remainder is the route part
+                    name = route.split("/")[0];
+                    if (route.startsWith(contextId)) {
+                        route = route.substring(name.length());
+                    }
+                } else {
+                    // listing not enabled then get current camel context as the name
+                    name = exchange.getContext().getName();
+                }
+
+                boolean match = true;
+                if (contextIdPattern != null) {
+                    if ("#name#".equals(contextIdPattern)) {
+                        match = name.equals(contextId);
+                    } else {
+                        match = PatternHelper.matchPattern(name, contextIdPattern);
+                    }
+                    if (LOG.isDebugEnabled()) {
+                        LOG.debug("Match contextId: {} with pattern: {} -> {}", name, contextIdPattern, match);
+                    }
+                }
+
+                if (!match) {
+                    adapter.noContent();
+                } else {
+                    support.renderResourceListing(exchange.getContext(), adapter, openApiConfig, name, route, json, yaml,
+                        exchange.getIn().getHeaders(), exchange.getContext().getClassResolver(), configuration);
+                }
+            }
+        } catch (Exception e) {
+            LOG.warn("Error rendering OpenApi API due " + e.getMessage(), e);
+        }
+    }
+
+}
---
+++ components/camel-openapi-java/src/test/java/org/apache/camel/openapi/RestOpenApiReaderModelBookOrderTest.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.openapi;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.engine.DefaultClassResolver;
+import org.apache.camel.model.rest.RestParamType;
+import org.apache.camel.openapi.BeanConfig;
+import org.apache.camel.openapi.RestOpenApiReader;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import io.apicurio.datamodels.Library;
+import io.apicurio.datamodels.openapi.models.OasDocument;
+
+
+public class RestOpenApiReaderModelBookOrderTest extends CamelTestSupport {
+
+    @BindToRegistry("dummy-rest")
+    private DummyRestConsumerFactory factory = new DummyRestConsumerFactory();
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // this user REST service is json only
+                rest("/books").tag("dude").description("Book order service").consumes("application/json").produces("application/json")
+
+                    .get("/{id}").description("Find order by id").outType(BookOrder.class).responseMessage().message("The order returned").endResponseMessage().param().name("id")
+                    .type(RestParamType.path).description("The id of the order to get").dataType("integer").endParam().to("bean:bookService?method=getOrder(${header.id})")
+                    .get("/books/{id}/line/{lineNum}").outType(LineItem.class).to("bean:bookService?method=getOrder(${header.id})");
+            }
+        };
+    }
+
+    @Test
+    public void testReaderRead() throws Exception {
+        BeanConfig config = new BeanConfig();
+        config.setHost("localhost:8080");
+        config.setSchemes(new String[] {"http"});
+        config.setBasePath("/api");
+        config.setTitle("Camel User store");
+        config.setLicense("Apache 2.0");
+        config.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+        config.setVersion("2.0");
+        RestOpenApiReader reader = new RestOpenApiReader();
+
+        OasDocument openApi = reader.read(context.getRestDefinitions(), null, config, context.getName(), new DefaultClassResolver());
+        assertNotNull(openApi);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Object dump = Library.writeNode(openApi);
+        String json = mapper.writeValueAsString(dump);
+        System.out.println("the json is =====>" + json);
+
+        log.info(json);
+
+        assertTrue(json.contains("\"host\" : \"localhost:8080\""));
+        assertTrue(json.contains("\"description\" : \"The order returned\""));
+        assertTrue(json.contains("\"BookOrder\""));
+        assertTrue(json.contains("\"LineItem\""));
+        assertTrue(json.contains("\"$ref\" : \"#/definitions/BookOrder\""));
+        assertTrue(json.contains("\"$ref\" : \"#/definitions/LineItem\""));
+        assertTrue(json.contains("\"x-className\""));
+        assertTrue(json.contains("\"format\" : \"org.apache.camel.openapi.BookOrder\""));
+        assertTrue(json.contains("\"format\" : \"org.apache.camel.openapi.LineItem\""));
+
+        context.stop();
+    }
+    
+    @Test
+    public void testReaderReadV3() throws Exception {
+        BeanConfig config = new BeanConfig();
+        config.setHost("localhost:8080");
+        config.setSchemes(new String[] {"http"});
+        config.setBasePath("/api");
+        config.setTitle("Camel User store");
+        config.setLicense("Apache 2.0");
+        config.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
+        RestOpenApiReader reader = new RestOpenApiReader();
+
+        OasDocument openApi = reader.read(context.getRestDefinitions(), null, config, context.getName(), new DefaultClassResolver());
+        assertNotNull(openApi);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        Object dump = Library.writeNode(openApi);
+        String json = mapper.writeValueAsString(dump);
+        System.out.println("the json is =====>" + json);
+
+        log.info(json);
+
+        assertTrue(json.contains("\"url\" : \"http://localhost:8080/api\""));
+        assertTrue(json.contains("\"description\" : \"The order returned\""));
+        assertTrue(json.contains("\"BookOrder\""));
+        assertTrue(json.contains("\"LineItem\""));
+        assertTrue(json.contains("\"$ref\" : \"#/components/schemas/BookOrder"));
+        assertTrue(json.contains("\"$ref\" : \"#/components/schemas/LineItem"));
+        assertTrue(json.contains("\"x-className\""));
+        assertTrue(json.contains("\"format\" : \"org.apache.camel.openapi.BookOrder\""));
+        assertTrue(json.contains("\"format\" : \"org.apache.camel.openapi.LineItem\""));
+
+        context.stop();
+    }
+
+}
