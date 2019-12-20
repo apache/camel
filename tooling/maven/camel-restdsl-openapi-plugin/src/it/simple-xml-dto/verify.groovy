3b286b1b957692dc75dfb60b1fac8622f5d5bb26
Freeman Fang <freeman.fang@gmail.com>
2019-12-20 13:53:33 -0500

    [CAMEL-14315]add camel-restdsl-openapi-plugin which uses openapi-rest-dsl-generator

---
+++ tooling/maven/camel-restdsl-openapi-plugin/src/it/simple-xml-dto/verify.groovy
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+def File restdsl = new File(basedir, "target/generated-sources/restdsl-openapi/camel-rest.xml")
+
+assert restdsl.exists()
+
+def String data = restdsl.text
+
+assert data.contains('<restConfiguration component="jetty"/>')
+
+def File restdto = new File(basedir, "target/generated-sources/openapi/src/main/java/io/swagger/client/model/Order.java")
+
+assert restdto.exists()
