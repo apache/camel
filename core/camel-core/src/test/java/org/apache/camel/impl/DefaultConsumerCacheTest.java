d0b68292cb6d58ad204079e8ff4f1e6976a385d4
Claus Ibsen <claus.ibsen@gmail.com>
2020-01-02 19:40:59 +0100

    CAMEL-14340: Improve tests

--- core/camel-core/src/test/java/org/apache/camel/impl/DefaultConsumerCacheTest.java
+++ core/camel-core/src/test/java/org/apache/camel/impl/DefaultConsumerCacheTest.java
@@ -42,11 +42,11 @@ public class DefaultConsumerCacheTest extends ContextTestSupport {
             assertNotNull("the polling consumer should not be null", p);
         }
 
-        // the eviction is async so force cleanup
-        cache.cleanUp();
-
-        await().atMost(3, TimeUnit.SECONDS).until(() -> cache.size() == 1000);
-        assertEquals("Size should be 1000", 1000, cache.size());
+        await().atMost(3, TimeUnit.SECONDS).untilAsserted(() -> {
+            // the eviction is async so force cleanup
+            cache.cleanUp();
+            assertEquals("Size should be 1000", 1000, cache.size());
+        });
 
         cache.stop();
     }
