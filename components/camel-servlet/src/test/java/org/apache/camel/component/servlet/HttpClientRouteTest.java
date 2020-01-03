2f8a4e7d144b413ab7fd08f5d5a31a881c5aee7a
Claus Ibsen <claus.ibsen@gmail.com>
2020-01-03 05:35:05 +0100

    CAMEL-14345: camel-core - Optimize SendProcessor to not use ProducerCache when not needed

--- components/camel-servlet/src/test/java/org/apache/camel/component/servlet/HttpClientRouteTest.java
+++ components/camel-servlet/src/test/java/org/apache/camel/component/servlet/HttpClientRouteTest.java
@@ -29,6 +29,7 @@ import com.meterware.httpunit.WebResponse;
 import com.meterware.servletunit.ServletUnitClient;
 import org.apache.camel.Exchange;
 import org.apache.camel.FailedToCreateProducerException;
+import org.apache.camel.FailedToStartRouteException;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.junit.Test;
@@ -123,7 +124,7 @@ public class HttpClientRouteTest extends ServletCamelRouterTestSupport {
             });
             fail("Excepts exception here");
         } catch (Exception ex) {
-            assertTrue("Get a wrong exception.", ex.getCause() instanceof FailedToCreateProducerException);
+            assertTrue("Get a wrong exception.", ex instanceof FailedToStartRouteException);
             assertTrue("Get a wrong cause of exception.", ex.getCause().getCause() instanceof UnsupportedOperationException);
         }
     }
