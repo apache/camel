98d7d5d30549a637e2d1090a42d005dcae370d15
Andrea Cosentino <ancosen@gmail.com>
2020-01-04 16:56:00 +0100

    CAMEL-14020 - Migrate deprecate getOut to getMessage in tests, camel-aws-translate

--- components/camel-aws-translate/src/main/java/org/apache/camel/component/aws/translate/TranslateProducer.java
+++ components/camel-aws-translate/src/main/java/org/apache/camel/component/aws/translate/TranslateProducer.java
@@ -123,11 +123,6 @@ public class TranslateProducer extends DefaultProducer {
     }
 
     public static Message getMessageForResponse(final Exchange exchange) {
-        if (exchange.getPattern().isOutCapable()) {
-            Message out = exchange.getOut();
-            out.copyFrom(exchange.getIn());
-            return out;
-        }
-        return exchange.getIn();
+        return exchange.getMessage();
     }
 }
