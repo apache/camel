8be3c0db3b11e297415b7102bf5c0df5724e5e15
Andrea Cosentino <ancosen@gmail.com>
2020-01-05 18:03:56 +0100

    CAMEL-14020 - Migrate deprecate getOut to getMessage in tests, camel-aws-eks

--- components/camel-aws-eks/src/main/java/org/apache/camel/component/aws/eks/EKSProducer.java
+++ components/camel-aws-eks/src/main/java/org/apache/camel/component/aws/eks/EKSProducer.java
@@ -172,11 +172,6 @@ public class EKSProducer extends DefaultProducer {
     }
     
     public static Message getMessageForResponse(final Exchange exchange) {
-        if (exchange.getPattern().isOutCapable()) {
-            Message out = exchange.getOut();
-            out.copyFrom(exchange.getIn());
-            return out;
-        }
-        return exchange.getIn();
+        return exchange.getMessage();
     }
 }
