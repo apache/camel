eec2bdba27c7f92497ac61444aa13ab9ea0d4ef3
Andrea Cosentino <ancosen@gmail.com>
2020-01-05 18:32:22 +0100

    CAMEL-14020 - Migrate deprecate getOut to getMessage in tests, camel-aws-kinesis

--- components/camel-aws-kinesis/src/main/java/org/apache/camel/component/aws/kinesis/KinesisProducer.java
+++ components/camel-aws-kinesis/src/main/java/org/apache/camel/component/aws/kinesis/KinesisProducer.java
@@ -60,11 +60,6 @@ public class KinesisProducer extends DefaultProducer {
     }
     
     public static Message getMessageForResponse(final Exchange exchange) {
-        if (exchange.getPattern().isOutCapable()) {
-            Message out = exchange.getOut();
-            out.copyFrom(exchange.getIn());
-            return out;
-        }
-        return exchange.getIn();
+        return exchange.getMessage();
     }
 }
