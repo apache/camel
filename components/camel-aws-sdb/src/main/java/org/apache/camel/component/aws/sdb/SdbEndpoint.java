bddf8923c82ab74a5e54646d4776b6d07a503ad6
Andrea Cosentino <ancosen@gmail.com>
2020-01-07 15:36:19 +0100

    CAMEL-14020 - Migrate deprecate getOut to getMessage in tests, camel-aws-sdb

--- components/camel-aws-sdb/src/main/java/org/apache/camel/component/aws/sdb/SdbEndpoint.java
+++ components/camel-aws-sdb/src/main/java/org/apache/camel/component/aws/sdb/SdbEndpoint.java
@@ -132,11 +132,6 @@ public class SdbEndpoint extends ScheduledPollEndpoint {
     }
     
     public static Message getMessageForResponse(final Exchange exchange) {
-        if (exchange.getPattern().isOutCapable()) {
-            Message out = exchange.getOut();
-            out.copyFrom(exchange.getIn());
-            return out;
-        }
-        return exchange.getIn();
+        return exchange.getMessage();
     }
 }
--- components/camel-aws-sdb/src/main/java/org/apache/camel/component/aws/sdb/SelectCommand.java
+++ components/camel-aws-sdb/src/main/java/org/apache/camel/component/aws/sdb/SelectCommand.java
@@ -50,11 +50,6 @@ public class SelectCommand extends AbstractSdbCommand {
     }
     
     public static Message getMessageForResponse(final Exchange exchange) {
-        if (exchange.getPattern().isOutCapable()) {
-            Message out = exchange.getOut();
-            out.copyFrom(exchange.getIn());
-            return out;
-        }
-        return exchange.getIn();
+        return exchange.getMessage();
     }
 }
