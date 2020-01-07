e448ef95258da3577efb35ddacbf1277f69ec21d
Andrea Cosentino <ancosen@gmail.com>
2020-01-07 15:40:28 +0100

    CAMEL-14020 - Migrate deprecate getOut to getMessage in tests, camel-aws-sns

--- components/camel-aws-sns/src/main/java/org/apache/camel/component/aws/sns/SnsProducer.java
+++ components/camel-aws-sns/src/main/java/org/apache/camel/component/aws/sns/SnsProducer.java
@@ -142,11 +142,6 @@ public class SnsProducer extends DefaultProducer {
     }
 
     public static Message getMessageForResponse(final Exchange exchange) {
-        if (exchange.getPattern().isOutCapable()) {
-            Message out = exchange.getOut();
-            out.copyFrom(exchange.getIn());
-            return out;
-        }
-        return exchange.getIn();
+        return exchange.getMessage();
     }
 }
