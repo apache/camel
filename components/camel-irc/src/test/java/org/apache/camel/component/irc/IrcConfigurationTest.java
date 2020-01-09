741b4bec090dd04bda31e5c2aad53ecd20f8c43e
JiriOndrusek <jondruse@redhat.com>
2020-01-09 10:26:22 +0100

    [CAMEL-14374] Camel-irc validation of endpoint's url fails for correct url (#3467)

--- components/camel-irc/src/test/java/org/apache/camel/component/irc/IrcConfigurationTest.java
+++ components/camel-irc/src/test/java/org/apache/camel/component/irc/IrcConfigurationTest.java
@@ -26,100 +26,6 @@ import org.junit.Test;
 public class IrcConfigurationTest extends CamelTestSupport {
 
     @Test
-    public void testInvalidUriConversion() throws Exception {
-        // Note: valid URIs won't throw on new URI(endpoint.getEndpointUri())
-        
-        String deprecate;
-        String sanitized;
-        Endpoint endpoint;
-        IrcComponent component = context.getComponent("irc", IrcComponent.class);
-
-        // Test conversion of the URI path to @channel parameter (drop the '#')
-        deprecate = "irc://camelbot@irc.freenode.net:1234/#camel";
-        sanitized = "irc://camelbot@irc.freenode.net:1234?channel=camel";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test conversion of the URI path to @channel parameter (encode the double '##')
-        deprecate = "irc://camelbot@irc.freenode.net/##camel";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=%23%23camel";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test drop path and both path and @channels are specified
-        deprecate = "irc://camelbot@irc.freenode.net/#karaf?channels=#camel,#cxf";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=camel&channel=cxf";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test multiple channels, no keys
-        deprecate = "irc://camelbot@irc.freenode.net?channels=#camel,#cxf";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=camel&channel=cxf";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test multiple channels, with keys
-        deprecate = "irc://camelbot@irc.freenode.net?channels=#camel,#cxf&keys=foo,bar";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=camel!foo&channel=cxf!bar";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test multiple channels, with keys (last key is empty)
-        deprecate = "irc://camelbot@irc.freenode.net?channels=#camel,#cxf&keys=foo,";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=camel!foo&channel=cxf";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test multiple channels, deprecated @username
-        deprecate = "irc://irc.freenode.net?keys=,foo&channels=#camel,#cxf&username=camelbot";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=camel&channel=cxf!foo";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test multiple channels, deprecated @username and @password
-        deprecate = "irc://irc.freenode.net?keys=,foo&channels=#camel,#cxf&username=camelbot&password=secret";
-        sanitized = "irc://camelbot:secret@irc.freenode.net?channel=camel&channel=cxf!foo";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test multiple channels, drop @nickname same as @username
-        deprecate = "irc://irc.freenode.net?channels=#camel,#cxf&nickname=camelbot";
-        sanitized = "irc://camelbot@irc.freenode.net?channel=camel&channel=cxf";
-        endpoint = component.createEndpoint(deprecate); 
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-
-        // Test with encoding of @realname
-        deprecate = "irc://user@irc.freenode.net?keys=foo,&channels=#camel,#cxf&realname=Camel Bot&username=user&nickname=camelbot";
-        sanitized = "irc://user@irc.freenode.net?realname=Camel%20Bot&nickname=camelbot&channel=camel!foo&channel=cxf";
-        endpoint = component.createEndpoint(deprecate);
-        assertEquals(sanitized, endpoint.getEndpointUri());
-        assertNotNull(new URI(endpoint.getEndpointUri()));
-    }
-
-    @Test
-    public void testConfigureFormat1() throws Exception {
-        IrcComponent component = context.getComponent("irc", IrcComponent.class);
-
-        // irc:nick@host[:port]/#room[?options]
-        IrcEndpoint endpoint = (IrcEndpoint) component.createEndpoint("irc://camelbot@irc.freenode.net/#camel");
-        IrcConfiguration conf = endpoint.getConfiguration();
-        assertEquals("camelbot", conf.getNickname());
-        assertEquals("irc.freenode.net", conf.getHostname());
-        List<IrcChannel> channels = conf.getChannels();
-        assertEquals(1, channels.size());
-        assertEquals("#camel", channels.get(0).getName());
-    }
-
-    @Test
     public void testConfigureFormat2() throws Exception {
         IrcComponent component = context.getComponent("irc", IrcComponent.class);
 
@@ -129,7 +35,7 @@ public class IrcConfigurationTest extends CamelTestSupport {
         IrcConfiguration conf = endpoint.getConfiguration();
         assertEquals("camelbot", conf.getNickname());
         assertEquals("irc.freenode.net", conf.getHostname());
-        List<IrcChannel> channels = conf.getChannels();
+        List<IrcChannel> channels = conf.getChannelList();
         assertEquals(1, channels.size());
         assertEquals("#camel", channels.get(0).getName());
     }
@@ -144,7 +50,7 @@ public class IrcConfigurationTest extends CamelTestSupport {
         IrcConfiguration conf = endpoint.getConfiguration();
         assertEquals("camelbot", conf.getNickname());
         assertEquals("irc.freenode.net", conf.getHostname());
-        List<IrcChannel> channels = conf.getChannels();
+        List<IrcChannel> channels = conf.getChannelList();
         assertEquals(1, channels.size());
         assertEquals("#camel", channels.get(0).getName());
     }
@@ -159,7 +65,7 @@ public class IrcConfigurationTest extends CamelTestSupport {
         IrcConfiguration conf = endpoint.getConfiguration();
         assertEquals("camelbot", conf.getNickname());
         assertEquals("irc.freenode.net", conf.getHostname());
-        List<IrcChannel> channels = conf.getChannels();
+        List<IrcChannel> channels = conf.getChannelList();
         assertEquals(2, channels.size());
         assertNotNull(conf.findChannel("#camel"));
         assertNotNull(conf.findChannel("#smx"));
@@ -177,7 +83,7 @@ public class IrcConfigurationTest extends CamelTestSupport {
         IrcConfiguration conf = endpoint.getConfiguration();
         assertEquals("camelbot", conf.getNickname());
         assertEquals("irc.freenode.net", conf.getHostname());
-        List<IrcChannel> channels = conf.getChannels();
+        List<IrcChannel> channels = conf.getChannelList();
         assertEquals(2, channels.size());
         assertNotNull(conf.findChannel("#camel"));
         assertEquals("foo", conf.findChannel("#camel").getKey());
@@ -195,7 +101,7 @@ public class IrcConfigurationTest extends CamelTestSupport {
         IrcConfiguration conf = endpoint.getConfiguration();
         assertEquals("camelbot", conf.getNickname());
         assertEquals("irc.freenode.net", conf.getHostname());
-        List<IrcChannel> channels = conf.getChannels();
+        List<IrcChannel> channels = conf.getChannelList();
         assertEquals(2, channels.size());
         assertNotNull(conf.findChannel("#camel"));
         assertNotNull(conf.findChannel("#smx"));
