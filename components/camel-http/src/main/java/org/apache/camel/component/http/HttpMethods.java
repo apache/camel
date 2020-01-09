3e805277dfafbb9415e34f7b31f6846a28a68e96
Amos Feng <zfeng@redhat.com>
2020-01-09 15:39:10 +0800

    CAMEL-14361 Update to avoid reflection in creating http method (#3469)

--- components/camel-http/src/main/java/org/apache/camel/component/http/HttpMethods.java
+++ components/camel-http/src/main/java/org/apache/camel/component/http/HttpMethods.java
@@ -51,8 +51,27 @@ public enum HttpMethods implements Expression {
 
     public HttpRequestBase createMethod(final String url) {
         try {
-            return clazz.getDeclaredConstructor(String.class).newInstance(url);
-        } catch (Exception e) {
+            switch (this) {
+            case GET:
+                return new HttpGet(url);
+            case PATCH:
+                return new HttpPatch(url);
+            case POST:
+                return new HttpPost(url);
+            case PUT:
+                return new HttpPut(url);
+            case DELETE:
+                return new HttpDelete(url);
+            case HEAD:
+                return new HttpHead(url);
+            case OPTIONS:
+                return new HttpOptions(url);
+            case TRACE:
+                return new HttpTrace(url);
+            default:
+                throw new RuntimeException("no such method " + this);
+            }
+        } catch (IllegalArgumentException e) {
             throw new RuntimeException(e);
         }
     }
