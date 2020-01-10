247cf9cc9a12a1fa584f61dba6f7e1c05a0a5816
Nicola Ferraro <ni.ferraro@gmail.com>
2020-01-10 13:30:38 +0100

    CAMEL-14385: add more tests

--- components/camel-cron/src/test/java/org/apache/camel/component/cron/CronLoaderTest.java
+++ components/camel-cron/src/test/java/org/apache/camel/component/cron/CronLoaderTest.java
@@ -16,8 +16,8 @@
  */
 package org.apache.camel.component.cron;
 
-import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cron.api.CamelCronService;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
@@ -26,21 +26,49 @@ public class CronLoaderTest extends CamelTestSupport {
 
     @Test
     public void testDummyCronServiceLoading() throws Exception {
+        configureRoutes();
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedBodiesReceived("x");
+
+        context.start();
         mock.assertIsSatisfied();
     }
 
-    @Override
-    protected RoutesBuilder createRouteBuilder() throws Exception {
-        return new RouteBuilder() {
+    @Test
+    public void testPreferRegistryOverServiceLoading() throws Exception {
+        context.getRegistry().bind("dummy2", new DummyCamelCronService("dummy2"));
+        configureRoutes();
+        context.start();
+        assertEquals("dummy2", getCamelCronService().getId());
+    }
+
+    @Test
+    public void testUseNamesWhenLoading() throws Exception {
+        context.getRegistry().bind("dummy2", new DummyCamelCronService("dummy2"));
+        context.getRegistry().bind("dummy3", new DummyCamelCronService("dummy3"));
+        configureRoutes();
+        context.getComponent("cron", CronComponent.class).setCronService("dummy3");
+        context.start();
+        assertEquals("dummy3", getCamelCronService().getId());
+    }
+
+    private CamelCronService getCamelCronService() {
+        return context.getComponent("cron", CronComponent.class).getService();
+    }
+
+    private void configureRoutes() throws Exception {
+        context.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
                 from("cron:tab?schedule=0/1 * * * * ?")
                         .setBody().constant("x")
                         .to("mock:result");
-
             }
-        };
+        });
+    }
+
+    @Override
+    public boolean isUseRouteBuilder() {
+        return false;
     }
 }
