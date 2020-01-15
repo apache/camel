060ade064f75c9655f889119287dd566c8d85509
Omar Al-Safi <omarsmak@gmail.com>
2020-01-15 17:41:13 +0100

    CAMEL-13807: First design of the component DSL APIs

---
+++ core/camel-componentdsl/src/test/java/org/apache/camel/builder/component/dsl/DebeziumMySqlComponentBuilderFactoryTest.java
@@ -0,0 +1,14 @@
+package org.apache.camel.builder.component.dsl;
+
+import org.apache.camel.builder.component.ComponentBuilder;
+import org.apache.camel.builder.component.ComponentBuilderFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DebeziumMySqlComponentBuilderFactoryTest {
+    @Test
+    public void test() {
+        ComponentBuilderFactory.debeziumMysql();
+    }
+}
