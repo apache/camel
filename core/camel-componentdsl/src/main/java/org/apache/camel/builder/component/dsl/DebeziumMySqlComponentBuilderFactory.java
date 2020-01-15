f28b64733de453d3649259ebd8f469b29febcb58
Omar Al-Safi <omarsmak@gmail.com>
2020-01-15 17:41:13 +0100

    CAMEL-13807: First design of the component DSL APIs

---
+++ core/camel-componentdsl/src/main/java/org/apache/camel/builder/component/dsl/DebeziumMySqlComponentBuilderFactory.java
@@ -0,0 +1,38 @@
+package org.apache.camel.builder.component.dsl;
+
+import org.apache.camel.Component;
+import org.apache.camel.builder.component.AbstractComponentBuilder;
+import org.apache.camel.builder.component.ComponentBuilder;
+import org.apache.camel.component.debezium.DebeziumMySqlComponent;
+import org.apache.camel.component.debezium.configuration.MySqlConnectorEmbeddedDebeziumConfiguration;
+
+public class DebeziumMySqlComponentBuilderFactory {
+    interface DebeziumMySqlComponentBuilder extends ComponentBuilder {
+        default DebeziumMySqlComponentBuilder withComponentName(String name) {
+            doSetComponentName(name);
+            return this;
+        }
+        default DebeziumMySqlComponentBuilder setConfiguration(MySqlConnectorEmbeddedDebeziumConfiguration configuration) {
+            doSetProperty("configuration", configuration);
+            return this;
+        }
+
+        default DebeziumMySqlComponentBuilder setBasicPropertyBinding(boolean basicPropertyBinding) {
+            doSetProperty("basicPropertyBinding", basicPropertyBinding);
+            return this;
+        }
+    }
+
+    public static DebeziumMySqlComponentBuilder debeziumMysql() {
+        class DebeziumMySqlComponentBuilderImpl extends AbstractComponentBuilder implements DebeziumMySqlComponentBuilder {
+            public DebeziumMySqlComponentBuilderImpl() {
+                super("debezium-mysql");
+            }
+            @Override
+            protected Component buildConcreteComponent() {
+                return new DebeziumMySqlComponent();
+            }
+        }
+        return new DebeziumMySqlComponentBuilderImpl();
+    }
+}
