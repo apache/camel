a88acc93070e70d353f9feffd4c9feafa035fcbe
Omar Al-Safi <omarsmak@gmail.com>
2020-01-16 10:58:18 +0100

    CAMEL-13807: Progress on the component builder

---
+++ core/camel-componentdsl/src/main/java/org/apache/camel/builder/component/AbstractComponentBuilder.java
@@ -0,0 +1,69 @@
+package org.apache.camel.builder.component;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.spi.GeneratedPropertyConfigurer;
+
+public abstract class AbstractComponentBuilder {
+    private String name;
+    private final Map<String, Object> properties = new LinkedHashMap<>();
+
+    protected AbstractComponentBuilder(final String name) {
+        this.name = name;
+    }
+
+    public Component build() {
+        final Component component = buildConcreteComponent();
+        component.init();
+
+        final GeneratedPropertyConfigurer propertyConfigurer = (GeneratedPropertyConfigurer) component.getComponentPropertyConfigurer();
+
+        configureComponentProperties(component, propertyConfigurer);
+
+        return component;
+    }
+
+    public Component build(final CamelContext context) {
+        final Component component = buildConcreteComponent();
+        component.setCamelContext(context);
+        component.init();
+
+        final GeneratedPropertyConfigurer propertyConfigurer = (GeneratedPropertyConfigurer) component.getComponentPropertyConfigurer();
+
+        configureComponentProperties(component, propertyConfigurer);
+
+        return component;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void doSetProperty(final String key, final Object value) {
+        properties.put(key, value);
+    }
+
+    public void doSetComponentName(final String name) {
+        this.name = name;
+    }
+
+    protected abstract Component buildConcreteComponent();
+
+    private void configureComponentProperties(final Component component, final GeneratedPropertyConfigurer propertyConfigurer) {
+        final CamelContext context = component.getCamelContext();
+        properties.forEach((key, value) -> {
+            propertyConfigurer.configure(context, component, key, resolvePropertyValue(value, context), false);
+        });
+    }
+
+    private Object resolvePropertyValue(final Object value, final CamelContext context) {
+        if (value instanceof String) {
+            return context.resolvePropertyPlaceholders((String) value);
+        }
+        return value;
+    }
+}
+
