050e85c9dfcef409a6acec3eb7e51fb535e77fe3
Claus Ibsen <claus.ibsen@gmail.com>
2020-01-16 05:41:31 +0100

    Fixed CS

--- core/camel-support/src/main/java/org/apache/camel/support/EventHelper.java
+++ core/camel-support/src/main/java/org/apache/camel/support/EventHelper.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -33,7 +33,6 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Helper for easily sending event notifications in a single line of code
- *
  */
 public final class EventHelper {
 
