6a98caf11efc680a0d1ced5b47732270c3700928
Claus Ibsen <claus.ibsen@gmail.com>
2020-01-16 06:06:19 +0100

    Fixed CS

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/ModelXmlParserGeneratorMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/ModelXmlParserGeneratorMojo.java
@@ -142,22 +142,28 @@ public class ModelXmlParserGeneratorMojo extends AbstractGeneratorMojo {
                 .collect(Collectors.toList());
 
         JavaClass parser = generateParser(model, classLoader);
-        return  "/*\n" +
-                " * Licensed to the Apache Software Foundation (ASF) under one or more\n" +
-                " * contributor license agreements.  See the NOTICE file distributed with\n" +
-                " * this work for additional information regarding copyright ownership.\n" +
-                " * The ASF licenses this file to You under the Apache License, Version 2.0\n" +
-                " * (the \"License\"); you may not use this file except in compliance with\n" +
-                " * the License.  You may obtain a copy of the License at\n" +
-                " *\n" +
-                " *      http://www.apache.org/licenses/LICENSE-2.0\n" +
-                " *\n" +
-                " * Unless required by applicable law or agreed to in writing, software\n" +
-                " * distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                " * See the License for the specific language governing permissions and\n" +
-                " * limitations under the License.\n" +
-                " */\n" + parser.printClass();
+        return  "/*\n"
+                + " * Licensed to the Apache Software Foundation (ASF) under one or more\n"
+                + " * contributor license agreements.  See the NOTICE file distributed with\n"
+                + " * this work for additional information regarding copyright ownership.\n"
+                + " * The ASF licenses this file to You under the Apache License, Version 2.0\n"
+                + " * (the \"License\"); you may not use this file except in compliance with\n"
+                + " * the License.  You may obtain a copy of the License at\n"
+                + " *\n"
+                + " *      http://www.apache.org/licenses/LICENSE-2.0\n"
+                + " *\n"
+                + " * Unless required by applicable law or agreed to in writing, software\n"
+                + " * distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+                + " * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+                + " * See the License for the specific language governing permissions and\n"
+                + " * limitations under the License.\n"
+                + " */\n"
+                + "\n"
+                + "//CHECKSTYLE:OFF\n"
+                + "\n"
+                + parser.printClass()
+                + "\n"
+                + "//CHECKSTYLE:ON\n";
     }
 
     protected Class<?> loadClass(ClassLoader loader, String name) {
