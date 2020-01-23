c733bd71124b6f0e30a1394c80745928dc3f4d11
Claus Ibsen <claus.ibsen@gmail.com>
2020-01-23 09:57:23 +0100

    CAMEL-14414: Polished

--- core/camel-base/src/main/java/org/apache/camel/processor/aggregate/AggregateProcessor.java
+++ core/camel-base/src/main/java/org/apache/camel/processor/aggregate/AggregateProcessor.java
@@ -434,14 +434,12 @@ public class AggregateProcessor extends AsyncProcessorSupport implements Navigat
     private Object removeFlagCompleteAllGroups(Exchange exchange) {
         Object removedHeader = exchange.getIn().removeHeader(Exchange.AGGREGATION_COMPLETE_ALL_GROUPS);
         Object removedProp = exchange.removeProperty(Exchange.AGGREGATION_COMPLETE_ALL_GROUPS);
-
         return removedHeader == null ? removedProp: removedHeader;
     }
 
     private Boolean isCompleteAllGroups(Exchange exchange) {
-        Boolean retVal;
-        retVal = exchange.getIn().getHeader(Exchange.AGGREGATION_COMPLETE_ALL_GROUPS, false, boolean.class);
-        if(!Boolean.TRUE.equals(retVal)) {
+        boolean retVal = exchange.getIn().getHeader(Exchange.AGGREGATION_COMPLETE_ALL_GROUPS, false, boolean.class);
+        if (!retVal) {
             // according to doc it is a property but it is sometimes read as header
             // some test don't fail because they use the header expression which contains a fallback to properties
             retVal = exchange.getProperty(Exchange.AGGREGATION_COMPLETE_ALL_GROUPS, false, boolean.class);
