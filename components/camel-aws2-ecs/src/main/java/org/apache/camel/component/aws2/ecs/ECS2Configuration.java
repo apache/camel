fda76b3788f2a790088c98f3d7d9f2ce2c9ba3ba
Andrea Cosentino <ancosen@gmail.com>
2020-01-27 16:17:34 +0100

    Camel-AWS2-ECS: Fixed Region description

--- components/camel-aws2-ecs/src/main/java/org/apache/camel/component/aws2/ecs/ECS2Configuration.java
+++ components/camel-aws2-ecs/src/main/java/org/apache/camel/component/aws2/ecs/ECS2Configuration.java
@@ -130,7 +130,7 @@ public class ECS2Configuration implements Cloneable {
     }
 
     /**
-     * The region in which Translate client needs to work. When using this
+     * The region in which ECS client needs to work. When using this
      * parameter, the configuration will expect the lowercase name of the
      * region (for example ap-east-1) You'll need to use the name
      * Region.EU_WEST_1.id()
