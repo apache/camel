8ee9a2cbe48fa0a8b5f4f035e96224efd0abbb12
Andrea Cosentino <ancosen@gmail.com>
2020-01-31 14:48:29 +0100

    Camel-Workday: Fixed the structure a bit, component and endpoint scheme should be the same

--- components/camel-workday/src/main/java/org/apache/camel/component/workday/WorkdayComponent.java
+++ components/camel-workday/src/main/java/org/apache/camel/component/workday/WorkdayComponent.java
@@ -33,8 +33,8 @@ public class WorkdayComponent extends DefaultComponent {
     public static final String RAAS_ENDPOINT_URL = "https://%s/ccx/service/customreport2/%s%s";
 
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
-
-        WorkdayEndpoint endpoint = new WorkdayEndpoint(uri, this);
+        WorkdayConfiguration configuration = new WorkdayConfiguration();
+        WorkdayEndpoint endpoint = new WorkdayEndpoint(uri, this, configuration);
         setProperties(endpoint, parameters);
 
         return endpoint;
