c13ce92cd65356066647fb68909cb012dae74452
Andrea Cosentino <ancosen@gmail.com>
2020-01-31 10:20:32 +0100

    Camel-Atom: Fixed CS

--- components/camel-atom/src/main/java/org/apache/camel/component/feed/FeedEndpoint.java
+++ components/camel-atom/src/main/java/org/apache/camel/component/feed/FeedEndpoint.java
@@ -35,11 +35,11 @@ public abstract class FeedEndpoint extends DefaultPollingEndpoint {
 
     @UriPath(description = "The URI to the feed to poll.") @Metadata(required = true)
     protected String feedUri;
-    @UriParam(defaultValue = "true", description = "Sets whether or not entries should be sent " +
-            "individually or whether the entire feed should be sent as a single message")
+    @UriParam(defaultValue = "true", description = "Sets whether or not entries should be sent " 
+            + "individually or whether the entire feed should be sent as a single message")
     protected boolean splitEntries = true;
-    @UriParam(description = "Sets the timestamp to be used for filtering entries from the " +
-            "atom feeds. This options is only in conjunction with the splitEntries.")
+    @UriParam(description = "Sets the timestamp to be used for filtering entries from the " 
+            + "atom feeds. This options is only in conjunction with the splitEntries.")
     protected Date lastUpdate;
     @UriParam(defaultValue = "true", description = "Sets whether to use filtering or not of the entries.")
     protected boolean filter = true;
@@ -47,9 +47,9 @@ public abstract class FeedEndpoint extends DefaultPollingEndpoint {
     private boolean feedHeader = true;
     @UriParam(description = "Sets whether to sort entries by published date. Only works when splitEntries = true.")
     private boolean sortEntries;
-    @UriParam(defaultValue = "true", description = "Sets whether all entries identified in a " +
-            "single feed poll should be delivered immediately. If true, only one entry is processed " +
-            "per consumer.delay. Only applicable when splitEntries = true.")
+    @UriParam(defaultValue = "true", description = "Sets whether all entries identified in a " 
+            + "single feed poll should be delivered immediately. If true, only one entry is processed " 
+            + "per consumer.delay. Only applicable when splitEntries = true.")
     private boolean throttleEntries = true;
     @UriParam(description = "Sets the username to be used for basic authentication when polling from a HTTP feed.")
     private String username;
