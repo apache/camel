cf4f5ae6a798dd86c93b9369f1246076db954a51
Thomas Diesler <tdiesler@redhat.com>
2020-02-03 16:29:40 +0100

    [CAMEL-14468] Add support for classification in camel-weka (#3536)

--- components/camel-weka/src/main/java/org/apache/camel/component/weka/WekaTypeConverters.java
+++ components/camel-weka/src/main/java/org/apache/camel/component/weka/WekaTypeConverters.java
@@ -17,18 +17,21 @@
 package org.apache.camel.component.weka;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
 
 import io.nessus.weka.Dataset;
 import org.apache.camel.Converter;
 import weka.core.Instances;
 
-@Converter
+@Converter(generateLoader = true)
 public final class WekaTypeConverters {
-    
+
     private WekaTypeConverters() {
     }
-
+    
     @Converter
     public static InputStream toInputStream(Dataset dataset) {
         Instances instances = dataset.getInstances();
@@ -39,5 +42,30 @@ public final class WekaTypeConverters {
     public static InputStream toInputStream(Instances instances) {
         byte[] bytes = instances.toString().getBytes();
         return new ByteArrayInputStream(bytes);
+    }
+
+    @Converter
+    public static Dataset toDataset(Instances instances) {
+        return Dataset.create(instances);
+    }
+
+    @Converter
+    public static Dataset toDataset(InputStream input) {
+        return Dataset.create(input);
+    }
+
+    @Converter
+    public static Dataset toDataset(File infile) {
+        return Dataset.create(infile.toPath());
+    }
+
+    @Converter
+    public static Dataset toDataset(Path inpath) {
+        return Dataset.create(inpath);
+    }
+
+    @Converter
+    public static Dataset toDataset(URL url) {
+        return Dataset.create(url);
     }
 }
