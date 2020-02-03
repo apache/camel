f925af13052606324822ebd699df7c410a22157c
Andrea Cosentino <ancosen@gmail.com>
2020-02-03 12:38:28 +0100

    CAMEL-14464 - Create an AWS-MKS component based on SDK v2, Region use updated

--- components/camel-aws2-msk/src/main/java/org/apache/camel/component/aws2/msk/MSK2Configuration.java
+++ components/camel-aws2-msk/src/main/java/org/apache/camel/component/aws2/msk/MSK2Configuration.java
@@ -131,8 +131,10 @@ public class MSK2Configuration implements Cloneable {
     }
 
     /**
-     * The region in which MSK client needs to work. When using this parameter, the configuration will expect the capitalized name of the region (for example AP_EAST_1)
-     * You'll need to use the name Regions.EU_WEST_1.name()
+     * The region in which MSK client needs to work. When using this
+     * parameter, the configuration will expect the lowercase name of the
+     * region (for example ap-east-1) You'll need to use the name
+     * Region.EU_WEST_1.id()
      */
     public void setRegion(String region) {
         this.region = region;
