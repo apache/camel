9ff08706c3fafd5823166b21c515ab439a5db409
Claus Ibsen <claus.ibsen@gmail.com>
2020-02-04 12:46:17 +0100

    CAMEL-14484: Make XML routes loader using JAXB into its own module

--- core/camel-core-engine/src/main/java/org/apache/camel/model/ModelHelper.java
+++ core/camel-core-engine/src/main/java/org/apache/camel/model/ModelHelper.java
@@ -17,7 +17,6 @@
 package org.apache.camel.model;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
@@ -32,7 +31,6 @@ import javax.xml.bind.Binder;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.TransformerException;
 
@@ -50,12 +48,10 @@ import org.apache.camel.NamedNode;
 import org.apache.camel.TypeConversionException;
 import org.apache.camel.converter.jaxp.XmlConverter;
 import org.apache.camel.model.language.ExpressionDefinition;
-import org.apache.camel.model.rest.RestDefinition;
 import org.apache.camel.model.rest.RestsDefinition;
 import org.apache.camel.spi.ModelJAXBContextFactory;
 import org.apache.camel.spi.NamespaceAware;
 import org.apache.camel.spi.TypeConverterRegistry;
-import org.apache.camel.util.ObjectHelper;
 import org.apache.camel.util.xml.XmlLineNumberParser;
 
 import static org.apache.camel.model.ProcessorDefinitionHelper.filterTypeInOutputs;
@@ -257,83 +253,17 @@ public final class ModelHelper {
      */
     @Deprecated
     public static RoutesDefinition loadRoutesDefinition(CamelContext context, InputStream inputStream) throws Exception {
-        XmlConverter xmlConverter = newXmlConverter(context);
-        Document dom = xmlConverter.toDOMDocument(inputStream, null);
-        return loadRoutesDefinition(context, dom);
-    }
-
-    /**
-     * Marshal the xml to the model definition
-     *
-     * @param context the CamelContext, if <tt>null</tt> then
-     *            {@link org.apache.camel.spi.ModelJAXBContextFactory} is not in
-     *            use
-     * @param node the xml node
-     * @throws Exception is thrown if an error is encountered unmarshalling from
-     *             xml to model
-     * @deprecated use {@link org.apache.camel.spi.XMLRoutesDefinitionLoader} from {@link ExtendedCamelContext}
-     */
-    @Deprecated
-    public static RoutesDefinition loadRoutesDefinition(CamelContext context, Node node) throws Exception {
-        JAXBContext jaxbContext = getJAXBContext(context);
-
-        Map<String, String> namespaces = new LinkedHashMap<>();
-
-        Document dom = node instanceof Document ? (Document)node : node.getOwnerDocument();
-        extractNamespaces(dom, namespaces);
-
-        Binder<Node> binder = jaxbContext.createBinder();
-        Object result = binder.unmarshal(node);
-
-        if (result == null) {
-            throw new JAXBException("Cannot unmarshal to RoutesDefinition using JAXB");
-        }
-
-        // can either be routes or a single route
-        RoutesDefinition answer;
-        if (result instanceof RouteDefinition) {
-            RouteDefinition route = (RouteDefinition)result;
-            answer = new RoutesDefinition();
-            applyNamespaces(route, namespaces);
-            answer.getRoutes().add(route);
-        } else if (result instanceof RoutesDefinition) {
-            answer = (RoutesDefinition)result;
-            for (RouteDefinition route : answer.getRoutes()) {
-                applyNamespaces(route, namespaces);
-            }
-        } else {
-            throw new IllegalArgumentException("Unmarshalled object is an unsupported type: " + ObjectHelper.className(result) + " -> " + result);
-        }
-
-        return answer;
+        ExtendedCamelContext ecc = context.adapt(ExtendedCamelContext.class);
+        return (RoutesDefinition) ecc.getXMLRoutesDefinitionLoader().loadRoutesDefinition(context, inputStream);
     }
 
     /**
      * @deprecated use {@link org.apache.camel.spi.XMLRoutesDefinitionLoader} from {@link ExtendedCamelContext}
      */
     @Deprecated
-    public static RestsDefinition loadRestsDefinition(CamelContext context, InputStream is) throws Exception {
-        // load routes using JAXB
-        Unmarshaller unmarshaller = getJAXBContext(context).createUnmarshaller();
-        Object result = unmarshaller.unmarshal(is);
-
-        if (result == null) {
-            throw new IOException("Cannot unmarshal to rests using JAXB from input stream: " + is);
-        }
-
-        // can either be routes or a single route
-        RestsDefinition answer;
-        if (result instanceof RestDefinition) {
-            RestDefinition rest = (RestDefinition)result;
-            answer = new RestsDefinition();
-            answer.getRests().add(rest);
-        } else if (result instanceof RestsDefinition) {
-            answer = (RestsDefinition)result;
-        } else {
-            throw new IllegalArgumentException("Unmarshalled object is an unsupported type: " + ObjectHelper.className(result) + " -> " + result);
-        }
-
-        return answer;
+    public static RestsDefinition loadRestsDefinition(CamelContext context, InputStream inputStream) throws Exception {
+        ExtendedCamelContext ecc = context.adapt(ExtendedCamelContext.class);
+        return (RestsDefinition) ecc.getXMLRoutesDefinitionLoader().loadRestsDefinition(context, inputStream);
     }
 
     private static <T extends NamedNode> T modelToXml(CamelContext context, InputStream is, String xml, Class<T> type) throws JAXBException {
