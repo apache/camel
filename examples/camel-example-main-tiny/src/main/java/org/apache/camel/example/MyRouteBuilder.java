2e293463f03d335d2063ff13e0f052a76efe17a1
Claus Ibsen <claus.ibsen@gmail.com>
2020-02-04 20:08:41 +0100

    Make tiny example more tiny

--- examples/camel-example-main-tiny/src/main/java/org/apache/camel/example/MyRouteBuilder.java
+++ examples/camel-example-main-tiny/src/main/java/org/apache/camel/example/MyRouteBuilder.java
@@ -22,7 +22,7 @@ public class MyRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("quartz:foo?cron={{myCron}}")
+        from("timer:foo?period={{myPeriod}}")
             .bean("myBean", "hello")
             .log("${body}")
             .bean("myBean", "bye")
