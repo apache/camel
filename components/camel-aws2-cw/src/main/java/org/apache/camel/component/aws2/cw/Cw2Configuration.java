85326933f206c8d4943d9525111ca0a8c32ae023
Andrea Cosentino <ancosen@gmail.com>
2020-02-06 17:36:38 +0100

    Camel-AWS2-CW: Fixed region description

--- components/camel-aws2-cw/src/main/java/org/apache/camel/component/aws2/cw/Cw2Configuration.java
+++ components/camel-aws2-cw/src/main/java/org/apache/camel/component/aws2/cw/Cw2Configuration.java
@@ -181,9 +181,10 @@ public class Cw2Configuration implements Cloneable {
     }
 
     /**
-     * The region in which CW client needs to work. When using this parameter,
-     * the configuration will expect the capitalized name of the region (for
-     * example AP_EAST_1) You'll need to use the name Regions.EU_WEST_1.name()
+     * The region in which EKS client needs to work. When using this
+     * parameter, the configuration will expect the lowercase name of the
+     * region (for example ap-east-1) You'll need to use the name
+     * Region.EU_WEST_1.id()
      */
     public void setRegion(String region) {
         this.region = region;
