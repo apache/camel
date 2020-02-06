6c369ba9bb155c7e7e377d9fdf5319486e8bf7ae
Claus Ibsen <claus.ibsen@gmail.com>
2020-02-06 10:44:54 +0100

    CAMEL-14498: camel-core - Make RuntimeCamelCatalog a simpler API

--- core/camel-api/src/main/java/org/apache/camel/runtimecatalog/RuntimeCamelCatalog.java
+++ core/camel-api/src/main/java/org/apache/camel/runtimecatalog/RuntimeCamelCatalog.java
@@ -21,178 +21,52 @@ import java.util.Map;
 
 import org.apache.camel.CamelContextAware;
 import org.apache.camel.StaticService;
+import org.apache.camel.component.extension.ComponentVerifierExtension;
+import org.apache.camel.spi.SendDynamicAware;
 
 /**
- * Runtime based CamelCatalog which are included in camel-core that can provided limit CamelCatalog capabilities
+ * Runtime catalog which limited API needed by components that supports
+ * {@link ComponentVerifierExtension} or {@link SendDynamicAware}.
  */
-// TODO: Remove not needed API and rename to a better name/package
-@Deprecated
 public interface RuntimeCamelCatalog extends StaticService, CamelContextAware {
 
-    // configuration
-
     /**
-     * Gets the {@link JSonSchemaResolver}.
-     */
-    JSonSchemaResolver getJSonSchemaResolver();
-
-    /**
-     * To use a custom {@link JSonSchemaResolver}
-     */
-    void setJSonSchemaResolver(JSonSchemaResolver resolver);
-
-    // functions
-
-    /**
-     * Returns the component information as JSon format.
-     *
-     * @param name the component name
-     * @return component details in JSon
-     */
-    String componentJSonSchema(String name);
-
-    /**
-     * Returns the data format information as JSon format.
-     *
-     * @param name the data format name
-     * @return data format details in JSon
-     */
-    String dataFormatJSonSchema(String name);
-
-    /**
-     * Returns the language information as JSon format.
-     *
-     * @param name the language name
-     * @return language details in JSon
-     */
-    String languageJSonSchema(String name);
-
-    /**
-     * Returns the model information as JSon format.
-     *
-     * @param name the model name
-     * @return model details in JSon
-     */
-    String modelJSonSchema(String name);
-
-    /**
-     * Parses the endpoint uri and constructs a key/value properties of each option
+     * Parses the endpoint uri and constructs a key/value properties of each option.
+     * <p/>
+     * This API is needed by {@link SendDynamicAware}.
      *
      * @param uri  the endpoint uri
      * @return properties as key value pairs of each endpoint option
      */
-    // TODO: needed by SendDynamicAware
     Map<String, String> endpointProperties(String uri) throws URISyntaxException;
 
     /**
      * Parses the endpoint uri and constructs a key/value properties of only the lenient properties (eg custom options)
      * <p/>
      * For example using the HTTP components to provide query parameters in the endpoint uri.
+     * <p/>
+     * This API is needed by {@link SendDynamicAware}.
      *
      * @param uri  the endpoint uri
      * @return properties as key value pairs of each lenient properties
      */
-    // TODO: needed by SendDynamicAware
     Map<String, String> endpointLenientProperties(String uri) throws URISyntaxException;
 
     /**
-     * Validates the pattern whether its a valid time pattern.
-     *
-     * @param pattern  the pattern such as 5000, 5s, 5sec, 4min, 4m30s, 1h, etc.
-     * @return <tt>true</tt> if valid, <tt>false</tt> if invalid
-     */
-    boolean validateTimePattern(String pattern);
-
-    /**
      * Validates the properties for the given scheme against component and endpoint
+     * <p/>
+     * This API is needed by {@link ComponentVerifierExtension}.
      *
      * @param scheme  the endpoint scheme
      * @param properties  the endpoint properties
      * @return validation result
      */
-    // TODO: needed for extension verifier (make SPI)
     EndpointValidationResult validateProperties(String scheme, Map<String, String> properties);
 
     /**
-     * Parses and validates the endpoint uri
-     *
-     * @param uri  the endpoint uri
-     * @return validation result
-     */
-    EndpointValidationResult validateEndpointProperties(String uri);
-
-    /**
-     * Parses and validates the endpoint uri
-     * <p/>
-     * The option ignoreLenientProperties can be used to ignore components that uses lenient properties.
-     * When this is true, then the uri validation is stricter but would fail on properties that are not part of the component
-     * but in the uri because of using lenient properties.
-     * For example using the HTTP components to provide query parameters in the endpoint uri.
-     *
-     * @param uri  the endpoint uri
-     * @param ignoreLenientProperties  whether to ignore components that uses lenient properties.
-     * @return validation result
-     */
-    EndpointValidationResult validateEndpointProperties(String uri, boolean ignoreLenientProperties);
-
-    /**
-     * Parses and validates the endpoint uri
-     * <p/>
-     * The option ignoreLenientProperties can be used to ignore components that uses lenient properties.
-     * When this is true, then the uri validation is stricter but would fail on properties that are not part of the component
-     * but in the uri because of using lenient properties.
-     * For example using the HTTP components to provide query parameters in the endpoint uri.
-     *
-     * @param uri  the endpoint uri
-     * @param ignoreLenientProperties  whether to ignore components that uses lenient properties.
-     * @param consumerOnly whether the endpoint is only used as a consumer
-     * @param producerOnly whether the endpoint is only used as a producer
-     * @return validation result
-     */
-    EndpointValidationResult validateEndpointProperties(String uri, boolean ignoreLenientProperties, boolean consumerOnly, boolean producerOnly);
-
-    /**
-     * Parses and validates the language as a predicate
-     * <p/>
-     * <b>Important:</b> This requires having <tt>camel-core</tt> and the language dependencies on the classpath
-     *
-     * @param classLoader a custom classloader to use for loading the language from the classpath, or <tt>null</tt> for using default classloader
-     * @param language the name of the language
-     * @param text  the predicate text
-     * @return validation result
-     */
-    LanguageValidationResult validateLanguagePredicate(ClassLoader classLoader, String language, String text);
-
-    /**
-     * Parses and validates the language as an expression
-     * <p/>
-     * <b>Important:</b> This requires having <tt>camel-core</tt> and the language dependencies on the classpath
-     *
-     * @param classLoader a custom classloader to use for loading the language from the classpath, or <tt>null</tt> for using default classloader
-     * @param language the name of the language
-     * @param text  the expression text
-     * @return validation result
-     */
-    LanguageValidationResult validateLanguageExpression(ClassLoader classLoader, String language, String text);
-
-    /**
-     * Parses and validates the configuration property
-     *
-     * @param line  the configuration line as key=value
-     * @return validation result
-     */
-    ConfigurationPropertiesValidationResult validateConfigurationProperty(String line);
-
-    /**
-     * Returns the component name from the given endpoint uri
-     *
-     * @param uri  the endpoint uri
-     * @return the component name (aka scheme), or <tt>null</tt> if not possible to determine
-     */
-    String endpointComponentName(String uri);
-
-    /**
      * Creates an endpoint uri in Java style from the information from the properties
+     * <p/>
+     * This API is needed by {@link SendDynamicAware}.
      *
      * @param scheme the endpoint schema
      * @param properties the properties as key value pairs
@@ -200,18 +74,6 @@ public interface RuntimeCamelCatalog extends StaticService, CamelContextAware {
      * @return the constructed endpoint uri
      * @throws java.net.URISyntaxException is thrown if there is encoding error
      */
-    // TODO: needed by SendDynamicAware
     String asEndpointUri(String scheme, Map<String, String> properties, boolean encode) throws URISyntaxException;
-
-    /**
-     * Creates an endpoint uri in XML style (eg escape & as &ampl;) from the information from the properties
-     *
-     * @param scheme the endpoint schema
-     * @param properties the properties as key value pairs
-     * @param encode whether to URL encode the returned uri or not
-     * @return the constructed endpoint uri
-     * @throws java.net.URISyntaxException is thrown if there is encoding error
-     */
-    String asEndpointUriXml(String scheme, Map<String, String> properties, boolean encode) throws URISyntaxException;
 
 }
