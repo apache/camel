fec229fba97376a0eef9c41775372e2f1ef37caa
Jan Filipski <globalbus@users.noreply.github.com>
2020-02-20 05:48:45 +0100

    CAMEL-6950 camel-sjms: Lacks reconnection logic in case of exception (#3581)
    * CAMEL-6950 camel-sjms: Lacks reconnection logic in case of exception
    
    * CAMEL-6950 camel-sjms: Lacks reconnection logic in case of exception
    Attempt with adjustable reconnect delay and connection separation for restart

---
+++ components/camel-sjms/src/test/java/org/apache/camel/component/sjms/ReconnectProducerTest.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.sjms;
+
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.TextMessage;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.component.sjms.support.JmsTestSupport;
+import org.junit.Test;
+
+public class ReconnectProducerTest extends JmsTestSupport {
+
+    private static final String TEST_DESTINATION_NAME = "sync.queue.producer.test";
+
+    @Override
+    protected boolean useJmx() {
+        return false;
+    }
+
+    @Test
+    public void testInOnlyQueueProducer() throws Exception {
+        MessageConsumer mc = createQueueConsumer(TEST_DESTINATION_NAME);
+        assertNotNull(mc);
+        final String expectedBody = "Hello World!";
+        MockEndpoint mock = getMockEndpoint("mock:result");
+
+        mock.expectedMessageCount(2);
+        mock.expectedBodiesReceived(expectedBody, expectedBody);
+
+        template.sendBody("direct:start", expectedBody);
+        Message message = mc.receive(5000);
+        assertNotNull(message);
+        assertTrue(message instanceof TextMessage);
+
+        TextMessage tm = (TextMessage) message;
+        String text = tm.getText();
+        assertNotNull(text);
+        template.sendBody("direct:finish", text);
+
+        reconnect(10000);
+
+        mc = createQueueConsumer(TEST_DESTINATION_NAME);
+        template.sendBody("direct:start", expectedBody);
+        message = mc.receive(5000);
+        assertNotNull(message);
+        assertTrue(message instanceof TextMessage);
+
+        tm = (TextMessage) message;
+        text = tm.getText();
+        assertNotNull(text);
+
+        template.sendBody("direct:finish", text);
+
+        mock.assertIsSatisfied();
+        mc.close();
+
+    }
+
+    /**
+     * @return
+     * @throws Exception
+     * @see org.apache.camel.test.junit4.CamelTestSupport#createRouteBuilder()
+     */
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:start")
+                    .to("sjms:queue:" + TEST_DESTINATION_NAME + "?consumerCount=10");
+
+                from("direct:finish")
+                    .to("log:test.log.1?showBody=true", "mock:result");
+            }
+        };
+    }
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext camelContext = super.createCamelContext();
+        camelContext.getComponent("sjms", SjmsComponent.class).setConnectionTestOnBorrow(true);
+        return camelContext;
+    }
+}
