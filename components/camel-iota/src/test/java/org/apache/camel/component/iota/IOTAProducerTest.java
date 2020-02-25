0500b9e52166cc6bf9c4c3a208439f5e508c3f84
Claus Ibsen <claus.ibsen@gmail.com>
2020-02-25 10:10:27 +0100

    Flaky CI test

--- components/camel-iota/src/test/java/org/apache/camel/component/iota/IOTAProducerTest.java
+++ components/camel-iota/src/test/java/org/apache/camel/component/iota/IOTAProducerTest.java
@@ -19,6 +19,7 @@ package org.apache.camel.component.iota;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
+import org.iota.jota.error.InternalException;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -37,7 +38,19 @@ public class IOTAProducerTest extends CamelTestSupport {
         MockEndpoint mock = getMockEndpoint("mock:iota-send-message-response");
         mock.expectedMinimumMessageCount(1);
 
-        template.sendBody("direct:iota-send-message", message);
+        try {
+            template.sendBody("direct:iota-send-message", message);
+        } catch (Exception e) {
+            if (e.getCause() instanceof InternalException) {
+                boolean flaky = e.getCause().getMessage().contains("Couldn't get a response from nodes");
+                if (!flaky) {
+                    throw e;
+                } else {
+                    log.warn("Flaky test as IOTA is not online and returning a response in time");
+                    return;
+                }
+            }
+        }
 
         assertMockEndpointsSatisfied();
     }
@@ -47,7 +60,19 @@ public class IOTAProducerTest extends CamelTestSupport {
         MockEndpoint mock = getMockEndpoint("mock:iota-new-address-response");
         mock.expectedMinimumMessageCount(1);
 
-        template.sendBody("direct:iota-new-address", "");
+        try {
+            template.sendBody("direct:iota-new-address", "");
+        } catch (Exception e) {
+            if (e.getCause() instanceof InternalException) {
+                boolean flaky = e.getCause().getMessage().contains("Couldn't get a response from nodes");
+                if (!flaky) {
+                    throw e;
+                } else {
+                    log.warn("Flaky test as IOTA is not online and returning a response in time");
+                    return;
+                }
+            }
+        }
 
         assertMockEndpointsSatisfied();
     }
@@ -57,7 +82,19 @@ public class IOTAProducerTest extends CamelTestSupport {
         MockEndpoint mock = getMockEndpoint("mock:iota-get-transfers-response");
         mock.expectedMinimumMessageCount(1);
 
-        template.sendBody("direct:iota-get-transfers", "");
+        try {
+            template.sendBody("direct:iota-get-transfers", "");
+        } catch (Exception e) {
+            if (e.getCause() instanceof InternalException) {
+                boolean flaky = e.getCause().getMessage().contains("Couldn't get a response from nodes");
+                if (!flaky) {
+                    throw e;
+                } else {
+                    log.warn("Flaky test as IOTA is not online and returning a response in time");
+                    return;
+                }
+            }
+        }
 
         assertMockEndpointsSatisfied();
     }
@@ -67,16 +104,24 @@ public class IOTAProducerTest extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() {
-                from("direct:iota-send-message").setHeader(IOTAConstants.SEED_HEADER, constant(SEED)).setHeader(IOTAConstants.TO_ADDRESS_HEADER, constant(ADDRESS))
+                from("direct:iota-send-message")
+                    .setHeader(IOTAConstants.SEED_HEADER, constant(SEED))
+                    .setHeader(IOTAConstants.TO_ADDRESS_HEADER, constant(ADDRESS))
                     .to("iota://test?url=" + IOTA_NODE_URL + "&securityLevel=2&tag=APACHECAMELTEST&depth=3&operation=" + IOTAConstants.SEND_TRANSFER_OPERATION)
                     .to("mock:iota-send-message-response");
 
-                from("direct:iota-new-address").setHeader(IOTAConstants.SEED_HEADER, constant(SEED)).setHeader(IOTAConstants.ADDRESS_INDEX_HEADER, constant(1))
-                    .to("iota://test?url=" + IOTA_NODE_URL + "&securityLevel=1&operation=" + IOTAConstants.GET_NEW_ADDRESS_OPERATION).to("mock:iota-new-address-response");
+                from("direct:iota-new-address")
+                    .setHeader(IOTAConstants.SEED_HEADER, constant(SEED))
+                    .setHeader(IOTAConstants.ADDRESS_INDEX_HEADER, constant(1))
+                    .to("iota://test?url=" + IOTA_NODE_URL + "&securityLevel=1&operation=" + IOTAConstants.GET_NEW_ADDRESS_OPERATION)
+                    .to("mock:iota-new-address-response");
                 
-                from("direct:iota-get-transfers").setHeader(IOTAConstants.SEED_HEADER, constant(SEED)).setHeader(IOTAConstants.ADDRESS_START_INDEX_HEADER, constant(1))
-                .setHeader(IOTAConstants.ADDRESS_END_INDEX_HEADER, constant(10))
-                .to("iota://test?url=" + IOTA_NODE_URL + "&securityLevel=1&operation=" + IOTAConstants.GET_TRANSFERS_OPERATION).to("mock:iota-get-transfers-response");
+                from("direct:iota-get-transfers")
+                    .setHeader(IOTAConstants.SEED_HEADER, constant(SEED))
+                    .setHeader(IOTAConstants.ADDRESS_START_INDEX_HEADER, constant(1))
+                    .setHeader(IOTAConstants.ADDRESS_END_INDEX_HEADER, constant(10))
+                    .to("iota://test?url=" + IOTA_NODE_URL + "&securityLevel=1&operation=" + IOTAConstants.GET_TRANSFERS_OPERATION)
+                    .to("mock:iota-get-transfers-response");
             }
         };
     }
