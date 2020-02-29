dd3ddd7d0f88d5b2df5433946fa77c42dcc737d1
Claus Ibsen <claus.ibsen@gmail.com>
2020-02-29 09:25:54 +0100

    CAMEL-14568 - Fixed camel-aws2-iam

--- components/camel-aws2-iam/src/test/java/org/apache/camel/component/aws2/iam/IAMComponentConfigurationTest.java
+++ components/camel-aws2-iam/src/test/java/org/apache/camel/component/aws2/iam/IAMComponentConfigurationTest.java
@@ -26,8 +26,8 @@ public class IAMComponentConfigurationTest extends CamelTestSupport {
     @Test
     public void createEndpointWithComponentElements() throws Exception {
         IAM2Component component = context.getComponent("aws2-iam", IAM2Component.class);
-        component.setAccessKey("XXX");
-        component.setSecretKey("YYY");
+        component.getConfiguration().setAccessKey("XXX");
+        component.getConfiguration().setSecretKey("YYY");
         IAM2Endpoint endpoint = (IAM2Endpoint)component.createEndpoint("aws2-iam://label");
 
         assertEquals("XXX", endpoint.getConfiguration().getAccessKey());
@@ -37,9 +37,9 @@ public class IAMComponentConfigurationTest extends CamelTestSupport {
     @Test
     public void createEndpointWithComponentAndEndpointElements() throws Exception {
         IAM2Component component = context.getComponent("aws2-iam", IAM2Component.class);
-        component.setAccessKey("XXX");
-        component.setSecretKey("YYY");
-        component.setRegion(Region.US_WEST_1.toString());
+        component.getConfiguration().setAccessKey("XXX");
+        component.getConfiguration().setSecretKey("YYY");
+        component.getConfiguration().setRegion(Region.US_WEST_1.toString());
         IAM2Endpoint endpoint = (IAM2Endpoint)component.createEndpoint("aws2-iam://label?accessKey=xxxxxx&secretKey=yyyyy&region=US_EAST_1");
 
         assertEquals("xxxxxx", endpoint.getConfiguration().getAccessKey());
@@ -50,9 +50,9 @@ public class IAMComponentConfigurationTest extends CamelTestSupport {
     @Test
     public void createEndpointWithComponentEndpointElementsAndProxy() throws Exception {
         IAM2Component component = context.getComponent("aws2-iam", IAM2Component.class);
-        component.setAccessKey("XXX");
-        component.setSecretKey("YYY");
-        component.setRegion(Region.US_WEST_1.toString());
+        component.getConfiguration().setAccessKey("XXX");
+        component.getConfiguration().setSecretKey("YYY");
+        component.getConfiguration().setRegion(Region.US_WEST_1.toString());
         IAM2Endpoint endpoint = (IAM2Endpoint)component
             .createEndpoint("aws2-iam://label?accessKey=xxxxxx&secretKey=yyyyy&region=US_EAST_1&proxyHost=localhost&proxyPort=9000&proxyProtocol=HTTP");
 
