4f5e08e62643d08bfcf78b8659af3b88032e60aa
Amos Feng <zfeng@redhat.com>
2020-03-03 15:11:39 +0800

    CAMEL-14326 add more tests

---
+++ components/camel-qute/src/test/java/org/apache/camel/component/qute/QuteBodyAsDomainObjectTest.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.qute;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class QuteBodyAsDomainObjectTest extends CamelTestSupport {
+
+    @Test
+    public void testWithObject() throws Exception {
+        String msg = String.format("Hi Claus how are you? Its a nice day.%sGive my regards to the family Ibsen.", "\n");
+
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+        mock.expectedBodiesReceived(msg);
+
+        MyPerson person = new MyPerson();
+        person.setFamilyName("Ibsen");
+        person.setGivenName("Claus");
+
+        template.requestBody("direct:in", person);
+
+        mock.assertIsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:in")
+                    .to("qute:org/apache/camel/component/qute/BodyAsDomainObject.txt")
+                    .to("mock:result");
+            }
+        };
+    }
+
+    public static class MyPerson {
+        private String givenName;
+        private String familyName;
+
+        public String getGivenName() {
+            return givenName;
+        }
+
+        public void setGivenName(String givenName) {
+            this.givenName = givenName;
+        }
+
+        public String getFamilyName() {
+            return familyName;
+        }
+
+        public void setFamilyName(String familyName) {
+            this.familyName = familyName;
+        }
+
+        @Override
+        public String toString() {
+            return "MyPerson{"
+                + "givenName='"
+                + givenName + '\''
+                + ", familyName='"
+                + familyName + '\''
+                + '}';
+        }
+    }
+}
---
+++ components/camel-qute/src/test/java/org/apache/camel/component/qute/QuteDynamicTemplateTest.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.qute;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+public class QuteDynamicTemplateTest extends CamelTestSupport {
+
+    private Exchange createLetter() {
+        Exchange exchange = context.getEndpoint("direct:a").createExchange();
+        Message msg = exchange.getIn();
+        msg.setHeader("firstName", "Claus");
+        msg.setHeader("lastName", "Ibsen");
+        msg.setHeader("item", "Camel in Action");
+        msg.setBody("PS: Next beer is on me, James");
+        return exchange;
+    }
+
+    @Test
+    public void testLetter() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+        mock.message(0).body(String.class).contains("Thanks for the order of Camel in Action");
+
+        template.send("direct:a", createLetter());
+
+        mock.assertIsSatisfied();
+
+        mock.reset();
+
+        mock.expectedMessageCount(1);
+        mock.message(0).body(String.class).contains("Regards Apache Camel Riders Bookstore");
+
+        template.send("direct:a", createLetter());
+
+        mock.assertIsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
+                MyBean bean = new MyBean();
+                from("direct:a").bean(bean).to("mock:result");
+            }
+        };
+    }
+
+    public static class MyBean {
+        ProducerTemplate template;
+        boolean useLetter2;
+
+        public void sendToNewTemplate(Exchange exchange) throws Exception {
+            if (template == null) {
+                template = exchange.getContext().createProducerTemplate();
+            }
+            template.send("qute:" + getNewTemplate(), exchange);
+            useLetter2 = !useLetter2;
+        }
+
+        private String getNewTemplate() {
+            if (useLetter2) {
+                return "org/apache/camel/component/qute/letter2.txt";
+            } else {
+                return "org/apache/camel/component/qute/letter.txt";
+            }
+        }
+    }
+}
