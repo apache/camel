af3034eadcee86e0beabfcdb869642536feb0fa7
Guillaume Nodet <gnodet@gmail.com>
2020-03-03 15:36:54 +0100

    Explicitely call the xml generation mojo

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/GenerateMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/GenerateMojo.java
@@ -60,8 +60,6 @@ public class GenerateMojo extends AbstractGenerateMojo {
         invoke(ComponentDslMojo.class);
         // update-readme
         invoke(UpdateReadmeMojo.class);
-        // generate-xml-parser
-        invoke(ModelXmlParserGeneratorMojo.class);
     }
 
 }
