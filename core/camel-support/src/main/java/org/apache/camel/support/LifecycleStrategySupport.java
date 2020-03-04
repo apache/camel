3bf45670dba43e9d965c99660ac523da2f162234
Guillaume Nodet <gnodet@gmail.com>
2020-03-04 10:31:12 +0100

    CS: Fix import order

--- core/camel-support/src/main/java/org/apache/camel/support/LifecycleStrategySupport.java
+++ core/camel-support/src/main/java/org/apache/camel/support/LifecycleStrategySupport.java
@@ -24,10 +24,10 @@ import org.apache.camel.Component;
 import org.apache.camel.Endpoint;
 import org.apache.camel.ErrorHandlerFactory;
 import org.apache.camel.Processor;
+import org.apache.camel.Route;
 import org.apache.camel.Service;
 import org.apache.camel.VetoCamelContextStartException;
 import org.apache.camel.spi.LifecycleStrategy;
-import org.apache.camel.Route;
 
 /**
  * A useful base class for {@link LifecycleStrategy} implementations.
