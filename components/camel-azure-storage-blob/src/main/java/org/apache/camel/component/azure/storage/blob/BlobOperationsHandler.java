70ce61bd65d1c5a9863d6a808440df6e6c59b8b7
Omar Al-Safi <omarsmak@gmail.com>
2020-03-10 17:45:36 +0100

    CAMEL-14654: Progress in refactoring

--- components/camel-azure-storage-blob/src/main/java/org/apache/camel/component/azure/storage/blob/BlobOperationsHandler.java
+++ components/camel-azure-storage-blob/src/main/java/org/apache/camel/component/azure/storage/blob/BlobOperationsHandler.java
@@ -2,9 +2,15 @@ package org.apache.camel.component.azure.storage.blob;
 
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.azure.core.http.rest.Response;
 import com.azure.core.util.Context;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
@@ -13,6 +19,9 @@ import com.azure.storage.blob.models.BlobContainerItem;
 import com.azure.storage.blob.models.BlobDownloadResponse;
 import com.azure.storage.blob.models.BlobItem;
 import com.azure.storage.blob.specialized.BlobClientBase;
+import com.azure.storage.blob.specialized.BlobInputStream;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.util.ObjectHelper;
 
 /**
@@ -31,12 +40,27 @@ public class BlobOperationsHandler {
                 .collect(Collectors.toList());
     }
 
-    public BlobDownloadResponse handleDownloadBlob(final BlobClient client) {
+    public void handleDownloadBlob(final Exchange exchange, final BlobClient client) throws IOException {
        checkIfContainerOrBlobIsEmpty(configuration);
 
-       ///client.download();
-
-       return client.downloadWithResponse(new ByteArrayOutputStream(), null, null, null, false, null, Context.NONE);
+       OutputStream outputStream = exchange.getIn().getBody(OutputStream.class);
+       if (outputStream == null && !ObjectHelper.isEmpty(configuration.getFileDir())) {
+           final File outputFile = new File(configuration.getFileDir(), configuration.getBlobName());
+           getMessageForResponse(exchange).setBody(outputFile);
+           outputStream = new FileOutputStream(outputFile);
+       }
+       try {
+           if (outputStream == null) {
+               final BlobInputStream blobInputStream = client.openInputStream();
+               exchange.getIn().setBody(blobInputStream);
+           } else {
+               client.download(outputStream);
+           }
+       } finally {
+           if (outputStream != null) {
+               outputStream.close();
+           }
+       }
     }
 
     public List<BlobItem> handleListBlobs(final BlobContainerClient client) {
@@ -52,5 +76,14 @@ public class BlobOperationsHandler {
         if (ObjectHelper.isEmpty(configuration.getContainerName()) || ObjectHelper.isEmpty(configuration.getBlobName())) {
             throw new IllegalArgumentException("No blob or container name was specified.");
         }
+    }
+
+    private static Message getMessageForResponse(final Exchange exchange) {
+        if (exchange.getPattern().isOutCapable()) {
+            Message out = exchange.getMessage();
+            out.copyFrom(exchange.getIn());
+            return out;
+        }
+        return exchange.getIn();
     }
 }
--- components/camel-azure-storage-blob/src/test/java/org/apache/camel/component/azure/storage/blob/BlobOperationsHandlerTest.java
+++ components/camel-azure-storage-blob/src/test/java/org/apache/camel/component/azure/storage/blob/BlobOperationsHandlerTest.java
@@ -1,10 +1,15 @@
 package org.apache.camel.component.azure.storage.blob;
 
+import java.io.IOException;
+
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.models.BlobDownloadResponse;
+import org.apache.camel.Exchange;
 import org.apache.camel.component.azure.storage.blob.client.BlobClientFactory;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.support.DefaultExchange;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -20,7 +25,7 @@ class BlobOperationsHandlerTest {
     public void setup() {
         configuration = new BlobConfiguration();
         configuration.setAccountName("cameldev");
-        configuration.setAccessKey("");
+        configuration.setAccessKey("2PS1ZnTqmR7ORJ33VSY+ASeq2yuRx+vIx10/+kaT/pu0yYe0iFkZDSMnXpHc33onLg8fxOyN/zx+4hPriZNC+g==");
     }
 
     @Test
@@ -43,15 +48,21 @@ class BlobOperationsHandlerTest {
     }
 
     @Test
-    public void testHandleGetBlob() {
+    public void testHandleGetBlob() throws IOException {
         configuration.setContainerName("test");
         configuration.setBlobName("0b4e673827795_1_V550.jpg");
+        configuration.setFileDir("/Users/oalsafi/Work/Apache/camel/components/camel-azure-storage-blob");
 
         final BlobClient client = BlobClientFactory.createBlobClient(configuration);
         final BlobOperationsHandler handler = new BlobOperationsHandler(configuration);
 
-        final BlobDownloadResponse downloadResponse = handler.handleDownloadBlob(client);
+        //final BlobDownloadResponse downloadResponse = handler.handleDownloadBlob(client);
 
-        System.out.println(downloadResponse.getHeaders());
+        //System.out.println(downloadResponse.getDeserializedHeaders());
+
+        final Exchange exchange = new DefaultExchange(new DefaultCamelContext());
+        handler.handleDownloadBlob(exchange, client);
+
+        System.out.println(exchange.getIn().getBody());
     }
 }
