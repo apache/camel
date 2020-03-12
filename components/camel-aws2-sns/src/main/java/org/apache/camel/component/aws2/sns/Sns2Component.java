0cbac82bbb1d975a917ce1446c0cd82e89b6ff9c
Andrea Cosentino <ancosen@gmail.com>
2020-03-12 10:26:29 +0100

    Camel-AWS2-SNS: Fixed CS

--- components/camel-aws2-sns/src/main/java/org/apache/camel/component/aws2/sns/Sns2Component.java
+++ components/camel-aws2-sns/src/main/java/org/apache/camel/component/aws2/sns/Sns2Component.java
@@ -27,13 +27,12 @@ import org.apache.camel.support.DefaultComponent;
 import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Component("aws2-sns")
 public class Sns2Component extends DefaultComponent {
-	
+
     private static final Logger LOG = LoggerFactory.getLogger(Sns2Component.class);
     
     @Metadata
