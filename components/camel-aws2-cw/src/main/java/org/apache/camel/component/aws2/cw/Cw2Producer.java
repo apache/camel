5010b67f3f419958367b8968b989de6f84847764
Andrea Cosentino <ancosen@gmail.com>
2020-03-17 12:27:19 +0100

    Camel-AWS2-CW: Fixed CS

--- components/camel-aws2-cw/src/main/java/org/apache/camel/component/aws2/cw/Cw2Producer.java
+++ components/camel-aws2-cw/src/main/java/org/apache/camel/component/aws2/cw/Cw2Producer.java
@@ -83,7 +83,7 @@ public class Cw2Producer extends DefaultProducer {
             metricDatum.dimensions(Dimension.builder().name(name).value(value).build());
         } else {
             @SuppressWarnings("unchecked")
-			Map<String, String> dimensions = exchange.getIn().getHeader(Cw2Constants.METRIC_DIMENSIONS, Map.class);
+            Map<String, String> dimensions = exchange.getIn().getHeader(Cw2Constants.METRIC_DIMENSIONS, Map.class);
             if (dimensions != null) {
                 Collection<Dimension> dimensionCollection = new ArrayList<>();
                 for (Map.Entry<String, String> dimensionEntry : dimensions.entrySet()) {
