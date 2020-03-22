969b46d93842532d5fc4aec503548a09c3a44fe8
Andrea Cosentino <ancosen@gmail.com>
2020-03-22 15:11:12 +0100

    Camel-HDFS: Fixed CS

--- components/camel-hdfs/src/main/java/org/apache/camel/component/hdfs/HdfsComponent.java
+++ components/camel-hdfs/src/main/java/org/apache/camel/component/hdfs/HdfsComponent.java
@@ -16,9 +16,10 @@
  */
 package org.apache.camel.component.hdfs;
 
-import javax.security.auth.login.Configuration;
 import java.net.URL;
 import java.util.Map;
+
+import javax.security.auth.login.Configuration;
 
 import org.apache.camel.Endpoint;
 import org.apache.camel.component.hdfs.kerberos.KerberosConfigurationBuilder;
