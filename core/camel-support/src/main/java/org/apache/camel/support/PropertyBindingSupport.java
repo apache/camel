25fc3a1b35590cfde18b46222007b06fc8e60af6
Claus Ibsen <claus.ibsen@gmail.com>
2020-04-08 14:47:42 +0200

    Polished

--- core/camel-support/src/main/java/org/apache/camel/support/PropertyBindingSupport.java
+++ core/camel-support/src/main/java/org/apache/camel/support/PropertyBindingSupport.java
@@ -1006,7 +1006,7 @@ public final class PropertyBindingSupport {
      *
      * @param camelContext       the camel context
      * @param name               the name of the bean
-     * @param value              how to resolve the bean with a prefix of either class#:, type#: or bean#:
+     * @param value              how to resolve the bean with a prefix of either #class:, #type: or #bean:
      * @return the resolve bean
      * @throws Exception is thrown if error resolving the bean, or if the value is invalid.
      */
