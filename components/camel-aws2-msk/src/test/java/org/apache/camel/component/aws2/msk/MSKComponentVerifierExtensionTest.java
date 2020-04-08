c09889d6377c5aefeb3fe251d31113fa961b7f9b
Andrea Cosentino <ancosen@gmail.com>
2020-04-08 08:06:54 +0200

    CAMEL-14850 - Camel-AWS2-*: Improve the verifiers to check if a particular service works on a specified region, Added test for MSK

--- components/camel-aws2-msk/src/test/java/org/apache/camel/component/aws2/msk/MSKComponentVerifierExtensionTest.java
+++ components/camel-aws2-msk/src/test/java/org/apache/camel/component/aws2/msk/MSKComponentVerifierExtensionTest.java
@@ -71,4 +71,20 @@ public class MSKComponentVerifierExtensionTest extends CamelTestSupport {
         assertEquals(ComponentVerifierExtension.Result.Status.ERROR, result.getStatus());
     }
 
+    @Test
+    public void testConnectivityAndRegion() throws Exception {
+        Component component = context().getComponent("aws2-msk");
+        ComponentVerifierExtension verifier = component.getExtension(ComponentVerifierExtension.class).orElseThrow(IllegalStateException::new);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("secretKey", "l");
+        parameters.put("accessKey", "k");
+        parameters.put("region", "l");
+        parameters.put("label", "test");
+        parameters.put("operation", MSK2Operations.listClusters);
+
+        ComponentVerifierExtension.Result result = verifier.verify(ComponentVerifierExtension.Scope.CONNECTIVITY, parameters);
+
+        assertEquals(ComponentVerifierExtension.Result.Status.ERROR, result.getStatus());
+    }
 }
