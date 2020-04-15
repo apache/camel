9b777395769cdbce0fbdc1e9e531b5db18644601
Andrea Cosentino <ancosen@gmail.com>
2020-04-15 12:46:17 +0200

    CAMEL-14868 - Camel-AWS2-*: Where possible, give the possiblity to the end user to pass an AWS Request pojo as body, aws2-mq describe broker

--- components/camel-aws2-mq/src/main/java/org/apache/camel/component/aws2/mq/MQ2Producer.java
+++ components/camel-aws2-mq/src/main/java/org/apache/camel/component/aws2/mq/MQ2Producer.java
@@ -333,8 +333,22 @@ public class MQ2Producer extends DefaultProducer {
         }
     }
 
-    private void describeBroker(MqClient mqClient, Exchange exchange) {
+    private void describeBroker(MqClient mqClient, Exchange exchange) throws InvalidPayloadException {
         String brokerId;
+        if (getConfiguration().isPojoRequest()) {
+            Object payload = exchange.getIn().getMandatoryBody();
+            if (payload instanceof DescribeBrokerRequest) {
+                DescribeBrokerResponse result;
+                try {
+                    result = mqClient.describeBroker((DescribeBrokerRequest) payload);
+                } catch (AwsServiceException ase) {
+                    LOG.trace("Reboot Broker command returned the error code {}", ase.awsErrorDetails().errorCode());
+                    throw ase;
+                }
+                Message message = getMessageForResponse(exchange);
+                message.setBody(result);
+            }
+        } else {
         DescribeBrokerRequest.Builder builder = DescribeBrokerRequest.builder();
         if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(MQ2Constants.BROKER_ID))) {
             brokerId = exchange.getIn().getHeader(MQ2Constants.BROKER_ID, String.class);
@@ -351,6 +365,7 @@ public class MQ2Producer extends DefaultProducer {
         }
         Message message = getMessageForResponse(exchange);
         message.setBody(result);
+        }
     }
 
     public static Message getMessageForResponse(final Exchange exchange) {
