626482829a398ae32cfdb2f076ce8f59b15a9dde
Louis Burton <louisburton@users.noreply.github.com>
2020-05-09 06:49:46 +0100

    [CAMEL-15027] Do not propagate record specific Kafka headers (#3813)
    * CAMEL-15027: Do not propagate record specific kafka headers
    
    * CAMEL-15027: Checkstyle fix - import spacing
    
    Co-authored-by: Louis Burton <louis.burton@winnowsolutions.com>

--- components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaHeaderFilterStrategy.java
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaHeaderFilterStrategy.java
@@ -16,9 +16,16 @@
  */
 package org.apache.camel.component.kafka;
 
+import java.util.regex.Pattern;
+
 import org.apache.camel.support.DefaultHeaderFilterStrategy;
 
 public class KafkaHeaderFilterStrategy extends DefaultHeaderFilterStrategy {
+
+    /**
+     * A filter pattern that only accepts keys starting with <tt>Camel</tt> or <tt>org.apache.camel.</tt>
+     */
+    public static final Pattern CAMEL_KAFKA_FILTER_PATTERN = Pattern.compile("(?i)(Camel|org\\.apache\\.camel|kafka\\.)[\\.|a-z|A-z|0-9]*");
 
     public KafkaHeaderFilterStrategy() {
         initialize();
@@ -28,8 +35,8 @@ public class KafkaHeaderFilterStrategy extends DefaultHeaderFilterStrategy {
         // filter out kafka record metadata
         getInFilter().add("org.apache.kafka.clients.producer.RecordMetadata");
 
-        // filter headers begin with "Camel" or "org.apache.camel"
-        setOutFilterPattern(CAMEL_FILTER_PATTERN);
-        setInFilterPattern(CAMEL_FILTER_PATTERN);
+        // filter headers beginning with "Camel" or "org.apache.camel" or "kafka."
+        setOutFilterPattern(CAMEL_KAFKA_FILTER_PATTERN);
+        setInFilterPattern(CAMEL_KAFKA_FILTER_PATTERN);
     }
 }
