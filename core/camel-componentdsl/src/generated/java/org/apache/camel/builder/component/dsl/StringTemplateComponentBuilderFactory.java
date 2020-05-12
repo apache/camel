e469664e88148b126e5938aa28674019c80955a8
Claus Ibsen <claus.ibsen@gmail.com>
2020-05-12 09:26:44 +0200

    CAMEL-15050: Templating components - Variable map to be limited to body/headers

--- core/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/StringTemplateComponentBuilderFactory.java
+++ core/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/StringTemplateComponentBuilderFactory.java
@@ -49,6 +49,23 @@ public interface StringTemplateComponentBuilderFactory {
             extends
                 ComponentBuilder<StringTemplateComponent> {
         /**
+         * Sets whether the context map should allow access to all details. By
+         * default only the message body and headers can be accessed. This
+         * option can be enabled for full access to the current Exchange and
+         * CamelContext. Doing so impose a potential security risk as this opens
+         * access to the full power of CamelContext API.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: producer
+         */
+        default StringTemplateComponentBuilder allowContextMapAll(
+                boolean allowContextMapAll) {
+            doSetProperty("allowContextMapAll", allowContextMapAll);
+            return this;
+        }
+        /**
          * Whether to allow to use resource template from header or not (default
          * false). Enabling this allows to specify dynamic templates via message
          * header. However this can be seen as a potential security
@@ -117,6 +134,7 @@ public interface StringTemplateComponentBuilderFactory {
                 String name,
                 Object value) {
             switch (name) {
+            case "allowContextMapAll": ((StringTemplateComponent) component).setAllowContextMapAll((boolean) value); return true;
             case "allowTemplateFromHeader": ((StringTemplateComponent) component).setAllowTemplateFromHeader((boolean) value); return true;
             case "lazyStartProducer": ((StringTemplateComponent) component).setLazyStartProducer((boolean) value); return true;
             case "basicPropertyBinding": ((StringTemplateComponent) component).setBasicPropertyBinding((boolean) value); return true;
