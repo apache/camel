b9e19a753a21933708c1108cb936e5f02b26fbfd
Claus Ibsen <claus.ibsen@gmail.com>
2020-05-24 14:23:43 +0200

    CAMEL-15084: Move health implementation in camel-base into camel-health

--- core/camel-health/src/test/java/org/apache/camel/impl/health/RouteHealthCheckTest.java
+++ core/camel-health/src/test/java/org/apache/camel/impl/health/RouteHealthCheckTest.java
@@ -18,34 +18,37 @@ package org.apache.camel.impl.health;
 
 import java.util.Collections;
 
-import org.apache.camel.ContextTestSupport;
+import junit.framework.TestCase;
+import org.apache.camel.CamelContext;
 import org.apache.camel.Route;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.health.HealthCheckResultBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.Test;
 
-public class RouteHealthCheckTest extends ContextTestSupport {
+public class RouteHealthCheckTest extends TestCase {
 
     private static final String TEST_ROUTE_ID = "Test-Route";
 
     @Test
-    public void testDoCallDoesNotHaveNPEWhenJmxDisabled() {
+    public void testDoCallDoesNotHaveNPEWhenJmxDisabled() throws Exception{
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:input").id(TEST_ROUTE_ID).log("Message");
+            }
+        });
+        context.start();
+
         Route route = context.getRoute(TEST_ROUTE_ID);
 
         RouteHealthCheck healthCheck = new RouteHealthCheck(route);
         final HealthCheckResultBuilder builder = HealthCheckResultBuilder.on(healthCheck);
 
         healthCheck.doCall(builder, Collections.emptyMap());
-    }
 
-    @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
-        return new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from("direct:input").id(TEST_ROUTE_ID).log("Message");
-            }
-        };
+        context.stop();
     }
 
 }
