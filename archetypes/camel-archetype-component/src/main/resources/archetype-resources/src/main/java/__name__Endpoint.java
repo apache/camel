738cb8bfe0351a0633dd136a5acf6d710c4a3295
Claus Ibsen <claus.ibsen@gmail.com>
2020-05-24 17:55:57 +0200

    CAMEL-15043: Fixed camel-archetype-component

--- archetypes/camel-archetype-component/src/main/resources/archetype-resources/src/main/java/__name__Endpoint.java
+++ archetypes/camel-archetype-component/src/main/resources/archetype-resources/src/main/java/__name__Endpoint.java
@@ -28,9 +28,11 @@ import org.apache.camel.spi.UriPath;
 import java.util.concurrent.ExecutorService;
 
 /**
- * Represents a ${name} endpoint.
+ * ${name} component which does bla bla.
+ *
+ * TODO: Update one line description above what the component does.
  */
-@UriEndpoint(firstVersion = "${version}", scheme = "${scheme}", title = "${name}", syntax="${scheme}:name", 
+@UriEndpoint(firstVersion = "${version}", scheme = "${scheme}", title = "${name}", syntax="${scheme}:name",
              consumerClass = ${name}Consumer.class, label = "custom")
 public class ${name}Endpoint extends DefaultEndpoint {
     @UriPath @Metadata(required = true)
@@ -50,11 +52,9 @@ public class ${name}Endpoint extends DefaultEndpoint {
     }
 
     public Consumer createConsumer(Processor processor) throws Exception {
-        return new ${name}Consumer(this, processor);
-    }
-
-    public boolean isSingleton() {
-        return true;
+        Consumer consumer = new ${name}Consumer(this, processor);
+        configureConsumer(consumer);
+        return consumer;
     }
 
     /**
