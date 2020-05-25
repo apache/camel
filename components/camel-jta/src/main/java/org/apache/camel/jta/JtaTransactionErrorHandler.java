072b11133ef88dfa230af5b354eb256948bc19b4
Amos Feng <zfeng@redhat.com>
2020-05-25 16:22:07 +0800

    CAMEL-15108: Extract the implementation of the JtaTransactionPolicy from camel-cdi to camel-jta

--- components/camel-jta/src/main/java/org/apache/camel/jta/JtaTransactionErrorHandler.java
+++ components/camel-jta/src/main/java/org/apache/camel/jta/JtaTransactionErrorHandler.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.cdi.transaction;
+package org.apache.camel.jta;
 
 import java.util.concurrent.ScheduledExecutorService;
 
