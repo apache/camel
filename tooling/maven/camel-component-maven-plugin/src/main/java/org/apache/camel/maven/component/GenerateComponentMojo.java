0555ef53556b78454a787ca98e367989feb5907d
Omar Al-Safi <omarsmak@gmail.com>
2020-06-05 10:57:36 +0200

    CAMEL-14886: Polishing

--- tooling/maven/camel-component-maven-plugin/src/main/java/org/apache/camel/maven/component/GenerateComponentMojo.java
+++ tooling/maven/camel-component-maven-plugin/src/main/java/org/apache/camel/maven/component/GenerateComponentMojo.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.maven.component;
 
 import org.apache.camel.maven.packaging.AbstractGenerateMojo;
@@ -8,8 +24,6 @@ import org.apache.camel.maven.packaging.PrepareComponentMojo;
 import org.apache.camel.maven.packaging.SpiGeneratorMojo;
 import org.apache.camel.maven.packaging.TypeConverterLoaderGeneratorMojo;
 import org.apache.camel.maven.packaging.ValidateComponentMojo;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
