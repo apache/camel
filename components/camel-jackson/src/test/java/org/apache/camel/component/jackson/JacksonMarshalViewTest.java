4577a393d430521490736dc3193854a58e219f53
Guillaume Nodet <gnodet@gmail.com>
2020-06-11 17:19:01 +0200

    [CAMEL-11807] Upgrade camel-jackson to junit5

--- components/camel-jackson/src/test/java/org/apache/camel/component/jackson/JacksonMarshalViewTest.java
+++ components/camel-jackson/src/test/java/org/apache/camel/component/jackson/JacksonMarshalViewTest.java
@@ -19,8 +19,10 @@ package org.apache.camel.component.jackson;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JacksonMarshalViewTest extends CamelTestSupport {
 
