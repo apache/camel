37e81086bebc753b0b5c7e4013a69e0fbbd9b22b
Guillaume Nodet <gnodet@gmail.com>
2020-06-12 07:41:16 +0200

    Fix checkstyle

--- tooling/camel-tooling-util/src/test/java/org/apache/camel/tooling/util/CamelVersionHelperTest.java
+++ tooling/camel-tooling-util/src/test/java/org/apache/camel/tooling/util/CamelVersionHelperTest.java
@@ -45,7 +45,7 @@ public class CamelVersionHelperTest {
         assertTrue(isGE("3.4.0", "3.4.0-SNAPSHOT"));
 
         assertTrue(isGE("3.3.0", "3.3.0.jdk11-800001-0000001"));
-   }
+    }
 
     @Test
     public void testPrevMinor() throws Exception {
