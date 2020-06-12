b8ae8772318c964f4b8ca760a04665bc3550a765
Guillaume Nodet <gnodet@gmail.com>
2020-06-12 07:39:15 +0200

    [CAMEL-11807] Upgrade camel-jdbc to junit5

--- components/camel-jdbc/src/test/java/org/apache/camel/component/jdbc/JdbcDefaultDataSourceTest.java
+++ components/camel-jdbc/src/test/java/org/apache/camel/component/jdbc/JdbcDefaultDataSourceTest.java
@@ -22,7 +22,9 @@ import java.util.List;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JdbcDefaultDataSourceTest extends AbstractJdbcTestSupport {
 
--- components/camel-jdbc/src/test/java/org/apache/camel/component/jdbc/JdbcProducerOutputTypeSelectOneOutputClassTest.java
+++ components/camel-jdbc/src/test/java/org/apache/camel/component/jdbc/JdbcProducerOutputTypeSelectOneOutputClassTest.java
@@ -19,7 +19,10 @@ package org.apache.camel.component.jdbc;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.camel.test.junit5.TestSupport.assertIsInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JdbcProducerOutputTypeSelectOneOutputClassTest extends AbstractJdbcTestSupport {
 
