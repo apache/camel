eea263243b33f514c14ed7306bf667cb63830603
Claus Ibsen <claus.ibsen@gmail.com>
2020-06-15 10:59:54 +0200

    Deprecate hystrix in camel-main

--- core/camel-main/src/main/java/org/apache/camel/main/MainConfigurationProperties.java
+++ core/camel-main/src/main/java/org/apache/camel/main/MainConfigurationProperties.java
@@ -81,6 +81,7 @@ public class MainConfigurationProperties extends DefaultConfigurationProperties<
     /**
      * To configure Circuit Breaker EIP with Hystrix
      */
+    @Deprecated
     public HystrixConfigurationProperties hystrix() {
         return hystrixConfigurationProperties;
     }
