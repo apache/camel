b24a1203d558f40da5bfefce832e295565e1229d
Claus Ibsen <claus.ibsen@gmail.com>
2020-06-22 12:26:24 +0200

    CAMEL-15224: camel-api-component - Avoid reflection when configured nested configuration classes.

--- components/camel-google-calendar/src/main/java/org/apache/camel/component/google/calendar/GoogleCalendarConfiguration.java
+++ components/camel-google-calendar/src/main/java/org/apache/camel/component/google/calendar/GoogleCalendarConfiguration.java
@@ -18,6 +18,7 @@ package org.apache.camel.component.google.calendar;
 
 import com.google.api.services.calendar.CalendarScopes;
 import org.apache.camel.component.google.calendar.internal.GoogleCalendarApiName;
+import org.apache.camel.spi.Configurer;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriParams;
@@ -27,6 +28,7 @@ import org.apache.camel.spi.UriPath;
  * Component configuration for GoogleCalendar component.
  */
 @UriParams
+@Configurer
 public class GoogleCalendarConfiguration {
 
     @UriPath
