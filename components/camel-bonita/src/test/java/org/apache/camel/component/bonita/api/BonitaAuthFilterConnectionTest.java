03ec69a6bc8cb013d6abf0082e30ff92784f1527
Guillaume Nodet <gnodet@gmail.com>
2020-06-22 14:36:00 +0200

    [CAMEL-11807] Upgrade camel-bonita to junit5

--- components/camel-bonita/src/test/java/org/apache/camel/component/bonita/api/BonitaAuthFilterConnectionTest.java
+++ components/camel-bonita/src/test/java/org/apache/camel/component/bonita/api/BonitaAuthFilterConnectionTest.java
@@ -16,18 +16,25 @@
  */
 package org.apache.camel.component.bonita.api;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.util.HashMap;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MultivaluedHashMap;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.apache.camel.component.bonita.api.filter.BonitaAuthFilter;
 import org.apache.camel.component.bonita.api.util.BonitaAPIConfig;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.apache.camel.test.AvailablePortFinder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -36,26 +43,35 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonitaAuthFilterConnectionTest {
-
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(0);
 
     @Mock
     private ClientRequestContext requestContext;
 
-    @Before
-    public void setup() {
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+    public void setup() throws IOException {
+        wireMockServer = new WireMockServer(WireMockConfiguration.options()/*.port(etc)*/);
+        wireMockServer.start();
+
         MockitoAnnotations.initMocks(this);
         Mockito.when(requestContext.getCookies()).thenReturn(new HashMap<String, Cookie>());
         Mockito.when(requestContext.getHeaders()).thenReturn(new MultivaluedHashMap());
     }
 
+
+    @AfterEach
+    public void tearDown() {
+        wireMockServer.stop();
+        wireMockServer = null;
+    }
+
     @Test
     public void testConnection() throws Exception {
-        String port = wireMockRule.port() + "";
+        String port = wireMockServer.port() + "";
         stubFor(post(urlEqualTo("/bonita/loginservice"))
                 .willReturn(aResponse().withHeader("Set-Cookie", "JSESSIONID=something")));
 
@@ -68,7 +84,7 @@ public class BonitaAuthFilterConnectionTest {
     
     @Test
     public void testConnectionSupportCSRF() throws Exception {
-        String port = wireMockRule.port() + "";
+        String port = wireMockServer.port() + "";
         stubFor(post(urlEqualTo("/bonita/loginservice"))
                 .willReturn(aResponse().withHeader("Set-Cookie", "JSESSIONID=something", "X-Bonita-API-Token=something")));
 
