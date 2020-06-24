be253b4c16c6ea8232316f8f8443087b4af31d28
Guillaume Nodet <gnodet@gmail.com>
2020-06-24 17:26:20 +0200

    [CAMEL-11807] Upgrade camel-file-watch to junit5

--- components/camel-file-watch/src/test/java/org/apache/camel/component/file/watch/FileWatchComponentNegativeTest.java
+++ components/camel-file-watch/src/test/java/org/apache/camel/component/file/watch/FileWatchComponentNegativeTest.java
@@ -19,7 +19,9 @@ package org.apache.camel.component.file.watch;
 import java.nio.file.Paths;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FileWatchComponentNegativeTest extends FileWatchComponentTestBase {
 
