74798b94c0fa811cbb8ba45c137a988b2c878be9
Guillaume Nodet <gnodet@gmail.com>
2020-06-24 17:58:17 +0200

    [CAMEL-11807] Upgrade camel-freemarker to junit5

--- components/camel-freemarker/src/test/java/org/apache/camel/component/freemarker/FreemarkerConcurrentTest.java
+++ components/camel-freemarker/src/test/java/org/apache/camel/component/freemarker/FreemarkerConcurrentTest.java
@@ -21,8 +21,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.camel.test.junit5.TestSupport.body;
 
 public class FreemarkerConcurrentTest extends CamelTestSupport {
 
