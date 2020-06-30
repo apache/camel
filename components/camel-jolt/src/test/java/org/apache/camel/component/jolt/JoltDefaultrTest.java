3573bd93b6313c5fe85741ba87ac24ca85271498
Guillaume Nodet <gnodet@gmail.com>
2020-06-30 10:43:23 +0200

    [CAMEL-11807] Upgrade camel-jolt to junit5

--- components/camel-jolt/src/test/java/org/apache/camel/component/jolt/JoltDefaultrTest.java
+++ components/camel-jolt/src/test/java/org/apache/camel/component/jolt/JoltDefaultrTest.java
@@ -21,8 +21,10 @@ import java.util.Map;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test testing the Removr.
