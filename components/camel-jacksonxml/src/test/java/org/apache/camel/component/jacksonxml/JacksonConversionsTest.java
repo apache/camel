2398c9fb9c966af50141a6c977dde2e5f28709e1
Guillaume Nodet <gnodet@gmail.com>
2020-06-30 09:38:09 +0200

    [CAMEL-11807] Upgrade camel-jacksonxml to junit5

--- components/camel-jacksonxml/src/test/java/org/apache/camel/component/jacksonxml/JacksonConversionsTest.java
+++ components/camel-jacksonxml/src/test/java/org/apache/camel/component/jacksonxml/JacksonConversionsTest.java
@@ -21,8 +21,10 @@ import java.util.Map;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JacksonConversionsTest extends CamelTestSupport {
 
