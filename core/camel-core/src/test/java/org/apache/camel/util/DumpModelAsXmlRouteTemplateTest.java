9eb900d622e0678b22dcf59bfbb306206cceb70d
Claus Ibsen <claus.ibsen@gmail.com>
2020-07-01 10:26:22 +0200

    CAMEL-14963: Route Template. WIP

---
+++ core/camel-core/src/test/java/org/apache/camel/util/DumpModelAsXmlRouteTemplateTest.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.util;
+
+import org.apache.camel.ContextTestSupport;
+import org.apache.camel.ExtendedCamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxp.XmlConverter;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ *
+ */
+public class DumpModelAsXmlRouteTemplateTest extends ContextTestSupport {
+
+    @Test
+    public void testDumpModelAsXml() throws Exception {
+        ExtendedCamelContext ecc = context.adapt(ExtendedCamelContext.class);
+        String xml = ecc.getModelToXMLDumper().dumpModelAsXml(context, context.getRouteTemplateDefinition("myTemplate"));
+        assertNotNull(xml);
+        log.info(xml);
+
+        Document doc = new XmlConverter().toDOMDocument(xml, null);
+        NodeList nodes = doc.getElementsByTagName("simple");
+        assertEquals(1, nodes.getLength());
+        Element node = (Element)nodes.item(0);
+        assertNotNull(node, "Node <simple> expected to be instanceof Element");
+        assertEquals("{{greeting}}", node.getTextContent());
+
+        nodes = doc.getElementsByTagName("to");
+        assertEquals(1, nodes.getLength());
+        node = (Element)nodes.item(0);
+        assertNotNull(node, "Node <to> expected to be instanceof Element");
+        assertEquals("mock:{{whereto}}", node.getAttribute("uri"));
+
+        nodes = doc.getElementsByTagName("routeTemplate");
+        assertEquals(1, nodes.getLength());
+        node = (Element)nodes.item(0);
+        assertEquals("myTemplate", node.getAttribute("id"));
+        assertEquals("greeting,whereto", node.getAttribute("parameters"));
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                routeTemplate("myTemplate", "greeting", "whereto")
+                    .from("direct:start").transform(simple("{{greeting}}")).to("mock:{{whereto}}");
+            }
+        };
+    }
+}
