f76d3b1cef62d28892574d74b76228525f0b80fc
Claus Ibsen <claus.ibsen@gmail.com>
2020-07-02 09:11:58 +0200

    CAMEL-14963: Route Template. WIP

--- core/camel-core-engine/src/main/java/org/apache/camel/model/RouteTemplateContextRefDefinition.java
+++ core/camel-core-engine/src/main/java/org/apache/camel/model/RouteTemplateContextRefDefinition.java
@@ -16,11 +16,12 @@
  */
 package org.apache.camel.model;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.List;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.spi.Metadata;
