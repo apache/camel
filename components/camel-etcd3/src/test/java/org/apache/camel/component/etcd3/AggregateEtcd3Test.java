a28d2581a5aa9169fa0fe242dc133fe1ce92489b
Marco Longobardi <mrclngbrd@gmail.com>
2020-07-08 18:23:56 +0200

    Added camel-etcd3 component and Etcd3AggregationRepository using jetcd
    Removed unused properties
    
    Added some fixes to Etcd3AggregationRepository
    
    Fixed java.lang.IndexOutOfBoundsException on Etcd3AggregationRepository
    
    Added test class for camel-etcd3 component
    
    Fixed indentation problems
    
    Added test class and fixed some bugs
    
    Modified test class
    
    Modified remove method on Etcd3AggregationRepository
    
    Rebase
    
    Modified Etcd3AggregationRepository

---
+++ components/camel-etcd3/src/test/java/org/apache/camel/component/etcd3/AggregateEtcd3Test.java
@@ -0,0 +1,51 @@
+package org.apache.camel.component.etcd3;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.component.etcd3.processor.aggregate.Etcd3AggregationRepository;
+import org.junit.Test;
+
+/**
+ * The ABC example for using the Aggregator EIP.
+ * <p/>
+ * This example have 4 messages send to the aggregator, by which one
+ * message is published which contains the aggregation of message 1,2 and 4
+ * as they use the same correlation key.
+ * <p/>
+ * See the class {@link camelinaction.MyAggregationStrategy} for how the messages
+ * are actually aggregated together.
+ *
+ * @see MyAggregationStrategy
+ */
+public class AggregateEtcd3Test extends CamelTestSupport {
+	
+	private String endpoint = System.getProperty("endpoint"); //http://ip:port
+	
+	@Test
+    public void testABC() throws Exception {
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedBodiesReceived("ABC");
+		template.sendBodyAndHeader("direct:start", "A", "myId", 1);
+        template.sendBodyAndHeader("direct:start", "B", "myId", 1);
+        template.sendBodyAndHeader("direct:start", "F", "myId", 2);
+        template.sendBodyAndHeader("direct:start", "C", "myId", 1);
+		assertMockEndpointsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+		return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                    .log("Sending ${body} with correlation key ${header.myId}")
+                    .aggregate(header("myId"), new MyAggregationStrategy())
+					.aggregationRepository(new Etcd3AggregationRepository("aggregation", endpoint))
+					.completionSize(3)
+                        .log("Sending out ${body}")
+                        .to("mock:result");
+            }
+        };
+    }
+}
