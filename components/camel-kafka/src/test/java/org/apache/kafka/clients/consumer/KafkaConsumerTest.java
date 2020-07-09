d687249d141450e112862cb682983e37817c23da
Guillaume Nodet <gnodet@gmail.com>
2020-07-09 17:58:08 +0200

    [CAMEL-11807] Fix checkstyle for camel-kafka

--- components/camel-kafka/src/test/java/org/apache/kafka/clients/consumer/KafkaConsumerTest.java
+++ components/camel-kafka/src/test/java/org/apache/kafka/clients/consumer/KafkaConsumerTest.java
@@ -18,11 +18,9 @@ package org.apache.kafka.clients.consumer;
 
 import org.hamcrest.core.IsNot;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.core.IsNull.nullValue;
