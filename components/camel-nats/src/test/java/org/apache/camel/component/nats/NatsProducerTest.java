cf6a66309c802bb95c22a66a6fee3b5d5dc7ecd7
Guillaume Nodet <gnodet@gmail.com>
2020-07-09 15:35:35 +0200

    [CAMEL-11807] Upgrade camel-nats to junit5

--- components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsProducerTest.java
+++ components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsProducerTest.java
@@ -17,7 +17,7 @@
 package org.apache.camel.component.nats;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class NatsProducerTest extends NatsTestSupport {
     
