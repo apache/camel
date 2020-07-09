9603ae4e537d7ef948cf63ce2a82fedac8a86568
Guillaume Nodet <gnodet@gmail.com>
2020-07-09 09:10:31 +0200

    [CAMEL-11807] Upgrade camel-stream to junit5

--- components/camel-stream/src/test/java/org/apache/camel/component/stream/ScanStreamFileManualTest.java
+++ components/camel-stream/src/test/java/org/apache/camel/component/stream/ScanStreamFileManualTest.java
@@ -19,21 +19,24 @@ package org.apache.camel.component.stream;
 import java.io.File;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.camel.test.junit5.TestSupport.createDirectory;
+import static org.apache.camel.test.junit5.TestSupport.deleteDirectory;
 
 /**
  * Unit test for scan stream file
  */
-@Ignore("For manual testing")
+@Disabled("For manual testing")
 public class ScanStreamFileManualTest extends CamelTestSupport {
 
     private File file;
 
     @Override
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         deleteDirectory("target/stream");
         createDirectory("target/stream");
