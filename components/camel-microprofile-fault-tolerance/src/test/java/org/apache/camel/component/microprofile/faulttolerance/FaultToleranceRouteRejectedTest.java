05120679b0efcd553de67f88ffe609ba43b77cb8
Guillaume Nodet <gnodet@gmail.com>
2020-07-15 17:09:54 +0200

    [CAMEL-11807] Upgrade camel-microprofile-fault-tolerance to junit5

--- components/camel-microprofile-fault-tolerance/src/test/java/org/apache/camel/component/microprofile/faulttolerance/FaultToleranceRouteRejectedTest.java
+++ components/camel-microprofile-fault-tolerance/src/test/java/org/apache/camel/component/microprofile/faulttolerance/FaultToleranceRouteRejectedTest.java
@@ -18,8 +18,12 @@ package org.apache.camel.component.microprofile.faulttolerance;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spi.CircuitBreakerConstants;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.camel.test.junit5.TestSupport.assertIsInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FaultToleranceRouteRejectedTest extends CamelTestSupport {
 
