94f88f041633d88961a3ac725cc8791050365e05
Guillaume Nodet <gnodet@gmail.com>
2020-07-17 17:30:39 +0200

    [CAMEL-11807] Upgrade camel-pubnub to junit5

--- components/camel-pubnub/src/test/java/org/apache/camel/component/pubnub/PubNubFireTest.java
+++ components/camel-pubnub/src/test/java/org/apache/camel/component/pubnub/PubNubFireTest.java
@@ -19,7 +19,7 @@ package org.apache.camel.component.pubnub;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
