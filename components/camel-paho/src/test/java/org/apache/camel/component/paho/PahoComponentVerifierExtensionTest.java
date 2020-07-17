fd88a83f8ffbc05a8835dd11defe4f0bdfee962a
Guillaume Nodet <gnodet@gmail.com>
2020-07-17 15:46:08 +0200

    [CAMEL-11807] Upgrade camel-paho to junit5

--- components/camel-paho/src/test/java/org/apache/camel/component/paho/PahoComponentVerifierExtensionTest.java
+++ components/camel-paho/src/test/java/org/apache/camel/component/paho/PahoComponentVerifierExtensionTest.java
@@ -21,9 +21,10 @@ import java.util.Map;
 
 import org.apache.camel.Component;
 import org.apache.camel.component.extension.ComponentVerifierExtension;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.junit.Assert;
-import org.junit.Test;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PahoComponentVerifierExtensionTest extends CamelTestSupport {
 
@@ -46,7 +47,7 @@ public class PahoComponentVerifierExtensionTest extends CamelTestSupport {
 
         ComponentVerifierExtension.Result result = verifier.verify(ComponentVerifierExtension.Scope.PARAMETERS, parameters);
 
-        Assert.assertEquals(ComponentVerifierExtension.Result.Status.OK, result.getStatus());
+        assertEquals(ComponentVerifierExtension.Result.Status.OK, result.getStatus());
     }
 
     @Test
@@ -59,7 +60,7 @@ public class PahoComponentVerifierExtensionTest extends CamelTestSupport {
 
         ComponentVerifierExtension.Result result = verifier.verify(ComponentVerifierExtension.Scope.CONNECTIVITY, parameters);
 
-        Assert.assertEquals(ComponentVerifierExtension.Result.Status.ERROR, result.getStatus());
+        assertEquals(ComponentVerifierExtension.Result.Status.ERROR, result.getStatus());
     }
 
 }
