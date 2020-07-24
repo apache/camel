ff057769da4f7d2958d2967eea49bc1adc3de164
Guillaume Nodet <gnodet@gmail.com>
2020-07-24 12:31:53 +0200

    [CAMEL-11807] Upgrade camel-vertx to junit5

--- components/camel-vertx/src/test/java/org/apache/camel/component/vertx/VertxRouteTest.java
+++ components/camel-vertx/src/test/java/org/apache/camel/component/vertx/VertxRouteTest.java
@@ -21,7 +21,7 @@ import java.util.List;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class VertxRouteTest extends VertxBaseTestSupport {
 
