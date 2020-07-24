dd3e937b27fca7249d56aa096f1c99bf991efad5
Guillaume Nodet <gnodet@gmail.com>
2020-07-24 10:30:43 +0200

    [CAMEL-11807] Upgrade camel-splunk-hec to junit5

--- components/camel-splunk-hec/src/test/java/org/apache/camel/component/splunkhec/SplunkHECConfigurationTest.java
+++ components/camel-splunk-hec/src/test/java/org/apache/camel/component/splunkhec/SplunkHECConfigurationTest.java
@@ -16,10 +16,10 @@
  */
 package org.apache.camel.component.splunkhec;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SplunkHECConfigurationTest {
 
