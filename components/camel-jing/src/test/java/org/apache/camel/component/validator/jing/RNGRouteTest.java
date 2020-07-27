c834a1a38e427ef7046acb094a63255dbfa04827
Guillaume Nodet <gnodet@gmail.com>
2020-07-27 13:03:02 +0200

    [CAMEL-11807] Upgrade camel-jing to junit5

--- components/camel-jing/src/test/java/org/apache/camel/component/validator/jing/RNGRouteTest.java
+++ components/camel-jing/src/test/java/org/apache/camel/component/validator/jing/RNGRouteTest.java
@@ -16,8 +16,8 @@
  */
 package org.apache.camel.component.validator.jing;
 
-import org.apache.camel.test.spring.CamelSpringTestSupport;
-import org.junit.Test;
+import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RNGRouteTest extends CamelSpringTestSupport {
