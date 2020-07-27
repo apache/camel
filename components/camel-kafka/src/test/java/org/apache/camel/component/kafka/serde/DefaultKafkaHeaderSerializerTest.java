86ec371a36f32fabd8d2ece2f003d1a2aef8f34f
Guillaume Nodet <gnodet@gmail.com>
2020-07-27 16:12:57 +0200

    [CAMEL-11807] Upgrade camel-kafka to junit5

--- components/camel-kafka/src/test/java/org/apache/camel/component/kafka/serde/DefaultKafkaHeaderSerializerTest.java
+++ components/camel-kafka/src/test/java/org/apache/camel/component/kafka/serde/DefaultKafkaHeaderSerializerTest.java
@@ -22,7 +22,7 @@ import java.util.Collection;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class DefaultKafkaHeaderSerializerTest {
 
