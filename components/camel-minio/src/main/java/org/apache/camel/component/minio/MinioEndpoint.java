409614e6426f1393c9c465c7dae4ff670902b2d2
nayananga <nayanangamuhandiram@gmail.com>
2020-08-07 08:41:57 +0530

    Minor fix

--- components/camel-minio/src/main/java/org/apache/camel/component/minio/MinioEndpoint.java
+++ components/camel-minio/src/main/java/org/apache/camel/component/minio/MinioEndpoint.java
@@ -128,11 +128,6 @@ public class MinioEndpoint extends ScheduledPollEndpoint {
 
     @Override
     public void doStop() throws Exception {
-        if (ObjectHelper.isEmpty(getConfiguration().getMinioClient())) {
-            if (minioClient != null) {
-                minioClient = null;
-            }
-        }
         super.doStop();
     }
 
