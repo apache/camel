29636d478d57613b64307d6cc11c6c8df89c3f19
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2020-08-24 18:49:49 +0200

    camel-itests: fix deprecation warnings (#4122)
    This replaces the HTTP test server with the stock one from HttpComponents.

--- tests/camel-itest/src/test/java/org/apache/camel/itest/http/HttpEndpointTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/http/HttpEndpointTest.java
@@ -53,7 +53,7 @@ public class HttpEndpointTest {
 
     @BeforeAll
     public static void setUp() throws Exception {
-        localServer = new HttpTestServer(null, null);
+        localServer = new HttpTestServer();
         localServer.register("/", new HttpRequestHandler() {
             public void handle(
                     HttpRequest request, HttpResponse response,
