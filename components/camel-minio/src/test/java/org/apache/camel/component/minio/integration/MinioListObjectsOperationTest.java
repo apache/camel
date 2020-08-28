722a8bd3151d1f0af86f8b329c2da0b1a5771439
Omar Al-Safi <omarsmak@gmail.com>
2020-08-28 11:19:33 +0200

    CAMEL-15477: Integration Tests in camel-minio polish (#4141)

--- components/camel-minio/src/test/java/org/apache/camel/component/minio/integration/MinioListObjectsOperationTest.java
+++ components/camel-minio/src/test/java/org/apache/camel/component/minio/integration/MinioListObjectsOperationTest.java
@@ -14,11 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.minio.integration.remoteTests;
+package org.apache.camel.component.minio.integration;
 
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.Properties;
 
 import com.google.common.collect.Iterators;
 import io.minio.MinioClient;
@@ -32,20 +31,17 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.minio.MinioConstants;
 import org.apache.camel.component.minio.MinioOperations;
-import org.apache.camel.component.minio.MinioTestUtils;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MinioListObjectsOperationIntegrationTest extends CamelTestSupport {
-    final Properties properties = MinioTestUtils.loadMinioPropertiesFile();
+class MinioListObjectsOperationTest extends MinioTestContainerSupport {
 
     @BindToRegistry("minioClient")
-    MinioClient minioClient = MinioClient.builder()
-            .endpoint(properties.getProperty("endpoint"))
-            .credentials(properties.getProperty("accessKey"), properties.getProperty("secretKey"))
+    MinioClient client = MinioClient.builder()
+            .endpoint("http://" + CONTAINER.getHost(), CONTAINER.getMappedPort(BROKER_PORT), false)
+            .credentials(ACCESS_KEY, SECRET_KEY)
             .build();
 
     @EndpointInject
@@ -54,7 +50,7 @@ class MinioListObjectsOperationIntegrationTest extends CamelTestSupport {
     @EndpointInject("mock:result")
     private MockEndpoint result;
 
-    MinioListObjectsOperationIntegrationTest() throws IOException {
+    MinioListObjectsOperationTest() throws IOException {
     }
 
     @SuppressWarnings("unchecked")
