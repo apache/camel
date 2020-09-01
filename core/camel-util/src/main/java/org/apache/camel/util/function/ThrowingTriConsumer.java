67f7d6799933f1744e7bcfd835893492094590cf
lburgazzoli <lburgazzoli@gmail.com>
2020-09-01 11:56:30 +0200

    Add javadoc and remove lamda usage in utility methods

--- core/camel-util/src/main/java/org/apache/camel/util/function/ThrowingTriConsumer.java
+++ core/camel-util/src/main/java/org/apache/camel/util/function/ThrowingTriConsumer.java
@@ -16,7 +16,24 @@
  */
 package org.apache.camel.util.function;
 
+/**
+ * Represents an operation that accepts three input arguments and returns no result and may thrown an exception.
+ *
+ * @param <I1> the type of the first argument to the operation
+ * @param <I2> the type of the second argument to the operation
+ * @param <I3> the type of the third argument to the operation
+ * @param <T>  the type of the exception the accept method may throw
+ */
 @FunctionalInterface
 public interface ThrowingTriConsumer<I1, I2, I3, T extends Throwable> {
+    /**
+     * Applies this function to the given arguments, potentially throwing an exception.
+     *
+     * @param  i1 the first argument
+     * @param  i2 the second argument
+     * @param  i3 the third argument
+     * @return    the function result
+     * @throws T  the exception that may be thrown
+     */
     void accept(I1 i1, I2 i2, I3 i3) throws T;
 }
