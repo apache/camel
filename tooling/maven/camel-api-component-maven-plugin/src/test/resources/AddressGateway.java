e5529443d230165421aa104c24daecfb9ca25266
Claus Ibsen <claus.ibsen@gmail.com>
2020-09-03 14:12:35 +0200

    Fixed CS

--- tooling/maven/camel-api-component-maven-plugin/src/test/resources/AddressGateway.java
+++ tooling/maven/camel-api-component-maven-plugin/src/test/resources/AddressGateway.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+//CHECKSTYLE:OFF
 package com.braintreegateway;
 
 import com.braintreegateway.exceptions.NotFoundException;
@@ -88,3 +89,4 @@ public class AddressGateway {
         return new Result<Address>(node, Address.class);
     }
 }
+//CHECKSTYLE:ON
--- tooling/maven/camel-api-component-maven-plugin/src/test/resources/DisputeGateway.java
+++ tooling/maven/camel-api-component-maven-plugin/src/test/resources/DisputeGateway.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+//CHECKSTYLE:OFF
 package com.braintreegateway;
 
 import com.braintreegateway.exceptions.NotFoundException;
@@ -259,3 +260,4 @@ public class DisputeGateway {
         return new PaginatedResult<Dispute>(response.findInteger("total-items"), response.findInteger("page-size"), disputes);
     }
 }
+//CHECKSTYLE:ON
