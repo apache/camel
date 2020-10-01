d9cacc72f6b46a576b8279c1491cdc02c0c28bc5
Claus Ibsen <claus.ibsen@gmail.com>
2020-10-01 17:23:31 +0200

    CAMEL-15605: Languages should be singleton for better performance.

--- core/camel-core/src/test/java/org/apache/camel/support/CustomizersTest.java
+++ core/camel-core/src/test/java/org/apache/camel/support/CustomizersTest.java
@@ -42,7 +42,6 @@ import static org.apache.camel.util.CollectionHelper.propertiesOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
