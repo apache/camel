f668efb690fab2932bce44e96dc3daa31ea510bb
Andrea Cosentino <ancosen@gmail.com>
2020-10-03 16:33:20 +0200

    Regen and sync properties

--- core/camel-base/src/main/java/org/apache/camel/processor/interceptor/DefaultDebugger.java
+++ core/camel-base/src/main/java/org/apache/camel/processor/interceptor/DefaultDebugger.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
