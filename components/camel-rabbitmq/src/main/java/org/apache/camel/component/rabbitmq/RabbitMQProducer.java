2aded3dc20ed36660c994899992566015574fa2c
Ganesh <rgannu@gmail.com>
2020-10-07 06:16:18 +0200

    CAMEL-15496: Headers and Properties support (#4371)
    * CAMEL-15496: Headers and Properties support
    
    The additional headers will be added on top of the existing headers
    from the message.
    
    The properties added are only basic AMQP properties as defined in the
    {@link com.rabbitmq.client.AMQP.BasicProperties.Builder}. When the
    message contains already these properties then will be considered and
    these additional properties will be ignored.
    
    Introduced a SimpleDataHolderBean to hold a hashmap for adding any
    header. As with this fix though we can add any header, only
    static/hard-coded values could be associated.
    
    The rabbitmq AMQP basic properties constant keys should not
    have dot or hyphen in their prefixes. This gives problem in trying to
    retrieve the value due to fix of CAMEL-15394. Changed to underscore.
    
    * CAMEL-15496: Headers and Properties support
    
    Updated the migration guide

--- components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQProducer.java
+++ components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQProducer.java
@@ -224,7 +224,8 @@ public class RabbitMQProducer extends DefaultAsyncProducer {
         // the request timeout can be overruled by a header otherwise the
         // endpoint configured value is used
         final long timeout
-                = exchange.getIn().getHeader(RabbitMQConstants.REQUEST_TIMEOUT, getEndpoint().getRequestTimeout(), long.class);
+                = exchange.getIn().getHeader(RabbitMQConstants.REQUEST_TIMEOUT, getEndpoint().getRequestTimeout(),
+                        long.class);
 
         final String originalCorrelationId = in.getHeader(RabbitMQConstants.CORRELATIONID, String.class);
 
