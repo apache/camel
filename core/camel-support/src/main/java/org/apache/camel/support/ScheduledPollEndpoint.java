bc06e4d3b6ba06e6ff4587b0481b396d01eeed48
Claus Ibsen <claus.ibsen@gmail.com>
2020-10-08 07:20:04 +0200

    CAMEL-15558 pollEnrich timeout issue added unit test

--- core/camel-support/src/main/java/org/apache/camel/support/ScheduledPollEndpoint.java
+++ core/camel-support/src/main/java/org/apache/camel/support/ScheduledPollEndpoint.java
@@ -191,10 +191,6 @@ public abstract class ScheduledPollEndpoint extends DefaultEndpoint {
         }
     }
 
-    public long getDefaultDelay() {
-        return (getDelay() == -1) ? DEFAULT_DELAY : getDelay();
-    }
-
     @Override
     protected void doStart() throws Exception {
         super.doStart();
@@ -242,6 +238,13 @@ public abstract class ScheduledPollEndpoint extends DefaultEndpoint {
      */
     public void setDelay(long delay) {
         this.delay = delay;
+    }
+
+    /**
+     * Gets the default delay.
+     */
+    public long getDefaultDelay() {
+        return DEFAULT_DELAY;
     }
 
     public TimeUnit getTimeUnit() {
