5333ddfa5dc1632ef41240eb69688e2c5f6382de
Andrea Cosentino <ancosen@gmail.com>
2020-10-08 14:28:08 +0200

    Camel-AWS2-SQS: Use Localstack 0.11.6

--- components/camel-aws2-sqs/src/test/java/org/apache/camel/component/aws2/sqs/localstack/Aws2SQSBaseTest.java
+++ components/camel-aws2-sqs/src/test/java/org/apache/camel/component/aws2/sqs/localstack/Aws2SQSBaseTest.java
@@ -32,7 +32,7 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class Aws2SQSBaseTest extends ContainerAwareTestSupport {
 
-    public static final String CONTAINER_IMAGE = "localstack/localstack:0.11.5";
+    public static final String CONTAINER_IMAGE = "localstack/localstack:0.11.6";
     public static final String CONTAINER_NAME = "sqs";
 
     @Override
