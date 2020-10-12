a48779e221f0f837650d19b6f41b43cc616f750f
Andrea Cosentino <ancosen@gmail.com>
2020-10-12 18:07:23 +0200

    CAMEL-15647 - Camel-AWS2-Kinesis-*: Added a CborEnabled option explicitly, kinesis

--- components/camel-aws2-kinesis/src/main/java/org/apache/camel/component/aws2/kinesis/Kinesis2Configuration.java
+++ components/camel-aws2-kinesis/src/main/java/org/apache/camel/component/aws2/kinesis/Kinesis2Configuration.java
@@ -70,6 +70,9 @@ public class Kinesis2Configuration implements Cloneable {
               description = "Setting the autoDiscoverClient mechanism, if true, the component will "
                             + " look for a client instance in the registry automatically otherwise it will skip that checking")
     private boolean autoDiscoverClient = true;
+    @UriParam(label = "common", defaultValue = "true", 
+    		description = "This option will set the CBOR_ENABLED property during the execution")
+    private boolean cborEnabled = true;
 
     public KinesisClient getAmazonKinesisClient() {
         return amazonKinesisClient;
@@ -190,12 +193,19 @@ public class Kinesis2Configuration implements Cloneable {
     public void setAutoDiscoverClient(boolean autoDiscoverClient) {
         this.autoDiscoverClient = autoDiscoverClient;
     }
+    
+    public boolean isCborEnabled() {
+		return cborEnabled;
+	}
+
+	public void setCborEnabled(boolean cborEnabled) {
+		this.cborEnabled = cborEnabled;
+	}
 
     // *************************************************
     //
     // *************************************************
-
-    public Kinesis2Configuration copy() {
+	public Kinesis2Configuration copy() {
         try {
             return (Kinesis2Configuration) super.clone();
         } catch (CloneNotSupportedException e) {
