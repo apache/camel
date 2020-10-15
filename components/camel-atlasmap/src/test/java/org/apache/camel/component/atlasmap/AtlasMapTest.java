c09aedb78c5f34e80d0d0f4e2ff846e53516fc69
Tomohisa Igarashi <tm.igarashi@gmail.com>
2020-10-15 10:42:55 -0400

    CAMEL-15689 - Add camel-atlasmap component (#4447)

---
+++ components/camel-atlasmap/src/test/java/org/apache/camel/component/atlasmap/AtlasMapTest.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.atlasmap;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import twitter4j.Status;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+public class AtlasMapTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("context/AtlasMapTest-context.xml");
+    }
+
+    @Test
+    public void testMocksAreValid() throws Exception {
+        MockEndpoint result = getMockEndpoint("mock:result");
+        result.setExpectedCount(1);
+
+        ProducerTemplate producerTemplate = context.createProducerTemplate();
+        producerTemplate.sendBody("direct:start", Util.generateMockTwitterStatus());
+
+        assertMockEndpointsSatisfied();
+        Object body = result.getExchanges().get(0).getIn().getBody();
+        assertEquals(String.class, body.getClass());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode outJson = mapper.readTree((String) body);
+        assertEquals("Bob", outJson.get("FirstName").asText());
+        assertEquals("Vila", outJson.get("LastName").asText());
+        assertEquals("bobvila1982", outJson.get("Title").asText());
+        assertEquals("Let's build a house!", outJson.get("Description").asText());
+        assertEquals("const foobar", outJson.get("Constant").asText());
+    }
+
+    @Test
+    public void testSeparateNotSucceed() throws Exception {
+        MockEndpoint result = getMockEndpoint("mock:result");
+        result.setExpectedCount(1);
+
+        ProducerTemplate producerTemplate = context.createProducerTemplate();
+        Status s = Util.generateMockTwitterStatus();
+        when(s.getUser().getName()).thenReturn("BobVila");
+        producerTemplate.sendBody("direct:start", s);
+
+        MockEndpoint.assertIsSatisfied(context);
+        Object body = result.getExchanges().get(0).getIn().getBody();
+        assertEquals(String.class, body.getClass());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode outJson = mapper.readTree((String) body);
+        assertEquals("BobVila", outJson.get("FirstName").asText());
+        assertNull(outJson.get("LastName"));
+        assertEquals("bobvila1982", outJson.get("Title").asText());
+        assertEquals("Let's build a house!", outJson.get("Description").asText());
+    }
+
+}
