013874a119a32e983027aa1f2d167e79debe62fc
Andrea Cosentino <ancosen@gmail.com>
2020-10-29 19:33:41 +0100

    CAMEL-15770 - Kafka serialize/deserialize properties are inconsistently named - kafkaHeaderDeserializer tests

--- components/camel-kafka/src/test/java/org/apache/camel/component/kafka/KafkaConsumerFullTest.java
+++ components/camel-kafka/src/test/java/org/apache/camel/component/kafka/KafkaConsumerFullTest.java
@@ -194,7 +194,7 @@ public class KafkaConsumerFullTest extends BaseEmbeddedKafkaTest {
     @Test
     public void headerDeserializerCouldBeOverridden() {
         KafkaEndpoint kafkaEndpoint
-                = context.getEndpoint("kafka:random_topic?kafkaHeaderDeserializer=#myHeaderDeserializer", KafkaEndpoint.class);
+                = context.getEndpoint("kafka:random_topic?headerDeserializer=#myHeaderDeserializer", KafkaEndpoint.class);
         assertIsInstanceOf(MyKafkaHeaderDeserializer.class, kafkaEndpoint.getConfiguration().getHeaderDeserializer());
     }
 
