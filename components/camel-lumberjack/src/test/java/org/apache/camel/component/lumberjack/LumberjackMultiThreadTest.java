a485c6eae13474c7066209202ace1e097a7202dd
Zineb BENDHIBA <bendhiba.zineb@gmail.com>
2020-10-30 12:31:23 +0100

    CAMEL-15718: Camel lumberjack server component not thread safe (#4540)

--- components/camel-lumberjack/src/test/java/org/apache/camel/component/lumberjack/LumberjackMultiThreadTest.java
+++ components/camel-lumberjack/src/test/java/org/apache/camel/component/lumberjack/LumberjackMultiThreadTest.java
@@ -16,9 +16,11 @@
  */
 package org.apache.camel.component.lumberjack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
@@ -29,7 +31,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LumberjackComponentTest extends CamelTestSupport {
+public class LumberjackMultiThreadTest extends CamelTestSupport {
+
     private static int port;
 
     @BeforeAll
@@ -51,13 +54,20 @@ public class LumberjackComponentTest extends CamelTestSupport {
     public void shouldListenToMessages() throws Exception {
         // We're expecting 25 messages with Maps
         MockEndpoint mock = getMockEndpoint("mock:output");
-        mock.expectedMessageCount(60);
+        mock.expectedMessageCount(125);
         mock.allMessages().body().isInstanceOf(Map.class);
 
-        List<Integer> windows = Arrays.asList(15, 10, 15, 10, 10);
-
         // When sending messages
-        List<Integer> responses = LumberjackUtil.sendMessages(port, null, windows);
+        List<Integer> windows = Arrays.asList(15, 10);
+
+        // create 5 threads
+        List<LumberjackThreadTest> threads = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            threads.add(new LumberjackThreadTest());
+        }
+
+        // sending messages on 5 parallel sessions
+        threads.stream().forEach(thread -> thread.start());
 
         // Then we should have the messages we're expecting
         mock.assertIsSatisfied();
@@ -68,7 +78,23 @@ public class LumberjackComponentTest extends CamelTestSupport {
         assertEquals("/home/qatest/collectNetwork/log/data-integration/00000000-f000-0000-1541-8da26f200001/absorption.log",
                 first.get("source"));
 
-        // And we should have replied with 2 acknowledgments for each window frame
-        assertEquals(windows, responses);
+        TimeUnit.MILLISECONDS.sleep(2000);
+
+        // And we should have replied with 2 acknowledgments for each session frame
+        threads.stream().forEach(thread -> assertEquals(windows, thread.responses));
+    }
+
+    class LumberjackThreadTest extends Thread {
+        private List<Integer> responses;
+
+        @Override
+        public void run() {
+            try {
+                this.responses = LumberjackUtil.sendMessages(port, null, Arrays.asList(15, 10));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
