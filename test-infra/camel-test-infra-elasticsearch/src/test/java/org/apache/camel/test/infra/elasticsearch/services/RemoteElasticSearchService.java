c118f03c4510783350914dfbf80c6795e8e377a6
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2020-11-02 10:29:13 +0100

    Moved the Elasticsearch test service infra-structure from CKC (#4549)
    Also adjust the elasticsearch component tests to use the new infra

---
+++ test-infra/camel-test-infra-elasticsearch/src/test/java/org/apache/camel/test/infra/elasticsearch/services/RemoteElasticSearchService.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.test.infra.elasticsearch.services;
+
+public class RemoteElasticSearchService implements ElasticSearchService {
+    private static final int ELASTIC_SEARCH_PORT = 9200;
+
+    @Override
+    public int getPort() {
+        String strPort = System.getProperty("elasticsearch.port");
+
+        if (strPort != null) {
+            return Integer.parseInt(strPort);
+        }
+
+        return ELASTIC_SEARCH_PORT;
+    }
+
+    @Override
+    public String getElasticSearchHost() {
+        return System.getProperty("elasticsearch.host");
+    }
+
+    @Override
+    public void initialize() {
+        // NO-OP
+    }
+
+    @Override
+    public void shutdown() {
+        // NO-OP
+    }
+}
