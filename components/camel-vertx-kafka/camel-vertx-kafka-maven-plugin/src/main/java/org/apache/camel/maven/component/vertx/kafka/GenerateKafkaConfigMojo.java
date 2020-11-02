8515be3ec20dd6f46a0b0f1971c94e800d5f9f24
Omar Al-Safi <omarsmak@gmail.com>
2020-11-02 17:00:42 +0100

    Start working on the pojo

--- components/camel-vertx-kafka/camel-vertx-kafka-maven-plugin/src/main/java/org/apache/camel/maven/component/vertx/kafka/GenerateKafkaConfigMojo.java
+++ components/camel-vertx-kafka/camel-vertx-kafka-maven-plugin/src/main/java/org/apache/camel/maven/component/vertx/kafka/GenerateKafkaConfigMojo.java
@@ -1,15 +1,25 @@
 package org.apache.camel.maven.component.vertx.kafka;
 
+import java.util.Map;
+
+import org.apache.camel.maven.component.vertx.kafka.config.ConfigFieldPojo;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "generate-kafka-config", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class GenerateKafkaConfigMojo extends AbstractMojo {
+
+    @Parameter
+    Map<String, ConfigFieldPojo> additionalConfigs;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
+        additionalConfigs.forEach((s, configFieldPojo) -> {
+            System.out.println(configFieldPojo.getName());
+        });
     }
 }
---
+++ components/camel-vertx-kafka/camel-vertx-kafka-maven-plugin/src/main/java/org/apache/camel/maven/component/vertx/kafka/config/ConfigFieldPojo.java
@@ -0,0 +1,17 @@
+package org.apache.camel.maven.component.vertx.kafka.config;
+
+public class ConfigFieldPojo {
+
+    private String name;
+
+    public ConfigFieldPojo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
