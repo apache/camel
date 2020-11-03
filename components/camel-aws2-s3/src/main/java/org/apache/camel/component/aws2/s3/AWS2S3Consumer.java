23200da84407f72836bed2a4245cb915639ac1e1
Filipe Portes <omeuefilipe@gmail.com>
2020-11-03 16:57:08 +0100

    includes Optional import

--- components/camel-aws2-s3/src/main/java/org/apache/camel/component/aws2/s3/AWS2S3Consumer.java
+++ components/camel-aws2-s3/src/main/java/org/apache/camel/component/aws2/s3/AWS2S3Consumer.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Queue;
 
 import org.apache.camel.AsyncCallback;
