85e8da6ff34fcbeaeefa0ea28b378dc29a6c4d3a
Luigi De Masi <ldemasi@redhat.com>
2020-11-06 03:56:32 +0100

    CAMEL-15822: [camel-file] fileExist=Move doesn't use FileMoveExistingStrategy when tempFile is also configured

--- components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/SftpDefaultMoveExistingFileStrategy.java
+++ components/camel-ftp/src/main/java/org/apache/camel/component/file/remote/SftpDefaultMoveExistingFileStrategy.java
@@ -26,6 +26,8 @@ import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.camel.component.file.MoveExistingFileStrategyUtils.completePartialRelativePath;
+
 public class SftpDefaultMoveExistingFileStrategy implements FileMoveExistingStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(SftpDefaultMoveExistingFileStrategy.class);
@@ -42,24 +44,29 @@ public class SftpDefaultMoveExistingFileStrategy implements FileMoveExistingStra
         // evaluation
         // we support only the following 3 tokens.
         Exchange dummy = endpoint.createExchange();
-        // we only support relative paths for the ftp component, so dont provide
-        // any parent
-        String parent = null;
+        // we only support relative paths for the ftp component, so we strip out
+        //any leading separator
+        String parent = FileUtil.stripLeadingSeparator(FileUtil.onlyPath(fileName));
         String onlyName = FileUtil.stripPath(fileName);
         dummy.getIn().setHeader(Exchange.FILE_NAME, fileName);
         dummy.getIn().setHeader(Exchange.FILE_NAME_ONLY, onlyName);
         dummy.getIn().setHeader(Exchange.FILE_PARENT, parent);
 
         String to = endpoint.getMoveExisting().evaluate(dummy, String.class);
+
         // we only support relative paths for the ftp component, so strip any
         // leading paths
         to = FileUtil.stripLeadingSeparator(to);
-        // normalize accordingly to configuration
-        to = ((SftpEndpoint) endpoint).getConfiguration().normalizePath(to);
+
         if (ObjectHelper.isEmpty(to)) {
             throw new GenericFileOperationFailedException(
                     "moveExisting evaluated as empty String, cannot move existing file: " + fileName);
         }
+
+        to = completePartialRelativePath(to, onlyName, parent);
+
+        // normalize accordingly to configuration
+        to = ((SftpEndpoint) endpoint).getConfiguration().normalizePath(to);
 
         // do we have a sub directory
         String dir = FileUtil.onlyPath(to);
--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpProducerMoveExistingTest.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpProducerMoveExistingTest.java
@@ -17,6 +17,10 @@
 package org.apache.camel.component.file.remote;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 import org.apache.camel.CamelExecutionException;
 import org.apache.camel.Exchange;
@@ -82,6 +86,78 @@ public class FtpProducerMoveExistingTest extends FtpServerTestSupport {
         assertFileExists(FTP_ROOT_DIR + "/move/renamed-hello.txt");
         assertEquals("Hello World",
                 context.getTypeConverter().convertTo(String.class, new File(FTP_ROOT_DIR + "/move/renamed-hello.txt")));
+    }
+
+    @Test
+    public void testExistingFileExistsTempFileNameMoveDynamicSubdir() throws Exception {
+        final String subdirPrefix = generateRandomString(5);
+        template.sendBodyAndHeader(getFtpUrl() + "&tempFileName=${file:onlyname}.temp&moveExisting=" + subdirPrefix
+                                   + "-${date:now:yyyyMMddHHmmssSSS}/",
+                "Hello World", Exchange.FILE_NAME, "hello.txt");
+        template.sendBodyAndHeader(getFtpUrl() + "&tempFileName=${file:onlyname}.temp&moveExisting=" + subdirPrefix
+                                   + "-${date:now:yyyyMMddHHmmssSSS}/",
+                "Bye World", Exchange.FILE_NAME, "hello.txt");
+        assertFileExists(FTP_ROOT_DIR + "/move/hello.txt");
+        assertEquals("Bye World",
+                context.getTypeConverter().convertTo(String.class, new File(FTP_ROOT_DIR + "/move/hello.txt")));
+
+        File folder = new File(FTP_ROOT_DIR + "/move");
+        String[] directories = folder.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File current, String name) {
+                String date = new SimpleDateFormat("yyyyMMdd").format(new Date());
+                return new File(current, name).isDirectory() && name.startsWith(subdirPrefix + "-" + date);
+            }
+        });
+        assertEquals(1, directories.length);
+        File movedFilePath = new File(FTP_ROOT_DIR + "/move/" + directories[0] + "/hello.txt");
+
+        assertTrue(movedFilePath.exists());
+        assertEquals("Hello World", context.getTypeConverter().convertTo(String.class, movedFilePath));
+    }
+
+    @Test
+    public void testExistingFileExistsTempFileNameMoveDynamicSubdirFullPath() throws Exception {
+        final String subdirPrefix = generateRandomString(5);
+        template.sendBodyAndHeader(getFtpUrl() + "&tempFileName=${file:onlyname}.temp&moveExisting=${file:parent}/"
+                                   + subdirPrefix + "-${date:now:yyyyMMddHHmmssSSS}/${file:onlyname}",
+                "Hello World", Exchange.FILE_NAME, "hello.txt");
+        template.sendBodyAndHeader(getFtpUrl() + "&tempFileName=${file:onlyname}.temp&moveExisting=${file:parent}/"
+                                   + subdirPrefix + "-${date:now:yyyyMMddHHmmssSSS}/${file:onlyname}",
+                "Bye World", Exchange.FILE_NAME, "hello.txt");
+        assertFileExists(FTP_ROOT_DIR + "/move/hello.txt");
+        assertEquals("Bye World",
+                context.getTypeConverter().convertTo(String.class, new File(FTP_ROOT_DIR + "/move/hello.txt")));
+
+        File folder = new File(FTP_ROOT_DIR + "/move");
+        String[] directories = folder.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File current, String name) {
+                String date = new SimpleDateFormat("yyyyMMdd").format(new Date());
+                return new File(current, name).isDirectory() && name.startsWith(subdirPrefix + "-" + date);
+            }
+        });
+        assertEquals(1, directories.length);
+        File movedFilePath = new File(FTP_ROOT_DIR + "/move/" + directories[0] + "/hello.txt");
+
+        assertTrue(movedFilePath.exists());
+        assertEquals("Hello World", context.getTypeConverter().convertTo(String.class, movedFilePath));
+    }
+
+    @Test
+    public void testExistingFileExistsTempFilenameMoveSubDir() throws Exception {
+        template.sendBodyAndHeader(getFtpUrl() + "&tempFileName=${file:onlyname}.temp&moveExisting=archive", "Hello World",
+                Exchange.FILE_NAME, "hello.txt");
+        template.sendBodyAndHeader(getFtpUrl() + "&tempFileName=${file:onlyname}.temp&moveExisting=archive", "Bye World",
+                Exchange.FILE_NAME, "hello.txt");
+
+        assertFileExists(FTP_ROOT_DIR + "/move/hello.txt");
+        assertEquals("Bye World",
+                context.getTypeConverter().convertTo(String.class, new File(FTP_ROOT_DIR + "/move/hello.txt")));
+
+        assertFileExists(FTP_ROOT_DIR + "/move/archive/hello.txt");
+        assertEquals("Hello World",
+                context.getTypeConverter().convertTo(String.class, new File(FTP_ROOT_DIR + "/move/archive/hello.txt")));
     }
 
     @Test
@@ -159,5 +235,17 @@ public class FtpProducerMoveExistingTest extends FtpServerTestSupport {
         assertFileExists(FTP_ROOT_DIR + "/move/renamed-hello.txt");
         assertEquals("Old file",
                 context.getTypeConverter().convertTo(String.class, new File(FTP_ROOT_DIR + "/move/renamed-hello.txt")));
+    }
+
+    private String generateRandomString(int targetStringLength) {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
     }
 }
