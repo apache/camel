90b1a2af4597eed7e26d4c35efd3bca3259a7121
Andrea Cosentino <ancosen@gmail.com>
2020-11-09 08:11:20 +0100

    CAMEL-15785 - Camel-AWS2: make possibile to use a client based on DefaultCredentialProvider in all the interested components - SNS

--- components/camel-aws2-sns/src/main/java/org/apache/camel/component/aws2/sns/Sns2Configuration.java
+++ components/camel-aws2-sns/src/main/java/org/apache/camel/component/aws2/sns/Sns2Configuration.java
@@ -65,6 +65,8 @@ public class Sns2Configuration implements Cloneable {
     private boolean trustAllCertificates;
     @UriParam(label = "common", defaultValue = "true")
     private boolean autoDiscoverClient = true;
+    @UriParam(defaultValue = "false")
+    private boolean useIAMCredentials;    
 
     public String getSubject() {
         return subject;
@@ -275,6 +277,18 @@ public class Sns2Configuration implements Cloneable {
      */
     public void setAutoDiscoverClient(boolean autoDiscoverClient) {
         this.autoDiscoverClient = autoDiscoverClient;
+    }
+    
+    public boolean isUseIAMCredentials() {
+        return useIAMCredentials;
+    }
+
+    /**
+     * Set whether the SQS client should expect to load credentials on an AWS infra instance or to expect static
+     * credentials to be passed in.
+     */
+    public void setUseIAMCredentials(boolean useIAMCredentials) {
+        this.useIAMCredentials = useIAMCredentials;
     }
 
     // *************************************************
