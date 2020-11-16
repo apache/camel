973fb17f40d0a52e3d66deab500027b7002bfda0
Andrea Cosentino <ancosen@gmail.com>
2020-11-16 14:51:30 +0100

    CAMEL-15836 - Camel-AWS2-IAM: enable autowire on the Iam client

--- components/camel-aws2-iam/src/main/java/org/apache/camel/component/aws2/iam/IAM2Configuration.java
+++ components/camel-aws2-iam/src/main/java/org/apache/camel/component/aws2/iam/IAM2Configuration.java
@@ -32,6 +32,7 @@ public class IAM2Configuration implements Cloneable {
     @Metadata(required = true)
     private String label;
     @UriParam
+    @Metadata(autowired = true)
     private IamClient iamClient;
     @UriParam(label = "security", secret = true)
     private String accessKey;
@@ -52,8 +53,6 @@ public class IAM2Configuration implements Cloneable {
     private boolean pojoRequest;
     @UriParam(defaultValue = "false")
     private boolean trustAllCertificates;
-    @UriParam(label = "common", defaultValue = "true")
-    private boolean autoDiscoverClient = true;
 
     public IamClient getIamClient() {
         return iamClient;
@@ -164,18 +163,6 @@ public class IAM2Configuration implements Cloneable {
      */
     public void setTrustAllCertificates(boolean trustAllCertificates) {
         this.trustAllCertificates = trustAllCertificates;
-    }
-
-    public boolean isAutoDiscoverClient() {
-        return autoDiscoverClient;
-    }
-
-    /**
-     * Setting the autoDiscoverClient mechanism, if true, the component will look for a client instance in the registry
-     * automatically otherwise it will skip that checking.
-     */
-    public void setAutoDiscoverClient(boolean autoDiscoverClient) {
-        this.autoDiscoverClient = autoDiscoverClient;
     }
 
     // *************************************************
