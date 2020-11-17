7b5433e6e7adccbad8a02e46218b665d9edb78d0
Andrea Cosentino <ancosen@gmail.com>
2020-11-17 08:37:38 +0100

    CAMEL-15836 - Camel-AWS2-Lambda: enable autowire on the Lambda Client, CS

--- components/camel-aws2-lambda/src/test/java/org/apache/camel/component/aws2/lambda/LambdaComponentClientRegistryTest.java
+++ components/camel-aws2-lambda/src/test/java/org/apache/camel/component/aws2/lambda/LambdaComponentClientRegistryTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
