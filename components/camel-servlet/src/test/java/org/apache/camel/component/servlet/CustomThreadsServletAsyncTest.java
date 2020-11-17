63ea631c01fd125b35f918e21d36fd093357dd7a
Romain Manni-Bucau <rmannibucau@gmail.com>
2020-11-17 10:16:32 +0100

    [CAMEL-11731] ensure camel-servlet can be used in really async mode

---
+++ components/camel-servlet/src/test/java/org/apache/camel/component/servlet/CustomThreadsServletAsyncTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.servlet;
+
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import org.springframework.web.context.ContextLoaderListener;
+
+public class CustomThreadsServletAsyncTest extends ServletAsyncTest {
+    @Override
+    protected DeploymentInfo getDeploymentInfo() {
+        return Servlets.deployment()
+                .setClassLoader(getClass().getClassLoader())
+                .setContextPath(CONTEXT)
+                .setDeploymentName(getClass().getName())
+                .addInitParameter("contextConfigLocation",
+                        "classpath:org/apache/camel/component/servlet/example-camelContext-pool.xml")
+                .addListener(Servlets.listener(ContextLoaderListener.class))
+                .addServlet(Servlets.servlet("CamelServlet", CamelHttpTransportServlet.class)
+                        .addInitParam("async", "true")
+                        .addInitParam("executorRef", "servletThreads")
+                        .setLoadOnStartup(1)
+                        .setAsyncSupported(true)
+                        .addMapping("/services/*"));
+    }
+
+}
