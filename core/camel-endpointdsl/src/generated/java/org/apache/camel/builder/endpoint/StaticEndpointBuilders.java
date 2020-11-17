ab42de089f5b4028795c0bb976a68a91c1bba3d9
Andrea Cosentino <ancosen@gmail.com>
2020-11-17 11:55:59 +0100

    Camel-AWS2-Eventbridge: Regen

--- core/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
+++ core/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
@@ -1182,10 +1182,6 @@ public class StaticEndpointBuilders {
      * Path parameter: eventbusNameOrArn (required)
      * Event bus name or ARN
      * 
-     * Path parameter: eventbusName (required)
-     * Logical name
-     * Default value: default
-     * 
      * @param path //eventbusNameOrArn
      */
     public static org.apache.camel.builder.endpoint.dsl.EventbridgeEndpointBuilderFactory.EventbridgeEndpointBuilder aws2Eventbridge(
@@ -1204,10 +1200,6 @@ public class StaticEndpointBuilders {
      * 
      * Path parameter: eventbusNameOrArn (required)
      * Event bus name or ARN
-     * 
-     * Path parameter: eventbusName (required)
-     * Logical name
-     * Default value: default
      * 
      * @param componentName to use a custom component name for the endpoint
      * instead of the default name
