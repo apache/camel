ac8f355912867e68a0b4f72fdedd83630145ea37
Otavio Rodolfo Piske <opiske@redhat.com>
2020-11-18 19:22:43 +0100

    Added test infra for AWS v2 Cloud Watch
    Also migrates the camel-aws2-cw tests to the new test infra

--- components/camel-aws2-cw/src/test/java/org/apache/camel/component/aws2/cw/localstack/Aws2CwBaseTest.java
+++ components/camel-aws2-cw/src/test/java/org/apache/camel/component/aws2/cw/localstack/Aws2CwBaseTest.java
@@ -16,61 +16,25 @@
  */
 package org.apache.camel.component.aws2.cw.localstack;
 
-import java.net.URI;
-
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.aws2.cw.Cw2Component;
-import org.apache.camel.test.testcontainers.junit5.ContainerAwareTestSupport;
-import org.apache.camel.test.testcontainers.junit5.Wait;
+import org.apache.camel.test.infra.aws.common.services.AWSService;
+import org.apache.camel.test.infra.aws2.clients.AWSSDKClientUtils;
+import org.apache.camel.test.infra.aws2.services.AWSServiceFactory;
+import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.TestInstance;
-import org.testcontainers.containers.GenericContainer;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class Aws2CwBaseTest extends ContainerAwareTestSupport {
-
-    public static final String CONTAINER_IMAGE = "localstack/localstack:0.12.2";
-    public static final String CONTAINER_NAME = "cloudwatch";
-
-    @Override
-    protected GenericContainer<?> createContainer() {
-        return localstackContainer();
-    }
-
-    public static GenericContainer localstackContainer() {
-        return new GenericContainer(CONTAINER_IMAGE)
-                .withNetworkAliases(CONTAINER_NAME)
-                .withEnv("SERVICES", "cloudwatch")
-                .withExposedPorts(4566)
-                .waitingFor(Wait.forListeningPort())
-                .waitingFor(Wait.forLogMessageContaining("Ready.", 1));
-    }
-
-    public String getEventbridgeUrl() {
-        return String.format(
-                "%s:%d",
-                getContainerHost(CONTAINER_NAME),
-                getContainerPort(CONTAINER_NAME, 4566));
-    }
-
-    public CloudWatchClient getCwClient() {
-        CloudWatchClient eventbridgeClient = CloudWatchClient
-                .builder()
-                .endpointOverride(URI.create("http://" + getEventbridgeUrl()))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("xxx", "yyy")))
-                .region(Region.EU_WEST_1)
-                .build();
-        return eventbridgeClient;
-    }
+public class Aws2CwBaseTest extends CamelTestSupport {
+    @RegisterExtension
+    public static AWSService service = AWSServiceFactory.createCloudWatchService();
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
         CamelContext context = super.createCamelContext();
         Cw2Component cwComponent = context.getComponent("aws2-cw", Cw2Component.class);
-        cwComponent.getConfiguration().setAmazonCwClient(getCwClient());
+        cwComponent.getConfiguration().setAmazonCwClient(AWSSDKClientUtils.newCloudWatchClient());
         return context;
     }
 }
