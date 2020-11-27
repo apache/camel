8e6b526bc41101541583332e406517fddd19ddde
Andrea Cosentino <ancosen@gmail.com>
2020-11-27 14:07:01 +0100

    Fixed CS and regen

--- components/camel-hazelcast/src/main/java/org/apache/camel/component/hazelcast/map/HazelcastMapConsumer.java
+++ components/camel-hazelcast/src/main/java/org/apache/camel/component/hazelcast/map/HazelcastMapConsumer.java
@@ -25,7 +25,6 @@ import org.apache.camel.Processor;
 import org.apache.camel.component.hazelcast.HazelcastDefaultConsumer;
 import org.apache.camel.component.hazelcast.listener.CamelMapListener;
 
-
 public class HazelcastMapConsumer extends HazelcastDefaultConsumer {
 
     private final IMap<Object, Object> cache;
