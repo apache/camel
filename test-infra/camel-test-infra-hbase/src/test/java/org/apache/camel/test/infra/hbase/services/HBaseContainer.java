bd6299b2f9d5bb2fb7685921791eef540354e94f
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2020-11-29 16:46:58 +0100

    Migrates camel-hbase to the new test infra (#4700)

--- test-infra/camel-test-infra-hbase/src/test/java/org/apache/camel/test/infra/hbase/services/HBaseContainer.java
+++ test-infra/camel-test-infra-hbase/src/test/java/org/apache/camel/test/infra/hbase/services/HBaseContainer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.hbase;
+package org.apache.camel.test.infra.hbase.services;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
