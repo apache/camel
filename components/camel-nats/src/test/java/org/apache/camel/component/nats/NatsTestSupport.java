98e06884635b456c1b32ea01b5064e5ff6411eff
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2020-12-01 16:40:35 +0100

    Migrates camel-nats to the new test infra (#4706)

--- components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsTestSupport.java
+++ components/camel-nats/src/test/java/org/apache/camel/component/nats/NatsTestSupport.java
@@ -17,39 +17,20 @@
 package org.apache.camel.component.nats;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.test.testcontainers.junit5.ContainerAwareTestSupport;
-import org.apache.camel.test.testcontainers.junit5.Wait;
-import org.testcontainers.containers.GenericContainer;
+import org.apache.camel.test.infra.nats.services.NatsService;
+import org.apache.camel.test.infra.nats.services.NatsServiceFactory;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-import static org.apache.camel.component.nats.NatsTestConstants.CONTAINER_IMAGE;
-
-public class NatsTestSupport extends ContainerAwareTestSupport {
-
-    public static final String CONTAINER_NAME = "nats";
-
-    @Override
-    protected GenericContainer<?> createContainer() {
-        return natsContainer();
-    }
-
-    public static GenericContainer natsContainer() {
-        return new GenericContainer(CONTAINER_IMAGE)
-                .withNetworkAliases(CONTAINER_NAME)
-                .waitingFor(Wait.forLogMessageContaining("Listening for route connections", 1));
-    }
-
-    public String getNatsBrokerUrl() {
-        return String.format(
-                "%s:%d",
-                getContainerHost(CONTAINER_NAME),
-                getContainerPort(CONTAINER_NAME, 4222));
-    }
+public class NatsTestSupport extends CamelTestSupport {
+    @RegisterExtension
+    static NatsService service = NatsServiceFactory.createService();
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
         CamelContext context = super.createCamelContext();
         NatsComponent nats = context.getComponent("nats", NatsComponent.class);
-        nats.setServers(getNatsBrokerUrl());
+        nats.setServers(service.getServiceAddress());
         return context;
     }
 }
