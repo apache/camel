3f46a60778f169505663700095779e2d407f020e
Liroyd <alex_liroyd@yahoo.com>
2020-12-04 14:56:59 +0200

    CAMEL-15917: Resilience4j Property Component doesn't work for configurationRef Support property placeholder for ResilienceReifier and FaultToleranceReifier circuit breaker configurationRef in routes definitions

--- components/camel-microprofile-fault-tolerance/src/main/java/org/apache/camel/component/microprofile/faulttolerance/FaultToleranceReifier.java
+++ components/camel-microprofile-fault-tolerance/src/main/java/org/apache/camel/component/microprofile/faulttolerance/FaultToleranceReifier.java
@@ -146,7 +146,7 @@ public class FaultToleranceReifier extends ProcessorReifier<CircuitBreakerDefini
         // Extract properties from referenced configuration, the one configured
         // on camel context takes the precedence over those in the registry
         if (definition.getConfigurationRef() != null) {
-            final String ref = definition.getConfigurationRef();
+            final String ref = parseString(definition.getConfigurationRef());
 
             loadProperties(properties, Suppliers.firstNotNull(
                 () -> camelContext.getExtension(Model.class).getFaultToleranceConfiguration(ref),
