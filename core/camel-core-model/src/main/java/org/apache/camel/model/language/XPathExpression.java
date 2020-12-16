e34f10d0f3122f2b3d3815ba09bf76fc6f798109
Claus Ibsen <claus.ibsen@gmail.com>
2020-12-16 14:51:48 +0100

    Polished description

--- core/camel-core-model/src/main/java/org/apache/camel/model/language/XPathExpression.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/language/XPathExpression.java
@@ -225,8 +225,8 @@ public class XPathExpression extends NamespaceAwareExpression {
      * default.
      *
      * This can be used to turn off, for example in cases the compilation phase is desired at the starting phase, such
-     * as if the application is pre-built with graalvm which would then load the xpath factory of the built operating
-     * system, and not a JVM runtime.
+     * as if the application is ahead of time compiled (for example with camel-quarkus) which would then load the xpath
+     * factory of the built operating system, and not a JVM runtime.
      */
     public void setPreCompile(String preCompile) {
         this.preCompile = preCompile;
