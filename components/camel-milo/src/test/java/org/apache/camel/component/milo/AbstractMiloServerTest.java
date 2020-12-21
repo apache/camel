89eee4fdf08acad4d67b5a996520651083227bbd
Andrea Cosentino <ancosen@gmail.com>
2020-12-21 11:50:02 +0100

    XPath test should run correctly with JDK 16

--- components/camel-milo/src/test/java/org/apache/camel/component/milo/AbstractMiloServerTest.java
+++ components/camel-milo/src/test/java/org/apache/camel/component/milo/AbstractMiloServerTest.java
@@ -36,118 +36,119 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public abstract class AbstractMiloServerTest extends CamelTestSupport {
 
-    private int serverPort;
+	private int serverPort;
 
-    @Override
-    protected void doPreSetup() throws Exception {
-        assumeTrue(isJavaVersionSatisfied(9), "Requires java 9+");
-        super.doPreSetup();
-        this.serverPort = AvailablePortFinder.getNextAvailable();
-    }
+	@Override
+	protected void doPreSetup() throws Exception {
+		assumeTrue(isJavaVersionSatisfied(9), "Requires java 9+");
+		super.doPreSetup();
+		this.serverPort = AvailablePortFinder.getNextAvailable();
+	}
 
-    public int getServerPort() {
-        return this.serverPort;
-    }
+	public int getServerPort() {
+		return this.serverPort;
+	}
 
-    protected boolean isAddServer() {
-        return true;
-    }
+	protected boolean isAddServer() {
+		return true;
+	}
 
-    /**
-     * Replace the port placeholder with the dynamic server port
-     * 
-     * @param  uri the URI to process
-     * @return     the result, may be {@code null} if the input is {@code null}
-     */
-    protected String resolve(String uri) {
-        if (uri == null) {
-            return uri;
-        }
+	/**
+	 * Replace the port placeholder with the dynamic server port
+	 * 
+	 * @param uri the URI to process
+	 * @return the result, may be {@code null} if the input is {@code null}
+	 */
+	protected String resolve(String uri) {
+		if (uri == null) {
+			return uri;
+		}
 
-        return uri.replace("@@port@@", Integer.toString(this.serverPort));
-    }
+		return uri.replace("@@port@@", Integer.toString(this.serverPort));
+	}
 
-    public static void testBody(final AssertionClause clause, final Consumer<DataValue> valueConsumer) {
-        testBody(clause, DataValue.class, valueConsumer);
-    }
+	public static void testBody(final AssertionClause clause, final Consumer<DataValue> valueConsumer) {
+		testBody(clause, DataValue.class, valueConsumer);
+	}
 
-    public static <T> void testBody(final AssertionClause clause, final Class<T> bodyClass, final Consumer<T> valueConsumer) {
-        clause.predicate(exchange -> {
-            final T body = exchange.getIn().getBody(bodyClass);
-            valueConsumer.accept(body);
-            return true;
-        });
-    }
+	public static <T> void testBody(final AssertionClause clause, final Class<T> bodyClass,
+			final Consumer<T> valueConsumer) {
+		clause.predicate(exchange -> {
+			final T body = exchange.getIn().getBody(bodyClass);
+			valueConsumer.accept(body);
+			return true;
+		});
+	}
 
-    public static Consumer<DataValue> assertGoodValue(final Object expectedValue) {
-        return value -> {
-            assertNotNull(value);
-            assertEquals(expectedValue, value.getValue().getValue());
-            assertTrue(value.getStatusCode().isGood());
-            assertFalse(value.getStatusCode().isBad());
-        };
-    }
+	public static Consumer<DataValue> assertGoodValue(final Object expectedValue) {
+		return value -> {
+			assertNotNull(value);
+			assertEquals(expectedValue, value.getValue().getValue());
+			assertTrue(value.getStatusCode().isGood());
+			assertFalse(value.getStatusCode().isBad());
+		};
+	}
 
-    @Override
-    protected CamelContext createCamelContext() throws Exception {
-        final CamelContext context = super.createCamelContext();
-        configureContext(context);
-        return context;
-    }
+	@Override
+	protected CamelContext createCamelContext() throws Exception {
+		final CamelContext context = super.createCamelContext();
+		configureContext(context);
+		return context;
+	}
 
-    protected void configureContext(final CamelContext context) throws Exception {
-        if (isAddServer()) {
-            final MiloServerComponent server = context.getComponent("milo-server", MiloServerComponent.class);
-            configureMiloServer(server);
-        }
-    }
+	protected void configureContext(final CamelContext context) throws Exception {
+		if (isAddServer()) {
+			final MiloServerComponent server = context.getComponent("milo-server", MiloServerComponent.class);
+			configureMiloServer(server);
+		}
+	}
 
-    protected void configureMiloServer(final MiloServerComponent server) throws Exception {
-        server.setBindAddresses("localhost");
-        server.setPort(this.serverPort);
-        server.setUserAuthenticationCredentials("foo:bar,foo2:bar2");
-        server.setUsernameSecurityPolicyUri(SecurityPolicy.None);
-        server.setSecurityPoliciesById("None");
-    }
+	protected void configureMiloServer(final MiloServerComponent server) throws Exception {
+		server.setBindAddresses("localhost");
+		server.setPort(this.serverPort);
+		server.setUserAuthenticationCredentials("foo:bar,foo2:bar2");
+		server.setUsernameSecurityPolicyUri(SecurityPolicy.None);
+		server.setSecurityPoliciesById("None");
+	}
 
-    /**
-     * Create a default key store for testing
-     *
-     * @return always returns a key store
-     */
-    protected KeyStoreLoader.Result loadDefaultTestKey() {
-        try {
+	/**
+	 * Create a default key store for testing
+	 *
+	 * @return always returns a key store
+	 */
+	protected KeyStoreLoader.Result loadDefaultTestKey() {
+		try {
 
-            final KeyStoreLoader loader = new KeyStoreLoader();
-            loader.setUrl("file:src/test/resources/keystore");
-            loader.setKeyStorePassword("testtest");
+			final KeyStoreLoader loader = new KeyStoreLoader();
+			loader.setUrl("file:src/test/resources/keystore");
+			loader.setKeyStorePassword("testtest");
 
-            loader.setKeyPassword("test");
-            return loader.load();
-        } catch (final GeneralSecurityException | IOException e) {
-            throw new RuntimeException(e);
-        }
+			loader.setKeyPassword("test");
+			return loader.load();
+		} catch (final GeneralSecurityException | IOException e) {
+			throw new RuntimeException(e);
+		}
 
-    }
+	}
 
-    /**
-     * Return true, if java version (defined by method getRequiredJavaVersion()) is satisfied. Works for java versions
-     * 9+
-     */
-    boolean isJavaVersionSatisfied(int requiredVersion) {
-        String version = System.getProperty("java.version");
-        if (!version.startsWith("1.")) {
-            int dot = version.indexOf('.');
-            if (dot != -1) {
-                version = version.substring(0, dot);
-            }
-            if (version.equalsIgnoreCase("16-ea")) {
-                return true;
-            } else if (Integer.parseInt(version) >= requiredVersion) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Return true, if java version (defined by method getRequiredJavaVersion()) is
+	 * satisfied. Works for java versions 9+
+	 */
+	boolean isJavaVersionSatisfied(int requiredVersion) {
+		String version = System.getProperty("java.version");
+		if (!version.startsWith("1.")) {
+			int dot = version.indexOf('.');
+			if (dot != -1) {
+				version = version.substring(0, dot);
+			}
+			if (version.equalsIgnoreCase("16-ea")) {
+				return true;
+			} else if (Integer.parseInt(version) >= requiredVersion) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
