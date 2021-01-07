5a56964bce4d4b0d747b5aed394f8df50239910e
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2021-01-07 13:50:37 +0100

    Make the sensitive string check easier to reuse so that we can eval and filter them (#4834) (#4835)

---
+++ core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.util;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public final class SensitiveUtils {
+    public static final String SENSITIVE_KEYS
+            = "passphrase|password|secretkey|accesstoken|clientsecret|authorizationtoken|sasljaasconfig|accesskey";
+
+    private SensitiveUtils() {
+
+    }
+
+    public static boolean containsSensitive(String text) {
+        return Arrays.stream(SENSITIVE_KEYS.split("\\|")).anyMatch(s -> text.toLowerCase(Locale.ENGLISH).contains(s));
+    }
+}
