c70abb2b7af33652246a6ab2430c41865cb7cf2b
javaduke <Eugene.berman@modusbox.com>
2021-01-13 01:42:03 -0700

    Added clientRequestValidation parameter (#4869)

--- tooling/maven/camel-restdsl-swagger-plugin/src/main/java/org/apache/camel/maven/generator/swagger/GenerateXmlMojo.java
+++ tooling/maven/camel-restdsl-swagger-plugin/src/main/java/org/apache/camel/maven/generator/swagger/GenerateXmlMojo.java
@@ -81,6 +81,9 @@ public class GenerateXmlMojo extends AbstractGenerateMojo {
 
         if (restConfiguration) {
             generator.withRestComponent(findAppropriateComponent());
+            if (clientRequestValidation) {
+                generator.withClientRequestValidation();
+            }
         }
 
         try {
