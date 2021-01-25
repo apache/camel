dc8e4bb3788f56f5112ba1fe24ca3444ab3e93b1
Claus Ibsen <claus.ibsen@gmail.com>
2021-01-25 09:49:51 +0100

    Allow to reset build taken for native builds that are super fast.

--- core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/AbstractCamelContext.java
+++ core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/AbstractCamelContext.java
@@ -2548,10 +2548,6 @@ public abstract class AbstractCamelContext extends BaseService
             }
         }
 
-        LOG.warn("TODO Sleeping for 3 sec in doBuild START");
-        Thread.sleep(3000);
-        LOG.warn("TODO Sleeping for 3 sec in doBuild DONE");
-
         startupStepRecorder.start();
         StartupStep step = startupStepRecorder.beginStep(CamelContext.class, null, "Build CamelContext");
 
