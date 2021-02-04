bab7ebafd4a0391347ff922796eab4a38f511558
Claus Ibsen <claus.ibsen@gmail.com>
2021-02-04 09:26:01 +0100

    CAMEL-16083: Route Scoped OnCompletion After Consumer routeId check

--- core/camel-api/src/generated/java/org/apache/camel/ExchangeConstantProvider.java
+++ core/camel-api/src/generated/java/org/apache/camel/ExchangeConstantProvider.java
@@ -11,7 +11,7 @@ public class ExchangeConstantProvider {
 
     private static final Map<String, String> MAP;
     static {
-        Map<String, String> map = new HashMap<>(149);
+        Map<String, String> map = new HashMap<>(150);
         map.put("ACCEPT_CONTENT_TYPE", "CamelAcceptContentType");
         map.put("AGGREGATED_COLLECTION_GUARD", "CamelAggregatedCollectionGuard");
         map.put("AGGREGATED_COMPLETED_BY", "CamelAggregatedCompletedBy");
@@ -113,6 +113,7 @@ public class ExchangeConstantProvider {
         map.put("MULTICAST_INDEX", "CamelMulticastIndex");
         map.put("NOTIFY_EVENT", "CamelNotifyEvent");
         map.put("ON_COMPLETION", "CamelOnCompletion");
+        map.put("ON_COMPLETION_ROUTE_IDS", "CamelOnCompletionRouteIds");
         map.put("OVERRULE_FILE_NAME", "CamelOverruleFileName");
         map.put("PARENT_UNIT_OF_WORK", "CamelParentUnitOfWork");
         map.put("RECEIVED_TIMESTAMP", "CamelReceivedTimestamp");
