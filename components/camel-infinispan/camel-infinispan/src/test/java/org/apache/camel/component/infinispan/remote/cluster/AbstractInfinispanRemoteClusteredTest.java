4ceccc1b2fbb6e807a4ce3976bf5e77631194436
Otavio Rodolfo Piske <opiske@redhat.com>
2021-02-05 11:28:14 +0100

    Minor performance improvements to the infinispan remote clustered tests
    Reorganizes the tests, using JUnit 5 nested classes, in order to reduce the test duration for
    these them

--- components/camel-infinispan/camel-infinispan/src/test/java/org/apache/camel/component/infinispan/remote/cluster/AbstractInfinispanRemoteClusteredTest.java
+++ components/camel-infinispan/camel-infinispan/src/test/java/org/apache/camel/component/infinispan/remote/cluster/AbstractInfinispanRemoteClusteredTest.java
@@ -21,15 +21,24 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.cluster.ClusteredRoutePolicy;
+import org.apache.camel.impl.cluster.ClusteredRoutePolicyFactory;
 import org.apache.camel.test.infra.infinispan.services.InfinispanService;
 import org.apache.camel.test.infra.infinispan.services.InfinispanServiceFactory;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.slf4j.Logger;
@@ -38,47 +47,169 @@ import org.slf4j.LoggerFactory;
 import static org.apache.camel.component.infinispan.remote.cluster.InfinispanRemoteClusteredTestSupport.createCache;
 import static org.apache.camel.component.infinispan.remote.cluster.InfinispanRemoteClusteredTestSupport.createConfiguration;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
-abstract class AbstractInfinispanRemoteClusteredTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AbstractInfinispanRemoteClusteredTest {
     @RegisterExtension
     public static InfinispanService service = InfinispanServiceFactory.createService();
 
-    @Timeout(value = 1, unit = TimeUnit.MINUTES)
-    @Test
-    public void test() throws Exception {
-        final Logger logger = LoggerFactory.getLogger(getClass());
+    private RemoteCacheManager cacheContainer;
+
+    private final String viewName = "myView";
+
+    class RunnerEnv {
+        int events;
+        CountDownLatch latch;
+        String id;
+    }
+
+    @BeforeEach
+    public void setUpManager() {
+        Configuration configuration = createConfiguration(service);
+        cacheContainer = new RemoteCacheManager(configuration);
+        createCache(cacheContainer, viewName);
+    }
+
+    public void runTest(Function<RunnerEnv, RouteBuilder> routeBuilderFunction) throws Exception {
         final List<String> clients = IntStream.range(0, 3).mapToObj(Integer::toString).collect(Collectors.toList());
         final List<String> results = new ArrayList<>();
-        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(clients.size() * 2);
+
         final CountDownLatch latch = new CountDownLatch(clients.size());
-        final String viewName = "myView";
+        final Logger logger = LoggerFactory.getLogger(getClass());
 
-        Configuration configuration = createConfiguration(service);
+        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(clients.size() * 2);
 
-        try (RemoteCacheManager cacheContainer = new RemoteCacheManager(configuration)) {
-            createCache(cacheContainer, viewName);
+        for (String id : clients) {
 
-            for (String id : clients) {
-                scheduler.submit(() -> {
-                    try {
-                        run(cacheContainer, viewName, id);
-                        logger.debug("Node {} is shutting down", id);
-                        results.add(id);
-                    } catch (Exception e) {
-                        logger.warn("", e);
-                    } finally {
-                        latch.countDown();
-                    }
-                });
-            }
+            scheduler.submit(() -> {
+                try {
+                    run(id, routeBuilderFunction);
 
-            latch.await();
-            scheduler.shutdownNow();
+                    logger.debug("Node {} is shutting down", id);
+                    results.add(id);
+                } catch (Exception e) {
+                    logger.warn("Failed to run job: {}", e.getMessage(), e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
 
-            assertThat(results).hasSameSizeAs(clients);
-            assertThat(results).containsAll(clients);
+        latch.await();
+        scheduler.shutdownNow();
+
+        assertThat(results).hasSameSizeAs(clients);
+        assertThat(results).containsAll(clients);
+    }
+
+    //Set up a single node cluster.
+    private InfinispanRemoteClusterService getInfinispanRemoteClusterService(String id) {
+        InfinispanRemoteClusterService clusterService = new InfinispanRemoteClusterService();
+
+        clusterService.setCacheContainer(cacheContainer);
+        clusterService.setId("node-" + id);
+
+        return clusterService;
+    }
+
+    public void run(String id, Function<RunnerEnv, RouteBuilder> routeBuilderFunction) {
+        int events = ThreadLocalRandom.current().nextInt(2, 6);
+        CountDownLatch contextLatch = new CountDownLatch(events);
+
+        InfinispanRemoteClusterService clusterService = getInfinispanRemoteClusterService(id);
+
+        try (DefaultCamelContext context = new DefaultCamelContext()) {
+            context.disableJMX();
+            context.setName("context-" + id);
+            context.addService(clusterService);
+
+            RunnerEnv contextEnv = new RunnerEnv();
+
+            contextEnv.id = id;
+            contextEnv.latch = contextLatch;
+            contextEnv.events = events;
+
+            RouteBuilder routeBuilder = routeBuilderFunction.apply(contextEnv);
+
+            context.addRoutes(routeBuilder);
+
+            // Start the context after some random time so the startup order
+            // changes for each test.
+            Thread.sleep(ThreadLocalRandom.current().nextInt(500));
+            context.start();
+
+            contextLatch.await();
+        } catch (Exception e) {
+            fail(e.getMessage());
         }
     }
 
-    protected abstract void run(RemoteCacheManager cacheContainer, String namespace, String id) throws Exception;
+    @Nested
+    class InfinispanRemoteClusteredMasterTestNested {
+        public RouteBuilder getRouteBuilder(RunnerEnv runnerEnv) {
+            return new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    fromF("timer:%s?delay=1000&period=1000&repeatCount=%d", runnerEnv.id, runnerEnv.events)
+                            .routeId("route-" + runnerEnv.id)
+                            .log("From id=${routeId} counter=${header.CamelTimerCounter}")
+                            .process(e -> runnerEnv.latch.countDown());
+                }
+            };
+        }
+
+        @Timeout(value = 1, unit = TimeUnit.MINUTES)
+        @Test
+        public void test() throws Exception {
+            runTest(this::getRouteBuilder);
+        }
+    }
+
+    @Nested
+    class InfinispanRemoteClusteredRoutePolicyFactoryTestNested {
+        public RouteBuilder getRouteBuilder(RunnerEnv runnerEnv) {
+            return new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    this.getContext().addRoutePolicyFactory(ClusteredRoutePolicyFactory.forNamespace(viewName));
+
+                    fromF("timer:%s?delay=1000&period=1000&repeatCount=%d", runnerEnv.id, runnerEnv.events)
+                            .routeId("route-" + runnerEnv.id)
+                            .log("From id=${routeId} counter=${header.CamelTimerCounter}")
+                            .process(e -> runnerEnv.latch.countDown());
+                }
+            };
+        }
+
+        @Timeout(value = 1, unit = TimeUnit.MINUTES)
+        @Test
+        public void test() throws Exception {
+            runTest(this::getRouteBuilder);
+        }
+    }
+
+    @Nested
+    class InfinispanRemoteClusteredRoutePolicyTestNested {
+        public RouteBuilder getRouteBuilder(RunnerEnv runnerEnv) {
+            return new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    this.getContext().addRoutePolicyFactory(ClusteredRoutePolicyFactory.forNamespace(viewName));
+
+                    fromF("timer:%s?delay=1000&period=1000&repeatCount=%d", runnerEnv.id, runnerEnv.events)
+                            .routeId("route-" + runnerEnv.id)
+                            .routePolicy(ClusteredRoutePolicy.forNamespace(viewName))
+                            .log("From id=${routeId} counter=${header.CamelTimerCounter}")
+                            .process(e -> runnerEnv.latch.countDown());
+                }
+            };
+        }
+
+        @Timeout(value = 1, unit = TimeUnit.MINUTES)
+        @Test
+        public void test() throws Exception {
+            runTest(this::getRouteBuilder);
+        }
+    }
 }
