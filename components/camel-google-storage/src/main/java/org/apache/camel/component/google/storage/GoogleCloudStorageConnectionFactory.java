01699e858a83e0a53b955a491d17e9b859865aa3
Raffaele Marcello <marcelloraffaele@gmail.com>
2021-02-14 20:25:52 +0100

    CAMEL-15964 create camel-google-storage component

--- components/camel-google-storage/src/main/java/org/apache/camel/component/google/storage/GoogleCloudStorageConnectionFactory.java
+++ components/camel-google-storage/src/main/java/org/apache/camel/component/google/storage/GoogleCloudStorageConnectionFactory.java
@@ -1,5 +1,3 @@
-package org.apache.camel.component.google.storage;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ package org.apache.camel.component.google.storage;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.camel.component.google.storage;
+
 import java.io.FileInputStream;
 
 import com.google.api.client.util.Strings;
