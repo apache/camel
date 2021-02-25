c98b572f69a8484bfa8f7aef0078bc408e11f208
Claus Ibsen <claus.ibsen@gmail.com>
2021-02-25 20:33:18 +0100

    Revert "camel-http - If payload is byte[] then use byte array entity as its most optimal"
    This reverts commit b91db4b410ba21552ee4ab0f483ea983dba75bd3.

--- components/camel-http/src/main/java/org/apache/camel/component/http/HttpProducer.java
+++ components/camel-http/src/main/java/org/apache/camel/component/http/HttpProducer.java
@@ -676,12 +676,6 @@ public class HttpProducer extends DefaultProducer {
                                 answer = new FileEntity(file);
                             }
                         }
-                    } else if (data instanceof byte[]) {
-                        ByteArrayEntity entity = new ByteArrayEntity((byte[]) data);
-                        if (contentType != null) {
-                            entity.setContentType(contentType.toString());
-                        }
-                        answer = entity;
                     } else if (data instanceof String) {
                         // be a bit careful with String as any type can most likely be converted to String
                         // so we only do an instanceof check and accept String if the body is really a String
@@ -696,9 +690,7 @@ public class HttpProducer extends DefaultProducer {
                             }
                         }
                         StringEntity entity = new StringEntity((String) data, charset);
-                        if (contentType != null) {
-                            entity.setContentType(contentType.toString());
-                        }
+                        entity.setContentType(contentType != null ? contentType.toString() : null);
                         answer = entity;
                     }
 
