592ae2ffca8cfffbc00378ef29b9426df79af07b
Freeman(Yue) Fang <freeman.fang@gmail.com>
2021-03-02 00:02:14 -0500

    [CAMEL-16063]don't start camel context if event is from the self management ApplicationContext (#5149)

--- components/camel-spring-xml/src/main/java/org/apache/camel/spring/xml/CamelContextFactoryBean.java
+++ components/camel-spring-xml/src/main/java/org/apache/camel/spring/xml/CamelContextFactoryBean.java
@@ -409,7 +409,6 @@ public class CamelContextFactoryBean extends AbstractCamelContextFactoryBean<Spr
         } catch (Exception e) {
             throw wrapRuntimeCamelException(e);
         }
-
         // when the routes are setup we need to start the Camel context
         context.start();
     }
@@ -458,6 +457,14 @@ public class CamelContextFactoryBean extends AbstractCamelContextFactoryBean<Spr
         // being started/refreshed, there could be a race condition with
         // other ApplicationListeners that react to
         // ContextRefreshedEvent but this is the best that we can do
+        if (event.getSource() instanceof ApplicationContext) {
+            ApplicationContext appCtx = (ApplicationContext) event.getSource();
+            if (appCtx.getId().equals("application:management")) {
+                //don't start camel context if
+                //event is from the self management ApplicationContext
+                return;
+            }
+        }
         start();
     }
 
