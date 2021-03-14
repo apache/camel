44fdcc68ea7c64e33293230ffa5d3c99b58fa07c
Claus Ibsen <claus.ibsen@gmail.com>
2021-03-14 13:01:14 +0100

    CAMEL-16353: camel-core - Force eager classloading in build phase

--- core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/PooledExchangeFactory.java
+++ core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/PooledExchangeFactory.java
@@ -47,6 +47,9 @@ public final class PooledExchangeFactory extends PrototypeExchangeFactory {
         // force to create and load the class during build time so the JVM does not
         // load the class on first exchange to be created
         DefaultPooledExchange dummy = new DefaultPooledExchange(camelContext);
+        // force message init to load classes
+        dummy.getIn();
+        dummy.getIn().getHeaders();
         LOG.trace("Warming up PooledExchangeFactory loaded class: {}", dummy.getClass().getName());
     }
 
