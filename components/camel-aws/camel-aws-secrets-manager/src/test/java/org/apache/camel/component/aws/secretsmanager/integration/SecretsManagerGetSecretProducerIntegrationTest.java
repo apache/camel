1dcf3101f473669d3dafde0e44b72f56fec84e38
Andrea Cosentino <ancosen@gmail.com>
2021-03-15 11:25:54 +0100

    CAMEL-16323 - Create a Camel-AWS-Secret-Manager component - getSecret operation

--- components/camel-aws/camel-aws-secrets-manager/src/test/java/org/apache/camel/component/aws/secretsmanager/integration/SecretsManagerGetSecretProducerIntegrationTest.java
+++ components/camel-aws/camel-aws-secrets-manager/src/test/java/org/apache/camel/component/aws/secretsmanager/integration/SecretsManagerGetSecretProducerIntegrationTest.java
@@ -23,6 +23,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.aws.secretsmanager.SecretsManagerConstants;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
