993adcf3fbbdacc1ad59e50c43d036530d593fb6
Claus Ibsen <claus.ibsen@gmail.com>
2021-03-15 18:25:26 +0100

    Typo

--- core/camel-core-processor/src/main/java/org/apache/camel/processor/MulticastProcessor.java
+++ core/camel-core-processor/src/main/java/org/apache/camel/processor/MulticastProcessor.java
@@ -405,7 +405,7 @@ public class MulticastProcessor extends AsyncProcessorSupport
     }
 
     /**
-     * Sub taks processed reactive via the {@link ReactiveExecutor}.
+     * Sub task processed reactive via the {@link ReactiveExecutor}.
      */
     protected class MulticastReactiveTask extends MulticastTask {
 
