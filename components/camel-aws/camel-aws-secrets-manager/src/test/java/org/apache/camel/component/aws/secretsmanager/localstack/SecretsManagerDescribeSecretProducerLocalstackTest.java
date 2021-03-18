69666b989390c75fb5b9f35ae286a1412c4a16e0
Andrea Cosentino <ancosen@gmail.com>
2021-03-18 07:57:59 +0100

    Camel-AWS-Secrets-Manager: Moving integration test to localstack

--- components/camel-aws/camel-aws-secrets-manager/src/test/java/org/apache/camel/component/aws/secretsmanager/localstack/SecretsManagerDescribeSecretProducerLocalstackTest.java
+++ components/camel-aws/camel-aws-secrets-manager/src/test/java/org/apache/camel/component/aws/secretsmanager/localstack/SecretsManagerDescribeSecretProducerLocalstackTest.java
@@ -30,7 +30,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SecretsManagerDescribeSecretProducerIntegrationTest extends AwsSecretsManagerBaseTest {
+public class SecretsManagerDescribeSecretProducerLocalstackTest extends AwsSecretsManagerBaseTest {
 
     @EndpointInject("mock:result")
     private MockEndpoint mock;
