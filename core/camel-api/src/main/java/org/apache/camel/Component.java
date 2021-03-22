5f3dcf4087e17ffa9fd8ae08b2bbf50bdcb2c90d
Claus Ibsen <claus.ibsen@gmail.com>
2021-03-22 13:28:45 +0100

    CAMEL-16382: Fixed setting autowried-enabled=false on component level.

--- core/camel-api/src/main/java/org/apache/camel/Component.java
+++ core/camel-api/src/main/java/org/apache/camel/Component.java
@@ -120,4 +120,14 @@ public interface Component extends CamelContextAware, Service {
         return null;
     }
 
+    /**
+     * Whether autowiring is enabled. This is used for automatic autowiring options (the option must be marked as
+     * autowired) by looking up in the registry to find if there is a single instance of matching type, which then gets
+     * configured on the component. This can be used for automatic configuring JDBC data sources, JMS connection
+     * factories, AWS Clients, etc.
+     */
+    default boolean isAutowiredEnabled() {
+        return true;
+    }
+
 }
