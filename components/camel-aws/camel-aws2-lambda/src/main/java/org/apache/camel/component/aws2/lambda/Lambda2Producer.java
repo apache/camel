57aa798988dd01ac9b9a3b81e584fee999a92981
Andrea Cosentino <ancosen@gmail.com>
2021-03-30 13:31:39 +0200

    Fixed CS

--- components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
+++ components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
@@ -370,37 +370,37 @@ public class Lambda2Producer extends DefaultProducer {
     }
 
     private void updateFunction(LambdaClient lambdaClient, Exchange exchange) throws Exception {
-    	UpdateFunctionCodeRequest request = null;
-    	UpdateFunctionCodeResponse result;
+        UpdateFunctionCodeRequest request = null;
+        UpdateFunctionCodeResponse result;
         if (getConfiguration().isPojoRequest()) {
             request = exchange.getIn().getMandatoryBody(UpdateFunctionCodeRequest.class);
         } else {
-        	UpdateFunctionCodeRequest.Builder builder = UpdateFunctionCodeRequest.builder();
-                builder.functionName(getEndpoint().getFunction());
+            UpdateFunctionCodeRequest.Builder builder = UpdateFunctionCodeRequest.builder();
+            builder.functionName(getEndpoint().getFunction());
 
-                if (ObjectHelper.isEmpty(exchange.getIn().getBody())
-                        && (ObjectHelper.isEmpty(exchange.getIn().getHeader(Lambda2Constants.S3_BUCKET))
-                                && ObjectHelper.isEmpty(exchange.getIn().getHeader(Lambda2Constants.S3_KEY)))) {
-                    throw new IllegalArgumentException("At least S3 bucket/S3 key or zip file must be specified");
-                }
-
-                if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.PUBLISH))) {
-                    Boolean publish = exchange.getIn().getHeader(Lambda2Constants.PUBLISH, Boolean.class);
-                    builder.publish(publish);
-                }
-
-                request = builder.build();
-        }
-        try {
-                result = lambdaClient.updateFunctionCode(request);
-
-            } catch (AwsServiceException ase) {
-                LOG.trace("updateFunction command returned the error code {}", ase.awsErrorDetails().errorCode());
-                throw ase;
+            if (ObjectHelper.isEmpty(exchange.getIn().getBody())
+                    && (ObjectHelper.isEmpty(exchange.getIn().getHeader(Lambda2Constants.S3_BUCKET))
+                            && ObjectHelper.isEmpty(exchange.getIn().getHeader(Lambda2Constants.S3_KEY)))) {
+                throw new IllegalArgumentException("At least S3 bucket/S3 key or zip file must be specified");
             }
 
-            Message message = getMessageForResponse(exchange);
-            message.setBody(result);
+            if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.PUBLISH))) {
+                Boolean publish = exchange.getIn().getHeader(Lambda2Constants.PUBLISH, Boolean.class);
+                builder.publish(publish);
+            }
+
+            request = builder.build();
+        }
+        try {
+            result = lambdaClient.updateFunctionCode(request);
+
+        } catch (AwsServiceException ase) {
+            LOG.trace("updateFunction command returned the error code {}", ase.awsErrorDetails().errorCode());
+            throw ase;
+        }
+
+        Message message = getMessageForResponse(exchange);
+        message.setBody(result);
     }
 
     private void createEventSourceMapping(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
