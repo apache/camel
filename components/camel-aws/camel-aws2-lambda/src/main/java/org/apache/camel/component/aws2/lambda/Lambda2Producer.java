aad40a0b914735561802c2fd1e893594cbe97497
Andrea Cosentino <ancosen@gmail.com>
2021-03-30 12:03:37 +0200

    Fixed CS

--- components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
+++ components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
@@ -215,21 +215,21 @@ public class Lambda2Producer extends DefaultProducer {
     private void invokeFunction(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
         InvokeRequest request = null;
         InvokeResponse result;
-    	if (getConfiguration().isPojoRequest()) {
+        if (getConfiguration().isPojoRequest()) {
             request = exchange.getIn().getMandatoryBody(InvokeRequest.class);
         } else {
             InvokeRequest.Builder builder = InvokeRequest.builder();
             request = builder.functionName(getEndpoint().getFunction())
-            .payload(SdkBytes.fromString(exchange.getIn().getBody(String.class), Charset.defaultCharset())).build();
+                    .payload(SdkBytes.fromString(exchange.getIn().getBody(String.class), Charset.defaultCharset())).build();
         }
-            try {
-                result = lambdaClient.invoke(request);
-            } catch (AwsServiceException ase) {
-                LOG.trace("invokeFunction command returned the error code {}", ase.awsErrorDetails().errorCode());
-                throw ase;
-            }
-            Message message = getMessageForResponse(exchange);
-            message.setBody(result.payload().asUtf8String());
+        try {
+            result = lambdaClient.invoke(request);
+        } catch (AwsServiceException ase) {
+            LOG.trace("invokeFunction command returned the error code {}", ase.awsErrorDetails().errorCode());
+            throw ase;
+        }
+        Message message = getMessageForResponse(exchange);
+        message.setBody(result.payload().asUtf8String());
     }
 
     @SuppressWarnings("unchecked")
