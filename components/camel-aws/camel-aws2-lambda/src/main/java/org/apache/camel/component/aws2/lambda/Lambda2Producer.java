c9649c90d66b5c3dff66ab1a989ace2e746f28f9
Andrea Cosentino <ancosen@gmail.com>
2021-03-31 08:22:22 +0200

    Fixed CS

--- components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
+++ components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
@@ -433,20 +433,20 @@ public class Lambda2Producer extends DefaultProducer {
     }
 
     private void deleteEventSourceMapping(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
-    	DeleteEventSourceMappingRequest request = null;
-    	DeleteEventSourceMappingResponse result;
+        DeleteEventSourceMappingRequest request = null;
+        DeleteEventSourceMappingResponse result;
         if (getConfiguration().isPojoRequest()) {
             request = exchange.getIn().getMandatoryBody(DeleteEventSourceMappingRequest.class);
         } else {
-                DeleteEventSourceMappingRequest.Builder builder = DeleteEventSourceMappingRequest.builder();
-                if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.EVENT_SOURCE_UUID))) {
-                	builder.uuid(exchange.getIn().getHeader(Lambda2Constants.EVENT_SOURCE_UUID, String.class));
-                } else {
-                    throw new IllegalArgumentException("Event Source Arn must be specified");
-                }
+            DeleteEventSourceMappingRequest.Builder builder = DeleteEventSourceMappingRequest.builder();
+            if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.EVENT_SOURCE_UUID))) {
+                builder.uuid(exchange.getIn().getHeader(Lambda2Constants.EVENT_SOURCE_UUID, String.class));
+            } else {
+                throw new IllegalArgumentException("Event Source Arn must be specified");
+            }
             request = builder.build();
-                
-            try {    
+
+            try {
                 result = lambdaClient.deleteEventSourceMapping(request);
             } catch (AwsServiceException ase) {
                 LOG.trace("deleteEventSourceMapping command returned the error code {}", ase.awsErrorDetails().errorCode());
