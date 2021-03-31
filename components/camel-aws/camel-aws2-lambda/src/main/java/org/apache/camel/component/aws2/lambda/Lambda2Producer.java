49dafb20225e40440048defbd31c2c4f73e0a406
Andrea Cosentino <ancosen@gmail.com>
2021-03-31 08:55:13 +0200

    Fixed CS

--- components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
+++ components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
@@ -504,34 +504,34 @@ public class Lambda2Producer extends DefaultProducer {
 
     @SuppressWarnings("unchecked")
     private void tagResource(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
-    	TagResourceRequest request = null;
-    	TagResourceResponse result;
+        TagResourceRequest request = null;
+        TagResourceResponse result;
         if (getConfiguration().isPojoRequest()) {
             request = exchange.getIn().getMandatoryBody(TagResourceRequest.class);
         } else {
-                TagResourceRequest.Builder builder = TagResourceRequest.builder();
-                if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN))) {
-                    String resource = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN, String.class);
-                    builder.resource(resource);
-                } else {
-                    throw new IllegalArgumentException("The resource ARN must be specified");
-                }
-                if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAGS))) {
-                    Map<String, String> tags = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAGS, Map.class);
-                    builder.tags(tags);
-                } else {
-                    throw new IllegalArgumentException("The tags must be specified");
-                }
-                request = builder.build();
+            TagResourceRequest.Builder builder = TagResourceRequest.builder();
+            if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN))) {
+                String resource = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN, String.class);
+                builder.resource(resource);
+            } else {
+                throw new IllegalArgumentException("The resource ARN must be specified");
+            }
+            if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAGS))) {
+                Map<String, String> tags = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAGS, Map.class);
+                builder.tags(tags);
+            } else {
+                throw new IllegalArgumentException("The tags must be specified");
+            }
+            request = builder.build();
         }
         try {
-                result = lambdaClient.tagResource(request);
-            } catch (AwsServiceException ase) {
-                LOG.trace("listTags command returned the error code {}", ase.awsErrorDetails().errorCode());
-                throw ase;
-            }
-            Message message = getMessageForResponse(exchange);
-            message.setBody(result);
+            result = lambdaClient.tagResource(request);
+        } catch (AwsServiceException ase) {
+            LOG.trace("listTags command returned the error code {}", ase.awsErrorDetails().errorCode());
+            throw ase;
+        }
+        Message message = getMessageForResponse(exchange);
+        message.setBody(result);
     }
 
     @SuppressWarnings("unchecked")
