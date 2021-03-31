e1dfffebedb5376941aee9a762aef992121e6a83
Andrea Cosentino <ancosen@gmail.com>
2021-03-31 09:20:16 +0200

    Fixed CS

--- components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
+++ components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
@@ -536,34 +536,34 @@ public class Lambda2Producer extends DefaultProducer {
 
     @SuppressWarnings("unchecked")
     private void untagResource(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
-    	UntagResourceRequest request = null;
-    	UntagResourceResponse result;
+        UntagResourceRequest request = null;
+        UntagResourceResponse result;
         if (getConfiguration().isPojoRequest()) {
             request = exchange.getIn().getMandatoryBody(UntagResourceRequest.class);
         } else {
-                UntagResourceRequest.Builder builder = UntagResourceRequest.builder();
-                if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN))) {
-                    String resource = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN, String.class);
-                    builder.resource(resource);
-                } else {
-                    throw new IllegalArgumentException("The resource ARN must be specified");
-                }
-                if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAG_KEYS))) {
-                    List<String> tagKeys = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAG_KEYS, List.class);
-                    builder.tagKeys(tagKeys);
-                } else {
-                    throw new IllegalArgumentException("The tag keys must be specified");
-                }
-                request = builder.build();
+            UntagResourceRequest.Builder builder = UntagResourceRequest.builder();
+            if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN))) {
+                String resource = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_ARN, String.class);
+                builder.resource(resource);
+            } else {
+                throw new IllegalArgumentException("The resource ARN must be specified");
+            }
+            if (ObjectHelper.isNotEmpty(exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAG_KEYS))) {
+                List<String> tagKeys = exchange.getIn().getHeader(Lambda2Constants.RESOURCE_TAG_KEYS, List.class);
+                builder.tagKeys(tagKeys);
+            } else {
+                throw new IllegalArgumentException("The tag keys must be specified");
+            }
+            request = builder.build();
         }
         try {
-                result = lambdaClient.untagResource(request);
-            } catch (AwsServiceException ase) {
-                LOG.trace("untagResource command returned the error code {}", ase.awsErrorDetails().errorCode());
-                throw ase;
-            }
-            Message message = getMessageForResponse(exchange);
-            message.setBody(result);
+            result = lambdaClient.untagResource(request);
+        } catch (AwsServiceException ase) {
+            LOG.trace("untagResource command returned the error code {}", ase.awsErrorDetails().errorCode());
+            throw ase;
+        }
+        Message message = getMessageForResponse(exchange);
+        message.setBody(result);
     }
 
     private void publishVersion(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
