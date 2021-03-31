c580074df8f7a2fcb0286e3e33517b09e1e7f7fd
Andrea Cosentino <ancosen@gmail.com>
2021-03-31 08:32:35 +0200

    Fixed CS

--- components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
+++ components/camel-aws/camel-aws2-lambda/src/main/java/org/apache/camel/component/aws2/lambda/Lambda2Producer.java
@@ -458,23 +458,23 @@ public class Lambda2Producer extends DefaultProducer {
     }
 
     private void listEventSourceMapping(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
-    	ListEventSourceMappingsRequest request = null;
-    	ListEventSourceMappingsResponse result;
+        ListEventSourceMappingsRequest request = null;
+        ListEventSourceMappingsResponse result;
         if (getConfiguration().isPojoRequest()) {
             request = exchange.getIn().getMandatoryBody(ListEventSourceMappingsRequest.class);
         } else {
-                ListEventSourceMappingsRequest.Builder builder = ListEventSourceMappingsRequest.builder();
-                builder.functionName(getEndpoint().getFunction());
-                request = builder.build();
+            ListEventSourceMappingsRequest.Builder builder = ListEventSourceMappingsRequest.builder();
+            builder.functionName(getEndpoint().getFunction());
+            request = builder.build();
         }
-            try {
-                result = lambdaClient.listEventSourceMappings(request);
-            } catch (AwsServiceException ase) {
-                LOG.trace("listEventSourceMapping command returned the error code {}", ase.awsErrorDetails().errorCode());
-                throw ase;
-            }
-            Message message = getMessageForResponse(exchange);
-            message.setBody(result);
+        try {
+            result = lambdaClient.listEventSourceMappings(request);
+        } catch (AwsServiceException ase) {
+            LOG.trace("listEventSourceMapping command returned the error code {}", ase.awsErrorDetails().errorCode());
+            throw ase;
+        }
+        Message message = getMessageForResponse(exchange);
+        message.setBody(result);
     }
 
     private void listTags(LambdaClient lambdaClient, Exchange exchange) throws InvalidPayloadException {
