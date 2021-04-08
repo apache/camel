eeb6bec4ce9f9b1d71dee14dae4ebae7b30fb4c0
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-04-08 15:13:26 +0200

    dsl(groovy): use java to implement the route buiolder loader instead of groovy

---
+++ dsl/camel-groovy-dsl/camel-groovy-dsl-common/src/main/groovy/org/apache/camel/dsl/groovy/common/model/RestConfiguration.groovy
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.dsl.groovy.common.model
+
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.model.rest.RestConfigurationDefinition
+
+class RestConfiguration extends RestVerbConfiguration {
+    RestConfiguration(RouteBuilder builder) {
+        super(builder, null)
+    }
+
+    def configuration(@DelegatesTo(RestConfigurationDefinition) Closure<?> callable) {
+        callable.resolveStrategy = Closure.DELEGATE_FIRST
+        callable.delegate = builder.restConfiguration()
+        callable.call()
+    }
+
+    def path(String path, @DelegatesTo(RestVerbConfiguration) Closure<?> callable) {
+        callable.resolveStrategy = Closure.DELEGATE_FIRST
+        callable.delegate = new RestVerbConfiguration(builder, path)
+        callable.call()
+    }
+}
