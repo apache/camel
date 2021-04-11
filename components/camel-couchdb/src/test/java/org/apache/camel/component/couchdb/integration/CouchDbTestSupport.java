93e0c6ea1ad4586827309be9cb222f160483833a
Otavio R. Piske <angusyoung@gmail.com>
2021-04-11 18:17:31 +0200

    CAMEL-16400: split unit and integration tests for camel-couchdb

--- components/camel-couchdb/src/test/java/org/apache/camel/component/couchdb/integration/CouchDbTestSupport.java
+++ components/camel-couchdb/src/test/java/org/apache/camel/component/couchdb/integration/CouchDbTestSupport.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.couchdb;
+package org.apache.camel.component.couchdb.integration;
 
 import org.apache.camel.test.infra.couchdb.services.CouchDbService;
 import org.apache.camel.test.infra.couchdb.services.CouchDbServiceFactory;
