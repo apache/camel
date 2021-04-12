9c29acda1b494383a24e6369d3eb6451a53cfcc1
Andrea Cosentino <ancosen@gmail.com>
2021-04-12 08:19:38 +0200

    Camel-AWS-Secrets-Manager: Moved component test out of integration package

--- components/camel-aws/camel-aws-secrets-manager/src/test/java/org/apache/camel/component/aws/secretsmanager/SecretsManagerComponentConfigurationTest.java
+++ components/camel-aws/camel-aws-secrets-manager/src/test/java/org/apache/camel/component/aws/secretsmanager/SecretsManagerComponentConfigurationTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.aws.secretsmanager.integration;
+package org.apache.camel.component.aws.secretsmanager;
 
 import org.apache.camel.component.aws.secretsmanager.SecretsManagerComponent;
 import org.apache.camel.component.aws.secretsmanager.SecretsManagerEndpoint;
