62d2444270efae611ee12fcc1b1479c2ab9507f6
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2021-04-14 19:38:22 +0200

    CAMEL-16400: split unit and integration tests for camel-redis (#5366)

--- components/camel-redis/src/test/java/org/apache/camel/component/redis/processor/aggregate/integration/MyAggregationStrategy.java
+++ components/camel-redis/src/test/java/org/apache/camel/component/redis/processor/aggregate/integration/MyAggregationStrategy.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.redis.processor.aggregate;
+package org.apache.camel.component.redis.processor.aggregate.integration;
 
 import org.apache.camel.AggregationStrategy;
 import org.apache.camel.Exchange;
