d5656b3ede5b7480936618e4fe428dafe9d1b05f
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2021-04-16 17:10:42 +0200

    CAMEL-16400: adjust manual integration tests for camel-smpp (#5404)

--- components/camel-smpp/src/test/java/org/apache/camel/component/smpp/integration/SmppConsumerReconnectManualIT.java
+++ components/camel-smpp/src/test/java/org/apache/camel/component/smpp/integration/SmppConsumerReconnectManualIT.java
@@ -27,7 +27,7 @@ import org.junit.jupiter.api.Test;
  * A SMSC for test is available here: http://www.seleniumsoftware.com/downloads.html
  */
 @Disabled("Must be manually tested")
-public class SmppProducerReconnectIntegrationTest extends CamelTestSupport {
+public class SmppConsumerReconnectManualIT extends CamelTestSupport {
 
     @Test
     public void test() throws Exception {
@@ -39,8 +39,8 @@ public class SmppProducerReconnectIntegrationTest extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("direct:start")
-                        .to("smpp://smppclient@localhost:2775?password=password&enquireLinkTimer=3000&transactionTimer=5000&systemType=producerr");
+                from("smpp://smppclient@localhost:2775?password=password&enquireLinkTimer=3000&transactionTimer=5000&systemType=consumer")
+                        .to("mock:result");
             }
         };
     }
