e09edcfaa0126705345328c1bd57177b6866ffb6
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2021-04-16 12:51:01 +0200

    CAMEL-16400: split unit and integration tests for camel-google-functions (#5389)

--- components/camel-google/camel-google-functions/src/test/java/org/apache/camel/component/google/functions/integration/GoogleCloudIT.java
+++ components/camel-google/camel-google-functions/src/test/java/org/apache/camel/component/google/functions/integration/GoogleCloudIT.java
@@ -25,8 +25,11 @@ import org.apache.camel.component.google.functions.GoogleCloudFunctionsConstants
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
-public class IntegrationTest extends CamelTestSupport {
+@EnabledIfEnvironmentVariable(named = "GOOGLE_APPLICATION_CREDENTIALS", matches = ".*",
+                              disabledReason = "Application credentials were not provided")
+public class GoogleCloudIT extends CamelTestSupport {
 
     final String functionNameReverseString = "function-reverse-string";
     final String serviceAccountKeyFile = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
