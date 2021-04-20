d18b28eeeb4894d96495cf7a24645467f2f997c2
Andrea Cosentino <ancosen@gmail.com>
2021-04-20 09:25:20 +0200

    AWS2-SQS component does not recognize or use proxy-host and proxy-port when default credentials provider is in use

--- components/camel-aws/camel-aws2-sqs/src/main/java/org/apache/camel/component/aws2/sqs/client/impl/Sqs2ClientIAMOptimized.java
+++ components/camel-aws/camel-aws2-sqs/src/main/java/org/apache/camel/component/aws2/sqs/client/impl/Sqs2ClientIAMOptimized.java
@@ -23,7 +23,6 @@ import org.apache.camel.component.aws2.sqs.client.Sqs2InternalClient;
 import org.apache.camel.util.ObjectHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.SdkHttpConfigurationOption;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
