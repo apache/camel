80b92e3624ae5db59a1a24a441f1b10b39eaa1a5
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2021-04-20 17:57:48 +0200

    camel-test-infra-azure-common: update Azurite container to 3.11.0 (#5446)

--- test-infra/camel-test-infra-azure-common/src/test/java/org/apache/camel/test/infra/azure/common/services/AzuriteContainer.java
+++ test-infra/camel-test-infra-azure-common/src/test/java/org/apache/camel/test/infra/azure/common/services/AzuriteContainer.java
@@ -26,7 +26,7 @@ public class AzuriteContainer extends GenericContainer<AzuriteContainer> {
     public static final String DEFAULT_ACCOUNT_KEY
             = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
 
-    public static final String IMAGE_NAME = "mcr.microsoft.com/azure-storage/azurite:3.10.0";
+    public static final String IMAGE_NAME = "mcr.microsoft.com/azure-storage/azurite:3.11.0";
 
     public AzuriteContainer() {
         this(IMAGE_NAME);
