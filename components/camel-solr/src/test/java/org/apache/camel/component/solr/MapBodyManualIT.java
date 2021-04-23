d1ea18791002328cbaac937c989860c015cb1572
Andrea Cosentino <ancosen@gmail.com>
2021-04-23 06:53:32 +0200

    CAMEL-16546 - Camel-Solr: Support Map as body while inserting, updating etc.

--- components/camel-solr/src/test/java/org/apache/camel/component/solr/MapBodyManualIT.java
+++ components/camel-solr/src/test/java/org/apache/camel/component/solr/MapBodyManualIT.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.solr;
 
 import java.util.HashMap;
@@ -54,7 +70,7 @@ public class MapBodyManualIT extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("direct:start").to("solr://localhost:8984/solr/films").to("mock:result");
+                from("direct:start").to("{{solr.address}}").to("mock:result");
             }
         };
     }
