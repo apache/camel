794a3d50e910bd3029701278ce96689ed00fa73f
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-04-25 12:04:59 -0500

    avoid cast of header fetch
    Co-authored-by: Omar Al-Safi <omarsmak@gmail.com>

--- components/camel-jt400/src/main/java/org/apache/camel/component/jt400/Jt400MsgQueueProducer.java
+++ components/camel-jt400/src/main/java/org/apache/camel/component/jt400/Jt400MsgQueueProducer.java
@@ -50,7 +50,7 @@ public class Jt400MsgQueueProducer extends DefaultProducer {
 
     private void process(MessageQueue queue, Exchange exchange) throws Exception {
         String msgText = exchange.getIn().getBody(String.class);
-        byte[] messageKey = (byte[]) exchange.getIn().getHeader(Jt400Constants.MESSAGE_REPLYTO_KEY);
+        byte[] messageKey =  exchange.getIn().getHeader(Jt400Constants.MESSAGE_REPLYTO_KEY, byte[].class);
         if (null != messageKey && !msgText.isEmpty()) {
             queue.reply(messageKey, msgText);
         } else {
