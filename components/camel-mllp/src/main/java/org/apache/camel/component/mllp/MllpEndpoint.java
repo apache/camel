32ef4dd0133a1d0640575935814af48f05f902ff
James Netherton <jamesnetherton@gmail.com>
2021-05-05 10:28:38 +0100

    CAMEL-16580: Remove deprecated code from MLLP component

--- components/camel-mllp/src/main/java/org/apache/camel/component/mllp/MllpEndpoint.java
+++ components/camel-mllp/src/main/java/org/apache/camel/component/mllp/MllpEndpoint.java
@@ -19,7 +19,6 @@ package org.apache.camel.component.mllp;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketAddress;
-import java.nio.charset.Charset;
 import java.util.Date;
 
 import org.apache.camel.Category;
@@ -51,22 +50,6 @@ import org.slf4j.Logger;
 @UriEndpoint(scheme = "mllp", firstVersion = "2.17.0", title = "MLLP", syntax = "mllp:hostname:port",
              category = { Category.NETWORKING, Category.RPC, Category.MLLP }, generateConfigurer = false)
 public class MllpEndpoint extends DefaultEndpoint {
-    // Use constants from MllpProtocolConstants
-    @Deprecated()
-    public static final char START_OF_BLOCK = MllpProtocolConstants.START_OF_BLOCK;
-    @Deprecated()
-    public static final char END_OF_BLOCK = MllpProtocolConstants.END_OF_BLOCK;
-    @Deprecated()
-    public static final char END_OF_DATA = MllpProtocolConstants.END_OF_DATA;
-    @Deprecated()
-    public static final int END_OF_STREAM = MllpProtocolConstants.END_OF_STREAM;
-    @Deprecated()
-    public static final char SEGMENT_DELIMITER = MllpProtocolConstants.SEGMENT_DELIMITER;
-    @Deprecated()
-    public static final char MESSAGE_TERMINATOR = MllpProtocolConstants.MESSAGE_TERMINATOR;
-
-    @Deprecated // Use MllpComponent.getDefaultCharset()
-    public static final Charset DEFAULT_CHARSET = MllpComponent.getDefaultCharset();
 
     @UriPath
     @Metadata(required = true)
@@ -282,16 +265,6 @@ public class MllpEndpoint extends DefaultEndpoint {
 
     public void setHl7Headers(Boolean hl7Headers) {
         configuration.setHl7Headers(hl7Headers);
-    }
-
-    /**
-     * @deprecated              this parameter will be ignored.
-     *
-     * @param      bufferWrites
-     */
-    @Deprecated
-    public void setBufferWrites(Boolean bufferWrites) {
-        configuration.setBufferWrites(bufferWrites);
     }
 
     public void setRequireEndOfData(Boolean requireEndOfData) {
