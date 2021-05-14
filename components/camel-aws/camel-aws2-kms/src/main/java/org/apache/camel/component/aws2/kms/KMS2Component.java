267fd3c2d6d5305e92687a3c1f86beab01700ef5
Andrea Cosentino <ancosen@gmail.com>
2021-05-14 07:20:04 +0200

    CAMEL-16465 - Camel-AWS: Add useDefaultCredentialProvider option to all the components - KMS Component

--- components/camel-aws/camel-aws2-kms/src/main/java/org/apache/camel/component/aws2/kms/KMS2Component.java
+++ components/camel-aws/camel-aws2-kms/src/main/java/org/apache/camel/component/aws2/kms/KMS2Component.java
@@ -53,9 +53,9 @@ public class KMS2Component extends DefaultComponent {
 
         KMS2Endpoint endpoint = new KMS2Endpoint(uri, this, configuration);
         setProperties(endpoint, parameters);
-        if (configuration.getKmsClient() == null
+        if (!configuration.isUseDefaultCredentialsProvider() && configuration.getKmsClient() == null
                 && (configuration.getAccessKey() == null || configuration.getSecretKey() == null)) {
-            throw new IllegalArgumentException("Amazon kms client or accessKey and secretKey must be specified");
+            throw new IllegalArgumentException("useDefaultCredentialsProvider is set to false, Amazon kms client or accessKey and secretKey must be specified");
         }
 
         return endpoint;
