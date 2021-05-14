729a3280791c10897d3c53185899938ee8304b7f
Andrea Cosentino <ancosen@gmail.com>
2021-05-14 14:50:07 +0200

    Camel-AWS2-Lambda: Regen

--- core/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/Aws2LambdaComponentBuilderFactory.java
+++ core/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/Aws2LambdaComponentBuilderFactory.java
@@ -141,7 +141,7 @@ public interface Aws2LambdaComponentBuilderFactory {
             return this;
         }
         /**
-         * The region in which ECS client needs to work. When using this
+         * The region in which Lambda client needs to work. When using this
          * parameter, the configuration will expect the lowercase name of the
          * region (for example ap-east-1) You'll need to use the name
          * Region.EU_WEST_1.id().
