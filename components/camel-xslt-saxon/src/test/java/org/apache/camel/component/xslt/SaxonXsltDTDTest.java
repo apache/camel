80446e9ce59189c5a9ad70761ee9c61ec49ff379
aldettinger <aldettinger@gmail.com>
2021-05-26 10:46:37 +0200

    CAMEL-16643: Dropped the useless dependency to camel-xslt-saxon from camel-saxon

--- components/camel-xslt-saxon/src/test/java/org/apache/camel/component/xslt/SaxonXsltDTDTest.java
+++ components/camel-xslt-saxon/src/test/java/org/apache/camel/component/xslt/SaxonXsltDTDTest.java
@@ -88,10 +88,10 @@ public class SaxonXsltDTDTest extends CamelTestSupport {
     }
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
 
                 from("direct:start1")
                         .to("xslt-saxon:org/apache/camel/component/xslt/transform_dtd.xsl")
