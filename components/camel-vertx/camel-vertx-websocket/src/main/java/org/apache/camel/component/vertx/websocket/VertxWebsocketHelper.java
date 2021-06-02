0a46d32fb8be183e5e79e53204cd4e7c24d9ca84
James Netherton <jamesnetherton@gmail.com>
2021-06-02 07:44:08 +0100

    CAMEL-16677: Move vertx SSL setup to vertx-common

--- components/camel-vertx/camel-vertx-websocket/src/main/java/org/apache/camel/component/vertx/websocket/VertxWebsocketHelper.java
+++ components/camel-vertx/camel-vertx-websocket/src/main/java/org/apache/camel/component/vertx/websocket/VertxWebsocketHelper.java
@@ -16,90 +16,10 @@
  */
 package org.apache.camel.component.vertx.websocket;
 
-import java.security.KeyStore;
-import java.util.function.Function;
-
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509KeyManager;
-
-import io.vertx.core.Vertx;
-import io.vertx.core.net.KeyCertOptions;
-import io.vertx.core.net.TCPSSLOptions;
-import io.vertx.core.net.TrustOptions;
-import org.apache.camel.support.jsse.KeyManagersParameters;
-import org.apache.camel.support.jsse.SSLContextParameters;
-import org.apache.camel.support.jsse.TrustManagersParameters;
-
 public final class VertxWebsocketHelper {
 
     private VertxWebsocketHelper() {
         // Utility class
-    }
-
-    /**
-     * Configures key store and trust store options for the Vert.x client and server
-     */
-    public static void setupSSLOptions(SSLContextParameters sslContextParameters, TCPSSLOptions options) throws Exception {
-        options.setSsl(true);
-        options.setKeyCertOptions(new KeyManagerFactoryOptions(createKeyManagerFactory(sslContextParameters)));
-        options.setTrustOptions(new TrustManagerFactoryOptions(createTrustManagerFactory(sslContextParameters)));
-    }
-
-    private static class KeyManagerFactoryOptions implements KeyCertOptions {
-        private final KeyManagerFactory keyManagerFactory;
-
-        public KeyManagerFactoryOptions(KeyManagerFactory keyManagerFactory) {
-            this.keyManagerFactory = keyManagerFactory;
-        }
-
-        private KeyManagerFactoryOptions(KeyManagerFactoryOptions other) {
-            this.keyManagerFactory = other.keyManagerFactory;
-        }
-
-        @Override
-        public KeyCertOptions copy() {
-            return new KeyManagerFactoryOptions(this);
-        }
-
-        @Override
-        public KeyManagerFactory getKeyManagerFactory(Vertx vertx) {
-            return keyManagerFactory;
-        }
-
-        @Override
-        public Function<String, X509KeyManager> keyManagerMapper(Vertx vertx) {
-            return keyManagerFactory.getKeyManagers()[0] instanceof X509KeyManager
-                    ? serverName -> (X509KeyManager) keyManagerFactory.getKeyManagers()[0] : null;
-        }
-    }
-
-    private static class TrustManagerFactoryOptions implements TrustOptions {
-        private final TrustManagerFactory trustManagerFactory;
-
-        public TrustManagerFactoryOptions(TrustManagerFactory trustManagerFactory) {
-            this.trustManagerFactory = trustManagerFactory;
-        }
-
-        private TrustManagerFactoryOptions(TrustManagerFactoryOptions other) {
-            trustManagerFactory = other.trustManagerFactory;
-        }
-
-        @Override
-        public TrustOptions copy() {
-            return new TrustManagerFactoryOptions(this);
-        }
-
-        @Override
-        public TrustManagerFactory getTrustManagerFactory(Vertx vertx) {
-            return trustManagerFactory;
-        }
-
-        @Override
-        public Function<String, TrustManager[]> trustManagerMapper(Vertx vertx) {
-            return serverName -> trustManagerFactory.getTrustManagers();
-        }
     }
 
     /**
@@ -156,66 +76,5 @@ public final class VertxWebsocketHelper {
      */
     public static VertxWebsocketHostKey createHostKey(VertxWebsocketConfiguration configuration) {
         return new VertxWebsocketHostKey(configuration.getHost(), configuration.getPort());
-    }
-
-    /**
-     * Creates a KeyManagerFactory from a given SSLContextParameters
-     */
-    private static KeyManagerFactory createKeyManagerFactory(SSLContextParameters sslContextParameters) throws Exception {
-        final KeyManagersParameters keyManagers = sslContextParameters.getKeyManagers();
-        if (keyManagers == null) {
-            return null;
-        }
-
-        String kmfAlgorithm = keyManagers.getAlgorithm();
-        if (kmfAlgorithm == null) {
-            kmfAlgorithm = KeyManagerFactory.getDefaultAlgorithm();
-        }
-
-        KeyManagerFactory kmf;
-        if (keyManagers.getProvider() == null) {
-            kmf = KeyManagerFactory.getInstance(kmfAlgorithm);
-        } else {
-            kmf = KeyManagerFactory.getInstance(kmfAlgorithm, keyManagers.getProvider());
-        }
-
-        char[] kmfPassword = null;
-        if (keyManagers.getKeyPassword() != null) {
-            kmfPassword = keyManagers.getKeyPassword().toCharArray();
-        }
-
-        KeyStore ks = keyManagers.getKeyStore() == null ? null : keyManagers.getKeyStore().createKeyStore();
-
-        kmf.init(ks, kmfPassword);
-        return kmf;
-    }
-
-    /**
-     * Creates a TrustManagerFactory from a given SSLContextParameters
-     */
-    private static TrustManagerFactory createTrustManagerFactory(SSLContextParameters sslContextParameters) throws Exception {
-        final TrustManagersParameters trustManagers = sslContextParameters.getTrustManagers();
-        if (trustManagers == null) {
-            return null;
-        }
-
-        TrustManagerFactory tmf = null;
-
-        if (trustManagers.getKeyStore() != null) {
-            String tmfAlgorithm = trustManagers.getAlgorithm();
-            if (tmfAlgorithm == null) {
-                tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-            }
-
-            if (trustManagers.getProvider() == null) {
-                tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-            } else {
-                tmf = TrustManagerFactory.getInstance(tmfAlgorithm, trustManagers.getProvider());
-            }
-
-            KeyStore ks = trustManagers.getKeyStore() == null ? null : trustManagers.getKeyStore().createKeyStore();
-            tmf.init(ks);
-        }
-        return tmf;
     }
 }
