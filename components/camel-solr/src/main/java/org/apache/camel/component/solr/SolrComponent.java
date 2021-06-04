ed146e49caaf11ae9a82c3c179e3060045ef4b0a
paulb <paul@search-solutions.net>
2021-06-04 11:59:08 +0200

    Removed wildcard import

--- components/camel-solr/src/main/java/org/apache/camel/component/solr/SolrComponent.java
+++ components/camel-solr/src/main/java/org/apache/camel/component/solr/SolrComponent.java
@@ -17,7 +17,11 @@
 package org.apache.camel.component.solr;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.camel.Endpoint;
