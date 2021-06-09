72e3b28e902f4c4348d65d7f561fc02ae7254146
Hokutor <hwcamel@huawei.com>
2021-06-09 09:56:49 -0400

    CAMEL-16674 - Base commit for huawei cloud function graph component (#5612)
    CAMEL-16674 - Huawei cloud function graph component

---
+++ components/camel-huaweicloud-functiongraph/src/main/java/org/apache/camel/constants/FunctionGraphConstants.java
@@ -0,0 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.constants;
+
+public final class FunctionGraphConstants {
+    public static final String DEFAULT_FUNCTION_PACKAGE = "default";
+    public static final String URN_FORMAT = "urn:fss:%s:%s:function:%s:%s:latest";
+    public static final String REQUEST_VERSION = "v1";
+    public static final String RESPONSE_BODY = "body";
+
+    private FunctionGraphConstants() {
+    }
+}
