99f4a0d3772bb10f857366b8a58d6193b0f0a4c5
oscerd <oscerd@users.noreply.github.com>
2021-06-10 12:58:04 +0000

    Regen for commit c56bdccede80c15d16f0ebfd6118a88356b91459
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/EndpointProducerDeserializersResolver.java
+++ dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/EndpointProducerDeserializersResolver.java
@@ -149,6 +149,8 @@ public final class EndpointProducerDeserializersResolver implements YamlDeserial
             case "hdfs":
             case "http":
             case "https":
+            case "hwcloud-functiongraph":
+            case "hwcloud-smn":
             case "iec60870-client":
             case "iec60870-server":
             case "ignite-cache":
