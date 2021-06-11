ec14b4c34cad3c555ee39008e396940266e6b382
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-06-11 17:03:44 +0200

    yaml-dsl: make it simple to configure route local beans

--- dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/ModelDeserializersResolver.java
+++ dsl/camel-yaml-dsl/camel-yaml-dsl-deserializers/src/generated/java/org/apache/camel/dsl/yaml/deserializers/ModelDeserializersResolver.java
@@ -300,8 +300,6 @@ public final class ModelDeserializersResolver implements YamlDeserializerResolve
             case "org.apache.camel.model.RouteContextRefDefinition": return new ModelDeserializers.RouteContextRefDefinitionDeserializer();
             case "route": return new ModelDeserializers.RouteDefinitionDeserializer();
             case "org.apache.camel.model.RouteDefinition": return new ModelDeserializers.RouteDefinitionDeserializer();
-            case "template-bean": return new ModelDeserializers.RouteTemplateBeanDefinitionDeserializer();
-            case "org.apache.camel.model.RouteTemplateBeanDefinition": return new ModelDeserializers.RouteTemplateBeanDefinitionDeserializer();
             case "template-parameter": return new ModelDeserializers.RouteTemplateParameterDefinitionDeserializer();
             case "org.apache.camel.model.RouteTemplateParameterDefinition": return new ModelDeserializers.RouteTemplateParameterDefinitionDeserializer();
             case "template-script": return new ModelDeserializers.RouteTemplateScriptDefinitionDeserializer();
