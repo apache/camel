23dbae9a0c3bd13c0d8462f6253c48944f6fc1c8
Claus Ibsen <claus.ibsen@gmail.com>
2021-06-12 10:32:36 +0200

    CAMEL-16716: camel-mongodb - Fixed streamFilter option

--- components/camel-mongodb/src/test/java/org/apache/camel/component/mongodb/integration/MongoDbChangeStreamsConsumerIT.java
+++ components/camel-mongodb/src/test/java/org/apache/camel/component/mongodb/integration/MongoDbChangeStreamsConsumerIT.java
@@ -141,11 +141,12 @@ public class MongoDbChangeStreamsConsumerIT extends AbstractMongoDbITSupport {
                         .autoStartup(false)
                         .to("mock:test");
 
-                from("mongodb:myDb?consumerType=changeStreams&database={{mongodb.testDb}}&collection={{mongodb.testCollection}}")
-                        .routeProperty("streamFilter", "{'$match':{'$or':[{'fullDocument.string': 'value2'}]}}")
-                        .id("filterConsumer")
-                        .autoStartup(false)
-                        .to("mock:test");
+                from("mongodb:myDb?consumerType=changeStreams&database={{mongodb.testDb}}&collection={{mongodb.testCollection}}"
+                     +
+                     "&streamFilter={'$match':{'$or':[{'fullDocument.string': 'value2'}]}}")
+                             .id("filterConsumer")
+                             .autoStartup(false)
+                             .to("mock:test");
             }
         });
     }
