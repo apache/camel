d01b0f3af9d8125ebc26d3ecf3a8ee335b6c0fa5
Andrea Cosentino <ancosen@gmail.com>
2021-06-17 11:01:49 +0200

    CAMEL-16720 - camel-aws - Component title - remove the using AWS SDK version 2.x. - AWS Kinesis Firehose

--- components/camel-aws/camel-aws2-kinesis/src/main/java/org/apache/camel/component/aws2/firehose/KinesisFirehose2Endpoint.java
+++ components/camel-aws/camel-aws2-kinesis/src/main/java/org/apache/camel/component/aws2/firehose/KinesisFirehose2Endpoint.java
@@ -32,7 +32,7 @@ import static software.amazon.awssdk.core.SdkSystemSetting.CBOR_ENABLED;
 /**
  * Produce data to AWS Kinesis Firehose streams using AWS SDK version 2.x.
  */
-@UriEndpoint(firstVersion = "3.2.0", scheme = "aws2-kinesis-firehose", title = "AWS 2 Kinesis Firehose",
+@UriEndpoint(firstVersion = "3.2.0", scheme = "aws2-kinesis-firehose", title = "AWS Kinesis Firehose",
              syntax = "aws2-kinesis-firehose:streamName", producerOnly = true, category = {
                      Category.CLOUD,
                      Category.MESSAGING })
