3daa39ddb043fa4760990d9676935ea9c2aeea59
Andrea Cosentino <ancosen@gmail.com>
2021-06-17 11:05:16 +0200

    CAMEL-16720 - camel-aws - Component title - remove the using AWS SDK version 2.x. - AWS S3

--- components/camel-aws/camel-aws2-s3/src/main/java/org/apache/camel/component/aws2/s3/AWS2S3Endpoint.java
+++ components/camel-aws/camel-aws2-s3/src/main/java/org/apache/camel/component/aws2/s3/AWS2S3Endpoint.java
@@ -40,7 +40,7 @@ import software.amazon.awssdk.services.s3.model.PutBucketPolicyRequest;
 /**
  * Store and retrieve objects from AWS S3 Storage Service using AWS SDK version 2.x.
  */
-@UriEndpoint(firstVersion = "3.2.0", scheme = "aws2-s3", title = "AWS 2 S3 Storage Service",
+@UriEndpoint(firstVersion = "3.2.0", scheme = "aws2-s3", title = "AWS S3 Storage Service",
              syntax = "aws2-s3://bucketNameOrArn", category = { Category.CLOUD, Category.FILE })
 public class AWS2S3Endpoint extends ScheduledPollEndpoint {
 
