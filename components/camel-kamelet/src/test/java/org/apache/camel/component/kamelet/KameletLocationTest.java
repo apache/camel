978ca1496b59f267bfa2b4b3e3cd70b94312aa59
Claus Ibsen <claus.ibsen@gmail.com>
2021-06-18 21:40:13 +0200

    CAMEL-16650: camel-kamelet - Add option to configure location for kamelet template to load from resource.

--- components/camel-kamelet/src/test/java/org/apache/camel/component/kamelet/KameletLocationTest.java
+++ components/camel-kamelet/src/test/java/org/apache/camel/component/kamelet/KameletLocationTest.java
@@ -16,13 +16,23 @@
  */
 package org.apache.camel.component.kamelet;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spi.Resource;
+import org.apache.camel.spi.RoutesBuilderLoader;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.apache.http.annotation.Obsolete;
 import org.junit.jupiter.api.Test;
 
 public class KameletLocationTest extends CamelTestSupport {
+
+    @Override
+    protected CamelContext createCamelContext() throws Exception {
+        CamelContext context = super.createCamelContext();
+        context.getRegistry().bind("routes-builder-loader-xml", new MyRoutesLoader());
+        return context;
+    }
 
     @Test
     public void testOne() throws Exception {
@@ -41,6 +51,50 @@ public class KameletLocationTest extends CamelTestSupport {
         template.sendBody("direct:start", "World");
 
         assertMockEndpointsSatisfied();
+    }
+
+    // we cannot use camel-xml-io-dsl to load the XML route so we fool Camel
+    // and use this class that has the route template hardcoded from java
+    public class MyRoutesLoader implements RoutesBuilderLoader {
+
+        private CamelContext camelContext;
+
+        @Override
+        public CamelContext getCamelContext() {
+            return camelContext;
+        }
+
+        @Override
+        public void setCamelContext(CamelContext camelContext) {
+            this.camelContext = camelContext;
+        }
+
+        @Override
+        public String getSupportedExtension() {
+            return "xml";
+        }
+
+        @Override
+        public RoutesBuilder loadRoutesBuilder(Resource resource) throws Exception {
+            return new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    routeTemplate("upper")
+                            .from("kamelet:source")
+                            .transform().simple("${body.toUpperCase()}");
+                }
+            };
+        }
+
+        @Override
+        public void start() {
+            // noop
+        }
+
+        @Override
+        public void stop() {
+            // noop
+        }
     }
 
     // **********************************************
