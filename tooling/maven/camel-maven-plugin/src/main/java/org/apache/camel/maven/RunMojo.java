apache/camel

commit 6d233b66756a109d4c414b8b917bb9c95859d7d6

(chores) avoid creating arrays unnecessarily (#5702)
components:
components/camel-couchdb components/camel-crypto, components/camel-cxf-transport
components/camel-cxf, components/camel-dataset components/camel-dozer,
components/camel-guava-eventbus, components/camel-http-common, components/camel-jms,
components/camel-jt400, components/camel-mina, components/camel-mongodb,
components/camel-netty, components/camel-quartz, components/camel-rabbitmq,
components/camel-sjms, components/camel-sql, components/camel-xmlsecurity,
components/camel-zookeeper, core/camel-api, core/camel-base-engine,
core/camel-core-processor, core/camel-support, core/camel-xml-jaxp, tooling/maven,

tooling/maven/camel-maven-plugin/src/main/java/org/apache/camel/maven/RunMojo.java
@@ -426,7 +426,7 @@ public class RunMojo extends AbstractExecMojo {
                     getLog().info("Starting Camel ...");
                     Method main = Thread.currentThread().getContextClassLoader()
                             .loadClass(mainClass).getMethod("main", String[].class);
-                    main.invoke(null, new Object[] { arguments });
+                    main.invoke(null, arguments);
 
                     afterBootstrapCamel();
                 } catch (Exception e) { // just pass it on

components/camel-couchdb/src/main/java/org/apache/camel/component/couchdb/CouchDbChangesetTracker.java
@@ -74,10 +74,7 @@ public class CouchDbChangesetTracker implements Runnable {
                         JsonObject doc = feed.getDoc();
 
                         Exchange exchange = consumer.createExchange(lastSequence, feed.getId(), doc, feed.isDeleted());
-                        if (LOG.isTraceEnabled()) {
-                            LOG.trace("Created exchange [exchange={}, _id={}, seq={}",
-                                    new Object[] { exchange, feed.getId(), lastSequence });
-                        }
+                        LOG.trace("Created exchange [exchange={}, _id={}, seq={}", exchange, feed.getId(), lastSequence);
 
                         try {
                             consumer.getProcessor().process(exchange);

components/camel-crypto/src/main/java/org/apache/camel/converter/crypto/PGPKeyAccessDataFormat.java
@@ -125,10 +125,10 @@ public class PGPKeyAccessDataFormat extends ServiceSupport implements DataFormat
 
     private static final Logger LOG = LoggerFactory.getLogger(PGPKeyAccessDataFormat.class);
 
-    private static final List<String> SIGNATURE_VERIFICATION_OPTIONS = Arrays.asList(new String[] {
+    private static final List<String> SIGNATURE_VERIFICATION_OPTIONS = Arrays.asList(
             SIGNATURE_VERIFICATION_OPTION_OPTIONAL,
             SIGNATURE_VERIFICATION_OPTION_REQUIRED, SIGNATURE_VERIFICATION_OPTION_IGNORE,
-            SIGNATURE_VERIFICATION_OPTION_NO_SIGNATURE_ALLOWED });
+            SIGNATURE_VERIFICATION_OPTION_NO_SIGNATURE_ALLOWED);
 
     private static final String BC = "BC";
     private static final int BUFFER_SIZE = 16 * 1024;

components/camel-cxf-transport/src/main/java/org/apache/camel/component/cxf/common/message/DefaultCxfMessageMapper.java
@@ -78,7 +78,7 @@ public class DefaultCxfMessageMapper implements CxfMessageMapper {
 
         LOG.trace(
                 "Processing {}, requestContentType = {}, acceptContentTypes = {}, encoding = {}, path = {}, basePath = {}, verb = {}",
-                new Object[] { camelExchange, requestContentType, acceptContentTypes, enc, path, basePath, verb });
+                camelExchange, requestContentType, acceptContentTypes, enc, path, basePath, verb);
 
         return answer;
     }
@@ -131,7 +131,7 @@ public class DefaultCxfMessageMapper implements CxfMessageMapper {
         if (value != null && !strategy.applyFilterToExternalHeaders(key, value, exchange)) {
             camelHeaders.put(Exchange.HTTP_RESPONSE_CODE, value);
             LOG.trace("Populate header from CXF header={} value={} as {}",
-                    new Object[] { key, value, Exchange.HTTP_RESPONSE_CODE });
+                    key, value, Exchange.HTTP_RESPONSE_CODE);
         }
 
         // propagate HTTP CONTENT_TYPE

components/camel-cxf/src/main/java/org/apache/camel/component/cxf/jaxrs/SimpleCxfRsBinding.java
@@ -109,22 +109,21 @@ public class SimpleCxfRsBinding extends DefaultCxfRsBinding {
     /** The JAX-RS annotations to be injected as headers in the IN message */
     private static final Set<Class<?>> HEADER_ANNOTATIONS = Collections.unmodifiableSet(
             new HashSet<>(
-                    Arrays.asList(new Class<?>[] {
+                    Arrays.asList(
                             CookieParam.class,
                             FormParam.class,
                             PathParam.class,
                             HeaderParam.class,
                             MatrixParam.class,
-                            QueryParam.class })));
+                            QueryParam.class)));
 
     private static final Set<Class<?>> BINARY_ATTACHMENT_TYPES = Collections.unmodifiableSet(
             new HashSet<>(
-                    Arrays.asList(new Class<?>[] {
+                    Arrays.asList(
                             Attachment.class,
                             DataHandler.class,
                             DataSource.class,
-                            InputStream.class,
-                    })));
+                            InputStream.class)));
 
     private static final Class<?>[] NO_PARAMETER_TYPES = null;
     private static final Object[] NO_PARAMETERS = null;

components/camel-dataset/src/main/java/org/apache/camel/component/dataset/DataSetEndpoint.java
@@ -247,10 +247,9 @@ public class DataSetEndpoint extends MockEndpoint implements Service {
         if (log.isDebugEnabled()) {
             if (copy.getIn().getHeader(Exchange.DATASET_INDEX) != null) {
                 log.debug("Received message: {} (DataSet index={}) = {}",
-                        new Object[] { index, copy.getIn().getHeader(Exchange.DATASET_INDEX, Integer.class), copy });
+                        index, copy.getIn().getHeader(Exchange.DATASET_INDEX, Integer.class), copy);
             } else {
-                log.debug("Received message: {} = {}",
-                        new Object[] { index, copy });
+                log.debug("Received message: {} = {}", index, copy);
             }
         }
 

components/camel-dozer/src/main/java/org/apache/camel/converter/dozer/DozerTypeConverterLoader.java
@@ -213,9 +213,9 @@ public class DozerTypeConverterLoader extends ServiceSupport implements CamelCon
             String dozerId, Class<?> to, Class<?> from) {
         if (log.isInfoEnabled()) {
             if (dozerId != null) {
-                log.info("Added Dozer: {} as Camel type converter: {} -> {}", new Object[] { dozerId, from, to });
+                log.info("Added Dozer: {} as Camel type converter: {} -> {}", dozerId, from, to);
             } else {
-                log.info("Added Dozer as Camel type converter: {} -> {}", new Object[] { from, to });
+                log.info("Added Dozer as Camel type converter: {} -> {}", from, to);
             }
         }
 

components/camel-guava-eventbus/src/main/java/org/apache/camel/component/guava/eventbus/FilteringCamelEventHandler.java
@@ -45,7 +45,7 @@ public class FilteringCamelEventHandler extends CamelEventHandler {
         } else {
             if (log.isDebugEnabled()) {
                 log.debug("Cannot process event: {} as its class type: {} is not assignable with: {}",
-                        new Object[] { event, event.getClass().getName(), eventClass.getName() });
+                        event, event.getClass().getName(), eventClass.getName());
             }
         }
     }

commit 95857ab8b39fb240642b73df7fcf81a8c9c9902a

Fix camel-maven-plugin error breaking the CI
Reverts one of the changes introduced by the commit 6d233b66756a109d4c414b8b917bb9c95859d7d6 which was causing the plugin to fail

tooling/maven/camel-maven-plugin/src/main/java/org/apache/camel/maven/RunMojo.java
@@ -426,7 +426,7 @@ public class RunMojo extends AbstractExecMojo {
                     getLog().info("Starting Camel ...");
                     Method main = Thread.currentThread().getContextClassLoader()
                             .loadClass(mainClass).getMethod("main", String[].class);
-                    main.invoke(null, arguments);
+                    main.invoke(null, new Object[] { arguments });
 
                     afterBootstrapCamel();
                 } catch (Exception e) { // just pass it on