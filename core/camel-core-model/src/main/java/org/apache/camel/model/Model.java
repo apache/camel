113b7240e4defff455c3b432506d3528c106197c
Claus Ibsen <claus.ibsen@gmail.com>
2021-07-08 15:08:34 +0200

    CAMEL-16757: camel-core - Global error handling, interceptor in all DSL

--- core/camel-core-model/src/main/java/org/apache/camel/model/Model.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/Model.java
@@ -49,11 +49,25 @@ public interface Model {
      */
     List<ModelLifecycleStrategy> getModelLifecycleStrategies();
 
-    // TODO: better names
+    /**
+     * Adds a collection of route configuration definitions to the context
+     *
+     * @param  routesConfigurations    the route configuration(s) definition to add
+     */
     void addRouteConfigurations(List<RouteConfigurationDefinition> routesConfigurations);
 
+    /**
+     * Adds a single route configuration definition to the context
+     *
+     * @param  routesConfiguration    the route configuration to add
+     */
     void addRouteConfiguration(RouteConfigurationDefinition routesConfiguration);
 
+    /**
+     * Returns a list of the current route configuration definitions
+     *
+     * @return list of the current route configuration definitions
+     */
     List<RouteConfigurationDefinition> getRouteConfigurationDefinitions();
 
     /**
