1cfe8e987972d11e91bd5e023c825579b077056e
zregvart <zregvart@users.noreply.github.com>
2021-07-14 15:45:31 +0000

    Regen for commit d3061854d6d1125430c63a26e2db4c04fe1c5afb
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
+++ core/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
@@ -14393,7 +14393,7 @@ public class StaticEndpointBuilders {
      * The name of the destination table the data is being pushed to. Table
      * names must be unique in each destination schema, or loading issues will
      * occur. Note: The number of characters in the table name should be within
-     * the destinations allowed limits or data will rejected.
+     * the destination's allowed limits or data will rejected.
      * 
      * @param path tableName
      * @return the dsl builder
@@ -14417,7 +14417,7 @@ public class StaticEndpointBuilders {
      * The name of the destination table the data is being pushed to. Table
      * names must be unique in each destination schema, or loading issues will
      * occur. Note: The number of characters in the table name should be within
-     * the destinations allowed limits or data will rejected.
+     * the destination's allowed limits or data will rejected.
      * 
      * @param componentName to use a custom component name for the endpoint
      * instead of the default name
