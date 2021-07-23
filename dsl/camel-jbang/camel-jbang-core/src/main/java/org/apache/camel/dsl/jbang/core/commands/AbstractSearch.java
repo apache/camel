d126c10d5773bddc71d0849d1c13fa7cc9c8c778
Otavio Rodolfo Piske <opiske@redhat.com>
2021-07-23 17:40:01 +0200

    camel-jbang-core: removed a bogus sysout from the AbstractSearch class

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/AbstractSearch.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/AbstractSearch.java
@@ -50,7 +50,6 @@ public abstract class AbstractSearch {
 
     protected void setResourceLocation(String baseResourceLocation, String resourcePath) {
         this.resourceLocation = baseResourceLocation + ":" + resourcePath;
-        System.out.println("Resource location is " + resourceLocation);
     }
 
     protected void setPattern(Pattern pattern) {
