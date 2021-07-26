9a518b26548dfe6ed4f082d1bf9dfa8e3a1fd6a9
Kodanda Ramu Kakarla <kkakarla@kkakarla.pnq.csb>
2021-07-26 08:58:50 +0530

    "CAMEL-16784:Disable some ftp tests"

--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/integration/FileToFtpsExplicitSSLWithClientAuthIT.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/integration/FileToFtpsExplicitSSLWithClientAuthIT.java
@@ -18,6 +18,7 @@ package org.apache.camel.component.file.remote.integration;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -32,6 +33,7 @@ public class FileToFtpsExplicitSSLWithClientAuthIT extends FtpsServerExplicitSSL
                + "&ftpClient.keyStore.algorithm=SunX509&ftpClient.keyStore.password=password&ftpClient.keyStore.keyPassword=password&delete=true";
     }
 
+    @Disabled("CAMEL-16784:Disable testFromFileToFtp tests")
     @Test
     public void testFromFileToFtp() throws Exception {
 
