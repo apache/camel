7b8ec3fab04270023cf5658518a50b9999ea61af
Claus Ibsen <claus.ibsen@gmail.com>
2021-07-29 21:26:10 +0200

    CAMEL-16820: Circuit Breaker EIPs - Fix nested splitter throws UnsupportedOperationException due to route not set on copy exchange.

--- components/camel-microprofile/camel-microprofile-fault-tolerance/src/main/java/org/apache/camel/component/microprofile/faulttolerance/FaultToleranceProcessor.java
+++ components/camel-microprofile/camel-microprofile-fault-tolerance/src/main/java/org/apache/camel/component/microprofile/faulttolerance/FaultToleranceProcessor.java
@@ -41,6 +41,7 @@ import org.apache.camel.ExtendedCamelContext;
 import org.apache.camel.ExtendedExchange;
 import org.apache.camel.Navigate;
 import org.apache.camel.Processor;
+import org.apache.camel.Route;
 import org.apache.camel.RuntimeExchangeException;
 import org.apache.camel.api.management.ManagedAttribute;
 import org.apache.camel.api.management.ManagedResource;
@@ -415,6 +416,11 @@ public class FaultToleranceProcessor extends AsyncProcessorSupport
                     // prepare uow on copy
                     uow = copy.getContext().adapt(ExtendedCamelContext.class).getUnitOfWorkFactory().createUnitOfWork(copy);
                     copy.adapt(ExtendedExchange.class).setUnitOfWork(uow);
+                    // the copy must be starting from the route where its copied from
+                    Route route = ExchangeHelper.getRoute(exchange);
+                    if (route != null) {
+                        uow.pushRoute(route);
+                    }
                 }
 
                 // process the processor until its fully done
