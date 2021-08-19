756816a96fe75de20023270700be70f4a2680f43
Claus Ibsen <claus.ibsen@gmail.com>
2021-08-19 15:56:00 +0200

    Regen

--- core/camel-api/src/main/java/org/apache/camel/ExtendedCamelContext.java
+++ core/camel-api/src/main/java/org/apache/camel/ExtendedCamelContext.java
@@ -108,9 +108,10 @@ public interface ExtendedCamelContext extends CamelContext {
      * <p/>
      * This can be useful to know by {@link LifecycleStrategy} or the likes, in case they need to react differently.
      * <p/>
-     * As the startup procedure of {@link CamelContext} is slightly different when using plain Java versus camel-spring-xml or
-     * camel-blueprint, then we need to know when spring/blueprint are setting up the routes, which can happen after the
-     * {@link CamelContext} itself is in started state, due the asynchronous event nature of especially blueprint.
+     * As the startup procedure of {@link CamelContext} is slightly different when using plain Java versus
+     * camel-spring-xml or camel-blueprint, then we need to know when spring/blueprint are setting up the routes, which
+     * can happen after the {@link CamelContext} itself is in started state, due the asynchronous event nature of
+     * especially blueprint.
      *
      * @return <tt>true</tt> if current thread is setting up route(s), or <tt>false</tt> if not.
      */
