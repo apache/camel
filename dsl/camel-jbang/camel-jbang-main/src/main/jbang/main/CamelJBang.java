3d7281b39ee5562b1d53d733e2783ab872b90c1e
Otavio Rodolfo Piske <opiske@redhat.com>
2021-08-21 13:35:24 +0200

    camel-jbang: align features to ensure they match the same version of CamelJBang by default
    Fixed commit 8fc78df4ba22a5f0f4ffdf7778d6b6db25fff351 (reverted by 1189f27e0d6f7228ea671d93a13f5773669b3b79)

--- dsl/camel-jbang/camel-jbang-main/src/main/jbang/main/CamelJBang.java
+++ dsl/camel-jbang/camel-jbang-main/src/main/jbang/main/CamelJBang.java
@@ -235,6 +235,10 @@ class SearchKamelets extends AbstractSearch implements Callable<Integer> {
             description = "Where to download the resources from")
     private String resourceLocation;
 
+    @Option(names = { "--branch" }, defaultValue = "main", hidden = true,
+            description = "The branch to use when downloading resources from (used for development/testing)")
+    private String branch;
+
     @Override
     public void printHeader() {
         System.out.printf("%-35s %-45s %s%n", "KAMELET", "DESCRIPTION", "LINK");
@@ -244,6 +248,7 @@ class SearchKamelets extends AbstractSearch implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         setResourceLocation(resourceLocation, "camel-kamelets:docs/modules/ROOT/nav.adoc");
+        setBranch(branch);
 
         MatchExtractor<Kamelet> matchExtractor;
 
@@ -282,6 +287,10 @@ class SearchComponents extends AbstractSearch implements Callable<Integer> {
             description = "Where to download the resources from")
     private String resourceLocation;
 
+    @Option(names = { "--branch" }, defaultValue = "camel-${camel.jbang.version}", hidden = true,
+            description = "The branch to use when downloading or searching resources (mostly used for development/testing)")
+    private String branch;
+
     @Override
     public void printHeader() {
         System.out.printf("%-35s %-45s %s%n", "COMPONENT", "DESCRIPTION", "LINK");
@@ -291,6 +300,7 @@ class SearchComponents extends AbstractSearch implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         setResourceLocation(resourceLocation, "camel:docs/components/modules/ROOT/nav.adoc");
+        setBranch(branch);
 
         MatchExtractor<Component> matchExtractor;
         if (searchTerm.isEmpty()) {
@@ -333,6 +343,10 @@ class SearchLanguages extends AbstractSearch implements Callable<Integer> {
             description = "Where to download the resources from")
     private String resourceLocation;
 
+    @Option(names = { "--branch" }, defaultValue = "camel-${camel.jbang.version}", hidden = true,
+            description = "The branch to use when downloading or searching resources (mostly used for development/testing)")
+    private String branch;
+
     @Override
     public void printHeader() {
         System.out.printf("%-35s %-45s %s%n", "LANGUAGE", "DESCRIPTION", "LINK");
@@ -342,6 +356,7 @@ class SearchLanguages extends AbstractSearch implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         setResourceLocation(resourceLocation, "camel:docs/components/modules/languages/nav.adoc");
+        setBranch(branch);
 
         MatchExtractor<Language> matchExtractor;
         if (searchTerm.isEmpty()) {
@@ -383,6 +398,10 @@ class SearchOthers extends AbstractSearch implements Callable<Integer> {
             description = "Where to download the resources from")
     private String resourceLocation;
 
+    @Option(names = { "--branch" }, defaultValue = "camel-${camel.jbang.version}", hidden = true,
+            description = "The branch to use when downloading or searching resources (mostly used for development/testing)")
+    private String branch;
+
     @Override
     public void printHeader() {
         System.out.printf("%-35s %-45s %s%n", "COMPONENT", "DESCRIPTION", "LINK");
@@ -392,6 +411,7 @@ class SearchOthers extends AbstractSearch implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
         setResourceLocation(resourceLocation, "camel:docs/components/modules/others/nav.adoc");
+        setBranch(branch);
 
         MatchExtractor<Other> matchExtractor;
         if (searchTerm.isEmpty()) {
