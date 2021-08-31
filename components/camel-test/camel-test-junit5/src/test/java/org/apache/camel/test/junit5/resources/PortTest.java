da37a344e29fde2773ba853deadb952831bb5d93
Guillaume Nodet <gnodet@gmail.com>
2021-08-31 10:15:32 +0200

    Remove checkstyle warnings

--- components/camel-test/camel-test-junit5/src/test/java/org/apache/camel/test/junit5/resources/PortTest.java
+++ components/camel-test/camel-test-junit5/src/test/java/org/apache/camel/test/junit5/resources/PortTest.java
@@ -21,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+@SuppressWarnings({ "checkstyle:HideUtilityClassConstructor", "checkstyle:DeclarationOrder" })
 public class PortTest {
 
     @Resources
