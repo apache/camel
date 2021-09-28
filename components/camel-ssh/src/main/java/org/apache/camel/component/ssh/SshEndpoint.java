7496887fdf9c38be0142410fbc13c797383680b7
Claus Ibsen <claus.ibsen@gmail.com>
2021-09-28 12:08:24 +0200

    CAMEL-17011: Fixed camel-ssh producer to not be singleton.

--- components/camel-ssh/src/main/java/org/apache/camel/component/ssh/SshEndpoint.java
+++ components/camel-ssh/src/main/java/org/apache/camel/component/ssh/SshEndpoint.java
@@ -24,8 +24,6 @@ import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.support.ScheduledPollEndpoint;
 import org.apache.sshd.common.keyprovider.KeyPairProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Execute commands on remote hosts using SSH.
@@ -33,7 +31,6 @@ import org.slf4j.LoggerFactory;
 @UriEndpoint(firstVersion = "2.10.0", scheme = "ssh", title = "SSH", syntax = "ssh:host:port",
              alternativeSyntax = "ssh:username:password@host:port", category = { Category.FILE })
 public class SshEndpoint extends ScheduledPollEndpoint {
-    protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @UriParam
     private SshConfiguration configuration;
@@ -51,12 +48,6 @@ public class SshEndpoint extends ScheduledPollEndpoint {
     }
 
     @Override
-    public boolean isSingletonProducer() {
-        // SshClient is not thread-safe to be shared
-        return false;
-    }
-
-    @Override
     public Producer createProducer() throws Exception {
         return new SshProducer(this);
     }
@@ -66,6 +57,13 @@ public class SshEndpoint extends ScheduledPollEndpoint {
         SshConsumer consumer = new SshConsumer(this, processor);
         configureConsumer(consumer);
         return consumer;
+    }
+
+    @Override
+    public boolean isSingletonProducer() {
+        // this producer is stateful because the ssh client is not
+        // thread safe
+        return false;
     }
 
     public SshConfiguration getConfiguration() {
