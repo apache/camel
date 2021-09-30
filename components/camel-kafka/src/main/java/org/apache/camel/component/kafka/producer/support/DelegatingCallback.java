eafe3e53eae767acbda051a58a118e8132369a6f
Otavio Rodolfo Piske <opiske@redhat.com>
2021-09-30 16:52:40 +0200

    CAMEL-17026: initial refactoring to allow easier implementation of the feature

---
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/producer/support/DelegatingCallback.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.component.kafka.producer.support;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public final class DelegatingCallback implements Callback {
+
+    private final List<Callback> callbacks;
+
+    public DelegatingCallback(Callback... callbacks) {
+        this.callbacks = Arrays.asList(callbacks);
+    }
+
+    @Override
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        callbacks.forEach(c -> c.onCompletion(metadata, exception));
+    }
+}
