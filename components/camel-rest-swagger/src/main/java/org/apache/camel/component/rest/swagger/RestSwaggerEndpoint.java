apache/camel

commit 0b901956aa9b1948ea9f793b87fcb478161e03db

CAMEL-17049 Changed camelContext.getEndpoint() variant in createProducer (#6226)
* CAMEL-17049 Changed camelContext.getEndpoint() variant in createProducer

* CAMEL-17049 Changed camelContext.getEndpoint() variant in createProducer

* CAMEL-17049 Fixed format issue

* CAMEL-17049 Changed camelContext.getEndpoint() variant in createConsumer

* CAMEL-17049 Formatting issue

components/camel-rest-swagger/src/main/java/org/apache/camel/component/rest/swagger/RestSwaggerEndpoint.java
@@ -402,11 +402,16 @@ public final class RestSwaggerEndpoint extends DefaultEndpoint {
             componentParameters.put("sslContextParameters", component.getSslContextParameters());
         }
 
+        final Map<Object, Object> nestedParameters = new HashMap<>();
         if (!componentParameters.isEmpty()) {
-            final Map<Object, Object> nestedParameters = new HashMap<>();
-            nestedParameters.put("component", componentParameters);
-
             // we're trying to set RestEndpoint.parameters['component']
+            nestedParameters.put("component", componentParameters);
+        }
+
+        // Add rest endpoint parameters
+        nestedParameters.putAll(this.parameters);
+
+        if (!nestedParameters.isEmpty()) {
             parameters.put("parameters", nestedParameters);
         }
 

components/camel-coap/src/main/java/org/apache/camel/coap/CoAPComponent.java
@@ -190,8 +190,7 @@ public class CoAPComponent extends DefaultComponent implements RestConsumerFacto
             url += "&" + query;
         }
 
-        CoAPEndpoint endpoint = camelContext.getEndpoint(url, CoAPEndpoint.class);
-        setProperties(endpoint, parameters);
+        CoAPEndpoint endpoint = (CoAPEndpoint) camelContext.getEndpoint(url, parameters);
 
         // configure consumer properties
         Consumer consumer = endpoint.createConsumer(processor);

components/camel-http/src/main/java/org/apache/camel/component/http/HttpComponent.java
@@ -577,8 +577,8 @@ public class HttpComponent extends HttpCommonComponent implements RestProducerFa
         // the component, one such case is when we switch from "http" to "https" component name
         RestProducerFactoryHelper.setupComponentFor(url, camelContext, (Map<String, Object>) parameters.remove("component"));
 
-        HttpEndpoint endpoint = camelContext.getEndpoint(url, HttpEndpoint.class);
-        setProperties(endpoint, parameters);
+        HttpEndpoint endpoint = (HttpEndpoint) camelContext.getEndpoint(url, parameters);
+
         String path = uriTemplate != null ? uriTemplate : basePath;
         endpoint.setHeaderFilterStrategy(new HttpRestHeaderFilterStrategy(path, queryParameters));
 

components/camel-jetty-common/src/main/java/org/apache/camel/component/jetty/JettyHttpComponent.java
@@ -1123,8 +1123,7 @@ public abstract class JettyHttpComponent extends HttpCommonComponent
 
         String url = RestComponentHelper.createRestConsumerUrl("jetty", scheme, host, port, path, map);
 
-        JettyHttpEndpoint endpoint = camelContext.getEndpoint(url, JettyHttpEndpoint.class);
-        setProperties(endpoint, parameters);
+        JettyHttpEndpoint endpoint = (JettyHttpEndpoint) camelContext.getEndpoint(url, parameters);
 
         if (!map.containsKey("httpBindingRef")) {
             // use the rest binding, if not using a custom http binding

components/camel-netty-http/src/main/java/org/apache/camel/component/netty/http/NettyHttpComponent.java
@@ -417,8 +417,7 @@ public class NettyHttpComponent extends NettyComponent
 
         String url = RestComponentHelper.createRestConsumerUrl("netty-http", scheme, host, port, path, map);
 
-        NettyHttpEndpoint endpoint = camelContext.getEndpoint(url, NettyHttpEndpoint.class);
-        setProperties(endpoint, parameters);
+        NettyHttpEndpoint endpoint = (NettyHttpEndpoint) camelContext.getEndpoint(url, parameters);
 
         // configure consumer properties
         Consumer consumer = endpoint.createConsumer(processor);
@@ -478,8 +477,7 @@ public class NettyHttpComponent extends NettyComponent
         // the component
         RestProducerFactoryHelper.setupComponentFor(url, camelContext, (Map<String, Object>) parameters.remove("component"));
 
-        NettyHttpEndpoint endpoint = camelContext.getEndpoint(url, NettyHttpEndpoint.class);
-        setProperties(endpoint, parameters);
+        NettyHttpEndpoint endpoint = (NettyHttpEndpoint) camelContext.getEndpoint(url, parameters);
         String path = uriTemplate != null ? uriTemplate : basePath;
         endpoint.setHeaderFilterStrategy(new NettyHttpRestHeaderFilterStrategy(path, queryParameters));
 

components/camel-platform-http/src/main/java/org/apache/camel/component/platform/http/PlatformHttpComponent.java
@@ -165,8 +165,7 @@ public class PlatformHttpComponent extends DefaultComponent implements RestConsu
 
         String url = RestComponentHelper.createRestConsumerUrl("platform-http", path, map);
 
-        PlatformHttpEndpoint endpoint = camelContext.getEndpoint(url, PlatformHttpEndpoint.class);
-        setProperties(endpoint, parameters);
+        PlatformHttpEndpoint endpoint = (PlatformHttpEndpoint) camelContext.getEndpoint(url, parameters);
         endpoint.setConsumes(consumes);
         endpoint.setProduces(produces);
 

components/camel-resteasy/src/main/java/org/apache/camel/component/resteasy/ResteasyComponent.java
@@ -205,8 +205,7 @@ public class ResteasyComponent extends HttpComponent implements RestConsumerFact
         // get the endpoint
         url = String.format(url, path, restrict);
 
-        ResteasyEndpoint endpoint = camelContext.getEndpoint(url, ResteasyEndpoint.class);
-        setProperties(endpoint, parameters);
+        ResteasyEndpoint endpoint = (ResteasyEndpoint) camelContext.getEndpoint(url, parameters);
 
         Consumer consumer = endpoint.createConsumer(processor);
         if (config.getConsumerProperties() != null && !config.getConsumerProperties().isEmpty()) {

components/camel-servlet/src/main/java/org/apache/camel/component/servlet/ServletComponent.java
@@ -314,8 +314,7 @@ public class ServletComponent extends HttpCommonComponent implements RestConsume
 
         String url = RestComponentHelper.createRestConsumerUrl("servlet", path, map);
 
-        ServletEndpoint endpoint = camelContext.getEndpoint(url, ServletEndpoint.class);
-        setProperties(endpoint, parameters);
+        ServletEndpoint endpoint = (ServletEndpoint) camelContext.getEndpoint(url, parameters);
 
         if (!map.containsKey("httpBinding")) {
             // use the rest binding, if not using a custom http binding

commit 46e8526f8e8c1bc3c203be81b1d600bc0d896dcc

CAMEL-17049: fix CI issue caused by parameters being passed to the endpoint
This should fix CI issues introduced by 0b901956aa9b1948ea9f793b87fcb478161e03db

components/camel-rest-swagger/src/main/java/org/apache/camel/component/rest/swagger/RestSwaggerEndpoint.java
@@ -409,13 +409,28 @@ public final class RestSwaggerEndpoint extends DefaultEndpoint {
         }
 
         // Add rest endpoint parameters
-        nestedParameters.putAll(this.parameters);
+        addRestEndpointParameters(operation, nestedParameters);
 
         if (!nestedParameters.isEmpty()) {
             parameters.put("parameters", nestedParameters);
         }
 
         return parameters;
+    }
+
+    private void addRestEndpointParameters(Operation operation, Map<Object, Object> nestedParameters) {
+        if (this.parameters != null && operation.getParameters() != null) {
+            for (Entry<String, Object> entry : this.parameters.entrySet()) {
+                for (Parameter param : operation.getParameters()) {
+                    // skip parameters that are part of the operation as path as otherwise
+                    // it will be duplicated as query parameter as well
+                    boolean clash = "path".equals(param.getIn()) && entry.getKey().equals(param.getName());
+                    if (!clash) {
+                        nestedParameters.put(entry.getKey(), entry.getValue());
+                    }
+                }
+            }
+        }
     }
 
     String determineHost(final Swagger swagger) {