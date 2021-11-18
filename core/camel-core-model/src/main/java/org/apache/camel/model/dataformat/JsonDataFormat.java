9b546cfc88225f57aeb63796ddec4698d1e83849
Andrea Cosentino <ancosen@gmail.com>
2021-11-18 13:54:02 +0100

    CAMEL-16912 - camel-jackson - Make it easy to configure property naming strategy

--- core/camel-core-model/src/main/java/org/apache/camel/model/dataformat/JsonDataFormat.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/dataformat/JsonDataFormat.java
@@ -97,6 +97,8 @@ public class JsonDataFormat extends DataFormatDefinition implements ContentTypeH
     @Metadata(javaType = "java.lang.Boolean", defaultValue = "true")
     private String autoDiscoverSchemaResolver;
     @XmlAttribute
+    @Metadata(description = "If set then Jackson will use the the defined Property Naming Strategy."
+                            + "Possible values are: LOWER_CAMEL_CASE, LOWER_DOT_CASE, LOWER_CASE, KEBAB_CASE, SNAKE_CASE and UPPER_CAMEL_CASE" )
     private String namingStrategy;
 
     public JsonDataFormat() {
@@ -439,7 +441,8 @@ public class JsonDataFormat extends DataFormatDefinition implements ContentTypeH
     }
 
     /**
-     * If set then Jackson will use the the defined Property Naming Strategy
+     * If set then Jackson will use the the defined Property Naming Strategy.
+     * Possible values are: LOWER_CAMEL_CASE, LOWER_DOT_CASE, LOWER_CASE, KEBAB_CASE, SNAKE_CASE and UPPER_CAMEL_CASE
      */
     public void setNamingStrategy(String namingStrategy) {
         this.namingStrategy = namingStrategy;
