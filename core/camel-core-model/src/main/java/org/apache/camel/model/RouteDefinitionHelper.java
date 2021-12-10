a2a22ccb7ddd1d70c949e9f0b3f70842d03a5a39
Claus Ibsen <claus.ibsen@gmail.com>
2021-12-10 20:23:07 +0100

    CAMEL-17295: rest-dsl - Fix parse uri template for query parameters.

--- core/camel-core-model/src/main/java/org/apache/camel/model/RouteDefinitionHelper.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/RouteDefinitionHelper.java
@@ -231,14 +231,14 @@ public final class RouteDefinitionHelper {
     private static VerbDefinition findVerbDefinition(RestDefinition rest, String endpointUri) throws Exception {
         VerbDefinition ret = null;
         String preVerbUri = "";
+        String target = URISupport.normalizeUri(endpointUri);
         for (VerbDefinition verb : rest.getVerbs()) {
             String verbUri = URISupport.normalizeUri(rest.buildFromUri(verb));
-            if (endpointUri.startsWith(verbUri) && preVerbUri.length() < verbUri.length()) {
-                // if there are multiple verb uri match, select the most
-                // specific one
+            if (target.startsWith(verbUri) && preVerbUri.length() < verbUri.length()) {
+                // if there are multiple verb uri match, select the most specific one
                 // for example if the endpoint Uri is
                 // rest:get:/user:/{id}/user?produces=text%2Fplain
-                // then the verbUri rest:get:/user:/{id}/user should overweigh
+                // then the verbUri rest:get:/user:/{id}/user should overrule
                 // the est:get:/user:/{id}
                 preVerbUri = verbUri;
                 ret = verb;
