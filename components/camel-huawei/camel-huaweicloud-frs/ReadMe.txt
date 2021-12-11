d200ad0fc33e1b7bbae25f1c6c655a66971aee11
hokutor <hwcamel@huawei.com>
2021-12-11 11:53:09 +0800

    CAMEL-17321: Add HuaweiCloud FaceRecognitionService(FRS) Component

---
+++ components/camel-huawei/camel-huaweicloud-frs/ReadMe.txt
@@ -0,0 +1,18 @@
+Camel Java Router Project
+=========================
+
+=== How to build
+
+To build this project use
+
+    mvn install
+
+=== How to run
+
+You can run this example using
+
+    mvn camel:run
+
+=== More information
+
+You can find more information about Apache Camel at the website: http://camel.apache.org/
---
+++ components/camel-huawei/camel-huaweicloud-frs/src/main/java/org/apache/camel/MyRouteBuilder.java
@@ -0,0 +1,28 @@
+package org.apache.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Java code...
+     */
+    public void configure() {
+
+        // here is a sample which processes the input files
+        // (leaving them in place - see the 'noop' flag)
+        // then performs content based routing on the message using XPath
+        from("file:src/data?noop=true")
+            .choice()
+                .when(xpath("/person/city = 'London'"))
+                    .log("UK message")
+                    .to("file:target/messages/uk")
+                .otherwise()
+                    .log("Other message")
+                    .to("file:target/messages/others");
+    }
+
+}
