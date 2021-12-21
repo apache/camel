e834245183e11e152a2c29b05ecc4ff845f60510
Claus Ibsen <claus.ibsen@gmail.com>
2021-12-21 09:48:39 +0100

    CAMEL-17361: camel-management - Move routecontroller mbeans to services

--- core/camel-management/src/test/java/org/apache/camel/management/ManagedProducerRouteAddRemoveRegisterAlwaysTest.java
+++ core/camel-management/src/test/java/org/apache/camel/management/ManagedProducerRouteAddRemoveRegisterAlwaysTest.java
@@ -36,7 +36,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DisabledOnOs(OS.AIX)
 public class ManagedProducerRouteAddRemoveRegisterAlwaysTest extends ManagementTestSupport {
 
-    private static final int SERVICES = 12;
+    private static final int SERVICES = 13;
 
     @Override
     protected CamelContext createCamelContext() throws Exception {
