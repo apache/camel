198d8d2e58809cac00978a5d8fd63b9a82d0d344
Claus Ibsen <claus.ibsen@gmail.com>
2022-01-08 10:22:01 +0100

    Regen

--- core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
+++ core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
@@ -110,8 +110,7 @@ public final class SensitiveUtils {
     }
 
     /**
-     * Whether the given configuration property contains a sensitive key (such as password, accesstoken,
-     * etc.)
+     * Whether the given configuration property contains a sensitive key (such as password, accesstoken, etc.)
      *
      * @param  text the configuration property
      * @return      true if sensitive, false otherwise
