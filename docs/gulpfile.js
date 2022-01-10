d2691cf98b14b3e1e008349723f27e5d7b94be2c
Zoran Regvart <zoran@regvart.com>
2022-01-10 19:43:43 +0100

    Refactor copying images
    We don't want images in `.../assets/images` but in `.../images`.

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -39,7 +39,7 @@ function deleteComponentSymlinks () {
 }
 
 function deleteComponentImageSymlinks () {
-  return del(['components/modules/ROOT/assets/images/*'])
+  return del(['components/modules/ROOT/images/*'])
 }
 
 function createComponentSymlinks () {
@@ -178,7 +178,7 @@ function createComponentImageSymlinks () {
       // }));
       // uncomment above .pipe() and remove the .pipe() below
       // when antora#188 is resolved
-      .pipe(dest('components/modules/ROOT/assets/images/'))
+      .pipe(dest('components/modules/ROOT/images/'))
   )
 }
 
