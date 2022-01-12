ab59ad48810dd6e6a8d0d852abb3a32257772b6e
Freeman Fang <freeman.fang@gmail.com>
2022-01-11 19:30:29 -0500

    [CAMEL-17475]upgrade to CXF 3.5.0

--- components/camel-cxf/src/test/java/org/apache/camel/component/cxf/mtom/CxfMtomDisabledProducerPayloadModeTest.java
+++ components/camel-cxf/src/test/java/org/apache/camel/component/cxf/mtom/CxfMtomDisabledProducerPayloadModeTest.java
@@ -167,7 +167,7 @@ public class CxfMtomDisabledProducerPayloadModeTest extends CxfMtomProducerPaylo
                 DataSource ds = new AttachmentDataSource("image/jpeg", getClass().getResourceAsStream("/Splash.jpg"));
                 map.put(MtomTestHelper.RESP_IMAGE_CID, new DataHandler(ds));
 
-            } catch (IOException e) {
+            } catch (Exception e) {
                 LOG.warn("I/O error: {}", e.getMessage(), e);
             }
 
@@ -177,7 +177,7 @@ public class CxfMtomDisabledProducerPayloadModeTest extends CxfMtomProducerPaylo
                         new ByteArrayInputStream(MtomTestHelper.RESP_PHOTO_DATA));
                 map.put(MtomTestHelper.RESP_PHOTO_CID, new DataHandler(ds));
 
-            } catch (IOException e) {
+            } catch (Exception e) {
                 LOG.warn("I/O error: {}", e.getMessage(), e);
             }
 
