409c261fb2326c20ed0853462cb9b41f6b9195eb
jamesnetherton <jamesnetherton@users.noreply.github.com>
2022-01-21 16:22:01 +0000

    Regen for commit 82b20113d2ea12555df429731f56ad0393938631
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
@@ -5599,10 +5599,10 @@ public class StaticEndpointBuilders {
      * 
      * Path parameter: apiName (required)
      * What kind of operation to perform
-     * There are 13 enums and the value can be one of: DRIVE_ABOUT, DRIVE_APPS,
+     * There are 12 enums and the value can be one of: DRIVE_ABOUT, DRIVE_APPS,
      * DRIVE_CHANGES, DRIVE_CHANNELS, DRIVE_CHILDREN, DRIVE_COMMENTS,
      * DRIVE_FILES, DRIVE_PARENTS, DRIVE_PERMISSIONS, DRIVE_PROPERTIES,
-     * DRIVE_REALTIME, DRIVE_REPLIES, DRIVE_REVISIONS
+     * DRIVE_REPLIES, DRIVE_REVISIONS
      * 
      * Path parameter: methodName (required)
      * What sub operation to use for the selected operation
@@ -5629,10 +5629,10 @@ public class StaticEndpointBuilders {
      * 
      * Path parameter: apiName (required)
      * What kind of operation to perform
-     * There are 13 enums and the value can be one of: DRIVE_ABOUT, DRIVE_APPS,
+     * There are 12 enums and the value can be one of: DRIVE_ABOUT, DRIVE_APPS,
      * DRIVE_CHANGES, DRIVE_CHANNELS, DRIVE_CHILDREN, DRIVE_COMMENTS,
      * DRIVE_FILES, DRIVE_PARENTS, DRIVE_PERMISSIONS, DRIVE_PROPERTIES,
-     * DRIVE_REALTIME, DRIVE_REPLIES, DRIVE_REVISIONS
+     * DRIVE_REPLIES, DRIVE_REVISIONS
      * 
      * Path parameter: methodName (required)
      * What sub operation to use for the selected operation
