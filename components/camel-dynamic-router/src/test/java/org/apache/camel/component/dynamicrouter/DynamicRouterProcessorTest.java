33c340381c5b0bdf300a40dfad832a0633d4908d
Steve Storck <steve973@gmail.com>
2022-01-27 03:13:07 -0500

    CAMEL-17500: Simplified Dynamic Router control channel messages. (#6810)
    * CAMEL-17500: Simplified Dynamic Router control channel messages.
    
    * CAMEL-17500: Fixed a few missed checkstyle problems.
    
    * CAMEL-17500: Updated to allow mode to match multiple participants.
    
    * CAMEL-17500: Added mode to send exchanges to all matching recipients.

--- components/camel-dynamic-router/src/test/java/org/apache/camel/component/dynamicrouter/DynamicRouterProcessorTest.java
+++ components/camel-dynamic-router/src/test/java/org/apache/camel/component/dynamicrouter/DynamicRouterProcessorTest.java
@@ -14,25 +14,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.dynamicrouter.processor;
+package org.apache.camel.component.dynamicrouter;
 
 import org.apache.camel.AsyncCallback;
 import org.apache.camel.Exchange;
-import org.apache.camel.component.dynamicrouter.support.CamelDynamicRouterTestSupport;
+import org.apache.camel.component.dynamicrouter.support.DynamicRouterTestSupport;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.apache.camel.component.dynamicrouter.DynamicRouterConstants.MODE_FIRST_MATCH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
-class DynamicRouterProcessorTest extends CamelDynamicRouterTestSupport {
+class DynamicRouterProcessorTest extends DynamicRouterTestSupport {
 
     @BeforeEach
     void localSetup() throws Exception {
         super.setup();
-        processor = new DynamicRouterProcessor(PROCESSOR_ID, context, false, () -> filterProcessorFactory);
+        processor = new DynamicRouterProcessor(PROCESSOR_ID, context, MODE_FIRST_MATCH, false, () -> filterProcessorFactory);
     }
 
     @Test
@@ -40,41 +42,41 @@ class DynamicRouterProcessorTest extends CamelDynamicRouterTestSupport {
         when(controlMessage.getPriority()).thenReturn(1);
         when(controlMessage.getPredicate()).thenReturn(e -> true);
         PrioritizedFilterProcessor result = processor.createFilter(controlMessage);
-        Assertions.assertEquals(filterProcessor, result);
+        assertEquals(filterProcessor, result);
     }
 
     @Test
     void addFilterAsControlMessage() {
         processor.addFilter(controlMessage);
-        Assertions.assertNotNull(processor.getFilter(MESSAGE_ID));
+        Assertions.assertNotNull(processor.getFilter(TEST_ID));
     }
 
     @Test
     void addFilterAsFilterProcessor() {
         processor.addFilter(filterProcessor);
-        PrioritizedFilterProcessor result = processor.getFilter(MESSAGE_ID);
-        Assertions.assertEquals(filterProcessor, result);
+        PrioritizedFilterProcessor result = processor.getFilter(TEST_ID);
+        assertEquals(filterProcessor, result);
     }
 
     @Test
     void removeFilter() {
         addFilterAsFilterProcessor();
-        processor.removeFilter(MESSAGE_ID);
-        PrioritizedFilterProcessor result = processor.getFilter(MESSAGE_ID);
+        processor.removeFilter(TEST_ID);
+        PrioritizedFilterProcessor result = processor.getFilter(TEST_ID);
         Assertions.assertNull(result);
     }
 
     @Test
     void matchFiltersMatches() {
         addFilterAsFilterProcessor();
-        PrioritizedFilterProcessor result = processor.matchFilters(exchange);
-        Assertions.assertEquals(MESSAGE_ID, result.getId());
+        PrioritizedFilterProcessor result = processor.matchFilters(exchange).get(0);
+        assertEquals(TEST_ID, result.getId());
     }
 
     @Test
     void matchFiltersDoesNotMatch() {
-        PrioritizedFilterProcessor result = processor.matchFilters(exchange);
-        Assertions.assertEquals(Integer.MAX_VALUE, result.getPriority());
+        PrioritizedFilterProcessor result = processor.matchFilters(exchange).get(0);
+        assertEquals(Integer.MAX_VALUE, result.getPriority());
     }
 
     @Test
@@ -82,7 +84,7 @@ class DynamicRouterProcessorTest extends CamelDynamicRouterTestSupport {
         addFilterAsFilterProcessor();
         when(filterProcessor.matches(exchange)).thenReturn(true);
         lenient().when(filterProcessor.process(any(Exchange.class), any(AsyncCallback.class))).thenReturn(true);
-        Assertions.assertTrue(processor.process(exchange, asyncCallback));
+        Assertions.assertFalse(processor.process(exchange, asyncCallback));
     }
 
     @Test
@@ -90,5 +92,15 @@ class DynamicRouterProcessorTest extends CamelDynamicRouterTestSupport {
         addFilterAsFilterProcessor();
         when(filterProcessor.matches(exchange)).thenReturn(false);
         Assertions.assertFalse(processor.process(exchange, asyncCallback));
+    }
+
+    @Test
+    void testStringIsId() {
+        assertEquals(PROCESSOR_ID, processor.toString());
+    }
+
+    @Test
+    void testTraceLabelIsId() {
+        assertEquals(PROCESSOR_ID, processor.getTraceLabel());
     }
 }
