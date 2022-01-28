9d9d40b5c5c0020bdafc622994a81edf1ecaf0e0
Claus Ibsen <claus.ibsen@gmail.com>
2022-01-28 10:12:01 +0100

    CAMEL-17398: Include core/dsl in catalog

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
@@ -252,7 +252,10 @@ public class PrepareCatalogMojo extends AbstractMojo {
             Stream.of(list(dslDir.toPath())).flatMap(s -> s)
                     .flatMap(p -> getComponentPath(p).stream())
                     .filter(dir -> Files.isDirectory(dir.resolve("src/generated/resources")))
-                    .flatMap(PackageHelper::walk).forEach(p -> {
+                    .map(p -> p.resolve("src/generated/resources"))
+                    .flatMap(PackageHelper::walk)
+                    .filter(Files::isRegularFile)
+                    .forEach(p -> {
                         String f = p.getFileName().toString();
                         if (f.endsWith(PackageHelper.JSON_SUFIX)) {
                             allJsonFiles.add(p);
