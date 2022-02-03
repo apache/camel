60b0f6179f7fa4ed4e57827c5f857eab58cda3e4
Claus Ibsen <claus.ibsen@gmail.com>
2022-02-03 07:11:59 +0100

    CAMEL-17580: CamelConfiguration to camel-api to make it generic for all Camel runtimes.

--- archetypes/camel-archetype-main/src/main/resources/archetype-resources/src/main/java/MyConfiguration.java
+++ archetypes/camel-archetype-main/src/main/resources/archetype-resources/src/main/java/MyConfiguration.java
@@ -17,24 +17,20 @@
 package ${package};
 
 import org.apache.camel.BindToRegistry;
-import org.apache.camel.CamelConfiguration;
+import org.apache.camel.Configuration;
 import org.apache.camel.CamelContext;
 import org.apache.camel.PropertyInject;
 
 /**
  * Class to configure the Camel application.
  */
-public class MyConfiguration implements CamelConfiguration {
+@Configuration
+public class MyConfiguration {
 
     @BindToRegistry
     public MyBean myBean(@PropertyInject("hi") String hi, @PropertyInject("bye") String bye) {
         // this will create an instance of this bean with the name of the method (eg myBean)
         return new MyBean(hi, bye);
-    }
-
-    @Override
-    public void configure(CamelContext camelContext) throws Exception {
-        // this method is optional and can be removed if no additional configuration is needed.
     }
 
 }
