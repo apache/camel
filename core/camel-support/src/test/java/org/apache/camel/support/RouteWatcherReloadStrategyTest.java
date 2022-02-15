098e1cedcdf39388fecb6984e7c9d82733843db6
JP Moresmau <jp@moresmau.fr>
2022-02-15 21:36:03 +0100

    formatting

--- core/camel-support/src/test/java/org/apache/camel/support/RouteWatcherReloadStrategyTest.java
+++ core/camel-support/src/test/java/org/apache/camel/support/RouteWatcherReloadStrategyTest.java
@@ -13,40 +13,42 @@ public class RouteWatcherReloadStrategyTest {
 
     /**
      * This used to fail on Windows because we hardcoded '/' as a file part separator
-     * @throws Exception
+     *
+     * @throws Exception if doStart fails
      */
     @Test
-    public void testBasePath() throws Exception{
-        RouteWatcherReloadStrategy strategy=new RouteWatcherReloadStrategy("./src/test/resources");
+    public void testBasePath() throws Exception {
+        RouteWatcherReloadStrategy strategy = new RouteWatcherReloadStrategy("./src/test/resources");
         strategy.setPattern("*");
         strategy.setCamelContext(new AbstractExchangeTest.MyCamelContext());
         strategy.doStart();
         assertNotNull(strategy.getFileFilter());
-        File folder=new File("./src/test/resources");
+        File folder = new File("./src/test/resources");
         assertTrue(folder.isDirectory());
-        File[] fs=folder.listFiles(strategy.getFileFilter());
+        File[] fs = folder.listFiles(strategy.getFileFilter());
         assertNotNull(fs);
-        assertEquals(1,fs.length);
-        assertEquals("log4j2.properties",fs[0].getName());
+        assertEquals(1, fs.length);
+        assertEquals("log4j2.properties", fs[0].getName());
     }
 
     /**
      * Forgetting to set the pattern caused it to be set to null, which caused a NullPointerException in the filter
-     * @throws Exception
+     *
+     * @throws Exception if doStart fails
      */
     @Test
     public void testNullPattern() throws Exception {
-        RouteWatcherReloadStrategy strategy=new RouteWatcherReloadStrategy("./src/test/resources");
+        RouteWatcherReloadStrategy strategy = new RouteWatcherReloadStrategy("./src/test/resources");
         strategy.setPattern(null);
         strategy.setCamelContext(new AbstractExchangeTest.MyCamelContext());
         strategy.doStart();
         assertNotNull(strategy.getFileFilter());
-        File folder=new File("./src/test/resources");
+        File folder = new File("./src/test/resources");
         assertTrue(folder.isDirectory());
-        File[] fs=folder.listFiles(strategy.getFileFilter());
+        File[] fs = folder.listFiles(strategy.getFileFilter());
         assertNotNull(fs);
-        assertEquals(0,fs.length);
+        assertEquals(0, fs.length);
         // null goes back to default
-        assertEquals("*.yaml,*.xml",strategy.getPattern());
+        assertEquals("*.yaml,*.xml", strategy.getPattern());
     }
 }
