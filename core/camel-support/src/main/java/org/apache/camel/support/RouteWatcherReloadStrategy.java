apache/camel

commit e98ac78cb3731499ba7d6393886fb60c2b78b75a

[CAMEL-17651] fix reload strategy and related doc (#6956)
* fix reload strategy and related doc

* formatting

* formatting

* formatting

* Fake CamelContext

* formatting and license

core/camel-support/src/main/java/org/apache/camel/support/RouteWatcherReloadStrategy.java
@@ -48,6 +48,13 @@ public class RouteWatcherReloadStrategy extends FileWatcherResourceReloadStrateg
 
     private static final Logger LOG = LoggerFactory.getLogger(RouteWatcherReloadStrategy.class);
 
+    /**
+     * The default pattern: all XML and all YAML files
+     */
+    private static final String DEFAULT_PATTERN = "*.yaml,*.xml";
+    /**
+     * The file name pattern to watch for
+     */
     private String pattern = "*";
     private boolean removeAllRoutes = true;
 
@@ -74,6 +81,10 @@ public class RouteWatcherReloadStrategy extends FileWatcherResourceReloadStrateg
      */
     public void setPattern(String pattern) {
         this.pattern = pattern;
+        // sanity
+        if (this.pattern == null || this.pattern.trim().length() == 0) {
+            this.pattern = DEFAULT_PATTERN;
+        }
     }
 
     public boolean isRemoveAllRoutes() {
@@ -106,7 +117,7 @@ public class RouteWatcherReloadStrategy extends FileWatcherResourceReloadStrateg
                     // strip starting directory, so we have a relative name to the starting folder
                     String path = f.getAbsolutePath();
                     if (path.startsWith(base)) {
-                        path = path.substring(path.lastIndexOf("/"));
+                        path = FileUtil.onlyPath(path);
                     }
                     path = FileUtil.stripLeadingSeparator(path);
 

commit eeb3145ff54ced45697f580b72f1ba5346c56979

CAMEL-17651: fix bug in reload strategy fileFilter introduced by PR #6956. The filter needs the filename not the path.

core/camel-support/src/main/java/org/apache/camel/support/RouteWatcherReloadStrategy.java
@@ -114,9 +114,8 @@ public class RouteWatcherReloadStrategy extends FileWatcherResourceReloadStrateg
                     // strip starting directory, so we have a relative name to the starting folder
                     String path = f.getAbsolutePath();
                     if (path.startsWith(base)) {
-                        path = FileUtil.onlyPath(path);
+                        path = FileUtil.stripPath(path);
                     }
-                    path = FileUtil.stripLeadingSeparator(path);
 
                     boolean result = matcher.match(part, path, false);
                     LOG.trace("Accepting file pattern:{} path:{} -> {}", part, path, result);