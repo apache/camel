0376d20b195b46b588590625bfff299a2c8ab393
Claus Ibsen <claus.ibsen@gmail.com>
2022-02-18 09:43:21 +0100

    CAMEL-17658 camel-debezium: additionalProperties are trimmed (#6960)

--- core/camel-support/src/main/java/org/apache/camel/support/PropertyBindingSupport.java
+++ core/camel-support/src/main/java/org/apache/camel/support/PropertyBindingSupport.java
@@ -34,7 +34,6 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ExtendedCamelContext;
@@ -329,14 +328,8 @@ public final class PropertyBindingSupport {
                 // prepare for next iterator
                 newTarget = prop;
                 newClass = newTarget.getClass();
-                //do not ignore remaining parts, which was not traversed
-                if (parts.length > 1 && i < parts.length - 2) {
-                    newName = IntStream.range(i + 1, parts.length)
-                            .mapToObj(j -> parts[j])
-                            .collect(Collectors.joining("."));
-                } else {
-                    newName = parts[i + 1];
-                }
+                // do not ignore remaining parts, which was not traversed
+                newName = Arrays.stream(parts, i + 1, parts.length).collect(Collectors.joining("."));
                 // if we have not yet found a configurer for the new target
                 if (configurer == null) {
                     configurer = PropertyConfigurerHelper.resolvePropertyConfigurer(camelContext, newTarget);
