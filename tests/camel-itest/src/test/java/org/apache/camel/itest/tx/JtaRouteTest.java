bfa959815d4969a12af0aaee7ab9382147154aca
Zheng Feng <zh.feng@gmail.com>
2022-03-01 21:59:05 +0800

    CAMEL-17474 add assertions to check the copied exchanges are marked as transactional

--- tests/camel-itest/src/test/java/org/apache/camel/itest/tx/JtaRouteTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/tx/JtaRouteTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.camel.itest.tx;
 
+import java.util.Arrays;
+
 import org.apache.camel.AggregationStrategy;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
@@ -25,6 +27,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.transaction.RequiresNewJtaTransactionPolicy;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JtaRouteTest extends CamelTestSupport {
@@ -70,10 +73,8 @@ public class JtaRouteTest extends CamelTestSupport {
                         .to("direct:split");
 
                 from("direct:split")
-                        .split(body()).delimiter("_").to("direct:splitted").end()
+                        .split(body()).delimiter("_").to("mock:splitted").end()
                         .log("after splitter log which you will never see...");
-
-                from("direct:splitted").to("mock:splitted");
 
                 from("direct:multicast_test").routeId("r.route1")
                         .log(LoggingLevel.DEBUG, "Entering route: ${routeId}")
@@ -88,6 +89,7 @@ public class JtaRouteTest extends CamelTestSupport {
                         .end();
 
                 from("direct:route3").routeId("r.route3")
+                        .process(e -> Assertions.assertTrue(e.isTransacted()))
                         .log(LoggingLevel.DEBUG, "Entering route: ${routeId}");
 
                 from("direct:recipient_test")
@@ -124,7 +126,10 @@ public class JtaRouteTest extends CamelTestSupport {
 
     @Test
     void testTransactedSplit() throws Exception {
+        splitted.setExpectedMessageCount(2);
         splitted.expectedBodiesReceived("requires", "new");
+        splitted.whenAnyExchangeReceived(e -> Assertions.assertTrue(e.isTransacted()));
+
         split.sendBody("requires_new");
         splitted.assertIsSatisfied();
     }
@@ -133,25 +138,33 @@ public class JtaRouteTest extends CamelTestSupport {
     public void testTransactedMultiCast() throws Exception {
         test.setExpectedMessageCount(1);
         test.expectedBodiesReceived("multicast");
+        splitted.whenAnyExchangeReceived(e -> Assertions.assertTrue(e.isTransacted()));
+
         multicast.sendBody("multicast");
         test.assertIsSatisfied();
     }
 
     @Test
     public void testTransactedRecipient() throws Exception {
-        a.setExpectedMessageCount(1);
-        b.setExpectedMessageCount(1);
-        c.setExpectedMessageCount(1);
+        Arrays.asList(a, b, c).forEach(m -> {
+            m.setExpectedMessageCount(1);
+            m.expectedBodiesReceived("recipient");
+            m.whenAnyExchangeReceived(e -> Assertions.assertTrue(e.isTransacted()));
+        });
+
         recipient.sendBody("recipient");
-        a.assertIsSatisfied();
-        b.assertIsSatisfied();
-        c.assertIsSatisfied();
+
+        for (MockEndpoint m : Arrays.asList(a, b, c)) {
+            m.assertIsSatisfied();
+        }
     }
 
     @Test
     public void testTransactedEnrich() throws Exception {
         result.setExpectedMessageCount(1);
         result.expectedBodiesReceived("Message Enrich");
+        result.whenAnyExchangeReceived(e -> Assertions.assertTrue(e.isTransacted()));
+
         enrich.sendBody("Message");
         result.assertIsSatisfied();
     }
