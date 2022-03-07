4d498eefcfba65c258c1942fb0a211dd3fe12de3
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-03-07 12:13:33 +0100

    CAMEL-15520: fixed deprecations in camel-rabbitmq

--- components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/reply/ReplyManagerSupport.java
+++ components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/reply/ReplyManagerSupport.java
@@ -180,13 +180,8 @@ public abstract class ReplyManagerSupport extends ServiceSupport implements Repl
                     // restore correlation id in case the remote server messed
                     // with it
                     if (holder.getOriginalCorrelationId() != null) {
-                        if (exchange.hasOut()) {
-                            exchange.getOut().setHeader(RabbitMQConstants.CORRELATIONID,
-                                    holder.getOriginalCorrelationId());
-                        } else {
-                            exchange.getIn().setHeader(RabbitMQConstants.CORRELATIONID,
-                                    holder.getOriginalCorrelationId());
-                        }
+                        exchange.getMessage().setHeader(RabbitMQConstants.CORRELATIONID,
+                                holder.getOriginalCorrelationId());
                     }
                 }
             } finally {
