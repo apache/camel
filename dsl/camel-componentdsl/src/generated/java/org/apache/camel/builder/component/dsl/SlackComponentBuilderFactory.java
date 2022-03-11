a5e8a0758bfeed833d90a0dbc99b58538529fcaa
oscerd <oscerd@users.noreply.github.com>
2022-03-11 18:44:14 +0000

    Regen for commit ac4294e01ecddb326fc90feccccfe338db540f02
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/SlackComponentBuilderFactory.java
+++ dsl/camel-componentdsl/src/generated/java/org/apache/camel/builder/component/dsl/SlackComponentBuilderFactory.java
@@ -115,7 +115,10 @@ public interface SlackComponentBuilderFactory {
             return this;
         }
         /**
-         * The token to use.
+         * The token to access Slack. This app needs to have channels:history,
+         * groups:history, im:history, mpim:history, channels:read, groups:read,
+         * im:read and mpim:read permissions. The User OAuth Token is the kind
+         * of token needed.
          * 
          * The option is a: &lt;code&gt;java.lang.String&lt;/code&gt; type.
          * 
