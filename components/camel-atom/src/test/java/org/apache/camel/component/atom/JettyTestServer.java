374f9e8e7d087d20f1ed4ded29105fd6d605cc3e
Gregor Zurowski <gregor@zurowski.org>
2022-03-13 21:07:04 -0400

    [Minor] Fix imports

--- components/camel-atom/src/test/java/org/apache/camel/component/atom/JettyTestServer.java
+++ components/camel-atom/src/test/java/org/apache/camel/component/atom/JettyTestServer.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
