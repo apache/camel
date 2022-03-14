2b7d57e4f6d73ffe4d0e6658d92ce0c6c91b53f1
Gregor Zurowski <gregor@zurowski.org>
2022-03-13 21:14:30 -0400

    [Minor] Apply formatter

--- components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/UnicodeHelperTest.java
+++ components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/UnicodeHelperTest.java
@@ -16,8 +16,6 @@
  */
 package org.apache.camel.dataformat.bindy;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
