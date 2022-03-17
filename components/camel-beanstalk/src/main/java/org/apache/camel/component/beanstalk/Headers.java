043239134e576ffd0e0c796118acfd1bdabac6f7
Nicolas Filotto <nfilotto@talend.com>
2022-03-17 11:21:30 +0100

    CAMEL-17792: Add doc about the message headers of camel-beanstalk

--- components/camel-beanstalk/src/main/java/org/apache/camel/component/beanstalk/Headers.java
+++ components/camel-beanstalk/src/main/java/org/apache/camel/component/beanstalk/Headers.java
@@ -16,29 +16,46 @@
  */
 package org.apache.camel.component.beanstalk;
 
+import org.apache.camel.spi.Metadata;
+
 public final class Headers {
 
     public static final String PREFIX = "beanstalk.";
 
     // in
+    @Metadata(description = "The priority value set", javaType = "long")
     public static final String PRIORITY = PREFIX + "priority";
+    @Metadata(description = "The Job delay in seconds", javaType = "Integer")
     public static final String DELAY = PREFIX + "delay";
+    @Metadata(description = "The Job time to run in seconds. (when 0, the beanstalkd daemon raises it to 1 automatically, see Beanstalk protocol)",
+              javaType = "Integer")
     public static final String TIME_TO_RUN = PREFIX + "timeToRun";
 
     // in/out
+    @Metadata(description = "Job ID", javaType = "long")
     public static final String JOB_ID = PREFIX + "jobId";
 
     // out
+    @Metadata(description = "The flag indicating if the operation was a success or not", javaType = "Boolean")
     public static final String RESULT = PREFIX + "result";
 
     // other info
+    @Metadata(description = "The name of the tube that contains this job", javaType = "String")
     public static final String TUBE = PREFIX + "tube";
+    @Metadata(description = "“ready” or “delayed” or “reserved” or “buried” (must be “reserved”)", javaType = "String")
     public static final String STATE = PREFIX + "state";
+    @Metadata(description = "The time in seconds since the put command that created this job", javaType = "Integer")
     public static final String AGE = PREFIX + "age";
+    @Metadata(description = "The number of seconds left until the server puts this job into the ready queue",
+              javaType = "Integer")
     public static final String TIME_LEFT = PREFIX + "time-left";
+    @Metadata(description = "The number of times this job has timed out during a reservation", javaType = "Integer")
     public static final String TIMEOUTS = PREFIX + "timeouts";
+    @Metadata(description = "The number of times a client has released this job from a reservation", javaType = "Integer")
     public static final String RELEASES = PREFIX + "releases";
+    @Metadata(description = "The number of times this job has been buried", javaType = "Integer")
     public static final String BURIES = PREFIX + "buries";
+    @Metadata(description = "The number of times this job has been kicked", javaType = "Integer")
     public static final String KICKS = PREFIX + "kicks";
 
     private Headers() {
