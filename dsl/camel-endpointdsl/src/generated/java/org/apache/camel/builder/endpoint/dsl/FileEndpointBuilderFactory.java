68403d2635b97b6a71ca5d696b4966316cd296ed
essobedo <essobedo@users.noreply.github.com>
2022-03-21 17:18:43 +0000

    Regen for commit a1b105e76d4d43432745a1a2720eb17552c9dbda
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/FileEndpointBuilderFactory.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/FileEndpointBuilderFactory.java
@@ -3199,7 +3199,7 @@ public interface FileEndpointBuilderFactory {
         }
         /**
          * Will keep the last modified timestamp from the source file (if any).
-         * Will use the Exchange.FILE_LAST_MODIFIED header to located the
+         * Will use the FileConstants.FILE_LAST_MODIFIED header to located the
          * timestamp. This header can contain either a java.util.Date or long
          * with the timestamp. If the timestamp exists and the option is enabled
          * it will set this timestamp on the written file. Note: This option
@@ -3221,7 +3221,7 @@ public interface FileEndpointBuilderFactory {
         }
         /**
          * Will keep the last modified timestamp from the source file (if any).
-         * Will use the Exchange.FILE_LAST_MODIFIED header to located the
+         * Will use the FileConstants.FILE_LAST_MODIFIED header to located the
          * timestamp. This header can contain either a java.util.Date or long
          * with the timestamp. If the timestamp exists and the option is enabled
          * it will set this timestamp on the written file. Note: This option
