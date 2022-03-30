c5280724591e4816dca111ab2ca973626137712e
Andrea Cosentino <ancosen@gmail.com>
2022-03-30 11:54:51 +0200

    CAMEL-17687 - Create a Camel Azure Key Vault component - Kit

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/MojoHelper.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/MojoHelper.java
@@ -49,7 +49,7 @@ public final class MojoHelper {
             case "camel-azure":
                 return Arrays.asList(dir.resolve("camel-azure-eventhubs"), dir.resolve("camel-azure-storage-blob"),
                         dir.resolve("camel-azure-storage-datalake"), dir.resolve("camel-azure-cosmosdb"),
-                        dir.resolve("camel-azure-storage-queue"), dir.resolve("camel-azure-servicebus"));
+                        dir.resolve("camel-azure-storage-queue"), dir.resolve("camel-azure-servicebus"), dir.resolve("camel-azure-key-vault"));
             case "camel-google":
                 return Arrays.asList(dir.resolve("camel-google-bigquery"), dir.resolve("camel-google-calendar"),
                         dir.resolve("camel-google-drive"), dir.resolve("camel-google-mail"), dir.resolve("camel-google-pubsub"),
