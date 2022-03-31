66e7f994104b8dca24912bc55df575e6f4202a20
Nicolas Filotto <nfilotto@talend.com>
2022-03-31 12:03:20 +0200

    CAMEL-17792: Add doc about the message headers of camel-micrometer

--- components/camel-micrometer/src/main/java/org/apache/camel/component/micrometer/MicrometerEndpoint.java
+++ components/camel-micrometer/src/main/java/org/apache/camel/component/micrometer/MicrometerEndpoint.java
@@ -35,7 +35,8 @@ import org.apache.camel.support.DefaultEndpoint;
  * Collect various metrics directly from Camel routes using the Micrometer library.
  */
 @UriEndpoint(firstVersion = "2.22.0", scheme = "micrometer", title = "Micrometer",
-             syntax = "micrometer:metricsType:metricsName", producerOnly = true, category = { Category.MONITORING })
+             syntax = "micrometer:metricsType:metricsName", producerOnly = true, category = { Category.MONITORING },
+             headersClass = MicrometerConstants.class)
 public class MicrometerEndpoint extends DefaultEndpoint {
 
     protected MeterRegistry registry;
