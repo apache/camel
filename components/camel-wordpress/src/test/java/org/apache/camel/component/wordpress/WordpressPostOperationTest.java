32143e8a3213712e1a9474533c57a6a1902b6591
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-08 16:06:47 +0200

    CAMEL-17763: cleanup unused exceptions in camel-wordpress

--- components/camel-wordpress/src/test/java/org/apache/camel/component/wordpress/WordpressPostOperationTest.java
+++ components/camel-wordpress/src/test/java/org/apache/camel/component/wordpress/WordpressPostOperationTest.java
@@ -97,7 +97,7 @@ public class WordpressPostOperationTest extends WordpressComponentTestSupport {
     }
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             public void configure() {
                 final WordpressConfiguration configuration = new WordpressConfiguration();
