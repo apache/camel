d2ebb0015ddec6a93909ac38a8b9ff11e517dea1
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-08 15:08:24 +0200

    CAMEL-17763: cleanup unused exceptions in camel-datasonnet

--- components/camel-datasonnet/src/test/java/org/apache/camel/language/datasonnet/CamelDatasonnetJavaDslTest.java
+++ components/camel-datasonnet/src/test/java/org/apache/camel/language/datasonnet/CamelDatasonnetJavaDslTest.java
@@ -70,10 +70,10 @@ public class CamelDatasonnetJavaDslTest extends CamelTestSupport {
     }
 
     @Override
-    protected RoutesBuilder createRouteBuilder() throws Exception {
+    protected RoutesBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:basicTransform")
                         .routeId("basicTransform")
                         .setProperty("test", constant("HelloWorld"))
