a57dd40a12332f138854dfdd99526dae30003ffd
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-08 15:37:49 +0200

    CAMEL-17763: cleanup unused exceptions in camel-freemaker

--- components/camel-freemarker/src/test/java/org/apache/camel/component/freemarker/FreemarkerLetterWithoutHeaderTest.java
+++ components/camel-freemarker/src/test/java/org/apache/camel/component/freemarker/FreemarkerLetterWithoutHeaderTest.java
@@ -21,9 +21,9 @@ import org.apache.camel.builder.RouteBuilder;
 public class FreemarkerLetterWithoutHeaderTest extends FreemarkerLetterTest {
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:a").to("freemarker:org/apache/camel/component/freemarker/header.ftl")
                         .to("freemarker:org/apache/camel/component/freemarker/letterWithoutHeader.ftl").to("mock:result");
             }
