b5f4ddafd680ea81076856d7c3287286a207ee51
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-12 11:04:37 +0200

    CAMEL-17763: cleanup unused exceptions in camel-ical

--- components/camel-ical/src/test/java/org/apache/camel/component/ical/ICalDataFormatTest.java
+++ components/camel-ical/src/test/java/org/apache/camel/component/ical/ICalDataFormatTest.java
@@ -98,10 +98,10 @@ public class ICalDataFormatTest extends CamelTestSupport {
     }
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:unmarshal")
                         .unmarshal("ical")
                         .to("mock:result");
