64107500ca7a921b64dcd348241d291101fb4bf0
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-12 18:21:05 +0200

    CAMEL-17763: cleaned up unused exceptions in camel-reactive-executor-vertx

--- components/camel-reactive-executor-vertx/src/test/java/org/apache/camel/reactive/SimpleMockLookupVertxTest.java
+++ components/camel-reactive-executor-vertx/src/test/java/org/apache/camel/reactive/SimpleMockLookupVertxTest.java
@@ -56,10 +56,10 @@ public class SimpleMockLookupVertxTest extends CamelTestSupport {
     }
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:start").to("log:foo").to("log:bar").to("mock:result");
             }
         };
