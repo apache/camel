38c38119512141e4e203401ea6154e2372d6558e
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-13 11:08:44 +0200

    CAMEL-17763: cleaned up unused exceptions in camel-threadpoolfactory-vertx

--- components/camel-threadpoolfactory-vertx/src/test/java/org/apache/camel/reactive/SplitParallelLookupVertxTest.java
+++ components/camel-threadpoolfactory-vertx/src/test/java/org/apache/camel/reactive/SplitParallelLookupVertxTest.java
@@ -48,10 +48,10 @@ public class SplitParallelLookupVertxTest extends CamelTestSupport {
     }
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:start")
                         .to("log:foo")
                         .split(body()).parallelProcessing()
