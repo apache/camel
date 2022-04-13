368b5f5dee0511704335cafe64a7460c4bb7ba7d
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-04-13 11:01:29 +0200

    CAMEL-17763: cleaned up unused exceptions in camel-slack

--- components/camel-slack/src/test/java/org/apache/camel/component/slack/SlackConsumerTest.java
+++ components/camel-slack/src/test/java/org/apache/camel/component/slack/SlackConsumerTest.java
@@ -83,10 +83,10 @@ public class SlackConsumerTest extends CamelTestSupport {
     }
 
     @Override
-    protected RouteBuilder createRouteBuilder() throws Exception {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from(String.format("slack://general?token=RAW(%s)&maxResults=1", token))
                         .to("mock:result");
             }
