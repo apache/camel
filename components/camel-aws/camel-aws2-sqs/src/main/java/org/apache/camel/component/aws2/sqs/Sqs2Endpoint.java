b5969c10c7eb59aec971c43853378629984b0766
Andrea Cosentino <ancosen@gmail.com>
2022-04-20 18:12:08 +0200

    CAMEL-16287 - camel-aws2-sqs should use pagination for deciding which aws sqs queues it should create

--- components/camel-aws/camel-aws2-sqs/src/main/java/org/apache/camel/component/aws2/sqs/Sqs2Endpoint.java
+++ components/camel-aws/camel-aws2-sqs/src/main/java/org/apache/camel/component/aws2/sqs/Sqs2Endpoint.java
@@ -169,7 +169,8 @@ public class Sqs2Endpoint extends ScheduledPollEndpoint implements HeaderFilterS
                 // check whether the queue already exists
                 boolean done = false;
                 while (!done) {
-                    ListQueuesResponse listQueuesResult = client.listQueues(ListQueuesRequest.builder().maxResults(1000).build());
+                    ListQueuesResponse listQueuesResult
+                            = client.listQueues(ListQueuesRequest.builder().maxResults(1000).build());
 
                     for (String url : listQueuesResult.queueUrls()) {
                         if (url.endsWith("/" + configuration.getQueueName())) {
