7e97744839212e96a94dfd45dde618fdf173bc68
brendan <b.marc.j@gmail.com>
2022-04-27 21:32:43 +0100

    Format

--- components/camel-sql/src/main/java/org/apache/camel/processor/idempotent/jdbc/JdbcMessageIdRepository.java
+++ components/camel-sql/src/main/java/org/apache/camel/processor/idempotent/jdbc/JdbcMessageIdRepository.java
@@ -38,7 +38,7 @@ public class JdbcMessageIdRepository extends AbstractJdbcMessageIdRepository {
 
     private String tableExistsString = "SELECT 1 FROM CAMEL_MESSAGEPROCESSED WHERE 1 = 0";
     private String createString = "CREATE TABLE CAMEL_MESSAGEPROCESSED (processorName VARCHAR(255), messageId VARCHAR(100), "
-            + "createdAt TIMESTAMP, PRIMARY KEY (processorName, messageId))";
+                                  + "createdAt TIMESTAMP, PRIMARY KEY (processorName, messageId))";
     private String queryString = "SELECT COUNT(*) FROM CAMEL_MESSAGEPROCESSED WHERE processorName = ? AND messageId = ?";
     private String insertString = "INSERT INTO CAMEL_MESSAGEPROCESSED (processorName, messageId, createdAt) VALUES (?, ?, ?)";
     private String deleteString = "DELETE FROM CAMEL_MESSAGEPROCESSED WHERE processorName = ? AND messageId = ?";
