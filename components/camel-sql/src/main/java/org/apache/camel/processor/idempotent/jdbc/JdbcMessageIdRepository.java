b7c2103c50937380bcd95b2afa1cebaf3e35774a
bmarcj <b.marc.j@gmail.com>
2022-04-28 08:46:49 +0100

    Add default constraint creation; improve docs (#7509)
    * Add default constraint creation; improve docs
    
    * Format
    
    * Fix typo

--- components/camel-sql/src/main/java/org/apache/camel/processor/idempotent/jdbc/JdbcMessageIdRepository.java
+++ components/camel-sql/src/main/java/org/apache/camel/processor/idempotent/jdbc/JdbcMessageIdRepository.java
@@ -37,8 +37,8 @@ public class JdbcMessageIdRepository extends AbstractJdbcMessageIdRepository {
     private String tableName;
 
     private String tableExistsString = "SELECT 1 FROM CAMEL_MESSAGEPROCESSED WHERE 1 = 0";
-    private String createString
-            = "CREATE TABLE CAMEL_MESSAGEPROCESSED (processorName VARCHAR(255), messageId VARCHAR(100), createdAt TIMESTAMP)";
+    private String createString = "CREATE TABLE CAMEL_MESSAGEPROCESSED (processorName VARCHAR(255), messageId VARCHAR(100), "
+                                  + "createdAt TIMESTAMP, PRIMARY KEY (processorName, messageId))";
     private String queryString = "SELECT COUNT(*) FROM CAMEL_MESSAGEPROCESSED WHERE processorName = ? AND messageId = ?";
     private String insertString = "INSERT INTO CAMEL_MESSAGEPROCESSED (processorName, messageId, createdAt) VALUES (?, ?, ?)";
     private String deleteString = "DELETE FROM CAMEL_MESSAGEPROCESSED WHERE processorName = ? AND messageId = ?";
