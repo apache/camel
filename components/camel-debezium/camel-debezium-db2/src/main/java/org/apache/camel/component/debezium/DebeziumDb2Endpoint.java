ea6804190cfb56a35ea0d365db9707bdd53c4add
Claus Ibsen <claus.ibsen@gmail.com>
2022-05-19 13:33:27 +0200

    Fixed title

--- components/camel-debezium/camel-debezium-db2/src/main/java/org/apache/camel/component/debezium/DebeziumDb2Endpoint.java
+++ components/camel-debezium/camel-debezium-db2/src/main/java/org/apache/camel/component/debezium/DebeziumDb2Endpoint.java
@@ -23,7 +23,7 @@ import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 
 /**
- * Capture changes from a Oracle database.
+ * Capture changes from a DB2 database.
  */
 @UriEndpoint(firstVersion = "3.17.0", scheme = "debezium-db2", title = "Debezium DB2 Connector",
              syntax = "debezium-db2:name", category = { Category.DATABASE, Category.SQL }, consumerOnly = true,
