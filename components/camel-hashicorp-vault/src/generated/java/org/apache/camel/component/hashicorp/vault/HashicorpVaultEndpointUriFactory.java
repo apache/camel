d504885c8973d28adcdc828b275839ed8988f3e2
Andrea Cosentino <ancosen@gmail.com>
2022-06-08 15:13:36 +0200

    CAMEL-17689 - Create a Camel Hashicorp Vault Component

--- components/camel-hashicorp-vault/src/generated/java/org/apache/camel/component/hashicorp/vault/HashicorpVaultEndpointUriFactory.java
+++ components/camel-hashicorp-vault/src/generated/java/org/apache/camel/component/hashicorp/vault/HashicorpVaultEndpointUriFactory.java
@@ -15,7 +15,7 @@ import org.apache.camel.spi.EndpointUriFactory;
  */
 public class HashicorpVaultEndpointUriFactory extends org.apache.camel.support.component.EndpointUriFactorySupport implements EndpointUriFactory {
 
-    private static final String BASE = ":vaultName";
+    private static final String BASE = ":secretsEngine";
 
     private static final Set<String> PROPERTY_NAMES;
     private static final Set<String> SECRET_PROPERTY_NAMES;
