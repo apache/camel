202333c0947916cde304fc48c445f127dd84e229
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-06-09 12:17:06 -0300

    CAMEL-18181: prevent non-printable characters from breaking the doc generation

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/UpdateReadmeMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/UpdateReadmeMojo.java
@@ -560,7 +560,7 @@ public class UpdateReadmeMojo extends AbstractGeneratorMojo {
             boolean copy = false;
             if (updated || RELOCATE_MANUAL_ATTRIBUTES) {
                 outer: for (String line : lines) {
-                    if (!copy && line.isEmpty()) {
+                    if (!copy && line.trim().isEmpty()) {
                         copy = true;
                     } else if (copy) {
 
