76c99d41e2b6218f4f088908d2c6bb72b2a9d1d7
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-06-26 11:21:42 +0200

    (chores) camel-spring-xml: fixed incorrectly disabled tests

--- components/camel-spring-xml/src/test/java/org/apache/camel/spring/processor/SpringLoopDoWhileTest.java
+++ components/camel-spring-xml/src/test/java/org/apache/camel/spring/processor/SpringLoopDoWhileTest.java
@@ -18,15 +18,15 @@ package org.apache.camel.spring.processor;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.processor.LoopDoWhileTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
 import static org.apache.camel.spring.processor.SpringTestHelper.createSpringCamelContext;
 
 public class SpringLoopDoWhileTest extends LoopDoWhileTest {
 
     @Override
-    @Test
-    public void testLoopDoWhileFunctional() throws Exception {
+    @Disabled("This test is not applicable in XML")
+    public void testLoopDoWhileFunctional() {
         // not applicable in XML
     }
 
