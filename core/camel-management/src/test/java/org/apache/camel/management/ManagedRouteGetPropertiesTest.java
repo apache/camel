c8ba3c22576c0d4bf8f9d3807302feb01db27b4c
orpiske <orpiske@users.noreply.github.com>
2022-08-05 18:26:34 +0000

    Regen for commit 0e13aa0d3542b548b4cc6a5a4407e61f69350cbb
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-management/src/test/java/org/apache/camel/management/ManagedRouteGetPropertiesTest.java
+++ core/camel-management/src/test/java/org/apache/camel/management/ManagedRouteGetPropertiesTest.java
@@ -30,7 +30,6 @@ import org.junit.jupiter.api.condition.OS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 @DisabledOnOs(OS.AIX)
 public class ManagedRouteGetPropertiesTest extends ManagementTestSupport {
