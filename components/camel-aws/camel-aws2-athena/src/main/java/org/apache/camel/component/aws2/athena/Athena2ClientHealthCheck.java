04153af37035fb986816bb514b6e9d5c6ff9995b
Rhuan Rocha <rhuan080@gmail.com>
2022-08-21 18:32:04 -0300

    CAMEL-18131 - Adding health check to test conectivity
    Signed-off-by: Rhuan Rocha <rhuan080@gmail.com>
    
    CAMEL-18131 - Adding health check to test conectivity
    
    Signed-off-by: Rhuan Rocha <rhuan080@gmail.com>
    
    CAMEL-18131 - Adding health check to test conectivity
    
    Signed-off-by: Rhuan Rocha <rhuan080@gmail.com>
    
    CAMEL-18131 - Adding health check to test conectivity
    
    Signed-off-by: Rhuan Rocha <rhuan080@gmail.com>

---
+++ components/camel-aws/camel-aws2-athena/src/main/java/org/apache/camel/component/aws2/athena/Athena2ClientHealthCheck.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.component.aws2.athena;
+
+import java.util.Map;
+
+import org.apache.camel.health.HealthCheckResultBuilder;
+import org.apache.camel.impl.health.AbstractHealthCheck;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.athena.AthenaClient;
+import software.amazon.awssdk.services.athena.AthenaClientBuilder;
+
+public class Athena2ClientHealthCheck extends AbstractHealthCheck {
+
+    private final Athena2Endpoint athena2Endpoint;
+    private final String clientId;
+
+    public Athena2ClientHealthCheck(Athena2Endpoint athena2Endpoint, String clientId) {
+        super("camel", "aws2-athena-client-" + clientId);
+        this.athena2Endpoint = athena2Endpoint;
+        this.clientId = clientId;
+    }
+
+    @Override
+    public boolean isLiveness() {
+        // this health check is only readiness
+        return false;
+    }
+
+    @Override
+    protected void doCall(HealthCheckResultBuilder builder, Map<String, Object> options) {
+
+        try {
+            if (!AthenaClient.serviceMetadata().regions().contains(Region.of(athena2Endpoint.getConfiguration().getRegion()))) {
+                builder.message("The service is not supported in this region");
+                builder.down();
+                return;
+            }
+            AwsBasicCredentials cred = AwsBasicCredentials.create(athena2Endpoint.getConfiguration().getAccessKey(),
+                    athena2Endpoint.getConfiguration().getSecretKey());
+            AthenaClientBuilder clientBuilder = AthenaClient.builder();
+            AthenaClient client = clientBuilder.credentialsProvider(StaticCredentialsProvider.create(cred))
+                    .region(Region.of(athena2Endpoint.getConfiguration().getRegion())).build();
+            client.listQueryExecutions();
+        } catch (AwsServiceException e) {
+            builder.message(e.getMessage());
+            builder.error(e);
+            builder.down();
+            return;
+
+        } catch (Exception e) {
+            builder.error(e);
+            builder.down();
+            return;
+        }
+        builder.up();
+    }
+}
