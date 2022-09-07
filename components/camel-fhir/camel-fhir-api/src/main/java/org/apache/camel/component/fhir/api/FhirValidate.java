bf4fac6406095dfeedfb246fb3c40bb38093ee40
davsclaus <davsclaus@users.noreply.github.com>
2022-09-07 08:27:12 +0000

    Regen for commit 75ed64573b438224d6a58a54ac550283ea9fa9e0
    Signed-off-by: GitHub <noreply@github.com>

--- components/camel-fhir/camel-fhir-api/src/main/java/org/apache/camel/component/fhir/api/FhirValidate.java
+++ components/camel-fhir/camel-fhir-api/src/main/java/org/apache/camel/component/fhir/api/FhirValidate.java
@@ -37,8 +37,8 @@ public class FhirValidate {
     /**
      * Validates the resource
      *
-     * @param  resource        the {@link IBaseResource} to validate
-     * @param  extraParameters see {@link ExtraParameters} for a full list of parameters that can be passed, may be NULL
+     * @param resource        the {@link IBaseResource} to validate
+     * @param extraParameters see {@link ExtraParameters} for a full list of parameters that can be passed, may be NULL
      */
     public MethodOutcome resource(IBaseResource resource, Map<ExtraParameters, Object> extraParameters) {
         IValidateUntyped validateUntyped = client.validate().resource(resource);
@@ -49,8 +49,8 @@ public class FhirValidate {
     /**
      * Validates the resource
      *
-     * @param  resourceAsString     raw resource to validate
-     * @param  extraParameters see {@link ExtraParameters} for a full list of parameters that can be passed, may be NULL
+     * @param resourceAsString raw resource to validate
+     * @param extraParameters  see {@link ExtraParameters} for a full list of parameters that can be passed, may be NULL
      */
     public MethodOutcome resource(String resourceAsString, Map<ExtraParameters, Object> extraParameters) {
         IValidateUntyped validateUntyped = client.validate().resource(resourceAsString);
