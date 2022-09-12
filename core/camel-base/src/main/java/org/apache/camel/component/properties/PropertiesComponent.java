b95d2cc1cc98949067b3c494e9771b97c9f9b863
davsclaus <davsclaus@users.noreply.github.com>
2022-09-12 16:30:44 +0000

    Regen for commit ee5416f94ebece00814f6a71570224b5c6c9f674
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-base/src/main/java/org/apache/camel/component/properties/PropertiesComponent.java
+++ core/camel-base/src/main/java/org/apache/camel/component/properties/PropertiesComponent.java
@@ -17,7 +17,6 @@
 package org.apache.camel.component.properties;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
