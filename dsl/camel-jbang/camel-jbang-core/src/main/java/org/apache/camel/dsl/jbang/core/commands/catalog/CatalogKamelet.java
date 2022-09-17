f1bb1405bb7f5d8d279b289364a8bdd8f95852ec
Claus Ibsen <claus.ibsen@gmail.com>
2022-09-17 09:58:42 +0200

    camel-jbang - Polished

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogKamelet.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogKamelet.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.dsl.jbang.core.commands.kamelet;
+package org.apache.camel.dsl.jbang.core.commands.catalog;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
