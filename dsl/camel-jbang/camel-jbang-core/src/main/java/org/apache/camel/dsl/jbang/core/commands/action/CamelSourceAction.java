29fae407638c553b7f20b1b72f9e84f53217dc9f
Claus Ibsen <claus.ibsen@gmail.com>
2022-09-25 19:26:36 +0200

    Fixed CS

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/action/CamelSourceAction.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/action/CamelSourceAction.java
@@ -212,10 +212,12 @@ public class CamelSourceAction extends ActionBaseCommand {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o)
+            if (this == o) {
                 return true;
-            if (o == null || getClass() != o.getClass())
+            }
+            if (o == null || getClass() != o.getClass()) {
                 return false;
+            }
 
             Row row = (Row) o;
 
