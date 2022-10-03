e81f919661e8db4c8e49827b4e603e81226ae309
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-10-03 14:55:34 +0200

    CAMEL-18575: use JUnit's 5 TempDir annotation in camel-jaxb

--- components/camel-jaxb/src/test/java/org/apache/camel/example/ExplicitFileEncodingTest.java
+++ components/camel-jaxb/src/test/java/org/apache/camel/example/ExplicitFileEncodingTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.camel.example;
 
+import java.nio.file.Path;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
@@ -24,10 +26,13 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExplicitFileEncodingTest extends CamelTestSupport {
+    @TempDir
+    Path testDirectory;
 
     @Test
     public void testISOFileEncoding() throws Exception {
@@ -39,14 +44,14 @@ public class ExplicitFileEncodingTest extends CamelTestSupport {
         order.setPrice(2.22);
 
         MockEndpoint result = getMockEndpoint("mock:file");
-        result.expectedFileExists(testFile("output.txt"));
+        result.expectedFileExists(testDirectory.resolve("output.txt"));
 
         template.sendBody("direct:start", order);
         assertMockEndpointsSatisfied();
 
         JAXBContext jaxbContext = JAXBContext.newInstance("org.apache.camel.example");
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        PurchaseOrder obj = (PurchaseOrder) unmarshaller.unmarshal(testFile("output.txt").toFile());
+        PurchaseOrder obj = (PurchaseOrder) unmarshaller.unmarshal(testDirectory.resolve("output.txt").toFile());
         assertEquals(obj.getName(), name);
     }
 
@@ -60,7 +65,7 @@ public class ExplicitFileEncodingTest extends CamelTestSupport {
 
                 from("direct:start")
                         .marshal(jaxb)
-                        .to(fileUri("?fileName=output.txt&charset=iso-8859-1"));
+                        .to(fileUri(testDirectory, "?fileName=output.txt&charset=iso-8859-1"));
             }
         };
     }
