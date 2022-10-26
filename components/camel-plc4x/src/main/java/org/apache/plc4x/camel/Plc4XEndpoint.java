345e7b185f406f262f981c6947f4a93201f2cff8
Christofer Dutz <christoferdutz@rivian.com>
2022-10-26 16:33:30 +0200

    Applied some of the suggestions by the sourceformatter.

--- components/camel-plc4x/src/main/java/org/apache/plc4x/camel/Plc4XEndpoint.java
+++ components/camel-plc4x/src/main/java/org/apache/plc4x/camel/Plc4XEndpoint.java
@@ -1,37 +1,36 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.plc4x.camel;
 
-import org.apache.camel.*;
+import org.apache.camel.Component;
+import org.apache.camel.Consumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
 import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
-import org.apache.commons.math3.util.Pair;
 import org.apache.plc4x.java.PlcDriverManager;
 import org.apache.plc4x.java.api.PlcConnection;
 import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
 import org.apache.plc4x.java.api.exceptions.PlcRuntimeException;
 import org.apache.plc4x.java.utils.connectionpool.PooledPlcDriverManager;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
--- components/camel-plc4x/src/main/java/org/apache/plc4x/camel/TagData.java
+++ components/camel-plc4x/src/main/java/org/apache/plc4x/camel/TagData.java
@@ -1,20 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *   https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.plc4x.camel;
 
@@ -23,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class TagData {
@@ -75,17 +74,13 @@ public class TagData {
             }
             if(canParse.get(Short.TYPE).test(val)){
                 value = Short.parseShort(val);
-            }
-            else if(canParse.get(Integer.TYPE).test(val)){
+            } else if(canParse.get(Integer.TYPE).test(val)){
                 value = Integer.parseInt(val);
-            }
-            else if(canParse.get(Long.TYPE).test(val)){
+            } else if(canParse.get(Long.TYPE).test(val)){
                 value = Long.parseLong(val);
-            }
-            else if(canParse.get(Double.TYPE).test(val)){
+            } else if(canParse.get(Double.TYPE).test(val)){
                 value = Double.parseDouble(val);
-            }
-            else if(canParse.get(Float.TYPE).test(val)){
+            } else if(canParse.get(Float.TYPE).test(val)){
                 value = Float.parseFloat(val);
             }
 
@@ -94,12 +89,54 @@ public class TagData {
 
     private Map<Class<?>, Predicate<String>> canParse = new HashMap<>();
     {
-        canParse.put(Integer.TYPE, s -> {try {Integer.parseInt(s); return true;} catch(Exception e) {return false;}});
-        canParse.put(Long.TYPE, s -> {try {Long.parseLong(s); return true;} catch(Exception e) {return false;}});
-        canParse.put(Short.TYPE, s -> {try {Short.parseShort(s); return true;} catch(Exception e) {return false;}});
-        canParse.put(Boolean.TYPE, s -> {try {Boolean.parseBoolean(s); return true;} catch(Exception e) {return false;}});
-        canParse.put(Double.TYPE, s -> {try {Double.parseDouble(s); return true;} catch(Exception e) {return false;}});
-        canParse.put(Float.TYPE, s -> {try {Float.parseFloat(s); return true;} catch(Exception e) {return false;}});
+        canParse.put(Integer.TYPE, s -> {
+            try {
+                Integer.parseInt(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
+        });
+        canParse.put(Long.TYPE, s -> {
+            try {
+                Long.parseLong(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
+        });
+        canParse.put(Short.TYPE, s -> {
+            try {
+                Short.parseShort(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
+        });
+        canParse.put(Boolean.TYPE, s -> {
+            try {
+                Boolean.parseBoolean(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
+        });
+        canParse.put(Double.TYPE, s -> {
+            try {
+                Double.parseDouble(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
+        });
+        canParse.put(Float.TYPE, s -> {
+            try {
+                Float.parseFloat(s);
+                return true;
+            } catch(Exception e) {
+                return false;
+            }
+        });
     };
 
     @Override
@@ -114,8 +151,11 @@ public class TagData {
             && ((TagData)tag).getQuery().equals(query) :
              ((TagData)tag).getTagName().equals(tagName)
             && ((TagData)tag).getQuery().equals(query);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(tagName, query, value, canParse);
     }
 
     public static Map<String,String> toMap(List<TagData> tags){
