be53c26eea1100004773896b2510a633f837d809
Claus Ibsen <claus.ibsen@gmail.com>
2022-11-05 10:53:17 +0100

    camel-controlbus - Fix list of languages in the endpoint option that was out of sync

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
@@ -2671,9 +2671,10 @@ public class StaticEndpointBuilders {
      * Allows you to specify the name of a Language to use for evaluating the
      * message body. If there is any result from the evaluation, then the result
      * is put in the message body.
-     * There are 19 enums and the value can be one of: bean, constant, el,
-     * exchangeProperty, file, groovy, header, jsonpath, mvel, ognl, ref,
-     * simple, spel, sql, terser, tokenize, xpath, xquery, xtokenize
+     * There are 22 enums and the value can be one of: bean, constant, csimple,
+     * datasonnet, exchangeProperty, file, groovy, header, hl7terser, joor, jq,
+     * jsonpath, mvel, ognl, python, ref, simple, spel, tokenize, xpath, xquery,
+     * xtokenize
      * 
      * @param path command:language
      * @return the dsl builder
@@ -2700,9 +2701,10 @@ public class StaticEndpointBuilders {
      * Allows you to specify the name of a Language to use for evaluating the
      * message body. If there is any result from the evaluation, then the result
      * is put in the message body.
-     * There are 19 enums and the value can be one of: bean, constant, el,
-     * exchangeProperty, file, groovy, header, jsonpath, mvel, ognl, ref,
-     * simple, spel, sql, terser, tokenize, xpath, xquery, xtokenize
+     * There are 22 enums and the value can be one of: bean, constant, csimple,
+     * datasonnet, exchangeProperty, file, groovy, header, hl7terser, joor, jq,
+     * jsonpath, mvel, ognl, python, ref, simple, spel, tokenize, xpath, xquery,
+     * xtokenize
      * 
      * @param componentName to use a custom component name for the endpoint
      * instead of the default name
