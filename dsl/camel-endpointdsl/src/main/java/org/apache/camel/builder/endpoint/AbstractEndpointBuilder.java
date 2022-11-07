11c3754dfa44fdd34034d7ec0ae16be48732a456
Claus Ibsen <claus.ibsen@gmail.com>
2022-11-07 11:37:32 +0100

    CAMEL-18689: camel-endpointdsl - Reloading routes should re-evaluate property placeholders

--- dsl/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/AbstractEndpointBuilder.java
+++ dsl/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/AbstractEndpointBuilder.java
@@ -42,8 +42,8 @@ public class AbstractEndpointBuilder {
     protected final String scheme;
     protected final String path;
     protected final Map<String, Object> properties = new LinkedHashMap<>();
-    protected final Map<String, Map<String, Object>> multivalues = new HashMap<>();
-    private volatile Endpoint resolvedEndpoint;
+    protected final Map<String, Map<String, Object>> multiValues = new HashMap<>();
+    private volatile Map<String, Object> originalProperties;
     private volatile Language simple;
 
     public AbstractEndpointBuilder(String scheme, String path) {
@@ -52,11 +52,13 @@ public class AbstractEndpointBuilder {
     }
 
     public Endpoint resolve(CamelContext context) throws NoSuchEndpointException {
-        if (resolvedEndpoint != null) {
-            return resolvedEndpoint;
-        }
-
         // properties may contain property placeholder which should be resolved
+        if (originalProperties == null) {
+            originalProperties = new LinkedHashMap<>(properties);
+        } else {
+            // reload from original properties before resolving placeholder in case its updated
+            properties.putAll(originalProperties);
+        }
         resolvePropertyPlaceholders(context, properties);
 
         Map<String, Object> remaining = new LinkedHashMap<>();
@@ -68,7 +70,6 @@ public class AbstractEndpointBuilder {
             throw new NoSuchEndpointException(uri.getUri());
         }
 
-        resolvedEndpoint = endpoint;
         return endpoint;
     }
 
@@ -113,9 +114,9 @@ public class AbstractEndpointBuilder {
 
         // sort parameters so it can be regarded as normalized
         Map<String, Object> params = new TreeMap<>();
-        // compute from properties and multivalues
+        // compute from properties and multi values
         computeProperties(remaining, camelContext, bindToRegistry, params, properties);
-        for (Map<String, Object> map : multivalues.values()) {
+        for (Map<String, Object> map : multiValues.values()) {
             computeProperties(remaining, camelContext, bindToRegistry, params, map);
         }
         if (!remaining.isEmpty()) {
@@ -179,7 +180,7 @@ public class AbstractEndpointBuilder {
     }
 
     public void doSetMultiValueProperty(String name, String key, Object value) {
-        Map<String, Object> map = multivalues.computeIfAbsent(name, k -> new LinkedHashMap<>());
+        Map<String, Object> map = multiValues.computeIfAbsent(name, k -> new LinkedHashMap<>());
         map.put(key, value);
     }
 
