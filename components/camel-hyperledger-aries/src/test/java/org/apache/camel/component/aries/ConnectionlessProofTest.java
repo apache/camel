1a4dcadde96e57bc8d018f4c33c1a92f24abf466
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2022-11-21 13:00:27 +0200

    ran format&sourcecheck

--- components/camel-hyperledger-aries/src/test/java/org/apache/camel/component/aries/ConnectionlessProofTest.java
+++ components/camel-hyperledger-aries/src/test/java/org/apache/camel/component/aries/ConnectionlessProofTest.java
@@ -1,4 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.aries;
+
+import java.io.IOException;
+import java.util.Map;
 
 import org.hyperledger.acy_py.generated.model.DID;
 import org.hyperledger.acy_py.generated.model.DIDEndpoint;
@@ -10,9 +29,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
-import java.io.IOException;
-import java.util.Map;
-
 import static org.hyperledger.aries.api.ledger.IndyLedgerRoles.ENDORSER;
 import static org.hyperledger.aries.api.present_proof.PresentationExchangeRole.VERIFIER;
 import static org.hyperledger.aries.api.present_proof.PresentationExchangeState.REQUEST_SENT;
@@ -23,7 +39,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * docker compose up --detach && docker compose logs -f acapy
  */
 @EnabledIfSystemProperty(named = "enable.hyperledger.aries.itests", matches = "true",
-        disabledReason = "Requires distributed ledger (i.e. blockchain)")
+                         disabledReason = "Requires distributed ledger (i.e. blockchain)")
 public class ConnectionlessProofTest extends AbstractCamelAriesTest {
 
     @BeforeEach
