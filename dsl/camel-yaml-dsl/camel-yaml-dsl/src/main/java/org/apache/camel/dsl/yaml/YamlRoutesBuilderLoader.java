a444ac88dbbe2631f671298ef1b110b7363e2283
Marc Boulanger <hellhand@gmail.com>
2022-11-24 16:40:24 +0100

    [CAMEL-18753] camel-yaml-dsl - OnCompletion is not added in the route… (#8771)
    * [CAMEL-18753] camel-yaml-dsl - OnCompletion is not added in the route definition.
    
    * [CAMEL-18753] camel-yaml-dsl - OnCompletion is not added in the route definition.
    
    Co-authored-by: Boulanger M <marc@adeops.be>

--- dsl/camel-yaml-dsl/camel-yaml-dsl/src/main/java/org/apache/camel/dsl/yaml/YamlRoutesBuilderLoader.java
+++ dsl/camel-yaml-dsl/camel-yaml-dsl/src/main/java/org/apache/camel/dsl/yaml/YamlRoutesBuilderLoader.java
@@ -42,6 +42,7 @@ import org.apache.camel.dsl.yaml.common.exception.InvalidEndpointException;
 import org.apache.camel.dsl.yaml.common.exception.InvalidNodeTypeException;
 import org.apache.camel.dsl.yaml.deserializers.OutputAwareFromDefinition;
 import org.apache.camel.model.KameletDefinition;
+import org.apache.camel.model.OnCompletionDefinition;
 import org.apache.camel.model.OnExceptionDefinition;
 import org.apache.camel.model.ProcessorDefinition;
 import org.apache.camel.model.RouteConfigurationDefinition;
@@ -168,6 +169,14 @@ public class YamlRoutesBuilderLoader extends YamlRoutesBuilderLoaderSupport {
                     return true;
                 } else if (item instanceof CamelContextCustomizer) {
                     ((CamelContextCustomizer) item).configure(getCamelContext());
+                    return true;
+                } else if (item instanceof OnCompletionDefinition) {
+                    if (!getRouteCollection().getRoutes().isEmpty()) {
+                        throw new IllegalArgumentException(
+                                "onCompletion must be defined before any routes in the RouteBuilder");
+                    }
+                    CamelContextAware.trySetCamelContext(getRouteCollection(), getCamelContext());
+                    getRouteCollection().getOnCompletions().add((OnCompletionDefinition) item);
                     return true;
                 } else if (item instanceof OnExceptionDefinition) {
                     if (!getRouteCollection().getRoutes().isEmpty()) {
