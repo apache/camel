2cec99fcf9f20fe75c5c1b89ebe9b16c6b426d73
Andrea Cosentino <ancosen@gmail.com>
2022-11-30 14:02:52 +0100

    CAMEL-18773 - camel-google-pubsub - Do not use dot in header keys
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-google/camel-google-pubsub/src/main/java/org/apache/camel/component/google/pubsub/GooglePubsubConstants.java
+++ components/camel-google/camel-google-pubsub/src/main/java/org/apache/camel/component/google/pubsub/GooglePubsubConstants.java
@@ -21,19 +21,19 @@ import org.apache.camel.spi.Metadata;
 public final class GooglePubsubConstants {
 
     @Metadata(description = "The ID of the message, assigned by the server when the message is published.", javaType = "String")
-    public static final String MESSAGE_ID = "CamelGooglePubsub.MessageId";
+    public static final String MESSAGE_ID = "CamelGooglePubsubMessageId";
     @Metadata(label = "consumer", description = "The ID used to acknowledge the received message.", javaType = "String")
-    public static final String ACK_ID = "CamelGooglePubsub.MsgAckId";
+    public static final String ACK_ID = "CamelGooglePubsubMsgAckId";
     @Metadata(label = "consumer", description = "The time at which the message was published",
               javaType = "com.google.protobuf.Timestamp")
-    public static final String PUBLISH_TIME = "CamelGooglePubsub.PublishTime";
+    public static final String PUBLISH_TIME = "CamelGooglePubsubPublishTime";
     @Metadata(description = "The attributes of the message.", javaType = "Map<String, String>")
-    public static final String ATTRIBUTES = "CamelGooglePubsub.Attributes";
+    public static final String ATTRIBUTES = "CamelGooglePubsubAttributes";
     @Metadata(label = "producer",
               description = "If non-empty, identifies related messages for which publish order should be\n" +
                             " respected.",
               javaType = "String")
-    public static final String ORDERING_KEY = "CamelGooglePubsub.OrderingKey";
+    public static final String ORDERING_KEY = "CamelGooglePubsubOrderingKey";
     public static final String RESERVED_GOOGLE_CLIENT_ATTRIBUTE_PREFIX = "goog";
 
     public enum AckMode {
