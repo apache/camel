4e58ebded8f09cbcb4f3bfb089692f6a1ca74109
Vladimir V. Bychkov <github@bychkov.name>
2022-11-30 19:14:15 +0100

    Google Spreadsheets tests refactoring (#8788)

--- components/camel-google/camel-google-sheets/src/test/java/org/apache/camel/component/google/sheets/SheetsSpreadsheetsValuesIT.java
+++ components/camel-google/camel-google-sheets/src/test/java/org/apache/camel/component/google/sheets/SheetsSpreadsheetsValuesIT.java
@@ -17,12 +17,14 @@
 package org.apache.camel.component.google.sheets;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
+import java.util.Optional;
 
+import com.google.api.client.testing.http.MockLowLevelHttpResponse;
 import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.ClearValuesRequest;
 import com.google.api.services.sheets.v4.model.ClearValuesResponse;
@@ -35,11 +37,9 @@ import org.apache.camel.component.google.sheets.internal.GoogleSheetsConstants;
 import org.apache.camel.component.google.sheets.internal.SheetsSpreadsheetsValuesApiMethod;
 import org.apache.camel.util.ObjectHelper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.apache.camel.component.google.sheets.server.GoogleSheetsApiTestServerAssert.assertThatGoogleApi;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -47,181 +47,209 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Test class for {@link com.google.api.services.sheets.v4.Sheets.Spreadsheets.Values} APIs.
  */
-@EnabledIf(value = "org.apache.camel.component.google.sheets.AbstractGoogleSheetsTestSupport#hasCredentials",
-           disabledReason = "Google Sheets credentials were not provided")
-public class SheetsSpreadsheetsValuesIT extends AbstractGoogleSheetsTestSupport {
+public class SheetsSpreadsheetsValuesIT {
 
     private static final Logger LOG = LoggerFactory.getLogger(SheetsSpreadsheetsValuesIT.class);
     private static final String PATH_PREFIX
             = GoogleSheetsApiCollection.getCollection().getApiName(SheetsSpreadsheetsValuesApiMethod.class).getName();
 
-    @Test
-    public void testGet() throws Exception {
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .createSpreadsheetRequest()
-                .hasSheetTitle("TestData")
-                .andReturnRandomSpreadsheet();
+    public static class GetTest extends AbstractGoogleSheetsTestSupport {
+        private Spreadsheet testSheet = getSpreadsheet();
 
-        Spreadsheet testSheet = getSpreadsheet();
+        @Test
+        public void test() throws Exception {
+            final Map<String, Object> headers = new HashMap<>();
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", TEST_RANGE);
 
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .getValuesRequest(testSheet.getSpreadsheetId(), TEST_SHEET + "!A1:B2")
-                .andReturnValues(Collections.emptyList());
+            final ValueRange result = requestBodyAndHeaders("direct://GET", null, headers);
 
-        final Map<String, Object> headers = new HashMap<>();
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", TEST_SHEET + "!A1:B2");
+            assertNotNull(result, "get result is null");
+            assertEquals(TEST_RANGE, result.getRange());
+            assertTrue(ObjectHelper.isEmpty(result.getValues()), "expected empty value range but found entries");
 
-        final ValueRange result = requestBodyAndHeaders("direct://GET", null, headers);
+            LOG.debug("get: " + result);
+        }
 
-        assertNotNull(result, "get result is null");
-        assertEquals(TEST_SHEET + "!A1:B2", result.getRange());
-        assertTrue(ObjectHelper.isEmpty(result.getValues()), "expected empty value range but found entries");
+        @Override
+        protected GoogleSheetsClientFactory getClientFactory() throws Exception {
+            return new MockGoogleSheetsClientFactory(
+                    new MockLowLevelHttpResponse()
+                            .setContent("{" + "\"range\": \"" + TEST_RANGE + "\"," + "\"majorDimension\": \"ROWS\"" + "}"));
+        }
 
-        LOG.debug("get: " + result);
+        @Override
+        protected RouteBuilder createRouteBuilder() {
+            return new RouteBuilder() {
+                @Override
+                public void configure() {
+                    from("direct://GET")
+                            .to("google-sheets://" + PATH_PREFIX + "/get");
+                }
+            };
+        }
     }
 
-    @Test
-    public void testUpdate() throws Exception {
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .createSpreadsheetRequest()
-                .hasSheetTitle("TestData")
-                .andReturnRandomSpreadsheet();
-
-        Spreadsheet testSheet = getSpreadsheet();
-
-        List<List<Object>> data = Arrays.asList(
+    public static class UpdateTest extends AbstractGoogleSheetsTestSupport {
+        private Spreadsheet testSheet = getSpreadsheet();
+        private String range = "TEST_SHEET!A1:B2";
+        private List<List<Object>> data = Arrays.asList(
                 Arrays.asList("A1", "B1"),
                 Arrays.asList("A2", "B2"));
 
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .updateValuesRequest(testSheet.getSpreadsheetId(), TEST_SHEET + "!A1:B2", data)
-                .andReturnUpdateResponse();
+        @Test
+        public void test() throws Exception {
 
-        ValueRange values = new ValueRange();
-        values.setValues(data);
+            ValueRange values = new ValueRange();
+            values.setValues(data);
 
-        final Map<String, Object> headers = new HashMap<>();
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", TEST_SHEET + "!A1:B2");
-        // parameter type is com.google.api.services.sheets.v4.model.ValueRange
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "values", values);
+            final Map<String, Object> headers = new HashMap<>();
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", range);
+            // parameter type is com.google.api.services.sheets.v4.model.ValueRange
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "values", values);
 
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "valueInputOption", "USER_ENTERED");
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "valueInputOption", "USER_ENTERED");
 
-        final UpdateValuesResponse result = requestBodyAndHeaders("direct://UPDATE", null, headers);
+            final UpdateValuesResponse result = requestBodyAndHeaders("direct://UPDATE", null, headers);
 
-        assertNotNull(result, "update result is null");
-        assertEquals(testSheet.getSpreadsheetId(), result.getSpreadsheetId());
-        assertEquals(TEST_SHEET + "!A1:B2", result.getUpdatedRange());
-        assertEquals(Integer.valueOf(2), result.getUpdatedRows());
-        assertEquals(Integer.valueOf(4), result.getUpdatedCells());
+            assertNotNull(result, "update result is null");
+            assertEquals(testSheet.getSpreadsheetId(), result.getSpreadsheetId());
+            assertEquals(range, result.getUpdatedRange());
+            assertEquals(data.size(), result.getUpdatedRows());
+            assertEquals(data.size() * data.get(0).size(), result.getUpdatedCells());
 
-        LOG.debug("update: " + result);
+            LOG.debug("update: " + result);
+        }
+
+        @Override
+        protected GoogleSheetsClientFactory getClientFactory() throws Exception {
+            return new MockGoogleSheetsClientFactory(
+                    new MockLowLevelHttpResponse()
+                            .setContent("{" + "\"spreadsheetId\": \"" + testSheet.getSpreadsheetId() + "\","
+                                        + "\"updatedRange\": \"" + range + "\"," + "\"updatedRows\": "
+                                        + data.size() + "," + "\"updatedColumns\": "
+                                        + Optional.ofNullable(data.get(0)).map(Collection::size).orElse(0) + ","
+                                        + "\"updatedCells\": "
+                                        + data.size()
+                                          * Optional.ofNullable(data.get(0)).map(Collection::size).orElse(0)
+                                        + "}"));
+        }
+
+        @Override
+        protected RouteBuilder createRouteBuilder() {
+            return new RouteBuilder() {
+                @Override
+                public void configure() {
+                    from("direct://UPDATE")
+                            .to("google-sheets://" + PATH_PREFIX + "/update");
+                }
+            };
+        }
     }
 
-    @Test
-    public void testAppend() throws Exception {
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .createSpreadsheetRequest()
-                .hasSheetTitle("TestData")
-                .andReturnRandomSpreadsheet();
+    public static class AppendTest extends AbstractGoogleSheetsTestSupport {
+        private Spreadsheet testSheet = getSpreadsheet();
+        private List<List<Object>> data = Collections.singletonList(Arrays.asList("A10", "B10", "C10"));
+        private String range = TEST_SHEET + "!A10";
+        private String updateRange = TEST_SHEET + "!" + data.get(0).get(0) + ":" + data.get(0).get(data.get(0).size() - 1);
 
-        Spreadsheet testSheet = getSpreadsheet();
+        @Test
+        public void test() throws Exception {
+            final Map<String, Object> headers = new HashMap<>();
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", range);
+            // parameter type is com.google.api.services.sheets.v4.model.ValueRange
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "values", new ValueRange().setValues(data));
 
-        List<List<Object>> data = Collections.singletonList(Arrays.asList("A10", "B10", "C10"));
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "valueInputOption", "USER_ENTERED");
 
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .appendValuesRequest(testSheet.getSpreadsheetId(), TEST_SHEET + "!A10", data)
-                .andReturnAppendResponse(TEST_SHEET + "!A10:C10");
+            final AppendValuesResponse result = requestBodyAndHeaders("direct://APPEND", null, headers);
 
-        final Map<String, Object> headers = new HashMap<>();
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", TEST_SHEET + "!A10");
-        // parameter type is com.google.api.services.sheets.v4.model.ValueRange
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "values", new ValueRange().setValues(data));
+            assertNotNull(result, "append result is null");
+            assertEquals(testSheet.getSpreadsheetId(), result.getSpreadsheetId());
+            assertEquals(updateRange, result.getUpdates().getUpdatedRange());
+            assertEquals(data.size(), result.getUpdates().getUpdatedRows());
+            assertEquals(data.get(0).size(), result.getUpdates().getUpdatedCells());
 
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "valueInputOption", "USER_ENTERED");
+            LOG.debug("append: " + result);
+        }
 
-        final AppendValuesResponse result = requestBodyAndHeaders("direct://APPEND", null, headers);
+        @Override
+        protected GoogleSheetsClientFactory getClientFactory() throws Exception {
+            return new MockGoogleSheetsClientFactory(
+                    new MockLowLevelHttpResponse()
+                            .setContent(
+                                    "{" + "\"spreadsheetId\": \"" + testSheet.getSpreadsheetId() + "\"," + "\"updates\":" + "{"
+                                        + "\"spreadsheetId\": \"" + testSheet.getSpreadsheetId() + "\","
+                                        + "\"updatedRange\": \"" + updateRange + "\"," + "\"updatedRows\": "
+                                        + data.size() + "," + "\"updatedColumns\": "
+                                        + Optional.ofNullable(data.get(0)).map(Collection::size).orElse(0) + ","
+                                        + "\"updatedCells\": "
+                                        + data.size()
+                                          * Optional.ofNullable(data.get(0)).map(Collection::size).orElse(0)
+                                        + "}" + "}"));
+        }
 
-        assertNotNull(result, "append result is null");
-        assertEquals(testSheet.getSpreadsheetId(), result.getSpreadsheetId());
-        assertEquals(TEST_SHEET + "!A10:C10", result.getUpdates().getUpdatedRange());
-        assertEquals(Integer.valueOf(1), result.getUpdates().getUpdatedRows());
-        assertEquals(Integer.valueOf(3), result.getUpdates().getUpdatedCells());
-
-        LOG.debug("append: " + result);
+        @Override
+        protected RouteBuilder createRouteBuilder() {
+            return new RouteBuilder() {
+                @Override
+                public void configure() {
+                    from("direct://APPEND")
+                            .to("google-sheets://" + PATH_PREFIX + "/append");
+                }
+            };
+        }
     }
 
-    @Test
-    public void testClear() throws Exception {
-        String spreadsheetId = UUID.randomUUID().toString();
+    public static class ClearTest extends AbstractGoogleSheetsTestSupport {
+        private Spreadsheet testSheet = getSpreadsheet();
 
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .createSpreadsheetRequest()
-                .hasSheetTitle("TestData")
-                .andReturnSpreadsheet(spreadsheetId);
+        @Test
+        public void test() throws Exception {
+            final Map<String, Object> headers = new HashMap<>();
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
+            // parameter type is String
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", TEST_RANGE);
+            // parameter type is com.google.api.services.sheets.v4.model.ClearValuesRequest
+            headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "clearValuesRequest", new ClearValuesRequest());
 
-        Spreadsheet testSheet = getSpreadsheet();
+            final ClearValuesResponse result = requestBodyAndHeaders("direct://CLEAR", null, headers);
 
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .updateValuesRequest(spreadsheetId, TEST_SHEET + "!A1:B2",
-                        Arrays.asList(Arrays.asList("a1", "b1"), Arrays.asList("a2", "b2")))
-                .andReturnUpdateResponse();
+            assertNotNull(result, "clear result is null");
+            assertEquals(testSheet.getSpreadsheetId(), result.getSpreadsheetId());
+            assertEquals(TEST_RANGE, result.getClearedRange());
 
-        applyTestData(testSheet);
+            LOG.debug("clear: " + result);
+        }
 
-        assertThatGoogleApi(getGoogleApiTestServer())
-                .clearValuesRequest(testSheet.getSpreadsheetId(), TEST_SHEET + "!A1:B2")
-                .andReturnClearResponse(TEST_SHEET + "!A1:B2");
+        @Override
+        protected GoogleSheetsClientFactory getClientFactory() throws Exception {
+            return new MockGoogleSheetsClientFactory(
+                    new MockLowLevelHttpResponse().setContent(
+                            "{" + "\"spreadsheetId\": \"" + testSheet.getSpreadsheetId() + "\"," + "\"clearedRange\": \""
+                                                              + TEST_RANGE + "\"" + "}"));
+        }
 
-        final Map<String, Object> headers = new HashMap<>();
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "spreadsheetId", testSheet.getSpreadsheetId());
-        // parameter type is String
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "range", TEST_SHEET + "!A1:B2");
-        // parameter type is com.google.api.services.sheets.v4.model.ClearValuesRequest
-        headers.put(GoogleSheetsConstants.PROPERTY_PREFIX + "clearValuesRequest", new ClearValuesRequest());
-
-        final ClearValuesResponse result = requestBodyAndHeaders("direct://CLEAR", null, headers);
-
-        assertNotNull(result, "clear result is null");
-        assertEquals(testSheet.getSpreadsheetId(), result.getSpreadsheetId());
-        assertEquals(TEST_SHEET + "!A1:B2", result.getClearedRange());
-
-        LOG.debug("clear: " + result);
-    }
-
-    @Override
-    protected RouteBuilder createRouteBuilder() {
-        return new RouteBuilder() {
-            @Override
-            public void configure() {
-                // test route for append
-                from("direct://APPEND")
-                        .to("google-sheets://" + PATH_PREFIX + "/append");
-
-                // test route for clear
-                from("direct://CLEAR")
-                        .to("google-sheets://" + PATH_PREFIX + "/clear");
-
-                // test route for get
-                from("direct://GET")
-                        .to("google-sheets://" + PATH_PREFIX + "/get");
-
-                // test route for update
-                from("direct://UPDATE")
-                        .to("google-sheets://" + PATH_PREFIX + "/update");
-            }
-        };
+        @Override
+        protected RouteBuilder createRouteBuilder() {
+            return new RouteBuilder() {
+                @Override
+                public void configure() {
+                    from("direct://CLEAR")
+                            .to("google-sheets://" + PATH_PREFIX + "/clear");
+                }
+            };
+        }
     }
 }
