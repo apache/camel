0d37df94c44b5536e8a35090cb5797d092716c64
吴伟杰 <wuweijie@apache.org>
2022-12-01 22:56:45 +0800

    CAMEL-14831: Format RocketMQConstants and document

--- components/camel-rocketmq/src/main/java/org/apache/camel/component/rocketmq/RocketMQConstants.java
+++ components/camel-rocketmq/src/main/java/org/apache/camel/component/rocketmq/RocketMQConstants.java
@@ -21,51 +21,51 @@ import org.apache.camel.spi.Metadata;
 
 public final class RocketMQConstants {
 
-    @Metadata(javaType = "String")
+    @Metadata(description = "Broker name", javaType = "String")
     public static final String BROKER_NAME = "rocketmq.BROKER_NAME";
-    @Metadata(javaType = "int")
+    @Metadata(description = "Queue ID", javaType = "int")
     public static final String QUEUE_ID = "rocketmq.QUEUE_ID";
-    @Metadata(javaType = "int")
+    @Metadata(description = "Store size", javaType = "int")
     public static final String STORE_SIZE = "rocketmq.STORE_SIZE";
-    @Metadata(javaType = "long")
+    @Metadata(description = "Queue offset", javaType = "long")
     public static final String QUEUE_OFFSET = "rocketmq.QUEUE_OFFSET";
-    @Metadata(javaType = "int")
+    @Metadata(description = "Sys flag", javaType = "int")
     public static final String SYS_FLAG = "rocketmq.SYS_FLAG";
-    @Metadata(javaType = "long")
+    @Metadata(description = "Born timestamp", javaType = "long")
     public static final String BORN_TIMESTAMP = "rocketmq.BORN_TIMESTAMP";
-    @Metadata(javaType = "java.net.SocketAddress")
+    @Metadata(description = "Born host", javaType = "java.net.SocketAddress")
     public static final String BORN_HOST = "rocketmq.BORN_HOST";
-    @Metadata(javaType = "long")
+    @Metadata(description = "Store timestamp", javaType = "long")
     public static final String STORE_TIMESTAMP = "rocketmq.STORE_TIMESTAMP";
-    @Metadata(javaType = "java.net.SocketAddress")
+    @Metadata(description = "Store host", javaType = "java.net.SocketAddress")
     public static final String STORE_HOST = "rocketmq.STORE_HOST";
-    @Metadata(javaType = "String")
+    @Metadata(description = "Message ID", javaType = "String")
     public static final String MSG_ID = "rocketmq.MSG_ID";
-    @Metadata(javaType = "long")
+    @Metadata(description = "Commit log offset", javaType = "long")
     public static final String COMMIT_LOG_OFFSET = "rocketmq.COMMIT_LOG_OFFSET";
-    @Metadata(javaType = "int")
+    @Metadata(description = "Body CRC", javaType = "int")
     public static final String BODY_CRC = "rocketmq.BODY_CRC";
-    @Metadata(javaType = "int")
+    @Metadata(description = "Reconsume times", javaType = "int")
     public static final String RECONSUME_TIMES = "rocketmq.RECONSUME_TIMES";
-    @Metadata(javaType = "long")
+    @Metadata(description = "Prepard transaction offset", javaType = "long")
     public static final String PREPARED_TRANSACTION_OFFSET = "rocketmq.PREPARED_TRANSACTION_OFFSET";
     @Metadata(description = "If this header is set, the message will be routed to the topic specified by this header\n" +
-            "instead of the origin topic in endpoint.",
-            javaType = "String")
+                            "instead of the origin topic in endpoint.",
+              javaType = "String")
     public static final String OVERRIDE_TOPIC_NAME = "rocketmq.OVERRIDE_TOPIC_NAME";
     @Metadata(description = "If this header is set, the message's tag will be set to value specified by this header\n" +
-            "instead of the sendTag defined in endpoint.",
-            javaType = "String")
+                            "instead of the sendTag defined in endpoint.",
+              javaType = "String")
     public static final String OVERRIDE_TAG = "rocketmq.OVERRIDE_TAG";
     @Metadata(description = "Set keys for the message. When using in-out pattern,\n" +
-            "the value will be prepended to the generated keys",
-            javaType = "String")
+                            "the value will be prepended to the generated keys",
+              javaType = "String")
     public static final String OVERRIDE_MESSAGE_KEY = "rocketmq.OVERRIDE_MESSAGE_KEY";
-    @Metadata(javaType = "String")
+    @Metadata(description = "Tag of message", javaType = "String")
     public static final String TAG = "rocketmq.TAG";
-    @Metadata(javaType = "String")
+    @Metadata(description = "Topic of message", javaType = "String")
     public static final String TOPIC = "rocketmq.TOPIC";
-    @Metadata(javaType = "String")
+    @Metadata(description = "Key of message", javaType = "String")
     public static final String KEY = "rocketmq.KEY";
 
     private RocketMQConstants() {
