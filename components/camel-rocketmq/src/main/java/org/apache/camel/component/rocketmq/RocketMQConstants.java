0699c59d8921d700a16a0dec78ed79e91ee2e23f
吴伟杰 <wuweijie@apache.org>
2022-12-02 20:40:36 +0800

    CAMEL-14831: Rename header keys

--- components/camel-rocketmq/src/main/java/org/apache/camel/component/rocketmq/RocketMQConstants.java
+++ components/camel-rocketmq/src/main/java/org/apache/camel/component/rocketmq/RocketMQConstants.java
@@ -21,52 +21,52 @@ import org.apache.camel.spi.Metadata;
 
 public final class RocketMQConstants {
 
-    @Metadata(description = "Broker name", javaType = "String")
-    public static final String BROKER_NAME = "rocketmq.BROKER_NAME";
-    @Metadata(description = "Queue ID", javaType = "int")
-    public static final String QUEUE_ID = "rocketmq.QUEUE_ID";
-    @Metadata(description = "Store size", javaType = "int")
-    public static final String STORE_SIZE = "rocketmq.STORE_SIZE";
-    @Metadata(description = "Queue offset", javaType = "long")
-    public static final String QUEUE_OFFSET = "rocketmq.QUEUE_OFFSET";
-    @Metadata(description = "Sys flag", javaType = "int")
-    public static final String SYS_FLAG = "rocketmq.SYS_FLAG";
-    @Metadata(description = "Born timestamp", javaType = "long")
-    public static final String BORN_TIMESTAMP = "rocketmq.BORN_TIMESTAMP";
-    @Metadata(description = "Born host", javaType = "java.net.SocketAddress")
-    public static final String BORN_HOST = "rocketmq.BORN_HOST";
-    @Metadata(description = "Store timestamp", javaType = "long")
-    public static final String STORE_TIMESTAMP = "rocketmq.STORE_TIMESTAMP";
-    @Metadata(description = "Store host", javaType = "java.net.SocketAddress")
-    public static final String STORE_HOST = "rocketmq.STORE_HOST";
-    @Metadata(description = "Message ID", javaType = "String")
-    public static final String MSG_ID = "rocketmq.MSG_ID";
-    @Metadata(description = "Commit log offset", javaType = "long")
-    public static final String COMMIT_LOG_OFFSET = "rocketmq.COMMIT_LOG_OFFSET";
-    @Metadata(description = "Body CRC", javaType = "int")
-    public static final String BODY_CRC = "rocketmq.BODY_CRC";
-    @Metadata(description = "Reconsume times", javaType = "int")
-    public static final String RECONSUME_TIMES = "rocketmq.RECONSUME_TIMES";
-    @Metadata(description = "Prepard transaction offset", javaType = "long")
-    public static final String PREPARED_TRANSACTION_OFFSET = "rocketmq.PREPARED_TRANSACTION_OFFSET";
+    @Metadata(description = "Topic of message", javaType = "String")
+    public static final String TOPIC = "CamelRockerMQTopic";
+    @Metadata(description = "Tag of message", javaType = "String")
+    public static final String TAG = "CamelRockerMQTag";
+    @Metadata(description = "Key of message", javaType = "String")
+    public static final String KEY = "CamelRockerMQKey";
     @Metadata(description = "If this header is set, the message will be routed to the topic specified by this header\n" +
                             "instead of the origin topic in endpoint.",
               javaType = "String")
-    public static final String OVERRIDE_TOPIC_NAME = "rocketmq.OVERRIDE_TOPIC_NAME";
+    public static final String OVERRIDE_TOPIC_NAME = "CamelRockerMQOverrideTopicName";
     @Metadata(description = "If this header is set, the message's tag will be set to value specified by this header\n" +
                             "instead of the sendTag defined in endpoint.",
               javaType = "String")
-    public static final String OVERRIDE_TAG = "rocketmq.OVERRIDE_TAG";
+    public static final String OVERRIDE_TAG = "CamelRockerMQOverrideTag";
     @Metadata(description = "Set keys for the message. When using in-out pattern,\n" +
                             "the value will be prepended to the generated keys",
               javaType = "String")
-    public static final String OVERRIDE_MESSAGE_KEY = "rocketmq.OVERRIDE_MESSAGE_KEY";
-    @Metadata(description = "Tag of message", javaType = "String")
-    public static final String TAG = "rocketmq.TAG";
-    @Metadata(description = "Topic of message", javaType = "String")
-    public static final String TOPIC = "rocketmq.TOPIC";
-    @Metadata(description = "Key of message", javaType = "String")
-    public static final String KEY = "rocketmq.KEY";
+    public static final String OVERRIDE_MESSAGE_KEY = "CamelRockerMQOverrideMessageKey";
+    @Metadata(description = "Broker name", javaType = "String")
+    public static final String BROKER_NAME = "CamelRockerMQBrokerName";
+    @Metadata(description = "Queue ID", javaType = "int")
+    public static final String QUEUE_ID = "CamelRockerMQQueueId";
+    @Metadata(description = "Store size", javaType = "int")
+    public static final String STORE_SIZE = "CamelRockerMQStoreSize";
+    @Metadata(description = "Queue offset", javaType = "long")
+    public static final String QUEUE_OFFSET = "CamelRockerMQQueueOffset";
+    @Metadata(description = "Sys flag", javaType = "int")
+    public static final String SYS_FLAG = "CamelRockerMQSysFlag";
+    @Metadata(description = "Born timestamp", javaType = "long")
+    public static final String BORN_TIMESTAMP = "CamelRockerMQBornTimestamp";
+    @Metadata(description = "Born host", javaType = "java.net.SocketAddress")
+    public static final String BORN_HOST = "CamelRockerMQBornHost";
+    @Metadata(description = "Store timestamp", javaType = "long")
+    public static final String STORE_TIMESTAMP = "CamelRockerMQStoreTimestamp";
+    @Metadata(description = "Store host", javaType = "java.net.SocketAddress")
+    public static final String STORE_HOST = "CamelRockerMQStoreHost";
+    @Metadata(description = "Msg ID", javaType = "String")
+    public static final String MSG_ID = "CamelRockerMQMsgId";
+    @Metadata(description = "Commit log offset", javaType = "long")
+    public static final String COMMIT_LOG_OFFSET = "CamelRockerMQCommitLogOffset";
+    @Metadata(description = "Body CRC", javaType = "int")
+    public static final String BODY_CRC = "CamelRockerMQBodyCrc";
+    @Metadata(description = "Reconsume times", javaType = "int")
+    public static final String RECONSUME_TIMES = "CamelRockerMQReconsumeTimes";
+    @Metadata(description = "Prepard transaction offset", javaType = "long")
+    public static final String PREPARED_TRANSACTION_OFFSET = "CamelRockerMQPreparedTransactionOffset";
 
     private RocketMQConstants() {
     }
