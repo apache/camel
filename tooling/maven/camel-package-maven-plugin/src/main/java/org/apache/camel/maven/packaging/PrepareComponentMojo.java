b2361a968fd783a7a7aa4b8abe12b7a7dbe66954
Claus Ibsen <claus.ibsen@gmail.com>
2022-12-02 06:53:51 +0100

    Fixed CS

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareComponentMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareComponentMojo.java
@@ -308,8 +308,9 @@ public class PrepareComponentMojo extends AbstractGeneratorMojo {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o)
+            if (this == o) {
                 return true;
+            }
             if (o == null || getClass() != o.getClass()) {
                 return false;
             }
