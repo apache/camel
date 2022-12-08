092dbc887c5eea9985990b8c2941090429862550
Bharati Kulkarni <bmk15897@gmail.com>
2022-12-08 17:03:17 -0600

    Removed Unused Import

--- components/camel-gson/src/test/java/org/apache/camel/component/gson/GsonMarshalExclusionTest.java
+++ components/camel-gson/src/test/java/org/apache/camel/component/gson/GsonMarshalExclusionTest.java
@@ -27,7 +27,6 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
