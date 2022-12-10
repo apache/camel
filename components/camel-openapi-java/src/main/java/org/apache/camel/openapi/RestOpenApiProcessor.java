3391380d6490188a17e2881de03a38f546fa38b9
Gilvan Filho <gilvan.sfilho@gmail.com>
2022-12-09 23:49:51 -0300

    (chores) camel-openapi-java: replace raw empty map by generic one

--- components/camel-openapi-java/src/main/java/org/apache/camel/openapi/RestOpenApiProcessor.java
+++ components/camel-openapi-java/src/main/java/org/apache/camel/openapi/RestOpenApiProcessor.java
@@ -33,7 +33,6 @@ public class RestOpenApiProcessor implements Processor {
     private final RestOpenApiSupport support;
     private final RestConfiguration configuration;
 
-    @SuppressWarnings("unchecked")
     public RestOpenApiProcessor(Map<String, Object> parameters,
                                 RestConfiguration configuration) {
         this.configuration = configuration;
@@ -41,7 +40,7 @@ public class RestOpenApiProcessor implements Processor {
         this.openApiConfig = new BeanConfig();
 
         if (parameters == null) {
-            parameters = Collections.EMPTY_MAP;
+            parameters = Collections.emptyMap();
         }
         support.initOpenApi(openApiConfig, parameters);
     }
