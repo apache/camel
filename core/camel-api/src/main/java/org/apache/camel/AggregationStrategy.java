c7902015232e2af9a5f7e3d432944c6556179626
Claus Ibsen <claus.ibsen@gmail.com>
2022-12-14 18:44:03 +0100

    CAMEL-18739: camel-core - AggregationStrategy onCompletion to have access to input exchange.

--- core/camel-api/src/main/java/org/apache/camel/AggregationStrategy.java
+++ core/camel-api/src/main/java/org/apache/camel/AggregationStrategy.java
@@ -125,9 +125,9 @@ public interface AggregationStrategy {
      *
      * <b>Important: </b> This method must <b>not</b> throw any exceptions.
      *
-     * @param exchange the current aggregated exchange, or the original {@link org.apache.camel.Exchange} if no
-     *                 aggregation has been done before the completion occurred
-     * @param  inputExchange the input exchange (input to the EIP)
+     * @param exchange      the current aggregated exchange, or the original {@link org.apache.camel.Exchange} if no
+     *                      aggregation has been done before the completion occurred
+     * @param inputExchange the input exchange (input to the EIP)
      */
     default void onCompletion(Exchange exchange, Exchange inputExchange) {
         onCompletion(exchange);
