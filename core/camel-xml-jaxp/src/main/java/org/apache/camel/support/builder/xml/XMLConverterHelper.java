3b12e488b52762c8d3012c5d4a10aa2a72e9800f
Claus Ibsen <claus.ibsen@gmail.com>
2022-12-19 09:59:34 +0100

    CAMEL-18825: Make XmlHelper more secure

--- core/camel-xml-jaxp/src/main/java/org/apache/camel/support/builder/xml/XMLConverterHelper.java
+++ core/camel-xml-jaxp/src/main/java/org/apache/camel/support/builder/xml/XMLConverterHelper.java
@@ -129,6 +129,13 @@ public class XMLConverterHelper {
                     XMLConstants.FEATURE_SECURE_PROCESSING, true, e.getMessage());
         }
         try {
+            // Set secure processing
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        } catch (ParserConfigurationException e) {
+            LOG.warn("DocumentBuilderFactory doesn't support the feature {} with value {}, due to {}.",
+                    "http://apache.org/xml/features/disallow-doctype-decl", true, e.getMessage());
+        }
+        try {
             // Disable the external-general-entities by default
             factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
         } catch (ParserConfigurationException e) {
@@ -140,7 +147,7 @@ public class XMLConverterHelper {
             Class<?> smClass = ObjectHelper.loadClass("org.apache.xerces.util.SecurityManager");
             if (smClass != null) {
                 Object sm = smClass.getDeclaredConstructor().newInstance();
-                // Here we just use the default setting of the SeurityManager
+                // Here we just use the default setting of the SecurityManager
                 factory.setAttribute("http://apache.org/xml/properties/security-manager", sm);
             }
         } catch (Exception e) {
