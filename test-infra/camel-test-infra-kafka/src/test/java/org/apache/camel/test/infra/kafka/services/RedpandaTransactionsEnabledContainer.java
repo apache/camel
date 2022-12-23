cf3f70e20bc9195bfce5317603f23599dc09f0ac
Claus Ibsen <claus.ibsen@gmail.com>
2022-12-23 10:27:46 +0100

    CAMEL-18839: camel-kafka - Upgrade to newer kafka images for camel-test-infra-kafka

--- test-infra/camel-test-infra-kafka/src/test/java/org/apache/camel/test/infra/kafka/services/RedpandaTransactionsEnabledContainer.java
+++ test-infra/camel-test-infra-kafka/src/test/java/org/apache/camel/test/infra/kafka/services/RedpandaTransactionsEnabledContainer.java
@@ -22,7 +22,7 @@ import org.testcontainers.redpanda.RedpandaContainer;
 
 public class RedpandaTransactionsEnabledContainer extends RedpandaContainer {
 
-    public static final String DEFAULT_REDPANDA_CONTAINER = "docker.redpanda.com/vectorized/redpanda:v22.2.6";
+    public static final String DEFAULT_REDPANDA_CONTAINER = "docker.redpanda.com/vectorized/redpanda:v22.3.9";
     public static final String REDPANDA_CONTAINER
             = System.getProperty("itest.redpanda.container.image", DEFAULT_REDPANDA_CONTAINER);
     public static final int REDPANDA_PORT = 9092;
