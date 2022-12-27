98501f305441ec686bd59a3d0849d7e9e4e47f7f
davsclaus <davsclaus@users.noreply.github.com>
2022-12-27 12:08:19 +0000

    Regen for commit 9bd7acd73f5ae0acfac4763570b4a5c252e989e3
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-core-model/src/main/java/org/apache/camel/model/dataformat/JaxbDataFormat.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/dataformat/JaxbDataFormat.java
@@ -358,12 +358,11 @@ public class JaxbDataFormat extends DataFormatDefinition implements ContentTypeH
     /**
      * Only in use if schema validation has been enabled.
      *
-     * Restrict access to the protocols specified for external reference set by the schemaLocation attribute, Import and Include element.
-     * Examples of protocols are file, http, jar:file.
+     * Restrict access to the protocols specified for external reference set by the schemaLocation attribute, Import and
+     * Include element. Examples of protocols are file, http, jar:file.
      *
-     * false or none to deny all access to external references;
-     * a specific protocol, such as file, to give permission to only the protocol;
-     * the keyword all to grant permission to all protocols.
+     * false or none to deny all access to external references; a specific protocol, such as file, to give permission to
+     * only the protocol; the keyword all to grant permission to all protocols.
      */
     public void setAccessExternalSchemaProtocols(String accessExternalSchemaProtocols) {
         this.accessExternalSchemaProtocols = accessExternalSchemaProtocols;
@@ -647,12 +646,11 @@ public class JaxbDataFormat extends DataFormatDefinition implements ContentTypeH
         /**
          * Only in use if schema validation has been enabled.
          *
-         * Restrict access to the protocols specified for external reference set by the schemaLocation attribute, Import and Include element.
-         * Examples of protocols are file, http, jar:file.
+         * Restrict access to the protocols specified for external reference set by the schemaLocation attribute, Import
+         * and Include element. Examples of protocols are file, http, jar:file.
          *
-         * false or none to deny all access to external references;
-         * a specific protocol, such as file, to give permission to only the protocol;
-         * the keyword all to grant permission to all protocols.
+         * false or none to deny all access to external references; a specific protocol, such as file, to give
+         * permission to only the protocol; the keyword all to grant permission to all protocols.
          */
         public Builder accessExternalSchemaProtocols(String accessExternalSchemaProtocols) {
             this.accessExternalSchemaProtocols = accessExternalSchemaProtocols;
