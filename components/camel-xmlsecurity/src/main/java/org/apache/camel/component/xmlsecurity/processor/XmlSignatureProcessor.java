92bfc6534492ec6d4334e845a778a74dce992351
Claus Ibsen <claus.ibsen@gmail.com>
2023-01-02 15:28:45 +0100

    CAMEL-18825: Make XML parser/transformers more secure out of the box.

--- components/camel-xmlsecurity/src/main/java/org/apache/camel/component/xmlsecurity/processor/XmlSignatureProcessor.java
+++ components/camel-xmlsecurity/src/main/java/org/apache/camel/component/xmlsecurity/processor/XmlSignatureProcessor.java
@@ -134,6 +134,9 @@ public abstract class XmlSignatureProcessor implements Processor {
         SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         schemaFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
         schemaFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        LOG.debug("Configuring SchemaFactory to not allow access to external DTD/Schema");
+        schemaFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        schemaFactory.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
         schemaFactory.setResourceResolver(new DefaultLSResourceResolver(
                 getCamelContext(), getConfiguration()
                         .getSchemaResourceUri()));
