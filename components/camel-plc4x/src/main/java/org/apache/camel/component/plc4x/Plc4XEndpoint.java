95c63a6e7bfebcaa05fe15c78459cb14543eb0a2
Stein Overtoom <stein.overtoom@triopsys.nl>
2023-01-04 11:52:46 +0100

    camel-plc4x: fix autoReconnect parameter style

--- components/camel-plc4x/src/main/java/org/apache/camel/component/plc4x/Plc4XEndpoint.java
+++ components/camel-plc4x/src/main/java/org/apache/camel/component/plc4x/Plc4XEndpoint.java
@@ -56,7 +56,7 @@ public class Plc4XEndpoint extends DefaultEndpoint {
     private int period;
     @UriParam
     @Metadata(description = "Whether to reconnect when no connection is present upon doing a request")
-    private boolean autoReconnect = false;
+    private boolean autoReconnect;
 
     private PlcDriverManager plcDriverManager;
     private PlcConnection connection;
