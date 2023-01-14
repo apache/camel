b1a6bd5c908abe178b0626c9a6a1d447e298e84e
Simon Rasmussen <sr@viabill.com>
2023-01-14 18:31:40 +0100

    CAMEL-18912: Reuse client from consumer (#9086)
    * CAMEL-18912: sqs - use client from consumer
    
    Before this change, autowired clients would fail to be detected.
    
    Also small optimization added by only requesting a single queue when
    listing the queues.
    
    * CAMEL-18912: athena - use client from endpoint
    
    * CAMEL-18912: cloudwatch - use client from endpoint
    
    * CAMEL-18912: dynamodb - use client from endpoint
    
    * CAMEL-18912: ec2 - use client from endpoint
    
    * CAMEL-18912: ses - use client from endpoint
    
    * CAMEL-18912: ecs - use client from endpoint
    
    * CAMEL-18912: eks - use client from endpoint
    
    * CAMEL-18912: iam - use client from endpoint
    
    * CAMEL-18912: msk - use client from endpoint
    
    * CAMEL-18912: s3 - use client from endpoint
    
    * CAMEL-18912: sns - use client from endpoint

--- components/camel-aws/camel-aws2-sqs/src/main/java/org/apache/camel/component/aws2/sqs/Sqs2ConsumerHealthCheck.java
+++ components/camel-aws/camel-aws2-sqs/src/main/java/org/apache/camel/component/aws2/sqs/Sqs2ConsumerHealthCheck.java
@@ -21,12 +21,10 @@ import java.util.Map;
 import org.apache.camel.health.HealthCheckResultBuilder;
 import org.apache.camel.impl.health.AbstractHealthCheck;
 import org.apache.camel.util.ObjectHelper;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.SqsClientBuilder;
+import software.amazon.awssdk.services.sqs.model.ListQueuesRequest;
 
 public class Sqs2ConsumerHealthCheck extends AbstractHealthCheck {
 
@@ -55,20 +53,10 @@ public class Sqs2ConsumerHealthCheck extends AbstractHealthCheck {
                     return;
                 }
             }
-            SqsClient client;
-            if (!configuration.isUseDefaultCredentialsProvider()) {
-                AwsBasicCredentials cred
-                        = AwsBasicCredentials.create(configuration.getAccessKey(), configuration.getSecretKey());
-                SqsClientBuilder clientBuilder = SqsClient.builder();
-                client = clientBuilder.credentialsProvider(StaticCredentialsProvider.create(cred))
-                        .region(Region.of(configuration.getRegion())).build();
-            } else if (ObjectHelper.isNotEmpty(configuration.getAmazonSQSClient())) {
-                client = configuration.getAmazonSQSClient();
-            } else {
-                SqsClientBuilder clientBuilder = SqsClient.builder();
-                client = clientBuilder.region(Region.of(configuration.getRegion())).build();
-            }
-            client.listQueues();
+
+            SqsClient client = sqs2Consumer.getClient();
+
+            client.listQueues(ListQueuesRequest.builder().maxResults(1).build());
         } catch (AwsServiceException e) {
             builder.message(e.getMessage());
             builder.error(e);
