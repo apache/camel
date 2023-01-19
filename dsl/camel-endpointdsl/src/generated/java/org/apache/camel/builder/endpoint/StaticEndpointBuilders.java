d879b0997b2fcaa11185181669409bc79f243b34
oscerd <oscerd@users.noreply.github.com>
2023-01-19 11:29:13 +0000

    Regen for commit dedef5fc924aa481c4b0c2a736c5c41a6ba70789
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
@@ -13485,6 +13485,51 @@ public class StaticEndpointBuilders {
         return org.apache.camel.builder.endpoint.dsl.StitchEndpointBuilderFactory.endpointBuilder(componentName, path);
     }
     /**
+     * Stomp (camel-stomp)
+     * Send and rececive messages to/from STOMP (Simple Text Oriented Messaging
+     * Protocol) compliant message brokers.
+     * 
+     * Category: messaging
+     * Since: 2.12
+     * Maven coordinates: org.apache.camel:camel-stomp
+     * 
+     * Syntax: <code>stomp:destination</code>
+     * 
+     * Path parameter: destination (required)
+     * Name of the queue
+     * 
+     * @param path destination
+     * @return the dsl builder
+     */
+    public static org.apache.camel.builder.endpoint.dsl.StompEndpointBuilderFactory.StompEndpointBuilder stomp(
+            String path) {
+        return org.apache.camel.builder.endpoint.dsl.StompEndpointBuilderFactory.endpointBuilder("stomp", path);
+    }
+    /**
+     * Stomp (camel-stomp)
+     * Send and rececive messages to/from STOMP (Simple Text Oriented Messaging
+     * Protocol) compliant message brokers.
+     * 
+     * Category: messaging
+     * Since: 2.12
+     * Maven coordinates: org.apache.camel:camel-stomp
+     * 
+     * Syntax: <code>stomp:destination</code>
+     * 
+     * Path parameter: destination (required)
+     * Name of the queue
+     * 
+     * @param componentName to use a custom component name for the endpoint
+     * instead of the default name
+     * @param path destination
+     * @return the dsl builder
+     */
+    public static org.apache.camel.builder.endpoint.dsl.StompEndpointBuilderFactory.StompEndpointBuilder stomp(
+            String componentName,
+            String path) {
+        return org.apache.camel.builder.endpoint.dsl.StompEndpointBuilderFactory.endpointBuilder(componentName, path);
+    }
+    /**
      * Stream (camel-stream)
      * Read from system-in and write to system-out and system-err streams.
      * 
