415c21046b17d75a83ff55beaf2f63370260e8e4
davsclaus <davsclaus@users.noreply.github.com>
2023-03-11 09:30:01 +0000

    Regen for commit d6ca5fca99d528b5394113433c5f4e9a1b0b5296
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-support/src/main/java/org/apache/camel/support/builder/ExpressionBuilder.java
+++ core/camel-support/src/main/java/org/apache/camel/support/builder/ExpressionBuilder.java
@@ -20,7 +20,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
