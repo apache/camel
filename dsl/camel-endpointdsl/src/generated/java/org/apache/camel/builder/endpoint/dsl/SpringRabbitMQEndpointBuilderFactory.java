fb9a80bf97a9a58b7c91e78ec44414fda065a540
Claus Ibsen <claus.ibsen@gmail.com>
2023-03-14 10:56:42 +0100

    CAMEL-19144: camel-catalog - Include information about existing Camel releases

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/SpringRabbitMQEndpointBuilderFactory.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/SpringRabbitMQEndpointBuilderFactory.java
@@ -1932,6 +1932,19 @@ public interface SpringRabbitMQEndpointBuilderFactory {
         public String springRabbitmqExchangeOverrideName() {
             return "SpringRabbitmqExchangeOverrideName";
         }
+
+        /**
+         * Delivery tag for manual acknowledge mode.
+         * 
+         * The option is a: {@code long} type.
+         * 
+         * Group: common
+         * 
+         * @return the name of the header {@code SpringRabbitmqDeliveryTag}.
+         */
+        public String springRabbitmqDeliveryTag() {
+            return "SpringRabbitmqDeliveryTag";
+        }
     }
     static SpringRabbitMQEndpointBuilder endpointBuilder(
             String componentName,
