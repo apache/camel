87ccb85d1b70267d8bf75fd9cc35f984fd4cd39b
Claus Ibsen <claus.ibsen@gmail.com>
2023-03-14 04:38:10 +0100

    Print user configuration values

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/Run.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/Run.java
@@ -217,9 +217,8 @@ public class Run extends CamelCommand {
 
     @Override
     public Integer call() throws Exception {
-        System.out.println("Running integration with the following configuration:");
-        printConfigurationValues();
-
+        printConfigurationValues("Running integration with the following configuration:");
+        // run
         return run();
     }
 
