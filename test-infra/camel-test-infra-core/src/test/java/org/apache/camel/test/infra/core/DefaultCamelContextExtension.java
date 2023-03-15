bbd7c363cfc47fa82e2afc28d95b9bd0033022ba
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-03-15 09:11:46 -0300

    CAMEL-18957: prevent a few NPEs if the context is not created

--- test-infra/camel-test-infra-core/src/test/java/org/apache/camel/test/infra/core/DefaultCamelContextExtension.java
+++ test-infra/camel-test-infra-core/src/test/java/org/apache/camel/test/infra/core/DefaultCamelContextExtension.java
@@ -16,6 +16,8 @@
  */
 package org.apache.camel.test.infra.core;
 
+import java.util.Objects;
+
 import org.apache.camel.BindToRegistry;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ConsumerTemplate;
@@ -26,6 +28,7 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.infra.core.annotations.ContextFixture;
 import org.apache.camel.test.infra.core.annotations.RouteFixture;
+import org.apache.camel.util.ObjectHelper;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -69,6 +72,7 @@ public class DefaultCamelContextExtension extends AbstractCamelContextExtension 
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         context = createCamelContext(fixtureProcessor, extensionContext);
+        Objects.requireNonNull(context, "Cannot run the test because the context is null");
 
         producerTemplate = context.createProducerTemplate();
         producerTemplate.start();
