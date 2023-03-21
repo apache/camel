4c6a7acb94c31d30a9467612147f936277037d3e
Dylan Piergies <Dylan.Piergies@tesco.com>
2023-03-21 14:53:05 +0000

    CAMEL-19155 Re-throw exceptions in `ServiceBusConsumer`
    If an exception occurred during Exchange processing, the exception must
    be rethrown in the message receive and error handlers to trigger the
    `ServiceBusReceiverAsyncClient` to abandon the message so that the
    message is requeued.

--- components/camel-azure/camel-azure-servicebus/src/main/java/org/apache/camel/component/azure/servicebus/ServiceBusConsumer.java
+++ components/camel-azure/camel-azure-servicebus/src/main/java/org/apache/camel/component/azure/servicebus/ServiceBusConsumer.java
@@ -145,6 +145,9 @@ public class ServiceBusConsumer extends DefaultConsumer {
         // use default consumer callback
         AsyncCallback cb = defaultConsumerCallback(exchange, true);
         getAsyncProcessor().process(exchange, cb);
+        if (exchange.getException() != null) {
+            throw RuntimeCamelException.wrapRuntimeException(exchange.getException());
+        }
     }
 
     private Exchange createServiceBusExchange(final ServiceBusReceivedMessage receivedMessage) {
@@ -190,6 +193,7 @@ public class ServiceBusConsumer extends DefaultConsumer {
             getExceptionHandler().handleException("Error processing exchange", exchange,
                     exchange.getException());
         }
+        throw RuntimeCamelException.wrapRuntimeException(errorContext);
     }
 
     private Exchange createServiceBusExchange(final Throwable errorContext) {
