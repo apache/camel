5bf8fa6576f61b2c2a92570a5aabbacbd9b8e697
Dylan Piergies <dylan.piergies@gmail.com>
2023-03-22 11:07:08 +0000

    Explicitly complete/abandon Azure Service Bus messages (#9592)
    * CAMEL-19155 Re-throw exceptions in `ServiceBusConsumer`
    
    If an exception occurred during Exchange processing, the exception must
    be rethrown in the message receive and error handlers to trigger the
    `ServiceBusReceiverAsyncClient` to abandon the message so that the
    message is requeued.
    
    * Revert "CAMEL-19155 Re-throw exceptions in `ServiceBusConsumer`"
    
    This reverts commit a2efbed012935b760d56dc935c4736c2bb303e81.
    
    * CAMEL-19155 - Explicitly complete/abandon Azure Service Bus messages
    
    Taking control of message complete/abandon when auto-complete is
    enabled, since allowing the Service Bus client to do this automatically
    causes messages to be completed even if the Exchange failed because
    errors do not propagate back to the reactive message Publisher.
    
    * CAMEL-19155 - Add explanatory comment
    
    * CAMEL-19155 - Correct explanatory comment
    
    ---------
    
    Co-authored-by: Dylan Piergies <Dylan.Piergies@tesco.com>

--- components/camel-azure/camel-azure-servicebus/src/main/java/org/apache/camel/component/azure/servicebus/ServiceBusConsumer.java
+++ components/camel-azure/camel-azure-servicebus/src/main/java/org/apache/camel/component/azure/servicebus/ServiceBusConsumer.java
@@ -30,14 +30,13 @@ import org.apache.camel.RuntimeCamelException;
 import org.apache.camel.component.azure.servicebus.client.ServiceBusClientFactory;
 import org.apache.camel.component.azure.servicebus.client.ServiceBusReceiverAsyncClientWrapper;
 import org.apache.camel.component.azure.servicebus.operations.ServiceBusReceiverOperations;
-import org.apache.camel.spi.Synchronization;
 import org.apache.camel.support.DefaultConsumer;
 import org.apache.camel.support.SynchronizationAdapter;
 import org.apache.camel.util.ObjectHelper;
+import reactor.core.scheduler.Schedulers;
 
 public class ServiceBusConsumer extends DefaultConsumer {
 
-    private Synchronization onCompletion;
     private ServiceBusReceiverAsyncClientWrapper clientWrapper;
     private ServiceBusReceiverOperations operations;
 
@@ -55,7 +54,6 @@ public class ServiceBusConsumer extends DefaultConsumer {
     @Override
     protected void doInit() throws Exception {
         super.doInit();
-        onCompletion = new ConsumerOnCompletion();
     }
 
     @Override
@@ -139,7 +137,7 @@ public class ServiceBusConsumer extends DefaultConsumer {
 
     private void onEventListener(final ServiceBusReceivedMessage message) {
         final Exchange exchange = createServiceBusExchange(message);
-
+        final ConsumerOnCompletion onCompletion = new ConsumerOnCompletion(message);
         // add exchange callback
         exchange.adapt(ExtendedExchange.class).addOnCompletion(onCompletion);
         // use default consumer callback
@@ -202,12 +200,28 @@ public class ServiceBusConsumer extends DefaultConsumer {
     }
 
     private class ConsumerOnCompletion extends SynchronizationAdapter {
+        private final ServiceBusReceivedMessage message;
+
+        public ConsumerOnCompletion(ServiceBusReceivedMessage message) {
+            this.message = message;
+        }
+
+        @Override
+        public void onComplete(Exchange exchange) {
+            super.onComplete(exchange);
+            if (!getConfiguration().isDisableAutoComplete()) {
+                clientWrapper.complete(message).subscribeOn(Schedulers.boundedElastic()).subscribe();
+            }
+        }
 
         @Override
         public void onFailure(Exchange exchange) {
             final Exception cause = exchange.getException();
             if (cause != null) {
                 getExceptionHandler().handleException("Error during processing exchange.", exchange, cause);
+            }
+            if (!getConfiguration().isDisableAutoComplete()) {
+                clientWrapper.abandon(message).subscribeOn(Schedulers.boundedElastic()).subscribe();
             }
         }
     }
