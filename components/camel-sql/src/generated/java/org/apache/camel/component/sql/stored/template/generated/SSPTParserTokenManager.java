86f2949b43ccb7b070ff3320a2c6117d887a6d25
dk2k <dk2k@ya.ru>
2023-03-24 22:00:32 +0300

    removed unused label

--- components/camel-sql/src/generated/java/org/apache/camel/component/sql/stored/template/generated/SSPTParserTokenManager.java
+++ components/camel-sql/src/generated/java/org/apache/camel/component/sql/stored/template/generated/SSPTParserTokenManager.java
@@ -508,52 +508,47 @@ public Token getNextToken()
   Token matchedToken;
   int curPos = 0;
 
-  EOFLoop :
-  for (;;)
-  {
-   try
-   {
-      curChar = input_stream.BeginToken();
-   }
-   catch(Exception e)
-   {
-      jjmatchedKind = 0;
-      jjmatchedPos = -1;
-      matchedToken = jjFillToken();
-      return matchedToken;
-   }
-
-   jjmatchedKind = 0x7fffffff;
-   jjmatchedPos = 0;
-   curPos = jjMoveStringLiteralDfa0_0();
-   if (jjmatchedKind != 0x7fffffff)
-   {
-      if (jjmatchedPos + 1 < curPos)
-         input_stream.backup(curPos - jjmatchedPos - 1);
+   for (; ; ) {
+      try {
+         curChar = input_stream.BeginToken();
+      } catch (Exception e) {
+         jjmatchedKind = 0;
+         jjmatchedPos = -1;
          matchedToken = jjFillToken();
          return matchedToken;
-   }
-   int error_line = input_stream.getEndLine();
-   int error_column = input_stream.getEndColumn();
-   String error_after = null;
-   boolean EOFSeen = false;
-   try { input_stream.readChar(); input_stream.backup(1); }
-   catch (java.io.IOException e1) {
-      EOFSeen = true;
-      error_after = curPos <= 1 ? "" : input_stream.GetImage();
-      if (curChar == '\n' || curChar == '\r') {
-         error_line++;
-         error_column = 0;
       }
-      else
-         error_column++;
+
+      jjmatchedKind = 0x7fffffff;
+      jjmatchedPos = 0;
+      curPos = jjMoveStringLiteralDfa0_0();
+      if (jjmatchedKind != 0x7fffffff) {
+         if (jjmatchedPos + 1 < curPos)
+            input_stream.backup(curPos - jjmatchedPos - 1);
+         matchedToken = jjFillToken();
+         return matchedToken;
+      }
+      int error_line = input_stream.getEndLine();
+      int error_column = input_stream.getEndColumn();
+      String error_after = null;
+      boolean EOFSeen = false;
+      try {
+         input_stream.readChar();
+         input_stream.backup(1);
+      } catch (java.io.IOException e1) {
+         EOFSeen = true;
+         error_after = curPos <= 1 ? "" : input_stream.GetImage();
+         if (curChar == '\n' || curChar == '\r') {
+            error_line++;
+            error_column = 0;
+         } else
+            error_column++;
+      }
+      if (!EOFSeen) {
+         input_stream.backup(1);
+         error_after = curPos <= 1 ? "" : input_stream.GetImage();
+      }
+      throw new TokenMgrError(EOFSeen, curLexState, error_line, error_column, error_after, curChar, TokenMgrError.LEXICAL_ERROR);
    }
-   if (!EOFSeen) {
-      input_stream.backup(1);
-      error_after = curPos <= 1 ? "" : input_stream.GetImage();
-   }
-   throw new TokenMgrError(EOFSeen, curLexState, error_line, error_column, error_after, curChar, TokenMgrError.LEXICAL_ERROR);
-  }
 }
 
 void SkipLexicalActions(Token matchedToken)
