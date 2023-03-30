db0c8a8d8be67eddf86a8aac427a9cc8773d5544
davsclaus <davsclaus@users.noreply.github.com>
2023-03-30 07:55:37 +0000

    Regen for commit e4414f26df1f4d6ba6c35d0fa70e763d0f21c1e4
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-api/src/main/java/org/apache/camel/CamelContext.java
+++ core/camel-api/src/main/java/org/apache/camel/CamelContext.java
@@ -599,11 +599,11 @@ public interface CamelContext extends CamelContextLifecycle, RuntimeConfiguratio
      * Camel end users should favour using {@link org.apache.camel.builder.TemplatedRouteBuilder} which is a fluent
      * builder with more functionality than this API.
      *
-     * @param      routeId         the id of the new route to add (optional)
-     * @param      routeTemplateId the id of the route template (mandatory)
-     * @param      parameters      parameters to use for the route template when creating the new route
-     * @return                     the id of the route added (for example when an id was auto assigned)
-     * @throws     Exception       is thrown if error creating and adding the new route
+     * @param  routeId         the id of the new route to add (optional)
+     * @param  routeTemplateId the id of the route template (mandatory)
+     * @param  parameters      parameters to use for the route template when creating the new route
+     * @return                 the id of the route added (for example when an id was auto assigned)
+     * @throws Exception       is thrown if error creating and adding the new route
      */
     String addRouteFromTemplate(String routeId, String routeTemplateId, Map<String, Object> parameters) throws Exception;
 
