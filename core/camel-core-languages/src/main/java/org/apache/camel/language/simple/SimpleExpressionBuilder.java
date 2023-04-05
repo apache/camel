8a502ef171b1297b1a8f9dc84eeb72449dbba5bc
davsclaus <davsclaus@users.noreply.github.com>
2023-04-05 07:44:24 +0000

    Regen for commit 7ca0064683f4063525811083466edc37adc6d5ce
    Signed-off-by: GitHub <noreply@github.com>

--- core/camel-core-languages/src/main/java/org/apache/camel/language/simple/SimpleExpressionBuilder.java
+++ core/camel-core-languages/src/main/java/org/apache/camel/language/simple/SimpleExpressionBuilder.java
@@ -20,7 +20,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
