75fa4ab130ffab71742a7d5c28f0bd3a2fba34a6
Dmitry Kryukov <dk2k@ya.ru>
2023-04-27 12:47:25 +0300

    Fixed checkstyle and source check issue

--- components/camel-as2/camel-as2-component/src/test/java/org/apache/camel/component/as2/MendelsonSslEndpointIT.java
+++ components/camel-as2/camel-as2-component/src/test/java/org/apache/camel/component/as2/MendelsonSslEndpointIT.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.as2;
 
 import java.nio.charset.Charset;
@@ -29,7 +45,6 @@ import org.slf4j.LoggerFactory;
 public class MendelsonSslEndpointIT extends AbstractAS2ITSupport {
 
     private static final Logger LOG = LoggerFactory.getLogger(MendelsonSslEndpointIT.class);
-    private MendelsonCertLoader mendelsonCertLoader;
     private static HostnameVerifier hostnameVerifier;
 
     private static final String[] SIGNED_RECEIPT_MIC_ALGORITHMS = new String[] { "sha1", "md5" };
@@ -60,6 +75,8 @@ public class MendelsonSslEndpointIT extends AbstractAS2ITSupport {
                                               + "MOA+8:525'\n"
                                               + "UNT+23+00000000000117'\n"
                                               + "UNZ+1+00000000000778'\n";
+
+    private MendelsonCertLoader mendelsonCertLoader;
 
     @BeforeAll
     public void setupTest() {
