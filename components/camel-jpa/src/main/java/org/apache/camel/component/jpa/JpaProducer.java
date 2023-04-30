0bc4533bd9c6074d186e2da6f93cdf304bd38da2
jszymanski <jacek.p.szymanski@gmail.com>
2023-04-30 17:56:49 +0200

    support both maximumResults and firstResult in headers

--- components/camel-jpa/src/main/java/org/apache/camel/component/jpa/JpaProducer.java
+++ components/camel-jpa/src/main/java/org/apache/camel/component/jpa/JpaProducer.java
@@ -19,6 +19,7 @@ package org.apache.camel.component.jpa;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -202,11 +203,15 @@ public class JpaProducer extends DefaultProducer {
 
     @SuppressWarnings("unchecked")
     private void configureParameters(Query query, Exchange exchange) {
-        int maxResults = getEndpoint().getMaximumResults();
+        final int maxResults = Objects.requireNonNullElse(
+                exchange.getIn().getHeader(JpaConstants.JPA_MAXIMUM_RESULTS, Integer.class),
+                getEndpoint().getMaximumResults());
         if (maxResults > 0) {
             query.setMaxResults(maxResults);
         }
-        int firstResult = getEndpoint().getFirstResult();
+        final int firstResult = Objects.requireNonNullElse(
+                exchange.getIn().getHeader(JpaConstants.JPA_FIRST_RESULT, Integer.class),
+                getEndpoint().getFirstResult());
         if (firstResult > 0) {
             query.setFirstResult(firstResult);
         }
