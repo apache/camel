b86b0345ef7df383727c23a7e42b20d64e7f8147
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-05-15 17:21:12 +0200

    (chores) camel-nats: prevent flaky tests from running on GH actions

--- components/camel-nats/src/test/java/org/apache/camel/component/nats/integration/NatsConsumerReplyToIT.java
+++ components/camel-nats/src/test/java/org/apache/camel/component/nats/integration/NatsConsumerReplyToIT.java
@@ -21,7 +21,9 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.component.nats.NatsConstants;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
+@DisabledIfSystemProperty(named = "ci.env.name", matches = "github.com", disabledReason = "Flaky on GitHub Actions")
 public class NatsConsumerReplyToIT extends NatsITSupport {
 
     @EndpointInject("mock:result")
