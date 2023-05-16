4e7de2ad9606d3effe0b1d7aeca6439733ab1a76
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-05-16 15:52:52 +0200

    (chores) camel-core: use static inner classes
    This should avoid them keeping a reference to the parent class, which provides a very small memory usage reduction in some cases

--- core/camel-core-model/src/main/java/org/apache/camel/builder/NotifyBuilder.java
+++ core/camel-core-model/src/main/java/org/apache/camel/builder/NotifyBuilder.java
@@ -1474,7 +1474,7 @@ public class NotifyBuilder {
         boolean onExchangeSent(Exchange exchange, Endpoint endpoint, long timeTaken);
     }
 
-    private abstract class EventPredicateSupport implements EventPredicate {
+    private abstract static class EventPredicateSupport implements EventPredicate {
 
         @Override
         public boolean isAbstract() {
