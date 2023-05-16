cc8f924b46fc248be4b4313e5e9d05eeb88cd084
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-05-16 08:45:12 +0200

    (chores) camel-zookeeper-master: fix integration test masquerading as unit test

--- components/camel-zookeeper-master/src/test/java/org/apache/camel/component/zookeepermaster/MasterEndpointIT.java
+++ components/camel-zookeeper-master/src/test/java/org/apache/camel/component/zookeepermaster/MasterEndpointIT.java
@@ -16,30 +16,67 @@
  */
 package org.apache.camel.component.zookeepermaster;
 
+import java.util.List;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.Route;
+import org.apache.camel.component.file.remote.SftpEndpoint;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.component.zookeepermaster.group.ZookeeprContainer;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.test.spring.junit5.CamelSpringTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @CamelSpringTest
 @ContextConfiguration
 @ExtendWith(ZookeeprContainer.class)
-public class MasterQuartzEndpointTest {
+public class MasterEndpointIT {
     @Autowired
     protected CamelContext camelContext;
 
     @EndpointInject("mock:results")
     protected MockEndpoint resultEndpoint;
 
+    @Produce("seda:bar")
+    protected ProducerTemplate template;
+
     @Test
     public void testEndpoint() throws Exception {
-        resultEndpoint.expectedMinimumMessageCount(2);
+        // check the endpoint configuration
+        List<Route> registeredRoutes = camelContext.getRoutes();
+        assertEquals(1, registeredRoutes.size(), "number of routes");
+        MasterEndpoint endpoint = (MasterEndpoint) registeredRoutes.get(0).getEndpoint();
+        assertEquals("seda:bar", endpoint.getConsumerEndpointUri(), "wrong endpoint uri");
+
+        String expectedBody = "<matched/>";
+
+        resultEndpoint.expectedBodiesReceived(expectedBody);
+
+        // lets wait for the entry to be registered...
+        Thread.sleep(5000);
+
+        template.sendBodyAndHeader(expectedBody, "foo", "bar");
 
         MockEndpoint.assertIsSatisfied(camelContext);
+    }
+
+    @Test
+    public void testRawPropertiesOnChild() {
+        final String uri
+                = "zookeeper-master://name:sftp://myhost/inbox?password=RAW(_BEFORE_AMPERSAND_&_AFTER_AMPERSAND_)&username=jdoe";
+
+        DefaultCamelContext ctx = new DefaultCamelContext();
+        MasterEndpoint master = (MasterEndpoint) ctx.getEndpoint(uri);
+        SftpEndpoint sftp = (SftpEndpoint) master.getEndpoint();
+
+        assertEquals("_BEFORE_AMPERSAND_&_AFTER_AMPERSAND_", sftp.getConfiguration().getPassword());
     }
 }
