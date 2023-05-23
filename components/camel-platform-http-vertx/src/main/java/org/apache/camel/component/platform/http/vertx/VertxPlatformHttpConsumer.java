28b2cd38dd5f0b74c1066c856bff704b31075408
Claus Ibsen <claus.ibsen@gmail.com>
2023-05-23 09:19:07 +0200

    CAMEL-19377: camel-platform-http-vertx - When suspended then return 503 like the other http components

--- components/camel-platform-http-vertx/src/main/java/org/apache/camel/component/platform/http/vertx/VertxPlatformHttpConsumer.java
+++ components/camel-platform-http-vertx/src/main/java/org/apache/camel/component/platform/http/vertx/VertxPlatformHttpConsumer.java
@@ -43,6 +43,8 @@ import org.apache.camel.ExchangePropertyKey;
 import org.apache.camel.ExtendedExchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.camel.Suspendable;
+import org.apache.camel.SuspendableService;
 import org.apache.camel.attachment.AttachmentMessage;
 import org.apache.camel.attachment.CamelFileDataSource;
 import org.apache.camel.component.platform.http.PlatformHttpEndpoint;
@@ -61,7 +63,7 @@ import static org.apache.camel.component.platform.http.vertx.VertxPlatformHttpSu
  * A {@link org.apache.camel.Consumer} for the {@link org.apache.camel.component.platform.http.spi.PlatformHttpEngine}
  * based on Vert.x Web.
  */
-public class VertxPlatformHttpConsumer extends DefaultConsumer {
+public class VertxPlatformHttpConsumer extends DefaultConsumer implements Suspendable, SuspendableService {
     private static final Logger LOGGER = LoggerFactory.getLogger(VertxPlatformHttpConsumer.class);
     private static final Pattern PATH_PARAMETER_PATTERN = Pattern.compile("\\{([^/}]+)\\}");
 
@@ -143,22 +145,6 @@ public class VertxPlatformHttpConsumer extends DefaultConsumer {
         super.doStop();
     }
 
-    @Override
-    protected void doSuspend() throws Exception {
-        if (route != null) {
-            route.disable();
-        }
-        super.doSuspend();
-    }
-
-    @Override
-    protected void doResume() throws Exception {
-        if (route != null) {
-            route.enable();
-        }
-        super.doResume();
-    }
-
     private String configureEndpointPath(PlatformHttpEndpoint endpoint) {
         String path = endpoint.getPath();
         if (endpoint.isMatchOnUriPrefix() && !path.endsWith("*")) {
@@ -169,6 +155,12 @@ public class VertxPlatformHttpConsumer extends DefaultConsumer {
     }
 
     protected void handleRequest(RoutingContext ctx) {
+        if (isSuspended()) {
+            ctx.response().setStatusCode(503);
+            ctx.end();
+            return;
+        }
+
         final Vertx vertx = ctx.vertx();
         final Exchange exchange = toExchange(ctx);
 
