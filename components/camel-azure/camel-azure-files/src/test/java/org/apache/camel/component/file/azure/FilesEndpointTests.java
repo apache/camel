ad6aaa85c419f3804c2358fb673e162ac97191da
Petr Kužel <PetrKuzel@Eurofins.com>
2023-05-24 16:18:29 +0200

    CAMEL-19279: component/marker file name and content matters
    component/azure-file was created manually with content below:
    
    # Generated by camel build tools - do NOT edit this file!
    class=org.apache.camel.component.file.azure.FilesComponent

--- components/camel-azure/camel-azure-files/src/test/java/org/apache/camel/component/file/azure/FilesEndpointTests.java
+++ components/camel-azure/camel-azure-files/src/test/java/org/apache/camel/component/file/azure/FilesEndpointTests.java
@@ -10,25 +10,25 @@ public class FilesEndpointTests extends CamelTestSupport {
 
   @Test
   void hostForValidURIShouldBeExtracted() {
-    FilesEndpoint<?> endpoint = context.getEndpoint("azure-files://host/share", FilesEndpoint.class);
+    var endpoint = context.getEndpoint("azure-files://host/share", FilesEndpoint.class);
     assertEquals("host", endpoint.filesHost());
   }
 
   @Test
   void hostForInvalidURIShouldBeNull() {
     // intentionally missing ://
-    FilesEndpoint<?> endpoint = context.getEndpoint("azure-files:host/share", FilesEndpoint.class);
-    assertNull(endpoint.filesHost());
+    var endpoint = context.getEndpoint("azure-files:host/share", FilesEndpoint.class);
+    assertNull(endpoint.filesHost()); // TODO Camel added missing // , acceptable?
   }
 
   @Test
   void sasTokenForCopyPastedURIShouldBePreserved() {
     // it calls SAS setters on endpoint
-    FilesEndpoint<?> endpoint = context.getEndpoint(
+    var endpoint = context.getEndpoint(
         "azure-files:host/share?sv=2021-12-02&ss=f&srt=sco&sp=rwdlc&se=2023-05-05T16:14:44Z&st=2023-04-28T08:14:44Z&spr=https&sig=95ZxXN3ST033Z4ym7quRqUUs2hjAtx63MAubaMKTyTg%3D", FilesEndpoint.class);
     assertEquals(
         "sv=2021-12-02&ss=f&srt=sco&sp=rwdlc&se=2023-05-05T16:14:44Z&st=2023-04-28T08:14:44Z&spr=https&sig=95ZxXN3ST033Z4ym7quRqUUs2hjAtx63MAubaMKTyTg%3D",
-        endpoint.token());
+        endpoint.token()); // TODO Camel decoded trailing %3D to = , acceptable?
   }
 
 }
