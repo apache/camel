5116ed7af5e85a9667bcf39787891dbe8fece468
davsclaus <davsclaus@users.noreply.github.com>
2023-05-29 14:28:40 +0000

    Regen for commit ef05846b0db52d27626c38eb20590aa3d9b6425f
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/CamelCommand.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/CamelCommand.java
@@ -79,12 +79,12 @@ public abstract class CamelCommand implements Callable<Integer> {
                 var provider = spec.defaultValueProvider();
                 String defaultValue = provider != null ? provider.defaultValue(argSpec) : null;
                 if (defaultValue != null &&
-                    argSpec instanceof CommandLine.Model.OptionSpec) {
+                        argSpec instanceof CommandLine.Model.OptionSpec) {
                     CommandLine.Model.OptionSpec optionSpec = (CommandLine.Model.OptionSpec) argSpec;
                     for (String name : optionSpec.names()) {
                         String placeholder = "$" + StringHelper.after(name, "--");
                         if (argSpec.getValue() != null &&
-                            argSpec.getValue().toString().contains(placeholder)) {
+                                argSpec.getValue().toString().contains(placeholder)) {
                             argSpec.setValue(argSpec.getValue().toString().replace(placeholder, defaultValue));
                         }
                     }
