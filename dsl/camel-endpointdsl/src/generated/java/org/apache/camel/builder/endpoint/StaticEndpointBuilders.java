9f7a3afdf8289d27e3ebcd2a2701c37cf3e14ada
davsclaus <davsclaus@users.noreply.github.com>
2023-06-09 14:27:33 +0000

    Regen for commit 5af948bfd656432145a9af36d1619d49a7585e37
    Signed-off-by: GitHub <noreply@github.com>

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/StaticEndpointBuilders.java
@@ -14102,10 +14102,6 @@ public class StaticEndpointBuilders {
      * Path parameter: webServiceEndpointUri
      * The default Web Service endpoint uri to use for the producer.
      * 
-     * Path parameter: expression
-     * The XPath expression to use when option type=xpathresult. Then this
-     * option is required to be configured.
-     * 
      * @param path type:lookupKey:webServiceEndpointUri
      * @return the dsl builder
      */
@@ -14144,10 +14140,6 @@ public class StaticEndpointBuilders {
      * 
      * Path parameter: webServiceEndpointUri
      * The default Web Service endpoint uri to use for the producer.
-     * 
-     * Path parameter: expression
-     * The XPath expression to use when option type=xpathresult. Then this
-     * option is required to be configured.
      * 
      * @param componentName to use a custom component name for the endpoint
      * instead of the default name
