9eecd9e13a761876e98a2e343841ac1b56b3e6a2
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-06-09 11:53:16 +0200

    camel-minio: use StandardCharsets for instead of resolving charsets dynamically

--- components/camel-minio/src/test/java/org/apache/camel/component/minio/integration/MinioObjectRangeOperationIT.java
+++ components/camel-minio/src/test/java/org/apache/camel/component/minio/integration/MinioObjectRangeOperationIT.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import io.minio.MinioClient;
@@ -98,7 +97,7 @@ class MinioObjectRangeOperationIT extends MinioIntegrationTestSupport {
     private String readInputStream(InputStream minioObject) throws IOException {
         StringBuilder textBuilder = new StringBuilder();
         try (Reader reader
-                = new BufferedReader(new InputStreamReader(minioObject, Charset.forName(StandardCharsets.UTF_8.name())))) {
+                = new BufferedReader(new InputStreamReader(minioObject, StandardCharsets.UTF_8))) {
             int c;
             while ((c = reader.read()) != -1) {
                 textBuilder.append((char) c);
