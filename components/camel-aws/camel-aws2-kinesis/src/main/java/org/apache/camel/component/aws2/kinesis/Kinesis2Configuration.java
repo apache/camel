860fb543f2261e2be79951b4b070c21f9e11f6a1
Andrea Cosentino <ancosen@gmail.com>
2023-06-16 10:14:01 +0200

    CAMEL-19159 - Camel-AWS: Support Profile Credential provider as configuration - AWS Kinesis
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-aws/camel-aws2-kinesis/src/main/java/org/apache/camel/component/aws2/kinesis/Kinesis2Configuration.java
+++ components/camel-aws/camel-aws2-kinesis/src/main/java/org/apache/camel/component/aws2/kinesis/Kinesis2Configuration.java
@@ -82,6 +82,13 @@ public class Kinesis2Configuration implements Cloneable {
                             +
                             "static credentials to be passed in.")
     private boolean useDefaultCredentialsProvider;
+    @UriParam(label = "common", defaultValue = "false",
+              description = "Set whether the Kinesis client should expect to load credentials through a profile credentials provider.")
+    private boolean useProfileCredentialsProvider;
+    @UriParam(label = "common",
+              description =  "If using a profile credentials provider this parameter will set the profile name.")
+    private String profileCredentialsName;
+
 
     public KinesisClient getAmazonKinesisClient() {
         return amazonKinesisClient;
@@ -227,6 +234,21 @@ public class Kinesis2Configuration implements Cloneable {
         this.useDefaultCredentialsProvider = useDefaultCredentialsProvider;
     }
 
+    public boolean isUseProfileCredentialsProvider() {
+        return useProfileCredentialsProvider;
+    }
+
+    public void setUseProfileCredentialsProvider(boolean useProfileCredentialsProvider) {
+        this.useProfileCredentialsProvider = useProfileCredentialsProvider;
+    }
+
+    public String getProfileCredentialsName() {
+        return profileCredentialsName;
+    }
+
+    public void setProfileCredentialsName(String profileCredentialsName) {
+        this.profileCredentialsName = profileCredentialsName;
+    }
     // *************************************************
     //
     // *************************************************
