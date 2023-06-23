4f1444555231aef6559e6ecc1cff293b11943c38
Andrea Cosentino <ancosen@gmail.com>
2023-06-23 11:31:18 +0200

    CAMEL-19159 - Camel-AWS: Support Profile Credential provider as configuration - Cloudtrail
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-aws/camel-aws-cloudtrail/src/main/java/org/apache/camel/component/aws/cloudtrail/CloudtrailConfiguration.java
+++ components/camel-aws/camel-aws-cloudtrail/src/main/java/org/apache/camel/component/aws/cloudtrail/CloudtrailConfiguration.java
@@ -66,6 +66,12 @@ public class CloudtrailConfiguration implements Cloneable {
                             +
                             "static credentials to be passed in.")
     private boolean useDefaultCredentialsProvider;
+    @UriParam(defaultValue = "false",
+              description = "Set whether the Cloudtrail client should expect to load credentials through a profile credentials provider.")
+    private boolean useProfileCredentialsProvider;
+    @UriParam(
+            description = "If using a profile credentials provider this parameter will set the profile name.")
+    private String profileCredentialsName;
 
     public String getLabel() {
         return label;
@@ -179,6 +185,21 @@ public class CloudtrailConfiguration implements Cloneable {
         this.eventSource = eventSource;
     }
 
+    public boolean isUseProfileCredentialsProvider() {
+        return useProfileCredentialsProvider;
+    }
+
+    public void setUseProfileCredentialsProvider(boolean useProfileCredentialsProvider) {
+        this.useProfileCredentialsProvider = useProfileCredentialsProvider;
+    }
+
+    public String getProfileCredentialsName() {
+        return profileCredentialsName;
+    }
+
+    public void setProfileCredentialsName(String profileCredentialsName) {
+        this.profileCredentialsName = profileCredentialsName;
+    }
     // *************************************************
     //
     // *************************************************
