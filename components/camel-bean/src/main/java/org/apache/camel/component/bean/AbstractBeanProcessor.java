apache/camel

commit 793e798f464572ae754fb99a9acc7d7bf28ae1b7

(chores) camel-bean: do not catch Throwable

components/camel-bean/src/main/java/org/apache/camel/component/bean/AbstractBeanProcessor.java
@@ -106,7 +106,7 @@ public abstract class AbstractBeanProcessor extends AsyncProcessorSupport {
                 }
                 try {
                     target.process(exchange);
-                } catch (Throwable e) {
+                } catch (Exception e) {
                     exchange.setException(e);
                 }
                 callback.done(true);

commit 262a7f7bffa0705bc6d99bd834520b39992b15d7

(chores) camel-bean: do catch AssertionErrors
Fix a bug introduced by 793e798f464572ae754fb99a9acc7d7bf28ae1b7, since this code does need to catch AssertionErrors.

components/camel-bean/src/main/java/org/apache/camel/component/bean/AbstractBeanProcessor.java
@@ -106,7 +106,7 @@ public abstract class AbstractBeanProcessor extends AsyncProcessorSupport {
                 }
                 try {
                     target.process(exchange);
-                } catch (Exception e) {
+                } catch (AssertionError | Exception e) {
                     exchange.setException(e);
                 }
                 callback.done(true);