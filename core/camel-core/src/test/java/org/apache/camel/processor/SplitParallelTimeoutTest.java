41e0df411bf80104f10929d8d80392564cbcaf72
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-06-26 14:50:15 +0200

    (chores) ci: temporarily disabled unstable test

--- core/camel-core/src/test/java/org/apache/camel/processor/SplitParallelTimeoutTest.java
+++ core/camel-core/src/test/java/org/apache/camel/processor/SplitParallelTimeoutTest.java
@@ -23,12 +23,14 @@ import org.apache.camel.ContextTestSupport;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Disabled("Still blocking on some environments")
 public class SplitParallelTimeoutTest extends ContextTestSupport {
 
     private volatile Exchange receivedExchange;
