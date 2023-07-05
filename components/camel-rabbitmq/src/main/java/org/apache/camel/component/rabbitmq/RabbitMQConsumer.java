44777e57118cd50e0ea34ef25a8f763904ee7534
nkg447 <nikunj.gupta447@gmail.com>
2023-07-05 14:36:28 +0530

    close all consumers concurrently Closing all consumers in RabbitMqConsumer concurrently

--- components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQConsumer.java
+++ components/camel-rabbitmq/src/main/java/org/apache/camel/component/rabbitmq/RabbitMQConsumer.java
@@ -155,13 +155,13 @@ public class RabbitMQConsumer extends DefaultConsumer implements Suspendable {
         if (startConsumerCallable != null) {
             startConsumerCallable.stop();
         }
-        for (RabbitConsumer consumer : this.consumers) {
+        this.consumers.parallelStream().forEach(consumer -> {
             try {
                 ServiceHelper.stopAndShutdownService(consumer);
             } catch (Exception e) {
                 LOG.warn("Error occurred while stopping consumer. This exception is ignored", e);
             }
-        }
+        });
         this.consumers.clear();
         if (conn != null) {
             LOG.debug("Closing connection: {} with timeout: {} ms.", conn, closeTimeout);
