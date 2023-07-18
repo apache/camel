99b0134684548c775c27ba1870084d437c7ce15b
Croway <federico.mariani.1990@gmail.com>
2023-07-18 12:07:26 +0200

    CAMEL-19610: Upgrade to RocketMQ v5

---
+++ test-infra/camel-test-infra-rocketmq/src/test/java/org/apache/camel/test/infra/rocketmq/services/RocketMQService.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.test.infra.rocketmq.services;
+
+import java.io.IOException;
+
+import org.apache.camel.test.infra.common.services.TestService;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+
+public interface RocketMQService extends TestService, BeforeTestExecutionCallback, AfterTestExecutionCallback {
+    String nameserverAddress();
+
+    default String defaultCluster() {
+        return "DefaultCluster";
+    }
+
+    void createTopic(String topic);
+
+    void deleteTopic(String topic) throws IOException, InterruptedException;
+}
