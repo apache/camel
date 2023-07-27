733979e313d07a3abbc14531a4871b29f7b0b591
Claus Ibsen <claus.ibsen@gmail.com>
2023-07-27 06:16:43 +0200

    CAMEL-18234: Exception building aws2-sqs route when certificate valid… (#10837)
    CAMEL-18234: Exception building aws2-sqs route when certificate validation is disabled and a proxy is configured

--- components/camel-aws/camel-aws2-ses/src/main/java/org/apache/camel/component/aws2/ses/client/impl/Ses2ClientStandardImpl.java
+++ components/camel-aws/camel-aws2-ses/src/main/java/org/apache/camel/component/aws2/ses/client/impl/Ses2ClientStandardImpl.java
@@ -90,13 +90,18 @@ public class Ses2ClientStandardImpl implements Ses2InternalClient {
             clientBuilder.endpointOverride(URI.create(configuration.getUriEndpointOverride()));
         }
         if (configuration.isTrustAllCertificates()) {
-            SdkHttpClient ahc = ApacheHttpClient.builder().buildWithDefaults(AttributeMap
+            if (httpClientBuilder == null) {
+                httpClientBuilder = ApacheHttpClient.builder();
+            }
+            SdkHttpClient ahc = httpClientBuilder.buildWithDefaults(AttributeMap
                     .builder()
                     .put(
                             SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES,
                             Boolean.TRUE)
                     .build());
+            // set created http client to use instead of builder
             clientBuilder.httpClient(ahc);
+            clientBuilder.httpClientBuilder(null);
         }
         client = clientBuilder.build();
         return client;
