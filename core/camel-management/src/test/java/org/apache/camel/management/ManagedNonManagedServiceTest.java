50d3e8aa0acd5fc61e8f0f2f9c53c8917f523aa2
Claus Ibsen <claus.ibsen@gmail.com>
2023-08-15 13:11:32 +0200

    CAMEL-19740: camel-stub - Add dev console

--- core/camel-management/src/test/java/org/apache/camel/management/ManagedNonManagedServiceTest.java
+++ core/camel-management/src/test/java/org/apache/camel/management/ManagedNonManagedServiceTest.java
@@ -34,7 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisabledOnOs(OS.AIX)
 public class ManagedNonManagedServiceTest extends ManagementTestSupport {
 
-    private static final int SERVICES = 14;
+    private static final int SERVICES = 15;
 
     @Test
     public void testService() throws Exception {
