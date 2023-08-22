bb7e6016ab95280f0535f6326801c1657242db3a
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2023-08-22 17:29:04 +0200

    (chores) checkstyle: removed unused import (#11172)

--- core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/AbstractCamelContext.java
+++ core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/AbstractCamelContext.java
@@ -31,7 +31,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
