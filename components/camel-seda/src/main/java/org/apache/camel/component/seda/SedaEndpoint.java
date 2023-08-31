d66469d6ac7243f0ae790c41a500965b885d3180
Otavio Rodolfo Piske <orpiske@users.noreply.github.com>
2023-08-31 13:17:32 +0200

    CAMEL-19815: avoid using invalid QueueReferences (#11253)
    This partially reverts the changes introduced on 23542fc5b65f33cde0f74f71876fe80d8c8c853f and b70584ddb3545a87acc1732f0e89b9cea704c461

--- components/camel-seda/src/main/java/org/apache/camel/component/seda/SedaEndpoint.java
+++ components/camel-seda/src/main/java/org/apache/camel/component/seda/SedaEndpoint.java
@@ -101,7 +101,6 @@ public class SedaEndpoint extends DefaultEndpoint implements AsyncEndpoint, Brow
     private boolean discardIfNoConsumers;
 
     private BlockingQueueFactory<Exchange> queueFactory;
-    private volatile QueueReference ref;
 
     public SedaEndpoint() {
         queueFactory = new LinkedBlockingQueueFactory<>();
@@ -215,22 +214,13 @@ public class SedaEndpoint extends DefaultEndpoint implements AsyncEndpoint, Brow
      * @return the reference, or <tt>null</tt> if no queue reference exists.
      */
     public QueueReference getQueueReference() {
-        if (ref == null) {
-            ref = tryQueueRefInit();
-        }
+        String key = getComponent().getQueueKey(getEndpointUri());
 
-        return ref;
+        synchronized (this) {
+            return getComponent().getQueueReference(key);
+        }
     }
 
-    private QueueReference tryQueueRefInit() {
-        final SedaComponent component = getComponent();
-        if (component != null) {
-            final String key = component.getQueueKey(getEndpointUri());
-            return component.getQueueReference(key);
-        }
-
-        return null;
-    }
 
     protected synchronized AsyncProcessor getConsumerMulticastProcessor() {
         if (!multicastStarted && consumerMulticastProcessor != null) {
@@ -557,8 +547,6 @@ public class SedaEndpoint extends DefaultEndpoint implements AsyncEndpoint, Brow
         if (queue == null) {
             queue = getQueue();
         }
-
-        ref = tryQueueRefInit();
     }
 
     @Override
@@ -568,8 +556,6 @@ public class SedaEndpoint extends DefaultEndpoint implements AsyncEndpoint, Brow
         } else {
             LOG.debug("There is still active consumers.");
         }
-
-        ref = null;
     }
 
     @Override
@@ -601,7 +587,8 @@ public class SedaEndpoint extends DefaultEndpoint implements AsyncEndpoint, Brow
 
         // clear queue, as we are shutdown, so if re-created then the queue must be updated
         queue = null;
-        ref = null;
     }
+
+
 
 }
