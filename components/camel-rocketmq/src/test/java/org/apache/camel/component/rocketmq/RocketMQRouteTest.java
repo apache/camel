9d9311c937bc1e2fd206136a1ae17b6539924fbb
Otavio Rodolfo Piske <angusyoung@gmail.com>
2023-09-05 13:39:37 +0200

    CAMEL-19832: disable camel-rocketmq tests because they are unreliable

--- components/camel-rocketmq/src/test/java/org/apache/camel/component/rocketmq/RocketMQRouteTest.java
+++ components/camel-rocketmq/src/test/java/org/apache/camel/component/rocketmq/RocketMQRouteTest.java
@@ -28,7 +28,10 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
+@DisabledIfSystemProperty(named = "ci.env.name", matches = "apache.org",
+        disabledReason = "These tests are flaky on Apache CI - see CAMEL-19832")
 public class RocketMQRouteTest extends RocketMQTestSupport {
 
     public static final String EXPECTED_MESSAGE = "hello, RocketMQ.";
