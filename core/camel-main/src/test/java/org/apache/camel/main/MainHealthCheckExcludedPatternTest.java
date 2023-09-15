4fac2bbf2cb771b22d43e32317d8d9fb536a5e76
Andrea Cosentino <ancosen@gmail.com>
2023-09-15 10:02:19 +0200

    Upgrade AWS SDK v2 to version 2.20.148 (#11405)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- core/camel-main/src/test/java/org/apache/camel/main/MainHealthCheckExcludedPatternTest.java
+++ core/camel-main/src/test/java/org/apache/camel/main/MainHealthCheckExcludedPatternTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
