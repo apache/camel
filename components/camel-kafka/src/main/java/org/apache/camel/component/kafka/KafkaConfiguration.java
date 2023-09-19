760e298c9b8a89b92f50f6a8c7113974baa7e6db
essobedo <essobedo@users.noreply.github.com>
2023-09-19 17:27:42 +0000

    Regen for commit fb3835dadc1f7a16dd1a6739cc31700ef3033174
    Signed-off-by: GitHub <noreply@github.com>

--- components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaConfiguration.java
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaConfiguration.java
@@ -1894,15 +1894,4 @@ public class KafkaConfiguration implements Cloneable, HeaderFilterStrategyAware 
     public void setIsolationLevel(String isolationLevel) {
         this.isolationLevel = isolationLevel;
     }
-
-    public String getKerberosConfigLocation() {
-        return kerberosConfigLocation;
-    }
-
-    /**
-     * Location of the kerberos config file.
-     */
-    public void setKerberosConfigLocation(String kerberosConfigLocation) {
-        this.kerberosConfigLocation = kerberosConfigLocation;
-    }
 }
