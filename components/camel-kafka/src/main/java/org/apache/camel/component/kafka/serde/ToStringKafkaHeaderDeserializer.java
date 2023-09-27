0a05658f24794bc41c7b3c8a1b2fc6cf8d8c5230
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2023-09-27 13:36:35 +0200

    [CAMEL-19919] camel-kafka: provided an out of the box byte[] to String header deserializer (#11562)

---
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/serde/ToStringKafkaHeaderDeserializer.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.kafka.serde;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class ToStringKafkaHeaderDeserializer implements KafkaHeaderDeserializer {
+    private Charset charset;
+
+    public ToStringKafkaHeaderDeserializer() {
+        this(StandardCharsets.UTF_8);
+    }
+
+    public ToStringKafkaHeaderDeserializer(Charset charset) {
+        this.charset = Objects.requireNonNull(charset);
+    }
+
+    public Charset getCharset() {
+        return charset;
+    }
+
+    public void setCharset(Charset charset) {
+        this.charset = Objects.requireNonNull(charset);
+    }
+
+    @Override
+    public Object deserialize(String key, byte[] value) {
+        return new String(value, this.charset);
+    }
+}
