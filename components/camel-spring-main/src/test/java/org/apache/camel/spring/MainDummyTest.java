e9d644f8f560c2beda0d8ea974b92dcfa8929e78
Claus Ibsen <claus.ibsen@gmail.com>
2023-10-05 16:02:44 +0200

    Regen

--- components/camel-spring-main/src/test/java/org/apache/camel/spring/MainDummyTest.java
+++ components/camel-spring-main/src/test/java/org/apache/camel/spring/MainDummyTest.java
@@ -20,8 +20,8 @@ import org.apache.camel.CamelContext;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MainDummyTest {
 
