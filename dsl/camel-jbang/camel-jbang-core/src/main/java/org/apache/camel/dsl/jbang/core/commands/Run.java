43f944a248a27bc53c0c812671c83623e3cf4474
Aurélien Pupier <apupier@redhat.com>
2023-10-10 10:55:31 +0200

    CAMEL-19970 - Avoid IllegalArgumentException for file without extension in same folder
    when using JBang and that we let auto-detect routes, the files without
    extensions were added as potential routes and, as expected, no
    RouteLoader were found. An IllegalArgumentExeption was thrown at this
    time.
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/Run.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/Run.java
@@ -1038,6 +1038,10 @@ public class Run extends CamelCommand {
         if (f.exists() && f.isDirectory()) {
             return true;
         }
+        
+        if (FileUtil.onlyExt(name) == null) {
+            return true;
+        }
 
         String on = FileUtil.onlyName(name, true);
         on = on.toLowerCase(Locale.ROOT);
