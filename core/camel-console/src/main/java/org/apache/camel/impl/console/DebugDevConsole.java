d3302b3d1863fc362f022a8ab473841e13fc7b9f
Claus Ibsen <claus.ibsen@gmail.com>
2023-10-31 14:21:39 +0100

    camel-jbang - debug command WIP

--- core/camel-console/src/main/java/org/apache/camel/impl/console/DebugDevConsole.java
+++ core/camel-console/src/main/java/org/apache/camel/impl/console/DebugDevConsole.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.debug;
+package org.apache.camel.impl.console;
 
 import java.io.LineNumberReader;
 import java.util.ArrayList;
