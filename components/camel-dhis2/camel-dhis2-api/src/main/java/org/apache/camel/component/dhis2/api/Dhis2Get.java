38e5226af7587d394f3590672f6ad862f6844b14
Claude Mamo <823038+claudemamo@users.noreply.github.com>
2023-11-03 14:54:19 +0100

    chore: backport fixes and other changes from camel-3.x branch (#11901)

--- components/camel-dhis2/camel-dhis2-api/src/main/java/org/apache/camel/component/dhis2/api/Dhis2Get.java
+++ components/camel-dhis2/camel-dhis2-api/src/main/java/org/apache/camel/component/dhis2/api/Dhis2Get.java
@@ -75,32 +75,20 @@ public class Dhis2Get {
         return getOperation;
     }
 
-    public <T> Iterator<T> collection(
-            String path, String itemType, Boolean paging, String fields, String filter, RootJunctionEnum rootJunction,
+    public Iterator<Dhis2Resource> collection(
+            String path, String arrayName, Boolean paging, String fields, String filter,
+            RootJunctionEnum rootJunction,
             Map<String, Object> queryParams) {
         GetOperation getOperation = newGetOperation(path, fields, filter, rootJunction, queryParams);
-        Iterable<T> iterable;
 
         IterableDhis2Response iteratorDhis2Response;
-        if (paging == null || paging) {
+        if (paging != null && paging) {
             iteratorDhis2Response = getOperation.withPaging().transfer();
         } else {
             iteratorDhis2Response = getOperation.withoutPaging().transfer();
         }
 
-        if (itemType == null) {
-            iterable = (Iterable<T>) iteratorDhis2Response
-                    .returnAs(Map.class, path);
-        } else {
-            try {
-                iterable = (Iterable<T>) iteratorDhis2Response
-                        .returnAs(Class.forName(itemType), path);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        return iterable.iterator();
+        return iteratorDhis2Response.returnAs(Dhis2Resource.class, arrayName).iterator();
     }
 
 }
