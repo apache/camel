be475bb71d7b8e6ae3b2c384391342c81c5c22e5
Claus Ibsen <claus.ibsen@gmail.com>
2023-11-13 13:53:12 +0100

    CAMEL-20094: camel-catalog: camel-spring.xsd keeps being regenerated. (#11979)

--- tooling/camel-tooling-util/src/main/java/org/apache/camel/tooling/util/PackageHelper.java
+++ tooling/camel-tooling-util/src/main/java/org/apache/camel/tooling/util/PackageHelper.java
@@ -24,6 +24,7 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
@@ -89,8 +90,12 @@ public final class PackageHelper {
     }
 
     public static Set<File> findJsonFiles(File rootDir, Set<File> files) {
+        return findJsonFiles(rootDir, files, (f) -> true);
+    }
+
+    public static Set<File> findJsonFiles(File rootDir, Set<File> files, Predicate<File> filter) {
         try (Stream<Path> stream = findJsonFiles(rootDir.toPath())) {
-            stream.forEach(p -> files.add(p.toFile()));
+            stream.map(Path::toFile).filter(filter).forEach(files::add);
         }
         return files;
     }
