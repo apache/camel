6f7f1044cd361fa6d72f8df459fd28ba534e22b6
Mike Barlotta <CodeSmell@users.noreply.github.com>
2023-11-20 11:12:41 -0500

    CAMEL-20044: backport breakOnFirstError Fix to Camel 4 (#12004)
    * add extra logging for BreakOnFirstError (#11920)
    * fix to handle commit on breakOnFirstError (#11959)
    * add breakOnFirstError to 4.3 upgrade guide
    * update tests to new approach
    * changes after doing local build
    * CAMEL-20044: fixed back-ported tests to work on Camel 4
    * change kafka group in tests
    
    ---------
    
    Co-authored-by: Otavio Rodolfo Piske <angusyoung@gmail.com>

--- components/camel-kafka/src/main/java/org/apache/camel/component/kafka/consumer/support/ProcessingResult.java
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/consumer/support/ProcessingResult.java
@@ -21,14 +21,19 @@ import org.apache.camel.component.kafka.consumer.AbstractCommitManager;
 
 public final class ProcessingResult {
     private static final ProcessingResult UNPROCESSED_RESULT
-            = new ProcessingResult(false, AbstractCommitManager.START_OFFSET, false);
+            = new ProcessingResult(
+                    false,
+                    AbstractCommitManager.NON_PARTITION,
+                    AbstractCommitManager.START_OFFSET, false);
 
     private final boolean breakOnErrorHit;
+    private final long lastPartition;
     private final long partitionLastOffset;
     private final boolean failed;
 
-    ProcessingResult(boolean breakOnErrorHit, long partitionLastOffset, boolean failed) {
+    ProcessingResult(boolean breakOnErrorHit, long lastPartition, long partitionLastOffset, boolean failed) {
         this.breakOnErrorHit = breakOnErrorHit;
+        this.lastPartition = lastPartition;
         this.partitionLastOffset = partitionLastOffset;
         this.failed = failed;
     }
@@ -39,6 +44,10 @@ public final class ProcessingResult {
 
     public long getPartitionLastOffset() {
         return partitionLastOffset;
+    }
+
+    public long getPartition() {
+        return lastPartition;
     }
 
     public boolean isFailed() {
