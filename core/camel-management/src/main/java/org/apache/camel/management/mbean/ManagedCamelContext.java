e712dd0350e237828dcff18059e9770dbeae9013
Claus Ibsen <claus.ibsen@gmail.com>
2023-11-23 15:57:09 +0100

    CAMEL-20140: Fix camel-report:route-coverage to be able to output coverage data (#12184)

--- core/camel-management/src/main/java/org/apache/camel/management/mbean/ManagedCamelContext.java
+++ core/camel-management/src/main/java/org/apache/camel/management/mbean/ManagedCamelContext.java
@@ -678,7 +678,7 @@ public class ManagedCamelContext extends ManagedPerformanceCounter implements Ti
                         getExchangesTotal(), getTotalProcessingTime()))
                 .append(">\n");
 
-        String xml = dumpRoutesAsXml();
+        String xml = dumpRoutesAsXml(false, false);
         if (xml != null) {
             // use the coverage xml parser to dump the routes and enrich with coverage stats
             Document dom = RouteCoverageXmlParser.parseXml(context, new ByteArrayInputStream(xml.getBytes()));
