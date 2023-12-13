096329ddfa893e47d74413ca54767abee27e62b5
Claus Ibsen <claus.ibsen@gmail.com>
2023-12-13 14:10:28 +0100

    CAMEL-20232: camel-core - Kamelets with Enrich and PollEnrich dynamic endpoints with template parameters (#12443)

--- core/camel-core-reifier/src/main/java/org/apache/camel/reifier/PollEnrichReifier.java
+++ core/camel-core-reifier/src/main/java/org/apache/camel/reifier/PollEnrichReifier.java
@@ -23,9 +23,12 @@ import org.apache.camel.Processor;
 import org.apache.camel.Route;
 import org.apache.camel.model.PollEnrichDefinition;
 import org.apache.camel.model.ProcessorDefinition;
+import org.apache.camel.model.ProcessorDefinitionHelper;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.model.language.ConstantExpression;
 import org.apache.camel.processor.PollEnricher;
 import org.apache.camel.support.DefaultExchange;
+import org.apache.camel.support.EndpointHelper;
 
 public class PollEnrichReifier extends ProcessorReifier<PollEnrichDefinition> {
 
@@ -35,23 +38,27 @@ public class PollEnrichReifier extends ProcessorReifier<PollEnrichDefinition> {
 
     @Override
     public Processor createProcessor() throws Exception {
-        // if no timeout then we should block, and there use a negative timeout
-        long time = parseDuration(definition.getTimeout(), -1);
-        boolean isIgnoreInvalidEndpoint = parseBoolean(definition.getIgnoreInvalidEndpoint(), false);
-        boolean isAggregateOnException = parseBoolean(definition.getAggregateOnException(), false);
-
-        PollEnricher enricher;
+        Expression exp;
+        String uri;
         if (definition.getExpression() instanceof ConstantExpression) {
-            Expression exp = createExpression(definition.getExpression());
+            exp = createExpression(definition.getExpression());
             Exchange ex = new DefaultExchange(camelContext);
-            String uri = exp.evaluate(ex, String.class);
-            enricher = new PollEnricher(uri, time);
+            uri = exp.evaluate(ex, String.class);
         } else {
-            Expression exp = createExpression(definition.getExpression());
-            String uri = definition.getExpression().getExpression();
-            enricher = new PollEnricher(exp, uri, time);
+            exp = createExpression(definition.getExpression());
+            uri = definition.getExpression().getExpression();
         }
 
+        // route templates should pre parse uri as they have dynamic values as part of their template parameters
+        RouteDefinition rd = ProcessorDefinitionHelper.getRoute(definition);
+        if (rd != null && rd.isTemplate() != null && rd.isTemplate()) {
+            uri = EndpointHelper.resolveEndpointUriPropertyPlaceholders(camelContext, uri);
+        }
+
+        // if no timeout then we should block, and there use a negative timeout
+        long timeout = parseDuration(definition.getTimeout(), -1);
+
+        PollEnricher enricher = new PollEnricher(exp, uri, timeout);
         AggregationStrategy strategy = getConfiguredAggregationStrategy(definition);
         if (strategy != null) {
             enricher.setAggregationStrategy(strategy);
@@ -60,8 +67,8 @@ public class PollEnrichReifier extends ProcessorReifier<PollEnrichDefinition> {
         if (num != null) {
             enricher.setCacheSize(num);
         }
-        enricher.setIgnoreInvalidEndpoint(isIgnoreInvalidEndpoint);
-        enricher.setAggregateOnException(isAggregateOnException);
+        enricher.setIgnoreInvalidEndpoint(parseBoolean(definition.getIgnoreInvalidEndpoint(), false));
+        enricher.setAggregateOnException(parseBoolean(definition.getAggregateOnException(), false));
         if (definition.getAutoStartComponents() != null) {
             enricher.setAutoStartupComponents(parseBoolean(definition.getAutoStartComponents(), true));
         }
