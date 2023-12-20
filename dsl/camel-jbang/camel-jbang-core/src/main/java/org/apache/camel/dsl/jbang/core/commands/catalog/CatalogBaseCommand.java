e4d9423f3c84c3a10967a81322825d10c00d7028
Claus Ibsen <claus.ibsen@gmail.com>
2023-12-20 14:48:08 +0100

    camel-jbang - Upgrade to quarkus 3.6.4

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogBaseCommand.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogBaseCommand.java
@@ -49,7 +49,7 @@ public abstract class CatalogBaseCommand extends CamelCommand {
     String runtime;
 
     @CommandLine.Option(names = { "--quarkus-version" }, description = "Quarkus Platform version",
-                        defaultValue = "3.6.3")
+                        defaultValue = "3.6.4")
     String quarkusVersion;
 
     @CommandLine.Option(names = { "--repos" },
