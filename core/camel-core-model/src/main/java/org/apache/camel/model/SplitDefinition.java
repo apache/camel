6fa30fea5a66c2cd65f7058b3a1390fd01d6e8b9
Claus Ibsen <claus.ibsen@gmail.com>
2023-12-22 14:18:10 +0100

    CAMEL-18482: Split EIP - Allow to split in single mode.

--- core/camel-core-model/src/main/java/org/apache/camel/model/SplitDefinition.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/SplitDefinition.java
@@ -117,10 +117,10 @@ public class SplitDefinition extends OutputExpressionNode implements ExecutorSer
     /**
      * Delimiter used in splitting messages.
      * Can be turned off using the value <tt>false</tt>.
-     * To force not splitting then the delimiter can be set to <tt>single</tt> to thread the value as a single list,
-     * this can be used in special situations.
+     * To force not splitting then the delimiter can be set to <tt>single</tt> to use the value as a single list,
+     * this can be needed in some special situations.
      * <p/>
-     * The default value is ,
+     * The default value is comma.
      *
      * @param  delimiter the delimiter
      * @return           the builder
