6849b51ae61ad634d9e748d8853b377b653dd5a3
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-01-04 14:20:32 +0100

    (chores) Ensures logging consistency for the Exchange

--- core/camel-support/src/main/java/org/apache/camel/support/AbstractExchange.java
+++ core/camel-support/src/main/java/org/apache/camel/support/AbstractExchange.java
@@ -939,7 +939,7 @@ class AbstractExchange implements ExtendedExchange {
     }
 
     @Override
-    public String toString() {
+    public final String toString() {
         // do not output information about the message as it may contain sensitive information
         if (exchangeId != null) {
             return "Exchange[" + exchangeId + "]";
