81277c434c1239c18396732e66238a327968bd8b
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-01-05 17:21:34 +0100

    (chores) camel-elasticsearch-rest: fixed incorrect name pattern for integration test

--- components/camel-elasticsearch-rest-client/src/test/java/org/apache/camel/component/elasticsearch/rest/client/integration/ElasticsearchRestClientComponentIT.java
+++ components/camel-elasticsearch-rest-client/src/test/java/org/apache/camel/component/elasticsearch/rest/client/integration/ElasticsearchRestClientComponentIT.java
@@ -32,7 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ElasticsearchRestClientComponentTest extends ElasticsearchRestClientTestSupport {
+public class ElasticsearchRestClientComponentIT extends ElasticsearchRestClientITSupport {
 
     @Override
     protected RouteBuilder createRouteBuilder() {
