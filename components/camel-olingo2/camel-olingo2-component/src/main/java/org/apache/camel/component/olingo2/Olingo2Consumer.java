7b4e8d993dcb5405919d678c20e9abd710ea7be1
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-01-15 11:05:40 +0100

    CAMEL-20297 camel-olingo2: do not swallow interrupted exceptions

--- components/camel-olingo2/camel-olingo2-component/src/main/java/org/apache/camel/component/olingo2/Olingo2Consumer.java
+++ components/camel-olingo2/camel-olingo2-component/src/main/java/org/apache/camel/component/olingo2/Olingo2Consumer.java
@@ -101,7 +101,9 @@ public class Olingo2Consumer extends AbstractApiConsumer<Olingo2ApiName, Olingo2
             } else {
                 return ApiConsumerHelper.getResultsProcessed(this, result[0], isSplitResult());
             }
-
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw RuntimeCamelException.wrapRuntimeCamelException(e);
         } catch (Exception t) {
             throw RuntimeCamelException.wrapRuntimeCamelException(t);
         }
