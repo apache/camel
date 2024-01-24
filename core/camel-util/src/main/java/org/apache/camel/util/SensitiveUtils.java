337cff25a2278f3bdd4298e256c1f4ee1d967606
Claus Ibsen <claus.ibsen@gmail.com>
2024-01-24 19:35:13 +0100

    Regen

--- core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
+++ core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
@@ -176,7 +176,7 @@ public final class SensitiveUtils {
                                                     + "|\\Qverificationcode\\E"
                                                     + "|\\Qwebhookverifytoken\\E"
                                                     + "|\\Qzookeeperpassword\\E"
-                                                    // SENSITIVE-PATTERN: END
+    // SENSITIVE-PATTERN: END
     ;
 
     private SensitiveUtils() {
