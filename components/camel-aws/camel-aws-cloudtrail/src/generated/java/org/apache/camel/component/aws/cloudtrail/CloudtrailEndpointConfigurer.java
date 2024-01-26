5bff6255946d955a03bce7fa53f653c4a0b8a710
Andrea Cosentino <ancosen@gmail.com>
2024-01-26 14:30:33 +0100

    CAMEL-20281 - Camel-AWS Components: Make it possible to use AwsSessionCredentials to support temporary credentials - AWS Cloudtrail (#12916)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-aws/camel-aws-cloudtrail/src/generated/java/org/apache/camel/component/aws/cloudtrail/CloudtrailEndpointConfigurer.java
+++ components/camel-aws/camel-aws-cloudtrail/src/generated/java/org/apache/camel/component/aws/cloudtrail/CloudtrailEndpointConfigurer.java
@@ -71,6 +71,8 @@ public class CloudtrailEndpointConfigurer extends PropertyConfigurerSupport impl
         case "secretKey": target.getConfiguration().setSecretKey(property(camelContext, java.lang.String.class, value)); return true;
         case "sendemptymessagewhenidle":
         case "sendEmptyMessageWhenIdle": target.setSendEmptyMessageWhenIdle(property(camelContext, boolean.class, value)); return true;
+        case "sessiontoken":
+        case "sessionToken": target.getConfiguration().setSessionToken(property(camelContext, java.lang.String.class, value)); return true;
         case "startscheduler":
         case "startScheduler": target.setStartScheduler(property(camelContext, boolean.class, value)); return true;
         case "timeunit":
@@ -85,6 +87,8 @@ public class CloudtrailEndpointConfigurer extends PropertyConfigurerSupport impl
         case "useFixedDelay": target.setUseFixedDelay(property(camelContext, boolean.class, value)); return true;
         case "useprofilecredentialsprovider":
         case "useProfileCredentialsProvider": target.getConfiguration().setUseProfileCredentialsProvider(property(camelContext, boolean.class, value)); return true;
+        case "usesessioncredentials":
+        case "useSessionCredentials": target.getConfiguration().setUseSessionCredentials(property(camelContext, boolean.class, value)); return true;
         default: return false;
         }
     }
@@ -147,6 +151,8 @@ public class CloudtrailEndpointConfigurer extends PropertyConfigurerSupport impl
         case "secretKey": return java.lang.String.class;
         case "sendemptymessagewhenidle":
         case "sendEmptyMessageWhenIdle": return boolean.class;
+        case "sessiontoken":
+        case "sessionToken": return java.lang.String.class;
         case "startscheduler":
         case "startScheduler": return boolean.class;
         case "timeunit":
@@ -161,6 +167,8 @@ public class CloudtrailEndpointConfigurer extends PropertyConfigurerSupport impl
         case "useFixedDelay": return boolean.class;
         case "useprofilecredentialsprovider":
         case "useProfileCredentialsProvider": return boolean.class;
+        case "usesessioncredentials":
+        case "useSessionCredentials": return boolean.class;
         default: return null;
         }
     }
@@ -219,6 +227,8 @@ public class CloudtrailEndpointConfigurer extends PropertyConfigurerSupport impl
         case "secretKey": return target.getConfiguration().getSecretKey();
         case "sendemptymessagewhenidle":
         case "sendEmptyMessageWhenIdle": return target.isSendEmptyMessageWhenIdle();
+        case "sessiontoken":
+        case "sessionToken": return target.getConfiguration().getSessionToken();
         case "startscheduler":
         case "startScheduler": return target.isStartScheduler();
         case "timeunit":
@@ -233,6 +243,8 @@ public class CloudtrailEndpointConfigurer extends PropertyConfigurerSupport impl
         case "useFixedDelay": return target.isUseFixedDelay();
         case "useprofilecredentialsprovider":
         case "useProfileCredentialsProvider": return target.getConfiguration().isUseProfileCredentialsProvider();
+        case "usesessioncredentials":
+        case "useSessionCredentials": return target.getConfiguration().isUseSessionCredentials();
         default: return null;
         }
     }
--- components/camel-aws/camel-aws-cloudtrail/src/main/java/org/apache/camel/component/aws/cloudtrail/CloudtrailConfiguration.java
+++ components/camel-aws/camel-aws-cloudtrail/src/main/java/org/apache/camel/component/aws/cloudtrail/CloudtrailConfiguration.java
@@ -34,6 +34,11 @@ public class CloudtrailConfiguration implements Cloneable {
     private String accessKey;
     @UriParam(label = "security", secret = true, description = "Amazon AWS Secret Key")
     private String secretKey;
+
+    @UriParam(label = "security", secret = true,
+              description = "Amazon AWS Session Token used when the user needs to assume a IAM role")
+    private String sessionToken;
+
     @UriParam(enums = "ap-south-2,ap-south-1,eu-south-1,eu-south-2,us-gov-east-1,me-central-1,il-central-1,ca-central-1,eu-central-1,us-iso-west-1,eu-central-2,us-west-1,us-west-2,af-south-1,eu-north-1,eu-west-3,eu-west-2,eu-west-1,ap-northeast-3,ap-northeast-2,ap-northeast-1,me-south-1,sa-east-1,ap-east-1,cn-north-1,us-gov-west-1,ap-southeast-1,ap-southeast-2,us-iso-east-1,ap-southeast-3,ap-southeast-4,us-east-1,us-east-2,cn-northwest-1,us-isob-east-1,aws-global,aws-cn-global,aws-us-gov-global,aws-iso-global,aws-iso-b-global",
               description = "The region in which Cloudtrail client needs to work. When using this parameter, the configuration will expect the lowercase name of the "
                             + "region (for example ap-east-1) You'll need to use the name Region.EU_WEST_1.id()")
@@ -67,6 +72,12 @@ public class CloudtrailConfiguration implements Cloneable {
     @UriParam(label = "security",
               description = "Set whether the Cloudtrail client should expect to load credentials through a profile credentials provider.")
     private boolean useProfileCredentialsProvider;
+
+    @UriParam(label = "security",
+              description = "Set whether the CloudTrail client should expect to use Session Credentials. This is useful in situation in which the user"
+                            +
+                            " needs to assume a IAM role for doing operations in CloudTrail.")
+    private boolean useSessionCredentials;
     @UriParam(label = "security",
               description = "If using a profile credentials provider this parameter will set the profile name.")
     private String profileCredentialsName;
@@ -109,6 +120,14 @@ public class CloudtrailConfiguration implements Cloneable {
 
     public void setSecretKey(String secretKey) {
         this.secretKey = secretKey;
+    }
+
+    public String getSessionToken() {
+        return sessionToken;
+    }
+
+    public void setSessionToken(String sessionToken) {
+        this.sessionToken = sessionToken;
     }
 
     public String getRegion() {
@@ -189,6 +208,14 @@ public class CloudtrailConfiguration implements Cloneable {
 
     public void setUseProfileCredentialsProvider(boolean useProfileCredentialsProvider) {
         this.useProfileCredentialsProvider = useProfileCredentialsProvider;
+    }
+
+    public boolean isUseSessionCredentials() {
+        return useSessionCredentials;
+    }
+
+    public void setUseSessionCredentials(boolean useSessionCredentials) {
+        this.useSessionCredentials = useSessionCredentials;
     }
 
     public String getProfileCredentialsName() {
