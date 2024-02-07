d18eda704edc6526512392b77d563caf5c8b346e
iMashtak <deralus.ws@yandex.ru>
2024-02-07 22:57:14 +0300

    CAMEL-20333 camel-kotlin-api: regen

--- dsl/camel-kotlin-api/src/generated/kotlin/org/apache/camel/kotlin/components/Aws2EventbridgeUriDsl.kt
+++ dsl/camel-kotlin-api/src/generated/kotlin/org/apache/camel/kotlin/components/Aws2EventbridgeUriDsl.kt
@@ -117,6 +117,10 @@ public class Aws2EventbridgeUriDsl(
     it.property("secretKey", secretKey)
   }
 
+  public fun sessionToken(sessionToken: String) {
+    it.property("sessionToken", sessionToken)
+  }
+
   public fun trustAllCertificates(trustAllCertificates: String) {
     it.property("trustAllCertificates", trustAllCertificates)
   }
@@ -139,5 +143,13 @@ public class Aws2EventbridgeUriDsl(
 
   public fun useProfileCredentialsProvider(useProfileCredentialsProvider: Boolean) {
     it.property("useProfileCredentialsProvider", useProfileCredentialsProvider.toString())
+  }
+
+  public fun useSessionCredentials(useSessionCredentials: String) {
+    it.property("useSessionCredentials", useSessionCredentials)
+  }
+
+  public fun useSessionCredentials(useSessionCredentials: Boolean) {
+    it.property("useSessionCredentials", useSessionCredentials.toString())
   }
 }
