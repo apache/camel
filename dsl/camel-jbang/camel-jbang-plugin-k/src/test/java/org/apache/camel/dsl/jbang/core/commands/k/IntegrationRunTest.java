417eae3390cb0511c8a1dfd088f82374d5fbd803
Claus Ibsen <claus.ibsen@gmail.com>
2024-02-08 09:23:11 +0100

    camel-jbang - Upgrade to camel-k 2.2.0 apis (#13035)

--- dsl/camel-jbang/camel-jbang-plugin-k/src/test/java/org/apache/camel/dsl/jbang/core/commands/k/IntegrationRunTest.java
+++ dsl/camel-jbang/camel-jbang-plugin-k/src/test/java/org/apache/camel/dsl/jbang/core/commands/k/IntegrationRunTest.java
@@ -88,6 +88,10 @@ class IntegrationRunTest extends KubeBaseTest {
                   traits:
                     container:
                       imagePullPolicy: ALWAYS
+                      name: integration
+                      port: 8080
+                      servicePort: 80
+                      servicePortName: http
                     logging:
                       level: DEBUG""", printer.getOutput());
     }
@@ -158,6 +162,7 @@ class IntegrationRunTest extends KubeBaseTest {
                         - to: log:info
                   traits:
                     mount:
+                      hotReload: false
                       volumes:
                       - /foo
                       - /bar""", printer.getOutput());
@@ -220,6 +225,8 @@ class IntegrationRunTest extends KubeBaseTest {
                         - to: log:info
                   traits:
                     environment:
+                      containerMeta: true
+                      httpProxy: true
                       vars:
                       - CAMEL_FOO=bar""", printer.getOutput());
     }
@@ -280,8 +287,11 @@ class IntegrationRunTest extends KubeBaseTest {
                         - to: log:info
                   traits:
                     builder:
+                      incrementalImageBuild: true
+                      orderStrategy: SEQUENTIAL
                       properties:
-                      - camel.foo=bar""", printer.getOutput());
+                      - camel.foo=bar
+                      strategy: ROUTINE""", printer.getOutput());
     }
 
     @Test
@@ -437,7 +447,8 @@ class IntegrationRunTest extends KubeBaseTest {
                     mount:
                       configs:
                       - secret:foo
-                      - configmap:bar""", printer.getOutput());
+                      - configmap:bar
+                      hotReload: false""", printer.getOutput());
     }
 
     @Test
@@ -466,6 +477,7 @@ class IntegrationRunTest extends KubeBaseTest {
                         - to: log:info
                   traits:
                     mount:
+                      hotReload: false
                       resources:
                       - configmap:foo/file.txt""", printer.getOutput());
     }
@@ -518,7 +530,11 @@ class IntegrationRunTest extends KubeBaseTest {
                 spec:
                   traits:
                     container:
-                      image: quay.io/camel/demo-app:1.0""", printer.getOutput());
+                      image: quay.io/camel/demo-app:1.0
+                      name: integration
+                      port: 8080
+                      servicePort: 80
+                      servicePortName: http""", printer.getOutput());
     }
 
     @Test
