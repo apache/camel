fedd27ac095f9350aca8f3786b3ff035c25f50c9
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-02-09 15:04:50 +0100

    (chores) camel-aws2-cw: documentation fixes
    - Fixed grammar and typos
    - Fixed punctuation
    - Added and/or fixed links

--- components/camel-aws/camel-aws2-cw/src/main/java/org/apache/camel/component/aws2/cw/Cw2Configuration.java
+++ components/camel-aws/camel-aws2-cw/src/main/java/org/apache/camel/component/aws2/cw/Cw2Configuration.java
@@ -99,7 +99,7 @@ public class Cw2Configuration implements Cloneable {
     }
 
     /**
-     * Amazon AWS Session Token used when the user needs to assume a IAM role
+     * Amazon AWS Session Token used when the user needs to assume an IAM role
      */
     public void setSessionToken(String sessionToken) {
         this.sessionToken = sessionToken;
@@ -210,7 +210,7 @@ public class Cw2Configuration implements Cloneable {
 
     /**
      * The region in which CW client needs to work. When using this parameter, the configuration will expect the
-     * lowercase name of the region (for example ap-east-1) You'll need to use the name Region.EU_WEST_1.id()
+     * lowercase name of the region (for example, ap-east-1) You'll need to use the name Region.EU_WEST_1.id()
      */
     public void setRegion(String region) {
         this.region = region;
@@ -232,7 +232,7 @@ public class Cw2Configuration implements Cloneable {
     }
 
     /**
-     * Set the need for overidding the endpoint. This option needs to be used in combination with uriEndpointOverride
+     * Set the need for overriding the endpoint. This option needs to be used in combination with the uriEndpointOverride
      * option
      */
     public void setOverrideEndpoint(boolean overrideEndpoint) {
@@ -278,8 +278,8 @@ public class Cw2Configuration implements Cloneable {
     }
 
     /**
-     * Set whether the CloudWatch client should expect to use Session Credentials. This is useful in situation in which
-     * the user needs to assume a IAM role for doing operations in CloudWatch.
+     * Set whether the CloudWatch client should expect to use Session Credentials. This is useful in a situation in which
+     * the user needs to assume an IAM role for doing operations in CloudWatch.
      */
     public void setUseSessionCredentials(boolean useSessionCredentials) {
         this.useSessionCredentials = useSessionCredentials;
@@ -290,7 +290,7 @@ public class Cw2Configuration implements Cloneable {
     }
 
     /**
-     * If using a profile credentials provider this parameter will set the profile name
+     * If using a profile credentials provider, this parameter will set the profile name
      */
     public void setProfileCredentialsName(String profileCredentialsName) {
         this.profileCredentialsName = profileCredentialsName;
