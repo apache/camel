6036a92389fb7c1e8e2a4cc19ec9e5da300a5fb7
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-02-09 17:40:40 +0100

    (chores) camel-aws2-translate: documentation fixes
    - Fixed grammar and typos
    - Fixed punctuation
    - Added and/or fixed links

--- components/camel-aws/camel-aws2-translate/src/main/java/org/apache/camel/component/aws2/translate/Translate2Configuration.java
+++ components/camel-aws/camel-aws2-translate/src/main/java/org/apache/camel/component/aws2/translate/Translate2Configuration.java
@@ -78,7 +78,7 @@ public class Translate2Configuration implements Cloneable {
     }
 
     /**
-     * To use a existing configured AWS Translate as client
+     * To use an existing configured AWS Translate client
      */
     public void setTranslateClient(TranslateClient translateClient) {
         this.translateClient = translateClient;
@@ -111,7 +111,7 @@ public class Translate2Configuration implements Cloneable {
     }
 
     /**
-     * Amazon AWS Session Token used when the user needs to assume a IAM role
+     * Amazon AWS Session Token used when the user needs to assume an IAM role
      */
     public void setSessionToken(String sessionToken) {
         this.sessionToken = sessionToken;
@@ -166,8 +166,8 @@ public class Translate2Configuration implements Cloneable {
     }
 
     /**
-     * The region in which Translate client needs to work. When using this parameter, the configuration will expect the
-     * lowercase name of the region (for example ap-east-1) You'll need to use the name Region.EU_WEST_1.id()
+     * The region in which the Translate client needs to work. When using this parameter, the configuration will expect the
+     * lowercase name of the region (for example, ap-east-1) You'll need to use the name Region.EU_WEST_1.id()
      */
     public void setRegion(String region) {
         this.region = region;
@@ -233,7 +233,7 @@ public class Translate2Configuration implements Cloneable {
     }
 
     /**
-     * Set the need for overidding the endpoint. This option needs to be used in combination with uriEndpointOverride
+     * Set the need for overriding the endpoint. This option needs to be used in combination with the uriEndpointOverride
      * option
      */
     public void setOverrideEndpoint(boolean overrideEndpoint) {
@@ -279,8 +279,8 @@ public class Translate2Configuration implements Cloneable {
     }
 
     /**
-     * Set whether the Translate client should expect to use Session Credentials. This is useful in situation in which
-     * the user needs to assume a IAM role for doing operations in Translate.
+     * Set whether the Translate client should expect to use Session Credentials. This is useful in a situation in which
+     * the user needs to assume an IAM role for doing operations in Translate.
      */
     public void setUseSessionCredentials(boolean useSessionCredentials) {
         this.useSessionCredentials = useSessionCredentials;
@@ -291,7 +291,7 @@ public class Translate2Configuration implements Cloneable {
     }
 
     /**
-     * If using a profile credentials provider this parameter will set the profile name
+     * If using a profile credentials provider, this parameter will set the profile name
      */
     public void setProfileCredentialsName(String profileCredentialsName) {
         this.profileCredentialsName = profileCredentialsName;
