4f75cd593d443556371e87cee02502eb2a43d310
Claus Ibsen <claus.ibsen@gmail.com>
2024-02-14 13:00:54 +0100

    Polished

--- core/camel-core/src/test/java/org/apache/camel/processor/transformer/custom/CustomDataTypeTransformer.java
+++ core/camel-core/src/test/java/org/apache/camel/processor/transformer/custom/CustomDataTypeTransformer.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.camel.processor.transformer.custom;
 
 import org.apache.camel.Message;
