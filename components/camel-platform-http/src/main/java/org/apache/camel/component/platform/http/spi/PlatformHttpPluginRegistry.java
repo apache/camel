5ce29b25c9e0803cf82a677769ca42b8e42ef672
ikulaga <kulagaivanandreevich@gmail.com>
2024-02-16 01:04:11 +0600

    CAMEL-20361: camel-jbang - Make jolokia pluggable for camel-platform-http-main PlatformHttpPluginRegistry was made to resolve plugins for camel-platform-http. Jolokia is now camel-platform-http plugin

---
+++ components/camel-platform-http/src/main/java/org/apache/camel/component/platform/http/spi/PlatformHttpPluginRegistry.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.platform.http.spi;
+
+import java.util.Optional;
+
+import org.apache.camel.CamelContextAware;
+import org.apache.camel.StaticService;
+
+/**
+ * Factory to abstract the creation of the Plugin Registry for camel-platform-http.
+ */
+public interface PlatformHttpPluginRegistry extends CamelContextAware, StaticService {
+
+    String PLATFORM_HTTP_FACTORY_PATH = "component/platform/http/";
+
+    String FACTORY = PLATFORM_HTTP_FACTORY_PATH + "platform-http-plugin-registry";
+
+    Optional<PlatformHttpPlugin> resolvePluginById(String id);
+
+    boolean register(PlatformHttpPlugin plugin);
+}
