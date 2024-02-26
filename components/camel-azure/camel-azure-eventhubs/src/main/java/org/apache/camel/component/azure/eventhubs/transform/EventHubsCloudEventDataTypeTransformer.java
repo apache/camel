d32370bde577eea441dd2ef24881f186054a3e96
Guillaume Nodet <gnodet@gmail.com>
2024-02-26 10:14:37 +0100

    Regen

--- components/camel-azure/camel-azure-eventhubs/src/main/java/org/apache/camel/component/azure/eventhubs/transform/EventHubsCloudEventDataTypeTransformer.java
+++ components/camel-azure/camel-azure-eventhubs/src/main/java/org/apache/camel/component/azure/eventhubs/transform/EventHubsCloudEventDataTypeTransformer.java
@@ -17,6 +17,8 @@
 
 package org.apache.camel.component.azure.eventhubs.transform;
 
+import java.util.Map;
+
 import org.apache.camel.Message;
 import org.apache.camel.component.azure.eventhubs.EventHubsConstants;
 import org.apache.camel.component.cloudevents.CloudEvent;
@@ -24,8 +26,6 @@ import org.apache.camel.component.cloudevents.CloudEvents;
 import org.apache.camel.spi.DataType;
 import org.apache.camel.spi.DataTypeTransformer;
 import org.apache.camel.spi.Transformer;
-
-import java.util.Map;
 
 /**
  * Data type transformer converts Azure Eventhubs consumer response to CloudEvent v1_0 data format. The data type sets
