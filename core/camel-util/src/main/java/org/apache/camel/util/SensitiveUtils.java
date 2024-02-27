5dc0a9dc2886dd30da79b82f3cb3dc4a5a72bf37
Andrea Cosentino <ancosen@gmail.com>
2024-02-27 19:55:22 +0100

    Camel AWS Bedrock: Added to Kit (#13332)
    * Camel-AWS-Bedrock: Added to MojoHelper
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * Camel-AWS-Bedrock: Added to kit
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * Camel-AWS-Bedrock: Added to kit
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    ---------
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
+++ core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
@@ -56,6 +56,7 @@ public final class SensitiveUtils {
                     "httpproxypassword",
                     "keystore",
                     "login",
+                    "modelid",
                     "oauthclientid",
                     "oauthclientsecret",
                     "oauthtoken",
@@ -135,6 +136,7 @@ public final class SensitiveUtils {
                                                     + "|\\Qhttpproxypassword\\E"
                                                     + "|\\Qkeystore\\E"
                                                     + "|\\Qlogin\\E"
+                                                    + "|\\Qmodelid\\E"
                                                     + "|\\Qoauthclientid\\E"
                                                     + "|\\Qoauthclientsecret\\E"
                                                     + "|\\Qoauthtoken\\E"
