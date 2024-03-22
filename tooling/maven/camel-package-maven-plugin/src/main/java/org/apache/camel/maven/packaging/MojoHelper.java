9af0e8db2a6b5fedf1d0591f59ff179c131a5373
Claus Ibsen <claus.ibsen@gmail.com>
2024-03-22 09:40:28 +0100

    CAMEL-20595: Move camel-djl into camel-ai folder (#13587)

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/MojoHelper.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/MojoHelper.java
@@ -37,7 +37,8 @@ public final class MojoHelper {
     public static List<Path> getComponentPath(Path dir) {
         switch (dir.getFileName().toString()) {
             case "camel-ai":
-                return Arrays.asList(dir.resolve("camel-langchain-chat"), dir.resolve("camel-langchain-embeddings"));
+                return Arrays.asList(dir.resolve("camel-djl"), dir.resolve("camel-langchain-chat"),
+                        dir.resolve("camel-langchain-embeddings"));
             case "camel-as2":
                 return Collections.singletonList(dir.resolve("camel-as2-component"));
             case "camel-avro-rpc":
