eff76f2037360893eec8c5e362227de741516b09
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-04-15 13:03:03 +0200

    (chores) camel-core: isolate flaky test

--- core/camel-core/src/test/java/org/apache/camel/component/file/FileRecursiveNoopTest.java
+++ core/camel-core/src/test/java/org/apache/camel/component/file/FileRecursiveNoopTest.java
@@ -22,7 +22,9 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
 
+@Isolated("Flaky test")
 public class FileRecursiveNoopTest extends ContextTestSupport {
 
     @BeforeEach
