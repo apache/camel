08e6f734d6e9bff05b1a09178d760959c6ee67b4
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-04-16 12:01:10 +0200

    (chores) camel-core: removed unused operations in tests

--- core/camel-core-xml/src/test/java/org/apache/camel/core/xml/AbstractCamelContextFactoryBeanTest.java
+++ core/camel-core-xml/src/test/java/org/apache/camel/core/xml/AbstractCamelContextFactoryBeanTest.java
@@ -43,7 +43,6 @@ import org.mockito.ArgumentCaptor;
 
 import static java.util.Arrays.asList;
 import static java.util.Arrays.stream;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
