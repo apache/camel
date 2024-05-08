b91a06bb6da2c1a89c27a60a37c12becab664eb7
Jang-Vijay Singh <jvsing@gmail.com>
2024-05-08 13:10:24 +0100

    CAMEL-20722 - formatting updates after mvn verify

--- components/camel-kafka/src/test/java/org/apache/camel/component/kafka/integration/KafkaBreakOnFirstErrorWithBatchUsingAsyncCommitManagerIT.java
+++ components/camel-kafka/src/test/java/org/apache/camel/component/kafka/integration/KafkaBreakOnFirstErrorWithBatchUsingAsyncCommitManagerIT.java
@@ -48,8 +48,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 @Tags({ @Tag("breakOnFirstError") })
 @EnabledOnOs(value = { OS.LINUX, OS.MAC, OS.FREEBSD, OS.OPENBSD, OS.WINDOWS },
-architectures = { "amd64", "aarch64", "s390x", "x86", "x86_64" },
-disabledReason = "This test does not run reliably on ppc64le")
+             architectures = { "amd64", "aarch64", "s390x", "x86", "x86_64" },
+             disabledReason = "This test does not run reliably on ppc64le")
 
 class KafkaBreakOnFirstErrorWithBatchUsingAsyncCommitManagerIT extends BaseExclusiveKafkaTestSupport {
     public static final String ROUTE_ID = "breakOnFirstErrorBatchIT";
