a58b929167fa55ceced2a20f4d8b4163485f6eba
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-05-27 19:01:41 +0200

    Create a langchain4j-core module

---
+++ components/camel-ai/camel-langchain4j-core/src/main/java/org/apache/camel/component/langchain4j/core/LangChain4jConverter.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.langchain4j.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.Metadata;
+import org.apache.camel.Converter;
+import org.apache.camel.Exchange;
+
+/**
+ * Converter methods to convert from / to LangChain4j types.
+ */
+@Converter(generateLoader = true)
+public class LangChain4jConverter {
+
+    @Converter
+    public static Document toDocument(String value, Exchange exchange) {
+        return Document.document(
+                value,
+                toMetadata(exchange));
+    }
+
+    private static Metadata toMetadata(Exchange exchange) {
+        if (exchange == null) {
+            return new Metadata();
+        }
+        if (exchange.getMessage() == null) {
+            return new Metadata();
+        }
+
+        Map<String, Object> metadata = null;
+        Map<String, Object> headers = exchange.getMessage().getHeaders();
+
+        for (Map.Entry<String, Object> entry : headers.entrySet()) {
+            if (!entry.getKey().startsWith(LangChain4j.METADATA_PREFIX)) {
+                continue;
+            }
+
+            if (metadata == null) {
+                metadata = new HashMap<>();
+            }
+
+            metadata.put(
+                    entry.getKey().substring(LangChain4j.METADATA_PREFIX_LEN),
+                    entry.getValue());
+        }
+
+        return metadata != null ? Metadata.from(metadata) : new Metadata();
+    }
+}
---
+++ components/camel-ai/camel-langchain4j-core/src/test/java/org/apache/camel/component/langchain4j/core/LangChain4jConverterTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.langchain4j.core;
+
+import dev.langchain4j.data.document.Document;
+import org.apache.camel.Exchange;
+import org.apache.camel.NoTypeConversionAvailableException;
+import org.apache.camel.support.DefaultExchange;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LangChain4jConverterTest extends CamelTestSupport {
+
+    @Test
+    public void testConverter() throws NoTypeConversionAvailableException {
+        Document doc = context.getTypeConverter().mandatoryConvertTo(Document.class, "foo");
+        assertThat(doc.text()).isEqualTo("foo");
+
+        Exchange exchange = new DefaultExchange(context);
+        exchange.getMessage().setHeader(LangChain4j.METADATA_PREFIX + "meta1", 1);
+        exchange.getMessage().setHeader(LangChain4j.METADATA_PREFIX + "meta2", "baz");
+        exchange.getMessage().setHeader("meta3", "ignored");
+
+        Document docWithMetadata = context.getTypeConverter().mandatoryConvertTo(
+                Document.class,
+                exchange,
+                "bar");
+
+        assertThat(docWithMetadata.text()).isEqualTo("bar");
+        assertThat(docWithMetadata.metadata().toMap())
+                .containsEntry("meta1", 1)
+                .containsEntry("meta2", "baz")
+                .doesNotContainKey("meta3");
+    }
+}
