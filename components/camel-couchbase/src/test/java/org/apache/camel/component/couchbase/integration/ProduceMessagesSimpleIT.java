a95615bdc77d2f8210285bf4438ef17afc0f824d
Otavio Rodolfo Piske <angusyoung@gmail.com>
2024-05-29 10:06:06 +0200

    (chores) camel-couchbase: cleanup disabling resource-intensive tests

--- components/camel-couchbase/src/test/java/org/apache/camel/component/couchbase/integration/ProduceMessagesSimpleIT.java
+++ components/camel-couchbase/src/test/java/org/apache/camel/component/couchbase/integration/ProduceMessagesSimpleIT.java
@@ -22,16 +22,10 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperties;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
-@DisabledIfSystemProperties({
-        @DisabledIfSystemProperty(named = "ci.env.name", matches = "apache.org",
-                                  disabledReason = "Apache CI nodes are too resource constrained for this test"),
-        @DisabledIfSystemProperty(named = "ci.env.name", matches = "github.com", disabledReason = "Flaky on GitHub Actions"),
-        @DisabledIfSystemProperty(named = "couchbase.enable.it", matches = "false",
-                                  disabledReason = "Too resource intensive for most systems to run reliably"),
-})
+@DisabledIfSystemProperty(named = "ci.env.name", matches = ".*",
+                          disabledReason = "Too resource intensive for most systems to run reliably")
 @Tags({ @Tag("couchbase-7") })
 public class ProduceMessagesSimpleIT extends CouchbaseIntegrationTestBase {
 
