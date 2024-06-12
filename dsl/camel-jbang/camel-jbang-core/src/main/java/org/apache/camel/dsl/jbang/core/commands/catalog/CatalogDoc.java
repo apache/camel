9ed04c516aaf424ffb5fdf4605e273000fa57aba
Claus Ibsen <claus.ibsen@gmail.com>
2024-06-12 14:29:09 +0200

    Upgrade jbang to 4.4.2

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogDoc.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogDoc.java
@@ -86,7 +86,7 @@ public class CatalogDoc extends CamelCommand {
     boolean headers;
 
     @CommandLine.Option(names = {
-            "--kamelets-version" }, description = "Apache Camel Kamelets version", defaultValue = "4.4.1")
+            "--kamelets-version" }, description = "Apache Camel Kamelets version", defaultValue = "4.4.2")
     String kameletsVersion;
 
     CamelCatalog catalog;
