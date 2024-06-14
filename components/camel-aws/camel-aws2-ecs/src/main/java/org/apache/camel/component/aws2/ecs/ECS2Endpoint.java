d7165d51cc7cd2790183cb5fe6f3feb6d2799a15
Andrea Cosentino <ancosen@gmail.com>
2024-06-14 11:43:15 +0200

    CAMEL-20798: EndpointServiceLocation on components to make it possible to know which remote system Camel connects to to assist for monitoring and observability - AWS ECS
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-aws/camel-aws2-ecs/src/main/java/org/apache/camel/component/aws2/ecs/ECS2Endpoint.java
+++ components/camel-aws/camel-aws2-ecs/src/main/java/org/apache/camel/component/aws2/ecs/ECS2Endpoint.java
@@ -22,6 +22,7 @@ import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
 import org.apache.camel.component.aws2.ecs.client.ECS2ClientFactory;
+import org.apache.camel.spi.EndpointServiceLocation;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.support.ScheduledPollEndpoint;
@@ -34,7 +35,7 @@ import software.amazon.awssdk.services.ecs.EcsClient;
 @UriEndpoint(firstVersion = "3.1.0", scheme = "aws2-ecs", title = "AWS Elastic Container Service (ECS)",
              syntax = "aws2-ecs:label", producerOnly = true, category = { Category.CLOUD, Category.MANAGEMENT },
              headersClass = ECS2Constants.class)
-public class ECS2Endpoint extends ScheduledPollEndpoint {
+public class ECS2Endpoint extends ScheduledPollEndpoint implements EndpointServiceLocation {
 
     private EcsClient ecsClient;
 
@@ -85,5 +86,22 @@ public class ECS2Endpoint extends ScheduledPollEndpoint {
 
     public EcsClient getEcsClient() {
         return ecsClient;
+    }
+
+    @Override
+    public String getServiceUrl() {
+        if (!configuration.isOverrideEndpoint()) {
+            if (ObjectHelper.isNotEmpty(configuration.getRegion())) {
+                return configuration.getRegion();
+            }
+        } else if (ObjectHelper.isNotEmpty(configuration.getUriEndpointOverride())) {
+            return configuration.getUriEndpointOverride();
+        }
+        return null;
+    }
+
+    @Override
+    public String getServiceProtocol() {
+        return "ecs";
     }
 }
