6958b938339058661be8021406a4001fb3026e2e
Tom Cunningham <tcunning@redhat.com>
2024-06-20 13:38:10 -0400

    Align camel-4.4.x branch with dependencies from spring-boot 3.2.7

--- components/camel-jsonpath/src/test/java/org/apache/camel/jsonpath/JsonPathSplitSingleListOptionTest.java
+++ components/camel-jsonpath/src/test/java/org/apache/camel/jsonpath/JsonPathSplitSingleListOptionTest.java
@@ -16,14 +16,14 @@
  */
 package org.apache.camel.jsonpath;
 
+import java.io.File;
+import java.util.Map;
+
 import com.jayway.jsonpath.Option;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
