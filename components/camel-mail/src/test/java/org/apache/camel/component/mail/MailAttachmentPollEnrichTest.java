67693e28296d8cea5162384344e6aa157b762787
Claus Ibsen <claus.ibsen@gmail.com>
2024-06-25 10:57:03 +0200

    CAMEL-20907: camel-mail - Added unit test with pollEnrich and attachments.

---
+++ components/camel-mail/src/test/java/org/apache/camel/component/mail/MailAttachmentPollEnrichTest.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.mail;
+
+import java.util.Map;
+
+import jakarta.activation.DataHandler;
+import jakarta.activation.FileDataSource;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Producer;
+import org.apache.camel.attachment.Attachment;
+import org.apache.camel.attachment.AttachmentMessage;
+import org.apache.camel.attachment.DefaultAttachment;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mail.Mailbox.MailboxUser;
+import org.apache.camel.component.mail.Mailbox.Protocol;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for Camel attachments and Mail attachments.
+ */
+public class MailAttachmentPollEnrichTest extends CamelTestSupport {
+    private static final MailboxUser james = Mailbox.getOrCreateUser("james", "secret");
+
+    @Test
+    public void testPollEnrichMailWithAttachments() throws Exception {
+        // clear mailbox
+        Mailbox.clearAll();
+
+        // START SNIPPET: e1
+
+        // create an exchange with a normal body and attachment to be produced as email
+        Endpoint endpoint = context.getEndpoint(james.uriPrefix(Protocol.smtp));
+
+        // create the exchange with the mail message that is multipart with a file and a Hello World text/plain message.
+        Exchange exchange = endpoint.createExchange();
+        AttachmentMessage in = exchange.getIn(AttachmentMessage.class);
+        in.setBody("Hello World");
+        DefaultAttachment att = new DefaultAttachment(new FileDataSource("src/test/data/logo.jpeg"));
+        att.addHeader("Content-Description", "some sample content");
+        in.addAttachmentObject("logo.jpeg", att);
+
+        // create a producer that can produce the exchange (= send the mail)
+        Producer producer = endpoint.createProducer();
+        // start the producer
+        producer.start();
+        // and let it go (processes the exchange by sending the email)
+        producer.process(exchange);
+
+        // END SNIPPET: e1
+
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMessageCount(1);
+
+        template.sendBody("direct:start", "Trigger");
+
+        mock.assertIsSatisfied();
+        Exchange out = mock.assertExchangeReceived(0);
+
+        // plain text
+        assertEquals("Hello World", out.getIn().getBody(String.class));
+
+        // attachment
+        Map<String, Attachment> attachments = out.getIn(AttachmentMessage.class).getAttachmentObjects();
+        assertNotNull(attachments, "Should have attachments");
+        assertEquals(1, attachments.size());
+
+        Attachment attachment = out.getIn(AttachmentMessage.class).getAttachmentObject("logo.jpeg");
+        DataHandler handler = attachment.getDataHandler();
+        assertNotNull(handler, "The logo should be there");
+
+        // content type should match
+        boolean match1 = "image/jpeg; name=logo.jpeg".equals(handler.getContentType());
+        boolean match2 = "application/octet-stream; name=logo.jpeg".equals(handler.getContentType());
+        assertTrue(match1 || match2, "Should match 1 or 2");
+
+        assertEquals("logo.jpeg", handler.getName(), "Handler name should be the file name");
+
+        assertEquals("some sample content", attachment.getHeader("content-description"));
+
+        producer.stop();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            public void configure() {
+                from("direct:start").streamCache("false")
+                        .pollEnrich(james.uriPrefix(Protocol.pop3) + "&initialDelay=100&delay=100", 5000)
+                        .process(e -> {
+                            AttachmentMessage attachmentMessage = e.getIn(AttachmentMessage.class);
+                            Map<String, DataHandler> attachments = attachmentMessage.getAttachments();
+                            assertEquals(1, attachments.size());
+                            assertEquals("logo.jpeg", attachments.keySet().iterator().next());
+                        })
+                        .to("mock:result");
+            }
+        };
+    }
+}
