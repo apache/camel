4c391d138d59e062ca9d65c5f4a7dae3a243ff64
Andrea Cosentino <ancosen@gmail.com>
2024-06-28 11:06:36 +0200

    CAMEL-20798: EndpointServiceLocation on components to make it possible to know which remote system Camel connects to to assist for monitoring and observability - Google Secrets Manager
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-google/camel-google-secret-manager/src/main/java/org/apache/camel/component/google/secret/manager/GoogleSecretManagerEndpoint.java
+++ components/camel-google/camel-google-secret-manager/src/main/java/org/apache/camel/component/google/secret/manager/GoogleSecretManagerEndpoint.java
@@ -21,10 +21,14 @@ import org.apache.camel.Category;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
+import org.apache.camel.spi.EndpointServiceLocation;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.support.DefaultEndpoint;
+import org.apache.camel.util.ObjectHelper;
+
+import java.util.Map;
 
 /**
  * Manage Google Secret Manager Secrets
@@ -38,7 +42,7 @@ import org.apache.camel.support.DefaultEndpoint;
 @Metadata(annotations = {
         "vault=google-secret-manager",
 })
-public class GoogleSecretManagerEndpoint extends DefaultEndpoint {
+public class GoogleSecretManagerEndpoint extends DefaultEndpoint implements EndpointServiceLocation {
 
     @UriParam
     private GoogleSecretManagerConfiguration configuration;
@@ -91,6 +95,19 @@ public class GoogleSecretManagerEndpoint extends DefaultEndpoint {
 
     public SecretManagerServiceClient getClient() {
         return secretManagerServiceClient;
+    }
+
+    @Override
+    public String getServiceUrl() {
+        if (ObjectHelper.isNotEmpty(ObjectHelper.isNotEmpty(configuration.getProject()) && ObjectHelper.isNotEmpty(configuration.getClient()))) {
+            return getServiceProtocol() + ":" + configuration.getProject() + ":" + configuration.getClient();
+        }
+        return null;
+    }
+
+    @Override
+    public String getServiceProtocol() {
+        return "secrets-manager";
     }
 
 }
