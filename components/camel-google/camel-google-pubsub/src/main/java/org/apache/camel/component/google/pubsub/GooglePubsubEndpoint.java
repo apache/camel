056292374bd5375f652bb55b41219971e1209eaf
Andrea Cosentino <ancosen@gmail.com>
2024-06-28 11:04:44 +0200

    CAMEL-20798: EndpointServiceLocation on components to make it possible to know which remote system Camel connects to to assist for monitoring and observability - Google Pubsub
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-google/camel-google-pubsub/src/main/java/org/apache/camel/component/google/pubsub/GooglePubsubEndpoint.java
+++ components/camel-google/camel-google-pubsub/src/main/java/org/apache/camel/component/google/pubsub/GooglePubsubEndpoint.java
@@ -277,7 +277,6 @@ public class GooglePubsubEndpoint extends DefaultEndpoint implements EndpointSer
     public Map<String, String> getServiceMetadata() {
         if (getDestinationName() != null) {
             return Map.of("destinationName", getDestinationName());
-            );
         }
         return null;
     }
