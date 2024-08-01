4d36252b68629ede1764c3dc02d74b3b8ac344a2
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-08-01 08:39:04 +0200

    Splunk HEC add sslContextParameters

--- components/camel-splunk-hec/src/main/java/org/apache/camel/component/splunkhec/SplunkHECComponent.java
+++ components/camel-splunk-hec/src/main/java/org/apache/camel/component/splunkhec/SplunkHECComponent.java
@@ -19,12 +19,20 @@ package org.apache.camel.component.splunkhec;
 import java.util.Map;
 
 import org.apache.camel.Endpoint;
+import org.apache.camel.SSLContextParametersAware;
+import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.annotations.Component;
 import org.apache.camel.support.DefaultComponent;
+import org.apache.camel.support.jsse.SSLContextParameters;
 import org.apache.camel.util.URISupport;
 
 @Component("splunk-hec")
-public class SplunkHECComponent extends DefaultComponent {
+public class SplunkHECComponent extends DefaultComponent implements SSLContextParametersAware {
+
+    @Metadata(label = "security")
+    private SSLContextParameters sslContextParameters;
+    @Metadata(label = "security", defaultValue = "false")
+    private boolean useGlobalSslContextParameters;
 
     public SplunkHECComponent() {
     }
@@ -40,5 +48,30 @@ public class SplunkHECComponent extends DefaultComponent {
         answer.setSplunkURL(remaining);
 
         return answer;
+    }
+
+    public SSLContextParameters getSslContextParameters() {
+        return sslContextParameters;
+    }
+
+    /**
+     * Sets the default SSL configuration to use for all the endpoints. You can also configure it directly at the
+     * endpoint level.
+     */
+    public void setSslContextParameters(SSLContextParameters sslContextParameters) {
+        this.sslContextParameters = sslContextParameters;
+    }
+
+    @Override
+    public boolean isUseGlobalSslContextParameters() {
+        return this.useGlobalSslContextParameters;
+    }
+
+    /**
+     * Enable usage of global SSL context parameters.
+     */
+    @Override
+    public void setUseGlobalSslContextParameters(boolean useGlobalSslContextParameters) {
+        this.useGlobalSslContextParameters = useGlobalSslContextParameters;
     }
 }
