0e9ae21df9ace97847850f91beb481123d165b5b
Claudio Miranda <claudio@claudius.com.br>
2024-08-09 10:52:38 +0100

    CAMEL-21059 Cannot run or export when --repository is set before the integration file https://issues.apache.org/jira/browse/CAMEL-21059

--- dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/ExportTest.java
+++ dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/ExportTest.java
@@ -22,11 +22,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.apache.camel.dsl.jbang.core.common.RuntimeType;
+import org.apache.camel.util.FileUtil;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -41,7 +45,12 @@ class ExportTest {
     @BeforeEach
     public void setup() throws IOException {
         workingDir = Files.createTempDirectory("camel-export").toFile();
-        workingDir.deleteOnExit();
+    }
+
+    @AfterEach
+    public void end() throws IOException {
+        // force removing, since deleteOnExit is not removing.
+        FileUtil.removeDir(workingDir);
     }
 
     private static Stream<Arguments> runtimeProvider() {
@@ -65,6 +74,48 @@ class ExportTest {
         Assertions.assertEquals("1.0.0", model.getVersion());
     }
 
+    @ParameterizedTest
+    @MethodSource("runtimeProvider")
+    public void shouldGenerateProjectWithBuildProperties(RuntimeType rt) throws Exception {
+        Export command = new ExportSpringBoot(new CamelJBangMain());
+        CommandLine.populateCommand(command, "--gav=examples:route:1.0.0", "--dir=" + workingDir,
+                "--runtime=%s".formatted(rt.runtime()), "--build-property=foo=bar", "target/test-classes/route.yaml");
+        int exit = command.doCall();
+
+        Assertions.assertEquals(0, exit);
+        Model model = readMavenModel();
+        Assertions.assertEquals("examples", model.getGroupId());
+        Assertions.assertEquals("route", model.getArtifactId());
+        Assertions.assertEquals("1.0.0", model.getVersion());
+        Assertions.assertEquals("bar", model.getProperties().getProperty("foo"));
+        command.printConfigurationValues("export command");
+    }
+
+    @ParameterizedTest
+    @MethodSource("runtimeProvider")
+    public void testShouldGenerateProjectMultivalue(RuntimeType rt) throws Exception {
+        Export command = new ExportSpringBoot(new CamelJBangMain());
+        CommandLine.populateCommand(command, "--gav=examples:route:1.0.0", "--dir=" + workingDir,
+                "--runtime=%s".formatted(rt.runtime()), "--dep=foo:bar:1.0,jupiter:rocks:2.0",
+                // it's important for the --build-property to be the last parameter to test a previous
+                // export error when this property had arity=*
+                "--build-property=foo=bar", "--build-property=camel=rocks", "target/test-classes/route.yaml");
+        int exit = command.doCall();
+
+        Assertions.assertEquals(0, exit);
+        Model model = readMavenModel();
+        Assertions.assertEquals("examples", model.getGroupId());
+        Assertions.assertEquals("route", model.getArtifactId());
+        Assertions.assertEquals("1.0.0", model.getVersion());
+        Assertions.assertEquals("rocks", model.getProperties().getProperty("camel"));
+        Assertions.assertEquals("bar", model.getProperties().getProperty("foo"));
+        Assertions.assertTrue(containsDependency(model.getDependencies(), "foo", "bar", "1.0"),
+                "Generated pom doesn't contain foo:bar:1.0 dependency");
+        Assertions.assertTrue(containsDependency(model.getDependencies(), "jupiter", "rocks", "2.0"),
+                "Generated pom doesn't contain jupiter:rocks:2.0 dependency");
+        command.printConfigurationValues("export command");
+    }
+
     private Export createCommand(RuntimeType rt, String[] files, String... args) {
         Export command = new ExportSpringBoot(new CamelJBangMain());
         CommandLine.populateCommand(command, "--gav=examples:route:1.0.0", "--dir=" + workingDir, "--quiet",
@@ -80,5 +131,22 @@ class ExportTest {
         Model model = mavenReader.read(new FileReader(f));
         model.setPomFile(f);
         return model;
+    }
+
+    private boolean containsDependency(List<Dependency> deps, String group, String artifact, String version) {
+        Dependency dep = new Dependency();
+        dep.setGroupId(group);
+        dep.setArtifactId(artifact);
+        dep.setVersion(version);
+        boolean found = false;
+        for (int i = 0; i < deps.size() && !found; i++) {
+            Dependency d = deps.get(i);
+            found = toGAV(d).equals(toGAV(dep));
+        }
+        return found;
+    }
+
+    private String toGAV(Dependency d) {
+        return d.getGroupId() + ":" + d.getArtifactId() + ":" + d.getVersion();
     }
 }
