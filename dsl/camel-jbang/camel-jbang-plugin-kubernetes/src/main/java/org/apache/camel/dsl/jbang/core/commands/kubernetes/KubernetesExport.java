58eb955c309c5a83081ea541de8d6a0e8c4106f4
Christoph Deppisch <cdeppisch@redhat.com>
2024-08-12 21:38:19 +0200

    chore(deps): Bump knative-client-version from 6.13.1 to 6.13.2
    - Align with kubernetes-client version and fixes Jbang Kubernetes plugin tests

--- dsl/camel-jbang/camel-jbang-plugin-kubernetes/src/main/java/org/apache/camel/dsl/jbang/core/commands/kubernetes/KubernetesExport.java
+++ dsl/camel-jbang/camel-jbang-plugin-kubernetes/src/main/java/org/apache/camel/dsl/jbang/core/commands/kubernetes/KubernetesExport.java
@@ -283,7 +283,7 @@ public class KubernetesExport extends Export {
 
             // TODO: remove when fixed kubernetes-client version is part of the Quarkus platform
             // pin kubernetes-client to this version because of https://github.com/fabric8io/kubernetes-client/issues/6059
-            addDependencies("io.fabric8:kubernetes-client:6.13.1");
+            addDependencies("io.fabric8:kubernetes-client:6.13.2");
 
             // Configure image builder - use Jib by default
             if (imageBuilder == null) {
