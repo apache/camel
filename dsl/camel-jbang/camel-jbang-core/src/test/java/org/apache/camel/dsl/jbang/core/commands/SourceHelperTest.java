84257c3afc3783c89993f4b07fe39676f66574f2
Claus Ibsen <claus.ibsen@gmail.com>
2024-09-11 08:36:45 +0200

    CAMEL-21194: Added unit test

---
+++ dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/SourceHelperTest.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.dsl.jbang.core.commands;
+
+import java.io.FileNotFoundException;
+
+import org.apache.camel.RuntimeCamelException;
+import org.apache.camel.dsl.jbang.core.common.SourceHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SourceHelperTest {
+
+    @Test
+    public void testResolveSource() {
+        Assertions.assertNotNull(SourceHelper.resolveSource("file:src/test/resources/hello.yaml"));
+        Assertions.assertNotNull(SourceHelper.resolveSource("file:src/test/resources/hello.camel.yaml"));
+        Assertions.assertNotNull(SourceHelper.resolveSource("file:src/test/resources/Hey.java"));
+        Assertions.assertNotNull(SourceHelper.resolveSource("file:src/test/resources/hi.xml"));
+        Assertions.assertNotNull(SourceHelper.resolveSource("file:src/test/resources/hi.camel.xml"));
+
+        try {
+            SourceHelper.resolveSource("file:src/test/resources/unknown.properties");
+            fail("Should throw exception");
+        } catch (RuntimeCamelException e) {
+            Assertions.assertInstanceOf(FileNotFoundException.class, e.getCause());
+        }
+    }
+}
