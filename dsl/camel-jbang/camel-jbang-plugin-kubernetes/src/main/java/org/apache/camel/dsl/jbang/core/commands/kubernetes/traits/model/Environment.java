2c26cc37015bb8a87aae8649bcd05f22e1cc9248
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2024-09-23 15:37:13 +0200

    CAMEL-21152: Create Trait Model

---
+++ dsl/camel-jbang/camel-jbang-plugin-kubernetes/src/main/java/org/apache/camel/dsl/jbang/core/commands/kubernetes/traits/model/Environment.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.dsl.jbang.core.commands.kubernetes.traits.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "configuration", "containerMeta", "enabled", "httpProxy", "vars" })
+public class Environment {
+    @JsonProperty("configuration")
+    @JsonPropertyDescription("Legacy trait configuration parameters. Deprecated: for backward compatibility.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Configuration configuration;
+    @JsonProperty("containerMeta")
+    @JsonPropertyDescription("Enables injection of `NAMESPACE` and `POD_NAME` environment variables (default `true`)")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Boolean containerMeta;
+    @JsonProperty("enabled")
+    @JsonPropertyDescription("Deprecated: no longer in use.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Boolean enabled;
+    @JsonProperty("httpProxy")
+    @JsonPropertyDescription("Propagates the `HTTP_PROXY`, `HTTPS_PROXY` and `NO_PROXY` environment variables (default `true`)")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Boolean httpProxy;
+    @JsonProperty("vars")
+    @JsonPropertyDescription("A list of environment variables to be added to the integration container. The syntax is KEY=VALUE, e.g., `MY_VAR=\"my value\"`. These take precedence over the previously defined environment variables.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private List<String> vars;
+
+    public Environment() {
+    }
+
+    public Configuration getConfiguration() {
+        return this.configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public Boolean getContainerMeta() {
+        return this.containerMeta;
+    }
+
+    public void setContainerMeta(Boolean containerMeta) {
+        this.containerMeta = containerMeta;
+    }
+
+    public Boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Boolean getHttpProxy() {
+        return this.httpProxy;
+    }
+
+    public void setHttpProxy(Boolean httpProxy) {
+        this.httpProxy = httpProxy;
+    }
+
+    public List<String> getVars() {
+        return this.vars;
+    }
+
+    public void setVars(List<String> vars) {
+        this.vars = vars;
+    }
+}
---
+++ dsl/camel-jbang/camel-jbang-plugin-kubernetes/src/main/java/org/apache/camel/dsl/jbang/core/commands/kubernetes/traits/model/Ingress.java
@@ -0,0 +1,175 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.dsl.jbang.core.commands.kubernetes.traits.model;
+
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.Nulls;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "annotations", "auto", "configuration", "enabled", "host", "path", "pathType", "tlsHosts", "tlsSecretName" })
+public class Ingress {
+    @JsonProperty("annotations")
+    @JsonPropertyDescription("The annotations added to the ingress. This can be used to set controller specific annotations, e.g., when using the NGINX Ingress controller: See https://github.com/kubernetes/ingress-nginx/blob/main/docs/user-guide/nginx-configuration/annotations.md")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Map<String, String> annotations;
+    @JsonProperty("auto")
+    @JsonPropertyDescription("To automatically add an ingress whenever the integration uses an HTTP endpoint consumer.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Boolean auto;
+    @JsonProperty("configuration")
+    @JsonPropertyDescription("Legacy trait configuration parameters. Deprecated: for backward compatibility.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Configuration configuration;
+    @JsonProperty("enabled")
+    @JsonPropertyDescription("Can be used to enable or disable a trait. All traits share this common property.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private Boolean enabled;
+    @JsonProperty("host")
+    @JsonPropertyDescription("To configure the host exposed by the ingress.")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private String host;
+    @JsonProperty("path")
+    @JsonPropertyDescription("To configure the path exposed by the ingress (default `/`).")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private String path;
+    @JsonProperty("pathType")
+    @JsonPropertyDescription("To configure the path type exposed by the ingress. One of `Exact`, `Prefix`, `ImplementationSpecific` (default to `Prefix`).")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private PathType pathType;
+    @JsonProperty("tlsHosts")
+    @JsonPropertyDescription("To configure tls hosts")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private List<String> tlsHosts;
+    @JsonProperty("tlsSecretName")
+    @JsonPropertyDescription("To configure tls secret name")
+    @JsonSetter(
+                nulls = Nulls.SKIP)
+    private String tlsSecretName;
+
+    public Ingress() {
+    }
+
+    public Map<String, String> getAnnotations() {
+        return this.annotations;
+    }
+
+    public void setAnnotations(Map<String, String> annotations) {
+        this.annotations = annotations;
+    }
+
+    public Boolean getAuto() {
+        return this.auto;
+    }
+
+    public void setAuto(Boolean auto) {
+        this.auto = auto;
+    }
+
+    public Configuration getConfiguration() {
+        return this.configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public Boolean getEnabled() {
+        return this.enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public PathType getPathType() {
+        return this.pathType;
+    }
+
+    public void setPathType(PathType pathType) {
+        this.pathType = pathType;
+    }
+
+    public List<String> getTlsHosts() {
+        return this.tlsHosts;
+    }
+
+    public void setTlsHosts(List<String> tlsHosts) {
+        this.tlsHosts = tlsHosts;
+    }
+
+    public String getTlsSecretName() {
+        return this.tlsSecretName;
+    }
+
+    public void setTlsSecretName(String tlsSecretName) {
+        this.tlsSecretName = tlsSecretName;
+    }
+
+    public enum PathType {
+        @JsonProperty("Exact")
+        EXACT("Exact"),
+        @JsonProperty("Prefix")
+        PREFIX("Prefix"),
+        @JsonProperty("ImplementationSpecific")
+        IMPLEMENTATIONSPECIFIC("ImplementationSpecific");
+
+        private final String value;
+
+        PathType(String value) {
+            this.value = value;
+        }
+
+        @JsonValue
+        public String getValue() {
+            return this.value;
+        }
+    }
+}
