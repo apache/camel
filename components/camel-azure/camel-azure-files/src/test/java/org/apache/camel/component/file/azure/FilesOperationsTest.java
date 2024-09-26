13c4cd30851061adb1c67dbb448cb8892264f932
Sven Clauw <s.clauw@esm.europa.eu>
2024-09-26 17:39:42 +0200

    CAMEL-21280: Add license & code formatting

--- components/camel-azure/camel-azure-files/src/test/java/org/apache/camel/component/file/azure/FilesOperationsTest.java
+++ components/camel-azure/camel-azure-files/src/test/java/org/apache/camel/component/file/azure/FilesOperationsTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.file.azure;
 
 import com.azure.core.http.policy.AzureSasCredentialPolicy;
@@ -12,7 +28,8 @@ public class FilesOperationsTest extends CamelTestSupport {
     @Test
     void testCreateClientAzureIdentity() {
         var endpoint = context
-                .getEndpoint("azure-files://account.file.core.windows.net/share?credentialType=AZURE_IDENTITY", FilesEndpoint.class);
+                .getEndpoint("azure-files://account.file.core.windows.net/share?credentialType=AZURE_IDENTITY",
+                        FilesEndpoint.class);
 
         var filesOperations = new FilesOperations(endpoint);
         var client = filesOperations.getClient();
@@ -31,7 +48,9 @@ public class FilesOperationsTest extends CamelTestSupport {
     @Test
     void testCreateClientSharedAccountKey() {
         var endpoint = context
-                .getEndpoint("azure-files://account.file.core.windows.net/share?credentialType=SHARED_ACCOUNT_KEY&sharedKey=sharedKey", FilesEndpoint.class);
+                .getEndpoint(
+                        "azure-files://account.file.core.windows.net/share?credentialType=SHARED_ACCOUNT_KEY&sharedKey=sharedKey",
+                        FilesEndpoint.class);
 
         var filesOperations = new FilesOperations(endpoint);
         var client = filesOperations.getClient();
@@ -50,7 +69,9 @@ public class FilesOperationsTest extends CamelTestSupport {
     @Test
     void testCreateClientAzureSAS() {
         var endpoint = context
-                .getEndpoint("azure-files://account.file.core.windows.net/share?credentialType=AZURE_SAS&sv=2022-11-02&ss=f&srt=sco&sp=rwdlc&se=2023-06-18T22:29:13Z&st=2023-06-05T14:29:13Z&spr=https&sig=MPsMh8zci0v3To7IT9SKdaFGZV8ezno63m9C8s9bdVQ%3D", FilesEndpoint.class);
+                .getEndpoint(
+                        "azure-files://account.file.core.windows.net/share?credentialType=AZURE_SAS&sv=2022-11-02&ss=f&srt=sco&sp=rwdlc&se=2023-06-18T22:29:13Z&st=2023-06-05T14:29:13Z&spr=https&sig=MPsMh8zci0v3To7IT9SKdaFGZV8ezno63m9C8s9bdVQ%3D",
+                        FilesEndpoint.class);
 
         var filesOperations = new FilesOperations(endpoint);
         var client = filesOperations.getClient();
