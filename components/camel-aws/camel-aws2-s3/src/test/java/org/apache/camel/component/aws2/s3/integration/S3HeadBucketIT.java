d0fd1bbc807ac882a5dd39dddb7112cb458e4bc0
Andrea Cosentino <ancosen@gmail.com>
2024-10-09 16:25:57 +0200

    CAMEL-21262 - Camel-AWS2-S3: Explicitly add Producer operations HeadObject and HeadBucket (#15902)
    * CAMEL-21262 - Camel-AWS2-S3: Explicitly add Producer operations HeadObject and HeadBucket - HeadBucket
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    * CAMEL-21262 - Camel-AWS2-S3: Explicitly add Producer operations HeadObject and HeadBucket - HeadObject
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>
    
    ---------
    
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

---
+++ components/camel-aws/camel-aws2-s3/src/test/java/org/apache/camel/component/aws2/s3/integration/S3HeadBucketIT.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.aws2.s3.integration;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.aws2.s3.AWS2S3Constants;
+import org.apache.camel.component.aws2.s3.AWS2S3Operations;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.jupiter.api.Test;
+
+public class S3HeadBucketIT extends Aws2S3Base {
+
+    @EndpointInject
+    private ProducerTemplate template;
+
+    @EndpointInject("mock:result")
+    private MockEndpoint result;
+
+    @Test
+    public void sendInHeadBucket() throws InterruptedException {
+        result.expectedMessageCount(1);
+
+        template.send("direct:headBucket", exchange -> {
+            exchange.getIn().setHeader(AWS2S3Constants.S3_OPERATION, AWS2S3Operations.headBucket);
+            exchange.getIn().setHeader(AWS2S3Constants.BUCKET_NAME, "mycamel");
+        });
+
+        result.assertIsSatisfied();
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                String awsEndpoint = "aws2-s3://test-ss3-s3";
+
+                from("direct:headBucket").to(awsEndpoint).log("${body}").to("mock:result");
+
+            }
+        };
+    }
+}
