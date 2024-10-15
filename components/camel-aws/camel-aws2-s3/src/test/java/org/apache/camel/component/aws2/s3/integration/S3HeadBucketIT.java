3bc69c499ca740e3607768116952288e52dab553
Claus Ibsen <claus.ibsen@gmail.com>
2024-10-15 07:59:31 +0200

    CAMEL-21314: Make test throw exception to see the failure

--- components/camel-aws/camel-aws2-s3/src/test/java/org/apache/camel/component/aws2/s3/integration/S3HeadBucketIT.java
+++ components/camel-aws/camel-aws2-s3/src/test/java/org/apache/camel/component/aws2/s3/integration/S3HeadBucketIT.java
@@ -16,32 +16,43 @@
  */
 package org.apache.camel.component.aws2.s3.integration;
 
+import java.util.UUID;
+
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.aws2.s3.AWS2S3Constants;
 import org.apache.camel.component.aws2.s3.AWS2S3Operations;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
 
 public class S3HeadBucketIT extends Aws2S3Base {
 
     @EndpointInject
     private ProducerTemplate template;
 
-    @EndpointInject("mock:result")
-    private MockEndpoint result;
+    @EndpointInject("mock:bucket")
+    private MockEndpoint bucket;
+
+    @EndpointInject("mock:noBucket")
+    private MockEndpoint noBucket;
 
     @Test
-    public void sendInHeadBucket() throws InterruptedException {
-        result.expectedMessageCount(1);
+    public void sendInHeadBucket() throws Exception {
+        bucket.expectedMessageCount(0);
+        noBucket.expectedMessageCount(1);
 
-        template.send("direct:headBucket", exchange -> {
+        Exchange res = template.send("direct:headBucket", exchange -> {
             exchange.getIn().setHeader(AWS2S3Constants.S3_OPERATION, AWS2S3Operations.headBucket);
-            exchange.getIn().setHeader(AWS2S3Constants.BUCKET_NAME, "mycamel");
+            exchange.getIn().setHeader(AWS2S3Constants.BUCKET_NAME, "doesnotexist" + UUID.randomUUID().toString());
         });
+        if (res.getException() != null) {
+            throw res.getException();
+        }
 
-        result.assertIsSatisfied();
+        MockEndpoint.assertIsSatisfied(context);
     }
 
     @Override
@@ -51,8 +62,12 @@ public class S3HeadBucketIT extends Aws2S3Base {
             public void configure() {
                 String awsEndpoint = "aws2-s3://test-ss3-s3";
 
-                from("direct:headBucket").to(awsEndpoint).log("${body}").to("mock:result");
-
+                from("direct:headBucket")
+                    .doTry()
+                        .to(awsEndpoint).log("${body}").to("mock:bucket")
+                    .doCatch(NoSuchBucketException.class)
+                        .to("mock:noBucket")
+                    .end();
             }
         };
     }
