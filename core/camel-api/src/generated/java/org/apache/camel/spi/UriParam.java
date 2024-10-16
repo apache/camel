8451b94f5e6157cbed6316b1d702e309bdd54a9f
Aurélien Pupier <apupier@redhat.com>
2024-10-16 17:58:02 +0200

    CAMEL-21356 - Javadoc compliance with doclint for Java 17
    in spi-annotations, tooling-model and util-json
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- core/camel-api/src/generated/java/org/apache/camel/spi/UriParam.java
+++ core/camel-api/src/generated/java/org/apache/camel/spi/UriParam.java
@@ -33,21 +33,21 @@ public @interface UriParam {
 
     /**
      * Returns the name of the parameter.
-     * <p/>
+     *
      * If this is not specified then the name of the field or property which has this annotation is used.
      */
     String name() default "";
 
     /**
      * A human display name of the parameter.
-     * <p/>
+     *
      * This is used for documentation and tooling only.
      */
     String displayName() default "";
 
     /**
      * The default value of the parameter.
-     * <p/>
+     *
      * Note that this attribute is only for documentation purpose. The default value in use at runtime is the value the
      * Java field was assigned.
      */
@@ -55,35 +55,35 @@ public @interface UriParam {
 
     /**
      * A special note about the default value.
-     * <p/>
+     *
      * This can be used to document special cases about the default value.
      */
     String defaultValueNote() default "";
 
     /**
      * Returns a description of this parameter.
-     * <p/>
+     *
      * This is used for documentation and tooling only.
      */
     String description() default "";
 
     /**
      * Allows to define enums this options accepts.
-     * <p/>
+     *
      * If the type is already an enum, then this option should not be used; instead you can use this option when the
      * type is a String that only accept certain values.
-     * <p/>
+     *
      * Multiple values is separated by comma.
      */
     String enums() default "";
 
     /**
      * To associate this parameter with label(s).
-     * <p/>
+     *
      * Multiple labels can be defined as a comma separated value.
-     * <p/>
-     * The labels is intended for grouping the parameters, such as <tt>consumer</tt>, <tt>producer</tt>,
-     * <tt>common</tt>, <tt>security</tt>, etc.
+     *
+     * The labels is intended for grouping the parameters, such as <var>consumer</var>, <var>producer</var>,
+     * <var>common</var>, <var>security</var>, etc.
      */
     String label() default "";
 
@@ -94,29 +94,29 @@ public @interface UriParam {
 
     /**
      * To re-associate the preferred Java type of this parameter.
-     * <p/>
+     *
      * This is used for parameters which are of a specialized type but can be configured by another Java type, such as
      * from a String.
      */
     String javaType() default "";
 
     /**
-     * If the parameter can be configured multiple times, such as configuring options to a <tt>Map</tt> type.
+     * If the parameter can be configured multiple times, such as configuring options to a <var>Map</var> type.
      */
     boolean multiValue() default false;
 
     /**
      * If the parameter must be configured with a prefix.
-     * <p/>
-     * For example to configure scheduler options, the parameters is prefixed with <tt>scheduler.foo=bar</tt>
+     *
+     * For example to configure scheduler options, the parameters is prefixed with <code>scheduler.foo=bar</code>
      */
     String prefix() default "";
 
     /**
      * If the parameter can be configured with an optional prefix.
-     * <p/>
-     * For example to configure consumer options, the parameters can be prefixed with <tt>consumer.</tt>, eg
-     * <tt>consumer.delay=5000</tt>
+     *
+     * For example to configure consumer options, the parameters can be prefixed with <var>consumer.</var>, eg
+     * <code>consumer.delay=5000</code>
      */
     String optionalPrefix() default "";
 
--- tooling/spi-annotations/src/main/java/org/apache/camel/spi/UriEndpoint.java
+++ tooling/spi-annotations/src/main/java/org/apache/camel/spi/UriEndpoint.java
@@ -40,28 +40,28 @@ public @interface UriEndpoint {
 
     /**
      * Represents the URI scheme name of this endpoint.
-     * <p/>
-     * Multiple scheme names can be defined as a comma separated value. For example to associate <tt>http</tt> and
-     * <tt>https</tt> to the same endpoint implementation.
-     * <p/>
+     *
+     * Multiple scheme names can be defined as a comma separated value. For example to associate <var>http</var> and
+     * <var>https</var> to the same endpoint implementation.
+     *
      * The order of the scheme names here should be the same order as in {@link #extendsScheme()} so their are paired.
-     * <p/>
+     *
      * The schema name must be lowercase, it may contain dashes as well. For example: robot-framework.
      */
     String scheme();
 
     /**
      * Used when an endpoint is extending another endpoint
-     * <p/>
-     * Multiple scheme names can be defined as a comma separated value. For example to associate <tt>ftp</tt> and
-     * <tt>ftps</tt> to the same endpoint implementation. The order of the scheme names here should be the same order as
-     * in {@link #scheme()} so their are paired.
+     *
+     * Multiple scheme names can be defined as a comma separated value. For example to associate <var>ftp</var> and
+     * <var>ftps</var> to the same endpoint implementation. The order of the scheme names here should be the same order
+     * as in {@link #scheme()} so their are paired.
      */
     String extendsScheme() default "";
 
     /**
      * Represent the URI syntax the endpoint must use.
-     * <p/>
+     *
      * The syntax follows the patterns such as:
      * <ul>
      * <li>scheme:host:port</li>
@@ -71,7 +71,7 @@ public @interface UriEndpoint {
      * </ul>
      * Where each path maps to the name of the endpoint {@link org.apache.camel.spi.UriPath} option. The query
      * parameters is implied and should not be included in the syntax.
-     * <p/>
+     *
      * Some examples:
      * <ul>
      * <li>file:directoryName</li>
@@ -99,16 +99,17 @@ public @interface UriEndpoint {
 
     /**
      * A human-readable title of this entity, such as the component name of the this endpoint.
-     * <p/>
+     *
      * For example: JMS, MQTT, Netty HTTP, SAP NetWeaver
      */
     String title();
 
     /**
      * To associate this endpoint with category(ies).
-     * <p/>
-     * This category is intended for grouping the endpoints, such as <tt>Category.CORE</tt>, <tt>Category.FILE</tt>,
-     * <tt>Category.DATABASE</tt>, etc, but supplied with as array of {@link Category} enums.
+     * <p>
+     * This category is intended for grouping the endpoints, such as <code>Category.CORE</code>,
+     * <code>Category.FILE</code>, <code>Category.DATABASE</code>, etc, but supplied with as array of {@link Category}
+     * enums.
      * </p>
      * For example: @UriEndpoint(category = {Category.CORE, Category.DATABASE})
      */
@@ -116,28 +117,28 @@ public @interface UriEndpoint {
 
     /**
      * Whether this endpoint can only be used as a producer.
-     * <p/>
+     *
      * By default, its assumed the endpoint can be used as both consumer and producer.
      */
     boolean producerOnly() default false;
 
     /**
      * Whether this endpoint can only be used as a consumer.
-     * <p/>
+     *
      * By default, its assumed the endpoint can be used as both consumer and producer.
      */
     boolean consumerOnly() default false;
 
     /**
      * Should all properties be known or does the endpoint allow unknown options?
-     * <p/>
-     * <tt>lenient = false</tt> means that the endpoint should validate that all given options is known and configured
-     * properly. <tt>lenient = true</tt> means that the endpoint allows additional unknown options to be passed to it
-     * but does not throw a ResolveEndpointFailedException when creating the endpoint.
-     * <p/>
+     *
+     * <code>lenient = false</code> means that the endpoint should validate that all given options is known and
+     * configured properly. <code>lenient = true</code> means that the endpoint allows additional unknown options to be
+     * passed to it but does not throw a ResolveEndpointFailedException when creating the endpoint.
+     *
      * This options is used by a few components for instance the HTTP based that can have dynamic URI options appended
      * that is targeted for an external system.
-     * <p/>
+     *
      * Most endpoints is configured to be <b>not</b> lenient.
      */
     boolean lenientProperties() default false;
@@ -159,11 +160,11 @@ public @interface UriEndpoint {
     /**
      * The class that contains all the name of headers that are supported by the consumer and/or producer. The name of
      * the headers are defined as {@code String} constants in the headers class.
-     * <p/>
+     *
      * The class to provide can be any class but by convention, we would expect a class whose name is of type
      * <i>xxxConstants</i> where <i>xxx</i> is the name of the corresponding component like for example
      * <i>FtpConstants</i> for the component <i>camel-ftp</i>.
-     * <p/>
+     *
      * The metadata of a given header are retrieved directly from the annotation {@code @Metadata} added to the
      * {@code String} constant representing its name and defined in the headers class.
      */
@@ -172,9 +173,9 @@ public @interface UriEndpoint {
     /**
      * The name of the field to get or the name of the method without parameters to invoke to get the name of the
      * headers defined in an enum.
-     * <p/>
+     *
      * Only took into account if and only if the class defined as {@code headersClass} is an enum.
-     * <p/>
+     *
      * For example, assuming that {@code SomeEnum} has been configured as the {@code headersClass} of a given component,
      * since the name of the header is actually the value of the field {@code headerName}, the element
      * {@code headersNameProvider} should be set to {@code "headerName"} to get the expected header names.
