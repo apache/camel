085f426ae43b1b8c6e59edefdee486ed11bb4622
Claus Ibsen <claus.ibsen@gmail.com>
2024-10-16 10:12:34 +0200

    Regen

--- core/camel-main/src/main/java/org/apache/camel/main/BaseMainSupport.java
+++ core/camel-main/src/main/java/org/apache/camel/main/BaseMainSupport.java
@@ -1721,7 +1721,8 @@ public abstract class BaseMainSupport extends BaseService {
             tmp = new TrustManagersParameters();
             tmp.setCamelContext(camelContext);
             tmp.setTrustManager(TrustAllTrustManager.INSTANCE);
-            LOG.warn("Trust all certifications enabled. Using this in production can expose the application to man-in-the-middle attacks");
+            LOG.warn(
+                    "Trust all certifications enabled. Using this in production can expose the application to man-in-the-middle attacks");
         } else if (sslConfig.getTrustStore() != null) {
             KeyStoreParameters tsp = new KeyStoreParameters();
             String store = sslConfig.getTrustStore();
