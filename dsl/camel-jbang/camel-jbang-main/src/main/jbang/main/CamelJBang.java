81536cf3931b00c11c4d50ea9bb2d068cd7c354e
Claus Ibsen <claus.ibsen@gmail.com>
2024-10-25 09:01:46 +0200

    Upgrade camel-jbang to 4.8.1

--- dsl/camel-jbang/camel-jbang-main/src/main/jbang/main/CamelJBang.java
+++ dsl/camel-jbang/camel-jbang-main/src/main/jbang/main/CamelJBang.java
@@ -19,8 +19,8 @@
 
 //JAVA 17+
 //REPOS central=https://repo1.maven.org/maven2,apache-snapshot=https://repository.apache.org/content/groups/snapshots/
-//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.8.0}@pom
-//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.8.0}
+//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.8.1}@pom
+//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.8.1}
 //DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.8.0}
 
 package main;
