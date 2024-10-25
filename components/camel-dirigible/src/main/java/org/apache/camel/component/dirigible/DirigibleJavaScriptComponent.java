11b183ed62f64028e62fa2975a15e198894ee17c
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-10-25 15:33:22 +0300

    remove logger

--- components/camel-dirigible/src/main/java/org/apache/camel/component/dirigible/DirigibleJavaScriptComponent.java
+++ components/camel-dirigible/src/main/java/org/apache/camel/component/dirigible/DirigibleJavaScriptComponent.java
@@ -16,21 +16,13 @@
  */
 package org.apache.camel.component.dirigible;
 
-import java.util.Map;
-
 import org.apache.camel.Endpoint;
 import org.apache.camel.support.DefaultComponent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 @org.apache.camel.spi.annotations.Component(DirigibleJavaScriptEndpoint.SCHEME)
 public class DirigibleJavaScriptComponent extends DefaultComponent {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(DirigibleJavaScriptComponent.class);
-
-    public DirigibleJavaScriptComponent() {
-        LOGGER.debug("Creating [{}]", this);
-    }
 
     @Override
     protected Endpoint createEndpoint(String uri, String remaining, Map<String, Object> parameters) throws Exception {
