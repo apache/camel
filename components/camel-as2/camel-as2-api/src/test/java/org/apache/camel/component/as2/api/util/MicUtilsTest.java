281692df4f18434116663ea89664af2b90ce0d07
Jono Morris <jono@apache.org>
2024-11-10 02:20:43 +1300

    CAMEL-21403: retrieve full message for content containing non ASCII chars (#16150)
    * CAMEL-21403 allow for non ASCII chars
    
    * CAMEL-21403 improve flaky test
    
    * CAMEL-21403 remove commented code

--- components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/util/MicUtilsTest.java
+++ components/camel-as2/camel-as2-api/src/test/java/org/apache/camel/component/as2/api/util/MicUtilsTest.java
@@ -76,6 +76,33 @@ public class MicUtilsTest {
                                               + "UNT+23+00000000000117'\n"
                                               + "UNZ+1+00000000000778'";
 
+    private static final String EDI_MESSAGE_WITH_NON_ASCII = "UNB+UNOA:1+005435656:1+006415160:1+060515:1434+00000000000778'\n"
+                                                             + "UNH+00000000000117+INVOIC:D:97B:UN'\n"
+                                                             + "BGM+380+342459+9'\n"
+                                                             + "DTM+3:20060515:102'\n"
+                                                             + "RFF+ON:521052'\n"
+                                                             + "NAD+BY+792820524::16++CUMMINS MID-RANGE ENGINE PLANT ΠΠΠ'\n"
+                                                             + "NAD+SE+005435656::16++GENERAL WIDGET COMPANY óóó'\n"
+                                                             + "CUX+1:USD'\n"
+                                                             + "LIN+1++157870:IN'\n"
+                                                             + "IMD+F++:::WIDGET ΣΣΣ'\n"
+                                                             + "QTY+47:1020:EA'\n"
+                                                             + "ALI+US'\n"
+                                                             + "MOA+203:1202.58'\n"
+                                                             + "PRI+INV:1.179'\n"
+                                                             + "LIN+2++157871:IN'\n"
+                                                             + "IMD+F++:::DIFFERENT WIDGET ΦΦΦ'\n"
+                                                             + "QTY+47:20:EA'\n"
+                                                             + "ALI+JP'\n"
+                                                             + "MOA+203:410'\n"
+                                                             + "PRI+INV:20.5'\n"
+                                                             + "UNS+S'\n"
+                                                             + "MOA+39:2137.58'\n"
+                                                             + "ALC+C+ABG'\n"
+                                                             + "MOA+8:525'\n"
+                                                             + "UNT+23+00000000000117'\n"
+                                                             + "UNZ+1+00000000000778'";
+
     private static final String EXPECTED_MESSAGE_DIGEST_ALGORITHM = "sha1";
     private static final String EXPECTED_ENCODED_MESSAGE_DIGEST = "XUt+ug5GEDD0X9+Nv8DGYZZThOQ=";
 
@@ -110,6 +137,34 @@ public class MicUtilsTest {
         LOG.debug("Encoded Message Digest: {}", receivedContentMic.getEncodedMessageDigest());
         assertEquals(EXPECTED_ENCODED_MESSAGE_DIGEST, receivedContentMic.getEncodedMessageDigest(),
                 "Unexpected encoded message digest value");
+    }
+
+    // verify that a MIC is calculated correctly for an EDI message containing non ASCII chars
+    @Test
+    public void createReceivedContentMicWithNonAsciiContentTest() throws Exception {
+
+        BasicClassicHttpRequest request = new BasicClassicHttpRequest("POST", "/");
+        request.addHeader(AS2Header.DISPOSITION_NOTIFICATION_OPTIONS, DISPOSITION_NOTIFICATION_OPTIONS_VALUE);
+        request.addHeader(AS2Header.CONTENT_TYPE, "application/edifact;charset=UTF-8");
+
+        InputStream is = new ApplicationEDIFACTEntity(
+                EDI_MESSAGE_WITH_NON_ASCII, StandardCharsets.UTF_8.name(), AS2TransferEncoding.NONE, true, "filename.txt")
+                .getContent();
+        request.setEntity(new BasicHttpEntity(is, ContentType.create(CONTENT_TYPE_VALUE, StandardCharsets.UTF_8)));
+        ReceivedContentMic receivedContentMic = MicUtils.createReceivedContentMic(request, null, null);
+
+        assertNotNull(receivedContentMic, "Failed to create Received Content MIC");
+        assertEquals(EXPECTED_MESSAGE_DIGEST_ALGORITHM, receivedContentMic.getDigestAlgorithmId(),
+                "Unexpected digest algorithm value");
+
+        // calculate the MIC of the EDI message directly for comparison
+        String expectedDigest = new ReceivedContentMic(
+                "sha1", MicUtils.createMic(
+                        // the entity parser appends 'CR' and 'LF' for each line
+                        EDI_MESSAGE_WITH_NON_ASCII.replaceAll("\n", "\r\n").getBytes(StandardCharsets.UTF_8), "sha1"))
+                .getEncodedMessageDigest();
+
+        assertEquals(expectedDigest, receivedContentMic.getEncodedMessageDigest(), "Unexpected encoded message digest value");
     }
 
     @ParameterizedTest
