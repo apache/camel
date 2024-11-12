c66cf053fba35b83316f11ecf06a177eb2afb40a
Andrea Cosentino <ancosen@gmail.com>
2024-11-12 11:16:48 +0100

    Upgrade Camel Kamelets to version 4.8.1 (#16235)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogDoc.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/catalog/CatalogDoc.java
@@ -98,7 +98,7 @@ public class CatalogDoc extends CamelCommand {
     boolean headers;
 
     @CommandLine.Option(names = {
-            "--kamelets-version" }, description = "Apache Camel Kamelets version", defaultValue = "4.8.0")
+            "--kamelets-version" }, description = "Apache Camel Kamelets version", defaultValue = "4.8.1")
     String kameletsVersion;
 
     CamelCatalog catalog;
