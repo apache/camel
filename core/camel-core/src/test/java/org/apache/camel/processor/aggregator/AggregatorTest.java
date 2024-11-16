e7d2ebce5abd5fe4ffb09a695e54b040635e5dc6
Claus Ibsen <claus.ibsen@gmail.com>
2024-11-16 20:20:28 +0100

    Flaky test

--- core/camel-core/src/test/java/org/apache/camel/processor/aggregator/AggregatorTest.java
+++ core/camel-core/src/test/java/org/apache/camel/processor/aggregator/AggregatorTest.java
@@ -37,7 +37,7 @@ public class AggregatorTest extends ContextTestSupport {
         // lets send a large batch of messages
         for (int i = 1; i <= messageCount; i++) {
             String body = "message:" + i;
-            template.sendBodyAndHeader("direct:start", body, "cheese", 123);
+            template.sendBodyAndHeader("direct:many", body, "cheese", 123);
         }
 
         resultEndpoint.assertIsSatisfied();
@@ -85,6 +85,9 @@ public class AggregatorTest extends ContextTestSupport {
                 // START SNIPPET: ex
                 // in this route we aggregate all from direct:state based on the
                 // header id cheese
+                from("direct:many").aggregate(header("cheese"), new UseLatestAggregationStrategy()).completionTimeout(2000)
+                        .completionTimeoutCheckerInterval(100).to("mock:result");
+
                 from("direct:start").aggregate(header("cheese"), new UseLatestAggregationStrategy()).completionTimeout(100)
                         .completionTimeoutCheckerInterval(10).to("mock:result");
 
