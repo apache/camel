3fc91face93506762cb407fce95a8c6e0b9b2dfa
Thomas Gantenbein <thomas.gantenbein@clavisit.com>
2024-11-26 17:49:12 +0100

    CAMEL-21472: try assign new traceId for exchanges that do not have a parent span

--- components/camel-opentelemetry/src/main/java/org/apache/camel/opentelemetry/OpenTelemetryTracer.java
+++ components/camel-opentelemetry/src/main/java/org/apache/camel/opentelemetry/OpenTelemetryTracer.java
@@ -163,8 +163,9 @@ public class OpenTelemetryTracer extends org.apache.camel.tracing.Tracer {
             builder = builder.setParent(Context.current().with(spanFromExchange.getOpenTelemetrySpan()));
             baggage = spanFromExchange.getBaggage();
         } else {
+            builder.setNoParent();
             ExtractAdapter adapter = sd.getExtractAdapter(exchange.getIn().getHeaders(), encoding);
-            Context ctx = getContextPropagators().getTextMapPropagator().extract(Context.current(), adapter,
+            Context ctx = getContextPropagators().getTextMapPropagator().extract(Context.root(), adapter,
                     new OpenTelemetryGetter(adapter));
             Span span = Span.fromContext(ctx);
             baggage = Baggage.fromContext(ctx);
