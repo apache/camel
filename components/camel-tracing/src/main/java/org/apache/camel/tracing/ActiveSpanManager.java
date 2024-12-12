ac34f977b773a6f76ef7dd86dc444b4619f0946d
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2024-12-12 12:42:37 +0100

    feat(components): deprecate tracing MDC
    Favoring instead the instrumentation provided into each specific tracing framework.
    
    Closes CAMEL-21538

--- components/camel-tracing/src/main/java/org/apache/camel/tracing/ActiveSpanManager.java
+++ components/camel-tracing/src/main/java/org/apache/camel/tracing/ActiveSpanManager.java
@@ -27,7 +27,11 @@ import org.slf4j.MDC;
  */
 public final class ActiveSpanManager {
 
+    @Deprecated
+    // Use specific MDC instrumentation provided by your tracing/telemetry SDK instead
     public static final String MDC_TRACE_ID = "trace_id";
+    @Deprecated
+    // Use specific MDC instrumentation provided by your tracing/telemetry SDK instead
     public static final String MDC_SPAN_ID = "span_id";
     private static final Logger LOG = LoggerFactory.getLogger(ActiveSpanManager.class);
 
