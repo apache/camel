10a503016b160ab70923d97fbd3f8999bcb51713
Freeman Fang <freeman.fang@gmail.com>
2024-12-16 09:54:46 -0500

    [CAMEL-21548]camel-cxf - Upgrade to 4.1

--- components/camel-cxf/camel-cxf-rest/src/test/java/org/apache/camel/component/cxf/jaxrs/testbean/CustomerService.java
+++ components/camel-cxf/camel-cxf-rest/src/test/java/org/apache/camel/component/cxf/jaxrs/testbean/CustomerService.java
@@ -16,11 +16,7 @@
  */
 package org.apache.camel.component.cxf.jaxrs.testbean;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
@@ -32,122 +28,39 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
-import org.apache.cxf.common.util.StringUtils;
-
 @Path("/customerservice/")
-public class CustomerService {
-    private final AtomicLong currentId = new AtomicLong(123L);
-    private final Map<Long, Customer> customers = new ConcurrentHashMap<>();
-    private final Map<Long, Order> orders = new ConcurrentHashMap<>();
-
-    public CustomerService() {
-        init();
-    }
+public interface CustomerService {
 
     @GET
     @Path("/customers/{id}/")
-    public Customer getCustomer(@PathParam("id") String id) {
-        long idNumber = Long.parseLong(id);
-        Customer c = customers.get(idNumber);
-        return c;
-    }
+    public Customer getCustomer(@PathParam("id") String id);
 
     @GET
     @Path("/customers")
-    public Customer getCustomerByQueryParam(@QueryParam("id") String id) {
-        long idNumber = Long.parseLong(id);
-        Customer c = customers.get(idNumber);
-        return c;
-    }
+    public Customer getCustomerByQueryParam(@QueryParam("id") String id);
 
     @GET
     @Path("/customers/")
     @Produces("application/xml")
-    public List<Customer> getCustomers() {
-        List<Customer> list = new ArrayList<>(customers.values());
-        return list;
-    }
+    public List<Customer> getCustomers();
 
     @PUT
     @Path("/customers/")
-    public Response updateCustomer(Customer customer) {
-        Customer c = customers.get(customer.getId());
-        Response r;
-        if (c != null) {
-            customers.put(customer.getId(), customer);
-            r = Response.ok().build();
-        } else {
-            r = Response.status(406).entity("Cannot find the customer!").build();
-        }
-
-        return r;
-    }
+    public Response updateCustomer(Customer customer);
 
     @POST
     @Path("/customers/")
-    public Response addCustomer(Customer customer) {
-        if (StringUtils.isEmpty(customer.getName())) {
-            return Response.status(422).build();
-        }
-
-        customer.setId(currentId.incrementAndGet());
-
-        customers.put(customer.getId(), customer);
-
-        return Response.ok(customer).build();
-    }
+    public Response addCustomer(Customer customer);
 
     @POST
     @Path("/customersUniqueResponseCode/")
-    public Response addCustomerUniqueResponseCode(Customer customer) {
-        customer.setId(currentId.incrementAndGet());
-
-        customers.put(customer.getId(), customer);
-
-        return Response.status(201).entity(customer).build();
-    }
+    public Response addCustomerUniqueResponseCode(Customer customer);
 
     @DELETE
     @Path("/customers/{id}/")
-    public Response deleteCustomer(@PathParam("id") String id) {
-        long idNumber = Long.parseLong(id);
-        Customer c = customers.get(idNumber);
-
-        Response r;
-        if (c != null) {
-            r = Response.ok().build();
-            customers.remove(idNumber);
-        } else {
-            r = Response.notModified().build();
-        }
-        if (idNumber == currentId.get()) {
-            currentId.decrementAndGet();
-        }
-        return r;
-    }
+    public Response deleteCustomer(@PathParam("id") String id);
 
     @Path("/orders/{orderId}/")
-    public Order getOrder(@PathParam("orderId") String orderId) {
-        long idNumber = Long.parseLong(orderId);
-        Order c = orders.get(idNumber);
-        return c;
-    }
-
-    final void init() {
-        Customer c = new Customer();
-        c.setName("John");
-        c.setId(123);
-        customers.put(c.getId(), c);
-
-        c = new Customer();
-        c.setName("Dan");
-        c.setId(113);
-        customers.put(c.getId(), c);
-
-        Order o = new Order();
-        o.setDescription("order 223");
-        o.setId(223);
-        orders.put(o.getId(), o);
-    }
+    public Order getOrder(@PathParam("orderId") String orderId);
 
 }
