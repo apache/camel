b67b9823dfed656230c3fed1e718a40ad9e28a15
Claus Ibsen <claus.ibsen@gmail.com>
2024-12-31 11:45:56 +0100

    CAMEL-21274: camel-test - Add option to dump routes

--- components/camel-test/camel-test-spring-junit5/src/main/java/org/apache/camel/test/spring/junit5/CamelSpringBootTest.java
+++ components/camel-test/camel-test-spring-junit5/src/main/java/org/apache/camel/test/spring/junit5/CamelSpringBootTest.java
@@ -26,12 +26,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+/**
+ * Camel Spring Boot unit test.
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
 @ExtendWith(SpringExtension.class)
-@TestExecutionListeners(
-                        value = {
+@TestExecutionListeners(value = {
                                 CamelSpringTestContextLoaderTestExecutionListener.class,
                                 CamelSpringBootExecutionListener.class,
                                 StopWatchTestExecutionListener.class
