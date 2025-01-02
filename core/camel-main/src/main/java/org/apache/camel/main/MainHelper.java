bf1d8c2a9dfc1eb7bdf04caea9b196aabe470578
Claus Ibsen <claus.ibsen@gmail.com>
2025-01-02 07:14:22 +0100

    CAMEL-21575: camel-main - Concurrency issue on JDK21.

--- core/camel-main/src/main/java/org/apache/camel/main/MainHelper.java
+++ core/camel-main/src/main/java/org/apache/camel/main/MainHelper.java
@@ -24,7 +24,6 @@ import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
