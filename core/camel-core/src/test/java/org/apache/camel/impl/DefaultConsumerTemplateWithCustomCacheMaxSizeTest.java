3bb7ca4f22419364382be6cb8e057d81c856568e
Claus Ibsen <claus.ibsen@gmail.com>
2025-01-08 16:34:48 +0100

    Flaky test on CI on slow CI servers

--- core/camel-core/src/test/java/org/apache/camel/impl/DefaultConsumerTemplateWithCustomCacheMaxSizeTest.java
+++ core/camel-core/src/test/java/org/apache/camel/impl/DefaultConsumerTemplateWithCustomCacheMaxSizeTest.java
@@ -53,7 +53,7 @@ public class DefaultConsumerTemplateWithCustomCacheMaxSizeTest extends ContextTe
 
         // the eviction is async so force cleanup
         template.cleanUp();
-        await().atMost(1, TimeUnit.SECONDS).until(() -> template.getCurrentCacheSize() == 200);
+        await().atMost(5, TimeUnit.SECONDS).until(() -> template.getCurrentCacheSize() == 200);
         assertEquals(200, template.getCurrentCacheSize(), "Size should be 200");
         template.stop();
 
