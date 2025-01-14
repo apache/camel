d30e62d7d6dc732334612ed7fb60fd0303c05cc7
Pulin Kumar <pulin@backbase.com>
2025-01-14 12:33:42 +0530

    Camel ASB: producer IT test cases updated for queues and scheduled messages

--- components/camel-azure/camel-azure-servicebus/src/test/java/org/apache/camel/component/azure/servicebus/integration/BaseServiceBusTestSupport.java
+++ components/camel-azure/camel-azure-servicebus/src/test/java/org/apache/camel/component/azure/servicebus/integration/BaseServiceBusTestSupport.java
@@ -25,6 +25,9 @@ import com.azure.messaging.servicebus.ServiceBusProcessorClient;
 import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
 import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClient;
 import com.azure.messaging.servicebus.administration.ServiceBusAdministrationClientBuilder;
+import com.azure.messaging.servicebus.administration.models.CreateQueueOptions;
+import com.azure.messaging.servicebus.administration.models.CreateSubscriptionOptions;
+import com.azure.messaging.servicebus.administration.models.CreateTopicOptions;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.azure.servicebus.ServiceBusComponent;
@@ -45,8 +48,11 @@ public abstract class BaseServiceBusTestSupport implements ConfigurableRoute {
     public static final String CONNECTION_STRING_PROPERTY_NAME = "camel.component.azure-servicebus.connection-string";
     protected static final String CONNECTION_STRING = getProperty(CONNECTION_STRING_PROPERTY_NAME);
     protected static final String QUEUE_NAME = "camelTestQueue";
+    protected static final String QUEUE_WITH_SESSIONS_NAME = "camelTestQueueSessions";
     protected static final String TOPIC_NAME = "camelTestTopic";
+    protected static final String TOPIC_WITH_SESSIONS_NAME = "camelTestTopicSessions";
     protected static final String SUBSCRIPTION_NAME = "camelTestSubscription";
+    protected static final String SUBSCRIPTION_WITH_SESSIONS_NAME = "camelTestSubscriptionSessions";
     protected static final String MOCK_RESULT = "mock:result";
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseServiceBusTestSupport.class);
     @RegisterExtension
@@ -55,34 +61,40 @@ public abstract class BaseServiceBusTestSupport implements ConfigurableRoute {
     protected CountDownLatch messageLatch;
     protected List<ServiceBusReceivedMessageContext> receivedMessageContexts;
 
-    @BeforeAll
-    static void baseBeforeAll() {
-        serviceBusAdminClient = new ServiceBusAdministrationClientBuilder()
-                .connectionString(CONNECTION_STRING)
-                .buildClient();
-        try {
-            serviceBusAdminClient.createQueue(QUEUE_NAME);
-        } catch (ResourceExistsException e) {
-            LOGGER.warn("Test queue already existed", e);
+        @BeforeAll
+        static void baseBeforeAll() {
+            serviceBusAdminClient = new ServiceBusAdministrationClientBuilder()
+                    .connectionString(CONNECTION_STRING)
+                    .buildClient();
+            try {
+                serviceBusAdminClient.createQueue(QUEUE_NAME);
+                serviceBusAdminClient.createQueue(QUEUE_WITH_SESSIONS_NAME, new CreateQueueOptions().setSessionRequired(true));
+            } catch (ResourceExistsException e) {
+                LOGGER.warn("Test queue already existed", e);
+            }
+            try {
+                serviceBusAdminClient.createTopic(TOPIC_NAME);
+                serviceBusAdminClient.createTopic(TOPIC_WITH_SESSIONS_NAME, new CreateTopicOptions().setSessionRequired(true));
+            } catch (ResourceExistsException e) {
+                LOGGER.warn("Test topic already existed", e);
+            }
+            try {
+                serviceBusAdminClient.createSubscription(TOPIC_NAME, SUBSCRIPTION_NAME);
+                serviceBusAdminClient.createSubscription(TOPIC_WITH_SESSIONS_NAME, SUBSCRIPTION_WITH_SESSIONS_NAME, new CreateSubscriptionOptions().setSessionRequired(true));
+            } catch (ResourceExistsException e) {
+                LOGGER.warn("Test subscription already existed", e);
+            }
         }
-        try {
-            serviceBusAdminClient.createTopic(TOPIC_NAME);
-        } catch (ResourceExistsException e) {
-            LOGGER.warn("Test topic already existed", e);
-        }
-        try {
-            serviceBusAdminClient.createSubscription(TOPIC_NAME, SUBSCRIPTION_NAME);
-        } catch (ResourceExistsException e) {
-            LOGGER.warn("Test subscription already existed", e);
-        }
-    }
 
-    @AfterAll
-    static void baseAfterAll() {
-        serviceBusAdminClient.deleteSubscription(TOPIC_NAME, SUBSCRIPTION_NAME);
-        serviceBusAdminClient.deleteTopic(TOPIC_NAME);
-        serviceBusAdminClient.deleteQueue(QUEUE_NAME);
-    }
+        @AfterAll
+        static void baseAfterAll() {
+            serviceBusAdminClient.deleteSubscription(TOPIC_NAME, SUBSCRIPTION_NAME);
+            serviceBusAdminClient.deleteSubscription(TOPIC_WITH_SESSIONS_NAME, SUBSCRIPTION_WITH_SESSIONS_NAME);
+            serviceBusAdminClient.deleteTopic(TOPIC_NAME);
+            serviceBusAdminClient.deleteTopic(TOPIC_WITH_SESSIONS_NAME);
+            serviceBusAdminClient.deleteQueue(QUEUE_NAME);
+            serviceBusAdminClient.deleteQueue(QUEUE_WITH_SESSIONS_NAME);
+        }
 
     @ContextFixture
     public void configureServiceBus(CamelContext context) {
@@ -110,6 +122,17 @@ public abstract class BaseServiceBusTestSupport implements ConfigurableRoute {
                 .buildProcessorClient();
     }
 
+    protected ServiceBusProcessorClient createSessionQueueProcessorClient() {
+        return new ServiceBusClientBuilder()
+                .connectionString(CONNECTION_STRING)
+                .sessionProcessor()
+                .queueName(QUEUE_WITH_SESSIONS_NAME)
+                .processMessage(this::processMessage)
+                .processError(serviceBusErrorContext -> LOGGER.error("Service Bus client error",
+                        serviceBusErrorContext.getException()))
+                .buildProcessorClient();
+    }
+
     protected ServiceBusProcessorClient createTopicProcessorClient() {
         return new ServiceBusClientBuilder()
                 .connectionString(CONNECTION_STRING)
@@ -127,8 +150,8 @@ public abstract class BaseServiceBusTestSupport implements ConfigurableRoute {
                 .connectionString(CONNECTION_STRING)
                 .sessionProcessor()
                 .disableAutoComplete()
-                .topicName(TOPIC_NAME)
-                .subscriptionName(SUBSCRIPTION_NAME)
+                .topicName(TOPIC_WITH_SESSIONS_NAME)
+                .subscriptionName(SUBSCRIPTION_WITH_SESSIONS_NAME)
                 .processMessage(this::processMessage)
                 .processError(serviceBusErrorContext -> LOGGER.error("Service Bus client error",
                         serviceBusErrorContext.getException()))
--- components/camel-azure/camel-azure-servicebus/src/test/java/org/apache/camel/component/azure/servicebus/integration/ServiceBusProducerIT.java
+++ components/camel-azure/camel-azure-servicebus/src/test/java/org/apache/camel/component/azure/servicebus/integration/ServiceBusProducerIT.java
@@ -42,7 +42,9 @@ public class ServiceBusProducerIT extends BaseServiceBusTestSupport {
     private static final String DIRECT_SEND_TO_QUEUE_URI = "direct:sendToQueue";
     private static final String DIRECT_SEND_TO_TOPIC_URI = "direct:sendToTopic";
     private static final String DIRECT_SEND_SCHEDULED_URI = "direct:sendScheduled";
+    private static final String DIRECT_SEND_SCHEDULED_URI_WITH_SESSIONS = "direct:sendScheduledSessions";
     private static final String DIRECT_SEND_TO_TOPIC_SESSION_URI = "direct:sendToTopicSessions";
+    private static final String DIRECT_SEND_TO_SESSION_QUEUE_URI = "direct:sendToQueueSessions";
     private static final Map<String, Object> PROPAGATED_HEADERS = new HashMap<>();
     private static final Pattern MESSAGE_BODY_PATTERN = Pattern.compile("^message-[0-4]$");
 
@@ -76,14 +78,20 @@ public class ServiceBusProducerIT extends BaseServiceBusTestSupport {
                 from(DIRECT_SEND_TO_QUEUE_URI)
                         .to("azure-servicebus:" + QUEUE_NAME);
 
+                from(DIRECT_SEND_TO_SESSION_QUEUE_URI)
+                        .to("azure-servicebus:" + QUEUE_WITH_SESSIONS_NAME + "?sessionId=123");
+
                 from(DIRECT_SEND_TO_TOPIC_URI)
                         .to("azure-servicebus:" + TOPIC_NAME + "?serviceBusType=topic");
+
+                from(DIRECT_SEND_TO_TOPIC_SESSION_URI)
+                        .to("azure-servicebus:" + TOPIC_WITH_SESSIONS_NAME + "?serviceBusType=topic&sessionId=123");
 
                 from(DIRECT_SEND_SCHEDULED_URI)
                         .to("azure-servicebus:" + QUEUE_NAME + "?producerOperation=scheduleMessages");
 
-                from(DIRECT_SEND_TO_TOPIC_SESSION_URI)
-                        .to("azure-servicebus:" + TOPIC_NAME + "?serviceBusType=topic&sessionId=123");
+                from(DIRECT_SEND_SCHEDULED_URI_WITH_SESSIONS)
+                        .to("azure-servicebus:" + QUEUE_WITH_SESSIONS_NAME + "?producerOperation=scheduleMessages&sessionId=123");
             }
         };
     }
@@ -221,6 +229,60 @@ public class ServiceBusProducerIT extends BaseServiceBusTestSupport {
         }
     }
 
+    /*
+        Tests for entities with session support
+     */
+
+    @Test
+    void camelSendsMessageToServiceBusSessionEnabledQueue() throws InterruptedException {
+        messageLatch = new CountDownLatch(5);
+        try (ServiceBusProcessorClient client = createSessionQueueProcessorClient()) {
+            client.start();
+            for (int i = 0; i < 5; i++) {
+                String message = "message-" + i;
+                producerTemplate.sendBodyAndHeaders(DIRECT_SEND_TO_SESSION_QUEUE_URI, message, PROPAGATED_HEADERS);
+            }
+
+            assertTrue(messageLatch.await(3000, TimeUnit.MILLISECONDS));
+            assertEquals(5, receivedMessageContexts.size());
+            receivedMessageContexts.forEach(messageContext -> {
+                ServiceBusReceivedMessage message = messageContext.getMessage();
+                String messageBody = message.getBody().toString();
+                assertTrue(MESSAGE_BODY_PATTERN.matcher(messageBody).matches());
+                Map<String, Object> applicationProperties = message.getApplicationProperties();
+                assertEquals(PROPAGATED_HEADERS, applicationProperties);
+            });
+        }
+    }
+
+    @Test
+    void camelSendsMessageBatchToServiceBusSessionEnabledQueue() throws InterruptedException {
+        messageLatch = new CountDownLatch(5);
+        try (ServiceBusProcessorClient client = createSessionQueueProcessorClient()) {
+            client.start();
+            List<Object> messageBatch = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                String message = "message-" + i;
+                messageBatch.add(message);
+            }
+
+            producerTemplate.send(DIRECT_SEND_TO_SESSION_QUEUE_URI, exchange -> {
+                exchange.getIn().setHeaders(PROPAGATED_HEADERS);
+                exchange.getIn().setBody(messageBatch);
+            });
+
+            assertTrue(messageLatch.await(3000, TimeUnit.MILLISECONDS));
+            assertEquals(5, receivedMessageContexts.size());
+            receivedMessageContexts.forEach(messageContext -> {
+                ServiceBusReceivedMessage message = messageContext.getMessage();
+                String messageBody = message.getBody().toString();
+                assertTrue(MESSAGE_BODY_PATTERN.matcher(messageBody).matches());
+                Map<String, Object> applicationProperties = message.getApplicationProperties();
+                assertEquals(PROPAGATED_HEADERS, applicationProperties);
+            });
+        }
+    }
+
     @Test
     void camelSendsMessageWithSessionToServiceBusTopic() throws InterruptedException {
         messageLatch = new CountDownLatch(5);
@@ -237,6 +299,62 @@ public class ServiceBusProducerIT extends BaseServiceBusTestSupport {
                 ServiceBusReceivedMessage message = messageContext.getMessage();
                 String messageBody = message.getBody().toString();
                 assertTrue(MESSAGE_BODY_PATTERN.matcher(messageBody).matches());
+            });
+        }
+    }
+
+    @Test
+    void camelSchedulesServiceBusMessageWithSessions() throws InterruptedException {
+        messageLatch = new CountDownLatch(5);
+        try (ServiceBusProcessorClient client = createSessionQueueProcessorClient()) {
+            client.start();
+            for (int i = 0; i < 5; i++) {
+                String message = "message-" + i;
+                Map<String, Object> headers = new HashMap<>(PROPAGATED_HEADERS);
+                headers.put(ServiceBusConstants.SCHEDULED_ENQUEUE_TIME, OffsetDateTime.now().plusSeconds(1));
+                producerTemplate.sendBodyAndHeaders(DIRECT_SEND_SCHEDULED_URI_WITH_SESSIONS, message, headers);
+            }
+
+            assertTrue(messageLatch.await(4000, TimeUnit.MILLISECONDS));
+            assertEquals(5, receivedMessageContexts.size());
+            receivedMessageContexts.forEach(messageContext -> {
+                ServiceBusReceivedMessage message = messageContext.getMessage();
+                String messageBody = message.getBody().toString();
+                assertTrue(MESSAGE_BODY_PATTERN.matcher(messageBody).matches());
+                Map<String, Object> applicationProperties = message.getApplicationProperties();
+                assertEquals(PROPAGATED_HEADERS, applicationProperties);
+                assertInstanceOf(OffsetDateTime.class, message.getScheduledEnqueueTime());
+            });
+        }
+    }
+
+    @Test
+    void camelSchedulesServiceBusMessageBatchWIthSessions() throws InterruptedException {
+        messageLatch = new CountDownLatch(5);
+        try (ServiceBusProcessorClient client = createSessionQueueProcessorClient()) {
+            client.start();
+            List<Object> messageBatch = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                String message = "message-" + i;
+                messageBatch.add(message);
+            }
+
+            producerTemplate.send(DIRECT_SEND_SCHEDULED_URI_WITH_SESSIONS, exchange -> {
+                Map<String, Object> headers = new HashMap<>(PROPAGATED_HEADERS);
+                headers.put(ServiceBusConstants.SCHEDULED_ENQUEUE_TIME, OffsetDateTime.now().plusSeconds(1));
+                exchange.getIn().setHeaders(headers);
+                exchange.getIn().setBody(messageBatch);
+            });
+
+            assertTrue(messageLatch.await(10000, TimeUnit.MILLISECONDS));
+            assertEquals(5, receivedMessageContexts.size());
+            receivedMessageContexts.forEach(messageContext -> {
+                ServiceBusReceivedMessage message = messageContext.getMessage();
+                String messageBody = message.getBody().toString();
+                assertTrue(MESSAGE_BODY_PATTERN.matcher(messageBody).matches());
+                Map<String, Object> applicationProperties = message.getApplicationProperties();
+                assertEquals(PROPAGATED_HEADERS, applicationProperties);
+                assertInstanceOf(OffsetDateTime.class, message.getScheduledEnqueueTime());
             });
         }
     }
