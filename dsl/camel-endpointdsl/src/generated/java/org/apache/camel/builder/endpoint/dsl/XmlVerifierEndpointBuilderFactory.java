671d22fd333514772f6396f85e54d26f0e71aa23
Claus Ibsen <claus.ibsen@gmail.com>
2025-01-16 09:31:25 +0100

    Regen

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/XmlVerifierEndpointBuilderFactory.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/XmlVerifierEndpointBuilderFactory.java
@@ -303,7 +303,7 @@ public interface XmlVerifierEndpointBuilderFactory {
         }
         /**
          * Indicator whether the XML signature elements (elements with local
-         * name Signature and namesapce http://www.w3.org/2000/09/xmldsig#)
+         * name Signature and namespace http://www.w3.org/2000/09/xmldsig#)
          * shall be removed from the document set to the output message.
          * Normally, this is only necessary, if the XML signature is enveloped.
          * The default value is Boolean#FALSE. This parameter is forwarded to
@@ -325,7 +325,7 @@ public interface XmlVerifierEndpointBuilderFactory {
         }
         /**
          * Indicator whether the XML signature elements (elements with local
-         * name Signature and namesapce http://www.w3.org/2000/09/xmldsig#)
+         * name Signature and namespace http://www.w3.org/2000/09/xmldsig#)
          * shall be removed from the document set to the output message.
          * Normally, this is only necessary, if the XML signature is enveloped.
          * The default value is Boolean#FALSE. This parameter is forwarded to
