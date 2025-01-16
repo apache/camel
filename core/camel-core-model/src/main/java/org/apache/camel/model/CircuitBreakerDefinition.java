85b93b0fd698e2297d1a310ae28ac3c03170b6e4
Claus Ibsen <claus.ibsen@gmail.com>
2025-01-16 07:52:17 +0100

    CAMEL-21620: camel-core - Remove onFallback in outputs in model

--- core/camel-core-model/src/main/java/org/apache/camel/model/CircuitBreakerDefinition.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/CircuitBreakerDefinition.java
@@ -24,8 +24,8 @@ import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlElementRef;
 import jakarta.xml.bind.annotation.XmlRootElement;
-
 import jakarta.xml.bind.annotation.XmlType;
+
 import org.apache.camel.spi.Metadata;
 
 /**
