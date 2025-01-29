b06d13c4c2ad1b3a0d526b21bf09edefed617451
James Netherton <jamesnetherton@gmail.com>
2025-01-29 14:23:12 +0000

    CAMEL-21681: Upgrade SmallRye Health to 4.2.0

--- components/camel-microprofile/camel-microprofile-health/src/test/java/org/apache/camel/microprofile/health/CamelMicroProfileHealthTestSupport.java
+++ components/camel-microprofile/camel-microprofile-health/src/test/java/org/apache/camel/microprofile/health/CamelMicroProfileHealthTestSupport.java
@@ -16,17 +16,23 @@
  */
 package org.apache.camel.microprofile.health;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.concurrent.CompletionStage;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import jakarta.enterprise.event.Event;
+import jakarta.enterprise.event.NotificationOptions;
+import jakarta.enterprise.util.TypeLiteral;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
 
 import io.smallrye.health.SmallRyeHealth;
 import io.smallrye.health.SmallRyeHealthReporter;
 import io.smallrye.health.api.HealthType;
+import io.smallrye.health.api.event.HealthStatusChangeEvent;
 import io.smallrye.health.registry.HealthRegistries;
 import io.smallrye.health.registry.HealthRegistryImpl;
 import io.smallrye.mutiny.Uni;
@@ -36,11 +42,20 @@ import org.apache.camel.health.HealthCheckRegistry;
 import org.apache.camel.health.HealthCheckResultBuilder;
 import org.apache.camel.impl.health.AbstractHealthCheck;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.apache.camel.util.ReflectionHelper;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
 public class CamelMicroProfileHealthTestSupport extends CamelTestSupport {
 
     protected SmallRyeHealthReporter reporter = new SmallRyeHealthReporter();
+
+    @Override
+    protected void setupResources() throws Exception {
+        super.setupResources();
+
+        // Work around to fulfil CDI injected fields
+        setupHealthReporterEventFields();
+    }
 
     @SuppressWarnings("unchecked")
     @Override
@@ -67,6 +82,18 @@ public class CamelMicroProfileHealthTestSupport extends CamelTestSupport {
         HealthCheckRegistry registry = new CamelMicroProfileHealthCheckRegistry();
         camelContext.getCamelContextExtension().addContextPlugin(HealthCheckRegistry.class, registry);
         return camelContext;
+    }
+
+    protected void setupHealthReporterEventFields() {
+        Stream.of("healthEvent", "livenessEvent", "readinessEvent", "wellnessEvent", "startupEvent")
+                .forEach(fieldName -> {
+                    try {
+                        Field field = SmallRyeHealthReporter.class.getDeclaredField(fieldName);
+                        ReflectionHelper.setField(field, reporter, new NoOpHealthEvent());
+                    } catch (NoSuchFieldException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
     protected void assertHealthCheckOutput(
@@ -151,5 +178,36 @@ public class CamelMicroProfileHealthTestSupport extends CamelTestSupport {
         };
         readinessCheck.setEnabled(enabled);
         return readinessCheck;
+    }
+
+    static final class NoOpHealthEvent implements Event<HealthStatusChangeEvent> {
+        @Override
+        public void fire(HealthStatusChangeEvent healthStatusChangeEvent) {
+        }
+
+        @Override
+        public <U extends HealthStatusChangeEvent> CompletionStage<U> fireAsync(U u) {
+            return null;
+        }
+
+        @Override
+        public <U extends HealthStatusChangeEvent> CompletionStage<U> fireAsync(U u, NotificationOptions notificationOptions) {
+            return null;
+        }
+
+        @Override
+        public Event<HealthStatusChangeEvent> select(Annotation... annotations) {
+            return null;
+        }
+
+        @Override
+        public <U extends HealthStatusChangeEvent> Event<U> select(Class<U> aClass, Annotation... annotations) {
+            return null;
+        }
+
+        @Override
+        public <U extends HealthStatusChangeEvent> Event<U> select(TypeLiteral<U> typeLiteral, Annotation... annotations) {
+            return null;
+        }
     }
 }
