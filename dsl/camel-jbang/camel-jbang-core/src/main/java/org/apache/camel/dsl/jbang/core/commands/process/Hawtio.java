2195e9e0bfa32f5e096ec1a269f452454bfd4492
Claus Ibsen <claus.ibsen@gmail.com>
2025-02-06 10:28:59 +0100

    Revert "camel-jbang - Upgrade to hawtio 4.3.0"
    This reverts commit 100fd96338447a2c449ea887fe72098fc63d9329.

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/process/Hawtio.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/process/Hawtio.java
@@ -37,8 +37,8 @@ public class Hawtio extends CamelCommand {
     String name;
 
     @CommandLine.Option(names = { "--version" },
-                        description = "Version of the Hawtio web console", defaultValue = "4.3.0")
-    String version = "4.3.0";
+                        description = "Version of the Hawtio web console", defaultValue = "4.2.0")
+    String version = "4.2.0";
 
     // use port 8888 as 8080 is too commonly used
     @CommandLine.Option(names = { "--port" },
