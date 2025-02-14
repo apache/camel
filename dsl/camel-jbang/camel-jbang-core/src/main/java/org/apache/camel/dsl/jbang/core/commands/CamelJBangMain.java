0c564ff38d6a1408d953c2d2ee559cc7daa2367c
John Poth <poth.john@gmail.com>
2025-02-14 20:46:11 +0100

    CAMEL-21723: camel-jbang add edit command (#17133)

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/CamelJBangMain.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/CamelJBangMain.java
@@ -82,6 +82,7 @@ public class CamelJBangMain implements Callable<Integer> {
         }
 
         commandLine = new CommandLine(main)
+                .addSubcommand("edit", new CommandLine(new Edit(main)))
                 .addSubcommand("shell", new CommandLine(new Shell(main)))
                 .addSubcommand("init", new CommandLine(new Init(main)))
                 .addSubcommand("run", new CommandLine(new Run(main)))
