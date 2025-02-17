47c293091d7e0377bd062fa2d67257834c60b8dd
gtkow <gtkow@users.noreply.github.com>
2025-02-17 16:10:22 +0100

    CAMEL-21752: camel-smb - Update SMBClient instantiation to consider `SmbConfiguration` and its `SmbConfig`. This ensures proper initialization with custom configurations when available, improving flexibility and reducing potential issues with null values. (#17178)

--- components/camel-smb/src/main/java/org/apache/camel/component/smb/SmbOperations.java
+++ components/camel-smb/src/main/java/org/apache/camel/component/smb/SmbOperations.java
@@ -66,7 +66,7 @@ public class SmbOperations implements SmbFileOperations {
 
     public SmbOperations(SmbConfiguration configuration) {
         this.configuration = configuration;
-        this.smbClient = new SMBClient();
+        this.smbClient = configuration == null || configuration.getSmbConfig() == null ? new SMBClient() : new SMBClient(configuration.getSmbConfig());
     }
 
     @Override
