5524566e59a94949d7320be8059d530a9bd5d28a
Andrea Cosentino <ancosen@gmail.com>
2025-03-07 12:28:44 +0100

    CAMEL-21843 - camel-kamelet - Move kamelet utils to corresponding components - MongoDB
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/SslAwareMongoClient.java
+++ components/camel-mongodb/src/main/java/org/apache/camel/component/mongodb/SslAwareMongoClient.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.kamelet.utils.mongodb;
+package org.apache.camel.component.mongodb;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
