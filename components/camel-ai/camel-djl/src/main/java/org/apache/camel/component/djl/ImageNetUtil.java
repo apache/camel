6ef0ebb948991e13b10666f7a45142f4f87adad6
Andrea Cosentino <ancosen@gmail.com>
2025-03-11 12:08:51 +0100

    CAMEL-21843 - camel-kamelet - Move kamelet utils to corresponding components - DJL
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-ai/camel-djl/src/main/java/org/apache/camel/component/djl/ImageNetUtil.java
+++ components/camel-ai/camel-djl/src/main/java/org/apache/camel/component/djl/ImageNetUtil.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.kamelet.utils.djl;
+package org.apache.camel.component.djl;
 
 import java.util.List;
 
