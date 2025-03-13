906a84ac04e2959f81373e68912a8a6055a274a5
Andrea Cosentino <ancosen@gmail.com>
2025-03-13 13:10:01 +0100

    CAMEL-21843 - camel-kamelet - Move kamelet utils to corresponding components - Langchain4j (#17451)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-ai/camel-langchain4j-chat/src/main/java/org/apache/camel/component/langchain4j/chat/openai/OpenAiChatLanguageModelBuilder.java
+++ components/camel-ai/camel-langchain4j-chat/src/main/java/org/apache/camel/component/langchain4j/chat/openai/OpenAiChatLanguageModelBuilder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.component.kamelet.utils.langchain4j.chat;
+package org.apache.camel.component.langchain4j.chat.openai;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
