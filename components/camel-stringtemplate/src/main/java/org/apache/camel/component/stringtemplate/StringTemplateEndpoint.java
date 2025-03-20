42e12fafbea480ee36e516973ce2da3523cad957
Dmitry Kryukov <dk2k@users.noreply.github.com>
2025-03-20 13:51:03 +0300

    CAMEL-19731 fixed resource leak in StringTemplateEndpoint (#17509)

--- components/camel-stringtemplate/src/main/java/org/apache/camel/component/stringtemplate/StringTemplateEndpoint.java
+++ components/camel-stringtemplate/src/main/java/org/apache/camel/component/stringtemplate/StringTemplateEndpoint.java
@@ -126,8 +126,9 @@ public class StringTemplateEndpoint extends ResourceEndpoint {
                 log.debug("{} set to {} creating new endpoint to handle exchange",
                         StringTemplateConstants.STRINGTEMPLATE_RESOURCE_URI,
                         newResourceUri);
-                StringTemplateEndpoint newEndpoint = findOrCreateEndpoint(getEndpointUri(), newResourceUri);
-                newEndpoint.onExchange(exchange);
+                try (StringTemplateEndpoint newEndpoint = findOrCreateEndpoint(getEndpointUri(), newResourceUri)) {
+                    newEndpoint.onExchange(exchange);
+                }
                 return;
             }
             variableMap = exchange.getIn().getHeader(StringTemplateConstants.STRINGTEMPLATE_VARIABLE_MAP, Map.class);
