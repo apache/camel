f40c88811954d9bd3df2ac4e73fc2588f37b65c5
anirudh-04 <100948748+anirudh-04@users.noreply.github.com>
2025-04-03 20:06:25 +0530

    CAMEL-21926 camel-attachment: remove ATTACHMENTS trait when last attachment is removed (#17657)

---
+++ components/camel-attachments/src/test/java/org/apache/camel/attachment/AttachmentRemovalTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.attachment;
+
+import jakarta.activation.DataHandler;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AttachmentRemovalTest extends CamelTestSupport {
+
+    @Test
+    public void testRemoval() throws Exception {
+        getMockEndpoint("mock:result").expectedMessageCount(1);
+
+        template.sendBody("direct:start", "");
+
+        MockEndpoint.assertIsSatisfied(context);
+
+        Exchange e1 = getMockEndpoint("mock:result").getReceivedExchanges().get(0);
+
+        AttachmentMessage am1 = e1.getMessage(AttachmentMessage.class);
+
+        Assertions.assertFalse(am1.hasAttachments());
+        Assertions.assertEquals(0, am1.getAttachmentNames().size());
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                        .process(exchange -> {
+                            AttachmentMessage message = exchange.getMessage(AttachmentMessage.class);
+                            byte[] data = exchange.getMessage().getBody(byte[].class);
+                            String attachmentName = "myfile.txt";
+                            String mimeType = "text/plain";
+                            DataHandler dataHandler = new DataHandler(data, mimeType);
+                            message.addAttachment(attachmentName, dataHandler);
+                            message.removeAttachment(attachmentName);
+                        })
+                        .to("mock:result");
+            }
+        };
+    }
+}
