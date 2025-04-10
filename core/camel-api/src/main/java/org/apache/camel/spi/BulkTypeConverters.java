046c40ed2f2d43617501ce174ba7f0b770901b29
Claus Ibsen <claus.ibsen@gmail.com>
2025-04-10 20:49:05 +0200

    CAMEL-21949: camel-core - Type converter with allowNull should be a valid response for ConvertBodyTo

--- core/camel-api/src/main/java/org/apache/camel/spi/BulkTypeConverters.java
+++ core/camel-api/src/main/java/org/apache/camel/spi/BulkTypeConverters.java
@@ -55,11 +55,6 @@ public interface BulkTypeConverters extends Ordered, TypeConverter {
      */
     <T> T convertTo(Class<?> from, Class<T> to, Exchange exchange, Object value) throws TypeConversionException;
 
-    default Object allowNullConvertTo(Class<?> from, Class<?> to, Exchange exchange, Object value)
-            throws TypeConversionException {
-        return convertTo(from, to, exchange, value);
-    }
-
     /**
      * Tries to convert the value to the specified type, returning <tt>null</tt> if not possible to convert.
      * <p/>
@@ -72,7 +67,7 @@ public interface BulkTypeConverters extends Ordered, TypeConverter {
      */
     default <T> T tryConvertTo(Class<?> from, Class<T> to, Exchange exchange, Object value) throws TypeConversionException {
         try {
-            Object t = allowNullConvertTo(from, to, exchange, value);
+            Object t = convertTo(from, to, exchange, value);
             if (t == Void.class) {
                 return null;
             }
@@ -98,7 +93,7 @@ public interface BulkTypeConverters extends Ordered, TypeConverter {
      */
     default <T> T mandatoryConvertTo(Class<?> from, Class<T> to, Exchange exchange, Object value)
             throws TypeConversionException, NoTypeConversionAvailableException {
-        Object t = allowNullConvertTo(from, to, exchange, value);
+        Object t = convertTo(from, to, exchange, value);
         if (t == Void.class) {
             return null;
         } else if (t == null) {
