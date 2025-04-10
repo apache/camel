dde2770db91a59491efd6218da735d2fe4ef47f4
Claudio Miranda <claudio@claudius.com.br>
2025-04-10 08:45:01 -0300

    camel-jbang: fix export not respecting --quarkus-group-id parameter (#17665)
    (cherry picked from commit cf951d735ec51a38e7b1c9addd8893809dc270be)

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/ExportBaseCommand.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/ExportBaseCommand.java
@@ -335,6 +335,7 @@ public abstract class ExportBaseCommand extends CamelCommand {
         run.camelVersion = camelVersion;
         run.camelSpringBootVersion = camelSpringBootVersion;
         run.quarkusVersion = quarkusVersion;
+        run.quarkusGroupId = quarkusGroupId;
         run.springBootVersion = springBootVersion;
         run.kameletsVersion = kameletsVersion;
         run.localKameletDir = localKameletDir;
