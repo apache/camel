bd9eafa1f34da51259111f27bb8a91b24f158e55
Claus Ibsen <claus.ibsen@gmail.com>
2025-04-22 16:47:07 +0200

    Upgrade kamelets

--- dsl/camel-kamelet-main/src/main/java/org/apache/camel/main/util/VersionHelper.java
+++ dsl/camel-kamelet-main/src/main/java/org/apache/camel/main/util/VersionHelper.java
@@ -28,7 +28,7 @@ public final class VersionHelper {
 
     private static String camelVersion;
 
-    private static final String KAMELETS_DEFAULT_VERSION = "4.8.0";
+    private static final String KAMELETS_DEFAULT_VERSION = "4.11.0";
     private static final Pattern KAMELETS_LIBRARY = Pattern.compile("camel-kamelets-(\\d[\\w\\d.-]*).jar", Pattern.DOTALL);
     private static final Pattern CAMEL_BASE_ENGINE_LIBRARY
             = Pattern.compile("camel-base-engine-(\\d[\\w\\d.-]*).jar", Pattern.DOTALL);
