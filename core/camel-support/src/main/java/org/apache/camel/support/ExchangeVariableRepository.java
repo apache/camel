b2016c1cf571948916d6ff4655e7b366ccd20607
Claus Ibsen <claus.ibsen@gmail.com>
2025-04-24 21:53:54 +0200

    CAMEL-22018: camel-core - Exchange.getVariables should include message headers (#17867)
    * CAMEL-22018: camel-core - Exchange.getVariables should include message headers

--- core/camel-support/src/main/java/org/apache/camel/support/ExchangeVariableRepository.java
+++ core/camel-support/src/main/java/org/apache/camel/support/ExchangeVariableRepository.java
@@ -16,8 +16,9 @@
  */
 package org.apache.camel.support;
 
+import java.util.HashSet;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Set;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
@@ -31,8 +32,6 @@ import org.apache.camel.util.StringHelper;
  */
 final class ExchangeVariableRepository extends AbstractVariableRepository {
 
-    private final Map<String, Object> headers = new ConcurrentHashMap<>(8);
-
     public ExchangeVariableRepository(CamelContext camelContext) {
         setCamelContext(camelContext);
         // ensure its started
@@ -41,7 +40,6 @@ final class ExchangeVariableRepository extends AbstractVariableRepository {
 
     void copyFrom(ExchangeVariableRepository source) {
         setVariables(source.getVariables());
-        this.headers.putAll(source.headers);
     }
 
     @Override
@@ -56,52 +54,41 @@ final class ExchangeVariableRepository extends AbstractVariableRepository {
                 prefix = prefix + ".";
                 // we want all headers for a given variable
                 Map<String, Object> map = new CaseInsensitiveMap();
-                for (Map.Entry<String, Object> entry : headers.entrySet()) {
+                for (Map.Entry<String, Object> entry : getVariables().entrySet()) {
                     String key = entry.getKey();
-                    if (key.startsWith(prefix)) {
+                    if (key.startsWith(name + ".")) {
                         key = StringHelper.after(key, prefix);
                         map.put(key, entry.getValue());
                     }
                 }
                 return map;
-            } else {
-                return headers.get(prefix);
             }
         }
         return super.getVariable(name);
     }
 
     @Override
-    public void setVariable(String name, Object value) {
-        String id = StringHelper.before(name, ":");
-        if ("header".equals(id)) {
-            name = StringHelper.after(name, ":");
-            if (value != null) {
-                // avoid the NullPointException
-                headers.put(name, value);
-            } else {
-                // if the value is null, we just remove the key from the map
-                headers.remove(name);
-            }
-        } else {
-            super.setVariable(name, value);
-        }
-    }
-
-    @Override
     public Object removeVariable(String name) {
         String id = StringHelper.before(name, ":");
         if ("header".equals(id)) {
-            name = StringHelper.after(name, ":");
-            return headers.remove(name);
+            String prefix = StringHelper.after(name, ":");
+            if (prefix == null || prefix.isBlank()) {
+                throw new IllegalArgumentException("Variable " + name + " must have header key");
+            }
+            if (!prefix.contains(".")) {
+                // we want to remove all headers for a given variable
+                Set<String> toRemove = new HashSet<>();
+                for (Map.Entry<String, Object> entry : getVariables().entrySet()) {
+                    String key = entry.getKey();
+                    if (key.startsWith(name + ".")) {
+                        toRemove.add(key);
+                    }
+                }
+                toRemove.forEach(super::removeVariable);
+                return null;
+            }
         }
         return super.removeVariable(name);
-    }
-
-    @Override
-    public void clear() {
-        super.clear();
-        headers.clear();
     }
 
     @Override
