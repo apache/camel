6170d37c4d42e8256cf367ca265e18b179beda34
Claus Ibsen <claus.ibsen@gmail.com>
2025-04-24 17:52:56 +0200

    Upgrade SB 3.4.5

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/common/RuntimeType.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/common/RuntimeType.java
@@ -25,7 +25,7 @@ public enum RuntimeType {
     main;
 
     public static final String QUARKUS_VERSION = "3.21.3";
-    public static final String SPRING_BOOT_VERSION = "3.4.4";
+    public static final String SPRING_BOOT_VERSION = "3.4.5";
 
     public static RuntimeType fromValue(String value) {
         return switch (value) {
