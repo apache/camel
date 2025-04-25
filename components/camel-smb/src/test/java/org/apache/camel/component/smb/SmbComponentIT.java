a1b33621fc7b5bf3310b635475a086e0cce65a8e
Claus Ibsen <claus.ibsen@gmail.com>
2025-04-25 09:33:30 +0200

    CAMEL-22019: camel-smb: Expose UncPath for backwardcompatibility

--- components/camel-smb/src/test/java/org/apache/camel/component/smb/SmbComponentIT.java
+++ components/camel-smb/src/test/java/org/apache/camel/component/smb/SmbComponentIT.java
@@ -48,6 +48,7 @@ public class SmbComponentIT extends CamelTestSupport {
     public void testSmbRead() throws Exception {
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMessageCount(100);
+        mock.allMessages().header(SmbConstants.SMB_UNC_PATH).startsWith("\\\\localhost\\data-rw\\");
 
         mock.assertIsSatisfied();
     }
@@ -55,7 +56,7 @@ public class SmbComponentIT extends CamelTestSupport {
     @Test
     public void testSmbSendFile() throws Exception {
         mockResultEndpoint.expectedMinimumMessageCount(1);
-        Exchange exchange = template.request("direct:smbSendFile", null);
+        template.request("direct:smbSendFile", null);
         MockEndpoint.assertIsSatisfied(context);
     }
 
