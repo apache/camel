8c575e2bfff9fb4f8ae0a4fd2683b71b8438ddad
James Netherton <jamesnetherton@gmail.com>
2025-04-30 08:43:55 +0100

    CAMEL-22036: Align camel-jackson-avro Avro version with Camel

--- components/camel-jackson-avro/src/test/java/org/apache/camel/component/jackson/avro/JacksonAvroMarshalUnmarshalJsonNodeTest.java
+++ components/camel-jackson-avro/src/test/java/org/apache/camel/component/jackson/avro/JacksonAvroMarshalUnmarshalJsonNodeTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.dataformat.avro.AvroSchema;
+import org.apache.avro.NameValidator;
 import org.apache.avro.Schema;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.SchemaResolver;
@@ -112,10 +113,10 @@ public class JacksonAvroMarshalUnmarshalJsonNodeTest extends CamelTestSupport {
                                 "  }\n" +
                                 "}";
 
-        Schema raw = new Schema.Parser().setValidate(true).parse(schemaJson);
+        Schema raw = new Schema.Parser(NameValidator.UTF_VALIDATOR).parse(schemaJson);
         AvroSchema schema = new AvroSchema(raw);
 
-        Schema rawList = new Schema.Parser().setValidate(true).parse(listSchemaJson);
+        Schema rawList = new Schema.Parser(NameValidator.UTF_VALIDATOR).parse(listSchemaJson);
         AvroSchema schemaList = new AvroSchema(rawList);
 
         SchemaResolver resolver = ex -> {
