be867f867beb2634586b75b6f4bce5eb45015856
Andrea Cosentino <ancosen@gmail.com>
2025-05-02 10:23:29 +0200

    Upgrade Braintree-java to version 3.41.0 (#17954)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- components/camel-braintree/src/generated/java/org/apache/camel/component/braintree/MerchantAccountGatewayEndpointConfiguration.java
+++ components/camel-braintree/src/generated/java/org/apache/camel/component/braintree/MerchantAccountGatewayEndpointConfiguration.java
@@ -15,7 +15,7 @@ import org.apache.camel.spi.UriParams;
  */
 @ApiParams(apiName = "merchantAccount", 
            description = "Provides methods to create, find, and update MerchantAccount objects",
-           apiMethods = {@ApiMethod(methodName = "all", signatures={"com.braintreegateway.PaginatedCollection<com.braintreegateway.MerchantAccount> all()"}), @ApiMethod(methodName = "create", signatures={"com.braintreegateway.Result<com.braintreegateway.MerchantAccount> create(com.braintreegateway.MerchantAccountRequest request)"}), @ApiMethod(methodName = "createForCurrency", signatures={"com.braintreegateway.Result<com.braintreegateway.MerchantAccount> createForCurrency(com.braintreegateway.MerchantAccountCreateForCurrencyRequest request)"}), @ApiMethod(methodName = "fetchMerchantAccounts", signatures={"com.braintreegateway.PaginatedResult<com.braintreegateway.MerchantAccount> fetchMerchantAccounts(int page)"}), @ApiMethod(methodName = "find", signatures={"com.braintreegateway.MerchantAccount find(String id)"}), @ApiMethod(methodName = "update", signatures={"com.braintreegateway.Result<com.braintreegateway.MerchantAccount> update(String id, com.braintreegateway.MerchantAccountRequest request)"})}, aliases = {})
+           apiMethods = {@ApiMethod(methodName = "all", signatures={"com.braintreegateway.PaginatedCollection<com.braintreegateway.MerchantAccount> all()"}), @ApiMethod(methodName = "createForCurrency", signatures={"com.braintreegateway.Result<com.braintreegateway.MerchantAccount> createForCurrency(com.braintreegateway.MerchantAccountCreateForCurrencyRequest request)"}), @ApiMethod(methodName = "fetchMerchantAccounts", signatures={"com.braintreegateway.PaginatedResult<com.braintreegateway.MerchantAccount> fetchMerchantAccounts(int page)"}), @ApiMethod(methodName = "find", signatures={"com.braintreegateway.MerchantAccount find(String id)"})}, aliases = {})
 @UriParams
 @Configurer(extended = true)
 public final class MerchantAccountGatewayEndpointConfiguration extends BraintreeConfiguration {
@@ -23,14 +23,11 @@ public final class MerchantAccountGatewayEndpointConfiguration extends Braintree
     @ApiParam(optional = false, apiMethods = {@ApiMethod(methodName = "createForCurrency")})
     private com.braintreegateway.MerchantAccountCreateForCurrencyRequest currencyRequest;
     @UriParam
-    @ApiParam(optional = false, apiMethods = {@ApiMethod(methodName = "find"), @ApiMethod(methodName = "update")})
+    @ApiParam(optional = false, apiMethods = {@ApiMethod(methodName = "find")})
     private String id;
     @UriParam
     @ApiParam(optional = false, apiMethods = {@ApiMethod(methodName = "fetchMerchantAccounts")})
     private Integer page;
-    @UriParam
-    @ApiParam(optional = false, apiMethods = {@ApiMethod(methodName = "create"), @ApiMethod(methodName = "update")})
-    private com.braintreegateway.MerchantAccountRequest request;
 
     public com.braintreegateway.MerchantAccountCreateForCurrencyRequest getCurrencyRequest() {
         return currencyRequest;
@@ -54,13 +51,5 @@ public final class MerchantAccountGatewayEndpointConfiguration extends Braintree
 
     public void setPage(Integer page) {
         this.page = page;
-    }
-
-    public com.braintreegateway.MerchantAccountRequest getRequest() {
-        return request;
-    }
-
-    public void setRequest(com.braintreegateway.MerchantAccountRequest request) {
-        this.request = request;
     }
 }
