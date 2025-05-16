485fed896080936c5a99f092c1133b090fa9a9ce
Claus Ibsen <claus.ibsen@gmail.com>
2025-05-16 09:03:10 +0200

    CAMEL-22073: camel-http: deprecate NTML

--- components/camel-http-common/src/main/java/org/apache/camel/http/common/HttpConfiguration.java
+++ components/camel-http-common/src/main/java/org/apache/camel/http/common/HttpConfiguration.java
@@ -58,8 +58,10 @@ public class HttpConfiguration implements Serializable {
                             "If you set this parameter to too small value, you can get 4xx http errors because camel will think that the received token is still valid, while in reality the token is expired for the Authentication server.")
     private long oauth2CachedTokensExpirationMarginSeconds = 5L;
     @Metadata(label = "producer,security", description = "Authentication domain to use with NTML")
+    @Deprecated
     private String authDomain;
     @Metadata(label = "producer,security", description = "Authentication host to use with NTML")
+    @Deprecated
     private String authHost;
     @Metadata(label = "producer,proxy", description = "Proxy hostname to use")
     private String proxyHost;
@@ -78,6 +80,7 @@ public class HttpConfiguration implements Serializable {
     @Metadata(label = "producer,proxy", description = "Proxy authentication port")
     private int proxyAuthPort;
     @Metadata(label = "producer,proxy", description = "Proxy authentication domain to use with NTML")
+    @Deprecated
     private String proxyAuthDomain;
 
     public String getAuthMethod() {
