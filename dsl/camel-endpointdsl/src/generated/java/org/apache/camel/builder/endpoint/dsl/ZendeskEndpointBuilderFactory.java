1ecebc903aab73ca0ed001c3492c048097e99cef
Andrea Cosentino <ancosen@gmail.com>
2025-05-19 10:31:50 +0200

    Upgrade Zendesk Client to version 1.2.0 (#18107)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/ZendeskEndpointBuilderFactory.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/ZendeskEndpointBuilderFactory.java
@@ -1055,7 +1055,7 @@ public interface ZendeskEndpointBuilderFactory {
          * 
          * Path parameter: methodName (required)
          * What operation to use
-         * There are 377 enums and the value can be one of:
+         * There are 378 enums and the value can be one of:
          * ADD_TAG_TO_ORGANISATIONS, ADD_TAG_TO_TICKET, ADD_TAG_TO_TOPICS,
          * ASSOCIATE_ATTACHMENTS_TO_ARTICLE, CHANGE_USER_PASSWORD,
          * CREATE_ARTICLE, CREATE_ARTICLE_1, CREATE_ARTICLE_TRANSLATION,
@@ -1098,7 +1098,7 @@ public interface ZendeskEndpointBuilderFactory {
          * DELETE_TRIGGER, DELETE_UPLOAD, DELETE_UPLOAD_1, DELETE_USER,
          * DELETE_USER_1, DELETE_USER_IDENTITY, DELETE_USER_IDENTITY_1,
          * DELETE_USER_IDENTITY_2, DELETE_USER_SEGMENT, DELETE_USER_SEGMENT_1,
-         * DELETE_USERS, GET_ACTIVE_TRIGGERS, GET_ARTICLE,
+         * DELETE_USERS, EXECUTE_VIEW, GET_ACTIVE_TRIGGERS, GET_ARTICLE,
          * GET_ARTICLE_FROM_SEARCH, GET_ARTICLE_FROM_SEARCH_1,
          * GET_ARTICLE_SUBSCRIPTIONS, GET_ARTICLE_SUBSCRIPTIONS_1,
          * GET_ARTICLE_TRANSLATIONS, GET_ARTICLES, GET_ARTICLES_1,
@@ -1216,7 +1216,7 @@ public interface ZendeskEndpointBuilderFactory {
          * 
          * Path parameter: methodName (required)
          * What operation to use
-         * There are 377 enums and the value can be one of:
+         * There are 378 enums and the value can be one of:
          * ADD_TAG_TO_ORGANISATIONS, ADD_TAG_TO_TICKET, ADD_TAG_TO_TOPICS,
          * ASSOCIATE_ATTACHMENTS_TO_ARTICLE, CHANGE_USER_PASSWORD,
          * CREATE_ARTICLE, CREATE_ARTICLE_1, CREATE_ARTICLE_TRANSLATION,
@@ -1259,7 +1259,7 @@ public interface ZendeskEndpointBuilderFactory {
          * DELETE_TRIGGER, DELETE_UPLOAD, DELETE_UPLOAD_1, DELETE_USER,
          * DELETE_USER_1, DELETE_USER_IDENTITY, DELETE_USER_IDENTITY_1,
          * DELETE_USER_IDENTITY_2, DELETE_USER_SEGMENT, DELETE_USER_SEGMENT_1,
-         * DELETE_USERS, GET_ACTIVE_TRIGGERS, GET_ARTICLE,
+         * DELETE_USERS, EXECUTE_VIEW, GET_ACTIVE_TRIGGERS, GET_ARTICLE,
          * GET_ARTICLE_FROM_SEARCH, GET_ARTICLE_FROM_SEARCH_1,
          * GET_ARTICLE_SUBSCRIPTIONS, GET_ARTICLE_SUBSCRIPTIONS_1,
          * GET_ARTICLE_TRANSLATIONS, GET_ARTICLES, GET_ARTICLES_1,
