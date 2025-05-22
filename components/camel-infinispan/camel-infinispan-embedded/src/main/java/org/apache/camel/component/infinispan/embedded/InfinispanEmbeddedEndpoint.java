d8fad85b4e7418199d11ca92f0a4ebd73ae2c34a
Claus Ibsen <claus.ibsen@gmail.com>
2025-05-22 09:37:14 +0200

    CAMEL-21963: camel-infinispan in embedded mode. Fix wrong cache when using query parameters and no query parameters as they then end up using different cache manager.

--- components/camel-infinispan/camel-infinispan-embedded/src/main/java/org/apache/camel/component/infinispan/embedded/InfinispanEmbeddedEndpoint.java
+++ components/camel-infinispan/camel-infinispan-embedded/src/main/java/org/apache/camel/component/infinispan/embedded/InfinispanEmbeddedEndpoint.java
@@ -27,7 +27,6 @@ import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
-import org.apache.camel.support.service.ServiceHelper;
 
 import static org.apache.camel.component.infinispan.InfinispanConstants.SCHEME_EMBEDDED;
 
@@ -45,37 +44,27 @@ public class InfinispanEmbeddedEndpoint extends InfinispanEndpoint {
     @UriParam
     private final InfinispanEmbeddedConfiguration configuration;
 
-    private final InfinispanEmbeddedManager manager;
-
     public InfinispanEmbeddedEndpoint(String uri, String cacheName, InfinispanComponent component,
                                       InfinispanEmbeddedConfiguration configuration) {
         super(uri, component);
         this.cacheName = cacheName;
         this.configuration = configuration;
-        this.manager = new InfinispanEmbeddedManager(component.getCamelContext(), configuration);
     }
 
     @Override
-    protected void doStart() throws Exception {
-        super.doStart();
-        ServiceHelper.startService(manager);
-    }
-
-    @Override
-    protected void doStop() throws Exception {
-        super.doStop();
-        ServiceHelper.stopService(manager);
+    public InfinispanEmbeddedComponent getComponent() {
+        return (InfinispanEmbeddedComponent) super.getComponent();
     }
 
     @Override
     public Producer createProducer() throws Exception {
-        return new InfinispanEmbeddedProducer(this, cacheName, manager, configuration);
+        return new InfinispanEmbeddedProducer(this, cacheName, getComponent().getManager(), configuration);
     }
 
     @Override
     public Consumer createConsumer(Processor processor) throws Exception {
         InfinispanEmbeddedConsumer consumer
-                = new InfinispanEmbeddedConsumer(this, processor, cacheName, manager, configuration);
+                = new InfinispanEmbeddedConsumer(this, processor, cacheName, getComponent().getManager(), configuration);
 
         configureConsumer(consumer);
         return consumer;
