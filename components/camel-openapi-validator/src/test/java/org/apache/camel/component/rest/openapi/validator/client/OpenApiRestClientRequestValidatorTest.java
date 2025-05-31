f2d1e5b728440eabfb107495e4098ec4512cb2ba
Björn Beskow <bbeskow@gmail.com>
2025-05-31 15:08:23 +0200

    CAMEL-22117: Include query params and headers in validation context. (#18233)
    * CAMEL-22117: Include query params and headers in validation context.
    
    * Header prefix check should be case insensitive.
    
    ---------
    
    Co-authored-by: Bjorn Beskow <bb@callistaenterprise.se>

--- components/camel-openapi-validator/src/test/java/org/apache/camel/component/rest/openapi/validator/client/OpenApiRestClientRequestValidatorTest.java
+++ components/camel-openapi-validator/src/test/java/org/apache/camel/component/rest/openapi/validator/client/OpenApiRestClientRequestValidatorTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.camel.component.rest.openapi.validator.client;
 
+import java.io.IOException;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
@@ -24,25 +26,31 @@ import org.apache.camel.spi.RestClientRequestValidator;
 import org.apache.camel.test.junit5.ExchangeTestSupport;
 import org.apache.camel.util.IOHelper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class OpenApiRestClientRequestValidatorTest extends ExchangeTestSupport {
 
-    @Test
-    public void testValidator() throws Exception {
+    static OpenAPI openAPI;
+    static OpenApiRestClientRequestValidator validator;
+
+    @BeforeAll
+    static void setup() throws IOException {
         String data = IOHelper.loadText(OpenApiRestClientRequestValidatorTest.class.getResourceAsStream("/petstore-v3.json"));
         OpenAPIV3Parser parser = new OpenAPIV3Parser();
         SwaggerParseResult out = parser.readContents(data);
-        OpenAPI openAPI = out.getOpenAPI();
+        openAPI = out.getOpenAPI();
+        validator = new OpenApiRestClientRequestValidator();
+    }
 
+    @Test
+    public void testValidateBody() {
         exchange.setProperty(Exchange.REST_OPENAPI, openAPI);
         exchange.setProperty(Exchange.CONTENT_TYPE, "application/json");
         exchange.getMessage().setHeader(Exchange.HTTP_METHOD, "PUT");
         exchange.getMessage().setHeader(Exchange.HTTP_PATH, "pet");
         exchange.getMessage().setHeader("Accept", "application/json");
         exchange.getMessage().setBody("");
-
-        OpenApiRestClientRequestValidator validator = new OpenApiRestClientRequestValidator();
 
         RestClientRequestValidator.ValidationError error
                 = validator.validate(exchange, new RestClientRequestValidator.ValidationContext(
@@ -52,6 +60,52 @@ public class OpenApiRestClientRequestValidatorTest extends ExchangeTestSupport {
         Assertions.assertTrue(error.body().contains("A request body is required but none found"));
 
         exchange.getMessage().setBody("{ some body here }");
+
+        error = validator.validate(exchange, new RestClientRequestValidator.ValidationContext(
+                "application/json", "application/json", true, null, null, null, null));
+        Assertions.assertNull(error);
+    }
+
+    @Test
+    public void testValidateQueryParam() {
+        exchange.setProperty(Exchange.REST_OPENAPI, openAPI);
+        exchange.setProperty(Exchange.CONTENT_TYPE, "application/json");
+        exchange.getMessage().setHeader(Exchange.HTTP_METHOD, "GET");
+        exchange.getMessage().setHeader(Exchange.HTTP_PATH, "pet/findByStatus");
+        exchange.getMessage().setHeader("Accept", "application/json");
+        exchange.getMessage().setBody("");
+
+        RestClientRequestValidator.ValidationError error
+                = validator.validate(exchange, new RestClientRequestValidator.ValidationContext(
+                        "application/json", "application/json", true, null, null, null, null));
+
+        Assertions.assertNotNull(error);
+        Assertions.assertTrue(error.body().contains("Query parameter 'status' is required"));
+
+        exchange.getMessage().setHeader(Exchange.HTTP_QUERY, "status=available");
+
+        error = validator.validate(exchange, new RestClientRequestValidator.ValidationContext(
+                "application/json", "application/json", true, null, null, null, null));
+        Assertions.assertNull(error);
+    }
+
+    @Test
+    public void testValidateHeader() {
+        exchange.setProperty(Exchange.REST_OPENAPI, openAPI);
+        exchange.setProperty(Exchange.CONTENT_TYPE, "application/json");
+        exchange.getMessage().setHeader(Exchange.HTTP_METHOD, "GET");
+        exchange.getMessage().setHeader(Exchange.HTTP_PATH, "pet/findByTags");
+        exchange.getMessage().setHeader("Accept", "application/json");
+        exchange.getMessage().setBody("");
+
+        RestClientRequestValidator.ValidationError error
+                = validator.validate(exchange, new RestClientRequestValidator.ValidationContext(
+                        "application/json", "application/json", true, null, null, null, null));
+
+        Assertions.assertNotNull(error);
+        Assertions.assertTrue(error.body().contains("Header parameter 'tags' is required"));
+
+        exchange.getMessage().setHeader("tags", "dog");
 
         error = validator.validate(exchange, new RestClientRequestValidator.ValidationContext(
                 "application/json", "application/json", true, null, null, null, null));
