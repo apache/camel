6ea3733a3c66be708d659e04b2d66a7298eb494b
Vineet Saurabh <vineetsaurabh@gmail.com>
2025-06-04 07:45:12 +0200

    CAMEL-16483: camel-nats: Add jetstream support
    * CAMEL-16483 Producer with Jetstream
    
    * CAMEL-16483 Support for endpoints for new parameters
    
    * CAMEL-16483 Fix subject overlap error
    
    * NATS Jetstream consumer
    
    Example Route:
    
    from("nats://my.topic?" +
                    "jetstreamEnabled=true") // Subscribe to the 'my.topic' subject
                    .process(exchange -> {
                        String message = exchange.getIn().getBody(String.class);
                        System.out.println("Received message on 'my.topic': " + message);
                    });
    
    Send Message through CLI:
    nats pub my.topic "Hello from NATS on my.topic"
    
    * Camel 16483 Fix from comments on PR #18212
    
    * Camel 16483 remove * from imports
    
    * Camel 16483 Jetstream default disable
    
    * Camel 16483 Publish method for Jetstream
    
    * Breaking publish up into 2 methods, so one is jetstream and the other is non jetstream and as-is today
    * json and NatsEndpointConfigurer after mvn clean install -Dquickly
    
    * Camel 16483 coding standards and style
    
    run mvn clean install -DskipTests locally from root folder and I have committed all auto-generated changes
    
    * CAMEL-16483 Consumer refactoring and minor changes
    
    * Camel 16483 Exception handling
    
    ---------
    
    Co-authored-by: Vineet Saurabh <vineet.saurabh@bbraun.com>
    Co-authored-by: Claus Ibsen <claus.ibsen@gmail.com>

--- components/camel-nats/src/main/java/org/apache/camel/component/nats/NatsConsumer.java
+++ components/camel-nats/src/main/java/org/apache/camel/component/nats/NatsConsumer.java
@@ -16,14 +16,21 @@
  */
 package org.apache.camel.component.nats;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 
 import io.nats.client.Connection;
 import io.nats.client.Connection.Status;
 import io.nats.client.Dispatcher;
+import io.nats.client.JetStreamApiException;
+import io.nats.client.JetStreamManagement;
+import io.nats.client.JetStreamSubscription;
 import io.nats.client.Message;
 import io.nats.client.MessageHandler;
+import io.nats.client.PushSubscribeOptions;
+import io.nats.client.api.ConsumerConfiguration;
+import io.nats.client.api.StreamConfiguration;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.spi.HeaderFilterStrategy;
@@ -41,6 +48,7 @@ public class NatsConsumer extends DefaultConsumer {
     private Connection connection;
     private Dispatcher dispatcher;
     private boolean active;
+    private JetStreamSubscription jetStreamSubscription;
 
     public NatsConsumer(NatsEndpoint endpoint, Processor processor) {
         super(endpoint, processor);
@@ -123,35 +131,86 @@ public class NatsConsumer extends DefaultConsumer {
         @Override
         public void run() {
             try {
-                NatsConsumer.this.dispatcher = this.connection.createDispatcher(new CamelNatsMessageHandler());
-                if (ObjectHelper.isNotEmpty(this.configuration.getQueueName())) {
-                    NatsConsumer.this.dispatcher = NatsConsumer.this.dispatcher.subscribe(
-                            NatsConsumer.this.getEndpoint().getConfiguration().getTopic(),
-                            NatsConsumer.this.getEndpoint().getConfiguration().getQueueName());
-                    if (ObjectHelper.isNotEmpty(NatsConsumer.this.getEndpoint().getConfiguration().getMaxMessages())) {
-                        NatsConsumer.this.dispatcher.unsubscribe(
-                                NatsConsumer.this.getEndpoint().getConfiguration().getTopic(),
-                                Integer.parseInt(NatsConsumer.this.getEndpoint().getConfiguration().getMaxMessages()));
-                    }
-                    if (NatsConsumer.this.dispatcher.isActive()) {
-                        NatsConsumer.this.setActive(true);
-                    }
+                NatsConfiguration config = getEndpoint().getConfiguration();
+                String topic = config.getTopic();
+                String queueName = config.getQueueName();
+                String maxMessagesStr = config.getMaxMessages();
+                Integer maxMessages = null;
+                if (ObjectHelper.isNotEmpty(maxMessagesStr)) {
+                    maxMessages = Integer.parseInt(maxMessagesStr);
+                }
+
+                if (config.isJetstreamEnabled() && connection.getServerInfo().isJetStreamAvailable()) {
+                    setupJetStreamConsumer(topic, queueName);
                 } else {
-                    NatsConsumer.this.dispatcher = NatsConsumer.this.dispatcher
-                            .subscribe(NatsConsumer.this.getEndpoint().getConfiguration().getTopic());
-                    if (ObjectHelper.isNotEmpty(NatsConsumer.this.getEndpoint().getConfiguration().getMaxMessages())) {
-                        NatsConsumer.this.dispatcher.unsubscribe(
-                                NatsConsumer.this.getEndpoint().getConfiguration().getTopic(),
-                                Integer.parseInt(NatsConsumer.this.getEndpoint().getConfiguration().getMaxMessages()));
-                    }
-                    if (NatsConsumer.this.dispatcher.isActive()) {
-                        NatsConsumer.this.setActive(true);
-                    }
+                    setupStandardNatsConsumer(topic, queueName, maxMessages);
                 }
             } catch (final Exception e) {
                 NatsConsumer.this.getExceptionHandler().handleException("Error during processing", e);
             }
 
+        }
+
+        private void setupJetStreamConsumer(String topic, String queueName) throws IOException, JetStreamApiException {
+            String streamName = this.configuration.getJetstreamName();
+            String consumerName
+                    = ObjectHelper.isNotEmpty(queueName) ? queueName : "consumer-" + System.currentTimeMillis(); // Generate a default consumer name if queueName is not provided
+            LOG.debug("Setting up JetStream PUSH consumer for stream: '{}', durable: '{}', topic: {} ", streamName,
+                    consumerName, this.configuration.getTopic());
+
+            JetStreamManagement jsm = connection.jetStreamManagement();
+            try {
+                // Try to get the stream, create it if it doesn't exist
+                jsm.getStreamInfo(streamName);
+            } catch (JetStreamApiException e) {
+                if (e.getErrorCode() == 404) {
+                    StreamConfiguration streamConfig = StreamConfiguration.builder()
+                            .name(streamName)
+                            .subjects(topic)
+                            .build();
+                    jsm.addStream(streamConfig);
+                } else {
+                    throw e;
+                }
+            }
+
+            ConsumerConfiguration.Builder ccBuilder = ConsumerConfiguration.builder()
+                    .durable(consumerName);
+
+            ccBuilder.deliverSubject(null);
+            ConsumerConfiguration cc = ccBuilder.build();
+
+            PushSubscribeOptions pushOptions = PushSubscribeOptions.builder()
+                    .configuration(cc)
+                    .build();
+
+            NatsConsumer.this.dispatcher = this.connection.createDispatcher(new CamelNatsMessageHandler());
+
+            NatsConsumer.this.jetStreamSubscription = this.connection.jetStream().subscribe(
+                    NatsConsumer.this.getEndpoint().getConfiguration().getTopic(),
+                    queueName,
+                    dispatcher,
+                    new CamelNatsMessageHandler(),
+                    true,
+                    pushOptions);
+
+            NatsConsumer.this.setActive(true);
+        }
+
+        private void setupStandardNatsConsumer(String topic, String queueName, Integer maxMessages) {
+            LOG.debug("Setting up standard NATS consumer for topic: {}", topic);
+            NatsConsumer.this.dispatcher = connection.createDispatcher(new CamelNatsMessageHandler());
+            if (ObjectHelper.isNotEmpty(queueName)) {
+                NatsConsumer.this.dispatcher = NatsConsumer.this.dispatcher.subscribe(topic, queueName);
+            } else {
+                NatsConsumer.this.dispatcher = NatsConsumer.this.dispatcher.subscribe(topic);
+            }
+            if (maxMessages != null) {
+                NatsConsumer.this.dispatcher.unsubscribe(topic, maxMessages);
+            }
+            if (NatsConsumer.this.dispatcher.isActive()) {
+                NatsConsumer.this.setActive(true);
+            }
         }
 
         class CamelNatsMessageHandler implements MessageHandler {
--- dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/NatsEndpointBuilderFactory.java
+++ dsl/camel-endpointdsl/src/generated/java/org/apache/camel/builder/endpoint/dsl/NatsEndpointBuilderFactory.java
@@ -134,6 +134,50 @@ public interface NatsEndpointBuilderFactory {
             return this;
         }
         /**
+         * Sets whether to enable JetStream support for this endpoint.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: common
+         * 
+         * @param jetstreamEnabled the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointConsumerBuilder jetstreamEnabled(boolean jetstreamEnabled) {
+            doSetProperty("jetstreamEnabled", jetstreamEnabled);
+            return this;
+        }
+        /**
+         * Sets whether to enable JetStream support for this endpoint.
+         * 
+         * The option will be converted to a <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: common
+         * 
+         * @param jetstreamEnabled the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointConsumerBuilder jetstreamEnabled(String jetstreamEnabled) {
+            doSetProperty("jetstreamEnabled", jetstreamEnabled);
+            return this;
+        }
+        /**
+         * Sets the name of the JetStream stream to use.
+         * 
+         * The option is a: <code>java.lang.String</code> type.
+         * 
+         * Group: common
+         * 
+         * @param jetstreamName the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointConsumerBuilder jetstreamName(String jetstreamName) {
+            doSetProperty("jetstreamName", jetstreamName);
+            return this;
+        }
+        /**
          * maximum number of pings have not received a response allowed by the
          * client.
          * 
@@ -814,6 +858,36 @@ public interface NatsEndpointBuilderFactory {
             return this;
         }
         /**
+         * Sets whether to operate JetStream requests asynchronously.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: true
+         * Group: advanced
+         * 
+         * @param jetstreamAsync the value to set
+         * @return the dsl builder
+         */
+        default AdvancedNatsEndpointConsumerBuilder jetstreamAsync(boolean jetstreamAsync) {
+            doSetProperty("jetstreamAsync", jetstreamAsync);
+            return this;
+        }
+        /**
+         * Sets whether to operate JetStream requests asynchronously.
+         * 
+         * The option will be converted to a <code>boolean</code> type.
+         * 
+         * Default: true
+         * Group: advanced
+         * 
+         * @param jetstreamAsync the value to set
+         * @return the dsl builder
+         */
+        default AdvancedNatsEndpointConsumerBuilder jetstreamAsync(String jetstreamAsync) {
+            doSetProperty("jetstreamAsync", jetstreamAsync);
+            return this;
+        }
+        /**
          * Whether or not connection trace messages should be printed to
          * standard out for fine grained debugging of connection issues.
          * 
@@ -945,6 +1019,50 @@ public interface NatsEndpointBuilderFactory {
          */
         default NatsEndpointProducerBuilder flushTimeout(String flushTimeout) {
             doSetProperty("flushTimeout", flushTimeout);
+            return this;
+        }
+        /**
+         * Sets whether to enable JetStream support for this endpoint.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: common
+         * 
+         * @param jetstreamEnabled the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointProducerBuilder jetstreamEnabled(boolean jetstreamEnabled) {
+            doSetProperty("jetstreamEnabled", jetstreamEnabled);
+            return this;
+        }
+        /**
+         * Sets whether to enable JetStream support for this endpoint.
+         * 
+         * The option will be converted to a <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: common
+         * 
+         * @param jetstreamEnabled the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointProducerBuilder jetstreamEnabled(String jetstreamEnabled) {
+            doSetProperty("jetstreamEnabled", jetstreamEnabled);
+            return this;
+        }
+        /**
+         * Sets the name of the JetStream stream to use.
+         * 
+         * The option is a: <code>java.lang.String</code> type.
+         * 
+         * Group: common
+         * 
+         * @param jetstreamName the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointProducerBuilder jetstreamName(String jetstreamName) {
+            doSetProperty("jetstreamName", jetstreamName);
             return this;
         }
         /**
@@ -1511,6 +1629,36 @@ public interface NatsEndpointBuilderFactory {
             return this;
         }
         /**
+         * Sets whether to operate JetStream requests asynchronously.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: true
+         * Group: advanced
+         * 
+         * @param jetstreamAsync the value to set
+         * @return the dsl builder
+         */
+        default AdvancedNatsEndpointProducerBuilder jetstreamAsync(boolean jetstreamAsync) {
+            doSetProperty("jetstreamAsync", jetstreamAsync);
+            return this;
+        }
+        /**
+         * Sets whether to operate JetStream requests asynchronously.
+         * 
+         * The option will be converted to a <code>boolean</code> type.
+         * 
+         * Default: true
+         * Group: advanced
+         * 
+         * @param jetstreamAsync the value to set
+         * @return the dsl builder
+         */
+        default AdvancedNatsEndpointProducerBuilder jetstreamAsync(String jetstreamAsync) {
+            doSetProperty("jetstreamAsync", jetstreamAsync);
+            return this;
+        }
+        /**
          * Whether or not connection trace messages should be printed to
          * standard out for fine grained debugging of connection issues.
          * 
@@ -1643,6 +1791,50 @@ public interface NatsEndpointBuilderFactory {
          */
         default NatsEndpointBuilder flushTimeout(String flushTimeout) {
             doSetProperty("flushTimeout", flushTimeout);
+            return this;
+        }
+        /**
+         * Sets whether to enable JetStream support for this endpoint.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: common
+         * 
+         * @param jetstreamEnabled the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointBuilder jetstreamEnabled(boolean jetstreamEnabled) {
+            doSetProperty("jetstreamEnabled", jetstreamEnabled);
+            return this;
+        }
+        /**
+         * Sets whether to enable JetStream support for this endpoint.
+         * 
+         * The option will be converted to a <code>boolean</code> type.
+         * 
+         * Default: false
+         * Group: common
+         * 
+         * @param jetstreamEnabled the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointBuilder jetstreamEnabled(String jetstreamEnabled) {
+            doSetProperty("jetstreamEnabled", jetstreamEnabled);
+            return this;
+        }
+        /**
+         * Sets the name of the JetStream stream to use.
+         * 
+         * The option is a: <code>java.lang.String</code> type.
+         * 
+         * Group: common
+         * 
+         * @param jetstreamName the value to set
+         * @return the dsl builder
+         */
+        default NatsEndpointBuilder jetstreamName(String jetstreamName) {
+            doSetProperty("jetstreamName", jetstreamName);
             return this;
         }
         /**
@@ -2119,6 +2311,36 @@ public interface NatsEndpointBuilderFactory {
          */
         default AdvancedNatsEndpointBuilder headerFilterStrategy(String headerFilterStrategy) {
             doSetProperty("headerFilterStrategy", headerFilterStrategy);
+            return this;
+        }
+        /**
+         * Sets whether to operate JetStream requests asynchronously.
+         * 
+         * The option is a: <code>boolean</code> type.
+         * 
+         * Default: true
+         * Group: advanced
+         * 
+         * @param jetstreamAsync the value to set
+         * @return the dsl builder
+         */
+        default AdvancedNatsEndpointBuilder jetstreamAsync(boolean jetstreamAsync) {
+            doSetProperty("jetstreamAsync", jetstreamAsync);
+            return this;
+        }
+        /**
+         * Sets whether to operate JetStream requests asynchronously.
+         * 
+         * The option will be converted to a <code>boolean</code> type.
+         * 
+         * Default: true
+         * Group: advanced
+         * 
+         * @param jetstreamAsync the value to set
+         * @return the dsl builder
+         */
+        default AdvancedNatsEndpointBuilder jetstreamAsync(String jetstreamAsync) {
+            doSetProperty("jetstreamAsync", jetstreamAsync);
             return this;
         }
         /**
