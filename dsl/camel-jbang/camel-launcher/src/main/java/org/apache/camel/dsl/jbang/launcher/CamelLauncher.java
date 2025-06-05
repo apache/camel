b8934018e4c8c0ad24e9bba9cddfb3626965cf14
Guillaume Nodet <gnodet@gmail.com>
2025-06-05 13:51:35 +0200

    CAMEL-21658: Create a fat-jar launcher for Camel JBang (#18127)
    CAMEL-21658: Create a fat-jar launcher for Camel JBang
    
    This commit implements a fat-jar launcher for Camel JBang that provides a pre-built distribution with all dependencies included. The launcher eliminates the two-step process where JBang first parses the .java file and then calls camel-jbang.

---
+++ dsl/camel-jbang/camel-launcher/src/main/java/org/apache/camel/dsl/jbang/launcher/CamelLauncher.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.dsl.jbang.launcher;
+
+import org.apache.camel.dsl.jbang.core.commands.CamelJBangMain;
+
+/**
+ * Main class for the Camel JBang Fat-Jar Launcher.
+ * <p>
+ * This launcher provides a self-contained executable JAR that includes all dependencies required to run Camel JBang
+ * without the need for the JBang two-step process.
+ */
+public class CamelLauncher {
+
+    /**
+     * Main entry point for the Camel JBang Fat-Jar Launcher.
+     *
+     * @param args command line arguments to pass to Camel JBang
+     */
+    public static void main(String... args) {
+        CamelJBangMain.run(args);
+    }
+}
