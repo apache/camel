c5754ad14896d19c8043d297a3267490a2a64068
Claus Ibsen <claus.ibsen@gmail.com>
2025-06-18 22:29:34 +0200

    CAMEL-22180: camel-simple - Using inlined jsonpath with 2-arg exp should trim so you can use space around comma

--- core/camel-core-languages/src/main/java/org/apache/camel/language/simple/ast/SimpleFunctionExpression.java
+++ core/camel-core-languages/src/main/java/org/apache/camel/language/simple/ast/SimpleFunctionExpression.java
@@ -571,6 +571,12 @@ public class SimpleFunctionExpression extends LiteralExpression {
                     || exp.startsWith("variable:")) {
                 String input = StringHelper.before(exp, ",");
                 exp = StringHelper.after(exp, ",");
+                if (input != null) {
+                    input = input.trim();
+                }
+                if (exp != null) {
+                    exp = exp.trim();
+                }
                 return ExpressionBuilder.singleInputLanguageExpression("jq", exp, input);
             }
             return ExpressionBuilder.languageExpression("jq", exp);
@@ -587,6 +593,12 @@ public class SimpleFunctionExpression extends LiteralExpression {
                     || exp.startsWith("variable:")) {
                 String input = StringHelper.before(exp, ",");
                 exp = StringHelper.after(exp, ",");
+                if (input != null) {
+                    input = input.trim();
+                }
+                if (exp != null) {
+                    exp = exp.trim();
+                }
                 return ExpressionBuilder.singleInputLanguageExpression("jsonpath", exp, input);
             }
             return ExpressionBuilder.languageExpression("jsonpath", exp);
@@ -602,6 +614,12 @@ public class SimpleFunctionExpression extends LiteralExpression {
                     || exp.startsWith("variable:")) {
                 String input = StringHelper.before(exp, ",");
                 exp = StringHelper.after(exp, ",");
+                if (input != null) {
+                    input = input.trim();
+                }
+                if (exp != null) {
+                    exp = exp.trim();
+                }
                 return ExpressionBuilder.singleInputLanguageExpression("xpath", exp, input);
             }
             return ExpressionBuilder.languageExpression("xpath", exp);
