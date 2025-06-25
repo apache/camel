7dcb1d493f400af870a5aa5b8bac932b9cef0fac
Claus Ibsen <claus.ibsen@gmail.com>
2025-06-25 14:46:50 +0200

    CAMEL-22198: camel-resilience4j - throwExceptionWhenHalfOpenOrOpenState is not always thrown if in OPEN / HALF_OPEN state

--- core/camel-core-model/src/main/java/org/apache/camel/model/Resilience4jConfigurationDefinition.java
+++ core/camel-core-model/src/main/java/org/apache/camel/model/Resilience4jConfigurationDefinition.java
@@ -101,8 +101,8 @@ public class Resilience4jConfigurationDefinition extends Resilience4jConfigurati
      * Whether to throw io.github.resilience4j.circuitbreaker.CallNotPermittedException when the call is rejected due
      * circuit breaker is half open (and was not attempted but rejected immediately) or open (always rejected).
      *
-     * This option is only in use when there is NOT a fallback configured on the circuit breaker. When there is a fallback
-     * then the fallback is always executed and CallNotPermittedException is not thrown.
+     * This option is only in use when there is NOT a fallback configured on the circuit breaker. When there is a
+     * fallback then the fallback is always executed and CallNotPermittedException is not thrown.
      */
     public Resilience4jConfigurationDefinition throwExceptionWhenHalfOpenOrOpenState(
             boolean throwExceptionWhenHalfOpenOrOpenState) {
