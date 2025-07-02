39cb9092a239a270fe0a5f955bcebed687e1df7e
Claus Ibsen <claus.ibsen@gmail.com>
2025-07-02 14:43:16 +0200

    CAMEL-22212: camel-smb - From smb to file with streamDownload and no stream caching

---
+++ components/camel-smb/src/main/java/org/apache/camel/component/smb/SmbConverter.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.smb;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.camel.Converter;
+import org.apache.camel.Exchange;
+
+@Converter(generateLoader = true)
+public class SmbConverter {
+
+    private SmbConverter() {
+        // Helper Class
+    }
+
+    @Converter(allowNull = true)
+    public static InputStream smbToInputStream(SmbFile file, Exchange exchange) throws Exception {
+        // allow null as valid response, because camel-smb can be set to download=false
+        Object body = file.getBody();
+        if (body == null) {
+            return null;
+        }
+        if (body instanceof InputStream is) {
+            return is;
+        } else if (body instanceof byte[] arr) {
+            return new ByteArrayInputStream(arr);
+        } else {
+            return exchange.getContext().getTypeConverter().convertTo(InputStream.class, exchange, body);
+        }
+    }
+
+}
