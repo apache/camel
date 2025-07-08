1a37238384e5777003fbfdcc37f11db60c0e2db3
Croway <federico.mariani.1990@gmail.com>
2025-07-08 18:35:40 +0200

    CAMEL-22222: Add component for Apache Iggy

---
+++ components/camel-iggy/src/main/java/org/apache/camel/component/iggy/IggyConstants.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.iggy;
+
+public final class IggyConstants {
+
+    public static final String MESSAGE_ID = "CamelIggyMessageId";
+    public static final String MESSAGE_OFFSET = "CamelIggyMessageOffset";
+    public static final String MESSAGE_TIMESTAMP = "CamelIggyMessageTimestamp";
+    public static final String MESSAGE_ORIGIN_TIMESTAMP = "CamelIggyMessageOriginTimestamp";
+    public static final String MESSAGE_CHECKSUM = "CamelIggyMessageChecksum";
+    public static final String MESSAGE_LENGTH = "CamelIggyMessageLength";
+    public static final String MESSAGE_SIZE = "CamelIggyMessageSize";
+
+    private IggyConstants() {
+        // Constants class
+    }
+}
