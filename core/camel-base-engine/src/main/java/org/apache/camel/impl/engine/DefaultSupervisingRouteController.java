475bcb32f2a58a3efdd3550a2fd4a9c23dac338a
Claus Ibsen <claus.ibsen@gmail.com>
2025-08-02 08:13:57 +0200

    CAMEL-22310: camel-core - Add RouteGroupMBean

--- core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/DefaultSupervisingRouteController.java
+++ core/camel-base-engine/src/main/java/org/apache/camel/impl/engine/DefaultSupervisingRouteController.java
@@ -68,9 +68,9 @@ import org.slf4j.LoggerFactory;
  * and takes control of starting the routes in a safe manner. This controller is able to retry starting failing routes,
  * and have various options to configure settings for backoff between restarting routes.
  * <p/>
- * When a route has been successfully started, then this supervising controller will no longer manage the route
- * and afterward the route is managed by the default route controller. The supervising capabilities are designed
- * to only be in use during the startup of the application.
+ * When a route has been successfully started, then this supervising controller will no longer manage the route and
+ * afterward the route is managed by the default route controller. The supervising capabilities are designed to only be
+ * in use during the startup of the application.
  *
  * @see DefaultRouteController
  */
