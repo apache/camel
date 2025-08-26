86b733b63b7a1e9d4b786a2c35f5ffe4fb6b5ee1
Claus Ibsen <claus.ibsen@gmail.com>
2025-08-26 10:49:07 +0200

    CAMEL-22364: camel-jms - Fix rare ConcurrentModificationException in camel-jms due to destination produced is set as header after sent, which can lead to a race condition when doing request/reply over JMS and reply is already received and continued processing. Thanks to Vladimir Dobos for the analysis. (#18996)

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsProducer.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsProducer.java
@@ -218,7 +218,6 @@ public class JmsProducer extends DefaultAsyncProducer {
             in.setHeader(correlationPropertyToUse, GENERATED_CORRELATION_ID_PREFIX + getUuidGenerator().generateUuid());
         }
 
-        final String to = destinationName != null ? destinationName : getDestinationName(destination);
         MessageCreator messageCreator = new MessageCreator() {
             public Message createMessage(Session session) throws JMSException {
                 Message answer = endpoint.getBinding().makeJmsMessage(exchange, in, session, null);
@@ -254,12 +253,7 @@ public class JmsProducer extends DefaultAsyncProducer {
             }
         };
 
-        doSend(true, destinationName, destination, messageCreator, messageSentCallback);
-
-        // record where we sent the message
-        if (to != null) {
-            exchange.getMessage().setHeader(JmsConstants.JMS_DESTINATION_NAME_PRODUCED, to);
-        }
+        doSend(exchange, true, destinationName, destination, messageCreator, messageSentCallback);
 
         // continue routing asynchronously (reply will be processed async when its received)
         return false;
@@ -396,14 +390,10 @@ public class JmsProducer extends DefaultAsyncProducer {
             }
         };
 
-        doSend(false, destinationName, destination, messageCreator, messageSentCallback);
+        doSend(exchange, false, destinationName, destination, messageCreator, messageSentCallback);
 
         // after sending then set the OUT message id to the JMSMessageID so its identical
         setMessageId(exchange);
-        // record where we sent the message
-        if (to != null) {
-            exchange.getMessage().setHeader(JmsConstants.JMS_DESTINATION_NAME_PRODUCED, to);
-        }
 
         // we are synchronous so return true
         callback.done(true);
@@ -413,6 +403,7 @@ public class JmsProducer extends DefaultAsyncProducer {
     /**
      * Sends the message using the JmsTemplate.
      *
+     * @param exchange        the exchange
      * @param inOut           use inOut or inOnly template
      * @param destinationName the destination name
      * @param destination     the destination (if no name provided)
@@ -420,8 +411,15 @@ public class JmsProducer extends DefaultAsyncProducer {
      * @param callback        optional callback to invoke when message has been sent
      */
     protected void doSend(
+            Exchange exchange,
             boolean inOut, String destinationName, Destination destination,
             MessageCreator messageCreator, MessageSentCallback callback) {
+
+        // record where we sent the message
+        String to = destinationName != null ? destinationName : getDestinationName(destination);
+        if (to != null) {
+            exchange.getMessage().setHeader(JmsConstants.JMS_DESTINATION_NAME_PRODUCED, to);
+        }
 
         CamelJmsTemplate template = (CamelJmsTemplate) (inOut ? getInOutTemplate() : getInOnlyTemplate());
 
