8bcab651f2f8bab5f2df9c764a2d1a1af2192377
Claus Ibsen <claus.ibsen@gmail.com>
2025-10-06 09:33:47 +0200

    Upgrade exec-maven-plugin and fix camel:dev and camel:debug maven commands.

--- tooling/maven/camel-maven-plugin/src/main/java/org/apache/camel/maven/DebugMojo.java
+++ tooling/maven/camel-maven-plugin/src/main/java/org/apache/camel/maven/DebugMojo.java
@@ -16,34 +16,15 @@
  */
 package org.apache.camel.maven;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import javax.inject.Inject;
 
 import org.apache.camel.spi.BacklogDebugger;
-import org.apache.camel.util.CastUtils;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.handler.DefaultArtifactHandler;
-import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
-import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
-import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProjectBuilder;
+import org.eclipse.aether.RepositorySystem;
 
 /**
  * The maven goal allowing to automatically set up the Camel application to debug the Camel routes thanks to the Camel
@@ -59,16 +40,9 @@ public class DebugMojo extends DevMojo {
     @Parameter(property = "camel.suspend", defaultValue = "true")
     private boolean suspend;
 
-    @Parameter(defaultValue = "${mojoExecution}", readonly = true)
-    private MojoExecution mojo;
-
-    @Parameter(defaultValue = "${session}", readonly = true)
-    private MavenSession session;
-
     @Inject
-    public DebugMojo(ArtifactResolver artifactResolver, ArtifactFactory artifactFactory, ArtifactMetadataSource metadataSource,
-                     MavenProjectBuilder projectBuilder) {
-        super(artifactResolver, artifactFactory, metadataSource, projectBuilder);
+    public DebugMojo(RepositorySystem repositorySystem) {
+        super(repositorySystem);
     }
 
     @Override
@@ -93,70 +67,4 @@ public class DebugMojo extends DevMojo {
         return "camel:debug";
     }
 
-    @Override
-    protected List<Artifact> getClasspath() throws MojoExecutionException, MojoFailureException {
-        List<Artifact> classpath = super.getClasspath();
-        if (classpath.stream().anyMatch(artifact -> "org.apache.camel".equals(artifact.getGroupId())
-                && "camel-debug".equals(artifact.getArtifactId()))) {
-            getLog().debug("The component camel-debug has been detected in the classpath so no need to add it");
-            return classpath;
-        }
-        getLog().info("The component camel-debug is not available in the classpath, it will be added automatically");
-        Optional<String> camelCoreVersion = classpath.stream()
-                .filter(artifact -> "org.apache.camel".equals(artifact.getGroupId())
-                        && Objects.nonNull(artifact.getArtifactId()) && artifact.getArtifactId().startsWith("camel-core"))
-                .map(Artifact::getBaseVersion)
-                .filter(Objects::nonNull)
-                .findAny();
-        if (camelCoreVersion.isEmpty()) {
-            getLog().info("The version of Camel could not be detected, the version of the plugin will be used instead");
-            addCamelDebug(classpath, mojo.getVersion());
-            return classpath;
-        }
-        addCamelDebug(classpath, camelCoreVersion.get());
-        return classpath;
-    }
-
-    /**
-     * Automatically retrieve the given version of camel-debug and add it to the classpath if it can be found.
-     *
-     * @param classpath the classpath to which camel-debug and its dependencies are added.
-     * @param version   the version of camel-debug to retrieve.
-     */
-    private void addCamelDebug(List<Artifact> classpath, String version) {
-        getLog().debug(String.format("Trying to retrieve the version %s of camel-debug", version));
-        ArtifactResolutionRequest request = new ArtifactResolutionRequest();
-        request.setResolveRoot(true);
-        request.setResolveTransitively(true);
-        request.setLocalRepository(session.getLocalRepository());
-        request.setRemoteRepositories(session.getCurrentProject().getRemoteArtifactRepositories());
-        request.setOffline(session.isOffline());
-        request.setForceUpdate(session.getRequest().isUpdateSnapshots());
-        request.setServers(session.getRequest().getServers());
-        request.setMirrors(session.getRequest().getMirrors());
-        request.setProxies(session.getRequest().getProxies());
-        request.setManagedVersionMap(Collections.emptyMap());
-        request.setArtifact(
-                new DefaultArtifact(
-                        "org.apache.camel", "camel-debug", version, Artifact.SCOPE_RUNTIME, "jar", null,
-                        new DefaultArtifactHandler("jar")));
-        request.setResolutionFilter(new ScopeArtifactFilter(Artifact.SCOPE_RUNTIME));
-        ArtifactResolutionResult result = artifactResolver.resolve(request);
-        if (result.isSuccess()) {
-            getLog().info(String.format("Adding the version %s of camel-debug", version));
-            classpath.addAll(CastUtils.cast(result.getArtifacts()));
-            return;
-        }
-
-        if (result.hasMissingArtifacts()) {
-            getLog().warn(
-                    String.format(
-                            "Could not find the artifacts: %s",
-                            result.getMissingArtifacts().stream().map(Objects::toString).collect(Collectors.joining(", "))));
-        }
-        if (result.hasExceptions()) {
-            result.getExceptions().forEach(
-                    ex -> getLog().warn(String.format("An error occurred while retrieving camel-debug: %s", ex.getMessage())));
-        }
-    }
 }
