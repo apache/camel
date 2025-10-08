d442ee502fcc3bfa4c7ac4f6101730ebb4db23db
Andrea Cosentino <ancosen@gmail.com>
2025-10-08 12:59:45 +0200

    Regen
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
+++ core/camel-util/src/main/java/org/apache/camel/util/SensitiveUtils.java
@@ -192,7 +192,7 @@ public final class SensitiveUtils {
                                                     + "|\\Qverificationcode\\E"
                                                     + "|\\Qwebhookverifytoken\\E"
                                                     + "|\\Qzookeeperpassword\\E"
-                                                    // SENSITIVE-PATTERN: END
+    // SENSITIVE-PATTERN: END
     ;
 
     private SensitiveUtils() {
