ed18db097b3f9696c0af17f2e2fca07de8c872eb
Mikołaj Diakowski <mikdiakowski@interia.pl>
2025-10-10 09:27:03 +0200

    CAMEL-22502: Undo some unnecessary changes regarding System.out and slf4j

--- components/camel-jasypt/src/main/java/org/apache/camel/component/jasypt/Main.java
+++ components/camel-jasypt/src/main/java/org/apache/camel/component/jasypt/Main.java
@@ -24,226 +24,223 @@ import java.util.List;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.iv.RandomIvGenerator;
 import org.jasypt.salt.RandomSaltGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+	private final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+	private final List<Option> options = new ArrayList<>();
+	private String command;
+	private String password;
+	private String input;
+	private String algorithm;
+	private String randomSaltGeneratorAlgorithm;
+	private String randomIvGeneratorAlgorithm;
 
-    private final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-    private final List<Option> options = new ArrayList<>();
-    private String command;
-    private String password;
-    private String input;
-    private String algorithm;
-    private String randomSaltGeneratorAlgorithm;
-    private String randomIvGeneratorAlgorithm;
+	private abstract class Option {
+		private String abbreviation;
+		private String fullName;
+		private String description;
 
-    private abstract class Option {
-        private String abbreviation;
-        private String fullName;
-        private String description;
+		protected Option(String abbreviation, String fullName, String description) {
+			this.abbreviation = "-" + abbreviation;
+			this.fullName = "-" + fullName;
+			this.description = description;
+		}
 
-        protected Option(String abbreviation, String fullName, String description) {
-            this.abbreviation = "-" + abbreviation;
-            this.fullName = "-" + fullName;
-            this.description = description;
-        }
+		public boolean processOption(String arg, LinkedList<String> remainingArgs) {
+			if (arg.equalsIgnoreCase(abbreviation) || fullName.startsWith(arg)) {
+				doProcess(arg, remainingArgs);
+				return true;
+			}
+			return false;
+		}
 
-        public boolean processOption(String arg, LinkedList<String> remainingArgs) {
-            if (arg.equalsIgnoreCase(abbreviation) || fullName.startsWith(arg)) {
-                doProcess(arg, remainingArgs);
-                return true;
-            }
-            return false;
-        }
+		public String getAbbreviation() {
+			return abbreviation;
+		}
 
-        public String getAbbreviation() {
-            return abbreviation;
-        }
+		public String getDescription() {
+			return description;
+		}
 
-        public String getDescription() {
-            return description;
-        }
+		public String getFullName() {
+			return fullName;
+		}
 
-        public String getFullName() {
-            return fullName;
-        }
+		public String getInformation() {
+			return "  " + getAbbreviation() + " or " + getFullName() + " = " + getDescription();
+		}
 
-        public String getInformation() {
-            return "  " + getAbbreviation() + " or " + getFullName() + " = " + getDescription();
-        }
+		protected abstract void doProcess(String arg, LinkedList<String> remainingArgs);
+	}
 
-        protected abstract void doProcess(String arg, LinkedList<String> remainingArgs);
-    }
+	private abstract class ParameterOption extends Option {
+		private String parameterName;
 
-    private abstract class ParameterOption extends Option {
-        private String parameterName;
+		protected ParameterOption(String abbreviation, String fullName, String description, String parameterName) {
+			super(abbreviation, fullName, description);
+			this.parameterName = parameterName;
+		}
 
-        protected ParameterOption(String abbreviation, String fullName, String description, String parameterName) {
-            super(abbreviation, fullName, description);
-            this.parameterName = parameterName;
-        }
+		@Override
+		protected void doProcess(String arg, LinkedList<String> remainingArgs) {
+			if (remainingArgs.isEmpty()) {
+				System.err.println("Expected fileName for ");
+				showOptions();
+			} else {
+				String parameter = remainingArgs.removeFirst();
+				doProcess(arg, parameter, remainingArgs);
+			}
+		}
 
-        @Override
-        protected void doProcess(String arg, LinkedList<String> remainingArgs) {
-            if (remainingArgs.isEmpty()) {
-                LOG.error("Expected parameter for option: {}", arg);
-                showOptions();
-            } else {
-                String parameter = remainingArgs.removeFirst();
-                doProcess(arg, parameter, remainingArgs);
-            }
-        }
+		@Override
+		public String getInformation() {
+			return "  " + getAbbreviation() + " or " + getFullName()
+					+ " <" + parameterName + "> = " + getDescription();
+		}
 
-        @Override
-        public String getInformation() {
-            return "  " + getAbbreviation() + " or " + getFullName()
-                   + " <" + parameterName + "> = " + getDescription();
-        }
+		protected abstract void doProcess(String arg, String parameter, LinkedList<String> remainingArgs);
+	}
 
-        protected abstract void doProcess(String arg, String parameter, LinkedList<String> remainingArgs);
-    }
+	public Main() {
+		addOption(new Option("h", "help", "Displays the help screen") {
+			protected void doProcess(String arg, LinkedList<String> remainingArgs) {
+				showOptions();
+				// no need to process further if user just wants help
+				System.exit(0);
+			}
+		});
 
-    public Main() {
-        addOption(new Option("h", "help", "Displays the help screen") {
-            protected void doProcess(String arg, LinkedList<String> remainingArgs) {
-                showOptions();
-                // no need to process further if user just wants help
-                System.exit(0);
-            }
-        });
+		addOption(new ParameterOption("c", "command", "Command can be encrypt or decrypt", "command") {
+			protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+				if ("encrypt".equals(parameter) || "decrypt".equals(parameter)) {
+					command = parameter;
+				} else {
+					throw new IllegalArgumentException("Unknown command, was: " + parameter);
+				}
+			}
+		});
 
-        addOption(new ParameterOption("c", "command", "Command can be encrypt or decrypt", "command") {
-            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
-                if ("encrypt".equals(parameter) || "decrypt".equals(parameter)) {
-                    command = parameter;
-                } else {
-                    throw new IllegalArgumentException("Unknown command, was: " + parameter);
-                }
-            }
-        });
+		addOption(new ParameterOption("p", "password", "Password to use", "password") {
+			protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+				password = parameter;
+			}
+		});
 
-        addOption(new ParameterOption("p", "password", "Password to use", "password") {
-            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
-                password = parameter;
-            }
-        });
+		addOption(new ParameterOption("i", "input", "Text to encrypt or decrypt", "input") {
+			protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+				input = parameter;
+			}
+		});
 
-        addOption(new ParameterOption("i", "input", "Text to encrypt or decrypt", "input") {
-            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
-                input = parameter;
-            }
-        });
+		addOption(new ParameterOption("a", "algorithm", "Optional algorithm to use", "algorithm") {
+			protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+				algorithm = parameter;
+			}
+		});
 
-        addOption(new ParameterOption("a", "algorithm", "Optional algorithm to use", "algorithm") {
-            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
-                algorithm = parameter;
-            }
-        });
+		addOption(new ParameterOption("rsga", "salt", "Optional random salt generator algorithm to use", "salt") {
+			protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+				randomSaltGeneratorAlgorithm = parameter;
+			}
+		});
 
-        addOption(new ParameterOption("rsga", "salt", "Optional random salt generator algorithm to use", "salt") {
-            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
-                randomSaltGeneratorAlgorithm = parameter;
-            }
-        });
+		addOption(new ParameterOption("riga", "iv", "Optional random iv generator algorithm to use", "iv") {
+			protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
+				randomIvGeneratorAlgorithm = parameter;
+			}
+		});
+	}
 
-        addOption(new ParameterOption("riga", "iv", "Optional random iv generator algorithm to use", "iv") {
-            protected void doProcess(String arg, String parameter, LinkedList<String> remainingArgs) {
-                randomIvGeneratorAlgorithm = parameter;
-            }
-        });
-    }
+	private void addOption(Option option) {
+		options.add(option);
+	}
 
-    private void addOption(Option option) {
-        options.add(option);
-    }
+	private void showOptions() {
+		System.out.println("Apache Camel Jasypt takes the following options:");
+		System.out.println();
+		for (Option option : options) {
+			System.out.println(option.getInformation());
+		}
+		System.out.println();
+		System.out.println();
+	}
 
-    private void showOptions() {
-        LOG.info("Apache Camel Jasypt takes the following options:");
-        for (Option option : options) {
-            LOG.info(option.getInformation());
-        }
-    }
+	private boolean parseArguments(String[] arguments) {
+		LinkedList<String> args = new LinkedList<>(Arrays.asList(arguments));
 
-    private boolean parseArguments(String[] arguments) {
-        LinkedList<String> args = new LinkedList<>(Arrays.asList(arguments));
+		boolean valid = true;
+		while (!args.isEmpty()) {
+			String arg = args.removeFirst();
 
-        boolean valid = true;
-        while (!args.isEmpty()) {
-            String arg = args.removeFirst();
+			boolean handled = false;
+			for (Option option : options) {
+				if (option.processOption(arg, args)) {
+					handled = true;
+					break;
+				}
+			}
+			if (!handled) {
+				System.out.println("Error: Unknown option: " + arg);
+				System.out.println();
+				valid = false;
+				break;
+			}
+		}
 
-            boolean handled = false;
-            for (Option option : options) {
-                if (option.processOption(arg, args)) {
-                    handled = true;
-                    break;
-                }
-            }
-            if (!handled) {
-                LOG.error("Error: Unknown option: {}", arg);
-                valid = false;
-                break;
-            }
-        }
+		return valid;
+	}
 
-        return valid;
-    }
+	public void run(String[] args) {
+		if (!parseArguments(args)) {
+			showOptions();
+			return;
+		}
 
-    public void run(String[] args) {
-        if (!parseArguments(args)) {
-            showOptions();
-            return;
-        }
+		if (command == null) {
+			System.out.println("Error: Command is empty");
+			System.out.println();
+			showOptions();
+			return;
+		}
+		if (password == null) {
+			System.out.println("Error: Password is empty");
+			System.out.println();
+			showOptions();
+			return;
+		}
+		if (input == null) {
+			System.out.println("Error: Input is empty");
+			System.out.println();
+			showOptions();
+			return;
+		}
 
-        if (command == null) {
-            LOG.error("Error: Command is empty");
-            showOptions();
-            return;
-        }
-        if (password == null) {
-            LOG.error("Error: Password is empty");
-            showOptions();
-            return;
-        }
-        if (input == null) {
-            LOG.error("Error: Input is empty");
-            showOptions();
-            return;
-        }
+		encryptor.setPassword(password);
+		if (algorithm != null) {
+			encryptor.setAlgorithm(algorithm);
+		}
+		if (randomSaltGeneratorAlgorithm != null) {
+			encryptor.setSaltGenerator(new RandomSaltGenerator(randomSaltGeneratorAlgorithm));
+		}
+		if (randomIvGeneratorAlgorithm != null) {
+			encryptor.setIvGenerator(new RandomIvGenerator(randomIvGeneratorAlgorithm));
+		}
+		if ("encrypt".equals(command)) {
+			System.out.println("Encrypted text: " + encryptor.encrypt(input));
+		} else {
+			System.out.println("Decrypted text: " + encryptor.decrypt(input));
+		}
+	}
 
-        encryptor.setPassword(password);
-        if (algorithm != null) {
-            encryptor.setAlgorithm(algorithm);
-        }
-        if (randomSaltGeneratorAlgorithm != null) {
-            encryptor.setSaltGenerator(new RandomSaltGenerator(randomSaltGeneratorAlgorithm));
-        }
-        if (randomIvGeneratorAlgorithm != null) {
-            encryptor.setIvGenerator(new RandomIvGenerator(randomIvGeneratorAlgorithm));
-        }
-        if ("encrypt".equals(command)) {
-            System.out.println("Encrypted text: " + encryptor.encrypt(input));
-        } else {
-            System.out.println("Decrypted text: " + encryptor.decrypt(input));
-        }
-    }
-
-    /**
-     * The main entrypoint is required as the Jasypt dependency can be also used as CLI.
-     *
-     * @param  args      the main arguments
-     * @throws Exception throws any processing exception
-     */
-    public static void main(String[] args) throws Exception {
-        Main main = new Main();
-        if (args.length == 0) {
-            main.showOptions();
-            return;
-        } else {
-            main.run(args);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		Main main = new Main();
+		if (args.length == 0) {
+			main.showOptions();
+			return;
+		} else {
+			main.run(args);
+		}
+	}
 
 }
