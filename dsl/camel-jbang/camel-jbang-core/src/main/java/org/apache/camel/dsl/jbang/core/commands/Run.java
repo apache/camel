6d1e540912b38e9809e2916718c0e99a04bf5ef2
Salvatore Mongiardo <smongiar@redhat.com>
2025-10-20 16:57:46 +0200

    JBang - Unsupported version fix, when cli 'run/debug/export' command is executed over jdk 17

--- dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/Run.java
+++ dsl/camel-jbang/camel-jbang-core/src/main/java/org/apache/camel/dsl/jbang/core/commands/Run.java
@@ -162,6 +162,9 @@ public class Run extends CamelCommand {
     @Option(names = { "--empty" }, defaultValue = "false", description = "Run an empty Camel without loading source files")
     public boolean empty;
 
+    @CommandLine.Option(names = { "--java-version" }, description = "Java version", defaultValue = "21")
+    protected String javaVersion = "21";
+
     @Option(names = { "--camel-version" }, description = "To run using a different Camel version than the default version.")
     String camelVersion;
 
@@ -663,6 +666,7 @@ public class Run extends CamelCommand {
         writeSetting(main, profileProperties, QUARKUS_VERSION, quarkusVersion);
         writeSetting(main, profileProperties, QUARKUS_GROUP_ID, quarkusGroupId);
         writeSetting(main, profileProperties, QUARKUS_ARTIFACT_ID, quarkusArtifactId);
+        writeSetting(main, profileProperties, JAVA_VERSION, javaVersion);
 
         // command line arguments
         if (property != null) {
@@ -1079,6 +1083,7 @@ public class Run extends CamelCommand {
         eq.quarkusGroupId = PropertyResolver.fromSystemProperty(QUARKUS_GROUP_ID, () -> this.quarkusGroupId);
         eq.quarkusArtifactId = PropertyResolver.fromSystemProperty(QUARKUS_ARTIFACT_ID, () -> this.quarkusArtifactId);
         eq.camelVersion = this.camelVersion;
+        eq.javaVersion = this.javaVersion;
         eq.kameletsVersion = this.kameletsVersion;
         eq.exportDir = runDirPath.toString();
         eq.localKameletDir = this.localKameletDir;
@@ -1364,6 +1369,7 @@ public class Run extends CamelCommand {
             camelVersion = answer.getProperty(CAMEL_VERSION, camelVersion);
             kameletsVersion = answer.getProperty(KAMELETS_VERSION, kameletsVersion);
             springBootVersion = answer.getProperty(SPRING_BOOT_VERSION, springBootVersion);
+            javaVersion = answer.getProperty(JAVA_VERSION, javaVersion);
             quarkusGroupId = answer.getProperty(QUARKUS_GROUP_ID, quarkusGroupId);
             quarkusArtifactId = answer.getProperty(QUARKUS_ARTIFACT_ID, quarkusArtifactId);
             quarkusVersion = answer.getProperty(QUARKUS_VERSION, quarkusVersion);
@@ -1439,6 +1445,9 @@ public class Run extends CamelCommand {
             cmds.removeIf(arg -> arg.startsWith("--jvm-debug"));
         }
 
+        if (javaVersion != null) {
+            jbangArgs.add("--java-version=" + javaVersion);
+        }
         if (repositories != null) {
             jbangArgs.add("--repos=" + repositories);
         }
