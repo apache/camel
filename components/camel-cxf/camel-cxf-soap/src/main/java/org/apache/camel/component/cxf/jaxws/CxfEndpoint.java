14931283b30d6e01773386969cd00a9ffed60eb7
Claus Ibsen <claus.ibsen@gmail.com>
2025-11-07 13:16:48 +0100

    CAMEL-22670: camel-jbang - Using CXF should automatic download HTTP transport if needed. This makes it easier when using camel-jbang. (#19849)
    * CAMEL-22670: camel-jbang - Using CXF should automatic download HTTP transport if needed. This makes it easier when using camel-jbang.

--- components/camel-cxf/camel-cxf-soap/src/main/java/org/apache/camel/component/cxf/jaxws/CxfEndpoint.java
+++ components/camel-cxf/camel-cxf-soap/src/main/java/org/apache/camel/component/cxf/jaxws/CxfEndpoint.java
@@ -118,6 +118,7 @@ import org.apache.cxf.service.model.BindingOperationInfo;
 import org.apache.cxf.service.model.MessagePartInfo;
 import org.apache.cxf.staxutils.StaxSource;
 import org.apache.cxf.staxutils.StaxUtils;
+import org.apache.cxf.transport.http.HttpDestinationFactory;
 import org.apache.cxf.wsdl.WSDLManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -357,6 +358,13 @@ public class CxfEndpoint extends DefaultEndpoint implements AsyncEndpoint, Heade
 
         sfb.setBus(getBus());
         sfb.setStart(false);
+
+        // add custom http destination factories
+        var factories = getCamelContext().getRegistry().findByType(HttpDestinationFactory.class);
+        for (var factory : factories) {
+            sfb.getBus().setExtension(factory, HttpDestinationFactory.class);
+        }
+
         getNullSafeCxfConfigurer().configure(sfb);
     }
 
---
+++ dsl/camel-kamelet-main/src/main/resources/auto-configure/camel-cxf-soap.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// check if we already have CXF transport on classpath
+org.apache.camel.main.download.MavenDependencyDownloader downloader = context.hasService(org.apache.camel.main.download.MavenDependencyDownloader.class);
+final var cxfVersion = org.apache.camel.main.download.CamelDependenciesHelper.dependencyVersion(context, "cxf-version");
+if (cxfVersion == null) {
+    return null;
+}
+boolean jetty = downloader.alreadyOnClasspath("org.apache.cxf", "cxf-rt-transports-http-jetty", cxfVersion);
+boolean undertow = downloader.alreadyOnClasspath("org.apache.cxf", "cxf-rt-transports-http-undertow", cxfVersion);
+if (jetty || undertow){
+    return null;
+}
+
+// automatic download cxf transport for undertow and register this with CXF
+context.getRegistry().bind("camelCxfUndertowHttpTransportDownloader", new org.apache.cxf.transport.http.HttpDestinationFactory() {
+    public org.apache.cxf.transport.http.AbstractHTTPDestination createDestination(org.apache.cxf.service.model.EndpointInfo endpointInfo, org.apache.cxf.Bus bus, org.apache.cxf.transport.http.DestinationRegistry registry) throws java.io.IOException {
+        downloader.downloadDependency("org.apache.cxf", "cxf-rt-transports-http-undertow", cxfVersion);
+        try {
+            Class c = context.getClassResolver().resolveClass("org.apache.cxf.transport.http_undertow.UndertowHTTPServerEngineFactory");
+            Object o = c.getConstructor(org.apache.cxf.Bus.class).newInstance(bus);
+            Class c2 = context.getClassResolver().resolveClass("org.apache.cxf.transport.http_undertow.UndertowHTTPDestination");
+            Object o2 = c2.getConstructor(org.apache.cxf.Bus.class, org.apache.cxf.transport.http.DestinationRegistry.class, org.apache.cxf.service.model.EndpointInfo.class, c)
+                    .newInstance(bus, registry, endpointInfo, o);
+            return (org.apache.cxf.transport.http.AbstractHTTPDestination) o2;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+});
+
+return "Camel JBang will automatic download org.apache.cxf:cxf-rt-transports-http-undertow:" + cxfVersion + " if needed by camel-cxf-soap";
