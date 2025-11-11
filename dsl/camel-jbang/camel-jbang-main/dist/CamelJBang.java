ef3dc3fab7c8ca87f307e85358b3f738468f369b
Andrea Cosentino <ancosen@gmail.com>
2025-11-11 11:32:36 +0100

    CAMEL-22683 - camel-jbang - Upgrade dist to 4.16.0 (#19887)
    Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

--- dsl/camel-jbang/camel-jbang-main/dist/CamelJBang.java
+++ dsl/camel-jbang/camel-jbang-main/dist/CamelJBang.java
@@ -19,9 +19,9 @@
 
 //JAVA 17+
 //REPOS central=https://repo1.maven.org/maven2,apache-snapshot=https://repository.apache.org/content/groups/snapshots/
-//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.15.0}@pom
-//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.15.0}
-//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.15.0}
+//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.16.0}@pom
+//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.16.0}
+//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.16.0}
 
 package main;
 
