0c0283ffa4a9a25e0ede57b231e7a45781d1851c
Tom Cunningham <tcunning@redhat.com>
2025-11-21 14:57:43 -0500

    API changes for spring-batch upgrade to 6.0.0

--- components/camel-spring-parent/camel-spring-batch/src/test/java/org/apache/camel/component/spring/batch/support/CamelJobExecutionListenerTest.java
+++ components/camel-spring-parent/camel-spring-batch/src/test/java/org/apache/camel/component/spring/batch/support/CamelJobExecutionListenerTest.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.job.JobExecution;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
