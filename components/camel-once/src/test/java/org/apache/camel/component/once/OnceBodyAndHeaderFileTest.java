20673d697a4f11d064a306965f18a3e7ccb37e71
Claus Ibsen <claus.ibsen@gmail.com>
2025-12-18 09:59:30 +0100

    CAMEL-22431: camel-once - A component for development to trigger only once

---
+++ components/camel-once/src/test/java/org/apache/camel/component/once/OnceBodyAndHeaderFileTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.component.once;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+public class OnceBodyAndHeaderFileTest extends CamelTestSupport {
+
+    @Test
+    public void testOnce() throws Exception {
+        getMockEndpoint("mock:result").expectedBodiesReceived("1,jack", "2,moe");
+        getMockEndpoint("mock:result").expectedHeaderReceived("foo", "abc");
+        getMockEndpoint("mock:result").expectedHeaderReceived("bar", "456");
+
+        MockEndpoint.assertIsSatisfied(context);
+    }
+
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("once:tick?delay=-1&body=file:src/test/resources/data.csv&header.foo=abc&header.bar=classpath:price.txt")
+                        .split(body().tokenize("\n"))
+                        .to("mock:result");
+            }
+        };
+    }
+}
