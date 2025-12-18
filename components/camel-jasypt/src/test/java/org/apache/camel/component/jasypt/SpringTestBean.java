ecd5a0847cc04ea55446a5eba5c9ba83bd0a6b5e
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2025-12-18 09:44:17 +0100

    chore(components): jasypt test should not have *Test classes without tests

--- components/camel-jasypt/src/test/java/org/apache/camel/component/jasypt/SpringTestBean.java
+++ components/camel-jasypt/src/test/java/org/apache/camel/component/jasypt/SpringTestBean.java
@@ -20,7 +20,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringTest {
+public class SpringTestBean {
 
     // defaulted and overwritten in myproperties.properties
     @Value("${available.prop:no Data}")
