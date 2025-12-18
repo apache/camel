f88fa02794a8d8761daab78f650119f9978e1a64
Claus Ibsen <claus.ibsen@gmail.com>
2025-12-18 22:35:34 +0100

    Fixed test

--- dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/version/VersionListTest.java
+++ dsl/camel-jbang/camel-jbang-core/src/test/java/org/apache/camel/dsl/jbang/core/commands/version/VersionListTest.java
@@ -38,7 +38,7 @@ class VersionListTest extends CamelCommandBaseTest {
         List<String> lines = printer.getLines();
         // there was a change where the information is stored in 4.15, thus the test on 4.14.1 and 4.15.0
         Assertions.assertThat(lines.stream().collect(Collectors.joining("\n")))
-                .contains("4.14.1         3.5.6     17,21   LTS")
+                .contains("4.14.1         3.5.6     17,21  LTS")
                 .contains("4.15.0         3.5.6     17,21");
     }
 
@@ -52,7 +52,7 @@ class VersionListTest extends CamelCommandBaseTest {
 
         List<String> lines = printer.getLines();
         Assertions.assertThat(lines.stream().collect(Collectors.joining("\n")))
-                .contains("4.14.0       3.27.0    17,21   LTS");
+                .contains("4.14.0       3.27.0    17,21  LTS");
     }
 
 }
