67463808361607587fea70e16f381132517769ab
Otavio Rodolfo Piske <angusyoung@gmail.com>
2026-01-26 10:44:49 +0000

    (chores): modernize instanceof checks in camel-service

--- components/camel-service/src/main/java/org/apache/camel/component/service/ServiceConsumer.java
+++ components/camel-service/src/main/java/org/apache/camel/component/service/ServiceConsumer.java
@@ -65,8 +65,8 @@ public class ServiceConsumer extends DefaultConsumer {
 
         // start delegate
         delegatedConsumer = delegatedEndpoint.createConsumer(processor);
-        if (delegatedConsumer instanceof StartupListener) {
-            getEndpoint().getCamelContext().addStartupListener((StartupListener) delegatedConsumer);
+        if (delegatedConsumer instanceof StartupListener startuplistener) {
+            getEndpoint().getCamelContext().addStartupListener(startuplistener);
         }
 
         ServiceHelper.startService(delegatedEndpoint);
@@ -92,28 +92,28 @@ public class ServiceConsumer extends DefaultConsumer {
 
     @Override
     protected void doResume() throws Exception {
-        if (delegatedConsumer instanceof SuspendableService) {
+        if (delegatedConsumer instanceof SuspendableService suspendableService) {
             final ServiceEndpoint endpoint = (ServiceEndpoint) getEndpoint();
             final ServiceDefinition definition = endpoint.getServiceDefinition();
 
             // register service
             serviceRegistry.register(definition);
 
-            ((SuspendableService) delegatedConsumer).resume();
+            suspendableService.resume();
         }
         super.doResume();
     }
 
     @Override
     protected void doSuspend() throws Exception {
-        if (delegatedConsumer instanceof SuspendableService) {
+        if (delegatedConsumer instanceof SuspendableService suspendableservice) {
             final ServiceEndpoint endpoint = (ServiceEndpoint) getEndpoint();
             final ServiceDefinition definition = endpoint.getServiceDefinition();
 
             // de-register service
             serviceRegistry.deregister(definition);
 
-            ((SuspendableService) delegatedConsumer).suspend();
+            suspendableservice.suspend();
         }
         super.doSuspend();
     }
