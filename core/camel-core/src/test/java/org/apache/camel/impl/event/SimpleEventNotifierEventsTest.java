1cb1cd68da2dea5f41a41fa3473bf64c787550eb
Guillaume Nodet <gnodet@gmail.com>
2026-03-06 15:35:41 +0100

    CAMEL-23079: Fix import order in SimpleEventNotifierEventsTest

--- core/camel-core/src/test/java/org/apache/camel/impl/event/SimpleEventNotifierEventsTest.java
+++ core/camel-core/src/test/java/org/apache/camel/impl/event/SimpleEventNotifierEventsTest.java
@@ -32,8 +32,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.apache.camel.TestSupport.assertIsInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleEventNotifierEventsTest {
 
