5e448b7280f1c8e5833b5832f19442cefea17f6b
Claus Ibsen <claus.ibsen@gmail.com>
2026-03-09 16:41:28 +0100

    CAMEL-23160: camel-core - Add type converter for camel-util-json

---
+++ core/camel-support/src/main/java/org/apache/camel/converter/json/JsonConverter.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.converter.json;
+
+import org.apache.camel.Converter;
+import org.apache.camel.Exchange;
+import org.apache.camel.util.json.JsonArray;
+import org.apache.camel.util.json.JsonObject;
+import org.apache.camel.util.json.Jsoner;
+
+/**
+ * A set of {@link Converter} methods for camel-util-json objects.
+ */
+@Converter(generateBulkLoader = true)
+public final class JsonConverter {
+
+    /**
+     * Utility classes should not have a public constructor.
+     */
+    private JsonConverter() {
+    }
+
+    @Converter(order = 1)
+    public static JsonObject convertToJsonObject(String json, Exchange exchange) throws Exception {
+        return Jsoner.deserialize(json, (JsonObject) null);
+    }
+
+    @Converter(order = 2)
+    public static JsonArray convertToJsonArray(String json, Exchange exchange) throws Exception {
+        return (JsonArray) Jsoner.deserialize(json);
+    }
+
+}
