f18cb2de5d62320402b0523b0878c6b00f1027db
Claus Ibsen <claus.ibsen@gmail.com>
2026-05-05 14:35:48 +0200

    CAMEL-23416: camel-jbang - Add route diagram web console to show route diagram when using --console

---
+++ dsl/camel-jbang/camel-jbang-console/src/main/java/org/apache/camel/jbang/console/DiagramDevConsole.java
@@ -0,0 +1,139 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.jbang.console;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+
+import javax.imageio.ImageIO;
+
+import org.apache.camel.console.DevConsoleRegistry;
+import org.apache.camel.diagram.RouteDiagramHelper;
+import org.apache.camel.diagram.RouteDiagramLayoutEngine;
+import org.apache.camel.diagram.RouteDiagramRenderer;
+import org.apache.camel.spi.annotations.DevConsole;
+import org.apache.camel.support.console.AbstractDevConsole;
+import org.apache.camel.util.IOHelper;
+import org.apache.camel.util.json.JsonArray;
+import org.apache.camel.util.json.JsonObject;
+
+@DevConsole(name = "route-diagram", group = "camel-jbang", displayName = "Route Diagram", description = "Visual route diagrams")
+public class DiagramDevConsole extends AbstractDevConsole {
+
+    /**
+     * Theme to use: dark, light, or text
+     */
+    public static final String THEME = "theme";
+
+    public DiagramDevConsole() {
+        super("camel-jbang", "route-diagram", "Route Diagram", "Visual route diagrams");
+    }
+
+    @Override
+    protected String doCallText(Map<String, Object> options) {
+        final StringJoiner sj = new StringJoiner("\n");
+
+        String theme = (String) options.get(THEME);
+
+        org.apache.camel.console.DevConsole dc
+                = getCamelContext().getCamelContextExtension().getContextPlugin(DevConsoleRegistry.class)
+                        .resolveById("route-structure");
+        JsonObject root = (JsonObject) dc.call(MediaType.JSON);
+        // parseRoutes expect a JsonArray and not ArrayList
+        JsonArray arr = new JsonArray(root.getCollection("routes"));
+        root.put("routes", arr);
+        var routes = RouteDiagramHelper.parseRoutes(root);
+        if ("text".equals(theme)) {
+            RouteDiagramRenderer renderer = new RouteDiagramRenderer();
+            var lines = renderer.printTextDiagram(routes);
+            lines.forEach(sj::add);
+            sj.add("");
+        } else {
+            try {
+                BufferedImage image = renderImage(routes, theme);
+                String base64 = imageToBase64(image, "png");
+                // For HTML embedding:
+                String html = "<html>%n<body>%n<img src=\"data:image/png;base64,%s\" alt=\"Route Diagram\"%n</body>%n</html>>"
+                        .formatted(base64);
+                sj.add(html);
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+        return sj.toString();
+    }
+
+    @Override
+    protected Map<String, Object> doCallJson(Map<String, Object> options) {
+        String theme = (String) options.get(THEME);
+
+        org.apache.camel.console.DevConsole dc
+                = getCamelContext().getCamelContextExtension().getContextPlugin(DevConsoleRegistry.class)
+                        .resolveById("route-structure");
+        JsonObject root = (JsonObject) dc.call(MediaType.JSON);
+        // parseRoutes expect a JsonArray and not ArrayList
+        JsonArray arr = new JsonArray(root.getCollection("routes"));
+        root.put("routes", arr);
+        var routes = RouteDiagramHelper.parseRoutes(root);
+
+        root = new JsonObject();
+        try {
+            BufferedImage image = renderImage(routes, theme);
+            String base64 = imageToBase64(image, "png");
+            root.put("image", base64);
+        } catch (Exception e) {
+            // ignore
+        }
+
+        return root;
+    }
+
+    private static BufferedImage renderImage(List<RouteDiagramLayoutEngine.RouteInfo> routes, String theme) {
+        RouteDiagramRenderer renderer = new RouteDiagramRenderer();
+        RouteDiagramLayoutEngine engine = new RouteDiagramLayoutEngine();
+
+        List<RouteDiagramLayoutEngine.LayoutRoute> layoutRoutes = new ArrayList<>();
+        int currentY = RouteDiagramLayoutEngine.PADDING;
+        for (RouteDiagramLayoutEngine.RouteInfo route : routes) {
+            RouteDiagramLayoutEngine.LayoutRoute lr = engine.layoutRoute(route, currentY);
+            layoutRoutes.add(lr);
+            currentY = lr.maxY + RouteDiagramLayoutEngine.V_GAP;
+        }
+        if (theme == null) {
+            theme = "transparent";
+        }
+        RouteDiagramRenderer.DiagramColors colors = RouteDiagramRenderer.DiagramColors.parse(theme);
+        return renderer.renderDiagram(layoutRoutes, currentY, colors);
+    }
+
+    private static String imageToBase64(BufferedImage image, String format) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(image, format, baos);
+        baos.flush();
+        try {
+            byte[] imageBytes = baos.toByteArray();
+            return Base64.getEncoder().encodeToString(imageBytes);
+        } finally {
+            IOHelper.close(baos);
+        }
+    }
+}
