5d5533849d558e7df3921b1de032ab1e10a72714
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2026-05-07 09:42:53 +0200

    chore(components): telemetry dev  core processors
    Ref CAMEL-23432

--- components/camel-telemetry-dev/src/test/java/org/apache/camel/telemetrydev/AsyncTest.java
+++ components/camel-telemetry-dev/src/test/java/org/apache/camel/telemetrydev/AsyncTest.java
@@ -41,6 +41,7 @@ public class AsyncTest extends TelemetryDevTracerTestSupport {
     protected CamelContext createCamelContext() throws Exception {
         TelemetryDevTracer tst = new TelemetryDevTracer();
         tst.setTraceFormat("json");
+        tst.setDisableCoreProcessors(true);
         CamelContext context = super.createCamelContext();
         CamelContextAware.trySetCamelContext(tst, context);
         tst.init(context);
