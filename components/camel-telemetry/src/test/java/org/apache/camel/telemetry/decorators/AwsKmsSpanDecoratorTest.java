b45e9544c95d5a83efc4ee7f4ebf0caf876b5010
Andrea Cosentino <ancosen@gmail.com>
2026-05-08 09:01:49 +0200

    CAMEL-23387: camel-telemetry - Add span decorators for AWS Athena, CloudWatch, KMS, MSK, Step Functions, Timestream, Redshift Data and CloudTrail (#23045)
    Third batch of AWS span decorators for camel-telemetry. Adds AbstractSpanDecorator
    implementations for aws2-athena, aws2-cw, aws2-kms, aws2-msk, aws2-step-functions,
    aws2-timestream, aws2-redshift-data; plus an AbstractMessagingSpanDecorator for the
    aws-cloudtrail consumer. Database/analytics decorators (Athena, Timestream, Redshift
    Data) set semantic-conventions tags db.system and db.name for uniform aggregation
    alongside DDB/JDBC/SQL/Mongo/Cassandra spans. Header constants are mirrored from each
    component's *Constants interface to avoid hard module dependencies, matching the
    convention from #23038 and #23040. After review, dropped keyId from KMS and username
    from CloudTrail to keep the trace-tag surface minimal. All 8 decorators registered in
    META-INF/services and covered by unit tests.
    
    CAMEL-23387 stays open; further AWS components will land in subsequent PRs.

---
+++ components/camel-telemetry/src/test/java/org/apache/camel/telemetry/decorators/AwsKmsSpanDecoratorTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.telemetry.decorators;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.telemetry.mock.MockSpanAdapter;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AwsKmsSpanDecoratorTest {
+
+    @Test
+    public void testPre() {
+        String operation = "createKey";
+        String keyArn = "arn:aws:kms:us-east-1:123456789012:key/1234abcd-12ab-34cd-56ef-1234567890ab";
+        String keyState = "Enabled";
+
+        Endpoint endpoint = Mockito.mock(Endpoint.class);
+        Exchange exchange = Mockito.mock(Exchange.class);
+        Message message = Mockito.mock(Message.class);
+
+        Mockito.when(endpoint.getEndpointUri()).thenReturn("aws2-kms:default");
+        Mockito.when(exchange.getIn()).thenReturn(message);
+        Mockito.when(exchange.getExchangeId()).thenReturn("exchange-1");
+        Mockito.when(message.getHeader(AwsKmsSpanDecorator.OPERATION, String.class)).thenReturn(operation);
+        Mockito.when(message.getHeader(AwsKmsSpanDecorator.KEY_ARN, String.class)).thenReturn(keyArn);
+        Mockito.when(message.getHeader(AwsKmsSpanDecorator.KEY_STATE, String.class)).thenReturn(keyState);
+
+        AbstractSpanDecorator decorator = new AwsKmsSpanDecorator();
+
+        MockSpanAdapter span = new MockSpanAdapter();
+
+        decorator.beforeTracingEvent(span, exchange, endpoint);
+
+        assertEquals(operation, span.tags().get(AwsKmsSpanDecorator.KMS_OPERATION));
+        assertEquals(keyArn, span.tags().get(AwsKmsSpanDecorator.KMS_KEY_ARN));
+        assertEquals(keyState, span.tags().get(AwsKmsSpanDecorator.KMS_KEY_STATE));
+    }
+
+}
