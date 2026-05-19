b36b99c59362c4d7dfe112144991d3f53318b3f7
Aurélien Pupier <apupier@ibm.com>
2026-05-19 15:23:58 +0200

    CAMEL-23499 - Disable again several FileToFtps* tests
    Several FTP tests were re-enabled recently with
    https://github.com/apache/camel/pull/23078 but unfortunately several are
    broken. They always fail on CI or when launching from Maven locally (at
    least for me)
    Also note that some of them are disabled in case the JVM does not
    support specific algorithm, which means that they are not raising an
    error in this case.
    
    Signed-off-by: Aurélien Pupier <apupier@ibm.com>

--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/integration/FileToFtpsExplicitSSLWithClientAuthAndSSLContextParametersIT.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/integration/FileToFtpsExplicitSSLWithClientAuthAndSSLContextParametersIT.java
@@ -21,9 +21,11 @@ import org.apache.camel.support.jsse.KeyManagersParameters;
 import org.apache.camel.support.jsse.KeyStoreParameters;
 import org.apache.camel.support.jsse.SSLContextParameters;
 import org.apache.camel.support.jsse.TrustManagersParameters;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Disabled("Test was flaky 4 years ago. It is now completely broken when launched with Maven. See CAMEL-23499")
 public class FileToFtpsExplicitSSLWithClientAuthAndSSLContextParametersIT extends FileToFtpsExplicitSSLWithClientAuthIT {
 
     @BindToRegistry("sslContextParameters")
