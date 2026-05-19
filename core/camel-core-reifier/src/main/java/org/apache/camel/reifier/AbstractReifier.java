828531b93f595437a8cff2813d7a19a292680084
Tom Cunningham <tcunning@redhat.com>
2026-05-19 09:41:33 -0400

    CAMEL-23550 Add test cases for local-only bean lookup and local/global bean conflict

--- core/camel-core-reifier/src/main/java/org/apache/camel/reifier/AbstractReifier.java
+++ core/camel-core-reifier/src/main/java/org/apache/camel/reifier/AbstractReifier.java
@@ -25,11 +25,14 @@ import java.util.Set;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Endpoint;
 import org.apache.camel.Expression;
+import org.apache.camel.NamedNode;
 import org.apache.camel.NoSuchBeanException;
 import org.apache.camel.NoSuchEndpointException;
 import org.apache.camel.Predicate;
 import org.apache.camel.Route;
+import org.apache.camel.RouteTemplateContext;
 import org.apache.camel.model.ExpressionSubElementDefinition;
+import org.apache.camel.model.RouteDefinition;
 import org.apache.camel.model.language.ExpressionDefinition;
 import org.apache.camel.reifier.language.ExpressionReifier;
 import org.apache.camel.spi.BeanRepository;
@@ -210,10 +213,9 @@ public abstract class AbstractReifier implements BeanRepository {
         }
         if (answer == null && route != null) {
             // check local bean repository from route template context first
-            org.apache.camel.NamedNode routeNode = route.getRoute();
-            if (routeNode instanceof org.apache.camel.model.RouteDefinition) {
-                org.apache.camel.model.RouteDefinition routeDef = (org.apache.camel.model.RouteDefinition) routeNode;
-                org.apache.camel.RouteTemplateContext rtc = routeDef.getRouteTemplateContext();
+            NamedNode routeNode = route.getRoute();
+            if (routeNode instanceof RouteDefinition routeDef) {
+                RouteTemplateContext rtc = routeDef.getRouteTemplateContext();
                 if (rtc != null) {
                     BeanRepository localRepo = rtc.getLocalBeanRepository();
                     if (localRepo != null) {
